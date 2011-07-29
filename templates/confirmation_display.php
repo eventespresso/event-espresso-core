7793331ad13987e622a12e84b66c4f758df5d2ea
Seth Shoultes <seth@eventespresso.com>
2011-07-29 19:18:12 +0000

    Added the attendee_num to the edit url so that the "Personal information only" will work for additional attendees.

--- templates/confirmation_display.php
+++ templates/confirmation_display.php
@@ -4,6 +4,7 @@
 
 //This page gets all of the varaibles from includes/process-registration/payment_page.php
 //Payment confirmation block
+$attendee_num = 1;
 ?>
 <form id="form1" name="form1" method="post" action="<?php echo home_url()?>/?page_id=<?php echo $event_page_id?>">
             <p align="left"><strong>
@@ -26,7 +27,7 @@
                 <td><strong class="event_espresso_name">
                   <?php _e('Attendee Name:','event_espresso'); ?>
                   </strong></td>
-                <td  valign="top"><span class="event_espresso_value"><?php echo stripslashes_deep($attendee_name)?> (<?php echo $attendee_email?>) <?php echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$attendee_id.'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;p_id='.$p_id.'&amp;coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'">'. __('Edit', 'event_espresso').'</a>'; ?>
+                <td  valign="top"><span class="event_espresso_value"><?php echo stripslashes_deep($attendee_name)?> (<?php echo $attendee_email?>) <?php echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$attendee_id.'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;p_id='.$p_id.'&amp;coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'&amp;attendee_num='.$attendee_num.'">'. __('Edit', 'event_espresso').'</a>'; ?>
 				<?php
 				//Create additional attendees
 				$sql = "SELECT * FROM " . EVENTS_ATTENDEE_TABLE;
@@ -35,14 +36,16 @@
 				$x_attendees = $wpdb->get_results( $sql, ARRAY_A );
 				if ( $wpdb->num_rows > 0 ){
 					foreach ($x_attendees as $x_attendee){
+						$attendee_num++;
+						//echo $attendee_num;
 						//print_r($x_attendees);
 						echo "<br/>" . $x_attendee['fname'] . " " . $x_attendee['lname'] . " ";
 						if ($x_attendee['email'] != '') { echo "(" . $x_attendee['email']  . ") "; }
 
 						//Create edit link
-						echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;p_id='.$p_id.'&amp;coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'">'. __('Edit', 'event_espresso').'</a>';
+						echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;p_id='.$p_id.'&amp;coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'&amp;attendee_num='.$attendee_num.'">'. __('Edit', 'event_espresso').'</a>';
 						//Create delete link
-						echo ' | <a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;delete_attendee=true&amp;p_id='.$p_id.'&amp;coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'">'. __('Delete', 'event_espresso').'</a>';
+						echo ' | <a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;delete_attendee=true&amp;p_id='.$p_id.'&amp;coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'">'. __('Delete', 'event_espresso').'</a>';						
 					}
 				}
 				?>
