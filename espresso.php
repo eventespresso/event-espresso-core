34b58378ec2c6061d2b1001606fdb29ca7c1c517
Seth Shoultes <seth@eventespresso.com>
2011-08-03 07:31:16 +0000

    Changed version

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 3.0.19.P.56
+  Version: 3.0.19.P.57
 
   Author: Seth Shoultes
   Author URI: http://www.eventespresso.com
@@ -30,7 +30,7 @@
 
 //Define the version of the plugin
 function espresso_version() {
-    return '3.0.19.P.56';
+    return '3.0.19.P.57';
 }
 
 function ee_init_session() {
