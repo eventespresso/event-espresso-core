2628a4c216571007c94f6fc52cf48a5b9573528b
Seth Shoultes <seth@eventespresso.com>
2011-08-03 04:11:57 +0000

    Added Total Registrations/Transactions reports and altered some of the layout.

--- includes/admin-reports/charts.php
+++ includes/admin-reports/charts.php
@@ -33,9 +33,81 @@ function espresso_charts() {
         font-size: 0.8em;
     }
   </style>
+      <!-- Total Registrations/Transactions -->
+    <?php 
+  $retVAl_a = array();
+  $sql_a = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
+  $results_a = $wpdb->get_results($sql_a);
+  foreach ($results_a as $row_a) {
+	$retVal_a[] = $row_a;
+  }
+ 
+  $attendees_a = '';
+  $amount_a ='';
+  $date_a = '';
+  foreach($retVal_a as $rec_a ){
+		//$label = $rec->amount." (".$rec->date.")";
+		//$vals[$label] = $rec->date;
+		$amount_a .= $rec_a->amount.',';
+		$date_a .= "'".$rec_a->date."', ";
+		$attendees_a .= $rec_a->quantity.", ";
+	}
+ //echo "<pre>".print_r($retVal_a,true)."</pre>";
+  ?>
+    <script>
+ jQuery(document).ready(function() {
+        var line1 = [<?php echo $attendees_a ?>];//bottom column
+		var line2 = [<?php echo $amount_a ?>];
+        var ticks = [<?php echo $date_a ?>];
+        
+        plot1 = jQuery.jqplot('total_reg', [line1, line2], {
+            //stackSeries: true,
+			title: '<?php _e('Total Registrations/Transactions', 'event_espresso'); ?>',
+			seriesDefaults:{
+				renderer:jQuery.jqplot.BarRenderer,
+                pointLabels: { show: true },
+            },
+            axes: {
+                xaxis: {
+                    renderer: jQuery.jqplot.CategoryAxisRenderer,
+                    ticks: ticks
+                }
+            },
+			series: [{
+				label: '# Attendees'
+			},
+			{
+				label: '<?php echo $org_options['currency_symbol'] ?> <?php _e('Amount', 'event_espresso'); ?>',
+				pointLabels: { formatString:'<?php echo utf8_encode(html_entity_decode($org_options['currency_symbol'])); ?>%.2f' },
+			}],
+
+			   highlighter: {
+				show: true,
+				sizeAdjust: 7.5
+			  },
+			legend: {
+				show: true,
+				location: 'ne',     // compass direction, nw, n, ne, e, se, s, sw, w.
+				placement: 'outsideGrid'
+
+			},
+
+        });
+		
+	
+    
+        jQuery('#chart1').bind('jqplotDataClick', 
+            function (ev, seriesIndex, pointIndex, data) {
+                jQuery('#info1').html('series: '+seriesIndex+', point: '+pointIndex+', data: '+data);
+            }
+        );
+    });</script>
+    
+
+   <!-- Completed Registrations/Transactions -->
 <?php 
   $retVAl = array();
-  $sql = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d %Y') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." AND payment_status='Completed' GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
+  $sql = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." AND payment_status='Completed' GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
   $results = $wpdb->get_results($sql);
   foreach ($results as $row) {
 	$retVal[] = $row;
@@ -59,13 +131,12 @@ function espresso_charts() {
 		var line2 = [<?php echo $amount ?>];
         var ticks = [<?php echo $date ?>];
         
-        plot1 = jQuery.jqplot('chart1', [line1, line2], {
+        plot1 = jQuery.jqplot('total_completed', [line1, line2], {
             //stackSeries: true,
-			title: 'Completed Transactions',
+			title: '<?php _e('Completed Registrations/Transactions', 'event_espresso'); ?>',
 			seriesDefaults:{
 				renderer:jQuery.jqplot.BarRenderer,
                 pointLabels: { show: true },
-				//label: ' $ Amount'
             },
             axes: {
                 xaxis: {
@@ -77,7 +148,8 @@ function espresso_charts() {
 				label: '# Attendees'
 			},
 			{
-				label: 'Total <?php echo $org_options['currency_symbol'] ?>'
+				label: '<?php echo $org_options['currency_symbol'] ?> <?php _e('Amount', 'event_espresso'); ?>',
+				pointLabels: { formatString:'<?php echo utf8_encode(html_entity_decode($org_options['currency_symbol'])); ?>%.2f' },
 			}],
 
             highlighter: { show: false },
@@ -98,10 +170,11 @@ function espresso_charts() {
             }
         );
     });</script>
-    
+  
+  <!-- Pending Registrations/Transactions -->
     <?php 
   $retVAl_p = array();
-  $sql_p = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d %Y') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." AND payment_status='Pending' GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
+  $sql_p = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." AND payment_status='Pending' GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
   $results_p = $wpdb->get_results($sql_p);
   foreach ($results_p as $row_p) {
 	$retVal_p[] = $row_p;
@@ -125,13 +198,13 @@ function espresso_charts() {
 		var line2 = [<?php echo $amount_p ?>];
         var ticks = [<?php echo $date_p ?>];
         
-        plot1 = jQuery.jqplot('chart2', [line1, line2], {
+        plot1 = jQuery.jqplot('total_pending', [line1, line2], {
             //stackSeries: true,
-			title: 'Pending Transactions',
+			title: '<?php _e('Pending Registrations/Transactions', 'event_espresso'); ?>',
 			seriesDefaults:{
 				renderer:jQuery.jqplot.BarRenderer,
-                pointLabels: { show: true },
-				//label: ' $ Amount'
+                pointLabels: { show: true},
+				
             },
             axes: {
                 xaxis: {
@@ -143,7 +216,8 @@ function espresso_charts() {
 				label: '# Attendees'
 			},
 			{
-				label: 'Total <?php echo $org_options['currency_symbol'] ?>'
+				label: '<?php echo $org_options['currency_symbol'] ?> <?php _e('Amount', 'event_espresso'); ?>',
+				pointLabels: { formatString:'<?php echo utf8_encode(html_entity_decode($org_options['currency_symbol'])); ?>%.2f' },
 			}],
 
             highlighter: { show: false },
@@ -164,10 +238,11 @@ function espresso_charts() {
             }
         );
     });</script>
-    
+  
+  <!-- Incomplete Registrations/Transactions -->  
     <?php 
   $retVAl_i = array();
-  $sql_i = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d %Y') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." AND (payment_status='Incomplete' OR payment_status='Payment Declined') GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
+  $sql_i = "SELECT SUM(a.amount_pd) amount, SUM(a.quantity) quantity, DATE_FORMAT(a.date,'%b %d') date FROM wp_events_attendee a WHERE event_id =".$_REQUEST['event_id']." AND (payment_status='Incomplete' OR payment_status='Payment Declined') GROUP BY DATE_FORMAT(a.date,'%m-%d-%Y')";
   $results_i = $wpdb->get_results($sql_i);
   foreach ($results_i as $row_i) {
 	$retVal_i[] = $row_i;
@@ -191,13 +266,12 @@ function espresso_charts() {
 		var line2 = [<?php echo $amount_i ?>];
         var ticks = [<?php echo $date_i ?>];
         
-        plot1 = jQuery.jqplot('chart3', [line1, line2], {
+        plot1 = jQuery.jqplot('total_incomplete', [line1, line2], {
             //stackSeries: true,
-			title: 'Incomplete Transactions',
+			title: '<?php _e('Incomplete Registrations/Transactions', 'event_espresso'); ?>',
 			seriesDefaults:{
 				renderer:jQuery.jqplot.BarRenderer,
                 pointLabels: { show: true },
-				//label: ' $ Amount'
             },
             axes: {
                 xaxis: {
@@ -209,7 +283,8 @@ function espresso_charts() {
 				label: '# Attendees'
 			},
 			{
-				label: 'Total <?php echo $org_options['currency_symbol'] ?>'
+				label: '<?php echo $org_options['currency_symbol'] ?> <?php _e('Amount', 'event_espresso'); ?>',
+				pointLabels: { formatString:'<?php echo utf8_encode(html_entity_decode($org_options['currency_symbol'])); ?>%.2f' },
 			}],
 
             highlighter: { show: false },
@@ -230,10 +305,12 @@ function espresso_charts() {
             }
         );
     });</script>
-      <div id="chart1" style="margin-top:20px; margin-left:20px; width:600px; height:200px;"></div>
-      <div id="chart2" style="margin-top:20px; margin-left:20px; width:600px; height:200px;"></div>
-      <div id="chart3" style="margin-top:20px; margin-left:20px; width:600px; height:200px;"></div>
+      <div id="total_reg" style="margin-top:20px; margin-left:20px; width:600px; height:200px; float:left;"></div>
+      <div id="total_completed" style="margin-top:20px; margin-left:20px; width:600px; height:200px; float:left;"></div>
+      <div id="total_pending" style="margin-top:20px; margin-left:20px; width:600px; height:200px; float:left;"></div>
+      <div id="total_incomplete" style="margin-top:20px; margin-left:20px; width:600px; height:200px; float:left;"></div>
       
+      <div style="clear:both"></div>
     </div>
   </div>
 </div>
