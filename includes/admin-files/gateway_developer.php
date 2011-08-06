b5732924e60ce597e98afa99f6ed4557a2ddbf66
Sidney Harrel <sidney@eventespresso.com>
2011-08-06 23:44:31 +0000

    added a check for whether the uploads/espresso/gateways folder is writable and copied that language from the template_files.php

--- includes/admin-files/gateway_developer.php
+++ includes/admin-files/gateway_developer.php
@@ -10,7 +10,7 @@
                     if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/index.php")) {
                         ?>
                         <p class="red_alert">
-                            <?php _e('Remember, if updates are made or features are added to these templates in the future. You will need to make the updates to your customized templates.', 'event_espresso'); ?>
+                            <?php _e('Remember, if updates are made or features are added to these gateways in the future. You will need to make the updates to your customized gateways.', 'event_espresso'); ?>
                         </p>
                         <p>
                             <?php _e("Modifying and adding payment gateways is easy.", 'event_espresso'); ?>
@@ -48,8 +48,24 @@
                             ?>
                             <p><?php _e('If you plan on adding additional payment gateways. Please use the link below to move your gateway files to a safe place. Only use this option if you absolutely need to or instructed to by a represntative from Event Espresso. ', 'event_espresso'); ?></p>
                             <p class="fugue f-warn"><?php _e("Your gateway files have not been moved.", 'event_espresso'); ?></p>
-                            <p class="updated"><?php printf(__("Click here to <a href='%s'>Move your files</a> to a safe place.", 'event_espresso'), wp_nonce_url("admin.php?event_espresso_admin_action=copy_gateways", 'copy_gateways')); ?> </p>
+                            <?php if (!is_writable(EVENT_ESPRESSO_GATEWAY_DIR)) { ?>
+                                <p>
+                                    <?php _e('In order to use this this feature, you will need to move the files located in the', 'event_espresso'); ?> <span class="display-path"><strong><?php echo EVENT_ESPRESSO_PLUGINFULLPATH ?>gateways/</strong></span> <?php _e('directory into the', 'event_espresso'); ?> <span class="display-path"><strong><?php echo EVENT_ESPRESSO_GATEWAY_DIR ?></strong></span> <?php _e('directory', 'event_espresso'); ?>.
+                                </p>
+                                <p class="fugue f-error">
+                                    <?php _e("The permissions on your GATEWAYs directory are incorrect.", 'event_espresso'); ?>
+                                </p>
+                                <p class="fugue f-error">
+                                    <?php _e("To move your files automatically, please set the permissions to 775 on the following directory.", 'event_espresso'); ?>
+                                    <br />
+                                    <br />
+                                    <span class='display-path'><strong>
+                                            <?php _e("Path:", 'event_espresso'); ?>
+                                        </strong> <?php echo EVENT_ESPRESSO_GATEWAY_DIR; ?> </span></p>
+                            <?php } else { ?>
+                                <p class="updated"><?php printf(__("Click here to <a href='%s'>Move your files</a> to a safe place.", 'event_espresso'), wp_nonce_url("admin.php?event_espresso_admin_action=copy_gateways", 'copy_gateways')); ?> </p>
                             <?php
+                            }
                         }
                     }
                     ?>
