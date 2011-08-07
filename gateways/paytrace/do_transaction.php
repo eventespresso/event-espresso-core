c167185d40b25b2692cc5ebcd2c29c7a3c7563a1
arguable somebody <arguable@nolongerwithus.com>
2011-08-07 15:05:18 +0000

    Added Paytrace payment gateway

---
+++ gateways/paytrace/do_transaction.php
@@ -0,0 +1,141 @@
+<?php
+Global $wpdb;
+
+require_once(dirname(__FILE__).'/paytrace.class.php');
+$cls_paytrace = new ClsPaytrace();
+$paytrace_settings = get_option( 'event_espresso_paytrace_settings' );
+
+$attendee_id = 0;
+$primary_registration_id = "";
+$registration_id = "";
+$amount_pd = 0.00;
+$multi_reg = false;
+$event_ids = array();
+$event_link = "";
+
+if ( isset($_POST['id']) && is_numeric($_POST['id']) && $_POST['id'] > 0 )
+{
+	$attendee_id = $_POST['id'];
+	$tmp_row = $wpdb->get_row("select registration_id from ".EVENTS_ATTENDEE_TABLE." where id = $attendee_id");
+	if ( $tmp_row !== NULL )
+	{
+		$tmp_registration_id = $tmp_row->registration_id;
+		$tmp_row = $wpdb->get_row("select * from ".EVENTS_MULTI_EVENT_REGISTRATION_ID_GROUP_TABLE." where registration_id = '{$tmp_registration_id}' ");
+		if ( $tmp_row !== NULL )
+		{
+			$primary_registration_id = $tmp_row->primary_registration_id;		
+			$multi_reg = true;
+		}
+		else
+		{
+			$primary_registration_id = $tmp_registration_id;
+		}
+	}
+}
+
+if ( $attendee_id > 0 && !empty($primary_registration_id) && strlen($primary_registration_id) > 0 )
+{
+	$registration_ids = array();
+	$rs = $wpdb->get_results("select * from ".EVENTS_MULTI_EVENT_REGISTRATION_ID_GROUP_TABLE." where primary_registration_id = '{$primary_registration_id}' ");
+	if ( $wpdb->num_rows > 0 )
+	{
+		foreach($rs as $row )
+		{
+			$registration_ids[] = $row->registration_id;
+		}
+	}
+	else
+	{
+		$registration_ids[] = $primary_registration_id;
+	}
+	
+	$attendees = array();
+	$total = 0.00;
+	$amount_pd = 0.00;
+	$line_item = "";
+	foreach($registration_ids as $registration_id){
+
+		$sql = "select ea.registration_id, ea.id as attendee_id, ea.amount_pd, ed.id as event_id, ed.event_name, ed.start_date, ea.fname, ea.lname, eac.quantity, eac.cost from ". EVENTS_ATTENDEE_TABLE ." ea
+				inner join ".EVENTS_ATTENDEE_COST_TABLE." eac on ea.id = eac.attendee_id
+				inner join " . EVENTS_DETAIL_TABLE . " ed on ea.event_id = ed.id
+				where ea.registration_id = '".$registration_id."' order by ed.event_name ";
+		
+		$tmp_attendees = $wpdb->get_results($sql,ARRAY_A);
+
+		foreach($tmp_attendees as $tmp_attendee){
+			$sub_total = $tmp_attendee["cost"] * $tmp_attendee["quantity"];
+			$attendees[] = array(	"attendee_info"=>$tmp_attendee["event_name"]."[".date('m-d-Y',strtotime($tmp_attendee['start_date']))."]" . " >> " . $tmp_attendee["fname"]." ".$tmp_attendee["lname"],
+									"quantity"=> $tmp_attendee["quantity"],
+									"cost"=> doubleval($tmp_attendee["cost"]),
+									"sub_total" => doubleval($sub_total) );
+			$line_item .= 	"LINEITEM~PRODUCTID=".$tmp_attendee['attendee_id']."+DESCRIPTION=".$tmp_attendee["event_name"]."[".date('m-d-Y',strtotime($tmp_attendee['start_date']))."]" . " >> " . $tmp_attendee["fname"]." ".$tmp_attendee["lname"]."
+							QUANTITY=".$tmp_attendee['quantity']."UNITCOST=".$tmp_attendee['cost']."+AMOUNTLI=".$sub_total."+|";
+			$amount_pd += $tmp_attendee["amount_pd"];
+			$total_cost += $sub_total;
+			if ( !in_array($tmp_attendee['event_id'],$event_ids) )
+			{
+				$event_ids[] = $tmp_attendee['event_id'];
+			}
+		}
+	}
+	$discount = 0;
+	if ( $amount_pd < $total_cost )
+	{
+		$discount = $total_cost - $amount_pd;
+	}
+	$cc 		= $_POST['cc'];
+	$exp_month 	= $_POST['exp_month'];
+	$exp_year 	= $_POST['exp_year'];
+	$csc 		= $_POST['csc'];
+	$bname 		= $_POST['first_name']." ".$_POST['last_name'];
+	$baddress 	= $_POST['address'];
+	$bcity 		= $_POST['city'];
+	$bzip 		= $_POST['zip'];
+	$email 		= $_POST['email'];
+	
+
+	$response = $cls_paytrace->do_transaction($amount_pd,$discount,$line_item,$cc,$csc,$exp_month,$exp_year,$csc,$bname,$baddress,$bcity,$bstate,$bzip,$email);
+	if ( isset($response['status']) )
+	{
+		echo "<div id='paytrace_response'>";
+		if ( $response['status'] > 0 )
+		{
+			echo "<div class='paytrace_status'>".$response['msg']."</div>";
+			$payment_status = 'Completed';
+			$payment_date = date("d-m-Y");
+			$txn_type = 'PayTrace';
+			$txn_id = $response['transaction_data']['TRANSACTIONID'];
+			foreach($registration_ids as $registration_id)
+			{
+				$sql = "UPDATE " . EVENTS_ATTENDEE_TABLE . " SET payment_status = '$payment_status', txn_id = '{$txn_id}', txn_type = '{$txn_type}'
+				, amount_pd = '{$amount_pd}', payment_date ='". date("Y-m-d H:i:s") ."' WHERE registration_id = '{$registration_id}' ";
+				$wpdb->query( $sql );
+			}
+		}
+		
+		if ( isset($response['error_msg']) && strlen(trim($response['error_msg'])) > 0 )
+		{
+			echo "<div class='paytrace_error'>ERROR: ".$response['error_msg']."  </div>";
+		}	
+		echo "</div>";
+		$att_registration_id = $primary_registration_id;
+		$row = $wpdb->get_row("select * from ".EVENTS_ATTENDEE_TABLE." where registration_id = '{$att_registration_id}' order by id limit 1 ");
+		$fname = $row->fname;
+		$lname = $row->lname;
+		$tmp_event_link = array();
+		foreach($event_ids as $event_id)
+		{
+			$row = $wpdb->get_row("select * from ".EVENTS_DETAIL_TABLE." where id = $event_id ");
+			$tmp_event_link[] = $row->event_name;
+		}
+		$event_link = implode(",",$tmp_event_link);
+	}
+	else
+	{
+		echo "<div id='paytrace_response' class='paytrace_error'>Looks like something went wrong.  Please try again or notify the website administrator.</div>";
+	}
+}
+else
+{
+	echo "<div id='paytrace_response' class='paytrace_error'>Looks like something went wrong.  Please try again or notify the website administrator.</div>";
+}
---
+++ gateways/paytrace/settings.php
@@ -0,0 +1,97 @@
+<?php
+
+function event_espresso_paytrace_payment_settings() {
+    if (isset($_POST['update_paytrace'])) {
+		
+        $paytrace_settings = array();#get_option('event_espresso_paytrace_settings');
+        $paytrace_settings['paytrace_user_id'] = $_POST['paytrace_user_id'];
+        $paytrace_settings['paytrace_user_pass'] = $_POST['paytrace_user_pass'];
+        #$paypal_settings['currency_format'] = $_POST['currency_format'];
+        #$paypal_settings['surcharge'] = $_POST['surcharge'];
+        #$paypal_settings['use_sandbox'] = $_POST['use_sandbox'];
+        #$paypal_settings['bypass_payment_page'] = $_POST['bypass_payment_page'];
+        #$paypal_settings['no_shipping'] = $_POST['no_shipping'];
+        #$paypal_settings['button_url'] = $_POST['button_url'];
+        update_option('event_espresso_paytrace_settings', $paytrace_settings);
+        echo '<div id="message" class="updated fade"><p><strong>' . __('Paytrace settings saved.', 'event_espresso') . '</strong></p></div>';
+    }
+    ?>
+
+    <div class="metabox-holder">
+        <div class="postbox">
+
+            <h3>
+    <?php _e('Paytrace Settings', 'event_espresso'); ?>
+            </h3>
+            <div class="padding">
+                <?php
+                if (isset($_REQUEST['activate_paytrace'])&&$_REQUEST['activate_paytrace'] == 'true') {
+                    add_option("events_paytrace_active", 'true', '', 'yes');
+                    add_option("event_espresso_paytrace_settings", '', '', 'yes');
+                }
+                if (isset($_REQUEST['reactivate_paytrace'])&&$_REQUEST['reactivate_paytrace'] == 'true') {
+                    update_option('events_paytrace_active', 'true');
+                }
+                if (isset($_REQUEST['deactivate_paytrace'])&&$_REQUEST['deactivate_paytrace'] == 'true') {
+                    update_option('events_paytrace_active', 'false');
+                }
+                echo '<ul>';
+                switch (get_option('events_paytrace_active')) {
+                    case 'false':
+                        echo '<li>Paytrace Gateway is installed.</li>';
+                        echo '<li style="width:30%;" onclick="location.href=\'' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=payment_gateways&reactivate_paytrace=true\';" class="green_alert pointer"><strong>' . __('Activate Paytrace?', 'event_espresso') . '</strong></li>';
+                        break;
+                    case 'true':
+                        echo '<li style="width:30%;" onclick="location.href=\'' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=payment_gateways&deactivate_paytrace=true\';" class="red_alert pointer"><strong>' . __('Deactivate Paytrace?', 'event_espresso') . '</strong></li>';
+                        event_espresso_display_paytrace_settings();
+
+                        break;
+                    default:
+                        echo '<li style="width:50%;" onclick="location.href=\'' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=payment_gateways&activate_paytrace=true\';" class="yellow_alert pointer"><strong>' . __('The Paytrace addon is installed. Would you like to activate it?', 'event_espresso') . '</strong></li>';
+                        break;
+                }
+                echo '</ul>';
+                ?>
+            </div>
+        </div>
+    </div>
+<?php
+} 
+
+
+//PayPal Settings Form
+function event_espresso_display_paytrace_settings() {
+    $paytrace_settings = get_option('event_espresso_paytrace_settings');
+
+    ?>
+    <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+        <table width="99%" border="0" >
+            <tr>
+                <td valign="top">
+                	<ul>
+                        <li>
+                            <label for="paytrace_user_id">
+    						<?php _e('Paytrace User ID:', 'event_espresso'); ?>
+                            </label>
+                            <br />
+                            <input type="text" name="paytrace_user_id" size="35" value="<?php echo $paytrace_settings['paytrace_user_id']; ?>">
+                        </li>
+                        <li>
+                            <label for="paytrace_user_pass">
+    						<?php _e('Paytrace User Password:', 'event_espresso'); ?>
+                            </label>
+                            <br />
+                            <input type="text" name="paytrace_user_pass" size="35" value="<?php echo $paytrace_settings['paytrace_user_pass']; ?>">
+                        </li>
+                        
+                    </ul>
+				</td>
+            </tr>
+        </table>
+        <p>
+            <input type="hidden" name="update_paytrace" value="update_paytrace">
+            <input class="button-primary" type="submit" name="Submit" value="<?php _e('Update Paytrace Settings', 'event_espresso') ?>" id="save_paytrace_settings" />
+        </p>
+    </form>
+    <?php
+}
