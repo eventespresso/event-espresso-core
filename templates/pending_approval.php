3d6af11197c8150547feef5f13ab9db2ba458fc9
Hugo wth hugo <hugo@nolongerwithus.com>
2011-08-13 12:09:08 +0000

    Minor re-factor pending approval page

--- templates/pending_approval.php
+++ templates/pending_approval.php
@@ -1,11 +1,10 @@
 <div id="espresso_confirmation_display">	
 <?php //Pending Approval Page Template ?>
-    <h3><?php echo $fname ?>,</h3>
-    <h3><?php _e('Your registration is not complete until admin approves.', 'event_espresso'); ?></h3>
-        
-	<p>
-        <strong class="event_espresso_name"><?php _e('Amount due: ', 'event_espresso'); ?></strong> 
-        <span class="event_espresso_value"><?php echo $org_options['currency_symbol']?><?php echo $event_cost; ?></span>
-	</p>
-	<p><strong><?php _e('Your Registration ID:', 'event_espresso'); ?> </strong><?php echo $registration_id ?></p>
+	<h3><?php echo $fname ?>,</h3>
+		<p class="instruct"><?php _e('Your registration is not complete until admin approves.', 'event_espresso'); ?></p>
+		<p>
+			<span class="event_espresso_name section-title"><?php _e('Amount due: ', 'event_espresso'); ?></span> 
+			<span class="event_espresso_value"><?php echo $org_options['currency_symbol']?><?php echo $event_cost; ?></span>
+		</p>
+		<p><span class="section-title"><?php _e('Your Registration ID:', 'event_espresso'); ?> </span><?php echo $registration_id ?></p>
 </div>
