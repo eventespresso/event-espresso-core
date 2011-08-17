22d80371874f164ecc8a5540fbf2258fe94f57e0
Seth Shoultes <seth@eventespresso.com>
2011-08-17 09:34:44 +0000

    Fixed some php warnings and applied source formatting for readability

--- templates/payment_page.php
+++ templates/payment_page.php
@@ -1,45 +1,34 @@
-			<div id="espresso_confirmation_display" class="event-display-boxes">	
-    <?php
+<div id="espresso_confirmation_display" class="event-display-boxes">
+  <?php
     //Confirmation Page Template
     if ($event_cost == '0.00') {
         unset($_SESSION['espresso_session_id']);
         ?>
-        <h2><?php echo $fname ?>,</h2>
-
-        <p class="instruct">
-						<?php _e('Thank you! Your registration is confirmed for', 'event_espresso'); ?> <b><?php echo stripslashes_deep($event_name) ?></b>
-					</p>
-
-        <p>
-						<span class="section-title"><?php _e('Your Registration ID: ', 'event_espresso'); ?> </span>
-							<?php echo $registration_id ?>
-					</p>
-
-        <p class="instruct">
-						<?php _e('A confirmation email has been sent with additional details of your registration.', 'event_espresso'); ?>
-					</p>
-    <?php
+  <h2><?php echo $fname ?>,</h2>
+  <p class="instruct">
+    <?php _e('Thank you! Your registration is confirmed for', 'event_espresso'); ?>
+    <b><?php echo stripslashes_deep($event_name) ?></b> </p>
+  <p> <span class="section-title">
+    <?php _e('Your Registration ID: ', 'event_espresso'); ?>
+    </span> <?php echo $registration_id ?> </p>
+  <p class="instruct">
+    <?php _e('A confirmation email has been sent with additional details of your registration.', 'event_espresso'); ?>
+  </p>
+  <?php
 } else {
     ?>
-        <h2><?php echo $fname ?>,</h2>
-        <p class="instruct"><?php _e('Your registration is not complete until payment is received.', 'event_espresso'); ?></p>
-
-        <p>
-						<span class="event_espresso_name section-title"><?php _e('Amount due: ', 'event_espresso'); ?></span>
-						<span class="event_espresso_value"><?php echo isset($org_options['currency_symbol'])?$org_options['currency_symbol']:''; ?><?php echo $event_cost; ?></span>
-					</p>
-        <p>
-						<span class="section-title"><?php _e('Your Registration ID: ', 'event_espresso'); ?> </span><?php echo $registration_id ?>
-					</p>
-        <p>
-						<?php echo $org_options['email_before_payment'] == 'Y' ? __('A confirmation email has been sent with additional details of your registration.', 'event_espresso') : ''; ?>
-					</p>
-
-        
-
-    <?php
+  <h2><?php echo $fname ?>,</h2>
+  <p class="instruct">
+    <?php _e('Your registration is not complete until payment is received.', 'event_espresso'); ?>
+  </p>
+  <p> <span class="event_espresso_name section-title">
+    <?php _e('Amount due: ', 'event_espresso'); ?>
+    </span> <span class="event_espresso_value"><?php echo isset($org_options['currency_symbol'])?$org_options['currency_symbol']:''; ?><?php echo $event_cost; ?></span> </p>
+  <p> <span class="section-title">
+    <?php _e('Your Registration ID: ', 'event_espresso'); ?>
+    </span><?php echo $registration_id ?> </p>
+  <p> <?php echo $org_options['email_before_payment'] == 'Y' ? __('A confirmation email has been sent with additional details of your registration.', 'event_espresso') : ''; ?> </p>
+  <?php
 }
 ?>
-			</div>
-			
-			
+</div>
