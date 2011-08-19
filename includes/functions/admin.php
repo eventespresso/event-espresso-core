7cc701cdf6ee0ead3775cbd3f5d87520f3d68062
Seth Shoultes <seth@eventespresso.com>
2011-08-19 09:12:15 +0000

    Adding some code to show the how many query are being run in the admin. Added some admin footer text and link to Event Espresso.

--- includes/functions/admin.php
+++ includes/functions/admin.php
@@ -1436,3 +1436,75 @@ function espresso_performance( $visible = false ) {
     echo $visible ? $stat : "<!-- {$stat} -->" ;
 }
 add_action( 'wp_footer', 'espresso_performance', 20 );
+
+function espresso_admin_performance($show=0) {
+	if($show==0)
+		return;
+		
+	global $wpdb, $EZSQL_ERROR;
+		$out = '';
+		$total_time = 0;
+
+		if ( !empty($wpdb->queries) ) {
+			$show_many = isset($_GET['debug_queries']);
+
+			if ( $wpdb->num_queries > 500 && !$show_many )
+				$out .= "<p>" . sprintf( __('There are too many queries to show easily! <a href="%s">Show them anyway</a>', 'debug-bar'), add_query_arg( 'debug_queries', 'true' ) ) . "</p>";
+
+			$out .= '<ol class="wpd-queries">';
+			$counter = 0;
+
+			foreach ( $wpdb->queries as $q ) {
+				list($query, $elapsed, $debug) = $q;
+
+				$total_time += $elapsed;
+
+				if ( ++$counter > 500 && ! $show_many )
+					continue;
+
+				$debug = explode( ', ', $debug );
+				$debug = array_diff( $debug, array( 'require_once', 'require', 'include_once', 'include' ) );
+				$debug = implode( ', ', $debug );
+				$debug = str_replace( array( 'do_action, call_user_func_array' ), array( 'do_action' ), $debug );
+				$query = nl2br(esc_html($query));
+
+				$out .= "<li>$query<br/><div class='qdebug'>$debug <span>#{$counter} (" . number_format(sprintf('%0.1f', $elapsed * 1000), 1, '.', ',') . "ms)</span></div></li>\n";
+			}
+			$out .= '</ol>';
+		} else {
+			if ( $wpdb->num_queries == 0 )
+				$out .= "<p><strong>" . __('There are no queries on this page.', 'debug-bar') . "</strong></p>";
+			else
+				$out .= "<p><strong>" . __('SAVEQUERIES must be defined to show the query log.', 'debug-bar') . "</strong></p>";
+		}
+
+		if ( ! empty($EZSQL_ERROR) ) {
+			$out .= '<h3>' . __( 'Database Errors', 'debug-bar' ) . '</h3>';
+			$out .= '<ol class="wpd-queries">';
+
+			foreach ( $EZSQL_ERROR as $e ) {
+				$query = nl2br(esc_html($e['query']));
+				$out .= "<li>$query<br/><div class='qdebug'>{$e['error_str']}</div></li>\n";
+			}
+			$out .= '</ol>';
+		}
+
+		$heading = '';
+		if ( $wpdb->num_queries )
+			$heading .= '<h2><span>Total Queries:</span>' . number_format( $wpdb->num_queries ) . "</h2>\n";
+		if ( $total_time )
+			$heading .= '<h2><span>Total query time:</span>' . number_format(sprintf('%0.1f', $total_time * 1000), 1) . " ms</h2>\n";
+		if ( ! empty($EZSQL_ERROR) )
+			$heading .= '<h2><span>Total DB Errors:</span>' . number_format( count($EZSQL_ERROR) ) . "</h2>\n";
+
+		$out = $heading . $out;
+
+		echo $out;
+}
+
+add_filter('admin_footer_text', 'espresso_admin_performance'); 
+
+function espresso_admin_footer () {
+	echo 'Event Registration and Ticketing Powered by <a href="http://eventespresso.com/" title="Event Registration Powered by Event Espresso" target="_blank">'.EVENT_ESPRESSO_POWERED_BY.'</a>';
+}
+add_filter('admin_footer_text', 'espresso_admin_footer'); 
