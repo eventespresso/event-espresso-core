f688a3cf965000b47ed8aee7ac7d3b4ffa3a8950
Seth Shoultes <seth@eventespresso.com>
2011-08-23 09:16:07 +0000

    Changed function name is_selected to espresso_is_selected

--- includes/admin-files/event_styles_settings.php
+++ includes/admin-files/event_styles_settings.php
@@ -1,6 +1,6 @@
 <?php
 // read our style dir and build an array of files
-$dhandle = opendir(EVENT_ESPRESSO_PLUGINFULLPATH . 'templates/css');
+$dhandle = opendir(EVENT_ESPRESSO_PLUGINFULLPATH . 'templates/css/');
 $files = array();
 
 		if ($dhandle) { //if we managed to open the directory
@@ -35,16 +35,17 @@ $files_color = array();
    closedir($dhandle);
 		}
 		
-function espresso_is_selected($name) {
-	   global $org_options;
-	   $input_item = $name;
-			 $option_selections = array($org_options['selected_style'], $org_options['style_color']  );
-	   if (!in_array( $input_item, $option_selections )  )
-	   return false;
-	   else
-	   return 'selected="selected"'; 
-}
-		($org_options['enable_default_style'] == 'N' || file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "templates/event_espresso_style.css"))? $disabled = 'disabled="disabled"' : $disabled = '';
+		function is_selected($name) {
+   global $org_options;
+   $input_item = $name;
+		 $option_selections = array($org_options['selected_style'], $org_options['style_color']  );
+   if (!in_array( $input_item, $option_selections )  )
+   return false;
+   else
+   echo  'selected="selected"';
+   return; 
+  }
+		('N' == $org_options['enable_default_style'] || file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "templates/event_espresso_style.css"))? $disabled = 'disabled="disabled"' : $disabled = '';
 		(!empty($disabled))? $styled = 'style="color: #ccc;"' : $styled = '';
 ?>
 <?php // var_dump($org_options['selected_style']); ?>
@@ -53,10 +54,10 @@ function espresso_is_selected($name) {
 	<label for="base-style-select" <?php echo $styled ?>><?php _e('Select Base stylesheet', 'event_espresso');  ?></label>
 		<select id="base-style-select"  <?php echo $disabled ?> name="selected_style">
 			
-			<option <?php echo espresso_is_selected($fname) ?> value="event_espresso_style.css"><?php _e('Default style - generic', 'event_espresso'); ?></option>
+			<option <?php is_selected($fname) ?> value="event_espresso_style.css">Default style - generic</option>
 				
 				<?php foreach( $files as $fname ) { ?>
-				<option <?php echo espresso_is_selected($fname) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
+				<option <?php is_selected($fname) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
 	<?php } ?>
 
 	</select>
@@ -66,10 +67,10 @@ function espresso_is_selected($name) {
 	<label for="style-color" <?php echo $styled ?>><?php _e('Select optional colors', 'event_espresso');  ?></label>
 		<select id="style-color"  <?php echo $disabled ?> name="style_color">
 			
-			<option <?php echo espresso_is_selected($fname_color) ?> value=""> - <?php _e('None', 'event_espresso'); ?> - </option>
+			<option <?php is_selected($fname_color) ?> value=""> - None - </option>
 				
 				<?php foreach( $files_color as $fname_color ) { ?>
-				<option <?php echo espresso_is_selected($fname_color) ?> value="<?php echo $fname_color ?>"><?php echo $fname_color; ?></option>
+				<option <?php is_selected($fname_color) ?> value="<?php echo $fname_color ?>"><?php echo $fname_color; ?></option>
 	<?php } ?>
 
 		</select>	
