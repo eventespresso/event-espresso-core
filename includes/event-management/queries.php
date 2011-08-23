da3b64ecd77d5bbde677a35cf98e2baf4aefbcf5
Seth Shoultes <seth@eventespresso.com>
2011-08-23 09:06:30 +0000

    Fixed some SQL errors in the attendee counts

--- includes/event-management/queries.php
+++ includes/event-management/queries.php
@@ -112,6 +112,7 @@ function espresso_total_events_this_month(){
 	function espresso_total_all_attendees(){
 		//Get number of total attendees
 		global $wpdb;
+		$WHERE = " WHERE ";
 		$asql1 = "(";
 		if (function_exists('espresso_member_data')&&espresso_member_data('role')=='espresso_group_admin'){
 			$group = get_user_meta(espresso_member_data('id'), "espresso_group", true);
@@ -123,22 +124,26 @@ function espresso_total_events_this_month(){
 				$asql1 .= " JOIN " . EVENTS_LOCALE_REL_TABLE . " l ON  l.venue_id = r.venue_id ";
 			}
 			$asql1 .= $group !='' ? " WHERE l.locale_id IN (" . implode(",",$group) . ") " : '';
-			$asql1 .= " AND quantity >= 1 AND (payment_status='Completed' OR payment_status='Pending') ";
-			$asql1 .= " AND event_status != 'D' ";
+			//$asql1 .= " AND quantity >= 1 ";
+			// AND (payment_status='Completed' OR payment_status='Pending') ";
+			$asql1 .= " WHERE event_status != 'D' ";
 			$asql1 .= ") UNION (";
 		}
 		$asql1 .= "SELECT SUM(a.quantity) quantity FROM " . EVENTS_ATTENDEE_TABLE. " a ";
 		$asql1 .= " JOIN ". EVENTS_DETAIL_TABLE ." e ON e.id=a.event_id ";
 		if(  function_exists('espresso_member_data') && ( espresso_member_data('role')=='espresso_event_manager' || espresso_member_data('role')=='espresso_group_admin') ){
-			$asql1 .= " WHERE wp_user = '" . espresso_member_data('id') ."'";
+			$asql1 .= " $WHERE wp_user = '" . espresso_member_data('id') ."'";
+			$WHERE = " AND ";
 		}
-		$asql1 .= " AND a.quantity >= 1 AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
-		$asql1 .= " AND e.event_status != 'D' ";
+		//$asql1 .= " $WHERE a.quantity >= 1 ";
+		//AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
+		$asql1 .= " $WHERE e.event_status != 'D' ";
 		$asql1 .= ")";
 		if(  function_exists('espresso_member_data') && ( espresso_member_data('role')=='espresso_event_manager' || espresso_member_data('role')=='espresso_group_admin') ){
 		}
 		$wpdb->query($asql1);
 		//echo $asql1;
+		//echo '<p>$asql1 '.$asql1.'</p>';
 		$total_a = 0;
 		if ($wpdb->num_rows > 0 && $wpdb->last_result[0]->quantity!=NULL) {
 			$total_a =  $wpdb->last_result[0]->quantity + (isset($wpdb->last_result[1]->quantity) ? $wpdb->last_result[1]->quantity:0);
@@ -162,7 +167,8 @@ function espresso_total_events_this_month(){
 			}
 			$asql2 .= " WHERE date BETWEEN '". $curdate.' 00:00:00'."' AND '". $curdate.' 23:59:59' ."' ";
 			$asql2 .= $group !='' ? " AND l.locale_id IN (" . implode(",",$group) . ") " : '';
-			$asql2 .= " AND quantity >= 1 AND (payment_status='Completed' OR payment_status='Pending') ";
+			$asql2 .= " AND quantity >= 1";
+			// AND (payment_status='Completed' OR payment_status='Pending') ";
 			$asql2 .= " AND event_status != 'D' ";
 			$asql2 .= ") UNION (";
 		}
@@ -172,7 +178,8 @@ function espresso_total_events_this_month(){
 		if(  function_exists('espresso_member_data') && ( espresso_member_data('role')=='espresso_event_manager' || espresso_member_data('role')=='espresso_group_admin') ){
 			$asql2 .= " AND e.wp_user = '" . espresso_member_data('id') ."' ";
 		}
-		$asql2 .= " AND a.quantity >= 1 AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
+		$asql2 .= " AND a.quantity >= 1 ";
+		//AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
 		$asql2 .= " AND e.event_status != 'D' ";
 		$asql2 .= ")";
 		//echo $asql2;
@@ -208,7 +215,8 @@ function espresso_total_events_this_month(){
 			}
 			$asql3 .= " WHERE date BETWEEN '".event_espresso_no_format_date($this_year_r. '-' .$this_month_r . '-01',$format = 'Y-m-d')."' AND '".event_espresso_no_format_date($this_year_r . '-' .$this_month_r. '-' . $days_this_month,$format = 'Y-m-d')."' ";
 			$asql3 .= $group !='' ? " AND l.locale_id IN (" . implode(",",$group) . ") " : '';
-			$asql3 .= " AND a.quantity >= 1 AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
+			$asql3 .= " AND a.quantity >= 1 ";
+			//AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
 			$asql3 .= " AND e.event_status != 'D' ";
 			$asql3 .= ") UNION (";
 		}
@@ -218,7 +226,8 @@ function espresso_total_events_this_month(){
 		if(  function_exists('espresso_member_data') && ( espresso_member_data('role')=='espresso_event_manager' || espresso_member_data('role')=='espresso_group_admin') ){
 			$asql3 .= " AND e.wp_user = '" . espresso_member_data('id') ."' ";
 		}
-		$asql3 .= " AND a.quantity >= 1 AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
+		$asql3 .= " AND a.quantity >= 1 ";
+		//AND (a.payment_status='Completed' OR a.payment_status='Pending') ";
 		$asql3 .= " AND e.event_status != 'D' ";
 		$asql3 .= ")";
 		//echo $asql3;
