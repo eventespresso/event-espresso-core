10f8153930658cf451965031a86b80a20f5feb42
Sidney Harrel <sidney@eventespresso.com>
2011-08-25 02:47:58 +0000

    php warning fixes

--- includes/admin-files/form-builder/questions/insert_question.php
+++ includes/admin-files/form-builder/questions/insert_question.php
@@ -4,17 +4,17 @@
 function event_espresso_form_builder_insert(){
 	global $wpdb,$current_user;
 	//$wpdb->show_errors();
-	$event_id = $_REQUEST['event_id'];
-	$event_name = $_REQUEST['event_name'];
+	$event_id = empty($_REQUEST['event_id']) ? 0 : $_REQUEST['event_id'];
+	$event_name = empty($_REQUEST['event_name']) ? '' : $_REQUEST['event_name'];
 	$question = $_POST['question'];
 	$question_type = $_POST['question_type'];
 	$question_values = $_POST['values'];
-	$required = $_POST['required'] ? 'Y':'N';
-	$admin_only = $_POST['admin_only'] ? 'Y':'N';
+	$required = !empty($_POST['required']) ? 'Y':'N';
+	$admin_only = !empty($_POST['admin_only']) ? 'Y':'N';
    	$sequence = $_POST['sequence'] ?  $_POST['sequence']:'0';
 	if (!function_exists('espresso_member_data'))
 			$current_user->ID = 1;
-   	
+
 		if ($wpdb->query("INSERT INTO " . EVENTS_QUESTION_TABLE . " (question_type, question, response, required,admin_only, sequence,wp_user)"
 				. " VALUES ('" . $question_type . "', '" . $question . "', '" . $question_values . "', '" . $required . "', '" . $admin_only . "', " . $sequence . ",'".$current_user->ID."')")){?>
 		<div id="message" class="updated fade"><p><strong>The question <?php echo htmlentities2($_REQUEST['question']);?> has been added.</strong></p></div>
