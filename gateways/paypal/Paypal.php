0a449138a7d091c000e87e14605386c5d7c8f3a4
Sidney Harrel <sidney@eventespresso.com>
2011-09-02 18:13:50 +0000

    added more verbose logging of all transactions, not just testmode.

--- gateways/paypal/Paypal.php
+++ gateways/paypal/Paypal.php
@@ -71,9 +71,11 @@ class Paypal extends PaymentGateway {
             // generate the post string from the _POST vars
             $req = '';
 
+            $errors = "\nUsing BUILT-IN PHP curl methods\n";
             // Run through the posted array
             foreach ($_POST as $key => $value) {
                 $this->ipnData["$key"] = $value;
+                $errors .= "key = " . $key . "\nvalue = " . $value . "\n";
                 $value = urlencode(stripslashes($value));
                 $value = preg_replace('/(.*[^%^0^D])(%0A)(.*)/i', '${1}%0D%0A${3}', $value); // IPN fix
                 $req .= $key . '=' . $value . '&';
@@ -88,32 +90,27 @@ class Paypal extends PaymentGateway {
             $error["set_timeout"] = curl_setopt($ch, CURLOPT_TIMEOUT, 3); // Sets a time limit for curl in seconds (do not set too low)
             $error["set_post"] = curl_setopt($ch, CURLOPT_POST, 1); // Set curl to send data using post
             $error["set_post_fields"] = curl_setopt($ch, CURLOPT_POSTFIELDS, $req); // Add the request parameters to the post
-            if ($this->testMode) {
-                $errors = "\nUsing BUILT-IN PHP curl methods\n";
-                $errors .= $error["set_host"] ? "Success" : "Failure";
-                $errors .= " Setting host: " . $url . "\n";
-                $errors .= $error["set_post"] ? "Success" : "Failure";
-                $errors .= " Setting request type to post\n";
-                $errors .= $error["set_post_fields"] ? "Success" : "Failure";
-                $errors .= " Setting post fields: " . htmlspecialchars($req) . "\n";
-                $errors .= $error["set_fail_on_error"] ? "Success" : "Failure";
-                $errors .= " Setting Fain On Error\n";
-                $errors .= $error["set_return_transfer"] ? "Success" : "Failure";
-                $errors .= " Setting return transfer\n";
-                $errors .= $error["set_timeout"] ? "Success" : "Failure";
-                $errors .= " Setting Timeout\n";
-                $error["set_verbose"] = curl_setopt($ch, CURLOPT_VERBOSE, 1);
-                $errors .= $error["set_verbose"] ? "Success" : "Failure";
-                $errors .= " Setting verbose mode\n";
-            }
+            $errors .= $error["set_host"] ? "Success" : "Failure";
+            $errors .= " Setting host: " . $url . "\n";
+            $errors .= $error["set_post"] ? "Success" : "Failure";
+            $errors .= " Setting request type to post\n";
+            $errors .= $error["set_post_fields"] ? "Success" : "Failure";
+            $errors .= " Setting post fields: " . htmlspecialchars($req) . "\n";
+            $errors .= $error["set_fail_on_error"] ? "Success" : "Failure";
+            $errors .= " Setting Fain On Error\n";
+            $errors .= $error["set_return_transfer"] ? "Success" : "Failure";
+            $errors .= " Setting return transfer\n";
+            $errors .= $error["set_timeout"] ? "Success" : "Failure";
+            $errors .= " Setting Timeout\n";
+            $error["set_verbose"] = curl_setopt($ch, CURLOPT_VERBOSE, 1);
+            $errors .= $error["set_verbose"] ? "Success" : "Failure";
+            $errors .= " Setting verbose mode\n";
             $result = curl_exec($ch); // run the curl process (and return the result to $result
-            $this->ipnResponse=$result;
+            $this->ipnResponse = $result;
             $error["result"] = curl_error($ch);
             curl_close($ch);
-            if ($this->testMode) {
-                $errors .= "Errors resulting from the execution of curl transfer: " . $error["result"];
-                $this->logErrors($errors);
-            }
+            $errors .= "Errors resulting from the execution of curl transfer: " . $error["result"];
+            $this->logErrors($errors);
 
             if (strcmp($result, "VERIFIED") == 0) { // It may seem strange but this function returns 0 if the result matches the string So you MUST check it is 0 and not just do strcmp ($result, "VERIFIED") (the if will fail as it will equate the result as false)
                 // Do some checks to ensure that the payment has been sent to the correct person
