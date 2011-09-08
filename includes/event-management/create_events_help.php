7209234967ccd74d2106f273a180c61816e93db3
Chris Reynolds <chris@eventespresso.com>
2011-09-08 16:19:57 +0000

    changed link in ee_meta help text for "forums" to point to the thread on shortcodes & fixed minor CSS issue where thickbox content was displaying a scrollbar the first time the thickbox was displayed

--- includes/event-management/create_events_help.php
+++ includes/event-management/create_events_help.php
@@ -13,7 +13,7 @@
 							<p><?php _e("The name parameter is the the first box labeled 'Key' and allows the shortcode to identify which meta box is to be displayed; the 'Value' is the actual content you wish to be shown.", "event_espresso") ?></p>
 							<p><?php _e("If you are using custom templates (moved to the uploads folder) you can add the shortcode directly to the template, this would take the form of:", "event_espresso") ?></p>
 							<p><?php _e("<code>&lt;?php echo do_shortcode('[EE_META type='event_meta' name='my_meta_key']'); ?></code>", "event_espresso") ?></p>
-							<p><?php _e("Further information on shortcodes is avaialable <a href='http://eventespresso.dev/wp-admin/admin.php?page=support#shortcodes'> on the Help &amp; Support page</a> or from the <a href='http://eventespresso.com/forums/'>forums</a>.", "event_espreso"); ?></p>
+							<p><?php _e("Further information on shortcodes is avaialable <a href='http://eventespresso.dev/wp-admin/admin.php?page=support#shortcodes'> on the Help &amp; Support page</a> or from the <a href='http://eventespresso.com/forums/2010/10/post-type-variables-and-shortcodes/'>forums</a>.", "event_espreso"); ?></p>
           
 					</div>
     </div>
