78aaee1ee5cf54f51f3b84a1178a701571d490e0
Seth Shoultes <seth@eventespresso.com>
2011-09-15 20:23:38 +0000

    Reverting changes I made to this file by accident

--- includes/admin-files/email-manager/index.php
+++ includes/admin-files/email-manager/index.php
@@ -1,20 +1,20 @@
 <?php
-require_once("add_new.php");
-require_once("edit.php");
-require_once("update.php");
-require_once("add_to_db.php");
+require_once("add_new_email.php");
+require_once("edit_email.php");
+require_once("update_email.php");
+require_once("add_email_to_db.php");
 
-function event_espresso_ticket_config_mnu() {
+function event_espresso_email_config_mnu() {
     global $wpdb, $current_user, $espresso_premium;
     ?>
 
 
     <div class="wrap">
         <div id="icon-options-event" class="icon32"> </div>
-        <h2><?php echo _e('Manage Event Tickets', 'event_espresso') ?>
+        <h2><?php echo _e('Manage Event Emails', 'event_espresso') ?>
             <?php
-            if (!isset($_REQUEST['action']) || ($_REQUEST['action'] != 'edit' && $_REQUEST['action'] != 'add_new_ticket')) {
-                echo '<a href="admin.php?page=event_tickets&amp;action=add_new_ticket" class="button add-new-h2" style="margin-left: 20px;">' . __('Add New Ticket', 'event_espresso') . '</a>';
+            if (!isset($_REQUEST['action']) || ($_REQUEST['action'] != 'edit' && $_REQUEST['action'] != 'add_new_email')) {
+                echo '<a href="admin.php?page=event_emails&amp;action=add_new_email" class="button add-new-h2" style="margin-left: 20px;">' . __('Add New Email', 'event_espresso') . '</a>';
             }
             ?>
         </h2>
@@ -25,19 +25,19 @@ function event_espresso_ticket_config_mnu() {
 
 
                     <?php
-                    if (!empty($_POST['delete_ticket'])) {
+                    if (!empty($_POST['delete_email'])) {
                         if (is_array($_POST['checkbox'])) {
                             while (list($key, $value) = each($_POST['checkbox'])):
                                 $del_id = $key;
-                                //Delete ticket data
-                                $sql = "DELETE FROM " . EVENTS_TICKET_TABLE . " WHERE id='$del_id'";
+                                //Delete email data
+                                $sql = "DELETE FROM " . EVENTS_EMAIL_TABLE . " WHERE id='$del_id'";
                                 $wpdb->query($sql);
                             endwhile;
                         }
                         ?>
                         <div id="message" class="updated fade">
                             <p><strong>
-                                    <?php _e('Ticketss have been successfully deleted.', 'event_espresso'); ?>
+                                    <?php _e('Emails have been successfully deleted.', 'event_espresso'); ?>
                                 </strong></p>
                         </div>
                     <?php } ?>
@@ -45,24 +45,24 @@ function event_espresso_ticket_config_mnu() {
                     if (isset($_REQUEST['action'])) {
                         switch ($_REQUEST['action']) {
                             case 'update':
-                                update_event_ticket();
+                                update_event_email();
                                 break;
                             case 'add':
-                                add_ticket_to_db();
+                                add_email_to_db();
                                 break;
-                            case 'add_new_ticket':
-                                add_new_event_ticket();
+                            case 'add_new_email':
+                                add_new_event_email();
                                 break;
                             case 'edit':
-                                edit_event_ticket();
+                                edit_event_email();
                                 break;
                         }
                     }
                     ?>
 
-                    <p><?php _e('Create customized tickets for use in multiple events.', 'event_espresso'); ?></p>
+                    <p><?php _e('Create customized emails for use in multiple events.', 'event_espresso'); ?></p>
                     <form id="form1" name="form1" method="post" action="<?php echo $_SERVER["REQUEST_URI"] ?>">
-                        <table id="table" class="widefat manage-tickets">
+                        <table id="table" class="widefat manage-emails">
                             <thead>
                                 <tr>
                                     <th class="manage-column column-cb check-column" id="cb" scope="col" style="width:3.5%;"><input type="checkbox"></th>
@@ -76,7 +76,7 @@ function event_espresso_ticket_config_mnu() {
                             </thead>
                             <tbody>
                                 <?php
-                                $sql = "SELECT * FROM " . EVENTS_TICKET_TABLE . " e";
+                                $sql = "SELECT * FROM " . EVENTS_EMAIL_TABLE . " e";
                                 if (function_exists('espresso_member_data') && ( espresso_member_data('role') == 'espresso_event_manager' || espresso_member_data('role') == 'espresso_group_admin')) {
                                     $sql .= " JOIN $wpdb->users u on u.ID = e.wp_user WHERE e.wp_user = " . $current_user->ID;
                                 }
@@ -84,21 +84,22 @@ function event_espresso_ticket_config_mnu() {
                                 if ($wpdb->num_rows > 0) {
                                     $results = $wpdb->get_results($sql . " ORDER BY e.id ASC");
                                     foreach ($results as $result) {
-                                        $ticket_id = $result->id;
-                                        $ticket_name = stripslashes($result->ticket_name);
-                                        $ticket_text = stripslashes($result->ticket_text);
+                                        $email_id = $result->id;
+                                        $email_name = stripslashes($result->email_name);
+                                        $email_text = stripslashes($result->email_text);
                                         $wp_user = $result->wp_user;
                                         ?>
                                         <tr>
-<td><input name="checkbox[<?php echo $ticket_id ?>]" type="checkbox"  title="Delete <?php echo stripslashes($ticket_name) ?>"></td>
-                                            <td><?php echo $ticket_id ?></td>
-                                             <td class="post-title page-title column-title"><strong><a href="admin.php?page=event_tickets&action=edit&id=<?php echo $ticket_id ?>"><?php echo $ticket_name?></a></strong>
-              <div class="row-actions"><span class="edit"><a href="admin.php?page=event_tickets&action=edit&id=<?php echo $ticket_id ?>"><?php _e('Edit', 'event_espresso'); ?></a> | </span><span class="delete"><a onclick="return confirmDelete();" class="delete submitdelete" href="admin.php?page=event_tickets&action=delete_ticket&id=<?php echo $ticket_id?>"><?php _e('Delete', 'event_espresso'); ?></a></span></div>
-              </td>                                            <?php if (function_exists('espresso_user_meta') && espresso_is_admin() == true) { ?>
+                                            <td><input name="checkbox[<?php echo $email_id ?>]" type="checkbox"  title="Delete <?php echo stripslashes($email_name) ?>"></td>
+                                            <td><?php echo $email_id ?></td>
+                                             <td class="post-title page-title column-title"><strong><a href="admin.php?page=event_emails&action=edit&id=<?php echo $email_id ?>"><?php echo $email_name?></a></strong>
+              <div class="row-actions"><span class="edit"><a href="admin.php?page=event_emails&action=edit&id=<?php echo $email_id ?>"><?php _e('Edit', 'event_espresso'); ?></a> | </span><span class="delete"><a onclick="return confirmDelete();" class="delete submitdelete" href="admin.php?page=event_emails&action=delete_email&id=<?php echo $email_id?>"><?php _e('Delete', 'event_espresso'); ?></a></span></div>
+              </td>
+                                            <?php if (function_exists('espresso_user_meta') && espresso_is_admin() == true) { ?>
                                                 <td><?php echo espresso_user_meta($wp_user, 'user_firstname') != '' ? espresso_user_meta($wp_user, 'user_firstname') . ' ' . espresso_user_meta($wp_user, 'user_lastname') : espresso_user_meta($wp_user, 'display_name'); ?></td>
                                             <?php } ?>
-                                            <td><a href="admin.php?page=event_tickets&action=edit&id=<?php echo $ticket_id ?>">
-                                                    <?php _e('Edit Ticket', 'event_espresso'); ?>
+                                            <td><a href="admin.php?page=event_emails&action=edit&id=<?php echo $email_id ?>">
+                                                    <?php _e('Edit Email', 'event_espresso'); ?>
                                                 </a></td>
                                         </tr>
                                     <?php }
@@ -111,7 +112,7 @@ function event_espresso_ticket_config_mnu() {
                             <strong>
                                 <?php _e('Check All', 'event_espresso'); ?>
                             </strong>
-                            <input name="delete_ticket" type="submit" class="button-secondary" id="delete_ticket" value="<?php _e('Delete Ticket', 'event_espresso'); ?>" style="margin-left:100px;" onclick="return confirmDelete();"> <?php echo '<a href="admin.php?page=event_tickets&amp;action=add_new_ticket" style="margin-left:5px"class="button-primary">' . __('Add New Ticket', 'event_espresso') . '</a>';?>
+                            <input name="delete_email" type="submit" class="button-secondary" id="delete_email" value="<?php _e('Delete Email', 'event_espresso'); ?>" style="margin-left:100px;" onclick="return confirmDelete();"> <?php echo '<a href="admin.php?page=event_emails&amp;action=add_new_email" style="margin-left:5px"class="button-primary">' . __('Add New Email', 'event_espresso') . '</a>';?>
                         </p>
                     </form>
                 </div>
@@ -144,5 +145,5 @@ function event_espresso_ticket_config_mnu() {
     </script>
 
     <?php
-    echo event_espresso_custom_ticket_info();
+    echo event_espresso_custom_email_info();
 }
