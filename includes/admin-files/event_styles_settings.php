e0aceb807222909be74e4666943a795aa32e1e7c
Seth Shoultes <seth@eventespresso.com>
2011-09-15 20:27:55 +0000

    Renaming the style functions to keep from having duplicated function names from other plugins. Need to make sure all function names are prefixed with "espresso_".

--- includes/admin-files/event_styles_settings.php
+++ includes/admin-files/event_styles_settings.php
@@ -35,43 +35,47 @@ $files_color = array();
    closedir($dhandle);
 		}
 		
-		function is_selected($name) {
-   global $org_options;
-   $input_item = $name;
-		 $option_selections = array($org_options['selected_style'], $org_options['style_color']  );
-   if (!in_array( $input_item, $option_selections )  )
-   return false;
-   else
-   echo  'selected="selected"';
-   return; 
-  }
+	function espresso_style_is_selected($name) {
+	   global $org_options;
+	   $input_item = $name;
+			 $option_selections = array($org_options['selected_style'], $org_options['style_color']  );
+	   if (!in_array( $input_item, $option_selections )  )
+	   return false;
+	   else
+	   echo  'selected="selected"';
+	   return; 
+	}
 		('N' == $org_options['enable_default_style'] || file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "templates/event_espresso_style.css"))? $disabled = 'disabled="disabled"' : $disabled = '';
 		(!empty($disabled))? $styled = 'style="color: #ccc;"' : $styled = '';
 ?>
 <?php // var_dump($org_options['selected_style']); ?>
-<h4 <?php echo $styled ?>><?php _e('Select Alternative Espresso Styles ', 'event_espresso'); ?><a class="thickbox"  href="#TB_inline?height=400&width=500&inlineId=alternative_styles_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" /></a></h4>
-	<p>
-	<label for="base-style-select" <?php echo $styled ?>><?php _e('Select Base stylesheet', 'event_espresso');  ?></label>
-		<select id="base-style-select" class="wide" <?php echo $disabled ?> name="selected_style">
-			
-			<option <?php is_selected($fname) ?> value="event_espresso_style.css">Default style - generic</option>
-				
-				<?php foreach( $files as $fname ) { ?>
-				<option <?php is_selected($fname) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
-	<?php } ?>
 
-	</select>
-	</p>
-
-	<p>
-	<label for="style-color" <?php echo $styled ?>><?php _e('Select optional colors', 'event_espresso');  ?></label>
-		<select id="style-color" class="wide" <?php echo $disabled ?> name="style_color">
-			
-			<option <?php is_selected($fname_color) ?> value=""> - None - </option>
-				
-				<?php foreach( $files_color as $fname_color ) { ?>
-				<option <?php is_selected($fname_color) ?> value="<?php echo $fname_color ?>"><?php echo $fname_color; ?></option>
-	<?php } ?>
-
-		</select>	
-	</p>
+<h4 <?php echo $styled ?>>
+  <?php _e('Select Alternative Espresso Styles ', 'event_espresso'); ?>
+  <a class="thickbox"  href="#TB_inline?height=400&width=500&inlineId=alternative_styles_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" /></a></h4>
+<p>
+  <label for="base-style-select" <?php echo $styled ?>>
+    <?php _e('Select Base stylesheet', 'event_espresso');  ?>
+  </label>
+  <select id="base-style-select" class="wide" <?php echo $disabled ?> name="selected_style">
+    <option <?php espresso_style_is_selected($fname) ?> value="event_espresso_style.css">
+    <?php _e('Default style - generic', 'event_espresso'); ?>
+    </option>
+    <?php foreach( $files as $fname ) { ?>
+    <option <?php espresso_style_is_selected($fname) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
+    <?php } ?>
+  </select>
+</p>
+<p>
+  <label for="style-color" <?php echo $styled ?>>
+    <?php _e('Select optional colors', 'event_espresso');  ?>
+  </label>
+  <select id="style-color" class="wide" <?php echo $disabled ?> name="style_color">
+    <option <?php espresso_style_is_selected($fname_color) ?> value=""> -
+    <?php _e('None', 'event_espresso'); ?>
+    - </option>
+    <?php foreach( $files_color as $fname_color ) { ?>
+    <option <?php espresso_style_is_selected($fname_color) ?> value="<?php echo $fname_color ?>"><?php echo $fname_color; ?></option>
+    <?php } ?>
+  </select>
+</p>
