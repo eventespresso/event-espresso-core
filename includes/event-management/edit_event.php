48d1538e40900fd1fba712482548bc64f94b09a0
Seth Shoultes <seth@eventespresso.com>
2011-09-30 22:40:16 +0000

    Adjusted code formatting

--- includes/event-management/edit_event.php
+++ includes/event-management/edit_event.php
@@ -776,8 +776,8 @@ function edit_event($event_id = 0) {
         		jQuery('#upload_image_button').click(function() {
 	            formfield = jQuery('#upload_image').attr('name');
 	            tb_show('', 'media-upload.php?type=image&amp;TB_iframe=1');
-												jQuery('p.event-featured-thumb').addClass('old');	
-			         header_clicked = true;
+				jQuery('p.event-featured-thumb').addClass('old');	
+				header_clicked = true;
 	            return false;
 	           });
 	 
