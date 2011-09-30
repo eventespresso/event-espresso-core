f18614a8a5d17c4d7960f09cd3c114f747e7d2b7
Seth Shoultes <seth@eventespresso.com>
2011-09-30 20:31:57 +0000

    Added check to make sure a user is logged in when getting member price type

--- includes/functions/main.php
+++ includes/functions/main.php
@@ -772,7 +772,7 @@ if (!function_exists('espresso_ticket_information')) {
                 $sql = $wpdb->get_results("SELECT * FROM " . EVENTS_PRICES_TABLE . " WHERE id ='" . $price_option . "'");
                 $num_rows = $wpdb->num_rows;
                 if ($num_rows > 0) {
-					if (function_exists('espresso_members_installed')) {
+					if (function_exists('espresso_members_installed') && is_user_logged_in()==true) {
 						return $wpdb->last_result[0]->member_price_type;
 					}else{
 						return $wpdb->last_result[0]->price_type;
