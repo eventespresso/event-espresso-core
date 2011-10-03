46e442c029908dc49cf7bf0bf026d1f9273a1671
Seth Shoultes <seth@eventespresso.com>
2011-10-03 15:16:52 +0000

    Added espresso_get_gravatar() function

--- includes/functions/main.php
+++ includes/functions/main.php
@@ -1164,4 +1164,30 @@ function espresso_ticket_links($registration_id, $attendee_id) {
     }
 }
 
-//End
+/**
+	 * Get either a Gravatar URL or complete image tag for a specified email address.
+	 *
+	 * @param string $email The email address
+	 * @param string $s Size in pixels, defaults to 80px [ 1 - 512 ]
+	 * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+	 * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+	 * @param boole $img True to return a complete IMG tag False for just the URL
+	 * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+	 * @return String containing either just a URL or a complete image tag
+	 * @source http://gravatar.com/site/implement/images/php/
+*/
+if (!function_exists('espresso_get_gravatar')) {
+	
+	function espresso_get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = true, $atts = array() ) {
+		$url = 'http://www.gravatar.com/avatar/';
+		$url .= md5( strtolower( trim( $email ) ) );
+		$url .= "?s=$s&d=$d&r=$r";
+		if ( $img ) {
+			$url = '<img src="' . $url . '"';
+			foreach ( $atts as $key => $val )
+				$url .= ' ' . $key . '="' . $val . '"';
+				$url .= ' />';
+		}
+		return $url;
+	}
+}
