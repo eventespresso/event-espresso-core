d05608c4c82cb315ce4b98e3cb73dd8f1647152a
Hugo wth hugo <hugo@nolongerwithus.com>
2011-10-24 17:58:16 +0000

    Missed adding new file.

---
+++ includes/admin-files/venue-management/venue_help.php
@@ -0,0 +1,23 @@
+<div style="display:none">
+<?php #### help dialogue box #### ?>
+<div id="venue_locale" >
+<div class="TB-ee-frame">
+<h2><?php _e('Venue Locale/Region', 'event_espresso'); ?></h2>
+      <p><?php _e('This can be used to group venues together by locales/regions.', 'event_espresso'); ?></p>
+				<p><?php _e('Once you have created a locale in the <a href="admin.php?page=event_locales"> Manage Locales/Regions</a> page it will be available to select on the \'Add a Venue\' page', 'event_espresso')?></p>
+</div>			
+</div>
+<?php #### end help #### ?>
+
+<?php #### help dialogue box #### ?>
+<div id="venue_gmap">
+<div class="TB-ee-frame">
+<h2><?php _e('Venue Google maps', 'event_espresso'); ?></h2>
+    <p><?php _e('Two options are provided for displaying Google maps from your venue addresses.', 'event_espresso'); ?></p>
+				<p><?php _e('The first option will attempt to generate a map from the address you enter for your venue, if Google is able to locate your address it will be displayed when you view the venue edit page for the venue having selected the Yes checkbox option', 'event_espresso')?></p>
+				<p><?php _e('The second option might be useful if need to get a more precise address and allows you to enter a static Google map url that you can obtain from a google map lookup.', 'event_espresso') ?></p>
+</div>			
+</div>
+<?php #### end help #### ?>
+
+</div>
