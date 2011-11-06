43812bbb8a8a0cebcbfda093a3c7cfbb98570d96
Seth Shoultes <seth@eventespresso.com>
2011-11-06 16:20:11 +0000

    Cleaned up code formatting

--- includes/admin-files/templates/event_styles_settings.php
+++ includes/admin-files/templates/event_styles_settings.php
@@ -3,57 +3,56 @@
 $dhandle = opendir(EVENT_ESPRESSO_PLUGINFULLPATH . 'templates/css/');
 $files = array();
 
-		if ($dhandle) { //if we managed to open the directory
-   // loop through all of the files
-   while (false !== ($fname = readdir($dhandle))) {
-      // if the file is not this file, and does not start with a '.' or '..',
-      // then store it for later display
-      if ( ($fname != '.') && ($fname != '..') && ($fname != '.svn') && ($fname != 'colors') &&
-          ($fname != basename($_SERVER['PHP_SELF'])) ) {
-          // store the filename
-						$files[] = $fname;
-      }
-   }
-   // close the directory
-   closedir($dhandle);
+if ($dhandle) { //if we managed to open the directory
+	// loop through all of the files
+	while (false !== ($fname = readdir($dhandle))) {
+		// if the file is not this file, and does not start with a '.' or '..',
+		// then store it for later display
+		if ( ($fname != '.') && ($fname != '..') && ($fname != '.svn') && ($fname != 'colors') && ($fname != basename($_SERVER['PHP_SELF'])) ) {
+			// store the filename
+			$files[] = $fname;
 		}
+	}
+	// close the directory
+	closedir($dhandle);
+}
+
 $dhandle = opendir(EVENT_ESPRESSO_PLUGINFULLPATH . 'templates/css/colors/');
 $files_color = array();
 
-		if ($dhandle) { //if we managed to open the directory
-   // loop through all of the files
-   while (false !== ($fname_color = readdir($dhandle))) {
-      // if the file is not this file, and does not start with a '.' or '..',
-      // then store it for later display
-      if ( ($fname_color != '.') && ($fname_color != '..') && ($fname_color != '.svn') &&
-          ($fname != basename($_SERVER['PHP_SELF'])) ) {
-          // store the filename
-						$files_color[] = $fname_color;
-      }
-   }
-   // close the directory
-   closedir($dhandle);
+if ($dhandle) { //if we managed to open the directory
+	// loop through all of the files
+	while (false !== ($fname_color = readdir($dhandle))) {
+		// if the file is not this file, and does not start with a '.' or '..',
+		// then store it for later display
+		if ( ($fname_color != '.') && ($fname_color != '..') && ($fname_color != '.svn') && ($fname != basename($_SERVER['PHP_SELF'])) ) {
+			// store the filename
+			$files_color[] = $fname_color;
 		}
-		
-	function espresso_style_is_selected($name) {
-	   global $org_options;
-	   $input_item = $name;
-			 $option_selections = array($org_options['selected_style'], $org_options['style_color']  );
-	   if (!in_array( $input_item, $option_selections )  )
-	   return false;
-	   else
-	   echo  'selected="selected"';
-	   return; 
 	}
-		('N' == $org_options['enable_default_style'] || file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "templates/event_espresso_style.css"))? $disabled = 'disabled="disabled"' : $disabled = '';
-		(!empty($disabled))? $styled = 'style="color: #ccc;"' : $styled = '';
+	// close the directory
+	closedir($dhandle);
+}
+		
+function espresso_style_is_selected($name) {
+	global $org_options;
+	$input_item = $name;
+	$option_selections = array($org_options['selected_style'], $org_options['style_color']  );
+	if (!in_array( $input_item, $option_selections )  ){
+		return false;
+	}else{
+		echo  'selected="selected"';
+		return;
+	}
+}
+$disabled = ( $org_options['enable_default_style'] == 'N' || file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "templates/event_espresso_style.css") ) ? 'disabled="disabled"' : '';
+$styled = ( !empty($disabled) ) ? 'style="color: #ccc;"' : '';
 ?>
 <?php // var_dump($org_options['selected_style']); ?>
 
 <p class="section-heading" <?php echo $styled ?>>
   <?php _e('Select Alternative Espresso Styles ', 'event_espresso'); ?>
-  <a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=alternative_styles_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
-</p>
+  <a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=alternative_styles_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a> </p>
 <p>
   <label for="base-style-select" <?php echo $styled ?>>
     <?php _e('Select Base stylesheet', 'event_espresso');  ?>
