164a80d27506e1fc9aa29910d2d15949dbe8a00d
Seth Shoultes <seth@eventespresso.com>
2011-11-06 04:21:54 +0000

    Removing the trailing ?>

--- includes/venue_management.php
+++ includes/venue_management.php
@@ -4,4 +4,3 @@ function event_espresso_venue_config_mnu(){
 	echo espresso_premium_feature(); 
 }
 //The files for this feature are stored in the "includes/admin-files/venue-management" directory
-?>
