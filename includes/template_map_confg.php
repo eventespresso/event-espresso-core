9de8c94a987f4b06b72d12a9301b96f3e0444e4a
Seth Shoultes <seth@eventespresso.com>
2011-11-06 04:21:28 +0000

    Adding a dummy page for the free version to point to in case the template_map_confg file is not present

---
+++ includes/template_map_confg.php
@@ -0,0 +1,6 @@
+<?php 
+//This is a premium feature, please upgrade
+function event_espresso_manage_maps(){
+	echo espresso_premium_feature(); 
+}
+//The files for this feature are stored in the "includes/admin-files/" directory
