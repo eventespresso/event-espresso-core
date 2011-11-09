4530342bd2d4d5ccc1b22b135acff6cd85c77ba9
Seth Shoultes <seth@eventespresso.com>
2011-11-09 00:10:38 +0000

    Added $atts array so that the correct values can be passed to the espresso_price_select_action action to be passed event_espresso_price_dropdown. [touch:348]

--- includes/functions/actions.php
+++ includes/functions/actions.php
@@ -1,13 +1,13 @@
 <?php
 //Show pricing in a dropdown or text
 if ( !function_exists('espresso_price_select_action') ){
-	function espresso_price_select_action($event_id){
+	function espresso_price_select_action($event_id, $atts = array() ){
 		$html = '';
 		$html .= is_admin() ? '' : '<p class="event_prices">';
-		$html .= event_espresso_price_dropdown($event_id);
+		$html .= event_espresso_price_dropdown($event_id, $show_label, $multi_reg, $current_value, $label);
 		$html .= is_admin() ? '' : '</p>';
 		echo $html;
 		return;
 	}
-	add_action('espresso_price_select', 'espresso_price_select_action', 10, 1);
+	add_action('espresso_price_select', 'espresso_price_select_action', 10, 2);
 }
