66760f535a725c6010c1fdeb163b538baddb39e6
Seth Shoultes <seth@eventespresso.com>
2011-11-12 08:22:22 +0000

    Added ThemeRoller help boxes

--- includes/admin-files/templates/templates_help.php
+++ includes/admin-files/templates/templates_help.php
@@ -56,15 +56,47 @@
 	</div>
 	<?php
 		/**
-		 * Select Alternative stylesheets Help Box
+		 * Custom Templates Help Box
+		 */
+	?>
+	<div id="custom_templates_info" class="pop-help" >
+		<div class="TB-ee-frame">
+			<h2>
+				<?php _e('Custom Templates', 'event_espresso'); ?>
+			</h2>
+			<p>
+				<?php _e('Sometimes these files will cause problems when Event Espresso has been updated. If you have moved these files and having problems, you can disable this feature to load the default files.', 'event_espresso'); ?>
+			</p>
+			
+		</div>
+	</div>
+	<?php
+		/**
+		 * ThemeRoller Style Settings Help Box
 		 */
 	?>
 	<div id="themeroller_info" class="pop-help" >
 		<div class="TB-ee-frame">
 			<h2>
-				<?php _e('Themeroller Style Settings ', 'event_espresso'); ?>
+				<?php _e('ThemeRoller Style Settings ', 'event_espresso'); ?>
 			</h2>
 			<p>
+			<?php _e('Event Espresso uses the <a href="http://themeroller.com/" target="_blank">jQuery UI ThemeRoller</a> CSS library to style the event listings and registration pages. The ThemeRoller CSS library makes it easy to get the desired look and feel for your events and calendar. If the built-in style are activated, Event Espresso will automatically load the "themeroller-base.css" style sheet which applies the necessary padding and margins to the Event Espresso generated content.', 'event_espresso'); ?>
+			</p>
+			<p><strong><?php _e('Adding Custom ThemeRoller Styles', 'event_espresso'); ?></strong><br />
+			<?php _e('To add your own custom ThemeRoller styles: ', 'event_espresso'); ?>
+			<ol>
+			<li><?php _e('Go to the <a href="http://themeroller.com/" target="_blank">jQuery UI ThemeRoller</a> site and design and download your custom style.', 'event_espresso'); ?></li> 
+			<li><?php _e('Unzip the downloaded file, then navigate to the "/css/" folder. Inside you will see a folder named "custom-theme" (or the name of the theme if you selected a pre-made style). If you like, rename the "custom-theme" folder to something memorable.', 'event_espresso'); ?></li>
+			<li><?php _e('This step is very important. Inside the "custom-theme" folder, find a file named "jquery-ui-*.*.**.custom.css". Rename that file to "style.css". If you do not rename this file, you will not see your custom theme in the selections.', 'event_espresso'); ?></li>
+			<li><?php _e('Upload your desired ThemeRoller styles to the "wp-content/uploads/espresso/themeroller/" folder on your server.', 'event_espresso'); ?></li>
+			<li><?php _e('This step is very important. Create an empty "index.php" file in the "wp-content/uploads/espresso/themeroller/" folder. This will tell Event Espresso that you want to use custom ThemeRoller CSS files.', 'event_espresso'); ?></li>
+			<li><strong><?php _e('IMPORTANT:', 'event_espresso'); ?></strong> <?php _e('Verify you have an "wp-content/uploads/espresso/themeroller/index.php" file. Verify you have uploaded your custom ThemeRoller CSS files files"wp-content/uploads/espresso/themeroller/custom-theme/style.css"', 'event_espresso'); ?> </li>
+			</ol>
+			</p>
+			<p>
+				<strong><?php _e('About ThemeRoller', 'event_espresso'); ?></strong>
+				<br />
 				<?php _e('ThemeRoller is a web app that offers a fun and intuitive interface for designing and downloading custom themes for jQuery UI. You can find ThemeRoller in the "Themes" section of the jQuery UI site, or by following this link: <a href="http://themeroller.com/" target="_blank">jQuery UI ThemeRoller</a>', 'event_espresso'); ?>
 			</p>
 		</div>
