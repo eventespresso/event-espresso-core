87329a512adbc7f9fa49ccae76f571c6d97c3b94
Seth Shoultes <seth@eventespresso.com>
2011-11-15 07:50:32 +0000

    Cleaned up spaces and formatting

--- includes/admin-files/event-management/promotions_box.php
+++ includes/admin-files/event-management/promotions_box.php
@@ -1,23 +1,23 @@
 <div id="event-discounts" class="postbox">
-      <div class="handlediv" title="Click to toggle"><br>
-      </div>
-      <h3 class="hndle"><span>
-        <?php _e('Event Promotions','event_espresso'); ?>
-        </span></h3>
-      <div class="inside">
-       
-                <p><strong><?php _e('Early Registration Discount','event_espresso'); ?></strong></p>
-                
-                <p><label><?php _e('End Date:','event_espresso'); ?></label><input type="text" class="datepicker" size="12" id="early_disc_date" name="early_disc_date" value="<?php echo isset($early_disc_date)?$early_disc_date:''; ?>"/></p>
-                <p class="promo-amnts"><span class="promo-amnt"><label><?php _e('Amount:','event_espresso'); ?></label><input type="text" size="3" id="early_disc" name="early_disc" value="<?php echo isset($early_disc)?$early_disc:''; ?>" /></span><span class="promo-pc"> <label><?php _e('Percentage:','event_espresso') ?></label> 		
-               	<?php echo select_input('early_disc_percentage', $values, !isset($early_disc_percentage) ? '' : $early_disc_percentage);?></p>
+	  <div class="handlediv" title="Click to toggle"><br>
+	  </div>
+	  <h3 class="hndle"><span>
+		<?php _e('Event Promotions','event_espresso'); ?>
+		</span></h3>
+	  <div class="inside">
+	   
+				<p><strong><?php _e('Early Registration Discount','event_espresso'); ?></strong></p>
+				
+				<p><label><?php _e('End Date:','event_espresso'); ?></label><input type="text" class="datepicker" size="12" id="early_disc_date" name="early_disc_date" value="<?php echo isset($early_disc_date)?$early_disc_date:''; ?>"/></p>
+				<p class="promo-amnts"><span class="promo-amnt"><label><?php _e('Amount:','event_espresso'); ?></label><input type="text" size="3" id="early_disc" name="early_disc" value="<?php echo isset($early_disc)?$early_disc:''; ?>" /></span><span class="promo-pc"> <label><?php _e('Percentage:','event_espresso') ?></label> 		
+			   	<?php echo select_input('early_disc_percentage', $values, !isset($early_disc_percentage) ? '' : $early_disc_percentage);?></p>
 				<p><?php _e('(Leave blank if not applicable)', 'event_espresso'); ?></p>
-        <p class="disc-codes">
-          <label><?php _e('Allow discount codes?','event_espresso');?></label>
+		<p class="disc-codes">
+		  <label><?php _e('Allow discount codes?','event_espresso');?></label>
 					
 			<?php echo select_input('use_coupon_code', $values, !isset($use_coupon_code) || $use_coupon_code ==''?'N':$use_coupon_code); ?>
-          <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=coupon_code_info"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL?>/images/question-frame.png" width="16" height="16" /></a></p>
-        <?php
+		  <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=coupon_code_info"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL?>/images/question-frame.png" width="16" height="16" /></a></p>
+		<?php
 		$sql = "SELECT * FROM ". EVENTS_DISCOUNT_CODES_TABLE;
 		if (function_exists('espresso_member_data') ) {
 			$wpdb->get_results("SELECT wp_user FROM " . EVENTS_DETAIL_TABLE . " WHERE id = '" . $event_id . "'");
@@ -29,7 +29,7 @@
 				$sql .= " wp_user = '" . $wp_user ."' ";
 			}
 		}
-        $event_discounts = $wpdb->get_results($sql);
+		$event_discounts = $wpdb->get_results($sql);
 		foreach ($event_discounts as $event_discount){
 			$discount_id = $event_discount->id;
 			$coupon_code = $event_discount->coupon_code;
@@ -44,7 +44,7 @@
 			}	
 			
 			echo '<p><a href="admin.php?page=discounts" target="_blank">'.__('Manage Promotional Codes ', 'event_espresso').'</a></p>';
-            ?>
-      </div>
-    </div>
-    <!-- /event-discounts -->
+			?>
+	  </div>
+	</div>
+	<!-- /event-discounts -->
