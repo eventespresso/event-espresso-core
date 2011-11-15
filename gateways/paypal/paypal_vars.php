5bd730d8cfd007c85a219db6e273412d765664b9
Seth Shoultes <seth@eventespresso.com>
2011-11-15 19:37:19 +0000

    Cleaned up some code and added the "isset" fix from Josh. [touch:376]

--- gateways/paypal/paypal_vars.php
+++ gateways/paypal/paypal_vars.php
@@ -6,8 +6,6 @@ include_once ('Paypal.php');
 echo '<!-- Event Espresso PayPal Gateway Version ' . $paypal_gateway_version . '-->';
 $myPaypal = new Paypal(); // initiate an instance of the class
 global $org_options;
-//echo $registration_id;
-//global $attendee_id;
 $paypal_settings = get_option('event_espresso_paypal_settings');
 $paypal_id = empty($paypal_settings['paypal_id']) ? '' : $paypal_settings['paypal_id'];
 //$image_url = $paypal_settings['button_url'];
@@ -18,7 +16,6 @@ if ($use_sandbox == 1) {
     // Enable test mode if needed
     $myPaypal->enableTestMode();
 }
-
 
 $attendees = array();
 $primary_registration_id = "";
@@ -82,10 +79,6 @@ if ($attendee_id > 0 && !empty($primary_registration_id) && strlen($primary_regi
             }
         }
     }
-//    $discount = 0;
-//    if ($amount_pd < $total_cost) {
-//        $discount = $total_cost - $amount_pd;
-//    }
 }
 
 #############################
@@ -96,27 +89,13 @@ $myPaypal->addField('business', $paypal_id);
 $myPaypal->addField('return', home_url() . '/?page_id=' . $org_options['return_url'] . '&id=' . $attendee_id);
 $myPaypal->addField('cancel_return', home_url() . '/?page_id=' . $org_options['cancel_return']);
 $myPaypal->addField('notify_url', home_url() . '/?page_id=' . $org_options['notify_url'] . '&id=' . $attendee_id . '&event_id=' . $event_id . '&attendee_action=post_payment&form_action=payment');
-//$myPaypal->addField('item_name', $event_name . ' | '.__('Reg. ID:','event_espresso').' '.$attendee_id. ' | '.__('Name:','event_espresso').' '. $attendee_name .' | '.__('Total Registrants:','event_espresso').' '.$num_people);
-//echo espresso_quantity_for_registration($attendee_id);
+
 $event_meta = event_espresso_get_event_meta($event_id);
 if (count($attendees) > 0) {
-    #$myPaypal->addField('cmd', '_cart');
     $myPaypal->addField('upload', '1');
-    #if ($event_meta['additional_attendee_reg_info'] == 1 || espresso_quantity_for_registration($attendee_id) >1) {
-    #    $div = $event_cost / $quantity;
-    #    for ($i = 1; $i <= $quantity; $i++) {
-    #        $myPaypal->addField('item_number_' . $i, $registration_id);
-    #        $myPaypal->addField('item_name_' . $i, stripslashes_deep($event_name) . ' | ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($fname . ' ' . $lname) . ' | ' . __('Registrant Email:', 'event_espresso') . ' ' . $attendee_email);
-    #        $myPaypal->addField('amount_' . $i, number_format($div, 2, '.', ''));
-    #    }
-    #} else {
-    #$sql = "SELECT * FROM " . EVENTS_ATTENDEE_TABLE . " WHERE registration_id='" . $registration_id . "' ORDER BY id ";
-    //echo $sql;
-    #$data = $wpdb->get_results($sql, ARRAY_A);
-    #if ($wpdb->num_rows > 0) {
+
     $i = 1;
     $div = $event_cost / $quantity;
-    #foreach ($data as $row)
     if ( count($attendees) == 1 ) {
         $myPaypal->addField('cmd', '_xclick');
         foreach ($attendees as $attendee) {
@@ -132,10 +111,6 @@ if (count($attendees) > 0) {
         
         $myPaypal->addField('cmd', '_cart');
         foreach ($attendees as $attendee) {
-            #$afname = $row['fname'];
-            #$alname = $row['lname'];
-            #$aemail = $row['email'];
-            #$myPaypal->addField('item_number_' . $i, $registration_id);
             $myPaypal->addField('item_name_' . $i, $attendee['attendee_info']); #stripslashes_deep($event_name) . ' | ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($afname . ' ' . $alname) . ' | ' . __('Registrant Email:', 'event_espresso') . ' ' . $aemail);
             $myPaypal->addField('amount_' . $i, number_format($attendee['unit_price'], 2, '.', '')); #number_format($div, 2, '.', ''));
             $myPaypal->addField('quantity_' . $i, $attendee['quantity']);
@@ -146,16 +121,8 @@ if (count($attendees) > 0) {
             $i++;
         }
     }  
-    #}
-    #}
+   
 }
-
-#else
-#{
-#    $myPaypal->addField('item_number', $registration_id);
-#    $myPaypal->addField('item_name', stripslashes_deep($event_name) . ' | ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($fname . ' ' . $lname) . ' | ' . __('Registrant Email:', 'event_espresso') . ' ' . $attendee_email);
-#    $myPaypal->addField('amount', number_format($event_cost, 2, '.', ''));
-#}
 
 $myPaypal->addField('currency_code', $paypal_cur);
 $myPaypal->addField('image_url', empty($paypal_settings['image_url']) ? '' : $paypal_settings['image_url']);
@@ -169,10 +136,6 @@ $myPaypal->addField('address1', $address);
 $myPaypal->addField('city', $city);
 $myPaypal->addField('state', $state);
 $myPaypal->addField('zip', $zip);
-
-//if ($amount_pd < $total_cost) {
-//    $myPaypal->addField('discount_amount_1', number_format($total_cost - $amount_pd, 2, '.', ''));
-//}
 
 
 //Enable this function if you want to send payment notification before the person has paid.
@@ -188,7 +151,7 @@ if (!empty($paypal_settings['bypass_payment_page']) && $paypal_settings['bypass_
         } else {
             $button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/paypal/btn_stdCheckout2.gif";
         }
-    } elseif (file_exists($paypal_settings['button_url'])) {
+    } elseif (isset($paypal_settings['button_url'])) {
         $button_url = $paypal_settings['button_url'];
     } else {
         //If no other buttons exist, then use the default location
