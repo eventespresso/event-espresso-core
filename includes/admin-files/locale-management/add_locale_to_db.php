3335b1ec2affc1456dd03a0e5c172b4126c0ecd4
Seth Shoultes <seth@eventespresso.com>
2011-11-15 08:10:07 +0000

    Adding support for "login as user" to the locale manager. [touch:378]

--- includes/admin-files/locale-management/add_locale_to_db.php
+++ includes/admin-files/locale-management/add_locale_to_db.php
@@ -1,34 +1,23 @@
 <?php
 function add_locale_to_db(){
-	global $wpdb, $current_user;
+	global $wpdb, $espresso_wp_user, $notices;
 	$wpdb->show_errors();
 	if ( $_REQUEST['action'] == 'add' ){
 		//print_r($_REQUEST);
-		$identifier=uniqid($current_user->ID.'-');
-		if (!function_exists('espresso_member_data'))
-			$current_user->ID = 1;
-		$sql=array('identifier'=>$identifier, 'name'=>$_REQUEST['name'], 'wp_user'=>$current_user->ID); 
+		$identifier=uniqid($espresso_wp_user.'-');
+		
+		$sql=array(
+			'identifier'=>$identifier,
+			'name'=>$_REQUEST['name'],
+			'wp_user'=>$espresso_wp_user
+		); 
+		
 		$sql_data = array('%s','%s','%d');		
 		  
-		 if ($wpdb->insert( EVENTS_LOCALE_TABLE, $sql, $sql_data )){?>
-				<div id="message" class="updated fade">
-				  <p><strong>
-					<?php _e('The locale  has been added.','event_espresso'); ?>
-					</strong></p>
-				</div>
-	<?php 
-			}else{ ?>
-				<div id="message" class="error">
-				  <p><strong>
-					<?php _e('The locale  was not saved.','event_espresso'); ?>
-					</strong></p>
-                    <?php echo 'Debug: <br />';
-					  print_r($sql);
-					  print 'Number of vars: ' . count ($sql);
-					  echo '<br />';
-					  print 'Number of cols: ' . count($sql_data);?>
-				</div>
-		<?php
-			}
+		if ($wpdb->insert( EVENTS_LOCALE_TABLE, $sql, $sql_data )){
+			$notices['updates'][] = __('The locale ', 'event_espresso') . $sql['name'] .  __(' has been added', 'event_espresso');
+		}else{ 
+			$notices['errors'][] = __('The locale', 'event_espresso') . $sql['name'] .  __(' was not saved!', 'event_espresso');		
+		}
 	}
 }
--- includes/admin-files/locale-management/update_locale.php
+++ includes/admin-files/locale-management/update_locale.php
@@ -1,20 +1,15 @@
 <?php 
 function update_event_locale(){
-	global $wpdb;
+	global $wpdb, $notices;
 	$wpdb->show_errors();
 	//print_r($_REQUEST);
 	$sql=array('name'=>$_REQUEST['name']); 
 	$update_id = array('id'=> $_REQUEST['locale_id']);
 	$sql_data = array('%s','%s','%s','%s','%s','%s','%s','%s','%s');
-	$wpdb->update( EVENTS_LOCALE_TABLE, $sql, $update_id, $sql_data, array( '%d' ) );
-	/*echo 'Debug: <br />';
-                    print_r($sql);
-                    print 'Number of vars: ' . count ($sql);
-                    echo '<br />';
-                    print 'Number of cols: ' . count($sql_data);*/
-?>
-		<div id="message" class="updated fade">
-			<p><strong><?php _e('The locale  has been updated.','event_espresso'); ?></strong></p>
-         </div>
-<?php
+	
+	if ($wpdb->update( EVENTS_LOCALE_TABLE, $sql, $update_id, $sql_data, array( '%d' ) )){
+		$notices['updates'][] = __('The locale ', 'event_espresso') . $sql['name'] .  __(' has been updated', 'event_espresso');
+	}else{ 
+		$notices['errors'][] = __('The locale', 'event_espresso') . $sql['name'] .  __(' was not updated!', 'event_espresso');		
+	}
 }
