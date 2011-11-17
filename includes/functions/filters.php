0db22d88ee88ab82ce1d0831aa525253b07b5a19
Seth Shoultes <seth@eventespresso.com>
2011-11-17 06:03:16 +0000

    Adding the espresso_help_popup function and espresso_help filter

--- includes/functions/filters.php
+++ includes/functions/filters.php
@@ -3,3 +3,8 @@ function espresso_admin_footer () {
 	echo 'Event Registration and Ticketing Powered by <a href="http://eventespresso.com/" title="Event Registration Powered by Event Espresso" target="_blank">'.EVENT_ESPRESSO_POWERED_BY.'</a>';
 }
 add_filter('admin_footer_text', 'espresso_admin_footer');
+
+function espresso_help_popup($name){
+	echo  '<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=' . $name . '" target="_blank"><img src="' . EVENT_ESPRESSO_PLUGINFULLURL . 'images/question-frame.png" width="16" height="16" alt="" /></a>';
+}
+add_filter('espresso_help', 'espresso_help_popup');
