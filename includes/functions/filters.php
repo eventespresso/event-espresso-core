9d68c567f0cfba6e5ce8a881f4c22a3b9ca2b014
Seth Shoultes <seth@eventespresso.com>
2011-11-17 05:21:28 +0000

    Moving functions around

--- includes/functions/filters.php
+++ includes/functions/filters.php
@@ -1,1 +1,5 @@
 <?php
+function espresso_admin_footer () {
+	echo 'Event Registration and Ticketing Powered by <a href="http://eventespresso.com/" title="Event Registration Powered by Event Espresso" target="_blank">'.EVENT_ESPRESSO_POWERED_BY.'</a>';
+}
+add_filter('admin_footer_text', 'espresso_admin_footer');
