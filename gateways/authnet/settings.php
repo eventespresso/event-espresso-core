73c0452614e6abb5e391450ada3dde69d54d05d8
Hugo wth hugo <hugo@nolongerwithus.com>
2011-11-18 08:31:14 +0000

    re-factor Aut.net gateway settings page, 'chosen', help icons positions

--- gateways/authnet/settings.php
+++ gateways/authnet/settings.php
@@ -84,49 +84,55 @@ function event_espresso_display_authnet_settings() {
 							<label for="authnet_login_id">
 								<?php _e('Authorize.net Login I.D.', 'event_espresso'); ?>
 							</label>
-							<br />
 							<input type="text" name="authnet_login_id" size="35" value="<?php echo $authnet_settings['authnet_login_id']; ?>">
 						</li>
+						
 						<li>
 							<label for="authnet_transaction_key">
 								<?php _e('Authorize.net Transaction Key', 'event_espresso'); ?>
 							</label>
-							<br />
 							<input type="text" name="authnet_transaction_key" size="35" value="<?php echo $authnet_settings['authnet_transaction_key']; ?>">
 						</li>
+						
 						<li>
 							<label for="button_url">
-								<?php _e('Button Image URL: ', 'event_espresso'); ?>
+								<?php _e('Button Image URL: ', 'event_espresso'); ?><?php apply_filters('espresso_help', 'button_image') ?>
 							</label>
-							<br />
 							<input type="text" name="button_url" size="35" value="<?php echo (($authnet_settings['button_url'] == '') ? $button_url : $authnet_settings['button_url'] ); ?>" />
-							<a href="media-upload.php?post_id=0&amp;type=image&amp;TB_iframe=true&amp;width=640&amp;height=580&amp;rel=button_url" id="add_image" class="thickbox" title="Add an Image"><img src="images/media-button-image.gif" alt="Add an Image"></a>
-							<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=button_image"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a></li>
-						<li>
+							<a href="media-upload.php?post_id=0&amp;type=image&amp;TB_iframe=true&amp;width=640&amp;height=580&amp;rel=button_url" id="add_image" class="thickbox user-add-images" title="Add an Image"><img src="images/media-button-image.gif" alt="Add an Image"></a>
+							</li>
+							
+							<li>
 							<label for="image_url">
-								<?php _e('Image URL:', 'event_espresso'); ?>
+								<?php _e('Image URL: ', 'event_espresso'); ?><?php apply_filters('espresso_help', 'authnet_image_url_info') ?>
 							</label>
-							<br />
+							
 							<input type="text" name="image_url" size="35" value="<?php echo $authnet_settings['image_url']; ?>" />
-							<a href="media-upload.php?post_id=0&amp;type=image&amp;TB_iframe=true&amp;width=640&amp;height=580&amp;rel=image_url" id="add_image" class="thickbox" title="Add an Image"><img src="images/media-button-image.gif" alt="Add an Image"></a>
-							<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=authnet_image_url_info"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a><br />
-							<?php _e('(used for your business/personal logo on the Authorize.net SIM payment page)', 'event_espresso'); ?>
+							<a href="media-upload.php?post_id=0&amp;type=image&amp;TB_iframe=true&amp;width=640&amp;height=580&amp;rel=image_url" id="add_image" class="thickbox user-add-images" title="Add an Image"><img src="images/media-button-image.gif" alt="Add an Image">
+							
+							<?php _e(' (used for your business/personal logo on the Authorize.net SIM payment page)', 'event_espresso'); ?></a>
 						</li>
-					</ul></td>
-				<td valign="top"><ul>
+					</ul>
+				</td>
+				<td valign="top">
+					<ul>
 						<li>
-							<?php _e('Relay Response URL :', 'event_espresso'); ?>
-							<br />
-							<span class="display-path" style="background-color: rgb(255, 251, 204); border:#999 solid 1px; padding:2px;"><?php echo home_url() . '/?page_id=' . $org_options['notify_url']; ?></span> &nbsp;<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=relay_response"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a> </li>
+							<label>
+								<?php _e('Relay Response URL: ', 'event_espresso'); ?><?php apply_filters('espresso_help', 'relay_response') ?>
+							</label>
+							<span class="display-path" style="background-color: rgb(255, 251, 204); border:#999 solid 1px; padding:2px;"><?php echo home_url() . '/?page_id=' . $org_options['notify_url']; ?></span> 
+						</li>
+						
 						<li>
 							<label for="use_sandbox">
-								<?php _e('Use the test mode feature for Autorize.net SIM?', 'event_espresso'); ?>
+								<?php _e('Use the test mode feature for Autorize.net SIM? ', 'event_espresso'); ?><?php apply_filters('espresso_help', 'authnet_sandbox') ?>
 							</label>
 							<input name="use_sandbox" type="checkbox" value="1" <?php echo $authnet_settings['use_sandbox'] == "1" ? 'checked="checked"' : '' ?> />
-							<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=authnet_sandbox"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a> </li>
+						</li>
+						
 						<li>
 							<label for="bypass_payment_page">
-								<?php _e('By-pass the payment confirmation page?', 'event_espresso'); ?>
+								<?php _e('By-pass the payment confirmation page? ', 'event_espresso'); ?><?php apply_filters('espresso_help', 'bypass_confirmation') ?>
 							</label>
 							<?php
 							$values = array(
@@ -134,12 +140,14 @@ function event_espresso_display_authnet_settings() {
 									array('id' => 'N', 'text' => __('No', 'event_espresso')));
 							echo select_input('bypass_payment_page', $values, $authnet_settings['bypass_payment_page']);
 							?>
-							&nbsp;<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=bypass_confirmation"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a> </li>
+						</li>
+						
 						<li>
-							<?php _e('Current Button Image:', 'event_espresso'); ?>
+							<?php _e('Current Button Image: ', 'event_espresso'); ?>
 							<br />
 							<?php echo (($authnet_settings['button_url'] == '') ? '<img src="' . $button_url . '" />' : '<img src="' . $authnet_settings['button_url'] . '" />'); ?></li>
-					</ul></td>
+					</ul>
+				</td>
 			</tr>
 		</table>
 		<p>
@@ -149,11 +157,15 @@ function event_espresso_display_authnet_settings() {
 	</form>
 	<div id="relay_response" style="display:none">
 		<h2><?php _e('Relay Response', 'event_espresso'); ?></h2>
+		
 		<p><?php _e('This shows the specific the URL to which the gateway should return the relay response for a transaction. This the page should be set in your Authorize.net account. Login to Authorize.net, goto Account > Response/Receipt URLs > Add URL and enter the following URL.', 'event_espresso'); ?></p>
+		
 		<p><strong><?php _e('Relay Response URL:', 'event_espresso'); ?></strong> <?php echo home_url() . '/?page_id=' . $org_options['notify_url'] ?><br />
 			<span style="color:red;"><?php _e('Note:', 'event_espresso'); ?></span> <?php _e('This URL can be changed in the "Organization Settings" page.', 'event_espresso'); ?></p>
 		<p><?php _e('For complete information on configuring relay response, please refer to', 'event_espresso'); ?> <a href="https://account.authorize.net/help/Merchant_Interface_RoboHelp_Project.htm#Miscellaneous/Reference.htm%3E%3Epan=2"><?php _e('Reference &amp; User Guides', 'event_espresso'); ?></a>.</p>
+	
 	</div>
+	
 	<div id="authnet_image_url_info" style="display:none">
 		<h2>
 			<?php _e('Authorize.net SIM Image URL (logo for payment page)', 'event_espresso'); ?>
@@ -162,6 +174,7 @@ function event_espresso_display_authnet_settings() {
 			<?php _e('The URL of the image displayed as your logo in the header of the Authorize.net checkout pages.', 'event_espresso'); ?>
 		</p>
 	</div>
+	
 	<div id="authnet_sandbox" style="display:none">
 		<h2><?php _e('Authorize.net Test Mode', 'event_espresso'); ?></h2>
 		<p><?php _e('Test Mode allows you to submit test transactions to the payment gateway. Transactions that are submitted while Test Mode is ON are NOT actually processed. The result of a transaction depends on the card number submitted, and the invoice amount. If you want a transaction to be approved, use one of the following card numbers.', 'event_espresso'); ?></p><p>370000000000002 (<?php _e('American Express', 'event_espresso'); ?>)<br />6011000000000012 (<?php _e('Discover', 'event_espresso'); ?>)<br />5424000000000015 (<?php _e('Master Card', 'event_espresso'); ?>)<br />4007000000027 (<?php _e('Visa', 'event_espresso'); ?>)</p>
