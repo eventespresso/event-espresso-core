5ba1ac696c2df572c06c2fa6c664bb4909f20acf
Hugo wth hugo <hugo@nolongerwithus.com>
2011-11-18 00:45:47 +0000

    clean up promo page 'chosen'

--- includes/admin-files/coupon-management/new_discount.php
+++ includes/admin-files/coupon-management/new_discount.php
@@ -11,61 +11,82 @@ function add_new_event_discount(){
 	  <?php _e('Add a New Promotional Code','event_espresso'); ?>
 	</h3>
 	<div class="inside">
-	  <form id="new-promo-code" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
+		<form id="new-promo-code" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
 		<input type="hidden" name="action" value="add">
 		<ul>
 		  <li>
+			<p class="inputunder">
 			<label for="coupon_code">
 			  <?php _e('Promotional Code','event_espresso'); ?>
-			  <em title="<?php _e('This field is required', 'event_espresso') ?>"> *</em> </label>
+			  <em title="<?php _e('This field is required', 'event_espresso') ?>"> *</em> <?php apply_filters('espresso_help', 'coupon_code_info') ?></label>
 			<input  id="coupon_code" type="text" name="coupon_code" size="25" />
-			<a class="thickbox"  href="#TB_inline?height=400&width=500&inlineId=coupon_code_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" /></a>
-		  </li>
+			
+		  </p>
+			</li>
 		  <li>
+			<p>
 			<label><strong>
 			  <?php _e('Limited quantity active?','event_espresso'); ?>
-			  </strong></label>
+			  </strong>
+			</label>
+			
 			<?php 
 	  			echo select_input('use_limit', $values, 'N'); ?>
+			</p>
+			<p>
 			<label><strong>
-			  <?php _e('Quantity:','event_espresso'); ?>
+			  <?php _e('Quantity: ','event_espresso'); ?><?php apply_filters('espresso_help', 'qty_info') ?>
 			  </strong></label>
 			<input type="text" name="quantity" size="7">
-			<a class="thickbox"  href="#TB_inline?height=400&width=500&inlineId=qty_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" /></a>
-		  </li>
-		  <li>
+		
+		  </p>
+			</li>
+		  
+			<li>
+			<p>
 			<label><strong>
 			  <?php _e('Expiration active?','event_espresso'); ?>
 			  </strong></label>
-			<?php 
-	  			echo select_input('use_exp_date', $values, 'N'); ?>
+			
+	  		<?php 	echo select_input('use_exp_date', $values, 'N'); ?>
+			</p>
+			<p>
 			<label><strong>
-			  <?php _e('Expiration Date:','event_espresso'); ?>
+			  <?php _e('Expiration Date: ','event_espresso'); ?><?php apply_filters('espresso_help', 'exp_date_info') ?>
 			  </strong></label>
 			<input type="text" class="datepicker" size="12" id="exp_date" name="exp_date" />
-			<a class="thickbox"  href="#TB_inline?height=400&width=500&inlineId=exp_date_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" /></a>
-		  </li>
-		  <li>
+		
+		  </p>
+			</li>
+		  
+			<li>
+			<p>
 			<label><strong>
-			  <?php _e('Price Discount','event_espresso'); ?>
+			  <?php _e('Price Discount ','event_espresso'); ?>
 			  </strong></label>
 			<input type="text" name="coupon_code_price" size="7" value="<?php echo $coupon_code_price;?>" />
+			</p>
+			<p>
 			<label><strong>
-			  <?php _e('Percentage discount?','event_espresso'); ?>
+			  <?php _e('Percentage discount? ','event_espresso'); ?><?php apply_filters('espresso_help', 'discount_amount_info') ?>
 			  </strong></label>
 			<?php 
 	  			echo select_input('use_percentage', $values, 'N'); ?>
-				<a class="thickbox"  href="#TB_inline?height=400&width=500&inlineId=discount_amount_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" /></a>
-		  </li>
-		  <li>
+		  </p>
+			</li>
+		  
+			<li>
+			<p>
 			<label><strong>
-			  <?php _e('Promotional Code Description','event_espresso'); ?>
+			  <?php _e('Promotional Code Description ','event_espresso'); ?><?php apply_filters('espresso_help', 'description_info') ?>
 			  </strong></label> 
-			  <a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=description_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a><br />
+			 
 			<br />
 			<textarea rows="5" cols="300" name="coupon_code_description" id="coupon_code_description_new"  class="my_ed"></textarea>
-		  </li>
-		  <li>
+		  </p>
+			</li>
+		  
+			<li>
 			<p>
 			  <input class="button-secondary" type="submit" name="add_new_discount" value="<?php _e('Submit','event_espresso'); ?>" id="add_new_discount" />
 								<?php wp_nonce_field( 'espresso_form_check', 'add_new_promocode' ) ?>
