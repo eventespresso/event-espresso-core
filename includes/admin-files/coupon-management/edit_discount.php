460589472ae6670f8809c548ae8c428f7e95c638
Seth Shoultes <seth@eventespresso.com>
2011-11-22 20:48:33 +0000

    Promotional Codes are done are done [touch:407]

--- includes/admin-files/coupon-management/edit_discount.php
+++ includes/admin-files/coupon-management/edit_discount.php
@@ -18,104 +18,90 @@ function edit_event_discount(){
 				$exp_date = $event_discount->exp_date;
 			}
 	$values=array(					
-				array('id'=>'N','text'=> __('No','event_espresso')),
-				array('id'=>'Y','text'=> __('Yes','event_espresso')));
-	?>
+		array('id'=>'N','text'=> __('No','event_espresso')),
+		array('id'=>'Y','text'=> __('Yes','event_espresso'))
+	);
+?>
 
-<!--Add event display-->
+<!--Add discount display-->
 
 <div class="metabox-holder">
-  <div class="postbox">
-	<h3>
-	  <?php _e('Edit Code:','event_espresso'); ?>
-	  <?php echo $coupon_code ?></h3>
-	<div class="inside">
-	  <form id="edit-promo-code" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
-			<input type="hidden" name="discount_id" value="<?php echo $discount_id; ?>">
-			<input type="hidden" name="action" value="update">
-		<ul>
-		  
-			<li>
-			<p>
-				<label><strong>
-			  <?php _e('Promotional Code: ','event_espresso'); ?><?php apply_filters('espresso_help', 'coupon_code_info') ?>
-			  </strong></label>
-				<input type="text" name="coupon_code" size="25" value="<?php echo $coupon_code;?>">
-				
-		  </p>
-			</li>
-			
-			<li>
-			<p>
-				<label><strong>
-			  <?php _e('Limited quantity active? ','event_espresso'); ?>
-			  </strong></label>
-				<?php 
-	  			echo select_input('use_limit', $values, $use_limit); ?>
-			</p>
-			<p>
-				<label><strong>
-			  <?php _e('Quantity: ','event_espresso'); ?><?php apply_filters('espresso_help', 'qty_info') ?>
-			  </strong></label>
-				<input type="text" name="quantity" size="7" value="<?php echo $quantity;?>">
-				
-		  </p>
-			</li>
-		  
-		  <li>
-			<p>
-				<label><strong>
-			  <?php _e('Expiration active? ','event_espresso'); ?>
-			  </strong></label>
-				<?php 
-	  			echo select_input('use_exp_date', $values, $use_exp_date); ?>
-			</p>
-			<p>
-				<label><strong>
-			  <?php _e('Expiration Date: ','event_espresso'); ?><?php apply_filters('espresso_help', 'exp_date_info') ?>
-			  </strong></label>
-			<input type="text" class="datepicker" size="12" id="exp_date" name="exp_date" value="<?php echo isset($exp_date)?$exp_date:''; ?>" />
-			
-		  </p>
-			</li>
-	  
-		  <li>
-			<p>
-			<label>
-				<strong>
-			  <?php _e('Price Discount ','event_espresso'); ?>
-			  </strong>
-			</label>
-			<input type="text" name="coupon_code_price" size="7" value="<?php echo $coupon_code_price;?>" />
-			</p>
-			<p>
-			 <label><strong><?php _e('Percentage discount? ','event_espresso'); ?></strong><?php apply_filters('espresso_help', 'discount_amount_info') ?></label>
-			<?php echo select_input('use_percentage', $values, $use_percentage); ?>
-				
-		  </p>
-			</li>
-		 
-		  <li>
-			<p>
-			 <label><strong>
-			
-			<?php _e('Short Description: ','event_espresso'); ?><?php apply_filters('espresso_help', 'description_info') ?>
-			</strong></label> 
-			
-			<textarea rows="5" cols="30" name="coupon_code_description" id="coupon_code_description_new"><?php echo $coupon_code_description; ?></textarea>
-		  </p>
-			</li>
-		  
-			<li>
-			<p>
-			  <input class="button-secondary" type="submit" name="Submit" value="<?php _e('Update','event_espresso'); ?>" id="update_discount" />
-								<?php wp_nonce_field( 'espresso_form_check', 'edit_promocode' ) ?>
-			</p>
-		  </li>
-		</ul>
-	  </form>
+	<div class="postbox">
+		<h3>
+			<?php _e('Edit Code','event_espresso'); ?>
+			</h3>
+		<div class="inside">
+			<form id="edit-promo-code" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
+			<h4><?php echo $coupon_code ?></h4>
+				<table class="form-table">
+					<tbody>
+						<tr>
+							<th><label for="coupon_code">
+								<?php _e('Promotional Code ','event_espresso'); ?>
+								<?php apply_filters('espresso_help', 'coupon_code_info') ?>
+							</label></th>
+							<td><input class="regular-text" type="text" name="coupon_code" size="25" value="<?php echo $coupon_code;?>"></td>
+						</tr>
+						<tr>
+							<th><label for="use_limit">
+								<?php _e('Limited Quantity','event_espresso'); ?>
+								<?php apply_filters('espresso_help', 'qty_info') ?>
+								</label></th>
+							<td><?php echo select_input('use_limit', $values, $use_limit); ?></td>
+						</tr>
+						<tr>
+							<th><label for="quantity">
+								<?php _e('Quantity','event_espresso'); ?>
+								</label></th>
+							<td><input class="regular-text" type="text" name="quantity" size="7" value="<?php echo $quantity;?>"></td>
+						</tr>
+						<tr>
+							<th><label for="use_exp_date">
+								<?php _e('Expiration Active','event_espresso'); ?>
+								<?php apply_filters('espresso_help', 'exp_date_info') ?>
+								</label></th>
+							<td><?php echo select_input('use_exp_date', $values, $use_exp_date); ?></td>
+						</tr>
+						<tr>
+							<th><label for="exp_date">
+								<?php _e('Expiration Date','event_espresso'); ?>
+								
+								</label></th>
+							<td><input type="text" class="datepicker" size="12" id="exp_date" name="exp_date" value="<?php echo isset($exp_date)?$exp_date:''; ?>" /></td>
+						</tr>
+						<tr>
+							<th><label for="coupon_code_price"> 
+								<?php _e('Price Discount','event_espresso'); ?>
+								 </label></th>
+							<td><input class="regular-text" type="text" name="coupon_code_price" size="7" value="<?php echo $coupon_code_price;?>" /></td>
+						</tr>
+						<tr>
+							<th><label for="use_percentage">
+								<?php _e('Percentage Discount','event_espresso'); ?>
+								<?php apply_filters('espresso_help', 'discount_amount_info') ?>
+							</label></th>
+							<td><?php echo select_input('use_percentage', $values, $use_percentage); ?></td>
+						</tr>
+						<tr>
+							<th><label for="coupon_code_description">
+								<?php _e('Short Description','event_espresso'); ?>
+								<?php apply_filters('espresso_help', 'description_info') ?>
+								</label></th>
+							<td><textarea rows="5" cols="30" name="coupon_code_description" id="coupon_code_description_new"><?php echo $coupon_code_description; ?></textarea></td>
+						</tr>
+						
+					</tbody>
+				</table>
+				<input type="hidden" name="discount_id" value="<?php echo $discount_id; ?>">
+				<input type="hidden" name="action" value="update">
+				<p>
+							<input class="button-secondary" type="submit" name="Submit" value="<?php _e('Update','event_espresso'); ?>" id="update_discount" />
+							<?php wp_nonce_field( 'espresso_form_check', 'edit_promocode' ) ?>
+						</p>
+	
+			</form>
+		</div>
 	</div>
-  </div>
 </div>
 <script type="text/javascript" charset="utf-8">
 	//<![CDATA[
--- includes/admin-files/coupon-management/new_discount.php
+++ includes/admin-files/coupon-management/new_discount.php
@@ -6,96 +6,77 @@ function add_new_event_discount(){
 	?>
 
 <div class="metabox-holder">
-  <div class="postbox">
-	<h3>
-	  <?php _e('Add a New Promotional Code','event_espresso'); ?>
-	</h3>
-	<div class="inside">
-		<form id="new-promo-code" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
-		<input type="hidden" name="action" value="add">
-		<ul>
-		  <li>
-			<p class="inputunder">
-			<label for="coupon_code">
-			  <?php _e('Promotional Code','event_espresso'); ?>
-			  <em title="<?php _e('This field is required', 'event_espresso') ?>"> *</em> <?php apply_filters('espresso_help', 'coupon_code_info') ?></label>
-			<input  id="coupon_code" type="text" name="coupon_code" size="25" />
-			
-		  </p>
-			</li>
-		  <li>
-			<p>
-			<label><strong>
-			  <?php _e('Limited quantity active?','event_espresso'); ?>
-			  </strong>
-			</label>
-			
-			<?php 
-	  			echo select_input('use_limit', $values, 'N'); ?>
-			</p>
-			<p>
-			<label><strong>
-			  <?php _e('Quantity: ','event_espresso'); ?><?php apply_filters('espresso_help', 'qty_info') ?>
-			  </strong></label>
-			<input type="text" name="quantity" size="7">
-		
-		  </p>
-			</li>
-		  
-			<li>
-			<p>
-			<label><strong>
-			  <?php _e('Expiration active?','event_espresso'); ?>
-			  </strong></label>
-			
-	  		<?php 	echo select_input('use_exp_date', $values, 'N'); ?>
-			</p>
-			<p>
-			<label><strong>
-			  <?php _e('Expiration Date: ','event_espresso'); ?><?php apply_filters('espresso_help', 'exp_date_info') ?>
-			  </strong></label>
-			<input type="text" class="datepicker" size="12" id="exp_date" name="exp_date" />
-		
-		  </p>
-			</li>
-		  
-			<li>
-			<p>
-			<label><strong>
-			  <?php _e('Price Discount ','event_espresso'); ?>
-			  </strong></label>
-			<input type="text" name="coupon_code_price" size="7" value="<?php echo $coupon_code_price;?>" />
-			</p>
-			<p>
-			<label><strong>
-			  <?php _e('Percentage discount? ','event_espresso'); ?><?php apply_filters('espresso_help', 'discount_amount_info') ?>
-			  </strong></label>
-			<?php 
-	  			echo select_input('use_percentage', $values, 'N'); ?>
-		  </p>
-			</li>
-		  
-			<li>
-			<p>
-			<label><strong>
-			  <?php _e('Promotional Code Description ','event_espresso'); ?><?php apply_filters('espresso_help', 'description_info') ?>
-			  </strong></label> 
-			 
-			<br />
-			<textarea rows="5" cols="300" name="coupon_code_description" id="coupon_code_description_new"  class="my_ed"></textarea>
-		  </p>
-			</li>
-		  
-			<li>
-			<p>
-			  <input class="button-secondary" type="submit" name="add_new_discount" value="<?php _e('Submit','event_espresso'); ?>" id="add_new_discount" />
-								<?php wp_nonce_field( 'espresso_form_check', 'add_new_promocode' ) ?>
-			</p>
-		  </li>
-		</ul>
-	  </form>
+	<div class="postbox">
+		<h3>
+			<?php _e('Add a New Promotional Code','event_espresso'); ?>
+		</h3>
+		<div class="inside">
+			<form id="new-promo-code" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
+				<table class="form-table">
+					<tbody>
+						<tr>
+							<th><label for="coupon_code">
+									<?php _e('Promotional Code ','event_espresso'); ?>
+									<?php apply_filters('espresso_help', 'coupon_code_info') ?>
+								</label></th>
+							<td><input class="regular-text" type="text" name="coupon_code" size="25" value="<?php echo $coupon_code;?>"></td>
+						</tr>
+						<tr>
+							<th><label for="use_limit">
+									<?php _e('Limited Quantity','event_espresso'); ?>
+									<?php apply_filters('espresso_help', 'qty_info') ?>
+								</label></th>
+							<td><?php echo select_input('use_limit', $values, $use_limit); ?></td>
+						</tr>
+						<tr>
+							<th><label for="quantity">
+									<?php _e('Quantity','event_espresso'); ?>
+								</label></th>
+							<td><input class="regular-text" type="text" name="quantity" size="7" value="<?php echo $quantity;?>"></td>
+						</tr>
+						<tr>
+							<th><label for="use_exp_date">
+									<?php _e('Expiration Active','event_espresso'); ?>
+									<?php apply_filters('espresso_help', 'exp_date_info') ?>
+								</label></th>
+							<td><?php echo select_input('use_exp_date', $values, $use_exp_date); ?></td>
+						</tr>
+						<tr>
+							<th><label for="exp_date">
+									<?php _e('Expiration Date','event_espresso'); ?>
+								</label></th>
+							<td><input type="text" class="datepicker" size="12" id="exp_date" name="exp_date" value="<?php echo isset($exp_date)?$exp_date:''; ?>" /></td>
+						</tr>
+						<tr>
+							<th><label for="coupon_code_price">
+									<?php _e('Price Discount','event_espresso'); ?>
+								</label></th>
+							<td><input class="regular-text" type="text" name="coupon_code_price" size="7" /></td>
+						</tr>
+						<tr>
+							<th><label for="use_percentage">
+									<?php _e('Percentage Discount','event_espresso'); ?>
+									<?php apply_filters('espresso_help', 'discount_amount_info') ?>
+								</label></th>
+							<td><?php echo select_input('use_percentage', $values, 'N'); ?></td>
+						</tr>
+						<tr>
+							<th><label>
+									<?php _e('Short Description','event_espresso'); ?>
+									<?php apply_filters('espresso_help', 'description_info') ?>
+								</label></th>
+							<td><textarea rows="5" cols="30" name="coupon_code_description" id="coupon_code_description_new"></textarea></td>
+						</tr>
+					</tbody>
+				</table>
+				<input type="hidden" name="action" value="add">
+				<p>
+					<input class="button-secondary" type="submit" name="add_new_discount" value="<?php _e('Submit','event_espresso'); ?>" id="add_new_discount" />
+					<?php wp_nonce_field( 'espresso_form_check', 'add_new_promocode' ) ?>
+				</p>
+			</form>
+		</div>
 	</div>
-  </div>
 </div>
 <script type="text/javascript" charset="utf-8">
 	//<![CDATA[
@@ -112,4 +93,5 @@ function add_new_event_discount(){
 </script>
 <?php 
 }
+
 
