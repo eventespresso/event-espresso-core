3102df99c50f528a20fec61ea94065df50876717
Seth Shoultes <seth@eventespresso.com>
2011-11-26 07:09:42 +0000

    Little bit of clean up

--- gateways/payment_gateways.php
+++ gateways/payment_gateways.php
@@ -12,27 +12,7 @@
 
 function event_espresso_gateways_options() {
 	global $wpdb, $espresso_premium, $notices;
-
-?>
-
-<div class="wrap">
-	<div id="icon-options-event" class="icon32"> </div>
-	<h2>
-		<?php _e('Manage Payment Gateways', 'event_espresso'); ?>
-	</h2>
-
-<?php #### Page sidebar righthand side #### ?>
-
-	<div id="poststuff" class="metabox-holder has-right-sidebar">
-		<?php event_espresso_display_right_column(); ?>
-
-<?php #### Primary page metaboxes - lefthand container #### ?>
-		
-		<div id="post-body">
-			<div id="post-body-content">					
-					<div class="meta-box-sortables ui-sortables">
-						
-<?php
+	
 	$gateways = array();
 	$gateways[] = 'check';
 	$gateways[] = 'bank';
@@ -50,70 +30,97 @@ function event_espresso_gateways_options() {
 	$gateways[] = 'paytrace';
 	$gateways[] = 'quickpay';
 	$gateways[] = 'worldpay';
-	
-	// loop over gateways array and call each gateways settings.php file
-	foreach ($gateways as $gateway) {	
-		$func 			= 'event_espresso_' . $gateway . '_payment_settings';
-		$fallback_func 	= 'event_espresso_' . $gateway . '_settings';
-		$fallback_func2 = 'event_espresso_' . $gateway . '_deposit_settings';
-		$fallback_func3 = 'event_espresso_authnet_' . $gateway . '_settings';
-		if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/" . $gateway . "/settings.php")) {
-			require_once(EVENT_ESPRESSO_GATEWAY_DIR . "/" . $gateway . "/settings.php");
-			if(function_exists($func)) $func();
-			elseif(function_exists($fallback_func)) $fallback_func();
-			elseif(function_exists($fallback_func2)) $fallback_func2();
-			elseif(function_exists($fallback_func3)) $fallback_func3();
-		} elseif
-		(file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/" . $gateway . "/settings.php")) {
-			require_once(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/" . $gateway . "/settings.php");
-			if(function_exists($func)) $func();
-			elseif(function_exists($fallback_func)) $fallback_func();
-			elseif(function_exists($fallback_func2)) $fallback_func2();
-			elseif(function_exists($fallback_func3)) $fallback_func3();
-		}
 
-	}
-										
-	//requires and empty alipay_active.php file in the gateways/alipay OR
-	//if you have moved the gateway files, place it in uploads/espresso/gateways
-	if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/alipay/alipay_active.php") || file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/alipay/alipay_active.php")) {
-		if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/alipay/settings.php")) {
-			require_once(EVENT_ESPRESSO_GATEWAY_DIR . "/alipay/settings.php");
-			event_espresso_alipay_settings();
-		} elseif (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/alipay/settings.php")) {
-			require_once(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/alipay/settings.php");
-			event_espresso_alipay_settings();
-		}
-	}
-	if (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH.'includes/admin-files/gateway_developer.php')){
-		require_once(EVENT_ESPRESSO_PLUGINFULLPATH.'includes/admin-files/gateway_developer.php');
-	}
+?>
 
-	?>	
-
-				
-				<?php if ($espresso_premium != true) { ?>
-				<h2> <?php _e('Need more payment options?', 'event_espresso') ?> <a href="http://eventespresso.com/download/" target="_blank"><?php  _e('Upgrade Now!', 'event_espresso') ?></a></h2>
-				<?php } ?>								
-				
+<div class="wrap">
+	<div id="icon-options-event" class="icon32"> </div>
+	<h2>
+		<?php _e('Manage Payment Gateways', 'event_espresso'); ?>
+	</h2>
+	<?php #### Page sidebar righthand side #### ?>
+	<div id="poststuff" class="metabox-holder has-right-sidebar">
+		<?php event_espresso_display_right_column(); ?>
+		<?php #### Primary page metaboxes - lefthand container #### ?>
+		<div id="post-body">
+			<div id="post-body-content">
+				<div class="meta-box-sortables ui-sortables">
+					<?php
+					// loop over gateways array and call each gateways settings.php file
+					foreach ($gateways as $gateway) {	
+						$func 			= 'event_espresso_' . $gateway . '_payment_settings';
+						$fallback_func 	= 'event_espresso_' . $gateway . '_settings';
+						$fallback_func2 = 'event_espresso_' . $gateway . '_deposit_settings';
+						$fallback_func3 = 'event_espresso_authnet_' . $gateway . '_settings';
+						if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/" . $gateway . "/settings.php")) {
+							require_once(EVENT_ESPRESSO_GATEWAY_DIR . "/" . $gateway . "/settings.php");
+							if(function_exists($func)) $func();
+							elseif(function_exists($fallback_func)) $fallback_func();
+							elseif(function_exists($fallback_func2)) $fallback_func2();
+							elseif(function_exists($fallback_func3)) $fallback_func3();
+						} elseif
+						(file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/" . $gateway . "/settings.php")) {
+							require_once(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/" . $gateway . "/settings.php");
+							if(function_exists($func)) $func();
+							elseif(function_exists($fallback_func)) $fallback_func();
+							elseif(function_exists($fallback_func2)) $fallback_func2();
+							elseif(function_exists($fallback_func3)) $fallback_func3();
+						}
+					}
+														
+					//requires an empty alipay_active.php file in the gateways/alipay OR
+					//if you have moved the gateway files, place it in uploads/espresso/gateways
+					if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/alipay/alipay_active.php") || file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/alipay/alipay_active.php")) {
+						if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/alipay/settings.php")) {
+							require_once(EVENT_ESPRESSO_GATEWAY_DIR . "/alipay/settings.php");
+							event_espresso_alipay_settings();
+						} elseif (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/alipay/settings.php")) {
+							require_once(EVENT_ESPRESSO_PLUGINFULLPATH . "gateways/alipay/settings.php");
+							event_espresso_alipay_settings();
+						}
+					}
+					
+					//Shows information for developers
+					if (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH.'includes/admin-files/gateway_developer.php')){
+						require_once(EVENT_ESPRESSO_PLUGINFULLPATH.'includes/admin-files/gateway_developer.php');
+					}
+					
+					//If the free version is installed, this will be displayed.
+					if ($espresso_premium != true) { 
+						echo '<h2>';
+						_e('Need more payment options?', 'event_espresso');
+						echo '<a href="http://eventespresso.com/download/" target="_blank">';
+						_e('Upgrade Now!', 'event_espresso');
+						echo '</a>';
+						echo '</h2>';
+					}
+?>
 				</div>
-				<!-- / .meta-box-sortables -->				
+				<!-- / .meta-box-sortables --> 
 				
-					<!-- Help boxes -->
-					<div id="button_image" style="display: none;">
-						<div class="TB-ee-frame">
-							<h2><?php _e('Button Image URL', 'event_espresso') ?></h2>
-							<p><?php _e('A default payment button is provided. A custom payment button may be used, choose your image or upload a new one, and just copy the "file url" here (optional.)', 'event_espresso') ?></p>
-						</div>
+				<!-- Help boxes -->
+				<div id="button_image" style="display: none;">
+					<div class="TB-ee-frame">
+						<h2>
+							<?php _e('Button Image URL', 'event_espresso') ?>
+						</h2>
+						<p>
+							<?php _e('A default payment button is provided. A custom payment button may be used, choose your image or upload a new one, and just copy the "file url" here (optional.)', 'event_espresso') ?>
+						</p>
 					</div>
-					<div id="bypass_confirmation" style="display: none;">
-						<div class="TB-ee-frame">
-							<h2><?php _e('By-passing the Confirmation Page', 'event_espresso') ?></h2>
-							<p><?php _e('This will allow you to send your customers directly to the payment gateway of your choice.', 'event_espresso') ?></p>
-						</div>
-					</div>				
-					<!-- / help boxes -->
-			
+				</div>
+				<div id="bypass_confirmation" style="display: none;">
+					<div class="TB-ee-frame">
+						<h2>
+							<?php _e('By-passing the Confirmation Page', 'event_espresso') ?>
+						</h2>
+						<p>
+							<?php _e('This will allow you to send your customers directly to the payment gateway of your choice.', 'event_espresso') ?>
+						</p>
+					</div>
+				</div>
+				<!-- / help boxes --> 
+				
 			</div>
 			<!-- / #post-body-content --> 
 		</div>
@@ -124,12 +131,12 @@ function event_espresso_gateways_options() {
 <!-- / #wrap --> 
 
 <script type="text/javascript" charset="utf-8">
-		//<![CDATA[
-		jQuery(document).ready(function() {
-			postboxes.add_postbox_toggles('payment_gateways');
-
-		});
-		//]]>
-	</script>
+	//<![CDATA[
+	jQuery(document).ready(function() {
+		postboxes.add_postbox_toggles('payment_gateways');
+	});
+	//]]>
+</script>
 <?php
 }
+
