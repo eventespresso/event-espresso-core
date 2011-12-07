41d869a75869dc140de85774856c40c5725e7117
Seth Shoultes <seth@eventespresso.com>
2011-12-07 07:06:20 +0000

    Changing the espresso_help_popup filter to use return instead of echo. This should fix all instances within the core plugin.[touch:464]

--- includes/functions/filters.php
+++ includes/functions/filters.php
@@ -1,10 +1,10 @@
 <?php
 function espresso_admin_footer () {
-	echo 'Event Registration and Ticketing Powered by <a href="http://eventespresso.com/" title="Event Registration Powered by Event Espresso" target="_blank">'.EVENT_ESPRESSO_POWERED_BY.'</a>';
+	return  'Event Registration and Ticketing Powered by <a href="http://eventespresso.com/" title="Event Registration Powered by Event Espresso" target="_blank">'.EVENT_ESPRESSO_POWERED_BY.'</a>';
 }
 add_filter('admin_footer_text', 'espresso_admin_footer');
 
 function espresso_help_popup($name){
-	echo  '<a class="thickbox" href="#TB_inline?height=400&amp;width=500&amp;inlineId=' . $name . '" target="_blank"><span class="question">[?]</span></a>';
+	return  '<a class="thickbox" href="#TB_inline?height=400&amp;width=500&amp;inlineId=' . $name . '" target="_blank"><span class="question">[?]</span></a>';
 }
 add_filter('espresso_help', 'espresso_help_popup');
