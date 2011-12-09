60fcfecf159cb4ff3579f5e6afe64a1df5982ab4
Seth Shoultes <seth@eventespresso.com>
2011-12-09 05:18:50 +0000

    Adding support text "Using the Event Meta boxes" [touch:480]

--- includes/event-management/help.php
+++ includes/event-management/help.php
@@ -231,25 +231,22 @@
 				<?php _e("Using the Event Meta boxes", "event_espresso"); ?>
 			</h2>
 			<p>
-				<?php _e("Event meta boxes allow you to add extra information to your event that you can display in your templates or use in your custom pages. ", "event_espresso"); ?>
+				<?php _e("Event meta boxes allow you to add extra information to your event that you can display in your templates or use in your custom pages. The name parameter is the the first box labeled 'Key' and allows the shortcode to identify which meta box is to be displayed; the 'Value' is the actual content you wish to be shown.", "event_espresso"); ?>
 			</p>
+			<h4><?php _e('Shortcodes', 'event_espresso'); ?></h4>
 			<p>
-				<?php _e("This extra information can be displayed in your event listings or registration pages via shortcodes.", "event_espresso"); ?>
+				<?php _e("This extra information can be displayed in your event listings or registration pages via shortcodes added to the event description. The Shortcodes take the form of:", "event_espresso"); ?>
 			</p>
-			<p>
-				<?php _e("The Shortcodes take the form of:<br /> <code>[EE_META type='event_meta' name='my_meta_key']</code>", "event_espresso") ?>
-			</p>
-			<p>
-				<?php _e("The name parameter is the the first box labeled 'Key' and allows the shortcode to identify which meta box is to be displayed; the 'Value' is the actual content you wish to be shown.", "event_espresso") ?>
-			</p>
+			<code>[EE_META type='event_meta' name='my_meta_key']</code>
+			
+			
 			<p>
 				<?php _e("If you are using custom templates (moved to the uploads folder) you can add the shortcode directly to the template, this would take the form of:", "event_espresso") ?>
 			</p>
+			<code>echo do_shortcode('[EE_META type='event_meta' name='my_meta_key']');</code>
+			<h4><?php _e('Adding Default Meta Key/Values', 'event_espresso'); ?></h4>
 			<p>
-				<?php _e("<code>&lt;?php echo do_shortcode('[EE_META type='event_meta' name='my_meta_key']'); ?></code>", "event_espresso") ?>
-			</p>
-			<p>
-				<?php _e('Example of adding default event meta to your theme-name/functions.php:', 'event_espresso'); ?></p>
+				<?php _e('To add default meta values, add the following code to your theme-name/functions.php file.', 'event_espresso'); ?></p>
 <pre>
 function my_event_meta(){
 	$array = array("event_hashtag" => "#eventespresso", 
@@ -263,8 +260,7 @@ add_filter('hook_espresso_filter_default_event_meta', 'my_event_meta');
 			<p> <?php echo __('Further information on shortcodes is available ', 'event_espresso') . "<a href='" . admin_url('admin.php?page=support#shortcodes') . " '>" . __('on the Help &amp; Support page', 'event_espresso') . "</a>" . __(' or from the', 'event_espreso'). " <a href='http://eventespresso.com/forums/2010/10/post-type-variables-and-shortcodes/'>" . __('forums', 'event_espresso') . "</a>."; ?> </p>
 		</div>
 	</div>
-</div>
-<div style="display: none;">
+
 	<?php ########## help box ########## ?>
 	<div id="event_custom_emails" class="pop-help" >
 		<div class="TB-ee-frame">
