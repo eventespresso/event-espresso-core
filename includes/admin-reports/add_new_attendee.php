7b2b4198ce671a7769533644e7de7515b91617d6
Brent Christenson <brent@eventespresso.com>
2012-01-10 19:30:04 +0000

    changed hook name from "espresso_price_select" to "hook_espresso_price_select"

--- includes/admin-reports/add_new_attendee.php
+++ includes/admin-reports/add_new_attendee.php
@@ -105,7 +105,7 @@ function add_new_attendee($event_id){
 		  
 		  <?php
 					//Show pricing in a dropdown or text
-					do_action('espresso_price_select', $event_id);
+					do_action('hook_espresso_price_select', $event_id);
 						
 					//Added for seating chart addon.  Creates a field to select a seat from a popup.
 					do_action( 'espresso_seating_chart_select', $event_id);
