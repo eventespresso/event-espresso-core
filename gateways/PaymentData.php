3cd2a7848aa1673786ef07926a2a535cc9585e10
Sidney Harrel <sidney@eventespresso.com>
2012-01-31 01:02:10 +0000

    Adds the four common functions that all the gateways will use. It's up to the individual gateways to add them to the hooks on the Thank You page or the Transactions page Trunk [touch:621]

--- gateways/PaymentData.php
+++ gateways/PaymentData.php
@@ -28,3 +28,51 @@ class PaymentData {
 	public $txn_type;
 
 }
+function espresso_prepare_payment_data_for_gateways($payment_data) {
+	global $wpdb, $org_options;
+	$sql = "SELECT ea.email, ea.event_id, ea.registration_id,";
+	$sql .= " ea.attendee_session, ed.event_name, ea.lname, ea.fname, ea.txn_id,";
+	$sql .= "	ea.payment_status, ea.payment_date FROM " . EVENTS_ATTENDEE_TABLE . " ea";
+	$sql .= " JOIN " . EVENTS_DETAIL_TABLE . " ed ON ed.id=ea.event_id";
+	$sql .= " WHERE ea.id='" . $payment_data->attendee_id . "'";
+	$temp_data = $wpdb->get_row($sql, ARRAY_A);
+	foreach ($temp_data as $key => $data) {
+		$payment_data->$key = $data;
+	}
+	$payment_data->contact = $org_options['contact_email'];
+	return $payment_data;
+}
+
+function espresso_prepare_event_link($payment_data) {
+	$event_url = espresso_reg_url($payment_data->event_id);
+	$payment_data->event_link = '<a href="' . $event_url . '">' . $payment_data->event_name . '</a>';
+	return $payment_data;
+}
+
+function espresso_get_total_cost($payment_data) {
+	global $wpdb;
+	$sql = "SELECT ac.cost, ac.quantity FROM " . EVENTS_ATTENDEE_TABLE . " a ";
+	$sql .= " JOIN " . EVENTS_ATTENDEE_COST_TABLE . " ac ON a.id=ac.attendee_id ";
+	$sql .= " WHERE a.attendee_session='" . $payment_data->attendee_session . "'";
+	$tickets = $wpdb->get_results($sql, ARRAY_A);
+	$total_cost = 0;
+	foreach ($tickets as $ticket) {
+		$total_cost += $ticket['quantity'] * $ticket['cost'];
+	}
+	$payment_data->total_cost = $total_cost;
+	return $payment_data;
+}
+
+function espresso_update_attendee_payment_status_in_db($payment_data) {
+	global $wpdb;
+	$payment_data->payment_date = date("m-d-Y");
+	$sql = "UPDATE " . EVENTS_ATTENDEE_TABLE . " SET amount_pd = '" . $payment_data->total_cost . "' WHERE id ='" . $payment_data->attendee_id . "' ";
+	$wpdb->query($sql);
+
+	$sql = "UPDATE " . EVENTS_ATTENDEE_TABLE . " SET payment_status = '" . $payment_data->payment_status . "', txn_type = '" . $payment_data->txn_type . "', txn_id = '" . $payment_data->txn_id . "', payment_date ='" . $payment_data->payment_date . "', transaction_details = '" . $payment_data->txn_details . "' WHERE attendee_session ='" . $payment_data->attendee_session . "' ";
+	$wpdb->query($sql);
+	return $payment_data;
+}
+
+
+
