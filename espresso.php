311462ec68c9e98a3e4611c107de3d6e0d47f593
Chris Reynolds <chris@eventespresso.com>
2012-02-02 21:36:53 +0000

    reverting version to 3.2

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 3.2.1.P
+  Version: 3.2.P
 
   Author: Seth Shoultes
   Author URI: http://www.eventespresso.com
@@ -30,7 +30,7 @@
 	
 	
 function espresso_version() {
-	return '3.2.1.P';
+	return '3.2.P';
 }
 function espresso_template_version() {
 	return '1.0';
