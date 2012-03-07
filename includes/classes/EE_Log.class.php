a13e78b88c99a56afc6f85263905bf8a460ac418
Sidney Harrel <sidney@eventespresso.com>
2012-03-07 17:37:45 +0000

    structural refactoring to make future changes to EE modular and incremental [touch:813]

--- includes/classes/EE_Log.class.php
+++ includes/classes/EE_Log.class.php
@@ -96,3 +96,35 @@ class espresso_log {
 	}
 
 }
+
+global $org_options;
+
+//Create logging function and action
+function espresso_log ($file,$function,$message) {
+	espresso_log::singleton()->log(array('file' => $file, 'function' => $function, 'status' => $message));
+}
+
+if (!empty($org_options['full_logging'])) {
+	add_action('action_hook_espresso_log', 'espresso_log', 10, 3);
+}
+
+//Remote logging stuff
+function espresso_remote_log($file,$function,$message) {
+	espresso_log::singleton()->remote_log(array('file' => $file, 'function' => $function, 'status' => $message));
+}
+
+function espresso_send_log() {
+	global $org_options;
+
+	if ( empty($org_options['remote_logging_url']) ) {
+		return;
+	}
+	$url = $org_options['remote_logging_url'];
+	espresso_log::singleton()->send_log($url);
+}
+
+if (!empty($org_options['remote_logging'])) {
+	//echo "<pre>".print_r($org_options,true)."</pre>";
+	add_action('action_hook_espresso_log', 'espresso_remote_log', 10, 3);
+	add_action('wp_footer', 'espresso_send_log');
+}
