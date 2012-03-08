9cf4b6a251b3e084ab43c5154604a9ebea26cfda
Chris Reynolds <chris@eventespresso.com>
2012-03-08 16:56:09 +0000

    adding new post type templates to trunk [touch:490]

--- post-type-samples/page-event_list.php
+++ post-type-samples/page-event_list.php
@@ -3,25 +3,46 @@
 /*
 Template Name: Espresso Events
 Template for displaying a list of event posts.
+Modified by Chris Reynolds for 3.1.20.P
 */
 
 
 get_header(); ?>
-		<div id="container">
-			<div id="content" role="main">
-			
-			<?php query_posts(array('post_type'=>'espresso_event')); ?>
+        <div id="container">
+            <div id="content" role="main">
 
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				
-			  <?php
-			  	// GET POST CUSTOM FIELDS //
-				//For more information on the get_post_meta function, check out this page:
+            <?php global $post, $wp_query;
+                // define some arguments for our Event Espresso query
+                $args = array(
+                        'post_type' => 'espresso_event',
+                        'meta_key' => 'event_start_date',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'event_start_date',
+                                'value' => date('Y-m-d'),
+                                'compare' => '>=', // compares the event_start_date against today's date so we only display events that haven't happened yet
+                                'type' => 'DATE'
+                                )
+                            ),
+                        'orderby' => 'meta_value',
+                        'order' => 'DESC' // change this to ASC if you want newer events on top
+                    );
+            // this saves the query to a temporary location so we can go back to it later after we run our query
+            $temp = $wp_query;
+            $wp_query = null;
+            $wp_query = new WP_Query();
+            $wp_query->query($args);
+
+            // now run the loop
+            while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+              <?php
+              	// GET POST CUSTOM FIELDS //
+                //For more information on the get_post_meta function, check out this page:
 				//http://codex.wordpress.org/Function_Reference/get_post_meta
-				
-				//List of available variables as of Event Espresso 3.0.17
+
+				//Here's a bunch of variables  you can use in your template
 				$event_identifier = get_post_meta($post->ID, 'event_identifier', true);
-				$event_id = get_post_meta($post->ID, 'event_id', true);
+                $event_id = get_post_meta($post->ID, 'event_id', true);
 				$event_start_date = get_post_meta($post->ID, 'event_start_date', true);
 				$event_end_date = get_post_meta($post->ID, 'event_end_date', true);
 				$event_location = get_post_meta($post->ID, 'event_location', true);
@@ -34,106 +55,75 @@ get_header(); ?>
 				$event_externalURL = get_post_meta($post->ID, 'event_externalURL', true);
 				$event_registration_start = get_post_meta($post->ID, 'event_registration_start', true);
 				$event_registration_end = get_post_meta($post->ID, 'event_registration_end', true);
-				
+                $event_cat = do_shortcode('[CATEGORY_NAME event_id="'.$event_id.'"]'); // displays the event category name, not used in this template but you can add it in
+                $event_price = '$' . do_shortcode('[EVENT_PRICE event_id="'.$event_id.'" number="0"]'); // this only displays the first price, change the currency symbol to the one that applies to you
+                $event_date = do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="start_date" format="l, F j, Y"]'); // change the date format if you so desire http://php.net/manual/en/function.date.php
+                $event_time = do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="start_time"]');
+                $event_venue = do_shortcode('[ESPRESSO_VENUE event_id="'.$event_id.'"]'); // modify this shortcode to display the venue however you want http://eventespresso.com/support/documentation/shortcodes-template-variables/#venue
+                $event_attendees_min_max = do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="num_attendees"]') . '/' . do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="reg_limit"]');
+                $event_attendees = do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="available_spaces"]');
+                $show_venue = '0'; // set this to 1 if you want to display the venue (this assumes you have venues set up so the default is to set this to 0 for off)
+                $show_attendees = '1'; // display number of attendees? set this to 0 if you don't want the attendee numbers. to change how they are displayed, change the shortcode in the $event_attendees variable above
+                //the_meta(); //this function displays all the meta values for the post -- more info here: http://codex.wordpress.org/Function_Reference/the_meta
 				//This gets the data that is entered into the custom write panels
 				//http://wefunction.com/2009/10/revisited-creating-custom-write-panels-in-wordpress/
 				//$event_meta = get_post_meta( $post->ID, 'event_meta', true );
-			
 				?>
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="entry-title event_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+                    <?php /* we're not using this section, but you could do something with it if you wanted
+                    <div class="entry-meta">
+                        <?php //twentyten_posted_on(); // uncomment if you're using twentyten ?>
+                    </div><!-- .entry-meta -->
+                    */ ?>
 
-					<div class="entry-meta">
-						<?php twentyten_posted_on();
-						
-						// ECHO POST CUSTOM FIELDS
-						 ?>
-				 
-						
-					</div><!-- .entry-meta -->
+                   <!-- <p>Event ID: <?php echo $event_id;  ?></p> -->
+                   <!-- event stuff starts here -->
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-											   
-					   <!-- <p>Event ID: <?php echo $event_id;  ?></p>-->
-						<?php if ($event_meta['event_thumbnail']) { ?>
+                <div id="post-<?php the_ID(); ?>" class="event_data <?php echo $event_cat; ?> event-data-display event-list-display event-display-boxes" <?php //post_class(); // if you'd rather use the regular post_class function, comment out all that class stuff there and use this instead ?>>
+                    <h2 id="event_title-<?php echo $event_id ?>" class="event_title entry-title">
+                        <?php if ($event_externalURL != '') { // if there's an alternate registration URL, use that link
+                            echo '<a href="' . $event_externalURL . '" title="' . the_title() . ' class="a_event_title" id="a_event_title-' . $event_id . '">';
+                        } else { // otherwise just use the regular link ?>
+                            <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>">
+                        <?php } the_title(); ?></a></h2>
+                    <div class="event_meta">
+                        <p id="p_event_price-<?php echo $event_id ?>" class="event_price">
+                            <span class="section-title">Price: </span>
+                            <?php echo $event_price; ?>
+                        </p>
+                        <p id="event_date-<?php echo $event_id ?>">
+                            <span class="section-title">Date: </span>
+                            <?php echo $event_date; ?><br />
+                            <?php echo $event_time; ?>
+                        </p>
+                    </div>
+                    <div class="event-desc">
+                        <?php the_content(); ?>
+                    </div>
 
-							<img src="<?php EVENT_ESPRESSO_INCLUDES_DIR;?>functions/timthumb.php?src=<?php echo $event_meta['event_thumbnail'];?>&amp;h=180&amp;w=431&amp;zc=1" alt="" class="pf-img alignleft" />
+                    <?php if ( $show_venue == '1' ) { ?>
+                        <p class="event_address" id="event_address-<?php echo $event_id ?>">
+                            <span class="section-title">Address: </span>
+                            <?php echo $event_venue; ?>
+                        </p>
+                    <?php }
+                    if ( $show_attendees == '1' ) { ?>
+                        <p id="available_spaces-<?php echo $event_id ?>">
+                            <span class="section-title">Available Spaces: </span>
+                            <?php echo $event_attendees;
+                            /* if you'd prefer to show the minimum/maximum number of attendees comment out or remove the above lines and use this instead:
+                            <?php echo $event_attendees_min_max; ?>
+                            */ ?>
+                        </p>
+                    <?php } ?>
 
-							<?php }?>
-						<?php echo do_shortcode('[EVENT_PRICE_DROPDOWN event_id="'.$event_id.'"]');?>
-						
-						 <div class="grid_12 details_front clearfix" style="margin-top:20px; height:141px; border:1px solid #dfdfdf;">
-		
-		<div class="grid_12 alpha">
-		
-			<ul style="float:left; list-style:none; margin-bottom:0;">
-			
-				<li style="float:left; height:70px; padding-right:30px; text-transform:uppercase; margin-left:0; padding-left:30px; border-right:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf;">
+                </div>
 
-					<h3 style="line-height:17px; margin-bottom:0; margin-top:18px;"><?php echo do_shortcode('[CATEGORY_NAME event_id="'.$event_id.'"]');?><br />
-					<span style=" font-family:Arial, sans-serif; font-weight:normal; font-size:11px; color:#777;"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></span></h3>
-				</li>
-				
-				<li style="float:left; height:70px; padding-right:30px; padding-left:30px; text-transform:uppercase; margin-left:0; padding-left:30px; border-right:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf;">
-					<h3 style="line-height:17px; margin-bottom:0; margin-top:18px;"><a href="<?php echo $event_meta[ 'venue_url' ]; ?>" rel="bookmark" title="<?php echo $event_meta[ 'event_venue' ]; ?>"><?php echo $extra_data[ 'event_venue' ]; ?></a><br />
-					<span style=" font-family:Arial, sans-serif; font-weight:normal; font-size:11px; color:#777;">Event Venue</span></h3>
-				</li>
+                <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
 
-				
-				<li style="float:left; height:70px; width:141px; padding-right:30px; padding-left:30px; text-transform:uppercase; margin-left:0; border-bottom:1px solid #dfdfdf;">
-					<h3 style="line-height:17px; margin-bottom:0; margin-top:18px;"><?php echo do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="start_date" format="F jS"]'); ?><br />
-					<span style="font-family:Arial, sans-serif; font-weight:normal; font-size:11px; color:#777;">Event Start Date</span></h3>
-				</li>
-				
-				<li style="float:left; height:70px; padding-right:30px; padding-left:30px; text-transform:uppercase; margin-left:0; border-right:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf;">
-					<h3 style="line-height:17px; margin-bottom:0; margin-top:18px;"><?php echo do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="start_time"]');?><br />
-					<span style="font-family:Arial, sans-serif; font-weight:normal; font-size:11px; color:#777;">Event Start Time</span></h3>
-
-				</li>
-				
-				<li style="float:left; height:70px; padding-right:30px; padding-left:30px; text-transform:uppercase; margin-left:0; border-right:1px solid #dfdfdf;">
-					<h3 style="line-height:17px; margin-bottom:0; margin-top:18px;">November 1st<br />
-					<span style="font-family:Arial, sans-serif; font-weight:normal; font-size:11px; color:#777;">Registration Ends</span></h3>
-				</li>
-				
-				<li style="float:left; height:70px; padding-right:30px; padding-left:30px; text-transform:uppercase; margin-left:0; border-right:1px solid #dfdfdf;">
-					<h3 style="line-height:17px; margin-bottom:0; margin-top:18px;"><?php echo do_shortcode('[EVENT_PRICE event_id="'.$event_id.'" number="0"]');?>/Team or <?php echo do_shortcode('[EVENT_PRICE event_id="73" number="1"]');?>/Single<br />
-
-					<span style="font-family:Arial, sans-serif; font-weight:normal; font-size:11px; color:#777;">Event Pricing</span></h3>
-				</li>
-				
-				<li style="float:left; height:70px; width:186px; padding-right:30px; padding-left:30px; text-transform:uppercase; margin-left:0;">
-					<div style="width:100%; float:left; margin-top:17px; height:21px; background:#dfdfdf;">
-						<div style="height:17px; padding:2px; width:80px; text-align:right; float:left; background:#CC0000;">
-					
-							<h3 style="line-height:17px; margin-bottom:0; color:#fff;"><?php echo do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="num_attendees"]');?> / <?php echo do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="reg_limit"]');?></h3>
-						
-						</div>
-					</div>
-
-					<span style="font-family:Arial, sans-serif; font-size:11px; color:#777;">Number Attending</span>
-				</li>
-				
-			</ul>
-		
-		</div>
-		<!-- end .grid_12 .alpha -->
-		
-	</div>
-	<!-- end .grid_12 .details_front -->
-
-						
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->
-
-				</div><!-- #post-## -->
-
-<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
+                <?php endwhile; // end of the loop. ?>
+                <?php $wp_query = null; $wp_query = $temp; // put the old query back where we left it ?>
+            </div><!-- #content -->
+        </div><!-- #container -->
+<?php //get_sidebar(); //uncomment this if you want to display the sidebar ?>
 <?php get_footer(); ?>
--- post-type-samples/single-espresso_event.php
+++ post-type-samples/single-espresso_event.php
@@ -3,43 +3,85 @@
  * Template for displaying all single event posts.
  * @package WordPress
  * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+ * @since Event Espresso 3.0.17
+ * last updated for 3.1.20.P by Chris Reynolds
  */
 get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+		<div id="container" class="event-display-boxes">
+			<div id="content" role="main" class="event_espresso_form_wrapper event-data-display">
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post();
 
+              	// GET POST CUSTOM FIELDS //
+                //For more information on the get_post_meta function, check out this page:
+				//http://codex.wordpress.org/Function_Reference/get_post_meta
+
+				//Here's a bunch of variables  you can use in your template
+				$event_identifier = get_post_meta($post->ID, 'event_identifier', true);
+                $event_id = get_post_meta($post->ID, 'event_id', true);
+				$event_start_date = get_post_meta($post->ID, 'event_start_date', true);
+				$event_end_date = get_post_meta($post->ID, 'event_end_date', true);
+				$event_location = get_post_meta($post->ID, 'event_location', true);
+				$event_address = get_post_meta($post->ID, 'event_address', true);
+				$event_address2 = get_post_meta($post->ID, 'event_address2', true);
+				$event_city = get_post_meta($post->ID, 'event_city', true);
+				$event_state = get_post_meta($post->ID, 'event_state', true);
+				$event_country = get_post_meta($post->ID, 'event_country', true);
+				$event_phone = get_post_meta($post->ID, 'event_phone', true);
+				$event_externalURL = get_post_meta($post->ID, 'event_externalURL', true);
+				$event_registration_start = get_post_meta($post->ID, 'event_registration_start', true);
+				$event_registration_end = get_post_meta($post->ID, 'event_registration_end', true);
+                $event_cat = do_shortcode('[CATEGORY_NAME event_id="'.$event_id.'"]'); // displays the event category name, not used in this template but you can add it in
+                $event_price = '$' . do_shortcode('[EVENT_PRICE event_id="'.$event_id.'" number="0"]'); // this only displays the first price, change the currency symbol to the one that applies to you
+                $event_date = do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="start_date" format="l, F j, Y"]'); // change the date format if you so desire http://php.net/manual/en/function.date.php
+                $event_time = do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="start_time"]');
+                $event_end_time = do_shortcode('[EVENT_TIME event_id="'.$event_id.'" type="end_time"]');
+                $event_venue = do_shortcode('[ESPRESSO_VENUE event_id="'.$event_id.'"]'); // modify this shortcode to display the venue however you want http://eventespresso.com/support/documentation/shortcodes-template-variables/#venue
+                $event_attendees_min_max = do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="num_attendees"]') . '/' . do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="reg_limit"]');
+                $event_attendees = do_shortcode('[ATTENDEE_NUMBERS event_id="'.$event_id.'" type="available_spaces"]');
+                $show_venue = '0'; // set this to 1 if you want to display the venue (this assumes you have venues set up so the default is to set this to 0 for off)
+                $show_attendees = '1'; // display number of attendees? set this to 0 if you don't want the attendee numbers. to change how they are displayed, change the shortcode in the $event_attendees variable above
+                //the_meta(); //this function displays all the meta values for the post -- more info here: http://codex.wordpress.org/Function_Reference/the_meta
+				//This gets the data that is entered into the custom write panels
+				//http://wefunction.com/2009/10/revisited-creating-custom-write-panels-in-wordpress/
+				//$event_meta = get_post_meta( $post->ID, 'event_meta', true );
+				?>
+
+				<?php // uncomment the following lines if you want prev/next post links -- personally, I don't think those really have a place for events, so I'm commenting this out
+				/*
 				<div id="nav-above" class="navigation">
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
 				</div><!-- #nav-above -->
-
+				*/ ?>
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<h1 class="entry-title"><?php the_title(); ?></h1>
 
+					<?php // uncomment the following lines if you're using Twentyten and you care when the event was posted. I don't think this is really relevant, so I'm commenting it out
+					/*
 					<div class="entry-meta">
 						<?php twentyten_posted_on(); ?>
 					</div><!-- .entry-meta -->
-
+					*/ ?>
 					<div class="entry-content">
-						<?php the_content(); ?>
-						
-						
-					 <?php  //Event Espresso ?>
-						<?php $event_id = get_post_meta($post->ID, 'event_id', true);?>
-						<p>Event ID: <?php echo $event_id;  ?></p>
-						<p>Event time: <?php echo do_shortcode('[EVENT_TIME event_id="'.$event_id.'"]');?></p>
-						<?php echo do_shortcode('[ESPRESSO_REG_FORM event_id="'.$event_id.'"]');?>
-						
-						
-						
+						<div class="event_description">
+							<?php the_content(); ?>
+						</div>
+                        <!-- <p>Event ID: <?php echo $event_id;  ?></p> -->
+                        <p class="start_date">
+                        	<span class="section-title">Date: </span>
+                        	<?php echo $event_date; ?>
+                        </p>
+
+                        <?php echo do_shortcode('[ESPRESSO_REG_FORM event_id="'.$event_id.'"]');?>
+
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
 					</div><!-- .entry-content -->
 
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+<?php // another function I don't see much use for on an event.  uncomment it if you want it
+/*
+if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
 					<div id="entry-author-info">
 						<div id="author-avatar">
 							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
@@ -54,25 +96,34 @@ get_header(); ?>
 							</div><!-- #author-link	-->
 						</div><!-- #author-description -->
 					</div><!-- #entry-author-info -->
-<?php endif; ?>
+<?php endif;
+*/?>
 
 					<div class="entry-utility">
-						<?php twentyten_posted_in(); ?>
+						<?php //twentyten_posted_in(); // uncomment this if you are using twentyten and you care what category this was posted in ?>
+						<?php // alternately, you could us this to display the event category:
+						/*
+						<p><?php echo $event_cat; ?></p>
+						*/ ?>
 						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
 					</div><!-- .entry-utility -->
 				</div><!-- #post-## -->
-
+				<?php // if you care about having prev/next navigation, uncomment these lines here:
+				/*
 				<div id="nav-below" class="navigation">
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
 				</div><!-- #nav-below -->
-
-				<?php comments_template( '', true ); ?>
+				*/ ?>
+				<?php // if you want comments on your event, uncomment this line
+				/*
+				comments_template( '', true );
+				*/ ?>
 
 <?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
 		</div><!-- #container -->
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); // uncomment if you want a sidebar. i have commented it out because the twentyten layout doesn't work with all themes ?>
 <?php get_footer(); ?>
