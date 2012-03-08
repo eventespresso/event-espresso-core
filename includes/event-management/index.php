571b0f344718d27af9035ad350524ab160211ff7
Sidney Harrel <sidney@eventespresso.com>
2012-03-08 21:27:59 +0000

    converting edit event page to post boxes [touch:813]

--- includes/event-management/index.php
+++ includes/event-management/index.php
@@ -4,95 +4,73 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
 //Add/Delete/Edit Events
 require_once('event_functions.php');
 require_once("event_list.php");
+require_once("copy_event.php");
+require_once("insert_event.php");
+require_once ('csv_import.php');
+require_once("edit_event.php");
+require_once("add_new_event.php");
 
 function event_espresso_manage_events() {
+	global $wpdb, $org_options;
+	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
+	//Update the event
+	if (isset($_REQUEST['edit_action']) && $_REQUEST['edit_action'] == 'update') {
+		require_once("update_event.php");
+		update_event();
+	}
+
+	if (isset($_REQUEST['action'])) {
+
+		// No output, so go through to event overview
+		// ToDo: convert them to notices
+		if ($_REQUEST['action'] == 'copy_event') {
+			copy_event();
+		}
+		if ($_REQUEST['action'] == 'delete') {
+			event_espresso_delete_event();
+		}
+		//Delete recurrence series of events
+		if ($_REQUEST['action'] == 'delete_recurrence_series') {
+			$r = $wpdb->get_results("SELECT id FROM " . EVENTS_DETAIL_TABLE . " ed WHERE recurrence_id = " . $_REQUEST['recurrence_id']);
+
+			if ($wpdb->num_rows > 0) {
+				foreach ($r as $row) {
+					event_espresso_delete_event($row->id);
+				}
+			}
+		}
+		if ($_REQUEST['action'] == 'add') {
+			add_event_to_db();
+		}
+
+
+		// These three are different views, so return after each
+		if ($_REQUEST['action'] == 'csv_import') {
+			csv_import();
+			return;
+		}
+		if ($_REQUEST['action'] == 'edit') {//show the edit form
+			edit_event($_REQUEST['event_id']);
+			return;
+		}
+		if ($_REQUEST['action'] == 'add') {//Show the add new event form
+			add_new_event();
+			return;
+		}
+	}
 	?>
-	<div id="event_overview" class="wrap meta-box-sortables ui-sortable">
-		<div id="event_reg_theme" class="wrap">
-			<div id="icon-options-event" class="icon32"></div>
-			<h2>
-				<?php
-				_e('Event Overview', 'event_espresso');
-				if (isset($_REQUEST['action']) && ($_REQUEST['action'] == 'edit' || $_REQUEST['action'] == 'add_new_event')) {
-
-				} else {
-					echo '<a href="admin.php?page=events&amp;action=add_new_event" class="button add-new-h2" style="margin-left: 20px;">' . __('Add New Event', 'event_espresso') . '</a>';
-				}
-				?>
-			</h2>
+	<div id="event_reg_theme" class="wrap">
+		<div id="icon-options-event" class="icon32"></div>
+		<h2>
 			<?php
-			global $wpdb, $org_options;
-			do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-
-			if ( isset($_REQUEST['action']) && $_REQUEST['action'] == 'copy_event' ) {
-				require_once("copy_event.php");
-				copy_event();
-			}
-
-			if ( isset($_REQUEST['action']) && $_REQUEST['action'] == 'delete' ) {
-				event_espresso_delete_event();
-			}
-
-			//Delete recurrence series of events
-			if ( isset($_REQUEST['action']) && $_REQUEST['action'] == 'delete_recurrence_series' ) {
-				$r = $wpdb->get_results("SELECT id FROM " . EVENTS_DETAIL_TABLE . " ed WHERE recurrence_id = " . $_REQUEST['recurrence_id']);
-
-				if ($wpdb->num_rows > 0) {
-					foreach ($r as $row) {
-						event_espresso_delete_event($row->id);
-					}
-				}
-			}
-
-			if ( isset($_REQUEST['action']) && $_REQUEST['action'] == 'csv_import' ) {
-				require_once ('csv_import.php');
-				csv_import();
-			}
-
-			if ( isset($_REQUEST['action']) && $_REQUEST['action'] == 'add' ) {
-				require_once("insert_event.php");
-				add_event_to_db();
-			}
-
-			//Update the event
-			if (isset($_REQUEST['edit_action']) && $_REQUEST['edit_action'] == 'update') {
-				require_once("update_event.php");
-				update_event();
-			}
-			//If we need to add or edit a new event then we show the add or edit forms
-			if (isset($_REQUEST['action']) && ($_REQUEST['action'] == 'add_new_event' || $_REQUEST['action'] == 'edit')) {
-				?>
-				<form name="form" method="post" action="<?php echo $_SERVER["REQUEST_URI"] ?>">
-					<div id="poststuff" class="metabox-holder has-right-sidebar">
-						<?php
-						if ($_REQUEST['action'] == 'edit') {//show the edit form
-							require_once("edit_event.php");
-							edit_event($_REQUEST['event_id']);
-						} else {//Show the add new event form
-							require_once("add_new_event.php");
-							add_new_event();
-						}
-						?>
-						<br class="clear" />
-					</div>
-					<!-- /poststuff -->
-				</form>
-				<!-- /event_reg_theme -->
-				<?php
-			} else {
-				//If we are not adding or editing an event then show the list of events
-				event_espresso_edit_list();
-			}
-//Do not remove anything below this line. These are the info box popups.
-			?></div>
+			_e('Event Overview', 'event_espresso');
+			echo '<a href="admin.php?page=events&amp;action=add_new_event" class="button add-new-h2" style="margin-left: 20px;">' . __('Add New Event', 'event_espresso') . '</a>';
+			?>
+		</h2>
+		<?php
+		event_espresso_edit_list();
+		?>
 	</div>
-
-	<div id="alt_email_info" style="display:none">
-		<h2><?php _e('Alternate Email Address', 'event_espresso'); ?></h2>
-		<p><?php _e('If an alternate email address is entered. Admin email notifications wil be sent to this address instead.', 'event_espresso'); ?></p>
-	</div>
-
 	<?php
-	echo event_espresso_custom_email_info();
-	include_once('help.php');
 }
+
