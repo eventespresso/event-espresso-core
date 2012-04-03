016996e772328cdef99d03b293d1092299251c1d
Brent Christenson <brent@eventespresso.com>
2012-04-03 19:59:51 +0000

    fixed empty attendee details problem [touch:917]

--- templates/reg_page_checkout/confirmation_page.template.php
+++ templates/reg_page_checkout/confirmation_page.template.php
@@ -5,22 +5,23 @@
 				<legend class="mer-reg-page-attendee-lgnd smaller-text lt-grey-text"><?php _e('Event #', 'event_espresso'); ?><?php echo $x; ?></legend>
 				<h4><strong><?php echo $event['name']; ?></strong></h4>
 				<h6><?php echo $event['date']; ?> at <?php echo $event['time']; ?></h6>
+				<h6><?php echo $event['ticket-price']; ?></h6>
 				<h5><strong><?php _e('Attendees', 'event_espresso'); ?></strong></h5>
 				<ol class="confirm-page-attendees-ul">
 	<?php	foreach ( $event['attendees'] as $attendee ) { ?>
 					<li>
-						<b><?php echo $attendee['name']; ?></b> - 
+						<b><?php echo $attendee['name']; ?></b> - <?php echo $attendee['extra_att_detail']; ?>
 <?php 		
-					$d = 1;
+/*					$d = 1;
 					foreach ( $attendee as $key => $value ) {
-						if ( is_numeric( $key )) {
+						if ( is_numeric( $key ) && $value != '' ) {
 							echo $value;
 							if ( $d < count( $attendee )) {
 								echo ', ';
 							}
-						}
+						} 
 						$d++;
-					}					
+					}	*/				
 ?>
 					</li>
 	<?php } ?>				
