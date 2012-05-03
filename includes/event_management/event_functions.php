fc877f238e00ca569e63b3da5ec02380c81c73e3
Brent Christenson <brent@eventespresso.com>
2012-05-03 15:29:11 +0000

    Changed end date to actually display the end date, NOT the start date [touch:1044]

--- includes/event_management/event_functions.php
+++ includes/event_management/event_functions.php
@@ -557,7 +557,7 @@ function espresso_event_editor_pricing_metabox($event) {
 								<tr valign="top">
 									<th><label for="edit_ticket_price[<?php echo $price->ID(); ?>][PRC_end_date]"><?php _e('End Date', 'event_espresso'); ?></label></th>
 									<td>
-										<input id="edit_ticket_price[<?php echo $price->ID(); ?>][PRC_end_date]" name="edit_ticket_price[<?php echo $price->ID(); ?>][PRC_end_date]" type="text" class="datepicker edit-ticket-price-input" value="<?php echo $price->start_date(); ?>" />
+										<input id="edit_ticket_price[<?php echo $price->ID(); ?>][PRC_end_date]" name="edit_ticket_price[<?php echo $price->ID(); ?>][PRC_end_date]" type="text" class="datepicker edit-ticket-price-input" value="<?php echo $price->end_date(); ?>" />
 										<span class="description">&nbsp;&nbsp;<?php _e( sprintf( 'If "Triggered by Date" is set to "Yes", then this is the date that this Event Price would become inactive and no longer displayed.' ), 'event_espresso'); ?></span>
 									</td>
 								</tr>			
