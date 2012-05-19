e5c844344f59071f9dbf8cca6c49e174b4f73c28
Sidney Harrel <sidney@eventespresso.com>
2012-05-19 04:26:34 +0000

    (trunk) fix to payment gateways screen for upcoming wp 3.4 release for formatting change {t:15} [touch:1020]

--- includes/admin_screens/payment_gateways.php
+++ includes/admin_screens/payment_gateways.php
@@ -7,25 +7,22 @@ function event_espresso_gateways_options() {
 	<div id="event_reg_theme" class="wrap">
 		<div id="icon-options-event" class="icon32"></div>
 		<h2><?php _e('Manage Payment Gateways', 'event_espresso'); ?></h2>
-		<div id="poststuff" class="metabox-holder has-right-sidebar">
-			<div id="side-info-column" class="inner-sidebar">
-				<?php do_meta_boxes('event-espresso_page_payment_gateways', 'side', null); ?>
-			</div>
-			<div id="post-body">
-				<div id="post-body-content">
-					<?php
-					do_meta_boxes('event-espresso_page_payment_gateways', 'advanced', null);
-					if (!$espresso_premium) {
-						?>
-						<h2><?php _e('Need more payment options?', 'event_espresso'); ?>
-							<a href="http://eventespresso.com/download/" target="_blank"><?php _e('Upgrade Now!', 'event_espresso'); ?></a>
-						</h2>
-						<?php
-					}
-					?>
-				</div><!-- / #post-body-content -->
-			</div><!-- / #post-body -->
-		</div><!-- / #poststuff -->
+		<?php
+		ob_start();
+		do_meta_boxes('event-espresso_page_payment_gateways', 'side', null);
+		$sidebar_content = ob_get_clean();
+		ob_start();
+		do_meta_boxes('event-espresso_page_payment_gateways', 'advanced', null);
+		if (!$espresso_premium) {
+			?>
+			<h2><?php _e('Need more payment options?', 'event_espresso'); ?>
+				<a href="http://eventespresso.com/download/" target="_blank"><?php _e('Upgrade Now!', 'event_espresso'); ?></a>
+			</h2>
+			<?php
+		}
+		$main_post_content = ob_get_clean();
+		espresso_choose_layout($main_post_content, $sidebar_content);
+		?>
 	</div><!-- / #wrap -->';
 	<?php
 }
