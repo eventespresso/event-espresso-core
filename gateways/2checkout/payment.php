7c40d5005644ee602039674e9530913528c18d42
Sidney Harrel <sidney@eventespresso.com>
2012-05-30 02:01:49 +0000

    got the itemized descriptions working on the 2checkout cart page [touch:728]

--- gateways/2checkout/payment.php
+++ gateways/2checkout/payment.php
@@ -5,8 +5,6 @@
  */
 function espresso_send_to_2checkout( $EE_Session, $payment_settings ) {
 
-	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-
 	global $org_options, $session_data;
 	
 	include_once ('lib/2checkout.php');	
@@ -22,9 +20,10 @@ function espresso_send_to_2checkout( $EE_Session, $payment_settings ) {
 		
 	$item_num = 1;
 	$registrations = $session_data['cart']['REG']['items'];
+	$my2checkout->addField('id_type', 1);
 	foreach($registrations as $registration) {
 		foreach($registration['attendees'] as $attendee) {
-			$my2checkout->addField('c_prod_' . $item_num, $registration['line_item'] . ',', $attendee['email']);
+			$my2checkout->addField('c_prod_' . $item_num, rand(1, 100));
 			$my2checkout->addField('c_name_' . $item_num, $registration['name']);
 			$my2checkout->addField('c_description_' . $item_num, $attendee['fname'] . ' ' . $attendee['lname'] . ' attending ' . $registration['name'] . ' on ' . $registration['options']['date'] . ' ' . $registration['options']['time'] . ', ' . $registration['options']['price_desc']);
 			$my2checkout->addField('c_price_' . $item_num, $attendee['price_paid']);
@@ -33,15 +32,12 @@ function espresso_send_to_2checkout( $EE_Session, $payment_settings ) {
 	}
 
 	$my2checkout->addField('sid', $two_checkout_settings['2checkout_id']);
-	//$my2checkout->addField('cart_order_id', rand(1, 100));
 	$my2checkout->addField('cart_order_id', $session_data['transaction']->ID() );
 	$my2checkout->addField('x_Receipt_Link_URL', home_url() . '/?page_id=' . $org_options['notify_url'] . '&id=' . $session_data['id'] . '&attendee_action=post_payment&form_action=payment');
 	$my2checkout->addField('total', number_format($session_data['_cart_grand_total_amount'], 2, '.', ''));
 	$my2checkout->addField('tco_currency', $two_checkout_settings['currency_format']);
 	$my2checkout->addField('type', '2checkout');
-
-//echo printr( $my2checkout, '$my2checkout' );
-//die();	
+	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, serialize($my2checkout));
 	$my2checkout->submitPayment();
 }
 
