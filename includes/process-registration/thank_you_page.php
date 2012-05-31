5bbd22438348bec60e99f39fc9564ce4993f0747
Sidney Harrel <sidney@eventespresso.com>
2012-05-31 19:39:35 +0000

    thank you page shortcode function completely rewritten to use new SPCO schema [touch:728]

--- includes/process-registration/thank_you_page.php
+++ includes/process-registration/thank_you_page.php
@@ -1,41 +1,33 @@
 <?php
 
-//This is the alternate PayPal button used for the email
 function espresso_thank_you_page() {
-	global $wpdb, $org_options, $espresso_wp_user, $payment_settings;
+	global $espresso_wp_user, $EE_Session;
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-	$payment_settings = get_user_meta($espresso_wp_user, 'payment_settings', true);
-	$active_gateways = get_user_meta($espresso_wp_user, 'active_gateways', true);
-	foreach ($active_gateways as $gateway => $path) {
-		require_once($path . "/init.php");
+	$EE_Session = EE_Session::instance();
+	$session_data = $EE_Session->get_session_data();
+	if ($session_data['billing_info']['type'] == 'offsite') {
+		$selected_gateway = $session_data['billing_info']['gateway'];
+		$gateway_path = $session_data['active_gateways'][$selected_gateway];
+		require_once($gateway_path . "/return.php");
+		do_action('action_hook_espresso_process_off_site_payment', $EE_Session);
+		$SPCO = EE_Single_Page_Checkout::instance();
+		$SPCO->process_registration_payment(FALSE);
 	}
-	$attendee_id = apply_filters('filter_hook_espresso_transactions_get_attendee_id', '');
-	if (!empty($_REQUEST['registration_id']) && empty($attendee_id)) {
-		$sql = "SELECT id FROM `" . EVENTS_ATTENDEE_TABLE . "` WHERE registration_id='" . $_REQUEST['registration_id'] . "' ORDER BY id LIMIT 1";
-		$attendee_id = $wpdb->get_var($sql);
-		$payment_data = new EE_Payment_Data($attendee_id);
-		$payment_data->populate_data_from_db();
-		$payment_data->calculate_costs();
-	} elseif (!empty($attendee_id)) {
-		$payment_data = new EE_Payment_Data($attendee_id);
-		$payment_data->populate_data_from_db();
-		if (empty($_GET['registration_id']) || $payment_data->attendees[0]->registration_id != $_GET['registration_id'])
-			die("Cheaters never win!");
-		$payment_data->calculate_costs();
-		$payment_data = apply_filters('filter_hook_espresso_thank_you_get_payment_data', $payment_data);
-		$payment_data->set_payment_date();
-		$payment_data->write_payment_data_to_db();
-		do_action('action_hook_espresso_email_after_payment', $payment_data);
+	if (!empty($session_data['txn_results'])) {
+		$data = array(
+			'event_links' => array(),
+			'fname' => $session_data['primary_attendee']['fname'],
+			'lname' => $session_data['primary_attendee']['lname'],
+			'txn_type' => $session_data['txn_results']['method'],
+			'payment_date' => '',
+			'payment_status' => $session_data['txn_results']['status'],
+			'total_cost' => $session_data['_cart_grand_total_amount'],
+			'registration_id' => '',
+			'txn_id' => ''
+		);
+		espresso_require_template('payment_overview.php');
+		do_action('action_hook_espresso_display_payment_overview_template', $data);
 	}
-	if (!empty($attendee_id)) {
-		do_action('action_hook_espresso_display_payment_overview_template', $payment_data);
-		if ($payment_data->payment_status != "Completed") {
-			echo '<a name="payment_options" id="payment_options"></a>';
-			do_action('action_hook_espresso_display_return_payment_template', $payment_data);
-		}
-	}
-	$_REQUEST['page_id'] = $org_options['return_url'];
-	espresso_init_session();
 }
 
 add_shortcode('ESPRESSO_PAYMENTS', 'espresso_thank_you_page');
