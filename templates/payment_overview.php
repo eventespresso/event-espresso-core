25a94280a6458ce2854dbe426b1544ec58144bb4
Sidney Harrel <sidney@eventespresso.com>
2012-05-31 19:38:30 +0000

    changed payment overview template to use array prepared for it by thank you page shortcode function [touch:728]

--- templates/payment_overview.php
+++ templates/payment_overview.php
@@ -11,9 +11,9 @@ function espresso_display_payment_overview_template($data) {
 			<h4 class="section-title list-events">
 			<?php _e('Class/Event:', 'event_espresso'); ?>
 			</h4>
-				<?php foreach ($data->events as $event) { ?>
+				<?php foreach ($data['event_links'] as $event_link) { ?>
 				<div>
-				<?php echo stripslashes_deep($event->event_link) ?>
+				<?php echo stripslashes_deep($event_link) ?>
 				</div>
 	<?php } ?>
 			<h4 class="section-title"><?php _e('Registration  Details:', 'event_espreso'); ?></h4>
@@ -22,23 +22,23 @@ function espresso_display_payment_overview_template($data) {
 					<dt>
 	<?php _e('Primary Registrant:', 'event_espresso'); ?>
 					</dt>
-					<dd><?php echo stripslashes_deep($data->attendees[0]->fname . ' ' . $data->attendees[0]->lname) ?></dd>
-					<?php echo $data->txn_type == '' ? '' : '<dt>' . __('Payment Type:', 'event_espresso') . '</dt> <dd>' . espresso_payment_type($data->txn_type) . '</dd>'; ?> <?php echo ($data->payment_date == '' || ($data->payment_status == 'Pending' && (espresso_payment_type($data->txn_type) == 'Invoice' || espresso_payment_type($data->txn_type) == 'Offline payment'))) ? '' : '<dt>' . __('Payment Date:', 'event_espresso') . '</dt> <dd>' . event_date_display($data->payment_date) . '</dd>'; ?>
+					<dd><?php echo stripslashes_deep($data['fname'] . ' ' . $data['lname']) ?></dd>
+					<?php echo $data['txn_type'] == '' ? '' : '<dt>' . __('Payment Type:', 'event_espresso') . '</dt> <dd>' . espresso_payment_type($data['txn_type']) . '</dd>'; ?> <?php echo ($data['payment_date'] == '' || ($data['payment_status'] == 'Pending' && (espresso_payment_type($data['txn_type']) == 'Invoice' || espresso_payment_type($data['txn_type']) == 'Offline payment'))) ? '' : '<dt>' . __('Payment Date:', 'event_espresso') . '</dt> <dd>' . event_date_display($data['payment_date']) . '</dd>'; ?>
 					<dt>
 	<?php _e('Amount Paid/Owed:', 'event_espresso'); ?>
 					</dt>
-					<dd><?php echo $org_options['currency_symbol'] ?><?php echo $data->total_cost ?>
-	<?php event_espresso_paid_status_icon($data->payment_status) ?>
+					<dd><?php echo $org_options['currency_symbol'] ?><?php echo $data['total_cost'] ?>
+	<?php event_espresso_paid_status_icon($data['payment_status']) ?>
 					</dd>
 					<dt>
 	<?php _e('Payment Status:', 'event_espresso'); ?>
 					</dt>
-					<dd><?php echo $data->payment_status ?></dd>
+					<dd><?php echo $data['payment_status'] ?></dd>
 					<dt>
 	<?php _e('Registration ID:', 'event_espresso'); ?>
 					</dt>
-					<dd><?php echo $data->attendees[0]->registration_id ?></dd>
-	<?php echo $data->txn_id == '' ? '' : '<dt>' . __('Transaction ID:', 'event_espresso') . '</dt> <dd>' . $data->txn_id . '</dd>'; ?>
+					<dd><?php echo $data['registration_id'] ?></dd>
+	<?php echo $data['txn_id'] == '' ? '' : '<dt>' . __('Transaction ID:', 'event_espresso') . '</dt> <dd>' . $data['txn_id'] . '</dd>'; ?>
 				</dl>
 			</div><!-- / .reg-gen-details -->
 		</div><!-- / .event-data-display -->
