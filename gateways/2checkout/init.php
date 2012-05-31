7ebf70e196fc3e60992be2f73834c42b84c06c54
Sidney Harrel <sidney@eventespresso.com>
2012-05-31 19:47:56 +0000

    changed, but will probably be eliminating init.php. not needed in new SPCO schema [touch:728]

--- gateways/2checkout/init.php
+++ gateways/2checkout/init.php
@@ -13,6 +13,6 @@ add_action('filter_hook_espresso_process_registration_payment', 'espresso_proces
 add_filter('filter_hook_espresso_prepare_event_link', 'espresso_prepare_event_link');
 add_action('action_hook_espresso_email_after_payment', 'espresso_email_after_payment');
 //printr( $billing_info, '$billing_info' );
-if (!empty($billing_info) && $billing_info['gateway'] == '2CheckOut.com') {
+if (!empty($billing_info) && $billing_info['gateway'] == '2checkout') {
 	add_action('action_hook_espresso_process_transaction', 'espresso_send_to_2checkout', 10, 2);
 }
