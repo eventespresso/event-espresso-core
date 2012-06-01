0231124d9336fdae502780d7d8647ec635f9c799
Sidney Harrel <sidney@eventespresso.com>
2012-06-01 08:44:57 +0000

    modified to use new gateway data structure in the session data [touch:728]

--- gateways/2checkout/payment.php
+++ gateways/2checkout/payment.php
@@ -1,10 +1,13 @@
 <?php
 
 function espresso_display_2checkout() {
-	global $EE_Session, $gateways;
-	$session_data = $EE_Session->get_session_data();
-	$twoco_settings = $session_data['payment_settings']['2checkout'];
+	global $EE_Session;
 	$gateway = '2checkout';
+	$gateway_data = $EE_Session->get_session_data(FALSE, 'gateway_data');
+
+	$twoco_settings = $gateway_data['payment_settings'][$gateway];
+	
+	$gateways = $gateway_data['html_data']
 ?>
 			<a id="payment-gateway-button-<?php echo $gateway;?>" class="reg-page-payment-option-lnk<?php echo $gateways[ $gateway ]['css_link_class'];?>" rel="<?php echo $gateway;?>" href="<?php echo $gateways[ $gateway ]['form_url'];?>" >
 				<img src="<?php echo $twoco_settings['button_url'];?>" alt="Pay using 2CheckOut.com" />
@@ -12,8 +15,6 @@ function espresso_display_2checkout() {
 
 			<div id="reg-page-billing-info-<?php echo $gateway;?>-dv" class="reg-page-billing-info-dv <?php echo $gateways[ $gateway ]['css_class'];?>">
 				<?php _e('After confirming the details of your registration in Step 3, you will be transferred to the 2CheckOut.com website where your payment will be securely processed.', 'event_espresso'); ?>
-				<input id="reg-page-gateway-off-site-<?php echo $gateway;?>" type="hidden" value="1" name="reg_page_gateway_off_site[<?php echo $gateway;?>]">
-				<input id="reg-page-selected-gateway-name-<?php echo $gateway;?>" type="hidden" value="<?php echo $gateway; ?>" name="selected_gateway_name[<?php echo $gateway;?>]">
 			</div>
 
 <?php
