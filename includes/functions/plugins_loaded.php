cef731b2025b1347bb4c10abe8997ac2dd54c951
Brent Christenson <brent@eventespresso.com>
2012-06-06 00:41:05 +0000

    moved event_espresso_run_install function to the plugins_loaded.php file [touch:1133]

--- includes/functions/plugins_loaded.php
+++ includes/functions/plugins_loaded.php
@@ -188,3 +188,59 @@ function espresso_check_for_import() {
 		}
 	}
 }
+
+
+
+
+
+/**
+ * 		used by EE and EE addons during plugin activation
+ *
+ * 		@access public
+ * 		@return void
+ */
+function event_espresso_run_install($table_name, $table_version, $sql, $engine = '') {
+
+	global $wpdb;
+
+	$wp_table_name = $wpdb->prefix . $table_name;
+
+	if ($wpdb->get_var("SHOW TABLES LIKE '" . $wp_table_name . "'") != $wp_table_name) {
+
+		$sql_create_table = "CREATE TABLE " . $wp_table_name . " ( " . $sql . " ) " . $engine . " DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;";
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql_create_table);
+
+		//create option for table version
+		$option_name = $table_name . '_tbl_version';
+		$newvalue = $table_version;
+		if (get_option($option_name)) {
+			update_option($option_name, $newvalue);
+		} else {
+			$deprecated = '';
+			$autoload = 'no';
+			add_option($option_name, $newvalue, $deprecated, $autoload);
+		}
+		//create option for table name
+		$option_name = $table_name . '_tbl';
+		$newvalue = $wp_table_name;
+		if (get_option($option_name)) {
+			update_option($option_name, $newvalue);
+		} else {
+			$deprecated = '';
+			$autoload = 'no';
+			add_option($option_name, $newvalue, $deprecated, $autoload);
+		}
+	}
+
+	// Code here with new database upgrade info/table Must change version number to work.
+
+	$installed_ver = get_option($table_name . '_tbl_version');
+	if ($installed_ver != $table_version) {
+		$sql_create_table = "CREATE TABLE " . $wp_table_name . " ( " . $sql . " ) ;";
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql_create_table);
+		update_option($table_name . '_tbl_version', $table_version);
+	}
+}
