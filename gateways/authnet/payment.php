40c25e38e6123ded84ab79f8ec09848eea2c7acc
Sidney Harrel <sidney@eventespresso.com>
2012-06-07 05:01:09 +0000

    prepared authnet sim display page for 3.2 [touch:728]

--- gateways/authnet/payment.php
+++ gateways/authnet/payment.php
@@ -1,75 +1,23 @@
 <?php
 
-// Setup class
-include_once ('Authorize.php');
-echo '<!--Event Espresso Authorize.net Gateway Version ' . $authnet_gateway_version . '-->';
-global $org_options;
-do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-$myAuthorize = new EE_Authorize(); // initiate an instance of the class
+function espresso_display_authnet_sim() {
+	global $EE_Session;
+	$gateway = 'authnet_sim';
+	$gateway_data = $EE_Session->get_session_data(FALSE, 'gateway_data');
 
-$payment_settings = get_option('payment_data_' . $espresso_wp_user);
-$authnet_login_id = empty($payment_settings['authnet_sim']['authnet_login_id']) ? '' : $payment_settings['authnet_sim']['authnet_login_id'];
-$authnet_transaction_key = empty($payment_settings['authnet_sim']['authnet_transaction_key']) ? '' : $payment_settings['authnet_sim']['authnet_transaction_key'];
-$button_type = empty($payment_settings['authnet_sim']['button_type']) ? '' : $payment_settings['authnet_sim']['button_type'];
-//$button_url = $payment_settings['authnet_sim']['button_url'];
-$image_url = empty($payment_settings['authnet_sim']['image_url']) ? '' : $payment_settings['authnet_sim']['image_url'];
-$use_sandbox = $payment_settings['authnet_sim']['use_sandbox'];
-$use_testmode = $payment_settings['authnet_sim']['test_transactions'];
-if ($use_testmode == true) {
-	// Enable test mode if needed
-	$myAuthorize->enableTestMode();
-}
-if ($use_sandbox == true) {
-	// Enable test mode if needed
-	$myAuthorize->useTestServer();
+	$authnet_sim_settings = $gateway_data['payment_settings'][$gateway];
+
+	$gateways = $gateway_data['html_data']
+?>
+			<a id="payment-gateway-button-<?php echo $gateway;?>" class="reg-page-payment-option-lnk<?php echo $gateways[ $gateway ]['css_link_class'];?>" rel="<?php echo $gateway;?>" href="<?php echo $gateways[ $gateway ]['form_url'];?>" >
+				<img src="<?php echo $authnet_sim_settings['button_url'];?>" alt="Pay using Authorize.net" />
+			</a>
+
+			<div id="reg-page-billing-info-<?php echo $gateway;?>-dv" class="reg-page-billing-info-dv <?php echo $gateways[ $gateway ]['css_class'];?>">
+				<?php _e('After confirming the details of your registration in Step 3, you will be transferred to the Authorize.net website where your payment will be securely processed.', 'event_espresso'); ?>
+			</div>
+
+<?php
 }
 
-$quantity = $quantity > 0 ? $quantity : espresso_count_attendees_for_registration($attendee_id);
-
-$myAuthorize->setUserInfo($authnet_login_id, $authnet_transaction_key);
-
-$myAuthorize->addField('x_Relay_URL', home_url() . '/?page_id=' . $org_options['notify_url']);
-$myAuthorize->addField('x_Description', stripslashes_deep($event_name) . ' | ' . __('Reg. ID:', 'event_espresso') . ' ' . $attendee_id . ' | ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($fname . ' ' . $lname) . ' | ' . __('Total Registrants:', 'event_espresso') . ' ' . $quantity);
-$myAuthorize->addField('x_Amount', number_format($event_cost, 2));
-$myAuthorize->addField('x_Logo_URL', $image_url);
-$myAuthorize->addField('x_Invoice_num', 'au-' . event_espresso_session_id());
-//Post variables
-$myAuthorize->addField('x_Cust_ID', $attendee_id);
-$myAuthorize->addField('x_first_name', $fname);
-$myAuthorize->addField('x_last_name', $lname);
-
-$myAuthorize->addField('x_Email', $attendee_email);
-$myAuthorize->addField('x_Address', $address);
-$myAuthorize->addField('x_City', $city);
-$myAuthorize->addField('x_State', $state);
-$myAuthorize->addField('x_Zip', $zip);
-
-
-//Enable this function if you want to send payment notification before the person has paid.
-//This function is copied on the payment processing page
-//event_espresso_send_payment_notification($attendee_id, $txn_id, $amount_pd);
-//Decide if you want to auto redirect to your payment website or display a payment button.
-if (!empty($payment_settings['authnet_sim']['bypass_payment_page'])) {
-	$myAuthorize->submitPayment(); //Enable auto redirect to payment site
-} else {
-	if (empty($payment_settings['authnet_sim']['button_url'])) {
-		//$button_url = EVENT_ESPRESSO_GATEWAY_URL . "authnet/btn_cc_vmad.gif";
-		if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/authnet/btn_cc_vmad.gif")) {
-			$button_url = EVENT_ESPRESSO_GATEWAY_DIR . "/authnet/btn_cc_vmad.gif";
-		} else {
-			$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/authnet/btn_cc_vmad.gif";
-		}
-	} elseif (file_exists($payment_settings['authnet_sim']['button_url'])) {
-		$button_url = $payment_settings['authnet_sim']['button_url'];
-	} else {
-		//If no other buttons exist, then use the default location
-		$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/authnet/btn_cc_vmad.gif";
-	}
-	$myAuthorize->submitButton($button_url, 'authnet'); //Display payment button
-}
-
-if ($use_sandbox == true || $use_testmode == true) {
-	echo '<p>Test credit card # 4007000000027</p>';
-	echo '<h3 style="color:#ff0000;" title="Payments will not be processed">' . __('Debug Mode Is Turned On', 'event_espresso') . '</h3>';
-	$myAuthorize->dump_fields(); // for debugging, output a table of all the fields
-}
+add_action('action_hook_espresso_display_offsite_payment_gateway', 'espresso_display_authnet_sim');
