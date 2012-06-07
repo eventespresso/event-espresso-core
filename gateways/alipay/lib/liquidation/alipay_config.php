19c162d1d9924461cb2381fdcd6c397db50190d6
Sidney Harrel <sidney@eventespresso.com>
2012-06-07 04:57:22 +0000

    deleting derelict alipay gateway. no test creds. hasn't been updated since before 3.0 [touch:728]

--- gateways/alipay/lib/liquidation/alipay_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$partner = "";//fill with the partnerID which we already offered you (required fields)
-$security_code = "";//fill with the security key which we already offered you (required fields)
-
-$sign_type = "MD5"; 
-$transport= "http";
-
-?>
