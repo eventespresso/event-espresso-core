d7097a422387b933f89d3e2e2f0ebd72de76c68c
Sidney Harrel <sidney@eventespresso.com>
2012-06-07 03:56:54 +0000

    removed unused vars in aim/return.php [touch:728]

--- gateways/aim/return.php
+++ gateways/aim/return.php
@@ -3,16 +3,16 @@
 function espresso_process_aim() {
 	global $EE_Session;
 	$session_data = $EE_Session->get_session_data();
-	$billing_info = $session_data['billing_info'];	
+	$billing_info = $session_data['billing_info'];
 
 	if ( $billing_info != 'no payment required' ) {
 
 		require_once 'lib/AuthorizeNet.php';
-	
+
 		$authnet_aim_settings = $session_data['gateway_data']['payment_settings']['aim'];
 		$authnet_aim_login_id = $authnet_aim_settings['authnet_aim_login_id'];
 		$authnet_aim_transaction_key = $authnet_aim_settings['authnet_aim_transaction_key'];
-	
+
 	// Enable test mode if needed
 	//4007000000027  <-- test successful visa
 	//4222222222222  <-- test failure card number
@@ -22,21 +22,21 @@ function espresso_process_aim() {
 		} else {
 			define("AUTHORIZENET_SANDBOX", false);
 		}
-	
+
 		$reg_info = $session_data['cart']['REG'];
 		$primary_attendee = $session_data['primary_attendee'];
-		
+
 		$grand_total = $session_data['_cart_grand_total_amount'];
 
 		$taxes = $session_data['tax_totals'];
 		foreach ( $taxes as $tax ) {
 			$grand_total += $tax;
 		}
-	
+
 	//start transaction
 		$transaction = new AuthorizeNetAIM($authnet_aim_login_id, $authnet_aim_transaction_key);
-		
-		
+
+
 		$transaction->amount = $grand_total;
 		$transaction->card_num = $billing_info['reg-page-billing-card-nmbr']['value'];
 		$transaction->exp_date = $billing_info['reg-page-billing-card-exp-date-mnth']['value'].$billing_info['reg-page-billing-card-exp-date-year']['value'];
@@ -50,26 +50,16 @@ function espresso_process_aim() {
 		$transaction->zip = $billing_info['reg-page-billing-zip']['value'];
 		$transaction->cust_id = $primary_attendee['registration_id']['value'];
 		$transaction->invoice_num = $EE_Session->id(); // <<<<<<<<<<<<<<<<<<<<<<< This actually should NOT be generated YET !!! right?? or is it ? and if so from where ?
-	
+
 		if ($authnet_aim_settings['test_transactions']) {
 			$transaction->test_request = "true";
 		}
-	
-		// create an object to hold payment data
-		$payment_data = new stdClass;
-	
-		$payment_data->txn_type = 'authorize.net AIM';
-		$payment_data->payment_status = 'Incomplete';
-		$payment_data->txn_id = 0;
-		$payment_data->txn_details = 'No response from authorize.net';
-		$payment_data->amount_paid = 0;
-		$payment_data = apply_filters('filter_hook_espresso_prepare_event_link', $payment_data);
-		$payment_data = apply_filters('filter_hook_espresso_get_total_cost', $payment_data);
+
 	//Capture response
-		$response = $transaction->authorizeAndCapture();	
+		$response = $transaction->authorizeAndCapture();
 
 		if ( ! empty( $response )) {
-		
+
 			if ($authnet_aim_settings['use_sandbox']) {
 				$payment_data->txn_id = $response->invoice_number;
 			} else {
@@ -77,10 +67,10 @@ function espresso_process_aim() {
 			}
 
 			$payment_data->payment_status = $response->approved ? 'Approved' :  'Declined';
-			
+
 		}
 		do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, $payment_data->payment_status);
-		
+
 		$txn_results = array(
 											'gateway' 			=> 'Authorize.Net AIM',
 											'approved' 			=> $response->approved ? $response->approved : 0,
@@ -93,21 +83,21 @@ function espresso_process_aim() {
 											'md5_hash' 			=> $response->md5_hash,
 											'transaction_id' 	=> $response->transaction_id,
 											'invoice_number' => $response->invoice_number,
-											'raw_response'	=> $response								
+											'raw_response'	=> $response
 										  );
-		
+
 		$EE_Session->set_session_data(  array( 'txn_results' => $txn_results ), $section = 'session_data' );
-	
+
 		add_action('action_hook_espresso_email_after_payment', 'espresso_email_after_payment');  //<-- Should this be here ? or in the successful txn bit above ( after line 80 ? ) or does this send failed txn info as well /
-	
+
 		// return $payment_data;  <<<<-------  do we need to return success or FALSE or anything ?
-		
+
 	} else {
 		// no payment required
 	}
-	
+
 	// return ? or redirect ?
-	// we need to update the registration and transaction tables 
+	// we need to update the registration and transaction tables
 
 }
 
