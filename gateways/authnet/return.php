9d7370c839d912b99e8460c80cb8f0a874f3bbc9
Sidney Harrel <sidney@eventespresso.com>
2012-06-11 23:40:38 +0000

    authnet sim [touch:728]

--- gateways/authnet/return.php
+++ gateways/authnet/return.php
@@ -1,128 +1,19 @@
 <?php
-// Include the authorize.net library
-include_once ('Authorize.php');
-echo '<!--Event Espresso Authorize.net SIM Gateway Version ' . $authnet_gateway_version . '-->';
-// Create an instance of the authorize.net library
-$myAuthorize = new EE_Authorize();
 
-// Log the IPN results
-$myAuthorize->ipnLog = TRUE;
-
-$authnet_login_id = $payment_settings['authnet_sim']['authnet_login_id'];
-$authnet_transaction_key = $payment_settings['authnet_sim']['authnet_transaction_key'];
-
-// Enable test mode if needed
-//4007000000027  <-- test successful visa
-//4222222222222  <-- test failure card number
-if ($payment_settings['authnet_sim']['use_sandbox']) {
-	$myAuthorize->enableTestMode();
-	$email_transaction_dump = true;
-}
-
-// Specify your authorize login and secret
-$myAuthorize->setUserInfo($authnet_login_id, $authnet_transaction_key);
-
-// Check validity and write down it
-if ($myAuthorize->validateIpn()) {
-
-	$txn_type = $myAuthorize->ipnData['x_method'];
-	$txn_id = $myAuthorize->ipnData['x_trans_id'];
-	$amount_pd = $myAuthorize->ipnData['x_amount'];
-	$attendee_id = $myAuthorize->ipnData['x_cust_id'];
-	$payment_date = date("d-m-Y");
-
-	//file_put_contents('authorize.txt', 'SUCCESS' . date("m-d-Y")); //Used for debugging purposes
-	//Be sure to echo something to the screen so authent knows that the ipn works
-	//store the results in reusable variables
-	if ($myAuthorize->ipnData['x_response_code'] == 1) {
-		?>
-		<h2><?php _e('Thank You!', 'event_espresso'); ?></h2>
-		<p><?php _e('Your transaction has been processed.', 'event_espresso'); ?></p>
-		<?php
-		$payment_status = 'Completed';
-		$sql = "SELECT * FROM " . EVENTS_ATTENDEE_TABLE . " WHERE registration_id='" . espresso_registration_id($attendee_id) . "' ";
-		$sql .= $id == '' ? '' : " AND id= '" . $id . "' ";
-		$sql .= " ORDER BY id LIMIT 0,1";
-
-		$attendees = $wpdb->get_results($sql);
-		foreach ($attendees as $attendee) {
-			$attendee_id = $attendee->id;
-			$att_registration_id = $attendee->registration_id;
-			$lname = $attendee->lname;
-			$fname = $attendee->fname;
-			$amount_pd = $attendee->amount_pd;
-			$total_cost = $attendee->amount_pd;
-			$event_id = $attendee->event_id;
-		}
-
-		$events = $wpdb->get_results("SELECT * FROM " . EVENTS_DETAIL_TABLE . " WHERE id='" . $event_id . "'");
-		foreach ($events as $event) {
-			$event_id = $event->id;
-			$event_name = $event->event_name;
-			$event_desc = $event->event_desc;
-			$event_description = $event->event_desc;
-			$event_identifier = $event->event_identifier;
-			$cost = $event->event_cost;
-			$active = $event->is_active;
-		}
-		//Build links
-		$event_url = espresso_reg_url($event_id);
-		$event_link = '<a href="' . $event_url . '">' . $event_name . '</a>';
-	} else {
-		?>
-		<h2 style="color:#F00;"><?php _e('There was an error processing your transaction!', 'event_espresso'); ?></h2>
-		<p><strong>Error:</strong> (<?php echo $response_reason_code; ?> - <?php echo $response_reason_code; ?>) - <?php echo $response_reason_text; ?></p>
-		<?php
-		$payment_status = 'Payment Declined';
-	}
-	global $wpdb;
-
-	$sql = "UPDATE " . EVENTS_ATTENDEE_TABLE . " SET payment_status = '" . $payment_status . "', txn_type = '" . $txn_type . "', txn_id = '" . $txn_id . "', payment_date ='" . $payment_date . "', transaction_details = '" . serialize($myAuthorize) . "'  WHERE registration_id ='" . espresso_registration_id($attendee_id) . "'";
-
-	$wpdb->query($sql);
-
-	//Debugging option
-	//$email_transaction_dump=true;
-	if ($email_transaction_dump == true) {
-		// For this, we'll just email ourselves ALL the data as plain text output.
-		$subject = 'Authorize.net Notification - Gateway Variable Dump';
-		$body = "An authorize.net payment notification was successfully recieved\n";
-		$body .= "from " . $myAuthorize->ipnData['x_email'] . " on " . date('m/d/Y');
-		$body .= " at " . date('g:i A') . "\n\nDetails:\n";
-		foreach ($myAuthorize->ipnData as $key => $value) {
-			$body .= "\n$key: $value\n";
-		}
-		wp_mail($contact, $subject, $body);
-	}
-} else {
-	file_put_contents('authorize.txt', "FAILURE\n\n" . $myAuthorize->ipnData);
-	//echo something to the screen so authent knows that the ipn works
-	$subject = 'Instant Payment Notification - Gateway Variable Dump';
-	$body = "An instant payment notification failed\n";
-	$body .= "from " . $myAuthorize->ipnData['x_email'] . " on " . date('m/d/Y');
-	$body .= " at " . date('g:i A') . "\n\nDetails:\n";
-	foreach ($myAuthorize->ipnData as $key => $value) {
-		$body .= "\n$key: $value\n";
-	}
-	wp_mail($contact, $subject, $body);
-}
-
-function espresso_process_authnet_sim($EE_Session) {
+function espresso_send_to_authnet_sim($EE_Session) {
 	// Setup class
-	include_once ('Authorize.php');
-	echo '<!--Event Espresso Authorize.net Gateway Version ' . $authnet_gateway_version . '-->';
+	include_once ('lib/Authorize.php');
 	global $org_options;
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
 	$myAuthorize = new EE_Authorize(); // initiate an instance of the class
 
-	$payment_settings = get_option('payment_data_' . $espresso_wp_user);
-	$authnet_login_id = empty($payment_settings['authnet_sim']['authnet_login_id']) ? '' : $payment_settings['authnet_sim']['authnet_login_id'];
-	$authnet_transaction_key = empty($payment_settings['authnet_sim']['authnet_transaction_key']) ? '' : $payment_settings['authnet_sim']['authnet_transaction_key'];
-	$button_type = empty($payment_settings['authnet_sim']['button_type']) ? '' : $payment_settings['authnet_sim']['button_type'];
-//$button_url = $payment_settings['authnet_sim']['button_url'];
-	$image_url = empty($payment_settings['authnet_sim']['image_url']) ? '' : $payment_settings['authnet_sim']['image_url'];
-	$use_sandbox = $payment_settings['authnet_sim']['use_sandbox'];
-	$use_testmode = $payment_settings['authnet_sim']['test_transactions'];
+	$session_data = $EE_Session->get_session_data();
+	$payment_settings = $session_data['gateway_data']['payment_settings']['authnet_sim'];
+	$authnet_login_id = $payment_settings['authnet_login_id'];
+	$authnet_transaction_key = $payment_settings['authnet_transaction_key'];
+	$image_url = $payment_settings['image_url'];
+	$use_sandbox = $payment_settings['use_sandbox'];
+	$use_testmode = $payment_settings['test_transactions'];
 	if ($use_testmode == true) {
 		// Enable test mode if needed
 		$myAuthorize->enableTestMode();
@@ -132,53 +23,94 @@ function espresso_process_authnet_sim($EE_Session) {
 		$myAuthorize->useTestServer();
 	}
 
-	$quantity = $quantity > 0 ? $quantity : espresso_count_attendees_for_registration($attendee_id);
+	$myAuthorize->setUserInfo($authnet_login_id, $authnet_transaction_key);
+	$x_line_item = array();
+	$item_num = 1;
+	$registrations = $session_data['cart']['REG']['items'];
+	foreach ($registrations as $registration) {
+		foreach ($registration['attendees'] as $attendee) {
+			$x_line_item[] = 'item' . $item_num . '<|>' . $registration['name'] . '<|>' . $attendee['fname'] . ' ' . $attendee['lname'] . ' attending ' . $registration['name'] . ' on ' . $registration['options']['date'] . ' ' . $registration['options']['time'] . ', ' . $registration['options']['price_desc'] . '<|>1<|>' . $attendee['price_paid'] . '<|>N&';
+			$item_num++;
+		}
+	}
 
+	$total = $session_data['_cart_grand_total_amount'];
+	if (isset($session_data['tax_totals'])) {
+		foreach ($session_data['tax_totals'] as $key => $taxes) {
+			$total = $total + $taxes;
+			$x_line_item[] = 'item' . $item_num . '<|>' . $session_data['taxes'][$key]['name'] . '<|>' . 'Tax' . '<|>1<|>' . $taxes . '<|>N&';
+			$item_num++;
+		}
+	}
+
+	$myAuthorize->addField('x_line_item', $x_line_item);
+	$myAuthorize->addField('x_Relay_URL', home_url() . '/?page_id=' . $org_options['return_url'] . '&session_id=' . $session_data['id'] . '&attendee_action=post_payment&form_action=payment');
+	$myAuthorize->addField('x_Amount', number_format($total, 2));
+	$myAuthorize->addField('x_Logo_URL', $image_url);
+	$myAuthorize->addField('x_Invoice_num', 'au-' . $session_data['id']);
+	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, serialize(get_object_vars($myAuthorize)));
+	$session_data['gateway_data']['off-site-form'] = $myAuthorize->submitPayment();
+	$EE_Session->set_session_data($session_data['gateway_data'], 'gateway_data');
+}
+
+add_action('action_hook_espresso_gateway_process_step_3', 'espresso_send_to_authnet_sim');
+
+function espresso_process_authnet_sim($EE_Session) {
+
+// Include the authorize.net library
+	include_once ('lib/Authorize.php');
+// Create an instance of the authorize.net library
+	$myAuthorize = new EE_Authorize();
+
+// Log the IPN results
+	$myAuthorize->ipnLog = TRUE;
+	$session_data = $EE_Session->get_session_data();
+	$payment_settings = $session_data['gateway_data']['payment_settings']['authnet_sim'];
+
+	$txn_details = array(
+			'gateway' => $payment_settings['display_name'],
+			'approved' => FALSE,
+			'response_msg' => __('You\'re registration has not been completed successfully.', 'event_espresso'),
+			'status' => 'Incomplete',
+			'raw_response' => serialize($_POST),
+			'amount' => 0.00,
+			'method' => sanitize_text_field($_POST['x_method']),
+			'auth_code' => sanitize_text_field($_POST['x_auth_code']),
+			'md5_hash' => sanitize_text_field($_POST['x_MD5_Hash']),
+			'invoice_number' => sanitize_text_field($_POST['x_invoice_num']),
+			'transaction_id' => sanitize_text_field($_POST['x_invoice_num'])
+	);
+	$authnet_login_id = $payment_settings['authnet_login_id'];
+	$authnet_transaction_key = $payment_settings['authnet_transaction_key'];
+
+// Enable test mode if needed
+//4007000000027  <-- test successful visa
+//4222222222222  <-- test failure card number
+	if ($payment_settings['use_sandbox']) {
+		$myAuthorize->enableTestMode();
+		$email_transaction_dump = true;
+	}
+
+// Specify your authorize login and secret
 	$myAuthorize->setUserInfo($authnet_login_id, $authnet_transaction_key);
 
-	$myAuthorize->addField('x_Relay_URL', home_url() . '/?page_id=' . $org_options['notify_url']);
-	$myAuthorize->addField('x_Description', stripslashes_deep($event_name) . ' | ' . __('Reg. ID:', 'event_espresso') . ' ' . $attendee_id . ' | ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($fname . ' ' . $lname) . ' | ' . __('Total Registrants:', 'event_espresso') . ' ' . $quantity);
-	$myAuthorize->addField('x_Amount', number_format($event_cost, 2));
-	$myAuthorize->addField('x_Logo_URL', $image_url);
-	$myAuthorize->addField('x_Invoice_num', 'au-' . event_espresso_session_id());
-//Post variables
-	$myAuthorize->addField('x_Cust_ID', $attendee_id);
-	$myAuthorize->addField('x_first_name', $fname);
-	$myAuthorize->addField('x_last_name', $lname);
+// Check validity and write down it
+	if ($myAuthorize->validateIpn()) {
 
-	$myAuthorize->addField('x_Email', $attendee_email);
-	$myAuthorize->addField('x_Address', $address);
-	$myAuthorize->addField('x_City', $city);
-	$myAuthorize->addField('x_State', $state);
-	$myAuthorize->addField('x_Zip', $zip);
-
-
-//Enable this function if you want to send payment notification before the person has paid.
-//This function is copied on the payment processing page
-//event_espresso_send_payment_notification($attendee_id, $txn_id, $amount_pd);
-//Decide if you want to auto redirect to your payment website or display a payment button.
-	if (!empty($payment_settings['authnet_sim']['bypass_payment_page'])) {
-		$myAuthorize->submitPayment(); //Enable auto redirect to payment site
-	} else {
-		if (empty($payment_settings['authnet_sim']['button_url'])) {
-			//$button_url = EVENT_ESPRESSO_GATEWAY_URL . "authnet/btn_cc_vmad.gif";
-			if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/authnet/btn_cc_vmad.gif")) {
-				$button_url = EVENT_ESPRESSO_GATEWAY_DIR . "/authnet/btn_cc_vmad.gif";
-			} else {
-				$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/authnet/btn_cc_vmad.gif";
-			}
-		} elseif (file_exists($payment_settings['authnet_sim']['button_url'])) {
-			$button_url = $payment_settings['authnet_sim']['button_url'];
-		} else {
-			//If no other buttons exist, then use the default location
-			$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/authnet/btn_cc_vmad.gif";
+		if ($myAuthorize->ipnData['x_response_code'] == 1) {
+		$txn_details['approved']			= TRUE;
+		$txn_details['amount']			= floatval($_REQUEST['x_amount']);
+		$txn_details['response_msg']	= __('You\'re registration has been completed successfully.', 'event_espresso');
+		$txn_details['status']				= 'Approved';
 		}
-		$myAuthorize->submitButton($button_url, 'authnet'); //Display payment button
-	}
+		$EE_Session->set_session_data(array('txn_results' => $txn_details), 'session_data');
 
-	if ($use_sandbox == true || $use_testmode == true) {
-		echo '<p>Test credit card # 4007000000027</p>';
-		echo '<h3 style="color:#ff0000;" title="Payments will not be processed">' . __('Debug Mode Is Turned On', 'event_espresso') . '</h3>';
-		$myAuthorize->dump_fields(); // for debugging, output a table of all the fields
+		if ($txn_details['approved'] == TRUE && $payment_settings['use_sandbox']) {
+			do_action('action_hook_espresso_mail_successful_transaction_debugging_output');
+		} else {
+			do_action('action_hook_espresso_mail_failed_transaction_debugging_output');
+		}
 	}
 }
+
+add_action('action_hook_espresso_process_off_site_payment', 'espresso_process_authnet_sim');
