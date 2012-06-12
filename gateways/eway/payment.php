8281eba427dad2553dcc5c6b6a12a0308f3f7df0
Sidney Harrel <sidney@eventespresso.com>
2012-06-12 06:13:18 +0000

    adding eway gateway [touch:728]

--- gateways/eway/payment.php
+++ gateways/eway/payment.php
@@ -1,76 +1,23 @@
 <?php
 
-// Setup class
-include_once ('Eway.php');
-echo '<!-- Event Espresso eway Gateway Version ' . $eway_gateway_version . '-->';
-$myeway = new eway(); // initiate an instance of the class
-global $org_options;
-do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-$payment_settings = get_option('payment_data_' . $espresso_wp_user);
-$eway_settings = $payment_settings['eway'];
-$eway_id = $eway_settings['eway_id'];
-$eway_username = $eway_settings['eway_username'];
-//$image_url = $eway_settings['button_url'];
-$eway_cur = $eway_settings['currency_format'];
-$use_sandbox = $eway_settings['use_sandbox'];
+function espresso_display_eway() {
+	global $EE_Session;
+	$gateway = 'eway';
+	$gateway_data = $EE_Session->get_session_data(FALSE, 'gateway_data');
 
-$quantity = isset($quantity) && $quantity > 0 ? $quantity : espresso_count_attendees_for_registration($attendee_id);
-if ($use_sandbox == 1) {
-	// Enable test mode if needed
-	$myeway->enableTestMode();
-	$myeway->addField('CustomerID', '87654321');
-	$myeway->addField('UserName', 'TestAccount');
-} else {
-	$myeway->addField('CustomerID', $eway_id);
-	$myeway->addField('UserName', $eway_username);
-}
-$myeway->addField('Amount', number_format($event_cost, 2, '.', ''));
-$myeway->addField('Currency', $eway_cur);
-$myeway->addField('PageTitle', '');
-$myeway->addField('PageDescription', '');
-$myeway->addField('PageFooter', '');
-$myeway->addField('Language', '');
-$myeway->addField('CompanyName', str_replace("&", "%26", $org_options['organization']));
-$myeway->addField('CustomerFirstName', $fname);
-$myeway->addField('CustomerLastName', $lname);
-$myeway->addField('CustomerAddress', $address);
-$myeway->addField('CustomerCity', $city);
-$myeway->addField('CustomerState', $state);
-$myeway->addField('CustomerPostCode', $zip);
-$myeway->addField('CustomerCountry', '');
-$myeway->addField('CustomerEmail', $attendee_email);
-$myeway->addField('CustomerPhone', $phone);
-$myeway->addField('InvoiceDescription', stripslashes_deep($event_name) . ' | ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($fname . ' ' . $lname) . ' | ' . __('Registrant Email:', 'event_espresso') . ' ' . $attendee_email . ' | ' . __('Total Registrants:', 'event_espresso') . ' ' . $quantity);
-$myeway->addField('CancelURL', str_replace("&", "%26", home_url() . '/?page_id=' . $org_options['cancel_return']));
-$myeway->addField('ReturnURL', str_replace("&", "%26", home_url() . '/?page_id=' . $org_options['notify_url'] . '&id=' . $attendee_id . '&event_id=' . $event_id . '&attendee_action=post_payment&form_action=payment'));
-$myeway->addField('CompanyLogo', $eway_settings['image_url']);
-$myeway->addField('PageBanner', '');
-$myeway->addField('MerchantReference', '');
-$myeway->addField('MerchantInvoice', '');
-$myeway->addField('MerchantOption1', '');
-$myeway->addField('MerchantOption2', '');
-$myeway->addField('MerchantOption3', '');
-$myeway->addField('ModifiableCustomerDetails', 'false');
+	$gateway_settings = $gateway_data['payment_settings'][$gateway];
+	
+	$gateways = $gateway_data['html_data']
+?>
+			<a id="payment-gateway-button-<?php echo $gateway;?>" class="reg-page-payment-option-lnk<?php echo $gateways[ $gateway ]['css_link_class'];?>" rel="<?php echo $gateway;?>" href="<?php echo $gateways[ $gateway ]['form_url'];?>" >
+				<img src="<?php echo $gateway_settings['button_url'];?>" alt="Pay using Eway" />
+			</a>
 
+			<div id="reg-page-billing-info-<?php echo $gateway;?>-dv" class="reg-page-billing-info-dv <?php echo $gateways[ $gateway ]['css_class'];?>">
+				<?php _e('After confirming the details of your registration in Step 3, you will be transferred to the Eway.com website where your payment will be securely processed.', 'event_espresso'); ?>
+			</div>
 
-if ($eway_settings['bypass_payment_page']) {
-	$myeway->submitPayment(); //Enable auto redirect to payment site
-} else {
-	if (empty($eway_settings['button_url'])) {
-		if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "eway/eway_logo.png")) {
-			$button_url = EVENT_ESPRESSO_GATEWAY_URL . "eway/eway_logo.png";
-		} else {
-			$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/eway/eway_logo.png";
-		}
-	} else {
-		$button_url = $eway_settings['button_url'];
-	}
-	$myeway->submitButton($button_url, 'eway'); //Display payment button
-	wp_deregister_script('jquery.validate.pack');
+<?php
 }
 
-if ($use_sandbox == true) {
-	echo '<h3 style="color:#ff0000;" title="Payments will not be processed">' . __('Debug Mode Is Turned On', 'event_espresso') . '</h3>';
-	$myeway->dump_fields(); // for debugging, output a table of all the fields
-}
-
+add_action('action_hook_espresso_display_offsite_payment_gateway', 'espresso_display_eway');
