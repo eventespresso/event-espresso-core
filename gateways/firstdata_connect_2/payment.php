7477b2e5fff720146ebba002769168389f285b52
Sidney Harrel <sidney@eventespresso.com>
2012-06-26 18:38:31 +0000

    modified payment.php for fd connect 2.0 in 3.2 [touch:728]

--- gateways/firstdata_connect_2/payment.php
+++ gateways/firstdata_connect_2/payment.php
@@ -1,23 +1,22 @@
 <?php
 
-global $org_options, $wpdb;
-do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-$firstdata_connect_2_settings = get_option('event_espresso_firstdata_connect_2_settings');
-include("Fdggutil.php");
-$sql = "SELECT amount_pd FROM " . EVENTS_ATTENDEE_TABLE . " WHERE id = '" . $attendee_id . "'";
-$total_cost = $wpdb->get_var($sql);
-$fdggutil = new Fdggutil($firstdata_connect_2_settings['storename'],
-								$firstdata_connect_2_settings['sharedSecret']);
-$fdggutil->set_timezone($firstdata_connect_2_settings['timezone']);
-$fdggutil->set_chargetotal($total_cost);
-$fdggutil->set_sandbox($firstdata_connect_2_settings['sandbox']);
-$fdggutil->set_returnUrl($org_options['notify_url']);
-$fdggutil->set_cancelUrl($org_options['cancel_return']);
-$fdggutil->set_attendee_id($attendee_id);
-$fdggutil->set_dateTime();
-$button_url = $firstdata_connect_2_settings['button_url'];
-if (!empty($firstdata_connect_2_settings['bypass_payment_page'])) {
-	echo $fdggutil->submitPayment();
-} else {
-	echo $fdggutil->submitButton($button_url);
+function espresso_display_firstdata_connect_2() {
+	global $EE_Session;
+	$gateway = 'firstdata_connect_2';
+	$gateway_data = $EE_Session->get_session_data(FALSE, 'gateway_data');
+
+	$gateway_settings = $gateway_data['payment_settings'][$gateway];
+	
+	$gateways = $gateway_data['html_data']
+?>
+			<a id="payment-gateway-button-<?php echo $gateway;?>" class="reg-page-payment-option-lnk<?php echo $gateways[ $gateway ]['css_link_class'];?>" rel="<?php echo $gateway;?>" href="<?php echo $gateways[ $gateway ]['form_url'];?>" >
+				<img src="<?php echo $gateway_settings['button_url'];?>" alt="Pay using FirstData Connect 2.0" />
+			</a>
+
+			<div id="reg-page-billing-info-<?php echo $gateway;?>-dv" class="reg-page-billing-info-dv <?php echo $gateways[ $gateway ]['css_class'];?>">
+				<?php _e('After confirming the details of your registration in Step 3, you will be transferred to the FirstData Connect 2.0 website where your payment will be securely processed.', 'event_espresso'); ?>
+			</div>
+<?php 
 }
+
+add_action('action_hook_espresso_display_offsite_payment_gateway', 'espresso_display_firstdata_connect_2');
