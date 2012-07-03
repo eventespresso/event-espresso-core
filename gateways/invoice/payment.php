bbdf50d4d5915cde037fa44e9feb259286f6fe79
Sidney Harrel <sidney@eventespresso.com>
2012-07-03 01:06:41 +0000

    (trunk) invoice changes for 3.2 [touch:728]

--- gateways/invoice/payment.php
+++ gateways/invoice/payment.php
@@ -1,30 +1,23 @@
 <?php
-// Setup payment page
-if (!$payment_settings['invoice']['show'])
-	return;
-if(isset($default_gateway_version))echo '<!--Event Espresso Default Gateway Version ' . $default_gateway_version . '-->';
-if(isset($payment_settings['invoice']['invoice_title'])) { ?>
+function espresso_display_invoice() {
+	global $EE_Session;
+	$gateway = 'invoice';
+	$gateway_data = $EE_Session->get_session_data(FALSE, 'gateway_data');
 
-<div class="event-display-boxes">
-	<?php	echo '<h4 id="invoice_title" class="payment_type_title section-heading">'.stripslashes_deep($payment_settings['invoice']['invoice_title']).'</h4>';
-}
-
-/*$pdf_url = home_url().'/?invoice_type=' . ( empty($invoice_type) ? '' : $invoice_type ) . '&amp;download_invoice=true&amp;attendee_id='.$attendee_id.'&amp;registration_id='.registration_id;
-
-$page_url = home_url().'/?invoice_type=' . ( empty($invoice_type) ? '' : $invoice_type ) . '&amp;download_invoice=true&amp;attendee_id='.$attendee_id.'&amp;registration_id='.registration_id;
-*/
+	$invoice_settings = $gateway_data['payment_settings'][$gateway];
+	
+	$gateways = $gateway_data['html_data']
 ?>
-	<p><a href="<?php echo espresso_invoice_url($attendee_id, $registration_id); ?>" class="inline-button ui-priority-primary ui-state-default ui-state-hover ui-state-focus ui-corner-all" target="_blank">
-		<?php _e('Download PDF Invoice', 'event_espresso'); ?>
-		</a></p>
-	<?php
-if(isset($payment_settings['invoice']['page_instructions'])) {
-	echo '<div class="event-messages ui-state-highlight"><span class="ui-icon ui-icon-alert"></span><p class="instruct">' . stripslashes_deep($payment_settings['invoice']['page_instructions']) . '</p></div>';
-}
-if(isset($invoice_payment_settings['payment_address'])){ ?>
-	<div class="address-block">
-		<?php	echo wpautop(stripslashes_deep($invoice_payment_settings['payment_address'])); ?>
-	</div>
-</div>
+			<a id="payment-gateway-button-<?php echo $gateway;?>" class="reg-page-payment-option-lnk<?php echo $gateways[ $gateway ]['css_link_class'];?>" rel="<?php echo $gateway;?>" href="<?php echo $gateways[ $gateway ]['form_url'];?>" >
+				<button type="button">Invoice</button>
+			</a>
+
+			<div id="reg-page-billing-info-<?php echo $gateway;?>-dv" class="reg-page-billing-info-dv <?php echo $gateways[ $gateway ]['css_class'];?>">
+				<?php _e('After confirming the details of your registration in Step 3, you will be transferred to the payment overview where your payment will be securely processed, and you can download your invoice.', 'event_espresso'); ?>
+			</div>
+
 <?php
 }
+
+add_action('action_hook_espresso_display_offsite_payment_gateway', 'espresso_display_invoice');
+
