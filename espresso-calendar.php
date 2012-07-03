7eaf1ed9fd1b5328969a9510faa95f2efdfbc1b3
Chris Reynolds <chris@eventespresso.com>
2012-07-03 00:01:01 +0000

    updates versions

--- espresso-calendar.php
+++ espresso-calendar.php
@@ -3,7 +3,7 @@
   Plugin Name: Event Espresso - Calendar
   Plugin URI: http://www.eventespresso.com
   Description: A full calendar addon for Event Espresso. Includes month, week, and day views.
-  Version: 2.0.2
+  Version: 2.0.3
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
   Copyright 2012 Seth Shoultes(email : seth@eventespresso.com)
@@ -24,7 +24,7 @@
 
 //Define the version of the plugin
 function espresso_calendar_version() {
-	return '2.0.2';
+	return '2.0.3';
 }
 
 //Update notifications
