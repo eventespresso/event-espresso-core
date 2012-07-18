13a1222461fad2f9074037914683b5fe9cbfc549
Sidney Harrel <sidney@eventespresso.com>
2012-07-18 18:27:24 +0000

    (3.2 - invoice-sidney branch) changes to invoice template file

--- gateways/invoice/lib/templates/index.php
+++ gateways/invoice/lib/templates/index.php
@@ -1,20 +1,16 @@
-<?php
-global $org_options, $payment_settings;
-$base_dir = dirname(__FILE__);
-?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title><?php echo stripslashes_deep($org_options['organization']) ?> <?php _e('Invoice for', 'event_espresso'); ?> <?php echo stripslashes_deep($primary_attendee['fname'] . ' ' .$primary_attendee['lname']) ?> | <?php echo $primary_attendee['registration_id'] ?></title>
+<title><?php echo $organization; ?> <?php _e('Invoice for', 'event_espresso'); ?> <?php echo $name; ?> | <?php echo $primary_attendee['registration_id'] ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <!-- Base Stylesheet do not change or remove -->
-<link rel="stylesheet" type="text/css" href="<?php echo $base_dir; ?>base.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>base.css" media="screen" />
 
 <!-- Print Style Sheet -->
-<link rel="stylesheet" type="text/css" href="<?php echo $base_dir; ?>css/print/<?php echo str_replace('.css', '',$payment_settings['invoice']['invoice_css']); ?>_print.css" media="print" />
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>css/print/<?php echo str_replace('.css', '',$invoice_css); ?>_print.css" media="print" />
 
 <!-- Primary Style Sheet -->
-<link rel="stylesheet" type="text/css" href="<?php echo $base_dir; ?>css/<?php echo $payment_settings['invoice']['invoice_css']; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>css/<?php echo $invoice_css; ?>" />
 
 <!-- Make sure the buttons don't print -->
 <style type="text/css">
@@ -28,45 +24,43 @@ $base_dir = dirname(__FILE__);
 	<form>
 		<input class="print_button noPrint" type="button" value=" Print Invoice " onclick="window.print();return false;" />
 	</form>
-	<form method="post" action="<?php echo home_url(); ?>/?download_invoice=true&amp;attendee_id=<?php echo $data->attendee->id ?>&amp;registration_id=<?php echo $data->attendee->registration_id ?>" >
+	<form method="post" action="<?php echo $download_link; ?>" >
 		<input class="print_button noPrint" type="submit" value=" Download PDF " />
 	</form>
 </div>
 <div id="invoice">
-	<div id="invoice-header"> <?php echo $data->event->invoice_logo_image ?>
+	<div id="invoice-header"> <?php echo $invoice_logo_image; ?>
 		<!-- hCard microformat -->
 		<div class="vcard" id="company-address">
-			<div class="fn org"><strong><?php echo stripslashes_deep($org_options['organization']) ?></strong></div>
+			<div class="fn org"><strong><?php echo $organization; ?></strong></div>
 			<div class="adr">
-				<div class="street-address"><?php echo $org_options['organization_street1'] ?><br/>
-					<!--<?php echo $org_options['organization_street2'] ?><br /> -->
+				<div class="street-address"><?php echo $street; ?>
 				</div>
 				<!-- street-address -->
-				<div class="locality"><?php echo $org_options['organization_city'] ?>, <?php echo $org_options['organization_state'] ?></div>
-				<div id="company-postcode"><span class="postal-code"><?php echo $org_options['organization_zip'] ?></span></div>
+				<div class="locality"><?php echo $city; ?>, <?php echo $state; ?></div>
+				<div id="company-postcode"><span class="postal-code"><?php echo $zip; ?></span></div>
 			</div>
 			<!-- adr -->
-			<div class="email"><?php echo $org_options['contact_email'] ?></div>
+			<div class="email"><?php echo $email; ?></div>
 		</div>
 		<!-- company-address vcard --> 
 		
 	</div>
 	<!-- #invoice-header -->
 	<div id="invoice-info">
-		<h2>Invoice <strong><?php echo $data->attendee->registration_id ?></strong></h2>
-		<h3><?php echo event_date_display($data->attendee->registration_date) ?></h3>
+		<h2>Invoice <strong><?php echo $registration_code; ?></strong></h2>
+		<h3><?php echo $registration_date; ?></h3>
 	</div>
 	<!-- #invoice-info -->
 	<div class="vcard" id="client-details">
-		<div class="fn"><?php echo stripslashes_deep($data->attendee->fname . ' ' .$data->attendee->lname) ?></div>
+		<div class="fn"><?php echo $name ?></div>
 		<!--<div class="org">Client Company</div> -->
 		<div class="adr">
-			<div class="street-address"><?php echo stripslashes_deep($data->attendee->address) ?><br/>
-				<!--<?php echo stripslashes_deep($data->attendee->address2) ?><br /> -->
+			<div class="street-address"><?php echo $attendee_address; ?>
 			</div>
 			<!-- street-address -->
-			<div class="locality"><?php echo stripslashes_deep($data->attendee->city) ?>, <?php echo stripslashes_deep($data->attendee->state) ?></div>
-			<div id="client-postcode"><?php echo stripslashes_deep($data->attendee->zip) ?></div>
+			<div class="locality"><?php echo $attendee_city; ?>, <?php echo $attendee_state; ?></div>
+			<div id="client-postcode"><?php echo $attendee_zip; ?></div>
 			<!--<div id="your-tax-number">SALES TAX: 193528491</div> -->
 		</div>
 		<!-- adr --> 
@@ -77,26 +71,15 @@ $base_dir = dirname(__FILE__);
 			<tr id="header_row">
 				<th class="quantity_th"><?php _e('Quantity', 'event_espresso'); ?></th>
 				<th class="left details_th"><?php _e('Event & Attendee', 'event_espresso'); ?></th>
-				<th class="unitprice_th"><?php _e('Unit Price', 'event_espresso'); ?> (<?php echo $org_options['currency_symbol'] ?>)</th>
-				<th class="subtotal_th"><?php _e('Net Subtotal', 'event_espresso'); ?> (<?php echo $org_options['currency_symbol'] ?>)</th>
+				<th class="unitprice_th"><?php _e('Unit Price', 'event_espresso'); ?> (<?php echo $currency_symbol ?>)</th>
+				<th class="subtotal_th"><?php _e('Net Subtotal', 'event_espresso'); ?> (<?php echo $currency_symbol ?>)</th>
 			</tr>
 		</thead>
 		<tfoot>
-		
-			<?php if ( $amount_pd != $total_cost ){
-		$net_total = espressoInvoiceTotals("",$total_cost,$left,$right);
-		$discount = $amount_pd - $total_cost;
-		if ( $discount < 0 ){
-			$text = "Discount:";
-		}else{
-			$text = "Extra:";
-		}
-		$discount = espressoInvoiceTotals($text,$discount);
-	}?>
 			<tr id="net_total_tr">
 				<td colspan="2">&nbsp;</td>
 				<td class="item_r"><?php _e('Net Total', 'event_espresso'); ?></td>
-				<td class="item_r"><?php echo $org_options['currency_symbol'].$data->attendee->amount_pd ?></td>
+				<td class="item_r"><?php echo $currency_symbol.$net_total ?></td>
 			</tr>
 			<!--<tr id="vat_tr">
 				<td colspan="2">&nbsp;</td>
@@ -105,13 +88,13 @@ $base_dir = dirname(__FILE__);
 			</tr> -->
 			<tr id="total_tr">
 				<td colspan="2">&nbsp;</td>
-				<td class="total" id="total_currency"><span class="currency"><?php echo $org_options['currency_format'] ?> </span> Total</td>
-				<td class="total"><?php echo $org_options['currency_symbol'].$data->attendee->amount_pd ?></td>
+				<td class="total" id="total_currency"><span class="currency"><?php echo $currency_symbol; ?> </span> Total</td>
+				<td class="total"><?php echo $currency_symbol.$total_cost ?></td>
 			</tr>
 		</tfoot>
 		<tbody>
 			<!-- Create the table rows for data -->
-			<?php echo espressoImprovedTable($attendees);?>
+			<?php echo $table_output;?>
 		</tbody>
 	</table>
 	<!-- invoice-amount -->
@@ -131,7 +114,7 @@ $base_dir = dirname(__FILE__);
 		<div id="payment-reference"><strong><?php _e('Payment Reference:', 'event_espresso'); ?></strong> INV001</div>
 	</div>
 	<!-- payment-details -->
-	<div id="comments"><?php echo wpautop(stripslashes_deep(html_entity_decode($payment_settings['invoice']['pdf_instructions'], ENT_QUOTES))) ?></div>
+	<div id="comments"><?php echo $pdf_instructions; ?></div>
 	<!-- comments --> 
 </div>
 <div class="print_button_div">
