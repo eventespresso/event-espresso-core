789c1fc7cc16362741c9311b3ec541cfe8ed71b7
Brent Christenson <brent@eventespresso.com>
2012-08-22 20:38:35 +0000

    commit changes after merging trunk to branch [touch:1299]

---
+++ includes/core/admin/registrations/templates/reg_admin_details_header.template.php
@@ -0,0 +1,6 @@
+	
+		<h3 style="padding:0;margin:-.5em 0 1em;"><?php echo __( 'Registration # ', 'event_espresso' ) . $reg_nmbr['value'];?></h3>
+		<h2 style="padding:0;margin:0 0 1em;"><?php echo $reg_datetime['value'];?></h2>
+		<h2 style="padding:0;margin:0 0 1em;"><?php echo __( 'Registration Status : ', 'event_espresso' );?><span class="<?php echo $reg_status['class'];?>"><?php echo $reg_status['value'];?></span></h2>
+		
+
---
+++ includes/core/admin/transactions/templates/txn_admin_details_header.template.php
@@ -0,0 +1,20 @@
+
+		<h3 style="padding:0;margin:-.5em 0 1em;"><?php echo __( 'Transaction # ', 'event_espresso' ) . $txn_nmbr['value'];?></h3>
+		
+		<h2 style="padding:0;margin:0 0 1em;"><?php echo $txn_datetime['value'];?></h2>
+		
+		<h2 id="txn-status-h2" style="padding:0;margin:0 0 1em;">
+			<?php echo __( 'Transaction Status: ', 'event_espresso' );?><span id="txn-status" class="<?php echo $txn_status['class'];?>"><?php echo $txn_status['value'];?></span>
+		</h2>
+		
+	<?php if ( $amount_due ) : ?>
+		<h2 id="txn-amount-due-h2" style="padding:0;margin:0 0 1em;">
+			<?php echo __( 'Total Amount Due: ', 'event_espresso' );?><span class="<?php echo $amount_due_class;?>"><?php echo $amount_due;?></span>
+		</h2>
+	<?php else : ?>
+		<h2 id="txn-amount-due-h2" class="hidden" style="padding:0;margin:0 0 1em;">
+			<?php echo __( 'Total Amount Due: ', 'event_espresso' );?><span class="<?php echo $amount_due_class;?>"><?php echo $amount_due;?></span>
+		</h2>
+	<?php endif; ?>
+				
+
