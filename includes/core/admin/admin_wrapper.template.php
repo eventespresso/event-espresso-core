f1d958f2e8e54f16cca212fde9101b70e4690849
Darren Ethier <darren@roughsmootheng.in>
2012-12-30 10:35:46 -0500

    modified template for new `$nav_tabs` value (prepared ahead of time) [touch: 1646]

--- includes/core/admin/admin_wrapper.template.php
+++ includes/core/admin/admin_wrapper.template.php
@@ -3,13 +3,10 @@
 	<div id="icon-options-event" class="icon32"></div>	
 		
 	<h2><?php esc_attr_e( 'Event Espresso', 'event_espresso' );?>&nbsp;-&nbsp;<?php  echo $admin_page_title; ?></h2>
+	
 	<?php echo $notices; ?>
+	<?php echo $nav_tabs; ?>
 
-	<h2 class="nav-tab-wrapper">
-		<?php foreach ( $nav_tabs as $nav_tab ) : ?>
-			<a class="nav-tab<?php echo $nav_tab['css_class'];?>" href="<?php echo $nav_tab['url'];?>"><?php echo $nav_tab['link_text'];?></a>
-		<?php endforeach; ?>
-	</h2>
 
 <?php 
 	do_action( 'action_hook_espresso_before_admin_page_content' );
