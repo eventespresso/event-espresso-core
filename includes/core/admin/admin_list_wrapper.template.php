ef0a4082215ae4ab6c0821c42bb96c6096875471
Darren Ethier <darren@roughsmootheng.in>
2012-12-29 21:49:53 -0500

    adding in new global admin page list table template [touch: 1646]

---
+++ includes/core/admin/admin_list_wrapper.template.php
@@ -0,0 +1,6 @@
+		
+	<?php $list_table->display_views(); ?>
+
+    <form id="<?php echo $current_page; ?>-table-frm" action="<?php echo $table_url;?>" method="post">
+		<?php echo $list_table->display(); ?>
+    </form>
