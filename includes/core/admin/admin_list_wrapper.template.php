da4d299dd73c7a275a8fa3b9025526f1a3cbbe01
Darren Ethier <darren@roughsmootheng.in>
2013-01-02 22:41:30 -0500

    add search box [touch: 1646]

--- includes/core/admin/admin_list_wrapper.template.php
+++ includes/core/admin/admin_list_wrapper.template.php
@@ -1,5 +1,5 @@
 	<?php $list_table->display_views(); ?>
-
     <form id="<?php echo $current_page; ?>-table-frm" action="<?php echo $table_url;?>" method="post">
+    	<?php echo $list_table->search_box(__('Search Events', 'event_espresso'), $current_page); ?>
 		<?php echo $list_table->display(); ?>
     </form>
