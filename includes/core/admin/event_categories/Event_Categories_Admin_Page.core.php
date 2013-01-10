be1bcfa0bc234bc73c7109ae0c0ad666640c7b45
Darren Ethier <darren@roughsmootheng.in>
2013-01-10 13:32:53 -0500

    loading event_categories overview admin page now. [touch: 1646] [touch: 1923]

--- includes/core/admin/event_categories/Event_Categories_Admin_Page.core.php
+++ includes/core/admin/event_categories/Event_Categories_Admin_Page.core.php
@@ -72,7 +72,193 @@ class Event_Categories_Admin_Page extends EE_Admin_Page {
 
 	protected function _set_page_routes() {
 		$this->_page_routes = array(
-			
+			'default' => '_overview_list_table',
+			'edit_category' => array(
+				'func' => '_category_details',
+				'args' => array('edit')
+				),
+			'add_category' => array(
+				'func' => '_category_details',
+				'args' => array('add')
+				),
+			'delete_categories' => array(
+				'func' => '_delete_categories', //coming from overview page
+				'noheader' => TRUE 
+				),
+			'delete_category' => array(
+				'func' => '_delete_category', //coming from details page
+				'noheader' => TRUE
+				),
+			'insert_category' => array(
+				'func' => '_insert_or_update_category',
+				'args' => array('new_category' => TRUE),
+				'noheader' => TRUE
+				),
+			'update_category' => array(
+				'func' => '_insert_or_update_category',
+				'args' => array('new_category' => FALSE),
+				'noheader' => TRUE
+				),
+			'export_categories' => array(
+				'func' => '_categories_export',
+				'noheader' => TRUE
+				),
+			'import_categories' => '_import_categories'
 		);
 	}
-}
+
+
+
+
+	protected function _set_page_config() {
+		$this->_page_config = array(
+			'default' => array(
+				'nav' => array(
+					'label' => __('Overview', 'event_espresso'),
+					'order' => 10
+					),
+				'list_table' => 'Event_Categories_Admin_List_Table',
+				'metaboxes' => array('_espresso_news_post_box', '_espresso_links_post_box'),
+				),
+			'add_category' => array(
+				'nav' => array(
+					'label' => __('Add Category', 'event_espresso'),
+					'order' => 5,
+					'persistent' => false),
+				'metaboxes' => array(),
+				),
+			'edit_category' => array(
+				'nav' => array(
+					'label' => __('Edit Category', 'event_espresso'),
+					'order' => 5,
+					'persistent' => FALSE),
+				'metaboxes' => array()
+				),
+			'import_categories' => array(
+				'nav' => array(
+					'label' => __('Import', 'event_espresso'),
+					'order' => 30
+					),
+				'metaboxes' => array('_espresso_news_post_box', '_espresso_links_post_box')
+				)
+		);
+	}
+
+
+
+
+
+	protected function _add_screen_options() {
+		//todo
+	}
+
+
+
+
+
+	protected function _add_screen_options_default() {
+		$this->_per_page_screen_option();
+	}
+
+
+
+
+
+
+	//none of the below group are currently used for Event Categories
+	protected function _add_help_tabs() {}
+	protected function _add_feature_pointers() {}
+	public function load_scripts_styles() {}
+	public function admin_init() {}
+	public function admin_notices() {}
+	public function admin_footer_scripts() {}
+
+
+
+
+
+
+	public function load_scripts_styles_add_event() {
+		$this->load_scripts_styles_edit_event();
+	}
+
+
+
+
+
+	public function load_scripts_styles_edit_event() {
+		//styles
+		wp_enqueue_style('jquery-ui-style');
+
+		//scripts
+		wp_enqueue_script('ee_admin_js');
+	}
+
+
+
+
+
+
+	protected function _set_list_table_views_default() {
+		$this->_views = array(
+			'all' => array(
+				'slug' => 'all',
+				'label' => __('All', 'event_espreso'),
+				'count' => 0,
+				'bulk_action' => array(
+					'delete_categories' => __('Delete Permanently', 'event_espresso'),
+					'export_categories' => __('Export Categories', 'event_espresso'),
+					)
+				)
+		);
+	}
+
+
+
+
+
+	protected function _overview_list_table() {
+		do_action( 'action_hook_espresso_log', __FILE__, __FUNCTION__, '' );
+
+		$this->_admin_page_title .= $this->_get_action_link_or_button('add_category', 'add', array(), 'button add-new-h2');
+		$this->display_admin_list_table_page_with_sidebar();
+	}
+
+
+
+
+
+	protected function _category_details() {}
+	protected function _delete_categories() {}
+	protected function _delete_category() {}
+	protected function _insert_or_update_category($new) {}
+	protected function _categories_export() {}
+	protected function _import_categories() {}
+
+
+	/***********/
+	/* QUERIES */
+
+
+	public function get_categories( $per_page = 10, $current_page = 1, $count = FALSE ) {
+		global $wpdb;
+
+		$offset = ($current_page-1)*$per_page; 
+		$limit = apply_filters('filter_hook_espresso_category_list_limit', $count ? '' : ' LIMIT ' . $offset . ',' . $per_page, $offset, $per_page);
+		$orderby = apply_filters( 'filter_hook_espresso_category_list_orderby', isset($_REQUEST['orderby']) ? " ORDER BY " . $_REQUEST['orderby'] : " ORDER BY c.category_name", $_REQUEST );
+		$order = apply_filters( 'filter_hook_espresso_category_list_order', isset($_REQUEST['order']) ? " " . $_REQUEST['order'] : " DESC", $_REQUEST);
+
+		$sql = $count ? "SELECT COUNT(c.id) FROM " . EVENTS_CATEGORY_TABLE . " c" : "SELECT * FROM " . EVENTS_CATEGORY_TABLE . " c";
+
+		$sql = apply_filters('filter_hook_espresso_category_list_sql', $sql);
+		$sql .= !$count ? " GROUP BY c.id " . $orderby . $order . $limit : '';
+
+		$categories = $count ? $wpdb->get_var( $sql ) : $wpdb->get_results( $sql );
+		return $categories;
+	}
+
+
+	/* end queries */
+	/**************/
+
+} //end Event_Categories_Admin_Page class
