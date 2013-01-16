12118e3b32ec202b06df9d2eb03dc06c1465afc6
Brent Christensen <brent@eventespresso.com>
2013-01-16 11:22:48 -0800

    remove constructor arg [touch:1924]

--- includes/core/admin/pricing/Pricing_Admin_Page.core.php
+++ includes/core/admin/pricing/Pricing_Admin_Page.core.php
@@ -42,8 +42,8 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 	 * 	@access public
 	 * 	@return void
 	 */
-	public function __construct($wp_page_slug) {
-		parent::__construct($wp_page_slug);
+	public function __construct() {		
+		parent::__construct();
 	}
 
 
