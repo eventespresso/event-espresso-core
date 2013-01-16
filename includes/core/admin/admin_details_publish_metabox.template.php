075915561a4fa8d7a433e655a87cadcdd65eb510
Brent Christensen <brent@eventespresso.com>
2013-01-16 13:56:41 -0800

    adds check for delete link [touch:1924]

--- includes/core/admin/admin_details_publish_metabox.template.php
+++ includes/core/admin/admin_details_publish_metabox.template.php
@@ -1,14 +1,19 @@
 <?php echo $publish_box_extra_content; ?>
+
 <div class="submitbox" id="submitpost">
+
+	<?php if ( $publish_delete_link ) : ?>
 	<div id="delete-action">
-			<?php echo $publish_delete_link; ?>
+		<?php echo $publish_delete_link; ?>
 	</div>
+	<?php endif; ?>
+	
 	<div class="hidden-fields">
-		<!-- any hidden fields -->
+		<!-- hidden fields -->
 		<?php echo $publish_hidden_fields; ?>
 	</div>
 	<br/>
-	<?php
-		echo $save_buttons;
-	?>
+	
+	<?php echo $save_buttons; ?>
+	
 </div> <!-- end #submitpost -->
