e32d46e5930aea779cc19ff12f8a5df8a805cb4e
Darren Ethier <darren@roughsmootheng.in>
2013-01-21 10:25:56 -0500

    - setup initial file structure - finished `Venues_Admin_Page_Init` class [touch: 1932]

---
+++ includes/core/admin/venues/Venues_Admin_Page_Init.core.php
@@ -0,0 +1,57 @@
+<?php
+if (!defined('EVENT_ESPRESSO_VERSION') )
+	exit('NO direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for Wordpress
+ *
+ * @package		Event Espresso
+ * @author		Seth Shoultes
+ * @copyright	(c)2009-2012 Event Espresso All Rights Reserved.
+ * @license		http://eventespresso.com/support/terms-conditions/  ** see Plugin Licensing **
+ * @link		http://www.eventespresso.com
+ * @version		3.2.P
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Venues_Admin_Page_Init
+ *
+ * This is the init for the EE Event Categories Admin Pages.  See EE_Admin_Page_Init for method inline docs. 
+ *
+ * NOTE:  TODO: This is a straight conversion from the legacy 3.1 event venues related pages.  It is NOT optimized and will need modification to fully use the new system (and also will need adjusted when Event Venues model is setup)
+ *
+ * @package		Venues_Admin_Page_Init
+ * @subpackage	includes/core/admin/Venues_Admin_Page_Init.core.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class Venues_Admin_Page_Init extends EE_Admin_Page_Init {
+
+
+	public function __construct() {
+		//define some event categories related constants
+		define( 'EE_VENUES_ADMIN_URL', admin_url('admin.php?page=ee_venues') );
+
+		parent::__construct();
+	}
+
+	protected function _set_init_properties() {
+		$this->label = __('Event Venues', 'event_espresso');
+		$this->menu_label = __('Venues', 'event_espresso');
+		$this->menu_slug = 'ee_venues';
+	}
+
+	public function get_menu_map() {
+		$map = array(
+			'group' => 'management',
+			'menu_order' => 30,
+			'show_on_menu' => TRUE,
+			'parent_slug' => 'events'
+			);
+		return $map;
+	}
+
+} //end Venues_Admin_Page_Init
