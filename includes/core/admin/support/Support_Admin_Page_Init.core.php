30ffa5714bed91a3c2f4996a994c50d2b6503312
Darren Ethier <darren@roughsmootheng.in>
2013-01-23 12:52:05 -0500

    finished `Support_Admin_Page_Init` [touch: 1938]

---
+++ includes/core/admin/support/Support_Admin_Page_Init.core.php
@@ -0,0 +1,56 @@
+<?php
+if (!defined('EVENT_ESPRESSO_VERSION') )
+	exit('NO direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for Wordpress
+ *
+ * @package		Event Espresso
+ * @author		Seth Shoultes
+ * @copyright	(c)2009-2012 Event Espresso All Rights Reserved.
+ * @license		http://eventespresso.com/support/terms-conditions/  ** see Plugin Licensing **
+ * @link		http://www.eventespresso.com
+ * @version		3.2.P
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Support_Admin_Page_Init
+ *
+ * This is the init for the EE Support Admin Pages.  See EE_Admin_Page_Init for method inline docs. 
+ *
+ *
+ * @package		Support_Admin_Page_Init
+ * @subpackage	includes/core/admin/Support_Admin_Page_Init.core.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class Support_Admin_Page_Init extends EE_Admin_Page_Init {
+
+
+	public function __construct() {
+		//define some help/support page related constants
+		define( 'EE_SUPPORT_ADMIN_URL', admin_url('admin.php?page=ee_support') );
+
+		parent::__construct();
+	}
+
+	protected function _set_init_properties() {
+		$this->label = __('Help & Support', 'event_espresso');
+		$this->menu_label = __('Help/Support', 'event_espresso');
+		$this->menu_slug = 'ee_support';
+	}
+
+	public function get_menu_map() {
+		$map = array(
+			'group' => 'extras',
+			'menu_order' => 30,
+			'show_on_menu' => TRUE,
+			'parent_slug' => 'events'
+			);
+		return $map;
+	}
+
+} //end class Support_Admin_Page_Init
