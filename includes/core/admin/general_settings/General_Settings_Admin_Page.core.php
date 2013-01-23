694a47eafa5bbea9b191c5b3a5347ab6c5d03483
Brent Christensen <brent@eventespresso.com>
2013-01-22 19:09:01 -0800

    finishes off event page settings and organization settings [touch:1933]

--- includes/core/admin/general_settings/General_Settings_Admin_Page.core.php
+++ includes/core/admin/general_settings/General_Settings_Admin_Page.core.php
@@ -181,35 +181,197 @@ class General_Settings_Admin_Page extends EE_Admin_Page {
 	public function admin_footer_scripts() {}
 
 
+	public function load_scripts_styles_your_organization_settings() {	
+		//styles
+		wp_enqueue_style('thickbox');
+		//scripts
+		wp_enqueue_script('media-upload');
+		wp_enqueue_script('thickbox');
+		wp_register_script( 'organization_settings', GEN_SET_ASSETS_URL . 'your_organization_settings.js', array( 'jquery','media-upload','thickbox' ), EVENT_ESPRESSO_VERSION, TRUE );
+		wp_enqueue_script( 'organization_settings' );	
+		$confirm_image_delete = array( 'text' => __('Do you really want to delete this image? Please remember to save your settings to complete the removal.', 'event_espresso')); 
+		wp_localize_script( 'organization_settings', 'confirm_image_delete', $confirm_image_delete );
+		// 
+
+	}
+
+
+	/*************		Espresso Pages 		*************/
+
 
 	protected function _espresso_page_settings() {
 	
 		global $org_options;
-		$this->_template_args['org_options'] = $org_options;
-		// array of pages containing critical EE shortcodes
-		$this->_template_args['ee_pages'] = array(		
-			$org_options['event_page_id'] => array( get_page( $org_options['event_page_id'] ), '[ESPRESSO_EVENTS]' ),			
-			$org_options['return_url'] => array( get_page( $org_options['return_url'] ), '[ESPRESSO_PAYMENTS]' ),			
-			$org_options['notify_url'] => array( get_page( $org_options['notify_url'] ), '[ESPRESSO_TXN_PAGE]' ),			
-			$org_options['cancel_return'] => array( get_page( $org_options['cancel_return'] ), 'ESPRESSO_CANCELLED' )			
-		);
+		$this->_template_args['event_ssl_active'] = isset( $org_options['event_ssl_active'] ) && ! empty( $org_options['event_ssl_active'] ) ? $org_options['event_ssl_active'] : FALSE;
+
+		$this->_template_args['event_page_id'] = isset( $org_options['event_page_id'] ) ? $org_options['event_page_id'] : NULL;
+		$this->_template_args['event_reg_page'] = isset( $org_options['event_page_id'] ) ? get_page( $org_options['event_page_id'] ) : FALSE;
+
+		$this->_template_args['return_url'] = isset( $org_options['return_url'] ) ? $org_options['return_url'] : NULL;
+		$this->_template_args['thank_you_page'] = isset( $org_options['return_url'] ) ? get_page( $org_options['return_url'] ) : FALSE;
+
+		$this->_template_args['notify_url'] = isset( $org_options['notify_url'] ) ? $org_options['notify_url'] : NULL;
+		$this->_template_args['transactions_page'] = isset( $org_options['notify_url'] ) ? get_page( $org_options['notify_url'] ) : FALSE;
+
+		$this->_template_args['cancel_return'] = isset( $org_options['cancel_return'] ) ? $org_options['cancel_return'] : NULL;
+		$this->_template_args['cancel_return_page'] = isset( $org_options['cancel_return'] ) ? get_page( $org_options['cancel_return'] ) : FALSE;
+		
 		$this->_set_add_edit_form_tags( 'update_espresso_page_settings' );
-		$this->_set_publish_post_box_vars();
+		$this->_set_publish_post_box_vars( NULL, FALSE, FALSE, NULL, FALSE );
 		$this->_template_args['admin_page_content'] = espresso_display_template( GEN_SET_TEMPLATE_PATH . 'espresso_page_settings.template.php', $this->_template_args, TRUE );
-		// the details template wrapper
 		$this->display_admin_page_with_sidebar();	
+		
 	}
 
 	protected function _update_espresso_page_settings() {
 		
 		$data = array();
-		$data['event_page_id'] = absint( $_POST['event_page_id'] );
-		$data['return_url'] = absint( $_POST['return_url'] );
-		$data['cancel_return'] = absint( $_POST['cancel_return'] );
-		$data['notify_url'] = absint( $_POST['notify_url'] );
+		$data['event_page_id'] = isset( $this->_req_data['event_page_id'] ) ? absint( $this->_req_data['event_page_id'] ) : NULL;
+		$data['return_url'] = isset( $this->_req_data['return_url'] ) ? absint( $this->_req_data['return_url'] ) : NULL;
+		$data['cancel_return'] = isset( $this->_req_data['cancel_return'] ) ? absint( $this->_req_data['cancel_return'] ) : NULL;
+		$data['notify_url'] = isset( $this->_req_data['notify_url'] ) ? absint( $this->_req_data['notify_url'] ) : NULL;
 		
 		$success = $this->_update_general_settings( $data, __FILE__, __FUNCTION__, __LINE__ );
-		$this->_redirect_after_action( $success, 'Page Settings', 'updated', array() );
+		$this->_redirect_after_action( $success, 'Template Settings', 'updated', array() );
+		
+	}
+
+
+
+	/*************		Templates 		*************/
+
+
+	protected function _template_settings() {
+	
+		global $org_options;
+		$this->_template_args['org_options'] = $org_options;
+
+		$this->_set_add_edit_form_tags( 'update_template_settings' );
+		$this->_set_publish_post_box_vars( NULL, FALSE, FALSE, NULL, FALSE );
+		$this->_template_args['admin_page_content'] = espresso_display_template( GEN_SET_TEMPLATE_PATH . 'template_settings.template.php', $this->_template_args, TRUE );
+		// the details template wrapper
+		$this->display_admin_page_with_sidebar();	
+	}
+
+	protected function _update_template_settings() {
+		
+		$data = array();
+//		$data['event_page_id'] = absint( $this->_req_data['event_page_id'] );
+//		$data['return_url'] = absint( $this->_req_data['return_url'] );
+//		$data['cancel_return'] = absint( $this->_req_data['cancel_return'] );
+//		$data['notify_url'] = absint( $this->_req_data['notify_url'] );
+		
+		$success = $this->_update_general_settings( $data, __FILE__, __FUNCTION__, __LINE__ );
+		$this->_redirect_after_action( $success, 'Template Settings', 'updated', array() );
+		
+	}
+
+
+
+	/*************		Google Maps 		*************/
+
+
+	protected function _google_map_settings() {
+	
+		global $org_options;
+		
+		$this->_template_args['default_logo_url'] = isset( $org_options['default_logo_url'] ) ? $org_options['default_logo_url'] : '';
+
+
+		$this->_set_add_edit_form_tags( 'update_google_map_settings' );
+		$this->_set_publish_post_box_vars( NULL, FALSE, FALSE, NULL, FALSE );
+		$this->_template_args['admin_page_content'] = espresso_display_template( GEN_SET_TEMPLATE_PATH . 'google_map.template.php', $this->_template_args, TRUE );
+		// the details template wrapper
+		$this->display_admin_page_with_sidebar();	
+	}
+
+	protected function _update_google_map_settings() {
+		
+		$data = array();
+//		$data['event_page_id'] = absint( $this->_req_data['event_page_id'] );
+//		$data['return_url'] = absint( $this->_req_data['return_url'] );
+//		$data['cancel_return'] = absint( $this->_req_data['cancel_return'] );
+//		$data['notify_url'] = absint( $this->_req_data['notify_url'] );
+		
+		$success = $this->_update_general_settings( $data, __FILE__, __FUNCTION__, __LINE__ );
+		$this->_redirect_after_action( $success, 'Google Map Settings', 'updated', array() );
+		
+	}
+
+
+
+	/*************		Your Organization 		*************/
+
+
+	protected function _your_organization_settings() {
+	
+		global $org_options;
+		$this->_template_args['default_logo_url'] = isset( $org_options['default_logo_url'] ) ? $this->_display_nice( $org_options['default_logo_url'] ) : FALSE;
+		$this->_template_args['organization'] = isset( $org_options['organization'] ) ? $this->_display_nice( $org_options['organization'] ) : '';
+		$this->_template_args['organization_street1'] = isset( $org_options['organization_street1'] ) ? $this->_display_nice( $org_options['organization_street1'] ) : '';
+		$this->_template_args['organization_street2'] = isset( $org_options['organization_street2'] ) ? $this->_display_nice( $org_options['organization_street2'] ) : '';
+		$this->_template_args['organization_city'] = isset( $org_options['organization_city'] ) ? $this->_display_nice( $org_options['organization_city'] ) : '';
+		$this->_template_args['organization_state'] = isset( $org_options['organization_state'] ) ? $this->_display_nice( $org_options['organization_state'] ) : '';
+		$this->_template_args['organization_zip'] = isset( $org_options['organization_zip'] ) ? $this->_display_nice( $org_options['organization_zip'] ) : '';
+		$this->_template_args['organization_country'] = isset( $org_options['organization_country'] ) ? $this->_display_nice( $org_options['organization_country'] ) : '';
+		$this->_template_args['currency_symbol'] = isset( $org_options['currency_symbol'] ) ? $this->_display_nice( $org_options['currency_symbol'] ) : '';
+		$this->_template_args['contact_email'] = isset( $org_options['contact_email'] ) ? $this->_display_nice( $org_options['contact_email'] ) : '';
+		
+		
+		$this->_set_add_edit_form_tags( 'update_your_organization_settings' );
+		$this->_set_publish_post_box_vars( NULL, FALSE, FALSE, NULL, FALSE );
+		$this->_template_args['admin_page_content'] = espresso_display_template( GEN_SET_TEMPLATE_PATH . 'your_organization_settings.template.php', $this->_template_args, TRUE );
+		// the details template wrapper
+		$this->display_admin_page_with_sidebar();	
+	}
+
+	protected function _update_your_organization_settings() {
+		
+		$data = array();
+		$data['default_logo_url'] = isset( $this->_req_data['default_logo_url'] ) ? esc_url_raw( $this->_req_data['default_logo_url'] ) : NULL;
+		$data['organization'] = isset( $this->_req_data['organization'] ) ? sanitize_text_field( $this->_req_data['organization'] ) : NULL;
+		$data['organization_street1'] = isset( $this->_req_data['organization_street1'] ) ? sanitize_text_field( $this->_req_data['organization_street1'] ) : NULL;
+		$data['organization_street2'] = isset( $this->_req_data['organization_street2'] ) ? sanitize_text_field( $this->_req_data['organization_street2'] ) : NULL;
+		$data['organization_city'] = isset( $this->_req_data['organization_city'] ) ? sanitize_text_field( $this->_req_data['organization_city'] ) : NULL;
+		$data['organization_state'] = isset( $this->_req_data['organization_state'] ) ? sanitize_text_field( $this->_req_data['organization_state'] ) : NULL;
+		$data['organization_zip'] = isset( $this->_req_data['organization_zip'] ) ? sanitize_text_field( $this->_req_data['organization_zip'] ) : NULL;
+		$data['organization_country'] = isset( $this->_req_data['organization_country'] ) ? absint( $this->_req_data['organization_country'] ) : NULL;
+		$data['contact_email'] = isset( $this->_req_data['contact_email'] ) ? sanitize_email( $this->_req_data['contact_email'] ) : NULL;
+		
+//		printr( $this->_req_data, '$this->_req_data  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
+//		printr( $data, '$data  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
+
+		$success = $this->_update_general_settings( $data, __FILE__, __FUNCTION__, __LINE__ );
+		$this->_redirect_after_action( $success, 'Your Organization Settings', 'updated', array( 'action' => 'your_organization_settings' ) );
+		
+	}
+	/*************		Admin Options 		*************/
+
+
+	protected function _admin_option_settings() {
+	
+		global $org_options;
+		$this->_template_args['org_options'] = $org_options;
+		$this->_template_args['values'] = $this->_yes_no_values;
+		$this->_template_args['expire_on_registration_end'] = isset( $org_options['expire_on_registration_end'] ) ? $this->_display_nice( $org_options['expire_on_registration_end'] ) : FALSE;
+
+		$this->_set_add_edit_form_tags( 'update_admin_option_settings' );
+		$this->_set_publish_post_box_vars( NULL, FALSE, FALSE, NULL, FALSE );
+		$this->_template_args['admin_page_content'] = espresso_display_template( GEN_SET_TEMPLATE_PATH . 'admin_option_settings.template.php', $this->_template_args, TRUE );
+		// the details template wrapper
+		$this->display_admin_page_with_sidebar();	
+	}
+
+	protected function _update_admin_option_settings() {
+		
+		$data = array();
+//		$data['event_page_id'] = absint( $this->_req_data['event_page_id'] );
+//		$data['return_url'] = absint( $this->_req_data['return_url'] );
+//		$data['cancel_return'] = absint( $this->_req_data['cancel_return'] );
+//		$data['notify_url'] = absint( $this->_req_data['notify_url'] );
+		
+		$success = $this->_update_general_settings( $data, __FILE__, __FUNCTION__, __LINE__ );
+		$this->_redirect_after_action( $success, 'Admin Options', 'updated', array() );
 		
 	}
 
@@ -221,6 +383,19 @@ class General_Settings_Admin_Page extends EE_Admin_Page {
 
 	/***********/
 
+
+
+
+
+	/**
+	 * correct variable display
+	 *
+	 * @param array $var
+	 * @return string
+	 */
+	private function _display_nice( $var ) {
+		return htmlentities( stripslashes( $var ), ENT_QUOTES, 'UTF-8' );
+	}
 
 
 
@@ -241,6 +416,9 @@ class General_Settings_Admin_Page extends EE_Admin_Page {
 		// make sure everything is in arrays
 		$org_options = is_array( $org_options ) ? $org_options : array( $org_options );
 		$data = is_array( $data ) ? $data : array( $data );
+		foreach ( $data as $key => $value ) {
+			$data[ $key ] = addslashes( html_entity_decode( $value, ENT_QUOTES, 'UTF-8' ));
+		}
 		// overwrite existing org options with new data
 		$data = array_merge( $org_options, $data );
 		// and save it
@@ -285,21 +463,21 @@ class General_Settings_Admin_Page extends EE_Admin_Page {
 //		printr( $ee_page, '$ee_page  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
 //		echo '<h4>$shortcode : ' . $shortcode . '  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h4>';
 		// page status
-		if ( ! isset( $ee_page[0] ) || ! isset( $ee_page[0]->post_status ) || $ee_page[0]->post_status != 'publish') { 
-			$pg_colour = 'red';
-			$pg_status = __('Page Visibility Problem', 'event_espresso');
-		 } else { 
+		if ( isset( $ee_page->post_status ) && $ee_page->post_status == 'publish') { 
 			$pg_colour = 'green';
 			$pg_status = __('Page Status OK', 'event_espresso');
+		 } else { 
+			$pg_colour = 'red';
+			$pg_status = __('Page Visibility Problem', 'event_espresso');
 		}
 		
 		// shortcode status
-		if ( ! isset( $ee_page[0] ) || ! isset( $ee_page[0]->post_content ) || strpos( $ee_page[0]->post_content, $shortcode ) === FALSE ) { 
-			$sc_colour = 'red';
-			$sc_status = __('Shortcode Problem', 'event_espresso');
-		 } else { 
+		if ( isset( $ee_page->post_content ) && strpos( $ee_page->post_content, $shortcode ) !== FALSE ) { 
 			$sc_colour = 'green';
 			$sc_status = __('Shortcode OK', 'event_espresso');
+		 } else { 
+			$sc_colour = 'red';
+			$sc_status = __('Shortcode Problem', 'event_espresso');
 		}
 
 		return '<span style="color:' . $pg_colour . '; margin-right:2em;"><strong>' . $pg_status . '</strong></span><span style="color:' . $sc_colour . '"><strong>' . $sc_status . '</strong></span>';		
