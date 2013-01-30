5ecdcd7b6aca0f36cc0fd7d5183a23be770a9809
Brent Christensen <brent@eventespresso.com>
2013-01-30 12:24:34 -0800

    List table for the Event Attendees [touch:1460]

--- includes/core/admin/attendees/EE_Attendees_List_Table.class.php
+++ includes/core/admin/attendees/EE_Attendees_List_Table.class.php
@@ -13,9 +13,13 @@ class EE_Attendees_List_Table extends EE_Admin_List_Table {
 
 	protected function _setup_data() {
 		$this->_per_page = $this->get_items_per_page( $this->_screen . '_per_page' );
-		$this->_data = $this->_view != 'trash' ? $this->_admin_page->get_attendees($this->_per_page) : $this->_admin_page->get_attendees($this->_per_page,FALSE,TRUE);
-		$this->_all_data_count = $this->_view != 'trash' ? $this->_admin_page->get_attendees($this->_per_page,TRUE) : $this->_admin_page->get_attendees(
-			$this->_per_page,TRUE, TRUE);
+		if ( $this->_view != 'trash' ) {
+			$this->_data = $this->_admin_page->get_event_attendees( $this->_per_page );
+			$this->_all_data_count = $this->_admin_page->get_event_attendees( $this->_per_page, TRUE );		
+		} else {
+			$this->_data = $this->_admin_page->get_event_attendees( $this->_per_page, FALSE, TRUE );
+			$this->_all_data_count = $this->_admin_page->get_event_attendees( $this->_per_page, TRUE, TRUE );
+		}
 	}
 
 
@@ -31,29 +35,24 @@ class EE_Attendees_List_Table extends EE_Admin_List_Table {
 
 		$this->_columns = array(
 				'cb' => '<input type="checkbox" />', //Render a checkbox instead of text
-				'ATT_ID' => __('ID', 'event_espresso'),
-				'ATT_fname' => __('First Name', 'event_espresso'),
-				'ATT_lname' => __('Last Name', 'event_espresso'),
-				'ATT_email' => __('Email Address', 'event_espresso'),
-				'ATT_phone' => __('Phone', 'event_espresso'),
-				'ATT_address' => __('Address', 'event_espresso'),
-				'ATT_city' => __('City', 'event_espresso'),
-				'STA_ID' => __('State', 'event_espresso'),
-				'CNT_ISO' => __('Country', 'event_espresso'),
+				'REG_date' => __('Registration Date', 'event_espresso'),
+				'ATT_name' => __('Attendee', 'event_espresso'),
+				'event_name' => __('Event', 'event_espresso'),
+				'DTT_EVT_start' => __('Event Date & Time', 'event_espresso'),
+				'PRC_name' => __('Ticket Option', 'event_espresso'),
+				'REG_final_price' => __('Price Paid', 'event_espresso')
 			);
 
 		$this->_sortable_columns = array(
-			'id' => array( 'ATT_ID' => FALSE ),
-			'ATT_lname' => array( 'ATT_lname', TRUE ), //true means its already sorted
-			'ATT_fname' => array( 'ATT_fname', FALSE ),
-			'ATT_email' => array( 'ATT_email', FALSE ),
-			'ATT_city' => array( 'ATT_city', FALSE ),
-			'STA_ID' => array( 'STA_ID', FALSE ),
-			'CNT_ISO' => array( 'CNT_ISO', FALSE )
+			'id' => array( 'REG_ID' => FALSE ),
+			'REG_date' => array( 'REG_date', TRUE ), //true means its already sorted
+			'ATT_name' => array( 'ATT_name', FALSE ),
+			'event_name' => array( 'event_name', FALSE )
 		);
 
 		$this->_hidden_columns = array();
 	}
+
 
 
 
@@ -65,11 +64,11 @@ class EE_Attendees_List_Table extends EE_Admin_List_Table {
 
 
 
+
 	protected function _add_view_counts() {
 		$this->_views['in_use']['count'] = $this->_admin_page->get_attendees($this->_per_page,TRUE);
 		$this->_views['trash']['count'] = $this->_admin_page->get_attendees($this->_per_page,TRUE, TRUE);
 	}
-
 
 
 
@@ -84,96 +83,80 @@ class EE_Attendees_List_Table extends EE_Admin_List_Table {
 
 
 	function column_cb($item) {
-		return sprintf( '<input type="checkbox" name="checkbox[%1$s]" />', /* $1%s */ $item->ID() );
+		return sprintf( '<input type="checkbox" name="checkbox[%1$s]" />', $item->REG_ID );
 	}
 
 
 
 
 
-	function column_ATT_ID($item) {
-		return $item->ID();
+	function column_REG_date($item) {
+		$view_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'view_registration', 'reg'=>$item->REG_ID ), REG_ADMIN_URL ), 'view_registration_nonce' );	
+		$REG_date = '<a href="'.$view_lnk_url.'" title="' . __( 'View Registration Details', 'event_espresso' ) . '">' . date( 'D M j, Y  g:i a',	$item->REG_date ) . '</a>';	
+		return $REG_date;	
 	}
 
 
 
 
 
-	function column_ATT_lname($item) {
-
+	function column_ATT_name($item) {
 		// edit attendee link
-		$edit_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'edit_attendee', 'id'=>$item->ID() ), ATT_ADMIN_URL ), 'edit_attendee_nonce' );
-		$name_link = '<a href="'.$edit_lnk_url.'" title="' . __( 'Edit Attendee', 'event_espresso' ) . '">' . html_entity_decode( stripslashes( $item->lname() ), ENT_QUOTES, 'UTF-8' ) . '</a>';
+		$edit_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'edit_attendee', 'id'=>$item->ATT_ID ), ATT_ADMIN_URL ), 'edit_attendee_nonce' );
+		$name_link = '<a href="'.$edit_lnk_url.'" title="' . __( 'Edit Attendee', 'event_espresso' ) . '">' . html_entity_decode( stripslashes( $item->ATT_name ), ENT_QUOTES, 'UTF-8' ) . '</a>';
 		return $name_link;
-
 	}
 
 
 
 
-	function column_ATT_fname($item) {
 
-		//Build row actions
-		$actions = array();
-		// edit attendee link
-		$edit_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'edit_attendee', 'id'=>$item->ID() ), ATT_ADMIN_URL ), 'edit_attendee_nonce' );
-		$actions['edit'] = '<a href="'.$edit_lnk_url.'" title="' . __( 'Edit Attendee', 'event_espresso' ) . '">' . __( 'Edit', 'event_espresso' ) . '</a>';
+	function column_event_name($item){
+		$edit_event_url = add_query_arg( array( 'event_id'=>$item->EVT_ID ), REG_ADMIN_URL );
+		$event_name = stripslashes( html_entity_decode( $item->event_name, ENT_QUOTES, 'UTF-8' ));
+		return '<a href="' . $edit_event_url . '" title="' . __( 'Edit Event #', 'event_espresso' ) . $item->EVT_ID.'">' .  wp_trim_words( $event_name, 30, '...' ) . '</a>';
+	}
 
-		if ( $this->_view == 'in_use' ) {
-			// trash attendee link
-			$trash_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'trash_attendees', 'id'=>$item->ID() ), ATT_ADMIN_URL ), 'trash_attendees_nonce' );
-			$actions['trash'] = '<a href="'.$trash_lnk_url.'" title="' . __( 'Move Attendee to Trash', 'event_espresso' ) . '">' . __( 'Move to Trash', 'event_espresso' ) . '</a>';
+
+
+
+
+	function column_DTT_EVT_start($item){
+		return date( 'D M j, Y  g:i a',	$item->DTT_EVT_start );
+	}
+
+
+
+
+
+	function column_PRC_name($item){
+		return $item->PRC_name;
+	}
+
+
+
+
+
+
+	/**
+	 * 		column_REG_final_price
+	*/
+	function column_REG_final_price($item){
+	
+		global $org_options;
+		$item->REG_final_price = abs( $item->REG_final_price );
+		
+		if ( $item->REG_final_price > 0 ) {
+			return '<span class="reg-overview-full-payment-spn reg-pad-rght">' . $org_options['currency_symbol'] . ' ' . number_format( $item->REG_final_price, 2 ) . '</span>';
 		} else {
-			// restore attendee link
-			$restore_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'restore_attendees', 'id'=>$item->ID() ), ATT_ADMIN_URL ), 'restore_attendees_nonce' );
-			$actions['restore'] = '<a href="'.$restore_lnk_url.'" title="' . __( 'Restore Attendee', 'event_espresso' ) . '">' . __( 'Restore', 'event_espresso' ) . '</a>';
-			// delete attendee link
-			$delete_lnk_url = wp_nonce_url( add_query_arg( array( 'action'=>'delete_attendees', 'id'=>$item->ID() ), ATT_ADMIN_URL ), 'delete_attendees_nonce' );
-			$actions['delete'] = '<a href="'.$delete_lnk_url.'" title="' . __( 'Delete Attendee Permanently', 'event_espresso' ) . '">' . __( 'Delete Permanently', 'event_espresso' ) . '</a>';
+			return '<span class="reg-overview-free-event-spn">' . __( 'zip', 'event_espresso' ) . '</span>';
 		}
-
-		//Return the name contents
-		return sprintf('%1$s %2$s',  html_entity_decode( stripslashes( $item->fname() ), ENT_QUOTES, 'UTF-8' ), $this->row_actions($actions) );		
+		
 	}
 
 
 
 
-
-	function column_ATT_email($item) {
-		return '<a href="mailto:' . $item->email() . '">' . $item->email() . '</a>';
-	}
-
-
-
-
-	function column_ATT_address($item) {
-		return html_entity_decode( stripslashes( $item->address() ), ENT_QUOTES, 'UTF-8' );
-	}
-
-
-
-	function column_ATT_city($item) {
-		return html_entity_decode( stripslashes( $item->city() ), ENT_QUOTES, 'UTF-8' );
-	}
-
-
-
-	function column_STA_ID($item) {
-		return $item->state_ID() != 0 ? $item->state_ID() : '';
-	}
-
-
-
-	function column_CNT_ISO($item) {
-		return $item->country_ISO();
-	}
-
-
-
-	function column_ATT_phone($item) {
-		return $item->phone();
-	}
 
 
 }
