6312c07f4df4eee6dbe5ea90516e6e63aeef20ef
Darren Ethier <darren@roughsmootheng.in>
2013-02-05 16:13:19 -0500

    fix extra slashes in autoloader for core/messages

--- includes/functions/plugins_loaded.php
+++ includes/functions/plugins_loaded.php
@@ -123,9 +123,9 @@ function espresso_classes_core_autoload($className) {
 	//todo:  more subsystems could be added in this array OR even better this array can be defined somewhere else!
 	$dir_ref = array(
 		'root' => array('core', 'class'),
-		'/messages/' => 'core',
-		'/messages/message_type/' => 'class',
-		'/messages/messenger/' => 'class'
+		'messages/' => 'core',
+		'messages/message_type/' => 'class',
+		'messages/messenger/' => 'class'
 		);
 
 	//assemble a list of filenames
