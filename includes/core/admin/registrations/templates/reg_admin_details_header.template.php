aeba9f58791007126366c559ede3d31a7170d8b2
Brent Christensen <brent@eventespresso.com>
2013-02-18 13:36:51 -0800

    adds buttons for updating reg status [touch:2206]

--- includes/core/admin/registrations/templates/reg_admin_details_header.template.php
+++ includes/core/admin/registrations/templates/reg_admin_details_header.template.php
@@ -1,7 +1,13 @@
 	
-		<h3 style="padding:0;margin:-.5em 0 1em;"><?php echo __( 'Registration # ', 'event_espresso' ) . $reg_nmbr['value'];?></h3>
-		<h2 style="padding:0;margin:0 0 1em;"><?php echo $reg_datetime['value'];?></h2>
-		<h2 style="padding:0;margin:0 0 1em;"><?php echo __( 'Registration Status : ', 'event_espresso' );?><span class="<?php echo $reg_status['class'];?>"><?php echo $reg_status['value'];?></span></h2>
+		<h3 id="reg-admin-reg-details-reg-nmbr-hdr"><?php echo __( 'Registration # ', 'event_espresso' ) . $reg_nmbr['value'];?></h3>
+		<h2 id="reg-admin-reg-details-reg-date-hdr"><?php echo $reg_datetime['value'];?></h2>
+		<h2 id="reg-admin-reg-details-reg-status-hdr">
+			<?php echo __( 'Registration Status : ', 'event_espresso' );?>
+			<span class="<?php echo $reg_status['class'];?>"><?php echo $reg_status['value'];?></span>
+			<span id="reg-admin-approve-decline-reg-status-spn">
+				<?php echo $approve_decline_reg_status_buttons;?>
+			</span>
+		</h2>
 		<a id="scroll-to-other-attendees" class="scroll-to" href="#other-attendees"><?php echo __( 'scroll to Other Attendees Registered in the Same Transaction', 'event_espresso' );?></a>
 		
 
