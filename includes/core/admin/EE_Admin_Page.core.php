943c7f41bb2e2bc909b3fd90eee505f2efd211f2
Brent Christensen <brent@eventespresso.com>
2013-02-25 16:19:46 -0800

    retreive nonces dyanmically based on incoming action request, adds and uses method for automagically generating nonces [touch:2085]

--- includes/core/admin/EE_Admin_Page.core.php
+++ includes/core/admin/EE_Admin_Page.core.php
@@ -642,15 +642,19 @@ abstract class EE_Admin_Page extends EE_BASE {
 		$this->_verify_routes();
 
 		if ( $this->_req_action != 'default' ) {
-			$nonce = isset($this->_req_data['_wpnonce']) ? $this->_req_data['_wpnonce'] : '';
-
-
-			if ( !wp_verify_nonce( $nonce, $this->_req_nonce ) ) {
+			// set nonce from post data
+			$nonce = isset($this->_req_data[ $this->_req_nonce  ]) ? sanitize_text_field( $this->_req_data[ $this->_req_nonce  ] ) : '';
+			// verify nonce against expected value
+			if ( ! wp_verify_nonce( $nonce, $this->_req_nonce ) ) {
+				// these are not the droids you are looking for !!!
 				$msg = sprintf(__('%sNonce Fail.%s' , 'event_espresso'), '<a href="http://www.youtube.com/watch?v=56_S0WeTkzs">', '</a>' );
-				if ( !defined('DOING_AJAX') )
+				if ( WP_DEBUG ) {
+					$msg .= "\n" . sprintf( __('In order to dynamically generate nonces for your actions, use the %s->_add_query_arg method. May the Nonce be with you!', 'event_espresso' ), __CLASS__  );
+				}
+				if ( ! defined( 'DOING_AJAX' )) {
 					wp_die( $msg );
-				else {
-					echo json_encode( array('error' => $msg ) );
+				} else {
+					echo json_encode( array('error' => $msg ));
 					exit();
 				}
 			}
@@ -685,6 +689,37 @@ abstract class EE_Admin_Page extends EE_BASE {
 
 
 	/**
+	 * _add_query_arg
+	 * adds nonce to array of arguments then calls WP add_query_arg function
+	 *
+	 * 	@access public
+	 *	@param array $args
+	 *	@param string $url
+	 * 	@return void
+	 */
+	public static function add_query_args_and_nonce( $args = array(), $url = FALSE ) {
+		if ( ! $url ) {
+			$user_msg = __('An error occured. A URL is a required parameter for the _add_query_arg method.', 'event_espresso' );
+			$dev_msg = $user_msg . "\n" . sprintf( 
+					__('In order to dynamically generate nonces for your actions, you need to supply a valid URL as a second parameter for the %s->_add_query_arg method.', 'event_espresso' ),
+					__CLASS__ 
+				);
+			EE_Error::add_error( $user_msg . '||' . $dev_msg, __FILE__, __FUNCTION__, __LINE__ );				
+		}
+		// check that an action exists
+		if ( isset( $args['action'] ) && ! empty( $args['action'] )) {
+			$args = array_merge( $args, array( $args['action'] . '_nonce' => wp_create_nonce( $args['action'] . '_nonce' )));
+		} else {
+			$args = array_merge( $args, array( 'action' => 'default', 'default_nonce' => wp_create_nonce( 'default_nonce' )));
+		}
+		return add_query_arg( $args, $url );
+		
+	}
+
+
+
+
+	/**
 	 * _set_nav_tabs
 	 * This sets up the nav tabs from the page_routes array.  This method can be overwritten by child classes if you wish to add additional tabs or modify accordingly.
 	 *
@@ -703,7 +738,7 @@ abstract class EE_Admin_Page extends EE_BASE {
 
 			$css_class = isset( $config['css_class'] ) ? $config['css_class'] . ' ' : '';
 			$this->_nav_tabs[$slug] = array(
-				'url' => isset($config['nav']['url']) ? $config['nav']['url'] : wp_nonce_url( add_query_arg( array( 'action'=>$slug ), $this->_admin_base_url), $slug . '_nonce'),
+				'url' => isset($config['nav']['url']) ? $config['nav']['url'] : self::add_query_args_and_nonce( array( 'action'=>$slug ), $this->_admin_base_url ),
 				'link_text' => isset( $config['nav']['label'] ) ? $config['nav']['label'] : ucwords(str_replace('_', ' ', $slug ) ),
 				'css_class' => $this->_req_action == $slug ? $css_class . 'nav-tab-active' : $css_class,
 				'order' => isset( $config['nav']['order'] ) ? $config['nav']['order'] : $i
@@ -1767,10 +1802,11 @@ abstract class EE_Admin_Page extends EE_BASE {
 		
 		$ajax_sorting_callback = $this->_list_table_object->get_ajax_sorting_callback();	
 		if( ! empty( $ajax_sorting_callback )) {
-			$reorder_action = 'espresso_' . $ajax_sorting_callback . '_nonce';
-			$sortable_list_table_form_fields = wp_nonce_field( $reorder_action, 'ajax_table_sort_nonce', FALSE, FALSE );
+			$sortable_list_table_form_fields = wp_nonce_field( $ajax_sorting_callback . '_nonce', $ajax_sorting_callback . '_nonce', FALSE, FALSE );
+//			$reorder_action = 'espresso_' . $ajax_sorting_callback . '_nonce';
+//			$sortable_list_table_form_fields = wp_nonce_field( $reorder_action, 'ajax_table_sort_nonce', FALSE, FALSE );
 			$sortable_list_table_form_fields .= '<input type="hidden" id="ajax_table_sort_page" name="ajax_table_sort_page" value="' . $this->page_slug .'" />';
-			$sortable_list_table_form_fields .= '<input type="hidden" id="ajax_table_sort_action" name="ajax_table_sort_action" value="espresso_' . $ajax_sorting_callback .'" />';
+			$sortable_list_table_form_fields .= '<input type="hidden" id="ajax_table_sort_action" name="ajax_table_sort_action" value="' . $ajax_sorting_callback . '" />';
 		} else {
 			$sortable_list_table_form_fields = '';
 		}
@@ -1975,7 +2011,8 @@ abstract class EE_Admin_Page extends EE_BASE {
 		// open form
 		$this->_template_args['before_admin_page_content'] = '<form name="form" method="post" action="' . $this->_admin_base_url . '" id="' . $route . '_event_form" >';
 		// add nonce
-		$nonce = wp_nonce_field( $route . '_nonce', '_wpnonce', FALSE, FALSE );
+		$nonce = wp_nonce_field( $route . '_nonce', $route . '_nonce', FALSE, FALSE );
+//		$nonce = wp_nonce_field( $route . '_nonce', '_wpnonce', FALSE, FALSE );
 		$this->_template_args['before_admin_page_content'] .= "\n\t" . $nonce;
 		// add REQUIRED form action
 		$hidden_fields = array( 
@@ -2046,20 +2083,12 @@ abstract class EE_Admin_Page extends EE_BASE {
 
 		//combine $query_args and $notices
 		$query_args = array_merge( $query_args, $notices );
-		// generate redirect url
-
-		// if redirecting to anything other than the main page, add a nonce
-		if ( isset( $query_args['action'] )) {
-			// manually generate wp_nonce and merge that with the query vars becuz the wp_nonce_url function wrecks havoc on some vars
-			$query_args['_wpnonce'] = wp_create_nonce( $query_args['action'] . '_nonce' );
-		} 
 
 		//we're adding some hooks and filters in here for processing any things just before redirects (example: an admin page has done an insert or update and we want to run something after that).
 		$classname = get_class($this);
 		do_action( 'action_hook_espresso_redirect_' . $classname . $this->_req_action, $query_args );
 
-
-		$redirect_url = apply_filters( 'filter_hook_espresso_redirect_' . $classname . $this->_req_action, add_query_arg( $query_args, $redirect_url ), $query_args ); 
+		$redirect_url = apply_filters( 'filter_hook_espresso_redirect_' . $classname . $this->_req_action, self::add_query_args_and_nonce( $query_args, $redirect_url ), $query_args ); 
 
 		// check if we're doing ajax.  If we are then lets just return the results and js can handle how it wants.
 		if ( defined('DOING_AJAX' ) ) {
@@ -2114,7 +2143,7 @@ abstract class EE_Admin_Page extends EE_BASE {
 		if ( !empty($extra_request) )
 			$query_args = array_merge( $extra_request, $query_args );
 
-		$url = wp_nonce_url( add_query_arg( $query_args, $_base_url), $action . '_nonce');
+		$url = self::add_query_args_and_nonce( $query_args, $_base_url );
 
 		$button = '<a href="' . $url . '" class="' . $class . '">' . $this->_labels['buttons'][$type] . '</a>';
 
