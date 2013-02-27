f26e8566482f7145699488fba95bb1f6d0d91a32
Darren Ethier <darren@roughsmootheng.in>
2013-02-26 20:54:12 -0500

    add `EE_Messages_Email_Registration_Validator` and `EE_Messages_Email_Payment_Validator` child classes. [touch: 2169]

---
+++ includes/core/messages/validators/email/EE_Messages_Email_Registration_Validator.class.php
@@ -0,0 +1,45 @@
+<?php
+
+if (!defined('EVENT_ESPRESSO_VERSION') )
+	exit('NO direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package		Event Espresso
+ * @ author			Seth Shoultes
+ * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license		http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link			http://www.eventespresso.com
+ * @ version		3.2
+ *
+ * ------------------------------------------------------------------------
+ *
+ * EE_Messages_Email_Registration_Validator class
+ *
+ * Holds any special validation rules for template fields with Email messenger and Registration message type.
+ *
+ * @package		Event Espresso
+ * @subpackage	includes/core/messages/validators/email/EE_Messages_Email_Registration_Validator.class.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Messages_Email_Registration_Validator extends EE_Messages_Validator {
+
+
+	public function __construct( $fields ) {
+		$this->_m_name = 'email';
+		$this->_mt_name = 'registration';
+
+		parent::__construct();
+	}
+
+	/**
+	 * at this point no custom validation needed for this messenger/message_type combo.
+	 */
+	protected function _modify_validator() {}
+
+} //end class EE_Messages_Email_Registration_Validator
