7f873e429a54599d042bdd1e2b235bc2c81656ed
Darren Ethier <darren@roughsmootheng.in>
2013-03-14 08:57:09 -0400

    fix incorrect setup of email headers [touch: 2311]

--- includes/core/messages/messenger/EE_Email_messenger.class.php
+++ includes/core/messages/messenger/EE_Email_messenger.class.php
@@ -294,10 +294,10 @@ class EE_Email_messenger extends EE_messenger  {
 	 */
 	protected function _headers() {
 		$headers = array(
-			'MIME-Version' => '1.0',
-			'From' => html_entity_decode( $this->_from ),
-			'Reply-To' => html_entity_decode( $this->_from ),
-			'Content-Type' => 'text/html; charset=utf-8'
+			'MIME-Version: 1.0',
+			'From:' . html_entity_decode( $this->_from ),
+			'Reply-To:' . html_entity_decode( $this->_from ),
+			'Content-Type:text/html; charset=utf-8'
 			);
 		return $headers;
 	}
