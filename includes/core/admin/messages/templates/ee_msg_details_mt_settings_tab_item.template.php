961d77b61271d78a6c9d86086e66927ad64a9fb3
Darren Ethier <darren@roughsmootheng.in>
2013-03-21 16:35:11 -0400

    activation and deactivation of individual message types (via draggable/droppable and ajax action) completed. [touch: 2131]

--- includes/core/admin/messages/templates/ee_msg_details_mt_settings_tab_item.template.php
+++ includes/core/admin/messages/templates/ee_msg_details_mt_settings_tab_item.template.php
@@ -1,4 +1,5 @@
-<li class="ui-widget-content ui-corner-tr mt-tab <?php echo $class; ?>">
+<li id="<?php echo $slug_class; ?>" class="ui-widget-content ui-corner-tr mt-tab <?php echo $class; ?>">
 	<h4 class="ui-widget-header"><?php echo $label; ?></h4>
 	<?php echo $content; ?>
+	<span class="mt_nonce hidden"><?php echo $mt_nonce; ?></span>
 </li>
