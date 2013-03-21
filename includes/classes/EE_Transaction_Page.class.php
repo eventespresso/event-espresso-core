2d21ffa69d7d458d22fed719a8a1d44fa11fefd1
Michael Nelson <michael@eventespresso.com>
2013-03-21 15:01:13 -0700

    [touch:2375]{t:30}changed querystring parameter reg_url_link to e_reg_url_link in order to avoid the &reg getting converted into the registration symbol by browsers

--- includes/classes/EE_Transaction_Page.class.php
+++ includes/classes/EE_Transaction_Page.class.php
@@ -69,7 +69,7 @@ class EE_Transaction_Page{
 	/**
 	 * handles the IPN. Gets the gateways model to call the currently-used gateway,
 	 * and handle teh IPN in its desired manner. 
-	 * Expects that the IPN sent a POST or GET requests with parameters 'reg_url_link'
+	 * Expects that the IPN sent a POST or GET requests with parameters 'e_reg_url_link'
 	 * (which is a registration's REG_url_link value) and 'ee_gateway' (which is the 
 	 * gateway_name of the gateway sending teh IPN. Eg, 'Paypal_Standard'). 
 	 * Maybe we'll want to die afterwards? For now I'll just let the gateway handle that
@@ -77,10 +77,10 @@ class EE_Transaction_Page{
 	function handle_ipn(){
 		//in the REQUEEST, we expect  reg_url_link, ee_gateway. If not, 
 		//it must not be an Instant Payment NOtification...
-		if((isset($_GET['reg_url_link']) && isset($_GET['ee_gateway']) )
+		if((isset($_GET['e_reg_url_link']) && isset($_GET['ee_gateway']) )
 				||
-				(isset($_POST['reg_url_link']) && isset($_POST['ee_gateway']))){		
-			$transaction = $this->_TXN->get_transaction_from_reg_url_link($_GET['reg_url_link']);
+				(isset($_POST['e_reg_url_link']) && isset($_POST['ee_gateway']))){		
+			$transaction = $this->_TXN->get_transaction_from_reg_url_link($_GET['e_reg_url_link']);
 			$indicated_gateway=array_key_exists('ee_gateway',$_GET)?$_GET['ee_gateway']:$_POST['ee_gateway'];
 			$this->_GATEWAYS->set_selected_gateway($indicated_gateway);
 			$this->_GATEWAYS->handle_ipn_for_transaction($transaction);
