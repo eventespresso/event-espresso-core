fd9b8b3e33f774f5a8e6c37704b7a6ea53e1c9e7
Darren Ethier <darren@roughsmootheng.in>
2013-03-31 13:00:39 -0400

    bump version for cache busting [touch: 2433]

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 			4.0.1
+  Version: 			4.0.1.beta.1
 
   Author: 				Seth Shoultes
   Author URI: 		http://www.eventespresso.com
@@ -30,7 +30,7 @@
  */
 //Returns the plugin version
 function espresso_version() {
-	return '4.0.1';
+	return '4.0.1.beta.1';
 }
 
 
