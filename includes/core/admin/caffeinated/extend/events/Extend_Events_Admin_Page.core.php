44c16621af9405c9095c89d1edd8b6d32dfdcb60
Darren Ethier <darren@roughsmootheng.in>
2013-04-11 10:51:35 -0400

    create new folders in caffeinated folder: - new = used for all new EE_Admin_Pages - extend = used for classes that will extend existing EE_Admin_Pages - hooks = used for files/classes that will just use the WP hook system to hook into existing CORE EE_Admin pages and any NEW EE_Admin pages.
    Moved EE_Venue admin pages into new folder.
    Created an experimental `Extend_Events_Admin_Page.core.php` file and class for testing new extends functionality
    [touch: 2498]

---
+++ includes/core/admin/caffeinated/extend/events/Extend_Events_Admin_Page.core.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('EVENT_ESPRESSO_VERSION') )
+	exit('NO direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for Wordpress
+ *
+ * @package		Event Espresso
+ * @author		Seth Shoultes
+ * @copyright	(c)2009-2012 Event Espresso All Rights Reserved.
+ * @license		http://eventespresso.com/support/terms-conditions/  ** see Plugin Licensing **
+ * @link		http://www.eventespresso.com
+ * @version		3.2.P
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Events_Admin_Page
+ *
+ * This contains the logic for setting up the Events related pages.  Any methods without phpdoc comments have inline docs with parent class. 
+ *
+ * NOTE:  TODO: This is a straight conversion from the legacy 3.1 events related pages.  It is NOT optimized and will need modification to fully use the new system (and also will need adjusted when Events model is setup)
+ *
+ * @package		Events_Admin_Page
+ * @subpackage	includes/core/admin/Events_Admin_Page.core.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class Extend_Events_Admin_Page extends Events_Admin_Page {
+
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+} //end class Events_Admin_Page
