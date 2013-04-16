524fd8d21f8e82fbd1ea1a434d99b9ac381b99c3
Darren Ethier <darren@roughsmootheng.in>
2013-04-15 20:33:24 -0400

    changing version for dev channel

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 			4.0.1.beta.3
+  Version: 			4.1.dev
 
   Author: 				Seth Shoultes
   Author URI: 		http://www.eventespresso.com
@@ -30,7 +30,7 @@
  */
 //Returns the plugin version
 function espresso_version() {
-	return '4.0.1.beta.3';
+	return '4.1.dev';
 }
 
 
