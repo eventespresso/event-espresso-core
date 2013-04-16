91336abec3638b98e79d14e31360a0c581d0fa9c
Darren Ethier <darren@roughsmootheng.in>
2013-04-15 20:33:58 -0400

    make sure `EE_VENUES_ASSETS_URL` points to the right directory [touch: 2498]

--- caffeinated/admin/new/venues/Venues_Admin_Page_Init.core.php
+++ caffeinated/admin/new/venues/Venues_Admin_Page_Init.core.php
@@ -35,7 +35,7 @@ class Venues_Admin_Page_Init extends EE_Admin_Page_Init {
 		//define some event categories related constants
 		define( 'EE_VENUES_PG_SLUG', 'espresso_venues' );	
 		define( 'EE_VENUES_ADMIN_URL', admin_url('admin.php?page=' . EE_VENUES_PG_SLUG ));
-		define( 'EE_VENUES_ASSETS_URL', EE_CORE_CAF_ADMIN_URL . 'venues/assets/');
+		define( 'EE_VENUES_ASSETS_URL', EE_CORE_CAF_ADMIN_URL . 'new/venues/assets/');
 
 		parent::__construct();
 		$this->_folder_path = EE_CORE_CAF_ADMIN . 'new/' . $this->_folder_name . DS;
