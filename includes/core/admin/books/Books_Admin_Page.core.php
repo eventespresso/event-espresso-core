bc9a9ffcf098840e287584f7bbbfe0d0e774810a
Darren Ethier <darren@roughsmootheng.in>
2013-04-20 15:23:19 -0400

    just adding in the category overview table for testing.  I don't want to build out an overview for the books cpt because this is just an experiment.  We'll still have to setup our overview tables manually but I think that will actually be better in the long run because then we just do a single query (using our model) for the table rather than multiple and can filter events on more criteria. [touch: 2541]

--- includes/core/admin/books/Books_Admin_Page.core.php
+++ includes/core/admin/books/Books_Admin_Page.core.php
@@ -42,6 +42,13 @@ class Books_Admin_Page extends EE_Admin_Page_CPT {
 	protected function _define_page_props() {
 		$this->_admin_base_url = BOOKS_ADMIN_URL;
 		$this->_admin_page_title = __('EE Books', 'event_espresso');
+		$this->_labels = array(
+			'buttons' => array(
+				'add' => __('Add New Category', 'event_espresso'),
+				'edit' => __('Edit Category', 'event_espresso'),
+				'delete' => __('Delete Category', 'event_espresso')
+			)
+		);
 		$this->_is_cpt = TRUE;
 	}
 
@@ -50,19 +57,9 @@ class Books_Admin_Page extends EE_Admin_Page_CPT {
 
 
 	protected function _set_page_routes() {
-		$this->_page_routes = array(
-		
-			//'default' => '_overview_list_table',
-			
-			'edit_book' => array(
-				'func' => '_book_details',
-				'args' => array('edit')
-				),
-
-			'default' => array(
-				'func' => '_book_details',
-				'args' => array('add')
-				)
+		$this->_page_routes = array(		
+			'default' => '_overview_list_table',
+			'add_category' => '_create_new_cpt_item'
 		);
 
 	}
@@ -73,30 +70,14 @@ class Books_Admin_Page extends EE_Admin_Page_CPT {
 	protected function _set_page_config() {
 		$this->_page_config = array(
 		
-			/*'default' => array(
+			'default' => array(
 				'nav' => array(
 					'label' => __('Overview', 'event_espresso'),
 					'order' => 10
 					),
 				'list_table' => 'Event_Categories_Admin_List_Table',
 				'metaboxes' => array('_espresso_news_post_box', '_espresso_links_post_box', '_espresso_sponsors_post_box'),
-				),*/
-
-			'default' => array(
-				'nav' => array(
-					'label' => __('Add Book', 'event_espresso'),
-					'order' => 5,
-					'persistent' => false),
-				),
-
-			'edit_book' => array(
-				'nav' => array(
-					'label' => __('Edit Book', 'event_espresso'),
-					'order' => 5,
-					'persistent' => FALSE,
-					'url' => isset($this->_req_data['EVT_CAT_ID']) ? add_query_arg(array('EVT_CAT_ID' => $this->_req_data['EVT_CAT_ID'] ), $this->_current_page_view_url )  : $this->_admin_base_url
-					)
-				),
+				)
 
 		);
 		
@@ -112,6 +93,9 @@ class Books_Admin_Page extends EE_Admin_Page_CPT {
 
 
 
+	protected function _add_screen_options_default() {
+		$this->_per_page_screen_option();
+	}
 
 
 
@@ -159,10 +143,45 @@ class Books_Admin_Page extends EE_Admin_Page_CPT {
 	}
 
 
+
+	protected function _overview_list_table() {
+		do_action( 'action_hook_espresso_log', __FILE__, __FUNCTION__, '' );
+		$this->_admin_page_title .= $this->_get_action_link_or_button('add_category', 'add', array(), 'button add-new-h2');
+		$this->display_admin_list_table_page_with_sidebar();
+	}
+
+
 	public function insert_update_cpt_item( $post_id, $post ) {}
 	public function trash_cpt_item( $post_id ) {}
 	public function restore_cpt_item( $post_id ) {}
 	public function delete_cpt_item( $post_id ) {}
+
+
+
+	/***********/
+	/* QUERIES */
+
+
+	public function get_categories( $per_page = 10, $current_page = 1, $count = FALSE ) {
+		global $wpdb;
+
+		$offset = ($current_page-1)*$per_page; 
+		$limit = apply_filters('filter_hook_espresso_category_list_limit', $count ? '' : ' LIMIT ' . $offset . ',' . $per_page, $offset, $per_page);
+		$orderby = apply_filters( 'filter_hook_espresso_category_list_orderby', isset($this->_req_data['orderby']) ? " ORDER BY " . $this->_req_data['orderby'] : " ORDER BY c.category_name", $this->_req_data );
+		$order = apply_filters( 'filter_hook_espresso_category_list_order', isset($this->_req_data['order']) ? " " . $this->_req_data['order'] : " DESC", $this->_req_data);
+
+		$sql = $count ? "SELECT COUNT(c.id) FROM " . EVENTS_CATEGORY_TABLE . " c" : "SELECT * FROM " . EVENTS_CATEGORY_TABLE . " c";
+
+		$sql = apply_filters('filter_hook_espresso_category_list_sql', $sql);
+		$sql .= !$count ? " GROUP BY c.id " . $orderby . $order . $limit : '';
+
+		$categories = $count ? $wpdb->get_var( $sql ) : $wpdb->get_results( $sql );
+		return $categories;
+	}
+
+
+	/* end queries */
+	/**************/
 
 
 }
