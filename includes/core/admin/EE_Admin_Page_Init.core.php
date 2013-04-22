b11a7890509802acf51495cf5497b00bd48fa827
Darren Ethier <darren@roughsmootheng.in>
2013-04-22 13:07:04 -0400

    another fix to _Hooks and _Hooks_Extend file/class loading. [touch: 2528]
    I had $this->hook_file set too early and it resulted in the decaf hook file ALWAYS being loaded even if there was a caf _hooks_extend file.

--- includes/core/admin/EE_Admin_Page_Init.core.php
+++ includes/core/admin/EE_Admin_Page_Init.core.php
@@ -265,8 +265,8 @@ abstract class EE_Admin_Page_Init extends EE_BASE {
 
 		//if this is an extended check (i.e. caf is active) then we will scan the caffeinated/extend directory first and any hook files that are found will be have their reference added to the $_files_hook array property.  Then, we make sure that when we loop through the core decaf directories to find hook files that we skip over any hooks files that have already been set by caf.
 		if ( $extend ) {
-			$hook_files_glob_path = apply_filters('filter_hook_espresso_admin_hook_files_glob_path', EE_CORE_CAF_ADMIN_EXTEND . $this->_folder_path . '*' . $this->_file_name . '_Hooks_Extend.class.php' );
-			$this->_register_hook_files( $hook_files_glob_path );
+			$hook_files_glob_path = apply_filters('filter_hook_espresso_admin_hook_files_glob_path', EE_CORE_CAF_ADMIN_EXTEND . $this->_folder_name . DS . '*' . $this->_file_name . '_Hooks_Extend.class.php' );
+			$this->_register_hook_files( $hook_files_glob_path, $extend );
 		}
 
 		//loop through decaf folders
@@ -278,17 +278,17 @@ abstract class EE_Admin_Page_Init extends EE_BASE {
 
 
 	protected function _register_hook_files( $hook_files_glob_path, $extend = FALSE ) {
-
 		if ( $hook_files = glob( $hook_files_glob_path ) ) {
 			foreach ( $hook_files as $file ) {
 				//lets get the linked admin.
-				$this->hook_file = str_replace($this->_folder_path, '', $file );
+				$hook_file = $extend ? str_replace( EE_CORE_CAF_ADMIN_EXTEND . $this->_folder_name . DS, '', $file ) : str_replace($this->_folder_path, '', $file );
 				$replace = $extend ? '_' . $this->_file_name . '_Hooks_Extend.class.php' : '_' . $this->_file_name . '_Hooks.class.php';
-				$rel_admin = str_replace( $replace, '', $this->hook_file);
+				$rel_admin = str_replace( $replace, '', $hook_file);
 				$rel_admin = strtolower($rel_admin);
 				//make sure we haven't already got a hook setup for this page path
 				if ( in_array( $rel_admin, $this->_files_hooked ) )
 					continue;
+				$this->hook_file = $hook_file;
 				$rel_admin_hook = 'filter_hook_espresso_do_other_page_hooks_' . $rel_admin;
 				$filter = add_filter( $rel_admin_hook, array($this, 'load_admin_hook') );
 				$this->_files_hooked[] = $rel_admin;
@@ -300,6 +300,7 @@ abstract class EE_Admin_Page_Init extends EE_BASE {
 
 
 	public function load_admin_hook($registered_pages) {
+		$this->hook_file;
 		$hook_file = (array) $this->hook_file;
 		return array_merge($hook_file, $registered_pages);
 	}
