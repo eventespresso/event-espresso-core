ba719917e40ae37207364c1d9a3aed7a75aaaddf
Brent Christensen <brent@eventespresso.com>
2013-05-01 08:38:03 -0700

    add function for loading caffeinated activation scripts [touch:2581]

--- caffeinated/init.php
+++ caffeinated/init.php
@@ -1,22 +1,30 @@
 <?php if (!defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
 
-	function espresso_system_check() {
-		global $caffeinated;
-		$caffeinated = defined('EE_CAFFEINATED') ? EE_CAFFEINATED : TRUE;
-		return $caffeinated;
-	}
+function espresso_system_check() {
+	global $caffeinated;
+	$caffeinated = defined('EE_CAFFEINATED') ? EE_CAFFEINATED : TRUE;
+	return $caffeinated;
+}
 
-	//Custom includes support
-	if (file_exists( EVENT_ESPRESSO_UPLOAD_DIR . "custom_includes.php")){
-		require_once(EVENT_ESPRESSO_UPLOAD_DIR . "custom_includes.php");
-	}
+//Custom includes support
+if (file_exists( EVENT_ESPRESSO_UPLOAD_DIR . "custom_includes.php")){
+	require_once(EVENT_ESPRESSO_UPLOAD_DIR . "custom_includes.php");
+}
 
-	//Custom functions support
-	if (file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "custom_functions.php")){
-		require_once(EVENT_ESPRESSO_UPLOAD_DIR . "custom_functions.php");
-	}
+//Custom functions support
+if (file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "custom_functions.php")){
+	require_once(EVENT_ESPRESSO_UPLOAD_DIR . "custom_functions.php");
+}
 
-	//Custom shortcode support
-	if (file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "custom_shortcodes.php")){
-		require_once(EVENT_ESPRESSO_UPLOAD_DIR . "custom_shortcodes.php");
+//Custom shortcode support
+if (file_exists(EVENT_ESPRESSO_UPLOAD_DIR . "custom_shortcodes.php")){
+	require_once(EVENT_ESPRESSO_UPLOAD_DIR . "custom_shortcodes.php");
+}
+
+function espresso_caffeinated_activation() {
+	if (  ! EE_DECAF && file_exists( EVENT_ESPRESSO_PLUGINFULLPATH . 'caffeinated/admin/activation.php' )) {
+		require_once( EVENT_ESPRESSO_PLUGINFULLPATH . 'caffeinated/admin/activation.php' );
 	}
+}
+
+
