7ab329a79c5fbbc02b642b482be2f33a73859ab7
Brent Christensen <brent@eventespresso.com>
2013-05-09 16:51:42 -0700

    fixes to accomoadate changes to EEM_Payment model  [touch:2671]

--- includes/core/admin/transactions/Transactions_Admin_Page.core.php
+++ includes/core/admin/transactions/Transactions_Admin_Page.core.php
@@ -965,10 +965,17 @@ class Transactions_Admin_Page extends EE_Admin_Page {
 		
 		if ( isset( $this->_req_data['ID'] )) {
 			if ( $PAY_ID = absint( $this->_req_data['ID'] )) {
-				//require_once(EVENT_ESPRESSO_INCLUDES_DIR . 'models/EEM_Payment.model.php');
-				if ( $return_data = EEM_Payment::instance()->delete_payment( $PAY_ID )) {
-					$return_data['PAY_ID'] = $PAY_ID;
-				}	
+				if ( $payment = EEM_Payment::instance()->get_payment_by_ID( absint( $PAY_ID ))) {
+					if ( $transaction = EEM_Payment::instance()->delete_payment( $PAY_ID )) {
+						$return_data = array( 
+							'amount' => $payment->amount(), 
+							'total_paid' => $transaction->paid(), 
+							'txn_status' => $transaction->status_ID(),
+							'pay_status' => $payment->STS_ID(),
+							'PAY_ID' => $PAY_ID
+						); 						
+					}						
+				}
 			}
 		} else {
 			$msg = __( 'An error occured. The payment form data could not be loaded.', 'event_espresso' );
