541303ce4561ece064150ccf61c4e536e68d4196
Brent Christensen <brent@eventespresso.com>
2013-05-22 08:57:51 -0700

    fixes array brackets [touch:2747]

--- libraries/pue/pue-client.php
+++ libraries/pue/pue-client.php
@@ -247,7 +247,7 @@ class PluginUpdateEngineChecker {
 	 */
 	private function _set_slug_and_slug_props( $slug, $options ) {
 
-		$this->pluginFile = $options('plugin_basename');
+		$this->pluginFile = $options['plugin_basename'];
 		$this->pluginName = $options['plugin_name'];
 		$this->lang_domain = isset( $options['lang_domain'] ) && !empty($options['lang_domain']) ? $options['lang_domain'] : NULL;
 
