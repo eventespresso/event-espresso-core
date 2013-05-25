b0f6830607162ba397d58031c8cb34e1a69caf04
Brent Christensen <brent@eventespresso.com>
2013-05-24 17:10:47 -0700

    update table name definition [touch:2801]

--- includes/classes/EE_Registration.class.php
+++ includes/classes/EE_Registration.class.php
@@ -686,7 +686,7 @@ class EE_Registration extends EE_Base_Class {
 		}
 
 		global $wpdb;
-		$SQL = "SELECT DTT_ID FROM " . ESP_DATETIME . " WHERE EVT_ID = %s";
+		$SQL = "SELECT DTT_ID FROM " . ESP_DATETIME_TABLE . " WHERE EVT_ID = %s";
 		return $wpdb->get_var( $wpdb->prepare( $SQL, $this->_EVT_ID ) );
 	}
 
