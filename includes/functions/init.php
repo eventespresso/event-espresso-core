dc21dfc56a4c068a128b77880c93bbd5945df8df
Brent Christensen <brent@eventespresso.com>
2013-05-27 12:35:57 -0700

    move espresso_events_on_frontpage() function to init.php file [touch:2801]

--- includes/functions/init.php
+++ includes/functions/init.php
@@ -59,6 +59,21 @@ function espresso_load_messages_init() {
 
 
 
+function espresso_events_on_frontpage() {
+	// first check if a page is being used for the frontpage
+	if ( get_option('show_on_front') == 'page' ) {
+		global $org_options;
+		// grab that page's id
+		$frontpage = get_option('page_on_front');
+		// compare to event_page_id
+		return  $frontpage == $org_options['event_page_id'] ? TRUE : FALSE;
+	}
+	return FALSE;
+}
+
+
+
+
 /**
  * 	espresso_add_rewrite_rules
  *
