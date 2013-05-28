5d03d394d3c5fe638774e02348cbf2b26fb14ac4
Brent Christensen <brent@eventespresso.com>
2013-05-27 18:59:42 -0700

    add rewrite rules to init.php [touch:2777]

--- espresso.php
+++ espresso.php
@@ -150,15 +150,18 @@ add_action('plugins_loaded', 'espresso_init', 25);
 
 function espresso_load_init() {
 	require_once(dirname(__FILE__) . '/includes/functions/init.php');
+	add_action('init', 'espresso_load_messages_init', 15);
+	add_action('init', 'espresso_add_rewrite_rules');
+	add_filter('query_vars', 'espresso_add_query_vars');	
 }
 add_action('plugins_loaded', 'espresso_load_init', 100);
 
 
 if ( is_admin() ) {
 	
-	register_activation_hook(__FILE__, 'espresso_plugin_activation');
-	add_filter('plugin_action_links', 'event_espresso_filter_plugin_actions', 10, 2);
 	require_once(dirname(__FILE__) . '/includes/functions/admin_init.php');
+	register_activation_hook(__FILE__, 'espresso_plugin_activation');
+	add_filter('plugin_action_links', 'event_espresso_filter_plugin_actions', 10, 2);	
 	add_action('plugins_loaded', 'espresso_check_for_export');
 	add_action('plugins_loaded', 'espresso_check_for_import');
 	add_action('admin_init', 'espresso_check_data_tables' );
@@ -173,13 +176,10 @@ if ( is_admin() ) {
 	
 	require_once(dirname(__FILE__) . '/includes/functions/frontend_init.php');
 	add_action('init', 'espresso_load_jquery', 10);
-	add_action('init', 'espresso_load_messages_init', 15);
-	add_action('init', 'espresso_add_rewrite_rules');
 	add_action('init', 'espresso_export_certificate', 30);
 	add_action('init', 'espresso_export_invoice', 30);
 	//add_action('init', 'espresso_export_ticket', 30);
 	add_action('init', 'espresso_frontend_init', 25);
-	add_filter('query_vars', 'espresso_add_query_vars');
 	add_action('widgets_init', 'espresso_widget');
 	add_action('wp_head', 'espresso_info_header');
 	add_action('wp_enqueue_scripts', 'add_espresso_stylesheet', 20);
