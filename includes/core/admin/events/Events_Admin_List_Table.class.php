5a0760d31c796f9d2888c9bcda017b30c1a9cb7d
Brent Christensen <brent@eventespresso.com>
2013-06-06 08:58:46 -0700

    grrrr... ".php"  got stripped from filename somehow [touch:2883]

--- includes/core/admin/events/Events_Admin_List_Table.class.php
+++ includes/core/admin/events/Events_Admin_List_Table.class.php
@@ -35,7 +35,7 @@ class Events_Admin_List_Table extends EE_Admin_List_Table {
 
 	public function __construct( $admin_page ) {
 		parent::__construct($admin_page);
-		require_once( EVENT_ESPRESSO_PLUGINFULLPATH . 'helpers/EE_DTT_Helper.helper' );
+		require_once ( EE_HELPERS . 'EE_DTT_Helper.helper.php' );
 	}
 
 	protected function _setup_data() {
