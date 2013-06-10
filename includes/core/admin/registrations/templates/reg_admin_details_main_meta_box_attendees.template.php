cfc27ca1fd1d3b58602a32b225a80ce0214548b6
Brent Christensen <brent@eventespresso.com>
2013-06-10 11:53:37 -0700

    change price paid label to ticket price [touch:2910]

--- includes/core/admin/registrations/templates/reg_admin_details_main_meta_box_attendees.template.php
+++ includes/core/admin/registrations/templates/reg_admin_details_main_meta_box_attendees.template.php
@@ -15,7 +15,7 @@
 					<th class="jst-cntr"><?php _e( 'REG ID', 'event_espresso' );?></th>
 					<th class="jst-left"><?php _e( 'Event Name', 'event_espresso' );?></th>
 					<th class="jst-left"><?php _e( 'Attendee', 'event_espresso' );?></th>
-					<th class="jst-rght"><?php _e( 'Price Paid', 'event_espresso' );?></th>
+					<th class="jst-rght"><?php _e( 'Ticket Price', 'event_espresso' );?></th>
 					<th class="jst-left"><?php _e( 'Email', 'event_espresso' );?></th>
 					<th class="jst-left"><?php _e( 'Address', 'event_espresso' );?></th>
 					<!--<th class="jst-cntr"><?php _e( 'Actions', 'event_espresso' );?></th>-->
