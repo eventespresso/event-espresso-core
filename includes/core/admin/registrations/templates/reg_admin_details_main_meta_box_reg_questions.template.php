76ef01b7bc7c5279c0b0fd5109504db609c6be7c
Brent Christensen <brent@eventespresso.com>
2013-06-10 12:02:57 -0700

    add notice regarding editing primary attendee data [touch:2890]

--- includes/core/admin/registrations/templates/reg_admin_details_main_meta_box_reg_questions.template.php
+++ includes/core/admin/registrations/templates/reg_admin_details_main_meta_box_reg_questions.template.php
@@ -7,6 +7,7 @@
 		<input  type="hidden" name="REG_ID" value="<?php echo $REG_ID; ?>"/>
 		<input  type="hidden" name="espresso_ajax" id="espresso-ajax" value="0"/>
 		<input  type="hidden" name="noheader" id="reg-admin-noheader-inp" value="true"/>
+		
 		<?php echo $att_questions; ?>
 		
 		<table class="form-table ee-width-50">
@@ -24,5 +25,9 @@
 	</form>
 
 	<br class="clear"/>
+	
+	<p class="ee-attention">
+		<?php _e( '<b>Please Note:</b><br/>Editing the primary personal information (first name, last name, email address) for a registrant will result in a new record being created in the attendee contact details table, that will then be applied to <strong>this</strong> registration only. If you wish to simply correct a typo for this attendee, or update other personal details, then update the attendee record directly via the Contact List.', 'event_espresso' );?>
+	</p>
 	
 </div>
