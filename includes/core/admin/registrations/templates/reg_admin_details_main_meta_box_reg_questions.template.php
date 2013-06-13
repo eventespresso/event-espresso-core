04461b709e580d02400dbb28526d3626a3e81ca2
Brent Christensen <brent@eventespresso.com>
2013-06-12 17:03:44 -0700

    improves wording of reg admin reg form notice [touch:2890]

--- includes/core/admin/registrations/templates/reg_admin_details_main_meta_box_reg_questions.template.php
+++ includes/core/admin/registrations/templates/reg_admin_details_main_meta_box_reg_questions.template.php
@@ -1,7 +1,13 @@
 <div id="admin-primary-mbox-questions-dv" class="admin-primary-mbox-dv">
 	
 	<p class="ee-attention">
-		<?php _e( '<b>Please Note:</b><br/>Editing the primary personal information below (first name, last name, email address) for a registrant will result in a <strong>new</strong> record being created in the attendee contact details table, that will then be applied to <strong>this</strong> registration only. If you wish to simply correct a typo for this attendee, or update other personal details, then update the attendee record directly via the Contact List. Editing the primary personal information below creates a new attendee record because this allows for a way to differenetiate between multiple registrations that were originally created using one set of attendee details.', 'event_espresso' );?>
+	<?php 
+		echo sprintf(
+			__( '<b>Please Note:</b><br/>Editing the primary personal information below (first name, last name, email address) for a registrant may result in a <strong>new</strong> record being created in the attendee contact details table (if an existing attendee matching those same details is not found). The new (or existing) attendee record will then be associated with this registration. <br/>If you wish to simply correct a typo for this attendee, or update other contact details, then update the attendee record directly via the %sContact List%s.<br/>The reason that editing the primary personal information below might create a new attendee record is because this allows for a way to differentiate between multiple registrations that were originally created using one set of attendee details, and avoid duplicate and/or conflicting registrations.', 'event_espresso' ),
+			'<a href=' . $contact_list_url . '>',
+			'</a>'
+		);
+	?>
 	</p>
 
 	<form name="reg-admin-attendee-questions-frm" id="reg-admin-attendee-questions-frm" action="<?php echo REG_ADMIN_URL; ?>">
@@ -32,3 +38,6 @@
 	
 	
 </div>
+
+
+http://localhost/4.0-DEV/wp-admin/admin.php?page=espresso_registrations&action=contact_list&contact_list_nonce=5650d7b7f3
