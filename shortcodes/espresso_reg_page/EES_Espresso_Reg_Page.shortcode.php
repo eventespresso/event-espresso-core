6bdd239caf9eeee70e58c30a0f733ed812ea266b
Brent Christensen <brent@eventespresso.com>
2013-06-25 16:43:12 -0700

    added EES_Espresso_Reg_Page shortcode (shell) for testing multiple shortcodes[touch:3003]

---
+++ shortcodes/espresso_reg_page/EES_Espresso_Reg_Page.shortcode.php
@@ -0,0 +1,99 @@
+<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package			Event Espresso
+ * @ author			Seth Shoultes
+ * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link					http://www.eventespresso.com
+ * @ version		 	4.0
+ *
+ * ------------------------------------------------------------------------
+ *
+ * EES_Espresso_Reg_Page
+ *
+ * @package			Event Espresso
+ * @subpackage	/shortcodes/
+ * @author				Brent Christensen 
+ *
+ * ------------------------------------------------------------------------
+ */
+class EES_Espresso_Reg_Page  extends EES_Shortcode {
+	
+	/**
+	 * 	register_shortcode - makes core aware of this shortcode
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public static function register_shortcode() {
+		EE_Front_Controller::register_shortcode(  __CLASS__ , __FILE__ );
+	}
+
+	/**
+	 * 	set_hooks - for hooking into EE Core, modules, etc
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public static function set_hooks() {
+//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
+//		add_filter( 'FHEE_run_EE_wp', '__return_true' );
+//		add_filter( 'FHEE_load_EE_Session', '__return_true' );
+	}
+
+	/**
+	 * 	set_hooks_admin - for hooking into EE Admin Core, modules, etc
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public static function set_hooks_admin() {
+	}
+
+
+
+	/**
+	 * 	init - calls class constructor
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public function init() {
+		$this->ouput =  '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
+	}
+
+
+
+	/**
+	 * 	process_shortcode - EVENT_LIST - Returns a list of events
+	 * 
+	 * 	[EVENT_LIST]
+	 * 	[EVENT_LIST limit=1]
+	 * 	[EVENT_LIST css_class=my-custom-class]
+	 * 	[EVENT_LIST show_expired=true]
+	 * 	[EVENT_LIST show_deleted=true]
+	 * 	[EVENT_LIST show_secondary=true]
+	 * 	[EVENT_LIST show_recurrence=true]
+	 * 	[EVENT_LIST category_identifier=your_category_identifier]
+	 * 
+	 *  @access 	public
+	 *  @param		array 	$attributes
+	 *  @return 	void
+	 */
+	public function process_shortcode( $attributes ) {
+		//event_espresso_get_event_details( $attributes );
+		//return '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
+		return $this->ouput;		
+	}
+
+	
+	
+
+
+}
+// End of file EES_Espresso_Reg_Page.shortcode.php
+// Location: /shortcodes/EES_Espresso_Reg_Page.shortcode.php
