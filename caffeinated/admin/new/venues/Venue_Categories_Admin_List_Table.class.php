b39ec0a1ed51d8cbc96f4d8e35b4865846cb0dc7
Darren Ethier <darren@roughsmootheng.in>
2013-06-24 20:03:39 -0400

    add venue category admin page(s) [touch: 3006]

--- caffeinated/admin/new/venues/Venue_Categories_Admin_List_Table.class.php
+++ caffeinated/admin/new/venues/Venue_Categories_Admin_List_Table.class.php
@@ -16,26 +16,22 @@ if (!defined('EVENT_ESPRESSO_VERSION') )
  *
  * ------------------------------------------------------------------------
  *
- * Event_Categories_Admin_List_Table
+ * Venue_Categories_Admin_List_Table
  *
- * Class for preparing the table listing all the event categories
+ * Class for preparing the table listing all the venue categories
  *
  * note: anywhere there are no php docs it is because the docs are available in the parent class.
  *
- * @package		Event_Categories_Admin_List_Table
- * @subpackage	includes/core/admin/events/Event_Category_Admin_List_Table.class.php
+ * @package		Venue_Categories_Admin_List_Table
+ * @subpackage	caffeinated/admin/new/venues/Venue_Categories_Admin_List_Table.class.php
  * @author		Darren Ethier
  *
  * ------------------------------------------------------------------------
  */
 
-class Event_Categories_Admin_List_Table extends EE_Admin_List_Table {
+class Venue_Categories_Admin_List_Table extends EE_Admin_List_Table {
 
-	public function __construct( $admin_page ) {
-		parent::__construct($admin_page);
-	}
-
-
+	
 
 
 	protected function _setup_data() {
@@ -50,8 +46,8 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table {
 
 	protected function _set_properties() {
 		$this->_wp_list_args = array(
-			'singular' => __('event category', 'event_espresso' ),
-			'plural' => __('event categories', 'event_espresso' ),
+			'singular' => __('venue category', 'event_espresso' ),
+			'plural' => __('venue categories', 'event_espresso' ),
 			'ajax' => TRUE, //for now,
 			'screen' => $this->_admin_page->get_current_screen()->id 
 			);
@@ -61,7 +57,7 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table {
 			'id' => __('ID', 'event_espresso'),
 			'name' => __('Name', 'event_espresso'),
 			'shortcode' => __('Shortcode', 'event_espresso'),
-			'count' => __('Events', 'event_espresso')
+			'count' => __('Venues', 'event_espresso')
 			);
 
 		$this->_sortable_columns = array(
@@ -97,7 +93,7 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table {
 
 
 	public function column_cb($item) {
-		return sprintf( '<input type="checkbox" name="EVT_CAT_ID[]" value="%s" />', $item->get('term_id') );
+		return sprintf( '<input type="checkbox" name="VEN_CAT_ID[]" value="%s" />', $item->get('term_id') );
 	}
 
 
@@ -125,16 +121,16 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table {
 	public function column_name($item) {
 		$edit_query_args = array(
 			'action' => 'edit_category',
-			'EVT_CAT_ID' => $item->get('term_id')
+			'VEN_CAT_ID' => $item->get('term_id')
 		);
 
 		$delete_query_args = array(
 			'action' => 'delete_category',
-			'EVT_CAT_ID' => $item->get('term_id')
+			'VEN_CAT_ID' => $item->get('term_id')
 		);
 
-		$edit_link = EE_Admin_Page::add_query_args_and_nonce( $edit_query_args, EVENTS_ADMIN_URL );
-		$delete_link = EE_Admin_Page::add_query_args_and_nonce( $delete_query_args, EVENTS_ADMIN_URL );
+		$edit_link = EE_Admin_Page::add_query_args_and_nonce( $edit_query_args, EE_VENUES_ADMIN_URL );
+		$delete_link = EE_Admin_Page::add_query_args_and_nonce( $delete_query_args, EE_VENUES_ADMIN_URL );
 
 		$actions = array(
 			'edit' => '<a href="' . $edit_link . '" title="' . __('Edit Category', 'event_espresso') . '">' . __('Edit', 'event_espresso') . '</a>'
@@ -164,7 +160,7 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table {
 			'action' => 'default',
 			'category' => $item->get_first_related('Term')->get('slug')
 			);
-		$e_link = EE_Admin_Page::add_query_args_and_nonce( $e_args, EVENTS_ADMIN_URL );
+		$e_link = EE_Admin_Page::add_query_args_and_nonce( $e_args, EE_VENUES_ADMIN_URL );
 		$content = '<a href="' . $e_link . '">' . $item->get('term_count') . '</a>';
 		return $content;
 	}
--- includes/core/admin/events/Events_Admin_Page.core.php
+++ includes/core/admin/events/Events_Admin_Page.core.php
@@ -1765,7 +1765,7 @@ class Events_Admin_Page extends EE_Admin_Page_CPT {
 		?>		
 			<h2><?php _e('Unique Category Identifier', 'event_espresso'); ?></h2>
 			<p><?php _e('This should be a unique identifier for the category. Example: "category1" (without qoutes.)', 'event_espresso'); ?></p>
-			<p><?php printf( __('The unique ID can also be used in individual pages using the %s shortcode', 'event_espresso'), '[EVENT_ESPRESSO_CATEGORY event_category_id="category_identifier"]' ); ?>.</p>		
+			<p><?php printf( __('The unique ID can also be used in individual pages using the %s shortcode', 'event_espresso'), '[EVENT_ESPRESSO_CATEGORY category_id="category_identifier"]' ); ?>.</p>		
 		<?php
 	}
 
