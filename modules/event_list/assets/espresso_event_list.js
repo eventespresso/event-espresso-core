60a189a25fd56dfb221e36301fd0991e04773c17
Brent Christensen <brent@eventespresso.com>
2013-06-27 14:13:48 -0700

    make grid respond better to various sized themes [touch:3003]

--- modules/event_list/assets/espresso_event_list.js
+++ modules/event_list/assets/espresso_event_list.js
@@ -1,45 +1,43 @@
 jQuery(document).ready(function($) {
-	
-	//blocksit define
-	$(window).load( function() {
-		$('#espresso-events-list-dv').BlocksIt({
-			blockElement: 'article',
-			numOfCol: 3,
-			offsetX: 10,
-			offsetY: 10
-		});
-	});
-	
+
 	//window resize
-	var currentWidth = 1100;
-	$(window).resize(function() {
-		var winWidth = $(window).width();
+	var currentWidth = 0;
+	
+	function espresso_resize_event_list() {
+		var winWidth = $('#espresso-events-list-wrap-dv').width();
 		var conWidth;
-		if(winWidth < 660) {
+		if(winWidth < 640) {
 			conWidth = 320;
 			col = 1
-		} else if(winWidth < 980) {
+		} else if(winWidth < 960) {
 			conWidth = 640;
 			col = 2;
-		} else if(winWidth < 1300) {
+		} else if(winWidth < 1280) {
 			conWidth = 960;
 			col = 3;
 		} else {
 			conWidth = 1280;
 			col = 4;
 		}
+		//alert( 'conWidth = ' + conWidth + '\n' + 'currentWidth = ' + currentWidth );
 		
 		if(conWidth != currentWidth) {
 			currentWidth = conWidth;
-			$('#espresso-events-list-dv').width(conWidth);
+			$('#espresso-events-list-dv').width(conWidth).fadeIn();
 			$('#espresso-events-list-dv').BlocksIt({
 				blockElement: 'article',
 				numOfCol: col,
 				offsetX: 10,
 				offsetY: 10
-			});
+			}); 
 		}
+		
+	};
+	
+	$(window).resize(function() {
+		espresso_resize_event_list();
 	});
 
+	espresso_resize_event_list();
 
 });
