05f7d7665ac82d11366440472d9f2bae6dbda7a1
Brent Christensen <brent@eventespresso.com>
2013-06-27 12:01:52 -0700

    add folder path and url constants [touch:3003]

--- modules/event_list/EED_Event_List.module.php
+++ modules/event_list/EED_Event_List.module.php
@@ -62,6 +62,8 @@ class EED_Event_List  extends EED_Module {
 	 *  @return 	void
 	 */
 	public function init() {
+		define( 'EVENT_LIST_ASSETS_URL', plugin_dir_url( __FILE__ ) . 'assets' . DS );
+		define( 'EVENT_LIST_TEMPLATES_PATH', plugin_dir_path( __FILE__ ) . 'templates' . DS );
 		add_filter( 'FHEE_load_org_options', '__return_true' );
 		add_filter( 'FHEE_load_css', '__return_true' );
 //		add_filter( 'FHEE_run_EE_wp', '__return_true' );
@@ -70,7 +72,10 @@ class EED_Event_List  extends EED_Module {
 //		add_action( 'wp', array( $this, 'wp' ));
 //		add_filter( 'the_content', array( $this, 'the_content' ));
 		// parse_request
-		add_filter( 'request', array( $this, 'filter_request' ));  
+		add_filter( 'request', array( $this, 'filter_request' )); 
+		remove_all_filters( 'excerpt_length' );
+		add_filter( 'excerpt_length', array( $this, 'excerpt_length' ), 10 );
+		add_filter('excerpt_more', array( $this, 'excerpt_more' ), 10 );
 		add_action('wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ), 10 );
 		add_filter( 'template_include', array( $this, 'template_include' ), 1 );
 		//$this->ouput =  '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
@@ -130,6 +135,27 @@ class EED_Event_List  extends EED_Module {
 	}
 
 
+	/**
+	 * 	excerpt_length
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public function excerpt_length( $length ) {
+		return 28;
+	}
+
+	/**
+	 * 	excerpt_more
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public function excerpt_more( $more ) {
+		return '&hellip;';
+	}
+
+
 
 	/**
 	 * 	wp_enqueue_scripts
@@ -141,17 +167,21 @@ class EED_Event_List  extends EED_Module {
 
 		// get some style
 		if ( apply_filters( 'FHEE_enable_default_espresso_css', FALSE )) {
-			// first check uploads folder
-			if ( file_exists( EVENT_ESPRESSO_UPLOAD_DIR . 'templates/event_list.css' )) {
-				wp_register_style( 'espresso_event_list', EVENT_ESPRESSO_UPLOAD_URL . 'templates/espresso_event_list.css', array() );
-			} else {
-				wp_register_style( 'espresso_event_list', plugin_dir_url( __FILE__ ) . '/templates/espresso_event_list.css', array() );
+			if ( is_archive() ) {
+				// first check uploads folder
+				if ( file_exists( EVENT_ESPRESSO_UPLOAD_DIR . 'templates/event_list.css' )) {
+					wp_register_style( 'espresso_event_list', EVENT_ESPRESSO_UPLOAD_URL . 'templates/espresso_event_list.css', array() );
+					wp_register_script( 'espresso_event_list', EVENT_ESPRESSO_UPLOAD_URL . 'templates/espresso_event_list.js', array( 'blocksit' ), '1.0', FALSE  );
+				} else {
+					wp_register_style( 'espresso_event_list', EVENT_LIST_ASSETS_URL . 'espresso_event_list.css', array() );
+					wp_register_script( 'espresso_event_list', EVENT_LIST_ASSETS_URL . 'espresso_event_list.js', array( 'blocksit' ), '1.0', FALSE );
+				}
+				wp_register_script( 'blocksit', EVENT_LIST_ASSETS_URL . 'blocksit.min.js', array( 'jquery' ), '1.0', FALSE );
+				wp_enqueue_style( 'espresso_event_list' );
+				wp_enqueue_script( 'blocksit' );
+				wp_enqueue_script( 'espresso_event_list' );
 			}
-			wp_enqueue_style( 'espresso_event_list' );
 		}
-
-
-
 
 	}
 
@@ -163,18 +193,19 @@ class EED_Event_List  extends EED_Module {
 	 *  @return 	void
 	 */
 	public function template_include( $template_path ) {
+
 		if ( get_post_type() == 'espresso_events' ) {
 			if ( is_single() ) {
 				// check if the template file exists in the theme first
 				if ( ! $template_path = locate_template( array( 'single-espresso_events.php' ))) {
 					// otherwise get it from 
-					$template_path = plugin_dir_path( __FILE__ ) . '/templates/single-espresso_events.php';
+					$template_path = EVENT_LIST_TEMPLATES_PATH . 'single-espresso_events.php';
 				}
 			} else if ( is_archive() ) {
 				// check if the template file exists in the theme first
 				if ( ! $template_path = locate_template( array( 'archive-espresso_events.php' ))) {
 					// otherwise get it from 
-					$template_path = plugin_dir_path( __FILE__ ) . '/templates/archive-espresso_events.php';
+					$template_path = EVENT_LIST_TEMPLATES_PATH . 'archive-espresso_events.php';
 				}
 			} 
 		}
