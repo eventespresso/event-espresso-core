d2478ee621a24fe30faad467a06ab056bbeb98b2
Brent Christensen <brent@eventespresso.com>
2013-06-28 11:58:47 -0700

    call event_list module to process event_list shortcode [touch:3003]

--- shortcodes/event_list/EES_Event_List.shortcode.php
+++ shortcodes/event_list/EES_Event_List.shortcode.php
@@ -40,52 +40,29 @@ class EES_Event_List  extends EES_Shortcode {
 	 *  @return 	void
 	 */
 	public static function set_hooks() {
-//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-//		add_filter( 'FHEE_run_EE_wp', '__return_true' );
-//		add_filter( 'FHEE_load_EE_Session', '__return_true' );
+		add_filter( 'AHEE__Front_Controller__process_request__modules', array( 'EES_Espresso_Events', 'load_event_list' ), 10, 1 );
 	}
 
 	/**
-	 * 	set_hooks_admin - for hooking into EE Admin Core, modules, etc
+	 * 	load_event_list - filter_modules
 	 *
 	 *  @access 	public
-	 *  @return 	void
+	 *  @return 	array
 	 */
-	public static function set_hooks_admin() {
+	public static function load_event_list( $modules ) {
+		$modules[] = 'Event_List';
+		return $modules;
 	}
 
 
 
 	/**
-	 * 	init - initial shortcode module setup called during "wp" hook
-	 * 	this method is primarily used for loading resources that will be required by the shortcode when it is actually processed
+	 * 	init - initial module setup
 	 *
 	 *  @access 	public
 	 *  @return 	void
 	 */
 	public function init() {
-//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-		add_filter( 'FHEE_load_org_options', '__return_true' );
-		// parse_request
-		//add_filter( 'request', array( $this, 'filter_request' ));  
-		//$this->ouput =  '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-	}
-
-
-
-	/**
-	 * 	wp_loaded - should fire after shortcode, module, addon, or other plugin's default priority init phases have run
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function filter_request(  $req  ) {
-		//printr( $req, '$req  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-/*	    if ( isset( $req['pagename'] ) && $req['pagename'] == $this->EE->CFG->events_page ) {
-	 		unset( $req['pagename'] );
-	 		$req['post_type'] = 'espresso_events';
-		}  */     
-	    return $req;
 	}
 
 
@@ -93,27 +70,13 @@ class EES_Event_List  extends EES_Shortcode {
 	/**
 	 * 	process_shortcode - EVENT_LIST - Returns a list of events
 	 * 
-	 * 	[EVENT_LIST]
-	 * 	[EVENT_LIST limit=1]
-	 * 	[EVENT_LIST css_class=my-custom-class]
-	 * 	[EVENT_LIST show_expired=true]
-	 * 	[EVENT_LIST show_deleted=true]
-	 * 	[EVENT_LIST show_secondary=true]
-	 * 	[EVENT_LIST show_recurrence=true]
-	 * 	[EVENT_LIST category_identifier=your_category_identifier]
-	 * 
 	 *  @access 	public
 	 *  @param		array 	$attributes
 	 *  @return 	void
 	 */
 	public function process_shortcode( $attributes ) {
-		//event_espresso_get_event_details( $attributes );
-		//return '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
 		return $this->ouput;		
-	}
-
-	
-	
+	}	
 
 
 }
