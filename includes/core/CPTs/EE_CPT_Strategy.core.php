4a156962c6fd83c93ac4b292abc3e32bfea46658
Brent Christensen <brent@eventespresso.com>
2013-07-06 10:18:09 -0700

    move files to sub-folder

--- includes/core/CPTs/EE_CPT_Strategy.core.php
+++ includes/core/CPTs/EE_CPT_Strategy.core.php
@@ -119,6 +119,7 @@ class EE_CPT_Strategy extends EE_BASE {
 		if ( $espresso_page = $this->EE->REQ->is_espresso_page() ) {
 			// but is this espresso page a CPT endpoint ?
 			if ( isset( $this->_CPT_endpoints[ $espresso_page ] )) {
+				
 				$this->CPT = $this->_CPTs[ $this->_CPT_endpoints[ $espresso_page ] ];
 				$this->CPT['post_type'] = $this->_CPT_endpoints[ $espresso_page ];
 				$this->CPT['requested_page'] = $espresso_page;
@@ -131,11 +132,12 @@ class EE_CPT_Strategy extends EE_BASE {
 					$this->CPT['meta_table'] = isset( $this->CPT['tables'][ $this->CPT['singular_name'] . '_Meta' ] ) ? $this->CPT['tables'][ $this->CPT['singular_name'] . '_Meta' ] : FALSE;
 					// creates classname like:  EE_CPT_Event_Strategy
 					$CPT_Strategy_class_name = 'EE_CPT_' . $this->CPT['singular_name'] . '_Strategy';
+					 $this->EE->load_file ( EE_CORE . 'CPTs' . DS, $CPT_Strategy_class_name, 'core', FALSE, FALSE );
 					// instantiate
 					$CPT_Strategy = new $CPT_Strategy_class_name( $this->EE, $this->CPT );
 //					printr( $CPT_Strategy, '$CPT_Strategy  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-					add_filter( 'posts_fields', array( $CPT_Strategy, 'posts_fields' ));
-					add_filter( 'posts_join',	array( $CPT_Strategy, 'posts_join' ));
+					add_filter( 'posts_fields', array( $this, 'posts_fields' ));
+					add_filter( 'posts_join',	array( $this, 'posts_join' ));
 					add_filter( 'get_' . $this->CPT['post_type'] . '_metadata', array( $CPT_Strategy, 'get_EE_post_type_metadata' ), 1, 4 );
 
 				}				
@@ -143,6 +145,39 @@ class EE_CPT_Strategy extends EE_BASE {
 		}
 	}
 
+
+
+	/**
+	 * 	posts_fields
+	 *
+	 *  @access 	public
+	 *  @return 	string
+	 */
+	public function posts_fields( $SQL ) {
+		// does this CPT have a meta table ?
+		if ( isset( $this->CPT['meta_table'] )) {
+			global $wpdb;
+			$SQL .= ', ' . $this->CPT['meta_table']->get_table_name() . '.* ' ;
+		}
+		return $SQL;
+	}
+
+
+
+	/**
+	 * 	posts_join
+	 *
+	 *  @access 	public
+	 *  @return 	string
+	 */
+	public function posts_join( $SQL ) {
+		// does this CPT have a meta table ?
+		if ( isset( $this->CPT['meta_table'] )) {
+			global $wpdb;
+			$SQL .= ' LEFT JOIN ' . $this->CPT['meta_table']->get_table_name() . ' ON (' . $this->CPT['meta_table']->get_table_name() . '.' . $this->CPT['meta_table']->get_fk_on_table() . ' = ' . $wpdb->posts . '.ID) ';
+		}
+		return $SQL;
+	}
 
 
 
@@ -159,7 +194,7 @@ class EE_CPT_Strategy extends EE_BASE {
 /**
  * ------------------------------------------------------------------------
  *
- * EE_CPT_Event_Strategy
+ * EE_CPT_Default_Strategy
  *
  * @package			Event Espresso
  * @subpackage	/core/
@@ -167,7 +202,7 @@ class EE_CPT_Strategy extends EE_BASE {
  *
  * ------------------------------------------------------------------------
  */
-class EE_CPT_Event_Strategy {
+class EE_CPT_Default_Strategy {
 
 
 	/**
@@ -197,56 +232,11 @@ class EE_CPT_Event_Strategy {
 		$this->EE = $EE;
 		$this->CPT = $CPT;
 		//printr( $this->CPT, '$this->CPT  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-		add_filter( 'request', array( $this, 'request' ), 1 );  
 		add_filter( 'pre_get_posts', array( $this, 'pre_get_posts' ), 999 );
 		add_action( 'loop_start', array( $this, 'loop_start' ), 1 );
 	}
 
 
-
-	/**
-	 * 	request 
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function request(  $req  ) {
-//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-//		if ( isset( $req['pagename'] ) && $req['pagename'] == $this->CPT['this_page']) {
-//			unset( $req['pagename'] );
-//		}		
- 		//$req['fields'] = 'ids';
-//		printr( $req, '$req  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-	    return $req;
-	}
-
-
-
-	/**
-	 * 	posts_fields
-	 *
-	 *  @access 	public
-	 *  @return 	string
-	 */
-	public function posts_fields( $SQL ) {
-		global $wpdb;
-		$SQL .= ', ' . $this->CPT['meta_table']->get_table_name() . '.* ' ;
-		return $SQL;
-	}
-
-
-
-	/**
-	 * 	posts_join
-	 *
-	 *  @access 	public
-	 *  @return 	string
-	 */
-	public function posts_join( $SQL ) {
-		global $wpdb;
-		$SQL .= ' LEFT JOIN ' . $this->CPT['meta_table']->get_table_name() . ' ON (' . $this->CPT['meta_table']->get_table_name() . '.EVT_ID = ' . $wpdb->posts . '.ID) ';
-		return $SQL;
-	}
 
 
 
@@ -277,158 +267,7 @@ class EE_CPT_Event_Strategy {
 	 *  @return 	void
 	 */
 	public function loop_start( $WP_Query ) {
-		$EVT = $this->EE->load_model( 'Event' );
-		$EVT_IDs = array();
-		foreach( $WP_Query->posts as $WP_Post ) {
-			$EVT_IDs[] = $WP_Post->ID;
-		}
-//		$events = $EVT->get_all( array( 0 =>array( 'EVT_ID' => array( 'IN', $EVT_IDs ), 'DTT_is_primary' => 1 ), 'force_join' =>array( 'Datetime' )));
-//		printr( $WP_Query, '$WP_Query  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-//		printr( $EVT_IDs, '$EVT_IDs  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-//		printr( $events, '$events  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-
-	}
-
-
-
-
-	/**
-	 * 	get_EE_post_type_metadata
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function get_EE_post_type_metadata( $meta_value = NULL, $post_id, $meta_key, $single ) {
-
-		return $meta_value;
-
-	}
-
-
-}
-
-
-
-
-
-/**
- * ------------------------------------------------------------------------
- *
- * EE_CPT_Venue_Strategy
- *
- * @package			Event Espresso
- * @subpackage	/core/
- * @author				Brent Christensen 
- *
- * ------------------------------------------------------------------------
- */
-class EE_CPT_Venue_Strategy extends EE_CPT_Strategy {
-
-
-	/**
-	 * 	EE_Registry Object
-	 *	@var 	EE_Registry	$EE	
-	 * 	@access 	protected
-	 */
-	protected $EE = NULL;
-
-	/**
-	 * $CPT - the current page, if it utilizes CPTs
-	 *	@var 	object	
-	 * 	@access 	protected
-	 */
-	protected $CPT = NULL;
-
-
-
-	
-	/**
-	 * 	class constructor
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function __construct( EE_Registry $EE, $CPT ) {
-		$this->EE = $EE;
-		$this->CPT = $CPT;
-		add_filter( 'request', array( $this, 'request' ), 1 );  
-		add_filter( 'pre_get_posts', array( $this, 'pre_get_posts' ), 999 );
-		add_action( 'loop_start', array( $this, 'loop_start' ), 1 );
-	}
-
-
-
-	/**
-	 * 	wp_loaded - should fire after shortcode, module, addon, or other plugin's default priority init phases have run
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function request(  $req  ) {
-//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-	    return $req;
-	}
-
-
-
-	/**
-	 * 	posts_fields
-	 *
-	 *  @access 	public
-	 *  @return 	string
-	 */
-	public function posts_fields( $SQL ) {
-		global $wpdb;
-		$SQL .= ', ' . $this->CPT['meta_table']->get_table_name() . '.* ' ;
-		return $SQL;
-	}
-
-
-
-	/**
-	 * 	posts_join
-	 *
-	 *  @access 	public
-	 *  @return 	string
-	 */
-	public function posts_join( $SQL ) {
-		global $wpdb;
-		$SQL .= ' LEFT JOIN ' . $this->CPT['meta_table']->get_table_name() . ' ON (' . $this->CPT['meta_table']->get_table_name() . '.VNU_ID = ' . $wpdb->posts . '.ID) ';
-		return $SQL;
-	}
-
-
-
-
-	/**
-	 * 	pre_get_posts
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function pre_get_posts(  $WP_Query  ) {
-//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-		if ( ! $WP_Query->is_main_query() && ! $WP_Query->is_archive() ) {
-			return;
-		}
-//		$WP_Query->set( 'post_type', array( $this->CPT['post_type'] ));
-//		$WP_Query->set( 'fields', 'ids' );
-//		printr( $WP_Query, '$WP_Query  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-		return $WP_Query;
-	}
-
-
-
-
-	/**
-	 * 	wp
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function loop_start( $WP_Query ) {
-		//$EVT = $this->EE->load_model( 'Event' );
-//		printr( $EVT, '$EVT  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
+//		$EVT = $this->EE->load_model( 'Event' );
 //		$EVT_IDs = array();
 //		foreach( $WP_Query->posts as $WP_Post ) {
 //			$EVT_IDs[] = $WP_Post->ID;
@@ -449,9 +288,9 @@ class EE_CPT_Venue_Strategy extends EE_CPT_Strategy {
 	 *  @access 	public
 	 *  @return 	void
 	 */
-	public function get_EE_post_type_metadata( $meta_value, $post_id, $meta_key, $single ) {
+	public function get_EE_post_type_metadata( $meta_value = NULL, $post_id, $meta_key, $single ) {
 
-
+		return $meta_value;
 
 	}
 
