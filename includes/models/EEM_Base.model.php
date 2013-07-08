51705b6d0473c8a061cfa1c21c09f4e89e2cdec6
Michael Nelson <michael@eventespresso.com>
2013-07-08 14:11:03 -0700

    [touch:3045]{t:30}resolved a bug that has resulted from removaled from new cl file inlusion policy. also removed my exception from EE_Event_Regsitatin_list_table because I was able to acccess it... although I do still suspect theres some unnecessary duplication between it and EE_Registratio_List_Table...

--- includes/models/EEM_Base.model.php
+++ includes/models/EEM_Base.model.php
@@ -1835,9 +1835,10 @@ abstract class EEM_Base extends EE_Base{
 	 * takes care of including the PHP file with the corresponding .class file to this model.
 	 */
 	private function _include_php_class(){
-		$className=$this->_get_class_name();
-		$filepath = $className.".class.php";
-		if(!class_exists($className)){
+		//$className=$this->_get_class_name();
+		//$filepath = $className.".class.php";
+		EE_Registry::instance()->load_model( $this->get_this_model_name() );
+		/*(if(!class_exists($className)){
 			if(file_exists($filepath)){
 				require_once($filepath);
 			}else{
@@ -1846,7 +1847,7 @@ abstract class EEM_Base extends EE_Base{
 		}
 		if(!class_exists($className)){
 			throw new EE_Error(sprintf(__('There is class with name %s contained in file %s.class.php. You must create one','event_espresso'),$className,$className));
-		}
+		}*/
 	}
 	
 	/**
@@ -1882,7 +1883,7 @@ abstract class EEM_Base extends EE_Base{
 				
 		//get the required info to instantiate the class whcih relates to this model.
 		$className=$this->_get_class_name();
-
+		EE_REGISTRY::instance()->load_class($this->get_this_model_name(), false,false,false);
 		$classInstance = call_user_func_array( array( $className, 'new_instance_from_db' ), array( $this_model_fields_n_values, $this->_timezone ) );
 
 		//it is entirely possible that the instantiated class object has a set timezone_string db field and has set it's internal _timezone property accordingly (see new_instance_from_db in model objects particularly EE_Event for example).  In this case, we want to make sure the model object doesn't have its timezone string overwritten by any timezone property currently set here on the model so, we intentially override the model _timezone property with the model_object timezone property.
