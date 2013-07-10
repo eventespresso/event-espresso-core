e28440efbcba28cc83609f138254883e591933ae
Michael Nelson <michael@eventespresso.com>
2013-07-10 09:31:38 -0700

    [touch:2929]{t:20}forgot to rename the EE_CPT_WHere_COnditions.strategy.php file too. Also added the soft delete where conditions strategy

--- includes/models/EEM_Venue.model.php
+++ includes/models/EEM_Venue.model.php
@@ -91,7 +91,7 @@ class EEM_Venue extends EEM_CPT_Base {
 			'Country'=>new EE_Belongs_To_Relation()
 		);
 		require_once( EE_CLASSES . 'EE_Venue.class.php');
-		require_once( EE_MODELS . 'strategies/EE_Default_CPT_Where_Conditions.strategy.php');
+		require_once( EE_MODELS . 'strategies/EE_CPT_Where_Conditions.strategy.php');
 		$this->_default_where_conditions_strategy = new EE_CPT_Where_Conditions('espresso_venues', 'VNUM_ID');
 		parent::__construct();
 	}
