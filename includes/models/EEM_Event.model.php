8ec73af647e28f97f90e887a450bff45d7c34e84
Darren Ethier <darren@roughsmootheng.in>
2013-07-11 12:28:42 -0400

    make sure that EEM_Event defines relation with the Datetime model using the new EE_Has_Many_Revision_Relation relation object [touch: 3134]

--- includes/models/EEM_Event.model.php
+++ includes/models/EEM_Event.model.php
@@ -111,7 +111,7 @@ class EEM_Event  extends EEM_CPT_Base{
 			));
 		$this->_model_relations = array(
 			'Registration'=>new EE_Has_Many_Relation(),
-			'Datetime'=>new EE_HABTM_Relation('Event_Datetime'),
+			'Datetime'=>new EE_Has_Many_Revision_Relation('EVT_ID', 'DTT_parent'),
 			'Price'=>new EE_Has_Many_Relation(),
 			'Question_Group'=>new EE_HABTM_Relation('Event_Question_Group'),
 			'Venue'=>new EE_HABTM_Relation('Event_Venue'),
