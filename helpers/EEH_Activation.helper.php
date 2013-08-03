65249128e826c929617da3a34df06a9ec7dd5002
Darren Ethier <darren@roughsmootheng.in>
2013-08-02 20:39:52 -0400

    add new  columns and modify existing ones for pricing changes (ee_decaf/caf) [touch: 3242]

--- helpers/EEH_Activation.helper.php
+++ helpers/EEH_Activation.helper.php
@@ -708,7 +708,7 @@ class EEH_Activation {
 					  PRC_name varchar(45) NOT NULL,
 					  PRC_desc text,
 					  PRC_reg_limit mediumint(8) unsigned DEFAULT NULL,
-					  PRC_tckts_left mediumint(8) unsigned DEFAULT NULL,
+					  PRC_tckts_sold mediumint(8) unsigned DEFAULT NULL,
 					  PRC_start_date datetime NOT NULL default '0000-00-00 00:00:00',
 					  PRC_end_date datetime NOT NULL default '0000-00-00 00:00:00',
 					  PRC_is_active tinyint(1) unsigned NOT NULL DEFAULT '1',
@@ -716,6 +716,7 @@ class EEH_Activation {
 					  PRC_deleted tinyint(1) unsigned NOT NULL DEFAULT '0',
 					  PRC_order tinyint(3) unsigned NOT NULL DEFAULT '0',
 					  PRC_display_order tinyint(3) unsigned NOT NULL DEFAULT '0',
+					  PRC_parent int(10) unsigned DEFAULT 0,
 					  PRIMARY KEY  (PRC_ID)";
 		EEH_Activation::create_table($table_name, $sql);
 
@@ -989,13 +990,13 @@ class EEH_Activation {
 			
 			if ( ! $prices_exist ) {
 				$SQL = "INSERT INTO " . ESP_PRICE_TABLE . "
-							(PRC_ID, PRT_ID, EVT_ID, PRC_amount, PRC_name, PRC_desc, PRC_start_date, PRC_end_date, PRC_is_active, PRC_overrides, PRC_order, PRC_deleted, PRC_display_order ) VALUES
-							(1, 1, 0, '10.00', 'General Admission', 'Regular price for all Events. Example content - delete if you want to', NULL, NULL, 1, NULL, 0, 0, 0),
-							(2, 3, 0, '20', 'Members Discount', 'Members receive a 20% discount off of the regular price. Example content - delete if you want to', NULL, NULL, 1, NULL, 10, 0, 1),
-							(3, 4, 0, '10', 'Early Bird Discount', 'Sign up early and receive an additional 10% discount off of the regular price. Example content - delete if you want to', NULL, NULL, 1, NULL, 20, 0, 2),
-							(4, 5, 0, '7.50', 'Service Fee', 'Covers administrative expenses. Example content - delete if you want to', NULL, NULL, 1, NULL, 30, 0, 3),
-							(5, 6, 0, '7.00', 'Local Sales Tax', 'Locally imposed tax. Example content - delete if you want to', NULL, NULL, 1, NULL, 40, 1, 4),
-							(6, 7, 0, '15.00', 'Sales Tax', 'Federally imposed tax. Example content - delete if you want to', NULL, NULL, 1, NULL, 50, 1, 5);";			
+							(PRC_ID, PRT_ID, EVT_ID, PRC_amount, PRC_name, PRC_desc, PRC_start_date, PRC_end_date, PRC_reg_limit, PRC_tckts_sold, PRC_is_active, PRC_overrides, PRC_order, PRC_deleted, PRC_display_order, PRC_parent ) VALUES
+							(1, 1, 0, '10.00', 'General Admission', 'Regular price for all Events. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 0, 0, 1, 0),
+							(2, 3, 0, '20', 'Members Discount', 'Members receive a 20% discount off of the regular price. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 10, 0, 2, 0),
+							(3, 4, 0, '10', 'Early Bird Discount', 'Sign up early and receive an additional 10% discount off of the regular price. Example content - delete if you want to', -1, 0, NULL, NULL, 1, NULL, 20, 0, 3, 0),
+							(4, 5, 0, '7.50', 'Service Fee', 'Covers administrative expenses. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 30, 0, 4, 0),
+							(5, 6, 0, '7.00', 'Local Sales Tax', 'Locally imposed tax. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 40, 1, 5, 0),
+							(6, 7, 0, '15.00', 'Sales Tax', 'Federally imposed tax. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 50, 1, 6, 0);";			
 				$SQL = apply_filters( 'FHEE_default_prices_activation_sql', $SQL );
 				$wpdb->query($SQL);			
 			}
