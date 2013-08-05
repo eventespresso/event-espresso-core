23c449f035871a69dfb9daea1bdbd5e7b7c5d121
Darren Ethier <darren@roughsmootheng.in>
2013-08-05 09:32:56 -0400

    add EE_Ticket class [completed: 3296]

---
+++ includes/classes/EE_Ticket.class.php
@@ -0,0 +1,185 @@
+<?php if (!defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+do_action('AHEE_log', __FILE__, ' FILE LOADED', '' );
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package			Event Espresso
+ * @ author				Seth Shoultes
+ * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license			{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
+ * @ link					{@link http://www.eventespresso.com}
+ * @ since		 		4.0
+ *
+ * ------------------------------------------------------------------------
+ *
+ * EE_Ticket class
+ *
+ * @package			Event Espresso
+ * @subpackage		includes/classes/EE_Ticket.class.php
+ * @author			Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Ticket extends EE_Base_Class{
+
+
+	/**
+	 * Primary key for Ticket. 
+	 * @var INT
+	 */
+	protected $_TKT_ID;
+
+
+
+	/**
+	 * Foreign Key to the Ticket Template
+	 * @var INT
+	 */
+	protected $_TTM_ID;
+
+
+
+
+	/**
+	 * Foreign Key to the Ticket Event
+	 * @var INT
+	 */
+	protected $_EVT_ID;
+
+
+
+
+	/**
+	 * Date the Ticket goes on sale (internally this is stored as a UNIX timestamp)
+	 * @var string
+	 */
+	protected $_TKT_start_date;
+
+
+
+
+
+	/**
+	 * Date the Ticket sales end (internally this is stored as a UNIX timestamp)
+	 * @var string
+	 */
+	protected $_TKT_end_date;
+
+
+
+
+
+	/**
+	 * The limit of this Ticket that can be sold
+	 * @var INT
+	 */
+	protected $_TKT_reg_limit;
+
+
+
+
+
+
+	/**
+	 * How many of this ticket have been sold
+	 * @var INT
+	 */
+	protected $_TKT_sold;
+
+
+
+
+
+
+	/**
+	 * The display order for this ticket (also used for autosaves etc)
+	 * @var INT
+	 */
+	protected $_TKT_order;
+
+
+
+
+
+
+	/**
+	 * If this ticket is the child of another (i.e. revisions autosaves) then the ID corresponds to the parent
+	 * @var INT
+	 */
+	protected $_TKT_parent;
+
+
+
+
+	/** Related Object Caches **/
+
+
+	/**
+	 * Registrations this ticket is attached to
+	 * @var EE_Registration[]
+	 */
+	protected $_Registration;
+
+
+
+
+	/**
+	 * Event this ticket is attached to
+	 * @var EE_Event
+	 */
+	protected $_Event;
+
+
+
+
+
+	/**
+	 * Datetimes attached to this ticket
+	 * @var EE_Datetime[]
+	 */
+	protected $_Datetime;
+
+
+
+
+	/**
+	 * Prices attached to this ticket
+	 * @var EE_Price[]
+	 */
+	protected $_Price;
+
+
+
+
+
+	/**
+	 * The template this ticket is using
+	 * @var EE_Ticket_Template
+	 */
+	protected $_Ticket_Template;
+
+
+
+	public static function new_instance( $props_n_values = array(), $timezone = NULL ) {
+		$classname = __CLASS__;
+		$has_object = parent::_check_for_object( $props_n_values, $classname, $timezone );
+		return $has_object ? $has_object : new self( $props_n_values, FALSE, $timezone );
+	}
+
+
+	public static function new_instance_from_db ( $props_n_values = array(), $timezone = NULL ) {
+		return new self( $props_n_values, TRUE, $timezone );
+	}
+
+
+
+
+
+	public function parent() {
+		return $this->get('TKT_parent');
+	}
+
+
+} //end EE_Ticket class
