a2c70668b13a85c359cbd5ca394b3aa88fbcc756
Darren Ethier <darren@roughsmootheng.in>
2013-08-06 13:21:14 -0400

    Revert "removed Ticket_Price HABTM join table and models since its no longer needed"
    This reverts commit 716c010f894367b97b3fd1b5db0ebd701e17f4f2.

---
+++ includes/models/EEM_Ticket_Price.model.php
@@ -0,0 +1,100 @@
+<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package			Event Espresso
+ * @ author				Seth Shoultes
+ * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link				http://www.eventespresso.com
+ * @ version		 	4.0
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Ticket Price Model
+ *
+ * @package			Event Espresso
+ * @subpackage		includes/models/EEM_Ticket_Price.model.php
+ * @author			Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+
+require_once ( EE_CLASSES . 'EE_Ticket_Price.class.php' );
+require_once ( EE_MODELS . 'EEM_Base.model.php' );
+
+class EEM_Ticket_Price extends EEM_Base {
+
+	// private instance of the EEM_Ticket_Price object
+	private static $_instance = NULL;
+
+
+
+	/**
+	 * 		This funtion is a singleton method used to instantiate the EEM_Attendee object
+	 *
+	 * 		@access public
+	 * 		@param string $timezone string representing the timezone we want to set for returned Date Time Strings (and any incoming timezone data that gets saved).  Note this just sends the timezone info to the date time model field objects.  Default is NULL (and will be assumed using the set timezone in the 'timezone_string' wp option)
+	 * 		@return EEM_Ticket_Price instance
+	 */
+	public static function instance( $timezone = NULL ) {
+
+		// check if instance of EEM_Ticket_Price already exists
+		if (self::$_instance === NULL) {
+			// instantiate Price_model
+			self::$_instance = new self( $timezone );
+		}
+
+		//set timezone if we have in incoming string
+		if ( !empty( $timezone ) )
+			self::$_instance->set_timezone( $timezone );
+		
+		// EEM_Ticket_Price object
+		return self::$_instance;
+	}
+
+
+
+	/**
+	 * 		private constructor to prevent direct creation
+	 * 		@Constructor
+	 * 		@access protected
+	 * 		@param string $timezone string representing the timezone we want to set for returned Date Time Strings (and any incoming timezone data that gets saved).  Note this just sends the timezone info to the date time model field objects.  Default is NULL (and will be assumed using the set timezone in the 'timezone_string' wp option)
+	 * 		@return void
+	 */
+	protected function __construct( $timezone ) {
+		$this->singlular_item = __('Ticket Price','event_espresso');
+		$this->plural_item = __('Ticket Prices','event_espresso');		
+
+		$this->_tables = array(
+			'Ticket_Price'=>new EE_Primary_Table('esp_ticket_price','TKP_ID')
+		);
+		$this->_fields = array(
+			'Ticket_Price'=> array(
+				'TKP_ID'=>new EE_Primary_Key_Int_Field('TKP_ID', 'Ticket Price ID', false, 0),
+				'TKT_ID'=>new EE_Foreign_Key_Int_Field('TKT_ID', 'Ticket Id', false, 0, 'Ticket'),
+				'PRC_ID'=>new EE_Foreign_Key_Int_Field('PRC_ID', 'Price ID', false, 0, 'Price'),
+			)
+		);
+		$this->_model_relations = array(
+			'Ticket'=>new EE_Belongs_To_Relation('Ticket'),
+			'Price'=>new EE_Belongs_To_Relation('Price')
+		);
+		parent::__construct( $timezone );
+
+	}
+
+
+
+	/**
+	 * defines  table name as a constant
+	 * @access public
+	 */
+	public static function define_table_name() {
+		global $wpdb;
+		define( 'EE_TICKET_PRICE_TABLE', $wpdb->prefix . 'esp_ticket_price' );
+	}
+
+} //end EEM_Ticket_Price class
