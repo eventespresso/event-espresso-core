cbafd65c8301c6586d8bc924b68b8908b58a92b5
Brent Christensen <brent@eventespresso.com>
2013-08-06 11:01:58 -0700

    Update PUE slug to 'espresso-calendar-pr' [touch:3330]

--- calendar_admin.php
+++ calendar_admin.php
@@ -17,7 +17,7 @@ function espresso_calendar_load_pue_update() {
 		require(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php' );
 		$api_key = $org_options['site_license_key'];
 		$host_server_url = 'http://eventespresso.com';
-		$plugin_slug = 'espresso-calendar';
+		$plugin_slug = 'espresso-calendar-pr';
 		$options = array(
 				'apikey' => $api_key,
 				'lang_domain' => 'event_espresso',
