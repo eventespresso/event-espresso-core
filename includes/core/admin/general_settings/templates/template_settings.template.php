5566a4dc46d09eb3de3e3eff221166414740df27
Brent Christensen <brent@eventespresso.com>
2013-08-07 17:09:38 -0700

    remove event list options from general settings admin , add action for injecting content into general settings admin templates tab [touch:3314]

--- includes/core/admin/general_settings/templates/template_settings.template.php
+++ includes/core/admin/general_settings/templates/template_settings.template.php
@@ -1,60 +1,6 @@
 <div class="padding">
 
-	<!--*************************   Event Listings  ****************************-->
-
-	<h4 class="ee-admin-settings-hdr">
-		<?php _e('Event Listings', 'event_espresso'); ?>
-	</h4>
-
-	<table class="form-table">
-		<tbody>
-
-			<tr>
-				<td colspan="2">
-					<strong>
-						<?php _e('Event Listings', 'event_espresso'); ?>
-					</strong>
-				</td>
-			</tr>
-
-			<tr>
-				<th>
-					<label for="display_description_in_event_list">
-						<?php _e('Display Descriptions', 'event_espresso'); ?>
-					</label>
-				</th>
-				<td>
-					<?php echo EE_Form_Fields::select_input('display_description_in_event_list', $values, $template_settings['display_description_in_event_list'], 'id="display_description_in_event_list"'); ?>
-				</td>
-			</tr>
-
-			<tr>
-				<th>
-					<label for="display_short_description_in_event_list">
-						<?php _e('Use SHORT Descriptions', 'event_espresso'); ?>
-					</label>
-				</th>
-				<td>
-					<?php echo EE_Form_Fields::select_input('display_short_description_in_event_list', $values, $template_settings['display_short_description_in_event_list'], 'id="display_short_description_in_event_list"'); ?>
-					<p class="description">
-						<?php _e('Be sure to use the more... tag in your event description', 'event_espresso'); ?>
-					</p>
-				</td>
-			</tr>
-
-			<tr>
-				<th>
-					<label for="display_address_in_event_list">
-						<?php _e('Display Addresses', 'event_espresso'); ?>
-					</label>
-				</th>
-				<td>
-					<?php echo EE_Form_Fields::select_input('display_address_in_event_list', $values, $template_settings['display_address_in_event_list'], 'id="display_address_in_event_list"'); ?>
-				</td>
-			</tr>
-
-		</tbody>
-	</table>
+	<?php do_action( 'AHEE__general_settings_admin__template_settings__before_settings_form' ); ?>
 
 	<!--*************************   Registration Pages   ****************************-->
 
