64638e5eb283204d2f0e07869e0c4438a94e66fb
Darren Ethier <darren@roughsmootheng.in>
2013-08-22 14:19:20 -0400

    Default Base Price is "Free Admission" [completed: 3475]

--- helpers/EEH_Activation.helper.php
+++ helpers/EEH_Activation.helper.php
@@ -992,7 +992,7 @@ class EEH_Activation {
 			if ( ! $prices_exist ) {
 				$SQL = "INSERT INTO " . ESP_PRICE_TABLE . "
 							(PRC_ID, PRT_ID, EVT_ID, PRC_amount, PRC_name, PRC_desc, PRC_start_date, PRC_end_date, PRC_reg_limit, PRC_tckts_sold, PRC_is_active, PRC_overrides, PRC_order, PRC_deleted, PRC_display_order, PRC_parent ) VALUES
-							(1, 1, 0, '10.00', 'General Admission', 'Regular price for all Events. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 0, 0, 1, 0),
+							(1, 1, 0, '0.00', 'Free Admission', 'Default Price for all NEW tickets created. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 0, 0, 1, 0),
 							(2, 3, 0, '20', 'Members Discount', 'Members receive a 20% discount off of the regular price. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 10, 0, 2, 0),
 							(3, 4, 0, '10', 'Early Bird Discount', 'Sign up early and receive an additional 10% discount off of the regular price. Example content - delete if you want to', -1, 0, NULL, NULL, 1, NULL, 20, 0, 3, 0),
 							(4, 5, 0, '7.50', 'Service Fee', 'Covers administrative expenses. Example content - delete if you want to', NULL, NULL, -1, 0, 1, NULL, 30, 0, 4, 0),
