6a954a9add066b3c901abbf6368f8d52536ff927
Darren Ethier <darren@roughsmootheng.in>
2013-08-22 11:51:50 -0400

    Event Price to Base Price [touch: 3295]

--- includes/models/EEM_Price_Type.model.php
+++ includes/models/EEM_Price_Type.model.php
@@ -99,7 +99,7 @@ class EEM_Price_Type extends EEM_Soft_Delete_Base {
 			'Price_Type'=>array(
 				'PRT_ID'=>new EE_Primary_Key_Int_Field('PRT_ID', __('Price Type ID','event_espresso'), false, 0),
 				'PRT_name'=>new EE_Plain_Text_Field('PRT_name', __('Price Type Name','event_espresso'), false, ''),
-				'PBT_ID'=>new EE_Enum_Field('PBT_ID', __('Price Base type ID, 1 = Event Price , 2 = Discount , 3 = Surcharge , 4 = Tax','event_espresso'), false, 1, $this->base_types,true),
+				'PBT_ID'=>new EE_Enum_Field('PBT_ID', __('Price Base type ID, 1 = Base Price , 2 = Discount , 3 = Surcharge , 4 = Tax','event_espresso'), false, 1, $this->base_types,true),
 				'PRT_is_member'=>new EE_Boolean_Field('PRT_is_member', __('Flag indicating price is only for members','event_espresso'), false, false),
 				'PRT_is_percent'=>new EE_Boolean_Field('PRT_is_percent', __('Flag indicating price is a percentage','event_espresso'), false, false),
 				'PRT_is_global'=>new EE_Boolean_Field('PRT_is_global', __('Flag indicating price shoudl automatically be added to all events','event_espresso'), false, false),
