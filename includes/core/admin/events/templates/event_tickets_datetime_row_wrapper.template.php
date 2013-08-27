d28866ae4a7a17289d8c6fd66bf183a9d5379e17
Darren Ethier <darren@roughsmootheng.in>
2013-08-26 21:35:47 -0400

    add datetime-row-wrapper template [touch: 3303]

---
+++ includes/core/admin/events/templates/event_tickets_datetime_row_wrapper.template.php
@@ -0,0 +1,14 @@
+<div id="event-datetime-DTTNUM" class="event-datetime-row edit-dtt-row">
+	<?php echo $dtt_display_row; ?>
+	<?php echo $dtt_edit_row; ?>
+	<?php echo $dtt_attached_tickets_row; ?>
+</div>
+
+<?php
+/**
+ * template vars used in template
+ *
+ * $dtt_display_row
+ * $dtt_edit_row
+ * $dtt_attached_tickets_row
+ */
