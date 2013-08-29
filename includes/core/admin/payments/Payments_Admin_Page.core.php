43a6d5cee0a47c8b1c5dfdb0b55acbbfc66bd0ed
Michael Nelson <michael@eventespresso.com>
2013-08-29 15:51:00 -0700

    [touch:3384]{t:20}updated references to the session in the payments page

--- includes/core/admin/payments/Payments_Admin_Page.core.php
+++ includes/core/admin/payments/Payments_Admin_Page.core.php
@@ -140,7 +140,7 @@ class Payments_Admin_Page extends EE_Admin_Page {
 			$EEM_Gateways->set_active_gateways();
 		}
 
-		$gateway_data = $this->EE->EE_Session->get_session_data(FALSE, 'gateway_data');
+		$gateway_data = $this->EE->SSN->get_session_data(FALSE, 'gateway_data');
 		$gateway_instances = $EEM_Gateways->get_gateway_instances();
 		//printr( $gateway_instances, '$gateway_instances  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
 		$payment_settings = array_key_exists('payment_settings',$gateway_data) ? $gateway_data['payment_settings'] : null;
@@ -183,7 +183,7 @@ class Payments_Admin_Page extends EE_Admin_Page {
 		
 		require_once EVENT_ESPRESSO_PLUGINFULLPATH . 'helpers/EE_Tabbed_Content.helper.php' ;
 		
-		$gateway_data = $this->EE->EE_Session->get_session_data(FALSE, 'gateway_data');
+		$gateway_data = $this->EE->SSN->get_session_data(FALSE, 'gateway_data');
 		$gateway_instances = $EEM_Gateways->get_gateway_instances();
 		$payment_settings = array_key_exists('payment_settings',$gateway_data) ? $gateway_data['payment_settings'] : null;
 		/* if there are no payment settings in the session yet, add them from the DB. This fixes a bug where on first page load
@@ -191,7 +191,7 @@ class Payments_Admin_Page extends EE_Admin_Page {
 		 * To reproduce that error, clear your cookies and delete the entry for 'payment_settings' in the usermeta table */
 		if (  empty($gateway_data['payment_settings']) ){
 			$payment_settings = get_user_meta($current_user->ID, 'payment_settings', true);
-			$this->EE->EE_Session->set_session_data($payment_settings,'payment_settings');
+			$this->EE->SSN->set_session_data($payment_settings,'payment_settings');
 		}
 
 		//lets add all the metaboxes
@@ -238,7 +238,7 @@ class Payments_Admin_Page extends EE_Admin_Page {
 			
 		}
 		//bandaid to fix bug where gateways wouldn't appear active on firsrt pag eload after activating them
-		$this->EE->EE_Session->set_session_data($gateway_data,'gateway_data');
+		$this->EE->SSN->set_session_data($gateway_data,'gateway_data');
 		
 		if ( ! $selected_gateway_name ) {
 //			$default = !empty( $gateway_data['active_gateways'] ) ? key($gateway_data['active_gateways']) : 'Paypal_Standard';
