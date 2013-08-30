b6b2e69f047afbc27d51db915772021b8170f212
Darren Ethier <darren@roughsmootheng.in>
2013-08-30 14:25:50 -0400

    fix improper implementation of currency property [completed: 3523]

--- includes/models/fields/EE_Float_Field_Base.php
+++ includes/models/fields/EE_Float_Field_Base.php
@@ -17,7 +17,7 @@ abstract class EE_Float_Field_Base extends EE_Model_Field_Base{
 	 */
 	function prepare_for_pretty_echoing($value_on_field_to_be_outputted) {
 		$EE = EE_Registry::instance();
-		return number_format( $value_on_field_to_be_outputted, max(array(2, $EE->CFG->currency->dec_plc)), $EE->CFG->currency->dec_mrk, $EE->CFG->currency_thsnds) ;
+		return number_format( $value_on_field_to_be_outputted, max(array(2, $EE->CFG->currency->dec_plc)), $EE->CFG->currency->dec_mrk, $EE->CFG->currency->thsnds) ;
 	
 	}
 	
