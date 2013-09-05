1515ea9f1dbda8995f19aca60e9684d6a8234d1c
Darren Ethier <darren@roughsmootheng.in>
2013-09-04 20:24:47 -0400

    clicking update or save button on cpt page resets our listener for input field changes on the page [completed: 3563]

--- includes/core/admin/events/assets/event_editor.js
+++ includes/core/admin/events/assets/event_editor.js
@@ -19,14 +19,19 @@ jQuery(document).ready(function($) {
 				return UNSAVED_DATA_MSG.eventmsg;
 		}
 
-		if ( UNSAVED_DATA_MSG.inputChanged )
+		if ( UNSAVED_DATA_MSG.inputChanged === 1 )
 			return UNSAVED_DATA_MSG.eventmsg;
 	};
 
 	// setup our listeners for change on inputs in registered autosave containers so we know to trigger EE_INPUT_CHANGED
 	$.each(EE_AUTOSAVE_IDS, function(i, v) {
-		$('input', '#' + v).on('change', function() {
+		$('input', '#' + v).on('focus', function() {
 			UNSAVED_DATA_MSG.inputChanged = 1;
 		});
+	});
+
+	//make sure clicking the wp save draft/publish or update button resets our inputChanged var
+	$('#submitdiv').on('click', 'button', function() {
+		UNSAVED_DATA_MSG.inputChanged = 0;
 	});
 });
