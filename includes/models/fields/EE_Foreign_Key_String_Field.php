3a0d06a3067db7b58de0142d1a995c34de70fd48
Michael Nelson <michael@eventespresso.com>
2013-09-10 13:39:57 -0700

    [touch:3584]{t:20}removed htmlentities on all prepar_for_set methods on model fields. I added that when I was trying to merely centralize the code that did this type of validation (I didnt personally have a urge to use htmlentities... I was just copying code already there). Note: htmlentities is still used in preparing database values elsewhere, so it might be good to do a global search for it and evaluate its usefuless each time its being used

--- includes/models/fields/EE_Foreign_Key_String_Field.php
+++ includes/models/fields/EE_Foreign_Key_String_Field.php
@@ -10,6 +10,6 @@ class EE_Foreign_Key_String_Field extends EE_Foreign_Key_Field_Base{
 	 * @return string
 	 */
 	function prepare_for_set($value_inputted_for_field_on_model_object) {
-		return strtoupper(htmlentities(wp_strip_all_tags($value_inputted_for_field_on_model_object)));
+		return strtoupper(wp_strip_all_tags($value_inputted_for_field_on_model_object));
 	}
 }
--- includes/models/fields/EE_Full_HTML_Field.php
+++ includes/models/fields/EE_Full_HTML_Field.php
@@ -2,10 +2,10 @@
 require_once( EE_MODELS . 'fields/EE_Text_Field_Base.php' );
 class EE_Full_HTML_Field extends EE_Text_Field_Base{
 	function prepare_for_set($value_inputted_for_field_on_model_object) {
-		return htmlentities( $value_inputted_for_field_on_model_object, ENT_QUOTES, 'UTF-8' );
+		return  $value_inputted_for_field_on_model_object;
 	}
 	function prepare_for_get($value_of_field_on_model_object) {
-		return $value_of_field_on_model_object ?  html_entity_decode( $value_of_field_on_model_object, ENT_QUOTES, 'UTF-8' ) : '';
+		return $value_of_field_on_model_object ?  $value_of_field_on_model_object : '';
 	//@todo: should we be adding or stripslashing? probably only when adding to DB, if ever...
 		
 	}
