37c0d3744ca545897009c584e232417eee1751e5
Brent Christensen <brent@eventespresso.com>
2013-09-13 10:31:04 -0700

    add template for core ajax notifications [touch:3577]

---
+++ templates/espresso-ajax-notices.template.php
@@ -0,0 +1,23 @@
+	
+	<div id="espresso-ajax-loading">
+		<img src="<?php echo EE_IMAGES_URL;?>ajax-loader-grey.gif" /><span><?php _e('loading...', 'event_espresso'); ?></span>
+	</div>
+
+	<div id="espresso-ajax-notices">
+	
+		<div id="espresso-ajax-notices-success" class="espresso-ajax-notices success fade-away">
+			<a class="close-espresso-notice">&times;</a>
+			<p class="espresso-notices-msg"></p>
+		</div>
+		
+		<div id="espresso-ajax-notices-attention" class="espresso-ajax-notices attention fade-away">
+			<a class="close-espresso-notice">&times;</a>
+			<p class="espresso-notices-msg"></p>
+		</div>
+		
+		<div id="espresso-ajax-notices-error" class="espresso-ajax-notices error fade-away">
+			<a class="close-espresso-notice">&times;</a>
+			<p class="espresso-notices-msg"></p>
+		</div>
+		
+	</div>
