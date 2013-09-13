af67a497cf5b9b87e4ed9e76356b6fbf3731f74f
Brent Christensen <brent@eventespresso.com>
2013-09-13 10:24:52 -0700

    tweak inputs ofr reg form  [touch:3577]

--- includes/classes/EE_Question_Form_Input.class.php
+++ includes/classes/EE_Question_Form_Input.class.php
@@ -139,7 +139,7 @@ class EE_Question_Form_Input {
 		
 		$default_q_meta = array(
 				'att_nmbr' => 1,
-				'price_id' => '',
+				'ticket_id' => '',
 				'date' => '',
 				'time' => '',
 				'input_name' => '',
@@ -163,8 +163,9 @@ class EE_Question_Form_Input {
 	private function _set_input_name( $qstn_id ) {
 		if ( ! empty( $qstn_id )) {
 			$ANS_ID = $this->get( 'ANS_ID' );
-			$qstn_id = isset( $ANS_ID ) ? '[' . $qstn_id . '][' . $ANS_ID . ']' : '[' . $qstn_id . ']';
+			$qstn_id = ! empty( $ANS_ID ) ? '[' . $qstn_id . '][' . $ANS_ID . ']' : '[' . $qstn_id . ']';
 		}
+		
 		$this->QST_input_name = $this->_QST_meta['append_qstn_id'] && ! empty( $qstn_id ) ? $this->_QST_meta['input_prefix'] . $this->_QST_meta['input_name'] . $qstn_id : $this->_QST_meta['input_prefix'] . $this->_QST_meta['input_name'];
 	}
 	
