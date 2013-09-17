e84a586824bc7b45b689ccd7371319f2f87ad94b
Darren Ethier <darren@roughsmootheng.in>
2013-09-17 11:15:05 -0400

    move dump and get_function_name() methods outside of jQuery('ready') block so they are available in the window namespace [touch: 3622]

--- scripts/espresso_core.js
+++ scripts/espresso_core.js
@@ -1,3 +1,49 @@
+//functions available to window
+/**
+ * Function : dump()
+ * Arguments: The data - array,hash(associative array),object
+ *    The level - OPTIONAL
+ * Returns  : The textual representation of the array.
+ * This function was inspired by the print_r function of PHP.
+ * This will accept some data as the argument and return a
+ * text that will be a more readable version of the
+ * array/hash/object that is given.
+ * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+ */
+function dump(arr,level) {
+	var dumped_text = "";
+	if(!level) level = 0;
+	
+	//The padding given at the beginning of the line.
+	var level_padding = "";
+	for(var j=0;j<level+1;j++) level_padding += "    ";
+	
+	if(typeof(arr) == 'object') { //Array/Hashes/Objects
+		for(var item in arr) {
+			var value = arr[item];
+			
+			if(typeof(value) == 'object') { //If it is an array,
+				dumped_text += level_padding + "'" + item + "' ...\n";
+				dumped_text += dump(value,level+1);
+			} else {
+				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+			}
+		}
+	} else { //Stings/Chars/Numbers etc.
+		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+	}
+	return dumped_text;
+}
+
+
+function getFunctionName() {
+	var myName = arguments.callee.toString();
+	myName = myName.substr('function '.length);
+	myName = myName.substr(0, myName.indexOf('('));
+	return myName;
+}
+
+
 jQuery(document).ready(function($) {
 
 	$('.show-if-js').css({ 'display' : 'inline-block' });
@@ -74,50 +120,11 @@ jQuery(document).ready(function($) {
 		this.css({ 'top' : element_top + 'px' });
 		this.css({ 'left' : element_left + 'px' });
 		return this;
-	};	
-		
+	};
+
+
 	/**
-	 * Function : dump()
-	 * Arguments: The data - array,hash(associative array),object
-	 *    The level - OPTIONAL
-	 * Returns  : The textual representation of the array.
-	 * This function was inspired by the print_r function of PHP.
-	 * This will accept some data as the argument and return a
-	 * text that will be a more readable version of the
-	 * array/hash/object that is given.
-	 * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+	 * hook into ajax send to make sure all frontend ajax sends the 'ee_frontend_ajax' param.
 	 */
-	function dump(arr,level) {
-		var dumped_text = "";
-		if(!level) level = 0;
-		
-		//The padding given at the beginning of the line.
-		var level_padding = "";
-		for(var j=0;j<level+1;j++) level_padding += "    ";
-		
-		if(typeof(arr) == 'object') { //Array/Hashes/Objects 
-			for(var item in arr) {
-				var value = arr[item];
-				
-				if(typeof(value) == 'object') { //If it is an array,
-					dumped_text += level_padding + "'" + item + "' ...\n";
-					dumped_text += dump(value,level+1);
-				} else {
-					dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-				}
-			}
-		} else { //Stings/Chars/Numbers etc.
-			dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
-		}
-		return dumped_text;
-	}
-	
-	
-	function getFunctionName() {
-		var myName = arguments.callee.toString();
-		myName = myName.substr('function '.length);
-		myName = myName.substr(0, myName.indexOf('('));
-		return myName;
-	}	
 	
 });
