c9430f1990ce1c26f699f57081cebaaf3b864afd
Michael Nelson <michael@eventespresso.com>
2013-09-24 09:37:07 -0700

    [touch:3321]{t:120}created a model for retrieiving all the sysmte status info and putting it into arrays and strings, which can be used for either displaying the info on the page, downloading it to a file, sending it directly in an email, whatever

---
+++ includes/models/EEM_System_Status.model.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * Model for retrieving Information about the Event Espresso status.
+ */
+class EEM_System_Status{
+	
+	// private instance of the EEM_Price object
+	private static $_instance = NULL;
+
+
+
+	/**
+	 * 		This funtion is a singleton method used to instantiate the EEM_Attendee object
+	 *
+	 * 		@access public
+	 * 		@param string $timezone string representing the timezone we want to set for returned Date Time Strings (and any incoming timezone data that gets saved).  Note this just sends the timezone info to the date time model field objects.  Default is NULL (and will be assumed using the set timezone in the 'timezone_string' wp option)
+	 * 		@return EEM_System_Status
+	 */
+	public static function instance() {
+
+		// check if instance of EEM_Price already exists
+		if (self::$_instance === NULL) {
+			// instantiate Price_model
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * 
+	 * @return array where each key is a function name on this class, and each value is SOMETHING--
+	 * it might be a value, an array, or an object
+	 */
+	function get_system_stati(){
+		return array(
+			'active_plugins'=>$this->get_active_plugins(),
+			'ee_activation_history'=>$this->get_ee_activation_history(),
+			'ee_config'=>$this->get_ee_config(),
+			'ee_gateway_settings'=>$this->get_ee_gateway_settings(),
+			'ee_migration_history'=>$this->get_ee_migration_history(),
+			'ee_version'=>$this->get_ee_version(),
+			'https_enabled'=>$this->get_https_enabled(),
+			'php_info'=>$this->get_php_info(),
+			'wp_settings'=>$this->get_wp_settings()
+		);
+	}
+	/**
+	 * 
+	 * @return string
+	 */
+	function get_ee_version(){
+		return espresso_version();
+	}
+	/**
+	 * 
+	 * @return array, where each key is a plugin name (lower-cased), values are sub-arrays.
+	 * Sub-arrays like described in wp function get_plugin_data. Ie,	 * 
+	 *	'Name' => 'Plugin Name',
+		'PluginURI' => 'Plugin URI',
+		'Version' => 'Version',
+		'Description' => 'Description',
+		'Author' => 'Author',
+		'AuthorURI' => 'Author URI',
+		'TextDomain' => 'Text Domain',
+		'DomainPath' => 'Domain Path',
+		'Network' => 'Network',
+	 */
+	function get_active_plugins(){
+		$active_plugins = (array) get_option( 'active_plugins', array() );
+		if ( is_multisite() )
+			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+		$active_plugins = array_map( 'strtolower', $active_plugins );
+		$plugin_info = array();
+		foreach ( $active_plugins as $plugin ) {
+				$plugin_data = @get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
+
+				$plugin_info[ $plugin ] = $plugin_data;
+		}
+		return $plugin_info;
+	}
+	
+	/**
+	 * 
+	 * @return array with keys 'home_url' and 'site_url'
+	 */
+	function get_wp_settings(){
+		
+		return array(
+			'is_multisite'=>is_multisite(),
+			'version'=>  get_bloginfo( 'version', 'display' ),
+			'home_url'=>home_url(),
+			'site_url'=>site_url(),
+			'WP_DEBUG'=>WP_DEBUG,
+			'permalink_structure'=>get_option('permalink_structure'),
+			'theme'=>wp_get_theme(),
+			
+			);
+	}
+	
+	/**
+	 * Gets an array of information about the history of ee versions installed
+	 * @return array
+	 */
+	function get_ee_activation_history(){
+		return get_option('espresso_db_update');
+	}
+	
+	/**
+	 * Gets information about gateways
+	 * return array 
+	 */
+	function get_ee_gateway_settings(){
+		return EEM_Gateways::instance();
+	}
+	
+	/**
+	 * 
+	 * @return EE_Data_Migration_Script_Base[]
+	 */
+	function get_ee_migration_history(){
+		return EE_Data_Migration_Manager::instance()->get_data_migrations_ran();
+	}
+	
+	/**
+	 * 
+	 * @return array like EE_Config class
+	 */
+	function get_ee_config(){
+		return EE_Config::instance()->get_espresso_config();
+	}
+	
+	/**
+	 * Gets an array of php setup info, pilfered from http://www.php.net/manual/en/function.phpinfo.php#87463
+	 * @return array like the output of phpinfo(), but in an array
+	 */
+	function get_php_info(){
+		ob_start(); 
+		phpinfo(-1);
+
+		$pi = preg_replace(
+		array('#^.*<body>(.*)</body>.*$#ms', '#<h2>PHP License</h2>.*$#ms',
+		'#<h1>Configuration</h1>#',  "#\r?\n#", "#</(h1|h2|h3|tr)>#", '# +<#',
+		"#[ \t]+#", '#&nbsp;#', '#  +#', '# class=".*?"#', '%&#039;%',
+		 '#<tr>(?:.*?)" src="(?:.*?)=(.*?)" alt="PHP Logo" /></a>'
+		 .'<h1>PHP Version (.*?)</h1>(?:\n+?)</td></tr>#',
+		 '#<h1><a href="(?:.*?)\?=(.*?)">PHP Credits</a></h1>#',
+		 '#<tr>(?:.*?)" src="(?:.*?)=(.*?)"(?:.*?)Zend Engine (.*?),(?:.*?)</tr>#',
+		 "# +#", '#<tr>#', '#</tr>#'),
+		array('$1', '', '', '', '</$1>' . "\n", '<', ' ', ' ', ' ', '', ' ',
+		 '<h2>PHP Configuration</h2>'."\n".'<tr><td>PHP Version</td><td>$2</td></tr>'.
+		 "\n".'<tr><td>PHP Egg</td><td>$1</td></tr>',
+		 '<tr><td>PHP Credits Egg</td><td>$1</td></tr>',
+		 '<tr><td>Zend Engine</td><td>$2</td></tr>' . "\n" .
+		 '<tr><td>Zend Egg</td><td>$1</td></tr>', ' ', '%S%', '%E%'),
+		ob_get_clean());
+
+		$sections = explode('<h2>', strip_tags($pi, '<h2><th><td>'));
+		unset($sections[0]);
+
+		$pi = array();
+		foreach($sections as $section){
+		  $n = substr($section, 0, strpos($section, '</h2>'));
+		  preg_match_all(
+		  '#%S%(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?%E%#',
+			$section, $askapache, PREG_SET_ORDER);
+		  foreach($askapache as $m)
+			  $pi[$n][$m[1]]=(!isset($m[3])||$m[2]==$m[3])?$m[2]:array_slice($m,2);
+		}
+
+		return $pi;
+	}
+	
+	/**
+	 * Checks if site responds ot HTTPS
+	 * @return boolean
+	 */
+	function get_https_enabled(){
+		$home = str_replace("http://", "https://", home_url());
+		@$handle = fopen($home, "r");
+		if(empty($handle)){ 
+			return FALSE;
+		}
+		return TRUE;
+	}
+	
+	
+}
