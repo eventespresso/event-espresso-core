fce02468038c472f64a84e0b737ae147e91f423c
Darren Ethier <darren@roughsmootheng.in>
2013-10-01 08:50:01 -0400

    clean up query a bit [touch: 3732]

--- includes/models/EEM_Message_Template_Group.model.php
+++ includes/models/EEM_Message_Template_Group.model.php
@@ -210,7 +210,9 @@ class EEM_Message_Template_Group extends EEM_Soft_Delete_Base {
 			'MTP_is_active' => $active
 			);
 
-		return $count ? $this->count(array(array($_where), 'order_by' => array($orderby => $order), 'limit' => $limit ), 'GRP_ID', TRUE ) : $this->get_all( array( array($_where), 'order_by' => array($orderby => $order), 'limit' => $limit) );
+		$query_params = array( $_where, 'order_by' => array($orderby=>$order), 'limit' => $limit );
+
+		return $count ? $this->count($query_params, 'GRP_ID', TRUE ) : $this->get_all( $query_params );
 	}
 
 
