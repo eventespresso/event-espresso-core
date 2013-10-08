e80d40d59a87c43c74a9500cd7a5ec10f22bd45b
Darren Ethier <darren@roughsmootheng.in>
2013-10-08 16:58:12 -0400

    fix minutes not sticking when triggering date/time picker attempt to fix greyed out date times bug. (see notes below) [touch: 3790]
    Okay the bug
    1. Click datetim start date
    2. click datetime end date
    3. edit a ticket and click the ticket start time.  Whatever date is there gets switched to the datetime end date but the TIME stays correct.
    
    If you go this way:
    1. reload page.
    2. click new datetime field for start date.
    3. click new datetime field for end date.
    4. click start date for a ticket.
    5. this time the DATE will stay correct but the time changes.
    
    however....
    1. reload page
    2. click anything BUT the start date for the datetime
    3. click another datetime field
    4. click the start date of a ticket.
    5. everything works as expected and the bug cannot be reproduced.
    
    The only way I was able to *fix* this was to comment out the setting of min/max values on the datetimepicker() method (via our object).  So for now, thats the fix I'm rolling with and we'll have to revisit on a later date.

--- includes/core/admin/assets/ee-datepicker.js
+++ includes/core/admin/assets/ee-datepicker.js
@@ -14,6 +14,7 @@ var dttPickerHelper = {
 		numberOfMonths: 2,
 		hour: null,
 		minute: null,
+		defaultDate: null,
 		showOn:'focus'
 	},
 
@@ -45,48 +46,47 @@ var dttPickerHelper = {
 
 
 	picker: function(start, end, next, doingstart) {
-
-		var dothis;
-
 		if ( typeof(doingstart) === 'undefined' ) doingstart = true;
 
 		this.startobj = start;
 		this.endobj = end;
+		var dothis = doingstart ? this.startobj : this.endobj;
+
 		this.nextobj = next;
 
 		this.startDate = this.startobj.val() === '' ? moment() : moment(this.startobj.val(), 'YYYY-MM-DD h:mm a');
 		this.endDate = this.endobj.val() === '' ? this.startDate.clone().add('hours', 1) : moment(this.endobj.val(), 'YYYY-MM-DD h:mm a');
 
 		this.dttOptions.hour = doingstart ? this.startDate.hours() : this.endDate.hours();
-		this.dttOptions.minutes = doingstart ? this.startDate.minutes() : this.endDate.minutes();
-
+		this.dttOptions.minute = doingstart ? this.startDate.minutes() : this.endDate.minutes();
+		this.dttOptions.defaultDate = doingstart ? this.startDate.toDate() : this.endDate.toDate();
 
 		//set min and max if necessary
-		if ( !doingstart ) {
+		/*if ( !doingstart ) {
 			var minDate = this.startDate;
 			this.dttOptions.minDate = this.dttOptions.minDate === null ? minDate.toDate() : this.dttOptions.minDate;
 			this.dttOptions.maxDate = this.dttOptions.maxDate === null ? minDate.clone().add('years', 100).toDate() : this.dttOptions.maxDate;
 		}/**/
 
+
 		this.dttOptions.onSelect = function(dateText, inst) {
 			//get diff from original start date
-			
 		};
 
-		this.dttOptions.onClose = function(dateText, inst) {
+		this.dttOptions.onClose = function(dateText, dpinst) {
 				var newDate = moment( dateText, 'YYYY-MM-DD h:mm a'),
-					lastVal = moment(inst.lastVal, 'YYYY-MM-DD h:mm a'),
+					lastVal = moment(dpinst.lastVal, 'YYYY-MM-DD h:mm a'),
 					diff = lastVal !== null ? lastVal.diff(newDate, 'minutes') : newDate;
-
 				if ( doingstart ) {
 					dttPickerHelper.startDate = newDate;
 					dttPickerHelper.endobj.val(dttPickerHelper.endDate.format('YYYY-MM-DD h:mm a'));
-					dttPickerHelper.nextobj.focus();
+					//dttPickerHelper.nextobj.focus();
 				} else {
 					dttPickerHelper.endDate = newDate;
 					dttPickerHelper.startobj.val(dttPickerHelper.startDate.format('YYYY-MM-DD h:mm a'));
-					dttPickerHelper.nextobj.focus();
+					//dttPickerHelper.nextobj.focus();
 				}
+
 
 				if ( dttPickerHelper.startDate.isAfter(dttPickerHelper.endDate) ) {
 					if ( doingstart )
@@ -95,21 +95,40 @@ var dttPickerHelper = {
 					else
 						dttPickerHelper.startobj.val(dttPickerHelper.startDate.clone().subtract('minutes', diff).format('YYYY-MM-DD h:mm a') );
 				}
-
-				
-				dttPickerHelper.reset();
-				return dttPickerHelper;
+				dttPickerHelper.resetpicker();
+				return false;
 			};
-
-		dothis = doingstart ? this.startobj : this.endobj;
-
 		dothis.datetimepicker(this.dttOptions);
 	},
 
 
-	reset: function() {
+	resetpicker: function() {
+		this.startobj = {};
+		this.endobj = {};
+		this.nextobj = {};
+		this.startDate = {};
+		this.endDate = {};
+		this.dttOptions = {
+			dateFormat : 'yy-mm-dd',
+			timeFormat: 'h:mm tt',
+			ampm: true,
+			separator: '  ',
+			stepHour: 1,
+			stepMinute: 5,
+			hourGrid: 2,
+			minuteGrid: 5,
+			minDate: null,
+			maxDate: null,
+			numberOfMonths: 2,
+			hour: null,
+			minute: null,
+			defaultDate: null,
+			showOn:'focus'
+		};
 		this.dttOptions.minDate = null;
 		this.dttOptions.maxDate = null;
+		this.dttOptions.defaultDate = null;
+		return this;
 	}
 
 };
