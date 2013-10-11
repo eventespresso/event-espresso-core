d2e35d2b2f7fe495ab4ecbf9a9079086eef9bf79
Darren Ethier <darren@roughsmootheng.in>
2013-10-10 23:03:38 -0400

    removed a console.log [touch: 3751]

--- includes/core/admin/assets/ee-cpt-wp-injects.js
+++ includes/core/admin/assets/ee-cpt-wp-injects.js
@@ -2,7 +2,6 @@
  * injecting ee_admin_ajax into every ajaxSend call (cause wp can send some calls and we want to catch any of our filters!.
  */
 jQuery(document).ajaxSend( function( e,x,a ) {
-	console.log(a.data);
 	s = 'get-comments';
 	if ( typeof a.data !== 'undefined' && a.data.indexOf(s) > -1 ) {
 		a.data += '&' + jQuery.param( {ee_admin_ajax : true} );
