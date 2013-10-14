569f22e5df011774a45bba94ec490a7f86fd8021
Darren Ethier <darren@roughsmootheng.in>
2013-10-13 22:15:11 -0400

    modify the re-help-tour js so that we test the provided callback function strings in the options array and make sure that they are for existing functions.  If they aren't then we set to $.noop.  This allows us to do complex help tours down the road where there may be js doing some special stuff during the tour and we just have to define our callbacks before ee-help-tour.js loads [touch: 3341]

--- includes/core/admin/assets/ee-help-tour.js
+++ includes/core/admin/assets/ee-help-tour.js
@@ -1,6 +1,22 @@
 jQuery(document).ready(function($) {
 	//if we've got an EE_HELP_TOUR object then we can loop through it to get the stuff needed for the joyride and kick it off.
-	$.each( EE_HELP_TOUR, function(i,v) {
-		$('#' + v.id).joyride(v.options);
+	$(window).load(function() {
+		$.each( EE_HELP_TOUR, function(i,v) {
+			/**
+			 * verify callbacks before sending to joyride.
+			 */
+			if ( typeof v[0].options.postExposeCallback !== 'undefined' && typeof window[v[0].options.postExposeCallback] !== 'function' )
+				v[0].options.postExposeCallback = $.noop;
+			if ( typeof v[0].options.preRideCallback !== 'undefined' && typeof window[v[0].options.preRideCallback] !== 'function' )
+				v[0].options.preRideCallback = $.noop;
+			if ( typeof v[0].options.postRideCallback !== 'undefined' && typeof window[v[0].options.postRideCallback] !== 'function' )
+				v[0].options.postRideCallback = $.noop;
+			if ( typeof v[0].options.preStepCallback !== 'undefined' && typeof window[v[0].options.preStepCallback] !== 'function' )
+				v[0].options.preStepCallback = $.noop;
+			if ( typeof v[0].options.postStepCallback !== 'undefined' && typeof window[v[0].options.postStepCallback] !== 'function' )
+				v[0].options.postStepCallback = $.noop;
+
+			$('#' + v[0].id).joyride(v[0].options);
+		});
 	});
 });
