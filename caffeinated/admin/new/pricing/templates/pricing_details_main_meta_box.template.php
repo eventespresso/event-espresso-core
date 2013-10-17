4a6e39f806951c68b8b12e70072c2ba9926b4722
Darren Ethier <darren@roughsmootheng.in>
2013-10-17 10:02:25 -0400

    fix bug for when adding a new price (doesn't have a related price_type object yet) [touch: 3831]

--- caffeinated/admin/new/pricing/templates/pricing_details_main_meta_box.template.php
+++ caffeinated/admin/new/pricing/templates/pricing_details_main_meta_box.template.php
@@ -5,7 +5,7 @@
 			<tr valign="top">
 				<th><label for="PRT_ID"><?php _e('Type', 'event_espresso'); ?></label></th>
 				<td>
-					<?php if ( $price->type_obj()->base_type() === 1 ) : ?>
+					<?php if ( $price->type_obj() && $price->type_obj()->base_type() === 1 ) : ?>
 						<input type="hidden" name="PRT_ID" id="PRT_ID" value="<?php echo $price->type(); ?>" />
 						<p><strong><?php _e('Base Price', 'event_espresso'); ?></strong></p>
 						<p class="description"><?php _e('This is the default base price. Every new ticket created will start off with this base price.', 'event_espresso'); ?></p>
