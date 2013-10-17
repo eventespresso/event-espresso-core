3eaa045f603fab5f8ec495fbec0d3349747337c1
Darren Ethier <darren@roughsmootheng.in>
2013-10-17 10:01:55 -0400

    fix labels for price types [touch: 3831]

--- caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
+++ caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
@@ -741,7 +741,7 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 	*		@return void
 	*/
 	protected function _price_types_overview_list_table() {
-		$this->_admin_page_title .= $this->get_action_link_or_button('add_new_price_type', 'add', array(), 'button add-new-h2');
+		$this->_admin_page_title .= $this->get_action_link_or_button('add_new_price_type', 'add_type', array(), 'button add-new-h2');
 		$this->admin_page_title .= $this->_learn_more_about_pricing_link();
 		$this->_search_btn_label = __('Price Types', 'event_espresso');
 		$this->display_admin_list_table_page_with_no_sidebar();
@@ -875,7 +875,7 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 	*		@return void
 	*/
 	protected function _price_type_details_meta_boxes() {		
-		add_meta_box( 'edit-price-details-mbox', __( 'Price Details', 'event_espresso' ), array( $this, '_edit_price_type_details_meta_box' ), $this->wp_page_slug, 'normal', 'high' );		
+		add_meta_box( 'edit-price-details-mbox', __( 'Price Type Details', 'event_espresso' ), array( $this, '_edit_price_type_details_meta_box' ), $this->wp_page_slug, 'normal', 'high' );		
 	}
 
 
