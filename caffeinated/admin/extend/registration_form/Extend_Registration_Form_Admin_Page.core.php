57e392eed618f25307c8b8b7a9c45689a6fc5678
Darren Ethier <darren@roughsmootheng.in>
2013-10-21 15:36:04 -0400

    make sure that the title is correct for questions and question groups admin pages (both caf and decaf) [touch: 3833]

--- caffeinated/admin/extend/registration_form/Extend_Registration_Form_Admin_Page.core.php
+++ caffeinated/admin/extend/registration_form/Extend_Registration_Form_Admin_Page.core.php
@@ -351,16 +351,16 @@ class Extend_Registration_Form_Admin_Page extends Registration_Form_Admin_Page {
 
 
 	protected function _questions_overview_list_table() {
-		$this->_admin_page_title .= $this->get_action_link_or_button('add_question', 'add_question', array(), 'button add-new-h2');
 		$this->_search_btn_label = __('Questions', 'event_espresso');
+		$this->_admin_page_title .= $this->_search_btn_label . $this->get_action_link_or_button('add_question', 'add_question', array(), 'button add-new-h2');
 		$this->display_admin_list_table_page_with_sidebar();
 	}
 
 
 
 	protected function _question_groups_overview_list_table() {
-		$this->_admin_page_title .= $this->get_action_link_or_button('add_question_group', 'add_question_group', array(), 'button add-new-h2');
 		$this->_search_btn_label = __('Question Groups', 'event_espressp');
+		$this->_admin_page_title .= $this->_search_btn_label . $this->get_action_link_or_button('add_question_group', 'add_question_group', array(), 'button add-new-h2');
 		$this->display_admin_list_table_page_with_sidebar();
 	}
 
