a1957145d225332ac6515a507022a86d943beacc
Darren Ethier <darren@roughsmootheng.in>
2013-10-21 14:34:54 -0400

    add in help tour for registration reports on the registrations admin page (caf) [touch: 3834]

---
+++ includes/core/admin/registrations/help_tours/Registration_Reports_Help_Tour.class.php
@@ -0,0 +1,90 @@
+<?php
+if (!defined('EVENT_ESPRESSO_VERSION') )
+	exit('NO direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for Wordpress
+ *
+ * @package		Event Espresso
+ * @author		Seth Shoultes
+ * @copyright	(c)2009-2012 Event Espresso All Rights Reserved.
+ * @license		http://eventespresso.com/support/terms-conditions/  ** see Plugin Licensing **
+ * @link		http://www.eventespresso.com
+ * @version		4.0
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Registration_Reports_Help_Tour
+ *
+ * This is the help tour object for the Registration Reports page
+ *
+ *
+ * @package		Registration_Reports_Help_Tour
+ * @subpackage	includes/core/admin/registration/help_tours/Registration_Reports_Help_Tour.class.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class Registration_Reports_Help_Tour extends EE_Help_Tour {
+
+	protected function _set_tour_properties() {
+		$this->_label = __('Reg Reports Tour', 'event_espresso');
+		$this->_slug = 'registration-reports-joyride';
+	}
+
+
+	protected function _set_tour_stops() {
+		$this->_stops = array(
+			10 => array(
+				'content' => $this->_start(),
+				),
+			20 => array(
+				'id' => 'reg-admin-registrations-per-day-report-dv',
+				'content' => $this->_reg_per_day_report_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentY' => -40
+					)
+				),
+			30 => array(
+				'id' => 'reg-admin-registrations-per-event-report-dv',
+				'content' => $this->_reg_per_event_report_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentY' => -40
+					)
+				),
+			40 => array(
+				'id' => 'contextual-help-link',
+				'content' => $this->_end(),
+				'button_text' => __('End Tour', 'event_espresso'),
+				'options' => array(
+					'tipLocation' => 'left',
+					'tipAdjustmentY' => -20,
+					'tipAdjustmentX' => 10
+					)
+				)
+			);
+	}
+
+
+	protected function _start() {
+		$content = '<h3>' . __('Welcome to the Registration Reports Page!', 'event_espresso') . '</h3>';
+		$content .= '<p>' . __('An introduction...', 'event_espresso') . '</p>';
+		return $content;
+	}
+
+	protected function _reg_per_day_report_stop() {
+		return '<p>' . __('about the reg per day report', 'event_espresso') . '</p>';
+	}
+
+	protected function _reg_per_event_report_stop() {
+		return '<p>' . __('about the reg_per_event_report', 'event_espresso') . '</p>';
+	}
+
+	protected function _end() {
+		return '<p>' . __('That\'s it for this tour!  At any time you can restart this tour by clicking on this help dropdown and then clicking the "Reg Reports Tour" Tour button.  All the best with your events!', 'event_espresso') . '</p>';
+	}
+}
