1fbe111a8f6a1af76e939136acc1a604c3502ded
Michael Nelson <michael@eventespresso.com>
2013-10-23 09:15:51 -0700

    [touch:3371]{t:30}got started on converting attendees

--- includes/core/data_migration_scripts/4_1_0P_stages/EE_DMS_4_1_0P_attendees.dmsstage.php
+++ includes/core/data_migration_scripts/4_1_0P_stages/EE_DMS_4_1_0P_attendees.dmsstage.php
@@ -1,30 +1,258 @@
 <?php 
 /**
  * migrates 3.1 attendee rows into 4.1 registrations, attendees, transactions, and payments
+ * 
+ * 3.1 Attendee table definition:
+ * delimiter $$
+
+CREATE TABLE `wp_events_detail` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `event_code` varchar(26) DEFAULT '0',
+  `event_name` varchar(100) DEFAULT NULL,
+  `event_desc` text,
+  `display_desc` varchar(1) DEFAULT 'Y',
+  `display_reg_form` varchar(1) DEFAULT 'Y',
+  `event_identifier` varchar(75) DEFAULT NULL,
+  `start_date` varchar(15) DEFAULT NULL,
+  `end_date` varchar(15) DEFAULT NULL,
+  `registration_start` varchar(15) DEFAULT NULL,
+  `registration_end` varchar(15) DEFAULT NULL,
+  `registration_startT` varchar(15) DEFAULT NULL,
+  `registration_endT` varchar(15) DEFAULT NULL,
+  `visible_on` varchar(15) DEFAULT NULL,
+  `address` text,
+  `address2` text,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `zip` varchar(11) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `venue_title` varchar(250) DEFAULT NULL,
+  `venue_url` varchar(250) DEFAULT NULL,
+  `venue_image` text,
+  `venue_phone` varchar(15) DEFAULT NULL,
+  `virtual_url` varchar(250) DEFAULT NULL,
+  `virtual_phone` varchar(15) DEFAULT NULL,
+  `reg_limit` varchar(25) DEFAULT '999999',
+  `allow_multiple` varchar(15) DEFAULT 'N',
+  `additional_limit` int(10) DEFAULT '5',
+  `send_mail` varchar(2) DEFAULT 'Y',
+  `is_active` varchar(1) DEFAULT 'Y',
+  `event_status` varchar(2) DEFAULT 'A',
+  `conf_mail` text,
+  `use_coupon_code` varchar(1) DEFAULT 'N',
+  `use_groupon_code` varchar(1) DEFAULT 'N',
+  `category_id` text,
+  `coupon_id` text,
+  `tax_percentage` float DEFAULT NULL,
+  `tax_mode` int(11) DEFAULT NULL,
+  `member_only` varchar(1) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `post_type` varchar(50) DEFAULT NULL,
+  `country` varchar(200) DEFAULT NULL,
+  `externalURL` varchar(255) DEFAULT NULL,
+  `early_disc` varchar(10) DEFAULT NULL,
+  `early_disc_date` varchar(15) DEFAULT NULL,
+  `early_disc_percentage` varchar(1) DEFAULT 'N',
+  `question_groups` longtext,
+  `item_groups` longtext,
+  `event_type` varchar(250) DEFAULT NULL,
+  `allow_overflow` varchar(1) DEFAULT 'N',
+  `overflow_event_id` int(10) DEFAULT '0',
+  `recurrence_id` int(11) DEFAULT '0',
+  `email_id` int(11) DEFAULT '0',
+  `alt_email` text,
+  `event_meta` longtext,
+  `wp_user` int(22) DEFAULT '1',
+  `require_pre_approval` int(11) DEFAULT '0',
+  `timezone_string` varchar(250) DEFAULT NULL,
+  `likes` int(22) DEFAULT NULL,
+  `ticket_id` int(22) DEFAULT '0',
+  `submitted` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `event_code` (`event_code`),
+  KEY `wp_user` (`wp_user`),
+  KEY `event_name` (`event_name`),
+  KEY `city` (`city`),
+  KEY `state` (`state`),
+  KEY `start_date` (`start_date`),
+  KEY `end_date` (`end_date`),
+  KEY `registration_start` (`registration_start`),
+  KEY `registration_end` (`registration_end`),
+  KEY `reg_limit` (`reg_limit`),
+  KEY `event_status` (`event_status`),
+  KEY `recurrence_id` (`recurrence_id`),
+  KEY `submitted` (`submitted`),
+  KEY `likes` (`likes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+
+ * 4.1 Attendee tables and fields:
+ * $this->_tables = array(
+			'Attendee_CPT'=> new EE_Primary_Table('posts', 'ID'),
+			'Attendee_Meta'=>new EE_Secondary_Table('esp_attendee_meta', 'ATTM_ID', 'ATT_ID')
+		);
+		$this->_fields = array(
+			'Attendee_CPT'=>array(
+				'ATT_ID'=>new EE_Primary_Key_Int_Field('ID', __("Attendee ID", "event_espresso")),
+				'ATT_full_name'=>new EE_Plain_Text_Field('post_title', __("Attendee Full Name", "event_espresso"), false, __("Unknown", "event_espresso")),
+				'ATT_bio'=>new EE_Simple_HTML_Field('post_content', __("Attendee Biography", "event_espresso"), false, __("No Biography Provided", "event_espresso")),
+				'ATT_slug'=>new EE_Slug_Field('post_name', __("Attendee URL Slug", "event_espresso"), false),
+				'ATT_created'=>new EE_Datetime_Field('post_date', __("Time Attendee Created", "event_espresso"), false, current_time('timestamp')),
+				'ATT_short_bio'=>new EE_Simple_HTML_Field('post_excerpt', __("Attendee Short Biography", "event_espresso"), true, __("No Biography Provided", "event_espresso")),
+				'ATT_modified'=>new EE_Datetime_Field('post_modified', __("Time Attendee Last Modified", "event_espresso"), true, current_time('timestamp')),
+				'ATT_author'=>new EE_Integer_Field('post_author', __("WP User that Created Attendee", "event_espresso"), false,0),
+				'ATT_parent'=>new EE_DB_Only_Int_Field('post_parent', __("Parent Attendee (unused)", "event_espresso"), true),
+				'post_type'=>new EE_DB_Only_Text_Field('post_type', __("Post Type of Attendee", "event_espresso"), false,'espresso_attendees'),
+				'status' => new EE_WP_Post_Status_Field('post_status', __('Attendee Status', 'event_espresso'), false, 'publish')
+			),
+			'Attendee_Meta'=>array(
+				'ATTM_ID'=> new EE_DB_Only_Int_Field('ATTM_ID', __('Attendee Meta Row ID','event_espresso'), false),
+				'ATT_ID_fk'=>new EE_DB_Only_Int_Field('ATT_ID', __("Foreign Key to Attendee in Post Table", "event_espresso"), false),
+				'ATT_fname'=>new EE_Plain_Text_Field('ATT_fname', __('First Name','event_espresso'), true, ''),
+				'ATT_lname'=>new EE_Plain_Text_Field('ATT_lname', __('Last Name','event_espresso'), true, ''),
+				'ATT_address'=>new EE_Plain_Text_Field('ATT_address', __('Address Part 1','event_espresso'), true, ''),
+				'ATT_address2'=>new EE_Plain_Text_Field('ATT_address2', __('Address Part 2','event_espresso'), true, ''),
+				'ATT_city'=>new EE_Plain_Text_Field('ATT_city', __('City','event_espresso'), true, ''),
+				'STA_ID'=>new EE_Foreign_Key_Int_Field('STA_ID', __('State','event_espresso'), true,0,'State'),
+				'CNT_ISO'=>new EE_Foreign_Key_String_Field('CNT_ISO', __('Country','event_espresso'), true,'','Country'),
+				'ATT_zip'=>new EE_Plain_Text_Field('ATT_zip', __('ZIP/Postal Code','event_espresso'), true, ''),
+				'ATT_email'=>new EE_Email_Field('ATT_email', __('Email Address','event_espresso'), true, ''),
+				'ATT_phone'=>new EE_Plain_Text_Field('ATT_phone', __('Phone','event_espresso'), true, ''),
+				'ATT_social'=>new EE_Serialized_Text_Field('ATT_social', __("Social Information", "event_espresso"), true, null),
+				'ATT_comments'=>new EE_Simple_HTML_Field('ATT_comments', __("Comments by Attendee", "event_espresso"), false,''),
+				'ATT_notes'=>new EE_Simple_HTML_Field('ATT_notes', __('Admin Notes','event_espresso'), true, ''),
+			));
+ * 
+ * 4.1 Registration tables and models:
+ * $this->_tables = array(
+			'Registration'=>new EE_Primary_Table('esp_registration','REG_ID')
+		);
+		$this->_fields = array(
+			'Registration'=>array(
+				'REG_ID'=>new EE_Primary_Key_Int_Field('REG_ID', __('Registration ID','event_espresso')),
+				'EVT_ID'=>new EE_Foreign_Key_Int_Field('EVT_ID', __('Even tID','event_espresso'), false, 0, 'Event'),
+				'ATT_ID'=>new EE_Foreign_Key_Int_Field('ATT_ID', __('Attendee ID','event_espresso'), false, 0, 'Attendee'),
+				'TXN_ID'=>new EE_Foreign_Key_Int_Field('TXN_ID', __('Transaction ID','event_espresso'), false, 0, 'Transaction'),
+				'TKT_ID'=>new EE_Foreign_Key_Int_Field('TKT_ID', __('Ticket ID','event_espresso'), false, 0, 'Ticket'),
+				'STS_ID'=>new EE_Foreign_Key_String_Field('STS_ID', __('Status ID','event_espresso'), false, EEM_Registration::status_id_not_approved, 'Status'),
+				'REG_date'=>new EE_Datetime_Field('REG_date', __('Time registration occured','event_espresso'), false, current_time('timestamp'), $timezone ),
+				'REG_final_price'=>new EE_Money_Field('REG_final_price', __('Final Price of registration','event_espresso'), false, 0),
+				'REG_session'=>new EE_Plain_Text_Field('REG_session', __('Session ID of registration','event_espresso'), false, ''),
+				'REG_code'=>new EE_Plain_Text_Field('REG_code', __('Unique Code for this registration','event_espresso'), false, ''),
+				'REG_url_link'=>new EE_Plain_Text_Field('REG_url_link', __('String to be used in URL for identifying registration','event_espresso'), false, ''),
+				'REG_count'=>new EE_Integer_Field('REG_count', __('Count of this registration in the group registraion ','event_espresso'), true, 1),
+				'REG_group_size'=>new EE_Integer_Field('REG_group_size', __('Number of registrations on this group','event_espresso'), false, 1),
+				'REG_att_is_going'=>new EE_Boolean_Field('REG_att_is_going', __('Flag indicating the registrant plans on attending','event_espresso'), false, false),	
+			)
+		);
+ * 
+ * 4.1 Transaction tables and models:
+ * $this->_tables = array(
+			'Transaction'=>new EE_Primary_Table('esp_transaction','TXN_ID')
+		);
+		$this->_fields = array(
+			'Transaction'=>array(
+				'TXN_ID'=>new EE_Primary_Key_Int_Field('TXN_ID', __('Transaction ID','event_espresso')),
+				'TXN_timestamp'=>new EE_Datetime_Field('TXN_timestamp', __('date when transaction was created','event_espresso'), false, current_time('timestamp'), $timezone ),
+				'TXN_total'=>new EE_Money_Field('TXN_total', __('Total value of Transaction','event_espresso'), false, 0),
+				'TXN_paid'=>new EE_Money_Field('TXN_paid', __('Amount paid towards transaction to date','event_espresso'), false, 0),
+				'STS_ID'=>new EE_Foreign_Key_String_Field('STS_ID', __('Status ID','event_espresso'), false, EEM_Transaction::incomplete_status_code, 'Status'),
+				'TXN_details'=>new EE_Serialized_Text_Field('TXN_details', __('Serialized Mess of details about the last payment on this transaction','event_espresso'), true, ''),
+				'TXN_tax_data'=>new EE_Serialized_Text_Field('TXN_tax_data', __('Serialized mess of tax data','event_espresso'), true, ''),
+				'TXN_session_data'=>new EE_Serialized_Text_Field('TXN_session_data', __('Serialized mess of session data','event_espresso'), true, ''),
+				'TXN_hash_salt'=>new EE_Plain_Text_Field('TXN_hash_salt', __('Transaction Hash Salt','event_espresso'), true, '')
+			)
+		);
+ * 
+ * 4.1 Payment tables and models:
+ * $this->_tables = array(
+			'Payment'=>new EE_Primary_Table('esp_payment','PAY_ID')
+		);
+		$this->_fields = array(
+			'Payment'=>array(
+				'PAY_ID'=>new EE_Primary_Key_Int_Field('PAY_ID', __('Payment ID','event_espresso')),
+				'TXN_ID'=>new EE_Foreign_Key_Int_Field('TXN_ID', __('Transaction ID','event_espresso'), false, 0, 'Transaction'),
+				'STS_ID'=>new EE_Foreign_Key_String_Field('STS_ID', __('STatus ID','event_espresso'), false, EEM_Payment::status_id_cancelled, 'Status'),
+				'PAY_timestamp'=> new EE_Datetime_Field('PAY_timestamp', __('Timestamp of when payment was attemped','event_espresso'), false, current_time('timestamp'), $timezone ),
+				'PAY_method'=>new EE_All_Caps_Text_Field('PAY_method', __('User-friendly description of payment','event_espresso'), false, 'CART'),
+				'PAY_amount'=>new EE_Money_Field('PAY_amount', __('Amount Payment should be for','event_espresso'), false, 0),
+				'PAY_gateway'=>new EE_Plain_Text_Field('PAY_gateway', __('Gateway name used for payment','event_espresso'), false, __('Unspecified','event_espresso')),
+				'PAY_gateway_response'=>new EE_Plain_Text_Field('PAY_gateway_response', __('Response from Gateway about the payment','event_espresso'), false, ''),
+				'PAY_txn_id_chq_nmbr'=>new EE_Plain_Text_Field('PAY_txn_id_chq_nmbr', __('Transaction ID or Cheque Number','event_espresso'), true, ''),
+				'PAY_po_number'=>new EE_Plain_Text_Field('PAY_po_number', __('Purchase or Sales Number','event_espresso'), true, ''),
+				'PAY_extra_accntng'=>new EE_Simple_HTML_Field('PAY_extra_accntng', __('Extra Account Info','event_espresso'), true, ''),
+				'PAY_via_admin'=>new EE_Boolean_Field('PAY_via_admin', __('Whehter payment made via admin','event_espresso'), false, false),
+				'PAY_details'=>new EE_Serialized_Text_Field('PAY_details', __('Full Gateway response about payment','event_espresso'), true, '')
+			)
+		);
  */
-class EE_DMS_4_1_0P_attendees extends EE_Data_Migration_Script_Stage{
+class EE_DMS_4_1_0P_attendees extends EE_Data_Migration_Script_Stage_Table{
 	function __construct() {
+		global $wpdb;
 		$this->_pretty_name = __("Attendees", "event_espresso");
+		$this->_old_table = $wpdb->prefix."events_attendee";
+		$this->_new_attendee_cpt_table = $wpdb->posts;
+		$this->_new_attendee_meta_table = $wpdb->prefix."esp_attendee_meta";
+		$this->_new_reg_table = $wpdb->prefix."esp_registration";
+		$this->_new_transaction_table = $wpdb->prefix."esp_transaction";
+		$this->_new_payment_table = $wpdb->prefix."esp_payment";
 		parent::__construct();
 	}
-	function _count_records_to_migrate() {
-		global $wpdb;
-		$count = $wpdb->get_var("SELECT COUNT(*) FROM ".$wpdb->prefix."events_attendee");
-		return intval($count);
+	
+	protected function _migrate_old_row($old_row) {
+//		$new_att_id = 
 	}
-	protected function _migration_step($num_items_to_migrate = 50) {
+	
+	private function _insert_new_attendee_cpt($old_attendee){
+		
 		global $wpdb;
-		$attendees = $wpdb->get_results($wpdb->prepare("SELECT * FROM ".$wpdb->prefix."events_attendee LIMIT %d,%d",$this->count_records_migrated(),$num_items_to_migrate));
-		if($attendees){
-			foreach($attendees as $attendee_row){
-				//insert new 4.1 Attendee object using $wpdb
-				
-			}
-			$this->set_status(EE_Data_Migration_Manager::status_continue);
-		}else{
-			$this->set_status(EE_Data_Migration_Manager::status_completed);
+		$cols_n_values = array(
+			'post_title'=>$old_attendee['fname']." ".$old_attendee['lname'],//ATT_full_name
+			'post_content'=>'',//ATT_bio
+			'post_name'=>sanitize_title($old_attendee['fname']."-".$old_attendee['lname']),//ATT_slug
+			'post_date'=>$old_attendee['date'],//ATT_created
+			'post_excerpt'=>'',//ATT_short_bio
+			'post_modified'=>$old_attendee['date'],//ATT_modified
+			'post_author'=>0,//ATT_author
+			'post_parent'=>0,//ATT_parent
+			'post_type'=>'espresso_attendees',//post_type
+			'post_status'=>'publish'//status
+		);
+		$datatypes = array(
+			'%s',//ATT_full_name
+			'%s',//ATT_bio
+			'%s',//ATT_slug
+			'%s',//ATT_created
+			'%s',//ATT_short_bio
+			'%s',//ATT_modified
+			'%d',//ATT_author
+			'%d',//ATT_parent
+			'%s',//post_type
+			'%s',//status
+		);
+		$success = $wpdb->insert($this->_new_price_table,$cols_n_values,$datatypes);
+		if ( ! $success){
+			$this->add_error($this->get_migration_script()->_create_error_message_for_db_insertion($this->_old_table, $old_price, $this->_new_price_table, $cols_n_values, $datatypes));
+			return 0;
 		}
-		return count($attendees);
+		$new_id = $wpdb->insert_id;
+		return $new_id;
+	}
+	
+	private function _insert_attendee_meta_row($old_attendee){
 		
 	}
+	
+	private function _insert_new_transaction($old_attendee){
+		
+	}
+	
+	private function _insert_new_registration($old_attendee){
+		
+	}
+	private function _insert_new_payment($old_attendee){
+		
+	}
+	
 }
