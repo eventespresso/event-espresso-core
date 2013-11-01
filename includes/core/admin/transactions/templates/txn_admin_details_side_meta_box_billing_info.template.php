edd5c25468da5578385a0a3f707f8deed73b4f39
Darren Ethier <darren@roughsmootheng.in>
2013-10-31 23:50:31 -0400

    modify billing info metabox for txn details page in admin [touch: 3887]

--- includes/core/admin/transactions/templates/txn_admin_details_side_meta_box_billing_info.template.php
+++ includes/core/admin/transactions/templates/txn_admin_details_side_meta_box_billing_info.template.php
@@ -1,51 +1,19 @@
 	<div id="admin-side-mbox-billing-info-dv" class="admin-side-mbox-dv">
-<?php if ( $free_event ) : ?>
+<?php if ( empty($billing_info) ) : ?>
 		<div class="clearfix">
-			<?php _e( 'Free Event', 'event_espresso' );?><br/>
-			- <?php echo $free_event;?>			
+			<?php _e( 'There is no billing info for this transaction.', 'event_espresso' );?><br/>
 		</div>
-<?php else : ?>	
-	
+<?php else : 
+		foreach ( $billing_info as $item => $props ) :
+			$value = $props['sanitize'] == 'ccard' ? substr( $props['value'], 0, 4 ) . 'XXXX XXXX ' . substr( $props['value'], -4 ) : $props['value'];
+			$value = $props['sanitize'] == 'ccv' ? 'XXX' : $props['value'];
+			?>
+
 		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $fname['label'];?></span><?php echo $fname['value'];?>
+			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $props['label'];?></span><?php echo $value;?>
 		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $lname['label'];?></span><?php echo $lname['value'];?>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $email['label'];?></span><a href="mailto:<?php echo $email['value'];?>"><?php echo $email['value'];?></a>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $address['label'];?></span>
-			<div class="admin-side-mbox-text-dv">
-				<?php echo $address['value'];?>
-			</div>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $city['label'];?></span><?php echo $city['value'];?>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $state['label'];?></span><?php echo $state['value'];?>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $country['label'];?></span><?php echo $country['value'];?>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $zip['label'];?></span><?php echo $zip['value'];?>
-		</div>
-		<br/>
 		
-	<?php if ( $credit_card_info ) : ?>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $card_nmbr['label'];?></span><?php echo $card_nmbr['value'];?>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $card_exp_date['label'];?></span><?php echo $card_exp_date['value'];?>
-		</div>
-		<div class="clearfix">
-			<span class="admin-side-mbox-label-spn lt-grey-txt float-left"><?php echo $card_ccv_code['label'];?></span><?php echo $card_ccv_code['value'];?>
-		</div>
-	<?php endif; ?>	
+		<?php endforeach; ?>
 
 <?php endif; ?>	
 
