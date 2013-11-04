f0783e0b27ebe5d81e8383f55ccbcccad685357a
Brent Christensen <brent@eventespresso.com>
2013-11-04 11:34:14 -0800

    evvent list shortcode [touch:3130]

--- shortcodes/event_list/EES_Event_List.shortcode.php
+++ shortcodes/event_list/EES_Event_List.shortcode.php
@@ -23,6 +23,8 @@
  * ------------------------------------------------------------------------
  */
 class EES_Event_List  extends EES_Shortcode {
+	
+	private $_css_class = NULL;
 
 	/**
 	 * 	set_hooks - for hooking into EE Core, modules, etc
@@ -51,27 +53,84 @@ class EES_Event_List  extends EES_Shortcode {
 	 *  @return 	void
 	 */
 	public function run( WP $WP ) {
-		//add_filter( 'the_content', array( $this, 'the_content' ));
-		$this->EE->REQ->set( 'ee', 'events' );
+		// this will trigger the EED_Event_list->event_list() method during the pre_get_posts hook point, which allows us initialize things, enqueue assets, etc
+		$this->EE->REQ->set( 'ee', 'event_list' );
 	}
 
 
 
 	/**
 	 * 	process_shortcode - EVENT_LIST - Returns a list of events
+	 * 	[EVENT_LIST]
+	 * 	[EVENT_LIST limit=1]
+	 * 	[EVENT_LIST css_class=my-custom-class]
+	 * 	[EVENT_LIST show_expired=true]
+	 * 	[EVENT_LIST show_deleted=true]
+	 * 	[EVENT_LIST category_identifier=your_category_identifier]
+	 * 	[EVENT_LIST order_by=date(start_date),id]
 	 * 
 	 *  @access 	public
 	 *  @param		array 	$attributes
 	 *  @return 	void
 	 */
 	public function process_shortcode( $attributes ) {
-//		echo '<h3>'. __CLASS__ . '->' . __FUNCTION__ . ' <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h3>';
-//		// get attributes and merge  with defaults
-//		extract( shortcode_atts( array(
-//				'single_event_id' => __('No ID Supplied', 'event_espresso')
-//		), $attributes ));
-		return $this->EE->REQ->output;		
+		// grab attributes and merge with defaults, then extract
+//		extract( shortcode_atts( 
+//			array(
+//				'limit' => 10,
+//				'css_class' => NULL,
+//				'show_expired' => FALSE,
+//				'show_deleted' => FALSE,
+//				'start_date' => NULL,
+//				'category_slug' => NULL,
+//				'order_by' => NULL,
+//				'sort' => NULL,
+//				'default_view' =>'text-list'
+//			), 
+//			$attributes 
+//		));
+//		
+//		// Show Expired ?
+//		$this->EE->REQ->set( 'elf_expired_chk', $show_expired );
+//		// Category
+//		 $this->EE->REQ->set( 'elf_category_dd', $category_slug );
+//		// Start Date
+//		$this->EE->REQ->set( 'elf_month_dd', $start_date );
+//		// default_view
+//		$this->EE->REQ->set( 'elf_default_view', $default_view );
+//		// add the css class
+//		$this->_css_class = $css_class;
+//		add_filter( 'EED_Event_List__event_list_css__event_list_css_array', array( $this, 'event_list_css' ));
+		
+		global $wp_query;
+		$wp_query = new WP_Query( array( 
+			'post_type' => 'espresso_events' 
+		));
+		
+		ob_start();
+		include( EVENT_LIST_TEMPLATES_PATH . EED_Event_List::get_template_part() );
+		$this->EE->REQ->add_output( ob_get_clean() );
+		//wp_reset_query();
+		wp_reset_postdata();		
+	
+		return $this->EE->REQ->get_output();		
 	}	
+
+
+
+	/**
+	 * 	event_list_css
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public function event_list_css( $event_list_css =array() ) {
+		if ( ! empty( $this->_css_class )) {
+			$event_list_css[] = $this->_css_class;
+		}
+		return $event_list_css;
+	}
+
 
 
 }
