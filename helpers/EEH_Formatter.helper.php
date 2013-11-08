ad27c393a5581f6ad58027196075e98a1f0f2c21
Brent Christensen <brent@eventespresso.com>
2013-11-08 08:47:34 -0800

    Adds EEI_Address_Formatter interface which is implemented by EE_MultiLine_Address_Formatter and EE_Inline_Address_Formatter classes, which are loaded by the EEH_Address factory class for outputting formated address information extracted from passed objects. Also adds EEH_Schema class for adding schema.org formatting to display data. [touch:3334]

--- helpers/EEH_Formatter.helper.php
+++ helpers/EEH_Formatter.helper.php
@@ -26,10 +26,6 @@ if (!defined('EVENT_ESPRESSO_VERSION') )
  *
  * ------------------------------------------------------------------------
  */
-
-
-
-
 class EEH_Formatter {
 
 	/**
@@ -128,4 +124,353 @@ class EEH_Formatter {
 	}
 
 
+
 }//end class EEH_Form_Fields
+
+
+
+
+
+
+interface EEI_Address_Formatter {
+	public function format( $address, $address2, $city, $state, $country, $zip );
+}
+
+
+
+
+
+/**
+ * ------------------------------------------------------------------------
+ *
+ * EE_MultiLine_Address_Formatter
+ *
+ * This class will format an address and add line breaks in appropriate places
+ *
+ * @package		Event Espresso
+ * @subpackage	/helper/EEH_Formatter.helper.php
+ * @author		Brent Christensen
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_MultiLine_Address_Formatter implements EEI_Address_Formatter {	
+	public function format( $address, $address2, $city, $state, $country, $zip ) {
+		$formatted_address = $address;
+		$formatted_address .= ! empty( $address2 ) ? '<br/>' . $address2 : '';
+		$formatted_address .= ! empty( $city ) ? '<br/>' . $city : '';
+		$formatted_address .= ! empty( $state ) ? ', ' . $state : '';
+		$formatted_address .= ! empty( $zip ) ? '<br/>' . $zip : '';
+		$formatted_address .= ! empty( $country ) ? '<br/>' . $country : '';
+		return $formatted_address;
+	}
+}
+
+
+
+
+
+/**
+ * ------------------------------------------------------------------------
+ *
+ * EE_Inline_Address_Formatter
+ *
+ * This class will format an address and add commas in appropriate places
+ *
+ * @package		Event Espresso
+ * @subpackage	/helper/EEH_Formatter.helper.php
+ * @author		Brent Christensen
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Inline_Address_Formatter implements EEI_Address_Formatter {	
+	public function format( $address, $address2, $city, $state, $country, $zip ) {
+		$formatted_address = $address;
+		$formatted_address .= ! empty( $address2 ) ? ', ' . $address2 : '';
+		$formatted_address .= ! empty( $city ) ? ', ' . $city : '';
+		$formatted_address .= ! empty( $state ) ? ', ' . $state : '';
+		$formatted_address .= ! empty( $zip ) ? ', ' . $zip : '';
+		$formatted_address .= ! empty( $country ) ? ', ' . $country : '';
+		return $formatted_address;
+	}
+}
+
+
+
+
+
+
+/**
+ * ------------------------------------------------------------------------
+ *
+ * EE_Null_Address_Formatter
+ *
+ * This class will return NULL
+ *
+ * @package		Event Espresso
+ * @subpackage	/helper/EEH_Formatter.helper.php
+ * @author		Brent Christensen
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Null_Address_Formatter implements EEI_Address_Formatter {
+	public function format( $address, $address2, $city, $state, $country, $zip ) {
+		return NULL;
+	}
+}
+
+
+
+
+
+
+
+/**
+ * ------------------------------------------------------------------------
+ *
+ * EEH_Address
+ *
+ * This class takes EE objects that possess address information and apply formatting to those address for display purposes
+ *
+ * @package		Event Espresso
+ * @subpackage	/helper/EEH_Formatter.helper.php
+ * @author		Brent Christensen
+ *
+ * ------------------------------------------------------------------------
+ */
+class EEH_Address {
+
+	
+	/**
+	* 	format - output formatted EE object address information
+	* 
+	* 	@access public
+	* 	@param object EEI_Has_Address $obj_with_address
+	* 	@param string $type how the address is formatted. for example: 'multiline' or 'inline'
+	* 	@param boolean $use_schema whether to apply schema.org formatting to the address
+	* 	@return string
+	*/	
+	public static function format ( $obj_with_address = NULL, $type = 'multiline', $use_schema = TRUE ) {
+		// check that incoming object implements the EEI_Has_Address interface
+		if ( ! $obj_with_address instanceof EEI_Has_Address ) {
+			$msg = __( 'The address could not be formatted.', 'event_espresso' );
+			$dev_msg = __( 'The EE_Address_Formater requires passed objects to implement the EEI_Has_Address interface.', 'event_espresso' );
+			EE_Error::add_error( $msg . '||' . $dev_msg, __FILE__, __FUNCTION__, __LINE__ );
+			return NULL;
+		}
+		// obtain an address formatter
+		$formatter = EEH_Address::_get_formatter( $type );
+		// apply schema.org formatting ?
+		$use_schema = ! is_admin() ? $use_schema : FALSE;
+		$formatted_address = '<div class="espresso-address-dv">';
+		$formatted_address .= $use_schema ? EEH_Address::_schema_formatting( $formatter, $obj_with_address ) : EEH_Address::_regular_formatting( $formatter, $obj_with_address ) ;
+		$formatted_address .= '</div>';
+		// return the formated address
+		return $formatted_address;
+	}
+
+
+
+	/**
+	* 	_get_formatter - obtain the requester formatter class
+	* 
+	* 	@access private
+	* 	@param object EEI_Has_Address $obj_with_address
+	* 	@param string $type how the address is formatted. for example: 'multiline' or 'inline'
+	* 	@param boolean $use_schema whether to apply schema.org formatting to the address
+	* 	@return string
+	*/	
+	private static function _get_formatter( $type ) {		
+		switch( $type ) {
+			case 'multiline' :
+				$formatter = new EE_MultiLine_Address_Formatter();
+				break;
+			case 'inline' :
+				$formatter = new EE_Inline_Address_Formatter();
+				break;
+			default :
+				$formatter = new EE_Null_Address_Formatter();
+		}		
+		return $formatter;
+	}
+
+
+
+	/**
+	* 	_regular_formatting
+	* 	adds formatting to an address
+	* 
+	* 	@access private
+	* 	@param object EEI_Address_Formatter $formatter
+	* 	@param object EEI_Has_Address $obj_with_address
+	* 	@return string
+	*/	
+	private static function _regular_formatting( $formatter, $obj_with_address ){
+		$formatted_address = '<div>';
+		$formatted_address .= $formatter->format(
+			$obj_with_address->address(),
+			$obj_with_address->address2(),
+			$obj_with_address->city(),
+			$obj_with_address->state_obj()->abbrev(),
+			$obj_with_address->country_ID(),
+			$obj_with_address->zip()
+		);
+		$formatted_address .= '</div>';
+		// return the formated address
+		return $formatted_address;
+	}	
+
+
+
+	/**
+	* 	_schema_formatting
+	* 	adds schema.org formatting to an address
+	* 
+	* 	@access private
+	* 	@param object EEI_Address_Formatter $formatter
+	* 	@param object EEI_Has_Address $obj_with_address
+	* 	@return string
+	*/	
+	private static function _schema_formatting( $formatter, $obj_with_address ){
+		$formatted_address = '<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">';
+		$formatted_address .= $formatter->format(
+			EEH_Schema::streetAddress( $obj_with_address ),
+			EEH_Schema::postOfficeBoxNumber( $obj_with_address ),
+			EEH_Schema::addressLocality( $obj_with_address ),
+			EEH_Schema::addressRegion( $obj_with_address ),
+			EEH_Schema::addressCountry( $obj_with_address ),
+			EEH_Schema::postalCode( $obj_with_address )
+		);
+		$formatted_address .= '</div>';
+		// return the formated address
+		return $formatted_address;
+	}	
+	
+}
+
+
+
+
+
+
+
+/**
+ * ------------------------------------------------------------------------
+ *
+ * EEH_Address
+ *
+ * This class is a collection of static methods for applying schema.org formatting to passed items
+ *
+ * @package		Event Espresso
+ * @subpackage	/helper/EEH_Formatter.helper.php
+ * @author		Brent Christensen
+ *
+ * ------------------------------------------------------------------------
+ */
+class EEH_Schema {
+
+	
+	/**
+	* 	location
+	* 	The location of the event, organization or action.
+	* 	Should include the Venue name AND schema formatted address info
+	* 
+	* 	@access public
+	* 	@param string $location
+	* 	@return string
+	*/	
+	public static function location ( $location = NULL ) {	
+		return ! empty( $location ) ? '<div itemprop="location" itemscope itemtype="http://schema.org/Place">' . $location . '</div>' : '';
+	}
+	
+	/**
+	* 	name
+	* 	The name of the Event or Venue.
+	* 
+	* 	@access public
+	* 	@param string $name
+	* 	@return string
+	*/	
+	public static function name ( $name = NULL ) {	
+		return ! empty( $name ) ? '<span itemprop="name">' . $name . '</span>' : '';
+	}
+	
+	/**
+	* 	streetAddress
+	* 	The street address. For example, 1600 Amphitheatre Pkwy.
+	* 
+	* 	@access public
+	* 	@param object $obj_with_address
+	* 	@return string
+	*/	
+	public static function streetAddress ( $obj_with_address = NULL ) {	
+		return $obj_with_address->address() !== NULL && $obj_with_address->address() !== '' ? '<span itemprop="streetAddress">' . $obj_with_address->address() . '</span>' : '';
+	}
+
+	/**
+	* 	postOfficeBoxNumber
+	* 	The post offce box number for PO box addresses.
+	* 
+	* 	@access public
+	* 	@param object $obj_with_address
+	* 	@return string
+	*/	
+	public static function postOfficeBoxNumber ( $obj_with_address = NULL ) {
+		// regex check for some form of PO Box or P.O. Box, etc, etc, etc
+		if ( preg_match("/^\s*((P(OST)?.?\s*(O(FF(ICE)?)?)?.?\s+(B(IN|OX))?)|B(IN|OX))/i", $obj_with_address->address2() )) {
+			return $obj_with_address->address2() !== NULL && $obj_with_address->address2() !== '' ? '<span itemprop="postOfficeBoxNumber">' . $obj_with_address->address2() . '</span>' : '';
+		} else {
+			return $obj_with_address->address2();
+		}		
+	}
+
+	/**
+	* 	addressLocality
+	* 	The locality (city, town, etc). For example, Mountain View.
+	* 
+	* 	@access public
+	* 	@param object $obj_with_address
+	* 	@return string
+	*/	
+	public static function addressLocality ( $obj_with_address = NULL ) {	
+		return $obj_with_address->city() !== NULL && $obj_with_address->city() !== '' ? '<span itemprop="addressLocality">' . $obj_with_address->city() . '</span>' : '';
+	}
+
+	/**
+	* 	addressRegion
+	* 	The region (state, province, etc). For example, CA.
+	* 
+	* 	@access public
+	* 	@param object $obj_with_address
+	* 	@return string
+	*/	
+	public static function addressRegion ( $obj_with_address = NULL ) {	
+		return $obj_with_address->state_obj() instanceof EE_State ? '<span itemprop="addressRegion">' . $obj_with_address->state_obj()->abbrev() . '</span>' : '';
+	}
+
+	/**
+	* 	addressCountry
+	* 	The country. For example, USA. You can also provide the two-letter ISO 3166-1 alpha-2 country code.
+	* 
+	* 	@access public
+	* 	@param object $obj_with_address
+	* 	@return string
+	*/	
+	public static function addressCountry ( $obj_with_address = NULL ) {
+		return $obj_with_address->country_ID() !== NULL && $obj_with_address->country_ID() !== '' ? '<span itemprop="addressCountry">' . $obj_with_address->country_ID() . '</span>' : '';
+	}
+
+	/**
+	* 	postalCode
+	* 	The postal code. For example, 94043.
+	* 
+	* 	@access public
+	* 	@param object $obj_with_address
+	* 	@return string
+	*/	
+	public static function postalCode ( $obj_with_address = NULL ) {
+		return $obj_with_address->zip() !== NULL && $obj_with_address->zip() !== ''  ? '<span itemprop="postalCode">' . $obj_with_address->zip() . '</span>' : '';
+	}
+
+
+
+}
