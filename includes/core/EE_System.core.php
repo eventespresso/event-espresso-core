56a0468dc3a3f750513d5a44ce5c9d3ed7354c4a
Darren Ethier <darren@roughsmootheng.in>
2013-11-11 20:35:39 -0500

    add in a system for loading hooks/callbacks when EE_Regular is running.  Also adds functionality for 3rd party add ons to have things that run ONLY when EE Regular is active. Add filters for enabling Venue CPT and Venue Categories nav menu boxes when EE Regular is active [touch: 4032]

--- includes/core/EE_System.core.php
+++ includes/core/EE_System.core.php
@@ -102,7 +102,10 @@ final class EE_System {
 		if ( WP_DEBUG === TRUE && ! class_exists( 'EEH_Debug_Tools' )) { 
 			espresso_load_required( 'EEH_Debug_Tools', EE_HELPERS . 'EEH_Debug_Tools.helper.php' );
 		}
+
 		$this->_load_registry();
+		$this->_maybe_brew_regular();
+
 		// load and setup EE_Config
 		EE_Registry::instance()->load_core( 'Config' );
 		// setup autoloaders
@@ -111,6 +114,20 @@ final class EE_System {
 		// continue with regular request
 		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ), 5 );
 		do_action('AHEE__EE_System__construct__end',$this);
+	}
+
+
+
+	/**
+	 * The purpose of this method is to simply check for a file named "caffeinated/brewing_regular.php" for any hooks that need to be setup before our EE_System launches.
+	 * @return void
+	 */
+	private function _maybe_brew_regular() {
+		$reg_file = EVENT_ESPRESSO_PLUGINFULLPATH . 'caffeinated/brewing_regular.php';
+		$eedecaf = ( defined( 'EE_DECAF' ) && ! EE_DECAF ) || ! defined('EE_DECAF') ? FALSE : TRUE;
+		if ( is_readable( $reg_file ) && ! $eedecaf ) {
+			require_once $reg_file;
+		}
 	}
 
 
