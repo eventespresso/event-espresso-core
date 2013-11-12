5cb79d210a63296f03860eb78c5df46d300c759f
Brent Christensen <brent@eventespresso.com>
2013-11-12 15:06:21 -0800

    add unique map_ID so that all maps show up in event list [touch:4089]

--- helpers/EEH_Venue_View.helper.php
+++ helpers/EEH_Venue_View.helper.php
@@ -82,8 +82,8 @@
 	 * @return string
 	 */
 	if ( ! function_exists( 'espresso_venue_gmap' )) {
-		function espresso_venue_gmap( $gmap = array(), $single_view = FALSE ) {
-			echo EEH_Venue_View:: venue_gmap( $gmap, $single_view );
+		function espresso_venue_gmap( $map_ID = FALSE, $gmap = array(), $VNU_ID = FALSE ) {
+			echo EEH_Venue_View::venue_gmap( $map_ID, $gmap, $VNU_ID );
 		}		
 	}
 
@@ -241,47 +241,53 @@ class EEH_Venue_View extends EEH_Base {
 	 * 	venue_website_link
 	 *
 	 *  @access 	public
-	 *  @param	string $text 
+	 *  @param	string $map_ID a unique identifier for this map 
+	 *  @param	array $gmap map options
 	 *  @return 	string
 	 */
-	public static function venue_gmap( $gmap = array(), $VNU_ID = FALSE, $single_view = TRUE ) {
+	public static function venue_gmap( $map_ID = FALSE, $gmap = array(), $VNU_ID = FALSE ) {
 		
-		$venue = EEH_Venue_View::get_venue( $VNU_ID );
+		static $static_map_id = 1;		
+		
+		$venue = EEH_Venue_View::get_venue();
 		if ( $venue instanceof EE_Venue ) {
 			
 			$map_cfg = EE_Registry::instance()->CFG->map_settings;
 			
-			if ( $map_cfg->use_google_maps ) {
+			if ( $map_cfg->use_google_maps && $venue->enable_for_gmap() ) {
 				
 				EE_Registry::instance()->load_helper( 'Maps' );
 				EE_Registry::instance()->load_helper( 'Formatter' );
 				
-				$options = array();				
-				$options['VNU_ID'] = $venue->ID();	
-				$options['location'] = EEH_Address::format( $venue, 'inline', FALSE );
+				$details_page = is_single();
+				$options = array();			
+				$options['map_ID'] = $map_ID . '-' . $venue->ID() . '-' . $static_map_id;
+				$static_map_id++;
+
+				$options['location'] = EEH_Address::format( $venue, 'inline', FALSE, FALSE );
 				
-				$options['ee_map_width'] = $single_view ? $map_cfg->event_details_map_width : $map_cfg->event_list_map_width;	
+				$options['ee_map_width'] = $details_page ? $map_cfg->event_details_map_width : $map_cfg->event_list_map_width;	
 				$options['ee_map_width'] = isset( $gmap['ee_map_width'] ) && ! empty( $gmap['ee_map_width'] ) ? $gmap['ee_map_width'] : $options['ee_map_width'];
 				
-				$options['ee_map_height'] = $single_view ? $map_cfg->event_details_map_height : $map_cfg->event_list_map_height;	
+				$options['ee_map_height'] = $details_page ? $map_cfg->event_details_map_height : $map_cfg->event_list_map_height;	
 				$options['ee_map_height'] = isset( $gmap['ee_map_height'] ) && ! empty( $gmap['ee_map_height'] ) ? $gmap['ee_map_height'] : $options['ee_map_height'];
 				
-				$options['ee_map_zoom'] = $single_view ? $map_cfg->event_details_map_zoom : $map_cfg->event_list_map_zoom;	
+				$options['ee_map_zoom'] = $details_page ? $map_cfg->event_details_map_zoom : $map_cfg->event_list_map_zoom;	
 				$options['ee_map_zoom'] = isset( $gmap['ee_map_zoom'] ) && ! empty( $gmap['ee_map_zoom'] ) ? $gmap['ee_map_zoom'] : $options['ee_map_zoom'];
 				
-				$options['ee_map_nav_display'] = $single_view ? $map_cfg->event_details_display_nav : $map_cfg->event_list_display_nav;	
+				$options['ee_map_nav_display'] = $details_page ? $map_cfg->event_details_display_nav : $map_cfg->event_list_display_nav;	
 				$options['ee_map_nav_display'] = isset( $gmap['ee_map_nav_display'] ) && ! empty( $gmap['ee_map_nav_display'] ) ? 'true' : $options['ee_map_nav_display'];;
 				
-				$options['ee_map_nav_size'] = $single_view ? $map_cfg->event_details_nav_size : $map_cfg->event_list_nav_size;	
+				$options['ee_map_nav_size'] = $details_page ? $map_cfg->event_details_nav_size : $map_cfg->event_list_nav_size;	
 				$options['ee_map_nav_size'] =  isset( $gmap['ee_map_nav_size'] ) && ! empty( $gmap['ee_map_nav_size'] )? $gmap['ee_map_nav_size'] : $options['ee_map_nav_size'];
 				
-				$options['ee_map_type_control'] = $single_view ? $map_cfg->event_details_control_type : $map_cfg->event_list_control_type;	
+				$options['ee_map_type_control'] = $details_page ? $map_cfg->event_details_control_type : $map_cfg->event_list_control_type;	
 				$options['ee_map_type_control'] =  isset( $gmap['ee_map_type_control'] ) && ! empty( $gmap['ee_map_type_control'] )? $gmap['ee_map_type_control'] : $options['ee_map_type_control'];
 				
-				$options['ee_map_align'] = $single_view ? $map_cfg->event_details_map_align : $map_cfg->event_list_map_align;	
+				$options['ee_map_align'] = $details_page ? $map_cfg->event_details_map_align : $map_cfg->event_list_map_align;	
 				$options['ee_map_align'] =  isset( $gmap['ee_map_align'] ) && ! empty( $gmap['ee_map_align'] )? $gmap['ee_map_align'] : $options['ee_map_align'];
 				
-				$options['ee_static_url'] =  isset( $gmap['ee_static_url'] ) && ! empty( $gmap['ee_static_url'] )? (bool)absint( $gmap['ee_static_url'] ) : FALSE;
+				$options['ee_static_url'] =  isset( $gmap['ee_static_url'] ) && ! empty( $gmap['ee_static_url'] ) ? (bool)absint( $gmap['ee_static_url'] ) : $venue->google_map_link();
 				
 				return EEH_Maps::google_map( $options );
 				
