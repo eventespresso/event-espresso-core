366ef331753caa33dbb4dd1c08d2ba78f97d4ae4
Darren Ethier <darren@roughsmootheng.in>
2013-11-14 12:31:50 -0500

    move css for lock/trash/gear etc icons into ee-admin css cause its used on multiple pages. add lock icon for events that cannot be permanently deleted (but only on the trash route) [touch: 4057]

--- includes/core/admin/events/Events_Admin_List_Table.class.php
+++ includes/core/admin/events/Events_Admin_List_Table.class.php
@@ -104,7 +104,10 @@ class Events_Admin_List_Table extends EE_Admin_List_Table {
 
 	public function column_cb($item) {
 		$this->_dtt = $item->primary_datetime(); //set this for use in other columns
-        return sprintf(
+
+		//does event have any attached registrations?
+		$regs = $item->count_related('Registration');
+        return $regs > 0 && $this->_view == 'trash' ? '<span class="lock-icon"></span>' : sprintf(
             '<input type="checkbox" name="EVT_IDs[]" value="%s" />', $item->ID()
         );    
     }
