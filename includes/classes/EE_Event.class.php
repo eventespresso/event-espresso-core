bd17710616741873ef875ed9a578ecdcb3749b04
Brent Christensen <brent@eventespresso.com>
2013-11-14 22:36:50 -0800

    change event_phone() method name to phone() [touch:3211]

--- includes/classes/EE_Event.class.php
+++ includes/classes/EE_Event.class.php
@@ -309,7 +309,7 @@ class EE_Event extends EE_CPT_Base{
 	function member_only(){
 		return $this->get('EVT_member_only');
 	}
-	function event_phone(){
+	function phone(){
 		return $this->get('EVT_phone');
 	}
 	function modified(){
