0c8e3c87a639528ca4b384d6a57626e2ce3d044c
Seth <seth@eventespresso.com>
2013-11-16 11:24:35 -0700

    Condensing the if else statements. [touch:4184]

--- templates/espresso_events/archive-espresso_events-grid-view.php
+++ templates/espresso_events/archive-espresso_events-grid-view.php
@@ -5,8 +5,8 @@
  * Event Registration and Management Plugin for WordPress
  *
  * @ package		Event Espresso
- * @ author		Seth Shoultes
- * @ copyright	(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ author			Seth Shoultes
+ * @ copyright		(c) 2008-2013 Event Espresso  All Rights Reserved.
  * @ license		http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
  * @ link			http://www.eventespresso.com
  * @ version		4+
@@ -24,9 +24,9 @@ espresso_grid_event_list( $ELID );
 		
 		<?php do_action( 'AHEE__archive_event_list_template__after_header' ); ?>
 		
-		<?php if ( have_posts() ) { ?>
+		<?php if ( have_posts() ) : ?>
 		<div id="espresso-events-list-<?php echo $ELID; ?>-dv" class="espresso-events-list-dv column columns" role="main">				
-			<?php while ( have_posts() ) { the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 			<?php global $post; ?>
 			<article id="<?php echo 'post-' . $ELID . '-' . $post->ID; ?>" <?php post_class( espresso_event_list_css() ); ?>>
 				
@@ -38,22 +38,22 @@ espresso_grid_event_list( $ELID );
 
 				<div id="events-list-<?php echo $ELID; ?>-event-wrap-<?php echo $post->ID; ?>" class="events-list-event-wrap-dv">
 
-			<?php
-				$wrap_class = '';
-				if ( has_post_thumbnail( $post->ID )) {
-					if ( $img_ID = get_post_thumbnail_id( $post->ID )) {
-						if ( $featured_img = wp_get_attachment_image_src( $img_ID, 'medium' )) {
-							$caption = esc_attr( get_post( get_post_thumbnail_id( $post->ID ))->post_excerpt );
-							$wrap_class = ' has-img';
-				?>
-					<div id="ee-event-img-dv-<?php echo $ELID; ?>-<?php echo $post->ID; ?>" class="ee-event-img-dv">
-						<img class="ee-event-img" src="<?php echo $featured_img[0]; ?>" width="<?php echo $featured_img[1]; ?>" height="<?php echo $featured_img[2]; ?>" alt="<?php echo $caption; ?>"/>		
-					</div>
-				<?php 
-						}			
-					}			
-				}				
-			?>
+					<?php
+						$wrap_class = '';
+						if ( has_post_thumbnail( $post->ID )) {
+							if ( $img_ID = get_post_thumbnail_id( $post->ID )) {
+								if ( $featured_img = wp_get_attachment_image_src( $img_ID, 'medium' )) {
+									$caption = esc_attr( get_post( get_post_thumbnail_id( $post->ID ))->post_excerpt );
+									$wrap_class = ' has-img';
+						?>
+							<div id="ee-event-img-dv-<?php echo $ELID; ?>-<?php echo $post->ID; ?>" class="ee-event-img-dv">
+								<img class="ee-event-img" src="<?php echo $featured_img[0]; ?>" width="<?php echo $featured_img[1]; ?>" height="<?php echo $featured_img[2]; ?>" alt="<?php echo $caption; ?>"/>		
+							</div>
+						<?php 
+								}			
+							}			
+						}				
+					?>
 					<div class="espresso-event-wrapper-dv<?php echo $wrap_class;?>">
 						<header class="event-header">
 							<h3 class="event-title">
@@ -66,28 +66,28 @@ espresso_grid_event_list( $ELID );
 						
 						<div class="event-content">
 							<?php 
-								if( espresso_display_full_description_in_event_list() ) {
+								if( espresso_display_full_description_in_event_list() ) :
 									the_content(); 
-								} else if ( espresso_display_excerpt_in_event_list() ) {
+								elseif ( espresso_display_excerpt_in_event_list() ) :
 									the_excerpt(); 
-								}							
+								endif;							
 							?> 
 						</div>				
 						<!-- .event-content -->
 						
-						<?php if ( espresso_display_venue_details_in_event_list() || espresso_display_venue_address_in_event_list() ) { ?>
+						<?php if ( espresso_display_venue_details_in_event_list() || espresso_display_venue_address_in_event_list() ) : ?>
 						<div class="espresso-venue-dv">
 							<p>
 								<strong><?php _e( 'Location: ', 'event_espresso' ); ?></strong><br/>
 								<strong><?php espresso_venue_name(); ?></strong>
 							</p>
-							<?php if ( espresso_display_venue_address_in_event_list() ) { ?>
+							<?php if ( espresso_display_venue_address_in_event_list() ) : ?>
 								<strong><?php _e( 'Address: ', 'event_espresso' ); ?></strong>
 								<?php espresso_venue_address( 'inline' ); ?>
 								<?php espresso_venue_gmap( $ELID . '-' . $post->ID ); ?>
 								<div class="clear"><br/></div>
-							<?php } ?>
-							<?php if ( espresso_display_venue_details_in_event_list() ) { ?>
+							<?php endif; ?>
+							<?php if ( espresso_display_venue_details_in_event_list() ) : ?>
 							<p>
 								<strong><?php _e( 'Description: ', 'event_espresso' ); ?></strong><br/>
 								<?php echo espresso_venue_excerpt(); ?>								
@@ -100,10 +100,10 @@ espresso_grid_event_list( $ELID );
 								<strong><?php _e( 'Phone: ', 'event_espresso' ); ?></strong>
 								<?php echo espresso_venue_phone(); ?>
 							</p>
-							<?php } ?>
+							<?php endif; ?>
 						</div>				
 						<!-- .espresso-venue-dv -->
-						<?php } ?>
+						<?php endif;/**/ ?>
 
 						<footer class="event-meta">
 							<p><?php the_terms( $post->ID, 'espresso_event_categories' );// the_terms( $post->ID, 'category' );  ?></p>
@@ -119,13 +119,13 @@ espresso_grid_event_list( $ELID );
 			</article>
 			<!-- #post -->
 
-			<?php } ?>
+			<?php endwhile; ?>
 			<div class="clear"></div>
 		</div>
 		<!-- #espresso-events-list-dv -->
 
 
-		<?php } else { ?>
+		<?php else : ?>
 
 			<article id="post-0" class="no-espresso-events-found">
 
@@ -142,11 +142,7 @@ espresso_grid_event_list( $ELID );
 			</article>
 			<!-- #post-0 -->
 
-		<?php } // end have_posts() check ?>
-		
-		<?php 
-//		printr( $wp_query, '$wp_query  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-		 ?>
+		<?php endif; // end have_posts() check ?>
 
 		<div class="ee-pagination-dv"><?php  //echo paginate_links( $pagination_args ); ?></div>
 			
