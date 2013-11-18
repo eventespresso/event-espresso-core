cc9916ba0b418368afe828843a679b0939250728
Darren Ethier <darren@roughsmootheng.in>
2013-11-17 19:16:25 -0500

    comment out the automatic appending of Price Type base type names to a price type name drop the usage of ucwords & strtolower on the price type name before saving. [touch: 4203]
    There were problems with the current implementation:
    
    1. It assumed we only had English users
    2. It assumed those users would only want sentence case for their price type name references.

--- caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
+++ caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
@@ -927,52 +927,53 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 				$this->_req_data['PRT_is_percent'] = 0;
 				$this->_req_data['PRT_order'] = 0;
 	
-				$pos = strpos($name, ' Price');
+				/*$pos = strpos($name, ' Price');
 				$trunc = strlen($name) - 6;
 				if ($pos == $trunc) {
 					$name = substr($name, 0, $trunc);
 				}
 				$name = trim($name) . ' Price';
-				$this->_req_data['PRT_name'] = $name;
+				$this->_req_data['PRT_name'] = $name;/**/
 				break;
 	
 			case 'Discount' :
 				$this->_req_data['PBT_ID'] = 2;
-				$pos = strpos($name, ' Discount');
+				/*$pos = strpos($name, ' Discount');
 				$trunc = strlen($name) - 9;
 				if ($pos == $trunc) {
 					$name = substr($name, 0, $trunc);
 				}
 				$name = trim($name) . ' Discount';
-				$this->_req_data['PRT_name'] = $name;
+				$this->_req_data['PRT_name'] = $name;/**/
 				break;
 	
 			case 'Surcharge' :
 				$this->_req_data['PBT_ID'] = 3;
-				$pos = strpos($name, ' Surcharge');
+				/*$pos = strpos($name, ' Surcharge');
 				$trunc = strlen($name) - 10;
 				if ($pos == $trunc) {
 					$name = substr($name, 0, $trunc);
 				}
 				$name = trim($name) . ' Surcharge';
-				$this->_req_data['PRT_name'] = $name;
+				$this->_req_data['PRT_name'] = $name;/**/
 				break;
 	
 			case 'Tax' :
 				$this->_req_data['PBT_ID'] = 4;
 				$this->_req_data['PRT_is_percent'] = 1;
-				$pos = strpos($name, ' Tax');
+				/*$pos = strpos($name, ' Tax');
 				$trunc = strlen($name) - 4;
 				if ($pos == $trunc) {
 					$name = substr($name, 0, $trunc);
 				}
 				$name = trim($name) . ' Tax';
-				$this->_req_data['PRT_name'] = $name;
+				$this->_req_data['PRT_name'] = $name;/**/
 				break;
-		}
+		}/**/
+
 	
-		//$this->_req_data['PRT_name'] = ucwords(strtolower($this->_req_data['PRT_name']));
 		$this->_req_data['PRT_name'] = $this->_req_data['PRT_name'];
+		//$this->_req_data['PRT_name'] = $this->_req_data['PRT_name'];
 		$set_column_values = array(
 				'PRT_name' => $this->_req_data['PRT_name'],
 				'PBT_ID' => absint($this->_req_data['PBT_ID']),
