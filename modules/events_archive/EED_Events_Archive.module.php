93d2101274697225f17f368463331695dc445783
Darren Ethier <darren@roughsmootheng.in>
2013-11-18 23:55:25 -0500

    missing $this [touch: 3557]

--- modules/events_archive/EED_Events_Archive.module.php
+++ modules/events_archive/EED_Events_Archive.module.php
@@ -121,7 +121,7 @@ class EED_Events_Archive  extends EED_Module {
 		do_action( 'AHEE__EED_Events_Archive__before_run' );
 		// set config
 		if ( ! isset( $EE->CFG->template_settings->EED_Events_Archive ) || ! $EE->CFG->template_settings->EED_Events_Archive instanceof EE_Events_Archive_Config ) {
-			$EE->CFG->template_settings->EED_Events_Archive = new EE_Events_Archive_Config();
+			$this->EE->CFG->template_settings->EED_Events_Archive = new EE_Events_Archive_Config();
 		}
 		// grid, text or dates ?
 		EED_Events_Archive::set_type();
