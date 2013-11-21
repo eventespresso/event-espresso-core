b3e3deb50e369ee6fcbb5dad04044fde1ee2a934
Seth <seth@eventespresso.com>
2013-11-21 12:20:43 -0700

    Updating the help tours for the Messages Overview page. Still need the message settings and editor pages. [touch:4274,3877]

--- admin_pages/messages/help_tours/Messages_Overview_Help_Tour.class.php
+++ admin_pages/messages/help_tours/Messages_Overview_Help_Tour.class.php
@@ -77,27 +77,28 @@ class Messages_Overview_Help_Tour extends EE_Help_Tour {
 
 
 	protected function _start() {
-		$content = '<h3>' . __('Welcome to the Messages overview page!', 'event_espresso') . '</h3>';
-		$content .= '<p>' . __('An introduction ...', 'event_espresso') . '</p>';
+		$content = '<h3>' . __('Messages Overview', 'event_espresso') . '</h3>';
+		$content .= '<p>' . __('Messages are a new way of dealing with emails and other notifications to both yourself and your customers.', 'event_espresso') . '</p>';
+		$content .= '<p>' . __('Please be sure to open the "Help" tab in the top right corer of this page to view the documentation for this page.', 'event_espresso') . '</p>';
 		return $content;
 	}
 
 
 	protected function _stop_about_filters() {
-		return '<p>' . __('You can filter the message templates in this list by blah blah blah', 'event_espresso') . '</p>';
+		return '<p>' . __('These dropdowns will allow you to filter the different notifications.', 'event_espresso') . '</p>';
 	}
 
 
 	protected function event_column_stop() {
-		return '<p>' . __('about the column', 'event_espresso') . '</p>';
+		return '<p>' . __('This column shows you if the message is specific to one or more events.', 'event_espresso') . '</p>';
 	}
 
 
 	protected function _message_type_column_stop() {
-		return '<p>' . __('about the column', 'event_espresso') . '</p>';
+		return '<p>' . __('Here you can see what type of message it is. The types refer to things like payment emails, or registration emails.', 'event_espresso') . '</p>';
 	}
 
 	protected function _messenger_column_stop() {
-		return '<p>' . __('about the column', 'event_espresso') . '</p>';
+		return '<p>' . __('Each message can be sent to multiple recipients, and this column shows which ones will receive the message.', 'event_espresso') . '</p>';
 	}
 }
