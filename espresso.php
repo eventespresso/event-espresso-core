86f5d2a2a1d8d3b36d58387611fda10ce8963499
Darren Ethier <darren@roughsmootheng.in>
2013-11-22 11:37:19 -0500

    changing version for alpha branch to have alpha in name

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 			4.1.028.dev
+  Version: 			4.1.028.alpha
 
   Author: 				Seth Shoultes
   Author URI: 		http://www.eventespresso.com
@@ -31,7 +31,7 @@
  */
 //Returns the plugin version
 function espresso_version() {
-	return '4.1.028.dev';
+	return '4.1.028.alpha';
 }
 //Returns the template version
 function espresso_template_version() {
