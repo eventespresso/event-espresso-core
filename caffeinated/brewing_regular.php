18fb5f959836db33193c1dcc8f52ea180e37da2a
Darren Ethier <darren@roughsmootheng.in>
2013-11-24 09:25:13 -0500

    add Question and Answer short codes rework things so that the custom [ANSWER_*] shortcode gets parsed by Attendee short codes parser (but only available to caf users) [touch: 2392]

--- caffeinated/brewing_regular.php
+++ caffeinated/brewing_regular.php
@@ -251,6 +251,10 @@ class EE_Brewing_Regular extends EE_Base {
 		add_filter('FHEE__EE_Email_messenger__get_default_field_content', array( $this, 'email_default_field_content'), 10, 2 );
 		add_filter('FHEE__EE_Messages_Base__get_default_field_content', array( $this, 'message_types_default_field_content'), 10, 2 );
 		add_filter('FHEE__EE_Messages_Base__get_valid_shortcodes', array( $this, 'message_types_valid_shortcodes', 10, 2 ) );
+
+		//shortcode parsers
+		add_filter('FHEE__EE_Attendee_Shortcodes__shortcodes', array( $this, 'additional_attendee_shortcodes'), 10 );
+		add_filter('FHEE__EE_Attendee_Shortcodes__parser_after', array( $this, 'additional_attendee_parser'), 10, 4 );
 	}
 
 
@@ -346,6 +350,34 @@ class EE_Brewing_Regular extends EE_Base {
 				break;
 		}
 		return $valid_shortcodes;
+	}
+
+
+
+
+	public function additional_attendee_shortcodes( $shortcodes ) {
+		$shortcodes['[ANSWER_*]'] = __('This is a special dynamic shortcode.  Replace the "*" with the exact text of a existing question, and if there is an answer for that question for this attendee, that will take the place of this shortcode.', 'event_espresso');
+		return $shortcodes;
+	}
+
+
+
+	public function additional_attendee_parser( $parsed, $shortcode, $data, $extra_data ) {
+		if ( !strpos( '[ANSWER_' ) || !isset( $data->questions) || !isset( $data->attendees) )
+			return '';
+
+		//let's get the question from the code.
+		$shortcode = str_replace('[ANSWER_', '', $shortcode);
+		$shortcode = str_replace(']', '', $shortcode);
+
+		//now let's figure out which question has this text.
+		foreach ( $data->questions as $ansid => $question ) {
+			if ( $question->get('QST_display_text') == $shortcode )
+				return $data->attendees[$data->ID()]['ans_objs'][$ansid];
+		}
+
+		//nothing!
+		return '';
 	}
 }
 $brewing = new EE_Brewing_Regular();
---
+++ caffeinated/core/libraries/shortcodes/EE_Question_Shortcodes.lib.php
@@ -0,0 +1,67 @@
+<?php
+
+if (!defined('EVENT_ESPRESSO_VERSION') )
+	exit('NO direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package			Event Espresso
+ * @ author				Seth Shoultes
+ * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link				http://www.eventespresso.com
+ * @ version		 	4.0
+ *
+ * ------------------------------------------------------------------------
+ *
+ * EE_Question_Shortcodes
+ * 
+ * this is a child class for the EE_Shortcodes library.  The EE_Question_Shortcodes lists all shortcodes related to Questions and Answers for an attendee. 
+ *
+ * NOTE: if a method doesn't have any phpdoc commenting the details can be found in the comments in EE_Shortcodes parent class.
+ * 
+ * @package		Event Espresso
+ * @subpackage	libraries/shortcodes/EE_Question_Shortcodes.lib.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Question_Shortcodes extends EE_Shortcodes {
+
+
+
+	protected function _init_props() {
+		$this->label = __('Attendee Shortcodes', 'event_espresso');
+		$this->description = __('All shortcodes specific to attendee related data', 'event_espresso');
+		$this->_shortcodes = array(
+			'[QUESTION]' => __('Will parse to a question.', 'event_espresso'),
+			'[ANSWER]' => __('Will parse to the answer for a question', 'event_espresso')
+			);
+	}
+
+
+	protected function _parser( $shortcode ) {
+
+		if ( ! $this->_data instanceof EE_Answer || !isset( $this->_extra_data->questions ) )
+			return '';
+
+		switch ( $shortcode ) {
+
+			case '[QUESTION]' :
+				return $this->_extra_data->questions[$this->_data->ID()];
+				break;
+
+			case '[ANSWER]' :
+				return $this->_data->get('ANS_value');
+				break;
+
+			default :
+				return '';
+				break;
+		}
+	}
+
+} //end EE_Question_Shortcodes class
