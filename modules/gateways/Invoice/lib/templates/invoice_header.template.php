84ac91b27c7b325d663d1fb45389c7978dd45e86
Seth <seth@eventespresso.com>
2013-11-24 00:32:23 -0700

    HTML and CSS adjustments in the receipt and invoice templates. Converted repeated id elements to class elements. [touch:4097, 4098]

--- modules/gateways/Invoice/lib/templates/invoice_header.template.php
+++ modules/gateways/Invoice/lib/templates/invoice_header.template.php
@@ -19,7 +19,7 @@
 
 	<div class="print_button_div">
 		<form>
-			<input class="print_button noPrint" type="button" value=" Print Invoice " onclick="window.print();return false;" />
+			<input class="print_button noPrint" type="button" value=" Print Invoice " onClick="window.print();return false;" />
 		</form>
 		<form method="post" action="<?php echo $download_link; ?>" >
 			<input class="print_button noPrint" type="submit" value=" Download PDF " />
