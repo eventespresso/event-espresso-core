ab8443f6ad0c3b54c35562adc0f64e839c3c7ca8
Brent Christensen <brent@eventespresso.com>
2013-11-25 10:24:44 -0800

    changed templates to use event link template tag for links to events [touch:4051]

--- templates/espresso_default/espresso_events/archive-espresso_events-dates-view.php
+++ templates/espresso_default/espresso_events/archive-espresso_events-dates-view.php
@@ -57,7 +57,7 @@ $ELID = espresso_get_event_list_ID();
 					<div class="espresso-event-wrapper-dv<?php echo $wrap_class;?>">
 						<header class="event-header">
 							<h3 class="event-title">
-								<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'event_espresso' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+								<a href="<?php espresso_event_link_url(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'event_espresso' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
 									<?php the_title(); ?>							
 								</a>
 							</h3>
@@ -110,7 +110,7 @@ $ELID = espresso_get_event_list_ID();
 
 						<footer class="event-meta">
 							
-							<a class="ee-register-button-lnk ee-button-lnk button" href="<?php the_permalink( $post->ID ); ?>" title=""><?php _e( 'Register Now', 'event_espresso' ); ?></a>
+							<a class="ee-register-button-lnk ee-button-lnk button" href="<?php espresso_event_link_url(); ?>" title=""><?php _e( 'Register Now', 'event_espresso' ); ?></a>
 							<p class="event-categories-pg"><?php _e( 'category:', 'event_espresso' ) . espresso_event_categories(); ?></p>
 							
 							<?php espresso_edit_event_link(); ?>
