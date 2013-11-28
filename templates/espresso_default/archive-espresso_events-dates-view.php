0376e63cd77011118d3212ac6e4218c95c4577cd
Brent Christensen <brent@eventespresso.com>
2013-11-28 14:34:55 -0800

    use new general css classes for buttons [touch:4131]

--- templates/espresso_default/archive-espresso_events-dates-view.php
+++ templates/espresso_default/archive-espresso_events-dates-view.php
@@ -111,7 +111,7 @@ $ELID = espresso_get_event_list_ID();
 
 						<footer class="event-meta">
 							
-							<a class="ee-register-button-lnk ee-button-lnk button" href="<?php espresso_event_link_url(); ?>" title=""><?php _e( 'Register Now', 'event_espresso' ); ?></a>
+							<a class="ee-button ee-register-button big ee-green" href="<?php espresso_event_link_url(); ?>" title=""><?php _e( 'Register Now', 'event_espresso' ); ?></a>
 							<p class="event-categories-pg"><?php _e( 'category:', 'event_espresso' ) . espresso_event_categories(); ?></p>
 							
 							<?php espresso_edit_event_link(); ?>
--- templates/espresso_default/archive-espresso_events-grid-view.php
+++ templates/espresso_default/archive-espresso_events-grid-view.php
@@ -109,7 +109,7 @@ $ELID = espresso_get_event_list_ID();
 						<footer class="event-meta">
 							<p><?php the_terms( $post->ID, 'espresso_event_categories' );// the_terms( $post->ID, 'category' );  ?></p>
 							<?php do_action( 'AHEE_events_list_footer', $post ); ?>
-							<a class="ee-register-button-lnk ee-button-lnk button" href="<?php espresso_event_link_url(); ?>" title=""><?php _e( 'Register Now', 'event_espresso' ); ?></a>
+							<a class="ee-button ee-register-button big ee-green" href="<?php espresso_event_link_url(); ?>" title=""><?php _e( 'Register Now', 'event_espresso' ); ?></a>
 							<?php espresso_edit_event_link(); ?>
 						</footer>
 						<!-- .entry-meta -->
