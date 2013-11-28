841135d19780381eea29ca42fd2434b3127a47c3
Darren Ethier <darren@roughsmootheng.in>
2013-11-27 19:00:49 -0500

    bump version to 4.1.038.alpha

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 		4.1.0367alpha
+  Version: 		4.1.038.alpha
 
   Author: 			Seth Shoultes
   Author URI: 		http://www.eventespresso.com
@@ -31,7 +31,7 @@
  */
 //Returns the plugin version
 function espresso_version() {
-	return '4.1.037.alpha';
+	return '4.1.038.alpha';
 }
 //Returns the template version
 function espresso_template_version() {
