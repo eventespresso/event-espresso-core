c7756cbd551a87b2cf40578f734e6753460da2d8
Michael Nelson <michael@eventespresso.com>
2013-12-04 14:23:23 -0800

    [touch:4380]{t:15}made a tweak that shows another way to use the models with INF... you can actually pass INF as a parameter for the query, and it will get switched to -1 in the query for you

--- core/EE_Front_Controller.core.php
+++ core/EE_Front_Controller.core.php
@@ -254,18 +254,18 @@ final class EE_Front_Controller {
 //		EE_Error::add_attention('This is an attention message. You should prolly read this... but... meh... do whatcha want!');
 //		EE_Error::add_error('Ermehgerd!!! MOAR ERRORS!!! Seff n Garf aw gonna be maaaaad at you!!!');
 
-//		$regs = EEM_Datetime::instance()->get_all( array(
-//			array( 'DTT_reg_limit' => -1 ),
-//			'limit'=>1,
-//			'order_by'=>array( 
-//				'DTT_EVT_start'=>'ASC',		
-//				'DTT_is_primary'=>'DESC'
-//			)
-//		));
-//		$reg = reset( $regs );
-//		d( $reg );		
-//		$reg->save();
-//		d( $reg );
+		$regs = EEM_Datetime::instance()->get_all( array(
+			array( 'DTT_reg_limit' => INF ),
+			'limit'=>1,
+			'order_by'=>array( 
+				'DTT_EVT_start'=>'ASC',		
+				'DTT_is_primary'=>'DESC'
+			)
+		));
+		$reg = reset( $regs );
+		d( $reg );		
+		$reg->save();
+		d( $reg );
 		
 //		$d = EE_Datetime::new_instance(array('DTT_reg_limit'=>INF));
 //		d($d);
