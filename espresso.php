d4fd632c8ad019074609ea2243fb88d826603eab
Darren Ethier <darren@roughsmootheng.in>
2013-12-05 13:56:27 -0500

    add a constant for helper assets folder (url) [touch: 4392]

--- espresso.php
+++ espresso.php
@@ -78,6 +78,7 @@ define( 'EE_TEMPLATES_URL', EE_PLUGIN_DIR_URL . 'templates' . DS );
 define( 'EE_GLOBAL_ASSETS_URL', EE_TEMPLATES_URL . 'global_assets' . DS );
 define( 'EE_IMAGES_URL',  EE_GLOBAL_ASSETS_URL . 'images' . DS );
 define( 'EE_THIRD_PARTY_URL', EE_PLUGIN_DIR_URL . 'core' . DS . 'third_party_libs' . DS );
+define( 'EE_HELPERS_ASSETS', EE_PLUGIN_DIR_URL . 'core/helpers/assets/' );
 
 // define upload paths
 $uploads = wp_upload_dir();
