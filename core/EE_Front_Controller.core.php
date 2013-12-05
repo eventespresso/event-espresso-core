6f9da631256ce6166c325bd67636f9a38bf15130
Michael Nelson <michael@eventespresso.com>
2013-12-05 09:35:46 -0800

    [touch:4389]{t:20}refactored, so update_tickets_sold is now also a function on EE_Tickets, and EEM_TIcket::update_tickets_sold now accepts an array of EE_Tickets, in order to avoid requerying for the data when we already have it

--- core/EE_Front_Controller.core.php
+++ core/EE_Front_Controller.core.php
@@ -81,7 +81,7 @@ final class EE_Front_Controller {
 	 *  @return 	void
 	 */
 	public function init(){
-//		EEM_Ticket::instance()->update_tickets_sold();
+//		EEM_Ticket::instance()->update_tickets_sold(EEM_Ticket::instance()->get_all());
 //		$t = EE_Ticket::new_instance(array('TKT_name'=>'prog1',));
 //		$p = EEM_Price::instance()->get_one();
 //		$d = EEM_Datetime::instance()->get_one();
