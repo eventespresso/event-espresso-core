9cee9c2d4a6afa35faa671b459649c9398ce0069
Darren Ethier <darren@roughsmootheng.in>
2013-12-05 21:25:32 -0500

    finish fleeing out the Qtip_Config parent, and the ee-qtip-helper.js - all ready for implementation! [touch: 4392]

--- core/helpers/assets/ee-qtip-helper.js
+++ core/helpers/assets/ee-qtip-helper.js
@@ -2,8 +2,27 @@ jQuery(document).ready(function($) {
 
 	//load all qtips
 	if ( EE_QTIP_HELPER.qtips.length > 0 ) {
+		var parse;
 		//loop through the qtips and set them up.
 		$.each(EE_QTIP_HELPER.qtips, function(i, v ) {
+			//make sure that content is refernecing content div
+			v.options.content.text = $(v.content_id);
+
+			if ( typeof( v.options.position.target) !== 'undefined' && v.options.position.target.indexOf("jQuery::") > -1 ) {
+				parse = v.options.position.target.replace('jQuery::', '');
+				v.options.position.target = $(parse);
+			}
+
+			if ( typeof( v.options.position.container) !== 'undefined' && v.options.position.container.indexOf('jQuery::') > -1 ) {
+				parse = v.options.position.container.replace('jQuery::', '');
+				v.options.position.container = $(parse);
+			}
+
+			if ( typeof( v.options.hide.target) !== 'undefined' && v.options.hide.target.indexOf('jQuery::') > -1 ) {
+				parse = v.options.hide.target.replace('jQuery::', '');
+				v.options.hide.target = $(parse);
+			}
+
 			$(v.target).qtip(v.options);
 		});
 	}
