1595b359bf194df0ef8ec4a9920748e9a22fe15b
Brent Christensen <brent@eventespresso.com>
2013-12-20 10:49:47 -0800

    don't run calendar until after EE_System has at least started up a bit [touch:3424]

--- espresso-calendar.php
+++ espresso-calendar.php
@@ -891,7 +891,7 @@ class EE_Calendar {
 	public function __destruct() { return FALSE; }		
 
 }
-EE_Calendar::instance();
+add_action( 'AHEE__EE_System__construct__end', array( 'EE_Calendar','instance' ));
 
 
 
