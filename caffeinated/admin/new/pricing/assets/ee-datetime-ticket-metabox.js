f100091bf6c4e5d820c7beb138b85ecb0cb3750c
Darren Ethier <darren@roughsmootheng.in>
2013-12-21 17:22:42 -0500

    fix bug with verifying last DTT on ticket [touch: 4616]

--- caffeinated/admin/new/pricing/assets/ee-datetime-ticket-metabox.js
+++ caffeinated/admin/new/pricing/assets/ee-datetime-ticket-metabox.js
@@ -1171,7 +1171,7 @@ jQuery(document).ready(function($) {
 			var relateditm = this.itemdata.context == 'datetime-ticket' ? $('.datetime-tickets-list', '#edit-ticketrow-' + this.itemdata.ticketRow).find('li[data-datetime-row="' + this.itemdata.datetimeRow + '"]') : $('.datetime-tickets-list', '#edit-event-datetime-tickets-' + this.itemdata.datetimeRow).find('li[data-ticket-row="' + this.itemdata.ticketRow + '"]');
 			var available_list_row = this.itemdata.context === 'datetime-ticket' ? $('li', '#dtt-existing-available-datetime-list-items-holder').find('[data-datetime-row="'+this.itemdata.datetimeRow+'"]') : $('li', '#dtt-existing-available-ticket-list-items-holder').find('[data-ticket-row="' + this.itemdata.ticketRow +'"]' );
 
-			if ( !selecting ) {
+			if ( ( !selecting && this.context != 'ticket' ) || ( !selecting && this.context == 'ticket' && this.itemdata.context == 'ticket-datetime' ) ) {
 				this.lastDTTtoggle = tktHelper.verifyLastDTT(this.itemdata.ticketRow);
 			}
 
