fb97dc890cf0e7dc7b47fdd3c7d6808983565b0e
Michael Nelson <michael@eventespresso.com>
2013-12-20 16:02:25 -0800

    [touch:4590]{t:90}converted advanced settings page to use ee config

--- admin/calendar/templates/calendar_advanced_settings.template.php
+++ admin/calendar/templates/calendar_advanced_settings.template.php
@@ -1,3 +1,6 @@
+<?php 
+/* @var $calendar_config EE_Calendar_Config */
+?>
 <div class="padding">					
 	<h4>
 		<?php _e('Formatting', 'event_espresso'); ?>
@@ -14,11 +17,11 @@
 				</th>
 				<td>
 					<?php _e('Left', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" name="espresso_calendar_header_left" id="espresso_calendar_header_left" class="medium-text" value="<?php echo htmlentities(stripslashes($espresso_calendar['header_left'])) ?>"><br />
+					<input type="text" name="calendar[header][left]" id="espresso_calendar_header_left" class="medium-text" value="<?php echo htmlentities($calendar_config->header->left) ?>"><br />
 					<?php _e('Center', 'event_espresso'); ?>:&nbsp;
-					<input type="text" name="espresso_calendar_header_center" id="espresso_calendar_header_center" class="medium-text" value="<?php echo htmlentities(stripslashes($espresso_calendar['header_center'])) ?>"><br />
+					<input type="text" name="calendar[header][center]" id="espresso_calendar_header_center" class="medium-text" value="<?php echo htmlentities($calendar_config->header->center) ?>"><br />
 					<?php _e('Right', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="espresso_calendar_header_right" id="espresso_calendar_header_right" value="<?php echo htmlentities(stripslashes($espresso_calendar['header_right'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[header][right]" id="espresso_calendar_header_right" value="<?php echo htmlentities($calendar_config->header->right) ?>"><br />
 					<span class="description">
 						<?php _e('Defines the buttons and title at the top of the calendar.', 'event_espresso'); ?>
 					</span>
@@ -34,28 +37,28 @@
 				<td>
 
 					<?php _e('Previous', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_prev" id="buttonText_prev" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_prev'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][prev]" id="buttonText_prev" value="<?php echo htmlentities($calendar_config->button_text->prev) ?>"><br />
 
 					<?php _e('Next', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_next" id="buttonText_next" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_next'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][next]" id="buttonText_next" value="<?php echo htmlentities($calendar_config->button_text->next) ?>"><br />
 
 					<?php _e('Previous Year', 'event_espresso'); ?>:&nbsp;
-					<input type="text" class="medium-text" name="buttonText_prevYear" id="buttonText_prevYear" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_prevYear'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][prev_year]" id="buttonText_prevYear" value="<?php echo htmlentities($calendar_config->button_text->prev_year) ?>"><br />
 
 					<?php _e('Next Year', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_nextYear" id="buttonText_nextYear" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_nextYear'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][next_year]" id="buttonText_nextYear" value="<?php echo htmlentities($calendar_config->button_text->next_year) ?>"><br />
 
 					<?php _e('Today', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_today" id="buttonText_today" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_today'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][today]" id="buttonText_today" value="<?php echo htmlentities($calendar_config->button_text->today) ?>"><br />
 
 					<?php _e('Month', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_month" id="buttonText_month" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_month'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][month]" id="buttonText_month" value="<?php echo htmlentities($calendar_config->button_text->month) ?>"><br />
 
 					<?php _e('Week', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_week" id="buttonText_week" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_week'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][week]" id="buttonText_week" value="<?php echo htmlentities($calendar_config->button_text->week) ?>"><br />
 
 					<?php _e('Day', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="buttonText_day" id="buttonText_day" value="<?php echo htmlentities(stripslashes($espresso_calendar['buttonText_day'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[button_text][day]" id="buttonText_day" value="<?php echo htmlentities($calendar_config->button_text->day) ?>"><br />
 
 					<span class="description">
 						<?php _e('Text that will be displayed on the buttons in the header.', 'event_espresso'); ?>
@@ -71,11 +74,11 @@
 				</th>
 				<td>
 					<?php _e('Month', 'event_espresso'); ?>:&nbsp;
-					<input type="text" class="medium-text" name="titleFormat_month" id="titleFormat_month" value="<?php echo htmlentities(stripslashes($espresso_calendar['titleFormat_month'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[title_format][month]" id="titleFormat_month" value="<?php echo htmlentities($calendar_config->title_format->month) ?>"><br />
 					<?php _e('Week', 'event_espresso'); ?>:&nbsp;
-					<input type="text" class="medium-text" name="titleFormat_week" id="titleFormat_week" value="<?php echo htmlentities(stripslashes($espresso_calendar['titleFormat_week'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[title_format][week]" id="titleFormat_week" value="<?php echo htmlentities($calendar_config->title_format->week) ?>"><br />
 					<?php _e('Day', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="titleFormat_day" id="titleFormat_day" value="<?php echo htmlentities(stripslashes($espresso_calendar['titleFormat_day'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[title_format][day]" id="titleFormat_day" value="<?php echo htmlentities($calendar_config->title_format->day) ?>"><br />
 					<span class="description">
 						<?php _e('Determines the text that will be displayed in the header\'s title.', 'event_espresso'); ?>
 					</span>
@@ -90,11 +93,11 @@
 				</th>
 				<td>
 					<?php _e('Month', 'event_espresso'); ?>:&nbsp;
-					<input type="text" class="medium-text" name="columnFormat_month" id="columnFormat_month" value="<?php echo htmlentities(stripslashes($espresso_calendar['columnFormat_month'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[column_format][day]" id="columnFormat_month" value="<?php echo htmlentities($calendar_config->column_format->month) ?>"><br />
 					<?php _e('Week', 'event_espresso'); ?>:&nbsp;
-					<input type="text" class="medium-text" name="columnFormat_week" id="columnFormat_week" value="<?php echo htmlentities(stripslashes($espresso_calendar['columnFormat_week'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[column_format][day]" id="columnFormat_week" value="<?php echo htmlentities($calendar_config->column_format->week) ?>"><br />
 					<?php _e('Day', 'event_espresso'); ?>:&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="text" class="medium-text" name="columnFormat_day" id="columnFormat_day" value="<?php echo htmlentities(stripslashes($espresso_calendar['columnFormat_day'])) ?>"><br />
+					<input type="text" class="medium-text" name="calendar[column_format][day]" id="columnFormat_day" value="<?php echo htmlentities($calendar_config->column_format->day) ?>"><br />
 					<span class="description">
 						<?php _e('Determines the text that will be displayed on the calendar\'s column headings.', 'event_espresso'); ?>
 					</span>
@@ -117,7 +120,7 @@
 					</label>
 				</th>
 				<td>
-					<?php echo EEH_Form_Fields::select_input('show_attendee_limit', $values, !empty($espresso_calendar['show_attendee_limit']) ? $espresso_calendar['show_attendee_limit'] : false, 'id="show_attendee_limit"'); ?><br />
+					<?php echo EEH_Form_Fields::select_input('calendar[show_attendee_limit]', $values, $calendar_config->show_attendee_limit, 'id="show_attendee_limit"'); ?><br />
 					<span class="description">
 						<?php _e('Enabling this setting increases the amount of database queries and may break the calendar on some servers.', 'event_espresso'); ?>
 					</span>
@@ -131,7 +134,7 @@
 					</label>
 				</th>
 				<td>
-					<?php echo EEH_Form_Fields::select_input('disable_categories', $values, !empty($espresso_calendar['disable_categories']) ? $espresso_calendar['disable_categories'] : false, 'id="disable_categories"'); ?><br />
+					<?php echo EEH_Form_Fields::select_input('calendar[disable_categories]', $values, $calendar_config->disable_categories, 'id="disable_categories"'); ?><br />
 					<span class="description">
 						<?php _e('Disabling categories in the calendar may potentially speed up the calendar and allow you to load more events, but you will not be able to use the category colors and css class options.', 'event_espresso'); ?>
 					</span>
