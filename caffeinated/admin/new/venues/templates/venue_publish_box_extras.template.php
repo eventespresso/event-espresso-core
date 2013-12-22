82523660de2ceb01b76f6326df20104d4d1fd805
Darren Ethier <darren@roughsmootheng.in>
2013-12-22 18:01:41 -0500

    - fix duplicate id - add some temp icons (from dash icons) for venue details in publish box [touch: 4441]

--- caffeinated/admin/new/venues/templates/venue_publish_box_extras.template.php
+++ caffeinated/admin/new/venues/templates/venue_publish_box_extras.template.php
@@ -1,9 +1,9 @@
-<div class="misc-pub-section" id="visibility">
+<div class="misc-pub-section ee-capacity-icon">
 	<?php _e('Capacity', 'event_espresso'); ?>: <input name="vnu_capacity" type="text" class="small-text" value="<?php echo $vnu_capacity; ?>" />
 </div>
-<div class="misc-pub-section" id="visibility">
+<div class="misc-pub-section ee-url-icon">
 	<?php _e('Venue Website', 'event_espresso'); ?>: <input name="vnu_url" type="text" class="all-options" value="<?php echo $vnu_url; ?>" />
 </div>
-<div class="misc-pub-section" id="visibility">
+<div class="misc-pub-section ee-phone-icon">
 	<?php _e('Venue Phone #', 'event_espresso'); ?>: <input name="vnu_phone" type="text" class="all-options" value="<?php echo $vnu_phone; ?>" />
 </div>
