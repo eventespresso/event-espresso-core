2013cfbc8cf934a3fc6ffb853ecd6cd818f9c4d5
Darren Ethier <darren@roughsmootheng.in>
2013-12-22 15:36:17 -0500

    whoops revert domain name change for PUE host server (that was used for testing) [touch: 4109]

--- core/EE_PUE.core.php
+++ core/EE_PUE.core.php
@@ -93,7 +93,7 @@ class EE_PUE {
 			require(EE_THIRD_PARTY . 'pue/pue-client.php' );
 
 			$api_key = isset( EE_Registry::instance()->CFG->site_license_key ) ? EE_Registry::instance()->CFG->site_license_key : '';
-			$host_server_url = 'http://beta.eventespresso.com'; //this needs to be the host server where plugin update engine is installed. Note, if you leave this blank then it is assumed the WordPress repo will be used and we'll just check there.
+			$host_server_url = 'http://eventespresso.com'; //this needs to be the host server where plugin update engine is installed. Note, if you leave this blank then it is assumed the WordPress repo will be used and we'll just check there.
 
 			//Note: PUE uses a simple preg_match to determine what type is currently installed based on version number.  So it's important that you use a key for the version type that is unique and not found in another key.
 			//For example:
