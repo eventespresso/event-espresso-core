56c1559311d812a0800729ffc6236bdfc076fb7a
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2013-12-23 15:44:00 -0500

    Create help tours for edit default price and edit default price type. [touch: 4638]

--- caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
+++ caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
@@ -215,6 +215,7 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 						),
 					'metaboxes' => array( '_publish_post_box', '_espresso_news_post_box', '_price_details_meta_boxes' ),
 					'help_tabs' => $pricing_help_tabs,
+					'help_tour' => array( 'Pricing_Edit_Default_Price_Help_Tour' ),
 					'require_nonce' => FALSE
 				),
 			'price_types' => array(
@@ -249,6 +250,7 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 							'order' => 40,
 							'persistent' => FALSE
 						),
+                    'help_tour' => array( 'Pricing_Edit_Price_Type_Help_Tour' ),
 					'metaboxes' => array( '_publish_post_box', '_espresso_news_post_box', '_price_type_details_meta_boxes' ),
 					
 					'require_nonce' => FALSE
