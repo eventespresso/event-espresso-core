0ef016afc6bfe7a14c2cb71f0724b47513c95d6a
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2013-12-26 17:49:48 -0500

    Move help tab information for general settings pages from templates folder to help tabs folder [touch:4652]

---
+++ admin_pages/general_settings/help_tabs/general_settings_countries.help_tab.php
@@ -0,0 +1,20 @@
+<h2>
+<?php _e('Select Your Country', 'event_espresso'); ?>
+</h2>
+<p>
+<?php echo __('Add the country where your business is located.', 'event_espresso'); ?>
+</p>
+
+<h2>
+<?php _e('Country Details', 'event_espresso'); ?>
+</h2>
+<p>
+<?php echo __('Here you can fine tune currency and contact data for your own country.', 'event_espresso'); ?>
+</p>
+
+<h2>
+<?php _e('States / Provinces', 'event_espresso'); ?>
+</h2>
+<p>
+<?php echo __('Used in certain areas of the plugin, here you can define what states/provinces will be displayed in case you do not do business in certain areas.', 'event_espresso'); ?>
+</p>
---
+++ admin_pages/general_settings/help_tabs/general_settings_your_organization.help_tab.php
@@ -0,0 +1,35 @@
+<h2>
+<?php _e('Site License Key', 'event_espresso'); ?>
+</h2>
+<p>
+<?php _e("Insert your licence key here to benefit from one click updates.", 'event_espresso'); ?>
+</p>
+<p>
+<?php _e('Please visit your account page on the <a href="http://eventespresso.com/">Event Espresso website</a> to manage your site license key.', 'event_espresso'); ?>
+</p>
+<p class="ee-attention">
+<?php _e('If this is a Development or Test site, <strong>DO NOT</strong> enter your Support License Key. Save it for the Live Production Site, otherwise you will unnecessarily run into issues with needing to have your Key reset.', 'event_espresso'); ?>
+</p>
+
+<h2>
+<?php _e('Contact Information', 'event_espresso'); ?>
+</h2>
+<p>
+<?php _e("It is important that you fill out this section and make sure the details are correct. These settings tell Event Espresso about yoru organization and will be used throughout the system.", 'event_espresso'); ?>
+</p>
+
+<?php _e('Company Logo', 'event_espresso'); ?>
+</h2>
+<p>
+<?php _e("Add your logo here to enable branding of your invoices, tickets, certificates and payment templates.", 'event_espresso'); ?>
+</p>
+<p>
+<?php _e("We recommend that the image be 400px or less.", 'event_espresso'); ?>
+</p>
+
+<h2>
+<?php _e('Social Links', 'event_espresso'); ?>
+</h2>
+<p>
+<?php _e("These URLs can be displayed automagically within your emails (using shortcodes) and front-end templates (using template tags).", 'event_espresso'); ?>
+</p>
