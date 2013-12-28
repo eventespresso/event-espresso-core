18534a13593bb839dee89a9894dd61238eb15a2f
Darren Ethier <darren@roughsmootheng.in>
2013-12-28 11:25:18 -0500

    modify description for Events/Default Settings/require pre approval global option to better explain its purpose [touch: 4409]

--- caffeinated/admin/extend/events/templates/event_settings_enable_attendee_pre_approval.template.php
+++ caffeinated/admin/extend/events/templates/event_settings_enable_attendee_pre_approval.template.php
@@ -7,7 +7,7 @@
 	<td>
 		<p><?php echo $attendee_pre_approval_select ?></p>
 		<p class="description">
-			<?php _e('Attendeess will be marked as pending approval, until an administrator manually changes their status to approved via the Registrations Admin page.', 'event_espresso'); ?>
+			<?php _e('When you create an event, there is an option to require pre-approval for that event. Pre-approval means that no matter what the transaction status, registrations (attendees) will be marked as pending approval, until an administrator manually changes their status to approved via the Registrations Admin page.  This setting here allows you to change what the default setting is for this option when creating new events.  It will not retroactively apply to existing events.', 'event_espresso'); ?>
 		</p>
 	</td>
 </tr>
