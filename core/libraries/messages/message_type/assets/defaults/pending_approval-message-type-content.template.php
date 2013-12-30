82782f7605ce83b284f8b3e9cd78dc9a16f45628
Seth <seth@eventespresso.com>
2013-12-30 11:02:30 -0700

    Fixing some places that have someone else's email address for the email URL. [touch:4603]

--- core/libraries/messages/message_type/assets/defaults/pending_approval-message-type-content.template.php
+++ core/libraries/messages/message_type/assets/defaults/pending_approval-message-type-content.template.php
@@ -72,7 +72,7 @@
                                                                         
                                             <h5 class="">Contact Info:</h5>                                             
                                             <p>Phone: <strong>[CO_PHONE]</strong><br>
-                        Email: <strong><a href="ehttps://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=hseldon@trantor.com" target="_blank">[CO_EMAIL]</a></strong></p>
+                        Email: <strong><a href="[CO_EMAIL]" target="_blank">[CO_EMAIL]</a></strong></p>
 
                                         </td>
                                     </tr>
