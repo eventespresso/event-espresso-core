9003c9c4aebdb2cbf78b84a46dd1db2c4abca957
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2013-12-31 11:00:30 -0500

    Update help tours for registration form pages. [touch 4674]

--- admin_pages/registration_form/help_tours/Registration_Form_Questions_Overview_Help_Tour.class.php
+++ admin_pages/registration_form/help_tours/Registration_Form_Questions_Overview_Help_Tour.class.php
@@ -30,26 +30,79 @@ if (!defined('EVENT_ESPRESSO_VERSION') )
 class Registration_Form_Questions_Overview_Help_Tour extends EE_Help_Tour {
 
 	protected function _set_tour_properties() {
-		$this->_label = __('Questions Tour', 'event_espresso');
+		$this->_label = __('Questions Overview Tour', 'event_espresso');
 		$this->_slug = $this->_is_caf ? 'questions-overview-caf-joyride' : 'questions-overview-joyride';
 	}
-
 
 	protected function _set_tour_stops() {
 		$this->_stops = array(
 			10 => array(
 				'content' => $this->_start(),
 				),
-			15 => array(
-				'id' => 'add-new-question',
-				'content' => $this->_add_new_question_stop(),
+			20 => array(
+				'id' => 'id',
+				'content' => $this->_question_id_stop(),
 				'options' => array(
-					'tipLocation' => 'right',
-					'tipAdjustmentY' => -50,
-					'tipAdjustmentX' => 20
+					'tipLocation' => 'top',
+					'tipAdjustmentX' => -20,
+					'tipAdjustmentY' => -25
 					)
 				),
-			20 => array(
+			30 => array(
+				'id' => 'display_text',
+				'content' => $this->_display_text_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentX' => -5,
+					'tipAdjustmentY' => -25
+					)
+				),
+			40 => array(
+				'id' => 'admin_label',
+				'content' => $this->_admin_label_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentX' => 20,
+					'tipAdjustmentY' => -25
+					)
+				),
+			50 => array(
+				'id' => 'type',
+				'content' => $this->_type_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentX' => -5,
+					'tipAdjustmentY' => -25
+					)
+				),
+			60 => array(
+				'id' => 'values',
+				'content' => $this->_values_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentY' => -10,
+					'tipAdjustmentX' => -15
+					)
+				),
+			70 => array(
+				'id' => 'required',
+				'content' => $this->_required_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentY' => -20,
+					'tipAdjustmentX' => -15
+					)
+				),
+			80 => array(
+				'class' => 'bulkactions',
+				'content' => $this->_bulk_actions_stop(),
+				'options' => array(
+					'tipLocation' => 'top',
+					'tipAdjustmentY' => -30,
+					'tipAdjustmentX' => -15
+					)
+				),
+			90 => array(
 				'id' => 'event-espresso_page_espresso_registration_form-search-input',
 				'content' => $this->_search_stop(),
 				'options' => array(
@@ -58,91 +111,60 @@ class Registration_Form_Questions_Overview_Help_Tour extends EE_Help_Tour {
 					'tipAdjustmentX' => -15
 					)
 				),
-			30 => array(
-				'id' => 'display_text',
-				'content' => $this->_display_text_stop(),
+			100 => array(
+				'id' => 'add-new-question',
+				'content' => $this->_add_new_question_stop(),
 				'options' => array(
-					'tipLocation' => 'top',
-					'tipAdjustmentY' => -20
+					'tipLocation' => 'right',
+					'tipAdjustmentY' => -50,
+					'tipAdjustmentX' => 15
 					)
 				),
-			40 => array(
-				'id' => 'admin_label',
-				'content' => $this->_admin_label_stop(),
-				'options' => array(
-					'tipLocation' => 'top',
-					'tipAdjustmentY' => -20
-					)
-				),
-			50 => array(
-				'id' => 'type',
-				'content' => $this->_type_stop(),
-				'options' => array(
-					'tipLocation' => 'top',
-					'tipAdjustmentY' => -20,
-					'tipAdjustmentX' => -15
-					)
-				),
-			55 => array(
-				'id' => 'values',
-				'content' => $this->_values_stop(),
-				'options' => array(
-					'tipLocation' => 'top',
-					'tipAdjustmentY' => -20,
-					'tipAdjustmentX' => -15
-					)
-				),
-			60 => array(
-				'id' => 'required',
-				'content' => $this->_required_stop(),
-				'options' => array(
-					'tipLocation' => 'top',
-					'tipAdjustmentY' => -20,
-					'tipAdjustmentX' => -15
-					)
-				)
 			);
 	}
 
 
 	protected function _start() {
-		$content = '<h3>' . __('Registration Questions', 'event_espresso') . '</h3>';
-		$content .= '<p>' . __('Creating the perfect registration form has never been easier. Manage and organize (using drag and drop) your questions and question groups here.', 'event_espresso') . '</p>';
+		$content = '<h3>' . __('Questions Overview', 'event_espresso') . '</h3>';
+		$content .= '<p>' . __('This tour of the questions overview page will go over different areas of the screen to help you understand what they are used for.', 'event_espresso') . '</p>';
 		
 		return $content;
 	}
 
+	protected function _question_id_stop() {
+		return '<p>' . __('View the question id. Can be sorted in ascending or descending order.', 'event_espresso') . '</p>';
+	}
+
+	protected function _display_text_stop() {
+		return '<p>' . __('View available questions. You can reorder your questions by dragging and dropping them.', 'event_espresso') . '</p>';
+	}
+
+	protected function _admin_label_stop() {
+		return '<p>' . __('View the admin label for your questions.', 'event_espresso') . '</p>';
+	}
+
+	protected function _type_stop() {
+		return '<p>' . __('View the type of question. Available options are text, textarea, single, dropdown, multiple choice, and date.', 'event_espresso') . '</p>';
+	}
 	
+	protected function _values_stop() {
+		return '<p>' . __('View stored values for checkboxes, radio buttons, and select boxes.', 'event_espresso') . '</p>';
+	}
+
+	protected function _required_stop() {
+		return '<p>' . __('View if a question is required.', 'event_espresso') . '</p>';
+	}
+
+	protected function _bulk_actions_stop() {
+		return '<p>' . __('Perform bulk actions to multiple questions.', 'event_espresso') . '</p>';
+	}
 
 	protected function _search_stop() {
-		return '<p>' . __('Fields that will be searched with the value from the search are: the question name (display text)', 'event_espresso') . '</p>';
+		return '<p>' . __('Search through questions. The following sources will be searched: name of question (display text).', 'event_espresso') . '</p>';
 	}
 
 	protected function _add_new_question_stop() {
 		return '<p>' . __("Click here to add a new question.", 'event_espresso') . '</p>';
-	}
-
-
-	protected function _display_text_stop() {
-		return '<p>' . __('This column lists all your available questions. Questions can be reordered by dragging and dropping them to the desired location. The question title is what is seen on your registration form.', 'event_espresso') . '</p>';
-	}
-
-
-	protected function _admin_label_stop() {
-		return '<p>' . __('This column shows you the admin name for the question alongside any other pertinent data such as informing you that it is a system question.', 'event_espresso') . '</p>';
-	}
-
-
-	protected function _type_stop() {
-		return '<p>' . __('This column will inform you about the type of input field used by the question, such as drop down, text or text area.', 'event_espresso') . '</p>';
-	}
-	
-	protected function _values_stop() {
-		return '<p>' . __('This column will display the stored values for checkboxes, radio buttons, and select boxes.', 'event_espresso') . '</p>';
-	}
-
-	protected function _required_stop() {
-		return '<p>' . __('Here you can see if a question is a required question or not.', 'event_espresso') . '</p>';
 	}
 
 }
