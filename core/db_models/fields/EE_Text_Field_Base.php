24541da9a33f02a3c0239e360d1436481fad90cf
Michael Nelson <michael@eventespresso.com>
2013-12-31 15:02:56 -0800

    [touch:4690]{t:10}added some comments

--- core/db_models/fields/EE_Text_Field_Base.php
+++ core/db_models/fields/EE_Text_Field_Base.php
@@ -12,6 +12,12 @@ abstract class EE_Text_Field_Base extends EE_Model_Field_Base{
 		return is_string($value_of_field_on_model_object) ? stripslashes( $value_of_field_on_model_object ) : $value_of_field_on_model_object;
 	}
 	
+	/**
+	 * Accepts schema of 'form_input' which formats the string for echoing in form input's value.
+	 * @param string $value_on_field_to_be_outputted
+	 * @param string $schema
+	 * @return string
+	 */
 	function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null) {
 		if($schema=='form_input'){
 			$value_on_field_to_be_outputted = htmlentities($value_on_field_to_be_outputted, ENT_QUOTES, 'UTF-8');
