c8302559298b4c1cbcf7aa3f8f93bd19331f70d5
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-02 18:45:41 -0500

    Update help tab content for event pages. [touch: 4659]

---
+++ admin_pages/events/help_tabs/event_editor_event_registration_options.help_tab.php
@@ -0,0 +1,36 @@
+<h3><?php _e('Event Registration Options', 'event_espresso'); ?></h3>
+<p>
+<?php _e('Customize the registration for your event.', 'event_espresso'); ?>
+</p>
+<p>
+<ul>
+<li>
+<?php _e('Active Status', 'event_espresso'); ?><br />
+<?php _e('Shows the current status for an event. A status will appear as upcoming, active, or inactive.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Maximum number of tickets allowed per order for this event', 'event_espresso'); ?><br />
+<?php _e('How many tickets should a registrant be able to purchase in a single order?', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Display Description', 'event_espresso'); ?><br />
+<?php _e('Should the event description be shown on the registration page?', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Display Registration Form', 'event_espresso'); ?><br />
+<?php _e('Should the registration form be shown?', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Alternate Registration Page', 'event_espresso'); ?><br />
+<?php _e('Is another regisration page available? If so, enter it here as a website address (URL).', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Event Phone Number', 'event_espresso'); ?><br />
+<?php _e('Is a phone number available for this event?', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Require Pre-approval', 'event_espresso'); ?><br />
+<?php _e('Should registrations be manually approved by an event administrator?', 'event_espresso'); ?>
+</li>
+</ul>
+</p>
---
+++ admin_pages/events/help_tabs/events_overview_table_column_headings.help_tab.php
@@ -0,0 +1,37 @@
+<h3><?php _e('Table Column Headings', 'event_espresso'); ?></h3>
+<p>
+<ul>
+<li>
+<?php _e('ID', 'event_espresso'); ?><br />
+<?php _e('The is the numerical ID of an event.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Name', 'event_espresso'); ?><br />
+<?php _e('This is the name of an event.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Venue', 'event_espresso'); ?><br />
+<?php _e('The place that is hosting the event. Note: an event is not required to have a venue associated with it.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Event Start', 'event_espresso'); ?><br />
+<?php _e('The date that the event begins on.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('On Sale', 'event_espresso'); ?><br />
+<?php _e('The date that registration becomes available for an event.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Status', 'event_espresso'); ?><br />
+<?php _e('An event may have a status of upcoming, active, or inactive.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Registrations', 'event_espresso'); ?><br />
+<?php _e('The number of current registrations.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Actions', 'event_espresso'); ?><br />
+<?php _e('Using the action buttons you can view an event, edit event details, view registrations for an event, or view event reports.', 'event_espresso'); ?>
+</li>
+</ul>
+</p>
