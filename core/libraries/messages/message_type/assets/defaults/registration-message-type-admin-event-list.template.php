127b3b0443e8feb3db6a3be013250898248fc8f9
Seth <seth@eventespresso.com>
2014-01-03 13:16:23 -0700

    Adding Garth's suggested message. [touch:4606]

--- core/libraries/messages/message_type/assets/defaults/registration-message-type-admin-event-list.template.php
+++ core/libraries/messages/message_type/assets/defaults/registration-message-type-admin-event-list.template.php
@@ -1,13 +1,12 @@
 <!-- content -->
 
 <div class="content">
+	<p>The following attendee(s) have registered for the following event(s)</p>
 	<table>
 		<tr>
 			<td>
-				
 				<h3>Event:</h3>
 				<p>[EVENT_NAME]</p>
-				
 				<!-- Callout Panel -->
 				<p><a href="[EVENT_URL]">View Event &raquo;</a></p>
 				<!-- /Callout Panel --></td>
