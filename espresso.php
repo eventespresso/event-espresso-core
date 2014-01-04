904edeb0e852a2528f191db7b741c3e2070c6884
Darren Ethier <darren@roughsmootheng.in>
2014-01-03 20:03:18 -0500

    bump version to 4.1.194.alpha

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 		4.1.193.alpha
+  Version: 		4.1.194.alpha
   Author: 			Seth Shoultes
   Author URI: 		http://www.eventespresso.com
   License: 		GPLv2
@@ -31,7 +31,7 @@
 //Returns the plugin version
 if ( ! function_exists( 'espresso_version' )) {
 	function espresso_version() {
-		return '4.1.193.alpha';
+		return '4.1.194.alpha';
 	}
 } else {
 	deactivate_plugins( plugin_basename( __FILE__ ));
