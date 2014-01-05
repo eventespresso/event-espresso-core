6bc38340e556b01faeec9da322845d6fdd89780e
Darren Ethier <darren@roughsmootheng.in>
2014-01-05 13:07:37 -0500

    clean up documentation for default registration status setting help tab [touch: 4780]

--- admin_pages/events/help_tabs/events_default_settings_status.help_tab.php
+++ admin_pages/events/help_tabs/events_default_settings_status.help_tab.php
@@ -1,17 +1,13 @@
 <h3><?php _e('Default Registration Status', 'event_espresso'); ?></h3>
 <p>
 <strong><?php _e('Approved', 'event_espresso'); ?></strong><br />
-<?php _e('A status of approved means that an attendee\'s registration has been accepted as complete. Their registration will count towards the registration limit, remaining tickets, and calculations for available seats. /*This default status can be overridden for a specific event.*/', 'event_espresso'); ?>
+<?php _e('A status of approved means that an attendee\'s registration has been accepted as complete. Their registration will count towards the registration limit, remaining tickets, and calculations for available seats. Payments (if necessary) can also be made by primary registrants with this status.', 'event_espresso'); ?>
 </p>
 <p>
 <strong><?php _e('Pending', 'event_espresso'); ?></strong><br />
-<?php _e('A status of pending means that an attendee has not paid but they do have a space reserved for them for an event. If a user downloads an invoice, then their payment status will change from incomplete to pending, thereby reserving a space for them but not indicating that they have paid (since a payment would need to be recorded manually).', 'event_espresso'); ?>
+<?php _e('A status of pending means that an attendee has not paid but they ARE able to make payments (if necessary).  Pending registrations do NOT count towards registration limits, remaining tickets, and calculations for available seats.  If registration for a free event occurs, then registrations are automatically set to Approved.  For paid tickets, Pending registrations are set to Approved when full payment is recorded.', 'event_espresso'); ?>
 </p>
-<h3><?php _e('Pending Registrations Count Towards Registration Limits and Ticket Sales', 'event_espresso'); ?></h3>
 <p>
-<?php _e('When this option is set to yes, then the attendee\'s registration status will be set to pending. This means that their registration will count towards an event registration limit and therefore affect number of spaces remaining, ticket sale calculations, and seating options (if applicable).', 'event_espresso'); ?><br />
-</p>
-<h3><?php _e('Enable Attendee Pre-approval', 'event_espresso'); ?></h3>
-<p>
-<?php _e('When enabled, an event administrator must manually approve the registration. This is not linked to the transaction status.', 'event_espresso'); ?>
+<strong><?php _e('Not Approved', 'event_espresso'); ?></strong><br />
+<?php _e('Not Approved, means that an attendee has not paid and they cannot make payments for towards an event.  Not approved registrations do NOT count towards registration limits, remaining tickets, and calculations for available seats.  This setting differs from the "Pending" default in that free ticket registrations, and paid in full ticket registrations do NOT automatically switch the Registration to APPROVED.  Instead, the event admin must manually set the registration to either "Pending" for registrations requiring payments, or "Approved" for free events', 'event_espresso'); ?>
 </p>
