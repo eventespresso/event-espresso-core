1ce0b12d5736e2a690554d0971900c7ea53d33ca
Dean Robinson <info@apinapress.com>
2014-01-09 14:16:14 +0200

    Registration form help tabs update.

--- admin_pages/registration_form/help_tabs/registration_form_edit_question_group.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_edit_question_group.help_tab.php
@@ -2,20 +2,20 @@
 <?php _e('Group Name', 'event_espresso'); ?>
 </h2>
 <p>
-<?php _e("A name or heading for this group of questions that can be used to organize your Registration Form. For example: Address Information.(HTML not permitted)", 'event_espresso'); ?>
+<?php _e("A name or heading for this group of questions that can be used to organize your Registration Form. For example: Address Information (HTML not permitted). Be aware that this can be shown to your website visitors.", 'event_espresso'); ?>
 </p>
 
 <h2>
 <?php _e('Group Identifier', 'event_espresso'); ?>
 </h2>
 <p>
-<?php _e('The "Group Identifier" is a unique name for this group that can be used to distinguish it from all other groups in the system. A Group Identifier therefore can not be the same as any other. It will NOT be displayed to site visitors. If left blank, one will be automagically generated for you, ie: address-info-12345. (HTML not permitted)','event_espresso')?>
+<?php _e('The "Group Identifier" is a unique name for this group that can be used to distinguish it from all other groups in the system. A Group Identifier therefore can not be the same as any other. It will NOT be displayed to site visitors. If left blank, one will be automatically generated for you, ie: address-info-12345. (HTML not permitted)','event_espresso')?>
 </p>
 <h2>
 <?php _e('Group Description', 'event_espresso'); ?>
 </h2>
 <p>
-<?php _e('A description of this question group.(HTML permitted)','event_espresso')?>
+<?php _e('A description of this question group. This can be shown to your website visitors. (HTML permitted)','event_espresso')?>
 </p>
 <h2>
 <?php _e('Show Group Name', 'event_espresso'); ?>
@@ -28,4 +28,11 @@
 </h2>
 <p>
 <?php _e('Whether or not to show the group description on the registration page.','event_espresso')?>
+</p>
+
+<h2>
+<?php _e('Questions that appear in this group', 'event_espresso'); ?>
+</h2>
+<p>
+<?php _e('This lists all available custom questions. If the checbox is checked, then the question will appear within this question group.','event_espresso')?>
 </p>
--- admin_pages/registration_form/help_tabs/registration_form_question_groups.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_question_groups.help_tab.php
@@ -5,5 +5,15 @@
 <?php _e("Question Groups allow you to create modular registration forms. Each registration form consists of one or more question groups, and these are allocated on a per event basis.", 'event_espresso'); ?>
 </p>
 <p>
+<?php _e("The question groups overview table shows you your available question groups."); ?>
+</p>
+<ul>
+<li><?php _e("ID - The ID number of the question group. Mainly used internally and in some shortcodes."); ?></li>
+<li><?php _e("Group Name - This column displays the question group name as it will be displayed on the registration form."); ?></li>
+<li><?php _e("Description - This column displays the description as it will be displayed on the registration form."); ?></li>
+<li><?php _e("Show Name - Here you can see if the question group is set to show the question group name on when used in a registration form."); ?></li>
+<li><?php _e("Show Desc - Here you can see if the question group is set to show the question group description on when used in a registration form."); ?></li>
+</ul>
+<p>
 <?php _e("Dragging and dropping the question groups will change their order, and thus the order they are seen in on the registration form.", 'event_espresso'); ?>
 </p>
