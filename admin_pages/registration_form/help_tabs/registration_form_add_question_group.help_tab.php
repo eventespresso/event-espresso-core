f188e56502de436dcf80b038c06b29fba1ec8fd4
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-09 17:49:17 -0500

    Update help tab content for registration pages. [touch: 4665]

--- admin_pages/registration_form/help_tabs/registration_form_add_question_group.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_add_question_group.help_tab.php
@@ -1,39 +1,42 @@
-<h2>
-<?php _e('Group Name', 'event_espresso'); ?>
-</h2>
+<h2><?php _e('Add New Question Group', 'event_espresso'); ?></h2>
 <p>
-<?php _e("A name or heading for this group of questions that can be used to organize your Registration Form. For example: Address Information (HTML not permitted). Be aware that this can be shown to your website visitors.", 'event_espresso'); ?>
+<?php _e('This page allows you to add a new question group for Event Espresso.', 'event_espresso'); ?>
 </p>
-
-<h2>
-<?php _e('Group Identifier', 'event_espresso'); ?>
-</h2>
 <p>
-<?php _e('The "Group Identifier" is a unique name for this group that can be used to distinguish it from all other groups in the system. A Group Identifier therefore can not be the same as any other. It will NOT be displayed to site visitors. If left blank, one will be automatically generated for you, ie: address-info-12345. (HTML not permitted)','event_espresso')?>
+<ul>
+<li>
+<?php _e('Group Name', 'event_espresso'); ?><br />
+<?php _e('This is the name (title) of the question group as it will be displayed on the registration form. HTML cannot be used.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Group Identifier', 'event_espresso'); ?><br />
+<?php _e('This is a unique name (slug) for this group. It helps you tell the difference between this group and other groups that may appear to be similar. It is not shown to registrants and HTML cannot be used.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Group Description', 'event_espresso'); ?><br />
+<?php _e('This is the description for the question group as it will be shown on the registration form. This can be shown to registrants and HTML is allowed.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Question Group Order', 'event_espresso'); ?><br />
+<?php _e('Question Groups can be sorted by using the drag and drop feature on the Questions Group page. Another way to sort question groups is to adjust their order of application by using this field. A smaller order of application will be shown first. For example, a question group with an order of application of 1 will appear before one that has an order of application of 5.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Show Name', 'event_espresso'); ?><br />
+<?php _e('Whether the group name will be displayed on the registration form.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Show Description', 'event_espresso'); ?><br />
+<?php _e('Whether the description for the question group be will displayed on the registration form.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Questions that appear in this group', 'event_espresso'); ?><br />
+<?php _e('This shows all available custom questions. If checked, then that question will appear with this question group.', 'event_espresso'); ?>
+</li>
+</ul>
 </p>
-<h2>
-<?php _e('Group Description', 'event_espresso'); ?>
-</h2>
 <p>
-<?php _e('A description of this question group. This can be shown to your website visitors. (HTML permitted)','event_espresso')?>
+<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Add New Question Group Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
 </p>
-<h2>
-<?php _e('Show Group Name', 'event_espresso'); ?>
-</h2>
 <p>
-<?php _e('Whether or not to show the group name on the registration page.','event_espresso')?>
+<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
 </p>
-<h2>
-<?php _e('Show Group Description', 'event_espresso'); ?>
-</h2>
-<p>
-<?php _e('Whether or not to show the group description on the registration page.','event_espresso')?>
-</p>
-
-<h2>
-<?php _e('Questions that appear in this group', 'event_espresso'); ?>
-</h2>
-<p>
-<?php _e('This lists all available custom questions. If the checbox is checked, then the question will appear within this question group.','event_espresso')?>
-</p>
-
--- admin_pages/registration_form/help_tabs/registration_form_questions_overview.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_questions_overview.help_tab.php
@@ -1,21 +1,10 @@
+<h2><?php _e('Questions Overview', 'event_espresso'); ?></h2>
 <p>
-<?php _e("Questions are the heart of your registration form, and Event Espresso makes it easy to control what questions your visitors will see. There are a variety of different question types, from a simple text field, to drop downs and radio buttons.", 'event_espresso'); ?>
+<?php _e('This page shows all questions that have been created with Event Espresso.', 'event_espresso'); ?>
 </p>
 <p>
-<?php _e("The questions overview table shows you your available questions."); ?>
+<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Questions Overview Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
 </p>
-<ul>
-<li><?php _e("ID - The ID number of the question. Mainly used internally and in some shortcodes."); ?></li>
-<li><?php _e("Question - This column displays the question as it will be displayed on the registration form."); ?></li>
-<li><?php _e("Admin Label - Only shown to the admin, this can help you differentiate between questions that might be called the same but used in different situations, or other internal notes. Also advises if the question is a system question."); ?></li>
-<li><?php _e("Type - Displays what type of question it is, e.g. drop down, text field, text area, etc."); ?></li>
-<li><?php _e("Values - If the question can have multiple values (like drop down and radio buttons) then those values are displayed here. Otherwise it will say N/A"); ?></li>
-<li><?php _e("Req - Short for required and this column allows you to see which questions must be answered when used in a registration form."); ?></li>
-</ul>
 <p>
-<?php _e("Dragging and dropping the questions will change their appearance priority, so a question at the top of the list will appear higher up in any question group it is assigned to, and one that is at the bottom of the list will appear lower down.", 'event_espresso'); ?>
-</p>
-
-<p>
-<?php _e("A note about System Questions: these questions are created by Event Espresso and cannot be removed, though they can be edited. The First name, Last Name and Email questions are necessary for the plugin to obtain valid registrations, and should only be modified at your own risk."); ?>
+<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content and even adjust the pagination for questions.', 'event_espresso'); ?>
 </p>
