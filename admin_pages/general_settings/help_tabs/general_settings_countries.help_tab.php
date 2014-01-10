211e7786136259420394078ebd2ef93a59db1ef5
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-09 23:16:24 -0500

    Update help tabs content for general settings pages. [touch:4666]

--- admin_pages/general_settings/help_tabs/general_settings_countries.help_tab.php
+++ admin_pages/general_settings/help_tabs/general_settings_countries.help_tab.php
@@ -1,20 +1,22 @@
-<h2>
-<?php _e('Select Your Country', 'event_espresso'); ?>
-</h2>
+<h2><?php _e('Your Organization', 'event_espresso'); ?></h2>
 <p>
-<?php echo __('Add the country where your business is located.', 'event_espresso'); ?>
+<?php _e("This page shows various options for your organization.", 'event_espresso'); ?>
 </p>
-
-<h2>
-<?php _e('Country Details', 'event_espresso'); ?>
-</h2>
+<h3><?php _e('Countries and States / Provinces', 'event_espresso'); ?></h3>
 <p>
-<?php echo __('Here you can fine tune currency and contact data for your own country.', 'event_espresso'); ?>
+<?php _e('Select your country from the dropdown field. This option will re-load options for states / provinces.', 'event_espresso'); ?>
 </p>
-
-<h2>
-<?php _e('States / Provinces', 'event_espresso'); ?>
-</h2>
+<h3><?php _e('Country Details', 'event_espresso'); ?></h3>
 <p>
-<?php echo __('Used in certain areas of the plugin, here you can define what states/provinces will be displayed in case you do not do business in certain areas.', 'event_espresso'); ?>
+<?php _e('Fine tune currency and contact data for your own country.', 'event_espresso'); ?>
+</p>
+<h3><?php _e('States / Provinces', 'event_espresso'); ?></h3>
+<p>
+<?php _e('Define the states / provinces that are used. New states / provinces can be added.', 'event_espresso'); ?>
+</p>
+<p>
+<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Countries Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
+</p>
+<p>
+<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
 </p>
--- admin_pages/general_settings/help_tabs/general_settings_templates.help_tab.php
+++ admin_pages/general_settings/help_tabs/general_settings_templates.help_tab.php
@@ -1,26 +1,53 @@
-<h2><?php _e('Event List Pages', 'event_espresso'); ?></h2>
-<p><?php _e('Define how your basic event list will look.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Default Event List Type', 'event_espresso'); ?></h2>
-<p><?php _e('Define how your basic event list will look.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Grid Size', 'event_espresso'); ?></h2>
-<p><?php _e('If your <strong>Default Event List Type</strong> is "Grid View", these settings modify the size of the grids.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Display Descriptions', 'event_espresso'); ?></h2>
-<p><?php _e('Choose whether descriptions are shown on the event list, and how big they will be.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Display Address', 'event_espresso'); ?></h2>
-<p><?php _e('Choose whether the venue address is displayed in the event list.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Display Venue Details', 'event_espresso'); ?></h2>
-<p><?php _e('Choose whether the venue information is displayed in the event list.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Display Expired Events', 'event_espresso'); ?></h2>
-<p><?php _e('Choose whether to show expired events or not on the default event list.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Event Listings URL', 'event_espresso'); ?></h2>
-<p><?php _e('This is the address to your event listings page.', 'event_espresso'); ?></p>
-
-<h2><?php _e('Display Addresses', 'event_espresso'); ?></h2>
-<p><?php _e('Choose whether to display the venues address in the single registration page or not.', 'event_espresso'); ?></p>
+<h2><?php _e('Templates', 'event_espresso'); ?></h2>
+<p>
+<?php _e("This page shows options for event list pages and registration pages.", 'event_espresso'); ?>
+</p>
+<h3><?php _e('Event List Pages', 'event_espresso'); ?></h3>
+<ul>
+<li>
+<?php _e('Default Event List Type', 'event_espresso'); ?><br />
+<?php _e('Define how your basic event list will appear.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Grid Size', 'event_espresso'); ?><br />
+<?php _e('Select the size of the grid for your event list page. Adjust as needed to provide a layout that matches your theme.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Display Descriptions', 'event_espresso'); ?><br />
+<?php _e('Whether descriptions should be shown on the event list page. If yes, you can show a short description or the full description.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Display Address', 'event_espresso'); ?><br />
+<?php _e('Whether the address should be shown on the event list page.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Display Venue Details', 'event_espresso'); ?><br />
+<?php _e('Whether the venue details should be shown on the event list page.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Display Expired Events', 'event_espresso'); ?><br />
+<?php _e('Whether expired events should be shown on the event list page.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Event Listings URL', 'event_espresso'); ?><br />
+<?php _e('This is the URL (website address) for your event list page.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Reset Event List Settings', 'event_espresso'); ?><br />
+<?php _e('This option allows you to reset your event list settings to defaults.', 'event_espresso'); ?>
+</li>
+</ul>
+<h3><?php _e('Registration Pages', 'event_espresso'); ?></h3>
+<ul>
+<li>
+<?php _e('Display Addresses', 'event_espresso'); ?><br />
+<?php _e('Whether addresses should be shown on the registration pages.', 'event_espresso'); ?>
+</li>
+</li>
+</ul>
+</p>
+<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Templates Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
+</p>
+<p>
+<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
+</p>
