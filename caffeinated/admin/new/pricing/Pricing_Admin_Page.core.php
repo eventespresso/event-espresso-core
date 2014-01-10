bf191f5a08da89875021d6242a35492d8348919a
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-10 17:58:19 -0500

    Change label for trash can to trash on pricing pages.

--- caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
+++ caffeinated/admin/new/pricing/Pricing_Admin_Page.core.php
@@ -385,7 +385,7 @@ class Pricing_Admin_Page extends EE_Admin_Page {
 				),
 			'trashed' => array(
 					'slug' => 'trashed',
-					'label' => __('Trash Can', 'event_espreso'),
+					'label' => __('Trash', 'event_espreso'),
 					'count' => 0,
 					'bulk_action' => array(
 							'restore_price_type' => __('Restore from Trash', 'event_espresso'),
