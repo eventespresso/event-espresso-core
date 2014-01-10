d4a138d1eaacbb5027973a66e81d56aa256cfb3f
Darren Ethier <darren@roughsmootheng.in>
2014-01-10 01:19:50 -0500

    setup transaction status array to be localized string for transaction admin pages [touch: 4858]

--- admin_pages/transactions/Transactions_Admin_Page.core.php
+++ admin_pages/transactions/Transactions_Admin_Page.core.php
@@ -218,7 +218,7 @@ class Transactions_Admin_Page extends EE_Admin_Page {
 	*		@return void
 	*/
 	private function _get_transaction_status_array() {
-		self::$_txn_status = EEM_Transaction::instance()->status_array();
+		self::$_txn_status = EEM_Transaction::instance()->status_array(TRUE);
 	}
 
 
