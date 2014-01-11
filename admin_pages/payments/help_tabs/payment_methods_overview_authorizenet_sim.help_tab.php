ebf23e3c966d99eeb4d49fe496caa1e154db36bb
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-10 19:39:54 -0500

    Update help tabs for payment methods pages (framework only). [touch:4667}

---
+++ admin_pages/payments/help_tabs/payment_methods_overview_authorizenet_sim.help_tab.php
@@ -0,0 +1,8 @@
+<h3><?php _e('Authorize.net SIM', 'event_espresso'); ?></h3>
+<p>
+<?php _e('TO-DO (LOC)', 'event_espresso'); ?>
+</p>
+<h3><?php _e('Authorize.net SIM Settings', 'event_espresso'); ?></h3>
+<p>
+<?php _e('TO-DO (LOC)', 'event_espresso'); ?>
+</p>
---
+++ admin_pages/payments/help_tabs/payment_methods_overview_eway.help_tab.php
@@ -0,0 +1,8 @@
+<h3><?php _e('eWay', 'event_espresso'); ?></h3>
+<p>
+<?php _e('TO-DO (LOC)', 'event_espresso'); ?>
+</p>
+<h3><?php _e('eWay Settings', 'event_espresso'); ?></h3>
+<p>
+<?php _e('TO-DO (LOC)', 'event_espresso'); ?>
+</p>
