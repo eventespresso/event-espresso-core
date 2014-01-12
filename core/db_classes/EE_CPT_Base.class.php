677c00d554d06cd4e2e681e2363c868637a458d7
Darren Ethier <darren@roughsmootheng.in>
2014-01-12 11:49:18 -0500

    fix typo [touch: 4891]

--- core/db_classes/EE_CPT_Base.class.php
+++ core/db_classes/EE_CPT_Base.class.php
@@ -292,6 +292,6 @@ abstract class EE_CPT_Base extends EE_Soft_Delete_Base_Class{
 
 
 	public function get_custom_post_statuses() {
-		return $this->get_model()->geT_custom_post_statuses();
+		return $this->get_model()->get_custom_post_statuses();
 	}
 }
