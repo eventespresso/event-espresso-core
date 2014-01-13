616d499fe077b43780131c626c01102b5ec65c98
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-13 10:48:29 -0500

    Update help tab content for payment methods pages. [4667]

--- admin_pages/payments/help_tabs/payment_methods_overview_authorizenet_aim.help_tab.php
+++ admin_pages/payments/help_tabs/payment_methods_overview_authorizenet_aim.help_tab.php
@@ -1,8 +1,27 @@
 <h3><?php _e('Authorize.net AIM', 'event_espresso'); ?></h3>
 <p>
-<?php _e('TO-DO (LOC)', 'event_espresso'); ?>
+<?php _e('Adjust the settings for the Authorize.net AIM payment gateway.', 'event_espresso'); ?>
 </p>
 <h3><?php _e('Authorize.net AIM Settings', 'event_espresso'); ?></h3>
-<p>
-<?php _e('TO-DO (LOC)', 'event_espresso'); ?>
-</p>
+<ul>
+<li>
+<?php _e('Authorize.net AIM Login ID', 'event_espresso'); ?><br />
+<?php _e('Enter your API Login ID for Authorize.net. Learn how to find your <a href="https://support.authorize.net/authkb/index?page=content&id=A405">API Login ID</a>.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Authorize.net AIM Transaction Key', 'event_espresso'); ?><br />
+<?php _e('Enter your Transaction Key for Authorize.net. Learn how to find your <a href="https://support.authorize.net/authkb/index?page=content&id=A405">Transaction Key</a>.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Is this an account on the Authorize.net development server?', 'event_espresso'); ?><br />
+<?php _e('Specify whether this is a live / production account or a test account on the Authorize.net development server.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Do you want to submit a test transaction?', 'event_espresso'); ?><br />
+<?php _e('Specify if you want to test the Authorize.net AIM payment gateway by submitting a test transaction. Be sure to turn this setting off when you are done testing.', 'event_espresso'); ?>
+</li>
+<li>
+<?php _e('Button Image URL', 'event_espresso'); ?><br />
+<?php _e('Change the image that is used for this payment gateway.', 'event_espresso'); ?>
+</li>
+</ul>
