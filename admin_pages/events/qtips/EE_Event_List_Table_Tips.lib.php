2b2e4956a9a5866bdc10751cf4d0dc5f87215ed5
Darren Ethier <darren@roughsmootheng.in>
2014-01-13 17:34:59 -0500

    add qtip for attendee icon on event list table [touch: 4901]

---
+++ admin_pages/events/qtips/EE_Event_List_Table_Tips.lib.php
@@ -0,0 +1,51 @@
+<?php
+if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('NO direct script access allowed'); }
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for Wordpress
+ *
+ * @package		Event Espresso
+ * @author		Seth Shoultes
+ * @copyright	(c)2009-2012 Event Espresso All Rights Reserved.
+ * @license		http://eventespresso.com/support/terms-conditions/  ** see Plugin Licensing **
+ * @link		http://www.eventespresso.com
+ * @version		4.0
+ *
+ * ------------------------------------------------------------------------
+ *
+ * EE_Event_List_Table_Tips	
+ *
+ * Qtip config for the event editor.
+ *
+ * @package		Event Espresso
+ * @subpackage	/admin_pages/events/qtips/EE_Event_List_Table_Tips.helper.php
+ * @author		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Event_List_Table_Tips extends EE_Qtip_Config {
+
+
+	protected function _set_tips_array() {
+		$this->_qtipsa = array(
+			0 => array(
+				'content_id' => 'attendee-column-tip',
+				'target' => '.column-attendees .dashicons-groups',
+				'content' => __('Approved Attendees', 'event_espresso'),
+				'options' => array( 
+					'position' => array(
+						'my' => 'bottom left',
+						'at' => 'top right',
+						'adjust' => array(
+							'x' => 0,
+							'y' => 0
+							)
+						)
+					)
+				)
+			);
+	}
+
+}
