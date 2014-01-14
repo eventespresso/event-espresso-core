a81e9144eb19e3d73e539400bccd4237dfc229c6
Seth <seth@eventespresso.com>
2014-01-14 13:53:00 -0700

    formatting the HTML for the payment reminders. [touch:4603]

--- core/libraries/messages/message_type/assets/defaults/payment-message-type-ticket-list.template.php
+++ core/libraries/messages/message_type/assets/defaults/payment-message-type-ticket-list.template.php
@@ -1,5 +1,7 @@
 <ul>
-<li><strong>Ticket Name:</strong> [TICKET_NAME]<br><em>[TICKET_DESCRIPTION]</em></li>
+<li><strong>Registrant Name:</strong> [FNAME] [LNAME]</li>
+<li class="ticket-name"><strong>Ticket Name:</strong> [TICKET_NAME]<br><em>[TICKET_DESCRIPTION]</em></li>
 <li><strong>Price:</strong> [TICKET_PRICE]</li>
 <li><strong>Quantity Purchased:</strong> [TKT_QTY_PURCHASED]</li>
 </ul>
+<hr />
