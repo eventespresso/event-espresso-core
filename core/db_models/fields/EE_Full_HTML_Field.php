18fa09f167e718ea70b7658e497b50a2e8c3f05d
Michael Nelson <michael@eventespresso.com>
2014-01-14 09:22:04 -0800

    [touch:4916]{t:10}added schmea no_wpautop to full html output fields to avoid the extra p tags when outputting

--- core/db_models/fields/EE_Full_HTML_Field.php
+++ core/db_models/fields/EE_Full_HTML_Field.php
@@ -4,7 +4,7 @@ class EE_Full_HTML_Field extends EE_Text_Field_Base{
 
 
 	/**
-	 * Does shortcodes and auto-paragraphs the content
+	 * Does shortcodes and auto-paragraphs the content (unless schema is 'no_wpautop')
 	 * @param type $value_on_field_to_be_outputted
 	 * @param type $schema
 	 * @return string
@@ -12,6 +12,8 @@ class EE_Full_HTML_Field extends EE_Text_Field_Base{
 	function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null) {
 		if($schema =='form_input'){
 			return parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema);
+		}elseif($schema == 'no_wpautop'){
+			return do_shortcode(parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema));
 		}else{
 			return wpautop(do_shortcode(parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema)));
 		}
