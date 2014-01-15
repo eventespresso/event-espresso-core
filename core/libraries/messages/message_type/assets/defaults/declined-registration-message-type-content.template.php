6587a2a14b9db8777b04bf25877f3d8af2c3e8df
Seth <seth@eventespresso.com>
2014-01-15 13:08:04 -0700

    html and css changes to the declined messages. [touch:4605]

--- core/libraries/messages/message_type/assets/defaults/declined-registration-message-type-content.template.php
+++ core/libraries/messages/message_type/assets/defaults/declined-registration-message-type-content.template.php
@@ -29,7 +29,7 @@
                 <table>
                     <tr>
                         <td>
-                            <h2>Hello, [FNAME]</h2>
+                            <h2>Hello, [FNAME]:</h2>
                             <p class="lead">This is a copy of your registration details!</p>
                             
     
