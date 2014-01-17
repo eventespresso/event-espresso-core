8ce7bb6f7a52dbcdbd5e77b728b7a94537596c47
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-17 16:19:57 -0500

    Update the Help Support Shortcodes Page [touch:4929]

--- admin_pages/support/templates/support_admin_details_shortcodes_event_listings.template.php
+++ admin_pages/support/templates/support_admin_details_shortcodes_event_listings.template.php
@@ -3,14 +3,16 @@
 <?php _e('Returns a list of events', 'event_espresso'); ?>
 		</p>
 		<ul>
-			<li>[EVENT_LIST]</li>
-			<li>[EVENT_LIST limit=1]</li>
-			<li>[EVENT_LIST show_expired=true]</li>
-			<li>[EVENT_LIST show_deleted=true]</li>
-			<li>[EVENT_LIST show_secondary=true]</li>
-			<li>[EVENT_LIST category_identifier=your_category_identifier]</li>
-			<li>[EVENT_LIST staff_id=staff_id_number]</li>
-			<li>[EVENT_LIST order_by=date(start_date),id]</li>
+			<li>[ESPRESSO_EVENTS]</li>
+			<li>[ESPRESSO_EVENTS title="My Super Event"]</li>
+			<li>[ESPRESSO_EVENTS limit=5]</li>
+			<li>[ESPRESSO_EVENTS css_class="my-custom-class"]</li>
+			<li>[ESPRESSO_EVENTS month="October 2104"]</li>
+			<li>[ESPRESSO_EVENTS show_expired=true]</li>
+			<li>[ESPRESSO_EVENTS category_slug="free-events"]</li>
+			<li>[ESPRESSO_EVENTS order_by="start_date,id"]</li>
+			<li>[ESPRESSO_EVENTS sort="ASC"]</li>
+			<li>[ESPRESSO_EVENTS list_type="grid"]</li>
 		</ul>
 		<h4>
 			<?php _e('Order by parameters:', 'event_espresso'); ?>
@@ -27,7 +29,8 @@
 			city<br />
 			state<br />
 			category_id<br />
-			venue_title </p>
+			venue_title 
+		</p>
 			<p><strong><?php _e('Attention:', 'event_espresso'); ?></strong><br />
 <?php _e('The [EVENT_LIST] shortcode should not be used as a replacement for the [ESPRESSO_EVENTS] shortcode. Replacing the [ESPRESSO_EVENTS] shortcode will break your registration pages.', 'event_espresso'); ?>
 		</p>
