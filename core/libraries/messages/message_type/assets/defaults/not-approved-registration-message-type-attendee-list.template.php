0a112261c7f7d97b253e9ae8ba4832291239d15a
Seth <seth@eventespresso.com>
2014-01-17 08:38:15 -0700

    Adding the registration code and some html formatting. [touch:4153]

--- core/libraries/messages/message_type/assets/defaults/not-approved-registration-message-type-attendee-list.template.php
+++ core/libraries/messages/message_type/assets/defaults/not-approved-registration-message-type-attendee-list.template.php
@@ -1,5 +1,5 @@
 <h5 class="attendee-name">[FNAME] [LNAME]</h5>
+		<p><strong>Registration Code:</strong> [REGISTRATION_CODE]</p>
 		<p><strong>Tickets:</strong></p>
-		[TICKET_LIST]
-		
+		[TICKET_LIST]		
 <hr />
