62972046189113e389123db823e4c7ac5536b5b1
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-19 22:13:34 -0500

    Update help tours for registration form pages. [touch: 4674]

--- admin_pages/registration_form/help_tours/Registration_Form_Add_Question_Help_Tour.class.php
+++ admin_pages/registration_form/help_tours/Registration_Form_Add_Question_Help_Tour.class.php
@@ -108,13 +108,13 @@ class Registration_Form_Add_Question_Help_Tour extends EE_Help_Tour {
 
 	protected function _start() {
 		$content = '<h3>' . __('Add Question', 'event_espresso') . '</h3>';
-		$content .= '<p>' . __('This tour of the add question page will go over different areas of the screen to help you understand what they are used for.', 'event_espresso') . '</p>';
+		$content .= '<p>' . __('This tour of the Add Question page will go over different areas of the screen to help you understand what they are used for.', 'event_espresso') . '</p>';
 		
 		return $content;
 	}
 
 	protected function _qst_display_text_stop() {
-		return '<p>' . __('The question that is displayed to registrants who are signing up for events.', 'event_espresso') . '</p>';
+		return '<p>' . __('This is the question that is displayed to registrants who are signing up for events.', 'event_espresso') . '</p>';
 	}
 
 	protected function _qst_admin_label_stop() {
@@ -126,7 +126,7 @@ class Registration_Form_Add_Question_Help_Tour extends EE_Help_Tour {
 	}
 
 	protected function _qst_admin_only_stop() {
-		return '<p>' . __('Should this question be shown to only admins?', 'event_espresso') . '</p>';
+		return '<p>' . __('Specify whether this question should be shown only to admins.', 'event_espresso') . '</p>';
 	}
 
 	protected function _qst_type_stop() {
@@ -134,7 +134,7 @@ class Registration_Form_Add_Question_Help_Tour extends EE_Help_Tour {
 	}
 
 	protected function _qst_required_stop() {
-		return '<p>' . __('Should this question be required?', 'event_espresso') . '</p>';
+		return '<p>' . __('Specify whether this question should be required.', 'event_espresso') . '</p>';
 	}
 
 	protected function _qst_required_text_stop() {
