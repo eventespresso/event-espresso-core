9cbcb47185cf1ef78a1d05d836a4306acba37066
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-21 17:10:08 -0500

    Update help tab content for event pages (add datetimes limit field to event tickets help tab) [touch:4659]

--- admin_pages/events/help_tabs/event_editor_event_tickets.help_tab.php
+++ admin_pages/events/help_tabs/event_editor_event_tickets.help_tab.php
@@ -6,6 +6,10 @@
 <?php _e('HTML tags cannot be used on the ticket title (name) but they can be used in the ticket description. The quantity in the "Sold" column for Available Tickets shows how many of this type of ticket has been sold. The quantity in the "Regs" column for Available Tickets shows the amount of registrations that have occurred from these tickets.', 'event_espresso'); ?>
 </p>
 <p>
+<?php _e('<strong>Event Datetimes Limit Field</strong><br />', 'event_espresso'); ?>
+<?php _e('The event datetimes limit field allows you to set a maximum number of tickets that you want to make available for an event datetime. For example, lets say that we had 70 free tickets available and 30 premium tickets available. If we set a limit of 50 using the event datetimes limit field, then the ticket sales will close once any combination of 50 tickets are sold.', 'event_espresso'); ?>
+</p>
+<p>
 <strong><?php _e('Advanced Settings', 'event_espresso'); ?></strong><br />
 <?php _e('The Advanced Settings icon<span class="gear-icon dashicons dashicons-admin-generic"></span> allows more customization to your tickets. You can add a description, set a number for datetimes, set a minimum quantity required to purchase, and set a maximum quantity. Price modifiers allow you to setup discounts or surcharges for your tickets. Additionally, you can mark a ticket as taxable by placing a checkmark in the taxable checkbox.', 'event_espresso'); ?>
 </p>
