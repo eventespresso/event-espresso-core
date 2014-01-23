01ed73fe1e1e3438b09f0ac1d3033959154b57b3
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-22 23:46:49 -0500

    Update help tabs for payment methods pages. [touch:4667]

--- admin_pages/payments/help_tabs/payment_methods_overview_bank_draft.help_tab.php
+++ admin_pages/payments/help_tabs/payment_methods_overview_bank_draft.help_tab.php
@@ -5,31 +5,31 @@
 <h3><?php _e('Bank Draft Settings', 'event_espresso'); ?></h3>
 <ul>
 <li>
-<?php _e('Page Title', 'event_espresso'); ?><br />
+<?php _e('<strong>Page Title</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter a title for this page.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Payment Instructions', 'event_espresso'); ?><br />
+<?php _e('<strong>Payment Instructions</strong>', 'event_espresso'); ?><br />
 <?php _e('Provide clear instructions for how a payment should be made.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Name on Bank Account', 'event_espresso'); ?><br />
+<?php _e('<strong>Name on Bank Account</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter the full name on the bank account.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Bank Account #', 'event_espresso'); ?><br />
+<?php _e('<strong>Bank Account #</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter the bank account number.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Bank Name', 'event_espresso'); ?><br />
+<?php _e('<strong>Bank Name</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter the name of the bank.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Bank Address', 'event_espresso'); ?><br />
+<?php _e('<strong>Bank Address</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter the address for the bank.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Button Image URL', 'event_espresso'); ?><br />
+<?php _e('<strong>Button Image URL</strong>', 'event_espresso'); ?><br />
 <?php _e('Change the image that is used for this payment gateway.', 'event_espresso'); ?>
 </li>
 </ul>
--- admin_pages/payments/help_tabs/payment_methods_overview_invoice.help_tab.php
+++ admin_pages/payments/help_tabs/payment_methods_overview_invoice.help_tab.php
@@ -5,42 +5,42 @@
 <h3><?php _e('Invoice Display Settings', 'event_espresso'); ?></h3>
 <ul>
 <li>
-<?php _e('Select Stylesheet', 'event_espresso'); ?><br />
+<?php _e('<strong>Select Stylesheet</strong>', 'event_espresso'); ?><br />
 <?php _e('Choose from 7 different styles for your invoice.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Instructions', 'event_espresso'); ?><br />
+<?php _e('<strong>Instructions</strong>', 'event_espresso'); ?><br />
 <?php _e('Provide instructions for how the invoice should be paid.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Logo Image', 'event_espresso'); ?><br />
+<?php _e('<strong>Logo Image</strong>', 'event_espresso'); ?><br />
 <?php _e('Upload a logo for your invoice. This will appear in the top left area of the invoice.', 'event_espresso'); ?>
 </li>
 </ul>
 <h3><?php _e('Invoice Gateway Settings', 'event_espresso'); ?></h3>
 <ul>
 <li>
-<?php _e('Show as an option on the payment page?', 'event_espresso'); ?><br />
+<?php _e('<strong>Show as an option on the payment page?</strong>', 'event_espresso'); ?><br />
 <?php _e('Specify whether invoices will be shown as a payment option on your payment page.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Invoice Title', 'event_espresso'); ?><br />
+<?php _e('<strong>Invoice Title</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter a title for your invoice.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Invoice Instructions', 'event_espresso'); ?><br />
+<?php _e('<strong>Invoice Instructions</strong>', 'event_espresso'); ?><br />
 <?php _e('Provide instructions for how the invoice should be paid.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Payable To', 'event_espresso'); ?><br />
+<?php _e('<strong>Payable To</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter the legal name of your business / organization.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Address to Send Payment', 'event_espresso'); ?><br />
+<?php _e('<strong>Address to Send Payment</strong>', 'event_espresso'); ?><br />
 <?php _e('Enter the address where the payment should be mailed to.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Button Image URL', 'event_espresso'); ?><br />
+<?php _e('<strong>Button Image URL</strong>', 'event_espresso'); ?><br />
 <?php _e('Change the image that is used for the invoice. This is optional.', 'event_espresso'); ?>
 </li>
 </ul>
