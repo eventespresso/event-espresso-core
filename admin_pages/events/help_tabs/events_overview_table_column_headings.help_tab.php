e5737b1e78cc61c5785c59e4cd02fa03ca086b73
Lorenzo Orlando Caum <lorenzocaum@Enzo-MacBook-Pro.local>
2014-01-22 21:44:06 -0500

    Update help tabs for event pages. [touch: 4659]

--- admin_pages/events/help_tabs/events_overview_table_column_headings.help_tab.php
+++ admin_pages/events/help_tabs/events_overview_table_column_headings.help_tab.php
@@ -2,37 +2,37 @@
 <p>
 <ul>
 <li>
-<?php _e('ID', 'event_espresso'); ?><br />
+<?php _e('<strong>ID</strong>', 'event_espresso'); ?><br />
 <?php _e('The is the numerical ID of an event. This value is used internally for Event Espresso.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Name', 'event_espresso'); ?><br />
+<?php _e('<strong>Name</strong>', 'event_espresso'); ?><br />
 <?php _e('This is the name of an event. ', 'event_espresso'); ?>
 <?php _e('Hovering over the Event Name will provide you will several options: View, Edit, Registrations, Export, and Trash. Clicking on View will take you to the event on the frontend of your site. Clicking on Edit will open the event for editing in the Event Editor. Clicking on Registrations will take you to a page that shows a list of attendees for that event. Clicking on Export will download a CSV for that event. Clicking on Trash will move that event to the trash. Note: an event can be completely deleted only after associated payments have been removed and registrations have been removed.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Venue', 'event_espresso'); ?><br />
+<?php _e('<strong>Venue</strong>', 'event_espresso'); ?><br />
 <?php _e('The place that is hosting the event. Note: an event is not required to have a venue associated with it.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Event Start', 'event_espresso'); ?><br />
+<?php _e('<strong>Event Start</strong>', 'event_espresso'); ?><br />
 <?php _e('The date that the event begins.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('On Sale', 'event_espresso'); ?><br />
+<?php _e('<strong>On Sale</strong>', 'event_espresso'); ?><br />
 <?php _e('The date that the first ticket is available for registration.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Registrations', 'event_espresso'); ?><br />
+<?php _e('<strong>Registrations</strong>', 'event_espresso'); ?><br />
 <?php _e('The number of registrations that have been approved. Clicking on this number will take you to a page that shows you a list of registrants for that event.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('Actions', 'event_espresso'); ?><br />
+<?php _e('<strong>Actions</strong>', 'event_espresso'); ?><br />
 <?php _e('Using the action buttons you can View event, Edit Event Details, View Registrations for Event, or View Event Reports.', 'event_espresso'); ?>
 </li>
 </ul>
 </p>
 <p>
-<?php _e('Event Name (Status)', 'event_espresso'); ?><br />
+<?php _e('<strong>Event Name (Status)</strong>', 'event_espresso'); ?><br />
 <?php _e('The name of an event may also have a status that appears directly to the left of the name. Some examples may be "event name (draft)" or "event name (sold_out). This status next to the event name is shown when the post status for an event is set to something different than published. This status is different than the status that is shown in the status column. When tickets for an event are sold out, the post status will automatically change to sold out. However, its still possible for an event admin to manually change this status from sold out back to pubished.', 'event_espresso'); ?>
 </p>
