2fc05e60a4d4be2bb15985dcc69c6ce4154cbb4b
Brent Christensen <brent@eventespresso.com>
2014-01-23 21:00:53 -0800

    add 2014 entry meta with added EE goodness [touch:4774]

--- templates/espresso_default/content-espresso_events-details.php
+++ templates/espresso_default/content-espresso_events-details.php
@@ -1,12 +1,23 @@
 <?php //echo '<h1>' . __FILE__ . '</h1>'; ?>
 <?php global $post; ?>
 <div class="event-content">
+	<div class="entry-meta">
+		<span class="tags-links"><?php espresso_event_categories( $post->ID, TRUE, TRUE ); ?><?php// echo $venue_categories; ?></span>
+		<?php
+			twentyfourteen_posted_on();
+			if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+		?>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
+		<?php
+			endif;
+
+			edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
+		?>
+	</div>
+
 	<h3 class="about-event-h3 ee-event-h3">
 		<span class="ee-icon ee-icon-event"></span><?php _e( 'Event Details', 'event_espresso' ); ?>
 	</h3>
-	<p>
-		<span class="tags-links"><?php espresso_event_categories( $post->ID ); ?></span>
-	</p>
 	<?php if ( espresso_event_phone( $post->ID, FALSE ) != '' ) : ?>
 	<p>
 		<span class="small-text"><strong><?php _e( 'Event Phone:', 'event_espresso' ); ?> </strong></span> <?php espresso_event_phone( $post->ID ); ?>
