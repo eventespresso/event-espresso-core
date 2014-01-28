f4b3bc57538c3fd0b2fb81619eb6973542bac4e6
Brent Christensen <brent@eventespresso.com>
2014-01-28 09:54:42 -0800

    break out events archive loop into it's own template part so that event list shortcodes don't load an ENTIRE tempalte with header, sidebar, footer, etc [touch:5086]

--- templates/Espresso_Arabica_2014/archive-espresso_events.php
+++ templates/Espresso_Arabica_2014/archive-espresso_events.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Event List
  *
- * This is template will display a list of your events 
+ * This template will display a list of your events 
  *
  * @ package		Event Espresso - Event Registration and Management Plugin for WordPress
  * @ link			http://www.eventespresso.com
@@ -13,49 +13,7 @@ get_header();
 
 	<section id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-
-			<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_day() ) :
-							printf( __( 'Today\'s Events: %s', 'event_espresso' ), get_the_date() );
-
-						elseif ( is_month() ) :
-							printf( __( 'Events This Month: %s', 'event_espresso' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyfourteen' ) ) );
-
-						elseif ( is_year() ) :
-							printf( __( 'Events This Year: %s', 'event_espresso' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentyfourteen' ) ) );
-
-						else :
-							echo apply_filters( 'FHEE__archive_espresso_events_template__upcoming_events_h1', __( 'Upcoming Events', 'event_espresso' ));
-
-						endif;
-					?>
-				</h1>
-			</header><!-- .page-header -->
-			
-			<?php 
-					// allow other stuff
-					do_action( 'AHEE__archive_espresso_events_template__before_loop' ); 
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
-						// Include the post TYPE-specific template for the content.
-						get_template_part( 'content', 'espresso_events' );
-					endwhile;
-					// Previous/next page navigation.
-					twentyfourteen_paging_nav();
-					// allow moar other stuff
-					do_action( 'AHEE__archive_espresso_events_template__after_loop' );
-
-				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
-					
-				endif;
-			?>		
-			
+			<?php get_template_part( 'loop', 'espresso_events' ); ?>			
 		</div><!-- #content -->
 	</section><!-- #primary -->
 
