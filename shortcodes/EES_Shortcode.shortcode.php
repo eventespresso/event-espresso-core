f969d71641bb578f9c6423b5b0391427c8c5e004
Brent Christensen <brent@eventespresso.com>
2014-01-29 20:44:52 -0800

    wrap code in if conditional based on capture of classname [touch:5114]

--- shortcodes/EES_Shortcode.shortcode.php
+++ shortcodes/EES_Shortcode.shortcode.php
@@ -66,10 +66,10 @@ abstract class EES_Shortcode extends EE_Base {
 	*/
 	final public static function fallback_shortcode_processor( $attributes ) { 
 		EE_Registry::instance()->load_helper( 'Class_Tools' );
-		$shortcode_class = EEH_Class_Tools::get_called_class();
-		$shortcode_obj = new $shortcode_class();
-		//d( $shortcode_obj );
-		$shortcode_obj->process_shortcode( $attributes );
+		if ( $shortcode_class = EEH_Class_Tools::get_called_class() ) {
+			$shortcode_obj = new $shortcode_class();
+			$shortcode_obj->process_shortcode( $attributes );
+		}
 	}
 
 
