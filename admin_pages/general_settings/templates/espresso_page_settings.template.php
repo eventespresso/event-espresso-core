962ffca4ed7476086d6f7aacaeeab6c0df69149a
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2014-01-31 23:09:10 -0500

    Capital P in WordPress. [touch:4936]

--- admin_pages/general_settings/templates/espresso_page_settings.template.php
+++ admin_pages/general_settings/templates/espresso_page_settings.template.php
@@ -166,7 +166,7 @@
 				</th>
 				<td>
 					<p class="description">
-						<?php echo __("Events are custom post types and use Wordpress' normal archive pages for displaying events.", "event_espresso") ?>
+						<?php echo __("Events are custom post types and use WordPress' normal archive pages for displaying events.", "event_espresso") ?>
 					</p>
 					<p class='description'>
 						<?php printf(__("If you would still like your events on a page like in Event Espresso 3.1 or earlier, simply create a page and place a shortcode to display them on the page, as described %s here %s", "event_espresso"),'<a href="admin.php?page=espresso_support&action=shortcodes">','</a>') ?>
