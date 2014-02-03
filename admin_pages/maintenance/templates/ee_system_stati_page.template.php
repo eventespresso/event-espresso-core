30e1ab030fb6ffa5cbfdf9fb1a92329933f09376
Brent Christensen <brent@eventespresso.com>
2014-02-03 11:37:37 -0800

    remove delete ee data option  [touch:5124] [touch:5125]

--- admin_pages/maintenance/templates/ee_system_stati_page.template.php
+++ admin_pages/maintenance/templates/ee_system_stati_page.template.php
@@ -52,9 +52,7 @@ function ee_resurse_into_array($data) {
 	<?php _e("System Status", "event_espresso");?>
 </h1>
 <div class="padding">
-	<h3>
-		<?php _e("Raw System Data", "event_espresso");?>
-	</h3>
+	<h3 class="espresso-header"><?php _e("Raw System Data", "event_espresso");?></h3>
 	<textarea class="system_status_info">
 		<?php print_r($system_stati)?>
 	</textarea>
@@ -104,13 +102,5 @@ function ee_resurse_into_array($data) {
 		}
 		?>
 	</table>
-	<br/><br/><br/><br/><br/>
-	
-	<h3><?php _e('Delete ALL Event Espresso Tables and Data', 'event_espresso');?></h3>
-	<p><?php _e('If you know for certain that you will no longer be using Event Espresso and you wish to remove ALL traces of the plugin from your system, then perform the following steps. Please note that this is permanent and can NOT be undone.', 'event_espresso');?><br/></p>
-	<ol>
-		<li><?php printf( __('first click this button to %sPermanently Delete All Event Espresso Tables, Records and Options%s  from your database', 'event_espresso'), '<a href="' . $delete_db_url . '" id="delete-all-data-btn" class="button-primary">', '</a>' );?><br/><br/></li>
-		<li><?php printf( __('then go to the %sWordPress Plugins page%s click the delete link within the Event Espresso plugin listing', 'event_espresso'), '<a href="' . admin_url('plugins.php') . '">', '</a>' );?><br/><br/></li>
-		<li><?php _e('follow the rest of the WordPress steps Event Espresso plugin listing', 'event_espresso');?></li>
-	</ol>
+
 </div>
