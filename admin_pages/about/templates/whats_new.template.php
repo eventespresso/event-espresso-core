e92879bc1a73a130b34243c1583513c7d979e4d0
Seth <seth@eventespresso.com>
2014-02-05 02:07:50 -0700

    Trnslatable text

--- admin_pages/about/templates/whats_new.template.php
+++ admin_pages/about/templates/whats_new.template.php
@@ -41,7 +41,7 @@
 		<div class="col-3 last-feature">
 			<img src="<?php echo EE_ABOUT_ASSETS_URL; ?>refined-bookkeeping.jpg">
 			<h3><?php  _e('Easy bookkeeping', 'event_espresso'); ?></h3>
-			<p><?php  _e('Registrations, payment, and transactions have been substantially improved.', 'event_espresso'); ?></p>
+			<p><?php  _e('Registrations, payment, and transactions have been substantially improved in Event Espresso 4.', 'event_espresso'); ?></p>
 		</div>
 	</div>
 
@@ -63,18 +63,18 @@
 </div>
 
 <hr>
-<h3><?php  _e('Pick a theme, any theme'); ?></h3>
+<h3><?php _e('Pick a theme, any theme', 'event_espresso'); ?></h3>
 			<p><?php  _e('We’ve made it super easy to integrate Event Espresso with almost any properly coded WordPress theme, including many of the thousands of themes available on WordPress.org.', 'event_espresso'); ?></p>
 <p><img class="about-overview-img" src="<?php echo EE_ABOUT_ASSETS_URL; ?>multiple-themes.jpg" /></p>
 
 <div class="changelog">
 	<div class="feature-section col two-col">
 		<div>
-			<h3>Refined event management</h3>
-			<p>The new event management screen lets you survey your events at a glance. Or want more information? Click to view more. Quickly add/edit prices, dates, or information in any event.</p>
-			<h4>Smoother price types, taxes, and price modifiers</h4>
-			<p>Price Types allow you to create new prices that adjust the default ticket (base) price for your system-default ticket.</p>
-			<p>Easily categorize a price modifier and indicate how that price gets applied to the running total when a transaction occurs.</p>
+			<h3><?php _e('Refined event management', 'event_espresso'); ?></h3>
+			<p><?php _e('The new event management screen lets you survey your events at a glance. Or want more information? Click to view more. Quickly add/edit prices, dates, or information in any event.', 'event_espresso'); ?></p>
+			<h4><?php _e('Smoother price types, taxes, and price modifiers', 'event_espresso'); ?></h4>
+			<p><?php _e('Price Types allow you to create new prices that adjust the default ticket (base) price for your system-default ticket', 'event_espresso'); ?>.</p>
+			<p><?php _e('Easily categorize a price modifier and indicate how that price gets applied to the running total when a transaction occurs.', 'event_espresso'); ?></p>
 		</div>
 		<div class="last-feature about-themes-img">
 			<img src="<?php echo EE_ABOUT_ASSETS_URL; ?>event-management.jpg">
@@ -82,14 +82,14 @@
 	</div>
 </div>
 <div class="changelog about-event-espresso">
-	<h2 class="about-headline-callout">Espresso Arabica 2014, a Twenty Fourteen child theme for events</h2>
+	<h2 class="about-headline-callout"><?php _e('Espresso Arabica 2014, a Twenty Fourteen child theme for events', 'event_espresso'); ?></h2>
 	<img src="<?php echo EE_ABOUT_ASSETS_URL; ?>single-event-page.jpg">
 
 	<div class="feature-section col one-col center-col">
 		<div>
-			<h3>Turn your blog into a complete event registration and management system</h3>
-			<p>Create a beautiful event page with ticket selection, venue details, and an integrated single page checkout system. With WordPress, Event Espresso, and Espresso Arabica 2014, your events will certainly sell out faster than ever!</p>
-			<p>With a striking design that does not compromise the simplicity of WordPress and Event Espresso 4, Espresso Arabica 2014 will be the best event theme on the market.</p>
+			<h3><?php _e('Turn your blog into a complete event registration and management system', 'event_espresso'); ?></h3>
+			<p><?php _e('Create a beautiful event page with ticket selection, venue details, and an integrated single page checkout system. With WordPress, Event Espresso, and Espresso Arabica 2014, your events will certainly sell out faster than ever!', 'event_espresso'); ?></p>
+			<p><?php _e('With a striking design that does not compromise the simplicity of WordPress and Event Espresso 4, Espresso Arabica 2014 will be the best event theme on the market.', 'event_espresso'); ?></p>
 		</div>
 	</div>
 </div>
