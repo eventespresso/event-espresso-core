f13234a338ae0e85f8ccbea92865f9fe41005f14
Brent Christensen <brent@eventespresso.com>
2014-02-05 13:14:35 -0800

    change EEM_Transaction::incomplete_status_code to EEM_Transaction::failed_status_code and EEM_Transaction::open_status_code to EEM_Transaction::incomplete_status_code [touch:5133]

--- admin_pages/transactions/qtips/Transactions_List_Table_Tips.lib.php
+++ admin_pages/transactions/qtips/Transactions_List_Table_Tips.lib.php
@@ -31,47 +31,47 @@ class Transactions_List_Table_Tips extends EE_Qtip_Config {
 	protected function _set_tips_array() {
 		$this->_qtipsa = array(
 			0 => array(
-				'content_id' => 'transaction-status-' . EEM_Transaction::complete_status_code,
-				'target' => '.txn-status-' . EEM_Transaction::complete_status_code,
-				'content' => $this->_transaction_status_legend(EEM_Transaction::complete_status_code),
-				'options' => array(
-					'position' => array(
-						'target' => 'mouse'
-						)
-					)
-				),
-			1 => array(
-				'content_id' => 'transaction-status-' . EEM_Transaction::incomplete_status_code,
-				'target' => '.txn-status-' . EEM_Transaction::incomplete_status_code,
-				'content' => $this->_transaction_status_legend(EEM_Transaction::incomplete_status_code),
-				'options' => array(
-					'position' => array(
-						'target' => 'mouse'
-						)
-					)
-				),
-			2 => array(
-				'content_id' => 'transaction-status-' . EEM_Transaction::open_status_code,
-				'target' => '.txn-status-' . EEM_Transaction::open_status_code,
-				'content' => $this->_transaction_status_legend(EEM_Transaction::open_status_code),
-				'options' => array(
-					'position' => array(
-						'target' => 'mouse'
-						)
-					)
-				),
-			3 => array(
 				'content_id' => 'transaction-status-' . EEM_Transaction::overpaid_status_code,
 				'target' => '.txn-status-' . EEM_Transaction::overpaid_status_code,
 				'content' => $this->_transaction_status_legend(EEM_Transaction::overpaid_status_code),
 				'options' => array(
 					'position' => array(
 						'target' => 'mouse'
-						)
 					)
-				),
+				)
+			),
+			1 => array(
+				'content_id' => 'transaction-status-' . EEM_Transaction::complete_status_code,
+				'target' => '.txn-status-' . EEM_Transaction::complete_status_code,
+				'content' => $this->_transaction_status_legend(EEM_Transaction::complete_status_code),
+				'options' => array(
+					'position' => array(
+						'target' => 'mouse'
+					)
+				)
+			),
+			2 => array(
+				'content_id' => 'transaction-status-' . EEM_Transaction::incomplete_status_code,
+				'target' => '.txn-status-' . EEM_Transaction::incomplete_status_code,
+				'content' => $this->_transaction_status_legend(EEM_Transaction::incomplete_status_code),
+				'options' => array(
+					'position' => array(
+						'target' => 'mouse'
+					)
+				)
+			),
+			3 => array(
+				'content_id' => 'transaction-status-' . EEM_Transaction::failed_status_code,
+				'target' => '.txn-status-' . EEM_Transaction::failed_status_code,
+				'content' => $this->_transaction_status_legend(EEM_Transaction::failed_status_code),
+				'options' => array(
+					'position' => array(
+						'target' => 'mouse'
+					)
+				)
+			)
 			
-			);
+		);
 	}
 
 
@@ -86,11 +86,11 @@ class Transactions_List_Table_Tips extends EE_Qtip_Config {
 	private function _transaction_status_legend( $status ) {
 
 		$status_array = array(
+			'overpaid' => EEM_Transaction::overpaid_status_code,
 			'complete' => EEM_Transaction::complete_status_code,
 			'incomplete' => EEM_Transaction::incomplete_status_code,
-			'open' => EEM_Transaction::open_status_code,
-			'overpaid' => EEM_Transaction::overpaid_status_code,
-			);
+			'failed' => EEM_Transaction::failed_status_code,
+		);
 
 		return EEH_Template::status_legend( $status_array, $status );
 	}
