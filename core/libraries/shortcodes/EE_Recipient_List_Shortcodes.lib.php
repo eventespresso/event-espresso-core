8e2fd2b68575f89797481d52ad612e2413c7f223
Darren Ethier <darren@roughsmootheng.in>
2014-02-08 12:28:02 -0500

    move new [RECIPIENT_TICKET_LIST] and [PRIMARY_REGISTRANT_TICKET_LIST] short codes from the ticket_list parser into the new recipient_list parser [touch: 5216]

--- core/libraries/shortcodes/EE_Recipient_List_Shortcodes.lib.php
+++ core/libraries/shortcodes/EE_Recipient_List_Shortcodes.lib.php
@@ -33,17 +33,96 @@ class EE_Recipient_List_Shortcodes extends EE_Shortcodes {
 
 
 	protected function _init_props() {
-		$this->label = __('Recipient Details Shortcodes', 'event_espresso');
-		$this->description = __('All shortcodes specific to registrant and primary registrant recipients data', 'event_espresso');
+		$this->label = __('Recipient List Shortcodes', 'event_espresso');
+		$this->description = __('All shortcodes specific to registrant and primary registrant recipients list type data.', 'event_espresso');
 		$this->_shortcodes = array(
+			'[RECIPIENT_TICKET_LIST]' => __('Will output a list of tickets for the recipient of the email. Note, if the recipient is the Event Author, then this is blank.', 'event_espresso'),
+			'[PRIMARY_REGISTRANT_TICKET_LIST]' => __('Will output a list of tickets that the primary registration receieved.', 'event_espresso')
 			);
 	}
 
 
 
 	protected function _parser( $shortcode ) {
+		switch ( $shortcode ) {
+			case '[RECIPIENT_TICKET_LIST]' :
+				return $this->_get_recipient_ticket_list();
+				break;
+
+			case '[PRIMARY_REGISTRANT_TICKET_LIST]' :
+				return $this->_get_recipient_ticket_list( TRUE );
+				break;
+		}
 		return '';
 	}
+
+
+
+	/**
+	 * figure out what the incoming data is and then return the appropriate parsed value
+	 *
+	 * @param  boolean $primary whether we're getting the primary registrant ticket_list.
+	 * @return string
+	 */
+	private function _get_recipient_ticket_list( $primary = FALSE ) {
+		$this->_validate_list_requirements();
+		$this->_set_shortcode_helper();
+
+		if ( $this->_data['data'] instanceof EE_Messages_Addressee )
+			return $this->_get_recipient_ticket_list_parsed( $this->_data['data'], $primary );
+
+		else if ( $this->_extra_data['data'] instanceof EE_Messages_Addressee )
+			return $this->_get_recipient_ticket_list_parsed( $this->_extra_data['data'], $primary );
+
+		else
+			return '';
+	}
+
+
+	private function _get_recipient_ticket_list_parsed( EE_Messages_Addressee $data, $primary = FALSE ) {
+		$attendee = $primary ? $data->primary_att_obj : $data->att_obj;
+			if ( ! $attendee instanceof EE_Attendee ) return '';
+		//setup valid shortcodes depending on what the status of the $this->_data property is
+		if ( $this->_data['data'] instanceof EE_Messages_Addressee ) {
+			$valid_shortcodes = array('ticket', 'event_list', 'attendee_list','datetime_list', 'registration_details', 'attendee');
+			$template = $this->_data['template'];
+			$tkts = $data->attendees[$attendee->ID()]['tkt_objs'];
+			$data = $this->_data;
+		} elseif ( $this->_data['data'] instanceof EE_Event ) {
+			$valid_shortcodes = array('ticket', 'attendee_list', 'datetime_list', 'attendee');
+			$template = is_array($this->_data['template'] ) && isset($this->_data['template']['ticket_list']) ? $this->_data['template']['ticket_list'] : $this->_extra_data['template']['ticket_list'];
+			//let's remove any existing [EVENT_LIST] shortcode from the ticket list template so that we don't get recursion.
+			$template = str_replace('[EVENT_LIST]', '', $template);
+			//data will be tickets for this event for this recipient.
+			$tkts = $this->_get_tickets_from_event( $this->_data['data'], $attendee );
+			$data = $this->_extra_data;
+		} else {
+			return '';
+		}
+
+		$tktparsed = '';
+		foreach ( $tkts as $ticket ) {
+			$tkt_parsed .= $this->_shortcode_helper->parse_ticket_list_template( $template, $ticket, $valid_shortcodes, $data );
+		}
+		return $tktparsed;
+	}
+
+
+	private function _get_tickets_from_event( EE_Event $event, $att = NULL ) {
+		$evt_tkts = isset($this->_extra_data['data']->events) ? $this->_extra_data['data']->events[$event->ID()]['tkt_objs'] : array(); 
+
+		if ( $att instanceof EE_Attendee && $this->_extra_data['data'] instanceof EE_Messages_Addressee ) {
+			$adj_tkts = array();
+			//return only tickets for the given attendee
+			foreach ( $evt_tkts as $tkt ) {
+				if ( isset( $this->_extra_data['data']->attendees[$attendee->ID()]['tkt_objs'][$tkt->ID()] ) )
+					$adj_tkts = $tkt;
+			}
+			$evt_tkts = $adj_tkts;
+		}
+		return $evt_tkts;
+	}
+
 
 	
 } // end EE_Recipient_List_Shortcodes class
