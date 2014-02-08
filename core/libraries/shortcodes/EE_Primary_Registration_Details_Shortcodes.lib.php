135045e954814647a004ceee0e00d8845f8acfb5
Darren Ethier <darren@roughsmootheng.in>
2014-02-08 17:56:06 -0500

    fix validations for recipient and primary_registration group shortcode parsers [touch: 5216]

--- core/libraries/shortcodes/EE_Primary_Registration_Details_Shortcodes.lib.php
+++ core/libraries/shortcodes/EE_Primary_Registration_Details_Shortcodes.lib.php
@@ -53,7 +53,7 @@ class EE_Primary_Registration_Details_Shortcodes extends EE_Shortcodes {
 
 		//make sure we end up with a copy of the EE_Messages_Addressee object
 		$primary_registration = $this->_data instanceof EE_Messages_Addressee ? $this->_data : NULL;
-		$primary_registration = ! $primary_registration instanceof EE_Messages_Addressee && isset( $this->_data['data'] ) && $this->_data['data'] instanceof EE_Messages_Addressee ? $this->_data['data'] : $primary_registration;
+		$primary_registration = ! $primary_registration instanceof EE_Messages_Addressee && is_array($this->_data) && isset( $this->_data['data'] ) && $this->_data['data'] instanceof EE_Messages_Addressee ? $this->_data['data'] : $primary_registration;
 		$primary_registration = ! $primary_registration instanceof EE_Messages_Addressee && !empty( $this->_extra_dataa['data'] ) && $this->_extra_data['data'] instanceof EE_Messages_Addressee ? $this->_extra_data['data'] : $primary_registration;
 
 		if ( ! $primary_registration instanceof EE_Messages_Addressee )
