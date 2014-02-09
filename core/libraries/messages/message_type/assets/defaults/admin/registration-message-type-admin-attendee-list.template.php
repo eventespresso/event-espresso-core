611ae92d6b49b18a60f0e13a4b0fb906d8df3e60
Seth <seth@eventespresso.com>
2014-02-09 16:32:05 -0700

    Rearranging the ticket list hmtl in the admin attendee lists. [touch:5216]

--- core/libraries/messages/message_type/assets/defaults/admin/registration-message-type-admin-attendee-list.template.php
+++ core/libraries/messages/message_type/assets/defaults/admin/registration-message-type-admin-attendee-list.template.php
@@ -1,7 +1,9 @@
 <h4><a href="[EDIT_ATTENDEE_LINK]">[FNAME] [LNAME]</a></h4>
 		<ul>
 <li><strong>Registration Code:</strong> <a href="[EDIT_ATTENDEE_LINK]">[REGISTRATION_CODE]</a></li>
-<li><strong>Tickets:</strong></li>
+<li><strong>Tickets:</strong>
+	<ul>[TICKET_LIST]</ul>
+</li>
 </ul>
-<ul>[TICKET_LIST]</ul>
+
 <hr />
