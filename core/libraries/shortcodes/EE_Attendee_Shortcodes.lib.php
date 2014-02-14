615a21774b19e09df63e8fae3f726b79933dcf78
Darren Ethier <darren@roughsmootheng.in>
2014-02-14 09:00:51 -0500

    switch attendee shortcode parsers to use Registration object instead of Attendee - add [FRONTEND_EDIT_REG_LINK] for attendee shortcodes [touch: 5268]

--- core/libraries/shortcodes/EE_Attendee_Shortcodes.lib.php
+++ core/libraries/shortcodes/EE_Attendee_Shortcodes.lib.php
@@ -50,92 +50,56 @@ class EE_Attendee_Shortcodes extends EE_Shortcodes {
 			'[FNAME]' => __('First Name of an attendee', 'event_espresso'),
 			'[LNAME]' => __('Last Name of an attendee', 'event_espresso'),
 			'[EDIT_ATTENDEE_LINK]' => __('Edit Registration Link (typically you\'d only use this for messages going to event administrators)', 'event_espresso'),
-			'[REGISTRATION_CODE]' => __('Unique Registration Code for the registration', 'event_espresso')
+			'[REGISTRATION_CODE]' => __('Unique Registration Code for the registration', 'event_espresso'),
+			'[FRONTEND_EDIT_REG_LINK]' => __('Generates a link for the given registration to edit this registration details on the frontend.', 'event_espresso')
 			);
 	}
 
 
 	protected function _parser( $shortcode ) {
 
+
 		$this->_xtra = !empty($this->_extra_data ) && $this->_extra_data['data'] instanceof EE_Messages_Addressee ? $this->_extra_data['data'] : NULL;
 
-		//setup _data appropriately
-		if ( !empty( $this->_data->fname ) ) {
-			//we're parsing admin details... let's setup a pseudo attendee object
-			$this->_data = EE_Attendee::new_instance( array(
-				'ATT_fname' => $this->_data->fname,
-				'ATT_lname' => $this->_data->lname,
-				'ATT_email' => $this->_data->admin_email
-				));
-		} else if ( empty( $this->_data->fname ) && $this->_data instanceof EE_Messages_Addressee && !empty( $this->_data->att_obj ) ) {
-			$this->_data = $this->_data->att_obj;
-		}
+		//incoming object should only be a registration object.
+		$registration = ! $this->_data instanceof EE_Registration ? NULL : $this->_data;
 
-		if ( ! $this->_data instanceof EE_Attendee )
+		if ( empty( $registration ) )
 			return '';
+
+		//attendee obj for this registration
+		$attendee = $registration->attendee();
 
 		switch ( $shortcode ) {
 			
 			case '[FNAME]' :
-				return $this->_data->fname();
+				return $attendee->fname();
 				break;
 
 			case '[LNAME]' :
-				return $this->_data->lname();
+				return $attendee->lname();
 				break;
 
 			case '[ATTENDEE_EMAIL]' :
-				return $this->_data->email();
+				return $attendee->email();
 				break;
 
 			case '[EDIT_ATTENDEE_LINK]' :
-				return $this->_get_attendee_edit_link();
+				return $registration->get_admin_edit_url();
 				break;
 
 			case '[REGISTRATION_CODE]' :
-				return $this->_get_reg_code();
+				return $registration->reg_code();
 				break;
+
+			case '[FRONTEND_EDIT_REG_LINK]' :
+				return $registration->edit_attendee_information_url();
+				break;
+
 		}
 
 		return '';
 	}
 
-
-
-
-	private function _get_attendee_edit_link() {
-		//first make sure we have what we need
-		$ID = !empty( $this->_data->ID ) ? $this->_data->ID : '';
-		$ID = empty( $ID ) && is_object( $this->_data ) ? $this->_data->ID() : $ID;
-		if ( !isset( $ID ) ) return '';
-
-		//ID will be attendee ID so let's get the reg ID instead!
-		$REG_ID = !empty( $this->_xtra ) ? $this->_xtra->attendees[$ID]['registration_id'] : $this->_data->get_most_recent_registration();
-
-		$REG_ID = $REG_ID instanceof EE_Registration ? $REG_ID->ID() : $REG_ID;
-
-		//made it here so we can generate the edit attendee link.
-		$query_args = array(
-			'page' => 'espresso_registrations',
-			'action' => 'view_registration',
-			'_REG_ID' => $REG_ID
-			);
-
-		//require EE_Admin_Page core
-		require_once EE_ADMIN . 'EE_Admin_Page.core.php';
-		$url = EE_Admin_Page::add_query_args_and_nonce( $query_args, admin_url('admin.php') );
-
-		return $url;
-	}
-
-
-	private function _get_reg_code() {
-		// we know that we have an attendee object because it's already been checked for.  However, we need to make sure we have the registration and we'll do this from xtra data if present
-		$REG = !empty( $this->_xtra ) && !empty( $this->_xtra->attendees ) ? $this->_xtra->attendees[$this->_data->ID()]['reg_obj'] : NULL;
-		$REG = empty( $REG ) ? $this->_data->get_most_recent_registration() : $REG;
-
-		return empty( $REG ) ? __('no code generated', 'event_espresso') : $REG->reg_code();
-
-	}
 
 } //end EE_Attendee_Shortcodes class
