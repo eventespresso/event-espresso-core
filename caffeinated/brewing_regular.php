645531d791168d878544f8cbb4641b462af45bd7
Darren Ethier <darren@roughsmootheng.in>
2014-02-14 11:06:12 -0500

    fix for question short codes not showing as valid short codes for the question_list field [touch: 5277]

--- caffeinated/brewing_regular.php
+++ caffeinated/brewing_regular.php
@@ -289,11 +289,11 @@ class EE_Brewing_Regular extends EE_Base {
 
 
 	public function message_types_valid_shortcodes( $valid_shortcodes, EE_Messages_Base $msg ) {
-		
 		if ( $msg instanceof EE_message_type ) {
 			$contexts = array_keys($msg->get_contexts());
 				foreach ( $contexts as $context ) {
 					$valid_shortcodes[$context][] = 'question_list';
+					$valid_shortcodes[$context][] = 'question';
 				}
 		}
 
