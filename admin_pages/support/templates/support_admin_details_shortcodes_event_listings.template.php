2f677571d6f657ad9606e3303a3286038e8a6166
Seth <seth@eventespresso.com>
2014-02-17 10:39:15 -0700

    Added descriptions to the short codes. [touch:5249]

--- admin_pages/support/templates/support_admin_details_shortcodes_event_listings.template.php
+++ admin_pages/support/templates/support_admin_details_shortcodes_event_listings.template.php
@@ -4,15 +4,15 @@
 		</p>
 		<ul>
 			<li>[ESPRESSO_EVENTS]</li>
-			<li>[ESPRESSO_EVENTS title="My Super Event List"]</li>
-			<li>[ESPRESSO_EVENTS limit=5]</li>
-			<li>[ESPRESSO_EVENTS css_class="my-custom-class"]</li>
-			<li>[ESPRESSO_EVENTS month="October 2104"]</li>
-			<li>[ESPRESSO_EVENTS show_expired=true]</li>
-			<li>[ESPRESSO_EVENTS category_slug="free-events"]</li>
-			<li>[ESPRESSO_EVENTS order_by="start_date,id"]</li>
-			<li>[ESPRESSO_EVENTS sort="ASC"]</li>
-			<li>[ESPRESSO_EVENTS list_type="grid"]</li>
+			<li>[ESPRESSO_EVENTS title="My Super Event List"] <?php _e('(Custom title for the event list)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS limit=5] <?php _e('(Limits the number of events returned)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS css_class="my-custom-class"] <?php _e('(Adds a custom CSS class to each event post/article)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS month="October 2104"] <?php _e('(Filters events by month/year)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS show_expired=true] <?php _e('(Shows expired events in the event list)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS category_slug="free-events"] <?php _e('(Filters events by category tag)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS order_by="start_date,id"] <?php _e('(Orders the event list)', 'event_espresso'); ?></li>
+			<li>[ESPRESSO_EVENTS sort="ASC"] <?php _e('(Sorts the event list)', 'event_espresso'); ?></li>
+			<!--<li>[ESPRESSO_EVENTS list_type="grid"]</li>-->
 		</ul>
 		<h4>
 			<?php _e('Acceptable Order by parameters:', 'event_espresso'); ?>
