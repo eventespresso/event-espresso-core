759e84a3fca2404b44aac0f041b72fb7c849124f
Darren Ethier <darren@roughsmootheng.in>
2014-02-18 11:20:27 -0500

    fixing merge problems (leftover inline merge commenting)

--- espresso.php
+++ espresso.php
@@ -3,11 +3,7 @@
   Plugin Name: 	Event Espresso
   Plugin URI: 		http://wordpress.org/plugins/event-espresso-free/
   Description: 	Manage your events from your WordPress dashboard. Reduce your admin, reduce your costs, make your life easier! | <a href="admin.php?page=espresso_support&action=contact_support">Support</a>
-<<<<<<< HEAD
   Version: 		4.1.296.alpha
-=======
-  Version: 		4.1.2.001.reg
->>>>>>> beta
   Author: 			Event Espresso
   Author URI: 		http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
   License: 		GPLv2
@@ -32,7 +28,7 @@
 //Returns the plugin version
 if ( ! function_exists( 'espresso_version' )) {
 	function espresso_version() {
-		return '4.1.3.reg';
+		return '4.1.296.alpha';
 	}
 } else {
 	unset( $_GET['activate'] );
