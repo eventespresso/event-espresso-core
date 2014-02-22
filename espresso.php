1245f9376e73f0744742ac70eebe0f85fda582c3
Brent Christensen <brent@eventespresso.com>
2014-02-21 20:48:49 -0800

    bump version to 4.1.6.002.reg

--- espresso.php
+++ espresso.php
@@ -3,7 +3,7 @@
   Plugin Name: 	Event Espresso
   Plugin URI: 		http://wordpress.org/plugins/event-espresso-free/
   Description: 	Manage your events from your WordPress dashboard. Reduce your admin, reduce your costs, make your life easier! | <a href="admin.php?page=espresso_support&action=contact_support">Support</a>
-  Version: 		4.1.6.001.reg
+  Version: 		4.1.6.002.reg
   Author: 			Event Espresso
   Author URI: 		http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
   License: 		GPLv2
@@ -28,7 +28,7 @@
 //Returns the plugin version
 if ( ! function_exists( 'espresso_version' )) {
 	function espresso_version() {
-		return '4.1.6.001.reg';
+		return '4.1.6.002.reg';
 	}
 } else {
 	unset( $_GET['activate'] );
