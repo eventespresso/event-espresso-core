2469f473f4fb4fa2786c0e3491e8564ddaef6edb
Brent Christensen <brent@eventespresso.com>
2014-02-25 16:58:47 -0800

    cleaned up template and fixed position of hook [touch:5347]

--- templates/Espresso_Arabica_2014/content-espresso_events-datetimes.php
+++ templates/Espresso_Arabica_2014/content-espresso_events-datetimes.php
@@ -1,11 +1,12 @@
-<?php //echo '<h1>' . __FILE__ . '</h1>'; ?>
-<?php global $post; ?>
-<?php do_action( 'AHEE_event_details_before_event_date', $post ); ?>
-<div class="event-datetimes">
-	<h3 class="event-datetimes-h3 ee-event-h3">
-		<span class="dashicons dashicons-calendar"></span><?php _e( 'Upcoming Date(s) and Time(s)', 'event_espresso' ); ?>
-	</h3>
-	<?php espresso_list_of_event_dates( $post->ID );?>
-	<?php do_action( 'AHEE_event_details_after_event_date', $post ); ?>
-</div>
-<!-- .event-datetimes -->
+<?php 
+global $post;
+do_action( 'AHEE_event_details_before_event_date', $post ); 
+?>
+	<div class="event-datetimes">
+		<h3 class="event-datetimes-h3 ee-event-h3">
+			<span class="dashicons dashicons-calendar"></span><?php _e( 'Upcoming Date(s) and Time(s)', 'event_espresso' ); ?>
+		</h3>
+		<?php espresso_list_of_event_dates( $post->ID );?>
+	</div>
+	<!-- .event-datetimes -->
+<?php do_action( 'AHEE_event_details_after_event_date', $post ); ?>
