3f403a3071f34e42f92ddf6ba521821ec324b159
Brent Christensen <brent@eventespresso.com>
2014-02-27 11:36:50 -0800

    wrap event tickets template in a display conditional [touch:4815]

--- templates/Espresso_Arabica_2014/content-espresso_events-tickets.php
+++ templates/Espresso_Arabica_2014/content-espresso_events-tickets.php
@@ -1,9 +1,9 @@
 <?php 
 global $post; 
-if ( espresso_display_ticket_selector() ) : 
+if ( espresso_display_ticket_selector() && ( is_single() || ( is_archive() && espresso_display_ticket_selector_in_event_list() ))) : 
 ?>
 <br/>
-<div class="event-tickets">
+<div class="event-tickets" style="clear: both;">
 	<h3 class="ticket-selector-h3 ee-event-h3">
 		<span class="ee-icon ee-icon-tickets"></span><?php _e( 'Ticket Options', 'event_espresso' ); ?>
 	</h3>
