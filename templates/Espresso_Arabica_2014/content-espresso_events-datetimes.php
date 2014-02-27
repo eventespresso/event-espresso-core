3863cae59d190ee29bbfd2db1d19a81bc7d204c3
Brent Christensen <brent@eventespresso.com>
2014-02-27 11:34:51 -0800

    wrap event dateteims template in a display conditional [touch:4815]

--- templates/Espresso_Arabica_2014/content-espresso_events-datetimes.php
+++ templates/Espresso_Arabica_2014/content-espresso_events-datetimes.php
@@ -1,4 +1,5 @@
 <?php 
+if ( is_single() || is_archive() && espresso_display_datetimes_in_event_list() ) :
 global $post;
 do_action( 'AHEE_event_details_before_event_date', $post ); 
 ?>
@@ -9,4 +10,7 @@ do_action( 'AHEE_event_details_before_event_date', $post );
 		<?php espresso_list_of_event_dates( $post->ID );?>
 	</div>
 	<!-- .event-datetimes -->
-<?php do_action( 'AHEE_event_details_after_event_date', $post ); ?>
+<?php 
+do_action( 'AHEE_event_details_after_event_date', $post ); 
+endif;  
+?>
