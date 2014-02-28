1160b926bf2f0ac379edc9daae2c22badd230db5
Darren Ethier <darren@roughsmootheng.in>
2014-02-28 09:29:44 -0500

    make sure we have billing_info before attempting to work with it [touch: 5365]

--- core/db_models/EEM_Gateways.model.php
+++ core/db_models/EEM_Gateways.model.php
@@ -834,12 +834,14 @@ Class EEM_Gateways {
 	 */
 	protected function _clean_billing_info_in_session($transaction){
 		$session_data = EE_Registry::instance()->SSN->get_session_data();
-		foreach($session_data['billing_info'] as $name=>$billing_input_array){
-			if($billing_input_array['sanitize'] == 'ccard'){
-				
-				$session_data['billing_info'][$name]['value'] = $this->MaskCreditCard($billing_input_array['value']);
-			}elseif($billing_input_array['sanitize'] == 'ccv'){
-				$billing_input_array['billing_info'][$name]['value'] = '';
+		if ( isset( $session_data['billing_info'] ) ) {
+			foreach($session_data['billing_info'] as $name=>$billing_input_array){
+				if($billing_input_array['sanitize'] == 'ccard'){
+					
+					$session_data['billing_info'][$name]['value'] = $this->MaskCreditCard($billing_input_array['value']);
+				}elseif($billing_input_array['sanitize'] == 'ccv'){
+					$billing_input_array['billing_info'][$name]['value'] = '';
+				}
 			}
 		}
 		$success = EE_Registry::instance()->SSN->update($session_data);
