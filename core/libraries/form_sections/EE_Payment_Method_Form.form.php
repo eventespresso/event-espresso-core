2f5976ad878c747e419f6108180b382e3196ea38
Michael Nelson <michael@eventespresso.com>
2014-03-18 16:58:44 -0700

    [touch:2639]{t:5}tweaked some commenting

--- core/libraries/form_sections/EE_Payment_Method_Form.form.php
+++ core/libraries/form_sections/EE_Payment_Method_Form.form.php
@@ -6,6 +6,14 @@
  */
 class EE_Payment_Method_Form extends EE_Model_Form_Section{
 	protected $_extra_meta_inputs = array();
+	/**
+	 * 
+	 * @param array $options_array keys:<ul>
+	 * <li>'extra_meta_inputs' should be EE_Form_Section_Base[] which
+	 * will be _subsections and will be saved as extra meta on the payment method object;</li>
+	 * <li>and parent's keys</li>
+	 * </ul>
+	 */
 	public function __construct($options_array){
 		$this->_model = EEM_Payment_Method::instance();
 		if(isset($options_array['extra_meta_inputs'])){
