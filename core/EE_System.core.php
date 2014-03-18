69660967f5f1f7a8841ce7dbc29d7a77bcb6da4a
Brent Christensen <brent@eventespresso.com>
2014-03-17 17:21:28 -0700

    make init hooks accessible regardless of M-Mode [touch:5446]

--- core/EE_System.core.php
+++ core/EE_System.core.php
@@ -615,15 +615,14 @@ final class EE_System {
 		add_action( 'init', array( $this, 'load_CPTs_and_session' ), 5 );
 		add_action( 'init', array( $this, 'load_controllers' ), 7 );
 		add_action( 'init', array( $this, 'core_loaded_and_ready' ), 9 );
+		add_action( 'init', array( $this, 'initialize' ), 10 );
+		add_action( 'init', array( $this, 'initialize_last' ), 100 );
+		add_action('wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ), 25 );
 		// check if M-mode is engaged
 		if ( EE_Maintenance_Mode::instance()->level() ) {
 			// shut 'er down down for maintenance ?
 			add_filter( 'the_content', array( 'EE_Maintenance_Mode', 'the_content' ), 999 );
-		} else {
-			add_action( 'init', array( $this, 'initialize' ), 10 );
-			add_action( 'init', array( $this, 'initialize_last' ), 100 );
-			add_action('wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ), 25 );
-		}
+		} 
 
 		add_action( 'admin_bar_menu', array( $this, 'espresso_toolbar_items' ), 100 );
 		do_action( 'AHEE__EE_System__brew_espresso__complete', $this );
