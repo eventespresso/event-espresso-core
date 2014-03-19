ea3c8ce9c0ae6de839037ab4523c7e536415bbf6
Darren Ethier <darren@roughsmootheng.in>
2014-03-19 14:34:38 -0400

    EEM_Event_Message_Template doesn't need timezone set on the model [touch: 5472]

--- core/db_models/EEM_Event_Message_Template.model.php
+++ core/db_models/EEM_Event_Message_Template.model.php
@@ -10,10 +10,10 @@
  *	EEM_Event_Message_Template
  *	Model for relation table between EEM_Message_Template_Group and EEM_Event
  *
- * @package			Event Espresso
- * @subpackage		models
- * @since 				4.4
- * @author			 	Darren Ethier
+ * @package		Event Espresso
+ * @subpackage 	models
+ * @since 			4.4
+ * @author			 Darren Ethier
  *
  * ------------------------------------------------------------------------
  */
@@ -28,14 +28,9 @@ class EEM_Event_Message_Template extends EEM_Base {
 	 * private constructor to prevent direct creation
 	 * @Constructor
 	 * @access private
-	 * @param string $timezone 	string representing the timezone we want to set for returned Date
-	 *                          				Time Strings (and any incoming timezone data that gets saved).
-	 *                         				Note this just sends the timezone info to the date time model
-	 *                         				field objects.  Default is NULL (and will be assumed using the set
-	 *                         				timezone in the 'timezone_string' wp option)
 	 * @return void
 	 */
-	protected function __construct( $timezone ) {
+	protected function __construct() {
 		$this->singlular_item = __('Event Message Template','event_espresso');
 		$this->plural_item = __('Event Message Templates','event_espresso');
 
@@ -53,7 +48,7 @@ class EEM_Event_Message_Template extends EEM_Base {
 			'Message_Template_Group'=>new EE_Belongs_To_Relation()
 		);
 
-		parent::__construct( $timezone );
+		parent::__construct();
 	}
 
 
@@ -63,24 +58,15 @@ class EEM_Event_Message_Template extends EEM_Base {
 	 * This function is a singleton method used to instantiate the EEM model object
 	 *
 	 * @access public
-	 * @param string $timezone 	string representing the timezone we want to set for returned Date
-	 *                         				Time Strings (and any incoming timezone data that gets saved).
-	 *                         				Note this just sends the timezone info to the date time model
-	 *                         				field objects.  Default is NULL (and will be assumed using the set
-	 *                         				timezone in the 'timezone_string' wp option)
 	 * @return EEM_Datetime_Ticket instance
 	 */
-	public static function instance( $timezone = NULL ){
+	public static function instance(){
 
 		// check if instance of EEM model already exists
 		if ( self::$_instance === NULL ) {
 			// instantiate EEM_Model
-			self::$_instance = new self( $timezone );
+			self::$_instance = new self();
 		}
-
-		//set timezone if we have in incoming string
-		if ( !empty( $timezone ) )
-			self::$_instance->set_timezone( $timezone );
 
 		// EEM_Model object
 		return self::$_instance;
