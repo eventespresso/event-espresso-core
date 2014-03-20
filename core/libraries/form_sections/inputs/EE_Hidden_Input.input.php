477efbf11714eab0b58ea18364f8e3f012bba8d7
Michael Nelson <michael@eventespresso.com>
2014-03-20 12:51:52 -0700

    [touch:2469]{t:20}added hidden input and display strategy

---
+++ core/libraries/form_sections/inputs/EE_Hidden_Input.input.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('EVENT_ESPRESSO_VERSION'))
+	exit('No direct script access allowed');
+
+/**
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package			Event Espresso
+ * @ author			Seth Shoultes
+ * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link					http://www.eventespresso.com
+ * @ version		 	4.3
+ *
+ * ------------------------------------------------------------------------
+ *
+ * EE_Hidden_Input
+ *
+ * @package			Event Espresso
+ * @subpackage		
+ * @author				Mike Nelson
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_Hidden_Input extends EE_Form_Input_Base{
+	function __construct($options = array()){
+		//require_once('strategies/display_strategies/EE_Text_Input_Display_Strategy.strategy.php');
+		$this->_set_display_strategy(new EE_Hidden_Display_Strategy());
+		$this->_set_normalization_strategy(new EE_Text_Normalization());
+		parent::__construct($options);
+	}
+}
+
+// End of file EE_Hidden_Input.input.php
