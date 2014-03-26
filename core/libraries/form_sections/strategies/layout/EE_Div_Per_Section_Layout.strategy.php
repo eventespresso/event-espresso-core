a683c536ef7bf5a2b26ad7087023bb4bfd4a6cd4
Brent Christensen <brent@eventespresso.com>
2014-03-26 15:22:37 -0700

    add indented newlines [touch:2469]

--- core/libraries/form_sections/strategies/layout/EE_Div_Per_Section_Layout.strategy.php
+++ core/libraries/form_sections/strategies/layout/EE_Div_Per_Section_Layout.strategy.php
@@ -2,16 +2,15 @@
 
 class EE_Div_Per_Section_Layout extends EE_Form_Section_Layout_Base{
 	function layout_form() {
-		$form = $this->_form_section;
-		$html = "<div class='{$form->html_class()}' id='{$form->html_id()}' style='{$form->html_style()}'>";
-		foreach($this->_form_section->subsections() as $name=>$subsection){
-			if($subsection instanceof EE_Form_Input_Base){
-				$html.=$this->layout_input($subsection);
-			}elseif($subsection instanceof EE_Form_Section_Proper){
-				$html.=$this->layout_proper_subsection($subsection);
+		$html = ee_newline(1) . '<div class="' . $this->_form_section->html_class() . '" id="' . $this->_form_section->html_id() . '" style="' . $this->_form_section->html_style() . '">';
+		foreach( $this->_form_section->subsections() as $name=>$subsection ){
+			if ( $subsection instanceof EE_Form_Input_Base ){
+				$html .= $this->layout_input( $subsection );
+			} elseif ( $subsection instanceof EE_Form_Section_Proper ){
+				$html .= $this->layout_proper_subsection( $subsection );
 			}
 		}
-		$html.="</div>";
+		$html .= ee_newline(-1) . '</div>';
 		return $html;
 	}
 	/**
@@ -19,14 +18,14 @@ class EE_Div_Per_Section_Layout extends EE_Form_Section_Layout_Base{
 	 * @param EE_Form_Input_Base $input
 	 * @return string
 	 */
-	public function layout_input($input) {
-		return '<div>'.$input->get_html_for_label().'<br/>'.$input->get_html_for_input().$input->get_html_for_help().$input->get_html_for_errors().'</div>';
+	public function layout_input( $input ) {
+		return ee_newline() . '<div>' . ee_newline(1) . $input->get_html_for_label() . '<br/>' . ee_newline() . $input->get_html_for_errors() . ee_newline() . $input->get_html_for_input() . ee_newline() . $input->get_html_for_help() . ee_newline(-1) . '</div>' . ee_newline(-1);
 	}
 	/**
 	 * Lays out a row for the subsection
 	 * @param EE_Form_Section_Proper $formsection
 	 */
-	public function layout_proper_subsection($formsection){
-		return '<div>'.$$formsection->get_html_and_js().'</div>';
+	public function layout_proper_subsection( $formsection ){
+		return ee_newline(1) . '<div>' . $formsection->get_html_and_js() . ee_newline(-1) . '</div>' . ee_newline(-1);
 	}
 }
