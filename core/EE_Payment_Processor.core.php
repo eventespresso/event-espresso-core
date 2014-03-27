68595ad0c40667086d24de30ddb20e3807d5d513
Brent Christensen <brent@eventespresso.com>
2014-03-27 11:41:29 -0700

    code tidy [touch:2639]

--- core/EE_Payment_Processor.core.php
+++ core/EE_Payment_Processor.core.php
@@ -79,8 +79,8 @@ class EE_Payment_Processor{
 			$transaction->save();
 			do_action( 'AHEE__EE_Gateway__update_transaction_with_payment__no_payment', $transaction );
 			
-		}else{
-			$payment = EEM_Payment::instance()->ensure_is_obj($payment,true);
+		} else {
+			$payment = EEM_Payment::instance()->ensure_is_obj( $payment, TRUE );
 			//ok, now process the transaction according to the payment
 			$transaction->update_based_on_payments();//also saves transaction
 			do_action( 'AHEE__EE_Gateway__update_transaction_with_payment__done', $transaction, $payment );
