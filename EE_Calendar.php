8f48c9d6ece083f2f2b1ccefd4cba3a8a6dee4b0
Michael Nelson <michael@eventespresso.com>
2014-03-28 10:01:02 -0700

    [touch:5208]{t:30}fixed shortcode parsing. it was actually removing the event_category_id and event_venue_id shortcode attributes

--- EE_Calendar.php
+++ EE_Calendar.php
@@ -424,14 +424,14 @@
 		// get calendar options
 		$calendar_config = EE_Calendar::get_calendar_config()->to_flat_array();
 		// merge incoming shortcode attributes with calendar config
-		$ee_calendar_js_options = shortcode_atts( $calendar_config, $ee_calendar_js_options, 'EE_Calendar' );
+		$ee_calendar_js_options = array_merge( $calendar_config, $ee_calendar_js_options );
 		//if the user has changed the filters, those should override whatever the admin specified in the shortcode
 		$overrides = array(
-			'event_category_id' => isset($_REQUEST['event_category_id']) ? sanitize_key( $_REQUEST['event_category_id'] ) : '', 
-			'event_venue_id'=> isset($_REQUEST['event_venue_id']) ? sanitize_key( $_REQUEST['event_venue_id'] ) : '', 
+			'event_category_id' => isset($_REQUEST['event_category_id']) ? sanitize_key( $_REQUEST['event_category_id'] ) : isset($ee_calendar_js_options['event_category_id']) ? $ee_calendar_js_options['event_category_id'] : NULL, 
+			'event_venue_id'=> isset($_REQUEST['event_venue_id']) ? sanitize_key( $_REQUEST['event_venue_id'] ) : isset($ee_calendar_js_options['event_venue_id']) ? $ee_calendar_js_options['event_venue_id'] : NULL, 
 			'month'=> isset($_REQUEST['month']) ? sanitize_text_field( $_REQUEST['month'] ) : $ee_calendar_js_options['month'], 
 			'year'=> isset($_REQUEST['year']) ? sanitize_text_field( $_REQUEST['year'] ) : $ee_calendar_js_options['year'], 
-		);
+		);		
 		// merge overrides into options
 		$ee_calendar_js_options = array_merge( $ee_calendar_js_options, $overrides );
 		// set and format month param
