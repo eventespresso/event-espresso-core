d41f1182abca1c8a174b3bdd2a27e05c601f98f2
Michael Nelson <michael@eventespresso.com>
2014-04-01 16:10:49 -0700

    [touch:2639]{t:10}removed a debug line

--- core/EE_Payment_Processor.core.php
+++ core/EE_Payment_Processor.core.php
@@ -122,7 +122,6 @@ class EE_Payment_Processor{
 	 */
 	public function process_ipn( $_req_data, $transaction = NULL, $payment_method = NULL, $save_txn = true ){
 		//do_action('AHEE__log',__FILE__,__FUNCTION__,  sprintf("Logged IPN for payment method %s, registration_url_link '%s'", ))
-		echo "process ipn<hr>";
 		$this->log("processing ipn. raw request data sent:".print_r($_req_data,true), $transaction,$payment_method);
 		try{
 			/**
