a665b112aaefe4e38a8c2b43f538c99a0cfa1d8f
Darren Ethier <darren@roughsmootheng.in>
2014-04-02 15:36:35 -0400

    send class along with the action so it can be used for filtering where buttons show [touch: 5350]

--- core/admin/EE_Admin_List_Table.core.php
+++ core/admin/EE_Admin_List_Table.core.php
@@ -526,7 +526,7 @@ abstract class EE_Admin_List_Table extends WP_List_Table {
 				$extra_request = isset( $action['extra_request'] ) ? $action['extra_request'] : '';
 				echo $this->_admin_page->get_action_link_or_button($route, $type, $extra_request);
 			}
-			do_action( 'AHEE__EE_Admin_List_Table__extra_tablenav__after_bottom_buttons' );
+			do_action( 'AHEE__EE_Admin_List_Table__extra_tablenav__after_bottom_buttons', $this );
 		}
 		//echo $this->_entries_per_page_dropdown;
 	}
