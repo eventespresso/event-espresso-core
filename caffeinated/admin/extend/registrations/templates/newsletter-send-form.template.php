223c0d453f236d4e523e288902b6121768422bd4
Darren Ethier <darren@roughsmootheng.in>
2014-04-02 17:44:00 -0400

    add template for batch message send template [touch: 5350]

---
+++ caffeinated/admin/extend/registrations/templates/newsletter-send-form.template.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is the template for newsletter send form
+ *
+ * @since 4.4.0
+ *
+ * @package Event Espresso
+ * @subpackage Admin, Messages
+ */
+
+/**
+ * Template args found in this template.
+ *
+ * $form_action
+ * $template_selector
+ * $shortcodes_available
+ */
+?>
+<div class="ee-batch-message-send-form">
+    <form id="newsletter-send-form" method="POST" action="<?php echo $form_action; ?>">
+            <h3><?php printf( __('Sending batch message to %s people...', 'event-espresso'), '[NUMPEOPLE]' ); ?></h3>
+            <label for="batch-message-template-selector"><?php _e('Select Template:', 'event_espresso'); ?></label>
+            <?php echo $template_selector; ?>
+            <div class="batch-message-edit-fields" style="display:none;">
+                <p>
+                    <label for="batch-message-from"><?php _e('From:', 'event_espresso'); ?></label><br>
+                    <input type="text" name="batch-message[from]" id="batch-message-from" class="batch-message-input">
+                </p>
+                <p>
+                    <label for="batch-message-subject"><?php _e('Subject:', 'event_espresso'); ?></label><br>
+                    <input type="text" name="batch-message[subject]" id="batch-message-subject" class="batch-message-input">
+                </p>
+                <p>
+                    <label for="batch-message-content"><?php _e('Message Content:', 'event_espresso'); ?></label>
+                    <span class="dashicons dash-icons-menu shortcodes_info" style="display:none;"></span><br>
+                    <div class="shortcodes-info-container">
+                        <?php echo $shortcodes_available; ?>
+                    </div>
+                    <textarea name="batch-message[content]" id="batch-message-content" class="batch-message-textarea"></textarea>
+                </p>
+                <input type="submit" class="batch-message-submit" name="batch-message-submit" value="<?php _e('Send', 'event_espresso'); ?>">
+            </div>
+    </form>
+</div>
