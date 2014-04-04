a4c8b2b03b585190d9d8bad3c9aa796a5eacb476
Brent Christensen <brent@eventespresso.com>
2014-04-04 11:16:57 -0700

    tweak thank you page payment details [touch:4066]

--- shortcodes/espresso_thank_you/templates/thank-you-page-payment-details.template.php
+++ shortcodes/espresso_thank_you/templates/thank-you-page-payment-details.template.php
@@ -2,50 +2,42 @@
 	<?php _e('Payment Overview', 'event_espresso'); ?>
 </h2>
 
-<div class='reg-payment-details'>
+<div id="espresso-thank-you-page-payment-details-dv">
 	<?php 
-	if ( empty( $payments )){
-		
-		if ( $transaction->total() ){
-			echo apply_filters( 'FHEE__payment_overview_template__no_payments_made',__("No payments have been made yet towards this registration.",'event_espresso') );
-		} else {
-			 echo apply_filters( 'FHEE__payment_overview_template__no_payment_required', __("No payment required",'event_espresso') );
-		}
-		
-	} else {
+	if ( ! empty( $payments )){
 		
 		foreach ( $payments as $payment ) { 
 	?>
 		<table class='ee-table'>
 			<tbody>
 				<tr>
-					<td>
+					<th>
 						<label><?php _e('Payment Type:', 'event_espresso'); ?></label>
-					</td>
+					</th>
 					<td>
 						<?php $payment->e('PAY_gateway'); ?>
 					</td>
 				</tr>
 				<tr>
-					<td>
+					<th>
 						<label><?php _e('Payment Date:', 'event_espresso'); ?></label>
-					</td>
+					</th>
 					<td>
 						<?php $payment->e('PAY_timestamp'); ?>
 					</td>
 				</tr>
 				<tr>
-					<td>
+					<th>
 						<label><?php _e("Payment Amount: ", 'event_espresso') ?></label>
-					</td>
+					</th>
 					<td>
 						<?php  $payment->e( 'PAY_amount' ); ?>
 					</td>
 				</tr>
 				<tr>
-					<td>
+					<th>
 						<label><?php _e("Payment Status: ", 'event_espresso') ?></label>
-					</td>
+					</th>
 					<td>
 						<?php $payment->e_pretty_status( TRUE );
 						if ( $show_try_pay_again_link &&  ! $payment->is_approved()) {?>
@@ -57,10 +49,33 @@
 				</tbody>
 			</table>
 		<?php }
+		
+		} else {
+			
+			if ( $transaction->total() ){ 
+				echo apply_filters( 
+					'FHEE__payment_overview_template__no_payments_made',
+					sprintf ( 
+						__('%sNo payments towards this transaction have been received.%s', 'event_espresso' ),
+						'<p class="important-notice">',
+						'</p>'
+					)
+				); 
+			} else {
+				echo apply_filters( 
+					'FHEE__payment_overview_template__no_payment_required',
+					sprintf ( 
+						__('%sNo payment is required for this transaction.%s', 'event_espresso' ),
+						'<p>',
+						'</p>'
+					)
+				); 
+			 }
+				
 		}
 		if ( ! empty( $gateway_content ) && ! $transaction->is_completed() ){
 			echo $gateway_content;
 		 }	
 		?>
+	<br/>	
 </div>
-<br/>	
