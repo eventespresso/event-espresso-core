13302185527c1349a0f061b92b26561d1766ec9a
Brent Christensen <brent@eventespresso.com>
2014-04-09 06:41:10 -0700

    fix defualt time for admin payments [touch:5631]

--- admin_pages/transactions/templates/txn_admin_details_main_meta_box_txn_details.template.php
+++ admin_pages/transactions/templates/txn_admin_details_main_meta_box_txn_details.template.php
@@ -42,7 +42,7 @@
 		<?php endif; // $taxes?>
 				<tr class="admin-primary-mbox-total-tr">
 					<th class=" jst-rght" colspan="5"><?php printf( __( 'Transaction Total %s', 'event_espresso' ), '(' . EE_Registry::instance()->CFG->currency->code . ')');?></th>
-					<th class=" jst-rght"><?php echo $grand_total;?></span></th>
+					<th class=" jst-rght"><?php echo $grand_total;?></th>
 				</tr>
 			</tbody>	
 		</table>
@@ -298,7 +298,7 @@
 					<div class="txn-admin-apply-payment-date-dv admin-modal-dialog-row">
 						<div class="validation-notice-dv"><?php _e( 'The following is  a required field', 'event_espresso' );?></div>
 						<label for="txn-admin-payment-date-inp" class=""><?php _e( 'Payment Date', 'event_espresso' );?></label>
-						<input name="txn_admin_payment[date]" id="txn-admin-payment-date-inp" class="txn-admin-apply-payment-inp required" type="text" value="<?php echo date( 'Y-m-d h:i a' ); ?>"/>
+						<input name="txn_admin_payment[date]" id="txn-admin-payment-date-inp" class="txn-admin-apply-payment-inp required" type="text" value="<?php echo current_time( 'mysql' );?>"/>
 						<br/>
 						<p class="description"><?php _e( 'The date the payment was actually made on', 'event_espresso' );?></p>
 					</div>
