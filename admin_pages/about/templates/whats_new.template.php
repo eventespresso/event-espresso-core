c80d5ef7af7d34f20a2c0fd5b7d3545bb271aaa2
Seth <seth@eventespresso.com>
2014-04-09 10:32:42 -0600

    Added a line that says the calendar is for sale. [touch:5636]

--- admin_pages/about/templates/whats_new.template.php
+++ admin_pages/about/templates/whats_new.template.php
@@ -39,7 +39,7 @@
 	<p>Mijireh Checkout helps increase your conversion rates by using your existing website design. Your checkout process remains seamless to your customers while Mijireh Checkout securely handles the collecting and transmitting of the credit card data for you.</p>
 	<h3>Support for the Calendar Add-on in EE 4.2+</h3>
 	<a href="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-calendar-3-0.jpg"><img class="alignright" alt="calendar-large" src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-calendar-3-0.jpg" width="206" height="282" /></a>
-	<p>The events calendar allow you to display your events in a way that customers are familiar with, gives you another page of your website or another way for customers to find your events, and allows you to invite attendees to add the events to their own calendars as a friendly reminder.</p>
+	<p>The calendar add-on for EE 4.2 is now available for purchase on the <a target="_blank" href="http://eventespresso.com">Event Espresso</a> website. The events calendar allow you to display your events in a way that customers are familiar with, gives you another page of your website or another way for customers to find your events, and allows you to invite attendees to add the events to their own calendars as a friendly reminder.</p>
 	<p>The EE4 Calendar Add-on includes:</p>
 	<ul>
 		<li>Inline calendar images</li>
