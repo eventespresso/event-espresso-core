52daea8e31d2d47c626b26c0a828915c1d2296ab
Seth <seth@eventespresso.com>
2014-04-08 20:31:36 -0600

    Added information about this release. [touch:5636]

--- admin_pages/about/templates/whats_new.template.php
+++ admin_pages/about/templates/whats_new.template.php
@@ -1,6 +1,6 @@
 <div class="changelog point-releases">
 	<h3><?php echo _n( 'Minor Release', 'Minor Releases', 1 ); ?></h3>
-	<p><?php printf( __( '<strong>Version %1$s</strong> is a minor bug fix/enhancement release.', 'event_espresso'), EVENT_ESPRESSO_VERSION ); ?>
+	<p><?php printf( __( '<strong>Version %1$s</strong> is a major bug fix/enhancement release.', 'event_espresso'), EVENT_ESPRESSO_VERSION ); ?>
 		<?php 
 			$ver = explode( '.', EVENT_ESPRESSO_VERSION ); 		
 			array_pop( $ver ); 		
@@ -30,78 +30,61 @@
 		<?php
 	}
 	?>	
-	<h2 class="about-headline-callout"><?php _e('Introducing an improved event management system!', 'event_espresso'); ?></h2>
-	<p><img class="about-overview-img" src="http://ee-screenshots.s3.amazonaws.com/ee4/eventeditor-screen.jpg" /></p>
-	<div class="feature-section col three-col about-updates">
-		<div class="col-1">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/publish_meta_box.jpg">
-			<h3><?php  _e('Optimized aesthetic', 'event_espresso'); ?></h3>
-			<p><?php  _e('The Event Espresso 4 dashboard has a fresh, uncluttered design that embraces clarity and simplicity.', 'event_espresso'); ?></p>
-		</div>
-		<div class="col-2">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/registrations-overview.jpg">
-			<h3><?php  _e('Integrated management', 'event_espresso'); ?></h3>
-			<p><?php  _e('We’ve made it easier to know who your customers are and how they’ve done business with you over time.', 'event_espresso'); ?></p>
-		</div>
-		<div class="col-3 last-feature">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/refined-bookkeeping.jpg">
-			<h3><?php _e('Easy bookkeeping', 'event_espresso'); ?></h3>
-			<p><?php _e('Registrations, payment, and transactions have been substantially improved in Event Espresso 4.', 'event_espresso'); ?></p>
-		</div>
-	</div>
+	<h2 class="about-headline-callout">Two New Major Features Added</h2>
+<h3>Support for Mijireh Chekout</h3>
+<a href="http://eventespresso.com/wp-content/uploads/2014/03/mijireh-checkout-large.png"><img class="alignright " alt="mijireh-checkout-large" src="http://eventespresso.com/wp-content/uploads/2014/03/mijireh-checkout-large.png" width="131" height="147" /></a><p>Announcing the ability to integrate with <strong>over 90 different gateways and payment providers</strong>, using the Mijireh Checkout system! <a href="http://www.mijireh.com/" target="_blank">Mijireh Checkout</a> is a PCI compliant checkout page that looks exactly like your site, with support for over 90 gateways and payment providers to use on Mijireh's <a href="http://www.mijireh.com/docs/payment-gateways/" target="_blank">secure PCI compliant servers</a>. (see the Payment Settings inside EE 4.2).</p>
+<p>Mijireh Checkout helps increase your conversion rates by using your existing website design. Your checkout process remains seamless to your customers while Mijireh Checkout securely handles the collecting and transmitting of the credit card data for you.</p>
+<h3>Calendar Add-on for EE 4.2+</h3>
+<a href="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-calendar-3-0.jpg"><img class="alignright" alt="calendar-large" src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-calendar-3-0.jpg" width="258" height="352" /></a>
+<p>Announcing the ability to integrate with <strong>over 90 different gateways and payment providers</strong>, using the Mijireh Checkout system! <a href="http://www.mijireh.com/" target="_blank">Mijireh Checkout</a> is a PCI compliant checkout page that looks exactly like your site, with support for over 90 gateways and payment providers to use on Mijireh's <a href="http://www.mijireh.com/docs/payment-gateways/" target="_blank">secure PCI compliant servers</a>. (see the Payment Settings inside EE 4.2).</p>
 
-</div>
+<p>The events calendar allow you to display your events in a way that customers are familiar with, gives you another page of your website or another way for customers to find your events, and allows you to invite attendees to add the events to their own calendars as a friendly reminder.</p>
+
+<p>The first version the the Calendar Add-on for EE4 is available in the <a href="http://eventespresso.com/wiki/pre-release-channel-guide/">Pre-release Channel</a> on your account.</p>
+
+<p>The EE4 Calendar Add-on includes:</p>
+<ul>
+	<li>Inline calendar images</li>
+	<li>Category filtering</li>
+	<li>Venue filtering</li>
+	<li>Featured images</li>
+	<li>Datetime and format settings</li>
+	<li>Tooltips</li>
+	<li>Color coded categories</li>
+	<li>Better compatibility with themes</li>
+	<li>and much more!</li>
+</ul>
 <hr>
-
-<div class="changelog">
-	<div class="feature-section col two-col">
-		<div>
-			<h3><?php  _e('Higher customer retention', 'event_espresso'); ?></h3>
-			<p><?php  _e('The Event Espresso 4 registration process is faster than ever. With quick ticket selections, single page check-out, and customizable notifications! Registration that can scale to your business needs.', 'event_espresso'); ?></p>
-			<h4><?php  _e('Ticket selection boxes on any post page or post', 'event_espresso'); ?></h4>
-			<p><?php  _e('Customers can easily register for classes, events, or conferences, in just a few simple steps. No matter how you use it, Event Espresso 4 will adapt to a multitude of different ticketing and pricing scenarios.', 'event_espresso'); ?></p>
-		</div>
-		<div class="last-feature about-colors-img">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/registrtation-page-large.jpg">
-		</div>
-	</div>
-</div>
-
-
-<div class="changelog">
-	<div class="feature-section col two-col">
-		<div>
-			<h3><?php _e('Refined event management', 'event_espresso'); ?></h3>
-			<p><?php _e('The new event management screen lets you survey your events at a glance. Want more information? Click to view more. Quickly add/edit prices, dates, or information in any event.', 'event_espresso'); ?></p>
-			<h4><?php _e('Smoother price types, taxes, and price modifiers', 'event_espresso'); ?></h4>
-			<p><?php _e('Price Types allow you to create new prices that adjust the default ticket (base) price for your system-default ticket', 'event_espresso'); ?>.</p>
-			<p><?php _e('Easily categorize a price modifier and indicate how that price gets applied to the running total when a transaction occurs.', 'event_espresso'); ?></p>
-		</div>
-		<div class="last-feature about-themes-img">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/event-management.jpg">
-		</div>
-	</div>
-</div>
-<br />
+<h2 class="about-headline-callout">Additional Features &amp; Enhancements in EE 4.2</h2>
+<p>In addition to better out of the box support for themes and plugins, we have added an array of new features. Such as drag and drop ordering of tickets in the event editor, ordering of questions within question groups, datetime names and descriptions, and the option to turn off the ticket selector in the event details page, and much more:</p>
+<ul>
+	<li>Added support for Mijireh Checkout</li>
+	<li>Added support for the Calendar Add-on</li>
+	<li>Drag and Drop ordering of tickets in the event editor.</li>
+	<li>Order Questions by Question Group (no longer ordering questions by question order).</li>
+	<li>Option to turn OFF the Ticket Selector in the Event Details page for events without tickets/registrations.</li>
+	<li>Ability to add/Edit Event Datetime Names.</li>
+	<li>Ability to add/Edit Event Datetime Descriptions.</li>
+	<li>Added Event Datetime Name messages shortcode.</li>
+	<li>Event Datetime Description messages shortcode.</li>
+	<li>Added datetime titles and descriptions to the order confirmation.</li>
+	<li>Added front-end "Maintenance Mode" option.</li>
+	<li>Added "Register Now" or "View Details" button in the event list when the Ticket Selector is turned off.</li>
+</ul>
 <hr>
-
-<div class="changelog about-event-espresso">
-	<h2 class="about-headline-callout"><?php _e('Espresso Arabica 2014, a child theme for events', 'event_espresso'); ?></h2>
-	<div class="feature-section col one-col">
-		<div>
-			<h3><?php _e('Turn your blog into a complete event registration and management system', 'event_espresso'); ?></h3>
-			<p><?php _e('Create a beautiful event page with ticket selection, venue details, and an integrated single page checkout system. With WordPress, Event Espresso, and Espresso Arabica 2014 (based on the "Twenty Fourteen" theme by WordPress), your events will certainly sell out faster than ever!', 'event_espresso'); ?></p>
-			<p><?php _e('With a striking design that does not compromise the simplicity of WordPress and Event Espresso 4, Espresso Arabica 2014 will be the best event theme on the market.', 'event_espresso'); ?></p>
-		</div>
-	</div>
-	<img src="http://ee-screenshots.s3.amazonaws.com/ee4/single-event-page.jpg">
-
-	
-</div>
+<h2 class="about-headline-callout">New Hooks, Filters, and Stuff for Developers</h2>
+<p>Here are a few more of the new features and cool stuff for developers available in 4.2 and we'd like to invite you to take a look and offer feedback (praises, report bugs, etc.):</p>
+<ul>
+	<li>Hook/Filter to Set No-cost events as "Free".</li>
+	<li>Add filter so the email messenger CSS file can be swapped out by developers for another css file.</li>
+	<li>Added a hook in the Admin panel after each ticket row description.</li>
+	<li>Added a hook in the Admin panel after each ticket create and update model.</li>
+	<li>Added hook in the Front-end after each ticket date on a single event.</li>
+	<li>Made Single Page Check Out (SPCO) button text filterable.</li>
+	<li>Models make acceptance of empty strings more consistent.</li>
+	<li>Simplified the EE_System Constructor and Hook point's to allow for external module/add-on/plugin development.</li>
+	<li>EEH_Template::locate_template() to accept full paths as well as relatives paths (from plugin root)</li>
+	<li>Filter for adding caffeinated modules to EE_Config $modules_to_register array.</li>
+	<li>Make Single Page Check Out (SPCO) button text filterable.</li>
+</ul>
 <hr>
-
-<h3><?php _e('Pick a theme, any theme', 'event_espresso'); ?></h3>
-			<p><?php  _e('We’ve made it super easy to integrate Event Espresso with almost any properly coded WordPress theme, including many of the thousands of themes available on WordPress.org.', 'event_espresso'); ?></p>
-<p><img class="about-overview-img" src="http://ee-screenshots.s3.amazonaws.com/ee4/multiple-themes.jpg" /></p>
-<hr>
-
