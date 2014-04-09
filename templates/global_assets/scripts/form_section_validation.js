b2c7edb2f2cea710f6f9a8c83d49cd145bfd7962
Michael Nelson <michael@eventespresso.com>
2014-04-09 13:38:13 -0700

    [touch:2639]{t:90}resolved an issue where if multiple form sections were added toa single html form, only the firsts validation rules were used. now both are used

--- templates/global_assets/scripts/form_section_validation.js
+++ templates/global_assets/scripts/form_section_validation.js
@@ -2,11 +2,28 @@
 //uses the variables localized in EE_Form_Section_Proper's _enqueue_and_localize_form_js()
 //to generate the validation js
 jQuery(document).ready(function(){
-	//foreach form section
+	//foreach ee form section
+	//compile an array of what validation rules apply to which html form
+	validation_rules_per_html_form = {};
 	jQuery.each(ee_form_section_vars.form_data,function(index,form_data){
 		//add its validation rules
-		jQuery(form_data.form_section_id).closest('form').validate({
-			rules : form_data.validation_rules
+		html_form = jQuery(form_data.form_section_id).closest('form');
+		//make sure the form tag has an id
+		id = html_form.attr('id');
+		if(typeof(id) == 'undefined' || id == ''){
+			html_form.attr('id',Date.now());
+			id = html_form.attr('id')
+		}
+		if(typeof(validation_rules_per_html_form[id]) == 'undefined'){
+			validation_rules_per_html_form[id] = form_data.validation_rules;
+		}else{
+			jQuery.extend(validation_rules_per_html_form[id],form_data.validation_rules);
+		}
+	});
+	//now apply those validation rules to each html form
+	jQuery.each(validation_rules_per_html_form,function(index,validation_rules_per_form){
+		jQuery('#'+index).validate({
+			rules : validation_rules_per_form
 		});
 	});
 });
