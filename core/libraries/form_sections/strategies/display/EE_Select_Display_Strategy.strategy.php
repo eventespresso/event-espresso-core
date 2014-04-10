a9ef6595da4c377af39115e088678add7e73174e
Brent Christensen <brent@eventespresso.com>
2014-04-09 21:19:57 -0700

    add indents and removed extra </select> tag becuz one was enough [touch:2639]

--- core/libraries/form_sections/strategies/display/EE_Select_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Select_Display_Strategy.strategy.php
@@ -13,20 +13,20 @@ class EE_Select_Display_Strategy extends EE_Display_Strategy_Base{
 		if( ! $input instanceof EE_Form_Input_With_Options_Base){
 			throw new EE_Error(sprintf(__("Cannot use Select Display Strategy with an input that doesn't ahve options", "event_espresso")));
 		}
-		$html= "<select id='{$input->html_id()}' name='{$input->html_name()}' class='{$input->html_class()}' style='{$input->html_style()}'/>";
+		$html= EEH_Formatter::nl(1) . "<select id='{$input->html_id()}' name='{$input->html_name()}' class='{$input->html_class()}' style='{$input->html_style()}'/>";
 		EE_Registry::instance()->load_helper('Array');
 		if(EEH_Array::is_multi_dimensional_array($input->options())){
 			foreach($input->options() as $opt_group_label => $opt_group){
 				$opt_group_label = esc_attr($opt_group_label);
-				$html.="<optgroup label='{$opt_group_label}'>";
+				$html.=EEH_Formatter::nl(1) . "<optgroup label='{$opt_group_label}'>";
 				$html.=$this->_display_options($opt_group);
-				$html.="</optgroup>";
+				$html.=EEH_Formatter::nl(-1) . "</optgroup>";
 			}
 		}else{
 			$html.=$this->_display_options($input->options());
 		}
 		
-		$html.="</select>";
+		$html.= EEH_Formatter::nl(-1) . "</select>";
 		return $html;
 	}
 	/**
@@ -49,7 +49,6 @@ class EE_Select_Display_Strategy extends EE_Display_Strategy_Base{
 			$html.= EEH_Formatter::nl($tabs) . "<option value='$value' $selected_attr>$display_text</option>";
 			$cntr++;
 		}
-		$html.= EEH_Formatter::nl(-1) . "</select>";
 		return $html;
 	}
 }
