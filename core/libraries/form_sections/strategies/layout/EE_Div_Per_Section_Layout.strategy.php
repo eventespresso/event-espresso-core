fa009820316e8049e5e5ea8df489285d3955e125
Brent Christensen <brent@eventespresso.com>
2014-04-09 21:17:17 -0700

    added comment doc blocks, removed line break between labels and inputs (this can be acheived with css which also allows for more flexibility) [touch:2639]

--- core/libraries/form_sections/strategies/layout/EE_Div_Per_Section_Layout.strategy.php
+++ core/libraries/form_sections/strategies/layout/EE_Div_Per_Section_Layout.strategy.php
@@ -1,25 +1,37 @@
 <?php
 
 class EE_Div_Per_Section_Layout extends EE_Form_Section_Layout_Base{
+
+	/**
+	 * opening div tag for a form
+	 */
 	public function layout_form_begin() {
 		return $html = EEH_Formatter::nl(1) . '<div class="' . $this->_form_section->html_class() . '" id="' . $this->_form_section->html_id() . '" style="' . $this->_form_section->html_style() . '">';
 	}
+
+
 	/**
 	 * Lays out the row for the input, including label and errors
 	 * @param EE_Form_Input_Base $input
 	 * @return string
 	 */
 	public function layout_input( $input ) {
-		return EEH_Formatter::nl() . '<div>' . EEH_Formatter::nl(1) . $input->get_html_for_label() . '<br/>' . EEH_Formatter::nl() . $input->get_html_for_errors() . EEH_Formatter::nl() . $input->get_html_for_input() . EEH_Formatter::nl() . $input->get_html_for_help() . EEH_Formatter::nl(-1) . '</div>' . EEH_Formatter::nl(-1);
+		return EEH_Formatter::nl() . '<div>' . EEH_Formatter::nl(1) . $input->get_html_for_label() . EEH_Formatter::nl() . $input->get_html_for_errors() . EEH_Formatter::nl() . $input->get_html_for_input() . EEH_Formatter::nl() . $input->get_html_for_help() . EEH_Formatter::nl(-1) . '</div>' . EEH_Formatter::nl(-1);
 	}
+
+
 	/**
 	 * Lays out a row for the subsection
 	 * @param EE_Form_Section_Proper $formsection
 	 */
-
 	public function layout_subsection( $formsection ){
 		return EEH_Formatter::nl(1) . '<div>' . $formsection->get_html_and_js() . EEH_Formatter::nl(-1) . '</div>' . EEH_Formatter::nl(-1);
 	}
+
+
+	/**
+	 * closing div tag for a form
+	 */
 	public function layout_form_end(){
 		return EEH_Formatter::nl(-1) . '</div>';
 	}
