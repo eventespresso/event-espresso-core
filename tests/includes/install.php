5fcf2865d6407245043d5bfaea9a128ce4ae5230
Darren Ethier <darren@roughsmootheng.in>
2014-04-13 20:33:26 -0400

    use new filter added in EE_System for running db setup IF db hasn't already been setup.

--- tests/includes/install.php
+++ tests/includes/install.php
@@ -21,6 +21,26 @@ function _load_event_espresso() {
 }
 tests_add_filter( 'muplugins_loaded', '_load_event_espresso' );
 
+
+/**
+ * Do the testsbypass filter for when EE is loaded so that we get db setup.  Need to make sure this
+ * ONLY runs here on initial install of db.  We can simply do a check for a EE db to know if we bypass or
+ * not.
+ *
+ * @since 4.3.0
+ *
+ */
+function _maybe_bypass_for_tests() {
+	global $wpdb;
+	//let's check if EE Table exists.  If it does then we do a bypass of is_admin() and user_logged_in check AND we set the $request_type to new_activation.
+	$table_name = $wpdb->prefix . "esp_country";
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+		return TRUE;
+	}
+	return FALSE; //table exists load things normally.
+}
+tests_add_filter('AHEE__EE_System__detect_if_activation_or_upgrade__begin', '_force_activation');
+
 define( 'EE_PLUGIN_DIR', dirname( dirname( dirname( __FILE__ ) ) ) . '/' );
 define( 'EE_ROOT_BLOG', 1 );
 
@@ -34,8 +54,3 @@ echo "Installing Event Espresso...\n";
 
 $wpdb->query( 'SET storage_engine = INNODB' );
 $wpdb->select( DB_NAME, $wpdb->dbh );
-
-// Install Event Espresso using EEH_Activation
-EEH_Activation::system_initialization();
-EEH_Activation::initialize_db_and_folders();
-EEH_Activation::initilaized_db_content();
