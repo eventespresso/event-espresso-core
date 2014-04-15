cbeaf0cfa0ef33cacd4885c078334b9cb6238b35
Darren Ethier <darren@roughsmootheng.in>
2014-04-15 09:09:32 -0400

    more structural placeholders for stuff for EE_UnitTestCase.  clean_up_global_scope() would be a good place to put any resets for singletons needing reset between tests

--- tests/includes/EE_UnitTestCase.class.php
+++ tests/includes/EE_UnitTestCase.class.php
@@ -19,13 +19,14 @@ require_once EE_TESTS_DIR . 'includes/factory.php';
  * @subpackage 	tests
  */
 class EE_UnitTestCase extends WP_UnitTestCase {
+	protected $_cached_SERVER_NAME = NULL;
 
 	public function setUp() {
 		parent::setUp();
 
-
-		//any necessary setup stuff including making sure all EE tables are empty should be done here.
-
+		// Fake WP mail globals, to avoid errors
+		add_filter( 'wp_mail', array( $this, 'setUp_wp_mail' ) );
+		add_filter( 'wp_mail_from', array( $this, 'tearDown_wp_mail' ) );
 
 		//factor
 		$this->factory = new EE_UnitTest_Factory;
@@ -34,11 +35,23 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 
 
 	/**
+	 *  Use this to clean up any global scope singletons etc that we may have being used by EE so
+	 *  that they are fresh between tests.
+	 *
+	 * @todo this of course means we need an easy way to reset our singletons...
+	 * @see parent::cleanup_global_scope();
+	 */
+	function clean_up_global_scope() {
+		parent::clean_up_global_scope();
+	}
+
+
+	/**
 	 * Set up globals necessary to avoid errors when using wp_mail()
 	 */
 	public function setUp_wp_mail( $args ) {
 		if ( isset( $_SERVER['SERVER_NAME'] ) ) {
-			$this->cached_SERVER_NAME = $_SERVER['SERVER_NAME'];
+			$this->_cached_SERVER_NAME = $_SERVER['SERVER_NAME'];
 		}
 
 		$_SERVER['SERVER_NAME'] = 'example.com';
@@ -47,13 +60,15 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		return $args;
 	}
 
+
+
 	/**
 	 * Tear down globals set up in setUp_wp_mail()
 	 */
 	public function tearDown_wp_mail( $args ) {
-		if ( ! empty( $this->cached_SERVER_NAME ) ) {
-			$_SERVER['SERVER_NAME'] = $this->cached_SERVER_NAME;
-			unset( $this->cached_SERVER_NAME );
+		if ( ! empty( $this->_cached_SERVER_NAME ) ) {
+			$_SERVER['SERVER_NAME'] = $this->_cached_SERVER_NAME;
+			unset( $this->_cached_SERVER_NAME );
 		} else {
 			unset( $_SERVER['SERVER_NAME'] );
 		}
