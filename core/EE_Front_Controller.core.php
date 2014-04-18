b82a34b24ce965bbeaa88bb9474268ce0adedd67
Brent Christensen <brent@eventespresso.com>
2014-04-17 21:49:37 -0700

    extra debugging for an issue on sandbox, will remove later [touch:5372]

--- core/EE_Front_Controller.core.php
+++ core/EE_Front_Controller.core.php
@@ -281,7 +281,7 @@ final class EE_Front_Controller {
 						if ( ! isset( EE_Registry::instance()->shortcodes[ $shortcode_class ] )) {
 							unset( EE_Registry::instance()->shortcodes[ $shortcode_class ] );
 							if ( defined( 'WP_DEBUG' ) && WP_DEBUG === TRUE ) {
-								$msg = sprintf( __( 'The [%s] shortcode has not been properly registered or the corresponding addon/module is not active for some reason. Either fix/remove the shortcode from the post, or activate the addon/module the shortcode is associated with.', 'event_espresso' ), $shortcode_class );
+								$msg = sprintf( __( 'The [%s] shortcode has not been properly registered or the corresponding addon/module is not active for some reason. Either fix/remove the shortcode from the post, or activate the addon/module the shortcode is associated with. The list of post shortcodes is %s, and the list of registered shortcodes is %s.', 'event_espresso' ), $shortcode_class, print_r( EE_Registry::instance()->CFG->core->post_shortcodes, TRUE ), print_r( EE_Registry::instance()->shortcodes, TRUE ));
 								EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
 								add_filter( 'FHEE_run_EE_the_content', '__return_true' );
 							}
