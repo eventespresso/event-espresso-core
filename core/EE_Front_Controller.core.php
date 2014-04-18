93f163d1ef1d022fbbdb85d73923ae77336fa865
Brent Christensen <brent@eventespresso.com>
2014-04-18 10:12:16 -0700

    removed debugging code added earlier [touch:5372]

--- core/EE_Front_Controller.core.php
+++ core/EE_Front_Controller.core.php
@@ -280,11 +280,10 @@ final class EE_Front_Controller {
 						// verify shortcode is in list of registered shortcodes
 						if ( ! isset( EE_Registry::instance()->shortcodes[ $shortcode_class ] )) {
 							if ( defined( 'WP_DEBUG' ) && WP_DEBUG === TRUE ) {
-								$msg = sprintf( __( 'The [%s] shortcode has not been properly registered or the corresponding addon/module is not active for some reason. Either fix/remove the shortcode from the post, or activate the addon/module the shortcode is associated with. The list of post shortcodes is %s, and the list of registered shortcodes is %s.', 'event_espresso' ), $shortcode_class, '<pre>' . print_r( EE_Registry::instance()->CFG->core->post_shortcodes, TRUE ) . '/<pre>', '<pre>' . print_r( EE_Registry::instance()->shortcodes, TRUE ) . '/<pre>' );
+								$msg = sprintf( __( 'The [%s] shortcode has not been properly registered or the corresponding addon/module is not active for some reason. Either fix/remove the shortcode from the post, or activate the addon/module the shortcode is associated with.', 'event_espresso' ), $shortcode_class );
 								EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
 								add_filter( 'FHEE_run_EE_the_content', '__return_true' );
 							}
-							unset( EE_Registry::instance()->shortcodes[ $shortcode_class ] );
 							break;
 						}
 						// is this : a shortcodes set exclusively for this post, or for the home page, or a category, or a taxonomy ?
