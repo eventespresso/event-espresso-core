8accb9ee072f40197cc4680599d01706626d1bf0
Brent Christensen <brent@eventespresso.com>
2014-04-17 17:15:44 -0700

    EE_Addon is a parent class for... (take a wild guess) EE Addons !!! Added setters and getters for properties and methods for settting, getting and updating configuration settings [touch:5372]

--- core/EE_Addon.core.php
+++ core/EE_Addon.core.php
@@ -24,7 +24,7 @@
  *
  * ------------------------------------------------------------------------
  */
-abstract class EE_Addon extends EED_Module {
+abstract class EE_Addon {
 
 	/**
 	 * @var $_version
@@ -38,11 +38,15 @@ abstract class EE_Addon extends EED_Module {
 	 */
 	private $_min_core_version;
 
+
+
 	/**
 	 * new_install - check for migration scripts
 	 * @return mixed
 	 */
 	abstract function new_install();
+
+
 
 	/**
 	 * upgrade - check for migration scripts
@@ -53,11 +57,18 @@ abstract class EE_Addon extends EED_Module {
 
 
 	/**
-	 * get__min_core_version
-	 * @return string
+	 * @param mixed $version
 	 */
-	public function min_core_version() {
-		return $this->_min_core_version;
+	public function set_version( $version = NULL ) {
+		$this->_version = $version;
+	}
+
+
+	/**
+	 * @param mixed $min_core_version
+	 */
+	public function set_min_core_version( $min_core_version = NULL ) {
+		$this->_min_core_version = $min_core_version;
 	}
 
 
@@ -73,9 +84,75 @@ abstract class EE_Addon extends EED_Module {
 
 
 	/**
+	 * get__min_core_version
+	 * @return string
+	 */
+	public function min_core_version() {
+		return $this->_min_core_version;
+	}
+
+
+
+
+	/**
 	 *get_db_update_option_name
 	 */
 	public function get_db_update_option_name() {
+	}
+
+
+
+	/**
+	 *    set_config
+	 *
+	 * @access    public
+	 * @param string $name
+	 * @param string $config_class
+	 * @param string $section
+	 * @throws EE_Error
+	 * @internal  param string $_config_class
+	 * @return    void
+	 */
+	public static function set_config( $name = '', $config_class = '',  $section = 'addons' ) {
+		$name = ! empty( $name ) ? $name : EEH_Class_Tools::get_called_class();
+		$config_class = ! empty( $config_class ) ? $config_class : $name . '_Config';
+		if ( ! isset( EE_Config::instance()->$section ) || ! ( EE_Config::instance()->$section instanceof EE_Config_Base || EE_Config::instance()->$section instanceof StdClass )) {
+			throw new EE_Error( sprintf( __( 'The %s configuration does not exist.', 'event_espresso' ), $section ));
+		}
+		if ( ! empty( $section )) {
+			self::$_config_section = $section;
+		}
+		if ( ! empty( $config_class )) {
+			self::$_config_class = $config_class;
+		}
+		if ( ! isset( EE_Config::instance()->$section->$name ) || ! EE_Config::instance()->$section->$name instanceof self::$_config_class ){
+			EE_Config::instance()->$section->$name = new self::$_config_class;
+			EE_Config::instance()->update_espresso_config();
+		}
+
+	}
+
+
+
+	/**
+	 *    get_config
+	 *
+	 * @access 	public
+	 * @param 	string $name
+	 * @return 	EE_Config_Base
+	 */
+	public static function get_config( $name = '' ) {
+		// check that config has even been set
+		if ( empty( self::$_config_class )) {
+			EE_Error::add_error( sprintf( __( 'No configuration has been set for %s.', 'event_espresso' ), $name ), __FILE__, __FUNCTION__, __LINE__ );
+			return NULL;
+		}
+		$config_class = self::$_config_class;
+		// check for cached config
+		if ( ! self::$_config ) {
+			self::$_config = isset( EE_Config::instance()->addons->$name ) || EE_Config::instance()->addons->$name instanceof $config_class ? EE_Config::instance()->addons->$name : new $config_class;
+		}
+		return self::$_config;
 	}
 
 
