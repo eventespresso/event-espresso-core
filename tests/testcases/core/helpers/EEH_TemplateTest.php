834b470580b409d05b91c56e77f1d70a0ef247cf
Brent Christensen <brent@eventespresso.com>
2014-04-21 08:27:11 -0700

    create unit test file for EEH_Template class [touch:5407]

---
+++ tests/testcases/core/helpers/EEH_TemplateTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Contains test class for /core/helpers/EEH_Template.helper.php
+ *
+ * @since 				$VID:$
+ * @package 			Event Espresso
+ * @subpackage 	tests
+ */
+/**
+ *
+ * Class EEH_TemplateTest
+ *
+ * Description
+ *
+ * @package 			Event Espresso
+ * @subpackage 	core
+ * @author 				Brent Christensen
+ * @since 				$VID:$
+ *
+ */
+class EEH_TemplateTest extends PHPUnit_Framework_TestCase {
+
+	public function test() {
+
+	}
+}
+
+
+// End of file EEH_TemplateTest.php
+// Location: /EEH_TemplateTest.php
