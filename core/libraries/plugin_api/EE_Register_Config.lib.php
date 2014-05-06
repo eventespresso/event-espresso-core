33bca9290ef3f9f20f6ceb93bd3a89f390660e2f
Brent Christensen <brent@eventespresso.com>
2014-05-05 17:49:36 -0700

    fix sprintf/translated string and tidy up code [touch:5803]

--- core/libraries/plugin_api/EE_Register_Config.lib.php
+++ core/libraries/plugin_api/EE_Register_Config.lib.php
@@ -50,17 +50,19 @@ class EE_Register_Config implements EEI_Plugin_API {
 		if ( did_action( 'AHEE__EE_System__load_core_configuration__begin' ) ) {
 			EE_Error::doing_it_wrong(
 				__METHOD__,
-				sprintf( __('An attempt made to register "%s" as an EE_Config object has failed because it was not registered at the correct runpoint.  Please regsiter before the "AHEE__EE_System__load_core_configuration__begin" hook has fired', 'event_espresso'), $config_name ),
+				sprintf(
+					__('An attempt to register "%s" as an EE_Config object has failed because it was not registered at the correct hookpoint.  Please register before the "AHEE__EE_System__load_core_configuration__begin" hook has fired', 'event_espresso'),
+					$config_name
+				),
 				'4.3'
 				);
 		}
 
 		//add incoming stuff to our registry property
-		self::$_ee_config_registry[$config_class_name] = $config_name;
+		self::$_ee_config_registry[ $config_class_name ] = $config_name;
 
 		add_filter( 'FHEE__EE_Config__construct__addons', array( 'EE_Register_Config', 'set_config' ), 10 );
 	}
-
 
 
 
@@ -72,23 +74,32 @@ class EE_Register_Config implements EEI_Plugin_API {
 	 * @since    4.3.0
 	 * @throws EE_Error
 	 * @param StdClass $addons_config Object that holds all existing registered configs.
+	 * @return \StdClass
 	 */
 	public static function set_config( $addons_config ) {
 		foreach ( self::$_ee_config_registry as $class_name => $config_name ) {
 			//first some validation of our incoming class_name.  We'll throw an error early if its' not registered correctly
 			if ( ! class_exists( $class_name ) ) {
-				throw EE_Error( sprintf( 'The config class with the name "%s" has cannot be registered with EE_Config because the class does not exist.  Verify that a autoloader has been set for this class', 'event_espresso'), $class_name );
+				throw EE_Error(
+					sprintf(
+						__( 'The "%s" config class can not be registered with EE_Config because the class does not exist.  Verify that an autoloader has been set for this class', 'event_espresso' ),
+						$class_name
+					)
+				 );
 			}
-			$name = empty( $config_name ) ? $class_name : $config_name;
-			$addons_config->{$name} = new $class_name;
+			$config_name = ! empty( $config_name ) ? $config_name : $class_name;
+			$addons_config->{$config_name} = new $class_name;
 		}
 		return $addons_config;
 	}
 
 
 
+	/**
+	 * @param mixed $config_class_name
+	 */
 	public static function deregister( $config_class_name ) {
-		if ( !empty( self::$_ee_config_registry[$config_class_name] ) )
-    			unset( self::$_ee_config_registry[$config_class_name] );
+		if ( ! empty( self::$_ee_config_registry[ $config_class_name ] ))
+    			unset( self::$_ee_config_registry[ $config_class_name ] );
 	}
 }
