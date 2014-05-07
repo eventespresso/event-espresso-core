198676ac47ec8e23ffd49a8b0a5119d4798f3734
Brent Christensen <brent@eventespresso.com>
2014-05-07 12:05:29 -0700

    ensure max tickets = 0 does not blow up Ticket Selctor. display "Closed" under QTY column [touch:5819]

--- modules/ticket_selector/templates/ticket_selector_chart.template.php
+++ modules/ticket_selector/templates/ticket_selector_chart.template.php
@@ -1,5 +1,5 @@
 <div id="tkt-slctr-tbl-wrap-dv-<?php echo $EVT_ID; ?>" class="tkt-slctr-tbl-wrap-dv" >
-	<table id="tkt-slctr-tbl-<?php echo $EVT_ID; ?>" class="tkt-slctr-tbl" border="0" cellspacing="0" cellpadding="0">		
+	<table id="tkt-slctr-tbl-<?php echo $EVT_ID; ?>" class="tkt-slctr-tbl" border="0" cellspacing="0" cellpadding="0">
 		<thead>
 			<tr>
 				<th scope="col" width=""><?php _e( 'Available Tickets', 'event_espresso' ); ?></th>
@@ -8,7 +8,7 @@
 			</tr>
 		</thead>
 		<tbody>
-<?php 
+<?php
 
 $row = 1;
 $ticket_count = count( $tickets );
@@ -18,7 +18,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 	$min = 0;
 	$remaining = $ticket->remaining();
 	if ( $ticket->is_on_sale() && $ticket->is_remaining() ) {
-		if ( $max_atndz > 1 ) { 
+//		if ( $max_atndz > 1 ) {
 			// offer the number of $tickets_remaining or $max_atndz, whichever is smaller
 			$max = min( $remaining, $max_atndz );
 			// but... we also want to restrict the number of tickets by the ticket max setting,
@@ -26,9 +26,9 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 			$max = $ticket->max() > 0 ? min( $ticket->max(), $max ) : $max;
 			// and we also want to restrict the minimum number of tickets by the ticket min setting
 			$min = $ticket->min() > 0 ? $ticket->min() : 0;
-		}
+//		}
 	}
-			
+
 	$ticket_price = apply_filters( 'FHEE__ticket_selector_chart_template__ticket_price', $ticket->get_ticket_total_with_taxes() );
 
 	$tkt_status = $ticket->ticket_status();
@@ -52,7 +52,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 		// pending
 		case EE_Ticket::pending :
 			$ticket_status = '<span class="ticket-pending">' . $ticket->ticket_status( TRUE ) . '</span>';
-			$status_class = 'ticket-pending';	
+			$status_class = 'ticket-pending';
 		break;
 		// onsale
 		case EE_Ticket::onsale :
@@ -62,11 +62,11 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 	}
 
 ?>
-			<tr class="tckt-slctr-tbl-tr <?php echo $status_class; ?>">		
+			<tr class="tckt-slctr-tbl-tr <?php echo $status_class; ?>">
 				<td class="tckt-slctr-tbl-td-name">
 					<b><?php echo $ticket->get_pretty('TKT_name');?></b>
 					<a id="display-tckt-slctr-tkt-details-<?php echo $EVT_ID . '-' . $TKT_ID; ?>" class="display-tckt-slctr-tkt-details display-the-hidden lt-grey-text smaller-text hide-if-no-js" rel="tckt-slctr-tkt-details-<?php echo $EVT_ID . '-' . $TKT_ID; ?>" title="<?php _e( 'click to show additional ticket details', 'event_espresso' ); ?>">
-						<?php echo sprintf( __( 'show%1$sdetails%1$s+', 'event_espresso' ), '&nbsp;' ); ?>						
+						<?php echo sprintf( __( 'show%1$sdetails%1$s+', 'event_espresso' ), '&nbsp;' ); ?>
 					</a>
 					<a id="hide-tckt-slctr-tkt-details-<?php echo $EVT_ID . '-' . $TKT_ID; ?>" class="hide-tckt-slctr-tkt-details hide-the-displayed lt-grey-text smaller-text hide-if-no-js" rel="tckt-slctr-tkt-details-<?php echo $EVT_ID . '-' . $TKT_ID; ?>" title="<?php _e( 'click to hide additional ticket details', 'event_espresso' ); ?>" style="display:none;">
 						<?php echo sprintf( __( 'hide%1$sdetails%1$s-', 'event_espresso' ), '&nbsp;' ); ?>
@@ -81,10 +81,10 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 //echo '<br/><b> $ticket->ticket_status() : ' .  $tkt_status . '</b>';
 //echo '<br/><b> $ticket->uses() : ' .  $ticket->uses() . '</b>';
 				?>
-				</td>	
+				</td>
 				<td class="tckt-slctr-tbl-td-price jst-rght"><?php echo EEH_Template::format_currency( $ticket_price ); ?></td>
 				<td class="tckt-slctr-tbl-td-qty cntr">
-			<?php 
+			<?php
 			 	$hidden_input_qty = $max_atndz > 1 ? TRUE : FALSE;
 				// sold out or other status ?
 				if ( $tkt_status == EE_Ticket::sold_out || $remaining == 0 ) {
@@ -92,7 +92,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 				} else if ( $tkt_status == EE_Ticket::expired || $tkt_status == EE_Ticket::archived ) {
 					echo $ticket_status;
 				} else if ( $tkt_status == EE_Ticket::pending ) {
-				?>	
+				?>
 				<div class="ticket-pending-pg">
 					<span class="ticket-pending"><?php _e( 'Goes&nbsp;On&nbsp;Sale', 'event_espresso' ); ?></span><br/>
 					<span class="small-text"><?php echo date_i18n( 'M d, Y', strtotime( $ticket->start_date() )); ?></span>
@@ -100,7 +100,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 				<?php
 				// min qty purchasable is less than tickets available
 				} else if ( $ticket->min() > $remaining ) {
-				?>	
+				?>
 				<div class="archived-ticket-pg">
 					<span class="archived-ticket small-text"><?php _e( 'Not Available', 'event_espresso' ); ?></span><br/>
 				</div>
@@ -113,8 +113,10 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 				<input type="radio" name="tkt-slctr-qty-<?php echo $EVT_ID; ?>" id="ticket-selector-tbl-qty-slct-<?php echo $EVT_ID . '-' . $row; ?>" class="ticket-selector-tbl-qty-slct" value="<?php echo $row . '-'; ?>1" <?php echo $row == 1 ? ' checked="checked"' : ''; ?> />
 		<?php
 					$hidden_input_qty = FALSE;
-					
-				} elseif ( $max > 0 ) { 
+
+				} else if ( $max_atndz  == 0 ) {
+					echo '<span class="sold-out">' . __( 'Closed', 'event_espresso' ) . '</span>';
+				} elseif ( $max > 0 ) {
 					// display submit button since we have tickets availalbe
 					add_filter( 'FHEE__EE_Ticket_Selector__display_ticket_selector_submit', '__return_true' );
 
@@ -130,17 +132,17 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 					<option value="<?php echo $i; ?>">&nbsp;<?php echo $i; ?>&nbsp;</option>
 				<?php } ?>
 				</select>
-			<?php 
+			<?php
 					$hidden_input_qty = FALSE;
-					
-				} 
+
+				}
 				// depending on group reg we need to change the format for qty
 				if ( $hidden_input_qty ) {
-				?>	
+				?>
 				<input type="hidden" name="tkt-slctr-qty-<?php echo $EVT_ID; ?>[]" value="0" />
 				<?php
-				} 
-			?>	
+				}
+			?>
 					<input type="hidden" name="tkt-slctr-ticket-id-<?php echo $EVT_ID; ?>[]" value="<?php echo $TKT_ID; ?>" />
 					<input type="hidden" name="tkt-slctr-ticket-obj-<?php echo $EVT_ID; ?>[]" value="<?php echo base64_encode( serialize( $ticket )); ?>" />
 
@@ -149,12 +151,12 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 			<tr class="tckt-slctr-tkt-details-tr">
 				<td class="tckt-slctr-tkt-details-td" colspan="4" style="padding: 0;border-top:none;">
 					<div id="tckt-slctr-tkt-details-<?php echo $EVT_ID . '-' . $TKT_ID; ?>-dv" class="tckt-slctr-tkt-details-dv" style="display: none;">
-						
+
 						<section class="tckt-slctr-tkt-details-sctn">
 							<h3><?php _e( 'Ticket Details', 'event_espresso' ); ?></h3>
 							<p><?php echo $ticket->description(); ?></p>
 						</section>
-								
+
 						<section class="tckt-slctr-tkt-price-sctn">
 							<h5><?php _e( 'Ticket Price Breakdown', 'event_espresso' ); ?></h5>
 							<div class="tckt-slctr-tkt-details-tbl-wrap-dv">
@@ -167,25 +169,25 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 										</tr>
 									</thead>
 									<tbody>
-										<?php if ( $ticket->base_price() instanceof EE_Price ) : ?>								
+										<?php if ( $ticket->base_price() instanceof EE_Price ) : ?>
 										<tr>
 											<td class="small-text"><b><?php echo $ticket->base_price()->name(); ?></b></td>
 											<td class="small-text"><?php echo $ticket->base_price()->desc(); ?></td>
 											<td class="jst-rght small-text"><?php echo $ticket->base_price()->pretty_price(); ?></td>
 										</tr>
-										<?php 
-												$running_total = $ticket->base_price()->amount(); 
-											else : 
-												$running_total = 0; 
-											endif; 
+										<?php
+												$running_total = $ticket->base_price()->amount();
+											else :
+												$running_total = 0;
+											endif;
 											// now add price modifiers
-											foreach ( $ticket->price_modifiers() as $price_mod ) : ?>								
+											foreach ( $ticket->price_modifiers() as $price_mod ) : ?>
 										<tr>
 											<td class="jst-rght small-text"><?php echo $price_mod->name(); ?></td>
 										<?php if ( $price_mod->is_percent() ) : ?>
 											<td class="jst-rght small-text"><?php echo $price_mod->amount(); ?>%</td>
-											<?php 
-												$new_sub_total = $running_total * ( $price_mod->amount() / 100 ); 
+											<?php
+												$new_sub_total = $running_total * ( $price_mod->amount() / 100 );
 												$new_sub_total = $price_mod->is_discount() ? $new_sub_total * -1 : $new_sub_total;
 											?>
 											<td class="jst-rght small-text"><?php echo EEH_Template::format_currency( $new_sub_total ); ?></td>
@@ -204,8 +206,8 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 											<td class="jst-rght small-text"><b><?php echo  EEH_Template::format_currency( $running_total ); ?></b></td>
 										</tr>
 
-									<?php 								
-									foreach ( $ticket->get_ticket_taxes_for_admin() as $tax ) : ?>								
+									<?php
+									foreach ( $ticket->get_ticket_taxes_for_admin() as $tax ) : ?>
 										<tr>
 											<td class="jst-rght small-text"><?php echo $tax->name(); ?></td>
 											<td class="jst-rght small-text"><?php echo $tax->amount(); ?>%</td>
@@ -213,7 +215,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 											<td class="jst-rght small-text"><?php echo EEH_Template::format_currency( $tax_amount ); ?></td>
 											<?php $running_total += $tax_amount; ?>
 										</tr>
-									<?php endforeach; ?>								
+									<?php endforeach; ?>
 									<?php endif; ?>
 										<tr>
 											<td colspan="2" class="jst-rght small-text"><b><?php _e( 'Total Ticket Price', 'event_espresso' ); ?></b></td>
@@ -224,7 +226,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 							</div>
 						</section>
 						<br/>
-							
+
 						<section class="tckt-slctr-tkt-sale-dates-sctn">
 							<h5><?php _e( 'Ticket Sale Dates', 'event_espresso' ); ?></h5>
 							<span class="drk-grey-text small-text no-bold"> - <?php _e( 'The dates when this ticket is available for purchase.', 'event_espresso' ); ?></span><br/>
@@ -234,7 +236,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 						<br/>
 
 						<?php do_action( 'AHEE__ticket_selector_chart_template__after_ticket_date', $ticket ); ?>
-							
+
 						<?php if ( $ticket->min() &&$ticket->max() ) : ?>
 						<section class="tckt-slctr-tkt-quantities-sctn">
 							<h5><?php _e( 'Purchasable Quantities', 'event_espresso' ); ?></h5>
@@ -246,15 +248,15 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 						</section>
 						<br/>
 						<?php endif; ?>
-							
-						<?php if ( $ticket->uses() !== INF && ( ! defined( 'EE_DECAF' ) || EE_DECAF !== TRUE )) : ?>						
+
+						<?php if ( $ticket->uses() !== INF && ( ! defined( 'EE_DECAF' ) || EE_DECAF !== TRUE )) : ?>
 						<section class="tckt-slctr-tkt-uses-sctn">
 							<h5><?php _e( 'Event Date Ticket Uses', 'event_espresso' ); ?></h5>
 							<span class="drk-grey-text small-text no-bold"> - <?php _e( 'The number of separate event datetimes (see table below) that this ticket can be used to gain admittance to.<br/> <strong>Admission is always one person per ticket.</strong>', 'event_espresso' ); ?></span><br/>
 							<span class="ticket-details-label-spn drk-grey-text"><?php _e( '# Datetimes:', 'event_espresso' ); ?></span><?php  echo $ticket->e( 'TKT_uses' );?><br/>
 						</section>
 						<?php endif; ?>
-							
+
 						<?php if ( $datetimes = $ticket->datetimes_ordered($event_is_expired,false)) : ?>
 						<section class="tckt-slctr-tkt-datetimes-sctn">
 							<h5><?php _e( 'Event Access', 'event_espresso' ); ?></h5>
@@ -285,28 +287,28 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 									</thead>
 									<tbody>
 									<?php foreach ( $datetimes as $datetime ) : ?>
-						
+
 									<tr>
 										<td class="small-text">
-											<?php $datetime_name = $datetime->name(); ?>										
-											<?php echo ! empty( $datetime_name ) ? '<b>' . $datetime_name . '</b><br/>' : ''; ?>										
-											<?php echo $datetime->date_range( 'l F jS, Y', __( ' to  ', 'event_espresso' )); ?>										
+											<?php $datetime_name = $datetime->name(); ?>
+											<?php echo ! empty( $datetime_name ) ? '<b>' . $datetime_name . '</b><br/>' : ''; ?>
+											<?php echo $datetime->date_range( 'l F jS, Y', __( ' to  ', 'event_espresso' )); ?>
 										</td>
 										<td class="cntr small-text">
-											<?php echo $datetime->time_range( NULL, __( ' to  ', 'event_espresso' )); ?>			
+											<?php echo $datetime->time_range( NULL, __( ' to  ', 'event_espresso' )); ?>
 										</td>
 										<td class="cntr small-text">
-											<?php echo $ticket->sold(); ?>										
-										</td>		
+											<?php echo $ticket->sold(); ?>
+										</td>
 										<td class="cntr small-text">
 											<?php echo $ticket->qty() === INF ? '<span class="smaller-text">' .  __( 'unlimited ', 'event_espresso' ) . '</span>' : $ticket->qty() - $ticket->sold(); ?>
-										</td>		
+										</td>
 										<td class="cntr small-text">
-											<?php echo $datetime->sold(); ?>										
-										</td>		
+											<?php echo $datetime->sold(); ?>
+										</td>
 								<?php $tkts_left = $datetime->sold_out() ? '<span class="sold-out smaller-text">' . __( 'Sold&nbsp;Out', 'event_espresso' ) . '</span>' : $datetime->spaces_remaining(); ?>
 										<td class="cntr small-text">
-											<?php echo $tkts_left === INF ? '<span class="smaller-text">' .  __( 'unlimited ', 'event_espresso' ) . '</span>' : $tkts_left; ?>		
+											<?php echo $tkts_left === INF ? '<span class="smaller-text">' .  __( 'unlimited ', 'event_espresso' ) . '</span>' : $tkts_left; ?>
 										</td>
 									</tr>
 									<?php endforeach; ?>
@@ -316,9 +318,9 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 						</section>
 						<br/>
 						<?php endif; ?>
-							
-							
-					</div>					
+
+
+					</div>
 				</td>
 			</tr>
 <?php
