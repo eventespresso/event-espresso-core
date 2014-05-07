04e8013079e0d9d56b6f79166a559681d92c8d67
Michael Nelson <michael@eventespresso.com>
2014-05-07 11:35:58 -0700

    [touch:5816]{t:45}missed an explicit usage of a property on EE_EVent when getting the primary datetime

--- core/db_classes/EE_Event.class.php
+++ core/db_classes/EE_Event.class.php
@@ -246,7 +246,7 @@ class EE_Event extends EE_CPT_Base{
 		if ( !empty ( $this->_Primary_Datetime )) {
 			return $this->_Primary_Datetime;
 		}
-		$this->_Primary_Datetime = EEM_Datetime::instance( $this->_timezone )->get_primary_datetime_for_event( $this->_EVT_ID ,$try_to_exclude_expired , $try_to_exclude_deleted );
+		$this->_Primary_Datetime = EEM_Datetime::instance( $this->_timezone )->get_primary_datetime_for_event( $this->ID() ,$try_to_exclude_expired , $try_to_exclude_deleted );
 //		$this->_Primary_Datetime = EEM_Datetime::instance( $this->_timezone )->get_most_important_datetime_for_event( $this->_EVT_ID );
 		return $this->_Primary_Datetime;
 	}
