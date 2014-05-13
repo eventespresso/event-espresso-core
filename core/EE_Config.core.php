af5281ce7a897b680e6ae4447d0c6d6458f655d7
Brent Christensen <brent@eventespresso.com>
2014-05-13 09:08:25 -0700

    just completely kill country query unless explicitly passed a country code [touch:5662]

--- core/EE_Config.core.php
+++ core/EE_Config.core.php
@@ -1406,9 +1406,7 @@ class EE_Currency_Config extends EE_Config_Base {
 	public function __construct( $CNT_ISO = NULL ) {
 
 		// get country code from organization settings or use default
-		$ORG_CNT = isset( EE_Registry::instance()->CFG->organization ) && EE_Registry::instance()->CFG->organization instanceof EE_Organization_Config ? EE_Registry::instance()->CFG->organization->CNT_ISO : 'US';
-		// but override if requested
-		$CNT_ISO = ! empty( $CNT_ISO ) ? $CNT_ISO : $ORG_CNT;
+		$CNT_ISO = isset( EE_Registry::instance()->CFG->organization ) && EE_Registry::instance()->CFG->organization instanceof EE_Organization_Config ? EE_Registry::instance()->CFG->organization->CNT_ISO : NULL;
 		// so if that all went well, and we are not in M-Mode (cuz you can't query the db in M-Mode)
 		if ( ! empty( $CNT_ISO ) && ! EE_Maintenance_Mode::instance()->level() && ! get_option( 'ee_espresso_activation' )) {
 			// retrieve the country settings from the db, just in case they have been customized
