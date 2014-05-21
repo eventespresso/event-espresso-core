f7239b429c10ef84244f3f93debcd7ebbc81f4b0
Brent Christensen <brent@eventespresso.com>
2014-05-21 14:14:15 -0700

    fixed minor errors, typos, missing/incorrect phpdocs [touch:5863]

--- core/db_classes/EE_Term_Taxonomy.class.php
+++ core/db_classes/EE_Term_Taxonomy.class.php
@@ -1,76 +1,102 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+<?php if ( !defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
 /**
  * Event Espresso
  *
  * Event Registration and Management Plugin for WordPress
  *
- * @ package			Event Espresso
- * @ author				Seth Shoultes
- * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license			{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
- * @ link					{@link http://www.eventespresso.com}
- * @ since		 		4.0
+ * @ package 		Event Espresso
+ * @ author 		Event Espresso
+ * @ copyright 	(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license 		{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
+ * @ link 				{@link http://www.eventespresso.com}
+ * @ since 			4.0
  *
- * ------------------------------------------------------------------------
+ */
+
+
+
+/**
+ * EE_Term_Taxonomy class
  *
- * EE_Answer class
- *
- * @package			Event Espresso
- * @subpackage		includes/classes/EE_Answer.class.php
- * @author				Mike Nelson
+ * @package 			Event Espresso
+ * @subpackage 	includes/classes/EE_Term_Taxonomy.class.php
+ * @author 				Mike Nelson
  *
  * ------------------------------------------------------------------------
  */
-class EE_Term_Taxonomy extends EE_Base_Class{
+class EE_Term_Taxonomy extends EE_Base_Class {
 
+	/**
+	 * @param array $props_n_values
+	 * @return EE_Term_Taxonomy
+	 */
 	public static function new_instance( $props_n_values = array() ) {
-		$classname = __CLASS__;
-		$has_object = parent::_check_for_object( $props_n_values, $classname );
-		return $has_object ? $has_object : new self( $props_n_values);
+		$has_object = parent::_check_for_object( $props_n_values, __CLASS__ );
+		return $has_object ? $has_object : new self( $props_n_values );
 	}
 
 
-	public static function new_instance_from_db ( $props_n_values = array() ) {
+
+	/**
+	 * @param array $props_n_values
+	 * @return EE_Term_Taxonomy
+	 */
+	public static function new_instance_from_db( $props_n_values = array() ) {
 		return new self( $props_n_values, TRUE );
 	}
+
+
+
 	/**
 	 * Gets taxonomy
 	 * @return string
 	 */
 	function taxonomy() {
-		return $this->get('taxonomy');
+		return $this->get( 'taxonomy' );
 	}
+
+
 
 	/**
 	 * Sets taxonomy
 	 * @param string $taxonomy
 	 * @return boolean
 	 */
-	function set_taxonomy($taxonomy) {
-		return $this->set('taxonomy', $taxonomy);
+	function set_taxonomy( $taxonomy ) {
+		$this->set( 'taxonomy', $taxonomy );
 	}
+
+
+
 	/**
 	 * Gets term_count
 	 * @return int
 	 */
 	function count() {
-		return $this->get('term_count');
+		return $this->get( 'term_count' );
 	}
+
+
 
 	/**
 	 * Sets term_count
 	 * @param int $term_count
 	 * @return boolean
 	 */
-	function set_count($term_count) {
-		return $this->set('term_count', $term_count);
+	function set_count( $term_count ) {
+		$this->set( 'term_count', $term_count );
 	}
+
+
+
 	/**
-	 * Gets the term for this term taxnomy
+	 * Gets the term for this term taxonomy
 	 * @return EE_Term
 	 */
-	function term(){
-		return $this->get_first_related('Term');
+	function term() {
+		return $this->get_first_related( 'Term' );
 	}
 
 
