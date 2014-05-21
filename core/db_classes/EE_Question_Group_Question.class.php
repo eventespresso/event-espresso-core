a2115113446654477b8de64db0cac7fa65221c50
Brent Christensen <brent@eventespresso.com>
2014-05-21 14:13:35 -0700

    fixed minor errors, typos, missing/incorrect phpdocs [touch:5863]

--- core/db_classes/EE_Question_Group_Question.class.php
+++ core/db_classes/EE_Question_Group_Question.class.php
@@ -3,16 +3,22 @@
  * Required  by EEM_Question_Group_Question in case someone queries for all its model objects
  */
 class EE_Question_Group_Question extends EE_Base_Class{
-	
+
+	/**
+	 * @param array $props_n_values
+	 * @return EE_Question_Group_Question|mixed
+	 */
 	public static function new_instance( $props_n_values = array() ) {
-		$classname = __CLASS__;
-		$has_object = parent::_check_for_object( $props_n_values, $classname );
+		$has_object = parent::_check_for_object( $props_n_values, __CLASS__ );
 		return $has_object ? $has_object : new self( $props_n_values );
 	}
 
 
 
-
+	/**
+	 * @param array $props_n_values
+	 * @return EE_Question_Group_Question
+	 */
 	public static function new_instance_from_db ( $props_n_values = array() ) {
 		return new self( $props_n_values, TRUE );
 	}
