f2203463398d7e63b0a5b9d49232e63b441c69d4
Brent Christensen <brent@eventespresso.com>
2014-05-21 14:09:30 -0700

    fixed minor errors, typos, missing/incorrect phpdocs [touch:5863]

--- core/db_classes/EE_Checkin.class.php
+++ core/db_classes/EE_Checkin.class.php
@@ -12,25 +12,36 @@ do_action( 'AHEE_log', __FILE__, __FUNCTION__, '' );
  * @ link					{@link http://www.eventespresso.com}
  * @ since		 		4.0
  *
- * ------------------------------------------------------------------------
- *
+ */
+
+
+
+/**
  * EE_Checkin class
  *
  * @package			Event Espresso
  * @subpackage		includes/classes/EE_Checkin.class.php
- * @author			Darren Ethier
- *
- * ------------------------------------------------------------------------
+ * @author				Darren Ethier
  */
 class EE_Checkin extends EE_Base_Class{
-	
+
+	/**
+	 * @param array $props_n_values
+	 * @param null  $timezone
+	 * @return EE_Checkin|mixed
+	 */
 	public static function new_instance( $props_n_values = array(), $timezone = NULL ) {
-		$classname = __CLASS__;
-		$has_object = parent::_check_for_object( $props_n_values, $classname, $timezone );
+		$has_object = parent::_check_for_object( $props_n_values, __CLASS__, $timezone );
 		return $has_object ? $has_object : new self( $props_n_values, FALSE, $timezone );
 	}
 
 
+
+	/**
+	 * @param array $props_n_values
+	 * @param null  $timezone
+	 * @return EE_Checkin
+	 */
 	public static function new_instance_from_db ( $props_n_values = array(), $timezone = NULL ) {
 		return new self( $props_n_values, TRUE, $timezone );
 	}
