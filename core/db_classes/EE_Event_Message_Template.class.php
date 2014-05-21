3ff3415bb7724bb2eefd07ed099d85cd19e8d468
Brent Christensen <brent@eventespresso.com>
2014-05-21 14:09:59 -0700

    fixed minor errors, typos, missing/incorrect phpdocs [touch:5863]

--- core/db_classes/EE_Event_Message_Template.class.php
+++ core/db_classes/EE_Event_Message_Template.class.php
@@ -1,11 +1,10 @@
-<?php
+<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
 /**
  * Contains definition for EE_Event_Message_Template model object
  * @package 		Event Espresso
  * @subpackage 	models
  * @since 			4.3.0
  */
- if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
 
 /**
  * EE_Event_Message_Template
@@ -18,15 +17,26 @@
  */
 class EE_Event_Message_Template extends EE_Base_Class {
 
+	/**
+	 * @param array $props_n_values
+	 * @param null  $timezone
+	 * @return EE_Event_Message_Template|mixed
+	 */
 	public static function new_instance( $props_n_values = array(), $timezone = NULL ) {
-		$classname = __CLASS__;
-		$has_object = parent::_check_for_object( $props_n_values, $classname, $timezone );
+		$has_object = parent::_check_for_object( $props_n_values, __CLASS__, $timezone );
 		return $has_object ? $has_object : new self( $props_n_values, FALSE, $timezone );
 	}
 
 
+
+	/**
+	 * @param array $props_n_values
+	 * @param null  $timezone
+	 * @return EE_Event_Message_Template
+	 */
 	public static function new_instance_from_db ( $props_n_values = array(), $timezone = NULL ) {
 		return new self( $props_n_values, TRUE, $timezone );
 	}
 
-} //end class EE_Event_Message_Template
+}
+//end class EE_Event_Message_Template
