0d5a5c4b5bd1bf469abab943f1b9e54f050d291c
Brent Christensen <brent@eventespresso.com>
2014-05-21 14:14:06 -0700

    fixed minor errors, typos, missing/incorrect phpdocs [touch:5863]

--- core/db_classes/EE_Term.class.php
+++ core/db_classes/EE_Term.class.php
@@ -1,87 +1,108 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+<?php if ( !defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
 /**
  * Event Espresso
  *
  * Event Registration and Management Plugin for WordPress
  *
- * @ package			Event Espresso
- * @ author				Seth Shoultes
- * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license			{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
- * @ link					{@link http://www.eventespresso.com}
- * @ since		 		4.0
+ * @ package 		Event Espresso
+ * @ author 		Event Espresso
+ * @ copyright 	(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @ license 		{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
+ * @ link 				{@link http://www.eventespresso.com}
+ * @ since 			4.0
  *
- * ------------------------------------------------------------------------
+ */
+
+
+
+/**
+ * EE_Term class
  *
- * EE_Answer class
- *
- * @package			Event Espresso
- * @subpackage		includes/classes/EE_Answer.class.php
- * @author				Mike Nelson
+ * @package 			Event Espresso
+ * @subpackage 	includes/classes/EE_Term.class.php
+ * @author 				Mike Nelson
  *
  * ------------------------------------------------------------------------
  */
-class EE_Term extends EE_Base_Class{
+class EE_Term extends EE_Base_Class {
 
 	/**
 	 * Sets some dynamic defaults
-	 * @param type $fieldValues
-	 * @param type $bydb
-	 * @param type $timezone
+	 * @param array $fieldValues
+	 * @param bool $bydb
+	 * @param string $timezone
 	 */
-	protected function __construct($fieldValues=null, $bydb = FALSE, $timezone = NULL ){
-		if( ! isset($fieldValues['slug'])){
-			$fieldValues['slug'] = $fieldValues['name'];
+	protected function __construct( $fieldValues = array(), $bydb = FALSE, $timezone = '' ) {
+		if ( ! isset( $fieldValues[ 'slug' ] ) ) {
+			$fieldValues[ 'slug' ] = $fieldValues[ 'name' ];
 		}
-		parent::__construct($fieldValues,$bydb,$timezone);
+		parent::__construct( $fieldValues, $bydb, $timezone );
 	}
 
 
+
+	/**
+	 * @param array $props_n_values
+	 * @return EE_Term|mixed
+	 */
 	public static function new_instance( $props_n_values = array() ) {
-		$classname = __CLASS__;
-		$has_object = parent::_check_for_object( $props_n_values, $classname );
-		return $has_object ? $has_object : new self( $props_n_values);
+		$has_object = parent::_check_for_object( $props_n_values, __CLASS__ );
+		return $has_object ? $has_object : new self( $props_n_values );
 	}
 
 
-	public static function new_instance_from_db ( $props_n_values = array() ) {
+
+	/**
+	 * @param array $props_n_values
+	 * @return EE_Term
+	 */
+	public static function new_instance_from_db( $props_n_values = array() ) {
 		return new self( $props_n_values, TRUE );
 	}
+
+
 
 	/**
 	 * Gets name
 	 * @return string
 	 */
 	function name() {
-		return $this->get('name');
+		return $this->get( 'name' );
 	}
+
+
 
 	/**
 	 * Sets name
 	 * @param string $name
 	 * @return boolean
 	 */
-	function set_name($name) {
-		return $this->set('name', $name);
+	function set_name( $name ) {
+		$this->set( 'name', $name );
 	}
+
+
+
 	/**
 	 * Gets slug
 	 * @return string
 	 */
 	function slug() {
-		return $this->get('slug');
+		return $this->get( 'slug' );
 	}
+
+
 
 	/**
 	 * Sets slug
 	 * @param string $slug
 	 * @return boolean
 	 */
-	function set_slug($slug) {
-		return $this->set('slug', $slug);
+	function set_slug( $slug ) {
+		$this->set( 'slug', $slug );
 	}
-
-
 }
 
 /* End of file EE_Answer.class.php */
