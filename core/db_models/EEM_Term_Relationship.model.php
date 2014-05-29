1b3b7001c59bd5063a999c9d8c988580057d6582
Michael Nelson <michael@eventespresso.com>
2014-05-29 16:39:37 -0700

    [touch:5586]{t:90}when updating, inserting, or deleting term relationships, make sure the term_taxonomy counts are still accurate

--- core/db_models/EEM_Term_Relationship.model.php
+++ core/db_models/EEM_Term_Relationship.model.php
@@ -33,12 +33,12 @@ class EEM_Term_Relationship extends EEM_Base {
 	 *
 	 *		@access public
 	 *		@return EEM_Attendee instance
-	 */	
+	 */
 	public static function instance(){
-	
+
 		// check if instance of EEM_Attendee already exists
 		if ( self::$_instance === NULL ) {
-			// instantiate Espresso_model 
+			// instantiate Espresso_model
 			self::$_instance = new self();
 		}
 		// EEM_Attendee object
@@ -66,10 +66,71 @@ class EEM_Term_Relationship extends EEM_Base {
 		$this->_indexes = array(
 			'PRIMARY'=>new EE_Primary_Key_Index(array('object_id','term_taxonomy_id'))
 		);
-		
+
 		parent::__construct();
 	}
 
+	/**
+	 * Makes sur eall term-taxonomy counts are correct
+	 * @param int $term_taxonomy_id the id of the term taxonomy to updte. If NULL, updates ALL
+	 * @global type $wpdb
+	 * @return int the number of rows affected
+	 */
+	public function update_term_taxonomy_counts($term_taxonomy_id = NULL){
+		//because this uses a subquery and sometimes assigning to column to be another column's
+		//value, we just write the SQL directly. 
+		global $wpdb;
+		if( $term_taxonomy_id ){
+			$second_operand = $wpdb->prepare('%d',$term_taxonomy_id);
+		}else{
+			$second_operand = 'tr.term_taxonomy_id';
+		}
+		$rows_affected = $wpdb->query("UPDATE {$wpdb->term_taxonomy} AS tt SET count = (select count(*) as proper_count
+from {$wpdb->term_relationships} AS tr WHERE tt.term_taxonomy_id = $second_operand)");
+		return $rows_affected;
+	}
+
+	/**
+	 * Overrides the parent to also make sure term-taxonomy counts are up-to-date after
+	 * inserting
+	 * @param array $field_n_values @see EEM_Base::insert
+	 * @return boolean
+	 */
+	public function insert($field_n_values) {
+		$return = parent::insert($field_n_values);
+		if( isset( $field_n_values[ 'term_taxonomy_id' ] ) ) {
+			$this->update_term_taxonomy_counts($field_n_values[ 'term_taxonomy_id' ] );
+		}
+		return $return;
+	}
+	/**
+	 * Overrides parent so that after an update, we also check the term_taxonomy_counts are
+	 * all ok
+	 * @param array $fields_n_values see EEM_Base::update
+	 * @param array $query_params @see EEM_Base::get_all
+	 * @return int
+	 */
+	public function update($fields_n_values, $query_params) {
+		$count = parent::update($fields_n_values, $query_params);
+		if( $count ){
+			$this->update_term_taxonomy_counts();
+		}
+		return $count;
+	}
+	/**
+	 * Overrides parent so taht after running this, we also double-check
+	 * the term taxonomy counts are up-to-date
+	 * @param array $query_params @see EEM_Base::get_all
+	 * @param boolean $allow_blocking
+	 * @return int @see EEM_Base::delete
+	 */
+	public function delete($query_params, $allow_blocking = true) {
+		$count = parent::delete($query_params, $allow_blocking);
+		if( $count ){
+			$this->update_term_taxonomy_counts();
+		}
+		return $count;
+	}
 
 
 }
