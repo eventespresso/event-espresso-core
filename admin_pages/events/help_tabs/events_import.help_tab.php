2494f86c66622a1b1a7d6460b911ad8000033bc8
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2014-06-02 23:55:57 -0400

    Fix translate for event pages in app documentation. [touch: 5988]

--- admin_pages/events/help_tabs/events_import.help_tab.php
+++ admin_pages/events/help_tabs/events_import.help_tab.php
@@ -3,12 +3,15 @@
 <?php _e('This page allows you to import information into Event Espresso.', 'event_espresso'); ?>
 </p>
 <p>
-<?php _e('<strong>Sample Import File</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Sample Import File', 'event_espresso'); ?></strong><br />
 <?php _e('Save time by downloading a sample import file. Then customize that file and import it back into Event Espresso.', 'event_espresso'); ?>
 </p>
 <p>
-<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Import Tour button which appears on the right side of the page.', 'event_espresso'); ?>
+<strong><?php _e('Recommendations', 'event_espresso'); ?></strong><br />
+<?php _e('Want to see a tour of this screen? Click on the Import Tour button which appears on the right side of the page.', 'event_espresso'); ?>
+
 </p>
 <p>
-<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
+<strong><?php _e('Screen Optins', 'event_espresso'); ?></strong><br />
+<?php _e('You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
 </p>
