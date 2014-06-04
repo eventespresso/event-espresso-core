aecd961f246275023810540e02a185ee4fd0f74a
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2014-06-04 14:20:26 -0400

    Fix translate for registrations in app documentation.

--- admin_pages/registrations/help_tabs/registrations_event_checkin_filters.help_tab.php
+++ admin_pages/registrations/help_tabs/registrations_event_checkin_filters.help_tab.php
@@ -1,15 +1,15 @@
 <h3><?php _e('Event Check-In Filters', 'event_espresso'); ?></h3>
 <p>
-<?php _e('Filters are a way to show registrations that fall into certain criteria. Notice: you must first select an Event to see all available options for filters. Only registrations with a status of Approved or Pending Payment are shown in this view.', 'event_espresso'); ?>
+<?php _e('Filters are a way to show registrations that fall into certain criteria. Note: you must first select an Event to see all available options for filters. Only registrations with a status of Approved or Pending Payment are shown in this view.', 'event_espresso'); ?>
 </p>
 <p>
 <ul>
 <li>
-<?php _e('<strong>Filter by Event</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Filter by Event', 'event_espresso'); ?></strong><br />
 <?php _e('Begin the check-in process by selecting an event and clicking on the Filter button.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Filter by date</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Filter by date', 'event_espresso'); ?></strong><br />
 <?php _e('Events that occur on multiple dates will have multiple datetimes available for check-in. Select your date and click on the Filter button.', 'event_espresso'); ?>
 </li>
 </ul>
