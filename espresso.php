d13a20b151a75ab29eea7436c7477e03e28a0ce2
Brent Christensen <brent@eventespresso.com>
2014-06-04 10:48:09 -0700

    remove user and nonce checks from activation and deactivation callbacks [touch:6001]

--- espresso.php
+++ espresso.php
@@ -106,7 +106,7 @@ $uploads = wp_upload_dir();
 // define the uploads directory and URL
 define( 'EVENT_ESPRESSO_UPLOAD_DIR', $uploads['basedir'] . DS . 'espresso' . DS );
 define( 'EVENT_ESPRESSO_UPLOAD_URL', $uploads['baseurl'] . DS . 'espresso' . DS );
-// define the templates dirrectory and URL
+// define the templates directory and URL
 define( 'EVENT_ESPRESSO_TEMPLATE_DIR', $uploads['basedir'] . DS . 'espresso' . DS . 'templates' . DS );
 define( 'EVENT_ESPRESSO_TEMPLATE_URL', $uploads['baseurl'] . DS . 'espresso' . DS . 'templates' . DS );
 // define the gateway directory and URL
@@ -119,7 +119,7 @@ define( 'EE_LANGUAGES_SAFE_DIR', EVENT_ESPRESSO_UPLOAD_DIR . 'languages' . DS );
 //ajax constants
 define( 'EE_FRONT_AJAX', isset($_REQUEST['ee_front_ajax']) ? TRUE : FALSE );
 define( 'EE_ADMIN_AJAX', isset($_REQUEST['ee_admin_ajax']) ? TRUE : FALSE );
-//just a handy constant occasionally needed for finding values reprsenting infinity in the DB
+//just a handy constant occasionally needed for finding values representing infinity in the DB
 //you're better to use this than its straight value (currently -1) in case you ever
 //want to change its default value! or find when -1 means infinity
 define('EE_INF_IN_DB', -1);
@@ -146,12 +146,6 @@ function espresso_duplicate_plugin_error() {
  * 	adds a wp-option to indicate that EE has been activated via the WP admin plugins page
  */
 function espresso_plugin_activation() {
-	// check permissions
-	if ( ! current_user_can( 'activate_plugins' )) {
-		return;
-	}
-	$plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
-	check_admin_referer( "activate-plugin_{$plugin}" );
 	update_option( 'ee_espresso_activation', TRUE );
 }
 register_activation_hook( EVENT_ESPRESSO_MAIN_FILE, 'espresso_plugin_activation' );
@@ -162,12 +156,6 @@ register_activation_hook( EVENT_ESPRESSO_MAIN_FILE, 'espresso_plugin_activation'
  * 	espresso_plugin_deactivation
  */
 function espresso_plugin_deactivation() {
-	// check permissions
-	if ( ! current_user_can( 'activate_plugins' )) {
-		return;
-	}
-	$plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
-	check_admin_referer( "deactivate-plugin_{$plugin}" );
 	espresso_load_required( 'EEH_Activation', EE_HELPERS . 'EEH_Activation.helper.php' );
 	EEH_Activation::plugin_deactivation();
 }
@@ -180,7 +168,7 @@ register_deactivation_hook( EVENT_ESPRESSO_MAIN_FILE, 'espresso_plugin_deactivat
  * 	this function loads EE's class for handling exceptions and errors
  */
 function espresso_load_error_handling() {
-	// loaddebugging tools
+	// load debugging tools
 	if ( WP_DEBUG === TRUE ) {
 		require_once( EE_HELPERS . 'EEH_Debug_Tools.helper.php' );
 		EEH_Debug_Tools::instance();
