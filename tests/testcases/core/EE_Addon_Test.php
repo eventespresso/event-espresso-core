92896dc7a00f5dae42dcfb239d1440d1887ca619
Michael Nelson <michael@eventespresso.com>
2014-06-10 15:19:33 -0700

    [touch:5034]{t:30}when testing addons, sometimes the times in the activation log are a second after teh current time and so the unit tests should be forgiving of the last second

--- tests/testcases/core/EE_Addon_Test.php
+++ tests/testcases/core/EE_Addon_Test.php
@@ -43,7 +43,7 @@ class EE_Addon_Test extends EE_UnitTestCase{
 	public function test_update_list_of_installed_versions(){
 		$initial_activation_history = $this->_addon->get_activation_history();
 		$this->assertEmpty( $initial_activation_history );
-
+		$now_string =  substr( date( 'Y-m-d H:i:s', time() ), 0, -1);
 		//now update the list
 		$this->_addon->update_list_of_installed_versions( $initial_activation_history, '2.0.0' );
 		//now let's try adding more to it
@@ -54,14 +54,14 @@ class EE_Addon_Test extends EE_UnitTestCase{
 		$this->assertEquals('2.0.0',$version_number);
 		$this->assertTrue( is_array( $times_installed ) );
 		$time_first_installed = array_shift( $times_installed );
-		$this->assertEquals( date( 'Y-m-d H:i:s', time() ), $time_first_installed );
+		$this->assertEquals( $now_string, substr( $time_first_installed, 0, -1) );
 
 		$second_version_number = key($new_activation_history);
 		$times_second_v_installed = array_shift($new_activation_history);
 		$this->assertEquals('3.0.0',$second_version_number);
 		$this->assertTrue( is_array( $times_second_v_installed ) );
 		$time_second_v_first_installed = array_shift( $times_second_v_installed );
-		$this->assertEquals( date( 'Y-m-d H:i:s', time() ), $time_second_v_first_installed );
+		$this->assertEquals($now_string, substr( $time_second_v_first_installed, 0, -1) );
 	}
 
 	public function test_get_main_plugin_file(){
