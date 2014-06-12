fc9045616e351826961e73169163db249f756348
Brent Christensen <brent@eventespresso.com>
2014-06-12 10:22:06 -0700

    remove event details header [touch:6073]

--- templates/Espresso_Arabica_2014/content-espresso_events-details.php
+++ templates/Espresso_Arabica_2014/content-espresso_events-details.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 //echo '<br/><h6 style="color:#2EA2CC;">'. __FILE__ . ' &nbsp; <span style="font-weight:normal;color:#E76700"> Line #: ' . __LINE__ . '</span></h6>';
-global $post; 
+global $post;
 ?>
 <div class="event-content">
 <?php if ( apply_filters( 'FHEE__content_espresso_events_details_template__display_entry_meta', TRUE )): ?>
@@ -15,24 +15,24 @@ global $post;
 		edit_post_link( __( 'Edit', 'event_espresso' ), '<span class="edit-link">', '</span>' );
 	?>
 	</div>
-<?php 
-endif; 
+<?php
+endif;
 $event_phone = espresso_event_phone( $post->ID, FALSE );
 if ( espresso_event_has_content_or_excerpt( $post->ID ) || $event_phone != '' ) : ?>
 
-	<h3 class="about-event-h3 ee-event-h3">
-		<span class="dashicons dashicons-flag"></span><?php _e( 'Event Details', 'event_espresso' ); ?>
-	</h3>
+<!--	<h3 class="about-event-h3 ee-event-h3">-->
+<!--		<span class="dashicons dashicons-flag"></span>--><?php //_e( 'Event Details', 'event_espresso' ); ?>
+<!--	</h3>-->
 	<?php endif;  ?>
 	<?php if ( $event_phone != '' ) : ?>
 	<p>
 		<span class="small-text"><strong><?php _e( 'Event Phone:', 'event_espresso' ); ?> </strong></span> <?php echo $event_phone; ?>
 	</p>
 <?php endif;  ?>
-<?php 
-	do_action( 'AHEE_event_details_before_the_content', $post );	
+<?php
+	do_action( 'AHEE_event_details_before_the_content', $post );
 	espresso_event_content_or_excerpt();
-	do_action( 'AHEE_event_details_after_the_content', $post ); 
+	do_action( 'AHEE_event_details_after_the_content', $post );
  ?>
 
 </div>
