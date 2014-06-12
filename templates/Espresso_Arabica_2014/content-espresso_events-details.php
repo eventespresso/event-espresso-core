93b1533e0da54449d6ea84a896084610f9774c36
Brent Christensen <brent@eventespresso.com>
2014-06-12 13:44:41 -0700

    remove commented out code [touch:6073]

--- templates/Espresso_Arabica_2014/content-espresso_events-details.php
+++ templates/Espresso_Arabica_2014/content-espresso_events-details.php
@@ -15,16 +15,9 @@ global $post;
 		edit_post_link( __( 'Edit', 'event_espresso' ), '<span class="edit-link">', '</span>' );
 	?>
 	</div>
-<?php
-endif;
-$event_phone = espresso_event_phone( $post->ID, FALSE );
-if ( espresso_event_has_content_or_excerpt( $post->ID ) || $event_phone != '' ) : ?>
-
-<!--	<h3 class="about-event-h3 ee-event-h3">-->
-<!--		<span class="dashicons dashicons-flag"></span>--><?php //_e( 'Event Details', 'event_espresso' ); ?>
-<!--	</h3>-->
-	<?php endif;  ?>
-	<?php if ( $event_phone != '' ) : ?>
+<?php endif;
+	$event_phone = espresso_event_phone( $post->ID, FALSE );
+	if ( $event_phone != '' ) : ?>
 	<p>
 		<span class="small-text"><strong><?php _e( 'Event Phone:', 'event_espresso' ); ?> </strong></span> <?php echo $event_phone; ?>
 	</p>
@@ -34,6 +27,5 @@ if ( espresso_event_has_content_or_excerpt( $post->ID ) || $event_phone != '' ) 
 	espresso_event_content_or_excerpt();
 	do_action( 'AHEE_event_details_after_the_content', $post );
  ?>
-
 </div>
 <!-- .event-content -->
