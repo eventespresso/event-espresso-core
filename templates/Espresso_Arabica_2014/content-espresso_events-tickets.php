5b70ddc10a9a4288605c2ad9853f36490988318e
Brent Christensen <brent@eventespresso.com>
2014-06-12 13:44:57 -0700

    remove Ticket Options header [touch:6073]

--- templates/Espresso_Arabica_2014/content-espresso_events-tickets.php
+++ templates/Espresso_Arabica_2014/content-espresso_events-tickets.php
@@ -3,11 +3,7 @@
 global $post;
 if ( espresso_display_ticket_selector( $post->ID ) && ( is_single() || ( is_archive() && espresso_display_ticket_selector_in_event_list() ))) :
 ?>
-<br/>
 <div class="event-tickets" style="clear: both;">
-	<h4 class="ticket-selector-h4 ee-event-h4">
-		<span class="ee-icon ee-icon-tickets"></span><?php _e( 'Ticket Options', 'event_espresso' ); ?>
-	</h4>
 	<?php espresso_ticket_selector( $post ); ?>
 </div>
 <!-- .event-tickets -->
