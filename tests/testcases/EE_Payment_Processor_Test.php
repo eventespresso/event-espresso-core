e3222a8544f1864bc6d2e764ca03c3ae37570b90
Michael Nelson <michael@eventespresso.com>
2014-06-13 23:17:02 -0700

    [touch:5588]{t:30}start of adding unit tests for paymetn processor

---
+++ tests/testcases/EE_Payment_Processor_Test.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('EVENT_ESPRESSO_VERSION')) {
+	exit('No direct script access allowed');
+}
+
+/**
+ *
+ * EE_Payment_Processor_Test
+ *
+ * @package			Event Espresso
+ * @subpackage
+ * @author				Mike Nelson
+ *
+ */
+class EE_Payment_Processor_Test extends EE_UnitTestCase{
+	
+}
+
+// End of file EE_Payment_Processor_Test.php
