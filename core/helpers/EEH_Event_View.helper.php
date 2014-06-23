a1d948b63352c859680461f4e18fb443e3a182d1
Brent Christensen <brent@eventespresso.com>
2014-06-23 14:20:49 -0700

    fix param and php doc [touch:5680]

--- core/helpers/EEH_Event_View.helper.php
+++ core/helpers/EEH_Event_View.helper.php
@@ -28,14 +28,16 @@ class EEH_Event_View extends EEH_Base {
 	private static $_event = NULL;
 
 
+
 	/**
-	 * 	get_event
-	* 	attempts to retrieve an EE_Event object any way it can
+	 *    get_event
+	 *    attempts to retrieve an EE_Event object any way it can
 	 *
-	 *  @access 	public
-	 *  @return 	object
+	 * @access    public
+	 * @param int $EVT_ID
+	 * @return    object
 	 */
-	public static function get_event( $EVT_ID = FALSE ) {
+	public static function get_event( $EVT_ID = 0 ) {
 		$EVT_ID = $EVT_ID instanceof WP_Post ? $EVT_ID->ID : absint( $EVT_ID );
 		// do we already have the Event  you are looking for?
 		if ( EEH_Event_View::$_event instanceof EE_Event && $EVT_ID && EEH_Event_View::$_event->ID() === $EVT_ID ) {
