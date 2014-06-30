1f9830522fd675dc90383cb5c7bb7620fe681ef7
Darren Ethier <darren@roughsmootheng.in>
2014-06-30 12:59:09 -0400

    do some data type checking [touch: 6042]

--- core/libraries/messages/EE_Payment_Base_message_type.lib.php
+++ core/libraries/messages/EE_Payment_Base_message_type.lib.php
@@ -83,7 +83,7 @@ abstract class EE_Payment_Base_message_type extends EE_message_type {
 	protected function _get_id_for_msg_url( $context, EE_Registration $registration ) {
 		//there should be a transaction and payment object in the incoming data.
 		if ( $this->_data instanceof EE_Messages_incoming_data  ) {
-			$payment = $this->_data->payment;
+			$payment = !empty( $this->_data->payment ) ? $this->_data->payment : NULL;
 
 			if ( $payment instanceof EE_Payment ) {
 				return $payment->ID();
