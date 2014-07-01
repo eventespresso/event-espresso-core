c28619fffc27a6646536069a17d19aeabd9c3900
Brent Christensen <brent@eventespresso.com>
2014-07-01 08:16:29 -0700

    add properties for $_is_current_step, $_reg_form_name && $reg_form, add getters and setters for new properties, add reg_step_hidden_inputs(), display_reg_form(), reg_step_submit_button() and other methods [touch:6081]

--- modules/single_page_checkout/reg_steps/EE_SPCO_Reg_Step.class.php
+++ modules/single_page_checkout/reg_steps/EE_SPCO_Reg_Step.class.php
@@ -35,11 +35,32 @@ abstract class EE_SPCO_Reg_Step {
 	protected $_order = NULL;
 
 	/**
+	 * 	$_is_current_step - TRUE if this is the current step
+	 * 	@access protected
+	 *	@type bool $_is_current_step
+	 */
+	protected $_is_current_step = FALSE;
+
+	/**
 	 * 	$_template - template name
 	 * 	@access protected
 	 *	@type string $_template
 	 */
 	protected $_template = NULL;
+
+	/**
+	 * 	$_reg_form_name - class name for this step's reg form
+	 * 	@access protected
+	 *	@var string $_reg_form_name
+	 */
+	protected $_reg_form_name = NULL;
+
+	/**
+	 * 	$reg_form - the registration form for this step
+	 * 	@access public
+	 *	@var EE_Form_Section_Proper $reg_form
+	 */
+	public $reg_form = NULL;
 
 	/**
 	 * 	$checkout - EE_Checkout object for handling the properties of the current checkout process
@@ -65,6 +86,24 @@ abstract class EE_SPCO_Reg_Step {
 	 */
 	public function slug() {
 		return $this->_slug;
+	}
+
+
+
+	/**
+	 * @param boolean $is_current_step
+	 */
+	public function set_is_current_step( $is_current_step ) {
+		$this->_is_current_step = $is_current_step;
+	}
+
+
+
+	/**
+	 * @return boolean
+	 */
+	public function is_current_step() {
+		return $this->_is_current_step;
 	}
 
 
@@ -96,6 +135,146 @@ abstract class EE_SPCO_Reg_Step {
 
 
 
+
+	/**
+	 * @return string
+	 */
+	public function reg_form_name() {
+		return $this->_reg_form_name;
+	}
+
+
+
+	/**
+	 * @return EE_Form_Input_Base[]
+	 */
+	public function reg_step_hidden_inputs() {
+		// multidimensional array containing the following input details: id, name, value
+		$input_details = array(
+			array(
+				'id' => 'spco-' . $this->slug() . '-action',
+				'name' => 'ajax_action',
+				'value' => 'espresso_process_reg_step'
+			),
+			array(
+				'id' => 'spco-' . $this->slug() . '-noheader',
+				'name' => 'noheader',
+				'value' => ''
+			),
+			array(
+				'id' => 'spco-' . $this->slug() . '-next-step',
+				'name' => 'next_step',
+				'value' => $this->checkout->next_step->slug()
+			),
+			array(
+				'id' => 'spco-reg_url_link',
+				'name' => 'e_reg_url_link',
+				'value' => $this->checkout->reg_url_link
+			),
+			array(
+				'id' => 'spco-revisit',
+				'name' => 'revisit',
+				'value' => $this->checkout->revisit
+			)
+		);
+		// array to hold generated inputs
+		$inputs = array();
+		// loop thru input details
+		foreach ( $input_details as $input ) {
+			// set array of args
+			$input_constructor_args = array(
+				'layout_strategy' => new EE_Div_Per_Section_Layout(),
+				'name' 				=> $input['name'],
+				'html_name' 		=> $input['name'],
+				'html_id' 				=> $input['id'],
+				'default'				=> $input['value']
+			);
+			// generate input
+			$inputs[ $input['name'] ] = new EE_Hidden_Input( $input_constructor_args );
+		}
+		$form_args = array(
+			'name' 			=> 'hidden-inputs',
+			'html_id' 			=> 'hidden-inputs',
+			'subsections' 	=> $inputs,
+			'exclude' 	=> array(),
+			'layout_strategy'	=> new EE_Div_Per_Section_Layout()
+		);
+		return new EE_Form_Section_Proper( $form_args );
+	}
+
+
+
+	/**
+	 * @return string
+	 */
+	public function display_reg_form() {
+		$html = '';
+		if ( $this->reg_form instanceof EE_Form_Section_Proper ) {
+			echo '<br/><h5 style="color:#2EA2CC;">' . __CLASS__ . '<span style="font-weight:normal;color:#0074A2"> -> </span>' . __FUNCTION__ . '() <br/><span style="font-size:9px;font-weight:normal;color:#666">' . __FILE__ . '</span>    <b style="font-size:10px;color:#333">  ' . __LINE__ . ' </b></h5>';
+			$this->reg_form->add_subsections( array( 'spco-next-step-btn' => $this->reg_step_submit_button() ));
+			$html .= $this->reg_form->form_open( add_query_arg( array( 'ee' => '_register', 'step' => $this->slug(), 'action' => 'process_reg_step' ), $this->checkout->reg_page_base_url ));
+			$html .= $this->reg_form->get_html_and_js();
+			$html .= $this->reg_form->form_close();
+		}
+		return $html;
+	}
+
+
+
+	/**
+	 * div_class - returns nothing for current step, but a css class of "hidden" for others
+	 * @return string
+	 */
+	public function reg_step_submit_button() {
+		$sbmt_btn = new EE_Submit_Input( array(
+			'layout_strategy' => new EE_Div_Per_Section_Layout(),
+			'name' 				=> 'spco-go-to-step-' . $this->checkout->next_step->slug(),
+			'html_name' 		=> 'spco-go-to-step-' . $this->checkout->next_step->slug(),
+			'html_id' 				=> 'spco-go-to-step-' . $this->checkout->next_step->slug(),
+			'html_class' 		=> 'spco-next-step-btn',
+			'default'				=> 'Proceed to ' . $this->checkout->next_step->name()
+		));
+		$sbmt_btn->set_button_css_attributes( TRUE, 'large', 'rel="' . $this->slug() . '"' );
+		ob_start();
+		do_action( 'AHEE__before_spco_whats_next_buttons', $this->slug(), $this->checkout->next_step->slug() );
+		echo '<div id="spco-' . $this->slug() . '-whats-next-buttons-dv" class="spco-whats-next-buttons">';
+		echo $sbmt_btn->get_html_for_input();
+		echo '</div>';
+		echo '<!--end spco-whats-next-buttons-->';
+		return new EE_Form_Section_HTML( ob_get_clean() );
+	}
+
+
+	/**
+	 * div_class - returns nothing for current step, but a css class of "hidden" for others
+	 * @return string
+	 */
+	public function div_class() {
+		return $this->is_current_step() ? '' : ' hidden';
+	}
+
+
+
+	/**
+	 * div_class - returns  a css class of "hidden" for current step, but nothing for others
+	 * @return string
+	 */
+	public function edit_lnk_url() {
+		return 	add_query_arg( array( 'ee' => '_register', 'step' => $this->slug() ), $this->checkout->reg_page_base_url );
+
+	}
+
+
+	/**
+	 * div_class - returns  a css class of "hidden" for current step, but nothing for others
+	 * @return string
+	 */
+	public function edit_link_class() {
+		return $this->is_current_step() ? ' hidden' : '';
+	}
+
+
+
 	/**
 	 * @return void
 	 */
@@ -114,7 +293,7 @@ abstract class EE_SPCO_Reg_Step {
 	/**
 	 * @return string
 	 */
-	abstract public function display_reg_step();
+	abstract public function generate_reg_form();
 
 	/**
 	 * @return boolean
