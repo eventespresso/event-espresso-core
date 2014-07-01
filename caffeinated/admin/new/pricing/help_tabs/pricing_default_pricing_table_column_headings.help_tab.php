35839283e5fe4723028905ffadde419dee03831b
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2014-07-01 18:33:15 -0400

    Correct translations code on pricing pages. [touch: 5988]

--- caffeinated/admin/new/pricing/help_tabs/pricing_default_pricing_table_column_headings.help_tab.php
+++ caffeinated/admin/new/pricing/help_tabs/pricing_default_pricing_table_column_headings.help_tab.php
@@ -2,42 +2,49 @@
 <p>
 <ul>
 <li>
-<?php _e('<strong>Name</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Name', 'event_espresso'); ?></strong><br />
 <?php _e('This is the name of the default pricing. Clicking on the name will take you to another page so you can edit options for the selected default pricing.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Price Type</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Price Type', 'event_espresso'); ?></strong><br />
 <?php _e('This is the type of pricing. Available options are Base Price, Percent Discount, Dollar Discount, Percent Surcharge, Dollar Surcharge, Regional Tax, and Federal Tax.', 'event_espresso'); ?>
 <ul>
 <li style="list-style-type: none;">
-<?php _e('<strong>Base Price</strong><br />This is the default price for all new tickets that are created. Base Price is required and cannot be deleted through the default pricing page.', 'event_espresso'); ?>
+<strong><?php _e('Base Price', 'event_espresso'); ?></strong><br />
+<?php _e('This is the default price for all new tickets that are created. Base Price is required and cannot be deleted through the default pricing page.', 'event_espresso'); ?>
 </li>
 <li style="list-style-type: none;">
-<?php _e('<strong>Percent Discount</strong><br />This discount is applied as a percentage (%).', 'event_espresso'); ?>
+<strong><?php _e('Percent Discount', 'event_espresso'); ?></strong><br />
+<?php _e('This discount is applied as a percentage (%).', 'event_espresso'); ?>
 </li>
 <li style="list-style-type: none;">
-<?php _e('<strong>Dollar Discount</strong><br />This discount is applied as a flat amount.', 'event_espresso'); ?>
+<strong><?php _e('Dollar Discount', 'event_espresso'); ?></strong><br />
+<?php _e('This discount is applied as a flat amount.', 'event_espresso'); ?>
 </li>
 <li style="list-style-type: none;">
-<?php _e('<strong>Percent Surcharge</strong><br />This surcharge is applied as a percentage (%). It is useful for helping with administrative costs.', 'event_espresso'); ?>
+<strong><?php _e('Percent Surcharge', 'event_espresso'); ?></strong><br />
+<?php _e('This surcharge is applied as a percentage (%). It is useful for helping with administrative costs.', 'event_espresso'); ?>
 </li>
 <li style="list-style-type: none;">
-<?php _e('<strong>Dollar Surcharge</strong><br />This surcharge is applied as flat amount. It is also useful for helping with administrative costs.', 'event_espresso'); ?>
+<strong><?php _e('Dollar Surcharge', 'event_espresso'); ?></strong><br />
+<?php _e('This surcharge is applied as flat amount. It is also useful for helping with administrative costs.', 'event_espresso'); ?>
 </li>
 <li style="list-style-type: none;">
-<?php _e('<strong>Regional Tax</strong><br />This is a local tax.', 'event_espresso'); ?>
+<strong><?php _e('Regional Tax', 'event_espresso'); ?></strong><br />
+<?php _e('This is a local tax.', 'event_espresso'); ?>
 </li>
 <li style="list-style-type: none;">
-<?php _e('<strong>Federal Tax</strong><br />This is a federal tax.', 'event_espresso'); ?>
+<strong><?php _e('Federal Tax', 'event_espresso'); ?></strong><br />
+<?php _e('This is a federal tax.', 'event_espresso'); ?>
 </li>
 </ul>
 </li>
 <li>
-<?php _e('<strong>Description</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Description', 'event_espresso'); ?></strong><br />
 <?php _e('This is the description for the default pricing.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Amount</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Amount', 'event_espresso'); ?></strong><br />
 <?php _e('This is the value of the default pricing and is shown as a flat amount or a percentage.', 'event_espresso'); ?>
 </li>
 </ul>
