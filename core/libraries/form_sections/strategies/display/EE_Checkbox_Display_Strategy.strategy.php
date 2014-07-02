d2c91afc1f424efa08dae20481d5900db5654735
Brent Christensen <brent@eventespresso.com>
2014-07-02 12:24:09 -0700

    cody tidy [touch:6081]

--- core/libraries/form_sections/strategies/display/EE_Checkbox_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Checkbox_Display_Strategy.strategy.php
@@ -1,38 +1,39 @@
 <?php
 class EE_Checkbox_Display_Strategy extends EE_Select_Display_Strategy{
-	
+
 	/**
-	 * 
+	 *
+	 * @throws EE_Error
 	 * @return string of html to display the field
 	 */
 	function display(){
-		$input = $this->_input;		
-		if( ! $input instanceof EE_Form_Input_With_Options_Base){
-			throw new EE_Error(sprintf(__("Cannot use Checkbox Display Strategy with an input that doesn't ahve options", "event_espresso")));
+		if( ! $this->_input instanceof EE_Form_Input_With_Options_Base ){
+			throw new EE_Error(sprintf(__("Cannot use Checkbox Display Strategy with an input that doesn't have options", "event_espresso")));
 		}
-		$html='';
-		foreach($input->options() as $value => $display_text){
-			if($input->raw_value() && in_array("$value", $input->raw_value())){
-				$selected_attr = 'checked';
-			}else{
-				$selected_attr ='';
+		//d( $this->_input );
+		$html = '';
+		foreach( $this->_input->options() as $value => $display_text ){
+			if ( $this->_input->raw_value() && in_array( $value, $this->_input->raw_value() )){
+				$selected_attr = ' checked="checked"';
+			} else {
+				$selected_attr = '';
 			}
-			$slugified_value = sanitize_key($value);
-			$value_inside_attribute = esc_attr($value);
-			$html.="<input id='{$input->html_id()}-{$slugified_value}' name='{$input->html_name()}[]' class='{$input->html_class()}' style='{$input->html_style()}' type='checkbox' value='$value_inside_attribute' $selected_attr><label for='{$input->html_id()}-{$slugified_value}'>$display_text</label>";
+			$value_slug = sanitize_key( $value );
+			$value_inside_attribute = esc_attr( $value );
+			$html .= '<label for="' . $this->_input->html_name() . '-' . $value_slug . '"><input id="' . $this->_input->html_id() . '-' . $value_slug . '" name="' . $this->_input->html_name() . '[]" class="' . $this->_input->html_class() . '" style="' . $this->_input->html_style() . '" type="checkbox" value="' . $value_inside_attribute . '"' . $selected_attr . '>' . $display_text . '</label>';
 		}
 		return $html;
 	}
+
+
+
 	/**
 	 * Gets the HTML for the 'label', which is just text for this (because labels
 	 * should be for each input)
 	 * @return string
 	 */
 	public function display_label(){
-		$input = $this->_input;
-		return "<span id='{$input->html_label_id()}' class='{$input->html_label_class()}' style='{$input->html_label_style()}'>".
-				$input->html_label_text().
-				"</span>";
+		return '<span id="' . $this->_input->html_label_id() . '" class="' . $this->_input->html_label_class() . '" style="' . $this->_input->html_label_style() . '">' . $this->_input->html_label_text() . '</span>';
 	}
-	
+
 }
