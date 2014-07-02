e682003d59145af4586b84c3b4027b00ba68c533
Brent Christensen <brent@eventespresso.com>
2014-07-02 14:08:53 -0700

    twerk labels and copy option links [touch:6081]

--- modules/single_page_checkout/templates/attendee_information/copy_attendee_info.template.php
+++ modules/single_page_checkout/templates/attendee_information/copy_attendee_info.template.php
@@ -1,15 +1,15 @@
 <div id="spco-copy-attendee-dv" class="hide-if-no-js">
 
 	<p class="spco-copy-all-attendee-pg">
-		<label class="wide"><?php  _e('Use Attendee #1\'s information for ALL attendees', 'event_espresso');?>
+		<label class="ee-checkbox-label-before ee-checkbox-label-wide"><?php  _e('Use Attendee #1\'s information for ALL attendees', 'event_espresso');?>
 			<input id="spco-copy-all-attendee-chk" class="spco-copy-all-attendee-chk ui-widget-content ui-corner-all" type="checkbox" value="copy-all">
 		</label>
 	</p>
 
 	<p class="spco-copy-attendee-pg"><?php echo apply_filters(  'FHEE__registration_page_attendee_information__copy_attendee_pg', sprintf( __('This option allows you to use the above information for all additional attendee question fields. %sPlease note:%s some events may have additional questions that you may still be required to answer in order to complete your registration.', 'event_espresso'), '<strong>', '</strong>' )); ?></p>
 
-	<a id="display-more-attendee-copy-options" class="display-the-hidden smaller-text float-right" rel="more-attendee-copy-options" ><?php  _e('advanced copy options', 'event_espresso');?></a>
-	<a id="hide-more-attendee-copy-options" class="hide-the-displayed smaller-text float-right" rel="more-attendee-copy-options" style="display: none;"><?php  _e('basic copy options', 'event_espresso');?></a>
+	<a id="display-more-attendee-copy-options" class="display-the-hidden smaller-text float-right" rel="more-attendee-copy-options" ><span class="dashicons dashicons-arrow-right"></span><?php  _e('advanced copy options', 'event_espresso');?></a>
+	<a id="hide-more-attendee-copy-options" class="hide-the-displayed smaller-text float-right" rel="more-attendee-copy-options" style="display: none;"><span class="dashicons dashicons-arrow-down"></span><?php  _e('basic copy options', 'event_espresso');?></a>
 
 	<div id="more-attendee-copy-options-dv" class="" style="display: none;">
 
