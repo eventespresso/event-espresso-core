00ae4fd4648d5123cc32592a36681b51c0dcf9d3
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2014-07-01 20:00:33 -0400

    Fix translation code for registration form pages. [touch:5988]

--- admin_pages/registration_form/help_tabs/registration_form_add_question.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_add_question.help_tab.php
@@ -5,43 +5,45 @@
 <p>
 <ul>
 <li>
-<?php _e('<strong>Question Text</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Question Text', 'event_espresso'); ?></strong><br />
 <?php _e('This shows the question as it will be displayed on the registration form. HTML can be used here.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Question Label (admin-only)</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Question Label (admin-only)', 'event_espresso'); ?></strong><br />
 <?php _e('This field is only shown to the admin. It is useful for understanding the difference between questions that appear to be similar but are used in various situations. HTML cannot be used.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Question Order</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Question Order', 'event_espresso'); ?></strong><br />
 <?php _e('Questions can be sorted by using the drag and drop feature on the Questions Overview page. Another way to sort questions is to adjust their order of application by using this field. A smaller order of application will be shown first. For example, a question with an order of application of 1 will appear before one that has an order of application of 5.', 'event_espresso'); ?>
 </li>
 </li>
 <li>
-<?php _e('<strong>Admin-Only Question?</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Admin-Only Question?', 'event_espresso'); ?></strong><br />
 <?php _e('Whether this question should be shown to admins only.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Question Type</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Question Type', 'event_espresso'); ?></strong><br />
 <?php _e('This shows the type of question. Available options are Text, Textarea, Single, Dropdown, Multiple Choice, and Date.', 'event_espresso'); ?>
 </li>
 <li>
-	<?php _e('Answer Options (for applicable question types)</strong>', 'event_espresso');?><br/>
-	<?php _e('Values do not allow any HTML, however, descriptions do allow HTML.', 'event_espresso');?>
+<strong><?php _e('Answer Options (for applicable question types)</strong>', 'event_espresso');?><br/>
+<strong><?php _e('Values do not allow any HTML, however, descriptions do allow HTML.', 'event_espresso');?>
 </li>
 <li>
-<?php _e('<strong>Required Question</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Required Question', 'event_espresso'); ?></strong><br />
 <?php _e('Whether registrants are required to answer this question.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Required Text</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Required Text', 'event_espresso'); ?></strong><br />
 <?php _e('This text is displayed when a registrant does not answer a question but is required to do so. The default message will show "this field is required."', 'event_espresso'); ?>
 </li>
 </ul>
 </p>
 <p>
-<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Add New Question Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
+<strong><?php _e('Recommendations', 'event_espresso'); ?></strong><br />
+<?php _e('Want to see a tour of this screen? Click on the Add New Question Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
 </p>
 <p>
-<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
+<strong><?php _e('Screen Options', 'event_espresso'); ?></strong><br />
+<?php _e('You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
 </p>
--- admin_pages/registration_form/help_tabs/registration_form_add_question_group.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_add_question_group.help_tab.php
@@ -5,38 +5,40 @@
 <p>
 <ul>
 <li>
-<?php _e('<strong>Group Name</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Group Name', 'event_espresso'); ?></strong><br />
 <?php _e('This is the name (title) of the question group as it will be displayed on the registration form. HTML cannot be used.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Group Identifier</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Group Identifier', 'event_espresso'); ?></strong><br />
 <?php _e('This is a unique name (slug) for this group. It helps you tell the difference between this group and other groups that may appear to be similar. It is not shown to registrants and HTML cannot be used.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Group Description</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Group Description', 'event_espresso'); ?></strong><br />
 <?php _e('This is the description for the question group as it will be shown on the registration form. This can be shown to registrants and HTML is allowed.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Question Group Order</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Question Group Order', 'event_espresso'); ?></strong><br />
 <?php _e('Question Groups can be sorted by using the drag and drop feature on the Questions Group page. Another way to sort question groups is to adjust their order of application by using this field. A smaller order of application will be shown first. For example, a question group with an order of application of 1 will appear before one that has an order of application of 5.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Show Name</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Show Name', 'event_espresso'); ?></strong><br />
 <?php _e('Whether the group name will be displayed on the registration form.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Show Description</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Show Description', 'event_espresso'); ?></strong><br />
 <?php _e('Whether the description for the question group be will displayed on the registration form.', 'event_espresso'); ?>
 </li>
 <li>
-<?php _e('<strong>Questions that appear in this group</strong>', 'event_espresso'); ?><br />
+<strong><?php _e('Questions that appear in this group', 'event_espresso'); ?></strong><br />
 <?php _e('This shows all available custom questions. If checked, then that question will appear with this question group.', 'event_espresso'); ?>
 </li>
 </ul>
 </p>
 <p>
-<?php _e('<strong>Recommendations</strong><br /> Want to see a tour of this screen? Click on the Add New Question Group Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
+<strong><?php _e('Recommendations', 'event_espresso'); ?></strong><br />
+<?php _e('Want to see a tour of this screen? Click on the Add New Question Group Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
 </p>
 <p>
-<?php _e('<strong>Screen Options</strong><br /> You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
+<strong><?php _e('Screen Options', 'event_espresso'); ?></strong><br />
+<?php _e('You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.', 'event_espresso'); ?>
 </p>
