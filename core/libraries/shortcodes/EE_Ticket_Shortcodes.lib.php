4d3934e5f311cb3d522a2fbcab03467a87a93d08
Darren Ethier <darren@roughsmootheng.in>
2014-07-03 09:17:58 -0400

    modification to EE_Ticket_Shortcodes library to allow for alternate incoming data when for a single recipient [touch: 6218]

--- core/libraries/shortcodes/EE_Ticket_Shortcodes.lib.php
+++ core/libraries/shortcodes/EE_Ticket_Shortcodes.lib.php
@@ -18,11 +18,11 @@ if (!defined('EVENT_ESPRESSO_VERSION') )
  * ------------------------------------------------------------------------
  *
  * EE_Ticket_Shortcodes
- * 
- * this is a child class for the EE_Shortcodes library.  The EE_Ticket_Shortcodes lists all shortcodes related to ticket specific info. 
+ *
+ * this is a child class for the EE_Shortcodes library.  The EE_Ticket_Shortcodes lists all shortcodes related to ticket specific info.
  *
  * NOTE: if a method doesn't have any phpdoc commenting the details can be found in the comments in EE_Shortcodes parent class.
- * 
+ *
  * @package		Event Espresso
  * @subpackage	libraries/shortcodes/EE_Ticket_Shortcodes.lib.php
  * @author		Darren Ethier
@@ -30,6 +30,14 @@ if (!defined('EVENT_ESPRESSO_VERSION') )
  * ------------------------------------------------------------------------
  */
 class EE_Ticket_Shortcodes extends EE_Shortcodes {
+
+
+	/**
+	 * Will hold the EE_Ticket if available
+	 *
+	 * @var EE_Ticket
+	 */
+	protected $_ticket;
 
 
 	protected function _init_props() {
@@ -48,29 +56,42 @@ class EE_Ticket_Shortcodes extends EE_Shortcodes {
 	protected function _parser( $shortcode ) {
 
 		EE_Registry::instance()->load_helper( 'Template' );
-		if ( ! $this->_data instanceof EE_Ticket )
-			return ''; //get out cause we can only parse with the ticket object.
+
+		$this->_ticket = $this->_data instanceof EE_Ticket ? $this->_data : null;
+
+		//if no event, then let's see if there is a reg_obj.  If there IS, then we'll try and grab the event from the reg_obj instead.
+		if ( empty( $this->_ticket ) ) {
+			$aee = $this->_data instanceof EE_Messages_Addressee ? $this->_data : NULL;
+			$aee = $this->_extra_data instanceof EE_Messages_Addressee ? $this->_extra_data : $aee;
+
+			$this->_ticket = $aee instanceof EE_Messages_Addressee && $aee->reg_obj instanceof EE_Registration ? $aee->reg_obj->ticket() : NULL;
+		}
+
+
+		//If there is no event objecdt by now then get out.
+		if ( ! $this->_ticket instanceof EE_Ticket )
+			return '';
 
 		switch ( $shortcode ) {
-			
+
 			case '[TICKET_ID]' :
-				return $this->_data->ID();
+				return $this->_ticket->ID();
 				break;
 
 			case '[TICKET_NAME]' :
-				return $this->_data->get('TKT_name');
+				return $this->_ticket->get('TKT_name');
 				break;
 
 			case '[TICKET_DESCRIPTION]' :
-				return $this->_data->get('TKT_description');
+				return $this->_ticket->get('TKT_description');
 				break;
 
 			case '[TICKET_PRICE]' :
-				return EEH_Template::format_currency( $this->_data->get('TKT_price') );
+				return EEH_Template::format_currency( $this->_ticket->get('TKT_price') );
 				break;
 
 			case '[TKT_QTY_PURCHASED]' :
-				return $this->_extra_data['data']->tickets[$this->_data->ID()]['count'];
+				return $this->_extra_data['data']->tickets[$this->_ticket->ID()]['count'];
 				break;
 		}
 
