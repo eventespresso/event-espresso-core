9b597807348a8b462ab0fcf6363a29838b37cbdb
Brent Christensen <brent@eventespresso.com>
2014-07-03 16:36:51 -0700

    add required input class and twerk code layout to improve readability [touch:6081]

--- core/libraries/form_sections/strategies/display/EE_Text_Input_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Text_Input_Display_Strategy.strategy.php
@@ -1,12 +1,19 @@
 <?php
 class EE_Text_Input_Display_Strategy extends EE_Display_Strategy_Base{
 	/**
-	 * 
+	 *
 	 * @return string of html to display the field
 	 */
 	function display(){
-		$input = $this->_input;		
-		return "<input type='text' id='{$input->html_id()}' name='{$input->html_name()}' class='{$input->html_class()}' style='{$input->html_style()}' value='{$input->raw_value_in_form()}'/>";
+		$input = '<input type="text" ';
+		$input .= 'name="' . $this->_input->html_name() . '" ';
+		$input .= 'id="' . $this->_input->html_id() . '" ';
+		$class = $this->_input->required() ? 'ee-needs-value ' . $this->_input->html_class() : $this->_input->html_class();
+		$input .= 'class="' . $class . '" ';
+		$input .= 'value="' . $this->_input->raw_value_in_form() . '"';
+		$input .= 'style="' . $this->_input->html_style() . '" ';
+		$input .= '/>';
+		return $input;
 	}
-	
+
 }
