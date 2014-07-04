737c5d462a429024ae3a6eecdfd13f7d5b079a77
Michael Nelson <michael@eventespresso.com>
2014-07-03 21:47:02 -0700

    [touch:2639]{t:90}refactored how server-side validation errors work. For non-js-enabled browsers we just put a validation errors label on each input with errors. For js-enabled browsers, we use js to remove those validation error labels, and just use jquery to populate the validation errors (because it puts them in the right place, and it was buggy if we manually placed the errors in dom elements it was going to use). Also getting better unique ids for html form tags that dont have names so we can later identify them

--- core/libraries/form_sections/strategies/layout/EE_Form_Section_Layout_Base.strategy.php
+++ core/libraries/form_sections/strategies/layout/EE_Form_Section_Layout_Base.strategy.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Abstract parent class for all form layouts. Mostly just contains a reference to the form 
+ * Abstract parent class for all form layouts. Mostly just contains a reference to the form
  * we are to lay out.
  * Form layouts should add HTML content for each form section (eg a header and footer)
  * for the form section, and dictate how to layout all the inputs and proper subsections
@@ -14,7 +14,7 @@ abstract class EE_Form_Section_Layout_Base{
 	 * @var EE_Form_Section_Proper
 	 */
 	protected $_form_section;
-		
+
 	function __construct(){
 		EE_Registry::instance()->load_helper('Formatter');
 	}
@@ -26,7 +26,7 @@ abstract class EE_Form_Section_Layout_Base{
 	function _construct_finalize(EE_Form_Section_Proper $form){
 		$this->_form_section = $form;
 	}
-	
+
 	/**
 	 * Also has teh side effect of enqueuing any needed JS and CSS for
 	 * this form.
@@ -59,7 +59,7 @@ abstract class EE_Form_Section_Layout_Base{
 	 * @return string
 	 */
 	abstract function layout_form_begin();
-	
+
 	/**
 	 * Should be used to end the form section (eg a /table tag, or a /div tag, etc)
 	 * @return string
@@ -70,14 +70,14 @@ abstract class EE_Form_Section_Layout_Base{
 	 * is putting each input in a row of its own, this should probably be called by a
 	 *  foreach loop in layout_form() (WITHOUT adding any content directly within layout_form()'s foreach loop.
 	 * Eg, this method should add the tr and td tags). This method is exposed in case you want to completely
-	 * customize the form's layout, but would like to make use of it for laying out 
+	 * customize the form's layout, but would like to make use of it for laying out
 	 * 'easy-to-layout' inputs
 	 * @param EE_Form_Input_Base $input
 	 * @return string html
 	 */
 	abstract function layout_input($input);
 	/**
-	 * Similar to layout_input(), should be used internally by layout_form() within a 
+	 * Similar to layout_input(), should be used internally by layout_form() within a
 	 * loop to layout each proper subsection. Unlike layout_input(), however, it is assumed
 	 * that the proper subsection will layout its container, label, etc on its own.
 	 * @param EE_Form_Section_Proper $subsection
@@ -85,7 +85,7 @@ abstract class EE_Form_Section_Layout_Base{
 	 * @return string html
 	 */
 	abstract function layout_subsection($subsection);
-	
+
 	/**
 	 * Gets the HTML for the label tag and its contents for the input
 	 * @param EE_Form_Input_Base $input
@@ -101,16 +101,21 @@ abstract class EE_Form_Section_Layout_Base{
 				"</label>";
 	}
 	/**
-	 * returns the HTML for the errors for the specified input
+	 * returns the HTML for the server-side validation errors for the specified input
+	 * Note that if JS is enabled, it should remove these and instead
+	 * populate the form's errors in the jquery validate fashion
+	 * using the localizated data provided to the JS
 	 * @param EE_Form_Input_Base $input
 	 * @return string
 	 */
 	public function display_errors($input){
-		return "<label id='{$input->html_id()}-errors' class='error' for='{$input->html_id()}'>".
-				$input->get_validation_error_string().
-				"</label>";
+		if( $input->get_validation_errors() ){
+			return  "<label class='error' for='{$input->html_name()}'>" . $input->get_validation_error_string() . "</label>";
+		}else{
+			return '';
+		}
 	}
-	
+
 	/**
 	 * Displays the help span for the specified input
 	 * @param EE_Form_Input_Base $input
@@ -120,7 +125,7 @@ abstract class EE_Form_Section_Layout_Base{
 		return "<span id='{$input->html_id()}-help' class='{$input->html_help_class()}' style='{$input->html_help_style()}'>".
 				$input->html_help_text().
 				"</span>";
-	}	
+	}
 
 	/**
 	 * Does an action and hook onto the end of teh form
