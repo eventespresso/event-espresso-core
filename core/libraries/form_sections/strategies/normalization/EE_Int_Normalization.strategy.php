45871d0297f48f35ecd54c319352b43bb33ebf04
Michael Nelson <michael@eventespresso.com>
2014-07-15 11:50:43 -0700

    [touch:6223]{t:20}added unit test for EE_Int_Normalization and verified brents suggested solution worked

--- core/libraries/form_sections/strategies/normalization/EE_Int_Normalization.strategy.php
+++ core/libraries/form_sections/strategies/normalization/EE_Int_Normalization.strategy.php
@@ -28,11 +28,19 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  */
 class EE_Int_Normalization extends EE_Normalization_Strategy_Base{
 	public function normalize($value_to_normalize) {
-		$value_string_with_only_numbers = preg_replace( "/[^0-9]/", "",$value_to_normalize);
-		if(strlen($value_to_normalize) > strlen($value_string_with_only_numbers)){
-			throw new EE_Validation_Error(sprintf(__("Only numeric characters, please!", "event_espresso")), 'numeric_only');
-		}else{
-			return intval($value_string_with_only_numbers);
+		if( is_int( $value_to_normalize ) ){
+			return $value_to_normalize;
+		}
+		if( ! is_string( $value_to_normalize ) ){
+			throw new EE_Validation_Error( sprintf( __( 'The value "%s" must be a string submitted for normalization, it was %s', 'event_espresso' ), print_r( $value_to_normalize, TRUE), gettype( $value_to_normalize ) ) );
+		}
+		$thousands_seperator = EE_Config::instance()->currency->thsnds;
+		$value_to_normalize = str_replace( $thousands_seperator, "", $value_to_normalize );
+		$value_to_normalize = str_replace( array(" ","\t"), '', $value_to_normalize );
+		if ( preg_match( '/^\d+$/', $value_to_normalize )) {
+			return intval( $value_to_normalize );
+		} else {
+			throw new EE_Validation_Error( sprintf( __( "Only numeric characters, please!", "event_espresso" ) ), 'numeric_only' );
 		}
 	}
 
