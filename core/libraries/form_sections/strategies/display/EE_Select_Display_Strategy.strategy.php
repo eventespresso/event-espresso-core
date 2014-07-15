d8661580ade8f871a609fe1bfade4c23a4682145
Michael Nelson <michael@eventespresso.com>
2014-07-15 12:52:58 -0700

    [touch:6034]{t:30}tweaked and tested EEH_Formatter indentation code for EE_Select_Display_Strategy

--- core/libraries/form_sections/strategies/display/EE_Select_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Select_Display_Strategy.strategy.php
@@ -9,18 +9,21 @@ class EE_Select_Display_Strategy extends EE_Display_Strategy_Base{
 	 * @return string of html to display the field
 	 */
 	function display(){
+		EE_Registry::instance()->load_helper('Formatter');
 		$input = $this->_input;
 		if( ! $input instanceof EE_Form_Input_With_Options_Base){
 			throw new EE_Error(sprintf(__("Cannot use Select Display Strategy with an input that doesn't ahve options", "event_espresso")));
 		}
-		$html= EEH_Formatter::nl(1) . "<select id='{$input->html_id()}' name='{$input->html_name()}' class='{$input->html_class()}' style='{$input->html_style()}'/>";
+		$html= EEH_Formatter::nl() . "<select id='{$input->html_id()}' name='{$input->html_name()}' class='{$input->html_class()}' style='{$input->html_style()}'/>" ;
 		EE_Registry::instance()->load_helper('Array');
+		EEH_Formatter::increase_indent( 1 );
 		if(EEH_Array::is_multi_dimensional_array($input->options())){
 			foreach($input->options() as $opt_group_label => $opt_group){
 				$opt_group_label = esc_attr($opt_group_label);
-				$html.=EEH_Formatter::nl(1) . "<optgroup label='{$opt_group_label}'>";
+				$html.=EEH_Formatter::nl() . "<optgroup label='{$opt_group_label}'>";
+				EEH_Formatter::increase_indent( 1 );
 				$html.=$this->_display_options($opt_group);
-				$html.=EEH_Formatter::nl(-1) . "</optgroup>";
+				$html.=EEH_Formatter::nl( -1 ) . "</optgroup>";
 			}
 		}else{
 			$html.=$this->_display_options($input->options());
@@ -35,19 +38,16 @@ class EE_Select_Display_Strategy extends EE_Display_Strategy_Base{
 	 * @return string
 	 */
 	protected function _display_options($options){
-		EE_Registry::instance()->load_helper('Formatter');
+
 		$html = '';
 		foreach($options as $value => $display_text){
-			$cntr = 0;
 			if($this->_check_if_option_selected($value)){
 				$selected_attr = 'selected="selected"';
 			}else{
 				$selected_attr ='';
 			}
-			$tabs = $cntr == 0 ? 1 : 0;
 			$value_in_form = esc_attr( $this->_input->get_normalization_strategy()->unnormalize( $value ) );
-			$html.= EEH_Formatter::nl($tabs) . "<option value='$value_in_form' $selected_attr>$display_text</option>";
-			$cntr++;
+			$html.= EEH_Formatter::nl() . "<option value='$value_in_form' $selected_attr>$display_text</option>";
 		}
 		return $html;
 	}
