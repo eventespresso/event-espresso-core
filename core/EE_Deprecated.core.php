7bde2af4306a25ede4854cf079ea7d519c23e06f
Darren Ethier <darren@roughsmootheng.in>
2014-07-16 15:20:58 -0400

    add deprecated handling for the FHEE__*__get_default_field_content filter. [touch: 6262]

--- core/EE_Deprecated.core.php
+++ core/EE_Deprecated.core.php
@@ -51,3 +51,45 @@ function ee_deprecated_get_templates( $templates, EE_messenger $messenger, EE_me
 	return $templates;
 }
 add_filter( 'FHEE__EE_Template_Pack___get_templates__templates', 'ee_deprecated_get_templates', 10, 4 );
+
+
+
+
+
+/**
+ * wrapper for the now deprecated FHEE__*__get_default_field_content filter.  Note depending on how this was used, it may or may not degrade gracefully for clients using the  filter format that is "FHEE__EE_Messages_Base__get_default_field_content" as that format might have had either a EE_messenger or EE_message_type object as a param.  The backward_compat implementation assumed EE_message_type.
+ *
+ * @deprecated %VER%
+ * @deprecated Use FHEE__EE_Messages_Template_Pack__get_specific_template__contents filter instead.
+ *
+ * @param string                    $contents        The template contents being used.
+ * @param string                    $actual_path   The actual path for the template contents.
+ * @param EE_messenger              $messenger
+ * @param EE_message_type           $message_type
+ * @param string                    $field         The field the contents are for.
+ * @param string                    $context       The context the contents are for.
+ * @param EE_Messages_Template_Pack $template_pack
+ *
+ * @return string                    The default cotents for the messenger, message type, context and field.
+ */
+function ee_deprecated_get_default_field_content( $contents, $actual_path, EE_messenger $messenger, EE_message_type $message_type, $field, $context, EE_Messages_Template_Pack $template_pack ) {
+
+	$classnames_to_try = array(
+		get_class( $messenger ) => $messenger,
+		get_class( $message_type ) => $message_type,
+		'EE_Messages_Base' => $message_type
+		);
+
+
+
+	foreach ( $classnames_to_try as $classname => $obj ) {
+		$filter_ref = 'FHEE__' . $classname . '__get_default_field_content';
+		if ( has_filter( $filter_ref ) ) {
+			EE_Error::doing_it_wrong( $filter_ref, __('This filter is deprecated.  It *may* work as an attempt was to build in backward compat.  However, it is recommended to use the new filter provided which is "FHEE__EE_Messages_Template_Pack__get_specific_template__contents" found in the EE_Messages_Template_Pack class.', 'event_espresso'), '%VER%' );
+		}
+		$contents = apply_filters( $filter_ref, $contents, $obj );
+	}
+
+	return $contents;
+}
+add_filter( 'FHEE__EE_Messages_Template_Pack__get_specific_template__contents', 'ee_deprecated_get_default_field_content', 10, 7 );
