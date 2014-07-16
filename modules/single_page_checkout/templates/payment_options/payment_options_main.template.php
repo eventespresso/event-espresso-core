fbf1db81e0abae355b7fa7bb8742fff60a62aecb
Brent Christensen <brent@eventespresso.com>
2014-07-16 14:32:22 -0700

    simplify payment method display [touch:6081]

--- modules/single_page_checkout/templates/payment_options/payment_options_main.template.php
+++ modules/single_page_checkout/templates/payment_options/payment_options_main.template.php
@@ -17,49 +17,7 @@
 			<tbody>
 			<?php echo $transaction_details;?>
 <?php
-/*	$prev_ticket = NULL;
-	foreach ( $event_queue['items'] as $line_item => $item ) {
-		if ( $item['ticket'] instanceof EE_Ticket && $prev_ticket != $item['ticket']->ID() ) {
-*/?><!--
-				<tr>
-					<td>
-					<?php
-/*						echo $item['ticket']->name();
-						echo $item['ticket']->description() ? '<br/>' . $item['ticket']->description() : '';
-					*/?>
-					</td>
-					<td class="jst-rght"><?php /*echo $ticket_count[ $item['ticket']->ID() ];*/?></td>
-					<td class="jst-rght"><?php /*echo EEH_Template::format_currency( $item['ticket']->price() );*/?></td>
-					<td class="jst-rght"><?php /*echo EEH_Template::format_currency( $item['ticket']->price() * $ticket_count[ $item['ticket']->ID() ] );*/?></td>
-				</tr>
-<?php
-/*			do_action( 'AHEE__registration_page_payment_options__payment_info_table_ticket_row_end', $item['ticket'] );
-			$prev_ticket = $item['ticket']->ID();
-		}
-	}
-	if ( $sub_total != ( $item['ticket']->price() * $ticket_count[ $item['ticket']->ID() ] )) {
-*/?>
-				<tr class="reg-page-totals-spn">
-					<td class="jst-rght"><?php /*echo __('Sub Total: ', 'event_espresso');*/?></td>
-					<td colspan="3" class="jst-rght"><?php /*echo EEH_Template::format_currency( $sub_total );*/?></td>
-					<?php	/*do_action( 'AHEE__registration_page_payment_options__payment_info_table_sub_total_row_end', $sub_total ); */?>
-				</tr>
-<?php
-/*	}
-	if ( $taxes ) {
-		foreach ( $taxes as $tax ){
-			if( (float)$tax->total() > 0 ) {
-*/?>
-				<tr class="reg-page-totals-spn">
-					<td class="jst-rght"><?php /*echo $tax->percent() . '% ' . $tax->name();*/?></td>
-					<td colspan="3" class="jst-rght"><?php /*echo EEH_Template::format_currency( $tax->total() );*/?></td>
-					<?php	/*do_action( 'AHEE__registration_page_payment_options__payment_info_table_tax_row_end', $tax ); */?>
-				</tr>
-<?php
-/*			}
-		}
-	}
-	if ( $payments ) {
+/*		if ( $payments ) {
 		foreach ( $payments as $payment ){
 			if ( $payment instanceof EE_Payment ) {
 */?>
@@ -93,15 +51,10 @@
 	</div>
 
 	<?php echo $hidden_inputs; ?>
+
 	<div id="methods-of-payment">
 		<h3 id="select-method-of-payment-hdr"><?php echo apply_filters( 'FHEE__registration_page_payment_options__select_method_of_payment_hdr', __( 'Please select your method of payment:', 'event_espresso' )); ?></h3>
-		<?php
-			foreach( $available_payment_methods as $payment_method_options ) {
-				foreach( $payment_method_options as $payment_method_option_html ) {
-					echo $payment_method_option_html;
-				}
-			}
-		 ?>
+		<?php echo $available_payment_methods; ?>
 		<a id="reg-page-select-other-method-of-payment-lnk" class="hidden smaller-text right" rel=""><?php echo apply_filters( 'FHEE__registration_page_payment_options__select_other_method_of_payment_lnk', __( 'select a different method of payment:', 'event_espresso' )); ?></a>
 	</div>
 	<!-- end #methods-of-payment -->
