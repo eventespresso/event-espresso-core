274fc08faf9caecff18d0808df3ad32984b10a45
Darren Ethier <darren@roughsmootheng.in>
2014-07-16 14:55:06 -0400

    add code for handling the deprecated FHEE__*___create_new_templates__templates filter. [touch: 6262]

--- core/EE_Deprecated.core.php
+++ core/EE_Deprecated.core.php
@@ -6,3 +6,48 @@
  * @since           4.5.0
  */
 if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+
+
+/** Messages System deprecated things **/
+
+
+/**
+ * wrapper for deprecated FHEE__*___create_new_templates__templates filter.  Note depending on how this was used, it may or may not degrade gracefully for clients using the existing filter because the old Default classes are NOT present anymore.
+ *
+ * @deprecated %VER%
+ * @deprecated Use FHEE__EE_Template_Pack___get_templates__templates filter instead.
+ *
+ * @param array                            $templates      array of generated templates
+ * @param EE_messenger              $messenger
+ * @param EE_message_type         $message_type
+ * @param EE_Messages_Template_Pack $template_pack
+ *
+ * @return array
+ */
+function ee_deprecated_get_templates( $templates, EE_messenger $messenger, EE_message_type $message_type, EE_Messages_Template_Pack $template_pack ) {
+	$old_default_classnames = array(
+		'EE_Messages_Email_Cancelled_Registration_Defaults',
+		'EE_Messages_Email_Declined_Registration_Defaults',
+		'EE_Messages_Email_Not_Approved_Registration_Defaults',
+		'EE_Messages_Email_Payment_Declined_Defaults',
+		'EE_Messages_Email_Payment_Defaults',
+		'EE_Messages_Email_Payment_Reminder_Defaults',
+		'EE_Messages_Email_Pending_Approval_Defaults',
+		'EE_Messages_Email_Registration_Defaults',
+		'EE_Messages_Email_Newsletter_Defaults',
+		'EE_Message_Template_Defaults'
+		);
+
+	$old_class_instance = new stdClass();
+
+	foreach ( $old_default_classnames as $classname ) {
+		$filter_ref = 'FHEE__' . $classname . '___create_new_templates___templates';
+		if ( has_filter( $filter_ref ) ) {
+			EE_Error::doing_it_wrong( $filter_ref, __('This filter is deprecated.  It *may* work as an attempt was to build in backward compat.  However, it is recommended to use the new filter provided which is "FHEE__EE_Template_Pack___get_templates__templates" found in the EE_Messages_Template_Pack class.', 'event_espresso'), '%VER%' );
+		}
+		$templates = apply_filters( $filter_ref, $templates, $old_class );
+	}
+
+	return $templates;
+}
+add_filter( 'FHEE__EE_Template_Pack___get_templates__templates', 'ee_deprecated_get_templates', 10, 4 );
