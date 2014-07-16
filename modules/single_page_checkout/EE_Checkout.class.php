ef31a2bdb568411cb55577b2df1a6f32b43e04ac
Brent Christensen <brent@eventespresso.com>
2014-07-16 08:46:11 -0700

    move all methods pertaining to reg steps out of main SPCO module  and into EE_CHeckout [touch:6081]

--- modules/single_page_checkout/EE_Checkout.class.php
+++ modules/single_page_checkout/EE_Checkout.class.php
@@ -148,6 +148,106 @@ class EE_Checkout {
 
 
 	/**
+	 *    add_reg_step
+	 *
+	 * @access    public
+	 * @param EE_SPCO_Reg_Step $reg_step_obj
+	 * @return    void
+	 */
+	public function add_reg_step( EE_SPCO_Reg_Step $reg_step_obj ) {
+		$this->reg_steps[ $reg_step_obj->slug()  ] = $reg_step_obj;
+	}
+
+
+
+	/**
+	 *    remove_reg_step
+	 *
+	 * @access    public
+	 * @param string $reg_step_slug
+	 * @return    void
+	 */
+	public function remove_reg_step( $reg_step_slug = '' ) {
+		unset( $this->reg_steps[ $reg_step_slug  ] );
+	}
+
+
+
+	/**
+	 *    set_reg_step_order
+	 *
+	 * @access    public
+	 * @param string $reg_step_slug
+	 * @param int    $order
+	 * @return    void
+	 */
+	public function set_reg_step_order( $reg_step_slug = '', $order = 100 ) {
+		if ( isset( $this->reg_steps[ $reg_step_slug  ] )) {
+			$this->reg_steps[ $reg_step_slug ]->set_order( $order );
+		}
+	}
+
+
+
+	/**
+	 *    set_current_step
+	 *
+	 * @access    public
+	 * @param string $current_step
+	 * @return    void
+	 */
+	public function set_current_step( $current_step = 'attendee_information' ) {
+		// grab what step we're on
+		$this->current_step = isset( $this->reg_steps[ $current_step ] ) ? $this->reg_steps[ $current_step ] : reset( $this->reg_steps );
+		$this->current_step->set_is_current_step( TRUE );
+	}
+
+
+
+	/**
+	 * 	set_next_step
+	 * advances the reg_steps array pointer and sets the next step, then reverses pointer back to the current step
+	 *
+	 *  @access 	public
+	 *  @return 	void
+	 */
+	public function set_next_step() {
+		// set pointer to start of array
+		reset( $this->reg_steps );
+		// if there is more than one step
+		if ( count( $this->reg_steps ) > 1 && $this->current_step->slug() != 'finalize_registration' ) {
+			// advance to the current step and set pointer
+			while ( key( $this->reg_steps ) != $this->current_step->slug() && key( $this->reg_steps ) != '' ) {
+				next( $this->reg_steps );
+			}
+		}
+		// advance one more spot ( if it exists )
+		$this->next_step = next( $this->reg_steps );
+		// verify instance
+		$this->next_step = $this->next_step instanceof EE_SPCO_Reg_Step ? $this->next_step  : 'finalize_registration';
+		// then back to current step to reset
+		prev( $this->reg_steps );
+	}
+
+
+
+
+	/**
+	 * 	get_next_reg_step
+	 * 	this simply returns the next step from reg_steps array
+	 *
+	 *  @access 	public
+	 *  @return 	EE_SPCO_Reg_Step
+	 */
+	public function get_next_reg_step() {
+		$next = next( $this->reg_steps );
+		prev( $this->reg_steps );
+		return $next;
+	}
+
+
+
+	/**
 	 * sort_reg_steps
 	 *
 	 * @access public
@@ -155,7 +255,7 @@ class EE_Checkout {
 	 */
 	public function sort_reg_steps() {
 		$reg_step_sorting_callback = apply_filters( 'FHEE__EE_Checkout__sort_reg_steps__reg_step_sorting_callback', 'reg_step_sorting_callback' );
-		usort( $this->reg_steps, array( $this, $reg_step_sorting_callback ));
+		uasort( $this->reg_steps, array( $this, $reg_step_sorting_callback ));
 	}
 
 
@@ -169,6 +269,12 @@ class EE_Checkout {
 	 * @return array()
 	 */
 	public function reg_step_sorting_callback( EE_SPCO_Reg_Step $reg_step_A, EE_SPCO_Reg_Step $reg_step_B ) {
+		// send finalize_registration step to the end of the array
+		if ( $reg_step_A->slug() == 'finalize_registration' ) {
+			return 1;
+		} else if ( $reg_step_B->slug() == 'finalize_registration' ) {
+			return -1;
+		}
 		if ( $reg_step_A->order() == $reg_step_B->order() ) {
 			return 0;
 		}
