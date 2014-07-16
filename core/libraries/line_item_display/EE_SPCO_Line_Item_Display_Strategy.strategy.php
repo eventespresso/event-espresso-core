bbdef241214a0c2c2077506b78e4fbd5bd9b5eb5
Brent Christensen <brent@eventespresso.com>
2014-07-16 13:02:15 -0700

    added $_show_taxes boolean property for toggling display of tax rows. combine name and desc fields. fix colspan attributes [touch:6081]

--- core/libraries/line_item_display/EE_SPCO_Line_Item_Display_Strategy.strategy.php
+++ core/libraries/line_item_display/EE_SPCO_Line_Item_Display_Strategy.strategy.php
@@ -14,6 +14,8 @@
 
 class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 
+	private $_show_taxes = FALSE;
+
 	/**
 	 * @param EE_Line_Item $line_item
 	 * @param array        $options
@@ -26,43 +28,12 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 		$html = '';
 		// set some default options and merge with incoming
 		$default_options = array(
-			'show_desc' => TRUE,
+			'show_desc' => TRUE,  // 	TRUE 		FALSE
 			'odd' => FALSE
 		);
 		$options = array_merge( $default_options, (array)$options );
 
 		switch( $line_item->type() ) {
-
-			case EEM_Line_Item::type_total:
-				// loop thru children
-				foreach( $line_item->children() as $child_line_item ) {
-					// recursively feed children back into this method
-					$html .= $this->display_line_item( $child_line_item, $options );
-				}
-				$html .= $this->_separator_row( $options );
-				$html .= $this->_total_row( $line_item, __('Total', 'event_espresso'), $options );
-				break;
-
-
-			case EEM_Line_Item::type_sub_total:
-				// loop thru children
-				foreach( $line_item->children() as $child_line_item ) {
-					// recursively feed children back into this method
-					$html .= $this->display_line_item( $child_line_item, $options );
-				}
-				$html .= $this->_total_row( $line_item, __('Sub-Total', 'event_espresso'), $options );
-				break;
-
-
-			case EEM_Line_Item::type_tax_sub_total:
-				// loop thru children
-				foreach( $line_item->children() as $child_line_item ) {
-					// recursively feed children back into this method
-					$html .= $this->display_line_item( $child_line_item, $options );
-				}
-				$html .= $this->_total_row( $line_item, __('Tax Total', 'event_espresso'), $options );
-				break;
-
 
 			case EEM_Line_Item::type_line_item:
 				// item row
@@ -73,14 +44,43 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 				}
 				break;
 
-
 			case EEM_Line_Item::type_sub_line_item:
 				$html .= $this->_sub_item_row( $line_item, $options );
 				break;
 
+			case EEM_Line_Item::type_sub_total:
+				// loop thru children
+				foreach( $line_item->children() as $child_line_item ) {
+					// recursively feed children back into this method
+					$html .= $this->display_line_item( $child_line_item, $options );
+				}
+				$html .= $this->_sub_total_row( $line_item, __('Sub-Total', 'event_espresso'), $options );
+				break;
 
 			case EEM_Line_Item::type_tax:
-				$html .= $this->_tax_row( $line_item, $options );
+				if ( $this->_show_taxes ) {
+					$html .= $this->_tax_row( $line_item, $options );
+				}
+				break;
+
+			case EEM_Line_Item::type_tax_sub_total:
+				if ( $this->_show_taxes ) {
+					// loop thru children
+					foreach( $line_item->children() as $child_line_item ) {
+						// recursively feed children back into this method
+						$html .= $this->display_line_item( $child_line_item, $options );
+					}
+					$html .= $this->_total_row( $line_item, __('Tax Total', 'event_espresso'), $options );
+				}
+				break;
+
+			case EEM_Line_Item::type_total:
+				// loop thru children
+				foreach( $line_item->children() as $child_line_item ) {
+					// recursively feed children back into this method
+					$html .= $this->display_line_item( $child_line_item, $options );
+				}
+				$html .= $this->_total_row( $line_item, __('Total', 'event_espresso'), $options );
 				break;
 
 		}
@@ -100,18 +100,20 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 	private function _item_row( EE_Line_Item $line_item, $options = array() ) {
 		// start of row
 		$row_class = $options['odd'] ? 'item odd' : 'item';
-		$html = EEH_HTML::tr( '', $row_class );
+		$html = EEH_HTML::tr( '', '', $row_class );
+		// name && desc
+		$name_and_desc = $line_item->name();
+		$name_and_desc .= $options['show_desc'] ? '<br/>' . $line_item->desc() : '';
 		// name td
-		$html .= EEH_HTML::td( $line_item->name(), '',  'item_l' );
-		// desc td
-		$html .= $options['show_desc'] ? EEH_HTML::td( $line_item->desc(), '',  'item_l' ) : '';
+		$html .= EEH_HTML::td( $name_and_desc, '',  'item_l' );
 		// quantity td
-		$html .= EEH_HTML::td( $line_item->quantity(), '',  'item_l' );
+		$html .= EEH_HTML::td( $line_item->quantity(), '',  'item_l jst-rght' );
 		// price td
-		$html .= EEH_HTML::td( $line_item->unit_price_no_code(), '',  'item_c' );
+		$html .= EEH_HTML::td( $line_item->unit_price_no_code(), '',  'item_c jst-rght' );
 		// total td
 		$total = $line_item->is_taxable() ? $line_item->total_no_code() . '*' : $line_item->total_no_code();
-		$html .= EEH_HTML::td( $total, '',  'item_r' );
+		$this->_show_taxes = $line_item->is_taxable() ? TRUE : $this->_show_taxes;
+		$html .= EEH_HTML::td( $total, '',  'item_r jst-rght' );
 		// end of row
 		$html .= EEH_HTML::trx();
 		return $html;
@@ -129,19 +131,19 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 	private function _sub_item_row( EE_Line_Item $line_item, $options = array() ) {
 		// start of row
 		$html = EEH_HTML::tr( '', 'item sub-item-row' );
+		// name && desc
+		$name_and_desc = $line_item->name();
+		$name_and_desc .= $options['show_desc'] ? '<br/>' . $line_item->desc() : '';
 		// name td
-		$html .= EEH_HTML::td( $line_item->name(), '',  'item_l sub-item' );
-		// desc td
-		$html .= $options['show_desc'] ? EEH_HTML::td( $line_item->desc(), '',  'item_l' ) : '';
-		$html .= EEH_HTML::td() . EEH_HTML::tdx();
+		$html .= EEH_HTML::td( $name_and_desc, '',  'item_l sub-item' );
 		// discount/surcharge td
 		if ( $line_item->is_percent() ) {
 			$html .= EEH_HTML::td( $line_item->percent() . '%', '',  'item_c' );
 		} else {
-			$html .= EEH_HTML::td( $line_item->unit_price_no_code(), '',  'item_c' );
+			$html .= EEH_HTML::td( $line_item->unit_price_no_code(), '',  'item_c jst-rght' );
 		}
 		// total td
-		$html .= EEH_HTML::td( $line_item->total_no_code(), '',  'item_r' );
+		$html .= EEH_HTML::td( $line_item->total_no_code(), '',  'item_r jst-rght' );
 		// end of row
 		$html .= EEH_HTML::trx();
 		return $html;
@@ -159,14 +161,15 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 	private function _tax_row( EE_Line_Item $line_item, $options = array() ) {
 		// start of row
 		$html = EEH_HTML::tr( '', 'item sub-item tax-total' );
+		// name && desc
+		$name_and_desc = $line_item->name();
+		$name_and_desc .= $options['show_desc'] ? '<br/>' . $line_item->desc() : '';
 		// name td
-		$html .= EEH_HTML::td( $line_item->name(), '',  'item_l sub-item' );
-		// desc td
-		$html .= $options['show_desc'] ? EEH_HTML::td( $line_item->desc(), '',  'item_l' ) : '';
+		$html .= EEH_HTML::td( $name_and_desc, '',  'item_l sub-item', '', ' colspan="2"' );
 		// percent td
-		$html .= EEH_HTML::td( $line_item->percent() . '%', '',  'item_c', '', ' colspan="2"' );
+		$html .= EEH_HTML::td( $line_item->percent() . '%', '',  'item_c', '' );
 		// total td
-		$html .= EEH_HTML::td( $line_item->total_no_code(), '',  'item_r' );
+		$html .= EEH_HTML::td( $line_item->total_no_code(), '',  'item_r jst-rght' );
 		// end of row
 		$html .= EEH_HTML::trx();
 		return $html;
@@ -182,19 +185,37 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 	 * @param array        $options
 	 * @return mixed
 	 */
+	private function _sub_total_row( EE_Line_Item $line_item, $text = '', $options = array() ) {
+		if ( $line_item->total() ) {
+			return $this->_total_row( $line_item, $text, $options);
+		}
+		return '';
+	}
+
+
+
+	/**
+	 * 	_total_row
+	 *
+	 * @param EE_Line_Item $line_item
+	 * @param string       $text
+	 * @param array        $options
+	 * @return mixed
+	 */
 	private function _total_row( EE_Line_Item $line_item, $text = '', $options = array() ) {
-		// colspan
-		$colspan = $options['show_desc'] ? ' colspan="2"' : '';
-		// start of row
-		$html = EEH_HTML::tr( '', 'total_tr odd' );
-		// empty td
-		$html .= EEH_HTML::td( EEH_HTML::nbsp(), '',  '',  '',  $colspan );
-		// total td
-		$html .= EEH_HTML::td( $text, 'total_currency',  'total',  '',  $colspan );
-		// total td
-		$html .= EEH_HTML::td( $line_item->total_no_code(), '',  'total' );
-		// end of row
-		$html .= EEH_HTML::trx();
+		$html = '';
+		if ( $line_item->total() ) {
+			// start of row
+			$html = EEH_HTML::tr( '', 'total_tr odd' );
+			// empty td
+			$html .= EEH_HTML::td( EEH_HTML::nbsp(), '',  '',  '',  ' colspan="2"' );
+			// total td
+			$html .= EEH_HTML::td( $text, 'total_currency',  'total jst-rght' );
+			// total td
+			$html .= EEH_HTML::td( $line_item->total_no_code(), '',  'total jst-rght' );
+			// end of row
+			$html .= EEH_HTML::trx();
+		}
 		return $html;
 	}
 
@@ -207,14 +228,8 @@ class EE_SPCO_Line_Item_Display_Strategy implements EEI_Line_Item_Display {
 	 * @return mixed
 	 */
 	private function _separator_row( $options = array() ) {
-		// colspan
-		$colspan = $options['show_desc'] ? ' colspan="5"' : ' colspan="4"';
 		// start of row
-		$html = EEH_HTML::tr( EEH_HTML::td( '<hr>', '',  '',  '',  $colspan ));
-		//		// separator td
-		//		$html .= EEH_HTML::td( '<hr>', '',  '',  '',  $colspan );
-		//		// end of row
-		//		$html .= EEH_HTML::trx();
+		$html = EEH_HTML::tr( EEH_HTML::td( '<hr>', '',  '',  '',  ' colspan="4"' ));
 		return $html;
 	}
 
