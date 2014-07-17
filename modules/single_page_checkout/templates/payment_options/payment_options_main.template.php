c9a7dd8eab05d1e04cc460072f0cd0541929732a
Brent Christensen <brent@eventespresso.com>
2014-07-17 13:38:29 -0700

    twerk the payment options template [touch:6081]

--- modules/single_page_checkout/templates/payment_options/payment_options_main.template.php
+++ modules/single_page_checkout/templates/payment_options/payment_options_main.template.php
@@ -20,41 +20,46 @@
 /*		if ( $payments ) {
 		foreach ( $payments as $payment ){
 			if ( $payment instanceof EE_Payment ) {
-*/?>
+?>
 				<tr class="reg-page-totals-spn">
-					<td class="jst-rght"><?php /*echo __('Payment: ', 'event_espresso') . date_i18n( $pay_date_frmt, (int)$payment->timestamp() ); */?></td>
-					<td colspan="3" class="jst-rght"><?php /*echo EEH_Template::format_currency( $payment->amount() );*/?></td>
-					<?php	/*do_action( 'AHEE__registration_page_payment_options__payment_info_table_payment_row_end', $payment ); */?>
+					<td class="jst-rght"><?php echo __('Payment: ', 'event_espresso') . date_i18n( $pay_date_frmt, (int)$payment->timestamp() ); ?></td>
+					<td colspan="3" class="jst-rght"><?php echo EEH_Template::format_currency( $payment->amount() );?></td>
+					<?php	do_action( 'AHEE__registration_page_payment_options__payment_info_table_payment_row_end', $payment ); ?>
 				</tr>
 <?php
-/*			}
+			}
 		}
 	}
 	if ( $grand_total != $amount_owing ) {
-*/?>
+?>
 				<tr class="reg-page-totals-spn">
-					<td class="jst-rght"><?php /*echo __('Grand Total: ', 'event_espresso');*/?></td>
-					<td colspan="3" class="jst-rght"><?php /*echo EEH_Template::format_currency( $grand_total );*/?></td>
-					<?php	/*do_action( 'AHEE__registration_page_payment_options__payment_info_table_grand_total_row_end', $grand_total ); */?>
+					<td class="jst-rght"><?php echo __('Grand Total: ', 'event_espresso');?></td>
+					<td colspan="3" class="jst-rght"><?php echo EEH_Template::format_currency( $grand_total );?></td>
+					<?php	do_action( 'AHEE__registration_page_payment_options__payment_info_table_grand_total_row_end', $grand_total ); ?>
 				</tr>
 <?php
-/*	}
-*/?>
+}
+?>
 				<tr id="reg-page-grand-total-dv" class="reg-page-totals-spn">
-					<td class="jst-rght"><?php /*echo __('Total Amount Due: ', 'event_espresso');*/?></td>
-					<td colspan="3" class="jst-rght"><?php /*echo EEH_Template::format_currency( $amount_owing );*/?></td>
-					<?php	/*do_action( 'AHEE__registration_page_payment_options__payment_info_table_amount_owing_row_end', $amount_owing ); */?>
+					<td class="jst-rght"><?php echo __('Total Amount Due: ', 'event_espresso');?></td>
+					<td colspan="3" class="jst-rght"><?php echo EEH_Template::format_currency( $amount_owing );?></td>
+					<?php	do_action( 'AHEE__registration_page_payment_options__payment_info_table_amount_owing_row_end', $amount_owing ); ?>
 				</tr>
-				--><?php  ?>
+				<?php */ ?>
 			</tbody>
 		</table>
 	</div>
 
-	<?php echo $hidden_inputs; ?>
+	<?php  echo $hidden_inputs;  ?>
 
 	<div id="methods-of-payment">
 		<h3 id="select-method-of-payment-hdr"><?php echo apply_filters( 'FHEE__registration_page_payment_options__select_method_of_payment_hdr', __( 'Please select your method of payment:', 'event_espresso' )); ?></h3>
-		<?php echo $available_payment_methods; ?>
+		<?php
+
+		echo $payment_options;
+//		d( $payment_options );
+
+		?>
 		<a id="reg-page-select-other-method-of-payment-lnk" class="hidden smaller-text right" rel=""><?php echo apply_filters( 'FHEE__registration_page_payment_options__select_other_method_of_payment_lnk', __( 'select a different method of payment:', 'event_espresso' )); ?></a>
 	</div>
 	<!-- end #methods-of-payment -->
