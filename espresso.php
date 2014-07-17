c592dc79a54f7fdafb04721071adec9c0c856f9c
Darren Ethier <darren@roughsmootheng.in>
2014-07-17 09:32:15 -0400

    bump alpha to 4.4.0.alpha.012

--- espresso.php
+++ espresso.php
@@ -32,7 +32,7 @@
  *
  * @package 		Event Espresso
  * @author 			Seth Shoultes
- * @copyright 	(c) 2008-2011 Event Espresso  All Rights Reserved.
+ * @copyright 	(c) 2008-2014 Event Espresso  All Rights Reserved.
  * @license 		{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
  * @link 				{@link http://www.eventespresso.com}
  * @since 			4.0
