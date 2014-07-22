de90fc2506037e3276f5d5d4bc8b929176bb26a8
Brent Christensen <brent@eventespresso.com>
2014-07-22 16:28:23 -0700

    fix hook point and move to end of init method [touch:6286]

--- shortcodes/espresso_thank_you/EES_Espresso_Thank_You.shortcode.php
+++ shortcodes/espresso_thank_you/EES_Espresso_Thank_You.shortcode.php
@@ -199,7 +199,6 @@ class EES_Espresso_Thank_You  extends EES_Shortcode {
 			);
 			return;
 		}
-		$this->_current_txn = do_action( 'AHEE__EES_Espresso_Thank_You__run', $this->_current_txn );
 		// soon to be derprecated
 		EE_Registry::instance()->load_model( 'Gateways' )->thank_you_page_logic( $this->_current_txn );
 		EE_Registry::instance()->LIB->EEM_Gateways->reset_session_data();
@@ -296,6 +295,9 @@ class EES_Espresso_Thank_You  extends EES_Shortcode {
 
 		EE_Registry::instance()->load_helper( 'Template' );
 		EE_Registry::instance()->load_helper( 'Template_Validator' );
+
+		do_action( 'AHEE__EES_Espresso_Thank_You__init_end', $this->_current_txn );
+
 	}
 
 
