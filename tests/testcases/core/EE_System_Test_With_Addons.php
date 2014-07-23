def1da605cf5022d6268bb5863c9e59e25c74f69
Michael Nelson <michael@eventespresso.com>
2014-07-23 14:01:59 -0700

    [touch:6272]{t:30}thought I had the failed tests fixed but dont

--- tests/testcases/core/EE_System_Test_With_Addons.php
+++ tests/testcases/core/EE_System_Test_With_Addons.php
@@ -209,7 +209,7 @@ class EE_System_Test_With_Addons extends EE_UnitTestCase{
 		//that it needed upon new activation (and so we call the function that does it, which
 		//is normally called a little later in the request)
 		EE_System::instance()->perform_activations_upgrades_and_migrations();
-		$this->assertTableExists('new_addon');
+		$this->assertTableExists('esp_new_addon_thing');
 	}
 
 	private function get_real_option( $option){
@@ -226,13 +226,8 @@ class EE_System_Test_With_Addons extends EE_UnitTestCase{
 	 * Registers the mock addon so it can be used for testing
 	 */
 	public function setUp(){
-		global $show_queries;
-		$show_queries = 1;
-		echo "setUp before calling parent db state is:";
-		var_dump( $this->get_real_option( EE_Data_Migration_Manager::current_database_state ));
+
 		parent::setUp();
-		echo "setUp after calling parent. db state is:";
-		var_dump( $this->get_real_option( EE_Data_Migration_Manager::current_database_state ));
 
 		$this->_pretend_addon_hook_time();
 		$mock_addon_path = EE_TESTS_DIR.'mocks/addons/new-addon/';
@@ -279,8 +274,6 @@ class EE_System_Test_With_Addons extends EE_UnitTestCase{
 			$this->_stop_pretending_addon_hook_time();
 		}
 		parent::tearDown();
-		echo "rela db state:".
-		$this->get_real_option( EE_Data_Migration_Manager::current_database_state );
 	}
 
 }
