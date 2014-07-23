4fffb206a5df18df085e647ce7b02a7c8b272696
Brent Christensen <brent@eventespresso.com>
2014-07-23 16:55:44 -0700

    add required css class [touch:6081]

--- core/libraries/form_sections/strategies/display/EE_Select_Multiple_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Select_Multiple_Display_Strategy.strategy.php
@@ -20,25 +20,26 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  * EE_Select_Multiple_Display_Strategy
  *
  * @package			Event Espresso
- * @subpackage		
+ * @subpackage
  * @author				Mike Nelson
  *
  * ------------------------------------------------------------------------
  */
 class EE_Select_Multiple_Display_Strategy extends EE_Select_Display_Strategy{
 	public function display() {
-		$input = $this->_input;		
+		$input = $this->_input;
 		if( ! $input instanceof EE_Form_Input_With_Options_Base){
 			throw new EE_Error(sprintf(__("Cannot use Select Multiple Display Strategy with an input that doesn't ahve options", "event_espresso")));
 		}
-		$html= EEH_Formatter::nl(1) . "<select multiple id='{$input->html_id()}' name='{$input->html_name()}[]' class='{$input->html_class()}' style='{$input->html_style()}'/>";
+		$class = $this->_input->required() ? 'ee-needs-value ' . $input->html_class() : $input->html_class();
+		$html= EEH_Formatter::nl(1) . "<select multiple id='{$input->html_id()}' name='{$input->html_name()}[]' class='{$class}' style='{$input->html_style()}'/>";
 		EE_Registry::instance()->load_helper('Array');
 		if(EEH_Array::is_multi_dimensional_array($input->options())){
 			throw new EE_Error(sprintf(__("Select multiple display strategy does not allow for nested arrays of options.", "event_espresso")));
 		}else{
 			$html.=$this->_display_options($input->options());
 		}
-		
+
 		$html.= EEH_Formatter::nl(-1) . "</select>";
 		return $html;
 	}
