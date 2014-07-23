3b4c8b4aff1925d3043c8c38ea36d42a63a19292
Michael Nelson <michael@eventespresso.com>
2014-07-23 15:15:16 -0700

    [touch:6272]{t:30}have super simple test case to reproduce wp no db reset issue

--- tests/testcases/misc/EE_WP_Test.php
+++ tests/testcases/misc/EE_WP_Test.php
@@ -17,96 +17,18 @@ class EE_WP_Test extends EE_UnitTestCase{
 
 	function setUp(){
 		//DONT call parent
+		$this->update_option('foobar', 'before_setup');
+		parent::setUp();
+	}
+	function test_it(){
+		$this->update_option('foobar', 'during_test');
 	}
 	function tearDown(){
 		//DONT call parent
-	}
-	function test_transaction(){
-		global $show_queries;
-		$show_queries = TRUE;
-		update_option('thingy','foobar');
-		$foobar = get_option('thingy');
-		$this->assertEquals('foobar',$foobar);
-
-		global $wpdb;
-		$wpdb->query( 'SET autocommit = 0;' );
-		$wpdb->query( 'START TRANSACTION;' );
-		$this->assertEquals('foobar',$foobar);
-
-		update_option('thingy','baz');
-		$foobar = get_option('thingy');
-		$this->assertEquals('baz',$foobar);
-
-		$wpdb->query('ROLLBACK;');
-		global $wp_object_cache;
-		$wp_object_cache->group_ops = array();
-		$wp_object_cache->stats = array();
-		$wp_object_cache->memcache_debug = array();
-		$wp_object_cache->cache = array();
-		if ( method_exists( $wp_object_cache, '__remoteset' ) ) {
-			$wp_object_cache->__remoteset();
-		}
-		wp_cache_flush();
-		wp_cache_add_global_groups( array( 'users', 'userlogins', 'usermeta', 'user_meta', 'site-transient', 'site-options', 'site-lookup', 'blog-lookup', 'blog-details', 'rss', 'global-posts', 'blog-id-cache' ) );
-		wp_cache_add_non_persistent_groups( array( 'comment', 'counts', 'plugins' ) );
-
-		$foobar = get_option('thingy');
-		$this->assertNotEquals('baz',$foobar);
-		$this->assertEquals('foobar',$foobar);
+		parent::tearDown();
+		$this->assertEquals('before_setup',$this->get_option('foobar'));
 	}
 
-//	function test_transaction2(){
-//		$original_attendee = $this->new_model_obj_with_dependencies('Attendee');
-//		$original_attendee->save();
-//		$this->assertEquals( $original_attendee->fname(), $this->get_attendee_name_in_db($original_attendee->ID()));
-//
-//		$original_attendee->set('ATT_fname','mongo');
-//		$original_attendee->save();
-//		$this->assertEquals( 'mongo', $this->get_attendee_name_in_db($original_attendee->ID()));
-//
-//		global $wpdb;
-//		$wpdb->query( 'SET autocommit = 0;' );
-//		$wpdb->query( 'START TRANSACTION;' );
-//		$this->assertEquals( $original_attendee->fname(), $this->get_attendee_name_in_db($original_attendee->ID()));
-//
-//		$original_attendee->set('ATT_fname','chango');
-//		$original_attendee->save();
-//		$this->assertEquals( 'chango', $this->get_attendee_name_in_db($original_attendee->ID()));
-//
-//		$wpdb->query('ROLLBACK;');
-//		global $wp_object_cache;
-//		$wp_object_cache->group_ops = array();
-//		$wp_object_cache->stats = array();
-//		$wp_object_cache->memcache_debug = array();
-//		$wp_object_cache->cache = array();
-//		if ( method_exists( $wp_object_cache, '__remoteset' ) ) {
-//			$wp_object_cache->__remoteset();
-//		}
-//		wp_cache_flush();
-//		wp_cache_add_global_groups( array( 'users', 'userlogins', 'usermeta', 'user_meta', 'site-transient', 'site-options', 'site-lookup', 'blog-lookup', 'blog-details', 'rss', 'global-posts', 'blog-id-cache' ) );
-//		wp_cache_add_non_persistent_groups( array( 'comment', 'counts', 'plugins' ) );
-//
-//		$this->assertEquals( 'mongo', $this->get_attendee_name_in_db($original_attendee->ID()));
-//	}
-//
-//	function test_transaction3(){
-//		$this->update_option('thingy','foobar');
-//		$foobar = $this->get_option('thingy');
-//		$this->assertEquals('foobar',$foobar);
-//
-//		global $wpdb;
-//		$wpdb->query( 'SET autocommit = 0;' );
-//		$wpdb->query( 'START TRANSACTION;' );
-//		$this->assertEquals('foobar',$foobar);
-//
-//		$this->update_option('thingy','baz');
-//
-//		$wpdb->query('ROLLBACK;');
-//
-//		$foobar = $this->get_option('thingy');
-//		$this->assertNotEquals('baz',$foobar);
-//		$this->assertEquals('foobar',$foobar);
-//	}
 	private function update_option( $option_name, $option_value ){
 		global $wpdb;
 		return $wpdb->update( $wpdb->options, array( 'option_value' => maybe_serialize($option_value) ), array( 'option_name' => $option_name ) );
