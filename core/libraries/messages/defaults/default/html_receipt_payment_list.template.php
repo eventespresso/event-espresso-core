caa0788655be3d6935dcf56da4a13858d509dd22
Darren Ethier <darren@roughsmootheng.in>
2014-07-25 20:25:32 -0400

    add default messages template for the html receipt payment_list field content [touch: 6007]

---
+++ core/libraries/messages/defaults/default/html_receipt_payment_list.template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This is the template for the html messenger and receipt message type payment_list field.
+ */
+?>
+<tr class='item'>
+	<td>[PAYMENT_GATEWAY]</td>
+	<td>[PAYMENT_TIMESTAMP]</td>
+	<td>[PAYMENT_GATEWAY_TXN_ID]</td>
+	<td>[PAYMENT_PO_NUMBER]</td>
+	<td>[PAYMENT_STATUS]</td>
+	<td class='item_r'>[PAYMENT_AMOUNT]</td>
+</tr>
