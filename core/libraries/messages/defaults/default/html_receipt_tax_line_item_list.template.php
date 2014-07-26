9dd48e57a7317a9508486b688ba30f24290a9882
Darren Ethier <darren@roughsmootheng.in>
2014-07-25 20:22:42 -0400

    add default messages template content for tax_line_item_list field [touch: 6007]

---
+++ core/libraries/messages/defaults/default/html_receipt_tax_line_item_list.template.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This is the template for the html messenger and receipt message type tax_line_item_list field.
+ */
+?>
+<tr>
+	<td>[LINE_ITEM_NAME]</td>
+	<td>[LINE_ITEM_DESCRIPTION]</td>
+	<td class="item_c">[LINE_ITEM_AMOUNT]</td>
+	<td class="item_r">[LINE_ITEM_TOTAL]</td>
+</tr>
