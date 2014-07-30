2111f7c769617be4fcdf63f5a92db4f3997beb89
Brent Christensen <brent@eventespresso.com>
2014-07-30 16:58:59 -0700

    fix typo and php docs  [touch:6081]

--- core/libraries/form_sections/strategies/validation/EE_Enum_Validation_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/validation/EE_Enum_Validation_Strategy.strategy.php
@@ -4,13 +4,16 @@ class EE_Enum_Validation_Strategy extends EE_Validation_Strategy_Base{
 
 	/**
 	 * Check that the value is in the allowed list
+	 * @param $normalized_value
+	 * @throws EE_Error
+	 * @throws EE_Validation_Error
 	 * @return boolean
 	 */
 	function validate($normalized_value) {
 		parent::validate($normalized_value);
 		$input = $this->_input;
 		if( ! $input instanceof EE_Form_Input_With_Options_Base){
-			throw new EE_Error(sprintf(__("Cannot use Enum Validation Strategy with an input that doesn't ahve options", "event_espresso")));
+			throw new EE_Error(sprintf(__("Cannot use Enum Validation Strategy with an input that doesn't have options", "event_espresso")));
 		}
 		$enum_options = $input->flat_options();
 		if( $normalized_value === TRUE){
