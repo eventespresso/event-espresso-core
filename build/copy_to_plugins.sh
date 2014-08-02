c561da1802505bb1c679d1fe33288cbde31f5897
Sidney Harrell <sidney.harrell@gmail.com>
2014-08-02 19:54:51 -0400

    adding build files for Jenkins

--- build/copy_to_plugins.sh
+++ build/copy_to_plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TARGETDIR="${basedir}/wordpress-develop/src/wp-content/plugins"
+TARGETDIR="$1/wordpress-develop/src/wp-content/plugins"
 mkdir $TARGETDIR/EE4
 for file in *
 do test "$file" != "wordpress-develop" && cp -r "$file" "$TARGETDIR/EE4/"
