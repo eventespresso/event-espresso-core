ed5e43255e32c436240ec92033cb5958401645dd
Darren Ethier <darren@roughsmootheng.in>
2014-08-03 14:01:26 -0400

    change edit_private_venues to ee_edit_private_venues [touch: 6376]

--- core/CPTs/EE_Register_CPTs.core.php
+++ core/CPTs/EE_Register_CPTs.core.php
@@ -201,7 +201,7 @@ class EE_Register_CPTs {
 						'delete_private_posts' => 'delete_private_venues',
 						'delete_published_posts' => 'delete_published_venues',
 						'delete_others_posts' => 'ee_edit_others_venues',
-						'edit_private_posts' => 'edit_private_venues',
+						'edit_private_posts' => 'ee_edit_private_venues',
 						'edit_published_posts' => 'ee_edit_published_venues'
 						),
 					'taxonomies'=> array(
