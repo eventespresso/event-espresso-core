3d7584fe7aebbc8bb8303d1a24463da6267d3c71
Brent Christensen <brent@eventespresso.com>
2014-08-06 18:49:16 -0700

    breakout the fileperms() check from EEH_File::verify_filepath_and_permissions() into separate EEH_File::_permissions_error_for_unreadable_filepath() method, add new EEH_File::ensure_folder_exists_and_is_writable() method, add new EEH_File::ensure_file_exists_and_is_writable() method add new EEH_File::verify_is_writable() method, add new EEH_File::write_to_file() method [touch:5970]

--- core/helpers/EEH_File.helper.php
+++ core/helpers/EEH_File.helper.php
@@ -47,26 +47,7 @@ class EEH_File extends EEH_Base {
 				$file_name
 			);
 			if ( file_exists( $full_file_path )) {
-				// check file permissions
-				$perms = substr( fileperms( $full_file_path ), 2 );
-				if ( $perms ) {
-					// file permissions exist, but way be set incorrectly
-					$type_of_file = ! empty( $type_of_file ) ? $type_of_file . ' ' : '';
-					$type_of_file .= ! empty( $type_of_file ) ? 'file' : 'folder';
-					$msg .= sprintf(
-						__( '%sFile permissions for the requested %s are currently set at "%s". The recommended permissions are 644 for files and 755 for folders.', 'event_espresso' ),
-						'<br />',
-						$type_of_file,
-						$perms
-					);
-				} else {
-					// file exists but file permissions could not be read ?!?!
-					$msg .= sprintf(
-						__( '%sPlease ensure that the server and/or PHP configuration allows the current process to access the following file: "%s".', 'event_espresso' ),
-						'<br />',
-						$full_file_path
-					);
-				}
+				$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $full_file_path, $type_of_file );
 			} else {
 				// no file permissions means the file was not found
 				$msg .= sprintf(
@@ -78,6 +59,148 @@ class EEH_File extends EEH_Base {
 			EE_Error::add_error( $msg . '||' . $msg, __FILE__, __FUNCTION__, __LINE__ );
 			return FALSE;
 		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * _permissions_error_for_unreadable_filepath - attempts to determine why permissions are set incorrectly for a file or folder
+	 *
+	 * @access private
+	 * @param string $full_file_path - full server path to the folder or file
+	 * @param string $type_of_file - general type of file (ie: "module"), this is only used to improve error messages
+	 * @return string
+	 */
+	private static function _permissions_error_for_unreadable_filepath( $full_file_path = '', $type_of_file = '' ){
+		// check file permissions
+		$perms = substr( fileperms( $full_file_path ), 2 );
+		if ( $perms ) {
+			// file permissions exist, but way be set incorrectly
+			$type_of_file = ! empty( $type_of_file ) ? $type_of_file . ' ' : '';
+			$type_of_file .= ! empty( $type_of_file ) ? 'file' : 'folder';
+			return sprintf(
+				__( '%sFile permissions for the requested %s are currently set at "%s". The recommended permissions are 644 for files and 755 for folders.', 'event_espresso' ),
+				'<br />',
+				$type_of_file,
+				$perms
+			);
+		} else {
+			// file exists but file permissions could not be read ?!?!
+			return sprintf(
+				__( '%sPlease ensure that the server and/or PHP configuration allows the current process to access the following file: "%s".', 'event_espresso' ),
+				'<br />',
+				$full_file_path
+			);
+		}
+	}
+
+
+
+	/**
+	 * ensure_folder_exists_and_is_writable
+	 * ensures that a folder exists and is writable, will attempt to create folder if it does not exist
+	 * @param string $folder
+	 * @param string $parent_folder
+	 * @param string $msg - Prepended to any error message produced ie : "Something something could not be setup because" + error message
+	 * @return bool
+	 */
+	public static function ensure_folder_exists_and_is_writable( $folder = '', $parent_folder = '', $msg = '' ){
+		$msg = ! empty( $msg ) ? $msg . ' the ' : 'The ';
+		if ( ! is_dir( $folder )) {
+			if ( ! is_writable( $parent_folder )) {
+				if ( ! EEH_File::verify_is_writable( $parent_folder, 'folder', $msg )) {
+					return FALSE;
+				}
+			} else {
+				if ( ! mkdir( $folder )) {
+					$msg .= sprintf( __( '"%s" could not be created.', 'event_espresso' ), $folder );
+					$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $folder );
+					EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
+					return FALSE;
+				}
+			}
+		} elseif ( ! is_writable( $folder )) {
+			if ( ! EEH_File::verify_is_writable( $folder, 'folder', $msg )) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * verify_is_writable - checks if a file or folder is writable
+	 * @param string $full_path - full server path to file or folder
+	 * @param string $file_or_folder - whether checking a file or folder
+	 * @param string $msg - prepended to any error message produced ie : "Something something could not be setup because" + error message
+	 * @return bool
+	 */
+	public static function verify_is_writable( $full_path = '', $file_or_folder = 'folder', $msg = '' ){
+		$msg = ! empty( $msg ) ? $msg : 'The ';
+		if ( ! is_writable( $full_path )) {
+			$msg .= sprintf( __( '"%s" %s is not writable.', 'event_espresso' ), $full_path, $file_or_folder );
+			$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $full_path );
+			EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * ensure_file_exists_and_is_writable
+	 * ensures that a file exists and is writable, will attempt to create file if it does not exist
+	 * @param string $full_file_path
+	 * @return bool
+	 */
+	public static function ensure_file_exists_and_is_writable( $full_file_path = '' ) {
+		if ( ! file_exists( $full_file_path )) {
+			if ( ! touch( $full_file_path )) {
+				$msg = sprintf( __( 'The "%s" file could not be created.', 'event_espresso' ), $full_file_path );
+				$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $full_file_path );
+				EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
+				return FALSE;
+			}
+		}
+		if ( ! EEH_File::verify_is_writable( $full_file_path, 'file' )) {
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * write_file
+	 * @param string $full_file_path
+	 * @param string $file_contents - the content to be written to the file
+	 * @param string $mode - the type of access required to the stream
+	 * @return bool
+	 */
+	public static function write_to_file( $full_file_path = '', $file_contents = '', $mode = 'w' ){
+		if ( ! EEH_File::verify_is_writable( $full_file_path, 'file' )) {
+			$msg = sprintf( __( 'The Espresso Log file "%s" is not writable.', 'event_espresso' ), $full_file_path );
+			$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $full_file_path );
+			EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
+			return FALSE;
+		}
+		$handle = fopen( $full_file_path, $mode );
+		if ( ! $handle ) {
+			$msg = sprintf( __( 'The Espresso Log file "%s" could not be opened for writing.', 'event_espresso' ), $full_file_path );
+			$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $full_file_path );
+			EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
+			return FALSE;
+		}
+		if ( ! fwrite( $handle, $file_contents )) {
+			$msg = sprintf( __( 'The Espresso Log file "%s" could not be written to.', 'event_espresso' ), $full_file_path );
+			$msg .= EEH_File::_permissions_error_for_unreadable_filepath( $full_file_path );
+			EE_Error::add_error( $msg, __FILE__, __FUNCTION__, __LINE__ );
+			return FALSE;
+		}
+		fclose( $handle );
 		return TRUE;
 	}
 
@@ -137,11 +260,11 @@ class EEH_File extends EEH_Base {
 	/**
 	 * takes teh folder name (with or without trailing slash) and finds the files it in,
 	 * and what the class's name inside of each should be.
-	 * @param string $folder_paths
+	 * @param array $folder_paths
 	 * @return array where keys are what the class names SHOULD be, and values are their filepaths
 	 * @throws EE_Error
 	 */
-	public static function get_contents_of_folders( $folder_paths ){
+	public static function get_contents_of_folders( $folder_paths = array() ){
 		$class_to_folder_path = array();
 		foreach( $folder_paths as $folder_path ){
 			$folder_path = self::standardise_and_end_with_directory_separator( $folder_path );
