7b367900f429f7ed75f19fe33d6539d768cb990e
Brent Christensen <brent@eventespresso.com>
2014-08-07 08:25:25 -0700

    add _minimum_php_version_required() and minimum_php_version_error() methods and fix plugin deactivation if min required versions are not met [touch:6419]

--- core/EE_System.core.php
+++ core/EE_System.core.php
@@ -120,7 +120,13 @@ final class EE_System {
 		if ( ! $this->_minimum_wp_version_required() ) {
 			unset( $_GET['activate'] );
 			add_action( 'admin_notices', array( $this, 'minimum_wp_version_error' ), 1 );
-			exit();
+			return;
+		}
+		// check required PHP version
+		if ( ! $this->_minimum_php_version_required() ) {
+			unset( $_GET['activate'] );
+			add_action( 'admin_notices', array( $this, 'minimum_php_version_error' ), 1 );
+			return;
 		}
 		// check recommended WP version
 		if ( ! $this->_minimum_wp_version_recommended() ) {
@@ -203,6 +209,16 @@ final class EE_System {
 	}
 
 	/**
+	 * 	_minimum_php_version_required
+	 *
+	 * 	@access private
+	 * 	@return boolean
+	 */
+	private function _minimum_php_version_required() {
+		return $this->_check_php_version( EE_MIN_PHP_VER_REQUIRED );
+	}
+
+	/**
 	 * 	_minimum_php_version_recommended
 	 *
 	 * 	@access private
@@ -231,6 +247,31 @@ final class EE_System {
 			$wp_version,
 			'<br/>',
 			'<a href="http://codex.wordpress.org/Updating_WordPress">http://codex.wordpress.org/Updating_WordPress</a>'
+		);
+		?>
+		</p>
+		</div>
+		<?php
+		deactivate_plugins( EE_PLUGIN_BASENAME );
+	}
+
+
+
+	/**
+	 * 	minimum_php_version_error
+	 *
+	 * 	@return void
+	 */
+	public function minimum_php_version_error() {
+		?>
+		<div class="error">
+		<p>
+		<?php
+		printf(
+			__( 'We\'re sorry, but Event Espresso requires PHP version %s or greater in order to operate. You are currently running version %s.%sIn order to update your version of PHP, you will need to contact your current hosting provider.', 'event_espresso' ),
+			EE_MIN_PHP_VER_REQUIRED,
+			PHP_VERSION,
+			'<br/>'
 		);
 		?>
 		</p>
