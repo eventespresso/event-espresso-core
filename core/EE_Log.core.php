a2518d322bb12a13a089760ee916eeb49f848672
Brent Christensen <brent@eventespresso.com>
2014-08-06 19:47:18 -0700

    rewrite EE_Log class to give it MOAR OOP [touch:5970]

--- core/EE_Log.core.php
+++ core/EE_Log.core.php
@@ -1,177 +1,223 @@
 <?php if (!defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
 /**
- * * Singleton logging class. Can be called from anywhere in the plugin to log data to a log file.
- * * Defaults to wp-content/uploads/espresso/logs/espresso_log.txt
- * */
-//Usage
-//espresso_log::singleton()->log( array ( 'file' => __FILE__, 'function' => __FUNCTION__, 'status' => '[INSERT MESSAGE]' ) );
+ *
+ * Class EE_Log
+ *
+ * Singleton logging class. Can be called from anywhere in the plugin to log data to a log file.
+ * Defaults to wp-content/uploads/espresso/logs/espresso_log.txt
+ * Usage:
+ * do_action( 'AHEE_log', __FILE__, __FUNCTION__, 'logging message' );
+ *
+ * @package 			Event Espresso
+ * @subpackage 	core
+ * @author 				Sidney Harrel, Brent Christensen
+ *
+ */
+class EE_Log {
 
-class espresso_log {
+	/**
+	 * @var string
+	 */
+	private $_logs_folder = '';
 
-	var $file;
-	var $folder;
-	var $url; //Used for remote logging
-	private static $inst;
+	/**
+	 * @var string
+	 */
+	private $_log_file = '';
 
-	//Set the file path - Change the file name is needed
-	function __construct() {
-		//echo __FILE__;
-		//echo dirname( __FILE__ );
-		$this->folder = EVENT_ESPRESSO_UPLOAD_DIR . 'logs/';
-		//echo $folder;
-		$this->file = $this->folder . 'espresso_log.txt';
-		
+	/**
+	 * @var string
+	 */
+	private $_log = '';
+
+	/**
+	 * @var string
+	 */
+	private $_debug_file = '';
+
+	/**
+	 * @var string
+	 */
+	private $_debug_log = '';
+
+	/**
+	 * Used for remote logging
+	 * @var string
+	 */
+	private $_remote_logging_url = '';
+
+	/**
+	 * @var string
+	 */
+	private $_remote_log = '';
+
+	/**
+	 * @var EE_Log
+	 */
+	private static $_instance;
+
+
+	/**
+	 * @return EE_Log
+	 */
+	public static function instance() {
+		if ( ! self::$_instance instanceof EE_Log ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * @access private
+	 * @return EE_Log
+	 */
+	private function __construct() {
+
+		if ( ! EE_Registry::instance()->CFG->admin->use_full_logging && ! EE_Registry::instance()->CFG->admin->use_remote_logging ) {
+			return;
+		}
+
+		EE_Registry::instance()->load_helper( 'File' );
+
+		$this->_logs_folder = EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS;
+		$this->_log_file = 'espresso_log.txt';
+		$this->_log = '';
+		$this->_debug_file = 'espresso_debug.txt';
+		$this->_debug_log = '';
+		$this->_remote_logging_url = EE_Registry::instance()->CFG->admin->remote_logging_url;
+		$this->_remote_log = '';
+
 		$uploads = wp_upload_dir();
-		if (!is_dir(EVENT_ESPRESSO_UPLOAD_DIR) && is_writable($uploads['baseurl'])) {
-			mkdir(EVENT_ESPRESSO_UPLOAD_DIR);
+		if ( ! EEH_File::ensure_folder_exists_and_is_writable( EVENT_ESPRESSO_UPLOAD_DIR, $uploads['baseurl'], 'Event Espresso Error Logging can not be set up because' )) {
+			return;
 		}
-		if (!is_dir(EVENT_ESPRESSO_UPLOAD_DIR.'logs') && is_writable(EVENT_ESPRESSO_UPLOAD_DIR)) {
-			mkdir(EVENT_ESPRESSO_UPLOAD_DIR.'logs');
-			EE_Error::add_htaccess();
+		if ( ! EEH_File::ensure_folder_exists_and_is_writable( $this->_logs_folder, EVENT_ESPRESSO_UPLOAD_DIR, 'Event Espresso Error Logging can not be set up because' )) {
+			return;
 		}
-		
-		if (is_writable(EVENT_ESPRESSO_UPLOAD_DIR.'logs') && !file_exists($this->file)) {
-			touch($this->file);
+		if ( ! EEH_File::add_htaccess_deny_from_all( $this->_logs_folder )) {
+			return;
 		}
-		if (is_writable(EVENT_ESPRESSO_UPLOAD_DIR.'logs') && !file_exists($this->folder.'espresso_debug.php')) {
-			touch($this->folder.'espresso_debug.php');
+		EEH_File::ensure_file_exists_and_is_writable( $this->_logs_folder . $this->_log_file );
+		EEH_File::ensure_file_exists_and_is_writable( $this->_logs_folder . $this->_debug_file );
+
+		add_action( 'AHEE_log', array( $this, 'log' ), 10, 4 );
+		if ( EE_Registry::instance()->CFG->admin->use_full_logging ) {
+			add_action( 'shutdown', array( $this, 'write_log' ));
+			add_action( 'shutdown', array( $this, 'write_debug' ));
 		}
+		if ( EE_Registry::instance()->CFG->admin->use_remote_logging ) {
+			add_action( 'shutdown', array( $this, 'send_log' ));
+		}
+
 	}
 
-	public static function singleton() {
-		if (!isset(self::$inst)) {
-			$c = __CLASS__;
-			self::$inst = new $c;
-		}
-		return self::$inst;
+
+
+	/**
+	 * @param string $file
+	 * @param string $function
+	 * @param string $message
+	 * @param string $type
+	 * @return string
+	 */
+	private function _format_message( $file = '', $function = '', $message = '', $type = '' ) {
+		$msg = '----------------------------------------------------------------------------------------' . PHP_EOL;
+		$msg .= '[' . current_time( 'mysql' ) . '] ';
+		$msg .= ! empty( $file ) ? basename( $file ) : '';
+		$msg .= ! empty( $file ) && ! empty( $function ) ? ' -> ' : '';
+		$msg .= ! empty( $function ) ? $function . '()' : '';
+		$msg .= PHP_EOL;
+		$type = ! empty( $type ) ? $type : 'log message';
+		$msg .= ! empty( $message ) ? '[' . $type . '] ' . $message . PHP_EOL : '';
+		return $msg;
 	}
 
-	public function log($message) {
-		if ( isset( $message['type'] ) ) {
-			$this->file = $this->folder . 'espresso_debug_' . $message['type'] . '.txt';
-			
-			if (is_writable(EVENT_ESPRESSO_UPLOAD_DIR.'logs') && !file_exists($this->file)) {
-				touch($this->file);
-			}
-		}
 
-		if (is_writable($this->file)) {
-			$fh = fopen($this->file, 'a') or die("Cannot open file! " . $this->file);
-			fwrite($fh, '[' . date("m.d.y H:i:s") . '], ' . basename($message['file']) . ' ->' . $message['function'] . ',  ' . $message['status'] . "\n");
-			fclose($fh);
-		} else {
-			global $notices;
-			$notices['errors'][] = sprintf(__('Your log file is not writable. Check if your server is able to write to %s.', 'event_espresso'), $this->file);
-		}
+
+	/**
+	 * @param string $file
+	 * @param string $function
+	 * @param string $message
+	 * @param string $type
+	 */
+	public function log( $file = '', $function = '', $message = '', $type = '' ) {
+		$this->_log .= $this->_format_message( $file, $function, $message, $type );
 	}
 
-	public function remote_log($message) {
-		global $remote_log;
-		if (empty($remote_log))
-			$remote_log = '';
-		$remote_log .= '[' . date("m.d.y H:i:s") . '], ' . basename($message['file']) . ' -> ' . $message['function'] . ',  ' . $message['status'] . "\n";
+
+
+	/**
+	 * write_log
+	 */
+	public function write_log() {
+		EEH_File::write_to_file( $this->_logs_folder . $this->_log_file, $this->_log, 'a' );
 	}
 
-	public function send_log($url) {
-		global $remote_log;
-		// send the $remote_log var to the server
 
-		$file_name = $_SERVER['SCRIPT_FILENAME'];
-		$domain = $_SERVER['HTTP_HOST'];
-		$ip = $_SERVER['SERVER_ADDR'];
-		$server_type = $_SERVER['SERVER_SOFTWARE'];
-		$request_array = json_encode($_REQUEST);
 
-		//Encrypt the $remote_log?
-		//$remote_log = base64_encode($remote_log);
-		//Encrypt the $request_array?
-		//$request_array = base64_encode($request_array);
+	/**
+	 * send_log
+	 */
+	public function send_log() {
 
-		$data = 'domain=' . $domain
-						. '&ip=' . $ip
-						. '&server_type=' . $server_type
-						. '&time=' . time()
-						. '&remote_log=' . $remote_log
-						. '&request_array=' . $request_array //<-- Do we want to leave this turned on?
-						. '&action=save';
+		if ( empty( $this->_remote_logging_url )) {
+			return;
+		}
 
-		if (defined('EELOGGING_PASS')) {
+		$data = 'domain=' . $_SERVER['HTTP_HOST'];
+		$data .= '&ip=' . $_SERVER['SERVER_ADDR'];
+		$data .= '&server_type=' . $_SERVER['SERVER_SOFTWARE'];
+		$data .= '&time=' . time();
+		$data .= '&remote_log=' . $this->_log;
+		$data .= '&request_array=' . json_encode( $_REQUEST );
+		$data .= '&action=save';
+
+		if ( defined( 'EELOGGING_PASS' )) {
 			$data .= '&pass=' . EELOGGING_PASS;
 		}
-
-		if (defined('EELOGGING_KEY')) {
+		if ( defined( 'EELOGGING_KEY' )) {
 			$data .= '&key=' . EELOGGING_KEY;
 		}
 
-
-		$c = curl_init($url);
-		curl_setopt($c, CURLOPT_POST, true);
-		curl_setopt($c, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-		$page = curl_exec($c);
-		curl_close($c);
+		$c = curl_init( $this->_remote_logging_url );
+		curl_setopt( $c, CURLOPT_POST, TRUE );
+		curl_setopt( $c, CURLOPT_POSTFIELDS, $data );
+		curl_setopt( $c, CURLOPT_RETURNTRANSFER, TRUE );
+		curl_exec( $c );
+		curl_close( $c );
 	}
 
+
+
+	/**
+	 * debug
+	 */
+	public function write_debug() {
+		$message = '<?php' . PHP_EOL;
+		foreach ( $_GET as $key => $value ) {
+			$message .= '$my_GET["' . $key . '"] = "' . serialize($value) . '"' . PHP_EOL;
+		}
+		foreach ( $_POST as $key => $value ) {
+			$message .= '$my_POST["' . $key . '"] = "' . serialize($value) . '"' . PHP_EOL;
+		}
+		foreach ( $_REQUEST as $key => $value ) {
+			$message .= '$my_REQUEST["' . $key . '"] = "' . serialize($value) . '"' . PHP_EOL;
+		}
+		$this->_debug_log = EE_Log::_format_message( '', '', $message );
+		EEH_File::write_to_file( $this->_logs_folder . $this->_debug_file, $this->_debug_log, 'w' );
+	}
+
+
+
+	/**
+	 * __clone
+	 */
 	public function __clone() {
-		trigger_error('Clone is not allowed.', E_USER_ERROR);
+		trigger_error( 'Clone is not allowed.', E_USER_ERROR );
 	}
 
+
+
 }
-
-
-//Create logging function and action
-function espresso_log($file, $function, $message) {
-	espresso_log::singleton()->log(array('file' => $file, 'function' => $function, 'status' => $message));
-}
-
-function espresso_log_shortcode_parser( $file, $function, $message ) {
-	espresso_log::singleton()->log(array('file' => $file, 'function' => $function, 'status' => $message, 'type' => 'shortcode_parser') );
-}
-
-if ( EE_Registry::instance()->CFG->admin->use_full_logging ) {
-	add_action('AHEE_log', 'espresso_log', 10, 3);
-	add_action('AHEE_log_shortcode_parser', 'espresso_log_shortcode', 10, 3);
-	add_action('AHEE_debug_file', 'espresso_debug_file');	
-}
-
-//Remote logging stuff
-function espresso_remote_log($file, $function, $message) {
-	espresso_log::singleton()->remote_log(array('file' => $file, 'function' => $function, 'status' => $message));
-}
-
-function espresso_send_log() {
-
-	if ( empty( EE_Registry::instance()->CFG->admin->remote_logging_url )) {
-		return;
-	}
-	espresso_log::singleton()->send_log( EE_Registry::instance()->CFG->admin->remote_logging_url );
-}
-
-if ( EE_Registry::instance()->CFG->admin->use_remote_logging ) {
-	add_action('AHEE_log', 'espresso_remote_log', 10, 3);
-	add_action('wp_footer', 'espresso_send_log');
-}
-
-function espresso_debug_file() {
-	$message = "<?php\n";
-	foreach ($_GET as $key => $value) {
-		$message .= '$my_GET["' . $key . '"] = ' . "'"  . serialize($value) . "';\n";
-	}
-	foreach ($_POST as $key => $value) {
-		$message .= '$my_POST["' . $key . '"] = ' . "'"  . serialize($value) . "';\n";
-	}
-	foreach ($_REQUEST as $key => $value) {
-		$message .= '$my_REQUEST["' . $key . '"] = ' . "'"  . serialize($value) . "';\n";
-	}
-	$file = EVENT_ESPRESSO_UPLOAD_DIR . 'logs/espresso_debug.php';
-
-	if (is_writable($file)) {
-		$fh = fopen($file, 'w') or die("Cannot open file! " . $file);
-		fwrite($fh, $message);
-		fclose($fh);
-	} else {
-		global $notices;
-		$notices['errors'][] = sprintf(__('Your debug file is not writable. Check if your server is able to write to %s.', 'event_espresso'), $file);
-	}
-}
-
+// End of file EE_Log.core.php
+// Location: /core/EE_Log.core.php
