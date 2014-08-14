bcc6747dad6fc48dfed7f543ae63d72b626b79f0
Brent Christensen <brent@eventespresso.com>
2014-08-14 13:00:13 -0700

    make sure SPCO gets tagged as EE page [touch:6466]

--- modules/single_page_checkout/EED_Single_Page_Checkout.module.php
+++ modules/single_page_checkout/EED_Single_Page_Checkout.module.php
@@ -300,6 +300,7 @@ class EED_Single_Page_Checkout  extends EED_Module {
 		if ( ! isset( EE_Registry::instance()->REQ )) {
 			EE_Registry::instance()->load_core( 'Request_Handler' );
 		}
+		EE_Registry::instance()->REQ->set_espresso_page( TRUE );
 //		printr( EE_Registry::instance()->REQ, 'EE_Registry::instance()->REQ  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
 		// make sure reg steps array is setup
 		if ( empty( self::$_reg_steps )) {
