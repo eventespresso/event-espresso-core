cb8f78a36d47fcf6e8dc693de9d5d3e517024ef2
Darren Ethier <darren@roughsmootheng.in>
2014-08-15 07:48:40 -0400

    make sure validation of messages_system happens after everything is loaded [touch: 6486]

--- core/EE_System.core.php
+++ core/EE_System.core.php
@@ -537,7 +537,9 @@ final class EE_System {
 	 * @return void
 	 */
 	private function _do_setup_validations( $request_type ) {
-		EEH_Activation::validate_messages_system();
+		if ( $request_type !== EE_System::req_type_new_activation ) {
+			add_action( 'AHEE__EE_System__core_loaded_and_ready', array( 'EEH_Activation', 'validate_messages_system' ), 1 );
+		}
 		do_action( 'AHEE__EE_System___do_setup_validations', $request_type );
 	}
 
