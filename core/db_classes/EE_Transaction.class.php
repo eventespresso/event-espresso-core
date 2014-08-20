717bdb0da31287432ea9ac2c2bc2bb26b01f787b
Michael Nelson <michael@eventespresso.com>
2014-08-20 14:44:46 -0700

    [touch:6327]{t:10}another minor tweak: EE_Transaction::line_items shouldnt require any query params

--- core/db_classes/EE_Transaction.class.php
+++ core/db_classes/EE_Transaction.class.php
@@ -512,7 +512,7 @@ class EE_Transaction extends EE_Base_Class {
 	 * @param array $query_params
 	 * @return EE_Line_Item[]
 	 */
-	public function line_items( $query_params ) {
+	public function line_items( $query_params = array() ) {
 		return $this->get_many_related( 'Line_Item', $query_params );
 	}
 
