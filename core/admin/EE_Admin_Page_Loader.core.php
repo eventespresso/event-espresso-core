763e30e4d8124aabe3ba69ce8f02de0684e2aabc
Seth <seth@eventespresso.com>
2014-08-26 13:40:26 -0600

    Fixed the spelling of the word add-on in several public facing places throughout the code. [touch:6546]

--- core/admin/EE_Admin_Page_Loader.core.php
+++ core/admin/EE_Admin_Page_Loader.core.php
@@ -206,7 +206,7 @@ class EE_Admin_Page_Loader {
 				'parent_slug' => 'espresso_events'
 				)),
 			'addons' => new EE_Admin_Page_Menu_Group( array(
-				'menu_label' => __('Addons', 'event_espresso'),
+				'menu_label' => __('Add-ons', 'event_espresso'),
 				'show_on_menu' => TRUE,
 				'menu_slug' => 'addons',
 				'capability' => 'administrator',
@@ -442,7 +442,7 @@ class EE_Admin_Page_Loader {
 				$page_map = $page->get_menu_map();
 				//if we've got an array then the menu map is in the old format so let's throw a persistent notice that the admin system isn't setup correctly for this item.
 				if ( is_array( $page_map ) || empty( $page_map ) ) {
-					EE_Error::add_persistent_admin_notice( 'menu_map_warning_' . str_replace(' ', '_', $page->label) . '_' . EVENT_ESPRESSO_VERSION, sprintf( __('The admin page for %s was not correctly setup because it is using an older method for integrating with Event Espresso Core.  This means that full functionality for this component is not available.  This error message usually appears with an Addon that is out of date.  Make sure you update all your Event Espresso 4 addons to the latest version to ensure they have necessary compatibility updates in place.', 'event_espresso' ), $page->label ) );
+					EE_Error::add_persistent_admin_notice( 'menu_map_warning_' . str_replace(' ', '_', $page->label) . '_' . EVENT_ESPRESSO_VERSION, sprintf( __('The admin page for %s was not correctly setup because it is using an older method for integrating with Event Espresso Core.  This means that full functionality for this component is not available.  This error message usually appears with an Add-on that is out of date.  Make sure you update all your Event Espresso 4 add-ons to the latest version to ensure they have necessary compatibility updates in place.', 'event_espresso' ), $page->label ) );
 					continue;
 				}
 
