5ac6bde5979ad735c4d26cbbe89342a859437c87
Brent Christensen <brent@eventespresso.com>
2014-08-26 15:20:36 -0700

    make sure to display labels for select inputs [touch:6442]

--- core/libraries/form_sections/strategies/layout/EE_Div_Per_Section_Layout.strategy.php
+++ core/libraries/form_sections/strategies/layout/EE_Div_Per_Section_Layout.strategy.php
@@ -21,24 +21,31 @@ class EE_Div_Per_Section_Layout extends EE_Form_Section_Layout_Base{
 		if ( $input instanceof EE_Hidden_Input  ) {
 			$html .= EEH_Formatter::nl() . $input->get_html_for_input();
 		} else if ( $input instanceof EE_Submit_Input  ) {
-			$html .= EEH_Formatter::nl() . '<div id="' . $input->html_id() . '-submit-dv" class="' . $input->html_class() . '-submit-dv">';
-			$html .= EEH_Formatter::nl() . $input->get_html_for_input();
+			$html .= EEH_Formatter::nl(1) . '<div id="' . $input->html_id() . '-submit-dv" class="' . $input->html_class() . '-submit-dv">';
+			$html .= EEH_Formatter::nl(1) . $input->get_html_for_input();
 			$html .= EEH_Formatter::nl(-1) . '</div>';
-		} else if ( $input instanceof EE_Form_Input_With_Options_Base  ) {
-			$html .= EEH_Formatter::nl() . '<div id="' . $input->html_id() . '-input-dv" class="' . $input->html_class() . '-input-dv">';
-			$html .= EEH_Formatter::nl() . $input->get_html_for_errors();
-			$html .= EEH_Formatter::nl() . $input->get_html_for_input();
-			$html .= EEH_Formatter::nl() . $input->get_html_for_help();
-			$html .= EEH_Formatter::nl(-1) . '</div>';
-		} else {
-			$html .= EEH_Formatter::nl() . '<div id="' . $input->html_id() . '-input-dv" class="' . $input->html_class() . '-input-dv">';
+		} else if ( $input instanceof EE_Select_Input  ) {
+			$html .= EEH_Formatter::nl(1) . '<div id="' . $input->html_id() . '-input-dv" class="' . $input->html_class() . '-input-dv">';
 			$html .= EEH_Formatter::nl(1) . $input->get_html_for_label();
 			$html .= EEH_Formatter::nl() . $input->get_html_for_errors();
 			$html .= EEH_Formatter::nl() . $input->get_html_for_input();
 			$html .= EEH_Formatter::nl() . $input->get_html_for_help();
 			$html .= EEH_Formatter::nl(-1) . '</div>';
-			$html .= EEH_Formatter::nl(-1);
+		} else if ( $input instanceof EE_Form_Input_With_Options_Base  ) {
+			$html .= EEH_Formatter::nl(1) . '<div id="' . $input->html_id() . '-input-dv" class="' . $input->html_class() . '-input-dv">';
+			$html .= EEH_Formatter::nl(1) . $input->get_html_for_errors();
+			$html .= EEH_Formatter::nl() . $input->get_html_for_input();
+			$html .= EEH_Formatter::nl() . $input->get_html_for_help();
+			$html .= EEH_Formatter::nl(-1) . '</div>';
+		} else {
+			$html .= EEH_Formatter::nl(1) . '<div id="' . $input->html_id() . '-input-dv" class="' . $input->html_class() . '-input-dv">';
+			$html .= EEH_Formatter::nl(1) . $input->get_html_for_label();
+			$html .= EEH_Formatter::nl() . $input->get_html_for_errors();
+			$html .= EEH_Formatter::nl() . $input->get_html_for_input();
+			$html .= EEH_Formatter::nl() . $input->get_html_for_help();
+			$html .= EEH_Formatter::nl(-1) . '</div>';
 		}
+		$html .= EEH_Formatter::nl(-1);
 		return $html;
 	}
 
