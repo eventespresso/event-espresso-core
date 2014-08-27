c329fba53e8955cfc5a0fb778643785566930ef6
Brent Christensen <brent@eventespresso.com>
2014-08-27 14:14:29 -0700

    use required_css_class(), reformat for readability [touch:6442]

--- core/libraries/form_sections/strategies/display/EE_Select_Multiple_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Select_Multiple_Display_Strategy.strategy.php
@@ -27,22 +27,37 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  */
 class EE_Select_Multiple_Display_Strategy extends EE_Select_Display_Strategy{
 	public function display() {
-		$input = $this->_input;
-		if( ! $input instanceof EE_Form_Input_With_Options_Base){
-			throw new EE_Error(sprintf(__("Cannot use Select Multiple Display Strategy with an input that doesn't ahve options", "event_espresso")));
+
+		if( ! $this->_input instanceof EE_Form_Input_With_Options_Base){
+			throw new EE_Error(sprintf(__('Cannot use Select Multiple Display Strategy with an input that doesn\'t have options', "event_espresso")));
 		}
-		$class = $this->_input->required() ? 'ee-needs-value ' . $input->html_class() : $input->html_class();
-		$html= EEH_Formatter::nl(1) . "<select multiple id='{$input->html_id()}' name='{$input->html_name()}[]' class='{$class}' style='{$input->html_style()}'/>";
+
+		$html = EEH_Formatter::nl();
+		$html .= '<select multiple';
+		$html .= ' id="' . $this->_input->html_id() . '"';
+		$html .= ' name="' . $this->_input->html_name() . '[]"';
+		$class = $this->_input->required() ? $this->_input->required_css_class() . ' ' . $this->_input->html_class() : $this->_input->html_class();
+		$html .= ' class="' . $class . '"';
+		// add html5 required
+		$html .= $this->_input->required() ? ' required' : '';
+		$html .= ' style="' . $this->_input->html_style() . '"';
+		$html .= '>';
+
+
+
 		EE_Registry::instance()->load_helper('Array');
-		if(EEH_Array::is_multi_dimensional_array($input->options())){
+		if( EEH_Array::is_multi_dimensional_array( $this->_input->options() )){
 			throw new EE_Error(sprintf(__("Select multiple display strategy does not allow for nested arrays of options.", "event_espresso")));
 		}else{
-			$html.=$this->_display_options($input->options());
+			$html.=$this->_display_options( $this->_input->options() );
 		}
 
 		$html.= EEH_Formatter::nl(-1) . "</select>";
 		return $html;
 	}
+
+
+
 	/**
 	 * Checks if that $value is one of the selected ones
 	 * @param string|int $value
