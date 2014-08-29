422432d4e3cedd0e43a8d8038db09b4096304df3
Brent Christensen <brent@eventespresso.com>
2014-08-28 17:37:19 -0700

    remove value slug from html id if there is only one element in options array, format for better readability [touch:6442]

--- core/libraries/form_sections/strategies/display/EE_Checkbox_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Checkbox_Display_Strategy.strategy.php
@@ -12,17 +12,20 @@ class EE_Checkbox_Display_Strategy extends EE_Display_Strategy_Base{
 		}
 		//d( $this->_input );
 		$html = '';
+		$multi = count( $this->_input->options() ) > 1 ? TRUE : FALSE;
 		foreach( $this->_input->options() as $value => $display_text ){
-			if ( $this->_input->raw_value() && in_array( $value, $this->_input->raw_value() )){
-				$selected_attr = ' checked="checked"';
-			} else {
-				$selected_attr = '';
-			}
-			$value_slug = sanitize_key( $value );
-			$value_inside_attribute = esc_attr( $value );
-			$html .= '<label for="' . $this->_input->html_id() . '-' . $value_slug . '" class="ee-checkbox-label-after">';
-			$html .= EEH_Formatter::nl(1) . '<input id="' . $this->_input->html_id() . '-' . $value_slug . '" name="' . $this->_input->html_name() . '[]" class="' . $this->_input->html_class() . '" style="' . $this->_input->html_style() . '" type="checkbox" value="' . $value_inside_attribute . '"' . $selected_attr . '>';
-			$html .= '&nbsp;' . $display_text;
+			$html_id = $multi ? $this->_input->html_id() . '-' . sanitize_key( $value ) : $this->_input->html_id();
+			$html .= '<label for="' . $html_id . '" class="ee-checkbox-label-after">';
+			$html .= EEH_Formatter::nl(1);
+			$html .= '<input type="checkbox"';
+			$html .= ' name="' . $this->_input->html_name() . '[]"';
+			$html .= ' id="' . $html_id . '"';
+			$html .= ' class="' . $this->_input->html_class() . '"';
+			$html .= ' style="' . $this->_input->html_style() . '"';
+			$html .= ' value="' . esc_attr( $value ) . '"';
+			$html .= $this->_input->raw_value() && in_array( $value, $this->_input->raw_value() ) ? ' checked="checked"' : '';
+			$html .= '>&nbsp;';
+			$html .= $display_text;
 			$html .= EEH_Formatter::nl(-1) . '</label>';
 		}
 		return $html;
