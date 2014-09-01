2de8eb02531572c753e8c44a17189f062694bbf9
Brent Christensen <brent@eventespresso.com>
2014-09-01 11:35:54 -0700

    add instanceof checks where applicable, fix possible undeclared variable, fix typos and php docs [touch:6523]

--- admin_pages/payments/Payments_Admin_Page.core.php
+++ admin_pages/payments/Payments_Admin_Page.core.php
@@ -35,6 +35,15 @@ class Payments_Admin_Page extends EE_Admin_Page {
 	 */
 	private $_sort_logs_again_direction;
 
+
+
+	/**
+	 * @Constructor
+	 *
+	 * @access public
+	 * @param bool $routing indicate whether we want to just load the object and handle routing or just load the object.
+	 * @return \Payments_Admin_Page
+	 */
 	public function __construct( $routing = TRUE ) {
 		parent::__construct( $routing );
 	}
@@ -196,25 +205,25 @@ class Payments_Admin_Page extends EE_Admin_Page {
 			)
 			);
 	}
+
+
+
+	/**
+	 * @return array
+	 */
 	protected function _add_payment_method_help_tabs(){
 		EE_Registry::instance()->load_lib('Payment_Method_Manager');
-		$pmts = EE_Payment_Method_Manager::instance()->payment_method_types();
+		$payment_method_types = EE_Payment_Method_Manager::instance()->payment_method_types();
 		$all_pmt_help_tabs_config = array();
-		foreach($pmts as $pmt){
-			foreach($pmt->help_tabs_config() as $help_tab_name => $config){
+		foreach( $payment_method_types as $payment_method_type ){
+			foreach( $payment_method_type->help_tabs_config() as $help_tab_name => $config ){
 				$all_pmt_help_tabs_config[$help_tab_name] = array(
 					'title'=>$config['title'],
-					'content'=>EEH_Template::display_template($pmt->file_folder().'help_tabs'.DS.$config['filename'].'.help_tab.php', array('admin_page_obj'=>$this), true)
+					'content'=>EEH_Template::display_template( $payment_method_type->file_folder().'help_tabs'.DS.$config['filename'].'.help_tab.php', array('admin_page_obj'=>$this), true)
 				);
 			}
 		}
 		return $all_pmt_help_tabs_config;
-//		return array(
-//			'monkeys_tabs'=>array(
-//				'content'=>'moenksy',
-//				'title'=>'MONKEYS'
-//			)
-//		);
 	}
 
 
@@ -318,15 +327,20 @@ class Payments_Admin_Page extends EE_Admin_Page {
 		$this->display_admin_page_with_sidebar();
 
 	}
+
+
+
 	/**
+	 * 	payment_method_settings_meta_box
 	 *
-	 * @param NULL $post_obj_which_is_null is an object containing the current post (as a $post object)
-	 * @param array $metabox is an array with metabox id, title, callback, and args elements.
-	 * the value at 'args' has key 'payment_method', as set within _payment_methods_list
+	 * @param NULL  $post_obj_which_is_null is an object containing the current post (as a $post object)
+	 * @param array $metabox                is an array with metabox id, title, callback, and args elements.
+	 *                                      the value at 'args' has key 'payment_method', as set within _payment_methods_list
+	 * @throws EE_Error
 	 */
-	public function payment_method_settings_meta_box($post_obj_which_is_null,$metabox){
+	public function payment_method_settings_meta_box( $post_obj_which_is_null, $metabox){
 		$payment_method = isset($metabox['args']) && isset($metabox['args']['payment_method']) ? $metabox['args']['payment_method'] : NULL;
-		if ( ! $payment_method){
+		if ( ! $payment_method instanceof EE_Payment_Method ){
 			throw new EE_Error(sprintf(__("Payment method metabox setup incorrectly. No Payment method object was supplied", "event_espresso")));
 		}
 		$template_args = array(
@@ -355,7 +369,7 @@ class Payments_Admin_Page extends EE_Admin_Page {
 	 */
 	protected function _simplify_form($form_section){
 		$form_section->exclude(array(
-			'PMD_type',//dont want them chaning the type
+			'PMD_type',//dont want them changing the type
 			'PMD_order',//or the order, for now
 			'PMD_slug',//or the slug (probably never)
 			'PRC_ID',//or the price surcharge (will probably change soon)
@@ -366,8 +380,8 @@ class Payments_Admin_Page extends EE_Admin_Page {
 	}
 
 	/**
-	 * Activates a payment method of that type. MOstly assuming there is only 1 of that type (or none so far)
-	 * @global type $current_user
+	 * Activates a payment method of that type. Mostly assuming there is only 1 of that type (or none so far)
+	 * @global WP_User $current_user
 	 */
 	protected function _activate_payment_method(){
 		if(isset($this->_req_data['payment_method_type'])){
@@ -378,6 +392,7 @@ class Payments_Admin_Page extends EE_Admin_Page {
 				global $current_user;
 				$pm_type_class = EE_Payment_Method_Manager::instance()->payment_method_class_from_type($payment_method_type);
 				if(class_exists($pm_type_class)){
+					/** @var $pm_type_obj EE_PMT_Base */
 					$pm_type_obj = new $pm_type_class;
 					$payment_method = EEM_Payment_Method::instance()->get_one_by_slug($pm_type_obj->system_name());
 					if( ! $payment_method){
@@ -430,24 +445,22 @@ class Payments_Admin_Page extends EE_Admin_Page {
 
 	/**
 	 * Processes the payment method form that was submitted. This is slightly trickier than usual form
-	 * processing because we first need to identify WHICH form was processed and which paymetn method
+	 * processing because we first need to identify WHICH form was processed and which payment method
 	 * it corresponds to. Once we have done that, we see if the form is valid. If it is, the
 	 * form's data is saved and we redirect to the default payment methods page, setting the updated payment method
 	 * as the currently-selected one. If it DOESN'T validate, we render the page with the form's errors (in the
-	 * susbequently called 'headers_sent_func' which is _payment_methods_list)
+	 * subsequently called 'headers_sent_func' which is _payment_methods_list)
 	 * @return void
 	 */
 	protected function _update_payment_method(){
 		if( $_SERVER['REQUEST_METHOD'] == 'POST'){
-
-			//echo "early processing ran";return;
 			//ok let's find which gateway form to use based on the form input
 			EE_Registry::instance()->load_lib('Payment_Method_Manager');
-			/**
-			 * @var $correct_pmt_form_to_use EE_Payment_Method_Form
-			 */
+			/** @var $correct_pmt_form_to_use EE_Payment_Method_Form */
 			$correct_pmt_form_to_use = NULL;
+			$pmt_obj = NULL;
 			foreach(EE_Payment_Method_Manager::instance()->payment_method_types() as $pmt_obj){
+				/** @var $pmt_obj EE_PMT_Base */
 				//get the form and simplify it, like what we do when we display it
 				$pmt_form = $pmt_obj->settings_form();
 				$this->_simplify_form($pmt_form);
@@ -458,16 +471,22 @@ class Payments_Admin_Page extends EE_Admin_Page {
 			}
 			//if we couldn't find the correct payment method type...
 			if( ! $correct_pmt_form_to_use ){
-				EE_Error::add_error(__("We could not find which payment metho type your form submission related to. Please contact support", 'event_espresso'));
+				EE_Error::add_error(__("We could not find which payment method type your form submission related to. Please contact support", 'event_espresso'));
 				$this->_redirect_after_action(FALSE, 'Payment Method', 'activated', array('action' => 'default'));
 			}
 			$correct_pmt_form_to_use->receive_form_submission($this->_req_data);
 			if($correct_pmt_form_to_use->is_valid()){
 				$correct_pmt_form_to_use->save();
 				$pm = $correct_pmt_form_to_use->get_model_object();
+				/** @var $pm EE_Payment_Method */
 				$this->_redirect_after_action(TRUE, 'Payment Method', 'updated', array('action' => 'default','payment_method'=>$pm->slug()));
 			}else{
-				EE_Error::add_error(sprintf(__("Payment method of type %s was not saved because there were validation errors. They have been marked in the form", 'event_espresso'),$pmt_obj->pretty_name()));
+				EE_Error::add_error(
+					sprintf(
+						__('Payment method of type %s was not saved because there were validation errors. They have been marked in the form', 'event_espresso'),
+						$pmt_obj instanceof EE_PMT_Base ? $pmt_obj->pretty_name() : __( '"(unknown)"', 'event_espresso' )
+					)
+				);
 			}
 		}
 		return;
@@ -519,12 +538,15 @@ class Payments_Admin_Page extends EE_Admin_Page {
 				)
 		);
 	}
+
+
+
 	/**
 	 *
-	 * @param type $payment_method_id
-	 * @param type $transaction_id
-	 * @param type $order_asc
-	 * @return type
+	 * @param int  $per_page
+	 * @param int  $current_page
+	 * @param bool $count
+	 * @return array
 	 */
 	public function get_payment_logs($per_page = 50, $current_page = 0, $count = false){
 		//we may need to do multiple queries (joining differently), so we actually wan tan array of query params
@@ -606,8 +628,8 @@ class Payments_Admin_Page extends EE_Admin_Page {
 	/**
 	 * Used by usort to RE-sort log query results, because we lose the ordering
 	 * because we're possibly combining the results from two queries
-	 * @param type $logA
-	 * @param type $logB
+	 * @param EE_Log $logA
+	 * @param EE_Log $logB
 	 * @return int
 	 */
 	protected function _sort_logs_again($logA,$logB){
@@ -625,23 +647,27 @@ class Payments_Admin_Page extends EE_Admin_Page {
 	}
 
 	protected function _payment_log_details() {
+		/** @var $payment_log EE_Log */
 		$payment_log = EEM_Log::instance()->get_one_by_ID($this->_req_data['ID']);
 		$payment_method = NULL;
 		$transaction = NULL;
-		if($payment_log->object()){
-			if($payment_log->object() instanceof EE_Payment){
+		if( $payment_log instanceof EE_Log ){
+			if( $payment_log->object() instanceof EE_Payment ){
 				$payment_method = $payment_log->object()->payment_method();
 				$transaction = $payment_log->object()->transaction();
 			}elseif($payment_log->object() instanceof EE_Payment_Method){
 				$payment_method = $payment_log->object();
 			}
 		}
-
-
-		$this->_template_args['admin_page_content'] = EEH_Template::display_template( EE_PAYMENTS_TEMPLATE_PATH . 'payment_log_details.template.php', array(
-			'payment_log'=>$payment_log,
-			'payment_method'=>$payment_method,
-			'transaction'=>$transaction), TRUE );
+		$this->_template_args['admin_page_content'] = EEH_Template::display_template(
+			EE_PAYMENTS_TEMPLATE_PATH . 'payment_log_details.template.php',
+			array(
+				'payment_log'=>$payment_log,
+				'payment_method'=>$payment_method,
+				'transaction'=>$transaction
+			),
+			TRUE
+		);
 		$this->display_admin_page_with_sidebar();
 
 	}
