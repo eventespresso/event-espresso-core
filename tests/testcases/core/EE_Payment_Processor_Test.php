b0b32b20e081dd6b35d431f534815bd6db151296
Brent Christensen <brent@eventespresso.com>
2014-09-01 12:07:51 -0700

    fix typos [touch:6523]

--- tests/testcases/core/EE_Payment_Processor_Test.php
+++ tests/testcases/core/EE_Payment_Processor_Test.php
@@ -70,33 +70,33 @@ class EE_Payment_Processor_Test extends EE_UnitTestCase{
 		$successful_payment_actions = EEH_Array::is_set( $wp_actions, 'AHEE__EE_Payment_Processor__update_txn_based_on_payment__successful', 0 );
 		$payment = EE_Payment_Processor::instance()->process_payment( $pm, $transaction, NULL, NULL, 'success', 'CART', TRUE, TRUE );
 		$this->assertInstanceOf( 'EE_Payment', $payment );
-		//assert that the paymetn still has its default status
+		//assert that the payment still has its default status
 		$this->assertEquals( EEM_Payment::instance()->field_settings_for( 'STS_ID' )->get_default_value(), $payment->status() );
 		//assert that the we haven't notified of successful payment JUST yet...
 		$this->assertEquals( $successful_payment_actions, EEH_Array::is_set($wp_actions, 'AHEE__EE_Payment_Processor__update_txn_based_on_payment__successful', 0 ) );
 
 		//DECLINED IPN
 		$payment = EE_Payment_Processor::instance()->process_ipn( array('status' => EEM_Payment::status_id_pending, 'gateway_txn_id' =>$payment->txn_id_chq_nmbr() ), $transaction, $pm );
-		//payment should be what the gatewya set it to be, whcih was failed
+		//payment should be what the gateway set it to be, whcih was failed
 		$this->assertEquals( EEM_Payment::status_id_pending, $payment->status() );
 		//and the payment-approved action should have NOT been triggered
 		$this->assertEquals( $successful_payment_actions, EEH_Array::is_set($wp_actions, 'AHEE__EE_Payment_Processor__update_txn_based_on_payment__successful', 0 ) );
 
 		//SUCCESSFUL IPN
 		$payment = EE_Payment_Processor::instance()->process_ipn( array('status' => EEM_Payment::status_id_approved, 'gateway_txn_id' =>$payment->txn_id_chq_nmbr() ), $transaction, $pm );
-		//payment should be what the gatewya set it to be, whcih was failed
+		//payment should be what the gateway set it to be, whcih was failed
 		$this->assertEquals( EEM_Payment::status_id_approved, $payment->status() );
 		//and the payment-approved action should have been triggered
 		$this->assertEquals( $successful_payment_actions + 1, EEH_Array::is_set($wp_actions, 'AHEE__EE_Payment_Processor__update_txn_based_on_payment__successful', 0 ) );
 
 		//DUPLICATE SUCCESS IPN
 		//for this, we need to reset payment model so we fetch a NEW payment object, instead of reusing the old
-		//and because the payment method caches a payment method type which caches a gateway which caches the paymetn model,
+		//and because the payment method caches a payment method type which caches a gateway which caches the payment model,
 		//we also need to reset the payment method
 		EEM_Payment::reset();
 		$pm = EEM_Payment_Method::reset()->get_one_by_ID( $pm->ID() );
 		$payment = EE_Payment_Processor::instance()->process_ipn( array('status' => EEM_Payment::status_id_approved, 'gateway_txn_id' =>$payment->txn_id_chq_nmbr() ), $transaction, $pm );
-		//payment should be what the gatewya set it to be, whcih was failed
+		//payment should be what the gateway set it to be, whcih was failed
 		$this->assertEquals( EEM_Payment::status_id_approved, $payment->status() );
 		//and the payment-approved action should have NOT been triggered this time because it's a duplicate
 		$this->assertEquals( $successful_payment_actions + 1, EEH_Array::is_set($wp_actions, 'AHEE__EE_Payment_Processor__update_txn_based_on_payment__successful', 0 ) );
