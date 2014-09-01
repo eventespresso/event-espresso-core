5d805da0ef297ce30deb1f34fa7fa829677c5454
Brent Christensen <brent@eventespresso.com>
2014-09-01 12:15:53 -0700

    change payment method's _default_description [touch:6523]

--- payment_methods/Mijireh/EE_PMT_Mijireh.pm.php
+++ payment_methods/Mijireh/EE_PMT_Mijireh.pm.php
@@ -30,7 +30,7 @@ class EE_PMT_Mijireh extends EE_PMT_Base{
 		require_once($this->file_folder().'EEG_Mijireh.gateway.php');
 		$this->_gateway = new EEG_Mijireh();
 		$this->_pretty_name = __("Mijireh", 'event_espresso');
-		$this->_default_description = __( 'On the next page you will be able to enter your billing information to make the payment', 'event_espresso' );
+		$this->_default_description = __( 'After clicking "Finalize Registration", you will be able to enter your billing information and complete your payment', 'event_espresso' );
 		parent::__construct($pm_instance);
 		$this->_default_button_url = $this->file_url() . 'lib' . DS . 'mijireh-checkout-logo.png';
 	}
