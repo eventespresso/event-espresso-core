8158023256b0ea608beb64076cff9226b8b9381e
Brent Christensen <brent@eventespresso.com>
2014-09-01 12:09:01 -0700

    fix typos and php docs [touch:6523]

--- core/db_classes/EE_Currency_Payment_Method.class.php
+++ core/db_classes/EE_Currency_Payment_Method.class.php
@@ -18,45 +18,46 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  * ------------------------------------------------------------------------
  *
  * EE_Currency_Payment_Method
- * Model for showing which currencies apply to which paymetn methods
+ * Model for showing which currencies apply to which payment methods
  *
  * @package			Event Espresso
- * @subpackage		
+ * @subpackage
  * @author				Mike Nelson
  *
  * ------------------------------------------------------------------------
  */
 class EE_Currency_Payment_Method extends EE_Base_Class{
-	/** Currency to Payment Method LInk ID @var CPM_ID*/ protected $_CPM_ID = NULL;
-	/** Currency Code @var CUR_code*/ protected $_CUR_code = NULL;
-	/** Paymetn Method ID @var PMD_ID*/ protected $_PMD_ID = NULL; 
+
+	/** Currency to Payment Method Link ID @var CPM_ID*/
+	protected $_CPM_ID = NULL;
+	/** Currency Code @var CUR_code*/
+	protected $_CUR_code = NULL;
+	/** Payment Method ID @var PMD_ID*/
+	protected $_PMD_ID = NULL;
 	protected $_Payment_Method;
 	protected $_Currency;
+
+
+
 	/**
-	 * 
-	 * @param type $props_n_values
+	 *
+	 * @param array $props_n_values
 	 * @return EE_Currency_Payment_Method
 	 */
 	public static function new_instance( $props_n_values = array() ) {
-		$classname = __CLASS__;
-		$has_object = parent::_check_for_object( $props_n_values, $classname );
-//		d( $has_object );
+		$has_object = parent::_check_for_object( $props_n_values, __CLASS__ );
 		return $has_object ? $has_object : new self( $props_n_values);
 	}
 
 	/**
-	 * 
-	 * @param type $props_n_values
+	 *
+	 * @param array $props_n_values
 	 * @return EE_Currency_Payment_Method
 	 */
 	public static function new_instance_from_db ( $props_n_values = array() ) {
-		$classname = __CLASS__;
-//		$mapped_object = parent::_get_object_from_entity_mapper($props_n_values, $classname);
-//		d( $mapped_object );
-//		return $mapped_object ? $mapped_object : new self( $props_n_values, TRUE );
 		return new self( $props_n_values, TRUE );
 	}
-	
+
 }
 
 // End of file EE_Currency_Payment_Method.class.php
