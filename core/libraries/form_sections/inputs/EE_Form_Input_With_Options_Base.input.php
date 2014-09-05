57cf9efc31d6c5670c138255d9d394cd74794260
Brent Christensen <brent@eventespresso.com>
2014-09-05 13:58:00 -0700

    add $_use_desc_in_label, $_label_size, & $_enforce_label_size properties, set _label_size and _enforce_label_size in constructor from input settings, refactor set_select_options() to accomodate an array of EE_Question_Options, add _set_label_size() methods and call it while looping through answer options to find longest label, add get_label_size_class() method for calculating the label css class based on the label_size [touch:6603]

--- core/libraries/form_sections/inputs/EE_Form_Input_With_Options_Base.input.php
+++ core/libraries/form_sections/inputs/EE_Form_Input_With_Options_Base.input.php
@@ -1,76 +1,102 @@
-<?php
-
-if (!defined('EVENT_ESPRESSO_VERSION'))
-	exit('No direct script access allowed');
-
+<?php if (!defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
 /**
- * Event Espresso
- *
- * Event Registration and Management Plugin for WordPress
- *
- * @ package			Event Espresso
- * @ author			Seth Shoultes
- * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link					http://www.eventespresso.com
- * @ version		 	4.3
- *
- * ------------------------------------------------------------------------
- *
  * EE_Form_Input_With_Options_Base
- * For form inputs which are meant to only have a
- * limit set of options that can be used (like for checkboxes or select dropdowns, etc; as opposed to more open-ended text boxes etc)
+ *
+ * For form inputs which are meant to only have a limit set of options that can be used
+ * (like for checkboxes or select dropdowns, etc; as opposed to more open-ended text boxes etc)
+ *
  * @package			Event Espresso
  * @subpackage
- * @author				Mike Nelson
- *
- * ------------------------------------------------------------------------
+ * @author				Mike Nelson, Brent Christensen
  */
 class EE_Form_Input_With_Options_Base extends EE_Form_Input_Base{
+
+	/**
+	 * array of available options to choose as an answer
+	 * @var array
+	 */
 	protected $_options = array();
+
+
+	/**
+	 * whether to display an question option description as part of the input label
+	 * @var boolean
+	 */
+	protected $_use_desc_in_label = TRUE;
+
+	/**
+	 * strlen() result for the longest input value (what gets displayed in the label)
+	 * this is used to apply a css class to the input label
+	 * @var int
+	 */
+	protected $_label_size = 0;
+
+	/**
+	 * whether to enforce the label size value passed in the constructor
+	 * @var boolean
+	 */
+	protected $_enforce_label_size = FALSE;
+
 	/**
 	 * whether to allow multiple selections (ie, the value of this input should be an array)
 	 * or not (ie, the value should be a simple int, string, etc)
 	 * @var boolean
 	 */
-	protected $_multiple_selections = false;
+	protected $_multiple_selections = FALSE;
 
 
 
 	/**
-	 * @param array $select_options
-	 * @param array $options_array
+	 * @param array $answer_options
+	 * @param array $input_settings
 	 */
-	public function __construct($select_options = array(), $options_array = array()) {
-		$this->set_select_options($select_options);
-		parent::__construct($options_array);
+	public function __construct( $answer_options = array(), $input_settings = array() ) {
+		if ( isset( $input_settings['label_size'] )) {
+			$this->_set_label_size( $input_settings['label_size'] );
+			if ( isset( $input_settings['enforce_label_size'] ) && $input_settings['enforce_label_size'] ) {
+				$this->_enforce_label_size = TRUE;
+			}
+		}
+		//d( $input_settings );
+		//d( $answer_options );
+		$this->set_select_options( $answer_options );
+		parent::__construct( $input_settings );
 	}
+
+
+
 	/**
 	 * Sets the allowed options for this input. Also has the side-effect of
 	 * updating the normalization strategy to match the keys provided in the array
-	 * @param array $select_options
-	 * @return null just has the side-effect of setting the options for this input
+	 * @param array $answer_options
+	 * @return void  just has the side-effect of setting the options for this input
 	 */
-	public function set_select_options($select_options){
-		$this->_options = $select_options;
-		//get the first item in the select options. Depending on what it is, use a different normalization strategy
-		if($select_options){
-			$flat_select_options = $this->_flatten_select_options($select_options);
-			$select_option_keys = array_keys($flat_select_options);
-			$first_key = reset($select_option_keys);
-			if( count($flat_select_options) ==2 && in_array(TRUE, $select_option_keys) && in_array(FALSE,$select_option_keys)){
-				$normalization = new EE_Boolean_Normalization();
-			}elseif(is_int($first_key)){
-				$normalization = new EE_Int_Normalization();
-			}else{
-				$normalization = new EE_Text_Normalization();
-			}
-			if($this->_multiple_selections){
-				$this->_set_normalization_strategy(new EE_Many_Valued_Normalization($normalization));
-			}else{
-				$this->_set_normalization_strategy($normalization);
-			}
-
+	public function set_select_options( $answer_options = array() ){
+		$answer_options = is_array( $answer_options ) ? $answer_options : array( $answer_options );
+		//get the first item in the select options and check it's type
+		if ( reset( $answer_options ) instanceof EE_Question_Option ) {
+			$this->_options = $this->_process_question_options( $answer_options );
+		} else {
+			$this->_options = $this->_flatten_select_options( $answer_options );
+		}
+		//d( $this->_options );
+		$select_option_keys = array_keys( $this->_options );
+		// grab key from first element of values
+		$first_key = reset( $select_option_keys );
+		// attempt to determine data type for values in order to set normalization type
+		if ( count( $this->_options ) == 2 && in_array( TRUE, $select_option_keys ) && in_array( FALSE, $select_option_keys )){
+			// values appear to be boolean, like TRUE, FALSE, 1, 0
+			$normalization = new EE_Boolean_Normalization();
+		} elseif ( is_int( $first_key )){
+			$normalization = new EE_Int_Normalization();
+		} else {
+			$normalization = new EE_Text_Normalization();
+		}
+		// does input type have multiple options ?
+		if ( $this->_multiple_selections ) {
+			$this->_set_normalization_strategy( new EE_Many_Valued_Normalization( $normalization ));
+		} else {
+			$this->_set_normalization_strategy( $normalization );
 		}
 	}
 
@@ -94,20 +120,99 @@ class EE_Form_Input_With_Options_Base extends EE_Form_Input_Base{
 	 * @param array $arr
 	 * @return array
 	 */
-	protected function _flatten_select_options($arr){
+	protected function _flatten_select_options( $arr ){
+		$flat_array = array();
 		EE_Registry::instance()->load_helper('Array');
-		if(EEH_Array::is_multi_dimensional_array($arr)){
-			$flat_array = array();
-			foreach($arr as $sub_array){
-				foreach($sub_array as $key => $value){
-					$flat_array[$key] = $value;
+		if ( EEH_Array::is_multi_dimensional_array( $arr )) {
+			foreach( $arr as $sub_array ){
+				foreach( $sub_array as $key => $value ) {
+					$flat_array[ $key ] = $value;
+					$this->_set_label_size( $value );
 				}
 			}
-			return $flat_array;
-		}else{
-			return $arr;
+		} else {
+			foreach( $arr as $key => $value ) {
+				$flat_array[ $key ] = $value;
+				$this->_set_label_size( $value );
+			}
+			return is_array( $arr ) ? $arr : array( $arr );
+		}
+		return $flat_array;
+	}
+
+
+
+	/**
+	 * @param EE_Question_Option[] $question_options_array
+	 * @return array
+	 */
+	protected function _process_question_options( $question_options_array = array() ) {
+		$flat_array = array();
+		foreach( $question_options_array as $question_option ) {
+			if ( $question_option instanceof EE_Question_Option ) {
+				$value = $this->_use_desc_in_label ? $question_option->value() . '<span class="ee-question-option-desc"> - ' . $question_option->desc() . '</span>' : $question_option->value();
+				$flat_array[ $question_option->value() ] = $value;
+				$this->_use_desc_in_label ? $this->_set_label_size( $question_option->value() . '123456' . $question_option->desc() ) : $this->_set_label_size( $value );
+			} elseif ( is_array( $question_option )) {
+				$non_question_option = $this->_flatten_select_options( $question_option );
+				$flat_array = $flat_array + $non_question_option;
+			}
+		}
+		return $flat_array;
+	}
+
+
+
+	/**
+	 *    _set_label_size_class
+	 * @param int|string $value
+	 * @return void
+	 */
+	private function _set_label_size( $value = '' ){
+		// did the input settings specifically say to NOT set the label size dynamically ?
+		if ( ! $this->_enforce_label_size ) {
+			// determine length of option value
+			$val_size = is_int( $value ) ? $value : strlen( $value );
+			// use new value if bigger than existing
+			$this->_label_size = $val_size > $this->_label_size ? $val_size : $this->_label_size;
 		}
 	}
-}
 
+
+
+	/**
+	 * 	get_label_size_class
+	 * @return string
+	 */
+	function get_label_size_class(){
+		// use maximum option value length to determine label size
+		switch( $this->_label_size ){
+			case $this->_label_size < 3 :
+				$size =  ' nano-lbl';
+				break;
+			case $this->_label_size < 6 :
+				$size =  ' micro-lbl';
+				break;
+			case $this->_label_size < 12 :
+				$size =  ' tiny-lbl';
+				break;
+			case $this->_label_size < 25 :
+				$size =  ' small-lbl';
+				break;
+			case $this->_label_size < 50 :
+				$size =  ' medium-lbl';
+				break;
+			case $this->_label_size >= 100 :
+				$size =  ' big-lbl';
+				break;
+			default:
+				$size =  ' medium-lbl';
+				break;
+		}
+		return $size;
+	}
+
+
+
+}
 // End of file EE_Form_Input_With_Options_Base.input.php
