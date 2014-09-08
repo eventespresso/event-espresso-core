7343e2811cbaafac5e8923bf1a1061f2b0aa654e
Michael Nelson <michael@eventespresso.com>
2014-09-08 14:56:36 -0700

    [touch:6603]{t:30}corrected order of required vs optional args in checkbox constructor

--- core/libraries/form_sections/inputs/EE_Checkbox_Multi_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Checkbox_Multi_Input.input.php
@@ -17,7 +17,7 @@ class EE_Checkbox_Multi_Input extends EE_Form_Input_With_Options_Base{
 	 * @param array $input_settings
 	 * @param array | EE_Question_Option[] $answer_options
 	 */
-	public function __construct( $answer_options = array(), $input_settings ) {
+	public function __construct( $answer_options, $input_settings = array() ) {
 		$this->_set_display_strategy( new EE_Checkbox_Display_Strategy() );
 		$this->_add_validation_strategy( new EE_Many_Valued_Validation_Strategy( array( new EE_Enum_Validation_Strategy() )));
 		$this->_multiple_selections = TRUE;
