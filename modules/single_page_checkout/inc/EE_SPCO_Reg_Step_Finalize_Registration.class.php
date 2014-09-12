c7f9f472b753fd78bb01b62dea0aea923917c449
Brent Christensen <brent@eventespresso.com>
2014-09-12 09:13:05 -0700

    remove unused code [touch:6588]

--- modules/single_page_checkout/inc/EE_SPCO_Reg_Step_Finalize_Registration.class.php
+++ modules/single_page_checkout/inc/EE_SPCO_Reg_Step_Finalize_Registration.class.php
@@ -106,49 +106,8 @@ class EE_SPCO_Reg_Step_Finalize_Registration extends EE_SPCO_Reg_Step {
 	 * @return boolean
 	 */
 	public function update_reg_step() {
-		echo '<br/><h5 style="color:#2EA2CC;">' . __CLASS__ . '<span style="font-weight:normal;color:#0074A2"> -> </span>' . __FUNCTION__ . '() <br/><span style="font-size:9px;font-weight:normal;color:#666">' . __FILE__ . '</span>    <b style="font-size:10px;color:#333">  ' . __LINE__ . ' </b></h5>';
-		die();
+		EE_Error::doing_it_wrong( __CLASS__ . '::' . __FILE__, __( 'Can not call update_reg_step() on the Finalize Registration reg step.', 'event_espresso'), '4.6.0' );
 	 }
-
-
-
-
-	/**
-	 * This processes the registration form from the admin and returns either the true or false depending on the success of the process.
-	 *
-	 * Note that this method handles not only validating the registration form but also saving to the database all the data in the session.
-	 *
-	 * @access  public
-	 * @return mixed bool|int (either false on fail OR TXN id on success)
-	 */
-	public function process_registration_from_admin() {
-		echo '<br/><h5 style="color:#2EA2CC;">' . __CLASS__ . '<span style="font-weight:normal;color:#0074A2"> -> </span>' . __FUNCTION__ . '() <br/><span style="font-size:9px;font-weight:normal;color:#666">' . __FILE__ . '</span>    <b style="font-size:10px;color:#333">  ' . __LINE__ . ' </b></h5>';
-		die();
-//		//nonce check was done in admin so no need to do here.
-//		//first lets validate the registration form
-//		$this->init_for_admin();
-//		//if failure in processing attendee info then let's get out early
-//		if ( ! $this->_process_attendee_information() ) {
-//			return FALSE;
-//		}
-//		// same deal when saving everything
-//		if ( ! $this->_save_all_registration_information() ) {
-//			return FALSE;
-//		}
-//		//all is good so let's continue with finalizing the registration.
-//		EE_Registry::instance()->SSN->set_session_data( array( 'transaction', NULL ));
-//		$this->_transaction->set_txn_session_data( EE_Registry::instance()->SSN->get_session_data() );
-//		$this->checkout->cart->get_grand_total()->save_this_and_descendants_to_txn( $this->_transaction->ID() );
-//		//is this free event?
-//		if ( $this->checkout->cart->get_grand_total()->total() == EEH_Template::format_currency( 0, TRUE ) ) {
-//			$this->_transaction->set_status( EEM_Transaction::complete_status_code );
-//		} else {
-//			$this->_transaction->set_status( EEM_Transaction::incomplete_status_code );
-//		}
-//		$this->_transaction->finalize( TRUE );
-//		EE_Registry::instance()->SSN->clear_session( __CLASS__, __FUNCTION__ );
-//		return $this->_transaction->ID();
-	}
 
 
 
