abee6aca7dfcdcf013f7941bfdfd1a52a66f902a
Michael Nelson <michael@eventespresso.com>
2014-09-16 14:16:06 -0700

    [touch:6691]{t:10}brent rightly pointed out I ought ot just delete the code instead of commenting it out

--- core/EE_System.core.php
+++ core/EE_System.core.php
@@ -409,16 +409,6 @@ final class EE_System {
 	public function detect_if_activation_or_upgrade() {
 		do_action('AHEE__EE_System___detect_if_activation_or_upgrade__begin');
 
-		//this filter is present to make it easier to bypass the admin/user check here so we can setup the db when running tests.
-//		$testsbypass = apply_filters( 'FHEE__EE_System__detect_if_activation_or_upgrade__testsbypass', FALSE );
-//
-//		if ( !$testsbypass &&
-//				( ! is_admin() ||
-//				( isset( $GLOBALS['pagenow'] ) && in_array( $GLOBALS['pagenow'], array( 'wp-login.php', 'wp-register.php' ))) ||
-//				( is_admin() && defined('DOING_AJAX') && DOING_AJAX  ) ) ) {
-//
-//			return;
-//		}
 		// load M-Mode class
 		EE_Registry::instance()->load_core( 'Maintenance_Mode' );
 		// check if db has been updated, or if its a brand-new installation
