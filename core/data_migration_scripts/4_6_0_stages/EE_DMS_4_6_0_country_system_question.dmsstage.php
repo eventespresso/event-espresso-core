8c1aaaf2152a692af7c5dbf9b56ed71bc685af3e
Brent Christensen <brent@eventespresso.com>
2014-09-17 16:51:26 -0700

    abstract properties and method names so that it can easily be copied over an dused for other DMSs [touch:6714]

--- core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_country_system_question.dmsstage.php
+++ core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_country_system_question.dmsstage.php
@@ -15,8 +15,10 @@ if (!defined('EVENT_ESPRESSO_VERSION')) {
  */
 class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stage{
 
-	protected $_question_table_name;
-	protected $_question_system_name;
+	protected $_table_name;
+	protected $_field_name;
+	protected $_old_field_value;
+	protected $_new_field_value;
 
 
 
@@ -28,8 +30,10 @@ class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stag
 	public function __construct() {
 		global $wpdb;
 		$this->_pretty_name = __( 'Country - System Question', 'event_espresso' );
-		$this->_question_table_name = $wpdb->prefix.'esp_question';
-		$this->_question_system_name = 'COUNTRY';
+		$this->_table_name = $wpdb->prefix.'esp_question';
+		$this->_field_name = 'QST_system';
+		$this->_old_field_value = 'country';
+		$this->_new_field_value = 'COUNTRY';
 		parent::__construct();
 	}
 
@@ -39,27 +43,27 @@ class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stag
 	 * @return int
 	 */
 	protected function _count_records_to_migrate() {
-		$questions_that_require_conversion = $this->_get_all_questions_that_require_conversion();
+		$questions_that_require_conversion = $this->_get_all_records_that_require_conversion();
 		$count = count( $questions_that_require_conversion );
 		return $count;
 	}
 
 
 	/**
-	 * _get_all_questions_that_require_conversion
+	 * _get_all_records_that_require_conversion
 	 *
 	 * @param int $limit
 	 * @param int $offset
 	 * @return array
 	 */
-	private function _get_all_questions_that_require_conversion( $limit = 0, $offset = 0 ){
+	private function _get_all_records_that_require_conversion( $limit = 0, $offset = 0 ){
 		global $wpdb;
-		$SQL = "SELECT * FROM %s WHERE QST_system = %s";
+		$SQL = "SELECT * FROM %s WHERE %s = %s";
 		if ( $limit ) {
 			$SQL .= "LIMIT %d OFFSET %d";
-			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_question_table_name, 'country', $limit, $offset ));
+			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_table_name, $this->_field_name, $this->_old_field_value, $limit, $offset ));
 		} else {
-			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_question_table_name, 'country' ));
+			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_table_name, $this->_field_name, $this->_old_field_value ));
 		}
 		return $questions;
 	}
@@ -73,10 +77,10 @@ class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stag
 	 */
 	protected function _migration_step( $num_items_to_migrate = 50 ) {
 		$items_actually_migrated = 0;
-		$questions_to_convert_this_step = $this->_get_all_questions_that_require_conversion( $this->count_records_migrated(), $num_items_to_migrate );
+		$questions_to_convert_this_step = $this->_get_all_records_that_require_conversion( $this->count_records_migrated(), $num_items_to_migrate );
 		foreach( $questions_to_convert_this_step as $question ){
 			if ( $question->QST_ID) {
-				$this->update_question_type( $question->QST_ID, $this->_question_system_name );
+				$this->update_question_type( $question->QST_ID, $this->_new_field_value );
 			}
 			$items_actually_migrated++;
 		}
@@ -91,28 +95,30 @@ class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stag
 	/**
 	 * updates the question with the new question type
 	 * @param int $QST_ID
-	 * @param string $question_system_name
+	 * @param string $new_field_value
 	 */
-	private function update_question_type( $QST_ID, $question_system_name ){
+	private function update_question_type( $QST_ID, $new_field_value ){
 		global $wpdb;
 		$success = $wpdb->update(
-			$this->_question_table_name,
-			array( 'QST_type' => $question_system_name ),
-			array( 'QST_ID' => $QST_ID ),
-			array( '%s' ), //CUR_code
-			array( '%d' ) //CUR_code
+			$this->_table_name,
+			array( $this->_field_name => $new_field_value ),  // data
+			array( 'QST_ID' => $QST_ID ),  // where
+			array( '%s' ),   // data format
+			array( '%d' )  // where format
 		);
 		if ( ! $success ) {
 			$this->add_error(
 				sprintf(
 					__( 'Could not update question system name "%1$s" for question ID=%2$d because "%3$s"', 'event_espresso' ),
-					json_encode( $question_system_name ),
+					json_encode( $new_field_value ),
 					$QST_ID,
 					$wpdb->last_error
 				)
 			);
 		}
 	}
-}
 
+
+
+}
 // End of file EE_DMS_4_6_0_question_types.dmsstage.php
