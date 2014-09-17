ac615019deb43f9f3f859e91f268a33f6cf80b42
Brent Christensen <brent@eventespresso.com>
2014-09-17 16:49:08 -0700

    add EE_DMS_4_6_0_country_system_question() and EE_DMS_4_6_0_state_system_question() to list of migration stages [touch:6714]

--- core/data_migration_scripts/EE_DMS_Core_4_6_0.dms.php
+++ core/data_migration_scripts/EE_DMS_Core_4_6_0.dms.php
@@ -29,7 +29,9 @@ class EE_DMS_Core_4_6_0 extends EE_Data_Migration_Script_Base{
 		$this->_migration_stages = array(
 			new EE_DMS_4_6_0_gateways(),
 			new EE_DMS_4_6_0_payment_method_currencies(),
-			new EE_DMS_4_6_0_question_types()
+			new EE_DMS_4_6_0_question_types(),
+			new EE_DMS_4_6_0_country_system_question(),
+			new EE_DMS_4_6_0_state_system_question()
 		);
 		parent::__construct();
 	}
