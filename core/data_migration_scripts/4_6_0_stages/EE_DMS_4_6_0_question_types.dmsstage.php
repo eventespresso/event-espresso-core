02a70c1af0d8fd8e4a081c28887c652705406d2e
Brent Christensen <brent@eventespresso.com>
2014-09-17 21:43:17 -0700

    totally refactor DMS to extend EE_Data_Migration_Script_Stage_Table [touch:6712]

--- core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_question_types.dmsstage.php
+++ core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_question_types.dmsstage.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined('EVENT_ESPRESSO_VERSION')) {
+if ( ! defined('EVENT_ESPRESSO_VERSION')) {
 	exit('No direct script access allowed');
 }
 
@@ -13,10 +13,9 @@ if (!defined('EVENT_ESPRESSO_VERSION')) {
  * @author				Brent Christensen
  *
  */
-class EE_DMS_4_6_0_question_types extends EE_Data_Migration_Script_Stage{
+class EE_DMS_4_6_0_question_types extends EE_Data_Migration_Script_Stage_Table {
 
-	protected $_question_table_name;
-	protected $_question_types = array();
+	protected $_question_type_conversions = array();
 
 
 
@@ -28,7 +27,7 @@ class EE_DMS_4_6_0_question_types extends EE_Data_Migration_Script_Stage{
 	public function __construct() {
 		global $wpdb;
 		$this->_pretty_name = __( 'Question Types', 'event_espresso' );
-		$this->_question_table_name = $wpdb->prefix.'esp_question';
+		$this->_old_table = $wpdb->prefix.'esp_question';
 		$this->_question_type_conversions = array(
 			'MULTIPLE' 			=> 'CHECKBOX',
 			'SINGLE' 				=> 'RADIO_BTN'
@@ -37,84 +36,29 @@ class EE_DMS_4_6_0_question_types extends EE_Data_Migration_Script_Stage{
 	}
 
 	/**
-	 * _count_records_to_migrate
-	 *
-	 * @return int
+	 * @param array $question an associative array where keys are column names and values are their values.
+	 * @return null
 	 */
-	protected function _count_records_to_migrate() {
-		$questions_that_require_conversion = $this->_get_all_questions_that_require_conversion();
-		$count = count( $questions_that_require_conversion );
-		return $count;
-	}
-
-
-	/**
-	 * _get_all_questions_that_require_conversion
-	 *
-	 * @param int $limit
-	 * @param int $offset
-	 * @return array
-	 */
-	private function _get_all_questions_that_require_conversion( $limit = 0, $offset = 0 ){
+	protected function _migrate_old_row( $question ) {
 		global $wpdb;
-		$question_types = implode( "','", array_keys( $this->_question_type_conversions ));
-		$SQL = "SELECT * FROM %s WHERE QST_type IN ('$question_types')";
-		if ( $limit ) {
-			$SQL .= "LIMIT %d OFFSET %d";
-			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_question_table_name, $limit, $offset ));
-		} else {
-			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_question_table_name ));
-		}
-		return $questions;
-	}
-
-
-	/**
-	 * _migration_step
-	 *
-	 * @param int $num_items_to_migrate
-	 * @return int number of items ACTUALLY migrated
-	 */
-	protected function _migration_step( $num_items_to_migrate = 50 ) {
-		$items_actually_migrated = 0;
-		$questions_to_convert_this_step = $this->_get_all_questions_that_require_conversion( $this->count_records_migrated(), $num_items_to_migrate );
-		foreach( $questions_to_convert_this_step as $question ){
-			if ( $question->QST_ID && isset( $this->_question_type_conversions[ $question->QST_type ] )) {
-				$this->update_question_type( $question->QST_ID, $this->_question_type_conversions[ $question->QST_type ] );
-			}
-			$items_actually_migrated++;
-		}
-		if ( $this->count_records_migrated() + $items_actually_migrated >= $this->count_records_to_migrate() ) {
-			$this->set_completed();
-		}
-		return $items_actually_migrated;
-	}
-
-
-
-	/**
-	 * updates the question with the new question type
-	 * @param int $QST_ID
-	 * @param string $question_type
-	 */
-	private function update_question_type( $QST_ID, $question_type ){
-		global $wpdb;
-		$success = $wpdb->update(
-			$this->_question_table_name,
-			array( 'QST_type' => $question_type ),
-			array( 'QST_ID' => $QST_ID ),
-			array( '%s' ), //CUR_code
-			array( '%d' ) //CUR_code
-		);
-		if ( ! $success ) {
-			$this->add_error(
-				sprintf(
-					__( 'Could not update question type %1$s for question ID=%2$d because "%3$s"', 'event_espresso' ),
-					json_encode( $question_type ),
-					$QST_ID,
-					$wpdb->last_error
-				)
+		if ( $question['QST_ID'] && isset( $this->_question_type_conversions[ $question['QST_type'] ] )) {
+			$success = $wpdb->update(
+				$this->_old_table,
+				array( 'QST_type' => $this->_question_type_conversions[ $question['QST_type'] ] ),
+				array( 'QST_ID' => $question['QST_ID'] ),
+				array( '%s' ), //CUR_code
+				array( '%d' ) //CUR_code
 			);
+			if ( ! $success ) {
+				$this->add_error(
+					sprintf(
+						__( 'Could not update question type %1$s for question ID=%2$d because "%3$s"', 'event_espresso' ),
+						json_encode( $question['QST_type'] ),
+						$question['QST_ID'],
+						$wpdb->last_error
+					)
+				);
+			}
 		}
 	}
 }
