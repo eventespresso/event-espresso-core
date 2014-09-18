939502b55ea41896e77455f5c2c3ef1d1e105982
Brent Christensen <brent@eventespresso.com>
2014-09-17 21:42:53 -0700

    totally refactor DMS to extend EE_Data_Migration_Script_Stage_Table [touch:6714]

--- core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_country_system_question.dmsstage.php
+++ core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_country_system_question.dmsstage.php
@@ -13,12 +13,7 @@ if (!defined('EVENT_ESPRESSO_VERSION')) {
  * @author				Brent Christensen
  *
  */
-class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stage{
-
-	protected $_table_name;
-	protected $_field_name;
-	protected $_old_field_value;
-	protected $_new_field_value;
+class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stage_Table{
 
 
 
@@ -30,91 +25,37 @@ class EE_DMS_4_6_0_country_system_question extends EE_Data_Migration_Script_Stag
 	public function __construct() {
 		global $wpdb;
 		$this->_pretty_name = __( 'Country - System Question', 'event_espresso' );
-		$this->_table_name = $wpdb->prefix.'esp_question';
-		$this->_field_name = 'QST_system';
-		$this->_old_field_value = 'country';
-		$this->_new_field_value = 'COUNTRY';
+		$this->_old_table = $wpdb->prefix.'esp_question';
 		parent::__construct();
-	}
-
-	/**
-	 * _count_records_to_migrate
-	 *
-	 * @return int
-	 */
-	protected function _count_records_to_migrate() {
-		$questions_that_require_conversion = $this->_get_all_records_that_require_conversion();
-		$count = count( $questions_that_require_conversion );
-		return $count;
-	}
-
-
-	/**
-	 * _get_all_records_that_require_conversion
-	 *
-	 * @param int $limit
-	 * @param int $offset
-	 * @return array
-	 */
-	private function _get_all_records_that_require_conversion( $limit = 0, $offset = 0 ){
-		global $wpdb;
-		$SQL = "SELECT * FROM %s WHERE %s = %s";
-		if ( $limit ) {
-			$SQL .= "LIMIT %d OFFSET %d";
-			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_table_name, $this->_field_name, $this->_old_field_value, $limit, $offset ));
-		} else {
-			$questions = $wpdb->query( $wpdb->prepare( $SQL, $this->_table_name, $this->_field_name, $this->_old_field_value ));
-		}
-		return $questions;
-	}
-
-
-	/**
-	 * _migration_step
-	 *
-	 * @param int $num_items_to_migrate
-	 * @return int number of items ACTUALLY migrated
-	 */
-	protected function _migration_step( $num_items_to_migrate = 50 ) {
-		$items_actually_migrated = 0;
-		$questions_to_convert_this_step = $this->_get_all_records_that_require_conversion( $this->count_records_migrated(), $num_items_to_migrate );
-		foreach( $questions_to_convert_this_step as $question ){
-			if ( $question->QST_ID) {
-				$this->update_question_type( $question->QST_ID, $this->_new_field_value );
-			}
-			$items_actually_migrated++;
-		}
-		if ( $this->count_records_migrated() + $items_actually_migrated >= $this->count_records_to_migrate() ) {
-			$this->set_completed();
-		}
-		return $items_actually_migrated;
 	}
 
 
 
 	/**
 	 * updates the question with the new question type
-	 * @param int $QST_ID
-	 * @param string $new_field_value
+	 * @param array $question an associative array where keys are column names and values are their values.
+	 * @return null
 	 */
-	private function update_question_type( $QST_ID, $new_field_value ){
-		global $wpdb;
-		$success = $wpdb->update(
-			$this->_table_name,
-			array( $this->_field_name => $new_field_value ),  // data
-			array( 'QST_ID' => $QST_ID ),  // where
-			array( '%s' ),   // data format
-			array( '%d' )  // where format
-		);
-		if ( ! $success ) {
-			$this->add_error(
-				sprintf(
-					__( 'Could not update question system name "%1$s" for question ID=%2$d because "%3$s"', 'event_espresso' ),
-					json_encode( $new_field_value ),
-					$QST_ID,
-					$wpdb->last_error
-				)
+	protected function _migrate_old_row( $question ) {
+		if ( $question['QST_ID'] && $question['QST_system'] == 'country' ) {
+			global $wpdb;
+			$success = $wpdb->update(
+				$this->_old_table,
+				array( 'QST_type' => 'COUNTRY' ),  // data
+				array( 'QST_ID' => $question['QST_ID'] ),  // where
+				array( '%s' ),   // data format
+				array( '%d' )  // where format
 			);
+			if ( ! $success ) {
+				$this->add_error(
+					sprintf(
+						__( 'Could not update question system name "%1$s" for question ID=%2$d because "%3$s"', 'event_espresso' ),
+						json_encode( $question['QST_system'] ),
+						$question['QST_ID'],
+						$wpdb->last_error
+					)
+				);
+			}
 		}
 	}
 
