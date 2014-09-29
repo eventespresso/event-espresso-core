e8c6a703cb3545294285af0a503b66dc84d2dac7
Brent Christensen <brent@eventespresso.com>
2014-09-29 09:33:04 -0700

    pass TXN object to payment method type billing_form() method [touch:6780] [touch:6776]

--- modules/single_page_checkout/inc/EE_SPCO_Reg_Step_Payment_Options.class.php
+++ modules/single_page_checkout/inc/EE_SPCO_Reg_Step_Payment_Options.class.php
@@ -529,7 +529,7 @@ class EE_SPCO_Reg_Step_Payment_Options extends EE_SPCO_Reg_Step {
 	 */
 	private function _get_billing_form_for_payment_method( EE_Payment_Method $payment_method, $setup_validation_rules = FALSE ) {
 		// if it exists, get billing form for the selected payment method
-		if( $payment_method->type_obj()->billing_form() instanceof EE_Billing_Info_Form ) {
+		if( $payment_method->type_obj()->billing_form( $this->checkout->transaction ) instanceof EE_Billing_Info_Form ) {
 			if ( $this->_transaction_has_primary_registrant() ) {
 				$payment_method->type_obj()->billing_form()->populate_from_attendee( $this->checkout->primary_attendee_obj );
 			}
