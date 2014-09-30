cc9eb2afc16e6fd72012e4a423777c948e9ddaf5
Darren Ethier <darren@roughsmootheng.in>
2014-09-30 18:53:35 -0400

    set timezone_string on setup and restore on teardown and remove no longer needed (duplicate?) test [touch: 6744]

--- tests/testcases/core/db_models/EEM_Datetime_Test.php
+++ tests/testcases/core/db_models/EEM_Datetime_Test.php
@@ -16,11 +16,17 @@
  */
 class EEM_Datetime_Test extends EE_UnitTestCase {
 
+	public function setUp() {
+		//set timezone string.
+		update_option( 'timezone_string', 'America/Vancouver' );
+		parent::setUp();
+	}
+
 
 	public function tearDown(){
 		//restore the timezone string to the default
 		update_option( 'timezone_string', '' );
-		return parent::tearDown();
+		parent::tearDown();
 	}
 
 	/**
@@ -30,55 +36,6 @@ class EEM_Datetime_Test extends EE_UnitTestCase {
 	 * @group 6744
 	 */
 	public function test_get_datetimes_for_event_ordered_by_DTT_order__different_timezone() {
-		echo "\r\n\r\ntest_get_datetimes_for_event_ordered_by_DTT_order__different_timezone";
-		//make sure WordPress Timezone is set
-		update_option( 'timezone_string', 'America/Vancouver' );
-
-
-		//create an event and datetime
-		$event = $this->factory->event->create();
-
-		//for test we want something today.
-		$start_date = (int) current_time('timestamp');
-		$end_date = (int) current_time('timestamp') + (60*60*2);
-		$dtt = $dtt = $this->factory->datetime->create( array( 'DTT_EVT_start' => $start_date, 'DTT_EVT_end' => $end_date ) );
-		//verify
-		$this->assertInstanceOf( 'EE_Event', $event );
-		$this->assertInstanceOf( 'EE_Datetime', $dtt );
-
-		//attach dtt to event and save
-		$event->_add_relation_to( $dtt, 'Datetime' );
-		$event->save();
-
-		//now let's run the method we're checking.
-		//first NO expiry flag.
-		$datetimes_on_event = EEM_Datetime::instance()->get_datetimes_for_event_ordered_by_DTT_order( $event->ID() );
-
-		//we should have the datetime returned
-		$dtt_checked = reset( $datetimes_on_event );
-		$this->assertInstanceOf( 'EE_Datetime', $dtt_checked );
-		$this->assertEquals( $dtt_checked->ID(), $dtt->ID() );
-
-		//now let's run the method excluding expired dtts
-		$datetimes_on_event = EEM_Datetime::instance()->get_datetimes_for_event_ordered_by_DTT_order( $event->ID(), false );
-		$second_dtt_chk = reset( $datetimes_on_event );
-		//we SHOULD also have the datetime returned because the end TIME is two hours from now.
-		$this->assertInstanceOf( 'EE_Datetime', $second_dtt_chk );
-		$this->assertEquals( $second_dtt_chk->ID(), $dtt->ID() );
-	}
-
-	/**
-	 * Tests the get_datetimes_for_event_ordered_by_DTT_order method.
-	 * @see https://events.codebasehq.com/projects/event-espresso/tickets/6744 for bug being tested below.
-	 * @since 4.5.0
-	 * @group 6744
-	 */
-	public function test_get_datetimes_for_event_ordered_by_DTT_order__default_timezone() {
-echo "\r\n\r\ntest_get_datetimes_for_event_ordered_by_DTT_order__default_timezone";
-		//make sure WordPress Timezone is set
-//		update_option( 'timezone_string', 'America/Toronto' );
-		$this->assertEquals( '', get_option( 'timezone_string' ) );
-
 		//create an event and datetime
 		$event = $this->factory->event->create();
 
@@ -96,7 +53,6 @@ echo "\r\n\r\ntest_get_datetimes_for_event_ordered_by_DTT_order__default_timezon
 
 		//now let's run the method we're checking.
 		//first NO expiry flag.
-
 		$datetimes_on_event = EEM_Datetime::instance()->get_datetimes_for_event_ordered_by_DTT_order( $event->ID() );
 
 		//we should have the datetime returned
@@ -107,10 +63,10 @@ echo "\r\n\r\ntest_get_datetimes_for_event_ordered_by_DTT_order__default_timezon
 		//now let's run the method excluding expired dtts
 		$datetimes_on_event = EEM_Datetime::instance()->get_datetimes_for_event_ordered_by_DTT_order( $event->ID(), false );
 		$second_dtt_chk = reset( $datetimes_on_event );
+
 		//we SHOULD also have the datetime returned because the end TIME is two hours from now.
 		$this->assertInstanceOf( 'EE_Datetime', $second_dtt_chk );
 		$this->assertEquals( $second_dtt_chk->ID(), $dtt->ID() );
 	}
-
 
 }
