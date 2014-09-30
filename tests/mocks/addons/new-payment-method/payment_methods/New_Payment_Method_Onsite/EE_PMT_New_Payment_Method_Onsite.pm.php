f5755c4438923edaba331a65ab9d0d91ac967752
Brent Christensen <brent@eventespresso.com>
2014-09-30 11:38:12 -0700

    add EE_Transaction object as a param to generate_new_billing_form() method; update phpdocs [touch:6780]

--- tests/mocks/addons/new-payment-method/payment_methods/New_Payment_Method_Onsite/EE_PMT_New_Payment_Method_Onsite.pm.php
+++ tests/mocks/addons/new-payment-method/payment_methods/New_Payment_Method_Onsite/EE_PMT_New_Payment_Method_Onsite.pm.php
@@ -16,13 +16,24 @@ if (!defined('EVENT_ESPRESSO_VERSION')) {
  */
 class EE_PMT_New_Payment_Method_Onsite extends EE_PMT_Base{
 	const help_tab_link = 'ee_new_payment_method_onsite_help';
+
+	/**
+	 *
+	 * @param EE_Payment_Method $pm_instance
+	 * @return EE_PMT_New_Payment_Method_Onsite
+	 */
 	public function __construct($pm_instance = NULL) {
 		require_once($this->file_folder().'EEG_New_Payment_Method_Onsite.gateway.php');
 		$this->_gateway = new EEG_New_Payment_Method_Onsite();
 		$this->_pretty_name = __("New Payment Method Onsite", 'event_espresso');
 		parent::__construct($pm_instance);
 	}
-	public function generate_new_billing_form() {
+
+	/**
+	 * @param \EE_Transaction $transaction
+	 * @return \EE_Billing_Attendee_Info_Form
+	 */
+	public function generate_new_billing_form( EE_Transaction $transaction = NULL ) {
 		$form = new EE_Billing_Attendee_Info_Form($this->_pm_instance,array(
 			'name'=>'New_Payment_Method_Onsite_Form',
 			'subsections'=>array(
@@ -40,6 +51,10 @@ class EE_PMT_New_Payment_Method_Onsite extends EE_PMT_Base{
 		return $form;
 	}
 
+	/**
+	 * Gets the form for all the settings related to this payment method type
+	 * @return EE_Payment_Method_Form
+	 */
 	public function generate_new_settings_form() {
 		EE_Registry::instance()->load_helper('Template');
 		$form = new EE_Payment_Method_Form(array(
