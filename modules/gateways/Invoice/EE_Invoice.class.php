d0ad17385f35714707d6f82c02091d48021fece4
Darren Ethier <darren@roughsmootheng.in>
2014-10-01 14:33:14 -0400

    if pdf_instructions value exists for invoice settings then use that as the default for template_payment_instructions field [touch: 6785]

--- modules/gateways/Invoice/EE_Invoice.class.php
+++ modules/gateways/Invoice/EE_Invoice.class.php
@@ -47,11 +47,14 @@ Class EE_Invoice extends EE_Offline_Gateway {
 
 	protected function _default_settings() {
 		$org_config = EE_Registry::instance()->CFG->organization;
+		$payment_settings = EE_Config::instance()->gateway->payment_settings;
+		$invoice_settings = !empty( $payment_settings['Invoice'] ) ? $payment_settings['Invoice'] : array();
+
 		$this->_payment_settings['active'] = '';
 
 		//dynamic info for templates
 		$this->_payment_settings['invoice_logo_url'] = '';
-		$this->_payment_settings['template_payment_instructions'] = __('Please send this invoice with payment attached to the address above, or use the payment link below. Payment must be received within 48 hours of event date.', 'event_espresso');
+		$this->_payment_settings['template_payment_instructions'] = !empty( $invoice_settings['pdf_instructions'] ) ? $invoice_settings['pdf_instructions'] : __('Please send this invoice with payment attached to the address above, or use the payment link below. Payment must be received within 48 hours of the event date.', 'event_espresso' );
 		$this->_payment_settings['template_invoice_company_name'] = '';
 		$this->_payment_settings['template_invoice_address'] = '';
 
