9b6d4561eaf5ef4b9bb9f5985cbf2b92fb9a7a75
Brent Christensen <brent@eventespresso.com>
2014-10-17 09:17:10 -0700

    Revert "code tidy [touch:6837]"
    This reverts commit a74eefd13466c9114e728753e71cf146da87195c.

--- core/libraries/form_sections/strategies/layout/EE_Admin_Two_Column_Layout.strategy.php
+++ core/libraries/form_sections/strategies/layout/EE_Admin_Two_Column_Layout.strategy.php
@@ -12,21 +12,19 @@ class EE_Admin_Two_Column_Layout extends EE_Two_Column_Layout{
 	 * @return string
 	 */
 	public function layout_input( $input ) {
-		if(
-			$input->get_display_strategy() instanceof EE_Checkbox_Display_Strategy
-			|| $input->get_display_strategy() instanceof EE_Hidden_Display_Strategy
-			|| $input->get_display_strategy() instanceof EE_Radio_Button_Display_Strategy
-			|| $input->get_display_strategy() instanceof EE_Submit_Input_Display_Strategy
-		){
+		if( $input->get_display_strategy() instanceof EE_Checkbox_Display_Strategy ||
+				$input->get_display_strategy() instanceof EE_Hidden_Display_Strategy ||
+				$input->get_display_strategy() instanceof EE_Radio_Button_Display_Strategy ||
+				$input->get_display_strategy() instanceof EE_Submit_Input_Display_Strategy
+				){
 			//leave these alone
-		} else if ( $input->get_display_strategy() instanceof EE_Text_Area_Display_Strategy ) {
+		}elseif( $input->get_display_strategy() instanceof EE_Text_Area_Display_Strategy ){
 			$input->set_html_class( 'large-text' );
 			if( $input instanceof EE_Text_Area_Input ){
 				$input->set_rows( 4 );
 				$input->set_cols( 60 );
 			}
-		} else {
-			//standard-like textbox
+		}else{//standard-like textbox
 			$input->set_html_class( 'large-text' );
 		}
 		return parent::layout_input( $input );
