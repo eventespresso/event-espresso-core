9fe9ee49ff03f38ad08b77fdfedbf022a5d8a0a3
Brent Christensen <brent@eventespresso.com>
2014-11-19 16:19:15 -0800

    display [LINE_ITEM_QUANTITY] shortcode in subline item rows [touch:7077]

--- core/libraries/messages/defaults/default/html_invoice_price_modifier_line_item_list.template.php
+++ core/libraries/messages/defaults/default/html_invoice_price_modifier_line_item_list.template.php
@@ -6,7 +6,7 @@
 <tr class="subitem-row">
 	<td class="item_I subitem">[LINE_ITEM_NAME]</td>
 	<td class="item_I">[LINE_ITEM_DESCRIPTION]</td>
-	<td class="item_c"></td>
-	<td class="item_c">[LINE_ITEM_AMOUNT]</td>
+	<td class="item_r">[LINE_ITEM_QUANTITY]</td>
+	<td class="item_r">[LINE_ITEM_AMOUNT]</td>
 	<td class="item_r">[LINE_ITEM_TOTAL]</td>
 </tr>
