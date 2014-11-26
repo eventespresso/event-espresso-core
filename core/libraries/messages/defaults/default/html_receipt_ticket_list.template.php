0c6dc970a33155c1e271abfb10b8a654d01a0bef
Brent Christensen <brent@eventespresso.com>
2014-11-26 11:47:52 -0800

    remove line breaks and html comments because they are somehow getting wrapped with "p" tags; [touch:7021]

--- core/libraries/messages/defaults/default/html_receipt_ticket_list.template.php
+++ core/libraries/messages/defaults/default/html_receipt_ticket_list.template.php
@@ -15,18 +15,14 @@
 					<th class="number-column item_r"><?php _e("Total", "event_espresso");?></th>
 				</tr>
 			</thead>
-			<tbody>
-			[TICKET_LINE_ITEM_LIST]
-			</tbody>
-		</table> <!-- end #invoice-amount -->
-	</div> <!-- end .ticket-details -->
+			<tbody>[TICKET_LINE_ITEM_LIST]</tbody>
+		</table>
+	</div>
 	<div class="reg-details-for-ticket">
 		<div class="ticket-time-and-place-details">
 			<div class="ticket-time-details">
 				<h4 class="sub-section-title no-bottom-margin"><img class="icon" src="<?php echo EE_IMAGES_URL.'clock-16x16.png';?>"><?php _e('Date/Time:', 'event_espresso'); ?></h4>
-				<ul class="event-dates">
-					[DATETIME_LIST]
-				</ul> <!-- end .event-dates -->
+				<ul class="event-dates">[DATETIME_LIST]</ul>
 			</div>
 			<div class="ticket-place-details">
 				<h4 class="sub-section-title no-bottom-margin"><img class="icon" src="<?php echo EE_IMAGES_URL.'location-pin-16x16.png';?>"><?php _e('Venue', 'event-espresso'); ?></h4>
@@ -34,14 +30,10 @@
 					<li>[VENUE_TITLE] <span class="small-text">( <a href="[VENUE_URL]"><?php _e('view', 'event_espresso'); ?></a> )</span></li>
 				</ul>
 			</div>
-		</div> <!-- end .ticket-time-and-place-details -->
+		</div>
 		<div class="ticket-registrations-area">
 			<h4 class="sub-section-title"><img class="icon" src="<?php echo EE_IMAGES_URL.'users-16x16.png';?>"><?php  _e("Registration Details", "event_espresso");?> <span class="small-text link">( <a class="print_button noPrint" href="[PRIMARY_REGISTRANT_FRONTEND_EDIT_REG_LINK]"><?php _e('edit', 'event_espresso'); ?></a> )</span></h4>
-			<ul class="ticket-registrations-list">
-				[ATTENDEE_LIST]
-			</ul> <!-- end .ticket-registrations-list -->
-		</div> <!-- end .ticket-registrations-area -->
-	</div> <!-- end .reg-details-for-ticket -->
-</li> <!-- end .event-ticket -->
-
-
+			<ul class="ticket-registrations-list">[ATTENDEE_LIST]</ul>
+		</div>
+	</div>
+</li>
