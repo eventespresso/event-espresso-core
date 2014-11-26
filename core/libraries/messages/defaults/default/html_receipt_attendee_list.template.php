ef55ff49a9eebc886282f4a287d3244f60948f17
Brent Christensen <brent@eventespresso.com>
2014-11-26 11:48:13 -0800

    remove line breaks and html comments because they are somehow getting wrapped with "p" tags; [touch:7021]

--- core/libraries/messages/defaults/default/html_receipt_attendee_list.template.php
+++ core/libraries/messages/defaults/default/html_receipt_attendee_list.template.php
@@ -6,14 +6,16 @@
 <li class="ticket-registration">
 	<table class="registration-details">
 		<tr class="odd">
-			<th><?php _e("Registration Code:", "event_espresso");?></th>
-			<td>[REGISTRATION_CODE] - <span class="[REGISTRATION_STATUS_ID]">[REGISTRATION_STATUS_LABEL]</span></td>
-		</tr>
-		<tr><th><?php __('Custom Questions and Answers:', 'event_espresso'); ?></th><td></td></tr>
-		[QUESTION_LIST]
-		<tr>
 			<th><?php	_e('Attendee', 'event_espresso');?></th>
 			<td>[FNAME] [LNAME] ([ATTENDEE_EMAIL])</td>
 		</tr>
-	</table> <!-- end .registration-details -->
-</li> <!-- end .ticket-registration -->
+		<tr>
+			<th><?php _e("Registration Code:", "event_espresso");?></th>
+			<td>[REGISTRATION_CODE] - <span class="[REGISTRATION_STATUS_ID]">[REGISTRATION_STATUS_LABEL]</span></td>
+		</tr>
+		<tr>
+			<th><?php _e('Custom Questions and Answers:', 'event_espresso'); ?></th>
+			<td>[QUESTION_LIST]</td>
+		</tr>
+	</table>
+</li>
