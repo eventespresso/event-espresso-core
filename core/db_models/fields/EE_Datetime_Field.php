ea02aa508fc1404d474b4b2431a08145887c93cd
Michael Nelson <michael@eventespresso.com>
2014-11-27 14:13:53 -0800

    [touch:7115]{t:90}switch order of formatting datetimes. First try to format as an excel date. If not an exact match, parse it normally (and if that doesnt work, again use a default date)

--- core/db_models/fields/EE_Datetime_Field.php
+++ core/db_models/fields/EE_Datetime_Field.php
@@ -357,7 +357,7 @@ class EE_Datetime_Field extends EE_Model_Field_Base {
 		$datetime = date( 'Y-m-d H:i:s', (int)$datetime_value );
 		$this->_set_date_obj( $datetime, 'UTC' );
 		if ( ! $this->_date instanceof DateTime ) {
-			throw new EE_Error( __('Something went wrong with setting the date/time. Likely, either there is an invalid datetime string or an invalid timezone string being used.', 'event_espresso' ) );
+			throw new EE_Error( sprintf( __('Something went wrong with setting the date/time. Likely, either there is an invalid datetime string or an invalid timezone string being used on %s', 'event_espresso' ) , $this->get_model_name() ) );
 		}
 		$this->_date->setTimezone( new DateTimeZone( $this->_timezone ) );
 		return $this->_date->format( $format );
@@ -609,17 +609,19 @@ class EE_Datetime_Field extends EE_Model_Field_Base {
 			return;
 		}
 		try {
-			$this->_date = new DateTime( $date_string, new DateTimeZone( $timezone ));
-		} catch( Exception $e ) {
-			//probably a badly formatted date string
+			//start off assuming it's a wonky excel format
+			if ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 ) {
+				// maybe it's the Microsoft excel format '16/08/2013 8:58' ?
+				$this->_date = DateTime::createFromFormat( 'd/m/Y H:i', $date_string, new DateTimeZone( $timezone ));
+			} else {
+				//change 'd/m/Y H:i'  to 'd-m-Y H:i'  because of how strtotime() interprets date formats. see: http://www.php.net/manual/en/datetime.formats.date.php
+				$this->_date = new DateTime( date( 'd-m-Y H:i', strtotime( $date_string )), new DateTimeZone( $timezone ));
+			}
+		} catch( Exception $e ) {}
+
+		if( ! $this->_date ){
 			try {
-				if ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 ) {
-					// maybe it's the Microsoft excel format '16/08/2013 8:58' ?
-					$this->_date = DateTime::createFromFormat( 'd/m/Y H:i', $date_string, new DateTimeZone( $timezone ));
-				} else {
-					//change 'd/m/Y H:i'  to 'd-m-Y H:i'  because of how strtotime() interprets date formats. see: http://www.php.net/manual/en/datetime.formats.date.php
-					$this->_date = new DateTime( date( 'd-m-Y H:i', strtotime( $date_string )), new DateTimeZone( $timezone ));
-				}
+				$this->_date = new DateTime( $date_string, new DateTimeZone( $timezone ));
 			} catch( Exception $e ) {
 				// because DateTime chokes on some formats, check if strtotime fails, and throw error regarding bad format
 				if ( strtotime( $date_string ) == 0 ) {
