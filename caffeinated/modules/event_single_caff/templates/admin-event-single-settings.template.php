d52e43e9dfa55f669c8ff972e6788239edffab5b
Jon Ang <jon@wingzcommunications.com>
2014-12-04 17:18:17 +0800

    fixed tabbing

--- caffeinated/modules/event_single_caff/templates/admin-event-single-settings.template.php
+++ caffeinated/modules/event_single_caff/templates/admin-event-single-settings.template.php
@@ -36,17 +36,17 @@ $values = array(
 				</td>
 			</tr>
 			
-			 <tr>
-                <th>
-                    <label for="display_tickets_sold">
-                        <?php _e('Display Tickets Sold', 'event_espresso'); ?><?php echo EEH_Template::get_help_tab_link('display_tickets_sold_info');?>
-                    </label>
-                </th>
-                <td>
-                    <?php echo EEH_Form_Fields::select_input( 'display_tickets_sold', $values, $display_tickets_sold, 'id="display_tickets_sold"' ); ?>
-                    <p class="description"><?php _e( 'This option applies to all instances where the ticket selector is displayed', 'event_espresso' ); ?></p>
-                </td>
-            </tr>
+		 	<tr>
+	                	<th>
+		                    <label for="display_tickets_sold">
+		                        <?php _e('Display Tickets Sold', 'event_espresso'); ?><?php echo EEH_Template::get_help_tab_link('display_tickets_sold_info');?>
+		                    </label>
+		                </th>
+		                <td>
+		                    <?php echo EEH_Form_Fields::select_input( 'display_tickets_sold', $values, $display_tickets_sold, 'id="display_tickets_sold"' ); ?>
+		                    <p class="description"><?php _e( 'This option applies to all instances where the ticket selector is displayed', 'event_espresso' ); ?></p>
+		                </td>
+	            	</tr>
 
 		</tbody>
 	</table>
