41d23c0d5f49366b560d31f0bcd505f4db2592b7
Michael Nelson <michael@eventespresso.com>
2014-12-10 09:36:18 -0800

    [touch:7154]{t:20}fixed 4.6 payments dms stages warning when no payment method found for old payment row

--- core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_payments.dmsstage.php
+++ core/data_migration_scripts/4_6_0_stages/EE_DMS_4_6_0_payments.dmsstage.php
@@ -26,7 +26,7 @@ class EE_DMS_4_6_0_payments extends EE_Data_Migration_Script_Stage_Table{
 		//get the payment method's ID
 		$PMD_ID = $this->_get_payment_method_id_by_gateway_name( $payment_row[ 'PAY_gateway' ] );
 		if( ! $PMD_ID ){
-			$this->add_error( sprintf( __( 'Could not find payment method with PMD_type = \'%1$s\' when migrating payment row %2$s', 'event_espresso' ), $payment_row[ 'EXM_value' ], $this->_json_encode( $payment_row ) ) );
+			$this->add_error( sprintf( __( 'Could not find payment method with PMD_type = \'%1$s\' when migrating payment row %2$s', 'event_espresso' ), $payment_row[ 'PAY_gateway' ], $this->_json_encode( $payment_row ) ) );
 			return;
 		}
 		$new_values = array( 'PMD_ID' => $PMD_ID, 'PAY_source' => $payment_row[ 'PAY_method' ] );
