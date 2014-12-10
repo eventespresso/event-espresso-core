677c56992cbbe9f8d81d4e5df848ccfe283b59a2
Sidney Harrell <sidney.harrell@gmail.com>
2014-12-09 23:36:14 -0500

    added extra underscores between fields in filter name. [touch: 6601]
    Signed-off-by: Sidney Harrell <sidney.harrell@gmail.com>

--- payment_methods/Paypal_Standard/EEG_Paypal_Standard.gateway.php
+++ payment_methods/Paypal_Standard/EEG_Paypal_Standard.gateway.php
@@ -125,7 +125,7 @@ class EEG_Paypal_Standard extends EE_Offsite_Gateway {
 		}
 		$redirect_args['no_shipping'] = $this->_shipping_details;
 		
-		$redirect_args = apply_filters( "FHEE_EEG_Paypal_Standard_set_redirection_info_arguments", $redirect_args );
+		$redirect_args = apply_filters( "FHEE__EEG_Paypal_Standard__set_redirection_info__arguments", $redirect_args );
 
 		$payment->set_redirect_url($this->_gateway_url);
 		$payment->set_redirect_args($redirect_args);
