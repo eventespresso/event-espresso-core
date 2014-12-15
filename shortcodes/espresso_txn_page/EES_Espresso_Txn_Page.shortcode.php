51389d4e881fd43e4312a2a752f383b826d3e8b0
Brent Christensen <brent@eventespresso.com>
2014-12-15 12:11:58 -0800

    EE_Payment_Processor::process_ipn() handles updating the TXN; [touch:7162]

--- shortcodes/espresso_txn_page/EES_Espresso_Txn_Page.shortcode.php
+++ shortcodes/espresso_txn_page/EES_Espresso_Txn_Page.shortcode.php
@@ -88,10 +88,6 @@ class EES_Espresso_Txn_Page  extends EES_Shortcode {
 			/** @type EE_Payment_Processor $payment_processor */
 			$payment_processor = EE_Registry::instance()->load_core('Payment_Processor');
 			$payment_processor->process_ipn( $_REQUEST, $this->_current_txn, $payment_method_slug );
-			/** @type EE_Transaction_Processor $transaction_processor */
-			$transaction_processor = EE_Registry::instance()->load_class( 'Transaction_Processor' );
-			// update the TXN if payment conditions have changed
-			$transaction_processor->update_transaction_and_registrations_after_checkout_or_payment( $this->_current_txn );
 		}
 
 	}
