8a5ceb92f47f1e243e84bab1001c1f7af0febd95
Darren Ethier <darren@roughsmootheng.in>
2014-12-17 14:23:40 -0500

    only show ONE event per matching conditions (not multiple copies of the same event matching conditons) [touch: 7183]

--- core/helpers/EEH_Event_Query.helper.php
+++ core/helpers/EEH_Event_Query.helper.php
@@ -99,6 +99,7 @@ class EEH_Event_Query {
 			add_filter( 'posts_join', array( 'EEH_Event_Query', 'posts_join' ), 10, 2 );
 			add_filter( 'posts_where', array( 'EEH_Event_Query', 'posts_where' ), 10, 2 );
 			add_filter( 'posts_orderby', array( 'EEH_Event_Query', 'posts_orderby' ), 10, 2 );
+			add_filter( 'posts_clauses_request', array( __CLASS__, 'posts_clauses' ), 10, 2 );
 		}
 	}
 
@@ -199,6 +200,25 @@ class EEH_Event_Query {
 	private static function _sort( $sort = 'ASC' ) {
 		$sort = EE_Registry::instance()->REQ->is_set( 'event_query_sort' ) ? sanitize_text_field( EE_Registry::instance()->REQ->get( 'event_query_sort' ) ) : $sort;
 		return in_array( $sort, array( 'ASC', 'asc', 'DESC', 'desc' )) ? strtoupper( $sort ) : 'ASC';
+	}
+
+
+
+	/**
+	 * Filters the clauses for the WP_Query object
+	 *
+	 * @param array   $clauses  array of clauses
+	 * @param WP_Query $wp_query
+	 *
+	 * @return array   array of clauses
+	 */
+	public static function posts_clauses( $clauses, WP_Query $wp_query ) {
+		global $wpdb;
+		if ( isset( $wp_query->query['post_type'] ) && $wp_query->query['post_type'] == 'espresso_events' ) {
+			$clauses['distinct'] = "DISTINCT";
+		}
+		d($clauses);
+		return $clauses;
 	}
 
 
