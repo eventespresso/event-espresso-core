1fadb6404d7aff84a9cafd641d9327dc94822002
Brent Christensen <brent@eventespresso.com>
2014-12-17 22:55:27 -0800

    add invalid_payment_method error message to EE_Registry::$i18n_js_strings; [touch:7171]

--- modules/single_page_checkout/EED_Single_Page_Checkout.module.php
+++ modules/single_page_checkout/EED_Single_Page_Checkout.module.php
@@ -862,8 +862,9 @@ class EED_Single_Page_Checkout  extends EED_Module {
 	public function translate_js_strings() {
 		EE_Registry::$i18n_js_strings['revisit'] = $this->checkout->revisit;
 		EE_Registry::$i18n_js_strings['e_reg_url_link'] = $this->checkout->reg_url_link;
-		EE_Registry::$i18n_js_strings['server_error'] = __('An unknown error occurred on the server while attempting to process your request. Please refresh the page and try again.', 'event_espresso');
+		EE_Registry::$i18n_js_strings['server_error'] = __('An unknown error occurred on the server while attempting to process your request. Please refresh the page and try again or contact support.', 'event_espresso');
 		EE_Registry::$i18n_js_strings['validation_error'] = __( 'There appears to be a problem with the form validation configuration! Please check the admin settings or contact support.', 'event_espresso' );
+		EE_Registry::$i18n_js_strings['invalid_payment_method'] = __( 'There appears to be a problem with the payment method configuration! Please refresh the page and try again or contact support.', 'event_espresso' );
 		EE_Registry::$i18n_js_strings['reg_step_error'] = __('This registration step could not be completed. Please refresh the page and try again.', 'event_espresso');
 		EE_Registry::$i18n_js_strings['invalid_coupon'] = __('We\'re sorry but that coupon code does not appear to be valid. If this is incorrect, please contact the site administrator.', 'event_espresso');
 		EE_Registry::$i18n_js_strings['process_registration'] = sprintf( __( 'Please wait while we process your registration.%sDo not refresh the page or navigate away while this is happening.%sThank you for your patience.', 'event_espresso' ), '<br/>', '<br/>' );
