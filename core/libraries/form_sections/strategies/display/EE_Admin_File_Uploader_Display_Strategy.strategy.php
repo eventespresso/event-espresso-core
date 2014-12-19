27044a75e7ac3804fb1bd4d67aa2347152036333
Brent Christensen <brent@eventespresso.com>
2014-12-19 15:05:33 -0800

    only show image if it exists; code formatting; [touch:6933]

--- core/libraries/form_sections/strategies/display/EE_Admin_File_Uploader_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Admin_File_Uploader_Display_Strategy.strategy.php
@@ -7,27 +7,27 @@ class EE_Admin_File_Uploader_Display_Strategy extends EE_Display_Strategy_Base{
 
 	function display(){
 
+		EE_Registry::instance()->load_helper( 'HTML' );
 		add_action('AHEE__EE_Form_Section_Proper__localize_script_for_all_forms__begin',array('EE_Admin_File_Uploader_Display_Strategy','enqueue_scripts'));
-		$input = $this->_input;
+		// the actual input
+		$input = '<input type="text" size="34" ';
+		$input .= 'name="' . $this->_input->html_name() . '" ';
+		$input .= $this->_input->html_class() != '' ? 'class="regular-text ee_media_url ' . $this->_input->html_class() . '" ' : 'class="regular-text ee_media_url" ';
+		$input .= 'value="' . $this->_input->raw_value_in_form() . '">';
+		// image uploader
+		$uploader = EEH_HTML::link( '#', '<img src="' . admin_url( 'images/media-button-image.gif' ) . '" >', __( 'click to add an image', 'event_espresso' ), '', 'ee_media_upload )' );
 		//only attempt to show the image if it at least exists
-		if( $this->src_exists( $input->raw_value() ) ){
-			$image_src = $input->raw_value();
-		}else{
-			$image_src = '';
-		}
-		return "<span class='ee_media_uploader_area'>".
-					"<input class='regular-text ee_media_url {$input->html_class()}' type='text' name='{$input->html_name()}' size='34' value='{$input->raw_value_in_form()}'>".
-					"&nbsp;<a href='#' class='ee_media_upload'><img src='".admin_url('images/media-button-image.gif')."' alt='Add an Image'></a>".
-					"<img class='ee_media_image' src='$image_src' />".
-				"</span>";
+		$image = $this->src_exists( $this->_input->raw_value() ) ? EEH_HTML::img( $this->_input->raw_value(), __( 'logo', 'event_espresso' ), '', 'ee_media_image' ) : '';
+		// html string
+		return EEH_HTML::span( $input . EEH_HTML::nbsp() . $uploader .  EEH_HTML::nbsp() . $image );
 	}
 	/**
 	 * Its important this media only get enqueued AFTER init, but before the footer... where the
-	 * rest of our forms JS gets enqueued. Otherwise the JS gets enqueud fine, and loaded on the page fine,
-	 * but when you upload an image it gets uploaded fine to the server, but it doesn't display andr eports an error
+	 * rest of our forms JS gets enqueued. Otherwise the JS gets enqueued fine, and loaded on the page fine,
+	 * but when you upload an image it gets uploaded fine to the server, but it doesn't display and reports an error
 	 * (also it doesn't show any of the currently existing media in the modal window that pops up when you click the button
 	 * to select media).
-	 * Besides that, no special consideration should be required to make the media uploader apepar, besides having
+	 * Besides that, no special consideration should be required to make the media uploader appear, besides having
 	 * this input displayed.
 	 */
 	static function enqueue_scripts(){
