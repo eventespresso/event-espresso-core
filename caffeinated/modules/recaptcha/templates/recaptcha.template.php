8167f94bef0cca925bcfa076f699a6110c207da1
Brent Christensen <brent@eventespresso.com>
2014-12-22 14:31:34 -0800

    remove all scripts from template; [touch:7171]

--- caffeinated/modules/recaptcha/templates/recaptcha.template.php
+++ caffeinated/modules/recaptcha/templates/recaptcha.template.php
@@ -6,13 +6,7 @@ if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowe
 /** @type string $recaptcha_type */
 ?>
 <div id="espresso-recaptcha-dv">
-	<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=<?php echo $recaptcha_language; ?>"></script>
-	<script type="text/javascript">
-		var verifyCallback = function(response) {
-			SPCO.additional_post_data = '&g-recaptcha-response=' + response;
-		};
-	</script>
-		<div class="g-recaptcha" data-sitekey="<?php echo $recaptcha_publickey; ?>" data-theme="<?php echo $recaptcha_theme; ?>" data-type="<?php echo $recaptcha_type; ?>" data-callback="verifyCallback"></div>
+		<div class="g-recaptcha" data-sitekey="<?php echo $recaptcha_publickey; ?>" data-theme="<?php echo $recaptcha_theme; ?>" data-type="<?php echo $recaptcha_type; ?>" data-callback="EE_RECAPTCHA.response_callback"></div>
 	<noscript>
 	  <div style="width: 302px; height: 352px;">
 		<div style="width: 302px; height: 352px; position: relative;">
