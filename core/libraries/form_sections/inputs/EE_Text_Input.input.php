cbbb594523ed7cc91b06bfe1cb69cfa7f2b4d0fa
Brent Christensen <brent@eventespresso.com>
2014-12-23 10:34:09 -0800

    add php docs; [touch:7195]

--- core/libraries/form_sections/inputs/EE_Text_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Text_Input.input.php
@@ -1,10 +1,27 @@
-<?php
-
+<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
+/**
+ * Class EE_Text_Input
+ *
+ * Description
+ *
+ * @package            Event Espresso
+ * @subpackage    core
+ * @author                Brent Christensen
+ * @since                $VID:$
+ *
+ */
 class EE_Text_Input extends EE_Form_Input_Base{
-	
+
+
+	/**
+	 * @param array $options
+	 */
 	function __construct($options = array()){
 		$this->_set_display_strategy(new EE_Text_Input_Display_Strategy());
 		$this->_set_normalization_strategy(new EE_Text_Normalization());
 		parent::__construct($options);
 	}
+
+
+
 }
