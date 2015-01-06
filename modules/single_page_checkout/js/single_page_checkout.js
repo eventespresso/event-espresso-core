3062c78a19bba8bc0b88c0df339d29c8964d0bfb
Brent Christensen <brent@eventespresso.com>
2015-01-06 09:22:11 -0800

    k there is no way to get around having to generate the entire reg form, so once again, set this back to 1; [touch:3456]

--- modules/single_page_checkout/js/single_page_checkout.js
+++ modules/single_page_checkout/js/single_page_checkout.js
@@ -799,7 +799,7 @@ jQuery(document).ready( function($) {
 			var form_data = 'step=payment_options';
 			form_data += '&action=spco_billing_form';
 			form_data += '&selected_method_of_payment=' + payment_method;
-			form_data += '&generate_reg_form=0';
+			form_data += '&generate_reg_form=1';
 			form_data += '&process_form_submission=0';
 			form_data += '&noheader=1';
 			form_data += '&ee_front_ajax=1';
