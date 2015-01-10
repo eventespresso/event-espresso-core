bf1ada192684c3c5f31ad38e9003848fccc36f73
Darren Ethier <darren@roughsmootheng.in>
2015-01-10 02:34:59 -0500

    final verification

--- espresso.php
+++ espresso.php
@@ -4,6 +4,7 @@
   Plugin URI:  		http://eventespresso.com/pricing/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
   Description: 		Manage your events from your WordPress dashboard. Reduce your admin, reduce your costs make your life easier! | <a href="admin.php?page=espresso_support&action=contact_support">Support</a>
   Version: 			4.4.9.rc.012
+
   Author: 				Event Espresso
   Author URI: 		http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
   License: 			GPLv2
