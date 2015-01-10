1cf3269707fefd45e9f61e66cd37f047b25b504c
Darren Ethier <darren@roughsmootheng.in>
2015-01-09 22:23:35 -0500

    wozer fix stuff

--- espresso.php
+++ espresso.php
@@ -3,12 +3,8 @@
   Plugin Name:		Event Espresso
   Plugin URI:  		http://eventespresso.com/pricing/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
   Description: 		Manage your events from your WordPress dashboard. Reduce your admin, reduce your costs make your life easier! | <a href="admin.php?page=espresso_support&action=contact_support">Support</a>
-<<<<<<< Updated upstream
-  Version: 		4.6.0.alpha.058
-=======
   Version: 		4.6.0.alpha.054
 
->>>>>>> Stashed changes
   Author: 		Event Espresso
   Author URI: 		http://eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
   License: 		GPLv2
@@ -50,7 +46,7 @@ if ( ! function_exists( 'espresso_version' )) {
 	 * @return string
 	 */
 	function espresso_version() {
-		return '4.6.0.alpha.058';
+		return '4.6.0.alpha.054';
 	}
 } else {
 	unset( $_GET['activate'] );
