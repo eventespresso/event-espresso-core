85dea3d74686d1306feae21a285b5da122646da7
Michael Nelson <michael@eventespresso.com>
2015-02-10 09:54:37 -0800

    [touch:7358]{t:30}verify the payments table still exists when checking for active payment methods. Also, the notice itself needs to check for payment methods on the FRONTEND, not backend too

--- admin_pages/payments/Payments_Admin_Page_Init.core.php
+++ admin_pages/payments/Payments_Admin_Page_Init.core.php
@@ -39,6 +39,7 @@ class Payments_Admin_Page_Init extends EE_Admin_Page_Init {
 		define( 'EE_PAYMENTS_ASSETS_URL', EE_ADMIN_PAGES_URL . 'payments/assets/' );
 
 		//check that there are active gateways on all admin page loads. but dont do it just yet
+//		echo "constrtucrting payments admin page";die;
 		add_action('admin_notices',array($this,'check_payment_gateway_setup'));
 		parent::__construct();
 	}
@@ -66,14 +67,18 @@ class Payments_Admin_Page_Init extends EE_Admin_Page_Init {
 	 */
 	public function check_payment_gateway_setup(){
 		//ONLY do this check if models can query
-		if ( ! EE_Maintenance_Mode::instance()->models_can_query() ) {
+		//and avoid a bug where when we nuke EE4's data that this causes a fatal error
+		//because the tables are deleted just before this request runs. see https://events.codebasehq.com/projects/event-espresso/tickets/7539
+		EE_Registry::instance()->load_helper('Activation');
+		if ( ! EE_Maintenance_Mode::instance()->models_can_query() && EEH_Activation::table_exists( EEM_Payment::instance()->table() ) ) {
+			echo "cant run query";
 			return;
 		}
 
 
 		// ensure Payment_Method model is loaded
 		EE_Registry::instance()->load_model( 'Payment_Method' );
-		$actives = EEM_Payment_Method::instance()->get_all_active();
+		$actives = EEM_Payment_Method::instance()->get_all_active( EEM_Payment_Method::scope_cart );
 		if( ! $actives || count( $actives ) < 1 ){
 			$url = EE_Admin_Page::add_query_args_and_nonce(array(), EE_PAYMENTS_ADMIN_URL);
 			echo '<div class="error">
