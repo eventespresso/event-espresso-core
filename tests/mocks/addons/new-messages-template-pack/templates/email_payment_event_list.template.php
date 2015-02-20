ffc082a8802ec454a094aa4d5826cff6a4e5f0f1
EE DevBox Server <admin@eventespresso.com>
2015-02-20 14:56:53 -0500

    Bumping version to 4.6.11.p and prepped for release

--- tests/mocks/addons/new-messages-template-pack/templates/email_payment_event_list.template.php
+++
@@ -1,6 +0,0 @@
-<!-- Callout Panel -->
-<p class="callout"><strong><?php _e('Event:', 'event_espress'); ?> [EVENT_LINK]</strong></p>
-<!-- /Callout Panel -->
-<div class="content">
-	[TICKET_LIST]
-</div>
