4eb7552aa29526619c4862dd709ac94055d5ce7c
Darren Ethier <darren@roughsmootheng.in>
2015-02-21 08:16:03 -0500

    add more tests for datetimes with tkts and dtts [touch: 7056]

--- tests/testcases/core/admin/pricing/espresso_events_Pricing_Hooks_Test.php
+++ tests/testcases/core/admin/pricing/espresso_events_Pricing_Hooks_Test.php
@@ -56,6 +56,8 @@ class espresso_events_Pricing_Hooks_Test extends EE_UnitTestCase {
 	protected function _load_pricing_mock() {
 		$this->_pricingMock = new espresso_events_Pricing_Hooks_mock();
 		$this->_event = $this->factory->event->create();
+		$this->_event->set_timezone( 'America/Vancouver' );
+		$this->_event->save();
 		$timezone = new DateTimeZone( 'America/Vancouver' );
 		$this->_default_dates = array(
 			'DTT_start' => new DateTime( '2015-02-20 11:30 am', $timezone ),
@@ -146,14 +148,14 @@ class espresso_events_Pricing_Hooks_Test extends EE_UnitTestCase {
 
 
 	/**
-	 * Contains tests for the update_dtts method
+	 * Contains tests for the update_dtts method and update_tkts method.
 	 * @since 4.6
 	 */
-	public function test_update_dtts() {
+	public function test_update_dtts_and_update_tkts() {
 		$this->_load_pricing_mock();
 		$formats_to_test = $this->date_formats_to_test();
 
-		$saved_dtts_for_tickets = array();
+		$saved_dtts_for_tickets = $formats_for_compare = $saved_tkts = array();
 
 		//test each date and time format combination for creating datetime objects
 		foreach( $formats_to_test['date'] as $date_format ) {
@@ -169,9 +171,10 @@ class espresso_events_Pricing_Hooks_Test extends EE_UnitTestCase {
 					//verify start and date
 					$this->assertEquals( $dtt->start_date_and_time(), $this->_default_dates['DTT_start']->format( $full_format ), sprintf( 'Start Date Format Tested: %s', $full_format ) );
 					$this->assertEquals( $dtt->end_date_and_time(), $this->_default_dates['DTT_end']->format( $full_format ), sprintf( 'End Date Format Tested: %s', $full_format ) );
-				}
 
-				$saved_dtts_for_tickets[$full_format] = $dtt;
+					$saved_dtts_for_tickets[$full_format] = $dtt;
+					$formats_for_compare[$dtt->ID()] = array( $date_format, $time_format );
+				}
 			}
 		}
 
@@ -188,13 +191,53 @@ class espresso_events_Pricing_Hooks_Test extends EE_UnitTestCase {
 				$tkts = $this->_pricingMock->update_tkts( $this->_event, $dtt_for_ticket, $data );
 
 				foreach ( $tkts as $tkt ) {
-					$this->assertInstanceof( 'EE_Ticket', $tkt );
+					$this->assertInstanceof( 'EE_Ticket', $tkt, sprintf( 'Format: %s', $full_format ) );
 
 					//verify start and date
 					$this->assertEquals( $tkt->start_date(), $this->_default_dates['TKT_start']->format( $full_format ), sprintf( 'Start Ticket DateFormat Tested: %s', $full_format ) );
 					$this->assertEquals( $tkt->end_date(), $this->_default_dates['TKT_end']->format( $full_format ), sprintf( 'End Ticket Date Format Tested: %s', $full_format ) );
+
+					$saved_tkts[$full_format] = $tkt;
 				}
 			}
+		}
+
+
+
+		//now let's verify these items were saved corectly in the db.
+		/*$new_tkts = $new_dtts = array();
+		foreach ( $tkt as $format => $tkt ) {
+			$new_tkts[$format] = EEM_Ticket::instance()->refresh_entity_map_from_db( $tkt->ID() );
+		}
+
+		foreach ( $saved_dtts_for_tickets[$full_format] as $format => $dtt ) {
+			$new_dtts[$format] = EEM_Datetime::instance()->refresh_entity_map_from_db( $dtt->ID() );
+		}/**/
+
+		EEM_Ticket::reset();
+		EEM_Datetime::reset();
+		EEM_Event::reset();
+
+		$new_event = EEM_Event::instance()->refresh_entity_map_from_db( $this->_event->ID() );
+		$new_event->set_timezone( 'America/Vancouver' );
+
+		$evt_dtts = $new_event->datetimes_ordered();
+
+		//now let's do comparisons.
+		foreach( $evt_dtts as  $edtt ) {
+			$formats = $formats_for_compare[$edtt->ID()];
+			$format = $formats[0] . ' ' . $formats[1];
+			$this->assertEquals( $edtt->start_date_and_time( $formats[0], $formats[1] ), $this->_default_dates['DTT_start']->format( $format), sprintf( 'DB DTT/Default DTT Start Date Format Checked: %s', $format ) );
+			$this->assertEquals( $edtt->end_date_and_time( $formats[0], $formats[1] ), $this->_default_dates['DTT_end']->format( $format ), sprintf( 'DB DTT/Default DTT End Date Format Checked: %s', $format ) );
+			$this->assertEquals( $edtt->start_date_and_time( $formats[0], $formats[1] ), $saved_dtts_for_tickets[$format]->start_date_and_time(), sprintf( 'DB DTT/Orig DTT Start Date Format Checked: %s', $format ) );
+			$this->assertEquals( $edtt->end_date_and_time( $formats[0], $formats[1] ), $saved_dtts_for_tickets[$format]->end_date_and_time(), sprintf( 'DB DTT/Orig DTT End Date Format Checked: %s', $format ) );
+
+			//get related ticket on this $edtt
+			$evt_tkt = $edtt->get_first_related( 'Ticket' );
+			$this->assertEquals( $evt_tkt->start_date( $formats[0], $formats[1] ), $this->_default_dates['TKT_start']->format( $format ), sprintf( 'DB TKT/Default TKT Start Date Format Checked: %s', $format ) );
+			$this->assertEquals( $evt_tkt->end_date( $formats[0], $formats[1] ), $this->_default_dates['TKT_end']->format( $format ), sprintf( 'DB TKT/Default TKT End Date Format Checked: %s', $format ) );
+			$this->assertEquals( $evt_tkt->start_date( $formats[0], $formats[1] ), $saved_tkts[$format]->start_date( $formats[0], $formats[1] ), sprintf( 'DB TKT/Orig TKT Start Date Format Checked: %s', $format ) );
+			$this->assertEquals( $evt_tkt->end_date( $formats[0], $formats[1] ), $saved_tkts[$format]->end_date( $formats[0], $formats[1] ), sprintf( 'DB TKT/Orig TKT End Date Format Checked: %s', $format ) );
 		}
 
 	}
