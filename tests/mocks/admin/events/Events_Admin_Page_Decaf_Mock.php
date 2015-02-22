235972da784a371391bf2318c81dcf87a95fd681
Darren Ethier <darren@roughsmootheng.in>
2015-02-21 21:12:19 -0500

    add mock and tests for Events_Admin_Page_Decaf [touch: 7056]

---
+++ tests/mocks/admin/events/Events_Admin_Page_Decaf_Mock.php
@@ -0,0 +1,52 @@
+<?php
+
+if (!defined('EVENT_ESPRESSO_VERSION'))
+	exit('No direct script access allowed');
+
+/**
+ *
+ * Events_Admin_Page_Decaf_Mock
+ * Used for testing Events Admin Page tests.
+ *
+ * @package			Event Espresso
+ * @subpackage		mocks
+ * @author			Darren
+ * @since  4.6
+ *
+ */
+require_once EE_ADMIN . 'EE_Admin_Page.core.php';
+require_once EE_ADMIN . 'EE_Admin_Page_CPT.core.php';
+require_once EE_ADMIN_PAGES . 'events/Events_Admin_Page.core.php';
+class Events_Admin_Page_Decaf_Mock extends Events_Admin_Page {
+
+
+	public function __construct( $routing = TRUE ) {
+		//core constants that might not be loaded yet.
+		if ( ! defined( 'EE_ADMIN_PAGES_URL' ) ) {
+			define( 'EE_ADMIN_URL', EE_PLUGIN_DIR_URL . 'core/admin/' );
+			define( 'EE_ADMIN_PAGES_URL', EE_PLUGIN_DIR_URL . 'admin_pages/' );
+			define( 'EE_ADMIN_TEMPLATE', EE_ADMIN . 'templates' . DS );
+			define( 'WP_ADMIN_PATH', ABSPATH . 'wp-admin/' );
+			define( 'WP_AJAX_URL', admin_url( 'admin-ajax.php' ));
+			define( 'JQPLOT_URL', EE_GLOBAL_ASSETS_URL . 'scripts/jqplot/' );
+		}
+
+		//define any constants that might not be defined yet when using this mock.
+		if ( ! defined( 'EVENTS_PG_SLUG' ) ) {
+			define( 'EVENTS_PG_SLUG', 'espresso_events' );
+			define( 'EVENTS_LABEL', __('Events', 'event_espresso'));
+			define( 'EVENTS_ADMIN', EE_ADMIN_PAGES . 'events' . DS );
+			define( 'EVENTS_ADMIN_URL', admin_url( 'admin.php?page=' . EVENTS_PG_SLUG ));
+			define( 'EVENTS_TEMPLATE_PATH', EVENTS_ADMIN . 'templates' . DS );
+			define( 'EVENTS_ASSETS_URL', EE_ADMIN_PAGES_URL . 'events/assets/' );
+		}
+
+		parent::__construct( false );
+	}
+
+
+	public function default_tickets_update( $evtobj, $data ) {
+		return $this->_default_tickets_update( $evtobj, $data );
+	}
+
+} //end class Events_Admin_Page_Decaf_Mock
