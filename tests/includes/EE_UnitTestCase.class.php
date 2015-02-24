ca0bf7d35404362cf7a20f688d193b2a078ced6a
Darren Ethier <darren@roughsmootheng.in>
2015-02-24 14:37:22 -0500

    add tests for EE_Registration_List_Table [touch: 7056]
    This tests implementation of new query helpers from EEM_Base

--- tests/includes/EE_UnitTestCase.class.php
+++ tests/includes/EE_UnitTestCase.class.php
@@ -283,6 +283,7 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		require_once EE_TESTS_DIR . 'mocks/admin/EE_Admin_Mocks.php';
 		require_once EE_TESTS_DIR . 'mocks/admin/admin_mock_valid/Admin_Mock_Valid_Admin_Page.core.php';
 		require_once EE_TESTS_DIR . 'mocks/admin/pricing/espresso_events_Pricing_Hooks_Mock.php';
+		require_once EE_TESTS_DIR . 'mocks/admin/registrations/EE_Registrations_List_Table_Mock.php';
 
 	}
 
