33e6f1cf4236ef6be6fc8219454c17d14a0d2b6d
Brent Christensen <brent@eventespresso.com>
2015-03-03 20:44:00 -0800

    fix typo in method name; fix typos phpdocs; [touch:7669]

--- core/libraries/plugin_api/EE_Register_Capabilities.lib.php
+++ core/libraries/plugin_api/EE_Register_Capabilities.lib.php
@@ -33,11 +33,18 @@ class EE_Register_Capabilities implements EEI_Plugin_API {
 	 *
 	 * @param string $cap_reference usually will be a class name that references capability related items setup for something.
 	 * @param array  $setup_args    {
-	 *                              An array of items related to registering capabilities.
-	 *                              @type array $capabilities 	An array mapping capability strings to core WP Role.  Something like array( 'administrator' => array( 'read_cap', 'edit_cap', 'delete_cap'), 'author' => array( 'read_cap' ) ).
-	 *                              @type array $capability_maps EE_Meta_Capability_Map[]   @see EE_Capabilities.php for php docs on these objects.  Should be indexed by the classname for the capability map and values representing the arguments for the map.
-	 * }
-	 *
+	 *                             	An array of items related to registering capabilities.
+	 *                             	@type array $capabilities 	An array mapping capability strings to core WP Role.
+	 * 																				Something like:
+	 * 																				array(
+	 * 																					'administrator' 	=> array( 'read_cap', 'edit_cap', 'delete_cap'),
+	 * 																					'author' 				=> array( 'read_cap' )
+	 * 																				).
+	 *                             	@type array $capability_maps EE_Meta_Capability_Map[]
+	 * 								@see EE_Capabilities.php for php docs on these objects.
+	 * 								Should be indexed by the classname for the capability map and values representing the arguments for the map.
+	 * 		}
+	 * @throws \EE_Error
 	 * @return void
 	 */
 	public static function register( $cap_reference = NULL, $setup_args = array() ) {
@@ -47,33 +54,26 @@ class EE_Register_Capabilities implements EEI_Plugin_API {
 				__( 'In order to register capabilities with EE_Register_Capabilities::register, you must include a unique name to reference the capabilities being registered, plus an array containing the following keys: "capabilities".', 'event_espresso' )
 			);
 		}
-
 		//make sure we don't register twice
 		if( isset( self::$_registry[ $cap_reference ] ) ){
 			return;
 		}
-
 		//make sure this is not registered too late or too early.
 		if ( ! did_action( 'AHEE__EE_System__load_espresso_addons' ) || did_action( 'AHEE__EE_System___detect_if_activation_or_upgrade__begin' ) ) {
 			EE_Error::doing_it_wrong( __METHOD__, sprintf( __('%s has been registered too late.  Please ensure that EE_Register_Capabilities::register has been called at some point before the "AHEE__EE_System___detect_if_activation_or_upgrade__begin" action hook has been called.', 'event_espresso'), $cap_reference ), '4.5.0' );
 		}
-
 		//some preliminary sanitization and setting to the $_registry property
 		self::$_registry[$cap_reference] = array(
 			'caps' => isset( $setup_args['capabilities'] ) && is_array( $setup_args['capabilities'] ) ? $setup_args['capabilities'] : array(),
 			'cap_maps' => isset( $setup_args['capability_maps'] ) ? $setup_args['capability_maps'] : array()
-			);
-
-
-		//set initial caps (note that EE_Capabilities takes care of making sure that the caps get added donly once)
+		);
+		//set initial caps (note that EE_Capabilities takes care of making sure that the caps get added only once)
 		add_filter( 'FHEE__EE_Capabilities__init_caps_map__caps', array( 'EE_Register_Capabilities', 'register_capabilities' ), 10 );
-
 		//add filter for cap maps
 		add_filter( 'FHEE__EE_Capabilities___set_meta_caps__meta_caps', array( 'EE_Register_Capabilities', 'register_cap_maps' ), 10 );
-
 		//init_role_caps to register new capabilities
 		if ( is_admin() ) {
-			EE_Registry::instance()->load('Capabilities');
+			EE_Registry::instance()->load_core( 'Capabilities' );
 			EE_Capabilities::instance()->init_caps();
 		}
 	}
@@ -84,7 +84,7 @@ class EE_Register_Capabilities implements EEI_Plugin_API {
 	 * callback for FHEE__EE_Capabilities__init_caps_map__caps filter.
 	 * Takes care of registering additional capabilities to the caps map.   Note, that this also on the initial registration ensures that new capabilities are added to existing roles.
 	 *
-	 * @param array $caps_and_cap_map The original caps map.
+	 * @param array $incoming_caps The original caps map.
 	 *
 	 * @return array merged in new caps.
 	 */
@@ -98,13 +98,12 @@ class EE_Register_Capabilities implements EEI_Plugin_API {
 
 
 	/**
-	 * Callback for the 'FHEE__EE_Capabilities___set_meta_caps__meta_caps' filter which regsiters an array of capability maps for the WP meta_caps filter called in EE_Capabilities.
+	 * Callback for the 'FHEE__EE_Capabilities___set_meta_caps__meta_caps' filter which registers an array of capability maps for the WP meta_caps filter called in EE_Capabilities.
 	 *
 	 * @since 4.5.0
-	 *
 	 * @param EE_Meta_Capability_Map[] $cap_maps The existing cap maps array.
-	 *
-	 * @return EE_Meta_Capability_Map[]
+	 * @return \EE_Meta_Capability_Map[]
+	 * @throws \EE_Error
 	 */
 	public static function register_cap_maps( $cap_maps ) {
 		//loop through and instantiate cap maps.
