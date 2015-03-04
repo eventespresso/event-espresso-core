b3f3368688f7661d0a02a86cec2da67f65427d4f
Brent Christensen <brent@eventespresso.com>
2015-03-04 13:45:41 -0800

    update New Addon mock to follow latest best practices; [touch:7669]

--- tests/mocks/addons/eea-new-addon/EE_New_Addon.class.php
+++ tests/mocks/addons/eea-new-addon/EE_New_Addon.class.php
@@ -1,28 +1,23 @@
 <?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit(); }
+
+// define the plugin directory path and URL
+define( 'EE_NEW_ADDON_BASENAME', plugin_basename( EE_NEW_ADDON_PLUGIN_FILE ) );
+define( 'EE_NEW_ADDON_PATH', plugin_dir_path( __FILE__ ) );
+define( 'EE_NEW_ADDON_URL', plugin_dir_url( __FILE__ ) );
+define( 'EE_NEW_ADDON_ADMIN', EE_NEW_ADDON_PATH . 'admin' . DS . 'new_addon' . DS );
+
+
+
 /**
- * ------------------------------------------------------------------------
  *
  * Class  EE_New_Addon
  *
  * @package			Event Espresso
  * @subpackage		eea-new-addon
  * @author			    Brent Christensen
- * @ version		 	$VID:$
  *
- * ------------------------------------------------------------------------
  */
-// define the plugin directory path and URL
-define( 'EE_NEW_ADDON_BASENAME', plugin_basename( EE_NEW_ADDON_PLUGIN_FILE ));
-define( 'EE_NEW_ADDON_PATH', plugin_dir_path( __FILE__ ));
-define( 'EE_NEW_ADDON_URL', plugin_dir_url( __FILE__ ));
-define( 'EE_NEW_ADDON_ADMIN', EE_NEW_ADDON_PATH . 'admin' . DS . 'new_addon' . DS );
 Class  EE_New_Addon extends EE_Addon {
-
-	/**
-	 * class constructor
-	 */
-	public function __construct() {
-	}
 
 	public static function register_addon() {
 		// register addon via Plugin API
@@ -30,12 +25,11 @@ Class  EE_New_Addon extends EE_Addon {
 			'New_Addon',
 			array(
 				'version' 					=> EE_NEW_ADDON_VERSION,
-				'min_core_version' => '4.3.0.dev.000',
+				'min_core_version' => '4.6.0.p',
+				'plugin_slug' 			=> 'new_addon',
 				'main_file_path' 		=> EE_NEW_ADDON_PLUGIN_FILE,
 				'admin_path' 			=> EE_NEW_ADDON_ADMIN,
-				'plugin_slug'			=> 'new_addon',
-				'plugins_page_row'=> EE_New_Addon::plugins_page_upsell_info(),
-				'admin_callback'		=> 'additional_admin_hooks',
+				'admin_callback'		=> '',
 				'config_class' 			=> 'EE_New_Addon_Config',
 				'config_name' 		=> 'EE_New_Addon',
 				'autoloader_paths' => array(
@@ -50,19 +44,22 @@ Class  EE_New_Addon extends EE_Addon {
 				'widget_paths' 		=> array( EE_NEW_ADDON_PATH . 'EEW_New_Addon.widget.php' ),
 				// if plugin update engine is being used for auto-updates. not needed if PUE is not being used.
 				'pue_options'			=> array(
-					'pue_plugin_slug' 	=> 'eea-new-addon',
-					'plugin_basename' => EE_NEW_ADDON_BASENAME,
-					'checkPeriod' 			=> '24',
-					'use_wp_update' 	=> FALSE,
-					),
+					'pue_plugin_slug' 		=> 'eea-new-addon',
+					'plugin_basename' 	=> EE_NEW_ADDON_BASENAME,
+					'checkPeriod' 				=> '24',
+					'use_wp_update' 		=> FALSE,
+				),
 				'capabilities' => array(
 					'administrator' => array(
 						'read_addon', 'edit_addon', 'edit_others_addon', 'edit_private_addon'
-						),
 					),
+				),
 				'capability_maps' => array(
-					new EE_Meta_Capability_Map_Edit( 'edit_addon', array( 'Event', '', 'edit_others_addon', 'edit_private_addon' ) )
-					),
+					'EE_Meta_Capability_Map_Edit' => array(
+						'edit_addon',
+						array( 'Event', '', 'edit_others_addon', 'edit_private_addon' )
+					)
+				),
 				'class_paths' 						=> EE_NEW_ADDON_PATH . 'core' . DS . 'db_classes',
 				'model_paths' 					=> EE_NEW_ADDON_PATH . 'core' . DS . 'db_models',
 				'class_extension_paths' 		=> EE_NEW_ADDON_PATH . 'core' . DS . 'db_class_extensions',
@@ -73,43 +70,6 @@ Class  EE_New_Addon extends EE_Addon {
 				'default_terms' 					=> array()
 			)
 		);
-	}
-
-
-
-	/**
-	 * 	additional_admin_hooks
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public function additional_admin_hooks() {
-	}
-
-
-
-	/**
-	 * 	plugins_page_row
-	 *
-	 * 	if not empty, inserts a new table row after this plugin's row on the WP Plugins page
-	 * this can be used for adding upgrading/marketing info.
-	 * Return an array (like below) to have the upsell info markup generated automatically, or
-	 * Return a string of HTML to customize the entire table row contents
-	 *
-	 *  @access 	public
-	 *  @return    string | array
-	 */
-	public static function plugins_page_upsell_info() {
-		return array(
-			'link_text' 		=>  __( 'UPGRADE TO PRO', 'event_espresso' ),
-			'link_url' 			=> '#',
-			'description' 	=> sprintf( __( 'Upgrade to Pro Version %1$s today and get moar betterer features.', 'event_espresso' ), '1.1.0' ),
-		);
-		// or return a string like:
-		//$html = '<td colspan="3" class="ee-addon-upsell-info-td column-description desc">';
-		//$html = sprintf( __( 'Upgrade to Pro Version %1$s today and get moar betterer features.', 'event_espresso' ), '1.1.0' );
-		//$html = '</td>';
-		//return $html;
 	}
 
 
--- tests/mocks/addons/eea-new-addon/eea-new-addon.php
+++ tests/mocks/addons/eea-new-addon/eea-new-addon.php
@@ -36,16 +36,73 @@
  *
  * ------------------------------------------------------------------------
  */
-define( 'EE_NEW_ADDON_VERSION', '1.0.0.dev.000' );
+// define versions and this file
+define( 'EE_NEW_ADDON_VERSION', '1.0.0.rc.000' );
+define( 'EE_NEW_ADDON_CORE_VERSION_REQUIRED', '4.6.0.p' );
 define( 'EE_NEW_ADDON_PLUGIN_FILE',  __FILE__ );
-function load_espresso_new_addon() {
-if ( class_exists( 'EE_Addon' )) {
-	// new_addon version
-	require_once ( plugin_dir_path( __FILE__ ) . 'EE_New_Addon.class.php' );
-	EE_New_Addon::register_addon();
+
+
+
+/**
+ *    captures plugin activation errors for debugging
+ */
+function espresso_new_addon_plugin_activation_errors() {
+
+	if ( WP_DEBUG ) {
+		$activation_errors = ob_get_contents();
+		file_put_contents( EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . 'espresso_new_addon_plugin_activation_errors.html', $activation_errors );
+	}
 }
+add_action( 'activated_plugin', 'espresso_new_addon_plugin_activation_errors' );
+
+
+
+/**
+ *    registers addon with EE core
+ */
+function load_espresso_new_addon() {
+  if ( class_exists( 'EE_Addon' )) {
+      // new_addon version
+      require_once ( plugin_dir_path( __FILE__ ) . 'EE_New_Addon.class.php' );
+      EE_New_Addon::register_addon();
+  } else {
+    add_action( 'admin_notices', 'espresso_new_addon_activation_error' );
+  }
 }
 add_action( 'AHEE__EE_System__load_espresso_addons', 'load_espresso_new_addon' );
+
+
+
+/**
+ *    verifies that addon was activated
+ */
+function espresso_new_addon_activation_check() {
+  if ( ! did_action( 'AHEE__EE_System__load_espresso_addons' ) ) {
+    add_action( 'admin_notices', 'espresso_new_addon_activation_error' );
+  }
+}
+add_action( 'init', 'espresso_new_addon_activation_check', 1 );
+
+
+
+/**
+ *    displays activation error admin notice
+ */
+function espresso_new_addon_activation_error() {
+  unset( $_GET[ 'activate' ] );
+  unset( $_REQUEST[ 'activate' ] );
+  if ( ! function_exists( 'deactivate_plugins' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+  }
+  deactivate_plugins( plugin_basename( EE_NEW_ADDON_PLUGIN_FILE ) );
+  ?>
+  <div class="error">
+    <p><?php printf( __( 'Event Espresso New Addon could not be activated. Please ensure that Event Espresso version %1$s or higher is running', 'event_espresso' ), EE_NEW_ADDON_CORE_VERSION_REQUIRED ); ?></p>
+  </div>
+<?php
+}
+
+
 
 // End of file espresso_new_addon.php
 // Location: wp-content/plugins/eea-new-addon/espresso_new_addon.php
