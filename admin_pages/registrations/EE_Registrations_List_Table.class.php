15400e03c3e4fd1b14c2deff391bb8dd5441cbf1
Darren Ethier <darren@roughsmootheng.in>
2015-03-06 08:36:07 -0500

    date_i18ncitize transactions list table and reg list table [touch: 7735]

--- admin_pages/registrations/EE_Registrations_List_Table.class.php
+++ admin_pages/registrations/EE_Registrations_List_Table.class.php
@@ -363,7 +363,7 @@ class EE_Registrations_List_Table extends EE_Admin_List_Table {
 		$this->_set_related_details($item);
        		 //Build row actions
 		$view_lnk_url = EE_Admin_Page::add_query_args_and_nonce( array( 'action'=>'view_transaction', 'TXN_ID'=> $this->_transaction_details['id'] ), TXN_ADMIN_URL );
-		return EE_Registry::instance()->CAP->current_user_can('ee_read_transaction', 'espresso_transactions_view_transaction') ? '<a class="ee-status-color-' . $this->_transaction_details['status'] . '" href="'.$view_lnk_url.'" title="' . $this->_transaction_details['title_attr'] . '">' . $item->reg_date() . '</a>' : $item->reg_date();
+		return EE_Registry::instance()->CAP->current_user_can('ee_read_transaction', 'espresso_transactions_view_transaction') ? '<a class="ee-status-color-' . $this->_transaction_details['status'] . '" href="'.$view_lnk_url.'" title="' . $this->_transaction_details['title_attr'] . '">' . $item->get_i18n_datetime( 'REG_date' ) . '</a>' : $item->get_i18n_datetime( 'REG_date' );
 	}
 
 
@@ -405,7 +405,7 @@ class EE_Registrations_List_Table extends EE_Admin_List_Table {
 			$remove_defaults = array('default_where_conditions' => 'none');
 			$datetimes = $ticket->datetimes($remove_defaults);
 			foreach($datetimes as $datetime){
-				$datetime_strings[] = $datetime->start_date_and_time();
+				$datetime_strings[] = $datetime->get_i18n_datetime( 'DTT_EVT_start' );
 			}
 			return implode("<br />",$datetime_strings);
 		} else {
