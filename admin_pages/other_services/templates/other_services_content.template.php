8f500400f34f2ea1a413ec3e8a8715796ae4a675
Seth <seth@eventespresso.com>
2015-03-08 21:49:47 -0600

    Adding content to the other services page. [touch:5683]

--- admin_pages/other_services/templates/other_services_content.template.php
+++ admin_pages/other_services/templates/other_services_content.template.php
@@ -1,3 +1,281 @@
-<div>
-<p>Content goes here.</p>
-</div>
+												<h1 class="service-settings-heading">Add-ons</h1>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Events Calendar Add-on</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/calendar4-231x151.jpg">Our WordPress events calendar displays events and categories of events in a nice graphical calendar. People often schedule themselves on a calendar, so make it easy for attendees to find your events and add them to their own schedule.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/ee4-events-calendar/" target="_blank">Buy Now																	</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/events-calendar-add-on/" target="_blank">View Documentation																	</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $29.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>WP User Integration</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-wp-user-integration-230x151.jpg">The “EE4 WP User Integration” add-on allows you to take full advantage of the WordPress user system. Create member only events and tickets, set the minimum WP User Capability for ticket purchase, create new users on registration, and more!</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-wp-user-integration/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/ee4-wp-user-integration/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $39.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Printable Tickets</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-ticketing-231x151.jpg">Give attendees custom-designed printable tickets for each event to keep as a souvenir, serve as a place to sell advertising and more. The days are gone when your ticket has to look like everyone elses’. Your events and brand are unique and your tickets can be unique too.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-ticketing/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/ee4-printable-tickets/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $59.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Ticket Scanning</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ticket-scanning-231x151.jpg">Control access to your event by validating each printable ticket at the door. The barcode scanning add-on interfaces with the Event Espresso 4 admin and a handheld, or bluetooth barcode scanner, to allow event managers to scan Event Espresso 4 generated tickets at the door of the event.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-barcode-scanner/" target="_blank">Buy Now																	</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/barcode-scanner-add-on/" target="_blank">View Documentation																	</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $79.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>MailChimp Integration</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/mailchimp-231x151.jpg"><a href="http://eepurl.com/OXgr" target="_blank">MailChimp</a> is an extremely popular tool among email marketers, businesses and bloggers. It is easy to use, powerful, and allows just about anyone to create amazing email campaigns quickly and easily. Once a visitor submits their information on your registration form, the attendee is instantly added to the MailChimp mailing LIST or GROUP that you have configured for that event.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-mailchimp/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/mailchimp-integration/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $39.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Infusionsoft Integration</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/infusionsoft-231x151.jpg"><a href="https://crm.infusionsoft.com/go/viewdemoaff/a49730/">Infusionsoft’s</a> all-in-one sales and marketing software is built to meet the specific needs of small business. With the Event Espresso 4 Infusionsoft integration add-on, you can completely automate your customer relations and email marketing campaigns. The fastest way to discover what Infusionsoft can do for your business is to watch a quick <a href="https://crm.infusionsoft.com/go/viewdemoaff/a49730/" target="_blank">product demonstration video</a>.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-infusionsoft/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/infusionsoft-integration/#ee4installation" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $69.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Events Social Sharing Add-on</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-social-sharing-addon-231x151.jpg">Allow your registrants and attendees to share a message about their event registration on Twitter and Facebook. Use the Events Social Sharing add-on to create more exposure for your events by allowing your attendees and registrants to share a message to Twitter or Facebook after registering. This add-on adds social sharing features to the end of your Event Espresso thank you page.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-events-social-sharing/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/events-social-sharing-add-on/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $29.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<h1 class="service-settings-heading">Template Views</h1>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Events Table View Template</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-events-table3-231x151.jpg">The Events Table Template add-on provides you with an easy to use system that allows you to change the look and feel of your event lists. It comes with a simple table template with category filters, table search, table paging, and more. It also allows developers to build their own custom templates using one of the included templates as a base.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-events-table-view-template/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/ee4-events-table-view-template/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $29.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Events Grid View Template</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/grid-view-template-231x151.jpg">Displays events in a pure CSS grid like structure, using the featured image. Hovering over the image will display further information. If the event does not have a featured image available, it will use a default image. We also added an easy to use parameter that allows you to set the default image to one of your choosing, such as your company logo.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-events-grid-view-template/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/ee4-events-grid-view-template/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $29.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<h1 class="service-settings-heading">Payment Gateways</h1>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>Stripe Payment Gateway</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-stripe-logo-231x151.jpg">Stripe is the easiest way to accept credit and debit card payments with Event Espresso. With Stripe, you can create exactly the payment experience you want in your Event Espresso powered website, and Stripe handles everything from security to transfers to your bank account. You can get started immediately.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://eventespresso.com/product/eea-stripe-gateway/" target="_blank">Buy Now</a>
+																	<a class="plugin-div-button button button-secondary" href="http://eventespresso.com/wiki/stripe-payment-gateway/" target="_blank">View Documentation</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="column-downloaded">
+															<strong>Cost: $59.95</strong><br>
+														</div>
+													</div>
+												</div>
+												<div class="service-div-settings">
+													<div class="service-div-settings-top">
+														<div class="service-div-inner-content">
+															<div class="name column-name">
+																<h3>iDeal Mollie Payment Gateway</h3>
+															</div>
+															<div class="desc column-description">
+																<p><img src="http://ee-screenshots.s3.amazonaws.com/ee4/ideal-mollie-gateway-231x151.jpg"><a href="https://www.mollie.com/nl/ideal" target="_blank">iDEAL</a> is to connect via Mollie easily without the usual technical and administrative burdens. iDEAL works directly with 9 large banks in the Netherlands: ABN AMRO, ASN Bank, ING, Knab, Rabobank, Regions Bank, SNS Bank, Triodos Bank and van Lanschot. Actual payments will take place in the familiar surroundings of the bank website.</p>
+															</div>
+														</div>
+														<div class="action-links">
+															<ul class="plugin-action-buttons-custom">
+																<li>
+																	<a class="plugin-div-button button button-primary" href="http://tech-banker.com/contact-us/" target="_blank">Send Enquiry</a>
+																	<a class="plugin-div-button button button-secondary" href="http://tech-banker.com/shop/wordpress/wordpress-theme-development/" target="_blank">Read More</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+													<div class="plugin-card-bottom_settings">
+														<div class="vers column-rating">
+															
+														</div>
+														<div class="column-downloaded">
+															<strong>Cost: $69.95</strong><br>
+														</div>
+													</div>
+												</div>
