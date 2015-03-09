12fe8aff292a0decf28c29ecbe87cfef37148924
Brent Christensen <brent@eventespresso.com>
2015-03-09 13:46:08 -0700

    add $_old_txn_status and $_new_txn_status properties as well as setters and getters, and update accordingly throughout class; [touch:7728]

--- core/business/EE_Transaction_Payments.class.php
+++ core/business/EE_Transaction_Payments.class.php
@@ -19,7 +19,21 @@ class EE_Transaction_Payments {
 	 * 	@var EE_Transaction_Payments $_instance
 	 * 	@access 	private
 	 */
-	private static $_instance = NULL;
+	private static $_instance = null;
+
+	/**
+	 * initial txn status at the beginning of this request.
+	 *
+	 * @var string
+	 */
+	protected $_old_txn_status = null;
+
+	/**
+	 * txn status at the end of the request after all processing.
+	 *
+	 * @var string
+	 */
+	protected $_new_txn_status = null;
 
 
 
@@ -47,6 +61,56 @@ class EE_Transaction_Payments {
 
 
 	/**
+	 * @return string
+	 */
+	public function old_txn_status() {
+		return $this->_old_txn_status;
+	}
+
+
+
+	/**
+	 * @param string $old_txn_status
+	 */
+	public function set_old_txn_status( $old_txn_status ) {
+		// only set the first time
+		if ( $this->_old_txn_status === null ) {
+			$this->_old_txn_status = $old_txn_status;
+		}
+	}
+
+
+
+	/**
+	 * @return string
+	 */
+	public function new_txn_status() {
+		return $this->_new_txn_status;
+	}
+
+
+
+	/**
+	 * @param string $new_txn_status
+	 */
+	public function set_new_txn_status( $new_txn_status ) {
+		$this->_new_txn_status = $new_txn_status;
+	}
+
+
+
+	/**
+	 * reg_status_updated
+	 *
+	 * @return bool
+	 */
+	public function txn_status_updated() {
+		return $this->_new_txn_status !== $this->_old_txn_status ? true : false;
+	}
+
+
+
+	/**
 	 * Updates the provided EE_Transaction with all the applicable payments
 	 * returns a boolean for whether the TXN was saved to the db
 	 * (meaning a status change occurred)
@@ -66,6 +130,8 @@ class EE_Transaction_Payments {
 			EE_Error::add_error( __( 'Please provide a valid EE_Transaction object.', 'event_espresso' ), __FILE__, __FUNCTION__, __LINE__ );
 			return false;
 		}
+		// set incoming TXN_Status
+		$this->set_old_txn_status( $transaction->status_ID() );
 		// calculate total paid
 		$total_paid = $this->recalculate_total_payments_for_transaction( $transaction );
 		// if total paid has changed
@@ -131,6 +197,8 @@ class EE_Transaction_Payments {
 			return FALSE;
 		}
 		EE_Registry::instance()->load_helper( 'Money' );
+		// set incoming TXN_Status
+		$this->set_old_txn_status( $transaction->status_ID() );
 		// set transaction status based on comparison of TXN_paid vs TXN_total
 		if ( EEH_Money::compare_floats( $transaction->paid(), $transaction->total(), '>' ) ){
 			$new_txn_status = EEM_Transaction::overpaid_status_code;
@@ -146,6 +214,8 @@ class EE_Transaction_Payments {
 			return FALSE;
 		}
 		if ( $new_txn_status !== $transaction->status_ID() ) {
+			// set incoming TXN_Status
+			$this->set_new_txn_status( $new_txn_status );
 			$transaction->set_status( $new_txn_status );
 			if ( $update_txn ) {
 				return $transaction->save() ? TRUE : FALSE;
