6ffcd80303949fa7fefea625e26a86937b5befcc
Brent Christensen <brent@eventespresso.com>
2015-03-11 09:47:06 -0700

    use EEH_Debug_Tools::log(); [touch:7728]

--- core/business/EE_Processor_Base.class.php
+++ core/business/EE_Processor_Base.class.php
@@ -44,59 +44,21 @@ class EE_Processor_Base {
 	 * @param array $info
 	 * @param bool $display_request
 	 */
-	function log( $class = '', $func = '', $line = '', EE_Transaction $transaction, $info = array(), $display_request = false ) {
+	protected function log( $class = '', $func = '', $line = '', EE_Transaction $transaction, $info = array(), $display_request = false ) {
+		EE_Registry::instance()->load_helper( 'Debug_Tools' );
 		if ( WP_DEBUG ) {
-			$debug_data = get_option( 'EE_DEBUG_SPCO_' . EE_Session::instance()->id(), array() );
-			$default_data = array(
-				$class   => $func . '() : ' . $line,
-				'REQ'    => $display_request ? $_REQUEST : '',
-			);
 			if ( $transaction instanceof EE_Transaction ) {
-				$default_data[ 'TXN_status' ] = $transaction->status_ID();
-				$default_data[ 'TXN_reg_steps' ] = $transaction->reg_steps();
+				// don't serialize objects
+				$info = EEH_Debug_Tools::strip_objects( $info );
 				if ( $transaction->ID() ) {
-					$TXN_ID = 'TXN_ID: ' . $transaction->ID();
-					// don't serialize objects
-					$info = $this->_strip_objects( $info );
-					if ( ! isset( $debug_data[ $TXN_ID ] ) ) {
-						$debug_data[ $TXN_ID ] = array();
-					}
-					$debug_data[ $TXN_ID ][ microtime() ] = array_merge(
-						$default_data,
-						$info
-					);
-					update_option( 'EE_DEBUG_SPCO_' . EE_Session::instance()->id(), $debug_data );
+					$info[ 'TXN_status' ] = $transaction->status_ID();
+					$info[ 'TXN_reg_steps' ] = $transaction->reg_steps();
+					$index = 'TXN_ID: ' . $transaction->ID();
+					EEH_Debug_Tools::log( $class, $func, $line, $info, $display_request, $index );
 				}
 			}
 		}
 
-	}
-
-
-
-	/**
-	 * _strip_objects
-	 *
-	 * @param array $info
-	 * @return array
-	 */
-	function _strip_objects( $info = array() ) {
-		foreach ( $info as $key => $value ) {
-			if ( is_array( $value ) ) {
-				$info[ $key ] = $this->_strip_objects( $value );
-			} else if ( is_object( $value ) ) {
-				$object_class = get_class( $value );
-				$info[ $object_class ] = array();
-				$info[ $object_class ][ 'ID' ] = method_exists( $value, 'ID' ) ? $value->ID() : 0;
-				if ( method_exists( $value, 'status' ) ) {
-					$info[ $object_class ][ 'status' ] = $value->status();
-				} else if ( method_exists( $value, 'status_ID' ) ) {
-					$info[ $object_class ][ 'status' ] = $value->status_ID();
-				}
-				unset( $info[ $key ] );
-			}
-		}
-		return (array)$info;
 	}
 
 
