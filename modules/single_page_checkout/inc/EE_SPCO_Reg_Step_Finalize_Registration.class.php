0c651ba497e2506137d3e78482b46cda4df97ed2
Brent Christensen <brent@eventespresso.com>
2015-03-12 14:37:04 -0700

    if notifications were already sent, and add blocking filter; [touch:7728]

--- modules/single_page_checkout/inc/EE_SPCO_Reg_Step_Finalize_Registration.class.php
+++ modules/single_page_checkout/inc/EE_SPCO_Reg_Step_Finalize_Registration.class.php
@@ -140,27 +140,26 @@ class EE_SPCO_Reg_Step_Finalize_Registration extends EE_SPCO_Reg_Step {
 		$transaction_payments = EE_Registry::instance()->load_class( 'Transaction_Payments' );
 		// maybe update status, but don't save transaction just yet
 		$transaction_payments->update_transaction_status_based_on_total_paid( $this->checkout->transaction, false );
-		$notifications_already_delivered = did_action(
-			'AHEE__EE_Registration_Processor__trigger_registration_update_notifications' );
 		// if an IPN or other payment hasn't already
 		if (
 			(
 				// first pass thru SPCO and an IPN or other payment hasn't already completed the TXN
 				! $this->checkout->revisit &&
-				! $transaction_processor->final_reg_step_completed( $this->checkout->transaction ) &&
-				! $notifications_already_delivered
-
-
+				! $transaction_processor->final_reg_step_completed( $this->checkout->transaction )
 			)
 			|| (
 				// SPCO revisit but TXN status has changed due to a payment
 				$this->checkout->revisit &&
-				$this->checkout->txn_status_updated &&
-				! $notifications_already_delivered
+				$this->checkout->txn_status_updated
 			)
 		) {
 			// send out notifications
 			add_filter( 'FHEE__EED_Messages___maybe_registration__deliver_notifications', '__return_true' );
+		}
+		// check that notifications were not already sent
+		if ( did_action( 'AHEE__EE_Registration_Processor__trigger_registration_update_notifications' ) ) {
+			// do NOT send out notifications
+			add_filter( 'FHEE__EED_Messages___maybe_registration__deliver_notifications', '__return_false', 11 );
 		}
 		// this will result in the base session properties getting saved to the TXN_Session_data field
 		$this->checkout->transaction->set_txn_session_data( EE_Registry::instance()->SSN->get_session_data( null, true ));
