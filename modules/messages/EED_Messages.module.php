556861606e36fdafefc39fcc89830e77e2a6a5a0
Brent Christensen <brent@eventespresso.com>
2015-03-12 14:58:04 -0700

    tweak messages logging; [touch:7728]

--- modules/messages/EED_Messages.module.php
+++ modules/messages/EED_Messages.module.php
@@ -415,14 +415,18 @@ class EED_Messages  extends EED_Module {
 	 * @return void
 	 */
 	public static function maybe_registration( EE_Registration $registration, $extra_details = array() ) {
-		self::log(
-			__CLASS__, __FUNCTION__, __LINE__,
-			$registration->transaction()
-		);
 		if ( ! self::_verify_registration_notification_send( $registration, $extra_details ) ) {
 			//no messages please
 			return;
 		}
+		self::log(
+			__CLASS__, __FUNCTION__, __LINE__,
+			$registration->transaction(),
+			array(
+				'deliver_notifications' => apply_filters(
+			'FHEE__EED_Messages___maybe_registration__deliver_notifications', false ),
+			)
+		);
 		EE_Registry::instance()->load_helper( 'MSG_Template' );
 		// send the message type matching the status if that message type is active.
 		$message_type = self::_get_reg_status_array( $registration->status_ID() );
