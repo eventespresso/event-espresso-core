c4cafc3f0fa7a51b338c5d676215bc1fda2bd09f
Brent Christensen <brent@eventespresso.com>
2015-03-16 12:38:03 -0700

    and display a heading; [touch:7728]

--- modules/single_page_checkout/templates/payment_options/no_payment_required.template.php
+++ modules/single_page_checkout/templates/payment_options/no_payment_required.template.php
@@ -1,19 +1,28 @@
 <?php
-$registrations_for_free_events = is_array( $registrations_for_free_events ) ? $registrations_for_free_events : array( $registrations_for_free_events );
-foreach ( $registrations_for_free_events as $registration_for_free_event ) {
-	if ( $registration_for_free_event instanceof EE_Registration && $registration_for_free_event->ticket()->is_free() ) {
-		if ( $registration_for_free_event->event() instanceof EE_Event ) {
-			?>
-			<p>
-				<?php echo apply_filters(
-					'FHEE__registration_page_payment_options__no_payment_required_pg',
-					sprintf(
-						__( '%1$s is a free event, so no billing will occur.', 'event_espresso' ),
-						$registration_for_free_event->event()->name()
-					)
-				); ?>
-			</p>
-		<?php
+if( is_array( $registrations_for_free_events ) && ! empty( $registrations_for_free_events ) ) {
+	echo apply_filters(
+		'FHEE__registration_page_payment_options__no_payment_required_hdr',
+		sprintf(
+			__( '%1$sNo Payment Required%2$s', 'event_espresso' ),
+			'<h6>',
+			'</h6>'
+		)
+	);
+	foreach ( $registrations_for_free_events as $registration_for_free_event ) {
+		if ( $registration_for_free_event instanceof EE_Registration && $registration_for_free_event->ticket()->is_free() ) {
+			if ( $registration_for_free_event->event() instanceof EE_Event ) {
+				?>
+				<p>
+					<?php echo apply_filters(
+						'FHEE__registration_page_payment_options__no_payment_required_pg',
+						sprintf(
+							__( '%1$s is a free event, so no billing will occur.', 'event_espresso' ),
+							$registration_for_free_event->event()->name()
+						)
+					); ?>
+				</p>
+			<?php
+			}
 		}
 	}
 }
