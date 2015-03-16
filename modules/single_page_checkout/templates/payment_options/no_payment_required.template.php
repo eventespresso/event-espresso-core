be32e3a40dcd2701a46fbcf6f8de78d75d109901
Brent Christensen <brent@eventespresso.com>
2015-03-16 12:33:18 -0700

    display free event notice on an event by event basis; [touch:7728]

--- modules/single_page_checkout/templates/payment_options/no_payment_required.template.php
+++ modules/single_page_checkout/templates/payment_options/no_payment_required.template.php
@@ -1,9 +1,22 @@
-
-<!--	<input type="hidden" id="reg-page-no-payment-required-payment_options" name="_reg-page-no-payment-required" value="1" />-->
-	<p><?php echo apply_filters( 'FHEE__registration_page_payment_options__no_payment_required_pg', __( 'This is a free event, so no billing will occur.', 'event_espresso' )); ?></p>
-
-	<?php echo $default_hidden_inputs;  ?>
-	<?php echo $extra_hidden_inputs;  ?>
-
-
-
+<?php
+$registrations_for_free_events = is_array( $registrations_for_free_events ) ? $registrations_for_free_events : array( $registrations_for_free_events );
+foreach ( $registrations_for_free_events as $registration_for_free_event ) {
+	if ( $registration_for_free_event instanceof EE_Registration && $registration_for_free_event->ticket()->is_free() ) {
+		if ( $registration_for_free_event->event() instanceof EE_Event ) {
+			?>
+			<p>
+				<?php echo apply_filters(
+					'FHEE__registration_page_payment_options__no_payment_required_pg',
+					sprintf(
+						__( '%1$s is a free event, so no billing will occur.', 'event_espresso' ),
+						$registration_for_free_event->event()->name()
+					)
+				); ?>
+			</p>
+		<?php
+		}
+	}
+}
+echo $default_hidden_inputs;
+echo $extra_hidden_inputs;
+?>
