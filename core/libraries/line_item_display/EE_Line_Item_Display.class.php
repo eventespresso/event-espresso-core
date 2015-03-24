fea7512024a13882b98579cfe85bf39e8ddf5bb0
Brent Christensen <brent@eventespresso.com>
2015-03-23 21:32:38 -0700

    add class_exists() check; [touch:7438]

--- core/libraries/line_item_display/EE_Line_Item_Display.class.php
+++ core/libraries/line_item_display/EE_Line_Item_Display.class.php
@@ -34,7 +34,14 @@ class EE_Line_Item_Display {
 				$this->strategy = new EE_SPCO_Line_Item_Display_Strategy();
 				break;
 			default :
-				$this->strategy = ! empty( $other_line_item_display_strategy ) ? new $other_line_item_display_strategy() : new EE_Invoice_Line_Item_Display_Strategy();
+				if (
+					! empty( $other_line_item_display_strategy ) &&
+					class_exists( $other_line_item_display_strategy )
+				) {
+					$this->strategy = new  $other_line_item_display_strategy();
+				} else {
+					$this->strategy = new EE_SPCO_Line_Item_Display_Strategy();
+				}
 		}
 	}
 
@@ -51,18 +58,5 @@ class EE_Line_Item_Display {
 
 
 
-/**
- * Interface EEI_Line_Item_Display
- */
-interface EEI_Line_Item_Display {
-
-	/**
-	 * @param EE_Line_Item $line_item
-	 * @param array        $options
-	 * @return mixed
-	 */
-	public function display_line_item( EE_Line_Item $line_item, $options = array() );
-
-}
 // End of file EE_Line_Item_Display.class.php
 // Location: /EE_Line_Item_Display.class.php
