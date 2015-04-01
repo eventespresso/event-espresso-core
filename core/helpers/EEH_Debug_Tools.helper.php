cfc9c125debf72e8eb8152ac20025cb673479aa5
Brent Christensen <brent@eventespresso.com>
2015-04-01 11:51:02 -0700

    change print_r() to var_dump(); [touch:7901]

--- core/helpers/EEH_Debug_Tools.helper.php
+++ core/helpers/EEH_Debug_Tools.helper.php
@@ -356,13 +356,13 @@ class EEH_Debug_Tools{
 	 * @param bool $die
 	 */
 	public static function printr( $var, $var_name = false, $file = __FILE__, $line = __LINE__, $height = 'auto', $die = false ) {
-		$print_r = false;
+		//$print_r = false;
 		if ( is_object( $var ) ) {
 			$var_name = ! $var_name ? 'object' : $var_name;
-			$print_r = true;
+			//$print_r = true;
 		} else if ( is_array( $var ) ) {
 			$var_name = ! $var_name ? 'array' : $var_name;
-			$print_r = true;
+			//$print_r = true;
 		} else if ( is_numeric( $var ) ) {
 			$var_name = ! $var_name ? 'numeric' : $var_name;
 		} else if ( is_string( $var ) ) {
@@ -374,7 +374,8 @@ class EEH_Debug_Tools{
 		ob_start();
 		echo '<pre style="display:block; width:100%; height:' . $height . '; border:2px solid light-blue;">';
 		echo '<h5 style="color:#2EA2CC;"><b>' . $var_name . '</b></h5><span style="color:#E76700">';
-		$print_r ? print_r( $var ) : var_dump( $var );
+		//$print_r ? print_r( $var ) : var_dump( $var );
+		var_dump( $var );
 		echo '</span><br /><span style="font-size:10px;font-weight:normal;">' . $file . '<br />line no: ' . $line . '</span></pre>';
 		$result = ob_get_clean();
 		if ( $die ) {
