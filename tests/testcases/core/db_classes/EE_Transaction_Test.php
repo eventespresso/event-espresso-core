a0b87c1d8b3dd186de0c0800d9bb40ec01cebd33
Darren Ethier <darren@roughsmootheng.in>
2015-04-06 17:33:18 -0400

    add test for no_html flag
    [touch: 7792]

--- tests/testcases/core/db_classes/EE_Transaction_Test.php
+++ tests/testcases/core/db_classes/EE_Transaction_Test.php
@@ -77,7 +77,10 @@ class EE_Transaction_Test extends EE_UnitTestCase{
 		$t->set( 'TXN_timestamp', $now->format( $t->get_format( $format_to_use ) ) );
 
 		//test getting pretty (should return formatted item in the correct timezone)
-		$this->assertEquals( $now->format( $format_to_use ) . '<span class="ee_dtt_timezone_string">(PDT)</span>', $t->datetime( true ), 'datetime( true ) test' );
+		$this->assertEquals( $now->format( $format_to_use ) . ' <span class="ee_dtt_timezone_string">(PDT)</span>', $t->datetime( true ), 'datetime( true ) test' );
+
+		//test getting pretty with no_html
+		$this->assertEquals( $now->format( $format_to_use ) . ' (PDT)', $t->get_pretty( 'TXN_timestamp', 'no_html'), 'get_pretty, no_html test' );
 
 		//test getting raw unixtimestamp
 		$this->assertEquals( $now->format( 'U' ), $t->datetime( false, true ), 'datetime( false, true) test' );
