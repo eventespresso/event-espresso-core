bdb71b14b6141b76828b98bcaa75e76de377e088
Brent Christensen <brent@eventespresso.com>
2015-04-10 13:25:44 -0700

    reactivate debugging; [touch:7901]

--- core/EE_Payment_Processor.core.php
+++ core/EE_Payment_Processor.core.php
@@ -375,7 +375,7 @@ class EE_Payment_Processor extends EE_Processor_Base {
 			$transaction_processor->set_reg_step_completed( $transaction, 'payment_options' );
 		}
 		// DEBUG LOG
-		//$this->log( __CLASS__, __FUNCTION__, __LINE__, $transaction );
+		$this->log( __CLASS__, __FUNCTION__, __LINE__, $transaction );
 		/** @type EE_Transaction_Payments $transaction_payments */
 		$transaction_payments = EE_Registry::instance()->load_class( 'Transaction_Payments' );
 		// maybe update status, but don't save transaction just yet
@@ -393,24 +393,24 @@ class EE_Payment_Processor extends EE_Processor_Base {
 			// and if we are all good to go, then send out notifications
 			add_filter( 'FHEE__EED_Messages___maybe_registration__deliver_notifications', '__return_true' );
 			// DEBUG LOG
-			//$this->log( __CLASS__, __FUNCTION__, __LINE__, $transaction );
+			$this->log( __CLASS__, __FUNCTION__, __LINE__, $transaction );
 			//ok, now process the transaction according to the payment
 			$transaction_processor->update_transaction_and_registrations_after_checkout_or_payment( $transaction, $payment );
 		}
 		// DEBUG LOG
-		//$this->log(
-		//	__CLASS__, __FUNCTION__, __LINE__,
-		//	$transaction,
-		//	array(
-		//		'IPN'                   => $IPN,
-		//		'payment_options' => $payment_options_step_completed,
-		//		'finalize_registration' => $finalized,
-		//		'payment'               => $payment,
-		//		'payment_method' => $payment->payment_method() instanceof EE_Payment_Method ? $payment->payment_method
-		//()->name() : 'off-line',
-		//		'deliver_notifications' => has_filter( 'FHEE__EED_Messages___maybe_registration__deliver_notifications' ),
-		//	)
-		//);
+		$this->log(
+			__CLASS__, __FUNCTION__, __LINE__,
+			$transaction,
+			array(
+				'IPN'  => $IPN,
+				'payment_options' => $payment_options_step_completed,
+				'finalized' => $finalized,
+				'payment' => $payment,
+				'payment_method' => $payment->payment_method() instanceof EE_Payment_Method ? $payment->payment_method
+		()->name() : 'off-line',
+				'deliver_notifications' => has_filter( 'FHEE__EED_Messages___maybe_registration__deliver_notifications' ),
+			)
+		);
 	}
 
 
