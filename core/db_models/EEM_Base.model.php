39ed383a69ee98fa7f80a9fade9cf22f728f4aa2
Darren Ethier <darren@roughsmootheng.in>
2015-04-29 14:25:18 -0400

    woops bad addition [touch: 8017]

--- core/db_models/EEM_Base.model.php
+++ core/db_models/EEM_Base.model.php
@@ -909,7 +909,7 @@ abstract class EEM_Base extends EE_Base{
 
 		//set direction
 		$incoming_orderby = isset( $query_params['order_by'] ) ? $query_params['orderby'] : array();
-		$query_params['order_by'] = $operand == '>' ? array( $field_to_order_by => 'ASC' + $incoming_orderby ) : array( $field_to_order_by => 'DESC' + $incoming_orderby );
+		$query_params['order_by'] = $operand == '>' ? array( $field_to_order_by => 'ASC' ) + $incoming_orderby : array( $field_to_order_by => 'DESC') + $incoming_orderby;
 
 		//if $columns_to_select is empty then that means we're returning EE_Base_Class objects
 		if ( empty( $columns_to_select ) ) {
