4ecc88e02ee327633c3d7e603168ddac18a25370
Brent Christensen <brent@eventespresso.com>
2015-05-01 13:10:38 -0700

    move "Events Requiring Pre-Approval" up beside "Important Notice:" header; [touch:8050]

--- modules/single_page_checkout/templates/payment_options/events_requiring_pre_approval.template.php
+++ modules/single_page_checkout/templates/payment_options/events_requiring_pre_approval.template.php
@@ -1,9 +1,8 @@
 
-	<h4 class="orange-text"><?php _e('Important Notice:', 'event_espresso');?></h4>
+	<h4><span class="orange-text"><?php _e('Important Notice:', 'event_espresso');?></span> <?php _e( 'Events Requiring Pre-Approval', 'event_espresso' ); ?></h4>
 	<p id="events-requiring-pre-approval-pg" class="small-text drk-grey-text">
 		<?php echo $events_requiring_pre_approval_msg; ?>
 	</p>
-	<h6><?php _e('Events Requiring Pre-Approval:', 'event_espresso');?></h6>
 	<ul id="spco-pre-approval-events-ul"><?php echo $events_requiring_pre_approval; ?></ul>
 
 	<?php echo $default_hidden_inputs;  ?>
