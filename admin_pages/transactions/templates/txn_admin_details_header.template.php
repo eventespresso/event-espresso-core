5f7c7031dfe65a9ddfad2abb0373334bedf1d56c
Michael Nelson <michael@eventespresso.com>
2015-05-05 21:29:50 -0700

    [touch:7757]{t:30}it seems this got missed during the gateway rewrite

--- admin_pages/transactions/templates/txn_admin_details_header.template.php
+++ admin_pages/transactions/templates/txn_admin_details_header.template.php
@@ -1,13 +1,13 @@
 
 		<h3 style="padding:0;margin:-.5em 0 1em;"><?php echo __( 'Transaction # ', 'event_espresso' ) . $txn_nmbr['value'];?></h3>
-		
+
 		<h2 id="txn-date-h2" style="padding:0;margin:0 0 1em;"><?php echo $txn_datetime['value'];?></h2>
-		
+
 		<?php echo $send_payment_reminder_button; ?>
 		<h2 id="txn-status-h2" style="padding:0;margin:0 0 1em;">
 			<?php echo __( 'Transaction Status: ', 'event_espresso' );?><span id="txn-status" class="<?php echo $txn_status['class'];?>"><?php echo $txn_status['value'];?></span>
 		</h2>
-		
+
 	<?php if ( $amount_due ) : ?>
 		<h2 id="txn-amount-due-h2" style="padding:0;margin:0 0 1em;">
 			<?php echo __( 'Total Amount Due: ', 'event_espresso' );?><span class="<?php echo $amount_due_class;?>"><?php echo $amount_due;?></span>
@@ -17,11 +17,11 @@
 			<?php echo __( 'Total Amount Due: ', 'event_espresso' );?><span class="<?php echo $amount_due_class;?>"><?php echo $amount_due;?></span>
 		</h2>
 	<?php endif; ?>
-				
-	<?php if ( $method_of_payment ) : ?>
+
+	<?php if ( $method_of_payment instanceof EE_Payment_Method ) : ?>
 		<h3 id="txn-selected-method-of-payment-h3" style="padding:0;margin:0 0 1em;">
-			<?php echo __( 'Selected Method of Payment: ', 'event_espresso' );?><?php echo $method_of_payment;?>
+			<?php echo __( 'Selected Method of Payment: ', 'event_espresso' );?><?php echo $method_of_payment->admin_name();?>
 		</h3>
 	<?php endif; ?>
-				
+
 
