d147b6fbcd64e9d6bbe5fe35bf69a40c03a4d863
Michael Nelson <michael@eventespresso.com>
2015-05-13 19:29:22 -0700

    [touch:7008]{t:20}actually lets call this 4.7 DMS too, even though MER is 4.7 for now too

--- core/data_migration_scripts/EE_DMS_Core_4_7_0.dms.php
+++ core/data_migration_scripts/EE_DMS_Core_4_7_0.dms.php
@@ -8,15 +8,15 @@
 //unfortunately, this needs to be done upon INCLUSION of this file,
 //instead of construction, because it only gets constructed on first page load
 //(all other times it gets resurrected from a wordpress option)
-$stages = glob(EE_CORE.'data_migration_scripts/4_8_0_stages/*');
+$stages = glob(EE_CORE.'data_migration_scripts/4_7_0_stages/*');
 $class_to_filepath = array();
 foreach($stages as $filepath){
 	$matches = array();
-	preg_match('~4_8_0_stages/(.*).dmsstage.php~',$filepath,$matches);
+	preg_match('~4_7_0_stages/(.*).dmsstage.php~',$filepath,$matches);
 	$class_to_filepath[$matches[1]] = $filepath;
 }
 //give addons a chance to autoload their stages too
-$class_to_filepath = apply_filters('FHEE__EE_DMS_4_8_0__autoloaded_stages',$class_to_filepath);
+$class_to_filepath = apply_filters('FHEE__EE_DMS_4_7_0__autoloaded_stages',$class_to_filepath);
 EEH_Autoloader::register_autoloader($class_to_filepath);
 
 
@@ -24,7 +24,7 @@ EEH_Autoloader::register_autoloader($class_to_filepath);
 
 
 /**
- * Class EE_DMS_Core_4_8_0
+ * Class EE_DMS_Core_4_7_0
  *
  * @package            Event Espresso
  * @subpackage    core
@@ -32,25 +32,16 @@ EEH_Autoloader::register_autoloader($class_to_filepath);
  * @since                4.6.0
  *
  */
-class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
+class EE_DMS_Core_4_7_0 extends EE_Data_Migration_Script_Base{
 
 	/**
-	 * return EE_DMS_Core_4_8_0
+	 * return EE_DMS_Core_4_7_0
 	 */
 	public function __construct() {
-		$this->_pretty_name = __("Data Migration to Event Espresso 4.6.0.P", "event_espresso");
+		$this->_pretty_name = __("Data Migration to Event Espresso 4.7.0.P (for promotions)", "event_espresso");
 		$this->_priority = 10;
 		$this->_migration_stages = array(
-			new EE_DMS_4_8_0_pretax_totals()
-//			new EE_DMS_4_8_0_gateways(),
-//			new EE_DMS_4_8_0_payment_method_currencies(),
-//			new EE_DMS_4_8_0_question_types(),
-//			new EE_DMS_4_8_0_country_system_question(),
-//			new EE_DMS_4_8_0_state_system_question(),
-//			new EE_DMS_4_8_0_billing_info(),
-//			new EE_DMS_4_8_0_transactions(),
-//			new EE_DMS_4_8_0_payments(),
-//			new EE_DMS_4_8_0_invoice_settings()
+			new EE_DMS_4_7_0_pretax_totals()
 		);
 		parent::__construct();
 	}
@@ -58,6 +49,10 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
 
 
 	/**
+	 * Because this is being done at basically the same time as the MER-ready branch
+	 * of core, it's possible people might have installed MEr-ready branch first,
+	 * and then this one, in whcih case we still want to perform this migration,
+	 * even though the version might not have increased
 	 * @param array $version_array
 	 * @return bool
 	 */
