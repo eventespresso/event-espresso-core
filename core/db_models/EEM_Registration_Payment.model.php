ba5877ce07e2e40666a58d1ee784f63afce1ad5c
Brent Christensen <brent@eventespresso.com>
2015-05-14 22:16:28 -0700

    fix primary key; [touch:8071]

--- core/db_models/EEM_Registration_Payment.model.php
+++ core/db_models/EEM_Registration_Payment.model.php
@@ -21,12 +21,12 @@ class EEM_Registration_Payment extends EEM_Base {
 		$this->plural_item 	= __( 'Registration Payments', 'event_espresso' );
 
 		$this->_tables = array(
-			'Registration_Payment' => new EE_Primary_Table( 'esp_registration_payment', 'PRL_ID' )
+			'Registration_Payment' => new EE_Primary_Table( 'esp_registration_payment', 'RPY_ID' )
 		);
 
 		$this->_fields = array(
 			'Registration_Payment'=>array(
-				'RPY_ID' 	=> new EE_Primary_Key_Int_Field( 'PRL_ID', __( 'Registration Payment ID', 'event_espresso' )),
+				'RPY_ID' 	=> new EE_Primary_Key_Int_Field( 'RPY_ID', __( 'Registration Payment ID', 'event_espresso' )),
 				'REG_ID' 	=> new EE_Foreign_Key_Int_Field( 'REG_ID', __( 'Registration ID', 'event_espresso' ), false, 0, 'Registration' ),
 				'PAY_ID' 	=> new EE_Foreign_Key_Int_Field( 'PAY_ID', __( 'Payment ID', 'event_espresso' ), true, null, 'Payment' ),
 			)
