e41cf58804b995f42f5a23efbcf1263824c35bf0
Darren Ethier <darren@roughsmootheng.in>
2015-06-04 13:09:58 -0400

    remove title [touch: 6145]

--- public/Espresso_Arabica_2014/loop-espresso_event_attendees.php
+++ public/Espresso_Arabica_2014/loop-espresso_event_attendees.php
@@ -16,24 +16,11 @@
  */
 
 $no_attendees_message =  apply_filters( 'FHEE__loop-espresso_attendees-shortcode__template__no_attendees_message', __('No Attendees Yet', 'event_espresso' ) );
-//setup title
-if ( $datetime instanceof EE_Datetime && $event instanceof EE_Event ) {
-	$title = sprintf( __( 'Attendees for %s and %s', 'event_espresso' ), $event->name(), $datetime->get_dtt_display_name( true ) );
-} elseif ( $ticket instanceof EE_Ticket && $event instanceof EE_Event ) {
-	$title = sprintf( __( 'Attendees for %s and the ticket %s', 'event_espresso' ), $event->name(), $ticket->name() );
-} elseif ( $event instanceof EE_Event ) {
-	$title = sprintf( __( 'Attendees for %s', 'event_espresso' ), $event->name() );
-} else {
-	$title = __( 'Attendees:' );
-}
 
-//filter the title
-$title = apply_filters( 'FHEE__loop-espresso_attendees-shortcode__template__title', $title, $event, $datetime, $ticket, $contacts );
 
 ?>
 
 <div class="event-attendees">
-	<h2 id="event-attendees-details-h2" class="entry-title"><?php echo $title; ?></h2>
 	<?php do_action( 'AHEE__loop-espresso_event_attendees__before', $contacts, $event, $datetime, $ticket, $show_gravatar ); ?>
 	<?php if ( $contacts ) : ?>
 		<ul class="event-attendees-list">
