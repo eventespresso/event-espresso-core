42617a0072cd749736acb5c5ca2aee95e409b17e
Brent Christensen <brent@eventespresso.com>
2015-06-16 13:32:57 -0700

    ARGH!!! use load_core() NOT load_class(); simulate IPN and nothing else; [touch:7953]

--- core/EE_Cron_Tasks.core.php
+++ core/EE_Cron_Tasks.core.php
@@ -295,9 +295,9 @@ class EE_Cron_Tasks extends EE_BASE {
 			// set revisit flag for txn processor
 			$transaction_processor->set_revisit( false );
 			/** @type EE_Transaction_Payments $transaction_payments */
-			//$transaction_payments = EE_Registry::instance()->load_class( 'Transaction_Payments' );
+			$transaction_payments = EE_Registry::instance()->load_class( 'Transaction_Payments' );
 			/** @type EE_Payment_Processor $payment_processor */
-			$payment_processor = EE_Registry::instance()->load_class( 'Payment_Processor' );
+			$payment_processor = EE_Registry::instance()->load_core( 'Payment_Processor' );
 			// load EEM_Transaction
 			EE_Registry::instance()->load_model( 'Transaction' );
 			foreach ( self::$_abandoned_transactions as $TXN_ID ) {
@@ -314,27 +314,8 @@ class EE_Cron_Tasks extends EE_BASE {
 				$transaction = EEM_Transaction::instance()->get_one_by_ID( $TXN_ID );
 				// verify transaction
 				if ( $transaction instanceof EE_Transaction ) {
-					// or have had all of their reg steps completed
-					if ( $transaction_processor->all_reg_steps_completed(	$transaction ) === true ) {
-						continue;
-					}
+					// let's simulate an IPN here which will trigger any notifications that need to go out
 					$payment_processor->update_txn_based_on_payment( $transaction, $transaction->last_payment(), true, true );
-					//$transaction_processor->set_old_txn_status( $transaction->status_ID() );
-					//do_action( 'AHEE_log', __CLASS__, __FUNCTION__, $transaction->status_ID(), '$transaction->status_ID()' );
-					//// maybe update status, but don't save transaction just yet
-					//$transaction_payments	->update_transaction_status_based_on_total_paid( $transaction, false );
-					//do_action( 'AHEE_log', __CLASS__, __FUNCTION__, $transaction->status_ID(), '$transaction->status_ID()' );
-					//do_action( 'AHEE_log', __CLASS__, __FUNCTION__, implode( ', ', $transaction->reg_steps() ), '$transaction->reg_steps()' );
-					//// check if enough Reg Steps have been completed to warrant finalizing the TXN
-					//if ( $transaction_processor->all_reg_steps_completed_except_final_step( $transaction ) ) {
-					//	// and if it hasn't already been set as being started...
-					//	$transaction_processor->set_reg_step_initiated( $transaction, 'finalize_registration' );
-					//}
-					//// now update the TXN and trigger notifications
-					//$transaction_processor->update_transaction_and_registrations_after_checkout_or_payment(
-					//	$transaction,
-					//	$transaction->last_payment()
-					//);
 				}
 				unset( self::$_abandoned_transactions[ $TXN_ID ] );
 			}
