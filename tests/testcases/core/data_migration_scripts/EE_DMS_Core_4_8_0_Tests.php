1a4209c159d60f3c2e8b1afa1111fb01e2472a80
Brent Christensen <brent@eventespresso.com>
2015-06-23 15:02:40 -0700

    fix line item code; [touch:8389]

--- tests/testcases/core/data_migration_scripts/EE_DMS_Core_4_8_0_Tests.php
+++ tests/testcases/core/data_migration_scripts/EE_DMS_Core_4_8_0_Tests.php
@@ -20,13 +20,13 @@ class EE_DMS_Core_4_8_0_Tests extends EE_UnitTestCase{
 		$script = EE_Registry::instance()->load_dms( 'EE_DMS_Core_4_8_0' );
 		$stage = new EE_DMS_4_8_0_pretax_totals();
 
-		//ok let's create a line item to with the LIN_code='tickets'
-		$tickets_subtottal = $this->new_model_obj_with_dependencies( 'Line_Item', array( 'LIN_code' => 'tickets' ) );
-//and anotehr that DOESN"T match it
+		// ok let's create a line item to with the LIN_code='tickets'
+		$tickets_subtotal = $this->new_model_obj_with_dependencies( 'Line_Item', array( 'LIN_code' => 'tickets' ) );
+		// and another that DOESN"T match it
 		$other_line_item = $this->new_model_obj_with_dependencies( 'Line_Item', array( 'LIN_code' => 'not-tickets' ) );
 		$stage->migration_step();
-		$new_tickets_subtotal = EEM_Line_Item::instance()->refresh_entity_map_from_db( $tickets_subtottal->ID() );
-		$this->assertEquals( 'pre-tax-total', $new_tickets_subtotal->get( 'LIN_code' ) );
+		$new_tickets_subtotal = EEM_Line_Item::instance()->refresh_entity_map_from_db( $tickets_subtotal->ID() );
+		$this->assertEquals( 'pre-tax-subtotal', $new_tickets_subtotal->get( 'LIN_code' ) );
 
 		$new_other_line_item = EEM_Line_Item::instance()->refresh_entity_map_from_db( $other_line_item->ID() );
 		$this->assertEquals( 'not-tickets', $new_other_line_item->get( 'LIN_code' ) );
