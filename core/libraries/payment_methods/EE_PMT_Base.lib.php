928ff5fe0ba455b1b53ecd7e16a8eb27261a86ec
Brent Christensen <brent@eventespresso.com>
2015-07-07 16:47:07 -0700

    don't set _has_billing_form where it is declared, but instead check in constructor if child class has set it's value, and hten if not, set a default based on the gateway type; [touch:8141]

--- core/libraries/payment_methods/EE_PMT_Base.lib.php
+++ core/libraries/payment_methods/EE_PMT_Base.lib.php
@@ -33,7 +33,7 @@ abstract class EE_PMT_Base{
 	/**
 	 * @var boolean
 	 */
-	protected $_has_billing_form = FALSE;
+	protected $_has_billing_form;
 
 	/**
 	 * @var EE_Gateway
@@ -110,9 +110,9 @@ abstract class EE_PMT_Base{
 			EE_Registry::instance()->load_helper( 'Money' );
 			$this->_gateway->set_money_helper( new EEH_Money() );
 		}
-		// by default, On Site gateways have a billing form
-		if ( $this->payment_occurs() == EE_PMT_Base::onsite ) {
-			$this->_has_billing_form = true;
+		if ( ! isset( $this->_has_billing_form ) ) {
+			// by default, On Site gateways have a billing form
+			$this->_has_billing_form = $this->payment_occurs() == EE_PMT_Base::onsite ? true : false;
 		}
 
 		if( ! $this->_pretty_name){
