b5fa167e41dfd36133530e3e000c48edbb841304
Darren Ethier <darren@roughsmootheng.in>
2015-07-17 21:14:56 -0400

    add 'message' as an allowable enum value for EEM_Status::STS_type [touch: 2195]

--- core/db_models/EEM_Status.model.php
+++ core/db_models/EEM_Status.model.php
@@ -48,7 +48,8 @@ class EEM_Status extends EEM_Base {
 							'registration'=>  __("Registration", "event_espresso"),
 							'transaction'=>  __("Transaction", "event_espresso"),
 							'payment'=>  __("Payment", "event_espresso"),
-							'email'=>  __("Email", "event_espresso")
+							'email'=>  __("Email", "event_espresso"),
+							'message' => __("Message", "event_espresso")
 						)),
 				'STS_can_edit'=>new EE_Boolean_Field('STS_can_edit', __('Editable?','event_espresso'), false),
 				'STS_desc'=>new EE_Simple_HTML_Field('STS_desc', __("Description", "event_espresso"), false, ''),
