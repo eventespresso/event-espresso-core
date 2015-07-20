15fdc0095b306cb1b2d0ff1f04d669f877384668
Darren Ethier <darren@roughsmootheng.in>
2015-07-20 10:27:45 -0400

    flesh out the methods for the EE_Messages_Data_Handler repository [touch: 2195]

--- core/repositories/EE_Messages_Data_Handler_Repository.php
+++ core/repositories/EE_Messages_Data_Handler_Repository.php
@@ -12,4 +12,76 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  */
 class EE_Messages_Data_Handler_Repository extends EE_Object_Repository {
 
+
+
+
+
+	/**
+	 * This adds the EE_Messages_incoming_data data handler object to the repository.
+	 *
+	 * @param EE_Messages_incoming_data  $data_handler
+	 * @param mixed                      $data           Usually an array of data used in combination with the $data_handler
+	 *                                                   classname to create an alternative index for retrieving data_handlers.
+	 * @return bool
+	 */
+	public function add( EE_Messages_incoming_data $data_handler, $data ) {
+		$info['key'] = $this->get_key( get_class( $data_handler ), $data );
+		return $this->addObject( $data_handler, $info );
+	}
+
+
+
+
+
+
+	/**
+	 * Remove given EE_Messages_incoming_data from repository.
+	 * @param EE_Messages_incoming_data $data_handler
+	 */
+	public function remove( EE_Messages_incoming_data $data_handler ) {
+		$this->removeObject( $data_handler );
+	}
+
+
+
+
+	/**
+	 * This returns a key for retrieving data for the given references used to generate the key.
+	 * Data handlers are cached to the repository along with a md5() generated key using known references.
+	 * @param string    $classname      The classname of the datahandler being checked for.
+	 * @param mixed     $data           The data that was used to instantiate the data_handler.
+	 *
+	 * @return  string      md5 hash using provided info.
+	 */
+	public function get_key( $classname, $data ) {
+		return md5( $classname . print_r( $data ) );
+	}
+
+
+
+
+
+
+	/**
+	 * This returns a saved EE_Messages_incoming_data object if there is one in the repository indexed by a key matching
+	 * the given string.
+	 *
+	 * @param string  $key  @see EE_Messages_Data_Handler_Repository::get_key() to setup a key formatted for searching.
+	 *
+	 * @return null|EE_Messages_incoming_data
+	 */
+	public function get_handler_by_key( $key ) {
+		$this->rewind();
+		while( $this->valid() ) {
+			$data = $this->getInfo();
+			if ( isset( $data['key'] ) && $data['key'] === $key ) {
+				$handler = $this->current();
+				$this->rewind();
+				return $handler;
+			}
+			$this->next();
+		}
+		return null;
+	}
+
 }
