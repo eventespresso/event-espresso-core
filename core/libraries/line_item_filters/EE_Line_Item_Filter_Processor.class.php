9fda90792703adff64059dd59bb5e5443781f302
Brent Christensen <brent@eventespresso.com>
2015-07-29 09:43:44 -0700

    throw exception if constructor is not passed a grand total line item; add clone_and_reset_line_item_tree() and clone_and_reset_line_item() methods; run incoming grand total line item through clone_and_reset_line_item_tree() during construction; [touch:8526]

--- core/libraries/line_item_filters/EE_Line_Item_Filter_Processor.class.php
+++ core/libraries/line_item_filters/EE_Line_Item_Filter_Processor.class.php
@@ -30,11 +30,53 @@ class EE_Line_Item_Filter_Processor {
 	/**
 	 * EE_Line_Item_Filter_Processor constructor.
 	 * @param \EE_Line_Item_Filter_Collection $line_item_filters
-	 * @param \EEI_Line_Item         $grand_total_line_item
+	 * @param \EEI_Line_Item                  $grand_total_line_item
+	 * @throws \EE_Error
 	 */
 	public function __construct( EE_Line_Item_Filter_Collection $line_item_filters, EEI_Line_Item $grand_total_line_item ) {
 		$this->line_item_filters = $line_item_filters;
-		$this->grand_total_line_item = clone( $grand_total_line_item );
+		if ( $grand_total_line_item->type() !== EEM_Line_Item::type_total ) {
+			throw new EE_Error( __( 'A Line Item of the type total is required', 'event_espresso' ) );
+		}
+		$this->grand_total_line_item = $this->clone_and_reset_line_item_tree( $grand_total_line_item );
+	}
+
+
+
+	/**
+	 * clone_and_reset_line_item_tree
+	 *
+	 * @param \EEI_Line_Item $line_item
+	 * @return \EEI_Line_Item
+	 */
+	protected function clone_and_reset_line_item_tree( EEI_Line_Item $line_item ) {
+		$cloned_line_item = $this->clone_and_reset_line_item( $line_item );
+		foreach ( $line_item->children() as $child_line_item ) {
+			$cloned_line_item->add_child_line_item( $this->clone_and_reset_line_item_tree( $child_line_item ) );
+		}
+		return $cloned_line_item;
+	}
+
+
+
+	/**
+	 * clone_and_reset_line_item
+	 *
+	 * clones the incoming object
+	 * resets any fields that represent database primary keys
+	 * resets total
+	 *
+	 * @param \EEI_Line_Item $line_item
+	 * @return \EEI_Line_Item
+	 */
+	protected function clone_and_reset_line_item( EEI_Line_Item $line_item ) {
+		// we don't actually want to work with the original line item, so clone it
+		$cloned_line_item = clone( $line_item );
+		// reset the LIN_ID, LIN_parent, and LIN_total
+		$cloned_line_item->set( 'LIN_ID', null );
+		$cloned_line_item->set( 'LIN_parent', null );
+		$cloned_line_item->set( 'LIN_total', null );
+		return $cloned_line_item;
 	}
 
 
@@ -52,6 +94,7 @@ class EE_Line_Item_Filter_Processor {
 		}
 		return $this->grand_total_line_item;
 	}
+
 
 
 }
