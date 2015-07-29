398f2f3179a9e51f2a63e8c05c9517d48120a1bb
Brent Christensen <brent@eventespresso.com>
2015-07-29 11:40:43 -0700

    make collection classes more DRY by moving interface methods to the parent class; [touch:8526]

--- core/libraries/line_item_filters/EE_Line_Item_Filter_Collection.class.php
+++ core/libraries/line_item_filters/EE_Line_Item_Filter_Collection.class.php
@@ -10,89 +10,21 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
  * @package               Event Espresso
  * @subpackage            core
  * @author                Brent Christensen
- * @since                 $VID:$
+ * @since                 4.8.0
+ *
+ *
  *
  */
-class EE_Line_Item_Filter_Collection extends EE_Object_Collection implements EEI_Object_Collection {
-
-	/**
-	 * add
-	 *
-	 * attaches an object to the Collection
-	 * and sets any supplied data associated with the current iterator entry
-	 * by calling EE_Line_Item_Filter_Collection::set_info()
-	 *
-	 * @access public
-	 * @param EEI_Line_Item_Filter $object
-	 * @param mixed  $info
-	 * @return bool
-	 */
-	public function add( $object, $info = null ) {
-		if ( ! $object instanceof EEI_Line_Item_Filter ) {
-			return false;
-		}
-		return $this->addObject( $object, $info );
-	}
+class EE_Line_Item_Filter_Collection extends EE_Object_Collection {
 
 
 
 	/**
-	 * set_info
-	 *
-	 * Sets the info associated with an object in the Collection
-	 *
-	 * @access public
-	 * @param EEI_Line_Item_Filter $object
-	 * @param mixed  $info
-	 * @return bool
+	 * EE_Line_Item_Filter_Collection constructor.
+	 * @param $interface
 	 */
-	public function set_info( $object, $info = null ) {
-		return $this->setObjectInfo( $object, $info );
-	}
-
-
-
-	/**
-	 * get_by_info
-	 *
-	 * finds and returns an object in the Collection based on the info that was set using set_info() or add()
-	 *
-	 * @access public
-	 * @param mixed
-	 * @return EEI_Line_Item_Filter
-	 */
-	public function get_by_info( $info ) {
-		return $this->getObjectByInfo( $info );
-	}
-
-
-
-	/**
-	 * has
-	 *
-	 * returns TRUE or FALSE depending on whether the supplied object is within the Collection
-	 *
-	 * @access public
-	 * @param object $object
-	 * @return bool
-	 */
-	public function has( $object ) {
-		return $this->hasObject( $object );
-	}
-
-
-
-	/**
-	 * remove
-	 *
-	 * detaches an object from the Collection
-	 *
-	 * @access public
-	 * @param $object
-	 * @return void
-	 */
-	public function remove( $object ) {
-		$this->removeObject( $object );
+	public function __construct( $interface ) {
+		$this->interface = 'EEI_Line_Item_Filter';
 	}
 
 
