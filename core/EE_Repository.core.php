b6d592688ef3808664caaf827bc86b67ae99e968
Brent Christensen <brent@eventespresso.com>
2015-07-29 15:59:13 -0700

    fix method name and scope; [touch:8578]

--- core/EE_Repository.core.php
+++ core/EE_Repository.core.php
@@ -20,21 +20,21 @@ abstract class EE_Repository extends EE_Collection implements EEI_Repository {
 
 
 	/**
-	 * persistObject
+	 * persist
 	 *
 	 * primarily used for saving EE_Base_Class classes to the database,
 	 * but can be supplied with a "persistence callback" that can be used for classes that are not instances of EE_Base_Class,
 	 * or for providing alternate ways to persist an object such as session caching, etc...
 	 * an array of arguments can also be supplied that will be passed along to the object's persistence method
 	 *
-	 * @access protected
+	 * @access public
 	 * @param object 	$object
 	 * @param string 	$persistence_callback 		name of method found on object that can be used for persisting the object
 	 * @param array 	$persistence_arguments	arrays of arguments that will be passed to the object's persistence method
 	 * @return bool | int
 	 * @throws \EE_Error
 	 */
-	protected function persistObject( $object, $persistence_callback = '', $persistence_arguments = array() ) {
+	public function persist( $object, $persistence_callback = '', $persistence_arguments = array() ) {
 		if ( $this->contains( $object ) ) {
 			$this->rewind();
 			while ( $this->valid() ) {
