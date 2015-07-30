cd047407f5309df8cc33d104dad7290100273052
Michael Nelson <michael@eventespresso.com>
2015-07-30 10:46:38 -0700

    [touch:8589]{t:30}added unit test that shows inconsistent behaviour

--- tests/testcases/core/db_classes/EE_Base_Class_Test.php
+++ tests/testcases/core/db_classes/EE_Base_Class_Test.php
@@ -588,7 +588,7 @@ class EE_Base_Class_Test extends EE_UnitTestCase{
 	/**
 	 * @group github-102
 	 */
-	public function test_get__serialized_data() {
+	public function test_get__serialized_data__once() {
 		$log_message = array(
 						'key1' => 'value1',
 						'key2' => 'value2'
@@ -609,10 +609,33 @@ class EE_Base_Class_Test extends EE_UnitTestCase{
 		$this->assertTrue( is_array( $log_from_db->get( 'LOG_message' ) ) );
 		$this->assertEquals( $log_message, $log_from_db->get( 'LOG_message' ) );
 
-		//but if you set it to be a string, you'll get a string back
-		$log_from_db->set( 'LOG_message', serialize( $log_message ) );
-		$this->assertTrue( is_string( $log_from_db->get( 'LOG_message' ) ) );
 	}
+
+		/**
+	 * @group github-102
+	 */
+	public function test_get__serialized_data__twice() {
+		$log_message = serialize( array(
+						'key1' => 'value1',
+						'key2' => 'value2'
+					) );
+		$log = EE_Change_Log::new_instance();
+		$log->set( 'LOG_message', $log_message );
+		$log->save();
+
+		//verify that when we get its LOG_message its still serialized
+		$this->assertTrue( is_array( $log->get( 'LOG_message' ) ) );
+		$this->assertEquals( unserialize( $log_message ), $log->get( 'LOG_message' ) );
+
+		//now when we get it from the DB, and get its LOG_message, its still serialized
+		$log_id = $log->ID();
+		EEM_Change_Log::reset();
+		unset( $log );
+		$log_from_db = EEM_Change_Log::instance()->get_one_by_ID( $log_id );
+		$this->assertTrue( is_array( $log_from_db->get( 'LOG_message' ) ) );
+		$this->assertEquals( unserialize( $log_message ), $log_from_db->get( 'LOG_message' ) );
+	}
+
 
 }
 
