caa3d06520355c34d4117a8a777816fd4d7d172a
Brent Christensen <brent@eventespresso.com>
2015-07-30 18:49:18 -0700

    fix test_persist() and and test_persist_all(); [touch:8578]

--- tests/testcases/core/EE_Object_Repository_Test.php
+++ tests/testcases/core/EE_Object_Repository_Test.php
@@ -27,10 +27,36 @@ class EE_Object_Repository_Test extends EE_UnitTestCase {
 		$ticket_1 = $this->new_model_obj_with_dependencies( 'Ticket', array( 'TKT_price'   => '5' ), false );
 		$this->repository->add( $ticket_1 );
 		$this->assertEquals( $ticket_1->ID(), 0 );
-		$saved = $this->repository->persist( $ticket_1 );
+		$saved = false;
+		$this->repository->rewind();
+		while ( $this->repository->valid() ) {
+			if ( $this->repository->current() === $ticket_1 ) {
+				$saved = $this->repository->persist();
+				break;
+			}
+			$this->repository->next();
+		}
 		$this->assertNotEquals( $saved, false );
 		$this->assertNotEquals( $ticket_1->ID(), 0 );
 	}
+
+
+
+
+	public function test_persist_all() {
+		$ticket_1 = $this->new_model_obj_with_dependencies( 'Ticket', array( 'TKT_price'   => '5' ), false );
+		$ticket_2 = $this->new_model_obj_with_dependencies( 'Ticket', array( 'TKT_price'   => '6' ), false );
+		$this->repository->add( $ticket_1 );
+		$this->repository->add( $ticket_2 );
+		$this->assertEquals( $ticket_1->ID(), 0 );
+		$this->assertEquals( $ticket_2->ID(), 0 );
+		$saved = $this->repository->persist_all();
+		$this->assertEquals( $saved, true );
+		$this->assertNotEquals( $ticket_1->ID(), 0 );
+		$this->assertNotEquals( $ticket_2->ID(), 0 );
+	}
+
+
 
 }
 // End of file EE_Object_Repository_Test.php
