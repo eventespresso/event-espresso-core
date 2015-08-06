046b48587c1dbc475961140039731f887fc551e3
Michael Nelson <michael@eventespresso.com>
2015-08-06 11:06:21 -0700

    [touch:8572]{t:45}add unit test that shows problem sometimes calculating flat discounts share of regs price when there are price modifiers

--- tests/testcases/core/libraries/line_item_filters/EE_Specific_Registrations_Line_Item_Filter_Test.php
+++ tests/testcases/core/libraries/line_item_filters/EE_Specific_Registrations_Line_Item_Filter_Test.php
@@ -146,8 +146,105 @@ class EE_Specific_Registrations_Line_Item_Filter_Test extends EE_UnitTestCase{
 	}
 
 
+	function test_process__1_taxless_ticket_with_modifier_and_fixed_discount() {
+		$grand_total = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_total,
+			'LIN_name' => 'Total',
+		));
+		$pretax_total = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_sub_total,
+			'LIN_code' => 'pretax',
+			'LIN_name' => 'pretax',
+			'LIN_order' => 1,
+		));
+		$grand_total->add_child_line_item( $pretax_total );
+
+		$tax_total = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_tax_sub_total,
+			'LIN_code' => 'tax-total',
+			'LIN_name' => 'tax-total',
+			'LIN_order' => 1000
+		));
+		$grand_total->add_child_line_item( $tax_total );
+		$event_subtotal =  EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_sub_total,
+			'LIN_code' => 'event1',
+			'LIN_name' => 'event1',
+		) );
+		$pretax_total->add_child_line_item( $event_subtotal );
+
+		$ticket_quantities = array(
+			1 => array(
+				'included' => 5,
+				'not' => 5
+			)
+		);
+		$ticket_li = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_line_item,
+			'LIN_code' => 'ticket1',
+			'LIN_name' => 'ticket1',
+			'OBJ_ID' => 1,
+			'OBJ_type' => 'Ticket',
+			'LIN_unit_price' => 10,
+			'LIN_quantity' => 10,
+			'LIN_total' => 100,
+		));
+		$event_subtotal->add_child_line_item( $ticket_li );
+
+		$base_price_li = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_sub_line_item,
+			'LIN_code' => 'price1',
+			'LIN_name' => 'price1',
+			'OBJ_ID' => 1,
+			'OBJ_type' => 'Price',
+			'LIN_unit_price' => 9,
+			'LIN_quantity' => 10,
+			'LIN_total' => 90,
+		));
+		$ticket_li->add_child_line_item( $base_price_li );
+
+		$modifier_li = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_sub_line_item,
+			'LIN_code' => 'price2',
+			'LIN_name' => 'price2',
+			'OBJ_ID' => 2,
+			'OBJ_type' => 'Price',
+			'LIN_unit_price' => 1,
+			'LIN_quantity' => 10,
+			'LIN_total' => 10,
+		));
+		$ticket_li->add_child_line_item( $modifier_li );
+
+		$percent_discount_li = EE_Line_Item::new_instance( array(
+			'LIN_type' => EEM_Line_Item::type_line_item,
+			'LIN_code' => 'discount1',
+			'LIN_name' => 'discount1',
+			'LIN_unit_price' => -50,
+			'LIN_percent' => 0,
+			'LIN_total' => -50,
+			'LIN_quantity' => 1
+		));
+		$event_subtotal->add_child_line_item( $percent_discount_li );
+		//also need to make registrations
+		$regs_to_include = $this->_create_regs( $ticket_quantities, $grand_total );
+		//ok now let's use the filter
+		$filter = new EE_Specific_Registrations_Line_Item_Filter( $regs_to_include );
+		$filtered_total = $filter->process( $grand_total );
+		//the filter doesn't recalculate totals, and it edits the inputted tree;
+		//hat's ok, the processor does both of those. But we need to manually do it here
+		$this->assertEquals( $grand_total, $filtered_total );
+		$grand_total->recalculate_total_including_taxes();
+		$this->assertEquals( 5, $ticket_li->quantity() );
+		$this->assertEquals( 10, $ticket_li->unit_price() );
+		$this->assertEquals( 50, $ticket_li->total() );
+		//and verify the percent line item's total has changed, but not its percent
+		$this->assertEquals( -25, $percent_discount_li->unit_price() );
+		$this->assertEquals( -25, $percent_discount_li->total() );
+	}
+
+
 /**
- * 
+ *
  */
 function test_process__2_events_some_taxed_with_discounts() {
 		$grand_total = EE_Line_Item::new_instance( array(
