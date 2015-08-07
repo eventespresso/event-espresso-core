71bf57e9f25b365c4fa1762b94e4789a9c94d3de
Darren Ethier <darren@roughsmootheng.in>
2015-08-07 15:41:08 -0400

    move esp_message creation into 4_9_0 dms [touch: 2195]

--- core/data_migration_scripts/EE_DMS_Core_4_9_0.dms.php
+++ core/data_migration_scripts/EE_DMS_Core_4_9_0.dms.php
@@ -1,25 +1,20 @@
 <?php
 /**
- * meant to convert DBs from 4.6 (OR 4.7, which basically supports MER and wasn't clear if it was
- * going to be released before this version) to 4.8 (which basiclaly supports promotions)
- * mostly just
- * -refacotrs line item trees, so that there are subtotals for EACH event purchased,
- * which is especially convenient for applying event-wide promotions
- * -does NOT actually make any database schema changes
+ * Meant to add the new ee_message table to the database.
  */
 //make sure we have all the stages loaded too
 //unfortunately, this needs to be done upon INCLUSION of this file,
 //instead of construction, because it only gets constructed on first page load
 //(all other times it gets resurrected from a wordpress option)
-$stages = glob(EE_CORE.'data_migration_scripts/4_8_0_stages/*');
+$stages = glob(EE_CORE.'data_migration_scripts/4_9_0_stages/*');
 $class_to_filepath = array();
 foreach($stages as $filepath){
 	$matches = array();
-	preg_match('~4_8_0_stages/(.*).dmsstage.php~',$filepath,$matches);
+	preg_match('~4_9_0_stages/(.*).dmsstage.php~',$filepath,$matches);
 	$class_to_filepath[$matches[1]] = $filepath;
 }
 //give addons a chance to autoload their stages too
-$class_to_filepath = apply_filters('FHEE__EE_DMS_4_8_0__autoloaded_stages',$class_to_filepath);
+$class_to_filepath = apply_filters('FHEE__EE_DMS_4_9_0__autoloaded_stages',$class_to_filepath);
 EEH_Autoloader::register_autoloader($class_to_filepath);
 
 
@@ -27,7 +22,7 @@ EEH_Autoloader::register_autoloader($class_to_filepath);
 
 
 /**
- * Class EE_DMS_Core_4_8_0
+ * Class EE_DMS_Core_4_9_0
  *
  * @package            Event Espresso
  * @subpackage    core
@@ -35,37 +30,31 @@ EEH_Autoloader::register_autoloader($class_to_filepath);
  * @since                4.6.0
  *
  */
-class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
+class EE_DMS_Core_4_9_0 extends EE_Data_Migration_Script_Base{
 
 	/**
-	 * return EE_DMS_Core_4_8_0
+	 * return EE_DMS_Core_4_9_0
 	 */
 	public function __construct() {
-		$this->_pretty_name = __("Data Migration to Event Espresso 4.8.0.P (for promotions)", "event_espresso");
+		$this->_pretty_name = __("Data Migration to Event Espresso 4.9.0.P", "event_espresso");
 		$this->_priority = 10;
-		$this->_migration_stages = array(
-			new EE_DMS_4_8_0_pretax_totals(),
-			new EE_DMS_4_8_0_event_subtotals(),
-		);
+		$this->_migration_stages = array();
 		parent::__construct();
 	}
 
 
 
 	/**
-	 * Because this is being done at basically the same time as the MER-ready branch
-	 * of core, it's possible people might have installed MEr-ready branch first,
-	 * and then this one, in which case we still want to perform this migration,
-	 * even though the version might not have increased
+	 * Whether to migrate or not.
 	 * @param array $version_array
 	 * @return bool
 	 */
 	public function can_migrate_from_version($version_array) {
 		$version_string = $version_array['Core'];
-		if( $version_string <= '4.8.0' && $version_string >= '4.7.0' ){
+		if( $version_string <= '4.9.0' && $version_string >= '4.8.0' ){
 //			echo "$version_string can be migrated from";
 			return true;
-		}elseif( ! $version_string ){
+		} elseif( ! $version_string ){
 //			echo "no version string provided: $version_string";
 			//no version string provided... this must be pre 4.3
 			return false;//changed mind. dont want people thinking they should migrate yet because they cant
@@ -81,7 +70,7 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
 	 * @return string|void
 	 */
 	public function pretty_name() {
-		return __("Core Data Migration to version 4.8.0", "event_espresso");
+		return __("Core Data Migration to version 4.9.0", "event_espresso");
 	}
 
 
@@ -91,6 +80,40 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
 	 */
 	public function schema_changes_before_migration() {
 		require_once( EE_HELPERS . 'EEH_Activation.helper.php' );
+		$table_name = 'esp_message';
+		$sql="MSG_ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+					GRP_ID INT(10) UNSIGNED NULL,
+					MSG_token VARCHAR(255) NULL,
+					TXN_ID INT(10) UNSIGNED NULL,
+					MSG_messenger VARCHAR(30) NOT NULL,
+					MSG_message_type VARCHAR(50) NOT NULL,
+					MSG_context VARCHAR(50),
+					MSG_recipient_ID BIGINT(20) NULL,
+					MSG_recipient_type VARCHAR(45) NULL,
+					MSG_content TEXT NULL,
+					MSG_to VARCHAR(255) NULL,
+					MSG_from VARCHAR(255) NULL,
+					MSG_subject VARCHAR(255) NULL,
+					MSG_priority TINYINT(1) NOT NULL DEFAULT 3,
+					STS_ID VARCHAR(3) NOT NULL DEFAULT 'MIC',
+					MSG_created DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+					MSG_modified DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+					PRIMARY KEY  (MSG_ID),
+					KEY GRP_ID (GRP_ID),
+					KEY TXN_ID (TXN_ID),
+					KEY MSG_messenger (MSG_messenger),
+					KEY MSG_message_type (MSG_message_type),
+					KEY MSG_context (MSG_context),
+					KEY MSG_recipient_ID (MSG_recipient_ID),
+					KEY MSG_recipient_type (MSG_recipient_type),
+					KEY MSG_to (MSG_to),
+					KEY MSG_from (MSG_from),
+					KEY MSG_subject (MSG_subject),
+					KEY MSG_priority (MSG_priority),
+					KEY STS_ID (STS_ID),
+					KEY MSG_created (MSG_created),
+					KEY MSG_modified (MSG_modified)";
+		$this->_table_is_new_in_this_version($table_name, $sql, 'ENGINE=InnoDB' );
 
 		$table_name='esp_answer';
 		$sql=" ANS_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -469,7 +492,7 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
 					  STS_open TINYINT(1) NOT NULL DEFAULT 1,
 					  UNIQUE KEY STS_ID_UNIQUE (STS_ID),
 					  KEY STS_type (STS_type)";
-		$this->_table_is_changed_in_this_version($table_name, $sql, 'ENGINE=InnoDB');
+		$this->_table_has_not_changed_since_previous($table_name, $sql, 'ENGINE=InnoDB');
 
 
 
@@ -608,13 +631,8 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base{
 	 * @return boolean
 	 */
 	public function schema_changes_after_migration() {
-		//this is actually the same as the last DMS
-		/** @var EE_DMS_Core_4_7_0 $script_4_7_defaults */
-		$script_4_7_defaults = EE_Registry::instance()->load_dms('Core_4_7_0');
-		return $script_4_7_defaults->schema_changes_after_migration();
+		return true;
 	}
 
-	public function migration_page_hooks(){
-
-	}
+	public function migration_page_hooks(){}
 }
