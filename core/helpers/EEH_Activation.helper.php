425e9cfbf581ea20da88674f2278078a2f15de4b
Brent Christensen <brent@eventespresso.com>
2015-08-17 09:54:01 -0800

    return true if no records exist, not the count itself; [touch:8656]

--- core/helpers/EEH_Activation.helper.php
+++ core/helpers/EEH_Activation.helper.php
@@ -680,7 +680,7 @@ class EEH_Activation {
 		$table_name = EEH_Activation::ensure_table_name_has_prefix( $table_name );
 		if ( EEH_Activation::table_exists( $table_name ) ) {
 			$count = $wpdb->query( "SELECT COUNT(*) FROM $table_name" );
-			return absint( $count );
+			return absint( $count ) === 0 ? true : false;
 		}
 		return false;
 	}
