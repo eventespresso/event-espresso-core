2d6bf914ddd0835f81a300031cb392c3969edc2b
Josh Feck <josh@nxnweb.com>
2015-09-02 10:10:34 -0400

    remove only "ticket" [touch: 8774]

--- modules/ticket_selector/templates/ticket_selector_chart.template.php
+++ modules/ticket_selector/templates/ticket_selector_chart.template.php
@@ -177,7 +177,7 @@ foreach ( $tickets as $TKT_ID => $ticket ) {
 						if ( $ticket_bundle ) {
 							echo apply_filters( 'FHEE__ticket_selector_chart_template__per_ticket_bundle_text', __( ' / bundle', 'event_espresso' ));
 						} else {
-							echo apply_filters( 'FHEE__ticket_selector_chart_template__per_ticket_text', '' );
+							echo apply_filters( 'FHEE__ticket_selector_chart_template__per_ticket_text', __( ' / ', 'event_espresso' ));
 						}?></span> &nbsp;
 					</td>
 					<?php } ?>
