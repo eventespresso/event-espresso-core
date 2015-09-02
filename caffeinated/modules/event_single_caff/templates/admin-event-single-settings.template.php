7cc715551091ee823f43cb7f14d65349a4236362
Seth <seth@eventespresso.com>
2015-09-02 14:23:40 -0600

    Adding display order to single event pages. [touch:8778]
    Signed-off-by: Seth <seth@eventespresso.com>

--- caffeinated/modules/event_single_caff/templates/admin-event-single-settings.template.php
+++ caffeinated/modules/event_single_caff/templates/admin-event-single-settings.template.php
@@ -3,6 +3,12 @@ $values = array(
 	array('id' => TRUE, 'text' => __('Yes', 'event_espresso')),
 	array('id' => FALSE, 'text' => __('No', 'event_espresso'))
 );
+$display_order_values = array(
+	array('id' => 0, 'text' => 1),
+	array('id' => 1, 'text' => 2),
+	array('id' => 2, 'text' => 3),
+
+);
 ?>
 
 	<!--*************************   Event Single  ****************************-->
@@ -33,6 +39,33 @@ $values = array(
 				<td>
 					<?php echo EEH_Form_Fields::select_input( 'display_venue', $values, $display_venue, 'id="display_venue"' ); ?>
 					<p class="description"><?php _e('Do not use this if you are using the venue shortcodes in your event description.', 'event_espresso'); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th>		
+					<?php _e('Display Order', 'event_espresso'); ?><?php echo EEH_Template::get_help_tab_link('display_addresses_in_reg_form_info');?>
+				</th>
+				<td>
+					<p>
+						<label for="display_order_tickets">
+						<?php _e('Tickets Display Order', 'event_espresso'); ?>
+						</label>
+						<?php echo EEH_Form_Fields::select_input( 'display_order_tickets', $display_order_values, $display_order_tickets, 'id="display_order_tickets"' ); ?>
+					</p>
+					<p>
+						<label for="display_order_datetimes">
+						<?php _e('Datetime Display Order', 'event_espresso'); ?>
+						</label>
+						<?php echo EEH_Form_Fields::select_input( 'display_order_datetimes', $display_order_values, $display_order_datetimes, 'id="display_order_datetimes"' ); ?>
+					</p>
+					<p>
+						<label for="display_order_venue">
+						<?php _e('Venue Display Order', 'event_espresso'); ?>
+						</label>
+						<?php echo EEH_Form_Fields::select_input( 'display_order_venue', $display_order_values, $display_order_venue, 'id="display_order_venue"' ); ?>
+					</p>
+					<p class="description"><?php _e('Use these settings to determine the display order of the ticket selector, datetime(s), and venue on the single event page.', 'event_espresso'); ?></p>
+
 				</td>
 			</tr>
 
