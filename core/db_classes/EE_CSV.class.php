e33d00fe30d6c11c2e72d204dcbf65def80837a7
Michael Nelson <michael@eventespresso.com>
2015-09-03 10:44:57 -0700

    [touch:8783]{t:45}specify seperator. This works well with excel, not sure about other csv reading programs

--- core/db_classes/EE_CSV.class.php
+++ core/db_classes/EE_CSV.class.php
@@ -315,7 +315,7 @@
 //		header("Content-Type: application/download");
 		header('Content-disposition: attachment; filename='.$filename);
 		header("Content-Type: text/csv; charset=utf-8");
-		echo "\xEF\xBB\xBF"; // makes excel open it as UTF-8. UTF-8 BOM, see http://stackoverflow.com/a/4440143/2773835
+		echo "\xEF\xBB\xBFsep=,\n"; // makes excel open it as UTF-8. UTF-8 BOM, see http://stackoverflow.com/a/4440143/2773835
 		$fh = fopen('php://output', 'w');
 		return $fh;
 	}
