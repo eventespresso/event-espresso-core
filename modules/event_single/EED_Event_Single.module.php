4ca90ac164bcbdf5b4c83314fc692de9036f44ec
Brent Christensen <brent@eventespresso.com>
2015-09-04 09:36:11 -0700

    se default values for properties and remove cruft from $corrected_keys array; [touch:8778]

--- modules/event_single/EED_Event_Single.module.php
+++ modules/event_single/EED_Event_Single.module.php
@@ -27,13 +27,13 @@ class EED_Event_Single  extends EED_Module {
 
 	protected static $event_position;
 
-	protected static $display_order_tickets;
+	protected static $display_order_tickets = 100;
 
-	protected static $display_order_datetimes;
+	protected static $display_order_datetimes = 101;
 
-	protected static $display_order_event;
+	protected static $display_order_event = 102;
 
-	protected static $display_order_venue;
+	protected static $display_order_venue = 103;
 
 
 
@@ -125,10 +125,6 @@ class EED_Event_Single  extends EED_Module {
 			101 => 100,
 			102 => 101,
 			103 => 103,
-			array( 'id' => 2, 'text' => 1 ),
-			array( 'id' => 0, 'text' => 2 ),
-			array( 'id' => 1, 'text' => 3 ),
-			array( 'id' => 3, 'text' => 4 ),
 		);
 		$corrected_key_array = array();
 		foreach ( EED_Event_Single::$filter_order_array as $key => $template ) {
