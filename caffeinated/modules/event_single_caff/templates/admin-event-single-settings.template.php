7ac8d26406fa730c1fd91677ce71ccb21ff2192a
Brent Christensen <brent@eventespresso.com>
2015-09-10 15:01:31 -0700

    add input for EED_Events_Single_use_sortable_display_order; update existing inputs; [touch:8778]

--- caffeinated/modules/event_single_caff/templates/admin-event-single-settings.template.php
+++ caffeinated/modules/event_single_caff/templates/admin-event-single-settings.template.php
@@ -1,7 +1,12 @@
 <?php
-$values = array(
-	array('id' => TRUE, 'text' => __('Yes', 'event_espresso')),
-	array('id' => FALSE, 'text' => __('No', 'event_espresso'))
+add_filter( 'FHEE__EEH_Form_Fields__label_html', '__return_empty_string' );
+EE_Registry::instance()->load_helper( 'Event_View' );
+EE_Registry::instance()->load_helper( 'Form_Fields' );
+$values = EEH_Form_Fields::prep_answer_options(
+	array(
+		array( 'id' => 1, 'text' => __( 'Yes', 'event_espresso' ) ),
+		array( 'id' => 0, 'text' => __( 'No', 'event_espresso' ) )
+	)
 );
 ?>
 
@@ -18,22 +23,52 @@ $values = array(
 					</label>
 				</th>
 				<td>
-					<?php echo EEH_Form_Fields::select_input( 'display_status_banner_single', $values, $display_status_banner_single, 'id="display_status_banner_single"' ); ?>
-					<p class="description"><?php _e('Selecting "Yes" will inject an Event Status banner with the title whenever Events are displaying on the single event page.', 'event_espresso'); ?></p>
+					<?php echo EEH_Form_Fields::select( 'display_status_banner_single', $display_status_banner_single, $values, 'display_status_banner_single', 'display_status_banner_single' ); ?>
+					<p class="description"><?php _e( 'Selecting "Yes" will inject an Event Status banner with the title whenever Events are displaying on the single event page.', 'event_espresso' ); ?></p>
 				</td>
 			</tr>
 
 			<tr>
 				<th>
 					<label for="display_venue">
-						<?php _e('Display Venue Details', 'event_espresso'); ?><?php echo EEH_Template::get_help_tab_link('display_addresses_in_reg_form_info');?>
+						<?php _e( 'Display Venue Details', 'event_espresso' ); ?><?php echo EEH_Template::get_help_tab_link('display_addresses_in_reg_form_info');?>
 					</label>
 				</th>
 				<td>
-					<?php echo EEH_Form_Fields::select_input( 'display_venue', $values, $display_venue, 'id="display_venue"' ); ?>
-					<p class="description"><?php _e('Do not use this if you are using the venue shortcodes in your event description.', 'event_espresso'); ?></p>
+					<?php echo EEH_Form_Fields::select( 'display_venue', $display_venue, $values, 'display_venue', 'display_venue' ); ?>
+					<p class="description"><?php _e( 'Do not use this if you are using the venue shortcodes in your event description.', 'event_espresso' ); ?></p>
 				</td>
 			</tr>
+
+			<tr>
+				<th>
+					<label for="EED_Events_Single_use_sortable_display_order">
+						<?php _e( 'Use Custom Display Order?', 'event_espresso' ); ?>
+					</label>
+				</th>
+				<td>
+					<?php
+						echo EEH_Form_Fields::select(
+							'use_sortable_display_order',
+							$use_sortable_display_order,
+							$values,
+							'EED_Events_Single_use_sortable_display_order',
+							'EED_Events_Single_use_sortable_display_order'
+						);
+					?>
+					<p class="description ">
+						<?php
+						echo sprintf(
+							__( '%1$sPlease Note:%2$s%3$sIf you are currently using filters to customize the display order for elements within the Event Single page display, then you do NOT activate this feature until those filters have been removed or disabled. If this feature is activated while still using such filters, duplicate event content such as the ticket selector, datetimes, or venue information could be displayed on the frontend of the site. Please verify that this is not the case after activating this feature.', 'event_espresso' ),
+							'<span class="important-notice">',
+							'</span>',
+							'<br />'
+						);
+						?>
+					</p>
+				</td>
+			</tr>
+
 			<tr>
 				<th>
 					<?php _e('Display Order', 'event_espresso'); ?><?php echo EEH_Template::get_help_tab_link('display_addresses_in_reg_form_info');?>
