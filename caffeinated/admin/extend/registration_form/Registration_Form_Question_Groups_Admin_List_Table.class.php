8202ab16e8abf8a8771c74f5ba4c52c3a646364a
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2015-09-10 17:36:08 -0400

    Update delete permanently links for events and registration form. [touch:7888]

--- caffeinated/admin/extend/registration_form/Registration_Form_Question_Groups_Admin_List_Table.class.php
+++ caffeinated/admin/extend/registration_form/Registration_Form_Question_Groups_Admin_List_Table.class.php
@@ -173,7 +173,7 @@ class Registration_Form_Question_Groups_Admin_List_Table extends EE_Admin_List_T
 			}
 
 			if ( !$item->has_questions_with_answers() &&  EE_Registry::instance()->CAP->current_user_can( 'ee_delete_question_group', 'espresso_registration_form_delete_question_group', $item->ID() ) ) {
-				$actions['delete permanently'] = '<a href="' . $delete_link . '" title="' . esc_attr__('Delete Question Group Permanently', 'event_espresso') . '">' . __('Delete Permanently', 'event_espresso') . '</a>';
+				$actions['delete'] = '<a href="' . $delete_link . '" title="' . esc_attr__('Delete Question Group Permanently', 'event_espresso') . '">' . __('Delete Permanently', 'event_espresso') . '</a>';
 			}
 		}
 
