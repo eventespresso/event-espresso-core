ca2da9d82d010debee6e1088d24d8ff3d7fc8a37
Brent Christensen <brent@eventespresso.com>
2015-09-28 08:59:58 -0700

    just some debug code;

--- tests/testcases/core/db_models/EEM_Ticket_Test.php
+++ tests/testcases/core/db_models/EEM_Ticket_Test.php
@@ -21,13 +21,17 @@ class EEM_Ticket_Test extends EE_UnitTestCase {
 	 * @group 8861
 	 */
 	public function test_sum_tickets_currently_available_at_datetime() {
+		//echo "\n\n test_sum_tickets_currently_available_at_datetime: ";
 		$scenarios = $this->scenarios->get_scenarios_by_type( 'datetime' );
 		foreach ( $scenarios as $scenario ) {
+			//echo "\n\nTesting" . $scenario->name;
 			if ( $scenario->get_expected( 'sum_tickets_currently_available_at_datetime' ) !== false ) {
-				$actual_returned = EEM_Ticket::instance()->sum_tickets_currently_available_at_datetime( $scenario->get_scenario_object()->ID() );
-				$this->assertEquals( $scenario->get_expected( 'sum_tickets_currently_available_at_datetime' ), $actual_returned );
+				//echo "\n scenario->get_scenario_object()->ID(): " . $scenario->get_scenario_object()->ID();
+				$actual = EEM_Ticket::instance()->sum_tickets_currently_available_at_datetime( $scenario->get_scenario_object()->ID() );
+				$this->assertEquals( $scenario->get_expected( 'sum_tickets_currently_available_at_datetime' ), $actual );
 			}
 		}
 	}
 
 }
+// Location: tests/testcases/core/db_models/EEM_Ticket_Test.php
