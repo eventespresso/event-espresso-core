1c209a4d9e63eb71747295c0e02bc07031d81262
Brent Christensen <brent@eventespresso.com>
2015-10-19 17:35:27 -0700

    add 'FHEE__EE_Ticket_Selector__after_ticket_selector_submit' and 'FHEE__EE_Ticket_Selector__after_view_details_btn' filters; [touch:8348]

--- modules/ticket_selector/EED_Ticket_Selector.module.php
+++ modules/ticket_selector/EED_Ticket_Selector.module.php
@@ -411,6 +411,7 @@ class EED_Ticket_Selector extends  EED_Module {
 				$html = '<input id="ticket-selector-submit-'. self::$_event->ID() .'-btn"';
 				$html .= ' class="ticket-selector-submit-btn ticket-selector-submit-ajax"';
 				$html .= ' type="submit" value="' . $btn_text . '" />';
+				$html .= apply_filters( 'FHEE__EE_Ticket_Selector__after_ticket_selector_submit', '', self::$_event );
 				$html .= '<div class="clear"><br/></div></form>';
 				return $html;
 			} else if ( is_archive() ) {
@@ -452,6 +453,7 @@ class EED_Ticket_Selector extends  EED_Module {
 		$view_details_btn = '<form method="POST" action="' . self::$_event->get_permalink() . '">';
 		$btn_text = apply_filters( 'FHEE__EE_Ticket_Selector__display_view_details_btn__btn_text', __( 'View Details', 'event_espresso' ), self::$_event );
 		$view_details_btn .= '<input id="ticket-selector-submit-'. self::$_event->ID() .'-btn" class="ticket-selector-submit-btn view-details-btn" type="submit" value="' . $btn_text . '" />';
+		$view_details_btn .= apply_filters( 'FHEE__EE_Ticket_Selector__after_view_details_btn', '', self::$_event );
 		$view_details_btn .= '<div class="clear"><br/></div>';
 		$view_details_btn .= '</form>';
 		return $view_details_btn;
