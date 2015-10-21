d4dc65ec2fdacb6733e53da062013c11341c6858
Brent Christensen <brent@eventespresso.com>
2015-10-21 09:06:55 -0700

    oops... copy over other hidden inputs; [touch:8986]

--- modules/ticket_selector/templates/ticket_selector_chart.template.php
+++ modules/ticket_selector/templates/ticket_selector_chart.template.php
@@ -474,7 +474,7 @@ if ( ! $hide_ticket_selector ) {
 	</table>
 
 	<input type="hidden" name="noheader" value="true" />
-	<input type="hidden" name="tkt-slctr-return-url-<?php echo $EVT_ID ?>" value="<?php echo $_SERVER['REQUEST_URI']?>" />
+	<input type="hidden" name="tkt-slctr-return-url-<?php echo $EVT_ID ?>" value="<?php echo filter_input( INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL );?>" />
 	<input type="hidden" name="tkt-slctr-rows-<?php echo $EVT_ID; ?>" value="<?php echo $row - 1; ?>" />
 	<input type="hidden" name="tkt-slctr-max-atndz-<?php echo $EVT_ID; ?>" value="<?php echo $max_atndz; ?>" />
 	<input type="hidden" name="tkt-slctr-event-id" value="<?php echo $EVT_ID; ?>" />
@@ -494,6 +494,11 @@ if ( $max_atndz > 0 && ! $hide_ticket_selector ) {
 <?php } else if ( isset( $TKT_ID ) ) { ?>
 <input type="hidden" name="tkt-slctr-qty-<?php echo $EVT_ID; ?>[]" value="1"/>
 <input type="hidden" name="tkt-slctr-ticket-id-<?php echo $EVT_ID; ?>[]" value="<?php echo $TKT_ID; ?>"/>
+<input type="hidden" name="noheader" value="true"/>
+<input type="hidden" name="tkt-slctr-return-url-<?php echo $EVT_ID ?>" value="<?php echo filter_input( INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL ); ?>"/>
+<input type="hidden" name="tkt-slctr-rows-<?php echo $EVT_ID; ?>" value="<?php echo $row - 1; ?>"/>
+<input type="hidden" name="tkt-slctr-max-atndz-<?php echo $EVT_ID; ?>" value="<?php echo $max_atndz; ?>"/>
+<input type="hidden" name="tkt-slctr-event-id" value="<?php echo $EVT_ID; ?>"/>
 <?php
 	if ( $ticket instanceof EE_Ticket ) {
 		do_action( 'AHEE__ticket_selector_chart__template__before_ticket_selector', $event );
