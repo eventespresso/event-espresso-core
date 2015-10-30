b241923588ddcf141e040ce062f832190e251037
Brent Christensen <brent@eventespresso.com>
2015-10-30 16:52:58 -0700

    add phpdoc comments to explain method usage; [touch:8919]

--- core/interfaces/EEI_Interfaces.php
+++ core/interfaces/EEI_Interfaces.php
@@ -33,10 +33,20 @@ interface EEI_Request_Decorator {
 
 	/**
 	 * converts a Request to a Response
+	 * can perform their logic either before or after the core application has run like so:
 	 *
-	 * @param EE_Request $request
-	 * @param EE_Response $response
-	 * @return EE_Response
+	 *    public function handle_request( EE_Request $request, EE_Response $response ) {
+	 *        $this->request = $request;
+	 *        $this->response = $response;
+	 *      // logic performed BEFORE core app has run
+	 *      $this->process_request_stack( $this->request, $this->response );
+	 *      // logic performed AFTER core app has run
+	 *      return $response;
+	 *    }
+	 *
+	 * @param 	EE_Request 	$request
+	 * @param 	EE_Response $response
+	 * @return 	EE_Response
 	 */
 	public function handle_request( EE_Request $request, EE_Response $response );
 }
