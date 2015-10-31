ee17516ba98b313916a7606afb54942313415da7
Brent Christensen <brent@eventespresso.com>
2015-10-30 17:11:41 -0700

    remove handle_request() declaration; remove call to EEH_Debug_Tools::ee_plugin_activation_errors(); pass EE_PLUGIN_BASENAME to espresso_deactivate_plugin(); [touch:8919]

--- core/middleware/EE_Middleware.core.php
+++ core/middleware/EE_Middleware.core.php
@@ -51,27 +51,6 @@ abstract class EE_Middleware implements EEI_Request_Decorator {
 
 
 	/**
-	 * converts a Request to a Response
-	 * can perform their logic either before or after the core application has run like so:
-	 *
-	 * 	public function handle_request( EE_Request $request, EE_Response $response ) {
-	 * 		$this->request = $request;
-	 * 		$this->response = $response;
-	 *      // logic performed BEFORE core app has run
-	 *      $this->process_request_stack( $this->request, $this->response );
-	 *      // logic performed AFTER core app has run
-	 *      return $response;
-	 * 	}
- 	 *
-	 * @param 	EE_Request 	$request
-	 * @param 	EE_Response $response
-	 * @return 	EE_Response
-	 */
-	abstract public function handle_request( EE_Request $request, EE_Response $response );
-
-
-
-	/**
 	 * process_request_stack
 	 *
 	 * @access 	protected
@@ -85,14 +64,10 @@ abstract class EE_Middleware implements EEI_Request_Decorator {
 		if ( ! $this->response->request_terminated() ) {
 			$this->response = $this->request_stack->handle_request( $this->request, $this->response );
 		} else {
-			if ( WP_DEBUG ) {
-				EEH_Debug_Tools::ee_plugin_activation_errors();
-			}
-			espresso_deactivate_plugin();
+			espresso_deactivate_plugin( EE_PLUGIN_BASENAME );
 		}
 		return $this->response;
 	}
-
 
 
 
