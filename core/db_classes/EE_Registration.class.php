fc0e681a8e23640c03c890c3552baec96bfd1896
Brent Christensen <brent@eventespresso.com>
2015-11-11 16:25:23 -0800

    don't check reg status, since that is done in update_transaction_after_canceled_or_declined_registration();  [touch:5580]

--- core/db_classes/EE_Registration.class.php
+++ core/db_classes/EE_Registration.class.php
@@ -128,11 +128,7 @@ class EE_Registration extends EE_Soft_Delete_Base_Class implements EEI_Registrat
 			$transaction_processor = EE_Registry::instance()->load_class( 'Transaction_Processor' );
 			/** @type EE_Transaction_Payments $transaction_payments */
 			$transaction_payments = EE_Registry::instance()->load_class( 'Transaction_Payments' );
-			// these reg statuses should not be considered in any calculations involving monies owing
-			$closed_reg_statuses = ! empty( $closed_reg_statuses ) ? $closed_reg_statuses : EEM_Registration::closed_reg_statuses();
-			if ( in_array( $this->status_ID(), $closed_reg_statuses ) ) {
-				$transaction_processor->update_transaction_after_canceled_or_declined_registration( $this, null, false );
-			}
+			$transaction_processor->update_transaction_after_canceled_or_declined_registration( $this, null, false );
 			$transaction_payments->recalculate_transaction_total( $this->transaction(), false );
 			$transaction_payments->update_transaction_status_based_on_total_paid( $this->transaction(), true );
 			do_action( 'AHEE__EE_Registration__set_status__after_update', $this );
