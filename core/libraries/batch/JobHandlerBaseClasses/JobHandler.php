27d221bf064d2e9d379d7dadbe9a188706814ae6
Brent Christensen <brent@eventespresso.com>
2015-11-17 13:50:48 -0800

    don't need to declare namespace use for classes when within the same namespace; formatting, typos, and phpdocs; [touch:8729]

--- core/libraries/batch/JobHandlerBaseClasses/JobHandler.php
+++ core/libraries/batch/JobHandlerBaseClasses/JobHandler.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  *
  * Class JobHandler
@@ -12,36 +12,46 @@
  *
  */
 namespace EventEspressoBatchRequest\JobHandlerBaseClasses;
-if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
 
-use EventEspressoBatchRequest\JobHandlerBaseClasses\JobHandlerInterface;
-use EventEspressoBatchRequest\Helpers\JobParameters;
 use EventEspressoBatchRequest\Helpers\BatchRequestException;
+use EventEspressoBatchRequest\Helpers\JobParameters;
+use EventEspressoBatchRequest\Helpers\JobStepResponse;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
 
 abstract class JobHandler implements JobHandlerInterface {
+
 	/**
 	 * Performs any necessary setup for starting the job. This is also a good
 	 * place to setup the $job_arguments which will be used for subsequent HTTP requests
 	 * when continue_job will be called
+	 *
 	 * @param JobParameters $job_parameters
 	 * @throws BatchRequestException
 	 * @return JobStepResponse
 	 */
 	abstract public function create_job( JobParameters $job_parameters );
-	
+
 	/**
 	 * Performs another step of the job
+	 *
 	 * @param JobParameters $job_parameters
 	 * @param int $batch_size
 	 * @return JobStepResponse
 	 * @throws BatchRequestException
 	 */
 	abstract public function continue_job( JobParameters $job_parameters, $batch_size = 50 );
-	
+
+
+
 	/**
 	 * Performs any clean-up logic when we know the job is completed
-	 * @param string $job_id
-	 * @param array $job_arguments
+	 *
+	 * @param JobParameters $job_parameters
 	 * @return JobStepResponse
 	 * @throws BatchRequestException
 	 */
