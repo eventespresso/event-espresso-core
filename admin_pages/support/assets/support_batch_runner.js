98367eec87254cff1450af31dd9b00dc757452c5
Brent Christensen <brent@eventespresso.com>
2015-11-17 15:49:01 -0800

    fix formatting, implied vars, typos, and jsdocs; [touch:8729]

--- admin_pages/support/assets/support_batch_runner.js
+++ admin_pages/support/assets/support_batch_runner.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Relies on there being a variable named
  * ee_job_response localized
  * @var array $ee_job_response {
@@ -11,42 +11,48 @@
 
 
 jQuery(document).ready(function() {
-	runner = new EE_BatchRunner( 
-			eei18n.ajax_url, 
-			{
-				'page' : 'espresso_support',
-				'action' : 'batch_continue',
-				'ee_admin_ajax' : true
-			},
-			ee_support_download_file,
-			eei18n.ajax_url, 
-			{
-				'page' : 'espresso_support',
-				'action' : 'batch_cleanup',
-				'ee_admin_ajax' : true
-			});
+
+    var runner = new EE_BatchRunner(
+        eei18n.ajax_url,
+        {
+            'page' : 'espresso_support',
+            'action' : 'batch_continue',
+            'ee_admin_ajax' : true
+        },
+        ee_support_download_file,
+        eei18n.ajax_url,
+        {
+            'page' : 'espresso_support',
+            'action' : 'batch_cleanup',
+            'ee_admin_ajax' : true
+        }
+    );
 	runner.set_job_id( ee_job_response.job_id );
 	runner.set_progress_bar_div( 'batch-progress' );
 	runner.continue_job();
+
+
+
 	/**
 	 * Checks for once the download is complete,
 	 * then gets the user to download the temp file
 	 * then cleans up the job
-	 * @param array response
-	 * @param string status
+	 * @param response array
+	 * @param status string
 	 * @param {type} xhr
 	 * @returns void
 	 */
 	function ee_support_download_file( response, status, xhr ) {
-		//once we're all done, downthe file
-		if( response.data.status == 'complete' &&
-				response.data.file_url != '' ) {
+		//once we're all done, download the file
+		if( response.data.status == 'complete' && response.data.file_url != '' ) {
 			window.location.href=response.data.file_url;
 			runner.cleanup_callback = function( response, data, xhr ) {
-					
-					window.history.back();
-				};
+                window.history.back();
+            };
 			runner.cleanup_job();
 		}
 	}
+
+
+
 });
