3386044626806fd8e9c0bab27e654e186cc230fa
Brent Christensen <brent@eventespresso.com>
2015-12-21 16:40:07 -0800

    update session mock file name; [touch:8284]

--- tests/testcases/core/db_models/EEM_Transaction_Test.php
+++ tests/testcases/core/db_models/EEM_Transaction_Test.php
@@ -20,7 +20,7 @@ class EEM_Transaction_Test extends EE_UnitTestCase{
 		$pretend_bot_creations = 9;
 		$pretend_real_recent_txns = 3;
 		$pretend_real_good_txns = 5;
-		require_once EE_TESTS_DIR . 'mocks' . DS . 'core' . DS . 'EE_Session_Mock.php';
+		require_once EE_TESTS_DIR . 'mocks' . DS . 'core' . DS . 'EE_Session_Mock.core.php';
 		$this->factory->transaction->create_many( $pretend_bot_creations, array( 'TXN_timestamp' => time() - WEEK_IN_SECONDS * 2 , 'STS_ID' => EEM_Transaction::failed_status_code ) );
 		$this->factory->transaction->create_many( $pretend_real_recent_txns, array( 'TXN_timestamp' => time() - EE_Session_Mock::instance()->lifespan() + MINUTE_IN_SECONDS , 'STS_ID' => EEM_Transaction::failed_status_code ) );
 		$this->factory->transaction->create_many( $pretend_real_good_txns, array( 'STS_ID' => EEM_Transaction::abandoned_status_code ) );
