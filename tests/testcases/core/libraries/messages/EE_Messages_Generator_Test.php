653a4ca5d6f07372b59f8ae785dd6171b734a592
Brent Christensen <brent@eventespresso.com>
2016-01-14 13:23:59 -0800

    standardize all references to EE_Messages and ensure that phpdocs are added so that all method usages can be found; [touch:9216]

--- tests/testcases/core/libraries/messages/EE_Messages_Generator_Test.php
+++ tests/testcases/core/libraries/messages/EE_Messages_Generator_Test.php
@@ -26,9 +26,10 @@ class EE_Messages_Generator_Test extends EE_UnitTestCase {
 	 * @return EE_Messages_Generator
 	 */
 	function test_construct() {
-		$ee_msg = EE_Registry::instance()->load_lib( 'Messages' );
-		$queue = EE_Registry::instance()->load_lib( 'Messages_Queue', $ee_msg );
-		$generator = new EE_Messages_Generator( $queue, $ee_msg );
+		/** @type EE_Messages $messages_controller */
+		$messages_controller = EE_Registry::instance()->load_lib( 'messages' );
+		$queue = EE_Registry::instance()->load_lib( 'Messages_Queue', $messages_controller );
+		$generator = new EE_Messages_Generator( $queue, $messages_controller );
 
 		//assert $generator loaded okay
 		$this->assertInstanceOf( 'EE_Messages_Generator', $generator );
