de06d9c644acd04c0195a05d17e0ec18f8549d34
Brent Christensen <brent@eventespresso.com>
2016-01-20 14:33:26 -0800

    update test to use MRM; [touch:9274]

--- tests/testcases/core/helpers/EEH_MSG_Template_Test.php
+++ tests/testcases/core/helpers/EEH_MSG_Template_Test.php
@@ -21,18 +21,24 @@ class EEH_MSG_Template_Test extends EE_UnitTestCase {
 
 
 	/**
-	 * testing the is_messenger_active() method
+	 * test messenger that should be active
 	 *
 	 * @since 4.4.1
 	 */
-	public function test_is_messenger_active() {
-		//test messenger that should be active
-		EE_Registry::instance()->load_helper('MSG_Template');
-		$this->assertTrue( EEH_MSG_Template::is_messenger_active( 'email' ) );
-
-		//test messenger that should NOT be active
-		$this->assertFalse( EEH_MSG_Template::is_messenger_active( 'some_random_messenger' ) );
+	public function test_get_active_messenger() {
+		/** @type EE_Message_Resource_Manager $Message_Resource_Manager */
+		$Message_Resource_Manager = EE_Registry::instance()->load_lib( 'Message_Resource_Manager' );
+		$Message_Resource_Manager->activate_default_messengers_and_message_types();
+		$this->assertInstanceOf(
+			'EE_Messenger',
+			$Message_Resource_Manager->get_active_messenger( 'email' )
+		);
+		$this->assertNotInstanceOf(
+			'EE_Messenger',
+			$Message_Resource_Manager->get_active_messenger( 'some_random_messenger' )
+		);
 	}
 
 
 } //end EEH_MSG_Template_Test
+// Location: tests/testcases/core/helpers/EEH_MSG_Template_Test.php
