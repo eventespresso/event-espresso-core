93f82ecfe7dd5c5e54fecb7baf5510ecd75d017f
Mike Nelson <michael@eventespresso.com>
2016-01-28 15:13:09 -0800

    [touch:9165]{t:20}scripts getting localized need to be arrays, not justa single variable

--- core/libraries/form_sections/base/EE_Form_Section_Proper.form.php
+++ core/libraries/form_sections/base/EE_Form_Section_Proper.form.php
@@ -393,7 +393,8 @@ class EE_Form_Section_Proper extends EE_Form_Section_Validatable{
 	public static function wp_enqueue_scripts( $init_form_validation_automatically = false ){
 		add_filter( 'FHEE_load_jquery_validate', '__return_true' );
 		wp_register_script( 'ee_form_section_validation', EE_GLOBAL_ASSETS_URL . 'scripts' . DS . 'form_section_validation.js', array( 'jquery-validate', 'jquery-ui-datepicker' ), EVENT_ESPRESSO_VERSION, TRUE );
-		wp_localize_script( 'ee_form_section_validation', 'ee_form_section_validation_init', $init_form_validation_automatically );
+		
+		wp_localize_script( 'ee_form_section_validation', 'ee_form_section_validation_init', array( 'init' => $init_form_validation_automatically ) );
 	}
 
 
