b042ca9fd0a618173f24f47ccbe0a8a97caed908
Seth <seth@eventespresso.com>
2016-01-28 13:52:17 -0700

    Removing a link and fixing a couple of typos [touch:9300]
    Signed-off-by: Seth <seth@eventespresso.com>

--- admin_pages/support/templates/developers_admin_details.template.php
+++ admin_pages/support/templates/developers_admin_details.template.php
@@ -2,8 +2,6 @@
 	<p>If you want to integrate with&nbsp;Event Espresso or participate in building code, then you're in the right place. The following resources can help you get started.</p>
 	<h3><strong>Developer Resources</strong></h3>
 	<ul>
-		<li><a href="https://eventespresso.com/developers/request-repo-access/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Request access</a> to the&nbsp;Event Espresso Core (4.0+) GitHub repository</li>
-
 		<li><a title="Event Espresso Core Code Documentation" href="http://code.eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Event Espresso 4 Core Code Documentation</a>&nbsp;-&nbsp;auto-generated documentation using the&nbsp;<a href="http://phpdoc.org/">phpDocumentor</a>&nbsp;tool from inline phpdoc blocs found in the code for&nbsp;Event Espresso 4.</li>
 		<li><a title="Event Espresso 4 Developer Portal" href="http://developer.eventespresso.com/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab" target="_blank">Event Espresso 4 Developer Portal </a>-&nbsp;<span style="color: #555555;">resources for developers (EE team and third-party) contributing to&nbsp;</span>Event Espresso<span style="color: #555555;">&nbsp;&nbsp;OR&nbsp;who are building things on the Event Espresso platform.</span></li>
 		<li><a title="Event Espresso 4 Core Documentation" href="http://developer.eventespresso.com/ee-plugin/event-espresso-core/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab" target="_blank">Event Espresso 4 Developer Core Documentation</a></li>
@@ -19,7 +17,7 @@
 		<li><a href="http://developer.eventespresso.com/docs/ee-capability-system-overview/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Capability System</a></li>
 		<li><a href="http://developer.eventespresso.com/docs/ee4-payment-method-development/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Payment Method Development</a></li>
 		<li><a href="https://eventespresso.com/2014/03/messages-systemyour-tool-getting-word/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Messages System in Event Espresso 4</a></li>
-		<li><a href="https://developer.eventespresso.com/docs/using-ee4-model-objects/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Database Model System</a> (used for intefacing with EE4 data via WordPress plugins and server side querying)</li>
+		<li><a href="http://developer.eventespresso.com/docs/using-ee4-model-objects/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=ee_support_page&utm_content=developers_tab">Database Model System</a> (used for intefacing with EE4 data via WordPress plugins and server side querying)</li>
 
 	</ul>
 	<h3>REST API Resources</h3>
