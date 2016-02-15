537b84f13394e5af7376f4e73e2eaf1ccd642a33
Brent Christensen <brent@eventespresso.com>
2016-02-15 13:16:07 -0800

    we can use property_exists() now that we are PHP 5.3+; [touch:9403]

--- core/libraries/messages/EE_Messages_Base.lib.php
+++ core/libraries/messages/EE_Messages_Base.lib.php
@@ -220,10 +220,10 @@ abstract class EE_Messages_Base extends EE_Base {
 	 */
 	public function get_existing_admin_settings( $messenger = '' ) {
 		// if admin_settings property empty lets try setting it.
-		if ( method_exists($this, '_set_existing_admin_settings') && empty( $this->_existing_admin_settings ) )
+		if ( method_exists($this, '_set_existing_admin_settings') && empty( $this->_existing_admin_settings ) ) {
 			$this->_set_existing_admin_settings( $messenger );
-
-		return EEH_Class_Tools::has_property($this, '_existing_admin_settings')
+		}
+		return property_exists( $this, '_existing_admin_settings' )
 			? $this->_existing_admin_settings
 			: null;
 	}
