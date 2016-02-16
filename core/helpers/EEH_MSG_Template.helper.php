973b2ce20fdc4ddb4a37b34e4976ea3ae7eecb68
Darren Ethier <darren@roughsmootheng.in>
2016-02-16 14:49:55 -0500

    add title attributes to action links in the messages list table [touch: 9399]

--- core/helpers/EEH_MSG_Template.helper.php
+++ core/helpers/EEH_MSG_Template.helper.php
@@ -689,6 +689,7 @@ class EEH_MSG_Template {
 	}
 
 
+
 	/**
 	 * This returns all the current urls for EE_Message actions.
 	 *
@@ -783,6 +784,8 @@ class EEH_MSG_Template {
 	public static function get_message_action_link( $type, EE_Message $message = null, $query_params = array() ) {
 		$url = EEH_MSG_Template::get_message_action_url( $type, $message, $query_params );
 		$icon_css = EEH_MSG_Template::get_message_action_icon( $type );
+		$title = isset( $icon_css['label'] ) ? 'title="' . $icon_css['label'] . '"' : '';
+
 		if ( empty( $url ) || empty( $icon_css ) || ! isset( $icon_css['css_class'] ) ) {
 			return '';
 		}
@@ -797,7 +800,7 @@ class EEH_MSG_Template {
 			)
 		);
 
-		return '<a href="' . $url . '"><span class="' . esc_attr( $icon_css['css_class'] ) . '"></span></a>';
+		return '<a href="' . $url . '"' . $title . '><span class="' . esc_attr( $icon_css['css_class'] ) . '"></span></a>';
 
 	}
 
