60f896b7beb437cc71af2b3633a3f1b1db310499
Brent Christensen <brent@eventespresso.com>
2016-02-24 18:57:25 -0800

    loosen regex validation of email address but add DNS checking for MX, and A records

--- core/libraries/form_sections/strategies/validation/EE_Email_Validation_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/validation/EE_Email_Validation_Strategy.strategy.php
@@ -29,8 +29,8 @@ class EE_Email_Validation_Strategy extends EE_Text_Validation_Strategy{
     * @return bool
     * @throws \EE_Validation_Error
     */
-	function validate($normalized_value) {
-		if( $normalized_value && ! $this->_validate_email($normalized_value) ){
+	function validate( $normalized_value ) {
+		if( $normalized_value && ! $this->_validate_email( $normalized_value ) ){
 			throw new EE_Validation_Error( $this->get_validation_error_message(), 'required');
 		}
 	}
@@ -41,7 +41,7 @@ class EE_Email_Validation_Strategy extends EE_Text_Validation_Strategy{
      * @return array
      */
    function get_jquery_validation_rule_array(){
-		return array( 'email'=>true, 'messages' => array( 'email' => $this->get_validation_error_message() ) );
+		return array( 'i18nEmail'=>true, 'messages' => array( 'email' => $this->get_validation_error_message() ) );
 	}
 
 
@@ -53,69 +53,42 @@ class EE_Email_Validation_Strategy extends EE_Text_Validation_Strategy{
     * @param $email
     * @return bool of whether the email is valid or not
     */
-private function _validate_email($email)
-{
-   $isValid = true;
-   $atIndex = strrpos($email, "@");
-   if (is_bool($atIndex) && !$atIndex)
-   {
-      $isValid = false;
-   }
-   else
-   {
-      $domain = substr($email, $atIndex+1);
-      $local = substr($email, 0, $atIndex);
-      $localLen = strlen($local);
-      $domainLen = strlen($domain);
-      if ($localLen < 1 || $localLen > 64)
-      {
-         // local part length exceeded
-         $isValid = false;
-      }
-      else if ($domainLen < 1 || $domainLen > 255)
-      {
-         // domain part length exceeded
-         $isValid = false;
-      }
-      else if ($local[0] == '.' || $local[$localLen-1] == '.')
-      {
-         // local part starts or ends with '.'
-         $isValid = false;
-      }
-      else if (preg_match('/\\.\\./', $local))
-      {
-         // local part has two consecutive dots
-         $isValid = false;
-      }
-      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
-      {
-         // character not valid in domain part
-         $isValid = false;
-      }
-      else if (preg_match('/\\.\\./', $domain))
-      {
-         // domain part has two consecutive dots
-         $isValid = false;
-      }
-      else if
-(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-                 str_replace("\\\\","",$local)))
-      {
-         // character not valid in local part unless
-         // local part is quoted
-         if (!preg_match('/^"(\\\\"|[^"])+"$/',
-             str_replace("\\\\","",$local)))
-         {
-            $isValid = false;
-         }
-      }
-	  //don't check the DNS records... just personal preference
-//      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
-//      {
-//         // domain not found in DNS
-//         $isValid = false;
-//      }
-   }
-   return $isValid;
-}
+	private function _validate_email( $email ) {
+		if ( ! preg_match( '/^.+\@\S+\.\S+$/', $email ) ) {
+			// email not in correct {string}@{string}.{string} format
+			return false;
+		} else {
+			$atIndex = strrpos( $email, "@" );
+			$domain = substr( $email, $atIndex + 1 );
+			$local = substr( $email, 0, $atIndex );
+			$localLen = strlen( $local );
+			$domainLen = strlen( $domain );
+			if ( $localLen < 1 || $localLen > 64 ) {
+				// local part length exceeded
+				return false;
+			} else if ( $domainLen < 1 || $domainLen > 255 ) {
+				// domain part length exceeded
+				return false;
+			} else if ( $local[ 0 ] == '.' || $local[ $localLen - 1 ] == '.' ) {
+				// local part starts or ends with '.'
+				return false;
+			} else if ( preg_match( '/\\.\\./', $local ) ) {
+				// local part has two consecutive dots
+				return false;
+			} else if ( preg_match( '/\\.\\./', $domain ) ) {
+				// domain part has two consecutive dots
+				return false;
+			} else if ( ! checkdnsrr( $domain, "MX" ) ) {
+				// domain not found in MX records
+				return false;
+			} else if ( ! checkdnsrr( $domain, "A" ) ) {
+				// domain not found in A records
+				return false;
+			}
+		}
+		// you have successfully run the gauntlet young Padawan
+	   return true;
+	}
+
+
 }
