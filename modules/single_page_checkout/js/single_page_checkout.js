98dcff7dc350c86f6935440c8cacdb719c3fa8a8
Brent Christensen <brent@eventespresso.com>
2016-02-24 18:53:23 -0800

    add much more lax email validation method to jQuery validate; [touch:9431]

--- modules/single_page_checkout/js/single_page_checkout.js
+++ modules/single_page_checkout/js/single_page_checkout.js
@@ -214,6 +214,13 @@ jQuery(document).ready( function($) {
 
 			});
 
+			jQuery.validator.addMethod(
+				"i18nEmail", function ( value, element ) {
+					// allow any non-whitespace characters as the host part
+					return this.optional( element ) || /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@(?:\S{1,63})$/.test( value );
+				}
+			);
+
 		},
 
 
