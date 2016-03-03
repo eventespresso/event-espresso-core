c4c69b1f4bc318a4b54deb797df4f427a16ceff8
Brent Christensen <brent@eventespresso.com>
2016-03-03 14:12:07 -0800

    formatting and phpdocs; [touch:9406]

--- core/libraries/rest_api/changes/Changes_In_4_8_36.php
+++ core/libraries/rest_api/changes/Changes_In_4_8_36.php
@@ -1,9 +1,10 @@
 <?php namespace EventEspresso\core\libraries\rest_api\changes;
+
 use EventEspresso\core\libraries\rest_api\controllers\model\Read;
 use EventEspresso\core\libraries\rest_api\controllers\model\Base;
 
-/* 
- * The checkin and checkout endpoints were added in 4.8.36, 
+/*
+ * The checkin and checkout endpoints were added in 4.8.36,
  * where we just added a response headers
  */
 class Changes_In_4_8_36 extends Changes_In_Base {
@@ -12,24 +13,24 @@ class Changes_In_4_8_36 extends Changes_In_Base {
 	 */
 	public function set_hooks() {
 		//set a hook to remove the "calculate" query param
-		add_filter( 
+		add_filter(
 			'FHEE__EED_Core_Rest_Api___get_response_selection_query_params',
 			array( $this, 'remove_calculate_query_param' ),
 			10,
 			3
 		);
 		//don't add the _calculated_fields either
-		add_filter( 
+		add_filter(
 			'FHEE__Read__create_entity_from_wpdb_results__entity_before_inaccessible_field_removal',
 			array( $this, 'remove_calculated_fields_from_response' ),
 			10,
 			5
 		);
-		//and also don't add the count headers 
+		//and also don't add the count headers
 		add_filter(
 			'FHEE__EventEspresso\core\libraries\rest_api\controllers\Base___get_response_headers',
 			array( $this, 'remove_headers_new_in_this_version' ),
-			10, 
+			10,
 			3
 		);
 		//before this, infinity was -1, now it's null
@@ -40,7 +41,7 @@ class Changes_In_4_8_36 extends Changes_In_Base {
 			4
 		);
 	}
-	
+
 	/**
 	 * Dont show "calculate" as an query param option in the index
 	 * @param array $query_params
@@ -54,7 +55,7 @@ class Changes_In_4_8_36 extends Changes_In_Base {
 		}
 		return $query_params;
 	}
-	
+
 	/**
 	 * Removes the "_calculate_fields" part of entity responses before 4.8.36
 	 * @param array $entity_response_array
@@ -64,18 +65,18 @@ class Changes_In_4_8_36 extends Changes_In_Base {
 	 * @param Read $controller
 	 * @return array
 	 */
-	public function remove_calculated_fields_from_response( 
-		$entity_response_array, 
-		\EEM_Base $model, 
-		$request_context, 
-		\WP_REST_Request $request, 
+	public function remove_calculated_fields_from_response(
+		$entity_response_array,
+		\EEM_Base $model,
+		$request_context,
+		\WP_REST_Request $request,
 		Read $controller ) {
 		if( $this->applies_to_version( $controller->get_model_version_info()->requested_version() ) ) {
 			unset( $entity_response_array[ '_calculated_fields' ] );
 		}
 		return $entity_response_array;
 	}
-	
+
 	/**
 	 * Removes the new headers just added in this version to any requests to older versions
 	 * of the API
@@ -87,7 +88,7 @@ class Changes_In_4_8_36 extends Changes_In_Base {
 	public function remove_headers_new_in_this_version(
 		$headers,
 		Base $controller,
-		$version 
+		$version
 	) {
 		if( $this->applies_to_version( $version ) ) {
 			$headers = array_diff_key(
@@ -101,25 +102,25 @@ class Changes_In_4_8_36 extends Changes_In_Base {
 		}
 		return $headers;
 	}
-	
+
 	/**
-	 * If the value was infinity, we now use null in our JSON responses, 
+	 * If the value was infinity, we now use null in our JSON responses,
 	 * but before this version we used -1.
 	 * @param mixed $new_value
-	 * @param EE_Model_Field_Base $field_obj
+	 * @param \EE_Model_Field_Base $field_obj
 	 * @param mixed $original_value
 	 * @param string $requested_value
 	 * @return mixed
 	 */
 	public function use_negative_one_for_infinity_before_this_version( $new_value, $field_obj, $original_value, $requested_value ) {
-		if( $this->applies_to_version( $requested_value ) 
+		if( $this->applies_to_version( $requested_value )
 			&& $original_value === EE_INF ) {
 			//return the old representation of infinity in the JSON
 			return -1;
 		}
 		return $new_value;
-		
+
 	}
-	
+
 }
 
