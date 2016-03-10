28844b68ea544c1291aa7efdfda555611888bc30
Darren Ethier <darren@roughsmootheng.in>
2016-03-10 13:10:31 -0500

    sigh...wrong conditional test

--- tests/includes/define-constants.php
+++ tests/includes/define-constants.php
@@ -18,7 +18,7 @@ if ( getenv( 'EE_TESTS_DIR' ) ) {
 
 define( 'EE_MOCKS_DIR', EE_TESTS_DIR . 'mocks/' );
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( $_tests_dir ) {
+if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
