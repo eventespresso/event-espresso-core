ef0da08b4c438bee723103ed2bc6af9ee41582b3
Darren Ethier <darren@roughsmootheng.in>
2016-03-11 13:45:22 -0500

    force set current user and see if that helps [touch: 9476]

--- tests/testcases/core/db_models/EEM_Attendee_Caps_Test.php
+++ tests/testcases/core/db_models/EEM_Attendee_Caps_Test.php
@@ -30,6 +30,7 @@ class EEM_Attendee_Caps_Test extends EE_UnitTestCase{
 
 		//ok now log the user in
 		$current_user = $user;
+		wp_set_current_user( $user->ID );
 		//and they still shouldn't be able to view the attendee
 		$this->assertEquals( 0, EEM_Attendee::instance()->count( array( 'caps' => EEM_Base::caps_read ) ) );
 
