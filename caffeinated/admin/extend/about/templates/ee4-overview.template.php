4deabfeb54fbd70e20a8fb1e2fab51e4aada1e0f
Seth <seth@eventespresso.com>
2016-03-24 13:02:24 -0600

    Update the admin about page and what's new content. [touch:9504]
    Signed-off-by: Seth <seth@eventespresso.com>

--- caffeinated/admin/extend/about/templates/ee4-overview.template.php
+++ caffeinated/admin/extend/about/templates/ee4-overview.template.php
@@ -1,21 +1,27 @@
 <div class="changelog">
 	<h2 class="about-headline-callout"><?php _e('Event Espresso, the most advanced event management system!', 'event_espresso'); ?></h2>
-	<p>Event Espresso is a <a href="http://eventespresso.com/?utm_source=wordpress_org&amp;utm_medium=link&amp;utm_campaign=plugin_description_tab&amp;utm_content=WordPress+event+manager" rel="nofollow">WordPress event manager</a> which makes it easy for you to register attendees for classes, workshops, events, trainings, conferences or concerts, all from your WordPress website. Event Espresso events are created from the WordPress admin area. You can create signup forms to collect information about your attendees, accept payments, and create reports. The lite version of the plugin provides everything that you need to manage your event using WordPress.</p>
+	<p>Event Espresso is an event management platform built for WordPress, which makes it easy for you to register attendees for classes, workshops, events, trainings, conferences or concerts, all from your WordPress website. Easily create signup forms to collect information about your attendees, accept payments, and create reports. </p>
 	<div class="feature-section col three-col about-updates">
 		<div class="col-1">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/publish_meta_box.jpg">
-			<h3><?php  _e('Optimized aesthetic', 'event_espresso'); ?></h3>
-			<p><?php  _e('The Event Espresso 4 dashboard has a fresh, uncluttered design that embraces clarity and simplicity.', 'event_espresso'); ?></p>
+			<h3><?php  _e('Integrated Event Editor', 'event_espresso'); ?></h3>
+			<p>Event Espresso events are created from the WordPress admin area.</p>
+			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-event-admin-page.jpg">
 		</div>
 		<div class="col-2">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/registrations-overview.jpg">
-			<h3><?php  _e('Integrated management', 'event_espresso'); ?></h3>
-			<p><?php  _e('We’ve made it easier to know who your customers are and how they’ve done business with you over time.', 'event_espresso'); ?></p>
+			<h3><?php  _e('Optimized Aesthetic', 'event_espresso'); ?></h3>
+			<p><?php  _e('The Event Espresso 4 dashboard has a fresh, uncluttered design that embraces clarity and simplicity.', 'event_espresso'); ?></p>
+			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-events-overview.jpg">
 		</div>
-		<div class="col-3 last-feature">
-			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/refined-bookkeeping.jpg">
-			<h3><?php _e('Easy bookkeeping', 'event_espresso'); ?></h3>
+		<div class="col-3">
+			
+			<h3><?php  _e('Integrated Attendee Management', 'event_espresso'); ?></h3>
+			<p><?php  _e('We’ve made it easier to know who your customers are and how they’ve done business with you over time.', 'event_espresso'); ?></p>
+			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-regsitrations-overview.jpg">
+		</div>
+		<div class="col-4 last-feature">
+			<h3><?php _e('Easy Bookkeeping', 'event_espresso'); ?></h3>
 			<p><?php _e('Registrations, payment, and transactions have been substantially improved in Event Espresso 4.', 'event_espresso'); ?></p>
+			<img src="http://ee-screenshots.s3.amazonaws.com/ee4/ee4-transactions-overview.jpg">
 		</div>
 	</div>
 
