25ae61bf40a9a7860c83aedbd45eed4593b5ea80
Seth <seth@eventespresso.com>
2016-03-24 13:11:31 -0600

    Capitalized titles [touch:9504]
    Signed-off-by: Seth <seth@eventespresso.com>

--- caffeinated/admin/extend/about/templates/ee4-overview.template.php
+++ caffeinated/admin/extend/about/templates/ee4-overview.template.php
@@ -31,7 +31,7 @@
 <div class="changelog">
 	<div class="feature-section col two-col">
 		<div>
-			<h3><?php  _e('Higher customer retention', 'event_espresso'); ?></h3>
+			<h3><?php  _e('Higher Customer Retention', 'event_espresso'); ?></h3>
 			<p><?php  _e('The Event Espresso 4 registration process is faster than ever. With quick ticket selections, single page check-out, and customizable notifications! Registration that can scale to your business needs.', 'event_espresso'); ?></p>
 			<h4><?php  _e('Ticket selection boxes on any post page or post', 'event_espresso'); ?></h4>
 			<p><?php  _e('Customers can easily register for classes, events, or conferences, in just a few simple steps. No matter how you use it, Event Espresso 4 will adapt to a multitude of different ticketing and pricing scenarios.', 'event_espresso'); ?></p>
@@ -46,7 +46,7 @@
 <div class="changelog">
 	<div class="feature-section col two-col">
 		<div>
-			<h3><?php _e('Refined event management', 'event_espresso'); ?></h3>
+			<h3><?php _e('Refined Event Management', 'event_espresso'); ?></h3>
 			<p><?php _e('The new event management screen lets you survey your events at a glance. Want more information? Click to view more. Quickly add/edit prices, dates, or information in any event.', 'event_espresso'); ?></p>
 			<h4><?php _e('Smoother price types, taxes, and price modifiers', 'event_espresso'); ?></h4>
 			<p><?php _e('Price Types allow you to create new prices that adjust the default ticket (base) price for your system-default ticket', 'event_espresso'); ?>.</p>
@@ -64,7 +64,7 @@
 	<h2 class="about-headline-callout"><?php _e('Espresso Arabica 2014, a child theme for events', 'event_espresso'); ?></h2>
 	<div class="feature-section col one-col">
 		<div>
-			<h3><?php _e('Turn your blog into a complete event registration and management system', 'event_espresso'); ?></h3>
+			<h3><?php _e('Turn Your Blog or Website Into a Complete Registration and Ticketing system', 'event_espresso'); ?></h3>
 			<p><?php _e('Create a beautiful event page with ticket selection, venue details, and an integrated single page checkout system. With WordPress, Event Espresso, and Espresso Arabica 2014 (based on the "Twenty Fourteen" theme by WordPress), your events will certainly sell out faster than ever!', 'event_espresso'); ?></p>
 			<p><?php _e('With a striking design that does not compromise the simplicity of WordPress and Event Espresso 4, Espresso Arabica 2014 will be the best event theme on the market.', 'event_espresso'); ?></p>
 		</div>
@@ -75,7 +75,7 @@
 </div>
 <hr>
 
-<h3><?php _e('Pick a theme, any theme', 'event_espresso'); ?></h3>
+<h3><?php _e('Pick a Theme, Any Theme', 'event_espresso'); ?></h3>
 			<p><?php  _e('We’ve made it super easy to integrate Event Espresso with almost any properly coded WordPress theme, including many of the thousands of themes available on WordPress.org.', 'event_espresso'); ?></p>
 <p><img class="about-overview-img" src="http://ee-screenshots.s3.amazonaws.com/ee4/multiple-themes.jpg" /></p>
 <hr>
