35d83cadccc8eb9a0675bcb7b4a680703f2e1af8
Brent Christensen <brent@eventespresso.com>
2016-04-08 14:21:13 -0700

    use new \EE_Offsite_Gateway::handle_IPN_in_this_request() method, and reorder conditional clauses to make sense; don't grab request data directly from global;  [touch:9578]

--- modules/single_page_checkout/reg_steps/payment_options/EE_SPCO_Reg_Step_Payment_Options.class.php
+++ modules/single_page_checkout/reg_steps/payment_options/EE_SPCO_Reg_Step_Payment_Options.class.php
@@ -1870,22 +1870,26 @@ class EE_SPCO_Reg_Step_Payment_Options extends EE_SPCO_Reg_Step {
 	 */
 	private function _process_off_site_payment( EE_Offsite_Gateway $gateway ) {
 		try {
+			$request_data = \EE_Registry::instance()->REQ->params();
 			// if gateway uses_separate_IPN_request, then we don't have to process the IPN manually
-			if ( $gateway instanceof EE_Offsite_Gateway && $gateway->uses_separate_IPN_request() ) {
-				$payment = $this->checkout->transaction->last_payment();
-				//$payment_source = 'last_payment';
-			} else {
+			if (
+				$gateway instanceof EE_Offsite_Gateway
+				&& $gateway->handle_IPN_in_this_request( $request_data, false )
+			) {
 				// get payment details and process results
 				/** @type EE_Payment_Processor $payment_processor */
 				$payment_processor = EE_Registry::instance()->load_core( 'Payment_Processor' );
 				$payment = $payment_processor->process_ipn(
-					$_REQUEST,
+					$request_data,
 					$this->checkout->transaction,
 					$this->checkout->payment_method,
 					true,
 					false
 				);
 				//$payment_source = 'process_ipn';
+			} else {
+				$payment = $this->checkout->transaction->last_payment();
+				//$payment_source = 'last_payment';
 			}
 		} catch ( Exception $e ) {
 			// let's just eat the exception and try to move on using any previously set payment info
