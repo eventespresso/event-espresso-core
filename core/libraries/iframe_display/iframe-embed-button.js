79aa983bd3cf51b8bd2bb3218189bd8da3639dcc
Brent Christensen <brent@eventespresso.com>
2016-04-13 15:23:38 -0700

    add listener to "#post-body-content" so it's usable on all admin pages; add iframe_embed_close_msg to end of content; [touch:9576]

--- core/libraries/iframe_display/iframe-embed-button.js
+++ core/libraries/iframe_display/iframe-embed-button.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	$('#edit-slug-box').on('click', '.iframe-embed-trigger-js', function(e) {
+	$('#post-body-content').on('click', '.iframe-embed-trigger-js', function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 		var iframeEmbedButton = $(this).data( 'iframe_embed_button' );
@@ -7,6 +7,7 @@ jQuery(document).ready(function($) {
 		content += '<br><textarea style="width: 100%; height: 100px;" id="' + iframeEmbedButton + '-content-js">';
 		content += $( iframeEmbedButton ).html();
 		content += '</textarea>';
+		content += '<div style="text-align:right; color: #999;">' + eei18n.iframe_embed_close_msg + '</div>';
 		dialogHelper.displayModal().addContent( content );
 		$( iframeEmbedButton + '-content-js').select();
 	});
