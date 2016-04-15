974fbef590a1940e8adc5a3767c8b69b3a9d61f5
Darren Ethier <darren@roughsmootheng.in>
2016-04-15 16:05:53 -0400

    doh, need to change the callback to point to the new method [touch: 9615]

--- core/admin/EE_Admin_Page_CPT.core.php
+++ core/admin/EE_Admin_Page_CPT.core.php
@@ -724,7 +724,7 @@ abstract class EE_Admin_Page_CPT extends EE_Admin_Page {
 			add_action('trashed_post', array( $this, 'before_trash_cpt_item' ), 10 );
 			add_action('trashed_post', array( $this, 'dont_permanently_delete_ee_cpts'), 10 );
 			add_action('untrashed_post', array( $this, 'before_restore_cpt_item'), 10 );
-			add_action('after_delete_post', array( $this, 'delete_cpt_item'), 10 );
+			add_action('after_delete_post', array( $this, 'before_delete_cpt_item'), 10 );
 		}
 
 	}
