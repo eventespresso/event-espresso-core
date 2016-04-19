35f91ce581c7604c59df0bc94ed06473c1c5fca1
Brent Christensen <brent@eventespresso.com>
2016-04-19 10:27:20 -0700

    type cast microtime() as a string otherwise it gets converted to an integer when used as an array key, which causes the loss of the  microseconds; [touch:9623]

--- core/EE_Config.core.php
+++ core/EE_Config.core.php
@@ -809,7 +809,7 @@ final class EE_Config {
 	public static function log( $config_option_name = '' ) {
 		if ( ! empty( $config_option_name ) ) {
 			$config_log = get_option( EE_Config::LOG_NAME, array() );
-			$config_log[ microtime( true ) ] = $config_option_name;
+			$config_log[ (string) microtime(true) ] = $config_option_name;
 			update_option( EE_Config::LOG_NAME, $config_log );
 		}
 	}
