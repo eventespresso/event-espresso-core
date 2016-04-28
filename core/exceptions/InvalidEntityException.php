05ed973a86fa56a7b272038fa5ef0e7503629289
Brent Christensen <brent@eventespresso.com>
2016-04-28 09:46:16 -0700

    swap around parameter order becuase it makes more sense this was for all of the new exceptions; [touch:9593]

--- core/exceptions/InvalidEntityException.php
+++ core/exceptions/InvalidEntityException.php
@@ -17,10 +17,10 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 	 /**
 	  * InvalidInterfaceException constructor.
 	  *
-	  * @param string $expected classname of the entity we wanted
 	  * @param string $actual classname of what we got
+	  * @param string $expected classname of the entity we wanted
 	  */
-	 public function __construct( $expected, $actual ) {
+	 public function __construct( $actual, $expected ) {
 		 parent::__construct(
 			 sprintf(
 				 __( 'The supplied entity is an instance of "%1$s", but an instance of "%2$s" was expected.', 'event_espresso' ),
