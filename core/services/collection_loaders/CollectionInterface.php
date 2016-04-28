782fe267f23825e65c105d064c9ae5da7f6e91be
Brent Christensen <brent@eventespresso.com>
2016-04-27 23:07:26 -0700

    create moar betterer interface; [touch:9593]

--- core/services/collection_loaders/CollectionInterface.php
+++ core/services/collection_loaders/CollectionInterface.php
@@ -18,53 +18,59 @@ interface CollectionInterface {
 	 * add
 	 * attaches an object to the Collection
 	 * and sets any supplied data associated with the current iterator entry
-	 * by calling EEI_Collection::set_info()
+	 * by calling EE_Object_Collection::set_identifier()
 	 *
 	 * @access public
-	 * @param object $object
-	 * @param mixed  $info
+	 * @param        $object
+	 * @param  mixed $identifier
 	 * @return bool
 	 */
-	public function add( $object, $info = null );
-
-
+	public function add( $object, $identifier = null );
 
 	/**
-	 * set_info
-	 * Sets the info associated with an object in the Collection
+	 * setIdentifier
+	 * Sets the data associated with an object in the Collection
+	 * if no $identifier is supplied, then the spl_object_hash() is used
 	 *
 	 * @access public
-	 * @param object $object
-	 * @param mixed  $info
+	 * @param        $object
+	 * @param  mixed $identifier
 	 * @return bool
 	 */
-	public function set_info( $object, $info = null );
-
-
+	public function setIdentifier( $object, $identifier = null );
 
 	/**
-	 * get_by_info
-	 * finds and returns an object in the Collection based on the info that was set using set_info() or add()
+	 * get
+	 * finds and returns an object in the Collection based on the identifier that was set using addObject()
+	 * PLZ NOTE: the pointer is reset to the beginning of the collection before returning
 	 *
 	 * @access public
-	 * @param mixed
-	 * @return null | object
+	 * @param mixed $identifier
+	 * @return mixed
 	 */
-	public function get_by_info( $info );
-
-
+	public function get( $identifier );
 
 	/**
 	 * has
+	 * returns TRUE or FALSE
+	 * depending on whether the object is within the Collection
+	 * based on the supplied $identifier
+	 *
+	 * @access public
+	 * @param  mixed $identifier
+	 * @return bool
+	 */
+	public function has( $identifier );
+
+	/**
+	 * hasObject
 	 * returns TRUE or FALSE depending on whether the supplied object is within the Collection
 	 *
 	 * @access public
-	 * @param object $object
+	 * @param $object
 	 * @return bool
 	 */
-	public function has( $object );
-
-
+	public function hasObject( $object );
 
 	/**
 	 * remove
@@ -72,9 +78,29 @@ interface CollectionInterface {
 	 *
 	 * @access public
 	 * @param $object
-	 * @return void
+	 * @return bool
 	 */
 	public function remove( $object );
+
+	/**
+	 * setCurrent
+	 * advances pointer to the object whose identifier matches that which was provided
+	 *
+	 * @access public
+	 * @param mixed $identifier
+	 * @return void
+	 */
+	public function setCurrent( $identifier ) ;
+
+	/**
+	 * setCurrentUsingObject
+	 * advances pointer to the provided object
+	 *
+	 * @access public
+	 * @param $object
+	 * @return void
+	 */
+	public function setCurrentUsingObject( $object );
 
 }
 // End of file CollectionInterface.php
