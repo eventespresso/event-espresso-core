dc3999e3321677cb598c97f12002cea14d763f59
Brent Christensen <brent@eventespresso.com>
2016-04-29 10:08:04 -0700

    add $message, $code, $previous parameters to constructor; only set default message if overridden during construction; pass $code and $previous to parent constructor as well; [touch:9593]

--- core/exceptions/InvalidDataTypeException.php
+++ core/exceptions/InvalidDataTypeException.php
@@ -20,13 +20,16 @@ class InvalidDataTypeException extends BaseException {
 	/**
 	 * InvalidDataTypeException constructor
 	 *
-	 * @param string $var_name name of the variable that was of the wrong type ie: "$my_var"
-	 * @param mixed  $variable the actual variable that was of the wrong data type, ie: $my_var
-	 * @param string $expected data type we wanted ie: "integer", "string", "array", etc.
+	 * @param string     $var_name name of the variable that was of the wrong type ie: "$my_var"
+	 * @param mixed      $variable the actual variable that was of the wrong data type, ie: $my_var
+	 * @param string     $expected data type we wanted ie: "integer", "string", "array", etc.
+	 * @param string     $message
+	 * @param int        $code
+	 * @param \Exception $previous
 	 */
-	public function __construct( $var_name, $variable, $expected ) {
-		parent::__construct(
-			sprintf(
+	public function __construct( $var_name, $variable, $expected, $message = '', $code = 0, \Exception $previous = null ) {
+		if ( empty( $message ) ) {
+			$message = sprintf(
 				__(
 					'The supplied data for "%1$s" was %2$s, but %3$s was expected.',
 					'event_espresso'
@@ -34,8 +37,9 @@ class InvalidDataTypeException extends BaseException {
 				$var_name,
 				$this->addIndefiniteArticle( gettype( $variable ) ),
 				$this->addIndefiniteArticle( $expected )
-			)
-		);
+			);
+		}
+		parent::__construct( $message, $code, $previous );
 	}
 
 
