9e309e62d93eb7f00dbca7be3dd8b8d7fd7184f8
Brent Christensen <brent@eventespresso.com>
2016-05-03 18:09:28 -0700

    fix namespace; [touch:9593]

--- core/services/collections/CollectionDetailsInterface.php
+++ core/services/collections/CollectionDetailsInterface.php
@@ -1,5 +1,5 @@
 <?php
-namespace EventEspresso\core\services\collection_loaders;
+namespace EventEspresso\core\services\collections;
 
 if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 	exit( 'No direct script access allowed' );
