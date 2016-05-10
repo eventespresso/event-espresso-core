a9242ede25084fccd52f78e5820de34af3901f02
Darren Ethier <darren@roughsmootheng.in>
2016-05-10 14:12:22 -0400

    additional places where capabilities need to be consistent [touch: 9733]

--- caffeinated/admin/extend/events/Extend_Events_Admin_Page.core.php
+++ caffeinated/admin/extend/events/Extend_Events_Admin_Page.core.php
@@ -395,7 +395,7 @@ class Extend_Events_Admin_Page extends Events_Admin_Page {
 			$reports_link = EE_Admin_Page::add_query_args_and_nonce( $reports_query_args, REG_ADMIN_URL );
 			$actionlinks[] = '<a href="' . $reports_link . '" title="' .  esc_attr__('View Report', 'event_espresso') . '"><div class="dashicons dashicons-chart-bar"></div></a>' . "\n\t";
 		}
-		if ( EE_Registry::instance()->CAP->current_user_can( 'ee_read_messages', 'view_filtered_messages' ) ) {
+		if ( EE_Registry::instance()->CAP->current_user_can( 'ee_read_global_messages', 'view_filtered_messages' ) ) {
 			EE_Registry::instance()->load_helper( 'MSG_Template' );
 			$actionlinks[] = EEH_MSG_Template::get_message_action_link(
 				'see_notifications_for',
@@ -417,7 +417,7 @@ class Extend_Events_Admin_Page extends Events_Admin_Page {
 					'desc' => __('Event Reports', 'event_espresso')
 				);
 		}
-		if ( EE_Registry::instance()->CAP->current_user_can( 'ee_read_messages', 'view_filtered_messages' ) ) {
+		if ( EE_Registry::instance()->CAP->current_user_can( 'ee_read_global_messages', 'view_filtered_messages' ) ) {
 			$related_for_icon = EEH_MSG_Template::get_message_action_icon( 'see_notifications_for' );
 			if ( isset( $related_for_icon['css_class']) && isset( $related_for_icon['label'] ) ) {
 				$items['view_related_messages'] = array(
