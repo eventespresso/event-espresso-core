4a31cc008d64e2cb687cd78472d5a8cdf347c316
Brent Christensen <brent@eventespresso.com>
2016-05-11 15:38:42 -0700

    add abstract parent class for forms that are part of a sequence of forms; [touch:9595]

---
+++ core/libraries/form_sections/SequentialStepForm.php
@@ -0,0 +1,74 @@
+<?php
+namespace EventEspresso\core\libraries\form_sections;
+
+use EventEspresso\Core\Exceptions\InvalidDataTypeException;
+use InvalidArgumentException;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
+
+/**
+ * Class SequentialStepForm
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+abstract class SequentialStepForm extends Form implements SequentialStepFormInterface {
+
+	/**
+	 * @var int $order
+	 */
+	protected $order = 1;
+
+
+
+	/**
+	 * SequentialStepForm constructor.
+	 *
+	 * @param int    $order
+	 * @param string $form_name
+	 * @param string $admin_name
+	 * @param string $slug
+	 * @throws InvalidDataTypeException
+	 * @throws InvalidArgumentException
+	 */
+	public function __construct( $order, $form_name, $admin_name, $slug ) {
+		$this->setOrder( $order );
+		parent::__construct( $form_name, $admin_name, $slug );
+	}
+
+
+
+	/**
+	 * @return int
+	 */
+	public function order() {
+		return $this->order;
+	}
+
+
+
+	/**
+	 * @param int $order
+	 * @throws InvalidArgumentException
+	 */
+	public function setOrder( $order ) {
+		$order = absint( $order );
+		if ( ! $order > 0 ) {
+			throw new InvalidArgumentException(
+				__( 'The form order property must be a positive integer.', 'event_espresso' )
+			);
+		}
+		$this->order = $order;
+	}
+
+
+
+}
+// End of file SequentialStepForm.php
+// Location: /SequentialStepForm.php
