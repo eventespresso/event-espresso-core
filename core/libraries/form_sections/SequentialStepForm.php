aa4d2dbcee58726b0631e30dd5c560ce53403c5f
Brent Christensen <brent@eventespresso.com>
2016-05-12 15:48:13 -0700

    add $redirect_url property; add redirectUrl(), setRedirectUrl() and addRedirectArgs() to satisfy interface; [touch:9595]

--- core/libraries/form_sections/SequentialStepForm.php
+++ core/libraries/form_sections/SequentialStepForm.php
@@ -16,14 +16,24 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
  *
  * @package       Event Espresso
  * @author        Brent Christensen
- * @since         $VID:$
+ * @since         4.9.0
  */
 abstract class SequentialStepForm extends Form implements SequentialStepFormInterface {
 
 	/**
+	 * numerical value used for sorting form steps
+	 * 
 	 * @var int $order
 	 */
 	private $order = 1;
+
+	/**
+	 * a final URL with all form related parameters added
+	 * that will be used to advance to the next step
+	 *
+	 * @var string $redirect_url
+	 */
+	private $redirect_url = '';
 
 
 
@@ -65,6 +75,56 @@ abstract class SequentialStepForm extends Form implements SequentialStepFormInte
 			);
 		}
 		$this->order = $order;
+	}
+
+
+
+
+	/**
+	 * @return string
+	 */
+	public function redirectUrl() {
+		return $this->redirect_url;
+	}
+
+
+
+	/**
+	 * @param string $redirect_url
+	 * @throws InvalidDataTypeException
+	 * @throws \InvalidArgumentException
+	 */
+	public function setRedirectUrl( $redirect_url ) {
+		if ( ! is_string( $redirect_url ) ) {
+			throw new InvalidDataTypeException( '$redirect_url', $redirect_url, 'string' );
+		}
+		if ( empty( $redirect_url ) ) {
+			throw new InvalidArgumentException(
+				__( 'The redirect URL can not be an empty string.', 'event_espresso' )
+			);
+		}
+		$this->redirect_url = $redirect_url;
+	}
+
+
+
+	/**
+	 * @param array $redirect_args
+	 * @throws InvalidDataTypeException
+	 * @throws \InvalidArgumentException
+	 */
+	public function addRedirectArgs( $redirect_args = array() ) {
+		if ( ! is_object( $redirect_args ) ) {
+			throw new InvalidDataTypeException( '$redirect_args', $redirect_args, 'anything other than an object' );
+		}
+		if ( empty( $redirect_args ) ) {
+			throw new InvalidArgumentException(
+				__( 'The redirect arguments can not be an empty array.', 'event_espresso' )
+			);
+		}
+		$this->setRedirectUrl(
+			add_query_arg( $redirect_args, $this->redirect_url )
+		);
 	}
 
 
