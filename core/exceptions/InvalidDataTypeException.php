f6744675d2257b36465a68df69a8a5d984766f55
Brent Christensen <brent@eventespresso.com>
2016-05-16 22:54:36 -0700

    tweak error message setting to be more flexible; [touch:9717]

--- core/exceptions/InvalidDataTypeException.php
+++ core/exceptions/InvalidDataTypeException.php
@@ -20,15 +20,19 @@ class InvalidDataTypeException extends BaseException {
 	/**
 	 * InvalidDataTypeException constructor
 	 *
-	 * @param string     $var_name name of the variable that was of the wrong type ie: "$my_var"
+	 * @param string     $var_name     name of the variable that was of the wrong type ie: "$my_var"
 	 * @param mixed      $variable the actual variable that was of the wrong data type, ie: $my_var
 	 * @param string     $expected data type we wanted ie: "integer", "string", "array", etc.
+	 *                             or an entire rewrite of: "{something something} was expected."
 	 * @param string     $message
 	 * @param int        $code
 	 * @param \Exception $previous
 	 */
 	public function __construct( $var_name, $variable, $expected, $message = '', $code = 0, \Exception $previous = null ) {
 		if ( empty( $message ) ) {
+			$expected .= strpos( ' was expected.', $expected ) === false
+				? $this->addIndefiniteArticle( $expected ) . ' was expected.'
+				: $expected;
 			$message = sprintf(
 				__(
 					'The supplied data for "%1$s" was %2$s, but %3$s was expected.',
@@ -36,7 +40,7 @@ class InvalidDataTypeException extends BaseException {
 				),
 				$var_name,
 				$this->addIndefiniteArticle( gettype( $variable ) ),
-				$this->addIndefiniteArticle( $expected )
+				$expected
 			);
 		}
 		parent::__construct( $message, $code, $previous );
