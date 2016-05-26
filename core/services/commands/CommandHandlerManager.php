79113516a93b1dbbca3c292d4f63a3fab1ac94ff
Brent Christensen <brent@eventespresso.com>
2016-05-26 14:32:53 -0700

    fix how commands and command handlers are resolved; add php doc desc. [touch:9783]

--- core/services/commands/CommandHandlerManager.php
+++ core/services/commands/CommandHandlerManager.php
@@ -9,7 +9,9 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 
 /**
  * Class CommandHandlerManager
- * Description
+ * Connects a Command with its corresponding Command Handler
+ * Command Handlers can be specified explicitly using addCommandHandler()
+ * or determined dynamically if the Command and Command Handler share the exact same namespace
  *
  * @package       Event Espresso
  * @author        Brent Christensen
@@ -27,15 +29,18 @@ class CommandHandlerManager implements CommandHandlerManagerInterface
 
 	/**
 	 * @param \EventEspresso\core\services\commands\CommandHandlerInterface $command_handler
+	 * @param string $fqcn_for_command
 	 * @return mixed
 	 */
-	public function addCommandHandler( CommandHandlerInterface $command_handler )
+	public function addCommandHandler( CommandHandlerInterface $command_handler, $fqcn_for_command = '' )
 	{
-		$command_name = preg_replace( '/(.*)Handler$/i', '$1', get_class( $command_handler ) );
-		if ( empty( $command_name ) ) {
-			throw new InvalidCommandHandlerException( $command_name );
+		$command = ! empty( $fqcn_for_command )
+			? $fqcn_for_command
+			: str_replace( 'CommandHandler', 'Command', get_class( $command_handler ) );
+		if ( empty( $command ) ) {
+			throw new InvalidCommandHandlerException( $command );
 		}
-		$this->commandHandlers[ $command_name ] = $command_handler;
+		$this->commandHandlers[ $command ] = $command_handler;
 	}
 
 
@@ -47,10 +52,15 @@ class CommandHandlerManager implements CommandHandlerManagerInterface
 	public function getCommandHandler( CommandInterface $command )
 	{
 		$command_name = get_class( $command );
+		$command_handler = str_replace( 'Command', 'CommandHandler', $command_name );
+		// has a command handler already been set for this class ?
+		// if not, can we find one via the FQCN ?
 		if ( isset( $this->commandHandlers[ $command_name ] ) ) {
 			return $this->commandHandlers[ $command_name ];
+		} else if ( class_exists( $command_handler ) ) {
+			return new $command_handler();
 		}
-		throw new CommandHandlerNotFoundException( $command_name );
+		throw new CommandHandlerNotFoundException( $command_handler );
 	}
 
 
