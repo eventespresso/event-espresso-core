bbb6fb75962d60695970435754c07906f3e54b35
Brent Christensen <brent@eventespresso.com>
2016-05-26 09:41:35 -0700

    add shell for exception logger class.. will fill out later; [touch:9717]

---
+++ core/exceptions/ExceptionLogger.php
@@ -0,0 +1,24 @@
+<?php
+namespace EventEspresso\core\exceptions;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
+
+/**
+ * Class ExceptionLogger
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class ExceptionLogger {
+
+
+
+}
+// End of file ExceptionLogger.php
+// Location: /ExceptionLogger.php
