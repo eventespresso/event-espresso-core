181a3601acfca2b1cd257a7fa642c098e2995aa5
Brent Christensen <brent@eventespresso.com>
2016-06-01 22:39:55 -0700

    rename class to ExceptionStackTraceDisplay and refactor; [touch:9717]

--- core/exceptions/ExceptionStackTraceDisplay.php
+++ core/exceptions/ExceptionStackTraceDisplay.php
@@ -8,105 +8,53 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 
 
 /**
- * Class BaseException
- * extended by other exceptions
+ * Class ExceptionStackTraceDisplay
+ * displays exceptions with a stack trace and class description for where the exception was thrown
  *
  * @package       Event Espresso
  * @author        Brent Christensen
  * @since         4.9.0
  */
-class BaseException extends \Exception {
-
-
-	/**
-	 *    name of the file to log exceptions to
-	 *
-	 * @access    private
-	 * @var string
-	 */
-	private static $_exception_log_file = 'espresso_error_log.txt';
-
-	/**
-	 *    tracks number of errors
-	 *
-	 * @access    private
-	 * @var int
-	 */
-	private static $_error_count = 0;
-
-	/**
-	 *    stores details for all exception
-	 *
-	 * @access    private
-	 * @var array
-	 */
-	private static $_all_exceptions = array();
+class ExceptionStackTraceDisplay {
 
 
 
 	/**
-	 * @param string     $message
-	 * @param int        $code
-	 * @param \Exception $previous
-	 * @throws \EventEspresso\core\exceptions\BaseException
+	 * @param \Exception $exception
 	 */
-	public function __construct( $message, $code = 0, \Exception $previous = null ) {
-		parent::__construct( $message, $code, $previous );
+	public function __construct( \Exception $exception ) {
 		if ( WP_DEBUG ) {
-			$this->get_error();
+			$this->displayException( $exception );
 		}
 	}
 
 
 
 	/**
-	 * @access public
-	 * @throws \EventEspresso\core\exceptions\BaseException
+	 * @access protected
+	 * @param \Exception $exception
 	 */
-	public function get_error() {
-		if ( apply_filters( 'FHEE__BaseException__get_error__show_normal_exceptions', false ) ) {
-			throw $this;
-		}
+	protected function displayException( \Exception $exception ) {
+
+		$error_code = '';
+		$trace_details = '';
+		$time = time();
+		$trace = $exception->getTrace();
 		// get separate user and developer messages if they exist
-		$msg = explode( '||', $this->getMessage() );
+		$msg = explode( '||', $exception->getMessage() );
 		$user_msg = $msg[0];
 		$dev_msg = isset( $msg[1] ) ? $msg[1] : $msg[0];
 		$msg = WP_DEBUG ? $dev_msg : $user_msg;
-		// add details to _all_exceptions array
-		$x_time = time();
-		self::$_all_exceptions[ $x_time ]['name'] = get_class( $this );
-		self::$_all_exceptions[ $x_time ]['file'] = $this->getFile();
-		self::$_all_exceptions[ $x_time ]['line'] = $this->getLine();
-		self::$_all_exceptions[ $x_time ]['msg'] = $msg;
-		self::$_all_exceptions[ $x_time ]['code'] = $this->getCode();
-		self::$_all_exceptions[ $x_time ]['trace'] = $this->getTrace();
-		self::$_all_exceptions[ $x_time ]['string'] = $this->getTraceAsString();
-		self::$_error_count++;
-		$this->display_errors();
-	}
-
-
-
-	/**
-	 *    display_errors
-	 *
-	 * @access public
-	 * @echo   string
-	 */
-	public function display_errors() {
-		$error_code = '';
-		$trace_details = '';
-		$output = BaseException::_exception_styles();
+		// start gathering output
+		$output = $this->exceptionStyles();
 		$output .= '
 <div id="ee-error-message" class="error">';
 		if ( ! WP_DEBUG ) {
 			$output .= '
 	<p>';
 		}
-		// cycle thru errors
-		foreach ( self::$_all_exceptions as $time => $ex ) {
 			// process trace info
-			if ( empty( $ex['trace'] ) ) {
+			if ( empty( $trace ) ) {
 				$trace_details .= __(
 					'Sorry, but no trace information was available for this exception.',
 					'event_espresso'
@@ -121,9 +69,9 @@ class BaseException extends \Exception {
 					<th scope="col" align="left" style="width:40%;">File</th>
 					<th scope="col" align="left">' . __( 'Class', 'event_espresso' ) . '->' . __( 'Method( arguments )', 'event_espresso' ) . '</th>
 				</tr>';
-				$last_on_stack = count( $ex['trace'] ) - 1;
+				$last_on_stack = count( $trace ) - 1;
 				// reverse array so that stack is in proper chronological order
-				$sorted_trace = array_reverse( $ex['trace'] );
+				$sorted_trace = array_reverse( $trace );
 				foreach ( $sorted_trace as $nmbr => $trace ) {
 					$file = isset( $trace['file'] ) ? $trace['file'] : '';
 					$class = isset( $trace['class'] ) ? $trace['class'] : '';
@@ -142,9 +90,9 @@ class BaseException extends \Exception {
 						}
 					}
 					if ( $nmbr === $last_on_stack ) {
-						$file = $ex['file'] !== '' ? $ex['file'] : $file;
-						$line = $ex['line'] !== '' ? $ex['line'] : $line;
-						$error_code = self::generate_error_code( $file, $trace['function'], $line );
+						$file = $exception->getFile() !== '' ? $exception->getFile() : $file;
+						$line = $exception->getLine() !== '' ? $exception->getLine() : $line;
+						$error_code = $this->generate_error_code( $file, $trace['function'], $line );
 					}
 					$file = \EEH_File::standardise_directory_separators( $file );
 					$nmbr = ! empty( $nmbr ) ? $nmbr : '&nbsp;';
@@ -166,13 +114,13 @@ class BaseException extends \Exception {
 			 </table>
 			</div>';
 			}
-			$ex['code'] = $ex['code'] ? $ex['code'] : $error_code;
+			$code = $exception->getCode() ? $exception->getCode() : $error_code;
 			// add generic non-identifying messages for non-privileged users
 			if ( ! WP_DEBUG ) {
 				$output .= '<span class="ee-error-user-msg-spn">'
-				           . trim( $ex['msg'] )
+				           . trim( $msg )
 				           . '</span> &nbsp; <sup>'
-				           . $ex['code']
+				           . $code
 				           . '</sup><br />';
 			} else {
 				// or helpful developer messages if debugging is on
@@ -183,19 +131,17 @@ class BaseException extends \Exception {
 				. sprintf(
 					__( '%1$sAn %2$s was thrown!%3$s code: %4$s', 'event_espresso' ),
 				    '<strong class="ee-error-dev-msg-str">',
-					$ex['name'],
+					get_class( $exception ),
 					'</strong>  &nbsp; <span>',
-					$ex['code'] . '</span>'
+					$code . '</span>'
 				)
 				. '<br />
 				<span class="big-text">"'
-				           . trim( $ex['msg'] )
+				           . trim( $msg )
 				           . '"</span><br/>
-				<a id="display-ee-error-trace-'
-				           . self::$_error_count
+				<a id="display-ee-error-trace-1'
 				           . $time
-				           . '" class="display-ee-error-trace-lnk small-text" rel="ee-error-trace-'
-				           . self::$_error_count
+				           . '" class="display-ee-error-trace-lnk small-text" rel="ee-error-trace-1'
 				           . $time
 				           . '">
 					'
@@ -203,17 +149,16 @@ class BaseException extends \Exception {
 				           . '
 				</a><br />
 				'
-				           . $ex['file']
+				           . $exception->getFile()
 				           . sprintf(
 					           __( '%1$s( line no: %2$s )%3$s', 'event_espresso' ),
 					           ' &nbsp; <span class="small-text lt-grey-text">',
-					           $ex['line'],
+					           $exception->getLine(),
 					           '</span>'
 				           )
 				           . '
 			</p>
-			<div id="ee-error-trace-'
-				           . self::$_error_count
+			<div id="ee-error-trace-1'
 				           . $time
 				           . '-dv" class="ee-error-trace-dv" style="display: none;">
 				'
@@ -234,8 +179,6 @@ class BaseException extends \Exception {
 		</div>
 		<br />';
 			}
-			$this->write_to_error_log( $time, $ex );
-		}
 		// remove last linebreak
 		$output = substr( $output, 0, count( $output ) - 7 );
 		if ( ! WP_DEBUG ) {
@@ -244,7 +187,7 @@ class BaseException extends \Exception {
 		}
 		$output .= '
 </div>';
-		$output .= self::_print_scripts( true );
+		$output .= $this->printScripts( true );
 		if ( defined( 'DOING_AJAX' ) ) {
 			echo json_encode( array( 'error' => $output ) );
 			exit();
@@ -329,13 +272,13 @@ class BaseException extends \Exception {
 	 * create error code from filepath, function name,
 	 * and line number where exception or error was thrown
 	 *
-	 * @access public
+	 * @access protected
 	 * @param string $file
 	 * @param string $func
 	 * @param string $line
 	 * @return string
 	 */
-	public static function generate_error_code( $file = '', $func = '', $line = '' ) {
+	protected function generate_error_code( $file = '', $func = '', $line = '' ) {
 		$file_bits = explode( '.', basename( $file ) );
 		$error_code = ! empty( $file_bits[0] ) ? $file_bits[0] : '';
 		$error_code .= ! empty( $func ) ? ' - ' . $func : '';
@@ -345,56 +288,7 @@ class BaseException extends \Exception {
 
 
 
-	/**
-	 * write exception details to log file
-	 *
-	 * @param int   $time
-	 * @param array $ex
-	 * @param bool  $clear
-	 */
-	public function write_to_error_log( $time = 0, $ex = array(), $clear = false ) {
-		if ( empty( $ex ) ) {
-			return;
-		}
-		if ( ! $time ) {
-			$time = time();
-		}
-		$exception_log = '----------------------------------------------------------------------------------------'
-		                 . PHP_EOL;
-		$exception_log .= '[' . date( 'Y-m-d H:i:s', $time ) . ']  Exception Details' . PHP_EOL;
-		$exception_log .= 'Message: ' . $ex['msg'] . PHP_EOL;
-		$exception_log .= 'Code: ' . $ex['code'] . PHP_EOL;
-		$exception_log .= 'File: ' . $ex['file'] . PHP_EOL;
-		$exception_log .= 'Line No: ' . $ex['line'] . PHP_EOL;
-		$exception_log .= 'Stack trace: ' . PHP_EOL;
-		$exception_log .= $ex['string'] . PHP_EOL;
-		$exception_log .= '----------------------------------------------------------------------------------------'
-		                  . PHP_EOL;
-		try {
-			\EEH_File::ensure_file_exists_and_is_writable(
-				EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . BaseException::$_exception_log_file
-			);
-			\EEH_File::add_htaccess_deny_from_all( EVENT_ESPRESSO_UPLOAD_DIR . 'logs' );
-			if ( ! $clear ) {
-				//get existing log file and append new log info
-				$exception_log = \EEH_File::get_file_contents(
-						EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . BaseException::$_exception_log_file
-					) . $exception_log;
-			}
-			\EEH_File::write_to_file(
-				EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . BaseException::$_exception_log_file,
-				$exception_log
-			);
-		} catch ( \Exception $e ) {
-			\EE_Error::add_error(
-				sprintf(
-					__( 'Event Espresso error logging could not be setup because: %s', 'event_espresso' ),
-					$e->getMessage()
-				)
-			);
-			return;
-		}
-	}
+
 
 
 
@@ -404,7 +298,7 @@ class BaseException extends \Exception {
 	 *
 	 * @return string
 	 */
-	private static function _exception_styles() {
+	private static function exceptionStyles() {
 		return '
 <style type="text/css">
 	#ee-error-message {
@@ -472,7 +366,7 @@ class BaseException extends \Exception {
 	 * @param bool $force_print
 	 * @return string|void
 	 */
-	private static function _print_scripts( $force_print = false ) {
+	private function printScripts( $force_print = false ) {
 		if ( ! $force_print  && ( did_action( 'admin_enqueue_scripts' ) || did_action( 'wp_enqueue_scripts' ) ) ) {
 			if ( wp_script_is( 'ee_error_js', 'enqueued' ) ) {
 				return '';
@@ -500,5 +394,5 @@ var ee_settings = {"wp_debug":"' . WP_DEBUG . '"};
 
 
 }
-// End of file BaseException.php
-// Location: /core/exceptions/BaseException.php
+// End of file ExceptionStackTraceDisplay.php
+// Location: /core/exceptions/ExceptionStackTraceDisplay.php
