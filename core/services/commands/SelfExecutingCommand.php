459290180eb5590f06a27b3252b468bce95eabb6
Brent Christensen <brent@eventespresso.com>
2016-06-07 12:18:53 -0700

    add SelfExecutingCommand class; [touch:9783]

--- core/services/commands/SelfExecutingCommand.php
+++ core/services/commands/SelfExecutingCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace EventEspresso\core\services\commands;
 
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+
 if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 	exit( 'No direct script access allowed' );
 }
@@ -10,7 +12,9 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 /**
  * Class SelfExecutingCommand
  * Commands that implement SelfExecutingCommandInterface
- * will automatically execute upon instantiation
+ * will automatically execute upon instantiation.
+ * If passed a valid property name,
+ * will assign the returned value from execute() to that property
  *
  * @package       Event Espresso
  * @author        Brent Christensen
@@ -18,6 +22,39 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
  */
 class SelfExecutingCommand extends Command implements SelfExecutingCommandInterface
 {
+
+
+
+	/**
+	 * Command constructor
+	 *
+	 * @param \EE_Registry $registry
+	 * @param CommandBusInterface $command_bus
+	 * @param string              $property if passed the name of a valid property on the Command object,
+	 *                                      will set it's value equal to that returned from execute()
+	 */
+	public function __construct( \EE_Registry $registry, CommandBusInterface $command_bus, $property = '' ) {
+		parent::__construct( $registry, $command_bus );
+		if ( ! empty( $property ) ) {
+			if ( ! is_string( $property ) ) {
+				throw new InvalidDataTypeException( '$property', $property, 'string' );
+			}
+			if ( ! property_exists( $this, $property ) ) {
+				throw new \InvalidArgumentException(
+					sprintf(
+						__( 'The "%1$s" property does not exist on the %2$s class.', 'event_espresso' ),
+						$property,
+						get_class( $this )
+					)
+				);
+			}
+			$this->{$property} = $this->execute();
+		} else {
+			$this->execute();
+		}
+	}
+
+
 
 	/**
 	 * @return mixed
