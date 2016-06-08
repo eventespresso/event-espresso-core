43769d9f50a24c3d5f9fe8fe965424f4e67d2074
Brent Christensen <brent@eventespresso.com>
2016-06-08 12:04:34 -0700

    extract logic for cancelling a line item into its own command; [touch:9783]

--- core/services/registration/CancelRegistrationAndTicketLineItemCommandHandler.php
+++ core/services/registration/CancelRegistrationAndTicketLineItemCommandHandler.php
@@ -1,7 +1,6 @@
 <?php
 namespace EventEspresso\core\services\registration;
 
-use EventEspresso\core\exceptions\EntityNotFoundException;
 use EventEspresso\core\exceptions\InvalidEntityException;
 use EventEspresso\core\services\commands\CommandHandlerInterface;
 use EventEspresso\core\services\commands\CommandInterface;
@@ -36,57 +35,16 @@ class CancelRegistrationAndTicketLineItemCommandHandler implements CommandHandle
 			throw new InvalidEntityException( get_class( $command ), 'CancelRegistrationAndTicketLineItemCommand' );
 		}
 		$registration = $command->registration();
-		$transaction = $registration->transaction();
-		$ticket = $registration->ticket();
-		// get line item for original ticket
-		$ticket_line_item = $this->getTicketLineItem( $transaction, $ticket );
-		// get event line item for ticket and decrement quantity
-		\EEH_Line_Item::decrement_quantity(
-			\EEH_Line_Item::get_event_line_item_for_ticket(
-				$transaction->total_line_item(),
-				$ticket
-			)
+		$command->executeSubCommand(
+			'CancelTicketLineItemCommand',
+			array( $registration->transaction(), $registration->ticket() )
 		);
-		// then cancel original line item for ticket
-		\EEH_Line_Item::cancel_line_item( $ticket_line_item );
 		// cancel original registration
 		$registration->set_status( \EEM_Registration::status_id_cancelled );
 		$registration->save();
 		return true;
 	}
 
-
-
-	/**
-	 * @param \EE_Transaction $transaction
-	 * @param \EE_Ticket      $ticket
-	 * @return \EE_Line_Item
-	 * @throws EntityNotFoundException
-	 * @throws \EE_Error
-	 */
-	protected static function getTicketLineItem( \EE_Transaction $transaction, \EE_Ticket $ticket )
-	{
-		$line_item = null;
-		$ticket_line_items = \EEH_Line_Item::get_line_items_by_object_type_and_IDs(
-			$transaction->total_line_item(),
-			'Ticket',
-			array( $ticket->ID() )
-		);
-		foreach ( $ticket_line_items as $ticket_line_item ) {
-			if (
-				$ticket_line_item instanceof \EE_Line_Item
-				&& $ticket_line_item->OBJ_type() === 'Ticket'
-				&& $ticket_line_item->OBJ_ID() === $ticket->ID()
-			) {
-				$line_item = $ticket_line_item;
-				break;
-			}
-		}
-		if ( ! ( $line_item instanceof \EE_Line_Item && $line_item->OBJ_type() === 'Ticket' ) ) {
-			throw new EntityNotFoundException( 'Line Item Ticket ID', $ticket->ID() );
-		}
-		return $line_item;
-	}
 
 
 
