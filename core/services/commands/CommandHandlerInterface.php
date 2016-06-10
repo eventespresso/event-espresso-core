e6251eea38d8d2ba65932191bc006596a4f5d4e8
Brent Christensen <brent@eventespresso.com>
2016-06-10 13:44:39 -0700

    add methods removed from Command; [touch:9783]

--- core/services/commands/CommandHandlerInterface.php
+++ core/services/commands/CommandHandlerInterface.php
@@ -21,6 +21,32 @@ interface CommandHandlerInterface
 	 */
 	public function handle( CommandInterface $command );
 
+
+
+	/**
+	 * @return \EventEspresso\core\services\commands\CommandBusInterface
+	 */
+	public function commandBus();
+
+
+
+	/**
+	 * @return \EE_Registry
+	 */
+	public function registry();
+
+
+
+	/**
+	 * Given a valid Command class name, will execute that Command using the supplied arguments array
+	 *
+	 * @param string $command_name
+	 * @param array  $arguments
+	 * @return mixed
+	 */
+	public function executeSubCommand( $command_name, $arguments = array() );
+
+
 }
 // End of file CommandHandlerInterface.php
 // Location: core/services/commands/CommandHandlerInterface.php
