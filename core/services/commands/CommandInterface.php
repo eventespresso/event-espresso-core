3299de37ef4ef053464de69bac5c6585b51d12c3
Brent Christensen <brent@eventespresso.com>
2016-06-10 13:43:54 -0700

    remove unnecessary methods; [touch:9783]

--- core/services/commands/CommandInterface.php
+++ core/services/commands/CommandInterface.php
@@ -15,25 +15,6 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 interface CommandInterface
 {
 
-	/**
-	 * @return \EventEspresso\core\services\commands\CommandBusInterface
-	 */
-	public function commandBus();
-
-	/**
-	 * @return \EE_Registry
-	 */
-	public function registry();
-
-	/**
-	 * Given a valid Command class name, will execute that Command using the supplied arguments array
-	 * 
-	 * @param string $command_name
-	 * @param array  $arguments
-	 * @return mixed
-	 */
-	public function executeSubCommand( $command_name, $arguments = array() );
-
 }
 // End of file CommandInterface.php
 // Location: core/services/commands/CommandInterface.php
