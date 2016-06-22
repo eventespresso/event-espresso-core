5ac8e5e817fbb33ea5717ac5330f39cd6b34460f
Brent Christensen <brent@eventespresso.com>
2016-06-22 16:49:57 -0700

    add properties for domain services and inject into constructor; convert usage of subcommands to domain services; [touch:9783]

--- core/services/commands/ticket/CancelTicketLineItemCommandHandler.php
+++ core/services/commands/ticket/CancelTicketLineItemCommandHandler.php
@@ -1,19 +1,23 @@
 <?php
 namespace EventEspresso\core\services\commands\ticket;
 
+use EventEspresso\core\domain\services\ticket\CancelTicketLineItemService;
 use EventEspresso\core\exceptions\InvalidEntityException;
 use EventEspresso\core\services\commands\CommandHandler;
 use EventEspresso\core\services\commands\CommandInterface;
 
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
+if ( ! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
 }
 
 
 
 /**
  * Class CancelTicketLineItemCommandHandler
- * Description
+ * Decrements the quantity for the provided ticket line item,
+ * then if it's new quantity is zero,
+ * either cancels it altogether (by setting its type to EEM_Line_Item::type_cancellation)
+ * or inserts a new line item with a type of EEM_Line_Item::type_cancellation
  *
  * @package       Event Espresso
  * @author        Brent Christensen
@@ -23,66 +27,42 @@ class CancelTicketLineItemCommandHandler extends CommandHandler
 {
 
 
+    /**
+     * @var CancelTicketLineItemService $cancel_ticket_line_item_service
+     */
+    private $cancel_ticket_line_item_service;
 
-	/**
-	 * @param \EventEspresso\core\services\commands\CommandInterface $command
-	 * @return mixed
-	 */
-	public function handle( CommandInterface $command )
-	{
-		/** @var CancelTicketLineItemCommand $command */
-		if ( ! $command instanceof CancelTicketLineItemCommand ) {
-			throw new InvalidEntityException( get_class( $command ), 'CancelTicketLineItemCommand' );
-		}
-		$ticket_line_item = $command->ticketLineItem();
-		// first we need to decrement the ticket quantity
-		\EEH_Line_Item::decrement_quantity( $ticket_line_item, 1 );
-		// no tickets left for this line item ?
-		if ( (int) $ticket_line_item->quantity() === 0 ) {
-			// then just set this line item as cancelled, save, and get out
-			$ticket_line_item->set_type( \EEM_Line_Item::type_cancellation );
-			$success = $ticket_line_item->save();
-		} else {
-			$transaction = $command->transaction();
-			$ticket = $command->ticket();
-			// otherwise create a new cancelled line item, so that we have a record of the cancellation
-			$items_subtotal = \EEH_Line_Item::get_pre_tax_subtotal(
-				\EEH_Line_Item::get_event_line_item_for_ticket(
-					$transaction->total_line_item(),
-					$ticket
-				)
-			);
-			$cancelled_line_item = \EE_Line_Item::new_instance(
-				array(
-					'LIN_name'       => $ticket_line_item->name(),
-					'LIN_desc'       => sprintf(
-						__( '%1$s Cancelled: %2$s', 'event_espresso' ),
-						$ticket_line_item->desc(),
-						date( 'Y-m-d h:i a' )
-					),
-					'LIN_unit_price' => (float) $ticket_line_item->unit_price(),
-					'LIN_quantity'   => 1,
-					'LIN_percent'    => null,
-					'LIN_is_taxable' => false,
-					'LIN_order'      => $items_subtotal instanceof \EE_Line_Item
-						? count( $items_subtotal->children() )
-						: 0,
-					'LIN_total'      => (float) $ticket_line_item->unit_price(),
-					'LIN_type'       => \EEM_Line_Item::type_cancellation
-				)
-			);
-			$success = \EEH_Line_Item::add_item( $transaction->total_line_item(), $cancelled_line_item );
-		}
-		if ( ! $success ) {
-			throw new \RuntimeException(
-				sprintf(
-					__( 'An error occurred while attempting to cancel ticket line item %1$s', 'event_espresso' ),
-					$ticket_line_item->ID()
-				)
-			);
-		}
-		return $success;
-	}
+
+
+    /**
+     * Command constructor
+     *
+     * @param CancelTicketLineItemService $cancel_ticket_line_item_service
+     */
+    public function __construct(CancelTicketLineItemService $cancel_ticket_line_item_service)
+    {
+        $this->cancel_ticket_line_item_service = $cancel_ticket_line_item_service;
+    }
+
+
+
+    /**
+     * @param \EventEspresso\core\services\commands\CommandInterface $command
+     * @return mixed
+     */
+    public function handle(CommandInterface $command)
+    {
+        /** @var CancelTicketLineItemCommand $command */
+        if ( ! $command instanceof CancelTicketLineItemCommand) {
+            throw new InvalidEntityException(get_class($command), 'CancelTicketLineItemCommand');
+        }
+        return $this->cancel_ticket_line_item_service->cancel(
+            $command->transaction(),
+            $command->ticket(),
+            $command->quantity(),
+            $command->ticketLineItem()
+        );
+    }
 
 
 }
