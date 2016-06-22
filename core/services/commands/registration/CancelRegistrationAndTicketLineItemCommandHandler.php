a824e4f8846ec20c3cea5403d6fcf1a700634a08
Brent Christensen <brent@eventespresso.com>
2016-06-22 16:49:36 -0700

    add properties for domain services and inject into constructor; convert usage of subcommands to domain services; [touch:9783]

--- core/services/commands/registration/CancelRegistrationAndTicketLineItemCommandHandler.php
+++ core/services/commands/registration/CancelRegistrationAndTicketLineItemCommandHandler.php
@@ -1,12 +1,13 @@
 <?php
 namespace EventEspresso\core\services\commands\registration;
 
+use EventEspresso\core\domain\services\ticket\CancelTicketLineItemService;
 use EventEspresso\core\exceptions\InvalidEntityException;
 use EventEspresso\core\services\commands\CommandHandler;
 use EventEspresso\core\services\commands\CommandInterface;
 
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
+if ( ! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
 }
 
 
@@ -23,28 +24,42 @@ class CancelRegistrationAndTicketLineItemCommandHandler extends CommandHandler
 {
 
 
+    /**
+     * @var CancelTicketLineItemService $cancel_ticket_line_item_service
+     */
+    private $cancel_ticket_line_item_service;
 
-	/**
-	 * @param \EventEspresso\core\services\commands\CommandInterface $command
-	 * @return boolean
-	 */
-	public function handle( CommandInterface $command )
-	{
-		/** @var CancelRegistrationAndTicketLineItemCommand $command */
-		if ( ! $command instanceof CancelRegistrationAndTicketLineItemCommand ) {
-			throw new InvalidEntityException( get_class( $command ), 'CancelRegistrationAndTicketLineItemCommand' );
-		}
-		$registration = $command->registration();
-		$this->executeSubCommand(
-			'EventEspresso\core\services\commands\ticket\CancelTicketLineItemCommand',
-			array( $registration->transaction(), $registration->ticket() )
-		);
-		// cancel original registration
-		$registration->set_status( \EEM_Registration::status_id_cancelled );
-		$registration->save();
-		return true;
-	}
 
+
+    /**
+     * Command constructor
+     *
+     * @param CancelTicketLineItemService $cancel_ticket_line_item_service
+     */
+    public function __construct(CancelTicketLineItemService $cancel_ticket_line_item_service)
+    {
+        $this->cancel_ticket_line_item_service = $cancel_ticket_line_item_service;
+    }
+
+
+
+    /**
+     * @param \EventEspresso\core\services\commands\CommandInterface $command
+     * @return boolean
+     */
+    public function handle(CommandInterface $command)
+    {
+        /** @var CancelRegistrationAndTicketLineItemCommand $command */
+        if ( ! $command instanceof CancelRegistrationAndTicketLineItemCommand) {
+            throw new InvalidEntityException(get_class($command), 'CancelRegistrationAndTicketLineItemCommand');
+        }
+        $registration = $command->registration();
+        $this->cancel_ticket_line_item_service->forRegistration($registration);
+        // cancel original registration
+        $registration->set_status(\EEM_Registration::status_id_cancelled);
+        $registration->save();
+        return true;
+    }
 
 
 
