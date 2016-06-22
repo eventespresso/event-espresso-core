615bc9257b6dc2051c474ece894cc909b8c62c67
Brent Christensen <brent@eventespresso.com>
2016-06-22 16:50:32 -0700

    add properties for domain services and inject into constructor; convert usage of subcommands to domain services; [touch:9783]

--- core/services/commands/registration/CopyRegistrationPaymentsCommandHandler.php
+++ core/services/commands/registration/CopyRegistrationPaymentsCommandHandler.php
@@ -1,13 +1,13 @@
 <?php
 namespace EventEspresso\core\services\commands\registration;
 
+use EventEspresso\core\domain\services\registration\CopyRegistrationService;
 use EventEspresso\core\exceptions\InvalidEntityException;
-use EventEspresso\core\exceptions\UnexpectedEntityException;
 use EventEspresso\core\services\commands\CommandHandler;
 use EventEspresso\core\services\commands\CommandInterface;
 
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
+if ( ! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
 }
 
 
@@ -26,47 +26,40 @@ class CopyRegistrationPaymentsCommandHandler extends CommandHandler
 {
 
 
+    /**
+     * @var CopyRegistrationService $copy_registration_service
+     */
+    private $copy_registration_service;
 
-	/**
-	 * @param \EventEspresso\core\services\commands\CommandInterface $command
-	 * @return boolean
-	 */
-	public function handle( CommandInterface $command ) {
-		/** @var CopyRegistrationPaymentsCommand $command */
-		if ( ! $command instanceof CopyRegistrationPaymentsCommand ) {
-			throw new InvalidEntityException( get_class( $command ), 'CopyRegistrationPaymentsCommand' );
-		}
-		$target_registration = $command->targetRegistration();
-		$registration_to_copy = $command->registrationToCopy();
-		$previous_payments = $registration_to_copy->registration_payments();
-		foreach ( $previous_payments as $previous_payment ) {
-			if (
-				$previous_payment instanceof \EE_Registration_Payment
-				&& $previous_payment->payment() instanceof \EE_Payment
-				&& $previous_payment->payment()->is_approved()
-			) {
-				$new_registration_payment = \EE_Registration_Payment::new_instance(
-					array(
-						'REG_ID'     => $target_registration->ID(),
-						'PAY_ID'     => $previous_payment->ID(),
-						'RPY_amount' => $previous_payment->amount(),
-					)
-				);
-				if ( ! $new_registration_payment instanceof \EE_Registration_Payment ) {
-					throw new UnexpectedEntityException( $new_registration_payment, 'EE_Registration_Payment' );
-				}
-				$new_registration_payment->save();
-				$target_registration->set_paid( $previous_payment->amount() );
-				$target_registration->save();
-				// if new reg payment is good, then set old reg payment amount to zero
-				$previous_payment->set_amount( 0 );
-				$previous_payment->save();
-				$registration_to_copy->set_paid( 0 );
-				$registration_to_copy->save();
-			}
-		}
-		return true;
-	}
+
+
+    /**
+     * Command constructor
+     *
+     * @param CopyRegistrationService $copy_registration_service
+     */
+    public function __construct(CopyRegistrationService $copy_registration_service)
+    {
+        $this->copy_registration_service = $copy_registration_service;
+    }
+
+
+
+    /**
+     * @param \EventEspresso\core\services\commands\CommandInterface $command
+     * @return boolean
+     */
+    public function handle(CommandInterface $command)
+    {
+        /** @var CopyRegistrationPaymentsCommand $command */
+        if ( ! $command instanceof CopyRegistrationPaymentsCommand) {
+            throw new InvalidEntityException(get_class($command), 'CopyRegistrationPaymentsCommand');
+        }
+        return $this->copy_registration_service->copyPaymentDetails(
+            $command->targetRegistration(),
+            $command->registrationToCopy()
+        );
+    }
 
 
 
