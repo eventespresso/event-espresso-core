32c343a628e4943343c156a3cd09c7edec1a1b12
Brent Christensen <brent@eventespresso.com>
2016-06-22 16:24:00 -0700

    add $quantity property plus getter, and inject into constructor; add static fromRegistration() method; [touch:9807]

--- core/services/commands/ticket/CancelTicketLineItemCommand.php
+++ core/services/commands/ticket/CancelTicketLineItemCommand.php
@@ -1,11 +1,10 @@
 <?php
 namespace EventEspresso\core\services\commands\ticket;
 
-use EventEspresso\core\exceptions\EntityNotFoundException;
 use EventEspresso\core\services\commands\Command;
 
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
+if ( ! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
 }
 
 
@@ -22,124 +21,121 @@ class CancelTicketLineItemCommand extends Command
 {
 
 
-	/**
-	 * @var \EE_Transaction $transaction
-	 */
-	private $transaction;
+    /**
+     * @var \EE_Transaction $transaction
+     */
+    private $transaction;
 
-	/**
-	 * @var \EE_Ticket $ticket
-	 */
-	private $ticket;
+    /**
+     * @var \EE_Ticket $ticket
+     */
+    private $ticket;
 
-	/**
-	 * @var \EE_Line_Item $ticket_line_item
-	 */
-	protected $ticket_line_item;
+    /**
+     * @var \EE_Line_Item $ticket_line_item
+     */
+    protected $ticket_line_item;
 
-
-
-	/**
-	 * @param \EE_Line_Item       $ticket_line_item
-	 * @param int                 $quantity
-	 */
-	public static function fromTicketLineItem(
-		\EE_Line_Item $ticket_line_item,
-		$quantity = 1
-	) {
-		new self(
-			$ticket_line_item->transaction(),
-			$ticket_line_item->ticket(),
-			$quantity,
-			$ticket_line_item
-		);
-	}
+    /**
+     * @var int $quantity
+     */
+    protected $quantity;
 
 
 
-	/**
-	 * CancelTicketLineItemCommand constructor.
-	 *
-	 * @param \EE_Transaction     $transaction
-	 * @param \EE_Ticket          $ticket
-	 * @param int                 $quantity
-	 * @param \EE_Line_Item       $ticket_line_item
-	 */
-	public function __construct(
-		\EE_Transaction $transaction,
-		\EE_Ticket $ticket,
-		$quantity = 1,
-		\EE_Line_Item $ticket_line_item = null
-	) {
-		$this->transaction = $transaction;
-		$this->ticket = $ticket;
-		$this->ticket_line_item = $ticket_line_item instanceof \EE_Line_Item
-			? $ticket_line_item
-			: $this->getTicketLineItem( $transaction, $ticket );
-	}
+    /**
+     * @param \EE_Registration $registration
+     * @param int              $quantity
+     */
+    public static function fromRegistration(\EE_Registration $registration, $quantity = 1)
+    {
+        new self(
+            $registration->transaction(),
+            $registration->ticket(),
+            1,
+            $registration->ticket_line_item()
+        );
+    }
 
 
 
-	/**
-	 * @return \EE_Transaction
-	 */
-	public function transaction()
-	{
-		return $this->transaction;
-	}
+    /**
+     * @param \EE_Line_Item $ticket_line_item
+     * @param int           $quantity
+     */
+    public static function fromTicketLineItem(
+        \EE_Line_Item $ticket_line_item,
+        $quantity = 1
+    ) {
+        new self(
+            $ticket_line_item->transaction(),
+            $ticket_line_item->ticket(),
+            $quantity,
+            $ticket_line_item
+        );
+    }
 
 
 
-	/**
-	 * @return \EE_Ticket
-	 */
-	public function ticket()
-	{
-		return $this->ticket;
-	}
+    /**
+     * CancelTicketLineItemCommand constructor.
+     *
+     * @param \EE_Transaction $transaction
+     * @param \EE_Ticket      $ticket
+     * @param int             $quantity
+     * @param \EE_Line_Item   $ticket_line_item
+     */
+    public function __construct(
+        \EE_Transaction $transaction,
+        \EE_Ticket $ticket,
+        $quantity = 1,
+        \EE_Line_Item $ticket_line_item = null
+    ) {
+        $this->transaction = $transaction;
+        $this->ticket = $ticket;
+        $this->quantity = min(1, absint($quantity));
+        $this->ticket_line_item = $ticket_line_item;
+    }
 
 
 
-	/**
-	 * @return \EE_Line_Item
-	 */
-	public function ticketLineItem()
-	{
-		return $this->ticket_line_item;
-	}
+    /**
+     * @return \EE_Transaction
+     */
+    public function transaction()
+    {
+        return $this->transaction;
+    }
 
 
 
-	/**
-	 * @param \EE_Transaction $transaction
-	 * @param \EE_Ticket      $ticket
-	 * @return \EE_Line_Item
-	 * @throws EntityNotFoundException
-	 * @throws \EE_Error
-	 */
-	protected static function getTicketLineItem( \EE_Transaction $transaction, \EE_Ticket $ticket )
-	{
-		$line_item = null;
-		$ticket_line_items = \EEH_Line_Item::get_line_items_by_object_type_and_IDs(
-			$transaction->total_line_item(),
-			'Ticket',
-			array( $ticket->ID() )
-		);
-		foreach ( $ticket_line_items as $ticket_line_item ) {
-			if (
-				$ticket_line_item instanceof \EE_Line_Item
-				&& $ticket_line_item->OBJ_type() === 'Ticket'
-				&& $ticket_line_item->OBJ_ID() === $ticket->ID()
-			) {
-				$line_item = $ticket_line_item;
-				break;
-			}
-		}
-		if ( ! ( $line_item instanceof \EE_Line_Item && $line_item->OBJ_type() === 'Ticket' ) ) {
-			throw new EntityNotFoundException( 'Line Item Ticket ID', $ticket->ID() );
-		}
-		return $line_item;
-	}
+    /**
+     * @return \EE_Ticket
+     */
+    public function ticket()
+    {
+        return $this->ticket;
+    }
+
+
+
+    /**
+     * @return \EE_Line_Item
+     */
+    public function ticketLineItem()
+    {
+        return $this->ticket_line_item;
+    }
+
+
+
+    /**
+     * @return int
+     */
+    public function quantity()
+    {
+        return $this->quantity;
+    }
 
 
 }
