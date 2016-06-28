7e2656b54bddac3b3b900aca427751d99c82d0f3
Brent Christensen <brent@eventespresso.com>
2016-06-28 09:49:54 -0700

    update phpdoc comments; [touch:9795]

--- core/interfaces/ResettableInterface.php
+++ core/interfaces/ResettableInterface.php
@@ -9,10 +9,10 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 
 /**
  * Interface ResettableInterface
-
  * denotes that any class implementing this interface
  * should have its reset() method called when switching between blogs, unit tests, etc
- * because the class utilizes the "singleton" pattern and contains a cached instance of itself.
+ * because the class utilizes the "singleton" pattern and contains a cached instance of itself,
+ * or possesses other static data that requires resetting
  *
  * @package EventEspresso\core\interfaces
  * @since   4.9.2
