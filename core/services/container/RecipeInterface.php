6d82f86a7479b6049c9f24852ce7ceb54caeeebb
Brent Christensen <brent@eventespresso.com>
2016-07-13 17:02:36 -0700

    add new methods and rearrange method order to match Recipe class; [touch:7992]

--- core/services/container/RecipeInterface.php
+++ core/services/container/RecipeInterface.php
@@ -22,43 +22,36 @@ interface RecipeInterface
      */
     public function identifier();
 
-
-
     /**
      * @return string
      */
     public function fqcn();
 
-
-
     /**
      * @return array
      */
-    public function filters();
-
-
+    public function ingredients();
 
     /**
      * @return string
      */
     public function type();
 
-
+    /**
+     * @return array
+     */
+    public function filters();
 
     /**
      * @return array
      */
     public function paths();
 
-
-
     /**
      * @param  string $identifier Identifier for the entity class that the Recipe applies to
      *                            Typically a Fully Qualified Class Name
      */
     public function setIdentifier($identifier);
-
-
 
     /**
      * Ensures incoming string is a valid Fully Qualified Class Name,
@@ -73,7 +66,17 @@ interface RecipeInterface
      */
     public function setFqcn($fqcn);
 
+    /**
+     * @param array $ingredients    an array of dependencies where keys are the aliases and values are the FQCNs
+     *                              example:
+     *                              array( 'ClassInterface' => 'Fully\Qualified\ClassName' )
+     */
+    public function setIngredients(array $ingredients);
 
+    /**
+     * @param string $type one of the class constants returned from CoffeeMaker::getTypes()
+     */
+    public function setType($type = CoffeeMaker::BREW_NEW);
 
     /**
      * @param array $filters    an array of filters where keys are the aliases and values are the FQCNs
@@ -81,15 +84,6 @@ interface RecipeInterface
      *                          array( 'ClassInterface' => 'Fully\Qualified\ClassName' )
      */
     public function setFilters(array $filters);
-
-
-
-    /**
-     * @param string $type one of the class constants returned from CoffeeMaker::getTypes()
-     */
-    public function setType($type = CoffeeMaker::BREW_NEW);
-
-
 
     /**
      * Ensures incoming paths is a valid filepath, or array of valid filepaths,
