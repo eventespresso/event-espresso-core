b9fb3329581cd263e3a1c23a2b929add359a45c7
Brent Christensen <brent@eventespresso.com>
2016-07-18 13:08:32 -0700

    remove michaelsTest() method; [touch:9783]

--- core/services/container/OpenCoffeeShop.php
+++ core/services/container/OpenCoffeeShop.php
@@ -1,10 +1,6 @@
 <?php
 namespace EventEspresso\core\services\container;
 
-use EventEspresso\tests\mocks\core\services\container\Coffee;
-use EventEspresso\tests\mocks\core\services\container\HonduranBean;
-use EventEspresso\tests\mocks\core\services\container\KenyanBean;
-
 if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 	exit( 'No direct script access allowed' );
 }
@@ -36,7 +32,8 @@ class OpenCoffeeShop {
 	/**
 	 * OpenCoffeeShop constructor.
 	 */
-	public function __construct() {
+	public function __construct()
+    {
 		// instantiate the container
 		$this->CoffeeShop = new CoffeeShop();
 		// create a dependency injector class for resolving class constructor arguments
@@ -178,88 +175,6 @@ class OpenCoffeeShop {
 	}
 
 
-
-    public function michaelsTest()
-    {
-        \EEH_Debug_Tools::printr(__FUNCTION__, __CLASS__, __FILE__, __LINE__, 2);
-        echo '<pre style="margin-left:180px;">';
-        // HonduranCoffee
-        echo '<h4>Add Recipe for using HonduranBean in place of BeanInterface</h4>';
-        // have one recipe for HonduranCoffee
-        $this->CoffeeShop->addRecipe(
-            new Recipe(
-                'HonduranBean',
-                'EventEspresso\tests\mocks\core\services\container\HonduranBean',
-                array(),
-                CoffeeMaker::BREW_SHARED,
-                array('EventEspresso\tests\mocks\core\services\container\BeanInterface')
-            )
-        );
-        // HonduranCoffee
-        echo '<h4>Add Recipe for HonduranCoffee</h4>';
-        $this->CoffeeShop->addRecipe(
-            new Recipe(
-                'HonduranCoffee',
-                'EventEspresso\tests\mocks\core\services\container\Coffee'
-            )
-        );
-        /** @var Coffee $HonduranCoffee */
-        $HonduranCoffee = $this->CoffeeShop->brew('HonduranCoffee');
-        // test it
-        echo 'brew HonduranCoffee directly (should be instance of Coffee): ';
-        var_dump($HonduranCoffee instanceof Coffee);
-        var_dump($HonduranCoffee);
-        // test bean type
-        echo '<br/>test that bean type is an instance of HonduranBean: ';
-        var_dump($HonduranCoffee->getBeans() instanceof HonduranBean);
-        var_dump($HonduranCoffee->getBeans());
-        // HonduranCoffee
-        echo '<br/><h4>Add Recipe for KenyanCoffee</h4>';
-        // and another recipe for KenyanCoffee
-        $this->CoffeeShop->addRecipe(
-            new Recipe(
-                'KenyanCoffee',
-                'EventEspresso\tests\mocks\core\services\container\Coffee',
-                array(), // NO ingredients
-                CoffeeMaker::BREW_NEW
-            )
-        );
-        /** @var Coffee $HonduranCoffee */
-        $KenyanCoffee = $this->CoffeeShop->brew('KenyanCoffee');
-        // test it
-        echo 'brew KenyanCoffee directly (should be instance of Coffee): ';
-        var_dump($KenyanCoffee instanceof Coffee);
-        var_dump($KenyanCoffee);
-        // test bean type
-        echo '<br/>test that bean type is an instance of HonduranBean: ';
-        var_dump($KenyanCoffee->getBeans() instanceof HonduranBean);
-        var_dump($KenyanCoffee->getBeans());
-        echo '<br/><h4>BUT... It can\'t be KenyanCoffee if it\'s using HonduranBean</h4>';
-        echo 'Remove Recipe for KenyanCoffee<br/>';
-        $this->CoffeeShop->removeRecipe('KenyanCoffee');
-        echo 'and the Closure used for generating instances of KenyanCoffee<br/><br/>';
-        $this->CoffeeShop->removeClosure('KenyanCoffee');
-        echo '<h4>Now Add NEW Recipe for KenyanCoffee that specifies KenyanBean</h4>';
-        // and another recipe for KenyanCoffee
-        $this->CoffeeShop->addRecipe(
-            new Recipe(
-                'KenyanCoffee',
-                'EventEspresso\tests\mocks\core\services\container\Coffee',
-                array('EventEspresso\tests\mocks\core\services\container\BeanInterface' => 'EventEspresso\tests\mocks\core\services\container\KenyanBean'),
-                CoffeeMaker::BREW_NEW
-            )
-        );
-        // test it
-        echo 'brew another KenyanCoffee directly: ';
-        $KenyanCoffee = $this->CoffeeShop->brew('KenyanCoffee');
-        var_dump($KenyanCoffee instanceof Coffee);
-        var_dump($KenyanCoffee);
-        // test bean type
-        echo '<br/>and test that bean type is NOW an instance of KenyanBean: ';
-        var_dump($KenyanCoffee->getBeans() instanceof KenyanBean);
-        var_dump($KenyanCoffee->getBeans());
-        echo '</pre>';
-    }
 
 
 }
