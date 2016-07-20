d943b6c6d451de8ad2bd42831d023f24689c0e86
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2016-07-20 15:25:31 -0400

    Add initial intro template files. [touch:6935]

--- payment_methods/Paypal_Standard/templates/paypal_standard_intro.template.php
+++ payment_methods/Paypal_Standard/templates/paypal_standard_intro.template.php
@@ -5,5 +5,4 @@ $closing_tag = '</a>';
 if( apply_filters( 'FHEE__ee_show_affiliate_links', true ) ) {
 	$closing_tag .= __( ' (affiliate link)', 'event_espresso' );
 }
-printf( __('PayPal Standard is an off-site payment method and is available to event organizers in many countries. A PayPal premier or business account is needed to accept payments. Need a PayPal account? Call 1-855-456-1338 or %1$sclick here to sign up for a merchant account%2$s.', 'event_espresso'), '<a href="https://eventespresso.com/go/paypalstandard/" target="_blank">', $closing_tag );
-
+printf( __('PayPal Standard (PayPal Payments Standard) is an off-site payment method and is available to event organizers in many countries. A PayPal premier or business account is needed to accept payments. Need a PayPal account? Call 1-855-456-1338 or %1$sclick here to sign up for a merchant account%2$s.', 'event_espresso'), '<a href="https://eventespresso.com/go/paypalstandard/" target="_blank">', $closing_tag );
