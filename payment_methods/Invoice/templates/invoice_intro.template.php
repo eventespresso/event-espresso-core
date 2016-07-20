4f404cdcb714fb19ccebeec240b8e55174ac9550
Mike Nelson <michael@eventespresso.com>
2016-07-20 14:36:52 -0700

    [touch:6935]{t:15}echo out the translations for the offline payment methods intro areas

--- payment_methods/Invoice/templates/invoice_intro.template.php
+++ payment_methods/Invoice/templates/invoice_intro.template.php
@@ -1,1 +1,1 @@
-<?php esc_html__('Invoice is an offline payment method for accepting payments. Payments are processed manually by providing your registrants/attendees with information on how to pay their invoice.', 'event_espresso'); ?>
+<?php echo esc_html__('Invoice is an offline payment method for accepting payments. Payments are processed manually by providing your registrants/attendees with information on how to pay their invoice.', 'event_espresso'); ?>
