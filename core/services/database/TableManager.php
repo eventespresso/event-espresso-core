76877a4aa171bcedafb6bf36382b77df120cd08c
Brent Christensen <brent@eventespresso.com>
2016-08-02 10:33:51 -0700

    fix phpdocs and classnames (althoguh those should change);[touch:9932]

--- core/services/database/TableManager.php
+++ core/services/database/TableManager.php
@@ -1,30 +1,35 @@
 <?php
 namespace EventEspresso\core\services\database;
+
+defined('EVENT_ESPRESSO_VERSION')|| exit('No direct script access allowed');
 /**
  *
  * Class TableManager
  *
- * For performing mysql databse table schema manipualtion
+ * For performing mysql database table schema manipulation
  *
  * @package         Event Espresso
- * @subpackage    
+ * @subpackage
  * @author				Mike Nelson
  * @since		 	   $VID:$
  *
  */
-if( !defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
-}
-
 class TableManager extends \EE_Base {
-	public function addColumn( $tableName, $columnName, $columnInfo='INT UNSIGNED NOT NULL' )
+
+    /**
+     * @param string $tableName
+     * @param string $columnName
+     * @param string $columnInfo
+     * @return bool|false|int
+     */
+    public function addColumn( $tableName, $columnName, $columnInfo='INT UNSIGNED NOT NULL' )
 	{
 		if( apply_filters( 'FHEE__EEH_Activation__add_column_if_it_doesnt_exist__short_circuit', FALSE ) ){
 			return FALSE;
 		}
 		global $wpdb;
-		$full_table_name= EEH_Activation::ensure_table_name_has_prefix( $tableName );
-		$fields = self::get_fields_on_table($tableName);
+		$full_table_name= \EEH_Activation::ensure_table_name_has_prefix( $tableName );
+		$fields = \EEH_Activation::get_fields_on_table($tableName);
 		if (!in_array($columnName, $fields)){
 			$alter_query="ALTER TABLE $full_table_name ADD $columnName $columnInfo";
 			//echo "alter query:$alter_query";
