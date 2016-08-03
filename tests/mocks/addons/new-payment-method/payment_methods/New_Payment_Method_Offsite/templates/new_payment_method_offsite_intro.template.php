93fe08d4a6ee7b0c6e4b50702ede4ca88aef64e1
EE DevBox Server <admin@eventespresso.com>
2016-08-03 08:30:21 -0400

    Bumping version to 4.9.7.p and prepped for release

--- tests/mocks/addons/new-payment-method/payment_methods/New_Payment_Method_Offsite/templates/new_payment_method_offsite_intro.template.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/* 
- * Note! This file's name is critical to having EE_PMT_New_Payment_Method::introductory_html() find it and use it in the right spot.
- * This will appear before the payment method is activated, and should describe it
- */
-_e( 'This is a payment method where users enter their payment information on a different site. (In this case, they actually enter it into a simple PHP file located on the same server)', 'event_espresso' );
