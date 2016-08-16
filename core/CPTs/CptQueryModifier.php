7d1d312880d99abe25022852349073443abc7cfb
Darren Ethier <darren@roughsmootheng.in>
2016-08-16 15:54:15 -0400

    instead of using `show_ui` add a custom cpt argument for usage [touch: 9990]

--- core/CPTs/CptQueryModifier.php
+++ core/CPTs/CptQueryModifier.php
@@ -493,15 +493,14 @@ class CptQueryModifier {
 	public function getEditPostLink( $url, $ID ) {
 		// need to make sure we only edit links if our cpt
 		global $post;
-		$cpt_details = $this->cptDetails();
-		//notice if the cpt is registered with `show_ui` set to true, we take that to mean that the WordPress core ui
+		//notice if the cpt is registered with `show_ee_ui` set to false, we take that to mean that the WordPress core ui
 		//for interacting with the CPT is desired and there is no EE UI for interacting with the CPT in the admin.
 		if (
 			! $post instanceof \WP_Post
 			|| $post->post_type !== $this->post_type
 			|| (
-				isset( $cpt_details['args']['show_ui'] )
-				&& $cpt_details['args']['show_ui']
+				isset( $this->cpt_details['args']['show_ee_ui'] )
+				&& ! $this->cpt_details['args']['show_ee_ui']
 			)
 		) {
 			return $url;
