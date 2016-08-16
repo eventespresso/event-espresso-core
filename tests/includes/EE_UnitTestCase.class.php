076a9b0e43d44db5d6b18b448f892b9c7a46877e
Brent Christensen <brent@eventespresso.com>
2016-08-16 15:06:29 -0700

    only reset the registray AFTER each test is run... no need to do it before; [touch:5580]

--- tests/includes/EE_UnitTestCase.class.php
+++ tests/includes/EE_UnitTestCase.class.php
@@ -76,7 +76,6 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		);
 		$this->_orig_current_user = $current_user instanceof WP_User ? clone $current_user : new WP_User(1);
 		parent::setUp();
-		EE_Registry::reset( TRUE );
 		$auto_made_thing_seed = 1;
 		//reset wpdb's list of queries executed so it only stores those from the current test
 		$wpdb->queries = array();
@@ -98,7 +97,6 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		// load scenarios
 		require_once EE_TESTS_DIR . 'includes/scenarios/EE_Test_Scenario_Classes.php';
 		$this->scenarios = new EE_Test_Scenario_Factory( $this );
-		EE_Registry::reset();
 
 		//IF we detect we're running tests on WP4.1, then we need to make sure current_user_can tests pass by implementing
 		//updating all_caps when `WP_User::add_cap` is run (which is fixed in later wp versions).  So we hook into the
@@ -133,6 +131,7 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 
 	public function tearDown(){
 		parent::tearDown();
+		EE_Registry::reset( true );
 		global $wp_filter, $wp_actions, $merged_filters, $wp_current_filter, $current_user;
 		$wp_filter = $this->wp_filters_saved[ 'wp_filter' ];
 		$wp_actions = $this->wp_filters_saved[ 'wp_actions' ];
@@ -145,6 +144,7 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		if( ! empty( $notices[ 'errors' ] ) ){
 			$this->fail(  $notices['errors'] );
 		}
+
 	}
 
 	/**
