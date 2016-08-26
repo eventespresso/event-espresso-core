2142d01f5e09523e0030e7c8bf788065f0c03532
Brent Christensen <brent@eventespresso.com>
2016-08-25 17:22:42 -0700

    just reset the registry ONCE; [touch:10019]

--- tests/includes/EE_UnitTestCase.class.php
+++ tests/includes/EE_UnitTestCase.class.php
@@ -85,7 +85,6 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		);
 		$this->_orig_current_user = $current_user instanceof WP_User ? clone $current_user : new WP_User(1);
 		parent::setUp();
-		EE_Registry::reset( TRUE );
 		$auto_made_thing_seed = 1;
 		//reset wpdb's list of queries executed so it only stores those from the current test
 		$wpdb->queries = array();
@@ -106,7 +105,6 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 		// load scenarios
 		require_once EE_TESTS_DIR . 'includes/scenarios/EE_Test_Scenario_Classes.php';
 		$this->scenarios = new EE_Test_Scenario_Factory( $this );
-		EE_Registry::reset();
 
 		//IF we detect we're running tests on WP4.1, then we need to make sure current_user_can tests pass by implementing
 		//updating all_caps when `WP_User::add_cap` is run (which is fixed in later wp versions).  So we hook into the
@@ -119,6 +117,7 @@ class EE_UnitTestCase extends WP_UnitTestCase {
 				return $WP_User->allcaps;
 			}, 10, 4 );
 		}
+		EE_Registry::reset( true );
 	}
 
 
