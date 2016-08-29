0d128af4ba8bdaa429eacbd366f42f3795ae7f90
Brent Christensen <brent@eventespresso.com>
2016-08-29 13:56:37 -0700

    set $_model_query_blog_id when referencing entity map (although not sure this entire line of cod eis needed); [touch:8698]

--- core/db_models/EEM_Base.model.php
+++ core/db_models/EEM_Base.model.php
@@ -1919,15 +1919,15 @@ abstract class EEM_Base extends EE_Base{
 				throw new EE_Error( sprintf( __( 'WPDB Error occurred, but no error message was logged by wpdb! The wpdb method called was "%1$s" and the arguments were "%2$s"', 'event_espresso' ), $wpdb_method, var_export( $arguments_to_provide, true ) ) );
 			}
 		}elseif( $result === false ) {
-			EE_Error::add_error( 
-				sprintf( 
+			EE_Error::add_error(
+				sprintf(
 					__( 'A database error has occurred. Turn on WP_DEBUG for more information.||A database error occurred doing wpdb method "%1$s", with arguments "%2$s". The error was "%3$s"', 'event_espresso' ),
 					$wpdb_method,
 					var_export( $arguments_to_provide, true ),
 					$wpdb->last_error
-				), 
-				__FILE__, 
-				__FUNCTION__, 
+				),
+				__FILE__,
+				__FUNCTION__,
 				__LINE__
 			);
 		}
@@ -4318,7 +4318,7 @@ abstract class EEM_Base extends EE_Base{
 					$obj_in_map->clear_cache($relation_name, NULL, TRUE );
 				}
 			}
-			$this->_entity_map[ $id ] = $obj_in_map;
+			$this->_entity_map[ EEM_Base::$_model_query_blog_id ][ $id ] = $obj_in_map;
 			return $obj_in_map;
 		}else{
 			return $this->get_one_by_ID( $id );
@@ -4581,7 +4581,7 @@ abstract class EEM_Base extends EE_Base{
 		}
 		return array( $this->primary_key_name() => $this->get_primary_key_field());
 	}
-	
+
 
 
 
