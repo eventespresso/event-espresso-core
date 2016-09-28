2d8bc4c8fb2e28805d488a89f1c2d6dc22938080
Brent Christensen <brent@eventespresso.com>
2016-09-28 12:15:05 -0700

    remove all static BS and convert all references to object; add embedButton() method to reduce duplication; extract logic from addEventListIframeEmbedButton() and move into parent class; [touch:9576]

--- core/libraries/iframe_display/EventListIframeEmbedButton.php
+++ core/libraries/iframe_display/EventListIframeEmbedButton.php
@@ -17,14 +17,26 @@ class EventListIframeEmbedButton extends IframeEmbedButton
 {
 
 
+    /**
+     * @return string
+     */
+    public function embedButton()
+    {
+        return $this->embedButtonHtml(
+            esc_html__('Event List', 'event_espresso'),
+            esc_html__('event_list', 'event_espresso')
+        );
+    }
+
+
 
     /**
      * Adds an iframe embed code button to the Event editor.
      */
-    public static function addEventEditorIframeEmbedButton()
+    public function addEventEditorIframeEmbedButton()
     {
         // add button for iframe code to event editor.
-        IframeEmbedButton::addEventEditorIframeEmbedButtonFilter(
+        $this->addEventEditorIframeEmbedButtonFilter(
             'event_list',
             esc_html__('Event', 'event_espresso'),
             esc_html__('event', 'event_espresso')
@@ -38,31 +50,10 @@ class EventListIframeEmbedButton extends IframeEmbedButton
      * Adds an iframe embed code button to the Event editor.
      * return string
      */
-    public static function addEventListIframeEmbedButton()
+    public function addEventListIframeEmbedButtonSection()
     {
-        // add button for iframe code to event editor.
-        $html = '';
-        //make sure this is ONLY when editing and the event id has been set.
-        if (\EE_Registry::instance()->REQ->get('page') === 'espresso_events') {
-            $html .= \EEH_HTML::h3(esc_html__('iFrame Embed Code', 'event_espresso'));
-            $html .= \EEH_HTML::p(
-                esc_html__(
-                    'Click the following button(s) to generate iframe HTML that will allow you to embed your event content within the content of other websites.',
-                    'event_espresso'
-                )
-            );
-            $html .= ' &nbsp; ' . IframeEmbedButton::embedButtonHtml(
-                    esc_html__('Event List', 'event_espresso'),
-                    esc_html__('event_list', 'event_espresso')
-                ) . ' ';
-            \EE_Registry::$i18n_js_strings['iframe_embed_title'] = esc_html__(
-                'copy and paste the following into any other site\'s content to display this event:',
-                'event_espresso'
-            );
-        }
-        return apply_filters(
-            'FHEE__Iframe__addEventListIframeEmbedButton__html',
-            $html
+        return $this->addIframeEmbedButtonsSection(
+            array( 'event_list' => $this->embedButton() )
         );
     }
 
