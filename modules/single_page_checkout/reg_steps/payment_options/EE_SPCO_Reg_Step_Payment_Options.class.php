656abf0dfb2632e34ad388c72512115f51df24cf
Brent Christensen <brent@eventespresso.com>
2016-09-30 16:32:48 -0700

    add filter switch for controlling whether to populate billing form from attendee data; [touch:10119] (reverted from commit 1d3c9003c6429a0e81003fc3d0676d70585e4cbb)

--- modules/single_page_checkout/reg_steps/payment_options/EE_SPCO_Reg_Step_Payment_Options.class.php
+++ modules/single_page_checkout/reg_steps/payment_options/EE_SPCO_Reg_Step_Payment_Options.class.php
@@ -1144,10 +1144,6 @@ class EE_SPCO_Reg_Step_Payment_Options extends EE_SPCO_Reg_Step {
 		if (
 			$payment_method_billing_form instanceof EE_Billing_Attendee_Info_Form
 		    && $this->checkout->transaction_has_primary_registrant()
-            && apply_filters(
-                'FHEE__EE_SPCO_Reg_Step_Payment_Options__get_billing_form_html_for_payment_method__populate_billing_form_from_attendee',
-                true
-            )
 		) {
 			$payment_method_billing_form->populate_from_attendee(
 				$this->checkout->transaction->primary_registration()->attendee()
