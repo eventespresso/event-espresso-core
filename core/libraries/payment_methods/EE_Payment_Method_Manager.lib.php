9c82072a0c8d2c604c22e7a5c526c87c0dd38837
Mike Nelson <michael@eventespresso.com>
2016-10-10 11:10:14 -0700

    [touch:10110]{t:20}use a persistent admin notice, in case invoice gets activated and the next request is somehow from the frontend

--- core/libraries/payment_methods/EE_Payment_Method_Manager.lib.php
+++ core/libraries/payment_methods/EE_Payment_Method_Manager.lib.php
@@ -244,12 +244,14 @@ class EE_Payment_Method_Manager {
 			$message_resource_manager = EE_Registry::instance()->load_lib( 'Message_Resource_Manager' );
 			$message_resource_manager->ensure_message_type_is_active( 'invoice', 'html' );
 			$message_resource_manager->ensure_messenger_is_active( 'pdf' );
-			EE_Error::add_attention(
+			EE_Error::add_persistent_admin_notice(
+				'invoice_pm_requirements_notice',
 				sprintf(
 					__( 'Note, when the invoice payment method is activated, the invoice message type, html messenger, and pdf messenger are activated as well for the %1$smessages system%2$s.', 'event_espresso' ),
 					'<a href="' . admin_url( 'admin.php?page=espresso_messages') . '">',
 					'</a>'
-				)
+				),
+				true
 			);
 		}
 		return $payment_method;
