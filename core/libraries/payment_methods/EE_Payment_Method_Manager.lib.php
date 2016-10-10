0b92fc634a82275393b6fa8fd8d4ff2f0c1f2c36
Mike Nelson <michael@eventespresso.com>
2016-10-10 11:14:50 -0700

    [touch:10110]{t:10}change wording of message to give a bit more info about what happened when invoice payment method gets activated

--- core/libraries/payment_methods/EE_Payment_Method_Manager.lib.php
+++ core/libraries/payment_methods/EE_Payment_Method_Manager.lib.php
@@ -247,7 +247,7 @@ class EE_Payment_Method_Manager {
 			EE_Error::add_persistent_admin_notice(
 				'invoice_pm_requirements_notice',
 				sprintf(
-					__( 'Note, when the invoice payment method is activated, the invoice message type, html messenger, and pdf messenger are activated as well for the %1$smessages system%2$s.', 'event_espresso' ),
+					__( 'The Invoice payment method has been activated. It requires the invoice message type, html messenger, and pdf messenger be activated as well for the %1$smessages system%2$s, so it has been automatically verified that they are also active.', 'event_espresso' ),
 					'<a href="' . admin_url( 'admin.php?page=espresso_messages') . '">',
 					'</a>'
 				),
