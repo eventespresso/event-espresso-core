561d99cd362ddbf900c9b0179a078c920b219eb7
Brent Christensen <brent@eventespresso.com>
2016-10-14 21:07:23 -0700

    don't serialize any forms when sleeping; [touch:10177]

--- modules/single_page_checkout/inc/EE_Checkout.class.php
+++ modules/single_page_checkout/inc/EE_Checkout.class.php
@@ -1184,6 +1184,21 @@ class EE_Checkout {
 
 
 
+    /**
+     *    __sleep
+     * to conserve db space, let's remove the reg_form and the EE_Checkout object from EE_SPCO_Reg_Step objects upon serialization
+     * EE_Checkout will handle the reimplementation of itself upon waking,
+     * but we won't bother with the reg form, because if needed, it will be regenerated anyways
+     *
+     * @return array
+     */
+    public function __sleep()
+    {
+        // remove the reg form and the checkout
+        return array_diff( array_keys( get_object_vars( $this ) ), array( 'billing_form', 'registration_form' ) );
+    }
+
+
 	/**
 	 * 	__wakeup
 	 * to conserve db space, we are removing the EE_Checkout object from EE_SPCO_Reg_Step objects upon serialization
