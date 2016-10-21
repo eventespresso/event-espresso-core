8efa3f6b4176b2a48e9f5246e7bda1a77756bd93
Brent Christensen <brent@eventespresso.com>
2016-10-21 14:15:56 -0700

    JS for toggling ticket options based on datetime selection; [touch:8385]

--- modules/ticket_selector/assets/ticket_selector.js
+++ modules/ticket_selector/assets/ticket_selector.js
@@ -1,13 +1,18 @@
-(function($) {
-	
-	// close a div
-//	$('.close-this-lnk').click(function() {
-//		$(this).parent().slideUp('fast'); 
-//	});	
-
-
-	//$('.fade-away').delay(8000).fadeOut();
-
-
-	
-})(jQuery);
+jQuery(document).ready(function ($) {
+    // collection of datetime selectors
+    var $datetime_selectors = $('.ticket-selector-datetime-selector-slct');
+    // reset all to default option
+    $.each( $datetime_selectors, function () {
+        $(this).val(0);
+    });
+    // update ticket selector if datetime is chosen
+    $datetime_selectors.on('change', function () {
+        var ticket_selector_id = '#' + $(this).data('tkt_slctr_tbl');
+        var $ticket_selector = $(ticket_selector_id);
+        if ( $ticket_selector.length ) {
+            var selected_datetime = $(this).val();
+            $ticket_selector.find('.tckt-slctr-tbl-tr').hide();
+            $ticket_selector.find('.ee-ticket-datetimes-' + selected_datetime).show();
+        }
+    });
+});
