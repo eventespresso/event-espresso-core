718d792806b198046cf350b2d515f475220730f0
Brent Christensen <brent@eventespresso.com>
2016-10-21 09:33:36 -0700

    class for loading template and resolving template args for the ticket row within a "DWMTS" ticket selector; [touch:8385]

---
+++ modules/ticket_selector/TicketSelectorRowDwmts.php
@@ -0,0 +1,23 @@
+<?php
+namespace EventEspresso\modules\ticket_selector;
+
+defined('ABSPATH') || exit;
+
+
+
+/**
+ * Class TicketSelectorRowDwmts
+ * class for loading template and resolving template args for the ticket row within a "DWMTS" ticket selector
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class TicketSelectorRowDwmts extends TicketSelectorRow
+{
+
+
+
+}
+// End of file TicketSelectorRowDwmts.php
+// Location: EventEspresso\modules\ticket_selector/TicketSelectorRowDwmts.php
