6494d02eb27d126e1dda8d20c84a138f1790c2fa
Brent Christensen <brent@eventespresso.com>
2016-10-25 16:21:42 -0700

    don't allow submit if datetime is not chosen; display notice regarding picking a datetime; [touch:8385]

--- modules/ticket_selector/assets/ticket_selector.js
+++ modules/ticket_selector/assets/ticket_selector.js
@@ -4,15 +4,31 @@ jQuery(document).ready(function ($) {
     // reset all to default option
     $.each( $datetime_selectors, function () {
         $(this).val(0);
-    });
+		var event_id = $( this ).data( 'tkt_slctr_evt' );
+		var submit_button_id = '#ticket-selector-submit-' + event_id + '-btn';
+		$(submit_button_id).addClass( 'ee-disabled-btn' );
+	});
     // update ticket selector if datetime is chosen
     $datetime_selectors.on('change', function () {
-        var ticket_selector_id = '#' + $(this).data('tkt_slctr_tbl');
+        var event_id = $(this).data('tkt_slctr_evt');
+        var ticket_selector_id = '#tkt-slctr-tbl-' + event_id;
+        var submit_button_id = '#ticket-selector-submit-' + event_id + '-btn';
         var $ticket_selector = $(ticket_selector_id);
         if ( $ticket_selector.length ) {
             var selected_datetime = $(this).val();
             $ticket_selector.find('.tckt-slctr-tbl-tr').hide();
             $ticket_selector.find('.ee-ticket-datetimes-' + selected_datetime).show();
-        }
+			$(submit_button_id).removeClass( 'ee-disabled-btn' );
+			$('.ticket-selector-disabled-submit-btn-msg').stop().hide();
+		}
     });
+
+	$('.ticket-selector-submit-btn').on('click', function (e) {
+		if( $( this ).hasClass( 'ee-disabled-btn' ) ) {
+			e.preventDefault();
+			e.stopPropagation();
+			$(this).prev('.ticket-selector-disabled-submit-btn-msg').stop().fadeIn(100).delay(6000).fadeOut();
+		}
+	});
+
 });
