a5e5dc4f411917f5aeb7959f4fd6fb08f028f3a0
Brent Christensen <brent@eventespresso.com>
2016-10-25 12:33:46 -0700

    rename class and file; [touch:8385]

--- modules/ticket_selector/TicketSelectorRowSimple.php
+++ modules/ticket_selector/TicketSelectorRowSimple.php
@@ -1,23 +1,23 @@
 <?php
 namespace EventEspresso\modules\ticket_selector;
 
-defined('ABSPATH') || exit;
+defined('EVENT_ESPRESSO_VERSION') || exit;
 
 
 
 /**
- * Class TicketSelectorRowDwmts
+ * Class TicketSelectorRowSimple
  * class for loading template and resolving template args for the ticket row within a "DWMTS" ticket selector
  *
  * @package       Event Espresso
  * @author        Brent Christensen
  * @since         $VID:$
  */
-class TicketSelectorRowDwmts extends TicketSelectorRow
+class TicketSelectorRowSimple extends TicketSelectorRow
 {
 
 
 
 }
-// End of file TicketSelectorRowDwmts.php
-// Location: EventEspresso\modules\ticket_selector/TicketSelectorRowDwmts.php
+// End of file TicketSelectorRowSimple.php
+// Location: EventEspresso\modules\ticket_selector/TicketSelectorRowSimple.php
