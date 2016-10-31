cf2936dc958fd1175c3029edf615cf9e59611051
Brent Christensen <brent@eventespresso.com>
2016-10-31 11:16:57 -0700

    remove unnecessary var; add braces and remove unnecessary return; add new listeners for controlling question group settings; [touch:9050]

--- caffeinated/admin/extend/events/assets/event-editor-heartbeat.js
+++ caffeinated/admin/extend/events/assets/event-editor-heartbeat.js
@@ -6,20 +6,41 @@ jQuery(document).ready(function($) {
 	//hooking into the heartbeat send.
 	$(document).on('heartbeat-send', function(e, data) {
 		//get a count of all inputs on the page.
-		var input_count = $('input').length + $('select').length + $('textarea').length;
-		data['input_count'] = input_count;
+		data['input_count'] = $('input').length + $('select').length + $('textarea').length;
 		data['ee_admin_ajax'] = true;
 	});
 
 	//Listen for the custom event "heartbeat-tick" on $(document).
 	$(document).on( 'heartbeat-tick', function(e, data) {
-		if ( ! data['max_input_vars_check'] )
-				return;
-
+		if ( ! data['max_input_vars_check'] ) {
+            return;
+        }
 		//if no space left then let's show a dialog with the message.
 		if ( ! data['max_input_vars_check'] !== '' ) {
 			dialogHelper.displayModal().addContent( data['max_input_vars_check'] );
 		}
-		return;
 	});
+    // if the Address Information question group value changes
+    $('[name="add_attendee_question_groups[2]"]').on('change', function () {
+        // check if it's checked, and if so
+        if ($(this).prop('checked')) {
+            // then check off the Personal Information question group as well
+            $('[name="add_attendee_question_groups[1]"]').prop('checked', true);
+            // and display a notice
+            $('#question-group-requirements-notice-pg').stop().slideDown(100);
+        } else {
+            // otherwise hide the notice
+            $('#question-group-requirements-notice-pg').stop().slideUp(100);
+        }
+    });
+    // if the Personal Information question group is clicked
+    $('[name="add_attendee_question_groups[1]"]').on('click', function (e) {
+        // check if the Address Information question group is checked
+        if ($('[name="add_attendee_question_groups[2]"]').prop('checked')) {
+            // and don't let this input be unchecked
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    });
+
 });
