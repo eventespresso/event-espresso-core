af9a5db8486481140a49386fd3a47dd56b1eb10d
Mike Nelson <michael@eventespresso.com>
2016-11-07 14:51:08 -0800

    [touch:8888]{t:20}use esc_html__ and esc_html_e instead of __ and _e in migration templates and core DMSs

--- core/data_migration_scripts/EE_DMS_Core_4_8_0.dms.php
+++ core/data_migration_scripts/EE_DMS_Core_4_8_0.dms.php
@@ -46,7 +46,7 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base
      */
     public function __construct(TableManager $table_manager = null, TableAnalysis $table_analysis = null)
     {
-        $this->_pretty_name = __("Data Update to Event Espresso 4.8.0", "event_espresso");
+        $this->_pretty_name = esc_html__("Data Update to Event Espresso 4.8.0", "event_espresso");
         $this->_priority = 10;
         $this->_migration_stages = array(
             new EE_DMS_4_8_0_pretax_totals(),
@@ -80,16 +80,6 @@ class EE_DMS_Core_4_8_0 extends EE_Data_Migration_Script_Base
 //			echo "$version_string doesnt apply";
             return false;
         }
-    }
-
-
-
-    /**
-     * @return string|void
-     */
-    public function pretty_name()
-    {
-        return __("Core Data Migration to version 4.8.0", "event_espresso");
     }
 
 
