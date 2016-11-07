577af4c0484371c5b4af5d267040b068bf45c0a2
Brent Christensen <brent@eventespresso.com>
2016-11-07 11:59:36 -0800

    extract some logic into setup() and teardown( )methods; refactor test_is_reg_page() to loop through standard permalink structures; [touch:10220]

--- tests/testcases/modules/EED_Single_Page_Checkout_Test.php
+++ tests/testcases/modules/EED_Single_Page_Checkout_Test.php
@@ -1,69 +1,98 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) {
-    exit('No direct script access allowed');
+<?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
 }
 
-class EED_Single_Page_CheckoutTest extends EE_UnitTestCase
-{
-
-    /**
-     * Used to hold the EED_Single_Page_Checkout_Mock class.
-     * @var
-     */
-    protected $spco_mock;
 
 
-    public function setUp()
-    {
-        parent::setUp();
-        $this->loadModuleMocks(array('EED_Single_Page_Checkout'));
-        $this->spco_mock = EED_Single_Page_Checkout_Mock::instance();
-    }
+class EED_Single_Page_Checkout_Test extends EE_UnitTestCase {
 
-    public function tearDown()
-    {
-        remove_all_filters('page_link');
-        parent::tearDown();
-    }
+	/**
+	 * Used to hold the EED_Single_Page_Checkout_Mock class.
+	 *
+	 * @var EED_Single_Page_Checkout_Mock $spco_mock
+	 */
+	protected $spco_mock;
 
+	/**
+	 * @var int $original_reg_page_id
+	 */
+	protected $original_reg_page_id = 0;
 
-    /**
-     * @group 10220
-     * @group spco
-     */
-    public function test_is_reg_page()
-    {
-        //add filter on permalink because that will
-        add_filter('page_link', function ($link) {
-            return add_query_arg(array('some_dummy_arg' => 'crazy'), $link);
-        });/**/
-
-        //for some reason, whatever the reg_page_id is set on EE_Config is not an actual page in the db.
-        //so we first setup a page and set it as the reg page for the purpose of this test
-        $original_reg_page_id = EE_Config::instance()->core->reg_page_id;
-        $new_page_id = $this->factory->post->create( array( 'post_type' => 'page' ) );
-        EE_Config::instance()->core->reg_page_id = $new_page_id;
+	/**
+	 * @var int $new_page_id
+	 */
+	protected $new_page_id = 0;
 
 
-        $actual_reg_page_permalink = get_permalink(EE_Config::instance()->core->reg_page_id);
 
-        //example reg_page_permalink where a plugin or theme adds query params to links clicked by users for additional functionality that is NOT
-        //generated via filtering get_permalink.
-        $current_url_from_clicking_other_source = add_query_arg(
-            array(
-                'utm'      => '5677',
-                'campaign' => 'analytics_campaign'
-            ),
-            $actual_reg_page_permalink
-        );
+	public function setUp() {
+		parent::setUp();
+		$this->loadModuleMocks( array( 'EED_Single_Page_Checkout' ) );
+		$this->spco_mock = EED_Single_Page_Checkout_Mock::instance();
+		//add filter on permalink to add some noise
+		add_filter( 'page_link', array( $this, 'add_dummy_query_args' ) );
+		// for some reason, whatever the reg_page_id is set on EE_Config is not an actual page in the db.
+		// so we first setup a page and set it as the reg page for the purpose of this test
+		$this->new_page_id = $this->factory->post->create( array( 'post_type' => 'page' ) );
+		// then swap out existing reg page id with the new one
+		$this->original_reg_page_id = EE_Config::instance()->core->reg_page_id;
+		EE_Config::instance()->core->reg_page_id = $this->new_page_id;
+	}
 
-        //goto the current url.
-        $this->go_to( $current_url_from_clicking_other_source );
 
-        //so we SHOULD be on the reg page
-        $this->assertTrue($this->spco_mock->is_reg_checkout());
-        EE_Config::instance()->core->reg_page_id = $original_reg_page_id;
-    }
+
+	public function tearDown() {
+		EE_Config::instance()->core->reg_page_id = $this->original_reg_page_id;
+		// remove filter on permalink
+		remove_filter( 'page_link', array( $this, 'add_dummy_query_args' ) );
+		parent::tearDown();
+	}
+
+
+
+	public function add_dummy_query_args( $link ) {
+		return add_query_arg( array( 'some_dummy_arg' => 'crazy' ), $link );
+	}
+
+
+
+	/**
+	 * @group 10220
+	 * @group spco
+	 */
+	public function test_is_reg_page() {
+		$permalink_structures = array(
+			// plain /?p=123
+			'plain'      => '',
+			// Day and name 2016/11/07/sample-post/
+			'day_name'   => '/%year%/%monthnum%/%day%/%postname%/',
+			// Month and name 	http://local.wordpress.dev/2016/11/sample-post/
+			'month_name' => '/%year%/%monthnum%/%postname%/',
+			// Numeric 	http://local.wordpress.dev/archives/123
+			'numeric'    => '/archives/%post_id%',
+			// Post name 	http://local.wordpress.dev/sample-post/
+			'post_name'  => '/%postname%/',
+		);
+		foreach ( $permalink_structures as $permalink_structure ) {
+			$this->set_permalink_structure( $permalink_structure );
+			// example reg_page_permalink where a plugin or theme adds query params
+			// to links clicked by users for additional functionality that is NOT
+			// generated via filtering get_permalink.
+			$this->go_to(
+				add_query_arg(
+					array(
+						'utm'      => '5677',
+						'campaign' => 'analytics_campaign',
+					),
+					get_permalink( EE_Config::instance()->core->reg_page_id )
+				)
+			);
+			//so we SHOULD be on the reg page
+			$this->assertTrue( $this->spco_mock->is_reg_checkout() );
+		}
+	}
+
 
 }
-// End of file EED_Single_Page_CheckoutTest.php
-// Location: tests/testcases/modules/EED_Single_Page_CheckoutTest.php
+// End of file EED_Single_Page_Checkout_Test.php
+// Location: tests/testcases/modules/EED_Single_Page_Checkout_Test.php
