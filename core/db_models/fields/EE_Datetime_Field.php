77b97848f799f9eb9cff12cf2eea3f616773f4ed
Darren Ethier <darren@roughsmootheng.in>
2016-11-07 19:58:49 -0500

    make sure we’re consistently instantiating DbSafeDateTime [touch: 10209]

--- core/db_models/fields/EE_Datetime_Field.php
+++ core/db_models/fields/EE_Datetime_Field.php
@@ -454,7 +454,7 @@ class EE_Datetime_Field extends EE_Model_Field_Base {
 				if ( WP_DEBUG ) {
 					throw new EE_Error( sprintf( __('EE_Datetime_Field::_prepare_for_display requires a DateTime class to be the value for the $DateTime argument because the %s field is not nullable.', 'event_espresso' ), $this->_nicename ) );
 				} else {
-					$DateTime = new DateTime( \EE_Datetime_Field::now );
+					$DateTime = new DbSafeDateTime( \EE_Datetime_Field::now );
 					EE_Error::add_error( sprintf( __('EE_Datetime_Field::_prepare_for_display requires a DateTime class to be the value for the $DateTime argument because the %s field is not nullable.  When WP_DEBUG is false, the value is set to "now" instead of throwing an exception.', 'event_espresso' ), $this->_nicename ) );
 				}
 			}
