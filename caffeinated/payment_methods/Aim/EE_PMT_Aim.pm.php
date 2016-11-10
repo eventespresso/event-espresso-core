dd63ac8edd17c9c46d7d884c91682c05d4c26af8
Brent Christensen <brent@eventespresso.com>
2016-11-09 16:49:36 -0800

    arg should be an array; [touch:9576] (reverted from commit 20fae12d97c656e2ccba8d9d8b81f023b6e3b3cd)

--- caffeinated/payment_methods/Aim/EE_PMT_Aim.pm.php
+++ caffeinated/payment_methods/Aim/EE_PMT_Aim.pm.php
@@ -23,7 +23,7 @@ class EE_PMT_Aim extends EE_PMT_Base{
 		$this->_setup_properties();
 		parent::__construct($pm_instance);
 	}
-
+	
 	/**
 	 * Sets up payment method type properties for this gateway, which is normally
 	 * done in the constructor, but we want this to be easy for similar gateways to override
@@ -59,9 +59,9 @@ class EE_PMT_Aim extends EE_PMT_Base{
 					'required'=>true,
 					'html_label_text' => __( 'Expiry Month', 'event_espresso' )
 				)),
-				'exp_year'=>new EE_Credit_Card_Year_Input( array(
+				'exp_year'=>new EE_Credit_Card_Year_Input( array( 
 					'required'=>true,
-					'html_label_text' => __( 'Expiry Year', 'event_espresso' )
+					'html_label_text' => __( 'Expiry Year', 'event_espresso' ) 
 				)),
 				'cvv'=>new EE_CVV_Input( array(
 					'required'=>true,
@@ -73,26 +73,24 @@ class EE_PMT_Aim extends EE_PMT_Base{
 				'html_label_text' => __('Company', 'event_espresso')
 			))
 		), 'email', false );
-		$billing_form->add_subsections(
+		$billing_form->add_subsections( 
 				array(
 					'fax' => new EE_Text_Input( array(
 						'html_label_text' => __('Fax', 'event_espresso')
 					))
-				),
-				'phone',
+				), 
+				'phone', 
 				false );
 		$settings_form = $this->settings_form();
 		if( $settings_form->get_input( 'excluded_billing_inputs' ) instanceof EE_Checkbox_Multi_Input ) {
-			$billing_form->exclude(
-				array( $settings_form->get_input( 'excluded_billing_inputs' )->normalized_value() )
-			);
+				$billing_form->exclude( $settings_form->get_input( 'excluded_billing_inputs' )->normalized_value() );
 		}
 		if( $settings_form->get_input( 'required_billing_inputs' ) instanceof EE_Checkbox_Multi_Input ) {
 			$required_inputs = $settings_form->get_input( 'required_billing_inputs' )->normalized_value();
 			//only change the requirement of inputs which are allowed to be changed
 			/** @var EE_Form_Input_Base[] $inputs_to_evaluate */
-			$inputs_to_evaluate = array_intersect_key(
-				$billing_form->inputs(),
+			$inputs_to_evaluate = array_intersect_key( 
+				$billing_form->inputs(), 
 				$this->billing_input_names()
 			);
 			foreach( $inputs_to_evaluate as $input_name => $input ) {
@@ -118,7 +116,7 @@ class EE_PMT_Aim extends EE_PMT_Base{
 	 */
 	public function apply_billing_form_debug_settings( EE_Billing_Info_Form $billing_form ) {
 		if (
-			$this->_pm_instance->debug_mode()
+			$this->_pm_instance->debug_mode() 
 			|| $this->_pm_instance->get_extra_meta( 'test_transactions', TRUE, FALSE )
 		) {
 			$billing_form->get_input( 'credit_card' )->set_default( '4007000000027' );
@@ -133,7 +131,7 @@ class EE_PMT_Aim extends EE_PMT_Base{
 			$billing_form->add_subsections(
 				array(
 					'debug_content' => new EE_Form_Section_HTML_From_Template(
-						__DIR__.DS.'templates'.DS.'authorize_net_aim_debug_info.template.php'
+						__DIR__.DS.'templates'.DS.'authorize_net_aim_debug_info.template.php' 
 					)
 				),
 				'first_name'
@@ -171,16 +169,16 @@ class EE_PMT_Aim extends EE_PMT_Base{
 							'required' => true
 						)
 					),
-					'excluded_billing_inputs' => new EE_Checkbox_Multi_Input(
+					'excluded_billing_inputs' => new EE_Checkbox_Multi_Input( 
 							$billing_input_names,
-					array(
+					array( 
 						'html_label_text' => sprintf( __("Excluded Payment Form Fields %s", 'event_espresso'),  $this->get_help_tab_link() ),
 						'default' => array(
 							'company',
 							'fax',
 						)
 					)),
-					'required_billing_inputs' => new EE_Checkbox_Multi_Input(
+					'required_billing_inputs' => new EE_Checkbox_Multi_Input( 
 						$billing_input_names,
 						array(
 							'html_label_text' => sprintf( __("Required Payment Form Fields %s", 'event_espresso'),  $this->get_help_tab_link() ),
@@ -205,12 +203,12 @@ class EE_PMT_Aim extends EE_PMT_Base{
 							'html_help_text' => __( 'The Gateway Server where payment requests will be sent', 'event_espresso' )
 						)
 					)
-
+						
 				)
 			)
 		);
 	}
-
+	
 	/**
 	 * Returns an array where keys are the slugs for billing inputs, and values
 	 * are their i18n names
@@ -233,11 +231,11 @@ class EE_PMT_Aim extends EE_PMT_Base{
 			'cvv' => __('CVV', 'event_espresso')
 		);
 	}
-
+	
 	/**
 	 * Overrides parent so we always have all billing inputs in the returned array,
 	 * not just the ones included at the time. This helps simplify the gateway code
-	 *
+	 * 
 	 * @param EE_Billing_Info_Form $billing_form
 	 * @return array
 	 */
@@ -249,7 +247,7 @@ class EE_PMT_Aim extends EE_PMT_Base{
 		return array_merge(
 				$all_billing_values_empty,
 				parent::_get_billing_values_from_form($billing_form) );
-
+		
 	}
 
 
