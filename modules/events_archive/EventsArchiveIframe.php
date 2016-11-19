f9335b9fd709906f5424e4fdb591847b1e86c3ab
Brent Christensen <brent@eventespresso.com>
2016-11-18 16:05:55 -0800

    since we're calling wp_head() and wp_footer() by default, and want theme assets loaded, we can skip loading TS assets manually; add filter to remove event list heading and move in other filter from EED_Events_Archive; [touch:7526]

--- modules/events_archive/EventsArchiveIframe.php
+++ modules/events_archive/EventsArchiveIframe.php
@@ -25,50 +25,21 @@ class EventsArchiveIframe extends Iframe
      * EventsArchiveIframe constructor.
      *
      * @param \EED_Events_Archive $EED_Events_Archive
-     * @throws \EE_Error
+     * @throws \DomainException
      */
     public function __construct( $EED_Events_Archive )
     {
         \EE_Registry::instance()->REQ->set_espresso_page( true );
+        add_filter('FHEE__EED_Events_Archive__event_list_iframe', '__return_true');
+        add_filter(
+            'FHEE__archive_espresso_events_template__upcoming_events_h1',
+            '__return_empty_string'
+        );
         $EED_Events_Archive->event_list();
         $event_list = new \EES_Espresso_Events();
         parent::__construct(
             esc_html__( 'Event List', 'event_espresso' ),
             $event_list->process_shortcode()
-        );
-        $this->addStylesheets(
-            apply_filters(
-                'FHEE__EventEspresso_modules_events_archive_EventsArchiveIframe__display__css',
-                array(
-                    'espresso_default'           => is_readable( EVENT_ESPRESSO_UPLOAD_DIR . 'css/style.css' )
-                        ? EVENT_ESPRESSO_UPLOAD_DIR . 'css/espresso_default.css?ver=' . EVENT_ESPRESSO_VERSION
-                        : EE_GLOBAL_ASSETS_URL . 'css/espresso_default.css?ver=' . EVENT_ESPRESSO_VERSION,
-                    $EED_Events_Archive->theme() => get_stylesheet_directory_uri()
-                                                    . $EED_Events_Archive->theme() . DS
-                                                    . 'style.css?ver=' . EVENT_ESPRESSO_VERSION,
-                )
-            )
-        );
-        $this->addScripts(
-            apply_filters(
-                'FHEE__EventEspresso_modules_events_archive_EventsArchiveIframe__display__js',
-                array(
-                    'gmap_api' => sprintf(
-                        "https://maps.googleapis.com/maps/api/js?key=%s",
-                        apply_filters(
-                            'FHEE__EEH_Maps__espresso_google_maps_js__api_key',
-                            \EE_Registry::instance()->CFG->map_settings->google_map_api_key
-                        )
-                    ),
-                    'ee_gmap'  => EE_HELPERS_ASSETS . 'ee_gmap.js?ver=1.0',
-                )
-            )
-        );
-        $this->addLocalizedVars(
-            array(
-                'ee_gmap' => \EEH_Maps::$gmap_vars,
-            ),
-            'ee_gmap_vars'
         );
     }
 
