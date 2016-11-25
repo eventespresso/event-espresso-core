c87266af8721b6a6e6118fae6c4d7d3fbd93ef10
Brent Christensen <brent@eventespresso.com>
2016-11-25 12:19:50 -0800

    add target="blank" to link if content is within an iframe; refactor code to increase readability; [touch:9576]

--- public/Espresso_Arabica_2014/content-espresso_events-header.php
+++ public/Espresso_Arabica_2014/content-espresso_events-header.php
@@ -1,14 +1,13 @@
 <?php
 //echo '<br/><h6 style="color:#2EA2CC;">'. __FILE__ . ' &nbsp; <span style="font-weight:normal;color:#E76700"> Line #: ' . __LINE__ . '</span></h6>';
 global $post;
+$tag = is_single() ? 'h1' : 'h2';
 ?>
 <header class="event-header">
-	<?php if ( is_single() ) : ?>
-	<h1 id="event-details-h1-<?php echo $post->ID; ?>" class="entry-title">
-	<?php else : ?>
-	<h2 id="event-details-h2-<?php echo $post->ID; ?>" class="entry-title">
-	<?php endif; ?>
-		<a class="" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	<?php if ( is_single() ) : ?></h1><?php else : ?></h2><?php endif; ?>
+	<?php echo "<{$tag}  id=\"event-details-{$tag}-{$post->ID}\" class=\"entry-title\">"; ?>
+		<a class="ee-event-header-lnk" href="<?php the_permalink(); ?>"<?php echo \EED_Events_Archive::link_target();?>>
+            <?php the_title(); ?>
+        </a>
+	<?php echo "</{$tag}"; ?>
 	<?php if ( ! is_archive() && has_excerpt( $post->ID )): the_excerpt(); endif;?>
 </header>
