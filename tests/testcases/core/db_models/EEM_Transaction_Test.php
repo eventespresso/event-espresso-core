7a53e0fad1e60f84c533c1c7272965b886ebc62d
Darren Ethier <darren@roughsmootheng.in>
2016-12-11 18:14:25 -0500

    formatting fixes [touch: 8853]

--- tests/testcases/core/db_models/EEM_Transaction_Test.php
+++ tests/testcases/core/db_models/EEM_Transaction_Test.php
@@ -19,19 +19,25 @@ class EEM_Transaction_Test extends EE_UnitTestCase
      */
     function test_delete_junk_transactions()
     {
-        $old_txn_count            = EEM_Transaction::instance()->count();
         $pretend_bot_creations    = 9;
         $pretend_real_recent_txns = 3;
         $pretend_real_good_txns   = 5;
         require_once EE_TESTS_DIR . 'mocks' . DS . 'core' . DS . 'EE_Session_Mock.core.php';
-        $this->factory->transaction->create_many($pretend_bot_creations,
-            array('TXN_timestamp' => time() - WEEK_IN_SECONDS * 2, 'STS_ID' => EEM_Transaction::failed_status_code));
-        $this->factory->transaction->create_many($pretend_real_recent_txns, array(
-            'TXN_timestamp' => time() - EE_Session_Mock::instance()->lifespan() + MINUTE_IN_SECONDS,
-            'STS_ID'        => EEM_Transaction::failed_status_code,
-        ));
-        $this->factory->transaction->create_many($pretend_real_good_txns,
-            array('STS_ID' => EEM_Transaction::abandoned_status_code));
+        $this->factory->transaction->create_many(
+            $pretend_bot_creations,
+            array('TXN_timestamp' => time() - WEEK_IN_SECONDS * 2, 'STS_ID' => EEM_Transaction::failed_status_code)
+        );
+        $this->factory->transaction->create_many(
+            $pretend_real_recent_txns,
+            array(
+                'TXN_timestamp' => time() - EE_Session_Mock::instance()->lifespan() + MINUTE_IN_SECONDS,
+                'STS_ID'        => EEM_Transaction::failed_status_code,
+            )
+        );
+        $this->factory->transaction->create_many(
+            $pretend_real_good_txns,
+            array('STS_ID' => EEM_Transaction::abandoned_status_code)
+        );
         $failed_transaction_with_real_payment = $this->factory->transaction->create(
             array( 'TXN_timestamp' => time() - WEEK_IN_SECONDS * 2, 'STS_ID' => EEM_Transaction::failed_status_code )
         );
@@ -50,4 +56,3 @@ class EEM_Transaction_Test extends EE_UnitTestCase
 
 
 }
-// End of file EEM_Transaction_Test.php
