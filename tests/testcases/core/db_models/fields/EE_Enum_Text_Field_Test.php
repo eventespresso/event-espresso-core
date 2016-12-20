475f6a4a7eaec548b73b1c3985eefd8e168dbea9
Brent Christensen <brent@eventespresso.com>
2016-12-20 13:07:00 -0800

    formatting; [touch:10321]

--- tests/testcases/core/db_models/fields/EE_Enum_Text_Field_Test.php
+++ tests/testcases/core/db_models/fields/EE_Enum_Text_Field_Test.php
@@ -19,7 +19,10 @@ class EE_Enum_Text_Field_Test extends EE_UnitTestCase
     /**
      * @var EE_Enum_Text_Field
      */
-    protected $_an_enum_field = null;
+    protected $_an_enum_field;
+
+
+
     public function setUp()
     {
         $this->_an_enum_field = new EE_Enum_Text_Field(
@@ -46,7 +49,7 @@ class EE_Enum_Text_Field_Test extends EE_UnitTestCase
     }
 
     /**
-     * verifies that when invalid input is provided, it's replaced with the deafult
+     * verifies that when invalid input is provided, it's replaced with the default
      */
     public function test_prepare_for_set__invalid()
     {
@@ -61,27 +64,39 @@ class EE_Enum_Text_Field_Test extends EE_UnitTestCase
 
     /**
      * Verify that when retrieving from the DB, we're accepting of invalid input.
-     * That's because data might have gotten into the db via models when the filter FHEE__EE_Enum_Text_Field___allowed_enum_options
-     * was used, but then the filter callback got removed. In that case, we don't really want to change that existing data
+     * That's because data might have gotten into the db via models
+     * when the filter FHEE__EE_Enum_Text_Field___allowed_enum_options
+     * was used, but then the filter callback got removed. In that case,
+     * we don't really want to change that existing data
      * in the DB, and we certainly don't want to have errors because of it.
      */
     public function test_prepare_for_set_from_db()
     {
-        $this->assertEquals('invalid-data-from-db', $this->_an_enum_field->prepare_for_set_from_db('invalid-data-from-db'));
+        $this->assertEquals(
+            'invalid-data-from-db',
+            $this->_an_enum_field->prepare_for_set_from_db('invalid-data-from-db')
+        );
     }
 
 
 
     /**
-     * Verify that when something borked got into the model (eg from prepare_for_set_from_db()) then it should actually
-     * stay that way, even inserting it into the DB. This way, if someone retrieves something borked from the DB and
-     * it gets set on the model, if they switch some other field on that model object and re-save it, this enum's "invalid"
+     * Verify that when something borked got into the model
+     * (eg from prepare_for_set_from_db()) then it should actually
+     * stay that way, even inserting it into the DB.
+     * This way, if someone retrieves something borked from the DB and
+     * it gets set on the model, if they switch some other field
+     * on that model object and re-save it, this enum's "invalid"
      * data won't get overridden.
-     * So if we want to enforce switching data back into a valid form, we need to explicitly do that, it won't happen
+     * So if we want to enforce switching data back into a valid form,
+     * we need to explicitly do that, it won't happen
      * automatically.
      */
     public function test_prepare_for_use_in_db()
     {
-        $this->assertEquals('invalid-data-on-model-object', $this->_an_enum_field->prepare_for_use_in_db('invalid-data-on-model-object'));
+        $this->assertEquals(
+            'invalid-data-on-model-object',
+            $this->_an_enum_field->prepare_for_use_in_db('invalid-data-on-model-object')
+        );
     }
 }
