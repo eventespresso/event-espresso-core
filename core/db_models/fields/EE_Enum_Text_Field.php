fcc4a8070f5cfff8c6079637cc1ef3c607bbcd91
Darren Ethier <darren@roughsmootheng.in>
2016-12-22 10:47:15 -0500

    autoformatting for PSR [touch: 10330]

--- core/db_models/fields/EE_Enum_Text_Field.php
+++ core/db_models/fields/EE_Enum_Text_Field.php
@@ -1,76 +1,77 @@
 <?php
-require_once( EE_MODELS . 'fields/EE_Text_Field_Base.php' );
+require_once(EE_MODELS . 'fields/EE_Text_Field_Base.php');
+
 /**
- *
  * Class EE_Enum_Text_Field
- *
  * Description
  *
- * @package 			Event Espresso
- * @subpackage  	core
- * @author 				Mike Nelson
- *
+ * @package               Event Espresso
+ * @subpackage            core
+ * @author                Mike Nelson
  */
-class EE_Enum_Text_Field extends EE_Text_Field_Base {
+class EE_Enum_Text_Field extends EE_Text_Field_Base
+{
 
-	var $_allowed_enum_values;
+    var $_allowed_enum_values;
 
-	/**
-	 *
-	 * @param string $table_column
-	 * @param string $nice_name
-	 * @param boolean $nullable
-	 * @param mixed $default_value
-	 * @param array $allowed_enum_values  keys are values to be used in the DB, values are how they should be displayed
-	 */
-	function __construct($table_column, $nice_name, $nullable, $default_value, $allowed_enum_values){
-		$this->_allowed_enum_values = $allowed_enum_values;
-		parent::__construct($table_column, $nice_name, $nullable, $default_value);
-	}
-
-
-
-	/**
-	 * When setting, just verify that the value being used matches what we've defined as allowable enum values.
-	 * If not, throw an error (but if WP_DEBUG is false, just set the value to default)
-	 * @param string $value_inputted_for_field_on_model_object
-	 * @return string
-	 * @throws EE_Error
-	 */
-	function prepare_for_set( $value_inputted_for_field_on_model_object ) {
-		if(
-			$value_inputted_for_field_on_model_object !== null
-			&& ! array_key_exists( $value_inputted_for_field_on_model_object, $this->_allowed_enum_values )
-		){
-			if( defined( 'WP_DEBUG' ) &&  WP_DEBUG ){
-				$msg = sprintf(
-					__('System is assigning incompatible value "%1$s" to field "%2$s"','event_espresso'),
-					$value_inputted_for_field_on_model_object,
-					$this->_name
-				);
-				$msg2 = sprintf(
-					__('Allowed values for "%1$s" are "%2$s". You provided: "%3$s"','event_espresso'),
-					$this->_name,
-					implode( ", ", array_keys( $this->_allowed_enum_values ) ),
-					$value_inputted_for_field_on_model_object
-				);
-				EE_Error::add_error( "$msg||$msg2", __FILE__, __FUNCTION__, __LINE__ );
-			}
-			return $this->get_default_value();
-
-		}
-		return $value_inputted_for_field_on_model_object;
-	}
+    /**
+     * @param string  $table_column
+     * @param string  $nice_name
+     * @param boolean $nullable
+     * @param mixed   $default_value
+     * @param array   $allowed_enum_values keys are values to be used in the DB, values are how they should be displayed
+     */
+    function __construct($table_column, $nice_name, $nullable, $default_value, $allowed_enum_values)
+    {
+        $this->_allowed_enum_values = $allowed_enum_values;
+        parent::__construct($table_column, $nice_name, $nullable, $default_value);
+    }
 
 
+    /**
+     * When setting, just verify that the value being used matches what we've defined as allowable enum values.
+     * If not, throw an error (but if WP_DEBUG is false, just set the value to default)
+     *
+     * @param string $value_inputted_for_field_on_model_object
+     * @return string
+     * @throws EE_Error
+     */
+    function prepare_for_set($value_inputted_for_field_on_model_object)
+    {
+        if (
+            $value_inputted_for_field_on_model_object !== null
+            && ! array_key_exists($value_inputted_for_field_on_model_object, $this->_allowed_enum_values)
+        ) {
+            if (defined('WP_DEBUG') && WP_DEBUG) {
+                $msg  = sprintf(
+                    __('System is assigning incompatible value "%1$s" to field "%2$s"', 'event_espresso'),
+                    $value_inputted_for_field_on_model_object,
+                    $this->_name
+                );
+                $msg2 = sprintf(
+                    __('Allowed values for "%1$s" are "%2$s". You provided: "%3$s"', 'event_espresso'),
+                    $this->_name,
+                    implode(", ", array_keys($this->_allowed_enum_values)),
+                    $value_inputted_for_field_on_model_object
+                );
+                EE_Error::add_error("$msg||$msg2", __FILE__, __FUNCTION__, __LINE__);
+            }
+            return $this->get_default_value();
 
-	/**
-	 * Gets the pretty version of the enum's value.
-	 * @param 	 int |string $value_on_field_to_be_outputted
-	 * @param 	null $schema
-	 * @return 	string
-	 */
-	function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null) {
-		return $this->_allowed_enum_values[$value_on_field_to_be_outputted];
-	}
+        }
+        return $value_inputted_for_field_on_model_object;
+    }
+
+
+    /**
+     * Gets the pretty version of the enum's value.
+     *
+     * @param     int |string $value_on_field_to_be_outputted
+     * @param    null         $schema
+     * @return    string
+     */
+    function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null)
+    {
+        return $this->_allowed_enum_values[$value_on_field_to_be_outputted];
+    }
 }
--- core/db_models/fields/EE_Post_Content_Field.php
+++ core/db_models/fields/EE_Post_Content_Field.php
@@ -1,22 +1,27 @@
 <?php
+
 /**
  * Field to only allow tags that are normally allowed on post_content:
  * address,a,abbr,acronym,area,article,aside,b,big,blockquote,br,button,caption,cite,code,col,del,dd,dfn,details,div,dl,dt,em,fieldset,figure,figcaption,font,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,hr,i,img,ins,kbd,label,legend,li,map,mark,menu,nav,p,pre,q,s,samp,span,section,small,strike,strong,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,title,tr,tt,u,ul,ol,var
  */
-class EE_Post_Content_Field extends EE_Text_Field_Base{
-	/**
-	 * removes all tags which a WP Post wouldn't allow in its content normally
-	 * @param string $value
-	 * @return string
-	 */
-	function prepare_for_set($value) {
-		if( ! current_user_can( 'unfiltered_html' ) ) {
-			$value =  wp_kses("$value",wp_kses_allowed_html( 'post' ));
-		}
-		return parent::prepare_for_set($value);
-	}
-	
-	function prepare_for_set_from_db($value_found_in_db_for_model_object){
-		return $value_found_in_db_for_model_object;
-	}
+class EE_Post_Content_Field extends EE_Text_Field_Base
+{
+    /**
+     * removes all tags which a WP Post wouldn't allow in its content normally
+     *
+     * @param string $value
+     * @return string
+     */
+    function prepare_for_set($value)
+    {
+        if (! current_user_can('unfiltered_html')) {
+            $value = wp_kses("$value", wp_kses_allowed_html('post'));
+        }
+        return parent::prepare_for_set($value);
+    }
+
+    function prepare_for_set_from_db($value_found_in_db_for_model_object)
+    {
+        return $value_found_in_db_for_model_object;
+    }
 }
