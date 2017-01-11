a1ae5c49e31f3ecb895c53c689d2b75ad2575c03
Brent Christensen <brent@eventespresso.com>
2017-01-10 21:07:17 -0800

    add base class for widgets; [touch:10381]

---
+++ widgets/EspressoWidget.php
@@ -0,0 +1,23 @@
+<?php
+namespace EventEspresso\widgets;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class EspressoWidget
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class EspressoWidget
+{
+
+
+
+}
+// End of file EspressoWidget.php
+// Location: EventEspresso\widgets/EspressoWidget.php
