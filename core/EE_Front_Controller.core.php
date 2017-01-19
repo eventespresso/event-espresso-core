a38400849839674191bb1dd293e9fc2b355db673
Josh Feck <josh@nxnweb.com>
2017-01-19 10:01:13 -0500

    add a sanity check, formatting [touch: 10409]

--- core/EE_Front_Controller.core.php
+++ core/EE_Front_Controller.core.php
@@ -612,7 +612,11 @@ final class EE_Front_Controller
     public function wp_print_scripts()
     {
         global $post;
-        if (get_post_type() === 'espresso_events' && is_singular()) {
+        if (
+            get_post_type() === 'espresso_events' 
+            && is_singular() 
+            && $post->EE_Event instanceof EE_Event
+        ) {
             \EEH_Schema::add_json_linked_data_for_event($post->EE_Event);
         }
     }
