df31083722629801f72881b3ebab97ae5bbca53a
Brent Christensen <brent@eventespresso.com>
2017-02-02 17:48:07 -0800

    add cacheExpiration() method; [touch:10381]

--- core/services/shortcodes/ShortcodeInterface.php
+++ core/services/shortcodes/ShortcodeInterface.php
@@ -16,6 +16,14 @@ interface ShortcodeInterface
     public function getTag();
 
     /**
+     * the length of time in seconds to cache the results of the processShortcode() method
+     * 0 means the processShortcode() results will NOT be cached at all
+     *
+     * @return int
+     */
+    public function cacheExpiration();
+
+    /**
      * a place for adding any initialization code that needs to run prior to wp_header().
      * this may be required for shortcodes that utilize a corresponding module,
      * and need to enqueue assets for that module
