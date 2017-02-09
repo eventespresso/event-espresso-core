f9ab5c52995ca468d333a9df77146780868ffb41
Brent Christensen <brent@eventespresso.com>
2017-02-09 09:42:40 -0800

    add $transient_manager property, inject TransientManager into constructor, and use for anything transient related; add clearPostRelatedCacheOnUpdate() and clearPostRelatedCache() methods for deleting post related cache when posts are updated; [touch:10381]

--- core/services/cache/CacheManager.php
+++ core/services/cache/CacheManager.php
@@ -2,6 +2,7 @@
 namespace EventEspresso\core\services\cache;
 
 use Closure;
+use EventEspresso\core\services\database\TransientManager;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
@@ -9,7 +10,7 @@ defined('EVENT_ESPRESSO_VERSION') || exit;
 
 /**
  * Class CacheManager
- * Description
+ * Controls the creation and deletion of cached content
  *
  * @package       Event Espresso
  * @author        Brent Christensen
@@ -26,39 +27,54 @@ class CacheManager
     const CACHE_TRANSIENT_PREFIX = 'ee_cache_';
 
     /**
+     * wp-option option_name for tracking post related cache
+     *
+     * @type string
+     */
+    const POST_CACHE_OPTIONS_KEY = 'ee_post_cache';
+
+    /**
+     * set to true to monitor when content is being served from cache or not
+     *
      * @type boolean
      */
     const DEBUG = true;
+
+    /**
+     * @var TransientManager $transient_manager
+     */
+    private $transient_manager;
 
 
 
     /**
      * CacheManager constructor.
+     *
+     * @param TransientManager $expired_transient_manager [required]
      */
-    public function __construct()
+    public function __construct(TransientManager $expired_transient_manager)
     {
-        \EEH_Debug_Tools::printr(__FUNCTION__, __CLASS__, __FILE__, __LINE__, 2);
-        // only perform during regular admin requests
-        if(( ! defined('DOING_AJAX') || ! DOING_AJAX) && is_admin()){
-            add_action('shutdown', array($this, 'clearExpiredCache'));
-        }
+        $this->transient_manager = $expired_transient_manager;
+        add_action('save_post', array($this, 'clearPostRelatedCache'));
     }
 
 
 
     /**
-     * @param string  $id_prefix Appended to all cache IDs. Can be helpful in finding specific cache types.
+     * @param string  $id_prefix [required] Appended to all cache IDs. Can be helpful in finding specific cache types.
      *                           May also be helpful to include an additional specific identifier,
      *                           such as a post ID as part of the $id_prefix so that individual caches
      *                           can be found and/or cleared. ex: "venue-28", or "shortcode-156".
      *                           CacheManager::CACHE_TRANSIENT_PREFIX will also be appended to the cache id.
-     * @param string  $cache_id  Additional identifying details that make this cache unique.
+     * @param string  $cache_id  [required] Additional identifying details that make this cache unique.
      *                           It is advisable to use some of the actual data
      *                           that is used to generate the content being cached,
      *                           in order to guarantee that the cache id is unique for that content.
      *                           The cache id will be md5'd before usage to make it more db friendly,
      *                           and the entire cache id string will be truncated to 190 characters.
-     * @param Closure $callback
+     * @param Closure $callback  [required] since the point of caching is to avoid generating content when not
+     *                           necessary,
+     *                           we wrap our content creation in a Closure so that it is not executed until needed.
      * @param int $expiration
      * @return Closure|mixed
      */
@@ -74,10 +90,10 @@ class CacheManager
                 $cache_id
             )
         );
-        $cache_id = substr(CacheManager::CACHE_TRANSIENT_PREFIX . $id_prefix . '-' . md5($cache_id), 0, 191);
+        $cache_id = substr(CacheManager::CACHE_TRANSIENT_PREFIX . $id_prefix . '-' . md5($cache_id), 0, 182);
         // is caching enabled for this shortcode ?
         if ($expiration) {
-            $content = get_transient($cache_id);
+            $content = $this->transient_manager->getTransient($cache_id);
         }
         // any existing content ?
         if (empty($content)) {
@@ -88,7 +104,7 @@ class CacheManager
                 if (CacheManager::DEBUG) {
                     \EEH_Debug_Tools::printr($cache_id, 'REFRESH CACHE', __FILE__, __LINE__);
                 }
-                set_transient($cache_id, $content, $expiration);
+                $this->transient_manager->addTransient($cache_id, $content, $expiration);
             }
         } else {
             if (CacheManager::DEBUG) {
@@ -100,6 +116,53 @@ class CacheManager
 
 
 
+    /**
+     * If you are caching content that pertains to a Post of any type,
+     * then it is recommended to pass the post id and cache id prefix to this method
+     * so that it can be added to the post related cache tracking.
+     * Then, whenever that post is updated, the cache will automatically be deleted,
+     * which helps to ensure that outdated cache content will not be served
+     *
+     * @param int $post_ID       [required]
+     * @param string $id_prefix  [required] Appended to all cache IDs. Can be helpful in finding specific cache types.
+     *                           May also be helpful to include an additional specific identifier,
+     *                           such as a post ID as part of the $id_prefix so that individual caches
+     *                           can be found and/or cleared. ex: "venue-28", or "shortcode-156".
+     *                           CacheManager::CACHE_TRANSIENT_PREFIX will also be appended to the cache id.
+     */
+    public function clearPostRelatedCacheOnUpdate($post_ID, $id_prefix)
+    {
+        $post_related_cache = (array) get_option(CacheManager::POST_CACHE_OPTIONS_KEY, array());
+        // if post is not already being tracked
+        if( ! isset($post_related_cache[ $post_ID ])){
+            // add array to add cache ids to
+            $post_related_cache[ $post_ID ] = array();
+        }
+        // add cache id to be tracked
+        $post_related_cache[ $post_ID ][] = $id_prefix;
+        update_option(CacheManager::POST_CACHE_OPTIONS_KEY, $post_related_cache);
+    }
+
+
+
+    /**
+     * callback hooked into the WordPress "save_post" action
+     * deletes any cache content associated with the post
+     *
+     * @param int $post_ID [required]
+     */
+    public function clearPostRelatedCache($post_ID)
+    {
+        $post_related_cache = (array)get_option(CacheManager::POST_CACHE_OPTIONS_KEY, array());
+        // if post is not being tracked
+        if ( ! isset($post_related_cache[ $post_ID ])) {
+            return;
+        }
+        // get cache id prefixes for post, and delete their corresponding transients
+        $this->transient_manager->deleteTransients((array)$post_related_cache[ $post_ID ]);
+        unset($post_related_cache[ $post_ID ]);
+        update_option(CacheManager::POST_CACHE_OPTIONS_KEY, $post_related_cache);
+    }
 
 }
 // End of file CacheManager.php
