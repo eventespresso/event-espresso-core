88b32ffdc799f70b6643eb4e67c68a2a2e2213b8
Brent Christensen <brent@eventespresso.com>
2017-02-09 13:48:09 -0800

    add properties for dependencies and inject; [touch:10381]

--- tests/mocks/core/EE_Session_Mock.core.php
+++ tests/mocks/core/EE_Session_Mock.core.php
@@ -1,4 +1,7 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
+<?php use EventEspresso\core\exceptions\InvalidSessionDataException;
+use EventEspresso\core\services\database\TransientManager;
+
+if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
 /**
  * Class EE_Session_Mock
  *
@@ -15,39 +18,54 @@ class EE_Session_Mock extends EE_Session {
 	/**
 	 * @type EE_Session_Mock $_instance
 	 */
-	private static $_instance = null;
+	private static $_instance;
+
+    /**
+     * @var TransientManager
+     */
+    private $expired_transient_manager;
 
 
 
-	/**
-	 * @singleton method used to instantiate class object
-	 * @access public
-	 * @param \EE_Encryption $encryption
-	 * @return EE_Session_Mock
-	 */
-	public static function instance( EE_Encryption $encryption = null ) {
+    /**
+     * @singleton method used to instantiate class object
+     * @param TransientManager $expired_transient_manager
+     * @param EE_Encryption    $encryption
+     * @return EE_Session_Mock
+     * @throws EE_Error
+     * @throws InvalidSessionDataException
+     */
+	public static function instance(
+	    TransientManager $expired_transient_manager = null,
+        EE_Encryption $encryption = null
+    ) {
 		// check if class object is instantiated
 		// session loading is turned ON by default, but prior to the init hook, can be turned back OFF via:
 		// add_filter( 'FHEE_load_EE_Session', '__return_false' );
 		if ( ! self::$_instance instanceof EE_Session_Mock ) {
-			self::$_instance = new self( $encryption );
+			self::$_instance = new self($expired_transient_manager, $encryption );
 		}
 		return self::$_instance;
 	}
 
 
 
-	/**
-	 * protected constructor to prevent direct creation
-	 * @Constructor
-	 * @access protected
-	 * @param \EE_Encryption $encryption
-	 */
-	protected function __construct( EE_Encryption $encryption = null ) {
+    /**
+     * protected constructor to prevent direct creation
+     *
+     * @Constructor
+     * @access protected
+     * @param TransientManager $expired_transient_manager
+     * @param EE_Encryption    $encryption
+     * @throws EE_Error
+     * @throws InvalidSessionDataException
+     */
+	protected function __construct(TransientManager $expired_transient_manager, EE_Encryption $encryption = null) {
 		add_filter( 'FHEE_load_EE_Session', '__return_false' );
-		$this->encryption = $encryption;
-		parent::__construct( $encryption );
-	}
+        parent::__construct($expired_transient_manager, $encryption );
+        $this->expired_transient_manager = $expired_transient_manager;
+        $this->encryption = $encryption;
+    }
 
 
 	public function lifespan() {
@@ -57,7 +75,7 @@ class EE_Session_Mock extends EE_Session {
 
 
 	/**
-	 * @return \EE_Encryption
+	 * @return EE_Encryption
 	 */
 	public function encryption() {
 		return $this->encryption;
