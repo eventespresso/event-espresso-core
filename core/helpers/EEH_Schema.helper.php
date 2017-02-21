4e3b3b6236a40042fc93a17da63e9038ad844afc
Tony Warwick <tony@eventespresso.com>
2017-02-21 13:37:19 +0000

    Use instanceof check rather then empty.
    [touch: 10525]

--- core/helpers/EEH_Schema.helper.php
+++ core/helpers/EEH_Schema.helper.php
@@ -25,7 +25,7 @@ class EEH_Schema {
     public static function add_json_linked_data_for_event(\EE_Event $event)
     {
     	//Check we have a valid datetime for the event
-    	if( empty( $event->primary_datetime() ) ) {
+    	if(! $event->primary_datetime() instanceof EE_Datetime) {
     		return;
     	}
 
