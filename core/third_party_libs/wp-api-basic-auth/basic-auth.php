99104e4cee87fcff1c3b5aec1f4a5d8a855ffb8a
Mike Nelson <michael@eventespresso.com>
2017-03-09 22:14:25 -0800

    [touch:10540]{t20}finally cleaned up basic auth plugin. This will make merging the original in harder, but makes this much clearer IMO. Also added two other globals used for passing info around in traditional basic auth fashion

--- core/third_party_libs/wp-api-basic-auth/basic-auth.php
+++ core/third_party_libs/wp-api-basic-auth/basic-auth.php
@@ -7,10 +7,23 @@
  * Version: 0.2
  * Plugin URI: https://github.com/WP-API/Basic-Auth
  */
+/**
+ * @global $wp_json_basic_auth_error WP_Error for using when displaying errors
+ * @global  $wp_json_basic_auth_success boolean indicating we successfully authenticated the user using basic auth (false
+ *                                              if we found basic auth data but it didn't correspond to a real user's credentials)
+ * @global $wp_json_basic_auth_received_data indicating whether basic auth data was received (either in the normal
+ *                                              HTTP header, or using the CGI-htaccess workaround, or the query string
+ *@param $user
 
+
+*
+*@return int|null|\WP_Error|\WP_User
+ */
 function json_basic_auth_handler( $user ) {
-    global $wp_json_basic_auth_error;
+    global $wp_json_basic_auth_error, $wp_json_basic_auth_success, $wp_json_basic_auth_received_data;
     $wp_json_basic_auth_error = null;
+    $wp_json_basic_auth_success = false;
+    $wp_json_basic_auth_received_data = false;
 
     // Don't authenticate twice
     if ( ! empty( $user ) ) {
@@ -20,7 +33,10 @@ function json_basic_auth_handler( $user ) {
 	//so instead look for auth info in a custom environment variable set by rewrite rules
 	//probably in .htaccess
     //and, as a last resort, look in the querystring
-    if( ! isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+    if( isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+        $username = $_SERVER['PHP_AUTH_USER'];
+        $password = $_SERVER['PHP_AUTH_PW'];
+    } else {
         if( isset( $_SERVER['HTTP_AUTHORIZATION'])) {
             $header = $_SERVER['HTTP_AUTHORIZATION'];
         } elseif( isset( $_SERVER[ 'REDIRECT_HTTP_AUTHORIZATION' ] ) ) {
@@ -38,20 +54,19 @@ function json_basic_auth_handler( $user ) {
                 $header_sans_word_basic = str_replace( 'Basic ', '', $header );
                 $auth_parts = explode( ':', base64_decode( $header_sans_word_basic ), 2 );
                 if ( is_array( $auth_parts ) && isset( $auth_parts[0], $auth_parts[1] ) ) {
-                    $_SERVER['PHP_AUTH_USER'] = $auth_parts[0];
-                    $_SERVER['PHP_AUTH_PW']   = $auth_parts[1];
+                    $username = $auth_parts[0];
+                    $password = $auth_parts[1];
                 }
             }
         }
     }
 
     // Check that we're trying to authenticate
-    if ( !isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+    if ( ! isset($username ) ) {
         return $user;
     }
-    $username = $_SERVER['PHP_AUTH_USER'];
-    $password = $_SERVER['PHP_AUTH_PW'];
-
+    //we have some authorization data we can try authenticating with
+    $wp_json_basic_auth_received_data = true;
     /**
      * In multi-site, wp_authenticate_spam_check filter is run on authentication. This filter calls
      * get_currentuserinfo which in turn calls the determine_current_user filter. This leads to infinite
@@ -75,7 +90,6 @@ function json_basic_auth_handler( $user ) {
     //if we found a user, remove regular cookie filters because
     //they're just going to overwrite what we've found
     if( $user->ID ){
-        global $wp_json_basic_auth_success;
         $wp_json_basic_auth_success = true;
         remove_filter( 'determine_current_user', 'wp_validate_auth_cookie' );
         remove_filter( 'determine_current_user', 'wp_validate_logged_in_cookie', 20 );
