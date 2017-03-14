3517cf69a73cdefa5febf453e331316635a7d4f0
Brent Christensen <brent@eventespresso.com>
2017-03-14 13:34:17 -0700

    PSR formatting; use use; rename method to PSR standards; [touch:10239]

--- core/services/commands/transaction/CreateTransactionCommand.php
+++ core/services/commands/transaction/CreateTransactionCommand.php
@@ -1,13 +1,15 @@
 <?php
 namespace EventEspresso\core\services\commands\transaction;
 
+use EE_Checkout;
 use EventEspresso\core\domain\services\capabilities\CapCheck;
 use EventEspresso\core\domain\services\capabilities\CapCheckInterface;
 use EventEspresso\core\domain\services\capabilities\PublicCapabilities;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
 use EventEspresso\core\services\commands\Command;
 use EventEspresso\core\services\commands\CommandRequiresCapCheckInterface;
 
-defined( 'EVENT_ESPRESSO_VERSION' ) || exit;
+defined('EVENT_ESPRESSO_VERSION') || exit;
 
 
 
@@ -19,64 +21,69 @@ defined( 'EVENT_ESPRESSO_VERSION' ) || exit;
  * @author        Brent Christensen
  * @since         $VID:$
  */
-class CreateTransactionCommand extends Command implements CommandRequiresCapCheckInterface {
+class CreateTransactionCommand extends Command implements CommandRequiresCapCheckInterface
+{
 
-	/**
-	 * @var \EE_Checkout $checkout
-	 */
-	protected $checkout;
+    /**
+     * @var EE_Checkout $checkout
+     */
+    protected $checkout;
 
-	/**
-	 * @var array $transaction_details
-	 */
-	protected $transaction_details;
-
-
-
-	/**
-	 * CreateTransactionCommand constructor.
-	 *
-	 * @param \EE_Checkout $checkout
-	 * @param array $transaction_details
-	 */
-	public function __construct( \EE_Checkout $checkout = null, array $transaction_details = array() ) {
-		$this->checkout = $checkout;
-		$this->transaction_details = $transaction_details;
-	}
+    /**
+     * @var array $transaction_details
+     */
+    protected $transaction_details;
 
 
 
-	/**
-	 * @return CapCheckInterface
-	 * @throws \EventEspresso\core\exceptions\InvalidDataTypeException
-	 */
-	public function getCapCheck() {
-		// need cap for non-AJAX admin requests
-		if ( ! ( defined('DOING_AJAX') && DOING_AJAX ) && is_admin() ) {
-			// there's no specific caps for editing/creating transactions,
-			// so that's why we are using ee_edit_registrations
-			return new CapCheck( 'ee_edit_registrations', 'create_new_transaction' );
-		}
-		return new PublicCapabilities( '', 'create_new_transaction' );
-	}
+    /**
+     * CreateTransactionCommand constructor.
+     *
+     * @param EE_Checkout $checkout
+     * @param array       $transaction_details
+     */
+    public function __construct(EE_Checkout $checkout = null, array $transaction_details = array())
+    {
+        $this->checkout = $checkout;
+        $this->transaction_details = $transaction_details;
+    }
 
 
 
-	/**
-	 * @return \EE_Checkout
-	 */
-	public function checkout() {
-		return $this->checkout;
-	}
+    /**
+     * @return CapCheckInterface
+     * @throws InvalidDataTypeException
+     */
+    public function getCapCheck()
+    {
+        // need cap for non-AJAX admin requests
+        if (! (defined('DOING_AJAX') && DOING_AJAX) && is_admin()) {
+            // there's no specific caps for editing/creating transactions,
+            // so that's why we are using ee_edit_registrations
+            return new CapCheck('ee_edit_registrations', 'create_new_transaction');
+        }
+        return new PublicCapabilities('', 'create_new_transaction');
+    }
 
 
 
-	/**
-	 * @return array
-	 */
-	public function transaction_details() {
-		return $this->transaction_details;
-	}
+    /**
+     * @return EE_Checkout
+     */
+    public function checkout()
+    {
+        return $this->checkout;
+    }
+
+
+
+    /**
+     * @return array
+     */
+    public function transactionDetails()
+    {
+        return $this->transaction_details;
+    }
 
 
 
