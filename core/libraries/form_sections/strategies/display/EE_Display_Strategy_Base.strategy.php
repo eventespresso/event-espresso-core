ff2314a5c14df9ca254a20694cd4940e10afdd97
Brent Christensen <brent@eventespresso.com>
2017-03-16 15:43:24 -0700

    PSR formatting; [touch:10586]

--- core/libraries/form_sections/strategies/display/EE_Display_Strategy_Base.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Display_Strategy_Base.strategy.php
@@ -1,14 +1,19 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
+<?php if (! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
+}
+
+
+
 /**
  * Class EE_Display_Strategy_Base
  *
- * @package 			Event Espresso
- * @subpackage    core
- * @author 				Mike Nelson, Brent Christensen
- * @since               	4.6
- *
+ * @package               Event Espresso
+ * @subpackage            core
+ * @author                Mike Nelson, Brent Christensen
+ * @since                 4.6
  */
-abstract class EE_Display_Strategy_Base extends EE_Form_Input_Strategy_Base{
+abstract class EE_Display_Strategy_Base extends EE_Form_Input_Strategy_Base
+{
 
 
     /**
@@ -37,38 +42,43 @@ abstract class EE_Display_Strategy_Base extends EE_Form_Input_Strategy_Base{
 
 
     /**
-	 * returns HTML and javascript related to the displaying of this input
-	 * @return string
-	 */
-	abstract public function display();
+     * returns HTML and javascript related to the displaying of this input
+     *
+     * @return string
+     */
+    abstract public function display();
 
 
 
-	/**
-	 * _remove_chars - takes an incoming string, and removes the string $chars from the end of it, but only if $chars is already there
-	 *
-	 * @param string $string - the string being processed
-	 * @param string $chars - exact string of characters to remove
-	 * @return string
-	 */
-	protected function _remove_chars( $string = '', $chars = '-' ) {
-		$char_length = strlen( $chars ) * -1;
-		// if last three characters of string is  " - ", then remove it
-		return substr( $string, $char_length ) === $chars ? substr( $string, 0, $char_length ) : $string;
-	}
+    /**
+     * _remove_chars - takes an incoming string, and removes the string $chars from the end of it, but only if $chars
+     * is already there
+     *
+     * @param string $string - the string being processed
+     * @param string $chars  - exact string of characters to remove
+     * @return string
+     */
+    protected function _remove_chars($string = '', $chars = '-')
+    {
+        $char_length = strlen($chars) * -1;
+        // if last three characters of string is  " - ", then remove it
+        return substr($string, $char_length) === $chars ? substr($string, 0, $char_length) : $string;
+    }
 
 
 
-	/**
-	 * _append_chars - takes an incoming string, and adds the string $chars to the end of it, but only if $chars is not already there
-	 *
-	 * @param string $string - the string being processed
-	 * @param string $chars - exact string of characters to be added to end of string
-	 * @return string
-	 */
-	protected function _append_chars( $string = '', $chars = '-' ) {
-		return  $this->_remove_chars( $string, $chars ) . $chars;
-	}
+    /**
+     * _append_chars - takes an incoming string, and adds the string $chars to the end of it, but only if $chars is not
+     * already there
+     *
+     * @param string $string - the string being processed
+     * @param string $chars  - exact string of characters to be added to end of string
+     * @return string
+     */
+    protected function _append_chars($string = '', $chars = '-')
+    {
+        return $this->_remove_chars($string, $chars) . $chars;
+    }
 
 
 
@@ -78,28 +88,34 @@ abstract class EE_Display_Strategy_Base extends EE_Form_Input_Strategy_Base{
      * @param bool $add_pound_sign
      * @return array
      */
-	public function get_html_input_ids( $add_pound_sign = false ) {
-		return array( $this->get_input()->html_id( $add_pound_sign ) );
-	}
+    public function get_html_input_ids($add_pound_sign = false)
+    {
+        return array($this->get_input()->html_id($add_pound_sign));
+    }
 
 
 
-	/**
-	 * Adds js variables for localization to the $other_js_data. These should be put
-	 * in each form's "other_data" javascript object.
-	 *
-	 * @param array $other_js_data
-	 * @return array
-	 */
-	public function get_other_js_data( $other_js_data = array() ) {
-		return $other_js_data;
-	}
+    /**
+     * Adds js variables for localization to the $other_js_data. These should be put
+     * in each form's "other_data" javascript object.
+     *
+     * @param array $other_js_data
+     * @return array
+     */
+    public function get_other_js_data($other_js_data = array())
+    {
+        return $other_js_data;
+    }
 
-	/**
-	 * Opportunity for this display strategy to call wp_enqueue_script and wp_enqueue_style.
-	 * This should be called during wp_enqueue_scripts
-	 */
-	public function enqueue_js() {}
+
+
+    /**
+     * Opportunity for this display strategy to call wp_enqueue_script and wp_enqueue_style.
+     * This should be called during wp_enqueue_scripts
+     */
+    public function enqueue_js()
+    {
+    }
 
 
 
@@ -180,15 +196,15 @@ abstract class EE_Display_Strategy_Base extends EE_Form_Input_Strategy_Base{
         );
         $attributes_string = '';
         foreach ($this->attributes as $attribute => $value) {
-            if(is_numeric($attribute)){
+            if (is_numeric($attribute)) {
                 $add = true;
                 if (is_array($value)) {
                     $attribute = isset($value[0]) ? $value[0] : '';
-                    $add       = isset($value[1]) ? $value[1] : false;
+                    $add = isset($value[1]) ? $value[1] : false;
                 } else {
                     $attribute = $value;
                 }
-                $attributes_string .=  $this->_single_attribute($attribute, $add);
+                $attributes_string .= $this->_single_attribute($attribute, $add);
             } else {
                 $attributes_string .= $this->_attribute($attribute, $value);
             }
