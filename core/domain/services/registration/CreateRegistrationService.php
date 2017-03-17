69f7e7a90fd883956c3f9f770827265e2e27deaf
Brent Christensen <brent@eventespresso.com>
2017-03-17 11:01:59 -0700

    use use for everything; add $reg_status property, getter method, and inject into constructor; [touch:10165]

--- core/domain/services/registration/CreateRegistrationService.php
+++ core/domain/services/registration/CreateRegistrationService.php
@@ -1,10 +1,20 @@
 <?php
 namespace EventEspresso\core\domain\services\registration;
 
+use EE_Error;
+use EE_Event;
+use EE_Line_Item;
+use EE_Registration;
+use EE_Registry;
+use EE_Ticket;
+use EE_Transaction;
+use EEH_Line_Item;
+use EEM_Registration;
 use EventEspresso\core\domain\entities\RegCode;
 use EventEspresso\core\domain\entities\RegUrlLink;
 use EventEspresso\core\domain\services\DomainService;
 use EventEspresso\core\exceptions\UnexpectedEntityException;
+use OutOfRangeException;
 
 if ( ! defined('EVENT_ESPRESSO_VERSION')) {
     exit('No direct script access allowed');
@@ -25,45 +35,47 @@ class CreateRegistrationService extends DomainService
 
 
 	/**
-	 * @param \EE_Event       $event
-	 * @param \EE_Transaction $transaction
-	 * @param \EE_Ticket      $ticket
-	 * @param \EE_Line_Item   $ticket_line_item
+	 * @param EE_Event       $event
+	 * @param EE_Transaction $transaction
+	 * @param EE_Ticket      $ticket
+	 * @param EE_Line_Item   $ticket_line_item
 	 * @param                 $reg_count
 	 * @param                 $reg_group_size
-	 * @return \EE_Registration
-	 * @throws \OutOfRangeException
-	 * @throws \EE_Error
+     * @param string          $reg_status
+     * @return EE_Registration
+	 * @throws OutOfRangeException
+	 * @throws EE_Error
 	 * @throws UnexpectedEntityException
 	 */
     public function create(
-        \EE_Event $event,
-        \EE_Transaction $transaction,
-        \EE_Ticket $ticket,
-        \EE_Line_Item $ticket_line_item,
+        EE_Event $event,
+        EE_Transaction $transaction,
+        EE_Ticket $ticket,
+        EE_Line_Item $ticket_line_item,
         $reg_count,
-        $reg_group_size
+        $reg_group_size,
+        $reg_status = EEM_Registration::status_id_incomplete
     ) {
         $registrations = $transaction->registrations();
         $reg_count = $reg_count ? $reg_count : count($registrations) + 1;
         $reg_url_link = new RegUrlLink($reg_count, $ticket_line_item);
         $reg_code = new RegCode($reg_url_link, $transaction, $ticket);
         // generate new EE_Registration
-        $registration = \EE_Registration::new_instance(
+        $registration = EE_Registration::new_instance(
             array(
                 'EVT_ID'          => $event->ID(),
                 'TXN_ID'          => $transaction->ID(),
                 'TKT_ID'          => $ticket->ID(),
-                'STS_ID'          => \EEM_Registration::status_id_incomplete,
+                'STS_ID'          => $reg_status,
                 'REG_final_price' => $this->resolveFinalPrice($transaction, $ticket, $ticket_line_item),
-                'REG_session'     => \EE_Registry::instance()->SSN->id(),
+                'REG_session'     => EE_Registry::instance()->SSN->id(),
                 'REG_count'       => $reg_count,
                 'REG_group_size'  => $reg_group_size ? $reg_group_size : $this->incrementRegCount($registrations),
                 'REG_url_link'    => $reg_url_link,
                 'REG_code'        => $reg_code,
             )
         );
-        if ( ! $registration instanceof \EE_Registration) {
+        if ( ! $registration instanceof EE_Registration) {
             throw new UnexpectedEntityException($registration, 'EE_Registration');
         }
         // save registration so that we have an ID
@@ -81,19 +93,19 @@ class CreateRegistrationService extends DomainService
 
 
 	/**
-	 * @param \EE_Transaction $transaction
-	 * @param \EE_Ticket      $ticket
-	 * @param \EE_Line_Item   $ticket_line_item
+	 * @param EE_Transaction $transaction
+	 * @param EE_Ticket      $ticket
+	 * @param EE_Line_Item   $ticket_line_item
 	 * @return float
-	 * @throws \EE_Error
-	 * @throws \OutOfRangeException
+	 * @throws EE_Error
+	 * @throws OutOfRangeException
 	 */
     protected function resolveFinalPrice(
-        \EE_Transaction $transaction,
-        \EE_Ticket $ticket,
-        \EE_Line_Item $ticket_line_item
+        EE_Transaction $transaction,
+        EE_Ticket $ticket,
+        EE_Line_Item $ticket_line_item
     ) {
-        $final_price = \EEH_Line_Item::calculate_final_price_for_ticket_line_item(
+        $final_price = EEH_Line_Item::calculate_final_price_for_ticket_line_item(
             $transaction->total_line_item(),
             $ticket_line_item
         );
@@ -104,17 +116,17 @@ class CreateRegistrationService extends DomainService
 
 
     /**
-     * @param  \EE_Registration[] $registrations
+     * @param  EE_Registration[] $registrations
      * @param  boolean            $update_existing_registrations
      * @return int
-     * @throws \EE_Error
+     * @throws EE_Error
      */
     protected function incrementRegCount(array $registrations, $update_existing_registrations = true)
     {
         $new_reg_count = count($registrations) + 1;
         if ($update_existing_registrations) {
             foreach ($registrations as $registration) {
-                if ($registration instanceof \EE_Registration) {
+                if ($registration instanceof EE_Registration) {
                     $registration->set_count($new_reg_count);
                     $registration->save();
                 }
