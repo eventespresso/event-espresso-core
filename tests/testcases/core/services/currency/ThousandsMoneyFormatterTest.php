e1fe18c35b166275d39c152c50c913e6ee3af86b
Brent Christensen <brent@eventespresso.com>
2017-03-24 09:53:09 -0700

    unit tests for ThousandsMoneyFormatter; [touch:10619]

---
+++ tests/testcases/core/services/currency/ThousandsMoneyFormatterTest.php
@@ -0,0 +1,34 @@
+<?php
+use EventEspresso\core\entities\money\Currency;
+use EventEspresso\core\services\currency\ThousandsMoneyFormatter;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class ThousandsMoneyFormatterTest
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class ThousandsMoneyFormatterTest extends \EE_UnitTestCase
+{
+
+    /**
+     * @group Money
+     */
+    public function test_format()
+    {
+        $formatter = new ThousandsMoneyFormatter();
+        $currency = Currency::createFromCountryCode('US');
+        $this->assertEquals(
+            $formatter->format(1234.5, $currency),
+            '1,234.50'
+        );
+    }
+}
+// End of file DecimalMoneyFormatterTest.php
+// Location: tests/testcases/core/services/currency/ThousandsMoneyFormatterTest.php
