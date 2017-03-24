72647f3f78acb7f6d9bc0698b5160c40aa95ad3c
Brent Christensen <brent@eventespresso.com>
2017-03-24 09:53:38 -0700

    CurrencySignMoneyFormatter: adds currency sign to the supplied amount; [touch:10619]

---
+++ core/services/currency/CurrencySignMoneyFormatter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EventEspresso\core\services\currency;
+
+use EventEspresso\core\entities\money\Currency;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class CurrencySignMoneyFormatter
+ * adds currency sign to the supplied amount
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class CurrencySignMoneyFormatter implements MoneyFormatter
+{
+
+
+
+    /**
+     * @param string   $money_amount
+     * @param Currency $currency
+     * @return string
+     */
+    public function format($money_amount, Currency $currency)
+    {
+        // add currency sign
+        if ($currency->signB4()) {
+            if ($money_amount >= 0) {
+                return $currency->sign() . $money_amount;
+            }
+            return '-' . $currency->sign() . str_replace('-', '', $money_amount);
+        }
+        return $money_amount . $currency->sign();
+    }
+}
+// End of file CurrencySignMoneyFormatter.php
+// Location: EventEspresso\core\services\currency/CurrencySignMoneyFormatter.php
