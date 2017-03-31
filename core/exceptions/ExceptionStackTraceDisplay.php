f63ca652ac662f57b467c37a0adefa39b6ef11f5
Brent Christensen <brent@eventespresso.com>
2017-03-31 16:01:35 -0700

    don't use filter switches to enqueue assets; [touch:9751]

--- core/exceptions/ExceptionStackTraceDisplay.php
+++ core/exceptions/ExceptionStackTraceDisplay.php
@@ -371,9 +371,9 @@ class ExceptionStackTraceDisplay {
 			if ( wp_script_is( 'ee_error_js', 'enqueued' ) ) {
 				return '';
 			} else if ( wp_script_is( 'ee_error_js', 'registered' ) ) {
-				add_filter( 'FHEE_load_css', '__return_true' );
-				add_filter( 'FHEE_load_js', '__return_true' );
-				wp_enqueue_script( 'ee_error_js' );
+                wp_enqueue_style('espresso_default');
+                wp_enqueue_style('espresso_custom_css');
+                wp_enqueue_script( 'ee_error_js' );
 				wp_localize_script( 'ee_error_js', 'ee_settings', array( 'wp_debug' => WP_DEBUG ) );
 			}
 		} else {
