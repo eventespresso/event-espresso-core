9d8fdedc36c83467dfefe9af42b11772a52b718c
Brent Christensen <brent@eventespresso.com>
2017-04-03 13:21:56 -0700

    add a couple more assertions; [touch:10643]

--- tests/testcases/core/libraries/form_sections/inputs/EE_State_Select_Input_Test.php
+++ tests/testcases/core/libraries/form_sections/inputs/EE_State_Select_Input_Test.php
@@ -17,37 +17,6 @@ class EE_State_Select_Input_Test extends EE_UnitTestCase{
      * @group 10643
      * @throws EE_Error
      */
-    public function test_EE_State_Input_with_input()
-    {
-        $form = new EE_Form_Section_Proper(
-            array(
-                'name'        => 'where-you-live',
-                'subsections' => array(
-                    'state' => new EE_State_Select_Input(null)
-                )
-            )
-        );
-        $submitted_data = array('where-you-live' => array('state' => 61));
-        $form->receive_form_submission($submitted_data);
-        $this->assertEquals(
-            61,
-            $form->get_input('state')->normalized_value()
-        );
-        $this->assertEquals(
-            '61',
-            $form->get_input('state')->raw_value()
-        );
-        $this->assertEquals(
-            $submitted_data,
-            $form->submitted_values(true)
-        );
-    }
-
-
-    /**
-     * @group 10643
-     * @throws EE_Error
-     */
     public function test_EE_State_Input_with_no_input()
     {
         $form = new EE_Form_Section_Proper(
@@ -61,6 +30,7 @@ class EE_State_Select_Input_Test extends EE_UnitTestCase{
         $submitted_data = array('where-you-live' => array());
         $form->receive_form_submission($submitted_data);
         $this->assertTrue($form->is_valid());
+        $this->assertEmpty($form->submission_error_message());
         $this->assertEquals(
             null,
             $form->get_input('state')->normalized_value()
@@ -95,6 +65,41 @@ class EE_State_Select_Input_Test extends EE_UnitTestCase{
         $this->assertFalse($form->is_valid());
         $this->assertNotEmpty($form->submission_error_message());
     }
+
+
+
+    /**
+     * @group 10643
+     * @throws EE_Error
+     */
+    public function test_EE_State_Input_with_input()
+    {
+        $form = new EE_Form_Section_Proper(
+            array(
+                'name'        => 'where-you-live',
+                'subsections' => array(
+                    'state' => new EE_State_Select_Input(null)
+                )
+            )
+        );
+        $submitted_data = array('where-you-live' => array('state' => 61));
+        $form->receive_form_submission($submitted_data);
+        $this->assertTrue($form->is_valid());
+        $this->assertEquals(
+            61,
+            $form->get_input('state')->normalized_value()
+        );
+        $this->assertEquals(
+            '61',
+            $form->get_input('state')->raw_value()
+        );
+        $this->assertEquals(
+            $submitted_data,
+            $form->submitted_values(true)
+        );
+    }
+
+
 }
 
 // End of file EE_Radio_Button_Input_Test.php
