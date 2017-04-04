f8a23b8467c449154b1a512a14844e1129203c36
Brent Christensen <brent@eventespresso.com>
2017-04-04 10:04:11 -0700

    optimize conditionals (fastest first); use EEH_Money::strip_localized_money_formatting() to prep incoming string; reduce code and variables used; remove unnecessary else blocks after return statements; [touch:10586]

--- core/libraries/form_sections/strategies/normalization/EE_Int_Normalization.strategy.php
+++ core/libraries/form_sections/strategies/normalization/EE_Int_Normalization.strategy.php
@@ -15,20 +15,22 @@ class EE_Int_Normalization extends EE_Normalization_Strategy_Base{
 	 * @throws \EE_Validation_Error
 	 */
 	public function normalize($value_to_normalize) {
-		if( is_int( $value_to_normalize ) ){
+        if ($value_to_normalize === null || $value_to_normalize === '') {
+            return null;
+        }
+        if( is_int( $value_to_normalize ) ){
 			return $value_to_normalize;
 		}
-
-		if ( is_null( $value_to_normalize ) || $value_to_normalize === '' ) {
-		    return null;
-        }
-
 		if( ! is_string( $value_to_normalize )){
-			throw new EE_Validation_Error( sprintf( __( 'The value "%s" must be a string submitted for normalization, it was %s', 'event_espresso' ), print_r( $value_to_normalize, TRUE), gettype( $value_to_normalize ) ) );
+			throw new EE_Validation_Error(
+			    sprintf(
+			        __( 'The value "%s" must be a string submitted for normalization, it was %s', 'event_espresso' ),
+                        print_r( $value_to_normalize, TRUE),
+                        gettype( $value_to_normalize )
+                )
+            );
 		}
-		$thousands_separator = EE_Config::instance()->currency->thsnds;
-		$value_to_normalize = str_replace( $thousands_separator, "", $value_to_normalize );
-		$value_to_normalize = str_replace( array(" ","\t"), '', $value_to_normalize );
+		$value_to_normalize = EEH_Money::strip_localized_money_formatting($value_to_normalize);
 		$matches = array();
 		if ( preg_match( '/^(-?)(\d+)$/', $value_to_normalize, $matches )) {
 		    if(count($matches) !== 3){
@@ -37,35 +39,28 @@ class EE_Int_Normalization extends EE_Normalization_Strategy_Base{
                     $value_to_normalize )
                 );
             }
-            if( $matches[1] === '-'){
-		       $negate = true;
-		       $digits = $matches[2];
-            } else{
-		        $negate = false;
-		        $digits = $matches[2];
-            }
-			$digits =  intval( $digits ) ;
-		    if( $negate ){
-		        $digits *= -1;
-            }
-            return $digits;
-		} else {
-			//find if this input has a int validation strategy
-			//in which case, use its message
-			$validation_error_message = NULL;
-			foreach( $this->_input->get_validation_strategies() as $validation_strategy ){
-				if( $validation_strategy instanceof EE_Int_Validation_Strategy ){
-					$validation_error_message = $validation_strategy->get_validation_error_message();
-				}
-			}
-			//this really shouldn't ever happen because fields with a int normalization strategy
-			//should also have a int validation strategy, but in case it doesnt use the default
-			if( ! $validation_error_message ){
-				$default_validation_strategy = new EE_Int_Validation_Strategy();
-				$validation_error_message = $default_validation_strategy->get_validation_error_message();
-			}
-			throw new EE_Validation_Error( $validation_error_message, 'numeric_only' );
+            // if first match is the negative sign,
+            // then the number needs to be multiplied by -1 to remain negative
+            return $matches[1] === '-'
+                ? (int) $matches[2] * -1
+                : (int) $matches[2];
+
 		}
+        //find if this input has a int validation strategy
+        //in which case, use its message
+        $validation_error_message = NULL;
+        foreach( $this->_input->get_validation_strategies() as $validation_strategy ){
+            if( $validation_strategy instanceof EE_Int_Validation_Strategy ){
+                $validation_error_message = $validation_strategy->get_validation_error_message();
+            }
+        }
+        //this really shouldn't ever happen because fields with a int normalization strategy
+        //should also have a int validation strategy, but in case it doesnt use the default
+        if( ! $validation_error_message ){
+            $default_validation_strategy = new EE_Int_Validation_Strategy();
+            $validation_error_message = $default_validation_strategy->get_validation_error_message();
+        }
+        throw new EE_Validation_Error( $validation_error_message, 'numeric_only' );
 	}
 
 	/**
@@ -76,11 +71,11 @@ class EE_Int_Normalization extends EE_Normalization_Strategy_Base{
 	public function unnormalize( $normalized_value ) {
 	    if ($normalized_value === null) {
 	        return '';
-        }elseif( empty( $normalized_value ) ){
+        }
+        if( empty( $normalized_value ) ){
 			return '0';
-		}else{
-			return "$normalized_value";
 		}
+		return "$normalized_value";
 	}
 }
 
