b4172ab6349aef15f3ced51a03905eee7b5ed468
Darren Ethier <darren@roughsmootheng.in>
2017-04-14 16:48:10 -0400

    include more things in the new reactivation test [touch: 10659]

--- tests/testcases/core/EeSystemWithAddonTest.php
+++ tests/testcases/core/EeSystemWithAddonTest.php
@@ -18,13 +18,21 @@ class EeSystemWithAddonTest extends EeAddonTestCase
     public function testDetectActivationsOrUpgradesOnReactivation()
     {
         global $wp_actions;
+        $times_its_new_install_hook_fired_before = isset($wp_actions["AHEE__EE_NewAddonMock__reactivation"])
+            ? $wp_actions["AHEE__EE_NewAddonMock__reactivation"]
+            : 0;
         $this->pretendReactivation();
-        // $times_its_new_install_hook_fired_before = isset($wp_actions["AHEE__NewAddonMock__reactivation"])
-        //     ? $wp_actions["AHEE__NewAddonMock__reactivation"]
-        //     : 0;
-
         //do our assertions
         $this->assertWPOptionExists($this->addon->get_activation_indicator_option_name());
         $this->assertEquals(EE_System::req_type_reactivation, $this->addon->detect_req_type());
+
+        //test EE_System reset which simulates activation/upgrades
+        //simulate activation process
+        EE_System::reset();
+        $this->assertEquals(
+            $times_its_new_install_hook_fired_before + 1,
+            $wp_actions["AHEE__EE_NewAddonMock__reactivation"]
+        );
+        $this->assertWPOptionDoesNotExist($this->addon->get_activation_indicator_option_name());
     }
 }
