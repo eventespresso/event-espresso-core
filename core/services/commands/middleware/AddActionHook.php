b0cf92c96daf32e3d73d86c96a249b61daee4412
Brent Christensen <brent@eventespresso.com>
2017-04-25 18:14:52 -0700

    AddActionHook: Triggers a WordPress do_action() hook before each Command is executed; [touch:10693]

---
+++ core/services/commands/middleware/AddActionHook.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EventEspresso\core\services\commands\middleware;
+
+use Closure;
+use EventEspresso\core\services\commands\CommandInterface;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class AddActionHook
+ * Triggers a WordPress do_action() hook before each Command is executed
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class AddActionHook implements CommandBusMiddlewareInterface
+{
+
+    /**
+     * @param CommandInterface $command
+     * @param Closure         $next
+     * @return mixed
+     */
+    public function handle(CommandInterface $command, Closure $next)
+    {
+        do_action(
+            'AHEE__EventEspresso_core_services_commands_middleware_AddActionHook__handle__command',
+            $command
+        );
+        return $next($command);
+    }
+}
+// End of file AddActionHook.php
+// Location: EventEspresso\core\services\commands\middleware/AddActionHook.php
