75601a9151fd584bc29213b0ad668d64087a3fab
Darren Ethier <darren@roughsmootheng.in>
2017-05-25 14:37:23 -0400

    change name of base acceptance test cept [ci skip]
    Also makes sure that messages are set to run on same request within the test environment.

--- acceptance_tests/tests/BasicCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-// this is going to do the very bare minimum that happens with just instantiating our actor
-// includes:
-// - logging in.
-// - asserting Event Espresso Core is active (and activates without problems)
-$I = new EventEspressoAcceptanceTester($scenario);
