7ae59a1d52b1fb2dce39aff9624b9d60bcf98a0f
Brent Christensen <brent@eventespresso.com>
2017-06-06 15:04:25 -0700

    update interface;
    update validate() parameter to  match  interface; 
    remove unnecessary else clauses;
    reorder/optimize conditionals;
    extract logic into new getAtIndex() method; [touch:9222]

--- core/services/validation/strategies/Basic.php
+++ core/services/validation/strategies/Basic.php
@@ -2,8 +2,8 @@
 
 namespace EventEspresso\core\services\validation\strategies;
 
-use EventEspresso\core\services\validation\strategies\EmailValidationInterface;
 use EventEspresso\core\domain\services\validation\EmailValidationException;
+use EventEspresso\core\domain\services\validation\EmailValidatorInterface;
 
 defined('EVENT_ESPRESSO_VERSION') || exit('No direct script access allowed');
 
@@ -17,76 +17,76 @@ defined('EVENT_ESPRESSO_VERSION') || exit('No direct script access allowed');
  * @author         Mike Nelson
  * @since          $VID:$
  */
-class Basic implements EmailValidationInterface
+class Basic implements EmailValidatorInterface
 {
 
     /**
-     * @param string $input
+     * @param string $email_address
      * @return bool
      * @throws EmailValidationException
      */
-    public function validate($input)
+    public function validate($email_address)
     {
-        if (! preg_match('/^.+\@\S+$/', $input)) {
+        if (! preg_match('/^.+\@\S+$/', $email_address)) {
             // email not in correct {string}@{string} format
             throw new EmailValidationException(
                 esc_html__('Email does not have the required @ sign.', 'event_espresso')
             );
-        } else {
-            $domain = $this->getDomainPartOfEmail($input);
-            $local = $this->getLocalPartOfEmail($input);
-            $localLen = strlen($local);
-            $domainLen = strlen($domain);
-            if ($localLen < 1) {
-                //no local part
-                throw new EmailValidationException(
-                    esc_html__('Email local-part (before the @) is required.', 'event_espresso')
-                );
-            }
-            if ($localLen > 64) {
-                // local part length exceeded
-                throw new EmailValidationException(
-                    esc_html__('Email local-part (before the @) is too long.', 'event_espresso')
-                );
-            }
-            if ($domainLen < 1) {
-                throw new EmailValidationException(
-                    esc_html__('Email domain (after the @) is required.', 'event_espresso')
-                );
-            }
-            if ($domainLen > 255) {
-                // domain part length exceeded
-                throw new EmailValidationException(
-                    esc_html__('Email domain (after the @) is too long.', 'event_espresso')
-                );
-            }
-            if ($local[0] === '.') {
-                // local part starts with '.'
-                throw new EmailValidationException(
-                    esc_html__('Email local-part (before the @) must not begin with a period.', 'event_espresso')
-                );
-            }
-            if ($local[$localLen - 1] === '.') {
-                // local part starts or ends with '.'
-                throw new EmailValidationException(
-                    esc_html__('Email local-part (before the @) must not end with a period.', 'event_espresso')
-                );
-            }
-            if (preg_match('/\\.\\./', $local)) {
-                // local part has two consecutive dots
-                throw new EmailValidationException(
-                    esc_html__(
-                        'Email local-part (before the @) must not have two consecutive periods.',
-                        'event_espresso'
-                    )
-                );
-            }
-            if (preg_match('/\\.\\./', $domain)) {
-                // domain part has two consecutive dots
-                throw new EmailValidationException(
-                    esc_html__('Email domain (after the @) must not have two consecutive periods.', 'event_espresso')
-                );
-            }
+        }
+        $atIndex = $this->getAtIndex($email_address);
+        $local = $this->getLocalPartOfEmail($email_address, $atIndex);
+        $localLen = strlen($local);
+        if ($localLen < 1) {
+            //no local part
+            throw new EmailValidationException(
+                esc_html__('Email local-part (before the @) is required.', 'event_espresso')
+            );
+        }
+        if ($localLen > 64) {
+            // local part length exceeded
+            throw new EmailValidationException(
+                esc_html__('Email local-part (before the @) is too long.', 'event_espresso')
+            );
+        }
+        if ($local[0] === '.') {
+            // local part starts with '.'
+            throw new EmailValidationException(
+                esc_html__('Email local-part (before the @) must not begin with a period.', 'event_espresso')
+            );
+        }
+        if ($local[$localLen - 1] === '.') {
+            // local part starts or ends with '.'
+            throw new EmailValidationException(
+                esc_html__('Email local-part (before the @) must not end with a period.', 'event_espresso')
+            );
+        }
+        if (preg_match('/\\.\\./', $local)) {
+            // local part has two consecutive dots
+            throw new EmailValidationException(
+                esc_html__(
+                    'Email local-part (before the @) must not have two consecutive periods.',
+                    'event_espresso'
+                )
+            );
+        }
+        $domain = $this->getDomainPartOfEmail($email_address, $atIndex);
+        $domainLen = strlen($domain);
+        if ($domainLen < 1) {
+            throw new EmailValidationException(
+                esc_html__('Email domain (after the @) is required.', 'event_espresso')
+            );
+        }
+        if ($domainLen > 255) {
+            // domain part length exceeded
+            throw new EmailValidationException(
+                esc_html__('Email domain (after the @) is too long.', 'event_espresso')
+            );
+        }
+        if (preg_match('/\\.\\./', $domain)) {
+            // domain part has two consecutive dots
+            throw new EmailValidationException(
+                esc_html__('Email domain (after the @) must not have two consecutive periods.', 'event_espresso')
+            );
         }
         return true;
     }
@@ -94,27 +94,42 @@ class Basic implements EmailValidationInterface
 
 
     /**
-     * Gets the local part of the email
-     * @param $input
+     * returns the location of the @ symbol
+     *
+     * @param string $email_address
      * @return bool|string
      */
-    protected function getLocalPartOfEmail($input)
+    protected function getAtIndex($email_address)
     {
-        $atIndex = strrpos($input, '@');
-        return substr($input, 0, $atIndex);
+        return strrpos($email_address, '@');
+    }
+
+
+
+    /**
+     * Gets the local part of the email
+     *
+     * @param string $email_address
+     * @param bool|int $atIndex
+     * @return bool|string
+     */
+    protected function getLocalPartOfEmail($email_address, $atIndex)
+    {
+        return substr($email_address, 0, $atIndex);
     }
 
 
 
     /**
      * Gets the domain part of the email
-     * @param $input
+     *
+     * @param string   $email_address
+     * @param bool|int $atIndex
      * @return bool|string
      */
-    protected function getDomainPartOfEmail($input)
+    protected function getDomainPartOfEmail($email_address, $atIndex)
     {
-        $atIndex = strrpos($input, '@');
-        return substr($input, $atIndex + 1);
+        return substr($email_address, $atIndex + 1);
     }
 }
 // End of file Basic.php
