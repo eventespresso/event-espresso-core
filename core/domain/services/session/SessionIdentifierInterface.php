4635e2e2939a7edca685bb6a9d5933b47bf3b67a
Brent Christensen <brent@eventespresso.com>
2017-06-06 09:57:25 -0700

    add SessionIdentifierInterface; [touch:10788]

---
+++ core/domain/services/session/SessionIdentifierInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EventEspresso\core\domain\services\session;
+
+interface SessionIdentifierInterface
+{
+
+    /**
+     * @return    string
+     */
+    public function id();
+
+}
