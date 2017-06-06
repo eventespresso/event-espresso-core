4bff38b4fd291b54773cb40a50f4b991fde6f664
Brent Christensen <brent@eventespresso.com>
2017-06-06 10:19:10 -0700

    Improve cache uniqueness;
    add $session property and inject SessionIdentifierInterface as dependency;
    don't turn off caching if WP_DEBUG is on;
    extract logic from get() and move into new generateCacheIdentifier() method that also adds the session ID plus some SERVER details to tthe cache ID to guarantee its uniqueness; 
    add displayCacheNotice() method and use for displaying cache details in a discrete manner; [touch:10788]

--- core/services/cache/BasicCacheManager.php
+++ core/services/cache/BasicCacheManager.php
@@ -3,6 +3,7 @@ namespace EventEspresso\core\services\cache;
 
 use Closure;
 use EEH_Debug_Tools;
+use EventEspresso\core\domain\services\session\SessionIdentifierInterface;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
@@ -36,16 +37,23 @@ class BasicCacheManager implements CacheManagerInterface
      */
     private $cache_storage;
 
+    /**
+     * @var SessionIdentifierInterface $session
+     */
+    private $session;
+
 
 
     /**
      * BasicCacheManager constructor.
      *
-     * @param CacheStorageInterface $cache_storage [required]
+     * @param CacheStorageInterface      $cache_storage [required]
+     * @param SessionIdentifierInterface $session
      */
-    public function __construct(CacheStorageInterface $cache_storage)
+    public function __construct(CacheStorageInterface $cache_storage, SessionIdentifierInterface $session)
     {
         $this->cache_storage = $cache_storage;
+        $this->session = $session;
     }
 
 
@@ -83,8 +91,6 @@ class BasicCacheManager implements CacheManagerInterface
     public function get($id_prefix, $cache_id, Closure $callback, $expiration = HOUR_IN_SECONDS)
     {
         $content = '';
-        // how long should we cache this content for? 0 means no caching.
-        $expiration = ! WP_DEBUG ? $expiration : 0;
         $expiration = absint(
             apply_filters(
                 'FHEE__CacheManager__get__cache_expiration',
@@ -93,7 +99,7 @@ class BasicCacheManager implements CacheManagerInterface
                 $cache_id
             )
         );
-        $cache_id = substr($this->cachePrefix() . $id_prefix . '-' . md5($cache_id), 0, 182);
+        $cache_id = $this->generateCacheIdentifier($id_prefix, $cache_id);
         // is caching enabled for this content ?
         if ($expiration) {
             $content = $this->cache_storage->get($cache_id);
@@ -104,17 +110,38 @@ class BasicCacheManager implements CacheManagerInterface
             $content = $callback();
             // save the new content if caching is enabled
             if ($expiration) {
-                if (BasicCacheManager::DEBUG) {
-                    EEH_Debug_Tools::printr($cache_id, 'REFRESH CACHE', __FILE__, __LINE__);
-                }
                 $this->cache_storage->add($cache_id, $content, $expiration);
+                if (BasicCacheManager::DEBUG || WP_DEBUG) {
+                    $content .= $this->displayCacheNotice($cache_id, 'REFRESH CACHE');
+                }
             }
         } else {
-            if (BasicCacheManager::DEBUG) {
-                EEH_Debug_Tools::printr($cache_id, 'CACHED CONTENT', __FILE__, __LINE__);
+            if (BasicCacheManager::DEBUG || WP_DEBUG) {
+                $content .= $this->displayCacheNotice($cache_id, 'CACHED CONTENT');
             }
         }
         return $content;
+    }
+
+
+
+    /**
+     * Generates a unique identifier string for the cache
+     *
+     * @param string $id_prefix  [required] see BasicCacheManager::get()
+     * @param string $cache_id   [required] see BasicCacheManager::get()
+     * @return string
+     */
+    private function generateCacheIdentifier($id_prefix, $cache_id)
+    {
+        // let's make the cached content unique for this session
+        $cache_id .= $this->session->id();
+        // and for this "page"
+        $cache_id .= filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL);
+        // with these parameters
+        $cache_id .= filter_input(INPUT_SERVER, 'QUERY_STRING', FILTER_SANITIZE_URL);
+        // then md5 the above to control it's length, add all of our prefixes, and truncate
+        return substr($this->cachePrefix() . $id_prefix . '-' . md5($cache_id), 0, 182);
     }
 
 
@@ -133,6 +160,23 @@ class BasicCacheManager implements CacheManagerInterface
     }
 
 
+
+    /**
+     * @param array|string $cache_id [required] Could be an ID prefix affecting many caches
+     *                               or a specific ID targeting a single cache item
+     * @param string       $type
+     * @return string
+     */
+    private function displayCacheNotice($cache_id, $type) {
+        return '
+<div class="ee-cached-content-notice" style="position:fixed; bottom:0; left: 0;">
+    <p style="font-size:9px;font-weight:normal;color:#666;line-height: 12px;margin:0 0 3px 5px">
+        <b>' . $type . '</b><span style="color:#999"> : </span>
+        <span>' . $cache_id . '</span>
+        <span style="margin-left:2em;">' . __FILE__ . '</span>
+    </p>
+</div>';
+    }
 
 }
 // End of file BasicCacheManager.php
