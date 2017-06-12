382e83e6c598757ef6862fd0a962ae6f94d495dc
Brent Christensen <brent@eventespresso.com>
2017-06-12 10:31:52 -0700

    add  EE_Event_List_Query to EE_Deprecated; [touch:10792]

--- core/EE_Deprecated.core.php
+++ core/EE_Deprecated.core.php
@@ -1032,3 +1032,32 @@ add_filter(
 		);
 	}
 );
+
+
+/**
+ * Class EE_Event_List_Query
+ *
+ * @deprecated 4.9.40
+ */
+class EE_Event_List_Query
+{
+
+    /**
+     * EE_Event_List_Query constructor.
+     *
+     * @param array $args
+     */
+    public function __construct($args = array())
+    {
+        \EE_Error::doing_it_wrong(
+            __METHOD__,
+            __(
+                'Usage is deprecated. Please use \EventEspresso\core\domain\services\wp_queries\EventListQuery instead.',
+                'event_espresso'
+            ),
+            '4.9.27'
+        );
+        new \EventEspresso\core\domain\services\wp_queries\EventListQuery($args);
+    }
+
+}
