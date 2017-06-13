4412576dcac490c793b8f5026aad4ccfb0a32995
Darren Ethier <darren@roughsmootheng.in>
2017-06-13 15:55:29 -0400

    add unit test for verifying registration summary message doesn’t get sent when all registrations in transaction share same status [touch: 10808] [ci skip]

--- acceptance_tests/tests/b-TestRegistrationSummaryCept.php
+++ acceptance_tests/tests/b-TestRegistrationSummaryCept.php
@@ -43,6 +43,14 @@ $I->waitForText('Event published.');
 $event_b_link = $I->observeLinkUrlAt(EventsAdmin::EVENT_EDITOR_VIEW_LINK_AFTER_PUBLISH_SELECTOR);
 $event_b_id = $I->observeValueFromInputAt(EventsAdmin::EVENT_EDITOR_EVT_ID_SELECTOR);
 
+$I->amOnDefaultEventsListTablePage();
+$I->click(EventsAdmin::ADD_NEW_EVENT_BUTTON_SELECTOR);
+$I->fillField(EventsAdmin::EVENT_EDITOR_TITLE_FIELD_SELECTOR, 'Event RSM C');
+$I->publishEvent();
+$I->waitForText('Event published.');
+$event_c_link = $I->observeLinkUrlAt(EventsAdmin::EVENT_EDITOR_VIEW_LINK_AFTER_PUBLISH_SELECTOR);
+$event_c_id = $I->observeValueFromInputAt(EventsAdmin::EVENT_EDITOR_EVT_ID_SELECTOR);
+
 //k now that our events are setup lets do some registrations
 $I->logOut();
 $I->amOnUrl($event_a_link);
@@ -100,6 +108,51 @@ $I->verifyMatchingCountofTextInMessageActivityListTableFor(
     'admin@example.com',
     'to',
     'Registration Multi-status Summary'
+);
+
+//okay now let's do some registrations for just the first event and verify that registration multi-status summary is NOT
+//generated
+$I->amGoingTo('Register for Event RSM A and C and verify registration multi-status message type is not used for generated message');
+$I->logOut();
+$I->amOnUrl($event_a_link);
+$I->see('Event RSM A');
+$I->selectOption(TicketSelector::ticketOptionByEventIdSelector($event_a_id), '1');
+$I->click(TicketSelector::ticketSelectionSubmitSelectorByEventId($event_a_id));
+$I->waitForText('successfully added');
+$I->click('.cart-results-go-back-button');
+$I->amOnUrl($event_b_link);
+$I->see('Event RSM B');
+$I->selectOption(TicketSelector::ticketOptionByEventIdSelector($event_c_id), '1');
+$I->click(TicketSelector::ticketSelectionSubmitSelectorByEventId($event_c_id));
+$I->waitForText('successfully added');
+$I->click('.cart-results-register-button');
+$I->waitForText('Personal Information');
+$I->fillOutFirstNameFieldForAttendee('RSM Tester B');
+$I->fillOutLastNameFieldForAttendee('Guy');
+$I->fillOutEmailFieldForAttendee('rsm_testerb@example.org');
+$I->goToNextRegistrationStep();
+$I->waitForText('Congratulations', 15);
+
+$I->amGoingTo('Visit Messages Activity List Table and verify there are no Registration Multi-Status Messages for the registrations we just did');
+$I->loginAsAdmin();
+$I->amOnMessagesActivityListTablePage();
+$I->dontSee(
+    'rsm_testerb@example.org',
+    MessagesAdmin::messagesActivityListTableCellSelectorFor(
+        'to',
+        'Registration Multi-status Summary',
+        MessagesAdmin::MESSAGE_STATUS_SENT,
+        '',
+        'Primary Registrant'
+    )
+);
+$I->dontSee(
+    'admin@example.com',
+    MessagesAdmin::messagesActivityListTableCellSelectorFor(
+        'to',
+        'Registration Multi-status Summary',
+        MessagesAdmin::MESSAGE_STATUS_SENT
+    )
 );
 
 //deactivate MER plugin so its not active for future tests
