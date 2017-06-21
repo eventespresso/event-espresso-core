fddafd66cd4ffd4fd042cb5c5bd75a6bcb649b91
Brent Christensen <brent@eventespresso.com>
2017-06-21 11:02:30 -0700

    fix payment method caps registration
    unit tests revealed that caps were not being setup properly for payment method addons;
    add caps manually if regular filter is missed;
    remove caps when de-registering;
    add new getPaymentMethodCapabilities() method; [touch:10236]

--- core/libraries/plugin_api/EE_Register_Payment_Method.lib.php
+++ core/libraries/plugin_api/EE_Register_Payment_Method.lib.php
@@ -74,51 +74,77 @@ class EE_Register_Payment_Method implements EEI_Plugin_API {
 			'payment_method_paths'  => isset( $setup_args['payment_method_paths'] ) ? (array)$setup_args['payment_method_paths'] : array(),
 		);
 		// add to list of modules to be registered
-		add_filter( 'FHEE__EE_Payment_Method_Manager__register_payment_methods__payment_methods_to_register', array( 'EE_Register_Payment_Method', 'add_payment_methods' ));
-
-		/**
-		 * If EE_Payment_Method_Manager::register_payment_methods has already been called, we need it to be called again
-		 * (because it's missing the payment method we JUST registered here). We are assuming EE_Register_payment_method::register()
-		 * will be called only once per payment method from an addon, so going with that assumption we should always do this.
-		 * If that assumption is false, we should verify this newly-registered payment method isn't on the EE_Payment_Method_Manager::_payment_method_types array before calling this (this code should be changed to improve performance)
-		 */
-		if ( did_action( 'FHEE__EE_Payment_Method_Manager__register_payment_methods__registered_payment_methods' ) ) {
-			EE_Registry::instance()->load_lib('Payment_Method_Manager');
-			EE_Payment_Method_Manager::instance()->maybe_register_payment_methods( TRUE );
-		}
-	}
-
+		add_filter(
+		    'FHEE__EE_Payment_Method_Manager__register_payment_methods__payment_methods_to_register',
+            array( 'EE_Register_Payment_Method', 'add_payment_methods' )
+        );
+        // If EE_Payment_Method_Manager::register_payment_methods has already been called,
+        // then we need to add our caps for this payment method manually
+        if (did_action('FHEE__EE_Payment_Method_Manager__register_payment_methods__registered_payment_methods')) {
+            EE_Capabilities::instance()->addCaps(
+                self::getPaymentMethodCapabilities(self::$_settings[$payment_method_id])
+            );
+        }
+    }
 
 
 
-	/**
-	 * Filters the list of payment methods to add ours.
-	 * and they're just full filepaths to FOLDERS containing a payment method class file. Eg.
-	 * @param array $payment_method_folders  array of paths to all payment methods that require registering
-	 * @return array
-	 */
-	public static function add_payment_methods( $payment_method_folders ){
-		foreach( self::$_settings as $settings ) {
-			$payment_method_folders = array_merge( $payment_method_folders, $settings['payment_method_paths'] );
-		}
-		return $payment_method_folders;
-	}
+    /**
+     * Filters the list of payment methods to add ours.
+     * and they're just full filepaths to FOLDERS containing a payment method class file. Eg.
+     *
+     * @param array $payment_method_folders array of paths to all payment methods that require registering
+     * @return array
+     */
+    public static function add_payment_methods($payment_method_folders)
+    {
+        foreach (self::$_settings as $settings) {
+            foreach ($settings['payment_method_paths'] as $payment_method_path) {
+                $payment_method_folders[] = $payment_method_path;
+            }
+        }
+        return $payment_method_folders;
+    }
 
 
 
-	/**
-	 * This deregisters a module that was previously registered with a specific $module_id.
-	 *
-	 * @since    4.3.0
-	 *
-	 * @param string  $module_id the name for the module that was previously registered
-	 * @return void
-	 */
+    /**
+     * This deregisters a module that was previously registered with a specific $module_id.
+     *
+     * @since    4.3.0
+     *
+     * @param string $module_id the name for the module that was previously registered
+     * @return void
+     * @throws \EE_Error
+     */
 	public static function deregister( $module_id = NULL ) {
 		if ( isset( self::$_settings[ $module_id ] )) {
-			unset( self::$_settings[ $module_id ] );
+            EE_Capabilities::instance()->removeCaps(
+                self::getPaymentMethodCapabilities(self::$_settings[$module_id])
+            );
+            unset( self::$_settings[ $module_id ] );
 		}
 	}
+
+
+
+    /**
+     * returns an array of the caps that get added when a Payment Method is registered
+     *
+     * @param array $settings
+     * @return array
+     */
+    private static function getPaymentMethodCapabilities(array $settings)
+    {
+        $payment_method_caps = array('administrator' => array());
+        if (isset($settings['payment_method_paths'])) {
+            foreach ($settings['payment_method_paths'] as $payment_method_path) {
+                $payment_method_caps['administrator'][] = 'ee_payment_method_'
+                                                          . strtolower(basename($payment_method_path));
+            }
+        }
+        return $payment_method_caps;
+    }
 
 }
 // End of file EE_Register_Payment_Method.lib.php
