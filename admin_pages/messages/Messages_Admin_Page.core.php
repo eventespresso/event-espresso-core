79cfb2672bb5cb5d718d40044a67f032123f9f92
Darren Ethier <darren@roughsmootheng.in>
2017-06-27 12:03:39 -0400

    remove bulk actions from global message template list table and add for custom message template list table [touch: 10636]

--- admin_pages/messages/Messages_Admin_Page.core.php
+++ admin_pages/messages/Messages_Admin_Page.core.php
@@ -846,10 +846,19 @@ class Messages_Admin_Page extends EE_Admin_Page
                 'slug'        => 'in_use',
                 'label'       => __('In Use', 'event_espresso'),
                 'count'       => 0,
-                'bulk_action' => array(
-                    'trash_message_template' => __('Move to Trash', 'event_espresso')
-                )
             )
+        );
+    }
+
+
+    /**
+     * Set views array for the Custom Template List Table
+     */
+    public function _set_list_table_views_custom_mtps()
+    {
+        $this->_set_list_table_views_global_mtps();
+        $this->_views['in_use']['bulk_action'] = array(
+                'trash_message_template' => esc_html__('Move to Trash', 'event_espresso')
         );
     }
     
