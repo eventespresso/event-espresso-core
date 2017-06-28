3f70af4354340f00c244672959e9a5210348d84e
Darren Ethier <darren@roughsmootheng.in>
2017-06-28 09:40:56 -0400

    improve testing to account for different currency setup as well [touch: 10817]

--- acceptance_tests/tests/d-TestEventEditorCept.php
+++ acceptance_tests/tests/d-TestEventEditorCept.php
@@ -16,3 +16,26 @@ $I->see('$32.50', EventsAdmin::eventEditorTicketAdvancedDetailsSubtotalSelector(
 $I->toggleTicketIsTaxableForTicketRow();
 $I->see('$4.88', EventsAdmin::eventEditorTicketTaxAmountDisplayForTaxIdAndTicketRowSelector());
 $I->see('$37.38', EventsAdmin::eventEditorTicketAdvancedDetailsTotalSelector());
+$I->saveEvent();
+
+$I->amGoingTo('Test the same ticket total calculations via js in the ticket editor but with this format: 1.000,00');
+$I->amOnCountrySettingsAdminPage();
+$I->setCurrencyDecimalMarkTo(',');
+$I->setCurrencyThousandsSeparatorTo('.');
+$I->saveCountrySettings();
+$I->amOnDefaultEventsListTablePage();
+$I->click(EventsAdmin::ADD_NEW_EVENT_BUTTON_SELECTOR);
+$I->fillField(EventsAdmin::eventEditorTicketPriceFieldSelector(), '32,50');
+$I->toggleAdvancedSettingsViewForTicketRow();
+$I->waitForElementVisible(EventsAdmin::eventEditorTicketAdvancedDetailsSubtotalSelector());
+$I->see('$32,50', EventsAdmin::eventEditorTicketAdvancedDetailsSubtotalSelector());
+$I->toggleTicketIsTaxableForTicketRow();
+$I->see('$4,88', EventsAdmin::eventEditorTicketTaxAmountDisplayForTaxIdAndTicketRowSelector());
+$I->see('$37,38', EventsAdmin::eventEditorTicketAdvancedDetailsTotalSelector());
+
+//restore country settings to original
+$I->saveEvent();
+$I->amOnCountrySettingsAdminPage();
+$I->setCurrencyDecimalMarkTo('.');
+$I->setCurrencyThousandsSeparatorTo(',');
+$I->saveCountrySettings();
