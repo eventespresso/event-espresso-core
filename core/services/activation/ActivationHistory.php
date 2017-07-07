986e65084721df65d779f8227ca215cdf2839f17
Brent Christensen <brent@eventespresso.com>
2017-07-07 08:14:56 -0700

    change properties to protected so they can be accessed by mocks; add setters for properties and move that logic out of constructor; use $version_history property  instead of local  variable; ensure version history array is set for current version; formatting; [touch:10766]

--- core/services/activation/ActivationHistory.php
+++ core/services/activation/ActivationHistory.php
@@ -2,6 +2,8 @@
 
 namespace EventEspresso\core\services\activation;
 
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+
 defined('EVENT_ESPRESSO_VERSION') || exit('No direct script access allowed');
 
 
@@ -28,22 +30,22 @@ class ActivationHistory
     /**
      * @var string $activation_history_option_name
      */
-    private $activation_history_option_name;
+    protected $activation_history_option_name;
 
     /**
      * @var string $activation_indicator_option_name
      */
-    private $activation_indicator_option_name;
+    protected $activation_indicator_option_name;
 
     /**
      * @var string $current_version
      */
-    private $current_version;
+    protected $current_version;
 
     /**
      * @var array|bool $version_history
      */
-    private $version_history;
+    protected $version_history;
 
 
 
@@ -53,23 +55,79 @@ class ActivationHistory
      * @param string $activation_history_option_name
      * @param string $activation_indicator_option_name
      * @param string $current_version
+     * @throws InvalidDataTypeException
      */
     public function __construct(
         $activation_history_option_name = '',
         $activation_indicator_option_name = '',
         $current_version = ''
     ) {
+        $this->setActivationHistoryOptionName($activation_history_option_name);
+        $this->setActivationIndicatorOptionName($activation_indicator_option_name);
+        $this->setCurrentVersion($current_version);
+        $this->version_history = get_option($this->activation_history_option_name);
+    }
+
+
+
+    /**
+     * @param string $activation_history_option_name
+     * @throws InvalidDataTypeException
+     */
+    protected function setActivationHistoryOptionName($activation_history_option_name)
+    {
+        if(! is_string($activation_history_option_name)) {
+            throw new InvalidDataTypeException(
+                '$activation_history_option_name',
+                $activation_history_option_name,
+                'string'
+            );
+        }
         $this->activation_history_option_name = $activation_history_option_name !== ''
             ? $activation_history_option_name
             : ActivationHistory::EE_ACTIVATION_HISTORY_OPTION_NAME;
+    }
+
+
+
+    /**
+     * @param string $activation_indicator_option_name
+     * @throws InvalidDataTypeException
+     */
+    protected function setActivationIndicatorOptionName($activation_indicator_option_name)
+    {
+        if (! is_string($activation_indicator_option_name)) {
+            throw new InvalidDataTypeException(
+                '$activation_indicator_option_name',
+                $activation_indicator_option_name,
+                'string'
+            );
+        }
         $this->activation_indicator_option_name = $activation_indicator_option_name !== ''
             ? $activation_indicator_option_name
             : ActivationHistory::EE_ACTIVATION_INDICATOR_OPTION_NAME;
+    }
+
+
+
+    /**
+     * @param string $current_version
+     * @throws InvalidDataTypeException
+     */
+    protected function setCurrentVersion($current_version)
+    {
+        if (! is_string($current_version)) {
+            throw new InvalidDataTypeException(
+                '$current_version',
+                $current_version,
+                'string'
+            );
+        }
         $this->current_version = $current_version !== ''
             ? $current_version
             : espresso_version();
-        $this->version_history = get_option($this->activation_history_option_name);
     }
+
 
 
 
@@ -103,13 +161,12 @@ class ActivationHistory
      */
     public function getMostRecentActiveVersion()
     {
-        $version_history = $this->version_history;
         $most_recently_active_version_activation = '1970-01-01 00:00:00';
         $most_recently_active_version = '0.0.0.dev.000';
-        if (is_array($version_history)) {
-            foreach ($version_history as $version => $times_activated) {
-                //check there is a record of when this version was activated. Otherwise,
-                //mark it as unknown
+        if (is_array($this->version_history)) {
+            foreach ($this->version_history as $version => $times_activated) {
+                // check there is a record of when this version was activated.
+                // Otherwise, mark it as unknown
                 if (! $times_activated) {
                     $times_activated = array('unknown-date');
                 }
@@ -117,9 +174,9 @@ class ActivationHistory
                     $times_activated = array($times_activated);
                 }
                 foreach ($times_activated as $an_activation) {
-                    if ($an_activation !== 'unknown-date'
-                        && $an_activation
-                           > $most_recently_active_version_activation
+                    if (
+                        $an_activation !== 'unknown-date'
+                        && $an_activation > $most_recently_active_version_activation
                     ) {
                         $most_recently_active_version = $version;
                         $most_recently_active_version_activation = $an_activation === 'unknown-date'
@@ -148,6 +205,9 @@ class ActivationHistory
         }
         if ($current_version !== '') {
             $this->current_version = $current_version;
+        }
+        if(! isset($this->version_history[$this->current_version])) {
+            $this->version_history[$this->current_version] =   array();
         }
         $this->version_history[ $this->current_version ][] = date('Y-m-d H:i:s', time());
         return update_option(
