f78004f521a850966c2f015bb71be8f78a0176b3
Brent Christensen <brent@eventespresso.com>
2017-07-07 09:45:50 -0700

    RequestTypeDetectorExtendedMock; [touch:10766]

---
+++ tests/mocks/core/services/request/RequestTypeDetectorExtendedMock.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace EventEspresso\tests\mocks\core\services\request;
+
+use EventEspresso\core\services\activation\RequestType;
+use EventEspresso\core\services\activation\RequestTypeDetector;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+class RequestTypeDetectorExtendedMock extends RequestTypeDetector
+{
+
+    /**
+     * @param RequestType $request_type
+     */
+    public function setRequestType(\EventEspresso\core\services\activation\RequestType $request_type)
+    {
+        $this->request_type = $request_type;
+    }
+
+}
