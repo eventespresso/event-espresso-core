59e6a6020261cec9ff544ce7d065b5de7911c2de
Brent Christensen <brent@eventespresso.com>
2017-07-07 09:19:02 -0700

    implement ActivatableInterface; update references to moved logic; extract all activation logic and move into new classes, and deprecate old functions; use esc_html__(); formatting; [touch:10766]

--- core/EE_Addon.core.php
+++ core/EE_Addon.core.php
@@ -4,11 +4,11 @@ use EventEspresso\core\domain\DomainInterface;
 use EventEspresso\core\domain\RequiresDependencyMapInterface;
 use EventEspresso\core\domain\RequiresDomainInterface;
 use EventEspresso\core\exceptions\InvalidDataTypeException;
-use EventEspresso\core\exceptions\InvalidInterfaceException;
+use EventEspresso\core\services\activation\ActivatableInterface;
 use EventEspresso\core\services\activation\ActivationHistory;
 use EventEspresso\core\services\loaders\LoaderFactory;
-use EventEspresso\core\services\request\RequestType;
 use EventEspresso\core\services\request\sanitizers\AllowedTags;
+use EventEspresso\core\services\activation\RequestType;
 
 /**
  * Class EE_Addon
@@ -18,7 +18,8 @@ use EventEspresso\core\services\request\sanitizers\AllowedTags;
  * @subpackage            core
  * @author                Michael Nelson, Brent Christensen
  */
-abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMapInterface, RequiresDomainInterface
+abstract class EE_Addon extends EE_Configurable
+    implements ActivatableInterface, RequiresDependencyMapInterface, RequiresDomainInterface
 {
     /**
      * prefix to be added onto an addon's plugin slug to make a wp option name
@@ -34,16 +35,14 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
     /**
      * A non-internationalized name to identify this addon. Eg 'Calendar','MailChimp',etc/
      *
-     * @type string _addon_name
+     * @var string
      */
     protected $_addon_name = '';
-
 
     /**
      * @var EE_Dependency_Map $dependency_map
      */
     private $dependency_map;
-
 
     /**
      * @var DomainInterface $domain
@@ -53,28 +52,26 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
     /**
      * filepath to the main file, which can be used for register_activation_hook, register_deactivation_hook, etc.
      *
-     * @type string
+     * @var string
      */
     protected $_main_plugin_file;
 
-
     /**
-     * @var $_min_core_version
-     * @type string
+     * @var string
      */
     protected $_min_core_version = '';
 
     /**
      * page slug to be used when generating the "Settings" link on the WP plugin page
      *
-     * @type string $_plugin_action_slug
+     * @var string
      */
     protected $_plugin_action_slug = '';
 
     /**
      * derived from plugin 'main_file_path using plugin_basename()
      *
-     * @type string $_plugin_basename
+     * @var string
      */
     protected $_plugin_basename = '';
 
@@ -82,39 +79,38 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
      * if not empty, inserts a new table row after this plugin's row on the WP Plugins page
      * that can be used for adding upgrading/marketing info
      *
-     * @type array $_plugins_page_row
+     * @var array
      */
     protected $_plugins_page_row = [];
 
     /**
      * A non-internationalized name to identify this addon for use in URLs, etc
      *
-     * @type string $_plugin_slug
+     * @var string
      */
     protected $_plugin_slug = '';
 
     /**
      *    This is the slug used to identify this add-on within the plugin update engine.
      *
-     * @type string
+     * @var string
      */
     protected $pue_slug;
 
     /**
      * one of the EE_System::req_type_* constants
      *
-     * @type int $_req_type
+     * @var int
      */
     protected $_req_type;
 
     /**
-     * @var RequestType $request_type
+     * @var RequestType
      */
     private $request_type;
 
     /**
-     * @var $_version
-     * @type string
+     * @var string
      */
     protected $_version = '';
 
@@ -298,7 +294,6 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
      */
     public function get_plugins_page_row(): array
     {
-
         return $this->_plugins_page_row;
     }
 
@@ -334,124 +329,6 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
 
     /**
-     * Takes care of double-checking that we're not in maintenance mode, and then
-     * initializing this addon's necessary initial data. This is called by default on new activations
-     * and reactivations.
-     *
-     * @param boolean|string $verify_schema whether to verify the database's schema for this addon, or just its data.
-     *                                      This is a resource-intensive job so we prefer to only do it when necessary
-     * @return void
-     * @throws EE_Error
-     * @throws InvalidInterfaceException
-     * @throws InvalidDataTypeException
-     * @throws InvalidArgumentException
-     * @throws ReflectionException
-     */
-    public function initialize_db_if_no_migrations_required($verify_schema = true)
-    {
-        if ($verify_schema === '') {
-            // wp core bug imo: if no args are passed to `do_action('some_hook_name')` besides the hook's name
-            // (ie, no 2nd or 3rd arguments), instead of calling the registered callbacks with no arguments, it
-            // calls them with an argument of an empty string (ie ""), which evaluates to false
-            // so we need to treat the empty string as if nothing had been passed, and should instead use the default
-            $verify_schema = true;
-        }
-        if (EE_Maintenance_Mode::instance()->level() !== EE_Maintenance_Mode::level_2_complete_maintenance) {
-            if ($verify_schema) {
-                $this->initialize_db();
-            }
-            $this->initialize_default_data();
-            // @todo: this will probably need to be adjusted in 4.4 as the array changed formats I believe
-            EE_Data_Migration_Manager::instance()->update_current_database_state_to(
-                [
-                    'slug'    => $this->name(),
-                    'version' => $this->version(),
-                ]
-            );
-            /* make sure core's data is a-ok
-             * (at the time of writing, we especially want to verify all the caps are present
-             * because payment method type capabilities are added dynamically, and it's
-             * possible this addon added a payment method. But it's also possible
-             * other data needs to be verified)
-             */
-            EEH_Activation::initialize_db_content();
-            /** @var EventEspresso\core\domain\services\custom_post_types\RewriteRules $rewrite_rules */
-            $rewrite_rules = LoaderFactory::getLoader()->getShared(
-                'EventEspresso\core\domain\services\custom_post_types\RewriteRules'
-            );
-            $rewrite_rules->flushRewriteRules();
-            // in case there are lots of addons being activated at once, let's force garbage collection
-            // to help avoid memory limit errors
-            // EEH_Debug_Tools::instance()->measure_memory( 'db content initialized for ' . get_class( $this), true );
-            gc_collect_cycles();
-        } else {
-            // ask the data migration manager to init this addon's data
-            // when migrations are finished because we can't do it now
-            EE_Data_Migration_Manager::instance()->enqueue_db_initialization_for($this->name());
-        }
-    }
-
-
-    /**
-     * Used to setup this addon's database tables, but not necessarily any default
-     * data in them. The default is to actually use the most up-to-date data migration script
-     * for this addon, and just use its schema_changes_before_migration() and schema_changes_after_migration()
-     * methods to setup the db.
-     *
-     * @throws EE_Error
-     * @throws ReflectionException
-     */
-    public function initialize_db()
-    {
-        // find the migration script that sets the database to be compatible with the code
-        $current_dms_name = EE_Data_Migration_Manager::instance()->get_most_up_to_date_dms($this->name());
-        if ($current_dms_name) {
-            $current_data_migration_script = EE_Registry::instance()->load_dms($current_dms_name);
-            $current_data_migration_script->set_migrating(false);
-            $current_data_migration_script->schema_changes_before_migration();
-            $current_data_migration_script->schema_changes_after_migration();
-            if ($current_data_migration_script->get_errors()) {
-                foreach ($current_data_migration_script->get_errors() as $error) {
-                    EE_Error::add_error($error, __FILE__, __FUNCTION__, __LINE__);
-                }
-            }
-        }
-        // if not DMS was found that should be ok. This addon just doesn't require any database changes
-        EE_Data_Migration_Manager::instance()->update_current_database_state_to(
-            [
-                'slug'    => $this->name(),
-                'version' => $this->version(),
-            ]
-        );
-    }
-
-
-    /**
-     * If you want to setup default data for the addon, override this method, and call
-     * parent::initialize_default_data() from within it. This is normally called
-     * from EE_Addon::initialize_db_if_no_migrations_required(), just after EE_Addon::initialize_db()
-     * and should verify default data is present (but this is also called
-     * on reactivations and just after migrations, so please verify you actually want
-     * to ADD default data, because it may already be present).
-     * However, please call this parent (currently it just fires a hook which other
-     * addons may be depending on)
-     */
-    public function initialize_default_data()
-    {
-        /**
-         * Called when an addon is ensuring its default data is set (possibly called
-         * on a reactivation, so first check for the absence of other data before setting
-         * default data)
-         *
-         * @param EE_Addon $addon the addon that called this
-         */
-        do_action('AHEE__EE_Addon__initialize_default_data__begin', $this);
-        // override to insert default data. It is safe to use the models here
-        // because the site should not be in maintenance mode
-    }
-
-
-    /**
      * Gets the name of the wp option that stores the activation history
      * of this addon
      *
@@ -460,18 +337,6 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
     public function get_activation_history_option_name(): string
     {
         return self::ee_addon_version_history_option_prefix . $this->name();
-    }
-
-
-    /**
-     * Gets the wp option which stores the activation history for this addon
-     *
-     * @return array
-     */
-    public function get_activation_history(): array
-    {
-        $this->setup_activation_history();
-        return $this->activation_history->getVersionHistory();
     }
 
 
@@ -587,20 +452,21 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
             $link_url    = $plugins_page_row['link_url'] ?? '';
             $description = $plugins_page_row['description'] ?? '';
             if (! empty($link_text) && ! empty($link_url) && ! empty($description)) {
-                $after_plugin_row .= '<tr id="' . sanitize_title($plugin_file) . '-ee-addon" class="' . $class . '">';
-                $after_plugin_row .= '<th class="check-column" scope="row"></th>';
-                $after_plugin_row .= '<td class="ee-addon-upsell-info-title-td plugin-title column-primary">';
-                $after_plugin_row .= '<p class="ee-addon-upsell-info-dv">
-	                <a class="ee-button" href="' . esc_url_raw($link_url) . '">'
-                    . $link_text
-                    . ' &nbsp;<span class="dashicons dashicons-arrow-right-alt2" style="margin:0;"></span>'
-                    . '</a>
-                </p>';
-                $after_plugin_row .= '</td>';
-                $after_plugin_row .= '<td class="ee-addon-upsell-info-desc-td column-description desc">';
-                $after_plugin_row .= $description;
-                $after_plugin_row .= '</td>';
-                $after_plugin_row .= '</tr>';
+                $after_plugin_row .= '
+                <tr id="' . sanitize_title($plugin_file) . '-ee-addon" class="' . $class . '">
+                    <th class="check-column" scope="row"></th>
+                    <td class="ee-addon-upsell-info-title-td plugin-title column-primary">
+                        <p class="ee-addon-upsell-info-dv">
+                            <a class="ee-button" href="' . esc_url_raw($link_url) . '">
+                                ' . $link_text . ' &nbsp;
+                                <span class="dashicons dashicons-arrow-right-alt2" style="margin:0;"></span>
+                            </a>
+                        </p>
+                    </td>
+                    <td class="ee-addon-upsell-info-desc-td column-description desc">
+                        ' . $description . '
+                    </td>
+                </tr>';
             } else {
                 $after_plugin_row .= $description;
             }
@@ -658,6 +524,7 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
      * ensures the activation history property is set
      *
      * @return void
+     * @throws InvalidDataTypeException
      */
     public function setup_activation_history()
     {
@@ -677,6 +544,7 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
      * Gets the ActivationHistory object for this addon
      *
      * @return ActivationHistory
+     * @throws InvalidDataTypeException
      */
     public function getActivationHistory(): ActivationHistory
     {
@@ -717,8 +585,20 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
 
     /**
+     * @return array
+     * @throws InvalidDataTypeException
+     * @deprecated $VID:$
+     */
+    public function get_activation_history(): array
+    {
+        $this->setup_activation_history();
+        return $this->activation_history->getVersionHistory();
+    }
+
+
+    /**
      * @return void
-     * @deprecated 4.9.40
+     * @deprecated $VID:$
      */
     public function new_install()
     {
@@ -727,7 +607,7 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
     /**
      * @return void
-     * @deprecated 4.9.40
+     * @deprecated $VID:$
      */
     public function reactivation()
     {
@@ -736,7 +616,7 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
     /**
      * @return void
-     * @deprecated 4.9.40
+     * @deprecated $VID:$
      */
     public function upgrade()
     {
@@ -744,7 +624,7 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
 
     /**
-     * @deprecated 4.9.40
+     * @deprecated $VID:$
      */
     public function downgrade()
     {
@@ -763,11 +643,12 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
     {
         EE_Error::doing_it_wrong(
             __FUNCTION__,
-            __(
+            esc_html__(
                 'EE_Addon::set_db_update_option_name was renamed to EE_Addon::set_activation_indicator_option',
                 'event_espresso'
             ),
-            '4.3.0.alpha.016'
+            '4.3.0.alpha.016',
+            '5.0.0'
         );
         //let's just handle this on the next request, ok? right now we're just not really ready
         return $this->set_activation_indicator_option();
@@ -786,11 +667,12 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
     {
         EE_Error::doing_it_wrong(
             __FUNCTION__,
-            __(
+            esc_html__(
                 'EE_Addon::get_db_update_option was renamed to EE_Addon::get_activation_indicator_option_name',
                 'event_espresso'
             ),
-            '4.3.0.alpha.016'
+            '4.3.0.alpha.016',
+            '5.0.0'
         );
         return $this->get_activation_indicator_option_name();
     }
@@ -810,7 +692,8 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
     /**
      * @return bool
-     * @deprecated 4.9.40
+     * @throws InvalidDataTypeException
+     * @deprecated $VID:$
      */
     public function set_activation_indicator_option(): bool
     {
@@ -821,7 +704,8 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
     /**
      * @param int $req_type
-     * @deprecated 4.9.40
+     * @deprecated 4.9.44
+     * @deprecated $VID:$
      */
     public function set_req_type(int $req_type)
     {
@@ -829,17 +713,18 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
 
 
     /**
-     * @deprecated 4.9.40
+     * @deprecated 4.9.44
      */
     public function detect_req_type(): int
     {
-        return $this->getRequestType()->requestType();
+        return $this->getRequestType()->getRequestType();
     }
 
 
     /**
      * @return void
-     * @deprecated 4.9.40
+     * @deprecated 4.9.44
+     * @deprecated $VID:$
      */
     public function detect_activation_or_upgrade()
     {
@@ -850,13 +735,72 @@ abstract class EE_Addon extends EE_Configurable implements RequiresDependencyMap
      * @param array|null  $version_history
      * @param string|null $current_version
      * @return boolean success
-     * @deprecated 4.9.40
+     * @throws InvalidDataTypeException
+     * @deprecated $VID:$
      */
-    public function update_list_of_installed_versions(array $version_history = null, string $current_version = null): bool
+    public function update_list_of_installed_versions(array $version_history = null,
+                                                      string $current_version = null): bool
     {
         $this->setup_activation_history();
-        return $this->activation_history->updateActivationHistory($version_history, $current_version);
+        return $this->activation_history->updateActivationHistory(
+            $version_history,
+            $current_version
+        );
     }
 
 
+    /**
+     * @param boolean $verify_schema
+     * @return void
+     * @deprecated 4.9.44
+     */
+    public function initialize_db_if_no_migrations_required($verify_schema = true)
+    {
+        EE_Error::doing_it_wrong(
+            __FUNCTION__,
+            sprintf(
+                esc_html__('The logic in "%1$s" was moved to "%2$s"', 'event_espresso'),
+                __METHOD__ . '()',
+                'EventEspresso\core\services\activation\InitializeAddon::initialize'
+            ),
+            '4.9.44',
+            '5.0.0'
+        );
+    }
+
+
+    /**
+     * @deprecated 4.9.44
+     */
+    public function initialize_db()
+    {
+        EE_Error::doing_it_wrong(
+            __FUNCTION__,
+            sprintf(
+                esc_html__('The logic in "%1$s" was moved to "%2$s"', 'event_espresso'),
+                __METHOD__ . '()',
+                'EventEspresso\core\services\activation\InitializeAddon::initializeDatabase'
+            ),
+            '4.9.44',
+            '5.0.0'
+        );
+    }
+
+
+    /**
+     * @deprecated 4.9.44
+     */
+    public function initialize_default_data()
+    {
+        EE_Error::doing_it_wrong(
+            __FUNCTION__,
+            sprintf(
+                esc_html__('The logic in "%1$s" was moved to "%2$s"', 'event_espresso'),
+                __METHOD__ . '()',
+                'EventEspresso\core\services\activation\InitializeAddon::initializeDefaultData'
+            ),
+            '4.9.44',
+            '5.0.0'
+        );
+    }
 }
