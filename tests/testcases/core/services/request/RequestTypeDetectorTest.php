9bb6434d605350d87e742b1e6297c58177540c0d
Brent Christensen <brent@eventespresso.com>
2017-07-07 09:46:49 -0700

    RequestTypeTest; [touch:10766]

---
+++ tests/testcases/core/services/request/RequestTypeDetectorTest.php
@@ -0,0 +1,204 @@
+<?php
+use EventEspresso\core\services\activation\RequestType;
+use EventEspresso\tests\includes\ActivationTestsHelper;
+use EventEspresso\tests\mocks\core\services\activation\ActivationHistoryExtendedMock;
+use EventEspresso\tests\mocks\core\services\request\RequestTypeDetectorExtendedMock;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * RequestTypeDetectorTest
+ * Tests \EventEspresso\core\services\activation\RequestTypeDetector
+ *
+ * @package    EventEspresso\tests
+ * @subpackage \testcases
+ * @author     Brent Christenson
+ * @since      4.9.44
+ * @group      Activation
+ */
+class RequestTypeDetectorTest extends EE_UnitTestCase
+{
+
+
+
+    public function setUp()
+    {
+        parent::setUp();
+        delete_option(ActivationHistoryExtendedMock::EE_ACTIVATION_HISTORY_OPTION_NAME);
+        delete_option(ActivationHistoryExtendedMock::EE_ACTIVATION_INDICATOR_OPTION_NAME);
+    }
+
+
+
+    public function testGetRequestTypeNotSet()
+    {
+        /** @var ActivationHistoryExtendedMock $activation_history */
+        $activation_history = ActivationTestsHelper::getActivationHistory();
+        $detector = new RequestTypeDetectorExtendedMock($activation_history);
+        PHPUnit_Framework_TestCase::assertNull($detector->getRequestType());
+    }
+
+
+
+    public function testGetRequestType()
+    {
+        /** @var ActivationHistoryExtendedMock $activation_history */
+        $activation_history = ActivationTestsHelper::getActivationHistory();
+        $detector = new RequestTypeDetectorExtendedMock($activation_history);
+        $detector->setRequestType(new RequestType());
+        PHPUnit_Framework_TestCase::assertInstanceOf(
+            'EventEspresso\core\services\activation\RequestType',
+            $detector->getRequestType()
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryNewActivation()
+    {
+        /** @var ActivationHistoryExtendedMock $activation_history */
+        $activation_history = ActivationTestsHelper::getActivationHistory();
+        $version_history = get_option(ActivationHistoryExtendedMock::EE_ACTIVATION_HISTORY_OPTION_NAME, false);
+        PHPUnit_Framework_TestCase::assertFalse($version_history);
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType($activation_history),
+            RequestType::NEW_ACTIVATION,
+            false
+        );
+    }
+
+
+
+    /**
+     * @group thisTest
+     */
+    public function testResolveFromActivationHistoryNormalRequest()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForNormalRequest()
+            ),
+            RequestType::NORMAL,
+            false
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryUpgradeRequest()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForUpgradeRequest()
+            ),
+            RequestType::UPGRADE,
+            true
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryDowngradeRequest()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForDowngradeRequest()
+            ),
+            RequestType::DOWNGRADE,
+            true
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryReActivation()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForReActivation()
+            ),
+            RequestType::REACTIVATION,
+            false
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryUpgradeActivation()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForUpgradeActivation()
+            ),
+            RequestType::UPGRADE,
+            true
+        );
+    }
+
+
+    public function testResolveFromActivationHistoryDowngradeActivation()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForDowngradeActivation()
+            ),
+            RequestType::DOWNGRADE,
+            true
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryNewVersionUpgrade()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForNewVersionUpgrade()
+            ),
+            RequestType::UPGRADE,
+            true
+        );
+    }
+
+
+
+    public function testResolveFromActivationHistoryNewVersionDowngrade()
+    {
+        $this->assertActivationDetails(
+            ActivationTestsHelper::getAndDetectRequestType(
+                ActivationTestsHelper::getActivationHistoryForNewVersionDowngrade()
+            ),
+            RequestType::DOWNGRADE,
+            true
+        );
+    }
+
+
+
+    /**
+     * @param RequestType $request_type
+     * @param int         $expected_request_type
+     * @param bool        $isMajorVersionChange
+     * @throws PHPUnit_Framework_Exception
+     */
+    private function assertActivationDetails($request_type, $expected_request_type, $isMajorVersionChange)
+    {
+        PHPUnit_Framework_TestCase::assertInstanceOf(
+            'EventEspresso\core\services\activation\RequestType',
+            $request_type
+        );
+        PHPUnit_Framework_TestCase::assertEquals(
+            $isMajorVersionChange,
+            $request_type->isMajorVersionChange()
+        );
+        PHPUnit_Framework_TestCase::assertEquals(
+            $expected_request_type,
+            $request_type->getRequestType()
+        );
+    }
+
+
+}
+// Location: testcases/core/services/request/RequestTypeDetectorTest.php
