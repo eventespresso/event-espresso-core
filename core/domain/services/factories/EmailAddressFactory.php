66b96c623faabe025cac6c10e9c565d1fff1649e
Brent Christensen <brent@eventespresso.com>
2017-07-08 13:19:18 -0700

    fix EmailAddress creation
    don't use Loader to generate EmailAddress because it throws off validation if email address string is empty, because Refelction fails to find default value; [touch:9222]

--- core/domain/services/factories/EmailAddressFactory.php
+++ core/domain/services/factories/EmailAddressFactory.php
@@ -2,6 +2,7 @@
 
 namespace EventEspresso\core\domain\services\factories;
 
+use EventEspresso\core\domain\services\validation\email\EmailValidationException;
 use EventEspresso\core\domain\values\EmailAddress;
 use EventEspresso\core\exceptions\InvalidDataTypeException;
 use EventEspresso\core\exceptions\InvalidInterfaceException;
@@ -25,15 +26,18 @@ class EmailAddressFactory implements FactoryInterface
     /**
      * @param string $email_address
      * @return EmailAddress
+     * @throws EmailValidationException
      * @throws InvalidDataTypeException
      * @throws InvalidInterfaceException
      * @throws InvalidArgumentException
      */
-    public static function create($email_address = '')
+    public static function create($email_address)
     {
-        return LoaderFactory::getLoader()->getNew(
-            'EventEspresso\core\domain\values\EmailAddress',
-            array($email_address)
+        return new EmailAddress(
+            $email_address,
+            LoaderFactory::getLoader()->getNew(
+                'EventEspresso\core\domain\services\validation\email\EmailValidatorInterface'
+            )
         );
     }
 
