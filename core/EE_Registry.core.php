e4a0c9ed4b7ae3fc09f5ffce686c9ee4b2454e82
Brent Christensen <brent@eventespresso.com>
2017-07-20 20:09:48 -0700

    fix resetting;
    remove shortcodes property from reset process since shortcodes do not have a reset() method, nor are they singletons, nor do they implement any of the new interfaces;
    remove static $count variable in _reset_and_unset_object() since it is not used;
    don't unset anything that's not an object;
    still reset modules, but don't unset them; [touch:10889]

--- core/EE_Registry.core.php
+++ core/EE_Registry.core.php
@@ -1295,7 +1295,7 @@ class EE_Registry implements ResettableInterface
         //messages reset
         EED_Messages::reset();
         //handle of objects cached on LIB
-        foreach (array('LIB', 'modules', 'shortcodes') as $cache) {
+        foreach (array('LIB', 'modules') as $cache) {
             foreach ($instance->{$cache} as $class_name => $class) {
                 if (EE_Registry::_reset_and_unset_object($class, $reset_models)) {
                     unset($instance->{$cache}->{$class_name});
@@ -1318,8 +1318,15 @@ class EE_Registry implements ResettableInterface
      */
     private static function _reset_and_unset_object($object, $reset_models)
     {
-        static $count = 0;
-        $count++;
+        if (! is_object($object)) {
+            // don't unset anything that's not an object
+            return false;
+        }
+        if ($object instanceof EED_Module) {
+            $object::reset();
+            // don't unset modules
+            return false;
+        }
         if ($object instanceof ResettableInterface) {
             if ($object instanceof EEM_Base) {
                 if ($reset_models) {
