b39fb9868beca1df2d8e9bad4a367ecbc1e121fd
Darren Ethier <darren@roughsmootheng.in>
2017-07-26 17:47:53 -0400

    listen for step value and send it to the display strategy for number inputs [touch: 10896]

--- core/libraries/form_sections/inputs/EE_Float_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Float_Input.input.php
@@ -12,10 +12,19 @@ class EE_Float_Input extends EE_Form_Input_Base{
 	 * @param array $input_settings
 	 */
 	function __construct($input_settings = array()){
-		$this->_set_display_strategy(new EE_Number_Input_Display_Strategy());
+		$this->_set_display_strategy(new EE_Number_Input_Display_Strategy(
+            isset($input_settings['min_value'])
+                ? $input_settings['min_value']
+                : null,
+            isset($input_settings['max_value'])
+                ? $input_settings['max_value']
+                : null,
+            isset($input_settings['step_value'])
+                ? $input_settings['step_value']
+                : null
+        ));
 		$this->_set_normalization_strategy(new EE_Float_Normalization( isset( $input_settings[ 'validation_error_message' ] ) ? $input_settings[ 'validation_error_message' ] : NULL ) );
 		$this->_add_validation_strategy( new EE_Float_Validation_Strategy( isset( $input_settings[ 'validation_error_message' ] ) ? $input_settings[ 'validation_error_message' ] : NULL ) );
 		parent::__construct($input_settings);
-        $this->_other_html_attributes .= ' step="any"';
 	}
 }
