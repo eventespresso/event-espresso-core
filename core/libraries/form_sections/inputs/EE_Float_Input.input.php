f5aa4f18d1e065f52bb4863968bc9429de0c851e
Brent Christensen <brent@eventespresso.com>
2017-07-26 15:49:52 -0700

    declare function visibility; extra formatting; [touch:10896]

--- core/libraries/form_sections/inputs/EE_Float_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Float_Input.input.php
@@ -13,26 +13,31 @@ class EE_Float_Input extends EE_Form_Input_Base
 
     /**
      * @param array $input_settings
+     * @throws InvalidArgumentException
      */
-    function __construct($input_settings = array())
+    public function __construct($input_settings = array())
     {
-        $this->_set_display_strategy(new EE_Number_Input_Display_Strategy(
-            isset($input_settings['min_value'])
-                ? $input_settings['min_value']
-                : null,
-            isset($input_settings['max_value'])
-                ? $input_settings['max_value']
-                : null,
-            isset($input_settings['step_value'])
-                ? $input_settings['step_value']
-                : null
-        ));
+        $this->_set_display_strategy(
+            new EE_Number_Input_Display_Strategy(
+                isset($input_settings['min_value'])
+                    ? $input_settings['min_value']
+                    : null,
+                isset($input_settings['max_value'])
+                    ? $input_settings['max_value']
+                    : null,
+                isset($input_settings['step_value'])
+                    ? $input_settings['step_value']
+                    : null
+            )
+        );
         $this->_set_normalization_strategy(new EE_Float_Normalization());
-        $this->_add_validation_strategy(new EE_Float_Validation_Strategy(
-            isset($input_settings['validation_error_message'])
-                ? $input_settings['validation_error_message']
-                : null
-        ));
+        $this->_add_validation_strategy(
+            new EE_Float_Validation_Strategy(
+                isset($input_settings['validation_error_message'])
+                    ? $input_settings['validation_error_message']
+                    : null
+            )
+        );
         parent::__construct($input_settings);
     }
 }
