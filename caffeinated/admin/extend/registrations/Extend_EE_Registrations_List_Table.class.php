c05f6b88eadb4e02d7b8614dd65a9a9f76a4cbfd
Darren Ethier <darren@roughsmootheng.in>
2017-07-28 14:50:36 -0400

    formatting and code cleanup [touch: 10569]

--- caffeinated/admin/extend/registrations/Extend_EE_Registrations_List_Table.class.php
+++ caffeinated/admin/extend/registrations/Extend_EE_Registrations_List_Table.class.php
@@ -1,30 +1,18 @@
-<?php if (! defined('EVENT_ESPRESSO_VERSION')) {
-    exit('No direct script access allowed');
-}
+<?php
+
+defined('EVENT_ESPRESSO_VERSION') || exit('No direct access allowed.');
 
 /**
- * Event Espresso
- * Event Registration and Management Plugin for WordPress
- * @ package        Event Espresso
- * @ author            Seth Shoultes
- * @ copyright        (c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license            {@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
- * @ link            {@link http://www.eventespresso.com}
- * @ since            4.0
- * ------------------------------------------------------------------------
  * Extend_EE_Registrations_List_Table
  *
  * @package            Event Espresso
  * @subpackage         caffeinated/admin/extend/registrations/Extend_EE_Registrations_List_Table.class.php
  * @author             Darren Ethier
- * ------------------------------------------------------------------------
  */
 class Extend_EE_Registrations_List_Table extends EE_Registrations_List_Table
 {
-
-
     /**
-     *        REG_date
+     * REG_date
      */
     function column_REG_date(EE_Registration $item)
     {
@@ -37,22 +25,34 @@ class Extend_EE_Registrations_List_Table extends EE_Registrations_List_Table
             'action'   => 'event_registrations',
             'event_id' => $item->event_ID(),
         ), REG_ADMIN_URL);
-        $actions['check_in'] = EE_Registry::instance()->CAP->current_user_can('ee_read_checkin',
-            'espresso_registrations_registration_checkins', $item->ID()) ? '
-			<a href="' . $check_in_url . '" title="' . esc_attr__('The Check-In List allows you to easily toggle check-in status for this event',
-                'event_espresso') . '">' . __('View Check-ins', 'event_espresso') . '</a>' : __('View Check-ins',
-            'event_espresso');
+        $actions['check_in'] = EE_Registry::instance()->CAP->current_user_can(
+            'ee_read_checkin',
+            'espresso_registrations_registration_checkins',
+            $item->ID()
+        )
+            ? '<a href="' . $check_in_url . '"'
+              . ' title="' . esc_attr__(
+                  'The Check-In List allows you to easily toggle check-in status for this event',
+                  'event_espresso'
+              )
+              . '">' . __('View Check-ins', 'event_espresso') . '</a>'
+            : __('View Check-ins', 'event_espresso');
 
         $view_lnk_url = EE_Admin_Page::add_query_args_and_nonce(array(
             'action' => 'view_transaction',
             'TXN_ID' => $item->transaction()->ID(),
         ), TXN_ADMIN_URL);
-        $REG_date     = EE_Regisry::instance()->CAP->current_user_can('ee_read_transaction',
-            'espresso_transactions_view_transaction') ? '<a href="' . $view_lnk_url . '" title="' . esc_attr__('View Transaction Details',
-                'event_espresso') . '">' . $item->get_i18n_datetime('REG_date') . '</a>' : $item->get_i18n_datetime('REG_date');
+        $REG_date     = EE_Registry::instance()->CAP->current_user_can(
+            'ee_read_transaction',
+            'espresso_transactions_view_transaction'
+        )
+            ? '<a href="' . $view_lnk_url . '"'
+              . ' title="' . esc_attr__('View Transaction Details', 'event_espresso') . '">'
+              . $item->get_i18n_datetime('REG_date')
+              . '</a>'
+            : $item->get_i18n_datetime('REG_date');
 
         return sprintf('%1$s %2$s', $REG_date, $this->row_actions($actions));
-
     }
 
 
@@ -61,6 +61,7 @@ class Extend_EE_Registrations_List_Table extends EE_Registrations_List_Table
      *
      * @param \EE_Registration $item
      * @return string
+     * @throws EE_Error
      */
     public function column_DTT_EVT_start(EE_Registration $item)
     {
@@ -70,13 +71,11 @@ class Extend_EE_Registrations_List_Table extends EE_Registrations_List_Table
         $EVT_ID          = $item->event_ID();
         $datetime_string = '';
         foreach ($datetimes as $datetime) {
-            if (
-            EE_Registry::instance()->CAP->current_user_can(
+            if (EE_Registry::instance()->CAP->current_user_can(
                 'ee_read_checkin',
                 'espresso_registrations_registration_checkins',
                 $item->ID()
-            )
-            ) {
+            )) {
                 // open "a" tag and "href"
                 $datetime_string .= '<a href="';
                 // checkin URL
@@ -107,20 +106,21 @@ class Extend_EE_Registrations_List_Table extends EE_Registrations_List_Table
             $datetime_string .= $this->row_actions(
                 array(
                     'event_datetime_filter' => '<a href="' . EE_Admin_Page::add_query_args_and_nonce(
-                            array('event_id' => $EVT_ID, 'datetime_id' => $datetime->ID()),
-                            REG_ADMIN_URL
-                        ) . '" title="' . sprintf(
+                        array('event_id' => $EVT_ID, 'datetime_id' => $datetime->ID()),
+                        REG_ADMIN_URL
+                    )
+                                               . '" title="' . sprintf(
                                                    esc_attr__(
                                                        'Filter this list to only show registrations for this datetime %s',
                                                        'event_espresso'
                                                    ),
                                                    $datetime->name()
-                                               ) . '">' . __('View Registrations', 'event_espresso') . '</a>',
+                                               ) . '">'
+                                               . __('View Registrations', 'event_espresso')
+                                               . '</a>',
                 )
             );
         }
         return $datetime_string;
     }
-
-
-} //end Extend_EE_Registrations_List_Table
+}
