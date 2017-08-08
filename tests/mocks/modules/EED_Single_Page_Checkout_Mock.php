5b1108d403477f76be926e1f983b8c135ed1b769
Brent Christensen <brent@eventespresso.com>
2017-08-08 12:39:00 -0700

    fix phpdocs; [touch:10766]

--- tests/mocks/modules/EED_Single_Page_Checkout_Mock.php
+++ tests/mocks/modules/EED_Single_Page_Checkout_Mock.php
@@ -2,15 +2,15 @@
 
 class EED_Single_Page_Checkout_Mock extends EED_Single_Page_Checkout
 {
-    
+
     /**
-     * @return EED_Single_Page_Checkout_Mock
+     * @return EED_Module|EED_Single_Page_Checkout_Mock
      */
     public static function instance()
     {
         return parent::get_instance(__CLASS__);
     }
-    
+
     public function is_reg_checkout()
     {
         return $this->_is_reg_checkout();
