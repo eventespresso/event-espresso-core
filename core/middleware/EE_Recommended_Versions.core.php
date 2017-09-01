3dbed06441c1679d3ec7e19e3dd22f17dc6c4fc0
Brent Christensen <brent@eventespresso.com>
2017-09-01 14:03:46 -0700

    formatting; use use; etc; [touch:10202]

--- core/middleware/EE_Recommended_Versions.core.php
+++ core/middleware/EE_Recommended_Versions.core.php
@@ -1,8 +1,9 @@
-<?php use EventEspresso\core\exceptions\InvalidDataTypeException;
+<?php
 
-if ( ! defined('EVENT_ESPRESSO_VERSION')) {
-    exit('No direct script access allowed');
-}
+use EventEspresso\core\domain\entities\notifications\PersistentAdminNotice;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+
+defined('EVENT_ESPRESSO_VERSION') || exit('No direct script access allowed');
 
 
 
@@ -23,18 +24,19 @@ class EE_Recommended_Versions extends EE_Middleware
     /**
      * converts a Request to a Response
      *
-     * @param    EE_Request  $request
-     * @param    EE_Response $response
-     * @return    EE_Response
+     * @param EE_Request  $request
+     * @param EE_Response $response
+     * @return EE_Response
+     * @throws InvalidDataTypeException
      */
     public function handle_request(EE_Request $request, EE_Response $response)
     {
-        $this->_request = $request;
+        $this->_request  = $request;
         $this->_response = $response;
         //$this->_response->add_output( "\n\t IN >>  " . __CLASS__ );
         //$this->_response->set_notice( 1, 'hey look at this' );
         // check required WP version
-        if ( ! $this->_minimum_wp_version_required()) {
+        if (! $this->_minimum_wp_version_required()) {
             $this->_request->un_set('activate', true);
             add_action('admin_notices', array($this, 'minimum_wp_version_error'), 1);
             //$this->_response->add_output( "\n<br />" . 'minimum_wp_version_error' );
@@ -42,7 +44,7 @@ class EE_Recommended_Versions extends EE_Middleware
             $this->_response->deactivate_plugin();
         }
         // check recommended PHP version
-        if ( ! $this->_minimum_php_version_recommended()) {
+        if (! $this->_minimum_php_version_recommended()) {
             $this->_display_minimum_recommended_php_version_notice();
         }
         $this->_response = $this->process_request_stack($this->_request, $this->_response);
@@ -54,9 +56,8 @@ class EE_Recommended_Versions extends EE_Middleware
     /**
      * Helper method to assess installed wp version against given values.
      * By default this compares the required minimum version of WP for EE against the installed version of WP
-     *
-     * Note, $wp_version is the first parameter sent into the PHP version_compare function (what is being checked against)
-     * so consider that when sending in your values.
+     * Note, $wp_version is the first parameter sent into the PHP version_compare function (what is being checked
+     * against) so consider that when sending in your values.
      *
      * @param string $version_to_check
      * @param string $operator
@@ -66,9 +67,11 @@ class EE_Recommended_Versions extends EE_Middleware
     {
         global $wp_version;
         return version_compare(
-            //first account for wp_version being pre-release (like RC, beta etc which are usually in the format like
-            //4.7-RC3-39519
-            strpos($wp_version, '-') > 0 ? substr($wp_version, 0, strpos($wp_version, '-')) : $wp_version,
+            // first account for wp_version being pre-release
+            // (like RC, beta etc) which are usually in the format like 4.7-RC3-39519
+            strpos($wp_version, '-') > 0
+                ? substr($wp_version, 0, strpos($wp_version, '-'))
+                : $wp_version,
             $version_to_check,
             $operator
         );
@@ -84,7 +87,7 @@ class EE_Recommended_Versions extends EE_Middleware
      */
     private function _minimum_wp_version_required()
     {
-        return self::check_wp_version();
+        return EE_Recommended_Versions::check_wp_version();
     }
 
 
@@ -111,7 +114,7 @@ class EE_Recommended_Versions extends EE_Middleware
      */
     private function _minimum_php_version_recommended()
     {
-        return $this->_check_php_version(EE_MIN_PHP_VER_RECOMMENDED);
+        return $this->_check_php_version();
     }
 
 
@@ -129,12 +132,12 @@ class EE_Recommended_Versions extends EE_Middleware
             <p>
                 <?php
                 printf(
-                        __('We\'re sorry, but Event Espresso requires WordPress version %1$s or greater in order to operate. You are currently running version %2$s.%3$sFor information on how to update your version of WordPress, please go to %4$s.',
-                                'event_espresso'),
-                        EE_MIN_WP_VER_REQUIRED,
-                        $wp_version,
-                        '<br/>',
-                        '<a href="http://codex.wordpress.org/Updating_WordPress">http://codex.wordpress.org/Updating_WordPress</a>'
+                    __('We\'re sorry, but Event Espresso requires WordPress version %1$s or greater in order to operate. You are currently running version %2$s.%3$sFor information on how to update your version of WordPress, please go to %4$s.',
+                        'event_espresso'),
+                    EE_MIN_WP_VER_REQUIRED,
+                    $wp_version,
+                    '<br/>',
+                    '<a href="http://codex.wordpress.org/Updating_WordPress">http://codex.wordpress.org/Updating_WordPress</a>'
                 );
                 ?>
             </p>
@@ -153,7 +156,7 @@ class EE_Recommended_Versions extends EE_Middleware
      */
     private function _display_minimum_recommended_php_version_notice()
     {
-        new \EventEspresso\core\domain\entities\notifications\PersistentAdminNotice(
+        new PersistentAdminNotice(
             'php_version_' . str_replace('.', '-', EE_MIN_PHP_VER_RECOMMENDED) . '_recommended',
             sprintf(
                 __(
