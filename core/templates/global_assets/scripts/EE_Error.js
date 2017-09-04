cab7e333588d367c9d75f651f1644486af40b60c
Brent Christensen <brent@eventespresso.com>
2017-09-04 16:23:46 -0700

    don't duplicate jQuery selectors; formatting; [touch:10202]

--- core/templates/global_assets/scripts/EE_Error.js
+++ core/templates/global_assets/scripts/EE_Error.js
@@ -1,28 +1,37 @@
 jQuery(document).ready(function($) {
 
-	var message = $('#ee-error-message');
-	var target;
-	var ee_dismiss = eei18n.ee_dismiss;
+	var $message = $('#ee-error-message');
+	var $notices = $('#espresso-notices');
+    var $body = $('body');
 
-	if ( message.length ) {
-		$('#ee-error-message').remove();
-		if ( $('#espresso-notices').size() ) {
-			target = $('#espresso-notices');
+    var target;
+
+	if ($message.length ) {
+        $message.remove();
+		if ($notices.size() ) {
+			target = $notices;
 		} else if ( $('#screen-meta-links').size() ) {
 			target = $('#screen-meta-links');
 		} else if ( $('#wpbody-content').size() ) {
 			target = $('#wpbody-content');
 		} else {
-			target = $('body');
+			target = $body;
 		}
 
-		$( target ).html( message ).css({ 'position' : 'relative', 'top' : 0, 'left' : 0 , 'margin' : '3em 1em 0 0' , 'z-index' : 5 });
+		$( target ).html($message).css(
+            {
+                'position': 'relative',
+                'top': 0, 'left': 0,
+                'margin': '3em 1em 0 0',
+                'z-index': 5
+            }
+		);
 		//set from wp_localize_script in php
 		if( ee_settings.wp_debug !== '1' ) {
 			$( '.ee-error-trace-dv').hide();
 		}
 
-		$('body').on( 'click', '.display-ee-error-trace-lnk', function(e) {
+        $body.on( 'click', '.display-ee-error-trace-lnk', function(e) {
 			e.preventDefault();
 			e.stopPropagation();
 			var traceTable = '#' + $(this).attr('rel') +'-dv';
@@ -58,9 +67,9 @@ jQuery(document).ready(function($) {
 						$('#'+nag_notice).fadeOut('fast');
 					}
 				},
-				error: function( response ) {
+				error: function() {
 					$('#'+nag_notice).fadeOut('fast');
-					msg = {};
+					var msg = {};
 					msg.errors = ee_dismiss.unknown_error;
 					console.log( msg );
 					window.show_admin_page_ajax_msg( msg );
