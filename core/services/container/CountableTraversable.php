a90c9370f8ad21e3004abb73c8c35c56dec7dc57
Brent Christensen <brent@eventespresso.com>
2017-10-17 13:10:12 -0700

    CountableTraversable interface; [touch:10911]

---
+++ core/services/container/CountableTraversable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EventEspresso\core\services\container;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Interface CountableTraversable
+ * Description
+ *
+ * @package EventEspresso\core\services\container
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+interface CountableTraversable
+{
+
+}
+// Location: CountableTraversable.php
