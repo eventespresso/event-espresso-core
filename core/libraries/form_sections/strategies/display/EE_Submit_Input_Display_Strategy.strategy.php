ddb563a24565e52ffe4505d963f3b7ce9299523e
Brent Christensen <brent@eventespresso.com>
2017-10-20 15:10:07 -0700

    use an  HTML button tag instead of an input; use the "html_label_text"  instead of the  default value, but only  if it has  a value;  [touch:11107]

--- core/libraries/form_sections/strategies/display/EE_Submit_Input_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Submit_Input_Display_Strategy.strategy.php
@@ -15,15 +15,20 @@ class EE_Submit_Input_Display_Strategy extends EE_Display_Strategy_Base{
 	 * @return string of html to display the input
 	 */
 	public function display(){
-		$html = '<input type="submit" ';
+	    $button_text = $this->_input->html_label_text();
+        $button_text = ! empty($button_text)
+            ? $button_text
+            : $this->_input->raw_value_in_form();
+		$html = '<button type="submit" ';
 		$html .= 'name="' . $this->_input->html_name() . '" ';
-		$html .= 'value="' . $this->_input->raw_value_in_form() . '" ';
 		$html .= 'id="' . $this->_input->html_id() . '-submit" ';
 		$html .= 'class="' . $this->_input->html_class() . ' ' . $this->_input->button_css_attributes() . '" ';
 		$html .= 'style="' . $this->_input->html_style() . '" ';
 		$html .= $this->_input->other_html_attributes();
-		$html .= '/>';
-		return $html;
+		$html .= '>';
+        $html .= $button_text;
+        $html .= '</button>';
+        return $html;
 	}
 
 }
