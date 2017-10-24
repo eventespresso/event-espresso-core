e971c1936056d6b4b6f42ff696a77357ab3d2147
Nick Heyvaert <nick@strak.be>
2017-10-24 09:56:28 +0200

    Only one return in function
    The previous change would break the function because it ends after the
    first return. Instead the output should be stored in a variable and
    returned only once at the end of the function.

--- admin_pages/events/Events_Admin_List_Table.class.php
+++ admin_pages/events/Events_Admin_List_Table.class.php
@@ -409,11 +409,11 @@ class Events_Admin_List_Table extends EE_Admin_List_Table
      */
     public function column_start_date_time(EE_Event $item)
     {
-        return ! empty($this->_dtt)
+        $output = ! empty($this->_dtt)
             ? $this->_dtt->get_i18n_datetime('DTT_EVT_start')
             : esc_html__('No Date was saved for this Event', 'event_espresso');
         //display in user's timezone?
-        return ! empty($this->_dtt)
+        $output .= ! empty($this->_dtt)
             ? $this->_dtt->display_in_my_timezone(
                 'DTT_EVT_start',
                 'get_i18n_datetime',
@@ -421,6 +421,7 @@ class Events_Admin_List_Table extends EE_Admin_List_Table
                 'My Timezone: '
             )
             : '';
+        return $output;
     }
 
 
@@ -432,11 +433,11 @@ class Events_Admin_List_Table extends EE_Admin_List_Table
     public function column_reg_begins(EE_Event $item)
     {
         $reg_start = $item->get_ticket_with_earliest_start_time();
-        return ! empty($reg_start)
+        $output = ! empty($reg_start)
             ? $reg_start->get_i18n_datetime('TKT_start_date')
             : esc_html__('No Tickets have been setup for this Event', 'event_espresso');
         //display in user's timezone?
-        return ! empty($reg_start)
+        $output .= ! empty($reg_start)
             ? $reg_start->display_in_my_timezone(
                 'TKT_start_date',
                 'get_i18n_datetime',
@@ -444,6 +445,7 @@ class Events_Admin_List_Table extends EE_Admin_List_Table
                 'My Timezone: '
             )
             : '';
+        return $output;
     }
 
 
