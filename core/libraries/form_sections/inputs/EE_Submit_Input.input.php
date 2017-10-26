7f616d243b4e4520ef46860656b86238cee9e089
Mike Nelson <michael@eventespresso.com>
2017-10-25 21:07:09 -0700

    [touch:11107]{t:45}submit inputs shouldnt be receiving  data like other inputs, they should just stay the same.

--- core/libraries/form_sections/inputs/EE_Submit_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Submit_Input.input.php
@@ -22,4 +22,39 @@ class EE_Submit_Input extends EE_Form_Input_Base{
 		$this->_add_validation_strategy( new EE_Plaintext_Validation_Strategy() );
 		parent::__construct($options);
 	}
+
+
+
+    /**
+     * Sets the default as normal (overriden so it can circumvent calling
+     * EE_Submit_Input::_normalize() and EE_Submit_Input::_set_raw_value())
+     * @param mixed $value
+     */
+	public function set_default($value)
+    {
+        parent::_set_normalized_value($value);
+        parent::_set_raw_value($value);
+    }
+
+
+
+    /**
+     * Does nothing, because it's actually not expected for submit input's values to change.
+     * If you want to change the input's value displayed, use `set_default()`
+     * @param array $req_data
+     * @return void
+     */
+    public function _normalize($req_data)
+    {
+        return;
+    }
+
+    /**
+     * Does nothing, because it's actually not expected for submit input's values to change.
+     * If you want to change the input's value displayed, use `set_default()`
+     */
+    public function _set_raw_value($value)
+    {
+        return;
+    }
 }
