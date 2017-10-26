42b92b596c1f8a680e93af1d59f0189ae78d8b4d
Brent Christensen <brent@eventespresso.com>
2017-10-26 10:58:11 -0700

    change default input type  to input; [touch:11107]

--- core/libraries/form_sections/strategies/display/EE_Submit_Input_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Submit_Input_Display_Strategy.strategy.php
@@ -26,13 +26,13 @@ class EE_Submit_Input_Display_Strategy extends EE_Display_Strategy_Base{
             : $this->_input->raw_value_in_form();
         $tag = apply_filters(
           'FHEE__EE_Submit_Input_Display_Strategy__display__submit_input_html_tag',
-          EE_Submit_Input_Display_Strategy::HTML_TAG_BUTTON
+          EE_Submit_Input_Display_Strategy::HTML_TAG_INPUT
         );
         // verify result is one of the two valid options
         $tag = $tag === EE_Submit_Input_Display_Strategy::HTML_TAG_BUTTON
                || $tag === EE_Submit_Input_Display_Strategy::HTML_TAG_INPUT
             ? $tag
-            : EE_Submit_Input_Display_Strategy::HTML_TAG_BUTTON;
+            : EE_Submit_Input_Display_Strategy::HTML_TAG_INPUT;
 		$html = '<' . $tag . ' type="submit" ';
 		$html .= 'name="' . $this->_input->html_name() . '" ';
 		$html .= 'value="' . $this->_input->raw_value_in_form() . '" ';
