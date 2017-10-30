ccab27f72390e8edc07f1acab3ae937425b118a1
Mike Nelson <michael@eventespresso.com>
2017-10-30 13:51:19 -0700

    [touch:11107]{t:45}add default property on inputs for remembering the original value. This may be handy on other inputs too.

--- core/libraries/form_sections/strategies/display/EE_Submit_Input_Display_Strategy.strategy.php
+++ core/libraries/form_sections/strategies/display/EE_Submit_Input_Display_Strategy.strategy.php
@@ -20,10 +20,15 @@ class EE_Submit_Input_Display_Strategy extends EE_Display_Strategy_Base{
 	 * @return string of html to display the input
 	 */
 	public function display(){
-	    $button_text = $this->_input->html_label_text();
-        $button_text = ! empty($button_text)
-            ? $button_text
-            : $this->_input->raw_value_in_form();
+	    $default_value = $this->_input->get_default();
+	    if ($this->_input->get_normalization_strategy() instanceof  EE_Normalization_Strategy_Base) {
+	        $default_value = $this->_input->get_normalization_strategy()->unnormalize($default_value);
+        }
+	    $value_in_form = htmlentities(
+            $default_value,
+            ENT_QUOTES,
+            'UTF-8'
+        );
         $tag = apply_filters(
           'FHEE__EE_Submit_Input_Display_Strategy__display__submit_input_html_tag',
           EE_Submit_Input_Display_Strategy::HTML_TAG_INPUT
@@ -35,12 +40,16 @@ class EE_Submit_Input_Display_Strategy extends EE_Display_Strategy_Base{
             : EE_Submit_Input_Display_Strategy::HTML_TAG_INPUT;
 		$html = '<' . $tag . ' type="submit" ';
 		$html .= 'name="' . $this->_input->html_name() . '" ';
-		$html .= 'value="' . $this->_input->raw_value_in_form() . '" ';
+		$html .= 'value="' . $value_in_form . '" ';
 		$html .= 'id="' . $this->_input->html_id() . '-submit" ';
 		$html .= 'class="' . $this->_input->html_class() . ' ' . $this->_input->button_css_attributes() . '" ';
 		$html .= 'style="' . $this->_input->html_style() . '" ';
 		$html .= $this->_input->other_html_attributes();
 		if($tag === EE_Submit_Input_Display_Strategy::HTML_TAG_BUTTON) {
+            $button_text = $this->_input->html_label_text();
+            $button_text = ! empty($button_text)
+                ? $button_text
+                : $value_in_form;
             $html .= '>';
             $html .= $button_text;
             $html .= '</button>';
