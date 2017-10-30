fcb9a3001202aeaf72f46cbe22b896239ad17ea8
Mike Nelson <michael@eventespresso.com>
2017-10-30 13:53:36 -0700

    [touch:11107]{30}show the default value of submit inputs. This way we can still receive the submitted form value and work on it too.

--- core/libraries/form_sections/inputs/EE_Submit_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Submit_Input.input.php
@@ -14,47 +14,12 @@ class EE_Submit_Input extends EE_Form_Input_Base{
 	 * @param array $options
 	 */
 	public function __construct($options = array()){
-        $options['html_label_text'] = ! empty($options['html_label_text'])
-            ? $options['html_label_text']
-            : $options['default'];
+	    if( empty($options['default'])) {
+	        $options['default'] = esc_html__('Submit', 'event_espresso');
+        }
 		$this->_set_display_strategy(new EE_Submit_Input_Display_Strategy());
 		$this->_set_normalization_strategy(new EE_Text_Normalization());
 		$this->_add_validation_strategy( new EE_Plaintext_Validation_Strategy() );
 		parent::__construct($options);
 	}
-
-
-
-    /**
-     * Sets the default as normal (overriden so it can circumvent calling
-     * EE_Submit_Input::_normalize() and EE_Submit_Input::_set_raw_value())
-     * @param mixed $value
-     */
-	public function set_default($value)
-    {
-        parent::_set_normalized_value($value);
-        parent::_set_raw_value($value);
-    }
-
-
-
-    /**
-     * Does nothing, because it's actually not expected for submit input's values to change.
-     * If you want to change the input's value displayed, use `set_default()`
-     * @param array $req_data
-     * @return void
-     */
-    public function _normalize($req_data)
-    {
-        return;
-    }
-
-    /**
-     * Does nothing, because it's actually not expected for submit input's values to change.
-     * If you want to change the input's value displayed, use `set_default()`
-     */
-    public function _set_raw_value($value)
-    {
-        return;
-    }
 }
