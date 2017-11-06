829be3ce370a5428f6e80f8d01831e51d888f10c
Brent Christensen <brent@eventespresso.com>
2017-11-06 15:53:04 -0800

    Factory class for generating addon Domain objects [touch:11137]

---
+++ core/domain/DomainFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace EventEspresso\core\domain;
+
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use InvalidArgumentException;
+use EventEspresso\core\services\loaders\LoaderFactory;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class DomainFactory
+ * Factory class for generating addon Domain objects
+ *
+ * @package EventEspresso\core\domain
+ * @author  Brent Christensen
+ * @since   4.9.50
+ */
+class DomainFactory
+{
+
+    /**
+     * @param string $domain_fcqn [required] Fully Qualified Class Name for the Domain class
+     * @param array  $arguments   [required] array of arguments to be passed to the Domain class constructor.
+     *                            must include the following two elements as a minimum:
+     *                            array(
+     *                              $plugin_file, // full server path to the addon main file (__FILE__)
+     *                              $version, // standard version string like #.#.#
+     *                            )
+     * @return mixed
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
+    public static function create($domain_fcqn = '', array $arguments)
+    {
+        if(!isset($arguments[0], $arguments[1])) {
+            throw new InvalidArgumentException(
+                esc_html__(
+                    'You need to pass at least two arguments, representing the addon plugin file and version, in order to generate a Domain class',
+                    'event_espresso'
+                )
+            );
+        }
+        $loader = LoaderFactory::getLoader();
+        return $loader->getShared($domain_fcqn, $arguments);
+    }
+
+}
+// Location: DomainFactory.php
