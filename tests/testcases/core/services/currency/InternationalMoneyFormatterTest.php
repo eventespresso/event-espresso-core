1b7b5d6da04a6d9819cc08abaa957e6ff39ad83f
Brent Christensen <brent@eventespresso.com>
2017-11-07 16:32:48 -0800

    remove all use of static; [touch:10619]

--- tests/testcases/core/services/currency/InternationalMoneyFormatterTest.php
+++ tests/testcases/core/services/currency/InternationalMoneyFormatterTest.php
@@ -1,6 +1,6 @@
 <?php
-use EventEspresso\core\services\currency\CreateCurrency;
 use EventEspresso\core\services\currency\InternationalMoneyFormatter;
+use EventEspresso\core\services\loaders\LoaderFactory;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
@@ -22,8 +22,11 @@ class InternationalMoneyFormatterTest extends \EE_UnitTestCase
      */
     public function test_format()
     {
+        $currency_factory = LoaderFactory::getLoader()->getShared(
+            'EventEspresso\core\services\currency\CurrencyFactory'
+        );
+        $currency = $currency_factory->createFromCountryCode('US');
         $formatter = new InternationalMoneyFormatter();
-        $currency = CreateCurrency::fromCountryCode('US');
         $this->assertEquals(
             $formatter->format(1234.5, $currency),
             '1234.5 <span class="currency-code">(USD)</span>'
