1e6c8ed9063a1080fa7ea1d71d76df6c54b2596d
Mike Nelson <michael@eventespresso.com>
2017-11-14 13:03:17 -0800

    [touch:11170]{t:90}EE_Money_FIeld now also deals in Money objects. Unit tested (there are a bunch of failures though, some which are also happening on 10619)

--- core/db_models/fields/EE_Money_Field.php
+++ core/db_models/fields/EE_Money_Field.php
@@ -1,24 +1,52 @@
 <?php
+
+use EventEspresso\core\domain\values\currency\Money;
+use EventEspresso\core\services\currency\MoneyFactory;
+use EventEspresso\core\services\currency\MoneyFormatter;
+use EventEspresso\core\services\loaders\LoaderFactory;
+
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
 /**
- * Text_Fields is a base class for any fields which are have float value. (Exception: foreign and private key fields.
- * Wish PHP had multiple-inheritance for this...)
+ * EE_Money_Field
+ * Model field for dealing with money amounts. Originally this accepted and returns float
+ * values, but now it also deals in Money entities.
  */
 class EE_Money_Field extends EE_Float_Field
 {
 
     /**
-     * @param string $table_column
-     * @param string $nicename
-     * @param bool   $nullable
-     * @param null   $default_value
+     * @var $money_factory MoneyFactory
      */
-    public function __construct($table_column, $nicename, $nullable, $default_value = null)
-    {
+    protected $money_factory;
+
+
+
+    /**
+     * @param string       $table_column
+     * @param string       $nicename
+     * @param bool         $nullable
+     * @param null         $default_value
+     * @param MoneyFactory $factory
+     * @throws \InvalidArgumentException
+     * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
+     * @throws \EventEspresso\core\exceptions\InvalidDataTypeException
+     */
+    public function __construct(
+        $table_column,
+        $nicename,
+        $nullable,
+        $default_value = null,
+        MoneyFactory $factory = null
+    ) {
+        if (! $factory instanceof MoneyFactory) {
+            $factory = LoaderFactory::getLoader()->getShared(MoneyFactory::class);
+        }
+        $this->money_factory = $factory;
         parent::__construct($table_column, $nicename, $nullable, $default_value);
         $this->setSchemaType('object');
     }
+
 
 
     /**
@@ -27,25 +55,95 @@ class EE_Money_Field extends EE_Float_Field
      *    'no_currency_code': "$3,023.00"
      *    null: "$3,023.00<span>USD</span>"
      *
-     * @param string $value_on_field_to_be_outputted
-     * @param string $schema
+     * @param string|Money $value_on_field_to_be_outputted
+     * @param string       $schema
      * @return string
+     * @throws \EE_Error
      */
-    function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null)
+    public function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null)
     {
-        $pretty_float = parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted);
+        //has someone hooked into the old currency formatter helper's filters?
+        //if so, we had better stick with it
+        if (apply_filters(
+            'FHEE__EE_Money_Field__prpeare_for_pretty_echoing',
+            has_filter('FHEE__EEH_Template__format_currency__raw_amount')
+            || has_filter('FHEE__EEH_Template__format_currency__CNT_ISO')
+            || has_filter('FHEE__EEH_Template__format_currency__amount')
+            || has_filter('FHEE__EEH_Template__format_currency__display_code')
+            || has_filter('FHEE__EEH_Template__format_currency__amount_formatted'),
+            $this
+        )) {
+            $pretty_float = parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted);
 
-        if ($schema == 'localized_float') {
-            return $pretty_float;
+            if ($schema === 'localized_float') {
+                return $pretty_float;
+            }
+            if ($schema === 'no_currency_code') {
+                //			echo "schema no currency!";
+                $display_code = false;
+            } else {
+                $display_code = true;
+            }
+            //we don't use the $pretty_float because format_currency will take care of it.
+            return EEH_Template::format_currency($value_on_field_to_be_outputted, false, $display_code);
         }
-        if ($schema == 'no_currency_code') {
-//			echo "schema no currency!";
-            $display_code = false;
-        } else {
-            $display_code = true;
+        //ok let's just use the new formatting code then
+        switch ($schema) {
+            case MoneyFormatter::ADD_CURRENCY_CODE:
+                $formatting_level = MoneyFormatter::ADD_CURRENCY_CODE;
+                break;
+            case 'no_currency_code':
+            case MoneyFormatter::ADD_CURRENCY_SIGN:
+                $formatting_level = MoneyFormatter::ADD_CURRENCY_SIGN;
+                break;
+            case MoneyFormatter::ADD_THOUSANDS:
+            case 'localized_float':
+                $formatting_level = MoneyFormatter::ADD_THOUSANDS;
+                break;
+            case MoneyFormatter::DECIMAL_ONLY:
+                $formatting_level = MoneyFormatter::DECIMAL_ONLY;
+                break;
+            default:
+                $formatting_level = MoneyFormatter::INTERNATIONAL;
         }
-        //we don't use the $pretty_float because format_currency will take care of it.
-        return EEH_Template::format_currency($value_on_field_to_be_outputted, false, $display_code);
+        $value_on_field_to_be_outputted = $this->ensureMoney($value_on_field_to_be_outputted);
+        return $value_on_field_to_be_outputted->format($formatting_level);
+    }
+
+
+
+    /**
+     * Make sure this value is a money object
+     *
+     * @param string|float|int|Money $value
+     * @return Money
+     * @throws \InvalidArgumentException
+     * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
+     * @throws \EventEspresso\core\exceptions\InvalidDataTypeException
+     * @throws \EE_Error
+     */
+    private function ensureMoney($value)
+    {
+        if (! $value instanceof Money) {
+            return $this->money_factory->createForSite($value);
+        }
+        return $value;
+    }
+
+
+
+    /**
+     * Ensures we're dealing with something that isn't Money
+     * (for passing off to legacy systems or the parent field)
+     * @param  string|float|int|Money $value
+     * @return string|float|int
+     */
+    private function ensureNotMoney($value)
+    {
+        if( $value instanceof Money) {
+            return $value->amount();
+        }
+        return $value;
     }
 
     /**
@@ -54,27 +152,67 @@ class EE_Money_Field extends EE_Float_Field
      * Also, interprets periods and commas according to the country's currency settings.
      * So if you want to pass in a string that NEEDS to interpret periods as decimal marks, call floatval() on it first.
      *
-     * @param string $value_inputted_for_field_on_model_object
-     * @return float
+     * @param string|Money $value_inputted_for_field_on_model_object
+     * @return Money
      */
-    function prepare_for_set($value_inputted_for_field_on_model_object)
+    public function prepare_for_set($value_inputted_for_field_on_model_object)
     {
-        //remove any currencies etc.
-//		if(is_string($value_inputted_for_field_on_model_object)){
-//			$value_inputted_for_field_on_model_object = preg_replace("/[^0-9,.]/", "", $value_inputted_for_field_on_model_object);
-//		}
+        if ($value_inputted_for_field_on_model_object instanceof Money) {
+            return $value_inputted_for_field_on_model_object;
+        }
         //now it's a float-style string or number
-        $float_val = parent::prepare_for_set($value_inputted_for_field_on_model_object);
-        //round to the correctly number of decimal places for this  currency
-        $rounded_value = round($float_val, EE_Registry::instance()->CFG->currency->dec_plc);
-        return $rounded_value;
+        return $this->ensureMoney(
+            parent::prepare_for_set($value_inputted_for_field_on_model_object)
+        );
     }
 
-    function prepare_for_get($value_of_field_on_model_object)
+
+
+    /**
+     * @param string|float|int|Money $value_of_field_on_model_object
+     * @return float
+     * @throws \InvalidArgumentException
+     * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
+     * @throws \EventEspresso\core\exceptions\InvalidDataTypeException
+     */
+    public function prepare_for_get($value_of_field_on_model_object)
     {
+        $value_of_field_on_model_object = $this->ensureNotMoney($value_of_field_on_model_object);
         $c = EE_Registry::instance()->CFG->currency;
         return round(parent::prepare_for_get($value_of_field_on_model_object), $c->dec_plc);
     }
+
+
+
+    /**
+     * Takes the incoming float and create a money entity for the model object
+     *
+     * @param mixed $value_found_in_db_for_model_object
+     * @return Money
+     * @throws \InvalidArgumentException
+     * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
+     * @throws \EventEspresso\core\exceptions\InvalidDataTypeException
+     * @throws \EE_Error
+     */
+    public function prepare_for_set_from_db($value_found_in_db_for_model_object)
+    {
+        return $this->money_factory->createForSite($value_found_in_db_for_model_object);
+    }
+
+
+
+    /**
+     * Prepares a value for use in the DB
+     * @param string|float|int|Money $value_of_field_on_model_object
+     * @return float
+     */
+    public function prepare_for_use_in_db($value_of_field_on_model_object)
+    {
+        $value_of_field_on_model_object = $this->ensureNotMoney($value_of_field_on_model_object);
+        return parent::prepare_for_use_in_db($value_of_field_on_model_object);
+    }
+
+
 
     public function getSchemaProperties()
     {
--- tests/testcases/core/db_models/fields/EE_Money_Field_Test.php
+++ tests/testcases/core/db_models/fields/EE_Money_Field_Test.php
@@ -1,5 +1,12 @@
 <?php
+
+use EventEspresso\core\services\currency\MoneyFactory;
+use EventEspresso\core\services\currency\MoneyFormatter;
+use EventEspresso\core\services\loaders\LoaderFactory;
+
 defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
 
 /**
  * Tests for the EE_Money_Field class
@@ -8,17 +15,20 @@ defined('EVENT_ESPRESSO_VERSION') || exit;
  * @subpackage tests
  * @author     Darren Ethier
  * @since      4.9.26.rc.000
- * @group   model_fields
- * @group   models
+ * @group      model_fields
+ * @group      models
  */
 class EE_Money_Field_Test extends EE_UnitTestCase
 {
 
     /**
      * holds the field being tested
+     *
      * @var EE_Money_Field
      */
     protected $_field;
+
+
 
     public function setUp()
     {
@@ -28,6 +38,7 @@ class EE_Money_Field_Test extends EE_UnitTestCase
     }
 
 
+
     public function tearDown()
     {
         $this->_field = null;
@@ -35,13 +46,274 @@ class EE_Money_Field_Test extends EE_UnitTestCase
     }
 
 
+
     public function test_getSchemaType()
     {
         $this->assertEquals('object', $this->_field->getSchemaType());
     }
 
+
+
     public function test_get_wpdb_data_type()
     {
         $this->assertEquals('%f', $this->_field->get_wpdb_data_type());
+    }
+
+
+
+    /**
+     * @return array {
+     * @type float $expected
+     * @type mixed $input
+     *               }
+     */
+    public function dataProviderForPrepareForGet()
+    {
+        return array(
+            'float' => array(1.23, 1.23),
+            'int'   => array(32, 32),
+            'money' => array(
+                1.23,
+                LoaderFactory::getLoader()->getShared(
+                    MoneyFactory::class
+                )->createForSite(1.23)
+            ),
+        );
+    }
+
+
+
+    /**
+     * @dataProvider dataProviderForPrepareForGet
+     * @param float $expected
+     * @param mixed $input
+     */
+    public function testPrepareForGet($expected, $input)
+    {
+        $value_for_setting = $this->_field->prepare_for_get($input);
+        $this->assertEquals($expected, $value_for_setting);
+    }
+
+
+
+    /**
+     * @return array {
+     * @type float $expected
+     * @type mixed $input
+     *               }
+     */
+    public function dataProviderForPrepareForSet()
+    {
+        $factory = LoaderFactory::getLoader()->getShared(
+            MoneyFactory::class
+        );
+        return array(
+            'float'       => array($factory->createForSite(1.23), 1.23),
+            'int'         => array($factory->createForSite(32), 32),
+            'money'       => array(
+                $factory->createForSite(1.43),
+                $factory->createForSite(1.43),
+            ),
+            'string'      => array(
+                $factory->createForSite(4.32),
+                '4.32'
+            ),
+            'i18n_string' => array(
+                $factory->createForSite(3200.32),
+                '$3,200.32 USD'
+            ),
+        );
+    }
+
+
+
+    /**
+     * @dataProvider dataProviderForPrepareForSet
+     * @param float $expected
+     * @param mixed $input
+     */
+    public function testPrepareForSet($expected, $input)
+    {
+        $value_for_setting = $this->_field->prepare_for_set($input);
+        $this->assertEquals($expected, $value_for_setting);
+    }
+
+
+
+    /**
+     * @return array {
+     * @type Money  $expected
+     * @type string $input
+     *               }
+     */
+    public function dataProviderForPrepareForSetFromDb()
+    {
+        $factory = LoaderFactory::getLoader()->getShared(
+            MoneyFactory::class
+        );
+        return array(
+            'basic'           => array(
+                $factory->createForSite(1.23),
+                '1.23'
+            ),
+            'extra_precision' => array(
+                $factory->createForSite(1.321),
+                '1.321',
+            ),
+            //@todo: I don't think this is right
+            'way_more_precision_than_we_can_handle' => array(
+                $factory->createForSite(1.23456789123456789),
+                '1.23456789123456789'
+            )
+        );
+    }
+
+
+
+    /**
+     * @dataProvider dataProviderForPrepareForSetFromDb
+     * @param float $expected
+     * @param mixed $input
+     */
+    public function testPrepareForSetFromDb($expected, $input)
+    {
+        $value_for_setting = $this->_field->prepare_for_set_from_db($input);
+        $this->assertEquals($expected, $value_for_setting);
+    }
+
+
+    /**
+     * @return array {
+     * @type Money  $expected
+     * @type string $input
+     *               }
+     */
+    public function dataProviderForPrepareForUseInDb()
+    {
+        $factory = LoaderFactory::getLoader()->getShared(
+            MoneyFactory::class
+        );
+        return array(
+            'money'           => array(
+                1.23,
+                $factory->createForSite(1.23),
+            ),
+            'int' => array(
+                23,
+                23
+            ),
+            'string' => array(
+                43.23,
+                '43.23'
+            ),
+            'float' => array(
+                1.23,
+                1.23
+            )
+        );
+    }
+
+
+
+    /**
+     * @dataProvider dataProviderForPrepareForUseInDb
+     * @param float $expected
+     * @param mixed $input
+     */
+    public function testPrepareForUseInDb($expected, $input)
+    {
+        $value_for_setting = $this->_field->prepare_for_use_in_db($input);
+        $this->assertEquals($expected, $value_for_setting);
+    }
+
+
+
+    /**
+     * @return array {
+     * @type Money  $expected
+     * @type string $input
+     * @type string|int $scheme
+     * @type boolean $add_filter
+     *               }
+     */
+    public function dataProviderForPrettyEchoing()
+    {
+        $factory = LoaderFactory::getLoader()->getShared(
+            MoneyFactory::class
+        );
+        return array(
+            'legacy_basic'           => array(
+                '$1,234.56 <span class="currency-code">(USD)</span>',
+                1234.56,
+                null,
+                true
+            ),
+            'new_basic'  => array(
+                '$1,234.56 <span class="currency-code">(USD)</span>',
+                1234.56,
+                null,
+                false
+            ),
+            'legacy_localized_float'=> array(
+                '1,234.56',
+                1234.56,
+                'localized_float',
+                true
+            ),
+            'new_localized_float'=> array(
+                '1,234.56',
+                1234.56,
+                'localized_float',
+                false
+            ),
+            'legacy_no_currency_code'=>array(
+                '$1,234.56',
+                1234.56,
+                'no_currency_code',
+                true
+            ),
+            'new_no_currency_code'=>array(
+                '$1,234.56',
+                1234.56,
+                'no_currency_code',
+                false
+            ),
+            'new_other_formatting_option'=>array(
+                '1234.56',
+                1234.56,
+                MoneyFormatter::DECIMAL_ONLY,
+                false
+            )
+        );
+    }
+
+
+
+    /**
+     * @dataProvider dataProviderForPrettyEchoing
+     * @param float $expected
+     * @param mixed $input
+     * @param mixed $scheme
+     * @param boolean $add_a_filter
+     */
+    public function testPrepareForPrettyEchoing($expected, $input, $scheme, $add_a_filter)
+    {
+        if ($add_a_filter) {
+            add_filter('FHEE__EEH_Template__format_currency__raw_amount',array($this,'do_nothing'));
+        }
+        $value_for_setting = $this->_field->prepare_for_pretty_echoing($input, $scheme);
+        $this->assertEquals($expected, $value_for_setting);
+    }
+
+
+
+    /**
+     * Callback that does nothing, used in testPrepareForPrettyEchoing
+     * @param mixed $input
+     * @return mixed
+     */
+    public function do_nothing($input)
+    {
+        return $input;
     }
 }
