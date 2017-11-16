85c8136a6ec6303bed9352ef8f041041684883dd
Brent Christensen <brent@eventespresso.com>
2017-11-15 20:56:25 -0800

    rename method to create() too match other factories; use switch to set desc based on slug; [touch:11083]

--- core/domain/services/contexts/RequestTypeContextFactory.php
+++ core/domain/services/contexts/RequestTypeContextFactory.php
@@ -40,11 +40,73 @@ class RequestTypeContextFactory
 
     /**
      * @param string $slug
-     * @param string $description
      * @return RequestTypeContext
      */
-    public function createRequestTypeContext($slug, $description)
+    public function create($slug)
     {
+        switch ($slug) {
+            case RequestTypeContext::ACTIVATION :
+                $description = esc_html__(
+                    'The current request is for some form of activation',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::API :
+                $description = esc_html__(
+                    'The current request is for the REST API',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::FRONT_AJAX :
+                $description = esc_html__(
+                    'The current request is for the frontend via AJAX',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::ADMIN_AJAX :
+                $description = esc_html__(
+                    'The current request is for the admin via AJAX',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::CRON :
+                $description = esc_html__(
+                    'The current request is for a WP_Cron',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::CLI :
+                $description = esc_html__(
+                    'The current request is from the command line',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::ADMIN :
+                $description = esc_html__(
+                    'The current request is for the admin',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::IFRAME :
+                $description = esc_html__(
+                    'The current request is for an iframe',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::FEED :
+                $description = esc_html__(
+                    'The current request is for a feed (ie: RSS)',
+                    'event_espresso'
+                );
+                break;
+            case RequestTypeContext::FRONTEND :
+            default :
+                $description = esc_html__(
+                    'The current request is for the frontend',
+                    'event_espresso'
+                );
+                break;
+        }
         // we're using the Loader with sharing turned on,
         // so that the generated RequestTypeContext object is accessible anywhere
         // by simply requesting it again from the loader
