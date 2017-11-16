ee27000bf76eeb6116b95dd78dd12d8a625488f7
Darren Ethier <darren@roughsmootheng.in>
2017-11-16 11:11:44 -0500

    fix tests
    - remove usage of construct and properly utilize `setUp` and `tearDown`
    - assign `EE_Payment_Method_Manger` to a property (using LoaderFactory) and  modify usage through tests (removes the need for `reset` calls within test.
    - remove duplicate test (the scenario it was testing was fixed in later work, so no need to do the same thing twice).

--- tests/testcases/core/libraries/plugin_api/EE_Register_Payment_Method_Test.php
+++ tests/testcases/core/libraries/plugin_api/EE_Register_Payment_Method_Test.php
@@ -1,5 +1,8 @@
 <?php
 
+use EventEspresso\core\services\loaders\LoaderFactory;
+
+
 /**
  * @group core/libraries/plugin_api
  * @group core
@@ -8,19 +11,35 @@
  */
 class EE_Register_Payment_Method_Test extends EE_UnitTestCase
 {
-    protected $_pmt_name = 'Mock_Onsite';
-    protected $_pmt_args = null;
+    protected $_pmt_name;
+    protected $_pmt_args;
 
-    public function __construct($name = null, array $data = array(), $dataName = '')
+    /**
+     * @var EE_Payment_Method_Manager
+     */
+    protected $payment_method_manager;
+
+
+    public function setUp()
     {
-        parent::__construct($name, $data, $dataName);
+        parent::setUp();
         $this->_pmt_args = array(
             'payment_method_paths' =>
                 array(
                     EE_TESTS_DIR . 'mocks' . DS . 'payment_methods' . DS . 'Mock_Onsite',
                 ),
         );
-        EE_Registry::instance()->load_lib('Payment_Method_Manager');
+        $this->_pmt_name = 'Mock_Onsite';
+        $this->payment_method_manager = LoaderFactory::getLoader()->getShared('EE_Payment_Method_Manager');
+    }
+
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        $this->_pmt_args = null;
+        $this->_pmt_name = null;
+        $this->payment_method_manager = null;
     }
 
     public function test_register__fail()
@@ -29,7 +48,7 @@ class EE_Register_Payment_Method_Test extends EE_UnitTestCase
         remove_all_filters('FHEE__EE_Payment_Method_Manager__register_payment_methods__payment_methods_to_register');
 
         //first verify it doesn't already exists
-        $pmt_exists = EE_Payment_Method_Manager::reset()->payment_method_type_exists($this->_pmt_name);
+        $pmt_exists = $this->payment_method_manager->payment_method_type_exists($this->_pmt_name);
         $this->assertFalse($pmt_exists);
 
         //try registering at wrong time
@@ -41,35 +60,14 @@ class EE_Register_Payment_Method_Test extends EE_UnitTestCase
         }
     }
 
-    /**
-     * @group 7230
-     */
-    public function test_register__too_late()
-    {
-        $this->_pretend_addon_hook_time();
-        //double-check no one else is filtering payment method types
-        remove_all_filters('FHEE__EE_Payment_Method_Manager__register_payment_methods__payment_methods_to_register');
-
-        //first verify it doesn't already exists
-        $pmt_exists = EE_Payment_Method_Manager::reset()->payment_method_type_exists($this->_pmt_name);
-        $this->assertFalse($pmt_exists);
-
-
-        EE_Register_Payment_Method::register($this->_pmt_name, $this->_pmt_args);
-        //now check it does exist
-        $pmt_exists = EE_Payment_Method_Manager::instance()->payment_method_type_exists($this->_pmt_name);
-        $this->assertTrue($pmt_exists);
-    }
-
     public function test_register__success()
     {
-        EE_Register_Payment_Method::deregister($this->_pmt_name);
         $this->_pretend_addon_hook_time();
         //double-check no one else is filtering payment method types
         remove_all_filters('FHEE__EE_Payment_Method_Manager__register_payment_methods__payment_methods_to_register');
 
         //first verify it doesn't already exists
-        $pmt_exists = EE_Payment_Method_Manager::reset()->payment_method_type_exists($this->_pmt_name);
+        $pmt_exists = $this->payment_method_manager->payment_method_type_exists($this->_pmt_name);
         $this->assertFalse($pmt_exists);
 
 
@@ -79,5 +77,3 @@ class EE_Register_Payment_Method_Test extends EE_UnitTestCase
         $this->assertTrue($pmt_exists);
     }
 }
-
-// End of file EE_Register_Payment_Method_Test.php
