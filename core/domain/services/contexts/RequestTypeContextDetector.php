778f78b0caf056e7510094ca9aad8224bf280481
Brent Christensen <brent@eventespresso.com>
2017-11-16 17:19:14 -0800

    refactor to better detect request types; [touch:11083]

--- core/domain/services/contexts/RequestTypeContextDetector.php
+++ core/domain/services/contexts/RequestTypeContextDetector.php
@@ -3,6 +3,7 @@
 namespace EventEspresso\core\domain\services\contexts;
 
 use EE_Request;
+use EventEspresso\core\domain\Domain;
 use InvalidArgumentException;
 use EventEspresso\core\domain\entities\contexts\RequestTypeContext;
 
@@ -51,22 +52,22 @@ class RequestTypeContextDetector
      */
     public function detectRequestTypeContext()
     {
-        // Detect REST API
-        if (defined('REST_REQUEST') && REST_REQUEST) {
+        // Detect EE REST API
+        if ($this->isEspressoRestApiRequest()) {
             return $this->factory->create(RequestTypeContext::API);
         }
         // Detect AJAX
-        if (defined('DOING_AJAX') && DOING_AJAX) {
-            if (filter_var($this->request->get('ee_front_ajax'), FILTER_VALIDATE_BOOLEAN)) {
+        if ($this->request->isAjax()) {
+            if ($this->request->isFrontAjax()) {
                 return $this->factory->create(RequestTypeContext::FRONT_AJAX);
             }
             return $this->factory->create(RequestTypeContext::ADMIN_AJAX);
         }
         // Detect WP_Cron
-        if (defined('DOING_CRON') && DOING_CRON) {
+        if ($this->isCronRequest()) {
             return $this->factory->create(RequestTypeContext::CRON);
         }
-        // Detect WP_Cron
+        // Detect command line requests
         if (defined('WP_CLI') && WP_CLI) {
             return $this->factory->create(RequestTypeContext::CLI);
         }
@@ -75,22 +76,73 @@ class RequestTypeContextDetector
             return $this->factory->create(RequestTypeContext::ADMIN);
         }
         // Detect iFrames
-        if (
-        apply_filters(
-            'FHEE__EventEspresso_core_domain_services_contexts_RequestTypeContextDetector__detectRequestTypeContext__iframe_route',
-            $this->request->get('event_list', '') === 'iframe'
-            || $this->request->get('ticket_selector', '') === 'iframe'
-            || $this->request->get('calendar', '') === 'iframe'
-        )
-        ) {
+        if ($this->isIframeRoute()) {
             return $this->factory->create(RequestTypeContext::IFRAME);
         }
         // Detect Feeds
-        if ($this->request->get('feed', false)) {
+        if ($this->isFeedRequest()) {
             return $this->factory->create(RequestTypeContext::FEED);
         }
         // and by process of elimination...
         return $this->factory->create(RequestTypeContext::FRONTEND);
+    }
+
+
+    /**
+     * @return bool
+     */
+    private function isEspressoRestApiRequest()
+    {
+        $ee_rest_url_prefix = trim(rest_get_url_prefix(), '/');
+        $ee_rest_url_prefix .= '/' . Domain::API_NAMESPACE;
+        return $this->uriPathMatches($ee_rest_url_prefix);
+    }
+
+
+    /**
+     * @return bool
+     */
+    private function isCronRequest()
+    {
+        return $this->uriPathMatches('wp-cron.php');
+    }
+
+
+    /**
+     * @return bool
+     */
+    private function isFeedRequest()
+    {
+        return $this->uriPathMatches('feed');
+    }
+
+
+    /**
+     * @param string $component
+     * @return bool
+     */
+    private function uriPathMatches($component)
+    {
+        $request_uri = $this->request->requestUri();
+        $parts       = explode('?', $request_uri);
+        $path        = trim(reset($parts), '/');
+        return strpos($path, $component) === 0;
+    }
+
+
+    /**
+     * @return bool
+     */
+    private function isIframeRoute()
+    {
+        $is_iframe_route = apply_filters(
+            'FHEE__EventEspresso_core_domain_services_contexts_RequestTypeContextDetector__isIframeRoute',
+            $this->request->get('event_list', '') === 'iframe'
+            || $this->request->get('ticket_selector', '') === 'iframe'
+            || $this->request->get('calendar', '') === 'iframe',
+            $this
+        );
+        return filter_var($is_iframe_route, FILTER_VALIDATE_BOOLEAN);
     }
 
 }
