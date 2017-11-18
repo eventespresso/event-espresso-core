4d025e759f5ed7bc1f71b1f32d58fa66551198b0
Brent Christensen <brent@eventespresso.com>
2017-11-17 19:00:55 -0800

    record user agent on request; [touch:11174]

--- core/services/request_stack/middleware/BotDetector.php
+++ core/services/request_stack/middleware/BotDetector.php
@@ -33,8 +33,9 @@ class BotDetector extends Middleware
         $this->request  = $request;
         $this->response = $response;
         $CrawlerDetect = new CrawlerDetect;
-        // Check the user agent of the current 'visitor'
+        // Check and record the user agent of the current 'visitor'
         $this->request->setIsBot($CrawlerDetect->isCrawler());
+        $this->request->setUserAgent($CrawlerDetect->userAgent());
         $this->response = $this->process_request_stack($this->request, $this->response);
         return $this->response;
     }
