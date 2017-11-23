0c08c9e1fc1df41abb2bc5a5f3b0e4280e509750
Mike Nelson <michael@eventespresso.com>
2017-11-23 13:25:08 -0800

    [touch:11195]{t:30}code cleanup: added some more phpdocs, declared method visibility, used a strict string comparison

--- core/db_models/fields/EE_Text_Field_Base.php
+++ core/db_models/fields/EE_Text_Field_Base.php
@@ -7,9 +7,17 @@
 abstract class EE_Text_Field_Base extends EE_Model_Field_Base
 {
 
-    function prepare_for_get($value_of_field_on_model_object)
+    /**
+     * Gets the value in the format expected when being set.
+     * For display on the front-end, usually you would use prepare_for_pretty_echoing() instead.
+     * @param mixed $value_of_field_on_model_object
+     * @return mixed|string
+     */
+    public function prepare_for_get($value_of_field_on_model_object)
     {
-        return is_string($value_of_field_on_model_object) ? stripslashes($value_of_field_on_model_object) : $value_of_field_on_model_object;
+        return is_string($value_of_field_on_model_object)
+            ? stripslashes($value_of_field_on_model_object)
+            : $value_of_field_on_model_object;
     }
 
     /**
@@ -19,25 +27,25 @@ abstract class EE_Text_Field_Base extends EE_Model_Field_Base
      * @param string $schema
      * @return string
      */
-    function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null)
+    public function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null)
     {
-        if ($schema == 'form_input') {
-            $value_on_field_to_be_outputted = esc_attr($value_on_field_to_be_outputted);
+        if ($schema === 'form_input') {
+            $value_on_field_to_be_outputted = (string)esc_attr($value_on_field_to_be_outputted);
         }
-        return parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema);
+        return parent::prepare_for_pretty_echoing($value_on_field_to_be_outputted);
     }
 
     /**
      * Data received from the user should be exactly as they hope to save it in the DB, with the exception that
      * quotes need to have slashes added to it. (We used to call html_entity_decode on the value here,
      * because we called htmlentities when in EE_Text_Field_Base::prepare_for_pretty_echoing, but that's not necessary
-     * because web browsers always decode HTML entities once. So if we do it again here, we'll be removing
-     * HTML entities the user intended to have.)
+     * because web browsers always decode HTML entities in element attributes, like a form element's value attribute.
+     * So if we do it again here, we'll be removing HTML entities the user intended to have.)
      *
      * @param string $value_inputted_for_field_on_model_object
      * @return string
      */
-    function prepare_for_set($value_inputted_for_field_on_model_object)
+    public function prepare_for_set($value_inputted_for_field_on_model_object)
     {
         return stripslashes(parent::prepare_for_set($value_inputted_for_field_on_model_object));
     }
