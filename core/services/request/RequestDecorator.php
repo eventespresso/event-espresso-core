1c2ff051a0a4d8d48c655f5cafb5f8bde385b82a
Brent Christensen <brent@eventespresso.com>
2017-11-24 14:30:45 -0800

    RequestDecoratorInterface [touch:11174]

---
+++ core/services/request/RequestDecorator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace EventEspresso\core\services\request;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Interface RequestDecoratorInterface
+ *
+ * @package EventEspresso\core\services\request
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+interface RequestDecorator
+{
+
+    /**
+     * converts a Request to a Response
+     * can perform their logic either before or after the core application has run like so:
+     *    public function handle_request( EE_Request $request, EE_Response $response ) {
+     *        $this->request = $request;
+     *        $this->response = $response;
+     *      // logic performed BEFORE core app has run
+     *      $this->process_request_stack( $this->request, $this->response );
+     *      // logic performed AFTER core app has run
+     *      return $response;
+     *    }
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface      $response
+     * @return ResponseInterface
+     */
+    public function handleRequest(RequestInterface $request, ResponseInterface $response);
+}
+// Location: RequestDecoratorInterface.php
