4aa008f7fc323db708f3000ab4618b11d27b5b0f
Brent Christensen <brent@eventespresso.com>
2017-11-24 15:23:13 -0800

    deprecated  EE_Request_Stack_Builder  [touch:11174]

--- core/request_stack/EE_Request_Stack_Builder.core.php
+++ core/request_stack/EE_Request_Stack_Builder.core.php
@@ -5,100 +5,51 @@ defined('EVENT_ESPRESSO_VERSION') || exit;
 
 /**
  * Class EE_Request_Stack_Builder
- *
  * Assembles the EE_Request_Stack
  *
+ * @deprecated  4.9.53
  * @package 	Event Espresso
  * @subpackage 	core
  * @author 		Brent Christensen
  * @since 		4.8.20
- *
  */
 class EE_Request_Stack_Builder {
 
-	/**
-	 * Stack of middleware objects
-	 *
-	 * @type array $_middleware_stack
-	 */
-	protected $_middleware_stack;
-
-
-
-	/**
-	 * EE_Request_Stack_Builder
-	 */
-	public function __construct() {
-		$this->_middleware_stack = array();
-	}
-
-
+    /**
+     * EE_Request_Stack_Builder
+     */
+    public function __construct()
+    {
+        EE_Error::doing_it_wrong(
+            __METHOD__,
+            esc_html__(
+                'This class is deprecated. Please use EventEspresso\core\services\request_stack\RequestStackBuilder instead. All Event Espresso request stack classes have been moved to \core\services\request_stack and are now under the EventEspresso\core\services\request_stack namespace',
+                'event_espresso'
+            ),
+            '4.9.53'
+        );
+    }
 
     /**
-     * Add an EE_Middleware class to the beginning of the middleware_stack
-     * First parameter is the middleware classname,
-     * any number of arguments can also be passed, and detected via func_get_args()
-
-     * @ param $class_name
-     * @ param $args
-     *
-     * @return EE_Request_Stack_Builder
-     * @throws \InvalidArgumentException
+     * @deprecated  4.9.53
      */
 	public function unshift( /*$class_name, $args*/ ) {
-		if ( func_num_args() === 0 ) {
-			throw new InvalidArgumentException( 'Missing argument(s) when calling unshift' );
-		}
-		$middleware = func_get_args();
-		array_unshift( $this->_middleware_stack, $middleware );
-		return $this;
-	}
+    }
 
 
 
     /**
-     * Add an EE_Middleware class to the end of the middleware_stack
-     * First parameter is the middleware classname,
-     * any number of arguments can also be passed, and detected via func_get_args()
-
-     * @ param $class_name
-     * @ param $args
-     *
-     * @return EE_Request_Stack_Builder
-     * @throws \InvalidArgumentException
+     * @deprecated  4.9.53
      */
 	public function push( /*$class_name, $args...*/ ) {
-		if ( func_num_args() === 0 ) {
-			throw new InvalidArgumentException( 'Missing argument(s) when calling push' );
-		}
-        $middleware = func_get_args();
-        $this->_middleware_stack[] = $middleware;
-		return $this;
 	}
 
 
 
     /**
-     * builds decorated middleware stack
-     * by continuously injecting previous middleware app into the next
-     *
-     * @param EEI_Request_Decorator $application
-     * @return EE_Request_Stack
+     * @deprecated  4.9.53
      */
 	public function resolve( EEI_Request_Decorator $application ) {
-		$middlewares = array( $application );
-		foreach ( $this->_middleware_stack as $middleware_args ) {
-			$class_name = array_shift( $middleware_args );
-			if ( is_callable( $class_name ) ) {
-				$application = $class_name( $application );
-			} else {
-				array_unshift( $middleware_args, $application );
-                $reflection = new ReflectionClass($class_name);
-                $application = $reflection->newInstanceArgs($middleware_args);
-            }
-			array_unshift( $middlewares, $application );
-		}
-		return new EE_Request_Stack( $application, $middlewares );
 	}
 
 
