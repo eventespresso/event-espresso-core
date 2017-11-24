abeb6d01fe539b57f924701bdb760fe17f1a62c1
Brent Christensen <brent@eventespresso.com>
2017-11-24 15:47:04 -0800

    deprecated EE_Response [touch:11174]

--- core/request_stack/EE_Response.core.php
+++ core/request_stack/EE_Response.core.php
@@ -1,153 +1,169 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+<?php if (! defined('EVENT_ESPRESSO_VERSION')) {
+    exit('No direct script access allowed');
+}
+
+
+
 /**
  * class EE_Response
- *
  * Passes output, notices, and request termination status between EE_Middleware classes
  *
+ * @deprecated  4.9.53
  * @package         Event Espresso
- * @subpackage  /core/
+ * @subpackage      /core/
  * @author          Brent Christensen
- *
  * ------------------------------------------------------------------------
  */
-class EE_Response {
+class EE_Response
+{
 
-	/**
-	 * @access 	protected
-	 * @type 		array $_notice
-	 */
-	protected $_notice = array();
+    /**
+     * @access    protected
+     * @type        array $_notice
+     */
+    protected $_notice = array();
 
-	/**
-	 *    rendered output to be returned to WP
-	 * @access 	protected
-	 * @type 		string
-	 */
-	protected $_output = '';
+    /**
+     *    rendered output to be returned to WP
+     *
+     * @access    protected
+     * @type        string
+     */
+    protected $_output = '';
 
-	/**
-	 * @access 	protected
-	 * @type 		bool
-	 */
-	protected $request_terminated = false;
+    /**
+     * @access    protected
+     * @type        bool
+     */
+    protected $request_terminated = false;
 
-	/**
-	 * @access 	protected
-	 * @type 		bool
-	 */
-	protected $deactivate_plugin = false;
-
-
-
-	/**
-	 *    class constructor
-	 *
-	 * @access    public
-	 * @return \EE_Response
-	 */
-	public function __construct() {
-		$this->terminate_request( false );
-	}
+    /**
+     * @access    protected
+     * @type        bool
+     */
+    protected $deactivate_plugin = false;
 
 
 
-	/**
-	 *    set_notice
-	 *
-	 * @access    public
-	 * @param $key
-	 * @param $value
-	 * @return    void
-	 */
-	public function set_notice( $key, $value ) {
-		$this->_notice[ $key ] = $value;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @return \EE_Response
+     */
+    public function __construct()
+    {
+        $this->terminate_request(false);
+        EE_Error::doing_it_wrong(
+            __METHOD__,
+            esc_html__(
+                'This class is deprecated. Please use EventEspresso\core\services\request\Response instead. All Event Espresso request stack classes have been moved to \core\services\request and are now under the EventEspresso\core\services\request_stack namespace',
+                'event_espresso'
+            ),
+            '4.9.53'
+        );
+    }
 
 
 
-	/**
-	 *    get_notice
-	 *
-	 * @access    public
-	 * @param $key
-	 * @return    mixed
-	 */
-	public function get_notice( $key ) {
-		return isset( $this->_notice[ $key ] ) ? $this->_notice[ $key ] : NULL;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @param $key
+     * @param $value
+     * @return    void
+     */
+    public function set_notice($key, $value)
+    {
+        $this->_notice[ $key ] = $value;
+    }
 
 
 
-	/**
-	 *    get_notices
-	 *
-	 * @access    public
-	 * @return    array
-	 */
-	public function get_notices() {
-		return $this->_notice;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @param $key
+     * @return    mixed
+     */
+    public function get_notice($key)
+    {
+        return isset($this->_notice[ $key ]) ? $this->_notice[ $key ] : null;
+    }
 
 
 
-	/**
-	 *    add_output
-	 *
-	 * @access    public
-	 * @param $string
-	 * @param bool $append
-	 */
-	public function add_output( $string, $append = true ) {
-		$this->_output = $append ? $this->_output . $string : $string . $this->_output;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @return    array
+     */
+    public function get_notices()
+    {
+        return $this->_notice;
+    }
 
 
 
-	/**
-	 * 	get_output
-	 *
-	 *  @access 	public
-	 *  @return 	string
-	 */
-	public function get_output() {
-		return $this->_output;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @param      $string
+     * @param bool $append
+     */
+    public function add_output($string, $append = true)
+    {
+        $this->_output = $append ? $this->_output . $string : $string . $this->_output;
+    }
 
 
 
-	/**
-	 * @return boolean
-	 */
-	public function request_terminated() {
-		return $this->request_terminated;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @return    string
+     */
+    public function get_output()
+    {
+        return $this->_output;
+    }
 
 
 
-	/**
-	 * @param boolean $request_terminated
-	 */
-	public function terminate_request( $request_terminated = true ) {
-		$this->request_terminated = filter_var( $request_terminated, FILTER_VALIDATE_BOOLEAN );
-	}
+    /**
+     * @deprecated  4.9.53
+     * @return boolean
+     */
+    public function request_terminated()
+    {
+        return $this->request_terminated;
+    }
 
 
 
-	/**
-	 * @return boolean
-	 */
-	public function plugin_deactivated() {
-		return $this->deactivate_plugin;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @param boolean $request_terminated
+     */
+    public function terminate_request($request_terminated = true)
+    {
+        $this->request_terminated = filter_var($request_terminated, FILTER_VALIDATE_BOOLEAN);
+    }
 
 
 
-	/**
-	 * sets $deactivate_plugin to true
-	 */
-	public function deactivate_plugin() {
-		$this->deactivate_plugin = true;
-	}
+    /**
+     * @deprecated  4.9.53
+     * @return boolean
+     */
+    public function plugin_deactivated()
+    {
+        return $this->deactivate_plugin;
+    }
+
+
+
+    /**
+     * @deprecated  4.9.53
+     * sets $deactivate_plugin to true
+     */
+    public function deactivate_plugin()
+    {
+        $this->deactivate_plugin = true;
+    }
 
 
 }
