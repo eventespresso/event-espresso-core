1232c2858e4faf933896ed327760f874963e05e4
Brent Christensen <brent@eventespresso.com>
2017-11-27 16:36:48 -0800

    update session dependencies  [touch:11174]

--- core/EE_Dependency_Map.core.php
+++ core/EE_Dependency_Map.core.php
@@ -473,7 +473,7 @@ class EE_Dependency_Map
             ),
             'EE_Session'                                                                                                  => array(
                 'EventEspresso\core\services\cache\TransientCacheStorage' => EE_Dependency_Map::load_from_cache,
-                'EE_Request'                                              => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\request\Request'             => EE_Dependency_Map::load_from_cache,
                 'EE_Encryption'                                           => EE_Dependency_Map::load_from_cache,
             ),
             'EE_Cart'                                                                                                     => array(
