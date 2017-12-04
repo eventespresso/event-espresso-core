cf74b8275483626d408e9c6fa026232f80ad4f22
Darren Ethier <darren@roughsmootheng.in>
2017-12-04 10:54:19 -0500

    Rework Notice DTO [touch: 11199]

--- core/services/notices/Notice.php
+++ core/services/notices/Notice.php
@@ -2,11 +2,14 @@
 
 namespace EventEspresso\core\services\notices;
 
+use EventEspresso\core\domain\services\capabilities\CapCheck;
+use EventEspresso\core\domain\services\capabilities\CapCheckInterface;
+use EventEspresso\core\domain\services\capabilities\PublicCapabilities;
+use EventEspresso\core\domain\services\capabilities\RequiresCapCheckInterface;
 use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\services\route\RouteMatcher;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
-
-
 
 /**
  * Class Notice
@@ -16,7 +19,7 @@ defined('EVENT_ESPRESSO_VERSION') || exit;
  * @author        Brent Christensen
  * @since         $VID:$
  */
-class Notice implements NoticeInterface
+final class Notice implements NoticeInterface, RequiresCapCheckInterface
 {
 
     const ERROR       = 'error';
@@ -26,6 +29,12 @@ class Notice implements NoticeInterface
     const ATTENTION   = 'attention'; // alias for warning
 
     const INFORMATION = 'information';
+
+
+    /**
+     * @var
+     */
+    private $identifier;
 
     /**
      * @var string $type
@@ -65,24 +74,129 @@ class Notice implements NoticeInterface
 
 
     /**
+     * @var CapCheckInterface
+     */
+    private $capability_checker;
+
+
+    /**
+     * @var bool
+     */
+    private $is_persistent;
+
+
+    /**
+     * @var string
+     */
+    private $route_match_config_identifier;
+
+
+    /**
+     * @var  bool
+     */
+    private $override_dismissed;
+
+
+    /**
+     * @var bool
+     */
+    private $dismissed;
+
+
+    /**
+     * @var bool
+     */
+    private $processed;
+
+
+    /**
+     * @var bool
+     */
+    private $purge;
+
+
+
+    /**
+     * @var bool
+     */
+    private $show_on_next_request;
+
+
+    /**
      * Notice constructor.
      *
      * @param string $type
+     * @param string $identifier
      * @param string $message
-     * @param bool   $dismissible
-     * @param string $file
-     * @param string $func
-     * @param string $line
      * @throws InvalidDataTypeException
      */
-    public function __construct($type, $message, $dismissible = true, $file = '', $func = '', $line = '')
-    {
+    public function __construct(
+        $type,
+        $identifier,
+        $message
+    ) {
+        $this->setIdentifier($identifier);
         $this->setType($type);
         $this->setMessage($message);
-        $this->setDismissible($dismissible);
-        $this->setFile($file);
-        $this->setFunc($func);
-        $this->setLine($line);
+        $this->is_persistent = false;
+        $this->processed = false;
+        //set defaults
+        $this->setDismissible()
+             ->setShowOnNextRequest(true)
+             ->overrideDismissed(false)
+             ->setDismissed(false)
+             ->setPurge(false)
+             ->setRouteMatchConfigIdentifier(RouteMatcher::ROUTE_ANY);
+    }
+
+
+    /**
+     * @param $identifier
+     * @throws InvalidDataTypeException
+     */
+    private function setIdentifier($identifier)
+    {
+        if (empty($identifier) || ! is_string($identifier)) {
+            throw new InvalidDataTypeException(
+                '$identifier',
+                $identifier,
+                esc_html__('non empty string', 'event_espresso')
+            );
+        }
+        $this->identifier = $identifier;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+
+    /**
+     * Internally used to set the cap checker.
+     *
+     * @param CapCheckInterface $capability_checker
+     */
+    private function setCapabilityCheck(CapCheckInterface $capability_checker)
+    {
+        $this->capability_checker = $capability_checker;
+    }
+
+
+    /**
+     * Returns what capability is required for viewing.  Defaults to publicly viewable if not explicitly set.
+     *
+     * @return CapCheckInterface
+     */
+    public function getCapCheck()
+    {
+        return $this->capability_checker instanceof CapCheckInterface
+            ? $this->capability_checker
+            : new PublicCapabilities('', 'read notice');
     }
 
 
@@ -92,13 +206,13 @@ class Notice implements NoticeInterface
      */
     private function types()
     {
-        return (array)apply_filters(
+        return (array) apply_filters(
             'FHEE__EventEspresso_core_services_notices_Notice__types',
             array(
-                Notice::ERROR,
-                Notice::SUCCESS,
-                Notice::ATTENTION,
-                Notice::INFORMATION,
+                self::ERROR,
+                self::SUCCESS,
+                self::ATTENTION,
+                self::INFORMATION,
             )
         );
     }
@@ -130,7 +244,7 @@ class Notice implements NoticeInterface
      */
     public function file()
     {
-        return $this->file;
+        return $this->file === null ? '' : $this->file;
     }
 
 
@@ -140,7 +254,7 @@ class Notice implements NoticeInterface
      */
     public function func()
     {
-        return $this->func;
+        return $this->func === null ? '' : $this->func;
     }
 
 
@@ -150,7 +264,7 @@ class Notice implements NoticeInterface
      */
     public function line()
     {
-        return $this->line;
+        return $this->line === null ? '' : $this->line;
     }
 
 
@@ -218,14 +332,14 @@ class Notice implements NoticeInterface
     }
 
 
-
     /**
      * @param string $file
+     * @return Notice
      * @throws InvalidDataTypeException
      */
     private function setFile($file)
     {
-        if ($this->type === Notice::ERROR && (empty($file) || ! is_string($file))) {
+        if ($this->type === self::ERROR && (empty($file) || ! is_string($file))) {
             throw new InvalidDataTypeException(
                 '$file',
                 $file,
@@ -233,17 +347,18 @@ class Notice implements NoticeInterface
             );
         }
         $this->file = $file;
+        return $this;
     }
-
 
 
     /**
      * @param string $func
+     * @return Notice
      * @throws InvalidDataTypeException
      */
     private function setFunc($func)
     {
-        if ($this->type === Notice::ERROR && (empty($func) || ! is_string($func))) {
+        if ($this->type === self::ERROR && (empty($func) || ! is_string($func))) {
             throw new InvalidDataTypeException(
                 '$func',
                 $func,
@@ -251,18 +366,19 @@ class Notice implements NoticeInterface
             );
         }
         $this->func = $func;
+        return $this;
     }
-
 
 
     /**
      * @param int $line
+     * @return Notice
      * @throws InvalidDataTypeException
      */
     private function setLine($line)
     {
         $line = absint($line);
-        if ($this->type === Notice::ERROR && $line === 0) {
+        if ($this->type === self::ERROR && $line === 0) {
             throw new InvalidDataTypeException(
                 '$line',
                 $line,
@@ -270,15 +386,250 @@ class Notice implements NoticeInterface
             );
         }
         $this->line = $line;
+        return $this;
     }
 
 
     /**
-     * @param boolean $dismissible
+     * Public method for more handily setting the file/function/line parameters.
+     * @param $file
+     * @param $function
+     * @param $line
+     * @throws InvalidDataTypeException
+     * @return Notice
      */
-    private function setDismissible($dismissible = true)
+    public function setFileFunctionLine($file, $function, $line)
     {
-        $this->dismissible = filter_var($dismissible, FILTER_VALIDATE_BOOLEAN);
+        $this->setFile($file)->setFunc($function)->setLine($line);
+        return $this;
     }
 
+
+    /**
+     * Set whether the notice should be dismissible or not
+     *
+     * @param boolean $dismissible
+     * @return Notice
+     */
+    public function setDismissible($dismissible = true)
+    {
+        $this->dismissible = filter_var($dismissible, FILTER_VALIDATE_BOOLEAN);
+        return $this;
+    }
+
+
+    /**
+     * Set the RouteMatch config identifier associated with this notice.
+     * This is used along with the RouteMatcher to control where the notice is displayed.  By default, the notice is
+     * displayed everywhere (because typically a notice is only displayed one time on the route its set on).
+     *
+     * For persistent notices this becomes more important.  See `EventEspresso\core\services\RouteMatcher` for more
+     * details.
+     *
+     * Note: if the string set here does not match a registered route match config
+     *
+     * @param string $route_match_config_identifier
+     * @return Notice
+     */
+    public function setRouteMatchConfigIdentifier($route_match_config_identifier)
+    {
+        $this->route_match_config_identifier = (string) $route_match_config_identifier;
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getRouteMatchConfigIdentifier()
+    {
+        if (empty($this->route_match_config_identifier)) {
+            return RouteMatcher::ROUTE_ANY;
+        }
+        return $this->route_match_config_identifier;
+    }
+
+
+    /**
+     * @param $capability
+     * @param $capability_context
+     * @return Notice
+     */
+    public function setCapabilityRequired($capability, $capability_context = '')
+    {
+        $capability_context = $capability_context === ''
+            ? 'view ' . $this->type() . ' notice'
+            : (string) $capability_context;
+        if (! empty($capability)) {
+            $this->setCapabilityCheck(
+                new CapCheck(
+                    $capability,
+                    $capability_context
+                )
+            );
+        }
+        return $this;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function dismissedOverridden()
+    {
+        return $this->override_dismissed;
+    }
+
+
+    /**
+     * @param $dismissed
+     * @return Notice
+     */
+    public function overrideDismissed($dismissed)
+    {
+        $this->dismissed = filter_var($dismissed, FILTER_VALIDATE_BOOLEAN);
+        return $this;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isDismissed()
+    {
+        return $this->dismissed;
+    }
+
+    /**
+     * Fluent setter for setting whether the notice was dismissed by the user.
+     *
+     * @param $dismissed
+     * @return Notice
+     */
+    public function setDismissed($dismissed)
+    {
+        $this->dismissed = filter_var($dismissed, FILTER_VALIDATE_BOOLEAN);
+        return $this;
+    }
+
+
+    /**
+     * Only allows toggling this Notice as a persistent message.  Defaults for the persistent message will be set if
+     * they have not already been set on the Notice.
+     *
+     * @return Notice
+     */
+    public function setPersistent()
+    {
+        $this->is_persistent = true;
+        if ($this->getRouteMatchConfigIdentifier() === RouteMatcher::ROUTE_ANY) {
+            //default persistent notices to any admin route
+            $this->setRouteMatchConfigIdentifier(RouteMatcher::ROUTE_ADMIN_ANY);
+        }
+        if (! $this->capability_checker instanceof CapCheckInterface) {
+            $this->setCapabilityCheck(
+                new CapCheck(
+                    'manage_options',
+                    'viewing persistent notice'
+                )
+            );
+        }
+        return $this;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isPersistent()
+    {
+        return $this->is_persistent === true;
+    }
+
+
+
+    /**
+     * Return whether a notice has been flagged as processed or not.
+     * @return bool
+     */
+    public function isProcessed()
+    {
+        return $this->processed;
+    }
+
+
+    /**
+     * Flag a notice as having been processed.
+     * @return Notice
+     */
+    public function setProcessed()
+    {
+        $this->processed = true;
+        return $this;
+    }
+
+    /**
+     * Fluent setter used to set that the notice should be purged from any persistence (mostly for Persistent notices)
+     *
+     * @param bool $purge
+     * @return Notice
+     */
+    public function setPurge($purge)
+    {
+        $this->purge = filter_var($purge, FILTER_VALIDATE_BOOLEAN);
+        return $this;
+    }
+
+    /**
+     * Return whether to purge this notice or not.
+     *
+     * @return bool
+     */
+    public function purge()
+    {
+        return $this->purge;
+    }
+
+    /**
+     * Fluent setter for setting the notice to display on the next request.
+     *
+     * @param $show
+     * @return Notice
+     */
+    public function setShowOnNextRequest($show)
+    {
+        $this->show_on_next_request = filter_var($show, FILTER_VALIDATE_BOOLEAN);
+        return $this;
+    }
+
+    /**
+     * Whether the notice should show on the next request or not.
+     *
+     * @return bool
+     */
+    public function showOnNextRequest()
+    {
+        return $this->show_on_next_request;
+    }
+
+
+    /**
+     * Returns the notice information as an array for persistence to the db.
+     */
+    public function toArray()
+    {
+        return array(
+            'type' => $this->type(),
+            'identifier' => $this->getIdentifier(),
+            'message' => $this->message(),
+            'route_match_config_identifier' => $this->getRouteMatchConfigIdentifier(),
+            'capability' => $this->getCapCheck()->capability(),
+            'context' => $this->getCapCheck()->context(),
+            'dismissed' => $this->isDismissed(),
+            'dismissible' => $this->isDismissible(),
+            'file' => $this->file(),
+            'func' => $this->func(),
+            'line' => $this->line()
+        );
+    }
 }
--- core/services/notices/NoticeInterface.php
+++ core/services/notices/NoticeInterface.php
@@ -4,15 +4,13 @@ namespace EventEspresso\core\services\notices;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
-
-
 /**
  * Class Notice
  * DTO for temporarily holding notification information until it can be processed
  *
  * @package       Event Espresso
  * @author        Brent Christensen
- * @since         $VID:$
+ * @since         4.9.53.rc
  */
 interface NoticeInterface
 {
@@ -36,6 +34,94 @@ interface NoticeInterface
 
 
     /**
+     * @return bool
+     */
+    public function isPersistent();
+
+
+    /**
+     * @return string
+     */
+    public function getIdentifier();
+
+
+    /**
+     * Fluent setter for setting the file/function/line properties.
+     * @param $file
+     * @param $function
+     * @param $line
+     * @return NoticeInterface
+     */
+    public function setFileFunctionLine($file, $function, $line);
+
+
+    /**
+     * Fluent setter for setting whether the notice should be dismissible or not.
+     * @param bool $dismissible
+     * @return NoticeInterface
+     */
+    public function setDismissible($dismissible = true);
+
+
+    /**
+     * Fluent setter for setting what RouteMatchConfig should be used for determining if the notice applies to that
+     * route.
+     * @param string $route_match_config_identifier
+     * @return NoticeInterface
+     */
+    public function setRouteMatchConfigIdentifier($route_match_config_identifier);
+
+
+    /**
+     * Returns what was set as the RouteMatchConfigIdentifier string attached to this notice.
+     *
+     * @return string
+     */
+    public function getRouteMatchConfigIdentifier();
+
+
+    /**
+     * Whether this notice has been set for ignoring any internal dismissed flags set.
+     * @return bool
+     */
+    public function dismissedOverridden();
+
+
+    /**
+     * Fluent setter for overriding the internal dismissed flag.  This applies when the dismissed item is persisted to
+     * the db.
+     *
+     * @param bool $override_dismissed
+     * @return NoticeInterface
+     */
+    public function overrideDismissed($override_dismissed);
+
+
+    /**
+     * Return whether the notice has been dismissed.
+     * @return bool
+     */
+    public function isDismissed();
+
+
+    /**
+     * Fluent setter for setting whether the notice was dismissed by the user.
+     * @param $dismissed
+     * @return NoticeInterface
+     */
+    public function setDismissed($dismissed);
+
+
+    /**
+     * Fluent setter for flagging the notice as a persistent notice (a notice that persists across all defined routes
+     * until dismissed.
+     * @return NoticeInterface
+     */
+    public function setPersistent();
+
+
+
+    /**
      * @return string
      */
     public function file();
@@ -53,5 +139,65 @@ interface NoticeInterface
     public function line();
 
 
+    /**
+     * Returns whether the notice has been automatically processed in the request yet or not.
+     * @return bool
+     */
+    public function isProcessed();
 
+
+    /**
+     * Fluent setter for flagging that the notice has been processed.
+     * @return NoticeInterface
+     */
+    public function setProcessed();
+
+
+    /**
+     * Fluent setter for setting the capability and capability context.
+     *
+     * @param string $capability
+     * @param string $capability_context
+     * @return NoticeInterface
+     */
+    public function setCapabilityRequired($capability, $capability_context);
+
+
+    /**
+     * Fluent setter used to set that the notice should be purged from any persistence (mostly for Persistent notices)
+     *
+     * @param bool $purge
+     * @return NoticeInterface
+     */
+    public function setPurge($purge);
+
+
+    /**
+     * Return whether to purge this notice or not.
+     * @return bool
+     */
+    public function purge();
+
+
+    /**
+     * Return the notice properties as an array of key/value pairs where the key is the property name and the value is
+     * the value on the property.
+     * @return array
+     */
+    public function toArray();
+
+
+    /**
+     * Fluent setter for setting the notice to display on the next request.
+     * @param $show
+     * @return NoticeInterface
+     */
+    public function setShowOnNextRequest($show);
+
+
+    /**
+     * Whether the notice should show on the next request or not.
+     * @return bool
+     */
+    public function showOnNextRequest();
 }
