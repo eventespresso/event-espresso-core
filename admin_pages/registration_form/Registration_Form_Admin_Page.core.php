a80be9a34005ce106ddd13033b46aefc59ce6ea1
Mike Nelson <michael@eventespresso.com>
2017-12-28 16:07:37 -0800

    [touch:11206]{t:30}pass data to the script so it knows which question types have options and which should have a max input (rather than having that logic itself)

--- admin_pages/registration_form/Registration_Form_Admin_Page.core.php
+++ admin_pages/registration_form/Registration_Form_Admin_Page.core.php
@@ -256,20 +256,29 @@ class Registration_Form_Admin_Page extends EE_Admin_Page
 
     public function load_scripts_styles_add_question()
     {
-        $this->load_scripts_styles_forms();
-        wp_register_script('espresso_registration_form_single',
-            REGISTRATION_FORM_ASSETS_URL . 'espresso_registration_form_admin.js', array('jquery-ui-sortable'),
-            EVENT_ESPRESSO_VERSION, true);
-        wp_enqueue_script('espresso_registration_form_single');
+        $this->load_scripts_styles_question_details();
     }
 
     public function load_scripts_styles_edit_question()
+    {
+        $this->load_scripts_styles_question_details();
+    }
+
+    protected function load_scripts_styles_question_details()
     {
         $this->load_scripts_styles_forms();
         wp_register_script('espresso_registration_form_single',
             REGISTRATION_FORM_ASSETS_URL . 'espresso_registration_form_admin.js', array('jquery-ui-sortable'),
             EVENT_ESPRESSO_VERSION, true);
         wp_enqueue_script('espresso_registration_form_single');
+        wp_localize_script(
+            'espresso_registration_form_single',
+            'ee_question_data',
+            array(
+                'question_types_with_max' => EEM_Question::instance()->questionTypesWithMaxLength(),
+                'question_type_with_options' => EEM_Question::instance()->question_types_with_options()
+            )
+        );
     }
 
 
