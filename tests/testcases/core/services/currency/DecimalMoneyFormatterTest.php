5791182f10a823c8470a00d17ecf1a845707a3f8
Brent Christensen <brent@eventespresso.com>
2018-01-03 14:08:37 -0800

    add unit test to reveal problem; [touch:10619]

--- tests/testcases/core/services/currency/DecimalMoneyFormatterTest.php
+++ tests/testcases/core/services/currency/DecimalMoneyFormatterTest.php
@@ -32,6 +32,22 @@ class DecimalMoneyFormatterTest extends \EE_UnitTestCase
             '1234.50'
         );
     }
+
+    /**
+     * @group Money
+     */
+    public function test_format_with_no_decimals()
+    {
+        $currency_factory = LoaderFactory::getLoader()->getShared(
+            'EventEspresso\core\services\currency\CurrencyFactory'
+        );
+        $currency = $currency_factory->createFromCountryCode('US');
+        $formatter= new DecimalCurrencyAmountFormatter();
+        $this->assertEquals(
+            $formatter->format(1234, $currency),
+            '1234.00'
+        );
+    }
 }
 // End of file DecimalMoneyFormatterTest.php
 // Location: tests/testcases/core/services/currency/DecimalMoneyFormatterTest.php
