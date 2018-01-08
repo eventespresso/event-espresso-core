16895b5428ec3ce843baf68318f437fc4f6ecf28
Darren Ethier <darren@roughsmootheng.in>
2018-01-08 16:09:28 -0500

    acceptance tests: additions to helper/page

--- acceptance_tests/Helpers/RegistrationsAdmin.php
+++ acceptance_tests/Helpers/RegistrationsAdmin.php
@@ -2,6 +2,7 @@
 
 namespace EventEspresso\Codeception\helpers;
 
+use Page\CoreAdmin;
 use Page\RegistrationsAdmin as RegistrationsAdminPage;
 
 /**
@@ -40,5 +41,18 @@ trait RegistrationsAdmin
         );
         //wait for page to fully load
         $this->actor()->wait(5);
+    }
+
+
+    /**
+     * Will enter the provided value in the registration list table search field and execute a search for that value.
+     * @param string $search_text
+     */
+    public function searchForRegistrationOnRegistrationListTableWithText($search_text)
+    {
+        $this->amOnDefaultRegistrationsListTableAdminPage();
+        $this->actor()->fillField(RegistrationsAdminPage::SEARCH_INPUT_SELECTOR_LIST_TABLE_REGISTRATION, $search_text);
+        $this->actor()->click(CoreAdmin::LIST_TABLE_SEARCH_SUBMIT_SELECTOR);
+        $this->actor()->waitForText('Displaying search results for');
     }
 }
