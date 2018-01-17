a6650f828ec8b619bd925ab756d4aaf38be08bfd
Darren Ethier <darren@roughsmootheng.in>
2018-01-17 09:51:21 -0500

    formatting and phpdoc fixes [touch: 4986]

--- admin_pages/registrations/EE_Attendee_Contact_List_Table.class.php
+++ admin_pages/registrations/EE_Attendee_Contact_List_Table.class.php
@@ -1,27 +1,38 @@
-<?php if (! defined('EVENT_ESPRESSO_VERSION')) {
-    exit('No direct script access allowed');
-}
+<?php
+
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+
+defined('EVENT_ESPRESSO_VERSION') || exit('No direct access allowed.');
 
 
+/**
+ * EE_Attendee_Contact_List_Table
+ * List table for the EE_Attendee records in the admin.
+ *
+ * @package EventEspresso
+ * @author  Darren Ethier
+ */
 class EE_Attendee_Contact_List_Table extends EE_Admin_List_Table
 {
 
-
-    public function __construct($admin_page)
-    {
-        parent::__construct($admin_page);
-    }
-
-
+    /**
+     * Initial setup of data (called by parent).
+     */
     protected function _setup_data()
     {
-        $this->_data = $this->_view != 'trash' ? $this->_admin_page->get_attendees($this->_per_page) : $this->_admin_page->get_attendees($this->_per_page,
-            false, true);
-        $this->_all_data_count = $this->_view != 'trash' ? $this->_admin_page->get_attendees($this->_per_page,
-            true) : $this->_admin_page->get_attendees($this->_per_page, true, true);
+        $this->_data = $this->_view != 'trash'
+            ? $this->_admin_page->get_attendees($this->_per_page)
+            : $this->_admin_page->get_attendees($this->_per_page, false, true);
+        $this->_all_data_count = $this->_view != 'trash'
+            ? $this->_admin_page->get_attendees($this->_per_page, true)
+            : $this->_admin_page->get_attendees($this->_per_page, true, true);
     }
 
 
+    /**
+     * Initial setup of properties.
+     */
     protected function _set_properties()
     {
         $this->_wp_list_args = array(
@@ -58,42 +69,72 @@ class EE_Attendee_Contact_List_Table extends EE_Admin_List_Table
     }
 
 
+    /**
+     * Initial setup of filters
+     * @return array
+     */
     protected function _get_table_filters()
     {
         return array();
     }
 
 
+    /**
+     * Initial setup of counts for views
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
     protected function _add_view_counts()
     {
         $this->_views['in_use']['count'] = $this->_admin_page->get_attendees($this->_per_page, true);
-        if (EE_Registry::instance()->CAP->current_user_can('ee_delete_contacts',
-            'espresso_registrations_delete_registration')) {
+        if (EE_Registry::instance()->CAP->current_user_can(
+            'ee_delete_contacts',
+            'espresso_registrations_delete_registration'
+        )) {
             $this->_views['trash']['count'] = $this->_admin_page->get_attendees($this->_per_page, true, true);
         }
     }
 
 
+    /**
+     * Get count of attendees.
+     * @return int
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
     protected function _get_attendees_count()
     {
         return EEM_Attendee::instance()->count();
     }
 
 
-    protected function _get_attendees_trash_count()
+    /**
+     * Checkbox column
+     *
+     * @param EE_Attendee $item
+     * @return string
+     * @throws EE_Error
+     */
+    public function column_cb($item)
     {
-
+        return sprintf(
+            '<input type="checkbox" name="checkbox[%1$s]" value="%1$s" />',
+            $item->ID()
+        );
     }
 
 
-    function column_cb($item)
-    {
-        return sprintf('<input type="checkbox" name="checkbox[%1$s]" value="%1$s" />', /* $1%s */
-            $item->ID());
-    }
-
-
-    function column_ATT_ID($item)
+    /**
+     * ATT_ID column
+     *
+     * @param EE_Attendee $item
+     * @return string
+     * @throws EE_Error
+     */
+    public function column_ATT_ID($item)
     {
         $content = $item->ID();
         $attendee_name = $item instanceof EE_Attendee ? $item->full_name() : '';
@@ -102,59 +143,99 @@ class EE_Attendee_Contact_List_Table extends EE_Admin_List_Table
     }
 
 
-    function column_ATT_lname($item)
+    /**
+     * ATT_lname column
+     *
+     * @param EE_Attendee $item
+     * @return string
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     * @throws EE_Error
+     */
+    public function column_ATT_lname($item)
     {
-
         // edit attendee link
-        $edit_lnk_url = EE_Admin_Page::add_query_args_and_nonce(array(
-            'action' => 'edit_attendee',
-            'post'   => $item->ID(),
-        ), REG_ADMIN_URL);
-        $name_link = EE_Registry::instance()->CAP->current_user_can('ee_edit_contacts',
-            'espresso_registrations_edit_attendee') ? '<a href="' . $edit_lnk_url . '" title="' . esc_attr__('Edit Contact',
-                'event_espresso') . '">' . $item->lname() . '</a>' : $item->lname();
+        $edit_lnk_url = EE_Admin_Page::add_query_args_and_nonce(
+            array(
+                'action' => 'edit_attendee',
+                'post'   => $item->ID(),
+            ),
+            REG_ADMIN_URL
+        );
+        $name_link = EE_Registry::instance()->CAP->current_user_can(
+            'ee_edit_contacts',
+            'espresso_registrations_edit_attendee'
+        )
+            ? '<a href="' . $edit_lnk_url . '" title="'
+              . esc_attr__('Edit Contact', 'event_espresso') . '">'
+              . $item->lname() . '</a>'
+            : $item->lname();
         return $name_link;
-
     }
 
 
-    function column_ATT_fname($item)
+    /**
+     * ATT_fname column
+     *
+     * @param EE_Attendee $item
+     * @return string
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     * @throws EE_Error
+     */
+    public function column_ATT_fname($item)
     {
-
         //Build row actions
         $actions = array();
         // edit attendee link
-        if (EE_Registry::instance()->CAP->current_user_can('ee_edit_contacts',
-            'espresso_registrations_edit_attendee')) {
-            $edit_lnk_url = EE_Admin_Page::add_query_args_and_nonce(array(
-                'action' => 'edit_attendee',
-                'post'   => $item->ID(),
-            ), REG_ADMIN_URL);
-            $actions['edit'] = '<a href="' . $edit_lnk_url . '" title="' . esc_attr__('Edit Contact',
-                    'event_espresso') . '">' . __('Edit', 'event_espresso') . '</a>';
+        if (EE_Registry::instance()->CAP->current_user_can(
+            'ee_edit_contacts',
+            'espresso_registrations_edit_attendee'
+        )) {
+            $edit_lnk_url = EE_Admin_Page::add_query_args_and_nonce(
+                array(
+                    'action' => 'edit_attendee',
+                    'post'   => $item->ID(),
+                ),
+                REG_ADMIN_URL
+            );
+            $actions['edit'] = '<a href="' . $edit_lnk_url . '" title="'
+                               . esc_attr__('Edit Contact', 'event_espresso') . '">'
+                               . __('Edit', 'event_espresso') . '</a>';
         }
 
         if ($this->_view == 'in_use') {
             // trash attendee link
-            if (EE_Registry::instance()->CAP->current_user_can('ee_delete_contacts',
-                'espresso_registrations_trash_attendees')) {
-                $trash_lnk_url = EE_Admin_Page::add_query_args_and_nonce(array(
-                    'action' => 'trash_attendee',
-                    'ATT_ID' => $item->ID(),
-                ), REG_ADMIN_URL);
-                $actions['trash'] = '<a href="' . $trash_lnk_url . '" title="' . esc_attr__('Move Contact to Trash',
-                        'event_espresso') . '">' . __('Trash', 'event_espresso') . '</a>';
+            if (EE_Registry::instance()->CAP->current_user_can(
+                'ee_delete_contacts',
+                'espresso_registrations_trash_attendees')
+            ) {
+                $trash_lnk_url = EE_Admin_Page::add_query_args_and_nonce(
+                    array(
+                        'action' => 'trash_attendee',
+                        'ATT_ID' => $item->ID(),
+                    ),
+                    REG_ADMIN_URL
+                );
+                $actions['trash'] = '<a href="' . $trash_lnk_url . '" title="'
+                                    . esc_attr__('Move Contact to Trash', 'event_espresso')
+                                    . '">' . __('Trash', 'event_espresso') . '</a>';
             }
         } else {
-            if (EE_Registry::instance()->CAP->current_user_can('ee_delete_contacts',
-                'espresso_registrations_restore_attendees')) {
+            if (EE_Registry::instance()->CAP->current_user_can(
+                'ee_delete_contacts',
+                'espresso_registrations_restore_attendees')
+            ) {
                 // restore attendee link
                 $restore_lnk_url = EE_Admin_Page::add_query_args_and_nonce(array(
                     'action' => 'restore_attendees',
                     'ATT_ID' => $item->ID(),
                 ), REG_ADMIN_URL);
-                $actions['restore'] = '<a href="' . $restore_lnk_url . '" title="' . esc_attr__('Restore Contact',
-                        'event_espresso') . '">' . __('Restore', 'event_espresso') . '</a>';
+                $actions['restore'] = '<a href="' . $restore_lnk_url . '" title="'
+                                      . esc_attr__('Restore Contact', 'event_espresso') . '">'
+                                      . __('Restore', 'event_espresso') . '</a>';
             }
         }
 
@@ -162,34 +243,63 @@ class EE_Attendee_Contact_List_Table extends EE_Admin_List_Table
             'action' => 'edit_attendee',
             'post'   => $item->ID(),
         ), REG_ADMIN_URL);
-        $name_link = EE_Registry::instance()->CAP->current_user_can('ee_edit_contacts',
-            'espresso_registrations_edit_attendee') ? '<a href="' . $edit_lnk_url . '" title="' . esc_attr__('Edit Contact',
-                'event_espresso') . '">' . $item->fname() . '</a>' : $item->fname();
+        $name_link = EE_Registry::instance()->CAP->current_user_can(
+            'ee_edit_contacts',
+            'espresso_registrations_edit_attendee'
+        )
+            ? '<a href="' . $edit_lnk_url . '" title="'
+              . esc_attr__('Edit Contact', 'event_espresso') . '">' . $item->fname() . '</a>'
+            : $item->fname();
 
         //Return the name contents
         return sprintf('%1$s %2$s', $name_link, $this->row_actions($actions));
     }
 
 
-    function column_ATT_email($item)
+    /**
+     * Email Column
+     * @param EE_Attendee $item
+     * @return string
+     */
+    public function column_ATT_email($item)
     {
         return '<a href="mailto:' . $item->email() . '">' . $item->email() . '</a>';
     }
 
 
-    function column_ATT_address($item)
+    /**
+     * ATT_address column
+     *
+     * @param EE_Attendee $item
+     * @return mixed
+     */
+    public function column_ATT_address($item)
     {
         return $item->address();
     }
 
 
-    function column_ATT_city($item)
+    /**
+     * ATT_city column
+     * @param EE_Attendee $item
+     * @return mixed
+     */
+    public function column_ATT_city($item)
     {
         return $item->city();
     }
 
 
-    function column_STA_ID($item)
+    /**
+     * State Column
+     * @param EE_Attendee $item
+     * @return string
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
+    public function column_STA_ID($item)
     {
         $states = EEM_State::instance()->get_all_states();
         $state = isset($states[$item->state_ID()]) ? $states[$item->state_ID()]->get('STA_name') : $item->state_ID();
@@ -197,19 +307,32 @@ class EE_Attendee_Contact_List_Table extends EE_Admin_List_Table
     }
 
 
-    function column_CNT_ISO($item)
+    /**
+     * Country Column
+     * @param EE_Attendee $item
+     * @return string
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
+    public function column_CNT_ISO($item)
     {
         $countries = EEM_Country::instance()->get_all_countries();
-        //EEH_Debug_Tools::printr( $countries, '$countries  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span>', 'auto' );
-        $country = isset($countries[$item->country_ID()]) ? $countries[$item->country_ID()]->get('CNT_name') : $item->country_ID();
+        $country = isset($countries[$item->country_ID()])
+            ? $countries[$item->country_ID()]->get('CNT_name')
+            : $item->country_ID();
         return ! is_numeric($country) ? $country : '';
     }
 
 
-    function column_ATT_phone($item)
+    /**
+     * Phone Number column
+     * @param EE_Attendee $item
+     * @return mixed
+     */
+    public function column_ATT_phone($item)
     {
         return $item->phone();
     }
-
-
 }
