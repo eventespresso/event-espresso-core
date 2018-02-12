e7274259e91a8fdfc03a26c83f225113ca786c64
Mike Nelson <michael@eventespresso.com>
2018-02-12 10:12:29 -0800

    [touch:11325]{t:10}dont use EE_Log::log because it write to a public folder

--- core/libraries/plugin_api/EE_Register_Addon.lib.php
+++ core/libraries/plugin_api/EE_Register_Addon.lib.php
@@ -1156,9 +1156,7 @@ class EE_Register_Addon implements EEI_Plugin_API
                 //the add-on was not yet registered in the registry, so RegistryContainer::__get() throws this exception.
                 //also no need to worry about this or log it, it's ok to deregister an add-on before its registered in the registry
             } catch (Exception $e) {
-                EE_Log::instance()->log(
-                    __FILE__,
-                    __FUNCTION__,
+                error_log(
                     sprintf(
                         esc_html__('Exception of type %1$s caught while deregistering add-on $2$s, its message was "%3$s" and stack trace was "%4$s"', 'event_espresso'),
                         get_class($e),
