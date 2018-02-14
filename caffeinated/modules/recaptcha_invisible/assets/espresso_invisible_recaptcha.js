5b5cd0ceda32a8e0f1eda13ae0024040b7b7ebdd
Brent Christensen <brent@eventespresso.com>
2018-02-13 19:20:12 -0800

    check that localized var is set; use supplied submit_button_id if set; wrap console logs in check for debug var; disable submit button class after clicking;  [touch:9575]

--- caffeinated/modules/recaptcha_invisible/assets/espresso_invisible_recaptcha.js
+++ caffeinated/modules/recaptcha_invisible/assets/espresso_invisible_recaptcha.js
@@ -5,34 +5,65 @@
  * @version 0.1
  * @author  Event Espresso
  * @link    http://www.eventespresso.com
+ *
+ * @namespace {object} eeRecaptcha
+ * @type {{
+ *     siteKey: string,
+ *     submit_button_id: string,
+ *     wp_debug: boolean,
+ * }}
+ *
+ * @namespace {object} grecaptcha
+ * @type {{
+ *     render: function,
+ *     execute: function,
+ * }}
  */
 function espressoLoadRecaptcha() {
     jQuery(document).ready(function($) {
-        if (typeof eeRecaptcha !== undefined) {
-            // console.log(JSON.stringify('eeRecaptcha.siteKey: ' + eeRecaptcha.siteKey, null, 4));
+        if (typeof eeRecaptcha !== 'undefined') {
+            var debug = eeRecaptcha.wp_debug;
+            if (debug) {
+                console.log(JSON.stringify('eeRecaptcha.siteKey: ' + eeRecaptcha.siteKey, null, 4));
+            }
             $('.g-recaptcha').each(function() {
                 var $recaptcha_div = $(this),
                     $recaptcha_id  = $recaptcha_div.attr('id'),
                     $form          = $(this).parents('form:first'),
-                    $submit        = $form.find(':submit');
-                // console.log(JSON.stringify('recaptcha id: ' + $recaptcha_id, null, 4));
-                // console.log(JSON.stringify('form name: ' + $form.attr('name'), null, 4));
-                // console.log(JSON.stringify('submit id: ' + $submit.attr('id'), null, 4));
-                widgetId = grecaptcha.render($recaptcha_id, {
-                    "sitekey":  eeRecaptcha.siteKey,
-                    "callback": function(token) {
-                        // console.log(JSON.stringify('eeRecaptcha.token: ' + token, null, 4));
-                        $recaptcha_div.parent().find(".g-recaptcha-response").val(token);
-                        $form.submit();
+                    $submit        = eeRecaptcha.submit_button_id !== ''
+                        ? $('#' + eeRecaptcha.submit_button_id)
+                        : $form.find(':submit');
+                if (debug) {
+                    console.log(JSON.stringify('recaptcha id: ' + $recaptcha_id, null, 4));
+                    console.log(JSON.stringify('form name: ' + $form.attr('name'), null, 4));
+                    console.log(JSON.stringify('eeRecaptcha.submit_button_id: ' + eeRecaptcha.submit_button_id, null, 4));
+                    console.log(JSON.stringify('submit id: ' + $submit.attr('id'), null, 4));
+                }
+                var widgetId = grecaptcha.render(
+                    $recaptcha_id,
+                    {
+                        'sitekey':  eeRecaptcha.siteKey,
+                        'callback': function(token) {
+                            if (debug) {
+                                console.log(JSON.stringify('eeRecaptcha.token: ' + token, null, 4));
+                            }
+                            $recaptcha_div.parent().find('.g-recaptcha-response').val(token);
+                            $form.submit();
+                        }
                     }
-                });
-                // console.log(JSON.stringify('widgetId: ' + widgetId, null, 4));
+                );
+                if (debug) {
+                    console.log(JSON.stringify('widgetId: ' + widgetId, null, 4));
+                }
                 $submit.on('click', function(e) {
+                    $submit.prop('disabled', true).addClass('disabled ee-button-disabled');
                     e.preventDefault();
                     e.stopPropagation();
                     if ($(this).data('g-recaptcha-id') !== $recaptcha_id) {
                         $(this).data('g-recaptcha-id', $recaptcha_id);
-                        // console.log(JSON.stringify('SUBMIT widgetId: ' + widgetId, null, 4));
+                        if (debug) {
+                            console.log(JSON.stringify('SUBMIT widgetId: ' + widgetId, null, 4));
+                        }
                         grecaptcha.execute(widgetId);
                     }
                 });
