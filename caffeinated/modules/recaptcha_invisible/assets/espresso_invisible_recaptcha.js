9a3ea635a43db5524e74aedde895eff17ff65f22
Brent Christensen <brent@eventespresso.com>
2018-02-14 21:02:28 -0800

    don't execeute recaptcha if already passed; get submit button id from recaptcha div data attribute; set ALL g-recaptcha-response fields when token is received; use timeout to delay click on submit button or else it does not work; [touch:9575]

--- caffeinated/modules/recaptcha_invisible/assets/espresso_invisible_recaptcha.js
+++ caffeinated/modules/recaptcha_invisible/assets/espresso_invisible_recaptcha.js
@@ -21,22 +21,31 @@
  */
 function espressoLoadRecaptcha() {
     jQuery(document).ready(function($) {
-        if (typeof eeRecaptcha !== 'undefined') {
-            var debug = eeRecaptcha.wp_debug;
-            if (debug) {
+        if (typeof eeRecaptcha === 'undefined'){
+            return;
+        }
+        // convert truthy values to pure booleans
+        eeRecaptcha.recaptcha_passed = eeRecaptcha.recaptcha_passed === 'true' || eeRecaptcha.recaptcha_passed === '1';
+        eeRecaptcha.mer_active = eeRecaptcha.mer_active === 'true' || eeRecaptcha.mer_active === '1';
+        eeRecaptcha.wp_debug = eeRecaptcha.wp_debug === 'true' || eeRecaptcha.wp_debug === '1';
+        console_log_object('eeRecaptcha', eeRecaptcha, 3);
+        if (eeRecaptcha.recaptcha_passed !== true) {
+            if (eeRecaptcha.wp_debug) {
+                console.log();
                 console.log(JSON.stringify('eeRecaptcha.siteKey: ' + eeRecaptcha.siteKey, null, 4));
+                console.log();
             }
             $('.g-recaptcha').each(function() {
                 var $recaptcha_div = $(this),
                     $recaptcha_id  = $recaptcha_div.attr('id'),
                     $form          = $(this).parents('form:first'),
-                    $submit        = eeRecaptcha.submit_button_id !== ''
-                        ? $('#' + eeRecaptcha.submit_button_id)
+                    $submit        = $recaptcha_div.data('submit_button_id') !== ''
+                        ? $('#' + $recaptcha_div.data('submit_button_id'))
                         : $form.find(':submit');
-                if (debug) {
+                if (eeRecaptcha.wp_debug) {
                     console.log(JSON.stringify('recaptcha id: ' + $recaptcha_id, null, 4));
                     console.log(JSON.stringify('form name: ' + $form.attr('name'), null, 4));
-                    console.log(JSON.stringify('eeRecaptcha.submit_button_id: ' + eeRecaptcha.submit_button_id, null, 4));
+                    console.log(JSON.stringify('$recaptcha_div.data(\'submit_button_id\'): ' + $recaptcha_div.data('submit_button_id'), null, 4));
                     console.log(JSON.stringify('submit id: ' + $submit.attr('id'), null, 4));
                 }
                 var widgetId = grecaptcha.render(
@@ -44,27 +53,69 @@ function espressoLoadRecaptcha() {
                     {
                         'sitekey':  eeRecaptcha.siteKey,
                         'callback': function(token) {
-                            if (debug) {
+                            if (eeRecaptcha.wp_debug) {
+                                console.log();
+                                console.log(JSON.stringify('FORM SUBMIT CALLBACK', null, 4));
                                 console.log(JSON.stringify('eeRecaptcha.token: ' + token, null, 4));
                             }
-                            $recaptcha_div.parent().find('.g-recaptcha-response').val(token);
-                            $form.submit();
+                            // $recaptcha_div.parent().find('.g-recaptcha-response').val(token);
+                            $('.g-recaptcha-response').each(function() {
+                                $(this).val(token);
+                                if (eeRecaptcha.wp_debug) {
+                                    console.log('set g-recaptcha-response: ' + $(this).attr('id'));
+                                    console.log('g-recaptcha-response: ' + $(this).val());
+                                }
+                            });
+                            eeRecaptcha.recaptcha_passed = true;
+                            // $form.submit();
+                            $submit.data('ee-submitted', true)
+                                   .prop('disabled', false)
+                                   .removeClass('disabled ee-button-disabled');
+                            if (eeRecaptcha.wp_debug) {
+                                console.log();
+                                console.log(JSON.stringify('>> TRIGGER CLICK ON #' + $submit.attr('id') + ' <<', null, 4));
+                            }
+                            // need a small timeout before triggering click on submit button
+                            setTimeout(
+                                function() {
+                                    $submit.click();
+                                    if (eeRecaptcha.mer_active !== true) {
+                                        $submit.prop('disabled', true).addClass('disabled ee-button-disabled');
+                                    }
+                                },
+                                100
+                            );
                         }
                     }
                 );
-                if (debug) {
+                if (eeRecaptcha.wp_debug) {
                     console.log(JSON.stringify('widgetId: ' + widgetId, null, 4));
+                    console.log();
                 }
-                $submit.on('click', function(e) {
-                    $submit.prop('disabled', true).addClass('disabled ee-button-disabled');
-                    e.preventDefault();
-                    e.stopPropagation();
-                    if ($(this).data('g-recaptcha-id') !== $recaptcha_id) {
-                        $(this).data('g-recaptcha-id', $recaptcha_id);
-                        if (debug) {
-                            console.log(JSON.stringify('SUBMIT widgetId: ' + widgetId, null, 4));
+                $submit.on('click', function(event) {
+                    if (eeRecaptcha.wp_debug) {
+                        console.log();
+                        console.log(JSON.stringify('>> SUBMIT BUTTON CLICKED <<', null, 4));
+                        console.log(JSON.stringify('data(ee-submitted): ' + $submit.data('ee-submitted'), null, 4));
+                        console.log(JSON.stringify('disabled: ' + $submit.prop('disabled'), null, 4));
+                    }
+                    if ($submit.data('ee-submitted') !== true && eeRecaptcha.recaptcha_passed !== true) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        if (eeRecaptcha.mer_active !== true) {
+                            $submit.prop('disabled', true).addClass('disabled ee-button-disabled');
                         }
-                        grecaptcha.execute(widgetId);
+                        if (eeRecaptcha.wp_debug) {
+                            console.log(JSON.stringify('disabled: ' + $submit.prop('disabled'), null, 4));
+                        }
+                        if ($(this).data('g-recaptcha-id') !== $recaptcha_id) {
+                            $(this).data('g-recaptcha-id', $recaptcha_id);
+                            if (eeRecaptcha.wp_debug) {
+                                console.log(JSON.stringify('SUBMIT WIDGET ID: ' + widgetId, null, 4));
+                                console.log();
+                            }
+                            grecaptcha.execute(widgetId);
+                        }
                     }
                 });
             });
