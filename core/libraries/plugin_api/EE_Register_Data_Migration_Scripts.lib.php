cd6ac2f42397ff74f87ea78c2f6af8dd7b700b4c
Brent Christensen <brent@eventespresso.com>
2018-02-22 14:36:52 -0800

    additional manual formatting; [touch:11379]

--- core/libraries/plugin_api/EE_Register_Data_Migration_Scripts.lib.php
+++ core/libraries/plugin_api/EE_Register_Data_Migration_Scripts.lib.php
@@ -1,17 +1,6 @@
-<?php if (! defined('EVENT_ESPRESSO_VERSION')) {
-    exit('No direct script access allowed');
-}
-/**
- * Event Espresso
- * Event Registration and Ticketing Management Plugin for WordPress
- * @ package            Event Espresso
- * @ author                Event Espresso
- * @ copyright        (c) 2008-2014 Event Espresso  All Rights Reserved.
- * @ license            http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link                    http://www.eventespresso.com
- * ------------------------------------------------------------------------
- */
+<?php
 
+defined('EVENT_ESPRESSO_VERSION') || exit('NO direct script access allowed');
 
 
 /**
@@ -21,10 +10,10 @@
  * such as autoloaders, configuration settings, data migration scripts, admin pages, modules, shortcodes, and even
  * widgets
  *
- * @package               Event Espresso
- * @subpackage            plugin api
- * @since                 4.3.0
- * @author                Brent Christensen
+ * @package     Event Espresso
+ * @subpackage  plugin api
+ * @since       4.3.0
+ * @author      Brent Christensen
  */
 class EE_Register_Data_Migration_Scripts implements EEI_Plugin_API
 {
@@ -38,13 +27,13 @@ class EE_Register_Data_Migration_Scripts implements EEI_Plugin_API
 
 
     /**
-     *    Method for registering new Data Migration Scripts
+     * Method for registering new Data Migration Scripts
      *
-     * @since    4.3.0
+     * @since 4.3.0
      * @param string $dms_id a unique identifier for this set of data migration scripts
-     * @param  array $setup_args {
-     * @type string  $dms_paths an array of full server paths to folders that contain data migration scripts
-     *                           }
+     * @param array $setup_args {
+     *      @type string  $dms_paths an array of full server paths to folders that contain data migration scripts
+     *  }
      * @throws EE_Error
      * @return void
      */
@@ -52,8 +41,12 @@ class EE_Register_Data_Migration_Scripts implements EEI_Plugin_API
     {
         //required fields MUST be present, so let's make sure they are.
         if (empty($dms_id) || ! is_array($setup_args) || empty($setup_args['dms_paths'])) {
-            throw new EE_Error(__('In order to register Data Migration Scripts with EE_Register_Data_Migration_Scripts::register(), you must include a "dms_id" (a unique identifier for this set of data migration scripts), and  an array containing the following keys: "dms_paths" (an array of full server paths to folders that contain data migration scripts)',
-                'event_espresso'));
+            throw new EE_Error(
+                __(
+                    'In order to register Data Migration Scripts with EE_Register_Data_Migration_Scripts::register(), you must include a "dms_id" (a unique identifier for this set of data migration scripts), and  an array containing the following keys: "dms_paths" (an array of full server paths to folders that contain data migration scripts)',
+                    'event_espresso'
+                )
+            );
         }
         //make sure we don't register twice
         if (isset(self::$_settings[ $dms_id ])) {
@@ -61,11 +54,14 @@ class EE_Register_Data_Migration_Scripts implements EEI_Plugin_API
         }
         //make sure this was called in the right place!
         if (! did_action('AHEE__EE_System__load_espresso_addons')
-            || did_action('AHEE__EE_System___detect_if_activation_or_upgrade__begin')) {
+            || did_action('AHEE__EE_System___detect_if_activation_or_upgrade__begin')
+        ) {
             EE_Error::doing_it_wrong(
                 __METHOD__,
-                __('An attempt to register Data Migration Scripts has failed because it was not registered at the correct time.  Please use the "AHEE__EE_System__load_espresso_addons" hook to register Data Migration Scripts.',
-                    'event_espresso'),
+                __(
+                    'An attempt to register Data Migration Scripts has failed because it was not registered at the correct time.  Please use the "AHEE__EE_System__load_espresso_addons" hook to register Data Migration Scripts.',
+                    'event_espresso'
+                ),
                 '4.3.0'
             );
         }
@@ -74,14 +70,14 @@ class EE_Register_Data_Migration_Scripts implements EEI_Plugin_API
             'dms_paths' => isset($setup_args['dms_paths']) ? (array) $setup_args['dms_paths'] : array(),
         );
         // setup DMS
-        add_filter('FHEE__EE_Data_Migration_Manager__get_data_migration_script_folders',
-            array('EE_Register_Data_Migration_Scripts', 'add_data_migration_script_folders'));
+        add_filter(
+            'FHEE__EE_Data_Migration_Manager__get_data_migration_script_folders',
+            array('EE_Register_Data_Migration_Scripts', 'add_data_migration_script_folders')
+        );
     }
 
 
     /**
-     *    add_data_migration_script_folders
-     *
      * @param array $dms_paths
      * @return array
      */
@@ -97,7 +93,7 @@ class EE_Register_Data_Migration_Scripts implements EEI_Plugin_API
     /**
      * This deregisters a set of Data Migration Scripts that were previously registered with a specific dms_id
      *
-     * @since    4.3.0
+     * @since 4.3.0
      * @param mixed $dms_id unique identifier for the set of Data Migration Scripts that were previously registered
      * @return void
      */
