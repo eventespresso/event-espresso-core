de761bcc509bdfe3bd18f55394fdfe05f735cbb4
Darren Ethier <darren@roughsmootheng.in>
2018-02-26 14:47:21 -0500

    use `update_plugins` capability instead [touch: 10888]

--- core/middleware/EE_Recommended_Versions.core.php
+++ core/middleware/EE_Recommended_Versions.core.php
@@ -197,7 +197,7 @@ class EE_Recommended_Versions extends EE_Middleware
     {
         if ($this->_request->isAdmin()
             && apply_filters('FHEE__EE_Recommended_Versions__displayUpcomingRequiredVersion', true, $this->_request)
-            && current_user_can('manage_options')
+            && current_user_can('update_plugins')
         ) {
             add_action('admin_notices', function () {
                 echo '<div class="notice event-espresso-admin-notice notice-warning"><p>'
