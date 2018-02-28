fee4b73480381375b215eebd98f8fc7be02beec6
Darren Ethier <darren@roughsmootheng.in>
2018-02-28 15:12:05 -0500

    add new column for mocking [touch: 11344]
    also added back in the instance property I removed in previous commit accidentally.

--- tests/mocks/core/db_models/EEM_Mock.model.php
+++ tests/mocks/core/db_models/EEM_Mock.model.php
@@ -10,6 +10,18 @@
 class EEM_Mock extends EEM_Base
 {
 
+    /**
+     * @var EEM_Mock
+     */
+    protected static $_instance;
+
+    /**
+     * EEM_Mock constructor.
+     *
+     * @param null $timezone
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     */
     public function __construct($timezone = null)
     {
         $this->_tables = array(
@@ -19,6 +31,7 @@ class EEM_Mock extends EEM_Base
             'Mock' => array(
                 'MCK_ID'    => new EE_Primary_Key_Int_Field('MCK_ID', "Mock Object ID"),
                 'MCK_value' => new EE_Plain_Text_Field('MCK_value', 'Mock Object Value', true),
+                'MCK_datetime' => new EE_DateTime_Field('MCK_datetime', 'Mock Datetime', false, EE_Datetime_Field::now)
             ),
         );
         parent::__construct($timezone);
