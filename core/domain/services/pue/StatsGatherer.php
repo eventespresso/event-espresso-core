94e298e4543c40b9517906bd0b2d0a242b926de9
Darren Ethier <darren@roughsmootheng.in>
2018-03-06 05:54:34 -0500

    tweak payment method stats key [touch: 11183]
    - fixes only the latest active payment method in the loop from being included
    - enables easier querying for payment method active state on the server

--- core/domain/services/pue/StatsGatherer.php
+++ core/domain/services/pue/StatsGatherer.php
@@ -162,7 +162,7 @@ class StatsGatherer
             );
             if ($active_payment_methods) {
                 foreach ($active_payment_methods as $payment_method) {
-                    $payment_method_stats['active_payment_method'] = $payment_method->name();
+                    $payment_method_stats[$payment_method->name() . '_active_payment_method'] = 1;
                 }
             }
         } catch (Exception $e) {
