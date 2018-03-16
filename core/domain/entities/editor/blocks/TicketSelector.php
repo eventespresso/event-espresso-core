38971c2c182e59948c1a8412caa39d83f535d83a
Brent Christensen <brent@eventespresso.com>
2018-03-16 14:05:17 -0700

    add Editor Block for Ticket Selector; [touch:11400]

---
+++ core/domain/entities/editor/blocks/TicketSelector.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace EventEspresso\core\domain\entities\editor\blocks;
+
+use EventEspresso\core\domain\entities\editor\EditorBlock;
+use WP_Block_Type;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class TicketSelector
+ * Description
+ *
+ * @package EventEspresso\core\domain\entities\editor\blocks
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+class TicketSelector extends EditorBlock
+{
+
+    const EDITOR_BLOCK_TYPE = 'ee-event-editor/ticket-editor-container';
+
+
+    /**
+     * Perform any early setup required by the block
+     * including setting the block type and supported post types
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        $this->setEditorBlockType(TicketSelector::EDITOR_BLOCK_TYPE);
+        $this->setSupportedPostTypes(array('espresso_events'));
+    }
+
+
+    /**
+     * Registers the Editor Block with WP core;
+     * Returns the registered block type on success, or false on failure.
+     *
+     * @return WP_Block_Type|false
+     */
+    public function registerBlock()
+    {
+        $wp_block_type = register_block_type(
+            new WP_Block_Type(
+                $this->editorBlockType(),
+                array(
+                    'editor_script' => 'ee-event-editor-blocks',
+                    'editor_style'  => 'ee-block-styles',
+                    'attributes'    => array(),
+                )
+            )
+        );
+        $this->setWpBlockType($wp_block_type);
+        return $wp_block_type;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getEditorContainer()
+    {
+        return array(
+            $this->editorBlockType(),
+            array()
+        );
+    }
+
+
+    /**
+     * @return  void
+     */
+    public function registerScripts()
+    {
+    }
+
+
+    /**
+     * @return void
+     */
+    public function registerStyles()
+    {
+    }
+
+
+    /**
+     * returns the rendered HTML for the block
+     *
+     * @param array $attributes
+     * @return string
+     */
+    public function renderBlock(array $attributes = array())
+    {
+        return '';
+    }
+}
