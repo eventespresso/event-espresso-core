e406ebe34598e0a5f27938f674d647d933d1371b
Brent Christensen <brent@eventespresso.com>
2018-03-20 11:18:47 -0700

    extend InterminableInterface; [touch:11439]

--- core/domain/DomainInterface.php
+++ core/domain/DomainInterface.php
@@ -2,6 +2,7 @@
 namespace EventEspresso\core\domain;
 
 use DomainException;
+use EventEspresso\core\interfaces\InterminableInterface;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
@@ -12,7 +13,7 @@ defined('EVENT_ESPRESSO_VERSION') || exit;
  * @author  Darren Ethier
  * @since   4.9.50
  */
-interface DomainInterface
+interface DomainInterface extends InterminableInterface
 {
 
     /**
