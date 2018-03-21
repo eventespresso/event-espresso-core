892d2414b2704cf40879748a079bf4809f1b4e00
Brent Christensen <brent@eventespresso.com>
2018-03-20 19:26:07 -0700

    improve session mock; [touch:11442]

--- tests/mocks/core/EE_Session_Mock.core.php
+++ tests/mocks/core/EE_Session_Mock.core.php
@@ -25,6 +25,13 @@ class EE_Session_Mock extends EE_Session {
 	 */
 	private static $_instance;
 
+    /**
+     * session data
+     *
+     * @var array
+     */
+    private $session_data = array();
+
 
     /**
      * @singleton method used to instantiate class object
@@ -103,7 +110,140 @@ class EE_Session_Mock extends EE_Session {
 	}
 
 
+    /**
+     * @param null $key
+     * @param bool $reset_cache
+     * @return    array
+     */
+    public function get_session_data($key = null, $reset_cache = false)
+    {
+        if ($reset_cache) {
+            $this->clear_session();
+        }
+        if (! empty($key)) {
+            return isset($this->session_data[ $key ]) ? $this->session_data[ $key ] : null;
+        }
+        return $this->session_data;
+    }
 
+
+    /**
+     * set session data
+     *
+     * @access    public
+     * @param    array $data
+     * @return    TRUE on success, FALSE on fail
+     */
+    public function set_session_data($data)
+    {
+        foreach ($data as $key => $value) {
+            $this->session_data[ $key ] = $value;
+        }
+        return true;
+    }
+
+
+    public function clear_session($class = '', $function = '')
+    {
+        $this->session_data = array();
+    }
+
+
+    /**
+     * @param \EE_Cart $cart
+     * @return bool
+     */
+    public function set_cart(EE_Cart $cart)
+    {
+        $this->session_data['cart'] = $cart;
+        return true;
+    }
+
+
+    /**
+     * reset_cart
+     */
+    public function reset_cart()
+    {
+        $this->session_data['cart'] = null;
+    }
+
+
+    /**
+     * @return \EE_Cart
+     */
+    public function cart()
+    {
+        return isset($this->session_data['cart']) && $this->session_data['cart'] instanceof EE_Cart
+            ? $this->session_data['cart']
+            : null;
+    }
+
+
+    /**
+     * @param \EE_Checkout $checkout
+     * @return bool
+     */
+    public function set_checkout(EE_Checkout $checkout)
+    {
+        $this->session_data['checkout'] = $checkout;
+        return true;
+    }
+
+
+    /**
+     * reset_checkout
+     */
+    public function reset_checkout()
+    {
+        $this->session_data['checkout'] = null;
+    }
+
+
+    /**
+     * @return \EE_Checkout
+     */
+    public function checkout()
+    {
+        return isset($this->session_data['checkout']) && $this->session_data['checkout'] instanceof EE_Checkout
+            ? $this->session_data['checkout']
+            : null;
+    }
+
+
+    /**
+     * @param \EE_Transaction $transaction
+     * @return bool
+     * @throws EE_Error
+     */
+    public function set_transaction(EE_Transaction $transaction)
+    {
+        // first remove the session from the transaction before we save the transaction in the session
+        $transaction->set_txn_session_data(null);
+        $this->session_data['transaction'] = $transaction;
+        return true;
+    }
+
+
+    /**
+     * reset_transaction
+     */
+    public function reset_transaction()
+    {
+        $this->session_data['transaction'] = null;
+    }
+
+
+    /**
+     * @return \EE_Transaction
+     */
+    public function transaction()
+    {
+        return isset($this->session_data['transaction'])
+               && $this->session_data['transaction'] instanceof EE_Transaction
+            ? $this->session_data['transaction']
+            : null;
+    }
 }
 // End of file EE_Session_Mock.core.php
 // Location: /tests/mocks/core/EE_Session_Mock.core.php
