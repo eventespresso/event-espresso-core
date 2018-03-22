e040d213f465039baeadc9d092e4381ad4c8cf84
Darren Ethier <darren@roughsmootheng.in>
2018-03-22 15:27:39 -0400

    add helper for determining caffeinated status [touch: 6645]

--- core/domain/Domain.php
+++ core/domain/Domain.php
@@ -2,6 +2,9 @@
 
 namespace EventEspresso\core\domain;
 
+use EventEspresso\core\domain\values\FilePath;
+use EventEspresso\core\domain\values\Version;
+
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
 
@@ -30,4 +33,40 @@ class Domain extends DomainBase
 
     const CONTEXT_REGISTRATION_STATUS_CHANGE_REGISTRATION_ADMIN_NOTIFY
         = 'manual_registration_status_change_from_registration_admin_and_notify';
+
+
+    /**
+     * Whether or not EE core is the full premium version.
+     * @since $VID:$
+     * @var bool
+     */
+    private $is_caffeinated;
+
+
+    public function __construct(FilePath $plugin_file, Version $version)
+    {
+        parent::__construct($plugin_file, $version);
+        $this->setIsCaffeinated();
+    }
+
+    /**
+     * Whether or not EE core is the full premium version.
+     * @since $VID:$
+     * @return bool
+     */
+    public function isCaffeinated()
+    {
+        return $this->is_caffeinated;
+    }
+
+
+    /**
+     * Setter for $is_caffeinated property.
+     * @since $VID:$
+     */
+    private function setIsCaffeinated()
+    {
+        $this->is_caffeinated = file_exists($this->pluginPath() . 'caffeinated');
+        $this->is_caffeinated = defined('EE_DECAF') && EE_DECAF ? false : $this->is_caffeinated;
+    }
 }
