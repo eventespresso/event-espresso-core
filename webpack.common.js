99ead83a097567c11fcfeced1d0e579a29780c2d
Darren Ethier <darren@roughsmootheng.in>
2018-04-03 15:35:50 -0400

    fix formatting [touch: 11474]

--- webpack.common.js
+++ webpack.common.js
@@ -6,8 +6,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const combineLoaders = require('webpack-combine-loaders');
 const autoprefixer = require('autoprefixer');
 const externals = {
-    jquery: "jQuery",
-    '@eventespresso/eejs' : 'eejs'
+    jquery: 'jQuery',
+    '@eventespresso/eejs': 'eejs',
 };
 const reactVendorPackages = [
     'react',
@@ -15,32 +15,36 @@ const reactVendorPackages = [
     'react-redux',
     'redux',
     'classnames',
-    'lodash'
+    'lodash',
 ];
 /** see below for multiple configurations.
-/** https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations */
+ /** https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations */
 config = [
     {
         externals: {
-          '@eventespresso/eejs': {
-            this: 'eejs',
-          }
+            '@eventespresso/eejs': {
+                this: 'eejs',
+            },
         },
         entry: {
-          eejs : [
-            assets + 'eejs/index.js'
-          ]
+            eejs: [
+                assets + 'eejs/index.js',
+            ],
         },
         module: {
-          rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-          ]
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+            ],
         },
         output: {
-          filename: '[name].[chunkhash].dist.js',
-          path: path.resolve(__dirname, 'assets/dist'),
-          library: ['eejs'],
-          libraryTarget: 'this'
+            filename: '[name].[chunkhash].dist.js',
+            path: path.resolve(__dirname, 'assets/dist'),
+            library: ['eejs'],
+            libraryTarget: 'this',
         },
     },
     {
@@ -56,60 +60,59 @@ config = [
             new ExtractTextPlugin('ee-[name].[contenthash].dist.css'),
             new webpack.NamedModulesPlugin(),
             new webpack.optimize.CommonsChunkPlugin({
-              name: 'reactVendor',
-              minChunks: Infinity
+                name: 'reactVendor',
+                minChunks: Infinity,
             }),
             new webpack.optimize.CommonsChunkPlugin({
-              name: 'manifest'
-            })
+                name: 'manifest',
+            }),
         ],
         externals,
         output: {
             filename: 'ee-[name].[chunkhash].dist.js',
-            path: path.resolve(__dirname, 'assets/dist')
+            path: path.resolve(__dirname, 'assets/dist'),
         },
         module: {
             rules: [
                 {
                     test: /\.js$/,
                     exclude: /node_modules/,
-                    use: "babel-loader"
+                    use: 'babel-loader',
                 },
                 {
                     test: /\.css$/,
                     loader: ExtractTextPlugin.extract(
                         combineLoaders([
-                          {
-                            loader: 'css-loader',
-                            query: {
-                                modules: true,
-                                localIdentName: '[local]'
+                            {
+                                loader: 'css-loader',
+                                query: {
+                                    modules: true,
+                                    localIdentName: '[local]',
+                                },
+                                //can't use minimize because cssnano (the
+                                // dependency) doesn't parser the browserlist
+                                // extension in packag.json correctly, there's
+                                // a pending update for it but css-loader
+                                // doesn't have the latest yet. options: {
+                                // minimize: true }
                             },
-                            //can't use minimize because cssnano (the dependency)
-                            //doesn't parser the browserlist extension in packag.json
-                            //correctly, there's a pending update for it but
-                            //css-loader doesn't have the latest yet.
-                            // options: {
-                            //   minimize: true
-                            // }
-                          },
-                          {
-                            loader: 'postcss-loader',
-                            options: {
-                              plugins: function(){
-                                return [autoprefixer]
-                              },
-                              sourceMap: true,
-                            }
-                          }
-                        ])
-                    )
-                }
-            ]
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    plugins: function() {
+                                        return [autoprefixer];
+                                    },
+                                    sourceMap: true,
+                                },
+                            },
+                        ]),
+                    ),
+                },
+            ],
         },
         watchOptions: {
-          poll: 1000
-        }
-    }
+            poll: 1000,
+        },
+    },
 ];
 module.exports = config;
