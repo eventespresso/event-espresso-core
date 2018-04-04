e91d8d5da8799156acb57d8f5759270670ee1fde
Darren Ethier <darren@roughsmootheng.in>
2018-04-04 05:07:51 -0400

    formatting fixes [touch: 11474]

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,15 +4,15 @@ const path = require('path');
 let common = require('./webpack.common.js');
 common.forEach((config, index) => {
     common[index] = merge(config, {
-        devtool:'inline-source-map',
+        devtool: 'inline-source-map',
         plugins: [
-          new AssetsPlugin({
-            filename: 'build-manifest.json',
-            path: path.resolve(__dirname, 'assets/dist'),
-            prettyPrint: true,
-            update: true
-          }),
-        ]
-    })
+            new AssetsPlugin({
+                filename: 'build-manifest.json',
+                path: path.resolve(__dirname, 'assets/dist'),
+                prettyPrint: true,
+                update: true,
+            }),
+        ],
+    });
 });
 module.exports = common;
