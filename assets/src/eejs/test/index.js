9918aef5ef032b459832035c050f522cef759a3b
Darren Ethier <darren@roughsmootheng.in>
2018-04-11 15:42:18 -0400

    add tests for eejs

---
+++ assets/src/eejs/test/index.js
@@ -0,0 +1,13 @@
+import { data, exception } from '../index.js';
+
+describe( 'eejs.data', () => {
+	it( 'ensures data object gets set from eejsdata.data global', () => {
+		expect( data.testData ).toBe( true );
+	} );
+	it( 'it should throw error with msg "error thrown"', () => {
+		const t = () => {
+			throw new exception( 'error thrown' );
+		};
+		expect( t ).toThrowError( 'error thrown' );
+	} );
+} );
