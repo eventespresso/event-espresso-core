923dc3ff704fb299742492bd3dd880eab60f727a
Darren Ethier <darren@roughsmootheng.in>
2018-04-21 19:36:32 -0400

    Update copyright.
    This is also testing the new buildmachine connection.

--- espresso.php
+++ espresso.php
@@ -31,7 +31,7 @@
  *
  * @package     Event Espresso
  * @author      Seth Shoultes
- * @copyright   (c) 2008-2017 Event Espresso  All Rights Reserved.
+ * @copyright   (c) 2008-2018 Event Espresso  All Rights Reserved.
  * @license     {@link http://eventespresso.com/support/terms-conditions/}
  * @see         Plugin Licensing
  * @link        {@link http://www.eventespresso.com}
