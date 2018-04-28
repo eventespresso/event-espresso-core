6e160b96660d3169fa31740914f64ab0449fd89f
Darren Ethier <darren@roughsmootheng.in>
2018-04-27 23:23:14 -0400

    Remove unnecessary mock
    This is taken care of globally so don’t need it in this file specifically.

--- assets/src/components/selection/event-select/test/index.js
+++ assets/src/components/selection/event-select/test/index.js
@@ -1,8 +1,6 @@
 import { shallow } from 'enzyme';
 import { EventSelect } from '../index';
 
-jest.mock( 'tinymce', () => jest.fn() );
-
 describe( 'EventSelect', () => {
 	it( 'should render', () => {
 		const wrapper = shallow( <EventSelect /> );
