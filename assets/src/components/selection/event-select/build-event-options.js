773fb65e659a99e619ecd05029edf93ee43683f2
Darren Ethier <darren@roughsmootheng.in>
2018-04-30 16:29:44 -0400

    add phpdoc for function

--- assets/src/components/selection/event-select/build-event-options.js
+++ assets/src/components/selection/event-select/build-event-options.js
@@ -3,6 +3,14 @@
  */
 import { reduce } from 'lodash';
 
+/**
+ * Receives an array of event entities and returns an array of simple objects
+ * that can be passed along to the options array used for the WordPress
+ * SelectControl component.
+ *
+ * @param { Array } events
+ * @return { Array }
+ */
 export const buildEventOptions = ( events ) => {
 	return reduce( events, function( options, event ) {
 		options.push(
