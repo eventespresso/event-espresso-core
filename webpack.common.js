c3c527cc13a24b7ec0b4f56d8712e9d8f9bc6fbd
Darren Ethier <darren@roughsmootheng.in>
2018-04-30 16:45:55 -0400

    Remove empty javascript file from build config

--- webpack.common.js
+++ webpack.common.js
@@ -65,9 +65,6 @@ const config = [
 			'wp-plugins-page': [
 				assets + 'wp-plugins-page/index.js',
 			],
-			'core-blocks': [
-				assets + 'blocks/index.js',
-			],
 		},
 		externals,
 		output: {
