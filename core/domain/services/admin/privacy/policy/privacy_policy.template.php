581ffda70f8d53af819f59c381b44b9330123135
Mike Nelson <michael@eventespresso.com>
2018-05-04 15:59:17 -0700

    initial stab at GDPR content

--- core/domain/services/admin/privacy/policy/privacy_policy.template.php
+++ core/domain/services/admin/privacy/policy/privacy_policy.template.php
@@ -1,1 +1,38 @@
-EE privacy policy text here
+<h1>Event Registration Cookies</h1>
+When you begin registering for an event and select a ticket quantity, a cookie will be used to track your registration. This cookie lasts {x-time}.
+
+// data collected during registration (ip address, user agent)
+Event registration entails providing us with the needed information to give you a good experience and manage our internal bookkeeping.
+The following information is collected:
+* first name and last name (in order to identify an attendee)
+* email (in order to send you confirmation of booking and information regarding the event)
+* address
+* other information in order to know how to best serve your needs
+
+This registration information is necessary for your attendance at the event.
+
+
+// billing information collected
+In order to process payments, we collect billing information on-site. Sensitive billing information is not stored on our server, but may be handled while in-transit to the payment processing server.
+Please see the privacy policy of the related payment processor.
+Masked billing information may be stored on our servers (eg the first 12 digits of credit card number).
+
+// billing info offsite
+Billing information is sent directly to the payment processor, and is not handled by our servers. Please see the privacy policy of the payment processor.
+
+// logs
+Site administrators may keep a log of communications with the payment processors in order to verify payments are being processed correctly. These
+logs are automatically deleted after a week.
+
+// email history
+We keep a record of the emails sent to you. This is to ensure communication is successfully sent and its information is accurate.
+
+// check-ins
+When you attend an event, an event manager may record the site you check in or out of the event.
+
+// how long data is stored
+Personal data is stored at least until the date of the event, and may be kept indefinitely in case of future registrations.
+
+// data erasure and export possible, just contact admin
+You have the right to request your personal data be sent to you electronically, and the right to request your registration data be erased after the event.
+To do so, please contact the event manager.
