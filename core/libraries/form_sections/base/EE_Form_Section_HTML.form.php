a2863b7d50a46b1d92c23683bb8ff5b831072f19
Darren Ethier <darren@roughsmootheng.in>
2018-05-05 20:00:10 -0400

    fix phpcs violations in *.form.php files

--- core/libraries/form_sections/base/EE_Form_Section_HTML.form.php
+++ core/libraries/form_sections/base/EE_Form_Section_HTML.form.php
@@ -1,42 +1,43 @@
 <?php
-if (!defined('EVENT_ESPRESSO_VERSION'))
-	exit('No direct script access allowed');
+
 /**
  * EE_Form_Section_HTML
  * HTML to be laid out like a proper subsection
  *
  *
- * @package			Event Espresso
+ * @package         Event Espresso
  * @subpackage
- * @author				Mike Nelson
+ * @author              Mike Nelson
  *
  * ------------------------------------------------------------------------
  */
-class EE_Form_Section_HTML extends EE_Form_Section_Base{
+class EE_Form_Section_HTML extends EE_Form_Section_Base
+{
 
-	protected $_html = '';
-
-
-
-	/**
-	 * @param string $html
-	 * @param array $options_array
-	 */
-	public function __construct( $html = '', $options_array = array() ) {
-		$this->_html = $html;
-		parent::__construct( $options_array );
-	}
+    protected $_html = '';
 
 
 
-	/**
-	 * Returns the HTML
-	 * @return string
-	 */
-	public function get_html() {
-		return $this->_html;
-	}
+    /**
+     * @param string $html
+     * @param array $options_array
+     */
+    public function __construct($html = '', $options_array = array())
+    {
+        $this->_html = $html;
+        parent::__construct($options_array);
+    }
 
+
+
+    /**
+     * Returns the HTML
+     * @return string
+     */
+    public function get_html()
+    {
+        return $this->_html;
+    }
 }
 
 // End of file EE_Form_Section_HTML.form.php
