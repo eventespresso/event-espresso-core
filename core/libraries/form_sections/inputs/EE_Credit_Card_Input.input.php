ee1a6d003f608925f8d7960669118d9a55538904
Darren Ethier <darren@roughsmootheng.in>
2018-05-06 12:43:23 -0400

    fix phpcs violations for all *.input.php files

--- core/libraries/form_sections/inputs/EE_Credit_Card_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Credit_Card_Input.input.php
@@ -1,23 +1,25 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
+<?php
 /**
  *
  * EE_Credit_Card_Input
  *
- * @package			Event Espresso
+ * @package         Event Espresso
  * @subpackage
- * @author				Mike Nelson
+ * @author              Mike Nelson
  */
-class EE_Credit_Card_Input extends EE_Form_Input_Base{
+class EE_Credit_Card_Input extends EE_Form_Input_Base
+{
 
-	/**
-	 * @param array $input_settings
-	 */
-	function __construct($input_settings = array()){
-		$input_settings['required'] = isset( $input_settings['required'] ) ? $input_settings['required'] : TRUE;
-		$this->_set_display_strategy(new EE_Text_Input_Display_Strategy());
-		$this->_set_normalization_strategy(new EE_Credit_Card_Normalization());
-		$this->_add_validation_strategy(new EE_Credit_Card_Validation_Strategy( isset( $input_settings[ 'validation_error_message' ] ) ? $input_settings[ 'validation_error_message' ] : NULL ));
-		$this->set_sensitive_data_removal_strategy(new EE_Credit_Card_Sensitive_Data_Removal());
-		parent::__construct($input_settings);
-	}
+    /**
+     * @param array $input_settings
+     */
+    public function __construct($input_settings = array())
+    {
+        $input_settings['required'] = isset($input_settings['required']) ? $input_settings['required'] : true;
+        $this->_set_display_strategy(new EE_Text_Input_Display_Strategy());
+        $this->_set_normalization_strategy(new EE_Credit_Card_Normalization());
+        $this->_add_validation_strategy(new EE_Credit_Card_Validation_Strategy(isset($input_settings['validation_error_message']) ? $input_settings['validation_error_message'] : null));
+        $this->set_sensitive_data_removal_strategy(new EE_Credit_Card_Sensitive_Data_Removal());
+        parent::__construct($input_settings);
+    }
 }
