a8e77c39d074064874f02ed1bf3b7671b1b213e3
Darren Ethier <darren@roughsmootheng.in>
2018-05-21 20:49:47 -0400

    update vendor js to reference the correct chunk and rename

--- core/domain/services/assets/CoreAssetManager.php
+++ core/domain/services/assets/CoreAssetManager.php
@@ -43,7 +43,7 @@ class CoreAssetManager extends AssetManager
 
     const JS_HANDLE_EE_JS_CORE         = 'eejs-core';
 
-    const JS_HANDLE_EE_VENDOR_REACT    = 'ee-vendor-react';
+    const JS_HANDLE_EE_VENDOR           = 'eventespresso-vendor';
 
     const JS_HANDLE_EE_JS_API          = 'eejs-api';
 
@@ -163,8 +163,8 @@ class CoreAssetManager extends AssetManager
         ->setHasInlineData();
 
         $this->addJavascript(
-            CoreAssetManager::JS_HANDLE_EE_VENDOR_REACT,
-            $this->registry->getJsUrl($this->domain->assetNamespace(), 'reactVendor'),
+            CoreAssetManager::JS_HANDLE_EE_VENDOR,
+            $this->registry->getJsUrl($this->domain->assetNamespace(), 'vendor'),
             array(CoreAssetManager::JS_HANDLE_EE_JS_CORE)
         );
 
@@ -342,7 +342,7 @@ class CoreAssetManager extends AssetManager
             $this->registry->getJsUrl($this->domain->assetNamespace(), 'wp-plugins-page'),
             array(
                 CoreAssetManager::JS_HANDLE_JQUERY,
-                CoreAssetManager::JS_HANDLE_EE_VENDOR_REACT,
+                CoreAssetManager::JS_HANDLE_EE_VENDOR,
             )
         )
         ->setRequiresTranslation();
