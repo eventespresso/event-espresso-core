71df9dec40972b47e7a06cdddf009ce7e3bffd39
Darren Ethier <darren@roughsmootheng.in>
2018-05-21 20:45:06 -0400

    Modify Webpack configuration
    - export `eejs` properties to a var (needed for the new build process… the end effect is the same).
    - instead of using webpack to bundle the vendor, we bundle vendor ourselves.  This allows for finer control and splitting out libraries in the gutenberg branch.

--- webpack.common.js
+++ webpack.common.js
@@ -6,30 +6,60 @@ const externals = {
 	jquery: 'jQuery',
 	'@eventespresso/eejs': 'eejs',
 	'@eventespresso/i18n': 'eejs.i18n',
+	react: 'eejs.vendor.react',
+	'react-dom': 'eejs.vendor.reactDom',
+	'react-redux': 'eejs.vendor.reactRedux',
+	redux: 'eejs.vendor.redux',
+	classnames: 'eejs.vendor.classnames',
+	lodash: 'eejs.vendor.lodash',
+	moment: 'eejs.vendor.moment',
 };
-const reactVendorPackages = [
-	'react',
-	'react-dom',
-	'react-redux',
-	'redux',
-	'classnames',
-	'lodash',
-];
+
 /** see below for multiple configurations.
  /** https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations */
 const config = [
 	{
 		configName: 'eejs',
-		externals: {
-			'@eventespresso/eejs': {
-				this: 'eejs',
-			},
-		},
 		entry: {
-			eejs: [
-				assets + 'eejs/index.js',
+			eejs: assets + 'eejs/index.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
 			],
 		},
+		output: {
+			filename: 'ee-[name].[chunkhash].dist.js',
+			path: path.resolve( __dirname, 'assets/dist' ),
+			library: [ 'eejs' ],
+			libraryTarget: 'var',
+		},
+	},
+	{
+		configName: 'eejsVendor',
+		entry: {
+			vendor: assets + 'eejs/vendor/index.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
+			],
+		},
+		output: {
+			filename: 'ee-[name].[chunkhash].dist.js',
+			path: path.resolve( __dirname, 'assets/dist' ),
+			library: [ 'eejs', '[name]' ],
+			libraryTarget: 'var',
+		},
+	},
 		module: {
 			rules: [
 				{
@@ -49,7 +79,6 @@ const config = [
 	{
 		configName: 'base',
 		entry: {
-			reactVendor: reactVendorPackages,
 			'wp-plugins-page': [
 				assets + 'wp-plugins-page/index.js',
 			],
--- webpack.prod.js
+++ webpack.prod.js
@@ -9,27 +9,12 @@ const wpi18nExtractor = require( './bin/i18n-map-extractor.js' );
 common.forEach( ( config, index ) => {
 	if ( common[ index ].configName === 'base' ) {
 		common[ index ].optimization = {
-			splitChunks: {
-				cacheGroups: {
-					vendors: {
-						test: 'reactVendor',
-						name: 'reactVendor',
-						chunks: 'all',
-					},
-				},
-			},
 			runtimeChunk: {
 				name: 'manifest',
 			},
 		};
 		common[ index ].plugins = [
 			new CleanWebpackPlugin( [ 'assets/dist', 'translation-map.json' ] ),
-			new webpack.ProvidePlugin( {
-				'React': 'react', // eslint-disable-line quote-props
-			} ),
-			new miniExtract( {
-				filename: 'ee-[name].[contenthash].dist.css',
-			} ),
 		];
 	}
 	common[ index ] = merge( config, {
@@ -43,14 +28,20 @@ common.forEach( ( config, index ) => {
 				aliases: {
 					'wp-plugins-page': 'ee-wp-plugins-page',
 				},
-				excludes: [ 'eejs' ],
+				excludes: [ 'eejs', 'vendor' ],
 			} ),
 			new WebpackAssetsManifest( {
 				output: path.resolve( __dirname,
 					'assets/dist/build-manifest.json',
 				),
 				merge: true,
-				entrypoints: true,
+				entrypoints: false,
+			} ),
+			new webpack.ProvidePlugin( {
+				React: 'react',
+			} ),
+			new miniExtract( {
+				filename: 'ee-[name].[contenthash].dist.css',
 			} ),
 		],
 		mode: 'production',
