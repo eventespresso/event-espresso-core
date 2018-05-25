ff09b42965716cd101d662f870e8fe416b41a608
Brent Christensen <brent@eventespresso.com>
2018-05-24 21:57:19 -0700

    add "selectedEventId" attribute and remove unnecessary properties

--- assets/src/blocks/widgets/event-attendees/index.js
+++ assets/src/blocks/widgets/event-attendees/index.js
@@ -18,22 +18,30 @@ registerBlockType(
 	'eventespresso/widgets-event-attendees',
 	{
 		title: __( 'Event Attendees', 'event_espresso' ),
+
 		description: __(
 			'Displays a list of people that have registered for the specified event',
 			'event_espresso',
 		),
+
 		icon: 'groups',
+
 		category: 'widgets',
+
 		keywords: [
 			__( 'event', 'event_espresso' ),
 			__( 'attendees', 'event_espresso' ),
 			__( 'list', 'event_espresso' ),
 		],
-		useOnce: false,
-		supports: {
-			anchor: true,
+
+		attributes: {
+			selectedEventId: {
+				type: 'number'
+			}
 		},
+
 		edit: EventAttendees,
+
 		save() {
 			return null;
 		},
