f125db1eb38cab9ae68d52cdad123293d6be72f0
Brent Christensen <brent@eventespresso.com>
2018-05-24 22:08:30 -0700

    extract event list display into separate function and file

---
+++ assets/src/blocks/widgets/event-attendees/list.js
@@ -0,0 +1,37 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@eventespresso/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { ContactAvatar } from '@eventespresso/components';
+
+const EventAttendeesList = ( { attendees } ) => {
+	return (
+		<div key="events-attendees-block"
+			 className="event-attendees-block-div" >
+			<h3>{ __( 'Event Attendees', 'event_espresso' ) }</h3>
+			<ul className="event-attendees-ul">
+				{ attendees.map( ( attendee, index ) => {
+					return (
+						<li key={ index } className="event-attendee-li">
+							<p>
+								<ContactAvatar
+									avatarUrl={ attendee._calculated_fields.userAvatar }
+									avatarClass={ 'event-attendee' }
+								/>
+								<span className="event-attendee-name-span">
+									{ attendee.ATT_full_name }
+								</span>
+							</p>
+						</li>
+					);
+				} ) }
+			</ul>
+		</div>
+	);
+};
+
+export default EventAttendeesList
