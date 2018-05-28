6df5e463287f6d145926b8747550fbcd2cd319f5
Brent Christensen <brent@eventespresso.com>
2018-05-28 13:21:11 -0700

    QueryLimit
    A React component for adding a WordPress Range Control input for setting a Query's Limit clause

--- assets/src/components/query/limit/index.js
+++ assets/src/components/query/limit/index.js
@@ -1,26 +1,70 @@
 /**
+ * USAGE:
+ *
+ * A React component for adding a WordPress Range Control input
+ * for setting a Query's Limit clause
+ *
+ * Step 1: import the following external dependencies:
+ *
+ * 		import { __ } from '@eventespresso/i18n';
+ *      import { QueryLimit } from '@eventespresso/components';
+ *      import { PanelBody } from '@wordpress/components';
+ *      import { InspectorControls } from '@wordpress/editor';
+ *
+ * Step 2: add a "limit" attribute:
+ *
+ *      attributes: {
+ *        	limit: {
+ *        		type: 'number',
+ *        		default: 5,
+ *        	},
+ *      },
+ *
+ * Step 3: wrap the QueryLimit component within
+ * the InspectorControls and PanelBody components
+ * (currently shown within a block's edit() function)
+ *
+ *      edit: ( ( { attributes, setAttributes } ) => {
+ *      	const { limit } = attributes;
+ *      	return (
+ *      		<InspectorControls>
+ *        			<PanelBody>
+ *        				<QueryLimit
+ *        					limit={ limit }
+ *        					onLimitChange={
+ *        						( value ) => setAttributes( { limit: value } )
+ *        					}
+ *        				/>
+ *        			</PanelBody>
+ *        		</InspectorControls>
+ *        	);
+ *      }),
+ */
+
+/**
  * External dependencies
  */
+import React from 'react';
 import { __ } from '@eventespresso/i18n';
 
 /**
  * WordPress dependencies
  **/
-const { RangeControl } = wp.components;
+import { RangeControl } from '@wordpress/components';
 
 const DEFAULT_MIN = 1;
 const DEFAULT_MAX = 100;
 
 export const QueryLimit = ( {
-	limit = 10,
+	limit,
+	onLimitChange,
 	label = __( 'Limit', 'event_espresso' ),
 	min = DEFAULT_MIN,
 	max = DEFAULT_MAX,
-	onLimitChange
 } ) => {
-	return onLimitChange && (
+	return (
 		<RangeControl
-			key="query-limit"
+			key={'query-limit'}
 			value={ limit }
 			label={ label }
 			min={ min }
