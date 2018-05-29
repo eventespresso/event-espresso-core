0c1f9c5fb2c8a1c045fa480a12aa2cc98da18ac2
Brent Christensen <brent@eventespresso.com>
2018-05-29 11:04:59 -0700

    remove inline documentation

--- assets/src/components/query/limit/index.js
+++ assets/src/components/query/limit/index.js
@@ -1,47 +1,4 @@
 /**
- * USAGE:
- *
- * A React component for adding a WordPress Range Control input
- * for setting a query's limit clause
- *
- * Step 1: import the following external dependencies:
- *
- *        import { __ } from '@eventespresso/i18n';
- *        import { QueryLimit } from '@eventespresso/components';
- *        import { PanelBody } from '@wordpress/components';
- *        import { InspectorControls } from '@wordpress/editor';
- *
- * Step 2: add a "limit" attribute:
- *
- *        attributes: {
- * 		  	limit: {
- * 		  		type: 'number',
- * 		  		default: 5, (10 is already set as default)
- * 		  	},
- * 		},
- *
- * Step 3: wrap the QueryLimit component within
- * the InspectorControls and PanelBody components
- * (currently shown within a block's edit() function)
- *
- *        edit: ( ( { attributes, setAttributes } ) => {
- * 			const { limit } = attributes;
- * 			return (
- * 				<InspectorControls>
- * 		  			<PanelBody>
- * 		  				<QueryLimit
- * 		  					limit={ limit }
- * 		  					onLimitChange={
- * 		  						( value ) => setAttributes( { limit: value } )
- * 		  					}
- * 		  				/>
- * 		  			</PanelBody>
- * 		  		</InspectorControls>
- * 		  	);
- * 		}),
- */
-
-/**
  * External dependencies
  */
 import { __ } from '@eventespresso/i18n';
