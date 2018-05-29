f58f509d265c4f6c3bf9f1788263611835adbf42
Brent Christensen <brent@eventespresso.com>
2018-05-29 12:28:29 -0700

    add TermSelectControl component

---
+++ assets/src/components/selection/term-select/index.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { unescape as unescapeString, repeat, flatMap, compact } from 'lodash';
+import { __ } from '@eventespresso/i18n';
+
+/**
+ * WordPress dependencies
+ */
+const { SelectControl } = '@wordpress/components';
+
+function getSelectOptions( terms, level = 0 ) {
+	return flatMap( terms, ( term ) => [
+		{
+			value: term.slug,
+			label: repeat( '\u00A0', level * 3 ) + unescapeString( term.name ),
+		},
+		...getSelectOptions( term.children, level + 1 ),
+	] );
+}
+
+export const TermSelectControl = ( {
+	selectedTerm,
+	terms,
+	label,
+	noOptionLabel,
+	onChange,
+} ) => {
+	const options = compact( [
+		noOptionLabel && { value: '', label: noOptionLabel },
+		...getSelectOptions( terms ),
+	] );
+	return (
+		<SelectControl
+			{ ...{ label, options, onChange } }
+			value={ selectedTerm }
+		/>
+	);
+};
