34c6dc1722714385b0d3c4be00733a54cfb3d0db
Darren Ethier <darren@roughsmootheng.in>
2018-05-31 17:53:12 -0400

    Fix bug with lodash in vendor bundle trouncing the _ global used by underscore. (#495)
    * make sure we use noConflict() mode for lodash
    
    This ensures that lodash is not exported to the `_` global and thus trouncing any underscore usage present in the loaded js.

--- assets/src/eejs/vendor/index.js
+++ assets/src/eejs/vendor/index.js
@@ -4,6 +4,6 @@ module.exports = {
 	reactRedux: require( 'react-redux' ),
 	redux: require( 'redux' ),
 	classnames: require( 'classnames' ),
-	lodash: require( 'lodash' ),
+	lodash: require( 'lodash' ).noConflict(),
 	moment: require( 'moment' ),
 };
