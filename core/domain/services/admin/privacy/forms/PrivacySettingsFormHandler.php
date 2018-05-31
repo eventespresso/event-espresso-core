d2036ae88a12d55a5b21af9d4da8b14886c9a78f
Mike Nelson <michael@eventespresso.com>
2018-05-31 10:26:00 -0700

    enable the session saving when using the privacy policy settings form (most forms will probably want this too but for now this form certainly does)

--- core/domain/services/admin/privacy/forms/PrivacySettingsFormHandler.php
+++ core/domain/services/admin/privacy/forms/PrivacySettingsFormHandler.php
@@ -61,6 +61,8 @@ class PrivacySettingsFormHandler extends FormHandler
      */
     public function generate()
     {
+        //this form makes use of the session for passing around invalid form submission data, so make sure its enabled
+        add_filter('FHEE__EE_Session___save_session_to_db__abort_session_save', '__return_false');
         /**
          * @var $reg_config EE_Registration_Config
          */
