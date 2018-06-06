db9d748c2c75aa634b7ef34c73b817d1b7243edf
Brent Christensen <brent@eventespresso.com>
2018-06-06 15:49:42 -0700

    Add DatetimeSelect component (#504)
    * DatetimeSelect component
    
    * convert component to class and fix props passing
    
    * fix linting errors
    
    * rebuild assets
    
    * remove unnecessary constructor
    
    * rebuild assets
    
    * rebuild assets
    
    * add addAllOption and addAllOptionLabel parameters
    
    * utilize addAllOption and addAllOptionLabel options
    
    * don't import translations

--- assets/src/components/index.js
+++ assets/src/components/index.js
@@ -2,3 +2,4 @@
 export { QueryLimit } from './query/limit';
 // selection components
 export { default as EventSelect } from './selection/event-select';
+export { default as DatetimeSelect } from './selection/datetime-select';
