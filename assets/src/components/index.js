557ec3f2b430546c29997431dc6064d0f0cf8c81
Brent Christensen <brent@eventespresso.com>
2018-06-06 14:08:29 -0700

    import contact entity

--- assets/src/components/index.js
+++ assets/src/components/index.js
@@ -1,5 +1,6 @@
+// entity components
+export * from './entities/contact/';
 // query components
 export { QueryLimit } from './query/limit';
 // selection components
 export { default as EventSelect } from './selection/event-select';
-export * from './entities/contact/'
