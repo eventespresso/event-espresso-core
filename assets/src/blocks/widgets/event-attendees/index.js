6fa7f3ca6ce17e5a5a1b96374eed5be05274ae6f
Brent Christensen <brent@eventespresso.com>
2018-06-06 14:04:54 -0700

    add transforms data for converting ESPRESSO_EVENT_ATTENDEES  shortcode

--- assets/src/blocks/widgets/event-attendees/index.js
+++ assets/src/blocks/widgets/event-attendees/index.js
@@ -62,6 +62,77 @@ registerBlockType(
 			},
 		},
 
+		transforms: {
+			from: [
+				{
+					type: 'shortcode',
+					tag: 'ESPRESSO_EVENT_ATTENDEES',
+					attributes: {
+						eventId: {
+							type: 'number',
+							shortcode: ( { named: { event_id } } ) => {
+								if ( ! event_id ) {
+									return;
+								}
+								return event_id;
+							}
+						},
+						datetimeId: {
+							type: 'number',
+							shortcode: ( { named: { datetime_id } } ) => {
+								if ( ! datetime_id ) {
+									return;
+								}
+								return datetime_id;
+							}
+						},
+						ticketId: {
+							type: 'number',
+							shortcode: ( { named: { ticket_id } } ) => {
+								if ( ! ticket_id ) {
+									return;
+								}
+								return ticket_id;
+							}
+						},
+						status: {
+							type: 'string',
+							shortcode: ( { named: { status } } ) => {
+								if ( ! status ) {
+									return;
+								}
+								return status;
+							}
+						},
+						showGravatar: {
+							type: 'boolean',
+							shortcode: ( { named: { show_gravatar } } ) => {
+								switch ( show_gravatar ) {
+									case 'true':
+									case '1':
+										return true;
+									default:
+										return false;
+								}
+							}
+						},
+						displayOnArchives: {
+							type: 'boolean',
+							shortcode: ( { named: { display_on_archives } } ) => {
+								switch ( display_on_archives ) {
+									case 'true':
+									case '1':
+										return true;
+									default:
+										return false;
+								}
+							}
+						},
+					}
+				}
+			]
+		},
+
 		edit: EventAttendeesEditor,
 
 		save() {
