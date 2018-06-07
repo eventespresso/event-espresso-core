82666687f7cb2389dd0d52034731eaa216943e8a
Brent Christensen <brent@eventespresso.com>
2018-06-07 15:54:09 -0700

    add TicketSelect component (#506)
    * add TicketSelect component
    
    * remove invalid line of code from other branch

--- assets/src/components/index.js
+++ assets/src/components/index.js
@@ -3,3 +3,4 @@ export { QueryLimit } from './query/limit';
 // selection components
 export { default as EventSelect } from './selection/event-select';
 export { default as DatetimeSelect } from './selection/datetime-select';
+export { default as TicketSelect } from './selection/ticket-select';
