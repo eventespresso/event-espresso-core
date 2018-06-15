0744d910b3de314519df22fd303dc1a970aa3705
Josh Feck <josh@nxnweb.com>
2018-06-15 15:07:56 -0400

    escape strings

--- admin_pages/venues/templates/venue_address_metabox_content.template.php
+++ admin_pages/venues/templates/venue_address_metabox_content.template.php
@@ -3,17 +3,17 @@
         <td valign="top">
             <fieldset>
                 <p>
-                    <label for="phys-addr"><?php _e('Address:', 'event_espresso'); ?></label><br/>
+                    <label for="phys-addr"><?php esc_html_e('Address:', 'event_espresso'); ?></label><br/>
                     <input class="all-options" id="phys-addr" type="text" value="<?php $_venue->f('VNU_address'); ?>"
                            name="vnu_address"/>
                 </p>
                 <p>
-                    <label for="phys-addr-2"><?php _e('Address 2:', 'event_espresso'); ?></label><br/>
+                    <label for="phys-addr-2"><?php esc_html_e('Address 2:', 'event_espresso'); ?></label><br/>
                     <input class="all-options" id="phys-addr-2" type="text" value="<?php $_venue->f('VNU_address2'); ?>"
                            name="vnu_address2"/>
                 </p>
                 <p>
-                    <label for="phys-city"><?php _e('City:', 'event_espresso'); ?></label><br/>
+                    <label for="phys-city"><?php esc_html_e('City:', 'event_espresso'); ?></label><br/>
                     <input class="all-options" id="phys-city" type="text" value="<?php $_venue->f('VNU_city'); ?>"
                            name="vnu_city"/>
                 </p>
@@ -21,7 +21,7 @@
                     <?php echo $states_dropdown; ?>
                 </p>
                 <p>
-                    <label for="zip-postal"><?php _e('Zip/Postal Code:', 'event_espresso'); ?></label><br/>
+                    <label for="zip-postal"><?php esc_html_e('Zip/Postal Code:', 'event_espresso'); ?></label><br/>
                     <input class="all-options" id="zip-postal" type="text" value="<?php $_venue->f('VNU_zip'); ?>"
                            name="vnu_zip"/>
                 </p>
