a387930025266535de030938abd3f2f85ac414b1
Brent Christensen <brent@eventespresso.com>
2018-06-18 14:38:06 -0700

    add EditorStatusSelect that wraps StatusSelect with BaseControl

--- assets/src/components/form/select/model-selects/index.js
+++ assets/src/components/form/select/model-selects/index.js
@@ -1,4 +1,7 @@
 export { default as EventSelect } from './event';
 export { default as DatetimeSelect } from './datetime';
-export { default as StatusSelect } from './status';
+export {
+	default as StatusSelect,
+	editorStatusSelect as EditorStatusSelect,
+} from './status';
 export { default as TicketSelect } from './ticket';
