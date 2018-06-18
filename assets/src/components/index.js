39eda3ea3bcac0c352c22ea68cd22ad898fe9c60
Brent Christensen <brent@eventespresso.com>
2018-06-18 14:11:20 -0700

    delete \components\selection\ and registration-status-select

--- assets/src/components/index.js
+++ assets/src/components/index.js
@@ -2,9 +2,5 @@
 export { QueryLimit } from './query/limit';
 // selection components
 export * from './form/select';
-export {
-	default as RegistrationStatusSelect,
-} from './selection/registration-status-select';
-
 // general ui components
 export * from './ui';
