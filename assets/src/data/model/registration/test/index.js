1b662049a5ca72a02ba89f2e3be7436385100b3f
Brent Christensen <brent@eventespresso.com>
2018-06-25 12:49:45 -0700

    tweak tests and add one for when ALL parameters provided

--- assets/src/data/model/registration/test/index.js
+++ assets/src/data/model/registration/test/index.js
@@ -18,28 +18,40 @@ describe( 'mapOrderBy()', () => {
 } );
 
 describe( 'whereConditions()', () => {
-	it( 'returns empty string when no parameters provided', () => {
+	it( 'returns empty string when NO parameters provided', () => {
 		expect( whereConditions( {} ) ).toEqual( '' );
 	} );
 	it( 'returns expected string for supplied Event ID', () => {
 		const testObject = { forEventId: 123 };
-		expect( whereConditions( testObject ) ).toEqual( 'where[EVT_ID]=' + 123 );
+		expect( whereConditions( testObject ) ).toEqual( 'where[EVT_ID]=123' );
 	} );
 	it( 'returns expected string for supplied Attendee ID', () => {
 		const testObject = { forAttendeeId: 123 };
-		expect( whereConditions( testObject ) ).toEqual( 'where[ATT_ID]=' + 123 );
+		expect( whereConditions( testObject ) ).toEqual( 'where[ATT_ID]=123' );
 	} );
 	it( 'returns expected string for supplied Transaction ID', () => {
 		const testObject = { forTransactionId: 123 };
-		expect( whereConditions( testObject ) ).toEqual( 'where[TXN_ID]=' + 123 );
+		expect( whereConditions( testObject ) ).toEqual( 'where[TXN_ID]=123' );
 	} );
 	it( 'returns expected string for supplied Ticket ID', () => {
 		const testObject = { forTicketId: 123 };
-		expect( whereConditions( testObject ) ).toEqual( 'where[TKT_ID]=' + 123 );
+		expect( whereConditions( testObject ) ).toEqual( 'where[TKT_ID]=123' );
 	} );
 	it( 'returns expected string for supplied Status ID', () => {
 		const testObject = { forStatusId: 'RPP' };
 		expect( whereConditions( testObject ) ).toEqual( 'where[STS_ID]=RPP' );
+	} );
+	it( 'returns expected string when ALL parameters provided', () => {
+		const testObject = {
+			forEventId: 1,
+			forAttendeeId: 2,
+			forTransactionId: 3,
+			forTicketId: 4,
+			forStatusId: 'RNA',
+		};
+		expect( whereConditions( testObject ) ).toEqual(
+			'where[EVT_ID]=1&where[ATT_ID]=2&where[TXN_ID]=3&where[TKT_ID]=4&where[STS_ID]=RNA'
+		);
 	} );
 } );
 
