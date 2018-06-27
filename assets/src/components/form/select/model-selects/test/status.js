236587a318f0302a73ff71d0244b6cda015ca890
Brent Christensen <brent@eventespresso.com>
2018-06-26 17:21:45 -0700

    default status order is ASC

--- assets/src/components/form/select/model-selects/test/status.js
+++ assets/src/components/form/select/model-selects/test/status.js
@@ -1,13 +1,13 @@
 import StatusSelect from '../status';
 import { shallow } from 'enzyme';
-import { QUERY_ORDER_DESC } from 'assets/src/data/model/base';
+import { QUERY_ORDER_ASC } from 'assets/src/data/model/base';
 import * as statusModel from 'assets/src/data/model/status';
 
 describe( 'StatusSelect()', () => {
 	const expectedDefaultQueryData = {
 		limit: 25,
 		orderBy: 'statusCode',
-		order: QUERY_ORDER_DESC,
+		order: QUERY_ORDER_ASC,
 		statusType: statusModel.STATUS_TYPE_REGISTRATION,
 	};
 	it( 'matches snapshot with default props plus provided statusType', () => {
