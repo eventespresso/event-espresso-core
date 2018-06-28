6288f9b534a7550aba9a9376d4964ee70b7f2ab0
Darren Ethier <darren@roughsmootheng.in>
2018-06-28 07:49:16 -0400

    implement new editor select for datetime select

--- assets/src/components/form/select/model-selects/datetime.js
+++ assets/src/components/form/select/model-selects/datetime.js
@@ -3,6 +3,7 @@
  */
 import ModelSelect from '../model-select';
 import { dateTimeModel } from '../../../../data/model';
+import { withBaseControl } from '../../../../higher-order-components';
 
 /**
  * External imports
@@ -87,3 +88,10 @@ export default class DatetimeSelect extends Component {
 		return <ModelSelect { ...props } />;
 	}
 }
+
+/**
+ * Enhanced Datetime Select for the WordPress Editor
+ */
+export const EditorDatetimeSelect = withBaseControl(
+	'select-datetime'
+)( DatetimeSelect );
