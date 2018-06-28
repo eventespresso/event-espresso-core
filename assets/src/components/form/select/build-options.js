f0ede666c200ab24bdfd09713842af1184465b44
Brent Christensen <brent@eventespresso.com>
2018-06-28 11:18:02 -0700

    remove DEFAULT_MODEL_OPTIONS_MAP and refactor mapping logic

--- assets/src/components/form/select/build-options.js
+++ assets/src/components/form/select/build-options.js
@@ -1,65 +1,59 @@
 /**
  * External dependencies
  */
-import { reduce, isFunction } from 'lodash';
-
-/**
- * Internal dependencies
- */
-import { dateTimeModel } from '../../../data/model';
-
-/**
- * A default map used for mapping options for select.
- * @type {Object}
- */
-const DEFAULT_MODEL_OPTIONS_MAP = {
-	event: {
-		label: 'EVT_name',
-		value: 'EVT_ID',
-	},
-	datetime: {
-		label: ( entity ) => {
-			return dateTimeModel.prettyDateFromDateTime( entity );
-		},
-		value: 'DTT_ID',
-	},
-	ticket: {
-		label: 'TKT_name',
-		value: 'TKT_ID',
-	},
-};
+import { isEmpty, isFunction, isUndefined, reduce } from 'lodash';
 
 /**
  * Receives an array of event entities and returns an array of simple objects
  * that can be passed along to the options array used for the WordPress
  * SelectControl component.
  *
- * @param { Array } entities
- * @param { string } modelName
- * @param { Object } map
- * @return { Array }  Returns an array of simple objects formatted for any
- * select control that receives its options in the format of an array of objects
- * with label and value keys.
+ * @param { Array }  entities
+ * @param { Object } optionsEntityMap 	Object of key value pairs where values are the
+ * 										'label:value' pairings used to populate the Select input
+ * @param { string } mapSelection		Determines which optionEntityMap pairing to use
+ * @return { Array }  					Returns an array of simple objects formatted for any
+ * 										select control that receives its options
+ * 										in the format of an array of objects
+ *										with label and value keys.
  */
 const buildOptions = (
 	entities,
-	modelName,
-	map = DEFAULT_MODEL_OPTIONS_MAP,
+	optionsEntityMap,
+	mapSelection = 'default'
 ) => {
-	const MAP_FOR_MODEL = map[ modelName ] ? map[ modelName ] : false;
-	return entities && MAP_FOR_MODEL ?
-		reduce( entities, function( options, entity ) {
-			const label = isFunction( MAP_FOR_MODEL.label ) ?
-				MAP_FOR_MODEL.label( entity ) :
-				entity[ MAP_FOR_MODEL.label ];
-			const value = isFunction( MAP_FOR_MODEL.value ) ?
-				MAP_FOR_MODEL.value( entity ) :
-				entity[ MAP_FOR_MODEL.value ];
-			if ( label && value ) {
-				options.push( { label, value } );
-			}
-			return options;
-		}, [] ) :
+	if ( isEmpty( entities ) || isEmpty( optionsEntityMap ) ) {
+		return [];
+	}
+	// if requested mapSelection exists then use that
+	let map = ! isUndefined( optionsEntityMap[ mapSelection ] ) ?
+		optionsEntityMap[ mapSelection ] :
+		null;
+	// if not but default exists then use that
+	map = map === null && ! isUndefined( optionsEntityMap.default ) ?
+		optionsEntityMap.default :
+		null;
+	// if default doesn't exist then just grab first option
+	map = map === null ?
+		optionsEntityMap[ Object.keys( optionsEntityMap )[ 0 ] ] :
+		map;
+	return ! isEmpty( map ) ?
+		reduce(
+			entities,
+			function( options, entity ) {
+				const label = isFunction( map.label ) ?
+					map.label( entity ) :
+					entity[ map.label ];
+				const value = isFunction( map.value ) ?
+					map.value( entity ) :
+					entity[ map.value ];
+				if ( label && value ) {
+					options.push( { label, value } );
+				}
+				return options;
+			},
+			[]
+		) :
 		[];
 };
 
