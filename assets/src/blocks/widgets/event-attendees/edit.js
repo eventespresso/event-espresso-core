53d16e82c36f615a58cdd57a6e80cab8c3304bba
Brent Christensen <brent@eventespresso.com>
2018-07-05 13:52:13 -0700

    update after GBM changes
    use statusModel.REGISTRATION_STATUS_ID constant
    use attendeeModel.getQueryString()
    remove uniqBy() usage
    don't set min limit of -1
    remove addAllOptionLabel usage

--- assets/src/blocks/widgets/event-attendees/edit.js
+++ assets/src/blocks/widgets/event-attendees/edit.js
@@ -18,6 +18,7 @@ import {
 	QueryLimit,
 } from '@eventespresso/components';
 import * as statusModel from '../../../data/model/status';
+import * as attendeeModel from '../../../data/model/attendee';
 import PropTypes from 'prop-types';
 import { isEmpty, uniqBy } from 'lodash';
 
@@ -88,10 +89,10 @@ class EventAttendeesEditor extends Component {
 	};
 
 	setStatus = ( status ) => {
-		const value = status !== null && status.value ? status.value : 'ANY';
-		console.log( 'status: ' + value );
+		const value = status !== null && status.value ?
+			status.value :
+			statusModel.REGISTRATION_STATUS_ID.APPROVED;
 		this.props.setAttributes( { status: value } );
-		console.log( 'this.props.attributes.status: ' + this.props.attributes.status );
 	};
 
 	setLimit = ( limit ) => {
@@ -141,15 +142,12 @@ class EventAttendeesEditor extends Component {
 	}
 
 	getAttendeesDisplay() {
-		return (
-			<EventAttendeesList attendees={ this.props.attendees }/>
-		);
+		return <EventAttendeesList attendees={ this.props.attendees }/>;
 	}
 
 	render() {
 		const { attributes } = this.props;
-		let { attendees } = this.props;
-		attendees = uniqBy( attendees, 'ATT_ID' );
+		const { attendees } = this.props;
 		const attendeesBlock = isEmpty( attendees ) ?
 			this.getNoAttendeesContent() :
 			this.getAttendeesDisplay();
@@ -166,11 +164,6 @@ class EventAttendeesEditor extends Component {
 						selectedDatetimeId={ attributes.datetimeId }
 						forEventId={ attributes.eventId }
 						onDatetimeSelect={ this.setDatetimeId }
-						addAllOptionLabel={
-							__( 'Show Attendees for All Datetimes',
-								'event_espresso'
-							)
-						}
 					/>
 					<EditorTicketSelect
 						key="attendees-ticket-select"
@@ -178,28 +171,20 @@ class EventAttendeesEditor extends Component {
 						forEventId={ attributes.eventId }
 						forDatetimeId={ attributes.datetimeId }
 						onTicketSelect={ this.setTicketId }
-						addAllOptionLabel={
-							__( 'Show Attendees for All Tickets', 'event_espresso' )
-						}
 					/>
 					<EditorStatusSelect
 						statusType={ statusModel.STATUS_TYPE_REGISTRATION }
 						selectedStatusId={ attributes.status }
 						onStatusSelect={ this.setStatus }
 						selectLabel={ __( 'Select Registration Status', 'event_espresso' ) }
-						addAllOptionLabel={
-							__( 'Show Attendees for All Registration Statuses', 'event_espresso' )
-						}
 					/>
 					<QueryLimit
 						label={ __( 'Number of Attendees to Display', 'event_espresso' ) }
 						limit={ attributes.limit }
 						onLimitChange={ this.setLimit }
-						min={ -1 }
 					/>
 					<ToggleControl
 						label={ __( 'Display Gravatar', 'event_espresso' ) }
-						limit={ attributes.limit }
 						checked={ attributes.showGravatar }
 						onChange={ this.toggleShowGravatar }
 					/>
@@ -220,41 +205,17 @@ class EventAttendeesEditor extends Component {
 
 export default withSelect( ( select, ownProps ) => {
 	const { attributes } = ownProps;
-	let { eventId, datetimeId, ticketId, status, showGravatar, limit } = attributes;
 	const { getItems, isRequestingItems } = select( 'eventespresso/lists' );
-	limit = parseInt( limit, 10 );
-	limit = !isNaN( limit ) ? limit : -1;
-	if ( limit === 0 ) {
-		return {
-			attendees: [],
-			isLoading: false,
-		};
-	}
-	// ensure that entity IDs are integers
-	ticketId = parseInt( ticketId, 10 );
-	datetimeId = parseInt( datetimeId, 10 );
-	eventId = parseInt( eventId, 10 );
-	const queryParams = [];
-	// add query param for entity we are filtering for
-	if ( ticketId !== 0 && !isNaN( ticketId ) ) {
-		queryParams.push( `where[Registration.Ticket.TKT_ID]=${ ticketId }` );
-	} else if ( datetimeId !== 0 && !isNaN( datetimeId ) ) {
-		queryParams.push( `where[Registration.Ticket.Datetime.DTT_ID]=${ datetimeId }` );
-	} else if ( eventId !== 0 && !isNaN( eventId ) ) {
-		queryParams.push( `where[Registration.EVT_ID]=${ eventId }` );
-	}
-	if ( status !== '' && status !== null && status !== 'ANY' ) {
-		queryParams.push( `where[Registration.Status.STS_ID]=${ status }` );
-	}
-	if ( showGravatar === true ) {
-		queryParams.push( 'calculate=userAvatar' );
-	}
-	if ( limit > 0 ) {
-		queryParams.push( `limit=${ limit }` );
-	}
-	queryParams.push( 'order_by[ATT_lname]=ASC' );
-	queryParams.push( 'order_by[ATT_fname]=ASC' );
-	const queryString = queryParams.join( '&' );
+	const queryString = attendeeModel.getQueryString(
+		{
+			forEventId: attributes.eventId,
+			forDatetimeId: attributes.datetimeId,
+			forTicketId: attributes.ticketId,
+			forStatusId: attributes.status,
+			showGravatar: attributes.showGravatar,
+			limit: attributes.limit,
+		}
+	);
 	// console.log( 'GET attendees ' + queryString );
 	return {
 		attendees: getItems( 'attendee', queryString ),
