2a4f1f4de1d611ce66e02f8d6b5566ac21096bb4
Brent Christensen <brent@eventespresso.com>
2018-07-20 10:31:35 -0700

    Enable Webpack CSS loading for Components (#599)
    * add eejs.hocComponents as external for components
    * add rules for css loading to components config
    * add CSS_HANDLE_EE_COMPONENTS const and register stylesheet

--- webpack.common.js
+++ webpack.common.js
@@ -144,6 +144,7 @@ const config = [
 			components: assets + 'components/index.js',
 		},
 		externals: Object.assign( externals, {
+			'@eventespresso/higher-order-components': 'eejs.hocComponents',
 			'@eventespresso/helpers': 'eejs.helpers',
 			'@eventespresso/model': 'eejs.model',
 		} ),
@@ -153,6 +154,36 @@ const config = [
 					test: /\.js$/,
 					exclude: /node_modules/,
 					loader: 'babel-loader',
+				},
+				{
+					test: /\.css$/,
+					use: [
+						miniExtract.loader,
+						{
+							loader: 'css-loader',
+							query: {
+								modules: true,
+								localIdentName: '[local]',
+							},
+							//can't use minimize because cssnano (the
+							// dependency) doesn't parser the browserlist
+							// extension in package.json correctly, there's
+							// a pending update for it but css-loader
+							// doesn't have the latest yet.
+							// options: {
+							//     minimize: true
+							// }
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: function() {
+									return [ autoprefixer ];
+								},
+								sourceMap: true,
+							},
+						},
+					],
 				},
 			],
 		},
