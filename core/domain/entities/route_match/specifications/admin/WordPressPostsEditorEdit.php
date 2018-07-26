70f75a95c4c39b941e8461bc8d59ffb58795c359
Brent Christensen <brent@eventespresso.com>
2018-07-26 16:49:40 -0700

    WordPressPostsEdit
    Returns true when the current request is for the WordPress Post Editor admin page while editing an existing post

---
+++ core/domain/entities/route_match/specifications/admin/WordPressPostsEditorEdit.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace EventEspresso\core\domain\entities\route_match\specifications\admin;
+
+use EventEspresso\core\domain\entities\route_match\RouteMatchSpecification;
+
+/**
+ * Class WordPressPostsEdit
+ * Returns true when the current request is for the WordPress Post Editor admin page while editing an existing post
+ *
+ * @package EventEspresso\core\domain\entities\route_match\specifications
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+class WordPressPostsEditorEdit extends RouteMatchSpecification
+{
+    /**
+     * returns true if current request matches specification
+     *
+     * @since $VID:$
+     * @return boolean
+     */
+    public function routeMatches()
+    {
+        return strpos($this->request->requestUri(),'wp-admin/post.php' ) !== false
+            && (
+                $this->request->getRequestParam('post_type') === 'post'
+                || $this->request->requestParamIsSet('post_type') === false
+            )
+            && $this->request->getRequestParam('action') === 'edit';
+    }
+}
