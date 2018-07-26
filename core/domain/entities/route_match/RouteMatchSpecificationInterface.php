874ae9bba290348fcdaf93fc531f53c08bf30927
Brent Christensen <brent@eventespresso.com>
2018-07-26 16:20:03 -0700

    RouteMatchSpecification  and RouteMatchSpecificationInterface
    Variation of the Specification design pattern for matching current request to specific routes

---
+++ core/domain/entities/route_match/RouteMatchSpecificationInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace EventEspresso\core\domain\entities\route_match;
+
+/**
+ * Class RouteMatchSpecificationInterface
+ * Variation of the Specification design pattern for matching current request to specific routes
+ *
+ * @package EventEspresso\core\domain\entities\route_match\specifications
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+interface RouteMatchSpecificationInterface
+{
+    /**
+     * returns true if current request matches specification
+     *
+     * @since $VID:$
+     * @return boolean
+     */
+    public function routeMatches();
+}
