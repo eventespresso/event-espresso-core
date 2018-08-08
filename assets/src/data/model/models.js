23e763c0ddf2292bece8ba644cefcf7554822173
Darren Ethier <darren@roughsmootheng.in>
2018-08-08 15:30:29 -0400

    add model and tests for checkin

--- assets/src/data/model/models.js
+++ assets/src/data/model/models.js
@@ -3,5 +3,12 @@ import * as eventModel from './event';
 import * as registrationModel from './registration';
 import * as statusModel from './status';
 import * as ticketModel from './ticket';
-
-export { dateTimeModel, eventModel, registrationModel, statusModel, ticketModel };
+import * as checkInModel from './checkin';
+export {
+	dateTimeModel,
+	eventModel,
+	registrationModel,
+	statusModel,
+	ticketModel,
+	checkInModel,
+};
