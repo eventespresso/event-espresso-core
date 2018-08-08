540127964ced18527dbb5b69c51ef06bf0f67d9c
Mike Nelson <michael@eventespresso.com>
2018-08-08 15:15:32 -0700

    first use sanitize_title to remove funky characters (like we would do with post slugs) but then urldecode that to remove percent-encoded unicode characters as WP has trouble with those in post slugs, see https://core.trac.wordpress.org/ticket/44759

--- caffeinated/admin/extend/events/Extend_Events_Admin_Page.core.php
+++ caffeinated/admin/extend/events/Extend_Events_Admin_Page.core.php
@@ -866,16 +866,15 @@ class Extend_Events_Admin_Page extends Events_Admin_Page
         $old_slug = EE_Registry::instance()->CFG->core->event_cpt_slug;
         EE_Registry::instance()->CFG->core->event_cpt_slug = empty($this->_req_data['event_cpt_slug'])
             ? EE_Registry::instance()->CFG->core->event_cpt_slug
-            : sanitize_title_with_dashes(
-                // first sanitize_title, like WP's get_sample_permalink() does
-                sanitize_title(
-                    $this->_req_data['event_cpt_slug'],
-                    esc_html__('events', 'event_espresso'),
-                    'save'
-                ),
-                '',
-                'save'
-            );
+            :   // url decode after sanitizing title to restore unicode characters,
+                // see https://github.com/eventespresso/event-espresso-core/issues/575
+                urldecode(
+                    sanitize_title(
+                        $this->_req_data['event_cpt_slug'],
+                        esc_html__('events', 'event_espresso'),
+                        'save'
+                    )
+                );
         $what = 'Template Settings';
         $success = $this->_update_espresso_configuration(
             $what,
