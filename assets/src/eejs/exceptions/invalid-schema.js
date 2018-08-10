3b2a951984c0c96ed2d03740279289ab150c129d
Darren Ethier <darren@roughsmootheng.in>
2018-08-10 16:46:56 -0400

    Create folder for Custom errors and add initial custom errors (#643)
    * Create folder for Custom errors and add initial custom errors
    
    * fix phpdocs
    
    * expand on phpdocs

---
+++ assets/src/eejs/exceptions/invalid-schema.js
@@ -0,0 +1,22 @@
+/**
+ * InvalidSchema
+ * Usage: throw new eejs.InvalidSchema('some message', [schema object])
+ *
+ * Typically this error is thrown when an object representing a model schema
+ * (at a minimum) does not have a "properties" property).
+ *
+ * @param {string} msg
+ * @param {mixed} schema Optional, the schema object which will be added to a
+ * schema property.
+ */
+export default class InvalidSchema extends TypeError {
+	constructor( ...args ) {
+		super( ...args );
+		if ( Error.captureStackTrace ) {
+			Error.captureStackTrace( this, InvalidSchema );
+		}
+		this.message = 'Invalid schema object provided. Must have a' +
+			' "properties" property.' + this.message;
+		this.schema = args[ 1 ] || {};
+	}
+}
