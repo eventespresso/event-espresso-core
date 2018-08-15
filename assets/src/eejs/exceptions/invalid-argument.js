744af719c4c6a0bb8d2ad65cf956be1ecd435c31
Darren Ethier <darren@roughsmootheng.in>
2018-08-15 11:11:02 -0400

    remove unnecessary parameter

--- assets/src/eejs/exceptions/invalid-argument.js
+++ assets/src/eejs/exceptions/invalid-argument.js
@@ -1,13 +1,12 @@
 /**
  * InvalidArgument
- * Usage: throw new eejs.InvalidArgument('some message', [parameter, argument])
+ * Usage: throw new eejs.InvalidArgument('some message'[, argument])
  *
  * Typically this error is thrown when a function or method is called with an
  * invalid argument for a given parameter.  It could still be the right type
  * but its an unexpected value.
  *
  * @param {string} msg
- * @param {string} parameter Optional, the name of the parameter.
  * @param {mixed} argument Optional, the argument that caused the error.
  */
 export default class InvalidArgument extends Error {
@@ -17,7 +16,6 @@ export default class InvalidArgument extends Error {
 			Error.captureStackTrace( this, InvalidArgument );
 		}
 		this.message = 'Invalid argument provided.' + this.message;
-		this.parameter = args[ 1 ] || '';
-		this.argument = args[ 2 ] || null;
+		this.argument = args[ 1 ] || null;
 	}
 }
