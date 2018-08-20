603cb0fcf30cff7c027a130099fb698d7bcb0fa3
Brent Christensen <brent@eventespresso.com>
2018-08-20 11:16:12 -0700

    change to extend MatchAnyRouteSpecification
    use EspressoStandardPostTypeEditor and EspressoAttendeePostTypeEditor to detect all EE CPT Editor routes

--- core/domain/entities/route_match/specifications/admin/EspressoPostTypeEditor.php
+++ core/domain/entities/route_match/specifications/admin/EspressoPostTypeEditor.php
@@ -2,30 +2,38 @@
 
 namespace EventEspresso\core\domain\entities\route_match\specifications\admin;
 
-use EventEspresso\core\domain\entities\route_match\RouteMatchSpecification;
+use EventEspresso\core\domain\entities\route_match\MatchAnyRouteSpecification;
+use EventEspresso\core\exceptions\InvalidEntityException;
+use EventEspresso\core\services\request\RequestInterface;
 
 /**
  * Class EspressoPostTypeEditor
- * Returns true when the current request is for an EE CPT Editor admin page while creating a new event
+ * Composite MatchAnyRouteSpecification using EspressoStandardPostTypeEditor and EspressoAttendeePostTypeEditor
+ * that will return true if either of the supplied Route Match Specifications returns true
+ *  ie: returns true when the current request is for ANY EE CPT editor
  *
  * @package EventEspresso\core\domain\entities\route_match\specifications
  * @author  Brent Christensen
  * @since   $VID:$
  */
-class EspressoPostTypeEditor extends RouteMatchSpecification
+class EspressoPostTypeEditor extends MatchAnyRouteSpecification
 {
     /**
-     * returns true if current request matches specification
+     * EspressoEventEditor constructor.
      *
-     * @since $VID:$
-     * @return boolean
+     * @param EspressoStandardPostTypeEditor $standard_route_match
+     * @param EspressoAttendeePostTypeEditor $attendee_route_match
+     * @param RequestInterface          $request
+     * @throws InvalidEntityException
      */
-    public function isMatchingRoute()
-    {
-        return $this->request->getMatch('espresso_*' ) !== false
-            && (
-                $this->request->getRequestParam('action') === 'edit'
-                || $this->request->getRequestParam('action') === 'create_new'
-            );
+    public function __construct(
+        EspressoStandardPostTypeEditor $standard_route_match,
+        EspressoAttendeePostTypeEditor $attendee_route_match,
+        RequestInterface $request
+    ) {
+        parent::__construct(
+            array($standard_route_match, $attendee_route_match),
+            $request
+        );
     }
 }
