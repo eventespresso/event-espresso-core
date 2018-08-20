63acbbe93b79f03f0f8819724bd22132746eaf13
Brent Christensen <brent@eventespresso.com>
2018-08-20 16:00:19 -0700

    disable eslint camelcase rule for old attribute var names

--- assets/src/blocks/event-attendees/index.js
+++ assets/src/blocks/event-attendees/index.js
@@ -75,28 +75,37 @@ export const settings = {
 				attributes: {
 					eventId: {
 						type: 'number',
+						// eslint-disable-next-line camelcase
 						shortcode: ( { named: { event_id } } ) => {
+							// eslint-disable-next-line camelcase
 							if ( ! event_id ) {
 								return;
 							}
+							// eslint-disable-next-line camelcase
 							return event_id;
 						},
 					},
 					datetimeId: {
 						type: 'number',
+						// eslint-disable-next-line camelcase
 						shortcode: ( { named: { datetime_id } } ) => {
+							// eslint-disable-next-line camelcase
 							if ( ! datetime_id ) {
 								return;
 							}
+							// eslint-disable-next-line camelcase
 							return datetime_id;
 						},
 					},
 					ticketId: {
 						type: 'number',
+						// eslint-disable-next-line camelcase
 						shortcode: ( { named: { ticket_id } } ) => {
+							// eslint-disable-next-line camelcase
 							if ( ! ticket_id ) {
 								return;
 							}
+							// eslint-disable-next-line camelcase
 							return ticket_id;
 						},
 					},
@@ -111,15 +120,20 @@ export const settings = {
 					},
 					showGravatar: {
 						type: 'boolean',
+						// eslint-disable-next-line camelcase
 						shortcode: ( { named: { show_gravatar } } ) => {
+							// eslint-disable-next-line camelcase
 							return show_gravatar === 'true' || show_gravatar ===
 								'1';
 						},
 					},
 					displayOnArchives: {
 						type: 'boolean',
+						// eslint-disable-next-line camelcase
 						shortcode: ( { named: { display_on_archives } } ) => {
+							// eslint-disable-next-line camelcase
 							return display_on_archives === 'true' ||
+								// eslint-disable-next-line camelcase
 								display_on_archives === '1';
 						},
 					},
