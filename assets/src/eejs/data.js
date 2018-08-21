1f89a2ce995df35a19314a38cb2c887b5cafc658
EE DevBox Server <admin@eventespresso.com>
2018-08-21 10:13:05 -0700

    Bumping version to 4.9.67.p and prepped for release

--- assets/src/eejs/data.js
+++
@@ -1,6 +0,0 @@
-/**
- * This will hold arbitrary data assigned by the Assets Registry.
- * @type {{}}
- */
-const data = eejsdata.data || {};
-export default data;
--- assets/src/eejs/vendor/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	react: require( 'react' ),
-	reactDom: require( 'react-dom' ),
-	reactRedux: require( 'react-redux' ),
-	redux: require( 'redux' ),
-	classnames: require( 'classnames' ),
-	lodash: require( 'lodash' ).noConflict(),
-	moment: require( 'moment' ),
-};
