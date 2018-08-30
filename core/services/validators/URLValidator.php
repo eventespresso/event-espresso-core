62cdf7793895be6833882a354fafb46782be469a
Mike Nelson <michael@eventespresso.com>
2018-08-30 12:35:42 -0700

    add initial url validator. Right now it just does filter_var, but thats just so we can build a baseline for testing

---
+++ core/services/validators/URLValidator.php
@@ -0,0 +1,29 @@
+<?php
+namespace EventEspresso\core\services\validators;
+/**
+ * Class URLValidator
+ *
+ * Replacement for `filter_var($url, FILTER_VALIDATE_URL)` because of all the problems mentioned on
+ * https://d-mueller.de/blog/why-url-validation-with-filter_var-might-not-be-a-good-idea/
+ *
+ * @package     Event Espresso
+ * @author         Mike Nelson
+ * @since         $VID:$
+ *
+ */
+class URLValidator
+{
+    /**
+     * Returns whether or not
+     * @since $VID:$
+     * @param $url
+     * @return boolean
+     */
+    public function isValid($url)
+    {
+        return filter_var($url, FILTER_VALIDATE_URL);
+    }
+
+}
+// End of file URLValidator.php
+// Location: ${NAMESPACE}/URLValidator.php
