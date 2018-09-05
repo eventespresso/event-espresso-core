7504575b355fc4b760d42645ee6d289753cb1293
Darren Ethier <darren@roughsmootheng.in>
2018-09-05 10:19:36 -0400

    fix jsdocs

--- assets/src/eejs/locale.js
+++ assets/src/eejs/locale.js
@@ -2,8 +2,8 @@ import data from './data';
 
 /**
  * Provided via the data passed along by the server.
- * This data a configuration object passed along from the server that exposes
- * the default timezone settings from the server.
+ * This data is a configuration object passed along from the server that exposes
+ * the default locale settings from the server.
  * @type {{}}
  */
 export const { locale = {
