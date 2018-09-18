15de3a9e99fdfd65f63692d7a81402b07216fb26
Darren Ethier <darren@roughsmootheng.in>
2018-09-18 11:34:32 -0400

    jsdocs, formatting and new functions

--- assets/src/data/model/entity-factory/validators.js
+++ assets/src/data/model/entity-factory/validators.js
@@ -7,17 +7,38 @@ import {
 	isString,
 	isPlainObject,
 	isBoolean,
+	isNumber,
 } from 'lodash';
 import { sprintf } from '@eventespresso/i18n';
 
 /**
  * Internal Imports
  */
-import { isEnumField } from './booleans';
-import { maybeConvertFromValueObject } from './extractors';
+import { isEnumField, isPrimaryKeyField } from './booleans';
+import { maybeConvertFromValueObjectWithAssertions } from './extractors';
 
+/**
+ * Validates the incoming value for given type.  Types allowed are:
+ *
+ * - integer: checks if value is an integer.
+ * - number: checks if value is classified as a Number primitive or object (this
+ *   means `Infinity`, `-Infinity`, and `NaN` are considered valid for this type
+ * - string
+ * - object - this validates as a "plainObject", that is an object created by
+ *   the Object constructor or one with a [[Prototype]] of null.
+ * - boolean
+ * - bool: (same as boolean check)
+ * - null: value must explicitly be `null`
+ *
+ * Note: if the passed in type does not exist, then the value is considered
+ * invalid.
+ *
+ * @param {string} type  The type to check
+ * @param {*} value  The value being validated
+ * @return {boolean}  True means the value is valid for the given type.
+ */
 export const validateType = ( type, value ) => {
-	let valid = true;
+	let valid = false;
 	// account for type definitions that are an array of allowed types.
 	if ( isArray( type ) ) {
 		for ( const singleType of type ) {
@@ -31,8 +52,10 @@ export const validateType = ( type, value ) => {
 	}
 	switch ( type ) {
 		case 'integer':
-		case 'number':
 			valid = isInteger( value );
+			break;
+		case 'number':
+			valid = isNumber( value );
 			break;
 		case 'string':
 			valid = isString( value );
@@ -48,22 +71,59 @@ export const validateType = ( type, value ) => {
 			valid = value === null;
 			break;
 	}
+	return valid;
 };
 
+/**
+ * This validates enum type of values.
+ *
+ * This means that the value must be one of the provided array of enumValues as
+ * well as being of the expected type.
+ *
+ * @param {string} type
+ * @param {Array} enumValues
+ * @param {*} value
+ * @return {boolean}  True means this value is valid.
+ */
 export const validateEnumType = ( type, enumValues, value ) => {
 	return validateType( type, value ) &&
+		isArray( enumValues ) &&
 		enumValues.indexOf( value ) > -1;
 };
 
+/**
+ * This method does a shallow validation for the given value and field.
+ *
+ * "Shallow" here means that if the field schema is of type 'object', then the
+ * validation only verifies that the value is an object.  The object contents
+ * are not validated.
+ *
+ * @param {string} fieldName
+ * @param {*} fieldValue
+ * @param {Object} schema
+ * @param {boolean} expectValueObjects  If true, then this flags the validator
+ * to assume the value might be a value object and attempt to retrieve the raw
+ * value from that value object for validation against the expected type in the
+ * schema for that field.
+ * @return {boolean}  True means the value is valid.
+ * @throws TypeError
+ * @throws InvalidDateTime
+ */
 export const isShallowValidValueForField = (
 	fieldName,
 	fieldValue,
 	schema,
 	expectValueObjects = true
 ) => {
+	// if field is a primary Key field then we override the validation so it can
+	// be either string or number
+	if ( isPrimaryKeyField( fieldName, schema ) ) {
+		return validateType( 'string', fieldValue ) ||
+			validateType( 'number', fieldValue );
+	}
 	const isEnum = isEnumField( fieldName, schema );
 	fieldValue = expectValueObjects ?
-		maybeConvertFromValueObject( fieldValue ) :
+		maybeConvertFromValueObjectWithAssertions( fieldValue ) :
 		fieldValue;
 	const isValid = isEnum ?
 		validateEnumType(
