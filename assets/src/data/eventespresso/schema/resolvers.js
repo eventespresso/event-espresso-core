5fd74b6494c621cb48e38c3ab248177f761b07e1
Darren Ethier <darren@roughsmootheng.in>
2018-10-16 11:19:44 -0400

    tweak how schema resolvers work and update tests
    - this allows for cross store usage of a resolver to retrieve the value when needed.

--- assets/src/data/eventespresso/schema/resolvers.js
+++ assets/src/data/eventespresso/schema/resolvers.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { isEmpty } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { receiveSchemaForModel, receiveFactoryForModel } from './actions';
@@ -8,23 +13,30 @@ import { fetchFromApi, select } from './controls';
 /**
  * A resolver for getting the schema for a given model name.
  * @param {string} modelName
+ * @return {Object} Retrieved schema.
  */
 export function* getSchemaForModel( modelName ) {
 	const path = getEndpoint( modelName );
 	const schema = yield fetchFromApi( { path, method: 'OPTIONS' } );
 	yield receiveSchemaForModel( modelName, schema );
+	return schema;
 }
 
 /**
  * A resolver for getting the model entity factory for a given model name.
  * @param {string} modelName
+ * @return {Object} retrieved factory
  */
 export function* getFactoryForModel( modelName ) {
-	const schema = yield select( 'getSchemaForModel', modelName );
+	let schema = yield select( 'getSchemaForModel', modelName );
+	if ( isEmpty( schema ) ) {
+		schema = yield getSchemaForModel( modelName );
+	}
 	const factory = createEntityFactory(
 		modelName,
 		schema.schema,
 		MODEL_PREFIXES( modelName )
 	);
 	yield receiveFactoryForModel( modelName, factory );
+	return factory;
 }
