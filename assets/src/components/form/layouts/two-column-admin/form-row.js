045639163305a5cb452687f371da1135f972a62b
Brent Christensen <brent@eventespresso.com>
2018-10-17 18:32:19 -0700

    FormRow
     "two-column-admin" form layout strategy component for displaying a single "row" in the form

---
+++ assets/src/components/form/layouts/two-column-admin/form-row.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+/**
+ * FormRow
+ * "two-column-admin" form layout strategy component for
+ * displaying a single "row" in the form
+ *
+ * @function
+ * @param {Object} children
+ * @param {string} htmlClass
+ * @return {Object} rendered form row
+ */
+const FormRow = ( { children, htmlClass = '' } ) => {
+	htmlClass = htmlClass ? `${ htmlClass } ee-form-row` : 'ee-form-row';
+	return (
+		<div className={ htmlClass }>
+			{ children }
+		</div>
+	);
+};
+
+FormRow.propTypes = {
+	children: PropTypes.oneOfType( [
+		PropTypes.object,
+		PropTypes.arrayOf( PropTypes.object ),
+	] ),
+	htmlClass: PropTypes.string,
+};
+
+export default FormRow;
