ebe1e9b81d313d33f3f20a42b04ec0b1f0198254
Brent Christensen <brent@eventespresso.com>
2018-10-17 18:39:30 -0700

    export all components for the "two-column-admin" form layout strategy

---
+++ assets/src/components/form/layouts/two-column-admin/index.js
@@ -0,0 +1,8 @@
+import { default as AutoColumnRow } from './auto-column-row';
+import { default as FormColumn } from './form-column';
+import { default as FormRow } from './form-row';
+import { default as FormSection } from './form-section';
+import { default as FormWrapper } from './form-wrapper';
+import './two-column-admin.css';
+
+export { FormColumn, FormRow, FormSection, FormWrapper, AutoColumnRow };
