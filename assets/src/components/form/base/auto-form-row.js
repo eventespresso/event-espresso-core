c2b7f42cca3f531ae5be406838eee8f907b8df01
Brent Christensen <brent@eventespresso.com>
2018-10-22 16:12:40 -0700

    refactor as functional component

--- assets/src/components/form/base/auto-form-row.js
+++ assets/src/components/form/base/auto-form-row.js
@@ -1,7 +1,6 @@
 /**
  * External imports
  */
-import { Component } from 'react';
 import { isArray } from 'lodash';
 
 /**
@@ -27,60 +26,56 @@ import { mapValidatorsToAttributes } from '../inputs/base/utils';
  * @param {Object} AutoColumnRow form layout strategy row component
  * @return {Object} rendered form row
  */
-export class AutoFormRow extends Component {
-	render() {
-		const {
-			FormElement,
-			InputLabel,
-			AutoColumnRow,
-		} = this.props;
-
-		let label = null;
-		let input = null;
-		let row = null;
-		if ( FormElement.type ) {
-			const name = FormElement.type.name;
-			if ( name === 'FormRow' ) {
-				return FormElement;
-			}
-			if ( name === 'InputLabel' ) {
-				label = FormElement;
-			}
-			if ( name === 'FormInput' || name === 'Field' ) {
-				input = FormElement;
-				const validations = isArray( FormElement.props.validations ) ?
-					FormElement.propsvalidations :
-					[ FormElement.props.validations ];
-				let attributes = Object.assign( {}, FormElement.props );
-				attributes = mapValidatorsToAttributes(
-					attributes,
-					validations
-				);
-				if ( attributes.label ) {
-					const required = attributes.required ?
-						attributes.required :
-						false;
-					label = (
-						<InputLabel
-							htmlFor={ attributes.htmlId }
-							label={ attributes.label }
-							required={ required }
-						/>
-					);
-				}
-			}
+export const AutoFormRow = ( {
+	FormElement,
+	InputLabel,
+	AutoColumnRow,
+} ) => {
+	let label = null;
+	let input = null;
+	let row = null;
+	if ( FormElement.type ) {
+		const name = FormElement.type.name;
+		if ( name === 'FormRow' ) {
+			return FormElement;
 		}
-		if ( label && input ) {
-			row = (
-				<AutoColumnRow>
-					{ label }
-					{ input }
-				</AutoColumnRow>
+		if ( name === 'InputLabel' ) {
+			label = FormElement;
+		}
+		if ( name === 'FormInput' || name === 'Field' ) {
+			input = FormElement;
+			const validations = isArray( FormElement.props.validations ) ?
+				FormElement.propsvalidations :
+				[ FormElement.props.validations ];
+			let attributes = Object.assign( {}, FormElement.props );
+			attributes = mapValidatorsToAttributes(
+				attributes,
+				validations
 			);
-			label = null;
-			input = null;
-			return row;
+			if ( attributes.label ) {
+				const required = attributes.required ?
+					attributes.required :
+					false;
+				label = (
+					<InputLabel
+						htmlFor={ attributes.htmlId }
+						label={ attributes.label }
+						required={ required }
+					/>
+				);
+			}
 		}
-		return null;
 	}
-}
+	if ( label && input ) {
+		row = (
+			<AutoColumnRow>
+				{ label }
+				{ input }
+			</AutoColumnRow>
+		);
+		label = null;
+		input = null;
+		return row;
+	}
+	return null;
+};
