9fb313fbd2365c0c300431b428c5d5ae771c3567
Brent Christensen <brent@eventespresso.com>
2018-10-23 13:24:07 -0700

    handle ALL the errors! define those that exist in later PHP versions
    defining these constants if they do not exist *should* be ok since they are only numeric values and any use of json_last_error() in early versions of PHP will simply not return their values.

--- core/services/validators/JsonValidator.php
+++ core/services/validators/JsonValidator.php
@@ -32,6 +32,21 @@ class JsonValidator
      */
     public function isValid($file, $func, $line)
     {
+        if( ! defined('JSON_ERROR_RECURSION')) {
+            define('JSON_ERROR_RECURSION', 6);
+        }
+        if( ! defined('JSON_ERROR_INF_OR_NAN')) {
+            define('JSON_ERROR_INF_OR_NAN', 7);
+        }
+        if( ! defined('JSON_ERROR_UNSUPPORTED_TYPE')) {
+            define('JSON_ERROR_UNSUPPORTED_TYPE', 8);
+        }
+        if( ! defined('JSON_ERROR_INVALID_PROPERTY_NAME')) {
+            define('JSON_ERROR_INVALID_PROPERTY_NAME', 9);
+        }
+        if( ! defined('JSON_ERROR_UTF16')) {
+            define('JSON_ERROR_UTF16', 10);
+        }
         switch (json_last_error()) {
             case JSON_ERROR_NONE:
                 return true;
@@ -39,22 +54,37 @@ class JsonValidator
                 $error = ': Maximum stack depth exceeded';
                 break;
             case JSON_ERROR_STATE_MISMATCH:
-                $error = ': Underflow or the modes mismatch';
+                $error = ': Invalid or malformed JSON';
                 break;
             case JSON_ERROR_CTRL_CHAR:
-                $error = ': Unexpected control character found';
+                $error = ': Control character error, possible malformed JSON';
                 break;
             case JSON_ERROR_SYNTAX:
                 $error = ': Syntax error, malformed JSON';
                 break;
             case JSON_ERROR_UTF8:
-                $error = ': Malformed UTF-8 characters, possibly incorrectly encoded';
+                $error = ': Malformed UTF-8 characters, possible malformed JSON';
+                break;
+            case JSON_ERROR_RECURSION:
+                $error = ': One or more recursive references in the value to be encoded';
+                break;
+            case JSON_ERROR_INF_OR_NAN:
+                $error = ': One or more NAN or INF values in the value to be encoded';
+                break;
+            case JSON_ERROR_UNSUPPORTED_TYPE:
+                $error = ': A value of a type that cannot be encoded was given';
+                break;
+            case JSON_ERROR_INVALID_PROPERTY_NAME:
+                $error = ': A property name that cannot be encoded was given';
+                break;
+            case JSON_ERROR_UTF16:
+                $error = ': Malformed UTF-16 characters, possibly incorrectly encoded';
                 break;
             default:
                 $error = ': Unknown error';
                 break;
         }
-        EE_Error::add_error('JSON decoding error' . $error, $file, $func, $line);
+        EE_Error::add_error('JSON decoding failed' . $error, $file, $func, $line);
         return false;
     }
 }
