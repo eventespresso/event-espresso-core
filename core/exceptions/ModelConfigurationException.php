40d3f7c6fdcdebeea16da27c382d46761f623bb4
Mike Nelson <michael@eventespresso.com>
2018-10-25 12:21:36 -0700

    add exception for errors because of a misconfigured model

---
+++ core/exceptions/ModelConfigurationException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EventEspresso\core\exceptions;
+
+/**
+ * Class ModelConfigurationException
+ *
+ * Description
+ *
+ * @package     Event Espresso
+ * @author         Mike Nelson
+ * @since         $VID:$
+ *
+ */
+class ModelConfigurationException
+{
+
+
+}
+// End of file ModelConfigurationException.php
+// Location: EventEspresso\core\exceptions/ModelConfigurationException.php
