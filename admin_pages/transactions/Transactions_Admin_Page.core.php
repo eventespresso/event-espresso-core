147dc1b3d9083faaf9db083a770808ee14891a15
Tony Warwick <tony@eventespresso.com>
2018-10-25 23:04:06 +0100

    Check if this request is to query/count incomplete transactions.

--- admin_pages/transactions/Transactions_Admin_Page.core.php
+++ admin_pages/transactions/Transactions_Admin_Page.core.php
@@ -2480,11 +2480,15 @@ class Transactions_Admin_Page extends EE_Admin_Page
                   || ($count && $view === 'failed');
         $abandoned = (! empty($this->_req_data['status']) && $this->_req_data['status'] === 'abandoned' && ! $count)
                      || ($count && $view === 'abandoned');
+        $incomplete = (! empty($this->_req_data['status']) && $this->_req_data['status'] === 'incomplete' && ! $count)
+                      || ($count && $view === 'incomplete');
 
         if ($failed) {
             $_where['STS_ID'] = EEM_Transaction::failed_status_code;
         } elseif ($abandoned) {
             $_where['STS_ID'] = EEM_Transaction::abandoned_status_code;
+        } elseif ($incomplete) {
+            $_where['STS_ID'] = EEM_Transaction::incomplete_status_code;
         } else {
             $_where['STS_ID'] = array('!=', EEM_Transaction::failed_status_code);
             $_where['STS_ID*'] = array('!=', EEM_Transaction::abandoned_status_code);
