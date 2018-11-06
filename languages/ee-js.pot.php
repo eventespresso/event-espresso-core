aacb617e1dbb57f7f0621b9483f1dfb198062cb3
EE DevBox Server <admin@eventespresso.com>
2018-11-06 08:06:10 -0800

    Bumping version to 4.9.70.p and prepped for release

---
+++ languages/ee-js.pot.php
@@ -0,0 +1,13 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: assets/src/exit-modal-survey/index.js:38
+	__( 'Do you have a moment to share why you are deactivating Event Espresso?', 'event_espresso' ),
+
+	// Reference: assets/src/exit-modal-survey/index.js:42
+	__( 'Sure I\'ll help', 'event_espresso' ),
+
+	// Reference: assets/src/exit-modal-survey/index.js:46
+	__( 'Skip', 'event_espresso' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
