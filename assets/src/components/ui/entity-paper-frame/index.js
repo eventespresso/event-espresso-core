00579b2d8cb77d318f9ba59f6224ade38e9b6ab4
Brent Christensen <brent@eventespresso.com>
2018-12-10 17:46:22 -0800

    withEntityPaperFrame
    adds a styled frame that gives the appearance of a piece of paper on a surface

---
+++ assets/src/components/ui/entity-paper-frame/index.js
@@ -0,0 +1,45 @@
+/**
+ * External imports
+ */
+import {
+	compose,
+	createHigherOrderComponent,
+	withInstanceId,
+} from '@wordpress/compose';
+import { Component } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import './style.css';
+
+/**
+ * withEntityPaperFrame
+ * Higher-Order-Component that wraps an "entity" component
+ * with an EntityPaperFrame component that adds a styled frame
+ * that gives the appearance of a piece of paper on a surface
+ *
+ * @return {Object} Entity with added EntityPaperFrame
+ */
+export default createHigherOrderComponent(
+	compose( [
+		withInstanceId,
+		( Entity ) => {
+			return class EntityPaperFrame extends Component {
+				render() {
+					const { instanceId, ...otherProps } = this.props;
+					return (
+						<div id={ `ee-entity-paper-frame-${ instanceId }` }
+							 className="ee-entity-paper-frame-wrapper"
+						>
+							<div className="ee-entity-paper-frame">
+								<Entity { ...otherProps } />
+							</div>
+						</div>
+					);
+				}
+			};
+		},
+	] ),
+	'withEntityPaperFrame'
+);
