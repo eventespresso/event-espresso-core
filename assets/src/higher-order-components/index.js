5e246882e9b28601717bd49b3304a81f71a71713
Brent Christensen <brent@eventespresso.com>
2018-12-14 13:45:06 -0800

    export all HOCs

--- assets/src/higher-order-components/index.js
+++ assets/src/higher-order-components/index.js
@@ -1,1 +1,11 @@
 export { default as withBaseControl } from './base-control';
+export { default as withEditorModal } from './editor-modal';
+export { default as withEntityPagination } from './pagination';
+export {
+	PaginatedEntityListWithFilterBar,
+	PaginatedEntityListWithFilterBarAndState,
+	EntityListFilterBar,
+	filterStateHandler,
+	viewFilterState,
+} from './filter-bar';
+
