4620877c1d6ff31dfdf4ad998e91e83a2909e776
Darren Ethier <darren@roughsmootheng.in>
2018-12-16 17:45:18 -0500

    add helper and test for getting an array of ids from an Array of BaseEntity instances

---
+++ assets/src/data/helpers/ids-from-base-entity-array.js
@@ -0,0 +1,18 @@
+import { isArray } from 'lodash';
+
+/**
+ * Extract the ids from an array of BaseEntity instances.
+ *
+ * Note, this could return a smaller count of array items if anything in the
+ * incoming array is not a BaseEntity.
+ *
+ * @param {Array<BaseEntity>} entities
+ * @return {Array} An array of ids.
+ */
+export function getIdsFromBaseEntityArray( entities ) {
+	return isArray( entities ) ?
+		entities
+			.map( entity => !! entity.id ? entity.id : false )
+			.filter( x => x ) :
+		entities;
+}
--- assets/src/data/helpers/index.js
+++ assets/src/data/helpers/index.js
@@ -5,3 +5,4 @@ export * from './merge-and-de-duplicate';
 export * from './map-reducer';
 export * from './convert-to-object-from-map';
 export * from './convert-to-map-from-object';
+export * from './ids-from-base-entity-array';
