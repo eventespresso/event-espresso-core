d034a21f3a041bf574d9748fa165b2f96bb5090f
Darren Ethier <darren@roughsmootheng.in>
2018-12-17 08:58:33 -0500

    convert mockStateForTests to an Immutable object

--- assets/src/data/eventespresso/core/test/fixtures/index.js
+++ assets/src/data/eventespresso/core/test/fixtures/index.js
@@ -6,48 +6,62 @@ import {
 	DateTimeEntities,
 } from '../../../test/fixtures/base';
 
-export const mockStateForTests = {
-	entities: {
-		event: {
-			10: EventEntities.a,
-			20: EventEntities.b,
-			30: EventEntities.c,
-		},
-		datetime: {
-			52: DateTimeEntities.a,
-			53: DateTimeEntities.b,
-			54: DateTimeEntities.c,
-		},
-	},
-	relations: {
-		index: {
-			datetimes: {
-				52: { event: [ 10 ] },
-				53: { event: [ 20 ] },
-				54: { event: [ 30 ] },
-			},
-		},
-		entityMap: {
-			event: {
-				10: {
-					datetimes: [ 52 ],
+/**
+ * External Imports
+ */
+import { Map, fromJS, List } from 'immutable';
+
+export const mockStateForTests = Map()
+	.set(
+		'entities',
+		Map().set(
+			'event',
+			Map()
+				.set( 10, EventEntities.a )
+				.set( 20, EventEntities.b )
+				.set( 30, EventEntities.c )
+		).set(
+			'datetime',
+			Map()
+				.set( 52, DateTimeEntities.a )
+				.set( 53, DateTimeEntities.b )
+				.set( 54, DateTimeEntities.c )
+		)
+	)
+	.set(
+		'relations',
+		Map()
+			.set(
+				'index',
+				Map().set(
+					'datetimes',
+					Map()
+						.set( 52, Map().set( 'event', List.of( 10 ) ) )
+						.set( 53, Map().set( 'event', List.of( 20 ) ) )
+						.set( 54, Map().set( 'event', List.of( 30 ) ) )
+				)
+			)
+			.set(
+				'entityMap',
+				Map().set(
+					'event',
+					Map()
+						.set( 10, Map().set( 'datetimes', List.of( 52 ) ) )
+						.set( 20, Map().set( 'datetimes', List.of( 53 ) ) )
+						.set( 30, Map().set( 'datetimes', List.of( 54 ) ) )
+				)
+			)
+	).set(
+		'dirty',
+		fromJS(
+			{
+				relations: {
+					index: {},
+					delete: {},
+					add: {},
 				},
-				20: {
-					datetimes: [ 53 ],
-				},
-				30: {
-					datetimes: [ 54 ],
-				},
-			},
-		},
-	},
-	dirty: {
-		relations: {
-			index: {},
-			delete: {},
-			add: {},
-		},
-		trash: {},
-		delete: {},
-	},
-};
+				trash: {},
+				delete: {},
+			}
+		)
+	);
