bdb9e6abdde1ce9f306c15037aec8968764eb05c
Brent Christensen <brent@eventespresso.com>
2018-12-17 18:05:41 -0800

    WordpressHeartbeat  Directs WordPress Heartbeat AJAX requests

---
+++ core/domain/services/admin/ajax/WordpressHeartbeat.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace EventEspresso\core\domain\services\admin\ajax;
+
+use EventEspresso\core\exceptions\InvalidClassException;
+use EventEspresso\core\services\loaders\LoaderInterface;
+use EventEspresso\core\services\request\RequestInterface;
+
+/**
+ * Class WordpressHeartbeat
+ * Directs WordPress Heartbeat AJAX requests to the appropriate handler
+ *
+ * @package EventEspresso\core\domain\services\admin\ajax
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+class WordpressHeartbeat
+{
+
+    /**
+     * @var LoaderInterface $loader
+     */
+    protected $loader;
+
+    /**
+     * @var RequestInterface $request
+     */
+    protected $request;
+
+
+    /**
+     * WordpressHeartbeat constructor.
+     *
+     * @param LoaderInterface $loader
+     * @param RequestInterface $request
+     */
+    public function __construct(
+        LoaderInterface $loader,
+        RequestInterface $request
+    ) {
+        $this->loader = $loader;
+        $this->request = $request;
+        do_action('AHEE__EventEspresso_core_domain_services_admin_ajax_WordpressHeartbeat__constructor', $this);
+        add_action('AHEE__EE_System__core_loaded_and_ready', array($this, 'resolveRoutes'));
+    }
+
+
+    /**
+     * @since $VID:$
+     * @throws InvalidClassException
+     */
+    public function resolveRoutes()
+    {
+        $screenID = $this->request->getRequestParam('screen_id');
+        $heartbeat_data = $this->request->getRequestParam('data', array());
+        if ($screenID === 'espresso_events') {
+            $this->loader->getShared(
+                'EventEspresso\core\domain\services\admin\ajax\EventEditorHeartbeat'
+            );
+        } else if ($screenID === 'front'&& ! empty($heartbeat_data['espresso_thank_you_page'])) {
+            $this->loader->getShared(
+                'EventEspresso\core\domain\services\admin\ajax\ThankYouPageIpnMonitor'
+            );
+        }
+    }
+
+}
