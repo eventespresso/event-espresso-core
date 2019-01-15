ad98167f41822b53f5ff675db5994f4d47b6c6e0
Brent Christensen <brent@eventespresso.com>
2019-01-14 17:34:35 -0800

    move FormSaveCancelButtons into layout and inject buttons

---
+++ assets/src/components/form/layouts/two-column-admin/form-save-cancel-buttons.js
@@ -0,0 +1,43 @@
+/**
+ * External imports
+ */
+import { Component } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { default as FormColumn } from './form-column';
+import { default as FormRow } from './form-row';
+import { default as FormSection } from './form-section';
+
+/**
+ * EditorSaveCancelButtons
+ * reusable form buttons
+ *
+ * @constructor
+ * @param {Object} submitButton
+ * @param {Object} cancelButton
+ * @param {string} htmlClass
+ * @return {Object} rendered form buttons
+ */
+class FormSaveCancelButtons extends Component {
+	render() {
+		const { submitButton, cancelButton } = this.props;
+		let { htmlClass } = this.props;
+		htmlClass = htmlClass ?
+			`${ htmlClass } ee-form-save-cancel-buttons-div` :
+			'ee-form-save-cancel-buttons-div';
+		return (
+			<FormSection htmlClass={ htmlClass }>
+				<FormRow>
+					<FormColumn colSize={ 6 } offset={ 2 }>
+						{ submitButton }
+						{ cancelButton }
+					</FormColumn>
+				</FormRow>
+			</FormSection>
+		);
+	}
+}
+
+export default FormSaveCancelButtons;
