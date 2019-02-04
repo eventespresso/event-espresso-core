b8a4d9dc682f5ed7636612bed074ebb1e71e2646
Brent Christensen <brent@eventespresso.com>
2019-02-04 12:04:48 -0800

    HiddenInput

---
+++ assets/src/components/form/inputs/hidden-input.js
@@ -0,0 +1,42 @@
+/**
+ * External imports
+ */
+import { Field } from 'react-final-form';
+import PropTypes from 'prop-types';
+
+/**
+ * generates an HTML5 text variant <input>
+ *
+ * @function
+ * @param {string} type
+ * @param {string} name
+ * @param {string} htmlId
+ * @param {Object} dataSet
+ * @param {Object} attributes
+ * @return {string} rendered date name form row
+ */
+export const HiddenInput = ( {
+	name,
+	htmlId,
+	dataSet,
+	...attributes
+} ) => {
+	return (
+		<Field
+			component="input"
+			type="hidden"
+			name={ name }
+			id={ htmlId }
+			{ ...dataSet }
+			{ ...attributes }
+		/>
+	);
+};
+
+HiddenInput.propTypes = {
+	name: PropTypes.string.isRequired,
+	htmlId: PropTypes.string.isRequired,
+	onChange: PropTypes.func,
+	dataSet: PropTypes.object,
+};
+
