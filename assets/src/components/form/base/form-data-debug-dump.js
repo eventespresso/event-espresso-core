05cb2e920385e4880bd951b789fd56678eac1144
Brent Christensen <brent@eventespresso.com>
2019-02-04 11:55:19 -0800

    return null when not in DEV

--- assets/src/components/form/base/form-data-debug-dump.js
+++ assets/src/components/form/base/form-data-debug-dump.js
@@ -14,12 +14,12 @@ import './form-data-debug-dump.css';
  * @return {Object} form data
  */
 export const FormDataDebugDump = ( { values } ) => {
-	return __DEV__ && (
+	return __DEV__ ? (
 		<div className="ee-form-data-dump-div">
 			<h3>Form Data</h3>
 			<pre id="form-data">
 				{ JSON.stringify( values, 0, 2 ) }
 			</pre>
 		</div>
-	);
+	) : null;
 };
