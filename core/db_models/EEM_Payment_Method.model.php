df82abb0300c4276251cdebbc4064fe905a409bd
Mike Nelson <michael@eventespresso.com>
2019-02-05 10:06:10 -0800

    actually, if there is no button URL whatsoever, reset it to the default

--- core/db_models/EEM_Payment_Method.model.php
+++ core/db_models/EEM_Payment_Method.model.php
@@ -264,37 +264,14 @@ class EEM_Payment_Method extends EEM_Base
         $payment_methods = is_array($payment_methods) ? $payment_methods : $this->get_all_active(EEM_Payment_Method::scope_cart);
         foreach ($payment_methods as $payment_method) {
             try {
+                // If there is really no button URL at all, reset it to the default.
                 $current_button_url = $payment_method->button_url();
-                $buttons_urls_to_try = apply_filters('FHEE__EEM_Payment_Method__verify_button_urls__button_urls_to_try', array(
-                    'current_ssl' => str_replace("http://", "https://", $current_button_url),
-                    'current' => str_replace("https://", "http://", $current_button_url),
-                    'default_ssl' => str_replace("http://", "https://", $payment_method->type_obj()->default_button_url()),
-                    'default' => str_replace("https://", "http://", $payment_method->type_obj()->default_button_url()),
-                ));
-                foreach ($buttons_urls_to_try as $button_url_to_try) {
-                    if ((// this is the current url and it exists, regardless of SSL issues
-                                $button_url_to_try == $current_button_url &&
-                                EEH_URL::remote_file_exists(
-                                    $button_url_to_try,
-                                    array(
-                                            'sslverify' => false,
-                                            'limit_response_size' => 4095,// we don't really care for a full response, but we do want headers at least. Lets just ask for a one block
-                                            )
-                                )
-                            )
-                            ||
-                            (// this is NOT the current url and it exists with a working SSL cert
-                                $button_url_to_try != $current_button_url &&
-                                EEH_URL::remote_file_exists($button_url_to_try)
-                            ) ) {
-                        if ($current_button_url != $button_url_to_try) {
-                            $payment_method->save(array( 'PMD_button_url' => $button_url_to_try ));
-                            EE_Error::add_attention(sprintf(__("Payment Method %s's button url was set to %s, because the old image either didnt exist or SSL was recently enabled.", "event_espresso"), $payment_method->name(), $button_url_to_try));
-                        }
-                        // this image exists. So if wasn't set before, now it is;
-                        // or if it was already set, we have nothing to do
-                        break;
-                    }
+                if (empty($current_button_url)) {
+                    $payment_method->save(
+                        [
+                            'PMD_button_url' => $payment_method->type_obj()->default_button_url()
+                        ]
+                    );
                 }
             } catch (EE_Error $e) {
                 $payment_method->set_active(false);
