89bb1ca68075ef1a3cffde0511a35b215434f3b7
Josh Feck <josh@nxnweb.com>
2019-02-06 13:10:59 -0500

    Trust no one.

--- admin_pages/events/help_tabs/event_editor.help_tab.php
+++ admin_pages/events/help_tabs/event_editor.help_tab.php
@@ -1,11 +1,11 @@
-<p><strong><?php _e('Event Editor', 'event_espresso'); ?></strong></p>
+<p><strong><?php esc_html_e('Event Editor', 'event_espresso'); ?></strong></p>
 <p>
-<?php _e('This page allows you to create and edit events with Event Espresso.', 'event_espresso'); ?>
+<?php esc_html_e('This page allows you to create and edit events with Event Espresso.', 'event_espresso'); ?>
 </p>
 <p class="ee-attention">
-<?php _e('Please be advised: If you update any registration-related details (i.e. custom questions, messages, tickets, datetimes, etc.) while a registration is in progress, the registration process could be interrupted and result in errors for the person registering and potentially incorrect registration or transaction data inside Event Espresso. We recommend editing events during a period of slow traffic, or even temporarily changing the status of an event to "Draft" until your edits are complete.', 'event_espresso'); ?>
+<?php esc_html_e('Please be advised: If you update any registration-related details (i.e. custom questions, messages, tickets, datetimes, etc.) while a registration is in progress, the registration process could be interrupted and result in errors for the person registering and potentially incorrect registration or transaction data inside Event Espresso. We recommend editing events during a period of slow traffic, or even temporarily changing the status of an event to "Draft" until your edits are complete.', 'event_espresso'); ?>
 </p>
 <p>
-<strong><?php _e('Screen Options', 'event_espresso'); ?></strong><br /> 
-<?php _e('You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content and even adjust the screen layout.', 'event_espresso'); ?>
+<strong><?php esc_html_e('Screen Options', 'event_espresso'); ?></strong><br /> 
+<?php esc_html_e('You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content and even adjust the screen layout.', 'event_espresso'); ?>
 </p>
