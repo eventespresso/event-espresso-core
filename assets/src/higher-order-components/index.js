aa139c30a42a6e97f3b24bd368446ce2825fd1f1
Brent Christensen <brent@eventespresso.com>
2019-02-13 13:59:51 -0800

    export editor-modal, filter-bar, and pagination HOCs

--- assets/src/higher-order-components/index.js
+++ assets/src/higher-order-components/index.js
@@ -1,2 +1,12 @@
 export { default as withBaseControl } from './base-control';
+export { default as withEditorModal } from './editor-modal';
+export {
+	PaginatedEntityListWithFilterBar,
+	PaginatedEntityListWithFilterBarAndState,
+	EntityListFilterBar,
+	filterStateHandler,
+	viewFilterState,
+} from './filter-bar';
 export { default as withMoney } from './money';
+export { default as withEntityPagination } from './pagination';
+
