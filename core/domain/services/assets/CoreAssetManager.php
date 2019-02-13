ef75497d5d94575d81f4903b7b9c3b1bd95cde32
Brent Christensen <brent@eventespresso.com>
2019-02-13 14:27:37 -0800

    add wp-components as dependency for ee components css

--- core/domain/services/assets/CoreAssetManager.php
+++ core/domain/services/assets/CoreAssetManager.php
@@ -468,7 +468,8 @@ class CoreAssetManager extends AssetManager
             $this->registry->getCssUrl(
                 $this->domain->assetNamespace(),
                 'components'
-            )
+            ),
+            ['wp-components']
         );
     }
 
