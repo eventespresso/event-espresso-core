484301946029f00354db47d3984cd92f14840464
Brent Christensen <brent@eventespresso.com>
2019-02-13 10:33:44 -0800

    export new form components

--- assets/src/components/form/index.js
+++ assets/src/components/form/index.js
@@ -1,1 +1,10 @@
 export * from './model';
+export { FormContainer } from './base/form-container';
+export { FormPlaceholder } from './base/form-placeholder';
+export { withFormHandler } from './base/with-form-handler';
+export { FormInput } from './inputs/form-input';
+export { HiddenInput } from './inputs/hidden-input';
+export { InlineEditInput } from './inputs/inline-edit-input';
+export { InputLabel } from './inputs/base/input-label';
+export { default as twoColumnAdminFormLayout } from './layouts/two-column-admin';
+export { default as validations } from './validations';
