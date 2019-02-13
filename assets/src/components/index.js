a51316e1cb3e9712b1eabe32209d58290f16905c
Brent Christensen <brent@eventespresso.com>
2019-02-13 10:50:30 -0800

    load everything in the forms subfolder

--- assets/src/components/index.js
+++ assets/src/components/index.js
@@ -1,9 +1,8 @@
 // query components
 export { QueryLimit } from './query/limit';
 // selection components
-export * from './form/model';
+export * from './form';
 // general ui components
 export * from './ui';
-
 // other
 export { default as EventAttendeeList } from './event-attendees';
