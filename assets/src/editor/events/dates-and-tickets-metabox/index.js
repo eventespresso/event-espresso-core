4e3b33c11cf9d54131f24846583d3072e4aedddb
Brent Christensen <brent@eventespresso.com>
2019-02-13 13:32:29 -0800

    load everything in the dates-and-tickets-metabox folder

---
+++ assets/src/editor/events/dates-and-tickets-metabox/index.js
@@ -0,0 +1,6 @@
+export {
+	default as DatesAndTicketsManagerModal,
+} from './dates-and-tickets-manager';
+export {
+	default as EventDatesAndTicketsMetabox,
+} from './event-dates-and-tickets-metabox';
