a7479c26d0bedf0a2db83473409444ec6ad01e0b
Brent Christensen <brent@eventespresso.com>
2019-02-13 11:47:59 -0800

    EditorDatesList
    EntityList component for displaying event dates in the editor

---
+++ assets/src/editor/events/dates-and-times/editor-date/editor-dates-list.js
@@ -0,0 +1,45 @@
+/**
+ * External imports
+ */
+import { Component } from 'react';
+import { EntityList } from '@eventespresso/components';
+import { __ } from '@eventespresso/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { EditorDatesGridView, EditorDatesListView } from './index';
+import { default as PaginatedDatesListWithFilterBar } from './filter-bar';
+
+/**
+ * EditorDatesList
+ * EntityList component for displaying event dates in the editor
+ *
+ * @function
+ * @param {Array} entities 	array of JSON objects defining the Event Dates
+ * @param {string} view
+ * @param {Function} retrieveDates
+ * @param {mixed} otherProps
+ * @return {Component}          list of rendered Event Dates
+ */
+class EditorDatesList extends Component {
+	render() {
+		const { view, ...otherProps } = this.props;
+		return (
+			<EntityList
+				EntityGridView={ EditorDatesGridView }
+				EntityListView={ EditorDatesListView }
+				view={ view }
+				noResultsText={
+					__(
+						'no results found (try changing filters)',
+						'event_espresso'
+					)
+				}
+				{ ...otherProps }
+			/>
+		);
+	}
+}
+
+export default PaginatedDatesListWithFilterBar( EditorDatesList );
