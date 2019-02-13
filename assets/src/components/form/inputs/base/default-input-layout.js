e13fd0570dc772a9f66fd0f2035aafd29039e96a
Brent Christensen <brent@eventespresso.com>
2019-02-13 10:35:52 -0800

    DefaultInputLayout
    Basic form input layout where input is followed by input error and help text

---
+++ assets/src/components/form/inputs/base/default-input-layout.js
@@ -0,0 +1,38 @@
+/**
+ * External imports
+ */
+import { Fragment } from 'react';
+
+/**
+ * Internal imports
+ */
+import { InputError } from './input-error';
+import { InputHelpText } from './input-help-text';
+
+/**
+ * Basic form input layout where input is followed by input error and help text
+ *
+ * @function
+ * @param {Object} formInput
+ * @param {string} inputName
+ * @param {string} helpTextID
+ * @param {string} helpText
+ * @return {Object} rendered form input
+ */
+export const DefaultInputLayout = ( {
+	formInput,
+	inputName,
+	helpTextID,
+	helpText,
+} ) => {
+	return formInput && (
+		<Fragment>
+			{ formInput }
+			<InputError inputName={ inputName } />
+			<InputHelpText
+				helpTextID={ helpTextID }
+				helpText={ helpText }
+			/>
+		</Fragment>
+	);
+};
