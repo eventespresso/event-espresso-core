0208585fb389c0c3eff80d684bc02c8ad026ae6c
Brent Christensen <brent@eventespresso.com>
2019-02-13 12:50:47 -0800

    load everything in editor-ticket

---
+++ assets/src/editor/events/tickets/editor-ticket/index.js
@@ -0,0 +1,7 @@
+export * from './action-handlers';
+export * from './actions-menu';
+export * from './edit-form';
+export * from './filter-bar';
+export * from './grid-view';
+export * from './list-view';
+export { default as EditorTicketsList } from './editor-tickets-list';
