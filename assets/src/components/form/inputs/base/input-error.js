4cb5905e4571fe1ee777bd487ba7cdf267332c54
Brent Christensen <brent@eventespresso.com>
2019-02-13 10:36:13 -0800

    InputError

---
+++ assets/src/components/form/inputs/base/input-error.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { Field } from 'react-final-form';
+
+/**
+ * Internal imports
+ */
+import './input-error.css';
+
+/**
+ * @function InputError
+ * @param {string} inputName
+ * @return {Object} rendered help text
+ */
+export const InputError = ( { inputName } ) => (
+	<Field
+		name={ inputName }
+		subscription={ { touched: true, error: true } }
+		render={ ( { meta: { touched, error } } ) =>
+			touched && error ?
+				<div
+					id={ `${ inputName }-input-error` }
+					className="espresso-input-error"
+				>
+					<span>{ error }</span>
+				</div> :
+				null
+		}
+	/>
+);
+
+InputError.propTypes = { inputName: PropTypes.string.isRequired };
