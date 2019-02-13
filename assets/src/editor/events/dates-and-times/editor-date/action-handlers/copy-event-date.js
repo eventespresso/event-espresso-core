ab4eaf6d9d9a9bbe877a5a6d01794f6d054d1479
Brent Christensen <brent@eventespresso.com>
2019-02-13 11:11:50 -0800

    action handlers (CRUD operations) for Event Dates

---
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/copy-event-date.js
@@ -0,0 +1,7 @@
+/**
+ * @function
+ * @param {Object} eventDate    JSON object defining the Event Date
+ */
+export const copyEventDate = ( eventDate ) => {
+	console.log( ' >>> CLICK <<< COPY EVENT DATE ', eventDate );
+};
---
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
@@ -0,0 +1,3 @@
+export { copyEventDate } from './copy-event-date';
+export { editEventDate } from './edit-event-date';
+export { trashEventDate } from './trash-event-date';
