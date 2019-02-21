8162f8c1efc74fadefdd98ce8f334e97745b3300
Brent Christensen <brent@eventespresso.com>
2019-02-21 09:08:57 -0800

    refactor entity list filter state to use wp data store

--- assets/src/higher-order-components/filter-bar/with-entity-list-filter-state.js
+++ assets/src/higher-order-components/filter-bar/with-entity-list-filter-state.js
@@ -3,78 +3,68 @@
  */
 import PropTypes from 'prop-types';
 import { Component } from '@wordpress/element';
-import { createHigherOrderComponent } from '@wordpress/compose';
-
-/**
- * Internal dependencies
- */
-import { default as filterStateHandler } from './filter-state-handler';
-import { default as viewFilterState } from './view-filter-state';
+import { createHigherOrderComponent, compose } from '@wordpress/compose';
+import { withSelect, withDispatch } from '@wordpress/data';
 
 /**
  * withEntityListFilterState
  * Higher-Order-Component that wraps an "EntityListFilterBar" component
  * in order to provide state management for it and its children
  *
- * @param {Object} paginationConfig
- * @return {Object} EntityList with added EntityPagination
+ * @param {Object} WrappedComponent
+ * @return {Object} WrappedComponent with added EntityListFilterState
  */
 export default createHigherOrderComponent(
-	( EntityListFilterBar ) => {
-		class EntityListFilterState extends Component {
-			static propTypes = {
-				entities: PropTypes.arrayOf( PropTypes.object ).isRequired,
+	compose( [
+		withSelect( ( select, ownProps ) => {
+			const {
+				perPage = 6,
+				view = 'grid',
+			} = ownProps;
+			const store = select( 'eventespresso/filter-state' );
+			return {
+				perPage: store.getFilter(
+					'entity-list',
+					'perPage',
+					perPage
+				),
+				view: store.getFilter(
+					'entity-list',
+					'view',
+					view
+				),
 			};
-
-			constructor( props ) {
-				super( props );
-				this.state = this.getFilterState( this.props.entities );
-			}
-
-			componentDidMount() {
-				filterStateHandler.addListener( this.hasUpdates );
-			}
-
-			componentWillUnmount() {
-				filterStateHandler.removeListener( this.hasUpdates );
-			}
-
-			/**
-			 * @param {Object} newState
-			 */
-			hasUpdates = newState => {
-				if ( this.state !== newState ) {
-					this.setState( newState );
+		} ),
+		withDispatch( ( dispatch ) => {
+			const store = dispatch( 'eventespresso/filter-state' );
+			return {
+				setPerPage: ( perPage ) => store.setFilter(
+					'entity-list',
+					'perPage',
+					perPage
+				),
+				setListView: () => store.setFilter(
+					'entity-list',
+					'view',
+					'list'
+				),
+				setGridView: () => store.setFilter(
+					'entity-list',
+					'view',
+					'grid'
+				),
+			};
+		} ),
+		( WrappedComponent ) => {
+			return class extends Component {
+				static propTypes = {
+					entities: PropTypes.arrayOf( PropTypes.object ).isRequired,
+				};
+				render() {
+					return <WrappedComponent { ...this.props } />;
 				}
 			};
-
-			/**
-			 * @param {Array} entities
-			 * @return {Object} filter state object
-			 */
-			getFilterState = entities => {
-				// merge initial state object for filter bar with entities
-				const state = {
-					...viewFilterState.initialState,
-					...{ entities: entities },
-				};
-				// state with added actions for view filters
-				return filterStateHandler.register(
-					viewFilterState.handler,
-					state
-				);
-			};
-
-			render() {
-				const { ...otherProps } = this.props;
-				return <EntityListFilterBar
-					{ ...this.state }
-					{ ...otherProps }
-				/>;
-			}
-		}
-
-		return EntityListFilterState;
-	},
+		},
+	] ),
 	'withEntityListFilterState'
 );
