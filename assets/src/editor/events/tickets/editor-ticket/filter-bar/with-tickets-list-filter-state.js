2124ac0002b71b12fb6d155188c044007d875dff
Brent Christensen <brent@eventespresso.com>
2019-02-20 16:33:34 -0800

    update ticket filter state to work with abstracted store

--- assets/src/editor/events/tickets/editor-ticket/filter-bar/with-tickets-list-filter-state.js
+++ assets/src/editor/events/tickets/editor-ticket/filter-bar/with-tickets-list-filter-state.js
@@ -1,78 +1,111 @@
 /**
  * External imports
  */
-import PropTypes from 'prop-types';
 import { Component } from '@wordpress/element';
 import { createHigherOrderComponent, compose } from '@wordpress/compose';
-import { withSelect, withDispatch } from '@wordpress/data'
-
-/**
- * Internal dependencies
- */
-import { default as ticketsListFilterState } from './tickets-list-filter-state';
-import { filterStateHandler } from '../../../../../higher-order-components/filter-bar';
+import { withSelect, withDispatch } from '@wordpress/data';
 
 /**
  * withTicketsListFilterState
  * Higher-Order-Component that wraps a "TicketsListFilterBar" component
  * in order to provide state management for it and its children
  *
- * @param {Object} paginationConfig
- * @return {Object} EntityList with added EntityPagination
+ * @param {Object} WrappedComponent
+ * @return {Object} WrappedComponent with added DatesListFilterState
  */
 export default createHigherOrderComponent(
-	( TicketsListFilterBar ) => {
-		class TicketsListFilterState extends Component {
-			static propTypes = {
-				entities: PropTypes.arrayOf( PropTypes.object ).isRequired,
+	compose( [
+		withSelect( ( select, ownProps ) => {
+			const {
+				showTickets = 'on-sale-and-pending',
+				sortTickets = 'chronologically',
+				displayTicketDate = 'start',
+				isChained = true,
+				ticketsPerPage = 6,
+				ticketsView = 'grid',
+			} = ownProps;
+			const store = select( 'eventespresso/filter-state' );
+			return {
+				showTickets: store.getFilter(
+					'event-editor-ticket-list',
+					'showTickets',
+					showTickets
+				),
+				sortTickets: store.getFilter(
+					'event-editor-ticket-list',
+					'sortTickets',
+					sortTickets
+				),
+				displayTicketDate: store.getFilter(
+					'event-editor-ticket-list',
+					'displayTicketDate',
+					displayTicketDate
+				),
+				isChained: store.getFilter(
+					'event-editor-ticket-list',
+					'isChained',
+					isChained
+				),
+				ticketsPerPage: store.getFilter(
+					'event-editor-ticket-list',
+					'ticketsPerPage',
+					ticketsPerPage
+				),
+				ticketsView: store.getFilter(
+					'event-editor-ticket-list',
+					'ticketsView',
+					ticketsView
+				),
 			};
-
-			render() {
-				return <TicketsListFilterBar{ ...this.props }/>;
-			}
-		}
-		return compose( [
-			withSelect( ( select, ownProps ) => {
-				const {
-					showTickets = 'on-sale-and-pending',
-					sortTickets = 'chronologically',
-					displayTicketDate = 'both',
-					isChained = true,
-				} = ownProps;
-				const selectData = select( 'eventespresso/filter-state' );
-				return {
-					showTickets: selectData
-						.getItemToShow( 'ticket', 'showTickets' ) ||
-						showTickets,
-					sortTickets: selectData
-						.getSortBy( 'ticket' ) ||
-						sortTickets,
-					displayTicketDate: selectData
-						.getItemToShowWithValue( 'ticket', 'displayTicketDate' ) ||
-						displayTicketDate,
-					isChained: selectData
-						.getIsChained( 'ticket' ) ||
-						isChained,
-				};
-			} ),
-			withDispatch( ( dispatch ) => {
-				const dispatchData = dispatch( 'eventespresso/filter-state' );
-				return {
-					setShowTickets: ( itemToShow ) => dispatchData
-						.setItemToShow( 'ticket', itemToShow ),
-					setSortTickets: ( sortBy ) => dispatchData
-						.setSortBy( 'ticket', sortBy ),
-					setDisplayTicketDate: ( ticketDateValue ) => dispatchData
-						.setItemToShowWithValue(
-							'ticket',
-							'displayTicketDate',
-							ticketDateValue
-						),
-					setIsChained: ( isChained ) => dispatchData
-						.setIsChained( 'ticket', isChained ),
-				};
-			} ),
-		] )( TicketsListFilterState );
-	},
+		} ),
+		withDispatch( ( dispatch ) => {
+			const store = dispatch( 'eventespresso/filter-state' );
+			return {
+				setShowTickets: ( showTickets ) => store.setFilter(
+					'event-editor-ticket-list',
+					'showTickets',
+					showTickets
+				),
+				setSortTickets: ( sortTickets ) => store.setFilter(
+					'event-editor-ticket-list',
+					'sortTickets',
+					sortTickets
+				),
+				setDisplayTicketDate: ( displayTicketDate ) => store
+					.setFilter(
+						'event-editor-ticket-list',
+						'displayTicketDate',
+						displayTicketDate
+					),
+				setIsChained: ( isChained ) => store.setFilter(
+					'event-editor-ticket-list',
+					'isChained',
+					isChained
+				),
+				setTicketsPerPage: ( ticketsPerPage ) => store.setFilter(
+					'event-editor-ticket-list',
+					'ticketsPerPage',
+					ticketsPerPage
+				),
+				setTicketsListView: () => store.setFilter(
+					'event-editor-ticket-list',
+					'ticketsView',
+					'list'
+				),
+				setTicketsGridView: () => store.setFilter(
+					'event-editor-ticket-list',
+					'ticketsView',
+					'grid'
+				),
+			};
+		} ),
+		( WrappedComponent ) => {
+			return class extends Component {
+				render() {
+					return <WrappedComponent { ...this.props } />;
+				}
+			};
+		},
+	] ),
 	'withTicketsListFilterState'
 );
