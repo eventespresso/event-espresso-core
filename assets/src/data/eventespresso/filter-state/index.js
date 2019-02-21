72101bfc640dea7febbab9c2fb58535acc41b7ff
Brent Christensen <brent@eventespresso.com>
2019-02-20 16:27:14 -0800

    completely abstract filter state store for handling all state changes
    move reducer, actions, and selectors into index.js

--- assets/src/data/eventespresso/filter-state/index.js
+++ assets/src/data/eventespresso/filter-state/index.js
@@ -1,14 +1,53 @@
 /**
  * External dependencies
  */
+import { Map } from 'immutable';
+import createSelector from 'rememo';
 import { registerStore } from '@wordpress/data';
 
-import reducer from './reducer.js';
-import * as actions from './actions';
-import * as selectors from './selectors';
+const DEFAULT_STATE = {};
+const SET_FILTER = 'SET_FILTER';
 
-export default registerStore( 'eventespresso/filter-state', {
-	reducer,
-	actions,
-	selectors,
-} );
+const getState = ( state, context ) => {
+	return typeof state[ context ] === 'undefined' ?
+		Map() :
+		state[ context ];
+};
+
+const filterStateReducer = ( state = DEFAULT_STATE, action ) => {
+	const { type, context, filter, value } = action;
+	if ( type === SET_FILTER ) {
+		return {
+			...state,
+			[ context ]: getState( state, context ).set( filter, value ),
+		};
+	}
+	return state;
+};
+
+const setFilter = ( context, filter, value ) => {
+	return {
+		type: SET_FILTER,
+		context,
+		filter,
+		value,
+	};
+};
+
+const getFilter = createSelector(
+	( state, context, filter, defaultValue ) => {
+		return getState( state, context ).get( filter, defaultValue );
+	},
+	( state, context, filter, defaultValue ) => [
+		getState( state, context ).get( filter, defaultValue ),
+	]
+);
+
+export default registerStore(
+	'eventespresso/filter-state',
+	{
+		reducer: filterStateReducer,
+		actions: { setFilter },
+		selectors: { getFilter },
+	}
+);
