f4bdef3b56161104a29e696cc12f49574f8f5f40
Brent Christensen <brent@eventespresso.com>
2019-02-20 16:34:58 -0800

    update dates list filter state to work with abstracted store

--- assets/src/editor/events/dates-and-times/editor-date/filter-bar/with-dates-list-filter-state.js
+++ assets/src/editor/events/dates-and-times/editor-date/filter-bar/with-dates-list-filter-state.js
@@ -1,78 +1,100 @@
 /**
  * External imports
  */
-import PropTypes from 'prop-types';
 import { Component } from '@wordpress/element';
-import { createHigherOrderComponent } from '@wordpress/compose';
-import { filterStateHandler } from '@eventespresso/higher-order-components';
-
-/**
- * Internal dependencies
- */
-import { default as datesListFilterState } from './dates-list-filter-state';
+import { createHigherOrderComponent, compose } from '@wordpress/compose';
+import { withSelect, withDispatch } from '@wordpress/data';
 
 /**
  * withDatesListFilterState
  * Higher-Order-Component that wraps a "DatesListFilterBar" component
+ * (or parent component that ultimately wraps a "DatesListFilterBar" component)
  * in order to provide state management for it and its children
  *
- * @param {Object} paginationConfig
- * @return {Object} EntityList with added EntityPagination
+ * @param {Object} WrappedComponent
+ * @return {Object} WrappedComponent with added DatesListFilterState
  */
 export default createHigherOrderComponent(
-	( DatesListFilterBar ) => {
-		class DatesListFilterState extends Component {
-			static propTypes = {
-				entities: PropTypes.arrayOf( PropTypes.object ).isRequired,
+	compose( [
+		withSelect( ( select, ownProps ) => {
+			const {
+				showDates = 'active-upcoming',
+				sortDates = 'chronologically',
+				displayDates = 'start',
+				datesPerPage = 6,
+				datesView = 'grid',
+			} = ownProps;
+			const store = select( 'eventespresso/filter-state' );
+			return {
+				showDates: store.getFilter(
+					'event-editor-dates-list',
+					'showDates',
+					showDates
+				),
+				sortDates: store.getFilter(
+					'event-editor-dates-list',
+					'sortDates',
+					sortDates
+				),
+				displayDates: store.getFilter(
+					'event-editor-dates-list',
+					'displayDates',
+					displayDates
+				),
+				datesPerPage: store.getFilter(
+					'event-editor-dates-list',
+					'datesPerPage',
+					datesPerPage
+				),
+				datesView: store.getFilter(
+					'event-editor-dates-list',
+					'datesView',
+					datesView
+				),
 			};
-
-			constructor( props ) {
-				super( props );
-				this.state = this.getFilterState( this.props.entities );
-			}
-
-			componentDidMount() {
-				filterStateHandler.addListener( this.hasUpdates );
-			}
-
-			componentWillUnmount() {
-				filterStateHandler.removeListener( this.hasUpdates );
-			}
-
-			/**
-			 * @param {Object} newState
-			 */
-			hasUpdates = newState => {
-				if ( this.state !== newState ) {
-					this.setState( newState );
+		} ),
+		withDispatch( ( dispatch ) => {
+			const store = dispatch( 'eventespresso/filter-state' );
+			return {
+				setShowDates: ( showDates ) => store.setFilter(
+					'event-editor-dates-list',
+					'showDates',
+					showDates
+				),
+				setSortDates: ( sortDates ) => store.setFilter(
+					'event-editor-dates-list',
+					'sortDates',
+					sortDates
+				),
+				setDisplayDates: ( displayDates ) => store.setFilter(
+					'event-editor-dates-list',
+					'displayDates',
+					displayDates
+				),
+				setDatesPerPage: ( datesPerPage ) => store.setFilter(
+					'event-editor-dates-list',
+					'datesPerPage',
+					datesPerPage
+				),
+				setDatesListView: () => store.setFilter(
+					'event-editor-dates-list',
+					'datesView',
+					'list'
+				),
+				setDatesGridView: () => store.setFilter(
+					'event-editor-dates-list',
+					'datesView',
+					'grid'
+				),
+			};
+		} ),
+		( WrappedComponent ) => {
+			return class extends Component {
+				render() {
+					return <WrappedComponent { ...this.props } />;
 				}
 			};
-
-			/**
-			 * @param {Array} entities
-			 * @return {Object} filter state object
-			 */
-			getFilterState = entities => {
-				// state with added actions for dates list filters
-				return filterStateHandler.register(
-					datesListFilterState.handler,
-					// merge initial filter bar state object with entities
-					{
-						...datesListFilterState.initialState,
-						...{ entities: entities, eventDates: entities },
-					}
-				);
-			};
-
-			render() {
-				const { ...otherProps } = this.props;
-				return <DatesListFilterBar
-					{ ...this.state }
-					{ ...otherProps }
-				/>;
-			}
-		}
-		return DatesListFilterState;
-	},
+		},
+	] ),
 	'withDatesListFilterState'
 );
