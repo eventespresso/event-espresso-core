9cd68b332e0acfc5c07512fa972e0da3918b8a2f
Brent Christensen <brent@eventespresso.com>
2019-02-26 11:32:02 -0800

    wire up trash date handler to dispatch

--- assets/src/editor/events/dates-and-times/editor-date/action-handlers/trash-event-date.js
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/trash-event-date.js
@@ -1,7 +1,31 @@
 /**
- * @function
- * @param {Object} eventDate    JSON object defining the Event Date
+ * External imports
  */
-export const trashEventDate = ( eventDate ) => {
-	console.log( ' >>> CLICK <<< TRASH EVENT DATE ', eventDate );
+import { dispatch } from '@wordpress/data';
+import { __ } from '@eventespresso/i18n';
+import { dateTimeModel } from '@eventespresso/model';
+import { isModelEntityOfModel } from '@eventespresso/validators';
+
+/**
+ * @function
+ * @param {Object} datetimeEntity  EE Date object
+ * @return {Object} new eventDate
+ */
+export const trashEventDate = async ( datetimeEntity ) => {
+	const { MODEL_NAME: DATETIME } = dateTimeModel;
+	if ( ! isModelEntityOfModel( datetimeEntity, DATETIME ) ) {
+		return null;
+	}
+	if ( ! window.confirm(
+		__(
+			'Are you sure you want to delete this Event Date?',
+			'event_espresso'
+		)
+	) ) {
+		return null;
+	}
+	return await dispatch( 'eventespresso/core' ).trashEntityById(
+		DATETIME,
+		datetimeEntity.id
+	);
 };
