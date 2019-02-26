20370e1fe499aa54d667c9185b3bdbeb93045be5
Brent Christensen <brent@eventespresso.com>
2019-02-25 17:42:58 -0800

    update eventDateEntityFormSubmitHandler() to work with model objects

--- assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-submit-handler.js
+++ assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-submit-handler.js
@@ -1,4 +1,11 @@
 /**
+ * External imports
+ */
+import { dispatch } from '@wordpress/data';
+import { DateTime } from '@eventespresso/value-objects';
+import { isModelEntityOfModel } from '@eventespresso/validators';
+
+/**
  * updates Event Date Entity properties
  * given the supplied form data
  *
@@ -7,15 +14,24 @@
  * @param {Object} formData
  * @return {Object} updates eventDate
  */
-export const eventDateEntityFormSubmitHandler = ( eventDate, formData ) => {
-	eventDate.name = formData[ `ee-event-date-name-${ eventDate.id }` ];
-	eventDate.description = formData[ `ee-event-date-description-${ eventDate.id }` ];
-	eventDate.start = formData[ `ee-event-date-start-${ eventDate.id }` ];
-	eventDate.end = formData[ `ee-event-date-end-${ eventDate.id }` ];
-	eventDate.regLimit = formData[ `ee-event-date-reg-limit-${ eventDate.id }` ];
-	eventDate.isPrimary = formData[ `ee-event-date-is-primary-${ eventDate.id }` ];
-	eventDate.order = formData[ `ee-event-date-order-${ eventDate.id }` ];
-	eventDate.parent = formData[ `ee-event-date-parent-${ eventDate.id }` ];
-	eventDate.deleted = formData[ `ee-event-date-deleted-${ eventDate.id }` ];
-	return eventDate;
+export const eventDateEntityFormSubmitHandler = async ( eventDate, formData ) => {
+	if ( ! isModelEntityOfModel( eventDate, 'datetime' ) ) {
+		return null;
+	}
+	const id = eventDate.id;
+	const prefix = 'ee-event-date';
+	const startDate = formData[ `${ prefix }-start-${ id }` ];
+	const endDate = formData[ `${ prefix }-end-${ id }` ];
+	eventDate.name = formData[ `${ prefix }-name-${ id }` ];
+	eventDate.description = formData[ `${ prefix }-description-${ id }` ];
+	eventDate.start = new DateTime( startDate );
+	eventDate.end = new DateTime( endDate );
+	eventDate.regLimit = parseInt( formData[ `${ prefix }-reg-limit-${ id }` ] );
+	eventDate.isPrimary = formData[ `${ prefix }-is-primary-${ id }` ];
+	eventDate.order = parseInt( formData[ `${ prefix }-order-${ id }` ] );
+	eventDate.parent = parseInt( formData[ `${ prefix }-parent-${ id }` ] );
+	eventDate.deleted = formData[ `${ prefix }-deleted-${ id }` ];
+	return await dispatch( 'eventespresso/core' ).persistDatetimeRecord(
+		eventDate
+	);
 };
