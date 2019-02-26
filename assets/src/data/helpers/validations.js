656e21011fa5c0b87b6286619b6e874d033818bd
Brent Christensen <brent@eventespresso.com>
2019-02-26 11:44:03 -0800

    code cleanup fix jsdocs

--- assets/src/data/helpers/validations.js
+++ assets/src/data/helpers/validations.js
@@ -1,22 +1,22 @@
 /**
  * @function
- * @param {mixed} value
+ * @param {number|string} value
  * @return {boolean} false on fail
  */
-export const isFloat = value => {
+export const isFloat = ( value ) => {
 	const regex1 = /^([-+])?([0-9]+(\.[0-9]+)?|Infinity)$/;
 	return regex1.test( +value ) &&
-		parseFloat( value, 10 ) === value &&
+		parseFloat( value ) === value &&
 		Number( value ) === value &&
 		value % 1 !== 0;
 };
 
 /**
  * @function
- * @param {mixed} value
+ * @param {number|string} value
  * @return {boolean} false on fail
  */
-export const isInteger = value => {
+export const isInteger = ( value ) => {
 	return parseInt( value, 10 ) === value &&
 		Number( value ) === value &&
 		value % 1 === 0;
@@ -24,10 +24,21 @@ export const isInteger = value => {
 
 /**
  * @function
- * @param {mixed} value
+ * @param {number|string} value
+ * @param {boolean} nullIsInfinity
  * @return {boolean} false on fail
  */
-export const isNumeric = value => {
+export const isInfinite = ( value, nullIsInfinity = true ) => {
+	return value === 'INF' || value === Infinity ||
+		( nullIsInfinity && value === null );
+};
+
+/**
+ * @function
+ * @param {number|string} value
+ * @return {boolean} false on fail
+ */
+export const isNumeric = ( value ) => {
 	return ! ( value instanceof Array ) &&
 		! isNaN( parseFloat( value ) ) &&
 		isFinite( value );
@@ -35,25 +46,25 @@ export const isNumeric = value => {
 
 /**
  * @function
- * @param {number} max
- * @param {number} value
+ * @param {number|string} max
+ * @param {number|string} value
  * @return {boolean} true if value less than or equal to max
  */
 export const floatLessThanOrEqualTo = ( max, value ) => {
-	return parseFloat( value, 10 ) <= parseFloat( max, 10 );
+	return parseFloat( value ) <= parseFloat( max );
 };
 
 /**
  * @function
- * @param {number} max
- * @param {number} value
+ * @param {number|string} max
+ * @param {number|string} value
  * @return {boolean} true if value less than or equal to max
  */
 export const stringLengthLessThanOrEqualTo = ( max = 1024, value ) => {
 	return value === null ||
 		value === undefined ||
-		typeof value === undefined ||
-		( typeof value !== undefined && typeof value.length === 'undefined' ) ||
+		typeof value === 'undefined' ||
+		( typeof value !== 'undefined' && typeof value.length === 'undefined' ) ||
 		// all of above equal to: zero or no length <= max length
 		( typeof value.length !== 'undefined' &&
 			parseInt( value.length, 10 ) <= parseInt( max, 10 )
@@ -62,8 +73,8 @@ export const stringLengthLessThanOrEqualTo = ( max = 1024, value ) => {
 
 /**
  * @function
- * @param {number} max
- * @param {number} value
+ * @param {number|string} max
+ * @param {number|string} value
  * @return {boolean} true if value less than or equal to max
  */
 export const numberLessThanOrEqualTo = ( max, value ) => {
@@ -72,32 +83,32 @@ export const numberLessThanOrEqualTo = ( max, value ) => {
 
 /**
  * @function
- * @param {number} min
- * @param {number} value
+ * @param {number|string} min
+ * @param {number|string} value
  * @return {boolean} true if value greater than or equal to min
  */
 export const floatGreaterThanOrEqualTo = ( min, value ) => {
-	return parseFloat( value, 10 ) >= parseFloat( min, 10 );
+	return parseFloat( value ) >= parseFloat( min );
 };
 
 /**
  * @function
- * @param {number} min
- * @param {number} value
+ * @param {number|string} min
+ * @param {number|string} value
  * @return {boolean} true if value greater than or equal to min
  */
 export const stringLengthGreaterThanOrEqualTo = ( min, value ) => {
 	return value !== null &&
 		value !== undefined &&
-		typeof value !== undefined &&
+		typeof value !== 'undefined' &&
 		typeof value.length !== 'undefined' &&
 		parseInt( value.length, 10 ) >= parseInt( min, 10 );
 };
 
 /**
  * @function
- * @param {number} min
- * @param {number} value
+ * @param {number|string} min
+ * @param {number|string} value
  * @return {boolean} true if value greater than or equal to min
  */
 export const numberGreaterThanOrEqualTo = ( min, value ) => {
