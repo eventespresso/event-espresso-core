3656ce1cf8fcccf5f65da8190b7f6cc1ddab5c06
Brent Christensen <brent@eventespresso.com>
2019-02-26 12:22:45 -0800

    temp copyEventDate handler (awaiting model changes)

--- assets/src/editor/events/dates-and-times/editor-date/action-handlers/copy-event-date.js
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/copy-event-date.js
@@ -1,7 +1,62 @@
 /**
- * @function
- * @param {Object} eventDate    JSON object defining the Event Date
+ * External imports
  */
-export const copyEventDate = ( eventDate ) => {
-	console.log( ' >>> CLICK <<< COPY EVENT DATE ', eventDate );
+import { isEmpty } from 'lodash';
+import { dispatch, select } from '@wordpress/data';
+import { dateTimeModel, eventModel, ticketModel } from '@eventespresso/model';
+import { isModelEntityOfModel } from '@eventespresso/validators';
+
+const { MODEL_NAME: DATETIME } = dateTimeModel;
+const { MODEL_NAME: EVENT } = eventModel;
+const { MODEL_NAME: TICKET } = ticketModel;
+
+/**
+ * @function
+ * @param {Object} datetimeEntity  EE Date object
+ * @return {Object} bool true if copy was successful
+ */
+export const copyEventDate = async ( datetimeEntity ) => {
+	if ( ! isModelEntityOfModel( datetimeEntity, DATETIME ) ) {
+		return false;
+	}
+	const store = select( 'eventespresso/core' );
+	const server = dispatch( 'eventespresso/core' );
+	let newDatetimeEntity = await server.createEntity(
+		DATETIME,
+		{
+			EVT_ID: datetimeEntity.EVT_ID,
+			DTT_name: datetimeEntity.name,
+			DTT_description: datetimeEntity.description,
+			DTT_EVT_start: datetimeEntity.start,
+			DTT_EVT_end: datetimeEntity.end,
+			DTT_reg_limit: datetimeEntity.regLimit,
+			DTT_sold: 0,
+			DTT_reserved: 0,
+			DTT_is_primary: datetimeEntity.isPrimary,
+			DTT_order: datetimeEntity.order,
+			DTT_parent: datetimeEntity.parent,
+			DTT_deleted: datetimeEntity.deleted,
+		}
+	);
+	server.createRelation(
+		EVENT,
+		datetimeEntity.EVT_ID,
+		DATETIME,
+		newDatetimeEntity
+	);
+	const ticketEntities = store.getRelatedEntities( datetimeEntity, TICKET );
+	if ( Array.isArray( ticketEntities ) && ! isEmpty( ticketEntities ) ) {
+		return await dispatch( 'eventespresso/core' ).createRelations(
+			DATETIME,
+			newDatetimeEntity.id,
+			TICKET,
+			ticketEntities
+		);
+	}
+	newDatetimeEntity = await server.persistEntityRecord(
+		DATETIME,
+		newDatetimeEntity
+	);
+	server.persistRelationsForEntityId( DATETIME, newDatetimeEntity.id );
+	return true;
 };
