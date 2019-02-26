76e6305675de67dedd3965e4c7a65fbfddf2bc17
Brent Christensen <brent@eventespresso.com>
2019-02-25 17:36:54 -0800

    use CalendarDateRange and extract status logic to helper

--- assets/src/editor/events/dates-and-times/editor-date/grid-view/editor-date-grid-item.js
+++ assets/src/editor/events/dates-and-times/editor-date/grid-view/editor-date-grid-item.js
@@ -6,7 +6,7 @@ import { IconButton } from '@wordpress/components';
 import { __ } from '@eventespresso/i18n';
 import {
 	BiggieCalendarDate,
-	MediumCalendarDate,
+	CalendarDateRange,
 	withEntityPaperFrame,
 } from '@eventespresso/components';
 import { dateTimeModel } from '@eventespresso/model';
@@ -18,7 +18,7 @@ import { isModelEntityOfModel } from '@eventespresso/validators';
 import { default as EditorDateDetails } from './editor-date-details';
 import { EditorDateActionsMenu } from '../';
 
-const { status, DATETIME_STATUS_ID, MODEL_NAME } = dateTimeModel;
+const { getBackgroundColorClass, MODEL_NAME } = dateTimeModel;
 
 /**
  * EditorDateGridItem
@@ -29,59 +29,17 @@ const { status, DATETIME_STATUS_ID, MODEL_NAME } = dateTimeModel;
  */
 class EditorDateGridItem extends Component {
 	/**
-	 * getStatusClass
-	 *
-	 * @function
-	 * @param {Object} eventDate    JSON object defining the Event Date
-	 * @return {string}    CSS class corresponding to the Date status
-	 */
-	getStatusClass = ( eventDate ) => {
-		switch ( status( eventDate ) ) {
-			case DATETIME_STATUS_ID.ACTIVE :
-				return 'ee-datetime-active';
-			case DATETIME_STATUS_ID.EXPIRED :
-				return 'ee-datetime-expired';
-			case DATETIME_STATUS_ID.SOLD_OUT :
-				return 'ee-datetime-sold-out';
-			case DATETIME_STATUS_ID.UPCOMING :
-				return 'ee-datetime-upcoming';
-		}
-	};
-
-	/**
-	 * getBgColorClass
-	 *
-	 * @function
-	 * @param {Object} eventDate    JSON object defining the Event Date
-	 * @return {string}    CSS class corresponding to the background color for the container
-	 */
-	getBgColorClass = ( eventDate ) => {
-		switch ( status( eventDate ) ) {
-			case DATETIME_STATUS_ID.ACTIVE :
-				return 'ee-green-background';
-			case DATETIME_STATUS_ID.EXPIRED :
-				return 'ee-lt-grey-background';
-			case DATETIME_STATUS_ID.SOLD_OUT :
-				return 'ee-orange-background';
-			case DATETIME_STATUS_ID.UPCOMING :
-			default:
-				return 'ee-blue-background';
-		}
-	};
-
-	/**
 	 * @function
 	 * @param {Object} eventDate
 	 * @param {string} showDate
 	 * @return {Object} rendered date
 	 */
 	displayDate = ( eventDate, showDate ) => {
-		// console.log( 'EditorDateGridItem.displayDate() eventDate: ', eventDate );
+		// console.log( 'EditorDateGridItem.displayDate()' );
+		// console.log( '> eventDate: ', eventDate );
 		let sidebarColorClass = 'ee-editor-date-calendar-sidebar ';
-		sidebarColorClass += this.getBgColorClass( eventDate );
-		// const startDate = moment( eventDate.start );
-		// const endDate = moment( eventDate.end );
-
+		sidebarColorClass += getBackgroundColorClass( eventDate );
+		// console.log( '> sidebarColorClass: ', sidebarColorClass );
 		switch ( showDate ) {
 			case 'end' :
 				return <BiggieCalendarDate
@@ -92,19 +50,11 @@ class EditorDateGridItem extends Component {
 				/>;
 			case 'both' :
 				return (
-					<div className={
-						`${ sidebarColorClass } medium-calendar-date-wrapper`
-					} >
-						<MediumCalendarDate
-							date={ eventDate.start }
-							footerText={ eventDate.start.toFormat( 'h:mm a' ) }
-						/>
-						<MediumCalendarDate
-							date={ eventDate.end }
-							headerText={ __( 'to', 'event_espresso' ) }
-							footerText={ eventDate.end.toFormat( 'h:mm a' ) }
-						/>
-					</div>
+					<CalendarDateRange
+						startDate={ eventDate.start }
+						endDate={ eventDate.end }
+						htmlClass={ sidebarColorClass }
+					/>
 				);
 			case 'start' :
 			default :
@@ -145,9 +95,12 @@ class EditorDateGridItem extends Component {
 				labelPosition="top right"
 			/>
 		) : null;
+		const dateStyleClass = showDate === 'both' ?
+			'ee-editor-date-range' :
+			'ee-editor-date-single';
 		return (
 			<Fragment>
-				<div className="ee-editor-date-main">
+				<div className={ `ee-editor-date-main ${ dateStyleClass }` }>
 					{ this.displayDate( eventDate, showDate ) }
 					<EditorDateDetails
 						eventDate={ eventDate }
