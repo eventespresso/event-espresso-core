07b7d628ae7e8d78acb710a653ef3cbf542dd16d
Brent Christensen <brent@eventespresso.com>
2019-02-26 07:48:52 -0800

    update to work with model objects

--- assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-schema.js
+++ assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-schema.js
@@ -1,4 +1,9 @@
 /**
+ * External imports
+ */
+import { isModelEntityOfModel } from '@eventespresso/validators';
+
+/**
  * returns an object mapping
  * Event Date Entity properties
  * to form inputs
@@ -7,27 +12,29 @@
  * @param {Object} eventDate  EE Date object
  * @return {Object} rendered form
  */
-export const eventDateEntityFormSchema = eventDate => {
-	// console.log( 'eventDateEntityFormSchema() eventDate', eventDate );
+export const eventDateEntityFormSchema = ( eventDate ) => {
+	if ( ! isModelEntityOfModel( eventDate, 'datetime' ) ) {
+		return {};
+	}
+	const id = eventDate.id;
+	const prefix = 'ee-event-date';
 	return {
-		[ `ee-event-date-id-${ eventDate.id }` ]: eventDate.id,
-		[ `ee-event-date-event-id-${ eventDate.id }` ]: eventDate.EVT_ID,
-		[ `ee-event-date-name-${ eventDate.id }` ]: eventDate.name || '',
-		[ `ee-event-date-description-${ eventDate.id }` ]: eventDate.description || '',
-		[ `ee-event-date-start-${ eventDate.id }` ]: eventDate.start,
-		[ `ee-event-date-end-${ eventDate.id }` ]: eventDate.end,
-		[ `ee-event-date-reg-limit-${ eventDate.id }` ]: stripInfinity(
-			eventDate.regLimit
-		),
-		[ `ee-event-date-sold-${ eventDate.id }` ]: eventDate.sold || 0,
-		[ `ee-event-date-reserved-${ eventDate.id }` ]: eventDate.reserved || 0,
-		[ `ee-event-date-is-primary-${ eventDate.id }` ]: eventDate.isPrimary || false,
-		[ `ee-event-date-order-${ eventDate.id }` ]: eventDate.order || 0,
-		[ `ee-event-date-parent-${ eventDate.id }` ]: eventDate.parent || 0,
-		[ `ee-event-date-deleted-${ eventDate.id }` ]: eventDate.deleted || false,
+		[ `${ prefix }-id-${ id }` ]: id,
+		[ `${ prefix }-event-id-${ id }` ]: eventDate.EVT_ID,
+		[ `${ prefix }-name-${ id }` ]: eventDate.name || '',
+		[ `${ prefix }-description-${ id }` ]: eventDate.description || '',
+		[ `${ prefix }-start-${ id }` ]: eventDate.start.toISO(),
+		[ `${ prefix }-end-${ id }` ]: eventDate.end.toISO(),
+		[ `${ prefix }-reg-limit-${ id }` ]: stripInf( eventDate.regLimit ),
+		[ `${ prefix }-sold-${ id }` ]: eventDate.sold || 0,
+		[ `${ prefix }-reserved-${ id }` ]: eventDate.reserved || 0,
+		[ `${ prefix }-is-primary-${ id }` ]: eventDate.isPrimary || false,
+		[ `${ prefix }-order-${ id }` ]: eventDate.order || 0,
+		[ `${ prefix }-parent-${ id }` ]: eventDate.parent || 0,
+		[ `${ prefix }-deleted-${ id }` ]: eventDate.deleted || false,
 	};
 };
 
-const stripInfinity = number => number !== 'INF' || number !== Infinity ?
+const stripInf = ( number ) => number !== 'INF' || number !== Infinity ?
 	number :
 	null;
