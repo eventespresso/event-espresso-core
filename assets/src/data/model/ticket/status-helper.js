3e3eb06a6408688d1698e0d2d4b9da7efd920f44
Brent Christensen <brent@eventespresso.com>
2019-02-26 11:35:11 -0800

    handle infinite qty, add status class methods

--- assets/src/data/model/ticket/status-helper.js
+++ assets/src/data/model/ticket/status-helper.js
@@ -1,6 +1,7 @@
 /**
  * External imports
  */
+import { __ } from '@eventespresso/i18n';
 import { isModelEntityOfModel } from '@eventespresso/validators';
 
 /**
@@ -8,6 +9,11 @@ import { isModelEntityOfModel } from '@eventespresso/validators';
  */
 import { MODEL_NAME, TICKET_STATUS_ID } from './constants';
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @throws {TypeError}
+ */
 const assertTicketEntity = ( TicketEntity ) => {
 	if ( ! isModelEntityOfModel( TicketEntity, MODEL_NAME ) ) {
 		throw new TypeError(
@@ -16,32 +22,65 @@ const assertTicketEntity = ( TicketEntity ) => {
 	}
 };
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {boolean} true if ticket is currently available for purchase
+ */
 export const isOnSale = ( TicketEntity ) => {
 	assertTicketEntity( TicketEntity );
 	return TicketEntity.startDate.diffNow() < 0 &&
 		TicketEntity.endDate.diffNow() > 0;
 };
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {boolean} true if ticket can no longer be purchased
+ */
 export const isExpired = ( TicketEntity ) => {
 	assertTicketEntity( TicketEntity );
 	return TicketEntity.endDate.diffNow() < 0;
 };
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {boolean} true if tickets sold meets or exceeds available quantity
+ */
 export const isSoldOut = ( TicketEntity ) => {
 	assertTicketEntity( TicketEntity );
-	return TicketEntity.sold >= TicketEntity.qty;
+	const qty = TicketEntity.qty;
+	return ( qty !== null && qty !== 'INF' && qty !== Infinity ) &&
+		TicketEntity.sold >= qty;
 };
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {boolean} 	true if ticket is not yet available for purchase,
+ * 						but will be at some date in the future
+ */
 export const isPending = ( TicketEntity ) => {
 	assertTicketEntity( TicketEntity );
 	return TicketEntity.startDate.diffNow() > 0;
 };
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {boolean} true if ticket is archived
+ */
 export const isArchived = ( TicketEntity ) => {
 	assertTicketEntity( TicketEntity );
 	return TicketEntity.deleted;
 };
 
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {string} status ID
+ */
 export const status = ( TicketEntity ) => {
 	assertTicketEntity( TicketEntity );
 	if ( isSoldOut( TicketEntity ) ) {
@@ -59,6 +98,55 @@ export const status = ( TicketEntity ) => {
 	if ( isArchived( TicketEntity ) ) {
 		return TICKET_STATUS_ID.ARCHIVED;
 	}
-	return null;
+	return '';
+};
 
+/**
+ * getBackgroundColorClass
+ *
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {string} CSS class corresponding to the background color
+ * 					for the ticket container based on the ticket status
+ */
+export const getBackgroundColorClass = ( TicketEntity ) => {
+	switch ( status( TicketEntity ) ) {
+		case TICKET_STATUS_ID.ONSALE :
+			return 'ee-green-background';
+		case TICKET_STATUS_ID.EXPIRED :
+			return 'ee-lt-grey-background';
+		case TICKET_STATUS_ID.SOLD_OUT :
+			return 'ee-orange-background';
+		case TICKET_STATUS_ID.PENDING :
+			return 'ee-blue-background';
+		case TICKET_STATUS_ID.ARCHIVED :
+			return 'ee-violet-background';
+	}
+};
+
+/**
+ * @function
+ * @param {Object} TicketEntity model object
+ * @return {string} ticket status
+ */
+export const getTicketStatusTextLabel = ( TicketEntity ) => {
+	let ticketStatus = null;
+	switch ( status( TicketEntity ) ) {
+		case TICKET_STATUS_ID.SOLD_OUT :
+			ticketStatus = __( 'SOLD OUT', 'event_espresso' );
+			break;
+		case TICKET_STATUS_ID.EXPIRED :
+			ticketStatus = __( 'EXPIRED', 'event_espresso' );
+			break;
+		case TICKET_STATUS_ID.PENDING :
+			ticketStatus = __( 'PENDING', 'event_espresso' );
+			break;
+		case TICKET_STATUS_ID.ONSALE :
+			ticketStatus = __( 'ON SALE', 'event_espresso' );
+			break;
+		case TICKET_STATUS_ID.ARCHIVED :
+			ticketStatus = __( 'ARCHIVED', 'event_espresso' );
+			break;
+	}
+	return ticketStatus;
 };
