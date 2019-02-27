1bfd527785c1b95e70a90b8ddc004d00a90894dc
Darren Ethier <darren@roughsmootheng.in>
2019-02-27 15:28:57 -0500

    add clone getter to BaseEntity

--- assets/src/data/model/entity-factory/base-entity.js
+++ assets/src/data/model/entity-factory/base-entity.js
@@ -118,6 +118,16 @@ class BaseEntity {
 		return ( fieldName ) => this.protectedFields.indexOf( fieldName ) > -1;
 	}
 
+	get clone() {
+		return new BaseEntity(
+			this.modelName,
+			this.forClone,
+			{ $schema: {}, properties: this.schema },
+			this.fieldPrefixes,
+			true
+		);
+	}
+
 	static name = 'BaseEntity'
 }
 
