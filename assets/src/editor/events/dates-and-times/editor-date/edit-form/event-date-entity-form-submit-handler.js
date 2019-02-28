3b05e119ef44ed8659d7512abc1e4f4ef15fe7c6
Brent Christensen <brent@eventespresso.com>
2019-02-27 16:18:10 -0800

    handle defaults better and return Promise instead of waiting

--- assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-submit-handler.js
+++ assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-submit-handler.js
@@ -2,6 +2,7 @@
  * External imports
  */
 import { dispatch } from '@wordpress/data';
+import { dateTimeModel } from '@eventespresso/model';
 import { DateTime } from '@eventespresso/value-objects';
 import { isModelEntityOfModel } from '@eventespresso/validators';
 
@@ -14,22 +15,24 @@ import { isModelEntityOfModel } from '@eventespresso/validators';
  * @param {Object} formData
  * @return {Object} updates eventDate
  */
-export const eventDateEntityFormSubmitHandler = async ( eventDate, formData ) => {
+export const eventDateEntityFormSubmitHandler = ( eventDate, formData ) => {
 	if ( ! isModelEntityOfModel( eventDate, 'datetime' ) ) {
 		return null;
 	}
 	const id = eventDate.id;
 	const prefix = 'ee-event-date';
-	eventDate.name = formData[ `${ prefix }-name-${ id }` ];
-	eventDate.description = formData[ `${ prefix }-description-${ id }` ];
-	eventDate.start = new DateTime( formData[ `${ prefix }-start-${ id }` ] );
-	eventDate.end = new DateTime( formData[ `${ prefix }-end-${ id }` ] );
-	eventDate.regLimit = parseInt( formData[ `${ prefix }-reg-limit-${ id }` ] );
-	eventDate.isPrimary = !! formData[ `${ prefix }-is-primary-${ id }` ];
-	eventDate.order = parseInt( formData[ `${ prefix }-order-${ id }` ] );
-	eventDate.parent = parseInt( formData[ `${ prefix }-parent-${ id }` ] );
-	eventDate.deleted = !! formData[ `${ prefix }-deleted-${ id }` ];
-	return await dispatch( 'eventespresso/core' ).persistDatetimeRecord(
+	eventDate.name = formData[ `${ prefix }-name-${ id }` ] || '';
+	eventDate.description = formData[ `${ prefix }-description-${ id }` ] || '';
+	eventDate.start = new DateTime( formData[ `${ prefix }-start-${ id }` ] || '' );
+	eventDate.end = new DateTime( formData[ `${ prefix }-end-${ id }` ] || '' );
+	eventDate.regLimit = parseInt( formData[ `${ prefix }-reg-limit-${ id }` ] || -1 );
+	eventDate.isPrimary = !! formData[ `${ prefix }-is-primary-${ id }` ] || false;
+	eventDate.order = parseInt( formData[ `${ prefix }-order-${ id }` ] || 0 );
+	eventDate.parent = parseInt( formData[ `${ prefix }-parent-${ id }` ] || 0 );
+	eventDate.deleted = !! formData[ `${ prefix }-deleted-${ id }` ] || false;
+	const { MODEL_NAME: DATETIME } = dateTimeModel;
+	return dispatch( 'eventespresso/core' ).persistEntityRecord(
+		DATETIME,
 		eventDate
 	);
 };
