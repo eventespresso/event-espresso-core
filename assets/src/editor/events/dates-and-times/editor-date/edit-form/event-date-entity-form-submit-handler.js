4963edeee3f4b775cea4e39458866c21a453f449
Brent Christensen <brent@eventespresso.com>
2019-02-28 08:12:48 -0800

    add relation to event if missing, update entity var names

--- assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-submit-handler.js
+++ assets/src/editor/events/dates-and-times/editor-date/edit-form/event-date-entity-form-submit-handler.js
@@ -2,37 +2,52 @@
  * External imports
  */
 import { dispatch } from '@wordpress/data';
-import { dateTimeModel } from '@eventespresso/model';
+import { dateTimeModel, eventModel } from '@eventespresso/model';
 import { DateTime } from '@eventespresso/value-objects';
 import { isModelEntityOfModel } from '@eventespresso/validators';
+
+const { MODEL_NAME: DATETIME } = dateTimeModel;
+const { MODEL_NAME: EVENT } = eventModel;
+const { createRelation, persistEntityRecord } = dispatch( 'eventespresso/core' );
 
 /**
  * updates Event Date Entity properties
  * given the supplied form data
  *
  * @function
- * @param {Object} eventDate  EE Date object
+ * @param {Object} eventEntity EE Event object
+ * @param {Object} dateEntity  EE Date object
  * @param {Object} formData
- * @return {Object} updates eventDate
+ * @return {Object} updates dateEntity
  */
-export const eventDateEntityFormSubmitHandler = ( eventDate, formData ) => {
-	if ( ! isModelEntityOfModel( eventDate, 'datetime' ) ) {
+export const eventDateEntityFormSubmitHandler = (
+	eventEntity,
+	dateEntity,
+	formData
+) => {
+	if ( ! isModelEntityOfModel( eventEntity, EVENT ) ||
+		! isModelEntityOfModel( dateEntity, DATETIME )
+	) {
 		return null;
 	}
-	const id = eventDate.id;
+	const id = dateEntity.id;
 	const prefix = 'ee-event-date';
-	eventDate.name = formData[ `${ prefix }-name-${ id }` ] || '';
-	eventDate.description = formData[ `${ prefix }-description-${ id }` ] || '';
-	eventDate.start = new DateTime( formData[ `${ prefix }-start-${ id }` ] || '' );
-	eventDate.end = new DateTime( formData[ `${ prefix }-end-${ id }` ] || '' );
-	eventDate.regLimit = parseInt( formData[ `${ prefix }-reg-limit-${ id }` ] || -1 );
-	eventDate.isPrimary = !! formData[ `${ prefix }-is-primary-${ id }` ] || false;
-	eventDate.order = parseInt( formData[ `${ prefix }-order-${ id }` ] || 0 );
-	eventDate.parent = parseInt( formData[ `${ prefix }-parent-${ id }` ] || 0 );
-	eventDate.deleted = !! formData[ `${ prefix }-deleted-${ id }` ] || false;
-	const { MODEL_NAME: DATETIME } = dateTimeModel;
-	return dispatch( 'eventespresso/core' ).persistEntityRecord(
-		DATETIME,
-		eventDate
-	);
+	dateEntity.name = formData[ `${ prefix }-name-${ id }` ] || '';
+	dateEntity.description = formData[ `${ prefix }-description-${ id }` ] || '';
+	dateEntity.start = new DateTime( formData[ `${ prefix }-start-${ id }` ] || '' );
+	dateEntity.end = new DateTime( formData[ `${ prefix }-end-${ id }` ] || '' );
+	dateEntity.regLimit = parseInt( formData[ `${ prefix }-reg-limit-${ id }` ] || -1 );
+	dateEntity.isPrimary = !! formData[ `${ prefix }-is-primary-${ id }` ] || false;
+	dateEntity.order = parseInt( formData[ `${ prefix }-order-${ id }` ] || 0 );
+	dateEntity.parent = parseInt( formData[ `${ prefix }-parent-${ id }` ] || 0 );
+	dateEntity.deleted = !! formData[ `${ prefix }-deleted-${ id }` ] || false;
+	if ( ! dateEntity.EVT_ID ) {
+		createRelation(
+			EVENT,
+			eventEntity.id,
+			DATETIME,
+			dateEntity
+		);
+	}
+	return persistEntityRecord( DATETIME, dateEntity );
 };
