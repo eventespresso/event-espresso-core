31ac1bccbadbd7ccb5c1340cc84e777e601058e0
Josh Feck <josh@nxnweb.com>
2019-03-06 14:58:36 -0500

    Mijireh settings help tab: fix links

--- caffeinated/payment_methods/Mijireh/help_tabs/payment_methods_overview_mijireh.help_tab.php
+++ caffeinated/payment_methods/Mijireh/help_tabs/payment_methods_overview_mijireh.help_tab.php
@@ -9,9 +9,9 @@
             'Mijireh is basically a middle-man between Event Espresso and over 90 payment gateways. Most of the configuration occurs on %1$sMijireh\' website%2$s, where you configure your Mijireh store with the %3$spayment gateway of your choice%4$s.',
             'event_espresso'
         ),
-        '<a href="http://www.mijireh.com/" target="_blank">',
+        '<a href="http://www.mijireh.com/" target="_blank" rel="noopener noreferrer">',
         '</a>',
-        '<a href="http://www.mijireh.com/docs/payment-gateways/" target="_blank">',
+        '<a href="http://www.mijireh.com/docs/payment-gateways/" target="_blank" rel="noopener noreferrer">',
         '</a>'
     );
     ?>
@@ -31,7 +31,7 @@
                 'Enter your Access Key for Mijireh. Your Access Key can be found in your %1$sMijireh account dashboard%2$s.',
                 'event_espresso'
             ),
-            '<a href="https://secure.mijireh.com/login" target="_blank">',
+            '<a href="https://secure.mijireh.com/login" target="_blank" rel="noopener noreferrer">',
             '</a>'
         );
         ?>
@@ -52,7 +52,7 @@
             "In order to do this, we automatically create a new WordPress page which is designed to be slurped by Mijireh. You can customize the page like any other, but the page must contain Mijireh's special '{{mijireh-checkout-form}}' shortcode. Once you have finished designing the page, publish it, and %s then click the special 'slurp now' button%s. Mijireh may take several minutes to slurp the page. Once Mijireh is finished slurping, you can delete the page, or mark it as a draft.",
             'event_espresso'
         ),
-        '<a href="http://ee-screenshots.s3.amazonaws.com/ee4/event-espresso-mijireh-slurp-page-example.jpg" target="_blank">',
+        '<a href="http://ee-screenshots.s3.amazonaws.com/ee4/event-espresso-mijireh-slurp-page-example.jpg" target="_blank" rel="noopener noreferrer">',
         '</a>'
     ); ?>
 </p>
