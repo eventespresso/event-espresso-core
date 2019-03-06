7c06f2c175610ecd102ea3dd1e505f9e03a2e377
Brent Christensen <brent@eventespresso.com>
2019-03-06 13:57:25 -0800

    add constants for common time periods
    thought these would exist elsewhere already, but couldn't find any

--- assets/src/data/model/datetime/constants.js
+++ assets/src/data/model/datetime/constants.js
@@ -14,3 +14,9 @@ export const DATETIME_STATUS_ID = {
 };
 
 export const DATETIME_STATUS_IDS = values( DATETIME_STATUS_ID );
+
+export const MINUTE_IN_SECONDS = 60;
+export const HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60;
+export const DAY_IN_SECONDS = HOUR_IN_SECONDS * 24;
+export const WEEK_IN_SECONDS = HOUR_IN_SECONDS * 7;
+export const MONTH_IN_SECONDS = DAY_IN_SECONDS * 30;
