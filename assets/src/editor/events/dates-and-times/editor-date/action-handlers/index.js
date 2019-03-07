c17dab8f00479ef28dc3005db62d3168085243ac
Brent Christensen <brent@eventespresso.com>
2019-03-07 08:27:16 -0800

    update handler name change

--- assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
@@ -1,3 +1,3 @@
 export { copyEventDate } from './copy-event-date';
-export { editEventDate } from './edit-event-date';
+export { updateEventDate } from './update-event-date';
 export { trashEventDate } from './trash-event-date';
