56af0afdd9cb03f19dcb44d4d9e8ac7e2c7879c8
Brent Christensen <brent@eventespresso.com>
2019-03-07 08:26:15 -0800

    rename edit handler to updateEventDate and persist entity changes

--- assets/src/editor/events/dates-and-times/editor-date/action-handlers/edit-event-date.js
+++
@@ -1,7 +0,0 @@
-/**
- * @function
- * @param {Object} eventDate    JSON object defining the Event Date
- */
-export const editEventDate = ( eventDate ) => {
-	console.log( ' >>> CLICK <<< EDIT EVENT DATE ', eventDate );
-};
