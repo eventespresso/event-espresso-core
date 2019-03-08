958c07e7c6df2b5fdb3ef06225cab036d4fa6571
Seth <seth@eventespresso.com>
2019-03-08 12:50:40 -0500

    Update admin_pages/general_settings/help_tabs/general_settings_admin_options.help_tab.php
    Co-Authored-By: joshfeck <josh@nxnweb.com>

--- admin_pages/general_settings/help_tabs/general_settings_admin_options.help_tab.php
+++ admin_pages/general_settings/help_tabs/general_settings_admin_options.help_tab.php
@@ -49,7 +49,7 @@ define( 'EELOGGING_KEY', 'YOUR REMOTE KEY' );</pre>
 <?php esc_html_e('Turn help tours off or on.', 'event_espresso'); ?>
 </p>
 <p>
-<strong><?php esc_html_e('Recommendations', 'event_espresso'); ?></strong><br />
+<strong><?php esc_html_e('Recommendations', 'event_espresso'); ?></strong>
 <?php esc_html_e('Want to see a tour of this screen? Click on the Admin Options Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
 </p>
 <p>
