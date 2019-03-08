ac40e036501302566556dbaefa438b7fcfcc52fe
Seth <seth@eventespresso.com>
2019-03-08 12:49:30 -0500

    Update admin_pages/registration_form/help_tabs/registration_form_reg_form_settings.help_tab.php
    Co-Authored-By: joshfeck <josh@nxnweb.com>

--- admin_pages/registration_form/help_tabs/registration_form_reg_form_settings.help_tab.php
+++ admin_pages/registration_form/help_tabs/registration_form_reg_form_settings.help_tab.php
@@ -32,7 +32,7 @@
 </div>
 <p>
 <strong><?php esc_html_e('Recommendations', 'event_espresso'); ?></strong><br />
-<?php esc_html_e('Want to see a tour of this screen? Click on the Registration Form Settings Tour button which appears on the right side of the page. <br />To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
+<?php esc_html_e('Want to see a tour of this screen? Click on the Registration Form Settings Tour button which appears on the right side of the page. To learn more about the options on this page, take a look at the different tabs that appear on the left side of the page.', 'event_espresso'); ?>
 </p>
 <p>
 <strong><?php esc_html_e('Screen Options', 'event_espresso'); ?></strong><br />
