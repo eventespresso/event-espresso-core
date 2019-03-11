ae0e3e0aee67947afe7111c0877f038b3cabec86
Mike Nelson <michael@eventespresso.com>
2019-03-11 13:14:39 -0700

    determine file extension from filename, not th temporary file

--- tests/testcases/core/services/request/files/FileSubmissionTest.php
+++ tests/testcases/core/services/request/files/FileSubmissionTest.php
@@ -48,6 +48,35 @@ class FileSubmissionTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($error, $file->getErrorCode());
     }
 
+    public function testFileExtensionDifferentOnTmpFile()
+    {
+        $filename = 'foobar.csv';
+        $filepath = __FILE__;
+        $filesize = 1234;
+        $error = UPLOAD_ERR_OK;
+        $file = new FileSubmission($filename, $filepath, $filesize, $error);
+        $this->assertEquals($filename, $file->getName());
+        $this->assertEquals($filepath, $file->getTmpFile());
+        $this->assertEquals('csv', $file->getExtension());
+        $this->assertEquals('text/x-php', $file->getType());
+        $this->assertEquals($filesize, $file->getSize());
+        $this->assertEquals($error, $file->getErrorCode());
+    }
+
+    public function testFileNpExtension()
+    {
+        $filename = 'foobar';
+        $filepath = __FILE__;
+        $filesize = 1234;
+        $error = UPLOAD_ERR_OK;
+        $file = new FileSubmission($filename, $filepath, $filesize, $error);
+        $this->assertEquals($filename, $file->getName());
+        $this->assertEquals($filepath, $file->getTmpFile());
+        $this->assertEquals('', $file->getExtension());
+        $this->assertEquals('text/x-php', $file->getType());
+        $this->assertEquals($filesize, $file->getSize());
+        $this->assertEquals($error, $file->getErrorCode());
+    }
 }
 // End of file FileSubmissionTest.php
 // Location: EventEspresso\tests\testcases\core\services\request\files/FileSubmissionTest.php
