3331bf95f40f8db83b88906f4fb66c68872e9a31
Brent Christensen <brent@eventespresso.com>
2019-03-12 18:07:28 -0700

    extract logic into separeate functions for readability

--- assets/src/editor/events/dates-and-tickets-metabox/dates-and-tickets-manager.js
+++ assets/src/editor/events/dates-and-tickets-metabox/dates-and-tickets-manager.js
@@ -1,8 +1,7 @@
 /**
  * External imports
  */
-
-import { filter, find, findIndex } from 'lodash';
+import { filter, find, findIndex, isFunction } from 'lodash';
 import PropTypes from 'prop-types';
 import { IconButton } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
@@ -23,6 +22,7 @@ import { isModelEntityOfModel } from '@eventespresso/validators';
 import {
 	default as withDatesAndTicketsManagerState,
 } from './with-dates-and-tickets-manager-state';
+import './dates-and-tickets-manager.css';
 
 const noIndex = -1;
 
@@ -46,6 +46,9 @@ export class DatesAndTicketsManager extends Component {
 		};
 	}
 
+	/**
+	 * @function
+	 */
 	processChanges = () => {
 		const relationUpdates = [];
 		this.setState( { submitting: true } );
@@ -85,21 +88,32 @@ export class DatesAndTicketsManager extends Component {
 		} );
 	};
 
+	/**
+	 * @function
+	 * @param {boolean} update
+	 */
 	toggleEditor = ( update = false ) => {
-		if ( this.props.closeModal &&
-			typeof this.props.closeModal === 'function'
-		) {
-			if ( update && this.props.onUpdate &&
-				typeof this.props.onUpdate === 'function'
-			) {
-				this.props.onUpdate();
+		if ( isFunction( this.closeModal ) ) {
+			if ( update && isFunction( this.onUpdate ) ) {
+				this.onUpdate();
 			}
-			this.resetRelationsMap();
+			if ( isFunction( this.resetRelationsMap ) ) {
+				this.resetRelationsMap();
+			}
 			this.setState( { submitting: false } );
-			this.props.closeModal();
+			this.closeModal();
 		}
 	};
 
+	/**
+	 * @function
+	 * @param {Object} assigned
+	 * @param {Object} date
+	 * @param {Object} ticket
+	 * @param {boolean} returnIndex
+	 * @return {number|boolean} index for date id in assigned array or
+	 *  						boolean if returnIndex is false
+	 */
 	isAssigned = ( assigned, date, ticket, returnIndex = false ) => {
 		let index = noIndex;
 		if ( Array.isArray( assigned[ date.id ] ) ) {
@@ -108,6 +122,13 @@ export class DatesAndTicketsManager extends Component {
 		return returnIndex ? index : index > noIndex;
 	};
 
+	/**
+	 * @function
+	 * @param {Object} assigned
+	 * @param {Object} date
+	 * @param {Object} ticket
+	 * @return {Object} assigned
+	 */
 	unAssignTicket = ( assigned, date, ticket ) => {
 		const index = this.isAssigned( assigned, date, ticket, true );
 		if ( index > noIndex ) {
@@ -116,6 +137,11 @@ export class DatesAndTicketsManager extends Component {
 		return assigned;
 	};
 
+	/**
+	 * @function
+	 * @param {Object} date
+	 * @param {Object} ticket
+	 */
 	assignTicket = ( date, ticket ) => {
 		const eventDate = find(
 			this.props.dates,
@@ -142,6 +168,15 @@ export class DatesAndTicketsManager extends Component {
 		}
 	};
 
+	/**
+	 * @function
+	 * @param {Object} removed
+	 * @param {Object} date
+	 * @param {Object} ticket
+	 * @param {boolean} returnIndex
+	 * @return {number|boolean} index for date id in removed array or
+	 *                        boolean if returnIndex is false
+	 */
 	isRemoved = ( removed, date, ticket, returnIndex = false ) => {
 		let index = noIndex;
 		if ( Array.isArray( removed[ date.id ] ) ) {
@@ -150,6 +185,13 @@ export class DatesAndTicketsManager extends Component {
 		return returnIndex ? index : index > noIndex;
 	};
 
+	/**
+	 * @function
+	 * @param {Object} removed
+	 * @param {Object} date
+	 * @param {Object} ticket
+	 * @return {Object} removed
+	 */
 	unRemoveTicket = ( removed, date, ticket ) => {
 		const index = this.isRemoved( removed, date, ticket, true );
 		if ( index > noIndex ) {
@@ -158,6 +200,11 @@ export class DatesAndTicketsManager extends Component {
 		return removed;
 	};
 
+	/**
+	 * @function
+	 * @param {Object} date
+	 * @param {Object} ticket
+	 */
 	removeTicket = ( date, ticket ) => {
 		const eventDate = find(
 			this.props.dates,
@@ -184,19 +231,242 @@ export class DatesAndTicketsManager extends Component {
 		}
 	};
 
+	/**
+	 * @function
+	 * @param {Array} tickets
+	 * @param {Object} cellWidth
+	 * @return {Object} rendered table header cell
+	 */
+	ticketHeaders = ( tickets, cellWidth ) => {
+		return tickets.map(
+			( ticket, index1 ) => {
+				if ( ! isModelEntityOfModel(
+					ticket,
+					TICKET
+				) ) {
+					return null;
+				}
+				return (
+					<td key={ index1 } style={ cellWidth } >
+						<div className="ee-dtm-ticket-header-title" >
+							{ `${ ticket.name }` }
+						</div>
+						<div className="ee-dtm-ticket-header-price" >
+							{ `${ ticket.price }` }
+						</div>
+					</td>
+				);
+			}
+		);
+	};
+
+	/**
+	 * @function
+	 * @param {Array} dates
+	 * @param {Array} tickets
+	 * @param {Object} eventDateTicketMap
+	 * @param {number} dateCount
+	 * @param {Object} datesWidth
+	 * @param {Object} cellWidth
+	 * @return {Object} rendered table rows
+	 */
+	dateRows = (
+		dates,
+		tickets,
+		eventDateTicketMap,
+		dateCount,
+		datesWidth,
+		cellWidth
+	) => {
+		let year = 0;
+		let yearRow = null;
+		let indexMod = 0;
+		return dates.map(
+			( eventDate, index2 ) => {
+				if ( ! isModelEntityOfModel(
+					eventDate,
+					DATETIME
+				) ) {
+					return null;
+				}
+				index2 = index2 + indexMod + 1;
+				const dateYear = parseInt(
+					eventDate.start.toFormat( 'YYYY' )
+				);
+				if ( dateCount > 1 && dateYear > year ) {
+					year = dateYear;
+					yearRow = this.yearRow( dateCount, year );
+					indexMod++;
+					index2++;
+				} else {
+					yearRow = null;
+				}
+				const eventDateTickets = eventDateTicketMap[ eventDate.id ] ?
+					eventDateTicketMap[ eventDate.id ] :
+					[];
+				return (
+					<Fragment key={ index2 }>
+						{ yearRow }
+						<tr>
+							{
+								this.dateHeader(
+									eventDate,
+									dateCount,
+									datesWidth,
+									cellWidth
+								)
+							}
+							{
+								this.ticketCells(
+									eventDate,
+									tickets,
+									eventDateTickets,
+									cellWidth
+								)
+							}
+						</tr>
+					</Fragment>
+				);
+			}
+		);
+	};
+
+	/**
+	 * @function
+	 * @param {number} dateCount
+	 * @param {number} year
+	 * @return {Object} rendered table row
+	 */
+	yearRow = ( dateCount, year ) => (
+		<tr>
+			<td colSpan={ dateCount } className="ee-dtm-year-row-style" >
+				<div>{ year }</div>
+			</td>
+		</tr>
+	);
+
+	/**
+	 * @function
+	 * @param {Object} eventDate
+	 * @param {number} dateCount
+	 * @param {Object} datesWidth
+	 * @return {Object} rendered table cell
+	 */
+	dateHeader = ( eventDate, dateCount, datesWidth ) => {
+		return dateCount > 1 ?
+			(
+				<td key={ 0 } className="ee-dtm-date-label">
+					<CalendarPageDate
+						startDate={ eventDate.start }
+						statusClass={
+							getBackgroundColorClass( eventDate )
+						}
+						size={ 'small' }
+					/>
+					<div className="ee-dtm-date-label-text">
+						{ eventDate.name }
+					</div>
+				</td>
+			) :
+			null;
+	};
+
+	/**
+	 * @function
+	 * @param {Object} eventDate
+	 * @param {Object} tickets
+	 * @param {Array} eventDateTickets
+	 * @param {Object} cellWidth
+	 * @return {Object} rendered table cell
+	 */
+	ticketCells = (
+		eventDate,
+		tickets,
+		eventDateTickets,
+		cellWidth
+	) => {
+		return tickets.map(
+			( ticket, index3 ) => {
+				index3++;
+				const hasTicket = findIndex(
+					eventDateTickets,
+					{ id: ticket.id }
+				) > noIndex;
+				const isAssigned = this.isAssigned(
+					this.state.assigned,
+					eventDate,
+					ticket,
+					true
+				);
+				const isRemoved = this.isRemoved(
+					this.state.removed,
+					eventDate,
+					ticket,
+					true
+				);
+				let icon = '';
+				let bgColor = 'ee-dtm-ticket-relation-button';
+				if ( hasTicket ) {
+					if ( isRemoved > noIndex ) {
+						icon = 'no';
+						bgColor += ' ee-dtm-remove-ticket-relation';
+					} else {
+						icon = 'tickets-alt';
+						bgColor += ' ee-dtm-has-ticket-relation';
+					}
+				} else if ( isAssigned > noIndex ) {
+					icon = 'tickets-alt';
+					bgColor += ' ee-dtm-add-ticket-relation';
+				} else {
+					icon = 'minus';
+					bgColor += ' ee-dtm-no-ticket-relation';
+				}
+				const action = isAssigned > noIndex ||
+				( hasTicket && isRemoved === noIndex ) ?
+					this.removeTicket :
+					this.assignTicket;
+				return (
+					<td key={ index3 } style={ cellWidth } >
+						<IconButton
+							icon={ icon }
+							className={ bgColor }
+							size={ 45 }
+							onClick={ ( event ) => {
+								event.preventDefault();
+								event.stopPropagation();
+								action( eventDate, ticket );
+							} }
+							onKeyDown={ ( event ) => {
+								if ( event.keyCode === ENTER ) {
+									event.preventDefault();
+									event.stopPropagation();
+									action( eventDate, ticket );
+								}
+							} }
+						/>
+					</td>
+				);
+			}
+		);
+	};
+
 	render() {
 		const {
 			loading,
 			dates,
 			tickets,
 			eventDateTicketMap,
-			resetRelationsMap,
 			addTickets,
 			removeTickets,
+			onUpdate,
+			closeModal,
+			resetRelationsMap,
 		} = this.props;
-		this.resetRelationsMap = resetRelationsMap;
 		this.addTickets = addTickets;
 		this.removeTickets = removeTickets;
+		this.onUpdate = onUpdate;
+		this.closeModal = closeModal;
+		this.resetRelationsMap = resetRelationsMap;
 
 		const {
 			FormSection,
@@ -205,72 +475,22 @@ export class DatesAndTicketsManager extends Component {
 			FormCancelButton,
 			FormSaveCancelButtons,
 		} = twoColumnAdminFormLayout;
-		// console.log( '' );
-		// console.log( 'DatesAndTicketsManager.render()' );
-		// console.log( ' > props: ', this.props );
-		// console.log( ' > addTickets: ', addTickets );
-		const width = tickets.length ? 75 / tickets.length : 75;
-		const header0 = {
-			borderBottom: '1px solid var(--ee-lite-grey)',
-			maxWidth: '25%',
-			textAlign: 'left',
-			verticalAlign: 'bottom',
-			width: '25%',
-		};
-		const header = {
-			borderBottom: '1px solid var(--ee-lite-grey)',
-			boxSizing: 'border-box',
-			padding: '0 5px',
+		const dateCount = dates.length;
+		let divisor = tickets.length ? tickets.length : 1;
+		divisor += dateCount > 1 ? 2 : 0;
+		let width = 100 / divisor;
+		const cellWidth = {
 			maxWidth: width + '%',
-			textAlign: 'center',
-			verticalAlign: 'bottom',
 			width: width + '%',
 		};
-		const headerText = {
-			display: 'block',
-			overflow: 'hidden',
-			textOverflow: 'ellipsis',
-			whiteSpace: 'nowrap',
+		width = width * 2;
+		const datesWidth = {
+			maxWidth: width + '%',
+			width: width + '%',
 		};
-		const dateLabel = {
-			display: 'block',
-			lineHeight: '16px',
-			overflow: 'hidden',
-			padding: '3px 10px',
-			textOverflow: 'ellipsis',
-			verticalAlign: 'middle',
-			whiteSpace: 'nowrap',
-		};
-		const yearRowStyle = {
-			color: 'var(--ee-grey-white)',
-			fontWeight: 'bold',
-			letterSpacing: '3px',
-			lineHeight: '16px',
-			padding: '10px 10px 1px 10px',
-			textAlign: 'left',
-			verticalAlign: 'middle',
-			width: '100%',
-		};
-		// const container = {
-		// 	boxSizing: 'border-box',
-		// 	padding: '25px',
-		// 	minWidth: '600px',
-		// };
-		const tbl = {
-		};
-		const cell = {
-			textAlign: 'center',
-		};
-		const btn = {
-			margin: '0 auto',
-		};
-		const odd = {
-			background: 'var(--ee-not-quite-white)',
-		};
-		const showDatesColumn = dates.length > 1;
-		const datesHeader = showDatesColumn ?
-			( <td key={ 0 } style={ header0 }></td> ) :
-			null;
+		const datesHeader = dateCount > 1 ? (
+			<td key={ 0 } className="ee-dtm-dates-header" style={ datesWidth } />
+		) : null;
 		const submitButton = (
 			<FormSubmitButton
 				onClick={
@@ -298,12 +518,6 @@ export class DatesAndTicketsManager extends Component {
 				}
 			/>
 		);
-		const getRowStyle = ( index ) => ( index - 1 ) % 2 === 1 ?
-			odd :
-			{};
-		let year = 0;
-		let yearRow = null;
-		let indexMod = 0;
 		return (
 			<Fragment>
 				<FormPlaceholder
@@ -316,194 +530,27 @@ export class DatesAndTicketsManager extends Component {
 				<FormContainer loading={ loading } >
 					<FormWrapper>
 						<FormSection>
-							<table style={ tbl }>
+							<table className="ee-date-tickets-assignments-manager">
 								<thead>
 									<tr>
 										{ datesHeader }
 										{
-											tickets.map(
-												( ticket, index1 ) => {
-													if ( ! isModelEntityOfModel(
-														ticket,
-														TICKET
-													) ) {
-														return null;
-													}
-													return (
-														<td key={ index1 } style={ header }>
-															<span style={ headerText }>
-																{ `${ ticket.name }` }
-																<br />
-																{ `$${ ticket.price }` }
-															</span>
-														</td>
-													);
-												}
+											this.ticketHeaders(
+												tickets,
+												cellWidth
 											)
 										}
 									</tr>
 								</thead>
 								<tbody>
 									{
-										dates.map(
-											( eventDate, index2 ) => {
-												if ( ! isModelEntityOfModel(
-													eventDate,
-													DATETIME
-												) ) {
-													return null;
-												}
-												index2 = index2 + indexMod + 1;
-												const dateYear = parseInt(
-													eventDate.start.toFormat( 'YYYY' )
-												);
-												if ( showDatesColumn && dateYear > year ) {
-													year = dateYear;
-													yearRow = (
-														<tr style={ getRowStyle( index2 ) } >
-															<td
-																key={ 0 }
-																colSpan={ 2 }
-																style={ yearRowStyle }
-															>
-																<div>
-																	{ year }
-																</div>
-															</td>
-														</tr>
-													);
-													indexMod++;
-													index2++;
-												} else {
-													yearRow = null;
-												}
-												// console.log( '' );
-												// console.log( 'DatesAndTicketsManager.render(293)' );
-												// console.log( ' > eventDate: ', eventDate );
-												const eventDateTickets = eventDateTicketMap[
-													eventDate.id
-												] ?
-													eventDateTicketMap[ eventDate.id ] :
-													[];
-												const rowStyle = getRowStyle( index2 );
-												const dateHeader = showDatesColumn ?
-													(
-														<td key={ 0 } style={ dateLabel }>
-															<div style={ { display: 'inline-block' } }>
-																<CalendarPageDate
-																	startDate={
-																		eventDate.start
-																	}
-																	statusClass={
-																		getBackgroundColorClass(
-																			eventDate
-																		)
-																	}
-																	size={ 'tiny' }
-																	style={ {
-																		border: '1px solid var(--ee-off-white)',
-																		display: 'inline-block',
-																		margin: '1px 10px 1px 0',
-																	} }
-																/>
-															</div>
-															<span
-																style={ {
-																	display: 'inline-block',
-																	fontSize: '16px',
-																	position: 'relative',
-																	top: '-8px',
-																} }>
-																{ eventDate.name }
-															</span>
-														</td>
-													) :
-													null;
-												return (
-													<Fragment key={ index2 }>
-														{ yearRow }
-														<tr style={ rowStyle }>
-															{ dateHeader }
-															{
-																tickets.map(
-																	( ticket, index3 ) => {
-																		index3++;
-																		const hasTicket = findIndex(
-																			eventDateTickets,
-																			{ id: ticket.id }
-																		) > noIndex;
-																		const isAssigned = this.isAssigned(
-																			this.state.assigned,
-																			eventDate,
-																			ticket,
-																			true
-																		);
-																		const isRemoved = this.isRemoved(
-																			this.state.removed,
-																			eventDate,
-																			ticket,
-																			true
-																		);
-																		const icon = ( hasTicket && isRemoved === noIndex ) ||
-																		isAssigned > noIndex ?
-																			'tickets-alt' :
-																			'no-alt';
-																		let style = btn;
-																		if ( ! hasTicket && isAssigned > noIndex ) {
-																			style = {
-																				...style,
-																				...{ color: 'var(--ee-brite-green)' },
-																			};
-																		}
-																		if ( hasTicket && isRemoved > noIndex ) {
-																			style = {
-																				...style,
-																				...{ color: 'var(--ee-red)' },
-																			};
-																		}
-																		const action = ( hasTicket && isRemoved === noIndex ) ||
-																		isAssigned > noIndex ?
-																			this.removeTicket :
-																			this.assignTicket;
-																		return (
-																			<td key={ index3 }
-																				style={ cell }
-																			>
-																				<IconButton
-																					icon={ icon }
-																					style={ style }
-																					onClick={
-																						( event ) => {
-																							event.preventDefault();
-																							event.stopPropagation();
-																							action(
-																								eventDate,
-																								ticket
-																							);
-																						}
-																					}
-																					onKeyDown={
-																						( event ) => {
-																							if ( event.keyCode === ENTER ) {
-																								event.preventDefault();
-																								event.stopPropagation();
-																								action(
-																									eventDate,
-																									ticket
-																								);
-																							}
-																						}
-																					}
-																				/>
-																			</td>
-																		);
-																	}
-																)
-															}
-														</tr>
-													</Fragment>
-												);
-											}
+										this.dateRows(
+											dates,
+											tickets,
+											eventDateTicketMap,
+											dateCount,
+											datesWidth,
+											cellWidth
 										)
 									}
 								</tbody>
