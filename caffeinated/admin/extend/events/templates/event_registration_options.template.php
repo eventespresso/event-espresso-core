579bf081f12c978e7b2417cac1a8bf7dff80a5a3
Josh Feck <josh@nxnweb.com>
2019-03-14 15:22:05 -0400

    Add escaping

--- caffeinated/admin/extend/events/templates/event_registration_options.template.php
+++ caffeinated/admin/extend/events/templates/event_registration_options.template.php
@@ -1,5 +1,5 @@
 <p>
-    <label><?php _e('Active Status: ', 'event_espresso'); ?></label>
+    <label><?php esc_html_e('Active Status: ', 'event_espresso'); ?></label>
     <?php echo $active_status; ?>
 </p>
 
@@ -7,7 +7,7 @@
 $settings_array = array(
     'max_registrants'                 => '<p>
         <label for="max-registrants">'
-                                         . __(
+                                         . esc_html__(
                                              'Maximum number of tickets allowed per order for this event: ',
                                              'event_espresso'
                                          ) . '</label>
@@ -15,18 +15,18 @@ $settings_array = array(
         </p>',
     'additional_registration_options' => $additional_registration_options,
     'display_ticket_selector'         => '<p>
-            <label>' . __('Display Ticket Selector', 'event_espresso') . '</label>' . $display_ticket_selector . '</p>',
+            <label>' . esc_html__('Display Ticket Selector', 'event_espresso') . '</label>' . $display_ticket_selector . '</p>',
     'alternative_registration_page'   => '<p>
-            <label>' . __('Alternative Registration Page', 'event_espresso') . '</label>
+            <label>' . esc_html__('Alternative Registration Page', 'event_espresso') . '</label>
             <input name="externalURL" size="20" type="text" value="' . $_event->external_url() . '"> 
             </p>',
     'event_phone_number'              => '<p>
-            <label>' . __('Event Phone Number', 'event_espresso') . '</label>
+            <label>' . esc_html__('Event Phone Number', 'event_espresso') . '</label>
             <input name="event_phone" size="20" type="text" value="' . $_event->phone() . '">
             </p>',
     'default_registration_status'     => '<p>
             <label>'
-                                         . __(
+                                         . esc_html__(
                                              'Default Registration Status',
                                              'event_espresso'
                                          ) . '</label>' 
