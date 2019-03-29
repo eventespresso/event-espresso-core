5c8b57a64e064b665692584545ebb2110314871e
Josh Feck <josh@nxnweb.com>
2019-03-29 15:00:43 -0400

    use aria-label instead of title for Event Category table actions

--- admin_pages/events/Event_Categories_Admin_List_Table.class.php
+++ admin_pages/events/Event_Categories_Admin_List_Table.class.php
@@ -103,16 +103,24 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table
         $edit_link = EE_Admin_Page::add_query_args_and_nonce($edit_query_args, EVENTS_ADMIN_URL);
         $delete_link = EE_Admin_Page::add_query_args_and_nonce($delete_query_args, EVENTS_ADMIN_URL);
 
-        $actions = array(
-            'edit' => '<a href="' . $edit_link . '" aria-label="' . esc_attr__(
-                'Edit Category',
-                'event_espresso'
-            ) . '">' . esc_html__('Edit', 'event_espresso') . '</a>',
-        );
+        $term_name = $item->get_first_related('Term')->get('name');
 
-        $actions['delete'] = '<a href="' . $delete_link . '" aria-label="' . esc_attr__(
-            'Delete Category',
-            'event_espresso'
+        $actions['edit'] = '<a href="' . $edit_link . '" aria-label="' . sprintf(
+			/* translators: The name of the event category */
+			esc_attr__(
+				'Edit Category (%s)',
+				'event_espresso'
+			),
+			$term_name
+		) . '">' . esc_html__('Edit', 'event_espresso') . '</a>';
+
+        $actions['delete'] = '<a href="' . $delete_link . '" aria-label="' . sprintf(
+            /* translators: The name of the event category */
+            esc_attr__(
+                'Delete Category (%s)',
+                'event_espresso'
+            ),
+            $term_name
         ) . '">' . esc_html__('Delete', 'event_espresso') . '</a>';
 
         $actions['view'] = sprintf(
@@ -128,9 +136,7 @@ class Event_Categories_Admin_List_Table extends EE_Admin_List_Table
             esc_html__('View', 'event_espresso')
         );
 
-        $content = '<strong><a class="row-title" href="' . $edit_link . '">' . $item->get_first_related('Term')->get(
-            'name'
-        ) . '</a></strong>';
+        $content = '<strong><a class="row-title" href="' . $edit_link . '">' . $term_name . '</a></strong>';
         $content .= $this->row_actions($actions);
         return $content;
     }
