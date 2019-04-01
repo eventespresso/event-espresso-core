f174e4e395771ac2badc4aaf8161ff41f9ad60f4
Brent Christensen <brent@eventespresso.com>
2019-04-01 10:14:20 -0700

    simplify formData and add priceIDs & priceTypes

--- assets/src/editor/events/tickets/editor-ticket/price-calculator/ticket-price-calculator-form-data-map.js
+++ assets/src/editor/events/tickets/editor-ticket/price-calculator/ticket-price-calculator-form-data-map.js
@@ -1,7 +1,7 @@
 /**
  * External imports
  */
-import { __ } from '@eventespresso/i18n';
+import { isArray, isEmpty, uniq } from 'lodash';
 import { isModelEntityOfModel } from '@eventespresso/validators';
 
 export const TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX = 'ee-ticket-price-calculator';
@@ -13,14 +13,11 @@ export const TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX = 'ee-ticket-price-calcul
  * @return {Object} form data
  */
 const buildTicketDataMap = ( formData, ticket ) => {
-	let prefix = TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX;
-	prefix += '-ticket-' + ticket.id;
-	formData[ `${ prefix }-type` ] = __( 'Base Price', 'event_espresso' );
-	formData[ `${ prefix }-name` ] = ticket.name || '';
-	formData[ `${ prefix }-desc` ] = ticket.description || '';
-	formData[ `${ prefix }-price` ] = ticket.price &&
-		ticket.price.amount ? ticket.price.amount.toNumber() : null;
-	formData[ `${ prefix }-taxable` ] = ticket.taxable;
+	formData.ticketID = ticket.id;
+	formData.ticketIsTaxable = ticket.taxable;
+	formData.ticketTotal = ticket.price && ticket.price.amount ?
+		ticket.price.amount.toNumber() :
+		null;
 	return formData;
 };
 
@@ -32,25 +29,54 @@ const buildTicketDataMap = ( formData, ticket ) => {
  * @return {Object} form data
  */
 const buildPricesDataMap = ( formData, ticket, prices ) => {
-	if ( ! Array.isArray( prices ) || prices.length === 0
+	if ( ! isArray( prices ) || prices.length === 0
 	) {
 		return {};
 	}
 	let prefix = TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX;
 	prefix += '-ticket-' + ticket.id + '-price';
+	const priceIDs = [];
+	const priceTypes = [];
 	for ( let i = 0; i < prices.length; i++ ) {
 		const price = prices[ i ];
 		if ( isModelEntityOfModel( price, 'price' ) ) {
-			formData[ `${ prefix }-${ price.id }-type` ] = price.prtId || 0;
-			formData[ `${ prefix }-${ price.id }-name` ] = price.name || '';
-			formData[ `${ prefix }-${ price.id }-desc` ] = price.desc || '';
-			formData[ `${ prefix }-${ price.id }-amount` ] = price.amount &&
+			const priceId = shortenCuid( price.id );
+			priceIDs.push( priceId );
+			priceTypes.push( price.prtId );
+			const pricePrefix = `${ prefix }-${ priceId }`;
+			formData[ `${ pricePrefix }-id` ] = priceId;
+			formData[ `${ pricePrefix }-type` ] = parseInt( price.prtId );
+			formData[ `${ pricePrefix }-name` ] = price.name || '';
+			formData[ `${ pricePrefix }-desc` ] = price.desc || '';
+			formData[ `${ pricePrefix }-amount` ] = price.amount &&
 			price.amount.amount ?
 				price.amount.amount.toNumber() :
-				null;
+				0;
+			if ( price.prtId === 1 ) {
+				formData.ticketTotal = formData[ `${ pricePrefix }-amount` ];
+			}
 		}
 	}
+	if ( isArray( priceIDs ) && ! isEmpty( priceIDs ) ) {
+		formData.priceIDs = uniq( priceIDs ).join();
+	}
+	if ( isArray( priceTypes ) && ! isEmpty( priceTypes ) ) {
+		formData.priceTypes = uniq( priceTypes ).join();
+	}
 	return formData;
+};
+
+/**
+ * @function
+ * @param {string} cuid
+ * @return {string} cuid snippet
+ */
+export const shortenCuid = ( cuid ) => {
+	if ( cuid.hasOwnProperty( 'length' ) && cuid.length > 18 ) {
+		// use a smaller more unique portion of the CUID
+		return cuid.substring( 12, 18 );
+	}
+	return cuid;
 };
 
 /**
