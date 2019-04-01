ecbd6a6583b20f13c60e84af0b80a6b868bb9dac
Brent Christensen <brent@eventespresso.com>
2019-04-01 10:09:10 -0700

    sort alphabetically

--- assets/src/data/model/models.js
+++ assets/src/data/model/models.js
@@ -1,16 +1,18 @@
+import * as attendeeModel from './attendee';
+import * as checkInModel from './checkin';
 import * as dateTimeModel from './datetime';
 import * as eventModel from './event';
+import * as priceTypeModel from './price-type';
 import * as registrationModel from './registration';
 import * as statusModel from './status';
 import * as ticketModel from './ticket';
-import * as checkInModel from './checkin';
-import * as attendeeModel from './attendee';
 export {
+	attendeeModel,
 	checkInModel,
 	dateTimeModel,
 	eventModel,
+	priceTypeModel,
 	registrationModel,
 	statusModel,
 	ticketModel,
-	attendeeModel,
 };
