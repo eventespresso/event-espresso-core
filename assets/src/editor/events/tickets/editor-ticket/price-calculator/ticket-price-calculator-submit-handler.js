6095e7f6008175b291a9e02cc405cfcf16687e0e
Brent Christensen <brent@eventespresso.com>
2019-04-05 19:22:30 -0700

    ticketPriceCalculatorSubmitHandler - updates ticket prices

---
+++ assets/src/editor/events/tickets/editor-ticket/price-calculator/ticket-price-calculator-submit-handler.js
@@ -0,0 +1,71 @@
+/**
+ * External imports
+ */
+import { isArray } from 'lodash';
+import { isModelEntityOfModel } from '@eventespresso/validators';
+import { Money, SiteCurrency } from '@eventespresso/value-objects';
+
+/**
+ * Internal dependencies
+ */
+import { updatePrice } from '../action-handlers/update-price';
+import { updateTicket } from '../action-handlers/update-ticket';
+import {
+	shortenCuid,
+	TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX,
+} from './ticket-price-calculator-form-data-map';
+
+/**
+ * updates Ticket and Price Entity properties given the supplied form data
+ *
+ * @function
+ * @param {Object} ticket
+ * @param {Array} prices
+ * @param {Object} formData
+ * @return {Promise} updated ticketEntity upon resolution
+ */
+export const ticketPriceCalculatorSubmitHandler = async (
+	ticket,
+	prices,
+	formData
+) => {
+	console.log( '' );
+	console.log( 'ticketPriceCalculatorSubmitHandler()' );
+	console.log( ' > ticketEntity: ', ticket );
+	console.log( ' > prices: ', prices );
+	console.log( ' > formData: ', formData );
+	if ( ! isModelEntityOfModel( ticket, 'ticket' ) ) {
+		return null;
+	}
+	ticket.price = new Money(
+		formData.ticketTotal || 0,
+		SiteCurrency
+	);
+	// if ( ! isArray( prices ) || prices.length === 0
+	// ) {
+	// 	return {};
+	// }
+	let prefix = TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX;
+	prefix += '-ticket-' + ticket.id + '-price';
+	for ( let i = 0; i < prices.length; i++ ) {
+		let price = prices[ i ];
+		if ( isModelEntityOfModel( price, 'price' ) ) {
+			const priceId = shortenCuid( price.id );
+			const pricePrefix = `${ prefix }-${ priceId }`;
+			if ( price.id === formData[ `${ pricePrefix }-id` ] ) {
+				price.prtId = parseInt( formData[ `${ pricePrefix }-type` ] );
+				price.name = formData[ `${ pricePrefix }-name` ] || '';
+				price.desc = formData[ `${ pricePrefix }-desc` ] || '';
+				price.amount = new Money(
+					formData[ `${ pricePrefix }-amount` ] || 0,
+					SiteCurrency
+				);
+				price = await updatePrice( price, ticket );
+				console.log( 'price', price );
+			}
+		}
+	}
+	ticket = await updateTicket( ticket );
+	console.log( 'ticket', ticket );
+	return true;
+};
