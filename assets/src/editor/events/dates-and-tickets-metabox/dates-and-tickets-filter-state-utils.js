2c271a4de49928cbb0db569e97fc8da747616ce6
Brent Christensen <brent@eventespresso.com>
2019-04-10 17:55:06 -0700

    drop buildEventDateTicketRelationsMap, simplify getDatetimeEntityIds

--- assets/src/editor/events/dates-and-tickets-metabox/dates-and-tickets-filter-state-utils.js
+++ assets/src/editor/events/dates-and-tickets-metabox/dates-and-tickets-filter-state-utils.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { isEmpty, isUndefined, uniq } from 'lodash';
+import { isUndefined, uniq } from 'lodash';
 import { isModelEntityOfModel } from '@eventespresso/validators';
 
 /**
@@ -25,91 +25,12 @@ export const condenseArray = ( a ) => uniq( a.filter( removeUndefined ) );
  */
 export const getDatetimeEntityIds = ( datetimeEntities ) => {
 	const datetimeEntityIds = [];
-	if ( Array.isArray( datetimeEntities ) && ! isEmpty( datetimeEntities ) ) {
-		for ( let i = 0; i < datetimeEntities.length; i++ ) {
-			const datetimeEntity = datetimeEntities[ i ];
-			if (
-				datetimeEntity &&
-				isModelEntityOfModel( datetimeEntity, 'datetime' ) &&
-				datetimeEntity.id
-			) {
-				datetimeEntityIds.push( datetimeEntity.id );
+	if ( Array.isArray( datetimeEntities ) ) {
+		datetimeEntities.forEach( ( eventDate ) => {
+			if ( isModelEntityOfModel( eventDate, 'datetime' ) ) {
+				datetimeEntityIds.push( eventDate.id );
 			}
-		}
+		} );
 	}
 	return condenseArray( datetimeEntityIds );
-};
-
-/**
- * generates a multidimensional array
- * where the first level keys are datetime IDs
- * and the values are arrays of each date's related ticket entities
- *
- * @function
- * @param {Array} eventDateIds
- * @param {Array} ticketEntities
- * @param {Array} datetimeTicketEntities
- * @return {Array} Event Date Ticket Relations Map
- */
-export const buildEventDateTicketRelationsMap = (
-	eventDateIds,
-	ticketEntities,
-	datetimeTicketEntities
-) => {
-	const relationsMap = {};
-	if (
-		Array.isArray( eventDateIds ) &&
-		! isEmpty( eventDateIds ) &&
-		Array.isArray( ticketEntities ) &&
-		! isEmpty( ticketEntities ) &&
-		Array.isArray( datetimeTicketEntities ) &&
-		! isEmpty( datetimeTicketEntities )
-	) {
-		for ( let j = 0; j < datetimeTicketEntities.length; j++ ) {
-			const relation = datetimeTicketEntities[ j ];
-			if (
-				relation &&
-				isModelEntityOfModel(
-					relation,
-					'datetime_ticket'
-				) &&
-				relation.DTT_ID &&
-				relation.TKT_ID
-			) {
-				if ( isEmpty( relationsMap[ relation.DTT_ID ] ) ) {
-					relationsMap[ relation.DTT_ID ] = [];
-				}
-				relationsMap[ relation.DTT_ID ].push(
-					relation.TKT_ID
-				);
-			}
-		}
-		if ( ! isEmpty( relationsMap ) ) {
-			for ( let x = 0; x < eventDateIds.length; x++ ) {
-				const eventDateId = eventDateIds[ x ];
-				const ticketIds = relationsMap[ eventDateId ];
-				if ( ! isEmpty( ticketIds ) ) {
-					for ( let y = 0; y < ticketIds.length; y++ ) {
-						const ticketId = ticketIds[ y ];
-						if ( ticketId ) {
-							for ( let z = 0; z < ticketEntities.length; z++ ) {
-								const ticketEntity = ticketEntities[ z ];
-								if (
-									isModelEntityOfModel(
-										ticketEntity,
-										'ticket'
-									) &&
-									ticketId === ticketEntity.id
-								) {
-									relationsMap[ eventDateId ][ y ] =
-										ticketEntity;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return relationsMap;
 };
