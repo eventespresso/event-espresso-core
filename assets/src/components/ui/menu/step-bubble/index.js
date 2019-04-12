af316a7c0b7f94689ed7010d6279562c43b6f54a
Brent Christensen <brent@eventespresso.com>
2019-04-12 12:14:46 -0700

    get bubble props from bubbleData, get slugs from bubbleData keys

--- assets/src/components/ui/menu/step-bubble/index.js
+++ assets/src/components/ui/menu/step-bubble/index.js
@@ -7,28 +7,16 @@ import StepBubble from './step-bubble';
 /**
  * External imports
  */
-import { isArray } from 'lodash';
 import PropTypes from 'prop-types';
 import { sprintf } from '@eventespresso/i18n';
 
 /**
  * A helper function for asserting a StepConfiguration object is valid.
  *
- * @param {string} slug
  * @param {string} label
  * @param {*} value
  */
-const assertStepConfiguration = ( { slug, label, value } ) => {
-	if ( typeof slug !== 'string' ) {
-		throw new TypeError(
-			sprintf(
-				'Step bubble configuration object requires a string for the ' +
-				'%1$s argument. %2$s was provided',
-				'slug',
-				slug
-			)
-		);
-	}
+const assertStepConfiguration = ( { label, value } ) => {
 	if ( typeof label !== 'string' ) {
 		throw new TypeError(
 			sprintf(
@@ -51,49 +39,50 @@ const assertStepConfiguration = ( { slug, label, value } ) => {
 	}
 };
 
-const StepBubbleMenu = ( {
-	bubbleClick,
-	bubbleData,
-	clickable,
-	activeBubble
-} ) => {
+const getStepBubbles = ( bubbleData, activeBubble ) => {
+	const stepBubbles = [];
+	for ( const slug in bubbleData ) {
+		if ( bubbleData.hasOwnProperty( slug ) ) {
+			assertStepConfiguration( bubbleData[ slug ] );
+			const {
+				label,
+				value,
+				action,
+				clickable,
+			} = bubbleData[ slug ];
+			stepBubbles.push(
+				<StepBubble
+					key={ slug }
+					label={ label }
+					slug={ slug }
+					action={ action }
+					stepValue={ value }
+					clickable={ clickable }
+					isActive={ activeBubble === slug }
+				/>
+			);
+		}
+	}
+	return stepBubbles;
+};
+
+const StepBubbleMenu = ( { bubbleData, activeBubble } ) => {
 	return (
 		<div className="ee-step-bubble-menu-container">
 			<div className="step-bubbles-container" role="menu" tabIndex={ 1 }>
-			{
-				bubbleData.map( ( bubble ) => {
-					assertStepConfiguration( bubble );
-					const { slug, label, value } = bubble;
-					const canClick = clickable.indexOf( bubble.slug ) > -1;
-					return (
-						<StepBubble
-							key={ slug }
-							label={ label }
-							slug={ slug }
-							stepValue={ value }
-							isActive={ activeBubble === slug }
-							canClick={ canClick }
-							bubbleClick={ bubbleClick }
-						/>
-					);
-				} )
-			}
+			{ getStepBubbles( bubbleData, activeBubble ) }
 			</div>
 		</div>
 	);
 };
 
 StepBubbleMenu.propTypes = {
-	bubbleClick: PropTypes.func,
-	bubbleData: PropTypes.array,
-	clickable: PropTypes.array,
+	bubbleData: PropTypes.object,
 	activeBubble: PropTypes.string,
 };
 
 StepBubbleMenu.defaultProps = {
-	bubbleClick: () => null,
-	bubbleData: [],
-	clickable: [],
+	bubbleData: {},
 	activeBubble: '',
 };
 
