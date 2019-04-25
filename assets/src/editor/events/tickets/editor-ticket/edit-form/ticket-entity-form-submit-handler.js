c6b69e8aa1cd2dba71c6766e713eecd71d081dd9
Brent Christensen <brent@eventespresso.com>
2019-04-25 13:22:42 -0700

    code cleanup

--- assets/src/editor/events/tickets/editor-ticket/edit-form/ticket-entity-form-submit-handler.js
+++ assets/src/editor/events/tickets/editor-ticket/edit-form/ticket-entity-form-submit-handler.js
@@ -1,48 +1,45 @@
 /**
  * External imports
  */
-import { ticketModel } from '@eventespresso/model';
-import { DateTime, Money, SiteCurrency } from '@eventespresso/value-objects';
 import { isModelEntityOfModel } from '@eventespresso/validators';
+import { DateTime, Money, SiteCurrency } from '@eventespresso/value-objects';
 
 /**
  * Internal dependencies
  */
 import { updateTicket } from '../action-handlers/update-ticket';
 
-const { MODEL_NAME: TICKET } = ticketModel;
-
 /**
  * updates Ticket Entity properties given the supplied form data
  *
  * @function
- * @param {Object} ticketEntity
+ * @param {Object} ticket
  * @param {Object} formData
- * @return {Promise} updated ticketEntity upon resolution
+ * @return {Object} updated ticket
  */
-export const ticketEntityFormSubmitHandler = async ( ticketEntity, formData ) => {
-	if ( ! isModelEntityOfModel( ticketEntity, TICKET ) ) {
+export const ticketEntityFormSubmitHandler = async ( { ticket, formData } ) => {
+	if ( ! isModelEntityOfModel( ticket, 'ticket' ) ) {
 		return null;
 	}
-	const prefix = `ee-ticket-${ ticketEntity.id }`;
-	ticketEntity.name = formData[ `${ prefix }-name` ];
-	ticketEntity.description = formData[ `${ prefix }-description` ];
-	ticketEntity.qty = parseInt( formData[ `${ prefix }-qty` ] || -1 );
-	ticketEntity.uses = parseInt( formData[ `${ prefix }-uses` ] || -1 );
-	ticketEntity.required = !! formData[ `${ prefix }-required` ];
-	ticketEntity.min = parseInt( formData[ `${ prefix }-min` ] || 0 );
-	ticketEntity.max = parseInt( formData[ `${ prefix }-max` ] || -1 );
-	ticketEntity.price = new Money(
+	const prefix = `ee-ticket-${ ticket.id }`;
+	ticket.name = formData[ `${ prefix }-name` ];
+	ticket.description = formData[ `${ prefix }-description` ];
+	ticket.qty = parseInt( formData[ `${ prefix }-qty` ] || -1 );
+	ticket.uses = parseInt( formData[ `${ prefix }-uses` ] || -1 );
+	ticket.required = !! formData[ `${ prefix }-required` ];
+	ticket.min = parseInt( formData[ `${ prefix }-min` ] || 0 );
+	ticket.max = parseInt( formData[ `${ prefix }-max` ] || -1 );
+	ticket.price = new Money(
 		formData[ `${ prefix }-price` ] || 0,
 		SiteCurrency
 	);
-	ticketEntity.startDate = new DateTime( formData[ `${ prefix }-start` ] );
-	ticketEntity.endDate = new DateTime( formData[ `${ prefix }-end` ] );
-	ticketEntity.taxable = !! formData[ `${ prefix }-taxable` ];
-	ticketEntity.order = parseInt( formData[ `${ prefix }-order` ] );
-	ticketEntity.isDefault = !! formData[ `${ prefix }-is-default` ];
-	ticketEntity.wpUser = parseInt( formData[ `${ prefix }-wp-user` ] || 0 );
-	ticketEntity.parent = parseInt( formData[ `${ prefix }-parent` ] || 0 );
-	ticketEntity.deleted = !! formData[ `${ prefix }-deleted` ];
-	return updateTicket( ticketEntity );
+	ticket.startDate = new DateTime( formData[ `${ prefix }-start` ] );
+	ticket.endDate = new DateTime( formData[ `${ prefix }-end` ] );
+	ticket.taxable = !! formData[ `${ prefix }-taxable` ];
+	ticket.order = parseInt( formData[ `${ prefix }-order` ] );
+	ticket.isDefault = !! formData[ `${ prefix }-is-default` ];
+	ticket.wpUser = parseInt( formData[ `${ prefix }-wp-user` ] || 0 );
+	ticket.parent = parseInt( formData[ `${ prefix }-parent` ] || 0 );
+	ticket.deleted = !! formData[ `${ prefix }-deleted` ];
+	return await updateTicket( ticket );
 };
