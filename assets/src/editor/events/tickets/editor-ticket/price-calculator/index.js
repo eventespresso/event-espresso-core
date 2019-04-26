5acb60f154a3d260255e1fb2160cfd15f5c297b3
Brent Christensen <brent@eventespresso.com>
2019-04-26 12:53:17 -0700

    add index file for ticket price calculator

---
+++ assets/src/editor/events/tickets/editor-ticket/price-calculator/index.js
@@ -0,0 +1,29 @@
+export {
+	TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX,
+} from './constants';
+export {
+	getPriceTypeObjects,
+	parseMoneyValue,
+	calculateTicketBasePrice,
+	calculateTicketPrices,
+	ticketPriceCalculator,
+} from './ticket-price-calculator';
+export {
+	default as TicketPriceCalculatorForm,
+} from './ticket-price-calculator-form';
+export {
+	shortenCuid,
+	ticketPriceCalculatorFormDataMap,
+} from './ticket-price-calculator-form-data-map';
+export {
+	default as TicketPriceCalculatorFormModal,
+} from './ticket-price-calculator-form-modal';
+export {
+	ticketPriceCalculatorMenuItem,
+} from './ticket-price-calculator-menu-item';
+export {
+	ticketPriceCalculatorSubmitHandler,
+} from './ticket-price-calculator-submit-handler';
+export {
+	default as withTicketPriceCalculator,
+} from './with-ticket-price-calculator';
