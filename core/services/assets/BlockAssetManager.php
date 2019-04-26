e8df38755fbd076e742788d3502b6fd9cd67a027
Darren Ethier <darren@roughsmootheng.in>
2019-04-26 08:29:10 -0400

    ensure `wp-block-editor` is included as dependencies in script registration

--- core/services/assets/BlockAssetManager.php
+++ core/services/assets/BlockAssetManager.php
@@ -254,6 +254,7 @@ abstract class BlockAssetManager extends AssetManager implements BlockAssetManag
                 'wp-i18n',      // Provides localization functions
                 'wp-element',   // Provides React.Component
                 'wp-components', // Provides many prebuilt components and controls
+                'wp-block-editor', // block editor components
                 CoreAssetManager::JS_HANDLE_EDITOR_HOCS,
                 $this->getScriptHandle(),
             );
