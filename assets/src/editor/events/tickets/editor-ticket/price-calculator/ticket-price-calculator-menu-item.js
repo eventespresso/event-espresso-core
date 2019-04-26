ddeb61a3d67f79357f662008e397f08e914bcb3f
Brent Christensen <brent@eventespresso.com>
2019-04-26 12:57:53 -0700

    ticketPriceCalculatorMenuItem extracted from EditorTicketActionsMenu

---
+++ assets/src/editor/events/tickets/editor-ticket/price-calculator/ticket-price-calculator-menu-item.js
@@ -0,0 +1,35 @@
+/**
+ * External imports
+ */
+import { EspressoIcon, IconMenuItem } from '@eventespresso/components';
+import { __ } from '@eventespresso/i18n';
+import { isModelEntityOfModel } from '@eventespresso/validators';
+
+/**
+ * @function ticketPriceCalculatorMenuItem
+ * @param {Object} ticket JSON object defining the Ticket
+ * @param {Function} toggleCalculator
+ * @param {boolean} noBasePrice
+ * @return {IconMenuItem}    View Tickets for Ticket IconMenuItem
+ */
+export const ticketPriceCalculatorMenuItem = (
+	ticket,
+	toggleCalculator,
+	noBasePrice = false
+) => {
+	const tooltip = noBasePrice ?
+		__( 'warning! no ticket price set - click to fix', 'event_espresso' ) :
+		__( 'ticket price calculator', 'event_espresso' );
+	return isModelEntityOfModel( ticket, 'ticket' ) ? (
+		<IconMenuItem
+			index={ 2 }
+			tooltip={ tooltip }
+			id={ `calculate-ticket-price-ticket-${ ticket.id }` }
+			htmlClass={ 'calculate-tickets-price' }
+			dashicon={ <EspressoIcon icon="calculator" /> }
+			tooltipPosition="top right"
+			onClick={ toggleCalculator }
+			itemCount={ noBasePrice ? 0 : null }
+		/>
+	) : null;
+};
