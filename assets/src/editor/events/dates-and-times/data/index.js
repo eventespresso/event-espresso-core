66b0a944c5a9b0e4f51fe7dd7227df839a7d72b9
Brent Christensen <brent@eventespresso.com>
2019-04-26 18:31:55 -0700

    export data HOCs

---
+++ assets/src/editor/events/dates-and-times/data/index.js
@@ -0,0 +1,5 @@
+export { withDatetimeTickets } from './with-datetime-tickets';
+export { withTicketsForAllEventDates } from './with-tickets-for-all-event-dates';
+export {
+	withGetRelatedTicketsForEventDates,
+} from './with-get-related-tickets-for-event-dates';
