8d28c4055b91efc30d5062af05f3828f04bb9e86
Josh Feck <josh@nxnweb.com>
2019-05-08 16:53:44 -0400

    remove errant p tags and html comment

--- admin_pages/venues/help_tabs/venues_overview_table_column_headings.help_tab.php
+++ admin_pages/venues/help_tabs/venues_overview_table_column_headings.help_tab.php
@@ -1,5 +1,4 @@
 <p><strong><?php _e('Venues Overview Table Column Headings', 'event_espresso'); ?></strong></p>
-<p>
 <ul>
 <li>
 <strong><?php _e('ID', 'event_espresso'); ?></strong><br />
@@ -21,10 +20,4 @@
 <strong><?php _e('Capacity', 'event_espresso'); ?></strong><br />
 <?php _e('This is the capacity of the venue. If no limit is set, then the capacity will be set to infinity (&#8734).', 'event_espresso'); ?>
 </li>
-<!--
-<li>
-<strong><?php _e('Shortcode', 'event_espresso'); ?></strong><br />
-<?php _e('This shortcode can be used to place a venue on an event page, a WordPress page, or a WordPress post.', 'event_espresso'); ?>
-</li> -->
 </ul>
-</p>
