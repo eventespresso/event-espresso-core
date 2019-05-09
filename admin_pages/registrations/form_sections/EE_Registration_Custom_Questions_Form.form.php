0138d6bdcc3232c8dd74083515b298174351dc4b
Josh Feck <josh@nxnweb.com>
2019-05-09 12:54:46 -0400

    Linting

--- admin_pages/registrations/form_sections/EE_Registration_Custom_Questions_Form.form.php
+++ admin_pages/registrations/form_sections/EE_Registration_Custom_Questions_Form.form.php
@@ -125,12 +125,12 @@ class EE_Registration_Custom_Questions_Form extends EE_Form_Section_Proper
             $parts_of_subsection['edit_link'] = new EE_Form_Section_HTML(
                 EEH_HTML::table(
                     EEH_HTML::tr(
-                       '<th/><td class="reg-admin-edit-attendee-question-td"><a class="reg-admin-edit-attendee-question-lnk" href="#" aria-label="' . esc_attr__('click to edit question', 'event_espresso') . '">
+                        '<th/><td class="reg-admin-edit-attendee-question-td"><a class="reg-admin-edit-attendee-question-lnk" href="#" aria-label="' . esc_attr__('click to edit question', 'event_espresso') . '">
 		  			<span class="reg-admin-edit-question-group-spn">' . __('edit the above question group', 'event_espresso') . '</span>
 		  			<div class="dashicons dashicons-edit"></div>
 		  		</a></td>'
                     ) .
-                    EEH_HTML::no_row('',2)
+                    EEH_HTML::no_row('', 2)
                 )
             );
         }
