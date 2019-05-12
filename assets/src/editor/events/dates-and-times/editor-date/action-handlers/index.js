869aad124cc32604835a09be3ba93e5d7dff1274
Darren Ethier <darren@roughsmootheng.in>
2019-05-11 20:02:46 -0400

    refactor copyEventDate to withCopyEventDate hoc

--- assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
@@ -1,3 +1,3 @@
-export { copyEventDate } from './copy-event-date';
+export { default as withCopyEventDate } from './with-copy-event-date';
 export { default as withUpdateEventDateRelation } from './with-update-event-date-relation';
 export { trashEventDate } from './trash-event-date';
