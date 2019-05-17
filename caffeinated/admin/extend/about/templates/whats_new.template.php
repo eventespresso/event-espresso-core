0de57a94297be2e4b3499f8e081d9abcbcd01a3e
Josh Feck <josh@nxnweb.com>
2019-05-17 12:09:19 -0400

    Clarify wording and update status of Wait List add-on. Add escaping

--- caffeinated/admin/extend/about/templates/whats_new.template.php
+++ caffeinated/admin/extend/about/templates/whats_new.template.php
@@ -1,12 +1,18 @@
 <div class="changelog point-releases">
-    <!-- <h3><?php echo _n('Minor Release Information', 'Minor Releases', 1, 'event_espresso'); ?></h3> -->
-    <h3><?php echo _n('Major Release Information', 'Major Releases', 1, 'event_espresso'); ?></h3>
+    <!-- <h3><?php echo esc_html(
+        _n('Minor Release Information', 'Minor Releases', 1, 'event_espresso')
+    ); ?></h3> -->
+    <h3><?php echo esc_html(
+        _n('Major Release Information', 'Major Releases', 1, 'event_espresso')
+    ); ?></h3>
     <?php // $type = 'minor'; ?>
     <?php $type = 'major'; ?>
     <p><?php
         printf(
-            __('<strong>Version %1$s</strong> is a %2$s release.', 'event_espresso'),
+            esc_html__('%1$sVersion %2$s%3$s is a %4$s release.', 'event_espresso'),
+            '<strong>',
             EVENT_ESPRESSO_VERSION,
+            '</strong>',
             $type
         ); ?>
         <?php
@@ -15,8 +21,9 @@
         $ver = implode('.', $ver);
         ?>
         <?php printf(
-            __('For more information, see <a href="%s" target="_blank">the release notes</a>.', 'event_espresso'),
-            'http://eventespresso.com/wiki/ee4-changelog/#' . $ver
+            esc_html__('For more information, see %1$sthe release notes%2$s.', 'event_espresso'),
+            '<a href="http://eventespresso.com/wiki/ee4-changelog/#' . $ver . '" target="_blank" rel="noopener noreferrer">',
+            '</a>'
         ); ?>
     </p>
 </div>
@@ -28,13 +35,13 @@
         ?>
         <div class="ee-attention">
             <h2 class="ee-maintenance-mode-callout"><?php
-                _e('Event Espresso is in full maintenance mode.', 'event_espresso');
+                esc_html_e('Event Espresso is in full maintenance mode.', 'event_espresso');
                 ?></h2>
             <p>
                 <?php
                 printf(
-                    __(
-                        'A previous version of Event Espresso has detected. But before anything else can happen, we need to know whether or not to migrate (copy over) your existing event data so that it can be utilized by EE4. For more instructions on what to do, please visit the %sEvent Espresso Maintenance%s page.',
+                    esc_html__(
+                        'A previous version of Event Espresso has detected. But before anything else can happen, we need to know whether or not to migrate (copy over) your existing event data so that it can be utilized by EE4. For more instructions on what to do, please visit the %1$sEvent Espresso Maintenance%2$s page.',
                         'event_espresso'
                     ),
                     '<a href="admin.php?page=espresso_maintenance_settings">',
@@ -64,12 +71,12 @@
     <ol>
         <li>Export Personal Data<br><a href="https://wordpress.org/news/2018/05/wordpress-4-9-6-privacy-and-maintenance-release/">WordPress 4.9.6 allows admins to generate a report of an individual’s personal information</a> and send it to them. In Event Espresso 4, we <a href="https://eventespresso.com/features/gdpr-compliant/#export-personal-data">add the individual’s registration details</a> to the report automatically when it’s being created.</li>
         <li>Erase Personal Data<br>Along with the ability to export data, WordPress 4.9.6 adds a tool that allows site admins to erase personal data stored in their WordPress site. When an admin erases an individual’s personal data, <a href="https://eventespresso.com/features/gdpr-compliant/#erase-personal-data">Event Espresso makes sure their registration data is also erased</a>.</li>
-        <li>GDPR/Privacy Policy Content Tool<br>Per GDPR regulations, site owners need to have a Privacy Policy page. By default, WordPress doesn’t collect any data from visitors unless they post a comment. However many plugins add third-party services that collect visitor data. <a href="https://wordpress.org/news/2018/05/wordpress-4-9-6-privacy-and-maintenance-release/">WordPress 4.9.6 adds a Privacy Policy guide</a> to help you create a comprehensive “Privacy Policy” page. Event Espresso 4.9.62 <a href="https://eventespresso.com/features/gdpr-compliant/#privacy-policy-tool">adds suggested text to this guide page</a>, to help you know what Event Espresso is doing with regards to user privacy.</li>
+        <li>GDPR/Privacy Policy Content Tool<br>Per GDPR regulations, site owners need to have a Privacy Policy page. By default, WordPress doesn’t collect any data from visitors unless they post a comment. However, some plugins add third-party services that collect visitor data. <a href="https://wordpress.org/news/2018/05/wordpress-4-9-6-privacy-and-maintenance-release/">WordPress 4.9.6 adds a Privacy Policy guide</a> to help you create a comprehensive “Privacy Policy” page. Event Espresso 4.9.62 <a href="https://eventespresso.com/features/gdpr-compliant/#privacy-policy-tool">adds suggested text to this guide page</a>, to help you know what Event Espresso is doing with regards to user privacy.</li>
     </ol>
     <p align="right"><a href="https://eventespresso.com/2018/05/gdpr-registration-data-event-ticketing-website/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=event_espresso_about_page&utm_content=gdpr_link&ap_id=EEspresso" target="_blank">Read More</a></p>
     <hr>   
     <h4>REST API WRITE Endpoints</h4>
-    <p>With WRITE endpoints in the Event Espresso 4 REST API, developers will find it much easier to create innovative new features for Event Espresso. In fact, we are already starting development of recurring events, and are close to releasing a beta version of the Waiting List Manager. Both of these add-ons wouldn’t be possible without the REST API.</p>
+    <p>With WRITE endpoints in the Event Espresso 4 REST API, developers will find it much easier to create innovative new features for Event Espresso. In fact, we are already starting development of recurring events, and have released the Wait List Manager add-on. Both of these add-ons wouldn’t be possible without the REST API.</p>
     <p align="right"><a href="https://eventespresso.com/2017/09/api-write-endpoints-event-espresso/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=event_espresso_about_page&utm_content=rest_api_link&ap_id=EEspresso">Read More</a>
 
     <hr>
