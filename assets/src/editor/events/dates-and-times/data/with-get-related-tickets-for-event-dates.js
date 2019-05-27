bc7175ddb54694bbd793ee3aabb20eaa5c07bb39
Darren Ethier <darren@roughsmootheng.in>
2019-05-27 09:12:19 -0400

    only return unique datetimeTickets

--- assets/src/editor/events/dates-and-times/data/with-get-related-tickets-for-event-dates.js
+++ assets/src/editor/events/dates-and-times/data/with-get-related-tickets-for-event-dates.js
@@ -43,7 +43,7 @@ export const withGetRelatedTicketsForEventDates = createHigherOrderComponent(
 					}
 				} );
 			}
-			return { datetimeTickets };
+			return { datetimeTickets: uniq( datetimeTickets ) };
 		}
 	),
 	'withGetRelatedTicketsForEventDates'
