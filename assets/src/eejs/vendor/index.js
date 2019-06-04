e260d314e685cbbf52dd07460b7a2b12770bcff6
Darren Ethier <darren@roughsmootheng.in>
2019-06-04 09:40:16 -0400

    remove unused  dependencies for react-redux and redux
    Neither of these packages are used so we can stop including them everywhere.  They can be added back in if we ever have need for them.

--- assets/src/eejs/vendor/index.js
+++ assets/src/eejs/vendor/index.js
@@ -1,6 +1,4 @@
 module.exports = {
-	reactRedux: require( 'react-redux' ),
-	redux: require( 'redux' ),
 	classnames: require( 'classnames' ),
 	moment: require( 'moment-timezone' ),
 	cuid: require( 'cuid' ),
