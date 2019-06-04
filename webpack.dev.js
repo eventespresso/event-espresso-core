8bae74e54ab22d914dd11a7ad2eff755635fea0b
Darren Ethier <darren@roughsmootheng.in>
2019-06-04 11:27:01 -0400

    add back in changes removed in a bad rebase

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,30 +5,51 @@ const webpack = require( 'webpack' );
 const common = require( './webpack.common.js' );
 const miniExtract = require( 'mini-css-extract-plugin' );
 const assetsData = Object.create( null );
+const DependencyExtractionWebpackPlugin = require(
+	'@wordpress/dependency-extraction-webpack-plugin'
+);
+const { requestToExternal, requestToHandle } = require(
+	'./bin/asset-dependency-maps'
+);
+const pluginsConfigWithExternals = [
+	new webpack.ProvidePlugin( {
+		React: 'react',
+	} ),
+	new DependencyExtractionWebpackPlugin( {
+		requestToExternal,
+		requestToHandle,
+	} ),
+	new WebpackAssetsManifest( {
+		output: path.resolve( __dirname,
+			'assets/dist/build-manifest.json',
+		),
+		assets: assetsData,
+	} ),
+	new miniExtract( {
+		filename: '[name].[contenthash].dist.css',
+	} ),
+];
+const pluginsConfigWithoutExternals = [
+	new WebpackAssetsManifest( {
+		output: path.resolve( __dirname,
+			'assets/dist/build-manifest.json',
+		),
+		assets: assetsData,
+	} ),
+	new webpack.ProvidePlugin( {
+		React: 'react',
+	} ),
+	new miniExtract( {
+		filename: '[name].[contenthash].dist.css',
+	} ),
+];
 common.forEach( ( config, index ) => {
-	if ( common[ index ].configName === 'base' ) {
-		common[ index ].optimization = {
-			runtimeChunk: {
-				name: 'manifest',
-			},
-		};
-	}
+	const plugins = config.entry[ 'eventespresso-vendor' ] ?
+		pluginsConfigWithoutExternals :
+		pluginsConfigWithExternals;
 	common[ index ] = merge( config, {
 		devtool: 'inline-source-map',
-		plugins: [
-			new WebpackAssetsManifest( {
-				output: path.resolve( __dirname,
-					'assets/dist/build-manifest.json',
-				),
-				assets: assetsData,
-			} ),
-			new webpack.ProvidePlugin( {
-				React: 'react',
-			} ),
-			new miniExtract( {
-				filename: 'ee-[name].[contenthash].dist.css',
-			} ),
-		],
+		plugins,
 		mode: 'development',
 	} );
 	//delete temporary named config item so no config errors
