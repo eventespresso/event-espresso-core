2018c5a9385b8f3c21fae1643008c3004fd71411
Brent Christensen <brent@eventespresso.com>
2019-06-04 12:33:10 -0700

    add status data for tickets and order alphabetically

--- bin/css-builder/default-data.js
+++ bin/css-builder/default-data.js
@@ -95,12 +95,14 @@ const fontSizeColors = {
 const loremIpsum = 'The approach will not be easy. You are required to maneuver straight down this trench and skim the surface to this point. The target area is only two meters wide. It’s a small thermal exhaust port, right below the main port. The shaft leads directly to the reactor system. A precise hit will start a chain reaction which should destroy the station. Only a precise hit will set up a chain reaction. The shaft is ray-shielded, so you’ll have to use proton torpedoes. That’s impossible, even for a computer. It’s not impossible. I used to bull’s-eye womp rats in my T-sixteen back home. They’re not much bigger than two meters. Man your ships! And may the Force be with you!';
 
 const entityStatusGroups = {
-	transaction: [
-		{ code: 'TOP', label: 'overpaid' },
-		{ code: 'TCM', label: 'complete' },
-		{ code: 'TIN', label: 'incomplete' },
-		{ code: 'TAB', label: 'abandoned' },
-		{ code: 'TFL', label: 'failed' },
+	datetime: [
+		{ code: 'DTS', label: 'sold out' },
+		{ code: 'DTA', label: 'active' },
+		{ code: 'DTU', label: 'upcoming' },
+		{ code: 'DTI', label: 'inactive' },
+		{ code: 'DTP', label: 'postponed' },
+		{ code: 'DTC', label: 'cancelled' },
+		{ code: 'DTT', label: 'trashed' },
 	],
 	payment: [
 		{ code: 'PAP', label: 'approved' },
@@ -118,14 +120,19 @@ const entityStatusGroups = {
 		{ code: 'RDC', label: 'declined' },
 		{ code: 'RCN', label: 'cancelled' },
 	],
-	datetime: [
-		{ code: 'DTS', label: 'sold out' },
-		{ code: 'DTA', label: 'active' },
-		{ code: 'DTU', label: 'upcoming' },
-		{ code: 'DTP', label: 'postponed' },
-		{ code: 'DTC', label: 'cancelled' },
-		{ code: 'DTE', label: 'expired' },
-		{ code: 'DTI', label: 'inactive' },
+	ticket: [
+		{ code: 'TKS', label: 'sold out' },
+		{ code: 'TKO', label: 'on sale' },
+		{ code: 'TKP', label: 'pending' },
+		{ code: 'TKE', label: 'expired' },
+		{ code: 'TKA', label: 'archived' },
+	],
+	transaction: [
+		{ code: 'TOP', label: 'overpaid' },
+		{ code: 'TCM', label: 'complete' },
+		{ code: 'TIN', label: 'incomplete' },
+		{ code: 'TAB', label: 'abandoned' },
+		{ code: 'TFL', label: 'failed' },
 	],
 };
 
