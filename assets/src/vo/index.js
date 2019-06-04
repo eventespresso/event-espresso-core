4d34d1d3b47fcd933323ad565b103913846a4979
Brent Christensen <brent@eventespresso.com>
2019-06-04 13:26:05 -0700

    don't export constants with VOs

--- assets/src/vo/index.js
+++ assets/src/vo/index.js
@@ -1,15 +1,4 @@
 export { default as Money } from './money';
-export {
-	default as SiteCurrency,
-	Currency,
-} from './currency';
+export { default as SiteCurrency, Currency } from './currency';
 export { default as Label } from './label';
-export {
-	DateTime,
-	Duration,
-	ServerDateTime,
-	DEFAULT_TIMEZONE_STRING,
-	DEFAULT_OFFSET,
-	DEFAULT_VALID_LOCALE,
-	DEFAULT_FORMAT,
-} from './date-time';
+export { DateTime, Duration, ServerDateTime } from './date-time';
