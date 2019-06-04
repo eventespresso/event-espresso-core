16cb71103547e87a0862a764ba90718442be17e5
Brent Christensen <brent@eventespresso.com>
2019-06-04 10:41:17 -0700

    remove entity lock

--- assets/src/components/ui/index.js
+++ assets/src/components/ui/index.js
@@ -5,7 +5,6 @@ export { default as MediumCalendarDate } from './dates-and-times/medium-calendar
 export { default as DropDownMenu } from './drop-down-menu';
 export { default as EntityDetailsPanel } from './entity-details-panel';
 export { default as EntityList } from './entity-list';
-export { default as EntityLock } from './entity-lock';
 export { default as withEntityPaperFrame } from './entity-paper-frame';
 export { default as EspressoButton } from './espresso-button';
 export { default as IconMenuItem } from './icon-menu-item';
