c9884b64f8567305c4efbf1b5bb9337dc2a637f8
Darren Ethier <darren@roughsmootheng.in>
2019-06-06 11:48:02 -0400

    rework dates and tickets metabox to use new HOC

--- assets/src/editor/events/dates-and-tickets-metabox/dates-and-tickets-metabox.js
+++ assets/src/editor/events/dates-and-tickets-metabox/dates-and-tickets-metabox.js
@@ -22,105 +22,98 @@ import {
 import '../../editor.css';
 import EditorDatesList from '../dates-and-times/editor-date/editor-dates-list';
 import EditorTicketsList from '../tickets/editor-ticket/editor-tickets-list';
-import DatesAndTicketsFilterState from './dates-and-tickets-filter-state';
+import withEventDateAndTicketEntityFilterState
+	from './with-event-date-and-ticket-entity-filter-state';
 
-/**
- * DatesAndTicketsMetabox
- * displays the Event Dates and Available Tickets "metaboxes"
- *
- * @function
- * @param {number} eventId
- * @return {Component} event dates and tickets available tickets metaboxes
- */
-const DatesAndTicketsMetabox = ( { eventId } ) => (
-	<DatesAndTicketsFilterState
-		eventId={ eventId }
-		render={ ( {
-			loading,
-			loadingDates,
-			loadingTickets,
-			datetimes,
-			allDates,
-			tickets,
-			allTickets,
-			isChained,
-			...otherProps
-		} ) => {
-			return (
-				<Fragment>
-					<FormPlaceholder
-						loading={ loading }
-						notice={ sprintf(
-							_x(
-								'loading event dates and available tickets%s',
-								'loading event dates and available tickets...',
-								'event_espresso'
-							),
-							String.fromCharCode( 8230 )
-						) }
-					/>
-					<FormContainer
-						loading={ loading }
-						id={ `ee-editor-event-dates-and-tickets-${ eventId }` }
-						htmlClass="ee-editor-event-dates-and-tickets"
+const MetaBox = ( {
+	loading,
+	eventId,
+	loadingEventDateEntities,
+	loadingTicketEntities,
+	filteredEventDateEntities,
+	allEventDateEntities,
+	filteredTicketEntities,
+	allTicketEntities,
+	isChained,
+	...otherProps,
+} ) => {
+	return (
+		<Fragment>
+			<FormPlaceholder
+				loading={ loading }
+				notice={ sprintf(
+					_x(
+						'loading event dates and available tickets%s',
+						'loading event dates and available tickets...',
+						'event_espresso'
+					),
+					String.fromCharCode( 8230 )
+				) }
+			/>
+			<FormContainer
+				loading={ loading }
+				id={ `ee-editor-event-dates-and-tickets-${ eventId }` }
+				htmlClass="ee-editor-event-dates-and-tickets"
+			>
+				<h1 className="ee-metabox-heading">
+					<EspressoIcon icon="calendar" />
+					{ __( 'Event Dates', 'event_espresso' ) }
+				</h1>
+				<Panel>
+					<PanelBody
+						id={ `ee-editor-event-dates-${ eventId }` }
+						className="ee-editor-event-dates espresso-editor"
 					>
-						<h1 className="ee-metabox-heading">
-							<EspressoIcon icon="calendar" />
-							{ __( 'Event Dates', 'event_espresso' ) }
-						</h1>
-						<Panel>
-							<PanelBody
-								id={ `ee-editor-event-dates-${ eventId }` }
-								className="ee-editor-event-dates espresso-editor"
-							>
-								<PanelRow
-									className="ee-editor-event-dates ee-form-row">
-									<div>
-										<EditorDatesList
-											loading={ loadingDates }
-											entities={ datetimes }
-											allDates={ allDates }
-											allTickets={ allTickets }
-											prefiltered
-											for="event-dates-metabox"
-											{ ...otherProps }
-										/>
-									</div>
-								</PanelRow>
-							</PanelBody>
-						</Panel>
-						<br />
-						<h1 className="ee-metabox-heading">
-							<Dashicon icon="tickets-alt" />
-							{ __( 'Available Tickets', 'event_espresso' ) }
-						</h1>
-						<Panel>
-							<PanelBody
-								id={ `ee-editor-event-tickets-${ eventId }` }
-								className="ee-editor-event-tickets espresso-editor"
-							>
-								<PanelRow
-									className="ee-editor-event-tickets ee-form-row">
-									<div>
-										<EditorTicketsList
-											loading={ loadingTickets }
-											entities={ tickets }
-											allDates={ allDates }
-											isChained={ isChained }
-											prefiltered
-											for="event-tickets-metabox"
-											{ ...otherProps }
-										/>
-									</div>
-								</PanelRow>
-							</PanelBody>
-						</Panel>
-						<br />
-					</FormContainer>
-				</Fragment>
-			);
-		} }
-	/>
+						<PanelRow
+							className="ee-editor-event-dates ee-form-row">
+							<div>
+								<EditorDatesList
+									loading={ loadingEventDateEntities }
+									entities={ filteredEventDateEntities }
+									allDates={ allEventDateEntities }
+									allTickets={ allTicketEntities }
+									prefiltered
+									for="event-dates-metabox"
+									{ ...otherProps }
+								/>
+							</div>
+						</PanelRow>
+					</PanelBody>
+				</Panel>
+				<br />
+				<h1 className="ee-metabox-heading">
+					<Dashicon icon="tickets-alt" />
+					{ __( 'Available Tickets', 'event_espresso' ) }
+				</h1>
+				<Panel>
+					<PanelBody
+						id={ `ee-editor-event-tickets-${ eventId }` }
+						className="ee-editor-event-tickets espresso-editor"
+					>
+						<PanelRow
+							className="ee-editor-event-tickets ee-form-row">
+							<div>
+								<EditorTicketsList
+									loading={ loadingTicketEntities }
+									entities={ filteredTicketEntities }
+									allDates={ allEventDateEntities }
+									isChained={ isChained }
+									prefiltered
+									for="event-tickets-metabox"
+									{ ...otherProps }
+								/>
+							</div>
+						</PanelRow>
+					</PanelBody>
+				</Panel>
+				<br />
+			</FormContainer>
+		</Fragment>
+	);
+};
+
+const DatesAndTicketsMetabox = withEventDateAndTicketEntityFilterState(
+	MetaBox
 );
 
 DatesAndTicketsMetabox.propTypes = {
