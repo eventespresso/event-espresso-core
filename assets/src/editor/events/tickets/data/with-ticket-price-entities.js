57e24e9c838a04243e55aed64a89b8f550244a91
Darren Ethier <darren@roughsmootheng.in>
2019-06-07 15:17:25 -0400

    renaming refactor of ticket and price hocs

---
+++ assets/src/editor/events/tickets/data/with-ticket-price-entities.js
@@ -0,0 +1,44 @@
+/**
+ * External imports
+ */
+import { isEmpty } from 'lodash';
+import { withSelect } from '@wordpress/data';
+import { isModelEntityOfModel } from '@eventespresso/validators';
+
+const EMPTY_OBJECT = {
+	noBasePrice: true,
+	priceEntitiesLoaded: false,
+	priceEntities: [],
+};
+
+/**
+ * withTicketPriceEntities
+ * returns an object containing the following:
+ *    priceEntities - an array of price entities for the supplied ticket
+ *    priceEntitiesLoaded - boolean true if loading is complete
+ *    noBasePrice - boolean true if loading is complete but priceEntities is empty
+ *
+ * @function
+ */
+export const withTicketPriceEntities = withSelect(
+	( select, { ticketEntity } ) => {
+		const { getRelatedEntities } = select( 'eventespresso/core' );
+		const { hasFinishedResolution } = select( 'core/data' );
+		if ( isModelEntityOfModel( ticketEntity, 'ticket' ) ) {
+			const priceEntities = getRelatedEntities( ticketEntity, 'prices' );
+			const priceEntitiesLoaded = hasFinishedResolution(
+				'eventespresso/core',
+				'getRelatedEntities',
+				[ ticketEntity, 'priceEntities' ]
+			);
+			if ( priceEntitiesLoaded ) {
+				return {
+					noBasePrice: isEmpty( priceEntities ),
+					priceEntitiesLoaded,
+					priceEntities,
+				};
+			}
+		}
+		return EMPTY_OBJECT;
+	}
+);
