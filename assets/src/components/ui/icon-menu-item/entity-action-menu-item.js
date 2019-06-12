1ad4691f0b014fb2dc80afffc35a388347b0dbce
Brent Christensen <brent@eventespresso.com>
2019-06-12 15:31:32 -0700

    convert to functional component

--- assets/src/components/ui/icon-menu-item/entity-action-menu-item.js
+++ assets/src/components/ui/icon-menu-item/entity-action-menu-item.js
@@ -1,17 +1,8 @@
 /**
- * External imports
- */
-import { Component } from 'react';
-
-/**
  * EntityActionMenuItem
  * just a wrapper for an IconButton and additional component
  * (most likely a modal component) so that it can be added to a menu
  */
-class EntityActionMenuItem extends Component {
-	render() {
-		return this.props.children;
-	}
-}
+const EntityActionMenuItem = ( { children } ) => children;
 
 export default EntityActionMenuItem;
