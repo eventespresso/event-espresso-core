ff5e45960b15aefb02d35e370c69c9bdcf80f9f2
Brent Christensen <brent@eventespresso.com>
2019-06-18 08:37:09 -0700

    remove unused import var js_only

--- assets/src/components/form/inputs/date-time-input.js
+++ assets/src/components/form/inputs/date-time-input.js
@@ -3,7 +3,7 @@
  */
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import { Component, Fragment } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 import { Field } from 'react-final-form';
 import {
 	Button,
