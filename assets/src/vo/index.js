627dc26aa10bd938522d9904490487ed9ea9f108
EE DevBox Server <admin@eventespresso.com>
2019-06-18 08:20:20 -0700

    Bumping version to 4.9.82.p and prepped for release

--- assets/src/vo/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Money } from './money';
-export {
-	default as SiteCurrency,
-	Currency,
-} from './currency';
-export { default as Label } from './label';
-export { DateTime, Duration, ServerDateTime } from './date-time';
