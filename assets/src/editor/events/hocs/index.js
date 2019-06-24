52ce7c415a16331d8f3e9e09a1aa05a3b42927da
Brent Christensen <brent@eventespresso.com>
2019-06-24 11:16:24 -0700

    move ifValidDateEntity and update exports for it and other moved HOC

--- assets/src/editor/events/hocs/index.js
+++ assets/src/editor/events/hocs/index.js
@@ -1,8 +1,6 @@
-export { default as ifValidDateEntity } from './if-valid-date-entity';
 export { default as withEditorDateEntities } from './with-editor-date-entities';
 export { default as withEditorDateTicketEntities } from './with-editor-date-ticket-entities';
 export { default as withEditorEventEntity } from './with-editor-event-entity';
-export { default as withEditorEventVenueEntity } from './with-editor-event-venue-entity';
 export { default as withEditorTicketDateEntities } from './with-editor-ticket-date-entities';
 export { default as withEditorTicketEntities } from './with-editor-ticket-entities';
 export { default as withPriceTypeEntities } from './with-price-type-entities';
--- assets/src/editor/hocs/index.js
+++ assets/src/editor/hocs/index.js
@@ -1,2 +1,4 @@
 export { withEditor, withEditorModal } from './editor-modal';
 export { default as NotWithPostTypeCheck } from './not-with-post-type-check';
+export { default as ifValidDateEntity } from './if-valid-date-entity';
+export { default as withEventVenueEntity } from './with-event-venue-entity';
