2ea2e0da8a496c75b07ac7ce2fdf59826ab4b315
Darren Ethier <darren@roughsmootheng.in>
2019-06-26 08:27:54 -0400

    remove unnecessary exports

--- assets/src/editor/events/dates-and-times/editor-date/edit-form/index.js
+++ assets/src/editor/events/dates-and-times/editor-date/edit-form/index.js
@@ -1,5 +1,2 @@
 export { default as DateEntityForm } from './date-entity-form';
 export { default as DateEntityFormModal } from './date-entity-form-modal';
-export { dateEntityFormInputs } from './date-entity-form-inputs';
-export { dateEntityFormSchema } from './date-entity-form-schema';
-export { dateEntityFormSubmitHandler } from './date-entity-form-submit-handler';
--- assets/src/editor/events/tickets/editor-ticket/edit-form/index.js
+++ assets/src/editor/events/tickets/editor-ticket/edit-form/index.js
@@ -1,4 +1,3 @@
 export { default as TicketEntityForm } from './ticket-entity-form';
 export { default as withTicketEntityFormModal } from './with-ticket-entity-form-modal';
-export { ticketEntityFormInputs } from './ticket-entity-form-inputs';
-export { ticketEntityFormSchema } from './ticket-entity-form-schema';
+
