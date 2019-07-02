9957bb5a26a72fa3331db6ae6e1d067b6de4c8b9
Darren Ethier <darren@roughsmootheng.in>
2019-07-02 16:28:00 -0400

    export hooks

---
+++ assets/src/editor/events/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useEditorEventEntity } from './use-editor-event-entity';
+export { default as useEditorUpdateEventDateRelation }
+	from './use-editor-update-event-date-relation';
+export { default as useEventVenueEntity } from './use-event-venue-entity';
