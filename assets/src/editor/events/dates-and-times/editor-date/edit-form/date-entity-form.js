7d673fa5dc4f17d21e670dfb723ee5991a8a7220
Brent Christensen <brent@eventespresso.com>
2019-07-05 12:05:35 -0700

    replace use of dateEntityFormInputs with dateEntityInputConfig()
    update form schema so that form data identifiers match property names

--- assets/src/editor/events/dates-and-times/editor-date/edit-form/date-entity-form.js
+++ assets/src/editor/events/dates-and-times/editor-date/edit-form/date-entity-form.js
@@ -2,98 +2,133 @@
  * External imports
  */
 import { isEmpty } from 'lodash';
-import { Component } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
+import { useMemo } from '@wordpress/element';
 import {
 	withFormHandler,
 	twoColumnAdminFormLayout,
 } from '@eventespresso/components';
+import { ifValidDateEntity } from '@eventespresso/editor-hocs';
 import { __ } from '@eventespresso/i18n';
-import { dateTimeModel } from '@eventespresso/model';
 import { isModelEntityOfModel } from '@eventespresso/validators';
 
 /**
  * Internal dependencies
  */
-import { dateEntityFormInputs } from './date-entity-form-inputs';
+import {
+	editEntityFormInputs,
+} from '../../../../helpers/forms/edit-entity-form-inputs';
+import { dateEntityInputConfig } from './date-entity-input-config';
+
+const {
+	FormSection,
+	FormWrapper,
+	FormSaveCancelButtons,
+	FormInfo,
+} = twoColumnAdminFormLayout;
 
 /**
  * @function
  * @param {Object} dateEntity model object defining the Event Date
+ * @param {Object} submitButton component
+ * @param {Object} cancelButton component
+ * @param {Object} currentValues form data
+ * @param {Object} initialValues initial form data
+ * @param {boolean} newObject if true then date has not ever been persisted
+ * @return {Object} rendered form for editing date entity
  */
-class DateEntityForm extends Component {
-	render() {
-		const {
+const DateEntityForm = ( {
+	dateEntity,
+	submitButton,
+	cancelButton,
+	currentValues = {},
+	initialValues = {},
+	newObject = false,
+} ) => {
+	// entity properties we don't want to be editable
+	const exclude = [
+		'eventId',
+		'sold',
+		'reserved',
+		'order',
+		'parent',
+		'deleted',
+	];
+	const inputConfig = useMemo(
+		() => dateEntityInputConfig( dateEntity ),
+		[ dateEntityInputConfig, dateEntity ]
+	);
+	const formRows = useMemo(
+		() => editEntityFormInputs(
 			dateEntity,
+			inputConfig,
+			`ee-event-date-${ dateEntity.id }`,
+			isEmpty( currentValues ) ? initialValues : currentValues,
+			exclude
+		),
+		[
+			editEntityFormInputs,
+			dateEntity,
+			inputConfig,
+			currentValues,
+			initialValues,
+			exclude,
+		]
+	);
+	const formInfo = useMemo(
+		() => (
+			<FormInfo
+				key="formInfo"
+				formInfo={
+					__(
+						'all fields marked with an asterisk are required',
+						'event_espresso'
+					)
+				}
+				dismissable={ false }
+			/>
+		),
+		[]
+	);
+	if ( Array.isArray( formRows ) ) {
+		formRows.unshift( formInfo );
+	}
+	return useMemo(
+		() => {
+			// edit forms for existing objects must have initial values
+			return ! ( ! newObject && isEmpty( initialValues ) ) &&
+			isModelEntityOfModel( dateEntity, 'datetime' ) ?
+				(
+					<FormWrapper>
+						<FormSection
+							htmlId={
+								`ee-event-date-editor-${ dateEntity.id }-form-section`
+							}
+							children={ formRows }
+						/>
+						<FormSaveCancelButtons
+							htmlClass={ `ee-event-date-editor-${ dateEntity.id }` }
+							submitButton={ submitButton }
+							cancelButton={ cancelButton }
+						/>
+					</FormWrapper>
+				) : null;
+		},
+		[
+			newObject,
+			initialValues,
+			dateEntity,
+			formRows,
 			submitButton,
 			cancelButton,
-			currentValues = {},
-			initialValues = {},
-			newObject = false,
-		} = this.props;
-		// edit forms for existing objects must have initial values
-		if (
-			( ! newObject && isEmpty( initialValues ) ) ||
-			isEmpty( currentValues )
-		) {
-			return null;
-		}
-		const {
-			FormInput,
-			FormSection,
-			FormWrapper,
-			FormSaveCancelButtons,
-			FormInfo,
-		} = twoColumnAdminFormLayout;
-
-		// entity properties we don't want to be editable
-		const exclude = [
-			'EVT_ID',
-			'sold',
-			'reserved',
-			'order',
-			'parent',
-			'deleted',
-		];
-		const formRows = dateEntityFormInputs(
-			dateEntity,
-			exclude,
-			currentValues,
-			FormInput
-		);
-		if ( Array.isArray( formRows ) ) {
-			formRows.unshift(
-				<FormInfo
-					key="formInfo"
-					formInfo={
-						__(
-							'all fields marked with an asterisk are required',
-							'event_espresso'
-						)
-					}
-					dismissable={ false }
-				/>
-			);
-		}
-		const { MODEL_NAME: DATETIME } = dateTimeModel;
-		return isModelEntityOfModel( dateEntity, DATETIME ) ? (
-			<FormWrapper>
-				<FormSection
-					htmlId={
-						`ee-event-date-editor-${ dateEntity.id }-form-section`
-					}
-					children={ formRows }
-				/>
-				<FormSaveCancelButtons
-					htmlClass={ `ee-event-date-editor-${ dateEntity.id }` }
-					submitButton={ submitButton }
-					cancelButton={ cancelButton }
-				/>
-			</FormWrapper>
-		) : null;
-	}
-}
+		]
+	);
+};
 
 /**
  * Enhanced DateEntityForm with FormHandler
  */
-export default withFormHandler( DateEntityForm );
+export default compose( [
+	ifValidDateEntity,
+	withFormHandler,
+] )( DateEntityForm );
