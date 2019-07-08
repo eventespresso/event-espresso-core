6e85ec3a0ef75a8ea8c805ae599daa3d61c2e899
Darren Ethier <darren@roughsmootheng.in>
2019-07-08 10:57:39 -0400

    add inline docs
    js_only

--- assets/src/editor/events/hooks/use-editor-date-entities.js
+++ assets/src/editor/events/hooks/use-editor-date-entities.js
@@ -1,5 +1,11 @@
 import { useSelect } from '@wordpress/data';
 
+/**
+ * A hook for retrieving all the date entities currently in the
+ * eventespresso/core data store.
+ *
+ * @return { BaseEntity[] } An array of Datetime entities.
+ */
 const useEditorDateEntities = () => {
 	return useSelect( ( select ) => {
 		return select( 'eventespresso/core' ).getDatetimes();
