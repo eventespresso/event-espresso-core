06dc6bc8bf67f3f3f4b0a95b98c5f96915223a97
Josh Feck <josh@nxnweb.com>
2019-07-10 18:21:31 -0400

    Revert "Add filter hook to allow filtering the output of answers on messages"
    This reverts commit 9f9265eb265614cf3092f8986f8b23295f50921f.

--- caffeinated/core/libraries/shortcodes/EE_Question_Shortcodes.lib.php
+++ caffeinated/core/libraries/shortcodes/EE_Question_Shortcodes.lib.php
@@ -85,12 +85,7 @@ class EE_Question_Shortcodes extends EE_Shortcodes
                         break;
 
                     default:
-                        $answer = apply_filters(
-                            'FHEE__EE_Question_Shortcodes___parser__answer',
-                            $this->_data->get_pretty('ANS_value', 'no_wpautop'),
-                            $question,
-                            $this->_data
-                        );
+                        $answer = $this->_data->get_pretty('ANS_value', 'no_wpautop');
                         break;
                 }
 
