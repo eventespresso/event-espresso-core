8393b2d9042c46ad061b8dae8e8932625fb4c6c2
Brent Christensen <brent@eventespresso.com>
2019-07-11 11:14:14 -0700

    convert EditorDateEntitiesListView to use ResponsiveTable component

--- assets/src/editor/events/dates-and-times/editor-date/list-view/index.js
+++ assets/src/editor/events/dates-and-times/editor-date/list-view/index.js
@@ -1,2 +1,2 @@
-export { default as EditorDateEntityListItem } from './editor-date-entity-list-item';
+export { default as EditorDateEntityListItem } from './dates-list-table-row';
 export { default as EditorDateEntitiesListView } from './editor-date-entities-list-view';
