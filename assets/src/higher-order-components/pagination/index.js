db3108225ef710d2e7c10c7d416ce419eb1601dc
Darren Ethier <darren@roughsmootheng.in>
2019-07-15 16:31:22 -0400

    refactor entity pagination component/hoc to use hooks.
    - also improved/fixed bugs related to pagination
    - part of the above involved adding a prop for passing along a `onPageChange` callback (which is invoked when a new page of entities is selected via the JwPagination component.

--- assets/src/higher-order-components/pagination/index.js
+++ assets/src/higher-order-components/pagination/index.js
@@ -1,21 +1,126 @@
 /**
  * External imports
  */
-import { isEqual } from 'lodash';
-import PropTypes from 'prop-types';
 import JwPagination from 'jw-react-pagination';
 import {
 	compose,
 	createHigherOrderComponent,
 	withInstanceId,
 } from '@wordpress/compose';
-import { Component, Fragment } from '@wordpress/element';
+import { Fragment, useState, useCallback, useEffect, useRef } from '@wordpress/element';
 import { __ } from '@eventespresso/i18n';
 
 /**
  * Internal dependencies
  */
 import './style.css';
+
+const EntityPaginationComponent = ( {
+	paginationConfig = {},
+	entities,
+	instanceId,
+	entitiesPerPage = 10,
+	position = 'top',
+	onPageChange = () => null,
+	EntityList = () => null,
+	...otherProps
+} ) => {
+	const [ pagedEntities, setPagedEntities ] = useState( entities );
+	const currentEntities = useRef( entities );
+	const onPaginationChange = useCallback( ( updatedPagedEntities ) => {
+		setPagedEntities( updatedPagedEntities );
+		onPageChange( updatedPagedEntities );
+	}, [ onPageChange ] );
+	useEffect(
+		() => {
+			// because jwPagination uses the incoming entities array to determine
+			// whether to recalculate the paged entities, we need to force
+			// that array to change if entitiesPerPage changes.
+			currentEntities.current = [ ...entities ];
+			// forces re-render because useEffect executes after initial
+			// render calculation done.
+			setPagedEntities( [] );
+		},
+		[ entitiesPerPage, entities ]
+	);
+	paginationConfig.labels = paginationConfig.labels &&
+	paginationConfig.labels.first &&
+	paginationConfig.labels.last &&
+	paginationConfig.labels.previous &&
+	paginationConfig.labels.next ?
+		paginationConfig.labels :
+		{
+			first: __( 'First', 'event_espresso' ),
+			last: __( 'Last', 'event_espresso' ),
+			previous: __( 'Prev', 'event_espresso' ),
+			next: __( 'Next', 'event_espresso' ),
+		};
+	const noResultsText = paginationConfig.noResultsText ?
+		paginationConfig.noResultsText :
+		__(
+			'no results found (try changing filters)',
+			'event_espresso'
+		);
+	const returnAsProp = paginationConfig.returnAsProp ?
+		paginationConfig.returnAsProp :
+		false;
+	const pagination = useCallback(
+		() => (
+			<div id={ `ee-entity-pagination-${ instanceId }` }
+				className="ee-entity-pagination"
+			>
+				<JwPagination
+					items={ currentEntities.current }
+					onChangePage={ onPaginationChange }
+					pageSize={ parseInt( entitiesPerPage, 10 ) }
+					{ ...paginationConfig }
+				/>
+			</div>
+		),
+		[
+			instanceId,
+			onPaginationChange,
+			entities,
+			entitiesPerPage,
+			paginationConfig,
+		]
+	);
+	const topPagination = position === ( 'top' || 'both' ) ?
+		pagination() :
+		null;
+	const bottomPagination = position === ( 'bottom' || 'both' ) ?
+		pagination() :
+		null;
+	return returnAsProp ? (
+		<EntityList
+			pagination={ pagination() }
+			entities={ pagedEntities }
+			noResultsText={ noResultsText }
+			{ ...otherProps }
+		/>
+	) : (
+		<Fragment>
+			{ topPagination }
+			<EntityList
+				entities={ pagedEntities }
+				noResultsText={ noResultsText }
+				{ ...otherProps }
+			/>
+			{ bottomPagination }
+		</Fragment>
+	);
+};
+
+const EntityPagination = ( paginationConfig ) => createHigherOrderComponent(
+	( WrappedComponent ) => ( props ) => {
+		return <EntityPaginationComponent
+			{ ...props }
+			paginationConfig={ paginationConfig }
+			EntityList={ WrappedComponent }
+		/>;
+	},
+	'EntityPagination'
+);
 
 /**
  * withEntityPagination
@@ -29,109 +134,7 @@ import './style.css';
 export default ( paginationConfig = {} ) => createHigherOrderComponent(
 	compose( [
 		withInstanceId,
-		( EntityList ) => {
-			return class EntityPagination extends Component {
-				static propTypes = {
-					entities: PropTypes.array.isRequired,
-					instanceId: PropTypes.number.isRequired,
-					entitiesPerPage: PropTypes.oneOfType( [
-						PropTypes.string,
-						PropTypes.number,
-					] ),
-					position: PropTypes.string,
-				};
-
-				constructor( props ) {
-					super( props );
-					this.state = {
-						entityPage: [],
-					};
-				}
-
-				shouldComponentUpdate( nextProps, nextState ) {
-					return ! (
-						isEqual( nextProps, this.props ) &&
-						isEqual( nextState.entityPage, this.state.entityPage )
-					);
-				}
-
-				/**
-				 * @function
-				 * @param {Array} entityPage
-				 */
-				onPaginationChange = ( entityPage ) => {
-					// update local state with new page of items
-					this.setState( { entityPage } );
-				};
-
-				render() {
-					const {
-						entities,
-						instanceId,
-						entitiesPerPage = 10,
-						position = 'top',
-						...otherProps
-					} = this.props;
-					paginationConfig.labels = paginationConfig.labels &&
-						paginationConfig.labels.first &&
-						paginationConfig.labels.last &&
-						paginationConfig.labels.previous &&
-						paginationConfig.labels.next ?
-						paginationConfig.labels :
-						{
-							first: __( 'First', 'event_espresso' ),
-							last: __( 'Last', 'event_espresso' ),
-							previous: __( 'Prev', 'event_espresso' ),
-							next: __( 'Next', 'event_espresso' ),
-						};
-					const noResultsText = paginationConfig.noResultsText ?
-						paginationConfig.noResultsText :
-						__(
-							'no results found (try changing filters)',
-							'event_espresso'
-						);
-					const returnAsProp = paginationConfig.returnAsProp ?
-						paginationConfig.returnAsProp :
-						false;
-					const pagination = (
-						<div id={ `ee-entity-pagination-${ instanceId }` }
-							className="ee-entity-pagination"
-						>
-							<JwPagination
-								items={ entities }
-								onChangePage={ this.onPaginationChange }
-								pageSize={ parseInt( entitiesPerPage, 10 ) }
-								{ ...paginationConfig }
-							/>
-						</div>
-					);
-					const topPagination = position === ( 'top' || 'both' ) ?
-						pagination :
-						null;
-					const bottomPagination = position === ( 'bottom' || 'both' ) ?
-						pagination :
-						null;
-					return returnAsProp ? (
-						<EntityList
-							pagination={ pagination }
-							entities={ this.state.entityPage }
-							noResultsText={ noResultsText }
-							{ ...otherProps }
-						/>
-					) : (
-						<Fragment>
-							{ topPagination }
-							<EntityList
-								entities={ this.state.entityPage }
-								noResultsText={ noResultsText }
-								{ ...otherProps }
-							/>
-							{ bottomPagination }
-						</Fragment>
-					);
-				}
-			};
-		},
+		EntityPagination( paginationConfig ),
 	] ),
 	'withEntityPagination'
 );
