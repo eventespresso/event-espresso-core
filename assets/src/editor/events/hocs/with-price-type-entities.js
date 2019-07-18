ddab21090c52a2daaca1ef581b0ad0e257314b50
Brent Christensen <brent@eventespresso.com>
2019-07-18 11:16:32 -0700

    convert HOCs to implement useSelect() instead of withSelect()

--- assets/src/editor/events/hocs/with-price-type-entities.js
+++ assets/src/editor/events/hocs/with-price-type-entities.js
@@ -1,30 +1,41 @@
 /**
  * External imports
  */
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import { createHigherOrderComponent } from '@wordpress/compose';
 
 /**
  * withPriceTypeEntities
- * returns an object containing the following:
+ * injects the following additional props into the provided component:
  *    priceTypeEntities - an array of price_type entities
  *    priceTypeEntitiesLoaded - boolean true if loading is complete
  *
  * @function
  */
 export default createHigherOrderComponent(
-	withSelect(
-		( select ) => {
-			const { getEntities } = select( 'eventespresso/lists' );
-			const priceTypeEntities = getEntities( 'price_type' );
-			const { hasFinishedResolution } = select( 'core/data' );
-			const priceTypeEntitiesLoaded = hasFinishedResolution(
-				'eventespresso/lists',
-				'getEntities',
-				[ 'price_type' ]
-			);
-			return { priceTypeEntities, priceTypeEntitiesLoaded };
-		}
-	),
+	( WrappedComponent ) => ( { ...otherProps } ) => {
+		const { priceTypeEntities, priceTypeEntitiesLoaded } = useSelect(
+			( select ) => {
+				const { getEntities } = select( 'eventespresso/lists' );
+				const { hasFinishedResolution } = select( 'core/data' );
+				const prices = getEntities( 'price_type' );
+				const pricesLoaded = hasFinishedResolution(
+					'eventespresso/lists',
+					'getEntities',
+					[ 'price_type' ]
+				);
+				return {
+					priceTypeEntities: prices,
+					priceTypeEntitiesLoaded: pricesLoaded,
+				};
+			},
+			[]
+		);
+		return <WrappedComponent
+			{ ...otherProps }
+			priceTypeEntities={ priceTypeEntities }
+			priceTypeEntitiesLoaded={ priceTypeEntitiesLoaded }
+		/>;
+	},
 	'withPriceTypeEntities'
 );
--- assets/src/editor/events/hocs/with-ticket-price-entities.js
+++ assets/src/editor/events/hocs/with-ticket-price-entities.js
@@ -2,7 +2,7 @@
  * External imports
  */
 import { isEmpty } from 'lodash';
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import { isModelEntityOfModel } from '@eventespresso/validators';
 import { createHigherOrderComponent } from '@wordpress/compose';
 
@@ -14,35 +14,49 @@ const EMPTY_OBJECT = {
 
 /**
  * withTicketPriceEntities
- * returns an object containing the following:
+ * injects the following additional props into the provided component:
  *    priceEntities - an array of price entities for the supplied ticket
  *    priceEntitiesLoaded - boolean true if loading is complete
  *    noBasePrice - boolean true if loading is complete but priceEntities is empty
- *
- * @function
  */
 export default createHigherOrderComponent(
-	withSelect(
-		( select, { ticketEntity } ) => {
-			const { getRelatedEntities } = select( 'eventespresso/core' );
-			const { hasFinishedResolution } = select( 'core/data' );
-			if ( isModelEntityOfModel( ticketEntity, 'ticket' ) ) {
-				const priceEntities = getRelatedEntities( ticketEntity, 'price' );
-				const priceEntitiesLoaded = hasFinishedResolution(
-					'eventespresso/core',
-					'getRelatedEntities',
-					[ ticketEntity, 'price' ]
-				);
-				if ( priceEntitiesLoaded ) {
-					return {
-						noBasePrice: isEmpty( priceEntities ),
-						priceEntitiesLoaded,
-						priceEntities,
-					};
+	( WrappedComponent ) => ( {
+		ticketEntity,
+		...otherProps
+	} ) => {
+		const { noBasePrice, priceEntitiesLoaded, priceEntities } = useSelect(
+			( select ) => {
+				if ( isModelEntityOfModel( ticketEntity, 'ticket' ) ) {
+					const { getRelatedEntities } = select( 'eventespresso/core' );
+					const { hasFinishedResolution } = select( 'core/data' );
+					const prices = getRelatedEntities(
+						ticketEntity,
+						'price'
+					);
+					const pricesLoaded = hasFinishedResolution(
+						'eventespresso/core',
+						'getRelatedEntities',
+						[ ticketEntity, 'price' ]
+					);
+					if ( pricesLoaded ) {
+						return {
+							noBasePrice: isEmpty( priceEntities ),
+							priceEntitiesLoaded: pricesLoaded,
+							priceEntities: prices,
+						};
+					}
 				}
-			}
-			return EMPTY_OBJECT;
-		}
-	),
+				return EMPTY_OBJECT;
+			},
+			[ ticketEntity ]
+		);
+		return <WrappedComponent
+			{ ...otherProps }
+			ticketEntity={ ticketEntity }
+			priceEntities={ priceEntities }
+			priceEntitiesLoaded={ priceEntitiesLoaded }
+			noBasePrice={ noBasePrice }
+		/>;
+	},
 	'withTicketPriceEntities'
 );
