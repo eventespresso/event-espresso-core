ffb080ccc40fa5894c3f47180f5d5bb1dadc2c1f
Darren Ethier <darren@roughsmootheng.in>
2019-07-22 09:44:50 -0400

    remove unnecessary comment block
    js_only

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = function( api ) {
 			'@wordpress/babel-preset-default',
 		],
 		plugins: [
-			/** this is needed because plugin exit modal uses react not wp.element */
 			'@babel/plugin-proposal-class-properties',
 		],
 		env: {
