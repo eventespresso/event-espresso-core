34eeccf9a0859e4b3a194ccb752336e6620f5da4
Brent Christensen <brent@eventespresso.com>
2019-07-22 17:55:58 -0700

    add getPriceAmount() 2B DRY, formatting

--- assets/src/editor/events/tickets/editor-ticket/price-calculator/use-ticket-price-calculators.js
+++ assets/src/editor/events/tickets/editor-ticket/price-calculator/use-ticket-price-calculators.js
@@ -10,7 +10,11 @@ import memoize from 'memize';
 
 const { BASE_PRICE_TYPES } = priceTypeModel;
 
-const normalCases = ( currentTotal, amount, isPercent ) => ( {
+const getPriceAmount = ( priceEntity ) => priceEntity.amount instanceof Money ?
+	priceEntity.amount.toNumber() :
+	parseFloat( priceEntity.amount );
+
+const ticketTotalCalculations = ( currentTotal, amount, isPercent ) => ( {
 	[ BASE_PRICE_TYPES.BASE_PRICE ]: () => currentTotal + amount,
 	[ BASE_PRICE_TYPES.DISCOUNT ]: () => isPercent ?
 		currentTotal - ( ( amount / 100 ) * currentTotal ) :
@@ -22,7 +26,7 @@ const normalCases = ( currentTotal, amount, isPercent ) => ( {
 		currentTotal + ( ( amount / 100 ) * currentTotal ),
 } );
 
-const reverseCases = ( currentTotal, amount, isPercent ) => ( {
+const basePriceCalculations = ( currentTotal, amount, isPercent ) => ( {
 	[ BASE_PRICE_TYPES.DISCOUNT ]: () => isPercent ?
 		currentTotal / ( ( 100 - amount ) / 100 ) :
 		currentTotal + amount,
@@ -38,64 +42,86 @@ const getBasePrice = memoize(
 	} )
 );
 
-const accrueAmount = ( cases ) => ( defaultCase ) => ( typeId ) =>
-	cases.hasOwnProperty( typeId ) ?
-		cases[ typeId ]() :
-		defaultCase;
+const accrueAmount = ( calculations ) => ( newTotal ) => ( basePriceType ) =>
+	calculations.hasOwnProperty( basePriceType ) ?
+		calculations[ basePriceType ]() :
+		newTotal;
 
-const getPriceTypeForPrice = ( priceEntity, priceTypes ) => {
-	const priceType = find( priceTypes, [ 'id', priceEntity.PRT_ID ] );
+const getPriceTypeForPrice = ( priceEntity, priceTypeEntities ) => {
+	const priceType = find( priceTypeEntities, [ 'id', priceEntity.PRT_ID ] );
 	if ( isModelEntityOfModel( priceType, 'price_type' ) ) {
 		return priceType;
 	}
-	return first( filter( priceTypes, ( pt ) => pt.id !== 1 ) );
+	return first( filter( priceTypeEntities, ( pt ) => pt.id !== 1 ) );
 };
 
 const useTicketPriceCalculators = ( priceTypeEntities ) => {
-	const calculateTicketTotal = useCallback( ( total = 0, priceEntities ) => {
-		priceEntities = sortBy( priceEntities, [ 'order', 'id' ] );
-		return priceEntities.reduce( ( newTotal, priceEntity ) => {
-			const priceTypeEntity = getPriceTypeForPrice( priceEntity, priceTypeEntities );
-			return accrueAmount( normalCases(
-				newTotal,
-				priceEntity.amount,
-				priceTypeEntity.isPercent
-			) )( newTotal )( priceTypeEntity.pbtId );
-		}, total );
-	}, [ priceTypeEntities ] );
+	const calculateTicketTotal = useCallback(
+		( total = 0, priceEntities ) => {
+			priceEntities = sortBy( priceEntities, [ 'order', 'id' ] );
+			return priceEntities.reduce(
+				( newTotal, priceEntity ) => {
+					const priceTypeEntity = getPriceTypeForPrice(
+						priceEntity,
+						priceTypeEntities
+					);
+					return accrueAmount(
+						ticketTotalCalculations(
+							newTotal,
+							getPriceAmount( priceEntity ),
+							priceTypeEntity.isPercent
+						)
+					)( newTotal )( priceTypeEntity.pbtId );
+				},
+				total
+			);
+		},
+		[ priceTypeEntities ]
+	);
 
-	const calculateTicketBasePrice = useCallback( (
-		total,
-		priceEntities,
-		updateBasePrice = false
-	) => {
-		if ( isEmpty( priceEntities ) ) {
-			return;
-		}
-		priceEntities = reverse( sortBy( priceEntities, [ 'order', 'id' ] ) );
-		const basePriceTotal = priceEntities.reduce( ( newTotal, priceEntity ) => {
-			const priceTypeEntity = getPriceTypeForPrice( priceEntity, priceTypeEntities );
-			const amount = priceEntity.amount instanceof Money ?
-				priceEntity.amount.toNumber() :
-				priceEntity.amount;
-			return accrueAmount( reverseCases(
-				newTotal,
-				amount,
-				priceTypeEntity.isPercent
-			) )( newTotal )( priceTypeEntity.pbtId );
-		}, total );
-		if ( updateBasePrice ) {
-			const basePrice = getBasePrice( priceEntities );
-			basePrice.amount = new Money( basePriceTotal, SiteCurrency );
-		}
-		return basePriceTotal;
-	}, [ priceTypeEntities ] );
+	const calculateTicketBasePrice = useCallback(
+		(
+			total,
+			priceEntities,
+			updateBasePrice = false
+		) => {
+			if ( isEmpty( priceEntities ) ) {
+				return;
+			}
+			priceEntities = reverse( sortBy( priceEntities, [ 'order', 'id' ] ) );
+			const basePriceTotal = priceEntities.reduce(
+				( newTotal, priceEntity ) => {
+					const priceTypeEntity = getPriceTypeForPrice(
+						priceEntity,
+						priceTypeEntities
+					);
+					return accrueAmount(
+						basePriceCalculations(
+							newTotal,
+							getPriceAmount( priceEntity ),
+							priceTypeEntity.isPercent
+						)
+					)( newTotal )( priceTypeEntity.pbtId );
+				},
+				total
+			);
+			if ( updateBasePrice ) {
+				const basePrice = getBasePrice( priceEntities );
+				basePrice.amount = new Money( basePriceTotal, SiteCurrency );
+			}
+			return basePriceTotal;
+		},
+		[ priceTypeEntities ]
+	);
 
-	const calculateTicketTotals = useCallback( ( total, priceEntities, reverseCalculate ) => {
-		return reverseCalculate ?
-			calculateTicketBasePrice( total, priceEntities ) :
-			calculateTicketTotal( total, priceEntities );
-	} );
+	const calculateTicketTotals = useCallback(
+		( total, priceEntities, reverseCalculate ) => {
+			return reverseCalculate ?
+				calculateTicketBasePrice( total, priceEntities ) :
+				calculateTicketTotal( total, priceEntities );
+		},
+		[ calculateTicketBasePrice, calculateTicketTotal ]
+	);
 
 	return {
 		calculateTicketBasePrice,
