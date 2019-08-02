8def34e4b707a9e3e5fa2661bf153cfd7ed2e7ed
Brent Christensen <brent@eventespresso.com>
2019-08-02 15:15:20 -0700

    add IconButtonInput component

---
+++ assets/src/components/form/inputs/icon-button-input.js
@@ -0,0 +1,67 @@
+/**
+ * External imports
+ */
+import PropTypes from 'prop-types';
+import { Field } from 'react-final-form';
+import { IconButton } from '@wordpress/components';
+
+/**
+ * connects a WP CheckboxControl or ToggleControl with React-Final-Form
+ *
+ * @function
+ * @param {string} icon
+ * @param {Function} onClick
+ * @param {string} htmlId
+ * @param {string} htmlClass
+ * @param {string} helpTextID
+ * @param {Object} dataSet
+ * @param {Object} attributes
+ * @return {string} rendered date name form row
+ */
+const EspressoIconButton = ( {
+	icon,
+	onClick,
+	htmlId,
+	htmlClass,
+	helpTextID,
+	dataSet,
+	...attributes
+} ) => {
+	htmlClass = htmlClass ?
+		`${ htmlClass } ee-toggle-input` :
+		'ee-toggle-input';
+	return (
+		<IconButton
+			icon={ icon }
+			onClick={ onClick }
+			id={ htmlId }
+			className={ htmlClass }
+			aria-describedby={ helpTextID }
+			{ ...dataSet }
+			{ ...attributes }
+		/>
+	);
+};
+
+EspressoIconButton.propTypes = {
+	icon: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired,
+	htmlId: PropTypes.string.isRequired,
+	htmlClass: PropTypes.string,
+	helpTextID: PropTypes.string,
+	dataSet: PropTypes.object,
+};
+
+/**
+ * @function
+ * @param {Object} attributes
+ * @return {Object} rendered input
+ */
+export const IconButtonInput = ( attributes ) => {
+	return (
+		<Field
+			component={ EspressoIconButton }
+			{ ...attributes }
+		/>
+	);
+};
