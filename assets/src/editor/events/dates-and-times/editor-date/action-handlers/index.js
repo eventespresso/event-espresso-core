06a19fcce053c6c62d7f5f932f8247428a8f3196
Brent Christensen <brent@eventespresso.com>
2019-08-05 14:58:16 -0700

    convert HOCs into custom hooks

--- assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
+++ assets/src/editor/events/dates-and-times/editor-date/action-handlers/index.js
@@ -1,3 +1,3 @@
-export { default as withCopyDateEntity } from './with-copy-date-entity';
+export { default as useCopyDateEntity } from './use-copy-date-entity';
 export { default as withUpdateEventDateRelation } from './with-update-event-date-relation';
-export { default as withTrashDateEntity } from './with-trash-date-entity';
+export { default as useTrashDateEntity } from './use-trash-date-entity';
