ca74d3c445abb6abfc37568763a429c54d76bed8
Brent Christensen <brent@eventespresso.com>
2019-08-09 13:05:52 -0700

    extract ticket creation logic into new AddNewTicketButton with hooks

--- assets/src/editor/events/tickets/editor-ticket/editor-ticket-entities-list.js
+++ assets/src/editor/events/tickets/editor-ticket/editor-ticket-entities-list.js
@@ -1,23 +1,15 @@
 /**
  * External dependencies
  */
-import { find } from 'lodash';
 import { compose, createHigherOrderComponent } from '@wordpress/compose';
-import { withDispatch } from '@wordpress/data';
-import { useState, useMemo, useEffect } from '@wordpress/element';
-import {
-	EntityList,
-	EspressoButton,
-	twoColumnAdminFormLayout,
-} from '@eventespresso/components';
-import { __, _x, sprintf } from '@eventespresso/i18n';
+import { useEffect } from '@wordpress/element';
+import { EntityList, twoColumnAdminFormLayout } from '@eventespresso/components';
+import { _x, sprintf } from '@eventespresso/i18n';
 
 /**
  * Internal dependencies
  */
-import { withPriceTypeEntities, withEditorTicketEntities } from '../../hocs';
-import { withTicketPriceCalculatorFormModal } from './price-calculator';
-import { withTicketEntityFormModal } from './edit-form';
+import AddNewTicketButton from './add-new-ticket-button';
 import { EditorTicketEntitiesGridView } from './grid-view/';
 import { EditorTicketEntitiesListView } from './list-view/';
 import { withPaginatedTicketEntitiesListAndFilterBar } from './filter-bar';
@@ -30,7 +22,6 @@ const {
 
 const EditorTicketEntitiesList = ( {
 	entities,
-	addNewTicketEntity,
 	toggleTicketEditor,
 	ticketEntity = null,
 	view = 'grid',
@@ -41,22 +32,6 @@ const EditorTicketEntitiesList = ( {
 			toggleTicketEditor();
 		}
 	}, [ ticketEntity ] );
-	/**
-	 * @function
-	 * @return {Object} rendered button
-	 */
-	const addNewTicketEntityButton = useMemo(
-		() => {
-			return (
-				<EspressoButton
-					icon="tickets-alt"
-					buttonText={ __( 'Add New Ticket', 'event_espresso' ) }
-					onClick={ addNewTicketEntity }
-				/>
-			);
-		},
-		[ addNewTicketEntity ]
-	);
 
 	return (
 		<FormWrapper>
@@ -75,76 +50,15 @@ const EditorTicketEntitiesList = ( {
 					String.fromCharCode( 8230 )
 				) }
 			/>
-			<FormSaveCancelButtons submitButton={ addNewTicketEntityButton } />
+			<FormSaveCancelButtons submitButton={ <AddNewTicketButton /> } />
 		</FormWrapper>
 	);
 };
 
-const withNewTicketEntity = createHigherOrderComponent(
-	( WrappedComponent ) => ( props ) => {
-		const [ newTicket, setNewTicketEntity ] = useState( null );
-		const basePriceTypeEntity = useMemo(
-			() => {
-				if ( ! props.priceTypeEntitiesLoaded ) {
-					return null;
-				}
-				return find(
-					props.priceTypeEntities,
-					( priceType ) => priceType.pbtId === 1
-				);
-			},
-			[ props.priceTypeEntities, props.priceTypeEntitiesLoaded ]
-		);
-		return <WrappedComponent
-			setNewTicketEntity={ setNewTicketEntity }
-			ticketEntity={ newTicket }
-			basePriceTypeEntity={ basePriceTypeEntity }
-			{ ...props }
-		/>;
-	},
-	'withNewTicket'
-);
-
-const createNewTicketEntity = withDispatch(
-	( dispatch, { setNewTicketEntity, basePriceTypeEntity } ) => {
-		const { createEntity, createRelations } = dispatch( 'eventespresso/core' );
-		const addNewTicketEntity = async ( event ) => {
-			if ( event && event.preventDefault ) {
-				event.preventDefault();
-				event.stopPropagation();
-			}
-			const newTicket = await createEntity( 'ticket', {} );
-			const newBasePrice = await createEntity(
-				'price',
-				{ PRT_ID: basePriceTypeEntity.id }
-			);
-			createRelations(
-				'ticket',
-				newTicket.id,
-				'price',
-				[ newBasePrice ]
-			);
-			setNewTicketEntity( newTicket );
-		};
-		return { addNewTicketEntity };
-	}
-);
-
 export default compose( [
-	withPriceTypeEntities,
-	withNewTicketEntity,
-	withEditorTicketEntities,
 	withPaginatedTicketEntitiesListAndFilterBar(),
 	// add ticketAssignments ManagerModal onClose
-	withTicketAssignmentsManagerModal( () => (
-		{
-			title: __(
-				'Ticket Assignments for All Event Dates',
-				'event_espresso'
-			),
-			closeButtonLabel: null,
-		}
-	) ),
+	withTicketAssignmentsManagerModal,
 	createHigherOrderComponent(
 		( WrappedComponent ) => ( props ) => {
 			return <WrappedComponent
@@ -154,19 +68,25 @@ export default compose( [
 		},
 		'withOnCloseTicketEditor'
 	),
-	withTicketPriceCalculatorFormModal,
-	withTicketEntityFormModal,
-	createNewTicketEntity,
+	// withTicketPriceCalculatorFormModal,
 ] )( EditorTicketEntitiesList );
 
 /**
  * a trimmed down ticket list: only includes ticket price calculator
  */
 export const EditorTicketEntitiesOnlyList = compose( [
-	withPriceTypeEntities,
-	withNewTicketEntity,
-	withEditorTicketEntities,
-	withTicketPriceCalculatorFormModal,
-	withTicketEntityFormModal,
-	createNewTicketEntity,
+	// withEditorTicketEntities,
+	// withTicketPriceCalculatorFormModal,
 ] )( EditorTicketEntitiesList );
+
+/*
+( () => (
+		{
+			title: __(
+				'Ticket Assignments for All Event Dates',
+				'event_espresso'
+			),
+			closeButtonLabel: null,
+		}
+	) )
+*/
