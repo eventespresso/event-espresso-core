29e8f6da1e7478ada7a48fc9e634438f55e6ae43
Brent Christensen <brent@eventespresso.com>
2019-08-08 18:08:29 -0700

    update exports and remove withEditor

--- assets/src/editor/hocs/editor-modal/index.js
+++ assets/src/editor/hocs/editor-modal/index.js
@@ -1,2 +1,5 @@
-export { default as withEditor } from './with-editor';
+export { default as EditorModal } from './editor-modal';
+export { default as useCloseEditor } from './use-close-editor';
+export { default as useIsEditorOpen } from './use-is-editor-open';
+export { default as useOpenEditor } from './use-open-editor';
 export { default as withEditorModal } from './with-editor-modal';
