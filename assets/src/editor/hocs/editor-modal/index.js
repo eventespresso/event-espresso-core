490b618baf2f50950f3a2b4b7a3e51128ff64905
Brent Christensen <brent@eventespresso.com>
2019-08-28 14:02:35 -0700

    convert withEditorModal HOC to EditorModal functional component

--- assets/src/editor/hocs/editor-modal/index.js
+++ assets/src/editor/hocs/editor-modal/index.js
@@ -2,4 +2,3 @@ export { default as EditorModal } from './editor-modal';
 export { default as useCloseEditor } from './use-close-editor';
 export { default as useIsEditorOpen } from './use-is-editor-open';
 export { default as useOpenEditor } from './use-open-editor';
-export { default as withEditorModal } from './with-editor-modal';
