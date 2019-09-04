721da818c40889706f74f9d001b25b00200b4e76
Brent Christensen <brent@eventespresso.com>
2019-09-04 09:48:25 -0700

    display LoadingNotice if base price is being generated

--- assets/src/editor/events/tickets/editor-ticket/price-calculator/form-info-warning-no-ticket-prices.js
+++ assets/src/editor/events/tickets/editor-ticket/price-calculator/form-info-warning-no-ticket-prices.js
@@ -4,32 +4,35 @@
 import { useMemo } from '@wordpress/element';
 import { Dashicon } from '@wordpress/components';
 import { __ } from '@eventespresso/i18n';
-import { twoColumnAdminFormLayout } from '@eventespresso/components';
+import {
+	LoadingNotice,
+	twoColumnAdminFormLayout,
+} from '@eventespresso/components';
 
 const { FormInfo } = twoColumnAdminFormLayout;
 
-const FormInfoWarningNoTicketPrices = ( { priceCount } ) => useMemo( () => (
-	priceCount < 1 ? (
+const FormInfoWarningNoTicketPrices = ( {
+	priceCount,
+	inProgress = false,
+} ) => useMemo( () => priceCount < 1 ? (
+	<>
 		<FormInfo
 			formInfo={
 				__(
-					'No ticket prices have been set! A base price' +
-					' is required at the very minimum. Please' +
-					' provide a "Label" and "Amount" and then' +
-					' click the %%var%% button in the "Actions"' +
-					' column to add the base price.',
+					'No ticket prices have been set! A base price is' +
+					' required at the very minimum. Please wait while' +
+					' a new base price is generated for you.',
 					'event_espresso'
 				)
 			}
-			formInfoVars={ [
-				<Dashicon icon="plus-alt" key={ 0 } />,
-			] }
+			formInfoVars={ [ <Dashicon icon="plus-alt" key={ 0 } /> ] }
 			dashicon={ 'warning' }
 			dismissable={ false }
 			colSize={ 11 }
 			offset={ 1 }
 		/>
-	) : null
-), [] );
+		<LoadingNotice loading={ inProgress } />
+	</>
+) : null, [ priceCount, inProgress ] );
 
 export default FormInfoWarningNoTicketPrices;
