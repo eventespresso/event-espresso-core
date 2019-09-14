3d5fb326d3dabbaece1b9f89fc4b2b8525e279ab
Brent Christensen <brent@eventespresso.com>
2019-09-13 18:58:19 -0700

    put error messages into an object and export for testing

--- assets/src/editor/events/ticket-assignments-manager/hooks/use-assignments-error-message.js
+++ assets/src/editor/events/ticket-assignments-manager/hooks/use-assignments-error-message.js
@@ -5,6 +5,33 @@ import PropTypes from 'prop-types';
 import { useMemo } from '@wordpress/element';
 import { __ } from '@eventespresso/i18n';
 
+export const assignmentsErrorMessages = {
+	singleDate: __(
+		'Event Dates must always have at least one Ticket assigned ' +
+		'to them. If the current assignment is not correct, assign the ' +
+		'correct Ticket first, then remove others as required.',
+		'event_espresso'
+	),
+	singleTicket: __(
+		'Tickets must always have at least one Event Date assigned ' +
+		'to them. If the current assignment is not correct, assign the ' +
+		'correct Event Date first, then remove others as required.',
+		'event_espresso'
+	),
+	multiTickets: __(
+		'Tickets must always have at least one date assigned ' +
+		'to them but one or more of the tickets below does not have any. ' +
+		'Please correct the assignments for the highlighted cells.',
+		'event_espresso'
+	),
+	multiDates: __(
+		'Event Dates must always have at least one Ticket assigned ' +
+		'to them but one or more of the Event Dates below does not have any. ' +
+		'Please correct the assignments for the highlighted cells.',
+		'event_espresso'
+	),
+};
+
 /**
  * @param {number} dateCount                number of event dates
  * @param {number} ticketCount              number of tickets
@@ -28,46 +55,22 @@ const useAssignmentsErrorMessage = ( {
 			datesMissing && ! ticketsMissing &&
 			( dateCount === 1 || ticketCount === 1 )
 		) {
-			return __(
-				'Event Dates must always have at least one Ticket' +
-				' assigned to them. If the current assignment is not' +
-				' correct, assign the correct Ticket first, then remove' +
-				' others as required.',
-				'event_espresso'
-			);
+			return assignmentsErrorMessages.singleDate;
 		}
 
 		if (
 			( datesMissing && ticketsMissing && ticketCount === 1 ) ||
 			( ticketsMissing && dateCount === 1 )
 		) {
-			return __(
-				'Tickets must always have at least one Event Date' +
-				' assigned to them. If the current assignment is not' +
-				' correct, assign the correct Event Date first, then' +
-				' remove others as required.',
-				'event_espresso'
-			);
+			return assignmentsErrorMessages.singleTicket;
 		}
 
 		if ( ticketsMissing ) {
-			return __(
-				'Tickets must always have at least one date assigned to' +
-				' them but one or more of the tickets below does not' +
-				' have any. Please correct the assignments for the' +
-				' highlighted cells.',
-				'event_espresso'
-			);
+			return assignmentsErrorMessages.multiTickets;
 		}
 
 		if ( datesMissing ) {
-			return __(
-				'Event Dates must always have at least one Ticket' +
-				' assigned to them but one or more of the Event Dates' +
-				' below does not have any. Please correct the' +
-				' assignments for the highlighted cells.',
-				'event_espresso'
-			);
+			return assignmentsErrorMessages.multiDates;
 		}
 
 		return '';
