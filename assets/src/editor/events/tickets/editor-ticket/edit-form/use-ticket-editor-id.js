fb3992c54e6fb8554f5008bb066156583319de66
Brent Christensen <brent@eventespresso.com>
2019-09-26 14:32:12 -0700

    add prefix to ticket editor id

--- assets/src/editor/events/tickets/editor-ticket/edit-form/use-ticket-editor-id.js
+++ assets/src/editor/events/tickets/editor-ticket/edit-form/use-ticket-editor-id.js
@@ -6,12 +6,16 @@ import { isModelEntityOfModel } from '@eventespresso/validators';
 /**
  * @function
  * @param {Object} ticket
+ * @param {string} prefix
  * @return {string} editor id for ticket
  */
-const useTicketEditorId = ( ticket ) => (
-	isModelEntityOfModel( ticket, 'ticket' ) ?
+const useTicketEditorId = ( ticket, prefix = '' ) => {
+	const editorId = isModelEntityOfModel( ticket, 'ticket' ) ?
 		`ticket-editor-${ ticket.id }` :
-		''
-);
+		'';
+	return prefix !== '' && editorId !== '' ?
+		`${ prefix }-${ editorId }` :
+		editorId;
+};
 
 export default useTicketEditorId;
