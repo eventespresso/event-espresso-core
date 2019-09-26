0f2ba3a2edd13b93b0485646f3b05f4e19cc6e98
Brent Christensen <brent@eventespresso.com>
2019-09-26 15:38:53 -0700

    massively improve wonky reset logic; fix prop types

--- assets/src/components/form/inputs/base/option-checked-state.js
+++ assets/src/components/form/inputs/base/option-checked-state.js
@@ -7,26 +7,24 @@ import PropTypes from 'prop-types';
 /**
  * generates the initial checked state
  * for a set of checkbox or radio button
- * options based on the set defaultValue.
- * This can be overridden by supplying
- * an alternate value via the fourth param,
- * which is used to reset all values.
+ * options based on the set values.
+ * This can be overridden by setting reset to true
+ * which will uncheck all options.
  *
  * @function
  * @param {string} inputId
  * @param {Array} options
- * @param {mixed} values
- * @param {mixed} reset
+ * @param {Array} values
+ * @param {boolean} reset
  * @return {Object} state
  */
-export const OptionCheckedState = ( inputId, options, values, reset ) => {
+const OptionCheckedState = ( inputId, options, values, reset = false ) => {
 	const checked = {};
 	options = isArray( options ) ? options : [];
 	values = isArray( values ) ? values : [ values ];
 	options.forEach( function( option, index ) {
 		const key = inputId + '-' + index;
-		const isSelected = indexOf( values, option.value ) !== -1;
-		checked[ key ] = reset === undefined ? isSelected : reset;
+		checked[ key ] = ! reset && indexOf( values, option.value ) !== -1;
 	} );
 	return checked;
 };
@@ -34,12 +32,11 @@ export const OptionCheckedState = ( inputId, options, values, reset ) => {
 OptionCheckedState.propTypes = {
 	inputId: PropTypes.string.isRequired,
 	options: PropTypes.array.isRequired,
-	defaultValue: PropTypes.oneOfType( [
+	values: PropTypes.arrayOf( [
 		PropTypes.number,
 		PropTypes.string,
 	] ),
-	value: PropTypes.oneOfType( [
-		PropTypes.number,
-		PropTypes.string,
-	] ),
+	reset: PropTypes.bool,
 };
+
+export default OptionCheckedState;
