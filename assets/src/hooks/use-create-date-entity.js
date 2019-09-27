6f5d1e55c8fcc45030c230e8aff6a56babb9942b
Brent Christensen <brent@eventespresso.com>
2019-09-26 17:08:28 -0700

    set all entity default values upon creation

--- assets/src/hooks/use-create-date-entity.js
+++ assets/src/hooks/use-create-date-entity.js
@@ -3,6 +3,7 @@
  */
 import { useDispatch } from '@wordpress/data';
 import { useCallback } from '@wordpress/element';
+import { ServerDateTime, Duration } from '@eventespresso/value-objects';
 
 /**
  * Internal dependencies
@@ -15,7 +16,34 @@ const useCreateDateEntity = ( event, cacheNewDate ) => {
 	const updateEventDateRelation = useCreateRelationForEventToEventDate();
 	return useCallback(
 		async () => {
-			const newDate = await createEntity( 'datetime', {} );
+			const nowJs = new Date();
+			nowJs.setHours(
+				nowJs.getHours(),
+				Math.ceil( nowJs.getMinutes() / 15 ) * 15,
+				0,
+				0
+			);
+			const now = ServerDateTime.fromJSDate( nowJs );
+			const newDate = await createEntity(
+				'datetime',
+				{
+					EVT_ID: event.id,
+					DTT_name: '',
+					DTT_description: '',
+					DTT_EVT_start: now.plus(
+						Duration.fromObject( { days: 30 } )
+					),
+					DTT_EVT_end: now.plus(
+						Duration.fromObject( { days: 30, hours: 2 } )
+					),
+					DTT_reg_limit: -1,
+					DTT_sold: 0,
+					DTT_reserved: 0,
+					DTT_order: 0,
+					DTT_parent: 0,
+					DTT_deleted: false,
+				}
+			);
 			await updateEventDateRelation( event, newDate );
 			cacheNewDate( newDate );
 		},
