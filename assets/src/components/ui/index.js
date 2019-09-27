407bdb177ff1633fd25b97f7fd4a9cf578e91e47
Brent Christensen <brent@eventespresso.com>
2019-09-27 09:08:57 -0700

    move entity list into own file and use index for exports

--- assets/src/components/ui/index.js
+++ assets/src/components/ui/index.js
@@ -3,17 +3,15 @@ export { default as CalendarDateRange } from './dates-and-times/calendar-date-ra
 export { default as CalendarPageDate } from './dates-and-times/calendar-page-date';
 export { default as MediumCalendarDate } from './dates-and-times/medium-calendar-date';
 export { default as DropDownMenu } from './drop-down-menu';
-export {
-	EntityActionMenuItem,
-	useEntityActionMenuItems,
-} from './entity-actions-menu';
+export { EntityActionMenuItem, useEntityActionMenuItems }
+	from './entity-actions-menu';
 export { default as EntityDetailsPanel }
 	from './entity-details-panel/entity-details-panel';
-export { default as EntityList } from './entity-list';
 export { default as EntityPaperFrame } from './entity-paper-frame';
 export { default as EspressoButton } from './espresso-button';
 export { default as IconMenuItem } from './icon-menu-item/icon-menu-item';
 export { default as LoadingNotice } from './loading-notice';
+export * from './entity-list';
 export * from './entity-list-filter-bar';
 export * from './entity-pagination';
 export * from './image';
