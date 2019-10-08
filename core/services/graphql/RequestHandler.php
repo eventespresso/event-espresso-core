0aef717303f09f768f5385bbcb45782fa16f8278
Brent Christensen <brent@eventespresso.com>
2019-10-07 18:39:03 -0700

    move collection entity loading into collection

--- core/services/graphql/RequestHandler.php
+++ core/services/graphql/RequestHandler.php
@@ -2,10 +2,8 @@
 
 namespace EventEspresso\core\services\graphql;
 
-use EventEspresso\core\services\collections\CollectionDetails;
 use EventEspresso\core\services\collections\CollectionDetailsException;
 use EventEspresso\core\services\collections\CollectionInterface;
-use EventEspresso\core\services\collections\CollectionLoader;
 use EventEspresso\core\services\collections\CollectionLoaderException;
 use EventEspresso\core\services\loaders\LoaderInterface;
 use EventEspresso\core\services\request\RequestInterface;
@@ -53,76 +51,19 @@ class RequestHandler
 
 
     /**
-     * @return CollectionInterface|ResolverInterface[]
-     * @throws CollectionLoaderException
-     * @throws CollectionDetailsException
-     * @since 4.9.71.p
-     */
-    private function populateResolverCollection()
-    {
-        $loader = new CollectionLoader(
-            new CollectionDetails(
-            // collection name
-                ResolverCollection::COLLECTION_NAME,
-                // collection interface
-                'EventEspresso\core\services\graphql\ResolverInterface',
-                // FQCNs for classes to add (all classes within each namespace will be loaded)
-                apply_filters(
-                    'FHEE__EventEspresso_core_services_graphql_RequestHandler__populateResolverCollection__collection_FQCNs',
-                    ['EventEspresso\core\domain\services\graphql\resolvers']
-                ),
-                // filepaths to classes to add
-                array(),
-                // file mask to use if parsing folder for files to add
-                '',
-                // what to use as identifier for collection entities
-                // using CLASS NAME prevents duplicates (works like a singleton)
-                CollectionDetails::ID_CLASS_NAME
-            ),
-            $this->resolvers
-        );
-        return $loader->getCollection();
-    }
-
-
-    /**
      * @throws CollectionDetailsException
      * @throws CollectionLoaderException
      * @since $VID:$
      */
     public function init()
     {
-        // getting data from current request ( $_GET, $_POST, $_REQUEST, etc )
-        // retrieve ALL params from $_REQUEST
-        $all = $this->request->getParams();
-        // retrieve a single param from $_REQUEST
-        $one = $this->request->getRequestParam( 'key', 'default value if not found' );
-        // retrieve params matching a pattern from $_REQUEST using regex
-        $matches = $this->request->getMatch( '/regex-pattern-with-?-or-*-wildcards/', 'default value if not found' );
-
-        $query_data = new \stdClass();
-        $query_data->eventId = 1;
-
-        // loading classes
-
-        // get shared instance (works like a singleton)
-        $sharedClass = $this->loader->getShared(
-            'EventEspresso\core\domain\services\graphql\mutators\QueryMutator',
-            [ $query_data ]
-        );
-        // get NON-shared unique instance (creates new instance every call)
-        $uniqueClass = $this->loader->getNew(
-            'EventEspresso\core\domain\services\graphql\mutators\QueryMutator',
-            [ $query_data ]
-        );
-
         // Register GQL resolvers by first loading all ResolverInterface objects
         // in EventEspresso\core\domain\services\graphql\resolvers.
         // Other folders can be added to that list by addons
         // simply by using the collection FQCNs filter in that method.
         // Alternately, an addon could load a shared instance of the collection
         // and manually add/remove/replace resolvers as needed
-        $this->populateResolverCollection();
+        $this->resolvers->loadResolvers();
         add_action('graphql_register_types', [$this, 'registerResolvers']);
     }
 
--- core/services/graphql/ResolverCollection.php
+++ core/services/graphql/ResolverCollection.php
@@ -4,6 +4,11 @@ namespace EventEspresso\core\services\graphql;
 
 use EventEspresso\core\exceptions\InvalidInterfaceException;
 use EventEspresso\core\services\collections\Collection;
+use EventEspresso\core\services\collections\CollectionDetails;
+use EventEspresso\core\services\collections\CollectionDetailsException;
+use EventEspresso\core\services\collections\CollectionInterface;
+use EventEspresso\core\services\collections\CollectionLoader;
+use EventEspresso\core\services\collections\CollectionLoaderException;
 
 /**
  * Class ResolverCollection
@@ -18,6 +23,10 @@ class ResolverCollection extends Collection
 
     const COLLECTION_NAME = 'espresso_graphql_resolvers';
 
+    /**
+     * @var CollectionLoader $loader
+     */
+    protected $loader;
 
     /**
      * ResolverCollection constructor
@@ -30,6 +39,52 @@ class ResolverCollection extends Collection
             'EventEspresso\core\services\graphql\ResolverInterface',
             ResolverCollection::COLLECTION_NAME
         );
+    }
+
+
+    /**
+     * @throws CollectionDetailsException
+     * @throws CollectionLoaderException
+     * @since $VID:$
+     */
+    private function loadCollection()
+    {
+        if (! $this->loader instanceof CollectionLoader) {
+            $this->loader = new CollectionLoader(
+                new CollectionDetails(
+                // collection name
+                    ResolverCollection::COLLECTION_NAME,
+                    // collection interface
+                    'EventEspresso\core\services\graphql\ResolverInterface',
+                    // FQCNs for classes to add (all classes within each namespace will be loaded)
+                    apply_filters(
+                        'FHEE__EventEspresso_core_services_graphql_ResolverCollection__loadCollection__collection_FQCNs',
+                        ['EventEspresso\core\domain\services\graphql\resolvers']
+                    ),
+                    // filepaths to classes to add
+                    array(),
+                    // file mask to use if parsing folder for files to add
+                    '',
+                    // what to use as identifier for collection entities
+                    // using CLASS NAME prevents duplicates (works like a singleton)
+                    CollectionDetails::ID_CLASS_NAME
+                ),
+                $this
+            );
+        }
+    }
+
+
+    /**
+     * @return CollectionInterface
+     * @throws CollectionDetailsException
+     * @throws CollectionLoaderException
+     * @since $VID:$
+     */
+    public function loadResolvers()
+    {
+        $this->loadCollection();
+        return $this->loader->getCollection();
     }
 
 
