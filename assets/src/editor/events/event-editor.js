135c7c4806da3c0a5aa7c24e7e6ed50522b2b274
Brent Christensen <brent@eventespresso.com>
2019-10-15 17:19:41 -0700

    get event ID from dom data dump

--- assets/src/editor/events/event-editor.js
+++ assets/src/editor/events/event-editor.js
@@ -9,7 +9,12 @@ const container = document.getElementById( 'normal-sortables' );
 const editor = document.createElement( 'div' );
 editor.className = 'ee-editor-div';
 container.prepend( editor );
-const eventId = window.eeEditorEventId;
+const eventData = window.eeEditorEventData;
+console.log( '%c > eventData: ', 'color: #BCBDAC;', eventData );
+const eventId = eventData.event && eventData.event.EVT_ID ?
+	eventData.event.EVT_ID :
+	0;
+console.log( '%c > eventId: ', 'color: #BCBDAC;', eventId );
 
 if ( eventId ) {
 	render(
