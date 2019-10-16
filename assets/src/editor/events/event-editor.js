b957454a9d9e88b495131f71d8e2de1fbd0e0bfa
Brent Christensen <brent@eventespresso.com>
2019-10-16 09:08:28 -0700

    comment out console.logs in order to pass linting

--- assets/src/editor/events/event-editor.js
+++ assets/src/editor/events/event-editor.js
@@ -10,11 +10,11 @@ const editor = document.createElement( 'div' );
 editor.className = 'ee-editor-div';
 container.prepend( editor );
 const eventData = window.eeEditorEventData;
-console.log( '%c > eventData: ', 'color: #BCBDAC;', eventData );
+// console.log( '%c > eventData: ', 'color: #BCBDAC;', eventData );
 const eventId = eventData.event && eventData.event.EVT_ID ?
 	eventData.event.EVT_ID :
 	0;
-console.log( '%c > eventId: ', 'color: #BCBDAC;', eventId );
+// console.log( '%c > eventId: ', 'color: #BCBDAC;', eventId );
 
 if ( eventId ) {
 	render(
