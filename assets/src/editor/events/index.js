4a7e5fb87a38ab69d801ef54b80f5204dde7e99c
Brent Christensen <brent@eventespresso.com>
2019-10-18 11:46:16 -0700

    grab dom data and render EventEditor

--- assets/src/editor/events/index.js
+++ assets/src/editor/events/index.js
@@ -1,6 +1,24 @@
-export * from './dates-and-tickets-metabox';
-export * from './dates-and-times';
-export * from './ticket-assignments-manager';
-export * from './tickets';
+/**
+ * External imports
+ */
+import { render } from '@wordpress/element';
 
-import './event-editor';
+/**
+ * Internal imports
+ */
+import EventEditor from './event-editor';
+
+// find the 'normal-sortables' metabox
+const container = document.getElementById( 'normal-sortables' );
+// create and place our 'ee-editor-div' div before that
+const editor = document.createElement( 'div' );
+editor.className = 'ee-editor-div';
+container.prepend( editor );
+// then load the editor data we dumped into DOM and render theEventEditor
+const eventData = window.eeEditorEventData;
+if ( eventData.eventId ) {
+	render(
+		<EventEditor eventId={ eventData.eventId } eventData={ eventData } />,
+		editor
+	);
+}
