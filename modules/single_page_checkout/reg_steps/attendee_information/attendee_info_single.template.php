8b78dcaabb6119a3dda94b0171054d418fb5af41
Rafael Angeline <me@rafaelangeline.com>
2019-10-24 18:06:18 -0300

    Add attendee info single template logic

---
+++ modules/single_page_checkout/reg_steps/attendee_information/attendee_info_single.template.php
@@ -0,0 +1,91 @@
+<?php
+/** @var boolean $revisit */
+/** @var array $ticket_line_item */
+/** @var string $default_hidden_inputs */
+?>
+<?php if (!is_admin()) : ?>
+    <p id="spco-attendee_information-pg" class="spco-steps-pg small-text drk-grey-text">
+        <?php echo apply_filters(
+            'FHEE__registration_page_attendee_information__attendee_information_pg',
+            sprintf(
+                __(
+                    'In order to process your registration, we ask you to provide the following information.%1$sPlease note that all fields marked with an asterisk (%2$s) are required.',
+                    'event_espresso'
+                ),
+                '<br />',
+                '<span class="asterisk">*</span>'
+            )
+        ); ?>
+</p>
+<?php endif; ?>
+
+<?php
+$att_nmbr = 0;
+$prev_event = 0;
+$prev_ticket = 0;
+
+if (count($registrations) > 0) {
+    ?>
+
+    <div id="spco-attendee-panel-dv-single"
+        class="spco-attendee-panel-dv spco-attendee-ticket-single">
+
+        <div class="spco-ticket-info-dv small-text">
+            <?php if (!is_admin()) : ?>
+                <h5><?php _e('Details', 'event_espresso'); ?></h5>
+            <?php endif; ?>
+            <table class="spco-ticket-details">
+                <thead>
+                <tr>
+                    <th scope="col" width="" class="jst-left"><?php _e('Name and Description', 'event_espresso'); ?></th>
+                    <th scope="col" width="7.5%" class="jst-rght">
+                        <?php _e(
+                            'Qty',
+                            'event_espresso'
+                        ); ?></th>
+                    <th scope="col" width="17.5%" class="jst-rght">
+                        <?php _e(
+                            'Price',
+                            'event_espresso'
+                        ); ?></th>
+                    <th scope="col" width="17.5%" class="jst-rght">
+                        <?php _e(
+                            'Total',
+                            'event_espresso'
+                        ); ?></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                // Display all tickets inside.
+                foreach ($registrations as $registration) {
+                    if ($registration instanceof EE_Registration) {
+                        echo $ticket_line_item[ $registration->ticket()->ID() ];
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <?php
+    // Display the forms below the table.
+    foreach ($registrations as $registration) {
+        if ($registration instanceof EE_Registration) {
+            // Attendee Questions.
+            $reg_form = EE_Template_Layout::get_subform_name($registration->reg_url_link());
+            echo ${$reg_form};
+        } // if ( $registration instanceof EE_Registration )
+    } // end foreach ( $registrations as $registration )
+
+    echo $default_hidden_inputs;
+} // end if ( count( $registrations ) > 0 )
+
+?>
+
+<div class="clearfix">
+    <a id="spco-display-event-questions-lnk" class="act-like-link smaller-text hidden hide-if-no-js float-right">
+        <?php _e('show&nbsp;event&nbsp;questions', 'event_espresso'); ?>
+    </a>
+</div>
