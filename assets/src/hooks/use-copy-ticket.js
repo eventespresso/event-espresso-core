61abf974ad017b45cea4272054e280f9c11403de
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-01 21:42:03 +0200

    Reverted string to constant conversion.

--- assets/src/hooks/use-copy-ticket.js
+++ assets/src/hooks/use-copy-ticket.js
@@ -13,8 +13,6 @@ import useCreateRelationsForTicketToEventDates
 	from './use-create-relations-for-ticket-to-event-dates';
 import useCreateRelationsForTicketToPrices
 	from './use-create-relations-for-ticket-to-prices';
-import { MODELS } from '../data/model/constants';
-const { PRICE, PRICES, TICKET } = MODELS;
 
 const falseFunc = () => false;
 
@@ -22,18 +20,18 @@ const useCopyTicket = ( ticketEntity, dateEntities ) => {
 	const { createEntity } = useDispatch( 'eventespresso/core' );
 	const relatedPrices = useSelect( ( select ) => {
 		const { getRelatedEntities } = select( 'eventespresso/core' );
-		return getRelatedEntities( ticketEntity, PRICES );
+		return getRelatedEntities( ticketEntity, 'prices' );
 	}, [ ticketEntity ] );
-	const newPrices = useCloneEntities( relatedPrices, PRICE );
+	const newPrices = useCloneEntities( relatedPrices, 'price' );
 	const updateTicketDateRelations = useCreateRelationsForTicketToEventDates();
 	const updateTicketPriceRelations = useCreateRelationsForTicketToPrices();
 	return useCallback( async () => {
-		if ( ! isModelEntityOfModel( ticketEntity, TICKET ) ) {
+		if ( ! isModelEntityOfModel( ticketEntity, 'ticket' ) ) {
 			return falseFunc;
 		}
 
 		const newTicket = await createEntity(
-			TICKET,
+			'ticket',
 			ticketEntity.forClone
 		);
 
