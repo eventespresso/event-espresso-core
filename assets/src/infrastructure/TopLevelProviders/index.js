593e7a279d7ff49db684cffee35d382169cbe5fe
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-11 23:03:13 +0200

    Added ApolloProvider and added to TopLevelProviders.

---
+++ assets/src/infrastructure/TopLevelProviders/index.js
@@ -0,0 +1,17 @@
+/* Internal dependencies */
+import ApolloProvider from './Apollo';
+
+/**
+ * A collection of top level providers that are used by multiple parts of the application.
+ *
+ * @param {ReactElement} children The element that should be wrapped.
+ *
+ * @returns {ReactElement} The wrapped element.
+ */
+const TopLevelProviders = ( { children } ) => (
+	<ApolloProvider>
+		{ children }
+	</ApolloProvider>
+);
+
+export default TopLevelProviders;
