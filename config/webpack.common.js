82652e365ccdfaa2d7c564532b93342198e2a8d9
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-12 18:56:51 +0200

    Updated webpack config.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -243,7 +243,7 @@ const config = [
 const enhance = ( conf ) => {
 	const resolve = {
 		// Add '.ts' and '.tsx' as resolvable extensions.
-		extensions: [ '', '.webpack.js', '.web.js', '.ts', '.tsx', '.js' ],
+		extensions: [ '.webpack.js', '.web.js', '.ts', '.tsx', '.js' ],
 	};
 	const enhancedConf = { ...conf, resolve };
 
@@ -258,25 +258,31 @@ const enhance = ( conf ) => {
 };
 
 const addLoaders = ( conf ) => {
-	const loaders = [
-		// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-		{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-	];
-	const preLoaders = [
-		// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-		{ test: /\.js$/, loader: 'source-map-loader' },
+	const enhancedConf = { ...conf };
+
+	enhancedConf.module.rules = [
+		...conf.module.rules,
+		{
+			test: /\.ts(x?)$/,
+			exclude: /node_modules/,
+			use: [
+				{
+					loader: 'awesome-typescript-loader',
+				},
+			],
+		},
+		{
+			enforce: 'pre',
+			test: /\.js$/,
+			loader: 'source-map-loader',
+		},
 	];
 
-	return {
-		...conf,
-		module: {
-			...conf.module,
-			loaders,
-			preLoaders,
-		},
-	};
+	return enhancedConf;
 };
 
-const enhancedConfig = config.map( enhance ).map( addLoaders );
+const enhancedConfig = config
+	.map( enhance )
+	.map( addLoaders );
 
 module.exports = enhancedConfig;
