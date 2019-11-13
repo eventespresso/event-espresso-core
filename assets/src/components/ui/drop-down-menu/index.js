af7b7bff285a9a34483b89d67bc5418a78907821
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-13 19:04:36 +0200

    Reverted change in DropDownMenu.

--- assets/src/components/ui/drop-down-menu/index.js
+++ assets/src/components/ui/drop-down-menu/index.js
@@ -1,7 +1,6 @@
 /**
  * External imports
  */
-import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types'
 import { DropdownMenu } from '@wordpress/components';
