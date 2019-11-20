b1720b4824561eb9dc665cbdb7e1f5ecf84454b4
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-20 21:33:10 +0200

    Added some context to useDateCardData.

--- assets/prototype/domain/eventEditor/containers/useDateCardData.js
+++ assets/prototype/domain/eventEditor/containers/useDateCardData.js
@@ -1,5 +1,9 @@
-import gql from 'graphql-tag';
+import { createContext, useReducer, useEffect, useState } from '@wordpress/element';
 import { useQuery } from '@apollo/react-hooks';
+import get from 'lodash/get';
+import gql from 'graphql-tag';
+
+export const DateCardDataContext = createContext();
 
 const GET_DATE = gql`
 	query getDatetimes($eventId: Number) {
@@ -22,12 +26,48 @@ const GET_DATE = gql`
 	}
 `;
 
-const useDateCardData = ( { eventId } ) => {
+const reducer = ( state, action ) => {
+	switch ( action.type ) {
+		case 'SET_FIELD_VALUE':
+			return {
+				...state,
+				[ action.field ]: action.value
+			};
+	}
+};
+
+const useDateCardData = ( { children, eventId } ) => {
 	const { data } = useQuery( GET_DATE, {
 		variables: { eventId },
 	} );
+	const datetimes = get( data, [ 'eventBy', 'datetimes', 'nodes' ] );
 
-	return data;
+	const [ state, dispatch ] = useReducer(
+		reducer,
+		datetimes
+	);
+
+	const [ contextValue, setContextValue ] = useState( {
+		state,
+		dispatch,
+	} );
+
+	// Update context value and trigger re-render
+	// This patterns avoids unnecessary deep renders
+	// https://reactjs.org/docs/context.html#caveats
+	useEffect( () => {
+		// eslint-disable-next-line no-shadow
+		setContextValue( ( contextValue ) => ( {
+			...contextValue,
+			state
+		} ) );
+	}, [ state ] );
+
+	return (
+		<DateCardDataContext.Provider value={ contextValue }>
+			{ children }
+		</DateCardDataContext.Provider>
+	);
 };
 
 export default useDateCardData;
