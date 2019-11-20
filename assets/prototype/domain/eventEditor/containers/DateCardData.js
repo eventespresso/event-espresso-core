d309d7c61047f8a6e88a7c6b9b8e4e14a86d7b82
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-20 20:28:30 +0200

    Conflicts fixed.

---
+++ assets/prototype/domain/eventEditor/containers/DateCardData.js
@@ -0,0 +1,33 @@
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_DATE = gql`
+	query getDatetimes($eventId: Number) {
+  		datetimes(where: $eventId) {
+    		edges {
+				node {
+					id
+					name
+					description
+					sold
+					reserved
+					order
+					startDate
+					endDate
+					startTime
+					endTime
+				}
+			}
+		}
+	}
+`;
+
+const DateCardData = ( { eventId } ) => {
+	const { data } = useQuery( GET_DATE, {
+		variables: { eventId },
+	} );
+
+	return data;
+};
+
+export default DateCardData;
