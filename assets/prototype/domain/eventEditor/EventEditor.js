3b5ce1b156b6195d6a9c8228f796f84e3b1f8b39
Brent Christensen <brent@eventespresso.com>
2019-11-20 17:15:02 -0800

    add Toaster

--- assets/prototype/domain/eventEditor/EventEditor.js
+++ assets/prototype/domain/eventEditor/EventEditor.js
@@ -1,5 +1,10 @@
 import DatesList from './datesList/DatesList';
+import { Position, Toaster, } from '@blueprintjs/core/lib/esm';
 import ContextProviders from '../../infrastructure/services/contextProviders/ContextProviders';
+
+export const AppToaster = Toaster.create( {
+	position: Position.BOTTOM_RIGHT,
+} );
 
 const EventEditor = ( { eventId } ) => (
 	<ContextProviders>
