46de55b0c5b02c09e785a92a6526552ef9edbd46
Brent Christensen <brent@eventespresso.com>
2019-11-20 22:57:27 -0800

    TicketsList, AddNewTicketButton, and TicketCard

--- assets/prototype/domain/eventEditor/EventEditor.js
+++ assets/prototype/domain/eventEditor/EventEditor.js
@@ -1,6 +1,7 @@
-import DatesList from './datesList/DatesList';
 import { Position, Toaster, } from '@blueprintjs/core/lib/esm';
 import ContextProviders from '../../infrastructure/services/contextProviders/ContextProviders';
+import DatesList from './datesList/DatesList';
+import TicketsList from './ticketsList/TicketsList';
 
 export const AppToaster = Toaster.create( {
 	position: Position.BOTTOM_RIGHT,
@@ -9,6 +10,7 @@ export const AppToaster = Toaster.create( {
 const EventEditor = ( { eventId } ) => (
 	<ContextProviders>
 		<DatesList eventId={ eventId } />
+		<TicketsList eventId={ eventId } />
 	</ContextProviders>
 );
 
---
+++ assets/prototype/domain/eventEditor/ticketsList/TicketCard.js
@@ -0,0 +1,98 @@
+import Currency from 'react-currency-formatter';
+import { useState } from '@wordpress/element';
+import {
+	Button,
+	Card,
+	EditableText,
+	Elevation,
+	H4,
+} from '@blueprintjs/core/lib/esm';
+import { AppToaster } from '../EventEditor';
+
+const console = window.console;
+
+const btnStyle = {
+	margin: '0 0 0 .5rem',
+};
+
+const cardStyle = {
+	margin: '0 0 2rem',
+	minWidth: '360px',
+	position: 'relative',
+	textAlign: 'center',
+	width: '32%',
+};
+
+const idStyle = {
+	color: 'grey',
+	fontSize: '9px',
+	left: '.75em',
+	position: 'absolute',
+	top: '.5em',
+};
+
+const priceStyle = {
+    color: 'grey',
+};
+
+const TicketCard = ( { ticket } ) => {
+	const [ editing, setEditing ] = useState( false );
+	const ticketPrice = editing ? (
+		<EditableText
+			isEditing={ editing }
+			placeholder={ 'set price...' }
+			defaultValue={ ticket.price }
+			onCancel={ () => setEditing( false ) }
+			onConfirm={ ( value ) => {
+				ticket.price = value;
+				setEditing( false );
+			} }
+			selectAllOnFocus
+		/>
+	) : (
+		<Currency quantity={ ticket.price }/>
+	);
+	return (
+		<Card elevation={ Elevation.ONE } style={ cardStyle }>
+			<div>
+				<div style={ idStyle }>{ ticket.id }</div>
+				<H4>
+					<EditableText
+						placeholder={ 'edit title...' }
+						defaultValue={ ticket.name }
+						onCancel={ ( value ) => console.log( value ) }
+						onConfirm={ ( value ) => { ticket.name = value } }
+						minWidth={ '320px' }
+						selectAllOnFocus
+					/>
+				</H4>
+			</div>
+			<div>
+				<H4 style={ priceStyle }>
+					{ ticketPrice }
+					<Button
+						icon="edit"
+						onClick={ () => setEditing( true ) }
+						style={ btnStyle }
+						minimal
+					/>
+				</H4>
+			</div>
+			<div style={ {
+				margin: '0 -15px -15px 0',
+				textAlign: 'right',
+			} }>
+				<Button
+					icon="trash"
+					onClick={ () => AppToaster.show( {
+						intent: 'danger',
+						message: `Ticket ${ ticket.id } Deleted`
+					} ) }
+					minimal
+				/>
+			</div>
+		</Card>
+	);
+};
+
+export default TicketCard;
