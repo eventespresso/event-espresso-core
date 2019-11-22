892cd09eb0b7ece76637c87914a652acdd83adac
Brent Christensen <brent@eventespresso.com>
2019-11-22 11:22:07 -0800

    tweak form appearance

--- assets/prototype/domain/eventEditor/datesList/NewDateForm.js
+++ assets/prototype/domain/eventEditor/datesList/NewDateForm.js
@@ -4,32 +4,48 @@ const lblStyle = {
 	color: 'grey',
 	display: 'inline-block',
 	fontSize: '1em',
-	lineHeight: '32px',
+	lineHeight: '2rem',
 	paddingRight: '1em',
 	textAlign: 'right',
-	width: '100px',
+	width: '30%',
+	minWidth: '80px',
+};
+
+const inputStyle = {
+	display: 'inline-block',
+	fontSize: '1em',
+	lineHeight: '2rem',
+	minWidth: '200px',
+	width: '60%',
+};
+
+const divStyle = {
+	display: 'block',
+	margin: '0 0 1em',
+	width: '100%',
 };
 
 const NewDateForm = () => {
 	return (
 		<>
-			<div>
+			<div style={ divStyle }>
 				<label style={ lblStyle } >Name</label>
 				<Field
 					name="name"
 					component="input"
 					type="text"
 					placeholder="Name"
+					style={ inputStyle }
 				/>
 			</div>
-
-			<div>
+			<div style={ divStyle }>
 				<label style={ lblStyle }>Description</label>
 				<Field
 					name="description"
 					component="input"
 					type="text"
 					placeholder="description"
+					style={ inputStyle }
 				/>
 			</div>
 		</>
