c30a88d404afa18db05ad67b61ed59d0c80bc584
Brent Christensen <brent@eventespresso.com>
2019-11-21 21:40:19 -0800

    initialize editor state with useQuery()

--- assets/prototype/domain/eventEditor/EventEditor.js
+++ assets/prototype/domain/eventEditor/EventEditor.js
@@ -1,17 +1,33 @@
 import { Position, Toaster, } from '@blueprintjs/core/lib/esm';
-import ContextProviders from '../../infrastructure/services/contextProviders/ContextProviders';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_DATETIMES } from '../eventEditor/containers/queries/dates'
 import DatesList from './datesList/DatesList';
 import TicketsList from './ticketsList/TicketsList';
+
+const { console } = window.console;
 
 export const AppToaster = Toaster.create( {
 	position: Position.BOTTOM_RIGHT,
 } );
 
-const EventEditor = ( { eventId } ) => (
-	<ContextProviders>
-		<DatesList eventId={ eventId } />
-		<TicketsList eventId={ eventId } />
-	</ContextProviders>
-);
+
+
+const EventEditor = ( { eventId } ) => {
+	const data = useQuery( GET_DATETIMES, {
+		variables: {
+			where: {
+				eventId
+			}
+		}
+	} );
+	console.log( '%c EventEditor', 'color: #1BE7FF;' );
+	console.log( '%c > data:', 'color: #99c043;', data );
+	return (
+		<>
+			<DatesList eventId={ eventId } { ...data } />
+			<TicketsList eventId={ eventId } { ...data }/>
+		</>
+	);
+};
 
 export default EventEditor;
