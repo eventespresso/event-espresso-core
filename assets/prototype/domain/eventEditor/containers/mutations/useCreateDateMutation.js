344515c89af73fef67bf5b52ffeb16d42a3a86dd
Brent Christensen <brent@eventespresso.com>
2019-11-22 14:11:04 -0800

    code cleanup

--- assets/prototype/domain/eventEditor/containers/mutations/useCreateDateMutation.js
+++ assets/prototype/domain/eventEditor/containers/mutations/useCreateDateMutation.js
@@ -1,50 +1,7 @@
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_DATE } from './dates';
 
-// const [createDate] = useMutation(CREATE_DATE, {
-// 	update(cache, { data: { createDate } }) {
-// 		try {
-// 			console.log('%c useCreateDateMutation update()', 'color: #1BE7FF;');
-// 			console.log('%c > createDate: ', 'color: #BCBDAC;', createDate);
-// 			const { datetimes } = cache.readQuery({
-// 				query: GET_DATETIMES,
-// 				variables: {
-// 					where: {
-// 						eventId
-// 					}
-// 				}
-// 			});
-// 			console.log('%c > datetimes: ', 'color: #BCBDAC;', datetimes);
-// 			cache.writeQuery({
-// 				query: GET_DATETIMES,
-// 				variables: {
-// 					where: {
-// 						eventId
-// 					}
-// 				},
-// 				data: { datetimes: datetimes.concat([createDate]) }
-// 			});
-// 			const { datetime } = cache.readQuery({
-// 				query: GET_DATETIME,
-// 				variables: {
-// 					id: 'xyz'
-// 				}
-// 			});
-// 			console.log('%c > datetime: ', 'color: #BCBDAC;', datetime);
-// 			cache.writeQuery({
-// 				query: GET_DATETIME,
-// 				variables: {
-// 					id: 'xyz'
-// 				},
-// 				data: { datetime }
-// 			});
-// 		} catch (error) {
-// 			console.error(error);
-// 		}
-// 	}
-// });
-
-const useCreateDateMutation = ({ eventId }) => {
+const useCreateDateMutation = () => {
 	const [createDate] = useMutation(CREATE_DATE);
 	return createDate;
 };
--- assets/prototype/domain/eventEditor/containers/mutations/useDeleteDateMutation.js
+++ assets/prototype/domain/eventEditor/containers/mutations/useDeleteDateMutation.js
@@ -4,8 +4,6 @@ import { DELETE_DATETIME } from './dates';
 const useDeleteDatetimeMutation = () => {
 	const [deleteDatetime, { loading, error }] = useMutation(DELETE_DATETIME);
 
-	console.log({ loading, error });
-
 	return deleteDatetime;
 };
 
