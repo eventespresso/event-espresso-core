b83a58b2d9c03209c6d309c22a77ffc8594205af
Brent Christensen <brent@eventespresso.com>
2019-11-21 21:45:32 -0800

    extract FormModal so it can be used for tickets

---
+++ assets/prototype/domain/eventEditor/FormModal.js
@@ -0,0 +1,96 @@
+import classNames from 'classnames';
+import { Classes, Overlay } from '@blueprintjs/core/lib/esm';
+import { Form } from 'react-final-form';
+import { EspressoButton } from '../../../ZZZ/components/ui';
+
+const FormModal = ({
+	FormComponent,
+	initialValues,
+	onSubmit,
+	onClose,
+	isOpen,
+}) => {
+	const overlayProps = {
+		autoFocus: true,
+		canEscapeKeyClose: true,
+		canOutsideClickClose: true,
+		enforceFocus: true,
+		hasBackdrop: true,
+		usePortal: true,
+		useTallContent: false
+	};
+
+	const classes = classNames(
+		Classes.CARD,
+		Classes.ELEVATION_4
+	);
+
+	const overlayStyle = {
+		top: '0',
+		left: 'calc(50vw - 200px)',
+		margin: '10vh 0',
+		width: '400px'
+	};
+
+	const formStyle = {
+		padding: '2em 2em 1em',
+	};
+
+	const btnRowStyle = {
+		padding: '1em 2em 2em',
+		textAlign: 'right',
+	};
+
+	return (
+		<Overlay
+			{...overlayProps}
+			className={Classes.OVERLAY_SCROLL_CONTAINER}
+			onClose={onClose}
+			isOpen={isOpen}
+		>
+			<div className={classes} style={overlayStyle}>
+				<Form
+					onSubmit={onSubmit}
+					initialValues={ initialValues }
+					render={({
+						form,
+						handleSubmit,
+						submitting,
+						pristine,
+						...formProps
+					}) => {
+						return (
+							<form onSubmit={ handleSubmit }>
+								<div style={ formStyle }>
+									<FormComponent
+										form={ form }
+										submitting={ submitting }
+										pristine={ pristine }
+										{ ...formProps }
+									/>
+								</div>
+								<div style={ btnRowStyle }>
+									<EspressoButton
+										icon={ 'save' }
+										type={ 'submit' }
+										style={ 'primary' }
+										buttonText={ 'Submit' }
+										disabled={ submitting || pristine }
+										onClick={ onClose }
+									/>
+									<EspressoButton
+										buttonText={ 'Reset' }
+										disabled={ submitting || pristine }
+										onClick={ form.reset }
+									/>
+								</div>
+							</form>
+						)
+					}}
+				/>
+			</div>
+		</Overlay>
+	);
+};
+
+export default FormModal;
--- assets/prototype/domain/eventEditor/datesList/AddNewDateModal.js
+++ assets/prototype/domain/eventEditor/datesList/AddNewDateModal.js
@@ -1,96 +1,28 @@
-import classNames from 'classnames';
-import { Button, Classes, Overlay } from '@blueprintjs/core/lib/esm';
-import { Form, Field } from 'react-final-form';
+import NewDateForm from './NewDateForm';
+import FormModal from '../FormModal';
 import useCreateDateMutation from '../containers/mutations/useCreateDateMutation';
 
-const AddNewDateModal = ({ eventId, handleClose, isOpen }) => {
-	const [createDate, { data }] = useCreateDateMutation();
+const { console } = window.console;
 
-	const onSubmit = async ({ description, name }) => {
+const AddNewDateModal = ({ eventId, handleClose, isOpen }) => {
+	const createDate = useCreateDateMutation({ eventId });
+	console.log( '%c > createDate: ', 'color: HotPink;', createDate );
+	const onSubmit = ({ description, name }) => {
 		const variables = {
 			input: { clientMutationId: 'xyz', description, name, eventId }
 		};
-
-		createDate({ variables });
-	};
-
-	const overlayProps = {
-		autoFocus: true,
-		canEscapeKeyClose: true,
-		canOutsideClickClose: true,
-		enforceFocus: true,
-		hasBackdrop: true,
-		usePortal: true,
-		useTallContent: false
-	};
-
-	const classes = classNames(
-		Classes.CARD,
-		Classes.ELEVATION_4,
-		'docs-overlay-example-transition',
-		'docs-overlay-example-tall'
-	);
-
-	const overlayStyle = {
-		top: '0',
-		left: 'calc(50vw - 200px)',
-		margin: '10vh 0',
-		width: '400px'
+		console.log( '%c > variables: ', 'color: tomato;', variables );
+		return createDate({ variables });
 	};
 
 	return (
-		<Overlay
-			{...overlayProps}
-			className={Classes.OVERLAY_SCROLL_CONTAINER}
-			onClose={handleClose}
-			isOpen={isOpen}
-		>
-			<div className={classes} style={overlayStyle}>
-				<Form
-					onSubmit={onSubmit}
-					initialValues={{ name: 'test' }}
-					render={({
-						handleSubmit,
-						form,
-						submitting,
-						pristine,
-						values
-					}) => (
-						<form onSubmit={handleSubmit}>
-							<div>
-								<label>Name</label>
-								<Field
-									name="name"
-									component="input"
-									type="text"
-									placeholder="Name"
-								/>
-							</div>
-
-							<div>
-								<label>Description</label>
-								<Field
-									name="description"
-									component="input"
-									type="text"
-									placeholder="description"
-								/>
-							</div>
-
-							<div className="buttons">
-								<Button
-									type="submit"
-									disabled={submitting || pristine}
-									intent={'primary'}
-								>
-									Submit
-								</Button>
-							</div>
-						</form>
-					)}
-				/>
-			</div>
-		</Overlay>
+		<FormModal
+			FormComponent={ NewDateForm }
+			initialValues={ {} }
+			onSubmit={ onSubmit }
+			onClose={ handleClose }
+			isOpen={ isOpen }
+		/>
 	);
 };
 
