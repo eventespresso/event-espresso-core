5b2e4efe96b7286ee16ca82eb795a06da5014a55
Brent Christensen <brent@eventespresso.com>
2019-11-24 21:23:58 -0800

    ticket muTa+10N5 !!!

---
+++ assets/prototype/domain/eventEditor/containers/mutations/useCreateTicketMutation.js
@@ -0,0 +1,9 @@
+import { useMutation } from '@apollo/react-hooks';
+import { CREATE_TICKET } from './tickets';
+
+const useCreateTicketMutation = () => {
+	const [createTicket, { loading, error }] = useMutation( CREATE_TICKET);
+	return { createTicket, loading, error };
+};
+
+export default useCreateTicketMutation;
