78c8be8536a797b25cfb548c90771d19f95e4e29
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-11-25 21:18:47 +0530

    Update tickets cache when adding a new datetime to avoid ticket refetching

--- assets/prototype/domain/eventEditor/datesList/AddNewDateModal.js
+++ assets/prototype/domain/eventEditor/datesList/AddNewDateModal.js
@@ -2,6 +2,7 @@ import NewDateForm from './NewDateForm';
 import FormModal from '../../shared/FormModal';
 import useCreateDateMutation from '../containers/mutations/useCreateDateMutation';
 import { GET_DATETIMES } from '../containers/queries/dates';
+import { GET_TICKETS } from '../containers/queries/tickets';
 
 const AddNewDateModal = ({ eventId, handleClose, isOpen }) => {
 	const { createDate } = useCreateDateMutation();
@@ -37,6 +38,7 @@ const AddNewDateModal = ({ eventId, handleClose, isOpen }) => {
 				}
 			}
 		) => {
+
 			const options = {
 				query: GET_DATETIMES,
 				variables: {
@@ -47,6 +49,31 @@ const AddNewDateModal = ({ eventId, handleClose, isOpen }) => {
 			};
 			// Read the data from our cache for this query.
 			const {datetimes = {}} = proxy.readQuery(options);
+
+			if ( datetime.id ) {
+				const datetimeIn = datetimes.nodes.map(({id}) => id);
+				// Read the data from our cache for this query.
+				const data = proxy.readQuery({
+					query: GET_TICKETS,
+					variables: {
+						where: {
+							datetimeIn
+						}
+					}
+				});
+
+				// write the data to cache without
+				// mutating the cache directly
+				proxy.writeQuery({
+					query: GET_TICKETS,
+					data,
+					variables: {
+						where: {
+							datetimeIn: [...datetimeIn, datetime.id],
+						}
+					}
+				});
+			}
 
 			// write the data to cache without
 			// mutating the cache directly
