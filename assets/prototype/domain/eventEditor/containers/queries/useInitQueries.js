5e3f2ef4b54adeddda4b70bf581b9cf9491cc800
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-11-25 21:16:56 +0530

    Fetch tickets conditionally (only if we have datetimes)

--- assets/prototype/domain/eventEditor/containers/queries/useInitQueries.js
+++ assets/prototype/domain/eventEditor/containers/queries/useInitQueries.js
@@ -1,5 +1,6 @@
 import get from 'lodash/get';
-import { useQuery } from '@apollo/react-hooks';
+import { useEffect } from '@wordpress/element';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
 import { GET_DATETIMES  } from './dates';
 import { GET_TICKETS } from './tickets';
 
@@ -18,20 +19,33 @@ const useInitQueries = ({ eventId }) => {
 	});
 	const datetimes = get(datetimesData, ['datetimes', 'nodes']);
 
-	const datetimeIn = datetimes && datetimes.map(({ id }) => id);
-	const {
+	const datetimeIn = datetimes ? datetimes.map(({ id }) => id) : [];
+
+	const [ fetchRelatedTickets, {
 		data: ticketsData,
 		error: ticketError,
 		loading: loadingTickets,
-	} = useQuery(GET_TICKETS, {
+		called,
+	} ] = useLazyQuery(GET_TICKETS, {
 		variables: {
 			where: {
 				datetimeIn
 			}
-		}
+		},
 	});
-	const ticketsNodes = get(ticketsData, ['tickets', 'edges']);
-	const tickets = ticketsNodes && ticketsNodes.map(({ node }) => node);
+
+	const tickets = get(ticketsData, ['tickets', 'nodes'], []);
+
+	// fetch tickets only when we have datetimes
+	// to avoid fetching all tickets irrespective of dates
+	useEffect(() => {
+		if (
+			! called && // Make sure the query function has not already been called.
+			datetimeIn.length // Only if we have datetimes.
+		) {
+			fetchRelatedTickets();
+		}
+	}, [ datetimeIn, called ] );
 
 	console.log(
 		'%c > datetimes, tickets, errors, & loading state',
