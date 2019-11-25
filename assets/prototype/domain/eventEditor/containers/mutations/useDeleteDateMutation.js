e44477a8b5f1e249a70643c92000e1431b07bf4e
Vadim Nicolai <vadim@eventespresso.com>
2019-11-25 20:54:19 +0200

    Date delete mutation + cache update. closes #1891 (#1893)
    * useDeleteDateMutation updated; js_only.
    
    * Refactored a bit useCreateDateMutation.
    
    * Updated useDeleteDatetimeMutation; js_only.

--- assets/prototype/domain/eventEditor/containers/mutations/useDeleteDateMutation.js
+++ assets/prototype/domain/eventEditor/containers/mutations/useDeleteDateMutation.js
@@ -1,9 +1,73 @@
 import { useMutation } from '@apollo/react-hooks';
 import { DELETE_DATETIME } from './dates';
+import { GET_DATETIMES } from '../queries/dates';
+import { GET_TICKETS } from '../queries/tickets';
 
-const useDeleteDatetimeMutation = () => {
+const useDeleteDatetimeMutation = ({ eventId, id }) => {
 	const [deleteDatetime, { loading, error }] = useMutation(DELETE_DATETIME);
-	return { deleteDatetime, loading, error };
+	const variables = { input: { clientMutationId: 'xyz', id } };
+
+	const update = (
+		proxy,
+		{
+			data: {
+				deleteDatetime: { datetime }
+			}
+		}
+	) => {
+		const options = {
+			query: GET_DATETIMES,
+			variables: {
+				where: {
+					eventId
+				}
+			}
+		};
+
+		const { datetimes = {} } = proxy.readQuery(options);
+
+		//TODO - improve readability.
+		const nodes = datetimes.nodes.filter(({ id }) => id !== datetime.id);
+		const data = {
+			datetimes: {
+				...datetimes,
+				nodes
+			}
+		};
+
+		proxy.writeQuery({
+			...options,
+			data
+		});
+
+		if (datetime.id) {
+			const ticketsData = proxy.readQuery({
+				query: GET_TICKETS,
+				variables: {
+					where: {
+						datetimeIn: datetimes.nodes.map(({ id }) => id)
+					}
+				}
+			});
+
+			proxy.writeQuery({
+				query: GET_TICKETS,
+				data: ticketsData,
+				variables: {
+					where: {
+						datetimeIn: nodes.map(({ id }) => id)
+					}
+				}
+			});
+		}
+	};
+
+	const deleteHandler = () => deleteDatetime({ variables, update });
+
+	console.log('loading: ', loading);
+	console.log('error: ', error);
+
+	return deleteHandler;
 };
 
 export default useDeleteDatetimeMutation;
