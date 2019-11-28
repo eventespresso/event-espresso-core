4d86ef357a6697fae3394b6a27b26cd5efa55489
Brent Christensen <brent@eventespresso.com>
2019-11-28 11:52:49 -0800

    formatting and phpdocs

--- assets/prototype/infrastructure/services/contextProviders/ContextProviders.js
+++ assets/prototype/infrastructure/services/contextProviders/ContextProviders.js
@@ -7,13 +7,15 @@ import { RelationsProvider } from './RelationsProvider';
  * A collection of top level providers that are used by multiple parts of the application.
  *
  * @param {ReactElement} children The element that should be wrapped.
- *
+ * @param {number} eventId
  * @returns {ReactElement} The wrapped element.
  */
 const ContextProviders = ({ children, eventId }) => (
 	<ToastProvider>
 		<ApolloProvider>
-			<RelationsProvider eventId={eventId}>{children}</RelationsProvider>
+			<RelationsProvider eventId={eventId}>
+				{children}
+			</RelationsProvider>
 		</ApolloProvider>
 	</ToastProvider>
 );
