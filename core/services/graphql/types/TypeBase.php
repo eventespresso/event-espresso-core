78c016dd0725b413117390fedafe069dd430bda5
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-11-28 20:14:16 +0530

    Fix lint 🙄

--- core/services/graphql/types/TypeBase.php
+++ core/services/graphql/types/TypeBase.php
@@ -267,7 +267,7 @@ abstract class TypeBase implements TypeInterface
      * Prepares a datetime value in ISO8601/RFC3339 format.
      * It is assumed that the value of $datetime is in the format
      * returned by EE_Base_Class::get_format().
-     * 
+     *
      * @param string        $datetime The datetime value.
      * @param EE_Base_Class $source   The source object.
      * @return string ISO8601/RFC3339 formatted datetime.
