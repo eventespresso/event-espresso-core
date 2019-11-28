0430594e722352fabe44df72994a42d23669ad1a
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-11-28 20:31:55 +0200

    Replaced lodash get with ramdas pathOr; js_only.

--- assets/prototype/domain/eventEditor/containers/queries/useInitQueries.js
+++ assets/prototype/domain/eventEditor/containers/queries/useInitQueries.js
@@ -1,4 +1,3 @@
-import get from 'lodash/get';
 import { pathOr } from 'ramda';
 import { useEffect, useState } from '@wordpress/element';
 import { useQuery, useLazyQuery } from '@apollo/react-hooks';
@@ -62,7 +61,7 @@ const useInitQueries = ({ eventId }) => {
 		toaster.error(ticketError);
 	}
 
-	const tickets = get(ticketsData, ['tickets', 'nodes'], []);
+	const tickets = pathOr([], ['tickets', 'nodes'], ticketsData);
 
 	// fetch tickets only when we have datetimes
 	// to avoid fetching all tickets irrespective of dates
