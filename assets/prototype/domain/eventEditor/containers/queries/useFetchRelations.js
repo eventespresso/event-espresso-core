7d6ec7e453ad91a245f7150f87d61630bb9e592c
Brent Christensen <brent@eventespresso.com>
2019-11-28 11:45:54 -0800

    tweak toaster messages (can remove later)

--- assets/prototype/domain/eventEditor/containers/queries/useFetchRelations.js
+++ assets/prototype/domain/eventEditor/containers/queries/useFetchRelations.js
@@ -1,27 +1,40 @@
 import { useQuery } from '@apollo/react-hooks';
+import { useEffect, useState } from '@wordpress/element';
 import { GET_RELATIONS_DATA } from './relations';
 import useToaster from '../../../../infrastructure/services/toaster/useToaster';
 
 const useFetchRelations = ({ eventId, onReceiveRelations }) => {
 	console.log('%c useFetchRelations: ', 'color: deeppink; font-size: 14px;');
+	const [initialized, setInitialized] = useState(false);
 
 	const toaster = useToaster();
-	const toasterDatesMessage = 'initializing event editor relations';
+	const initializingMessage = 'initializing event editor relations';
 
 	const { data, error, loading } = useQuery(GET_RELATIONS_DATA, {
 		variables: { eventId },
 		onCompleted: (r_data) => {
-			if (onReceiveRelations) {
+			if (typeof onReceiveRelations === 'function') {
 				onReceiveRelations(r_data);
 			}
-			toaster.dismiss(toasterDatesMessage);
+			toaster.dismiss(initializingMessage);
 			toaster.success(`event editor relations initialized`);
 		},
 		onError: (error) => {
-			toaster.dismiss(toasterDatesMessage);
+			toaster.dismiss(initializingMessage);
 			toaster.error(error);
 		},
 	});
+
+	if (!initialized) {
+		toaster.loading(loading, initializingMessage);
+		toaster.error(error);
+	}
+
+	useEffect(() => {
+		if (!initialized && !loading) {
+			setInitialized(true);
+		}
+	}, [initialized]);
 
 	return {
 		data,
