3fd096d4128d1c0cce54d2bcac4bc2e9daede815
Brent Christensen <brent@eventespresso.com>
2019-11-28 18:43:30 -0800

    extract logic into CurrencyInput : was going to use in calculator

---
+++ assets/prototype/domain/shared/CurrencyInput.js
@@ -0,0 +1,39 @@
+import Currency from 'react-currency-formatter';
+import { useState } from '@wordpress/element';
+import {Button, EditableText } from '@blueprintjs/core/lib/esm';
+
+const nullFunc = () => {};
+
+const btnStyle = {
+	margin: '0 0 0 .5rem',
+};
+
+const CurrencyInput = ({ id = 0, amount = 0, placeholder = '', onConfirm = nullFunc, onCancel = nullFunc }) => {
+	const [editing, setEditing] = useState(false);
+	return editing ? (
+		<EditableText
+			key={id}
+			isEditing={editing}
+			placeholder={placeholder}
+			defaultValue={amount}
+			onCancel={() => {
+				setEditing(false);
+				if (typeof onCancel === 'function') {
+					onCancel(id);
+				}
+			}}
+			onConfirm={(amount) => {
+				setEditing(false);
+				onConfirm({ amount, id })
+			}}
+			selectAllOnFocus
+		/>
+	) : (
+		<>
+			<Currency quantity={amount} />
+			<Button icon='edit' onClick={() => setEditing(true)} style={btnStyle} minimal/>
+		</>
+	);
+};
+
+export default CurrencyInput;
