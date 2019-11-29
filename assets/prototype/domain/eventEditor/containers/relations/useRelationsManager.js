b37ea7751629f3d0c45db75adbe013d194cc0697
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-11-29 20:38:06 +0530

    Enhance Relations Manager hook

--- assets/prototype/domain/eventEditor/containers/relations/useRelationsManager.js
+++ assets/prototype/domain/eventEditor/containers/relations/useRelationsManager.js
@@ -7,7 +7,7 @@ import cloneDeep from 'lodash/cloneDeep';
 const useRelationsManager = ({ eventId }) => {
 	const [state, dispatch] = useReducer(relationsReducer, {});
 
-	const onReceiveRelations = ({ relations = '{}' }) => {
+	const onReceiveRelations = ({ eventRelations: relations = '{}' }) => {
 		dispatch({ type: 'SET_DATA', data: JSON.parse(relations) });
 	};
 
@@ -68,6 +68,12 @@ const useRelationsManager = ({ eventId }) => {
 	/**
 	 * Removes the relation between two entities.
 	 *
+	 * If `relationId` is provided, then `relationId` will be removed from
+	 * `state[entity][entityId][relation]` list.
+	 *
+	 * If `relationId` is NOT provided, the `entity` will be removed
+	 * from all the `relation`s i.e. from `state[relation][...][entity]`
+	 *
 	 * @param {String} entity       data Type for entity
 	 * @param {String} entityId     GUID for entity
 	 * @param {String} relation     data Type for relation
@@ -83,6 +89,29 @@ const useRelationsManager = ({ eventId }) => {
 		});
 	};
 
+	/**
+	 * Removes all the relations originating "FROM" the given entity.
+	 * It does not remove the relations "FROM" other entities "TO" the given entity.
+	 *
+	 * This should ideally be used after removing the entity from all the relations
+	 * pointing towards it, so as to remove all traces from relationship map.
+	 *
+	 * For example, before dropping relations for a ticket,
+	 * You should remove it from all the datetimes related to it and vice versa
+	 * i.e. before `dropRelations({ entity: 'tickets', entityId: ticketId });`
+	 * You should `removeRelation({ entity: 'tickets', entityId: ticketId, relation: 'datetimes' });`
+	 *
+	 * @param {String} entity       data Type for entity
+	 * @param {String} entityId     GUID for entity
+	 */
+	const dropRelations = ({ entity, entityId }) => {
+		dispatch({
+			type: 'DROP_RELATIONS',
+			entity,
+			entityId,
+		});
+	};
+
 	useFetchRelations({ eventId, onReceiveRelations });
 
 	return {
@@ -90,19 +119,94 @@ const useRelationsManager = ({ eventId }) => {
 		addRelation,
 		removeRelation,
 		updateRelations,
+		dropRelations,
 	};
 };
 
 const relationsReducer = (state, action) => {
+	console.log('relationsReducer action: ', action);
+	const {
+		entity,
+		entityId,
+		relation,
+		relationId,
+		relationIds,
+	} = action;
+	let newState, relations;
 	switch (action.type) {
 		case 'SET_DATA':
 			return action.data;
-		case 'UPDATE_RELATIONS':
-			const { entity, entityId, relation, relationIds } = action;
-			const newState = cloneDeep(state);
-			set(newState, [entity, entityId, relation], relationIds);
-			console.log({ newState });
+
+		case 'ADD_RELATION':
+			relations = get(state, [entity, entityId, relation], []);
+			// If the relation already exists
+			if (relations.includes(relationId)) {
+				return state;
+			}
+			newState = cloneDeep(state);
+			set(newState, [entity, entityId, relation], [...relations, relationId]);
+			console.log('ADD_RELATION newState:', newState);
 			return newState;
+
+		case 'REMOVE_RELATION':
+			newState = cloneDeep(state);
+			// existing relation list.
+			relations = get(state, [entity, entityId, relation], []);
+			console.log('REMOVE_RELATION relations:', relations)
+			// if relationId is given remove it from the list.
+			if (relationId) {
+				set(newState, [entity, entityId, relation], relations.filter(id => id !== relationId));
+				return newState;
+			}
+
+			if (relations.length) {
+				/**
+				 * If we are here, it means that we have values for `entityId` in `relation`
+				 * i.e. if we are trying to remove a datetime (`entityId`) from all the tickets
+				 * we luckily have `state.datetimes[entityId].tickets` list, which means we know
+				 * from where to remove `entityId`.
+				 */
+				relations.forEach((id) => {
+					set(
+						newState,
+						[relation, id, entity],
+						get(newState, [relation, id, entity], []).filter(_id => _id !== entityId)
+					);
+				});
+				console.log('REMOVE_RELATION newState with relations:', newState);
+				return newState;
+			}
+			/**
+			 * If we are here it means that we don't have the values for `entityId` in `relation`
+			 * which means we will have to loop through all the entries in `state[relation]`
+			 * to remove `entityId` from them.
+			 * For example if we are trying to remove a datetime (`entityId`) from all the tickets,
+			 * We will loop through `state.tickets` to delete `entityId` from `state.tickets[ticketId].datetimes`
+			 */
+			for (const id in newState[relation]) {
+				if (newState[relation].hasOwnProperty(id)) {
+					set(
+						newState,
+						[relation, id, entity],
+						get(newState, [relation, id, entity], []).filter(_id => _id !== entityId)
+					);
+				}
+			}
+			console.log('REMOVE_RELATION newState without relations:', newState);
+			return newState;
+
+		case 'UPDATE_RELATIONS':
+			newState = cloneDeep(state);
+			set(newState, [entity, entityId, relation], relationIds);
+			console.log('UPDATE_RELATIONS newState:', newState);
+			return newState;
+
+		case 'DROP_RELATIONS':
+			newState = cloneDeep(state);
+			delete newState[entity][entityId];
+			console.log('DROP_RELATIONS newState:', newState);
+			return newState;
+
 		default:
 			throw new Error();
 	}
