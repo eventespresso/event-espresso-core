35a1cf888fa07eee71d60988e8b9853b35ce2c0e
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-11-29 20:37:15 +0530

    Use try...catch to avoid errors for non-existent objects

--- assets/prototype/domain/eventEditor/containers/queries/useDateItem.js
+++ assets/prototype/domain/eventEditor/containers/queries/useDateItem.js
@@ -3,15 +3,20 @@ import { GET_DATETIME } from './dates';
 
 const useDateItem = ({ id }) => {
 	const client = useApolloClient();
+	let data;
 
-	const data = client.readQuery({
-		query: GET_DATETIME,
-		variables: {
-			id,
-		},
-	});
+	try {
+		data = client.readQuery({
+			query: GET_DATETIME,
+			variables: {
+				id,
+			},
+		});
+	} catch (error) {
+		data = {};
+	}
 
-	return data.datetime;
+	return data.datetime || {};
 };
 
 export default useDateItem;
