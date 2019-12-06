5f231840de41ae8b250bec6f53ef93465d5fae49
Rafael Angeline <me@rafaelangeline.com>
2019-12-06 14:44:37 -0300

    Convert another one to useInitToaster

--- assets/prototype/domain/eventEditor/containers/queries/useFetchRelations.js
+++ assets/prototype/domain/eventEditor/containers/queries/useFetchRelations.js
@@ -1,43 +1,28 @@
 import { useQuery } from '@apollo/react-hooks';
 import { useEffect, useState } from '@wordpress/element';
 import { GET_RELATIONS_DATA } from './relations';
-import useToaster from '../../../../infrastructure/services/toaster/useToaster';
+import useInitToaster from '../../../../infrastructure/services/toaster/useInitToaster';
 import useEventId from './useEventId';
 
 const useFetchRelations = ({ onReceiveRelations }) => {
 	console.log('%c useFetchRelations: ', 'color: deeppink; font-size: 14px;');
-	const [initialized, setInitialized] = useState(false);
 
 	const eventId = useEventId();
 
-	const toaster = useToaster();
 	const initializingMessage = 'initializing event editor relations';
+
+	const initToaster = useInitToaster({
+		toasterMessage: initializingMessage,
+		loadingMessage: initializingMessage,
+		successMessage: 'event editor relations initialized'
+	});
 
 	const { data, error, loading } = useQuery(GET_RELATIONS_DATA, {
 		variables: { eventId },
-		onCompleted: (r_data) => {
-			if (typeof onReceiveRelations === 'function') {
-				onReceiveRelations(r_data);
-			}
-			toaster.dismiss(initializingMessage);
-			toaster.success(`event editor relations initialized`);
-		},
-		onError: (error) => {
-			toaster.dismiss(initializingMessage);
-			toaster.error(error);
-		},
+		...initToaster,
 	});
 
-	if (!initialized) {
-		toaster.loading(loading, initializingMessage);
-		toaster.error(error);
-	}
-
-	useEffect(() => {
-		if (!initialized && !loading) {
-			setInitialized(true);
-		}
-	}, [initialized]);
+	initToaster.initializationNotices(loading, error);
 
 	return {
 		data,
