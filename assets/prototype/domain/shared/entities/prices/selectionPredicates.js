bca99e90ef8ffe18005f55d8946eef31bae39ab7
Brent Christensen <brent@eventespresso.com>
2019-12-09 12:58:37 -0800

    extract logic into new updatePredicates file

--- assets/prototype/domain/shared/entities/prices/selectionPredicates.js
+++ assets/prototype/domain/shared/entities/prices/selectionPredicates.js
@@ -1,20 +1,22 @@
-import { assoc, find, map, propEq, when } from 'ramda';
+import { find, propEq } from 'ramda';
 
+// the following return `true` if price satisfies predicate
 export const priceHasDbId = (dbid) => propEq('dbid', dbid);
 export const priceHasGuid = (guid) => propEq('id', guid);
+// is a base price ?
 export const isBasePrice = propEq('isBasePrice', true);
+export const isNotBasePrice = propEq('isBasePrice', false);
+// is a discount ?
+export const isDiscount = propEq('isDiscount', true);
+export const isNotDiscount = propEq('isDiscount', false);
+// is a percent based modifier ?
+export const isPercent = propEq('isPercent', true);
+export const isNotPercent = propEq('isPercent', false);
+// is a tax ?
+export const isTax = propEq('isTax', true);
+export const isNotTax = propEq('isTax', false);
+
+// returns price if found in array of prices
 export const findBasePrice = (prices) => find(isBasePrice)(prices);
 export const findPriceByDbId = ({ prices, dbid }) => find(priceHasDbId(dbid))(prices);
 export const findPriceByGuid = ({ prices, guid }) => find(priceHasGuid(guid))(prices);
-export const updatePriceAmount = (amount) => assoc('amount', amount);
-export const updatePriceType = (type) => assoc('priceType', type);
-
-export const updateBasePriceAmount = ({ prices, amount }) => map(when(isBasePrice, updatePriceAmount(amount)), prices);
-export const updatePriceTypeForPrice = ({ prices, guid, type }) => map(
-	when(priceHasGuid(guid), updatePriceType(type)),
-	prices
-);
-export const updatePriceAmountForPrice = ({ prices, guid, amount }) => map(
-	when(priceHasGuid(guid), updatePriceAmount(amount)),
-	prices
-);
