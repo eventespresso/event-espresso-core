c34f9b20122950cf7c79f6726336c685ad1419ce
Mike Nelson <michael@eventespresso.com>
2019-12-09 10:00:09 -0800

    Add classes to identify dependent model objects. Merge to CASC/… (#1977)
    * wrote stubs of methods for doing tree traversal. Probably doesnt work but thought Id commit hwat I have so far
    
    * added tree traversal classes
    
    * confirmed we are at least picking the right relations
    
    * confirm term relationships wont be a problem
    
    * tests are passing. it seems a single step of traversing a model obj tree works ok
    
    * add tests to verify we can leave a tree partially traversed and pick up again later. Not yet saving it to the DB
    
    * add some more explanations of the classes and the algorithm, and phpcs fixes
    
    * more comments
    
    * put abstract methods at the top

---
+++ core/services/orm/tree_traversal/ModelObjNode.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace EventEspresso\core\services\orm\tree_traversal;
+
+use EE_Base_Class;
+use EE_HABTM_Relation;
+use EE_Has_Many_Relation;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use InvalidArgumentException;
+use ReflectionException;
+
+/**
+ * Class ModelObjNode
+ * Wraps a model object and stores which of its model's relations have already been traversed and which haven't.
+ *
+ * @package     Event Espresso
+ * @author         Mike Nelson
+ * @since         $VID:$
+ *
+ */
+class ModelObjNode extends BaseNode
+{
+    /**
+     * @var EE_Base_Class
+     */
+    protected $model_obj;
+
+    /**
+     * @var RelationNode[]
+     */
+    protected $relation_nodes;
+
+    public function __construct($instance)
+    {
+        $this->model_obj = $instance;
+    }
+
+    /**
+     * Creates a relation node for each relation of this model's relations.
+     * Does NOT call `discover` on them yet though.
+     * @since $VID:$
+     * @throws \EE_Error
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     */
+    protected function discover()
+    {
+        $this->relation_nodes = [];
+        foreach ($this->model_obj->get_model()->relation_settings() as $relationName => $relation) {
+            if ($relation instanceof EE_Has_Many_Relation) {
+                $this->relation_nodes[ $relationName ] = new RelationNode($this->model_obj, $relation->get_other_model());
+            } elseif ($relation instanceof EE_HABTM_Relation) {
+                $this->relation_nodes[ $relation->get_join_model()->get_this_model_name() ] = new RelationNode($this->model_obj, $relation->get_join_model());
+            }
+        }
+        ksort($this->relation_nodes);
+    }
+
+
+    /**
+     * Whether this item has already been initialized
+     */
+    protected function isDiscovered()
+    {
+        return $this->relation_nodes !== null && is_array($this->relation_nodes);
+    }
+
+    /**
+     * @since $VID:$
+     * @return boolean
+     */
+    public function isComplete()
+    {
+        if ($this->complete === null) {
+            $this->complete = false;
+        }
+        return $this->complete;
+    }
+
+    /**
+     * Triggers working on each child relation node that has work to do.
+     * @since $VID:$
+     * @param $model_objects_to_identify
+     * @return int units of work done
+     */
+    protected function work($model_objects_to_identify)
+    {
+        $num_identified = 0;
+        // Begin assuming we'll finish all the work on this node and its children...
+        $this->complete = true;
+        foreach ($this->relation_nodes as $relation_node) {
+            $num_identified += $relation_node->visit($model_objects_to_identify);
+            if ($num_identified >= $model_objects_to_identify) {
+                // ...but admit we're wrong if the work exceeded the budget.
+                $this->complete = false;
+                break;
+            }
+        }
+        return $num_identified;
+    }
+
+    /**
+     * @since $VID:$
+     * @return array
+     * @throws \EE_Error
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     */
+    public function toArray()
+    {
+        $tree = [
+            'id' => $this->model_obj->ID(),
+            'complete' => $this->isComplete(),
+            'rels' => []
+        ];
+        if ($this->relation_nodes === null) {
+            $tree['rels'] = null;
+        } else {
+            foreach ($this->relation_nodes as $relation_name => $relation_node) {
+                $tree['rels'][ $relation_name ] = $relation_node->toArray();
+            }
+        }
+        return $tree;
+    }
+}
+// End of file Visitor.php
+// Location: EventEspresso\core\services\orm\tree_traversal/Visitor.php
---
+++ tests/testcases/core/services/orm/tree_traversal/RelationNodeTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace EventEspresso\core\services\orm\tree_traversal;
+
+use EE_Error;
+use EE_UnitTestCase;
+use EEM_Term_Relationship;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use InvalidArgumentException;
+use ReflectionException;
+
+/**
+ * Class RelationNodeTest
+ *
+ * Description
+ *
+ * @package     Event Espresso
+ * @author         Mike Nelson
+ * @since         $VID:$
+ *
+ */
+class RelationNodeTest extends EE_UnitTestCase
+{
+    /**
+     * The Term Relationship is the only model with no primary key. And its the bane of my existence.
+     * Try it.
+     * @since $VID:$
+     * @throws EE_Error
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     */
+    public function testVisitTermRelationshipZeroItems(){
+        $e = $this->new_model_obj_with_dependencies('Event');
+        $term_relationship_relation_node = new RelationNode($e, EEM_Term_Relationship::instance());
+        $work_done = $term_relationship_relation_node->visit(1);
+        $this->assertEquals(0, $work_done);
+        $tree = $term_relationship_relation_node->toArray();
+
+        // We should have a count.
+        $this->assertEquals(0, $tree['count']);
+        $this->assertEquals([], $tree['objs']);
+        $this->assertEquals(true, $tree['complete']);
+    }
+
+    public function testVisitTermRelationshipOneItem(){
+        $e = $this->new_model_obj_with_dependencies('Event');
+        $term = wp_insert_term(
+            'forceful',
+            'espresso_event_categories'
+        );
+        wp_add_object_terms(
+            $e->ID(),
+            [
+                $term['term_id']
+            ],
+            'espresso_event_categories'
+        );
+        $term_relationship_relation_node = new RelationNode($e, EEM_Term_Relationship::instance());
+        $work_done = $term_relationship_relation_node->visit(2);
+        $this->assertEquals(1, $work_done);
+        $tree = $term_relationship_relation_node->toArray();
+        // We should have a count.
+        $this->assertEquals(1, $tree['count']);
+        $this->assertNotEmpty( $tree['objs']);
+        $this->assertEquals(true, $tree['complete']);
+    }
+
+    /**
+     * Verifies relation nodes work OK even for a join table across a HABTM relation.
+     * Eg, events don't declare a relationship to "event venues"- it's implied by their HABTM relation to venues.
+     * @since $VID:$
+     * @throws EE_Error
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     */
+    public function testVisitHABTM()
+    {
+        $e = $this->new_model_obj_with_dependencies('Event');
+        $v = $this->new_model_obj_with_dependencies('Venue');
+        $ev = $this->new_model_obj_with_dependencies('Event_Venue',
+            [
+                'EVT_ID' => $e->ID(),
+                'VNU_ID' => $v->ID()
+            ]
+        );
+
+        $e_node = new RelationNode($e, \EEM_Event_Venue::instance());
+        $e_node->visit(2);
+        $tree = $e_node->toArray();
+        $this->assertEquals(1, $tree['count']);
+        $this->assertNotEmpty($tree['objs']);
+        $this->assertArrayHasKey($ev->ID(), $tree['objs']);
+        $this->assertEquals($ev->ID(), $tree['objs'][$ev->ID()]['id']);
+        $this->assertEquals(true, $tree['complete']);
+    }
+}
+// End of file RelationNodeTest.php
+// Location: EventEspresso\core\services\orm\tree_traversal/RelationNodeTest.php
