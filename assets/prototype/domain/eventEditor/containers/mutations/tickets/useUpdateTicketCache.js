730ba4f5842083e8cc5a6cc839323bcbec8a0571
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-09 23:21:20 +0530

    Create cache updaters

---
+++ assets/prototype/domain/eventEditor/containers/mutations/tickets/useUpdateTicketCache.js
@@ -0,0 +1,27 @@
+import useTicketQueryOptions from './useTicketQueryOptions';
+
+const useUpdateTicketCache = () => {
+	const options = useTicketQueryOptions();
+
+	const updateTicketCache = ({ proxy, tickets, ticket, remove = false }) => {
+		const { nodes = [] } = tickets;
+		// remove from or add to the list
+		const newNodes = remove ? nodes.filter(({ id }) => id !== ticket.id) : [...nodes, ticket];
+
+		// write the data to cache without
+		// mutating the cache directly
+		proxy.writeQuery({
+			...options,
+			data: {
+				tickets: {
+					...tickets,
+					nodes: newNodes,
+				},
+			},
+		});
+	};
+
+	return updateTicketCache;
+};
+
+export default useUpdateTicketCache;
