463d9437ec005c104cbd878db6b0ad2228289894
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-10 16:25:21 +0530

    Create query generator for prices

---
+++ assets/prototype/domain/eventEditor/containers/mutations/prices/usePriceQueryOptions.js
@@ -0,0 +1,20 @@
+import useTicketIds from '../../queries/useTicketIds';
+import { queries } from '../../queries';
+
+const { GET_PRICES } = queries;
+
+const usePriceQueryOptions = () => {
+	const ticketIn = useTicketIds();
+	const options = {
+		query: GET_PRICES,
+		variables: {
+			where: {
+				ticketIn,
+			},
+		},
+	};
+
+	return options;
+};
+
+export default usePriceQueryOptions;
