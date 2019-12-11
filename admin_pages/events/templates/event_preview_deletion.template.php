dfeeba94b55654fdc33fab682334feab553599e5
Mike Nelson <michael@eventespresso.com>
2019-12-10 16:35:38 -0800

    Casc/initial ui (#1988)
    * remove the lock symbol. Deleting events should now be possible
    
    * always show the deletion link
    
    * added a page and template for previewing event deletions; added a route for actually performing the deletion
    
    * remove the lock symbol. Deleting events should now be possible
    
    * always show the deletion link
    
    * added a page and template for previewing event deletions; added a route for actually performing the deletion
    
    * redirect after where we do the deletion
    
    * deleting stuff. Of course term_relationships are a pain though
    
    * take that term relationship model. Deleting works
    
    * phpcs fixes
    
    * also delete related tickets, prices and message template groups (and their dependent data)
    
    * report a failure if there were any failures
    
    * phpcs fixes
    
    * remove commented out code
    
    * Update admin_pages/events/Events_Admin_Page.core.php
    
    Co-Authored-By: Brent Christensen <brent@eventespresso.com>
    
    * explain why we are redirecting instead of invoking the preview page directly
    
    * fix typo in code

---
+++ admin_pages/events/templates/event_preview_deletion.template.php
@@ -0,0 +1,5 @@
+<h2><?php esc_html_e('Please Confirm You Want to Permanently Delete the Following Data', 'event_espresso'); ?></h2>
+A bunch of events
+<form action="<?php echo $form_url;?>" method="POST">
+    <input type="submit" value="<?php echo esc_attr(esc_html__('Confirm', 'event_espresso')); ?>">
+</form>
