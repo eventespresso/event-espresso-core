8c7c33d6d581b71de1e06e07b603a9d1b5e2937b
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-13 00:10:20 +0530

    Create/update data access hooks

--- assets/prototype/domain/eventEditor/containers/queries/useDatetimeIds.js
+++ assets/prototype/domain/eventEditor/containers/queries/useDatetimeIds.js
@@ -1,34 +1,9 @@
-import * as R from 'ramda';
-import { useApolloClient } from '@apollo/react-hooks';
-import { GET_DATETIMES } from './datetimes';
-import useEventId from './useEventId';
-import useStatus from '../../../../infrastructure/services/status/useStatus';
+import useDatetimes from './useDatetimes';
 
 const useDatetimeIds = () => {
-	const { isLoaded } = useStatus();
-	const eventId = useEventId();
-	const client = useApolloClient();
-	if (!isLoaded('datetimes')) {
-		return [];
-	}
-	let data;
+	const datetimes = useDatetimes();
 
-	try {
-		data = client.readQuery({
-			query: GET_DATETIMES,
-			variables: {
-				where: {
-					eventId,
-				},
-			},
-		});
-	} catch (error) {
-		data = {};
-	}
-
-	const datetimeIds = R.pathOr([], ['datetimes', 'nodes'], data).map(({ id }) => id);
-
-	return datetimeIds;
+	return datetimes.map(({ id }) => id);
 };
 
 export default useDatetimeIds;
--- assets/prototype/domain/eventEditor/containers/queries/useTicketIds.js
+++ assets/prototype/domain/eventEditor/containers/queries/useTicketIds.js
@@ -1,35 +1,9 @@
-import * as R from 'ramda';
-import { useApolloClient } from '@apollo/react-hooks';
-
-import useDatetimeIds from './useDatetimeIds';
-import { GET_TICKETS } from './tickets';
-import useStatus from '../../../../infrastructure/services/status/useStatus';
+import useTickets from './useTickets';
 
 const useTicketIds = () => {
-	const { isLoaded } = useStatus();
-	const client = useApolloClient();
-	const datetimeIn = useDatetimeIds();
-	if (!isLoaded('tickets')) {
-		return [];
-	}
-	let data;
+	const tickets = useTickets();
 
-	try {
-		data = client.readQuery({
-			query: GET_TICKETS,
-			variables: {
-				where: {
-					datetimeIn,
-				},
-			},
-		});
-	} catch (error) {
-		data = {};
-	}
-
-	const ticketIds = R.pathOr([], ['tickets', 'nodes'], data).map(({ id }) => id);
-
-	return ticketIds;
+	return tickets.map(({ id }) => id);
 };
 
 export default useTicketIds;
