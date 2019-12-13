46277a02796524a124e113036224b2e93b4ef103
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-12-13 20:58:57 +0200

    Added TicketListFilterBar; js_only.

---
+++ assets/prototype/domain/eventEditor/tickets/ticketList/filterBar/TicketListFilterBar.js
@@ -0,0 +1,17 @@
+import EntityListFilterBar from '../../../../../application/ui/components/display/entityList/filterBar/EntityListFilterBar';
+import TicketsListEntityFilters from './TicketsListEntityFilters';
+
+/**
+ * TicketListFilterBar
+ * filters for controlling the display of a list of Event Tickets
+ *
+ * @return {Object} EditorTicketsListView with added TicketListFilters
+ */
+const TicketListFilterBar = () => {
+	const listId = 'event-editor-tickets-list';
+	const entityFilters = <TicketsListEntityFilters />;
+
+	return <EntityListFilterBar listId={listId} entityFilters={entityFilters} />;
+};
+
+export default TicketListFilterBar;
