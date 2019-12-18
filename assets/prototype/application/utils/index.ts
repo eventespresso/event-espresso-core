cb09013cb5cd4ec1dad853951fe45f606c9d8b23
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-18 06:26:53 +0530

    Add case safety to type for mutator js_only

---
+++ assets/prototype/application/utils/index.ts
@@ -0,0 +1,3 @@
+export const lcfirst = (str: string): string => str.charAt(0).toLowerCase() + str.substr(1);
+
+export const ucfirst = (str: string): string => str.charAt(0).toUpperCase() + str.substr(1);
