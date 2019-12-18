f241f5249131d0697de8705454e6e4b2b20571c1
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-12-18 19:09:03 +0200

    Updated toInteger; js_only.

--- assets/prototype/application/utilities/converters/number/toInteger.js
+++ assets/prototype/application/utilities/converters/number/toInteger.js
@@ -1,13 +1,13 @@
-export default function toInteger(dirtyNumber) {
-	if (dirtyNumber === null || dirtyNumber === true || dirtyNumber === false) {
-		return NaN;
+import { is } from 'ramda';
+
+const toInteger = (dirtyNumber) => {
+	const number = Number(dirtyNumber);
+
+	if (is(Number, number)) {
+		return number < 0 ? Math.ceil(number) : Math.floor(number);
 	}
 
-	var number = Number(dirtyNumber);
+	return NaN;
+};
 
-	if (isNaN(number)) {
-		return number;
-	}
-
-	return number < 0 ? Math.ceil(number) : Math.floor(number);
-}
+export default toInteger;
