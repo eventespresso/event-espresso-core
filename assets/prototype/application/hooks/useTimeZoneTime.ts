98b7d20b493017a5b3ec96d0be14b237a08575d1
Brent Christensen <brent@eventespresso.com>
2019-12-19 08:20:50 -0800

    use new Timezone VOs

--- assets/prototype/application/hooks/useTimeZoneTime.ts
+++ assets/prototype/application/hooks/useTimeZoneTime.ts
@@ -1,15 +1,15 @@
 import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
-import useConfig from '../services/config/useConfig';
+import { configTypes, useConfig } from '../services/config/useConfig';
 
 const useTimeZoneTime = () => {
-	const { timezone } = useConfig();
+	const { timezone }: configTypes = useConfig();
 
 	const localTimeToUtc = (date: Date | string | number) => {
-		return zonedTimeToUtc(date, timezone);
+		return zonedTimeToUtc(date, timezone.name);
 	};
 
 	const utcToLocalTime = (isoDate: Date | string | number) => {
-		return utcToZonedTime(isoDate, timezone);
+		return utcToZonedTime(isoDate, timezone.name);
 	};
 
 	return {
