257c0303ea06fe6b05b2a5d7180c6963f6bec353
Brent Christensen <brent@eventespresso.com>
2019-12-20 16:48:25 -0800

    oops... we want path, not pick! type cast all vars as well

--- assets/prototype/application/services/config/ConfigData.ts
+++ assets/prototype/application/services/config/ConfigData.ts
@@ -1,4 +1,4 @@
-import pick from 'ramda/src/pick';
+import path from 'ramda/src/path';
 import {
 	Currency,
 	CurrencyProps,
@@ -14,7 +14,7 @@ import {
 	TimezoneProps,
 } from '../../valueObjects/config';
 
-const data = pick(['eejsdata', 'data'], window) as eeJsDataProps;
+const data = path(['eejsdata', 'data'], window) as eeJsDataProps;
 
 type eeJsDataProps = {
 	brandName: string;
@@ -55,21 +55,21 @@ export type ConfigDataProps = {
 
 export const ConfigData: ConfigDataProps = {
 	brandName: data.brandName || 'Event Espresso',
-	currency: Currency(data.currency_config),
+	currency: Currency(data.currency_config as CurrencyProps),
 	currentUser: CurrentUser({} as CurrentUserProps),
 	dateTimeFormats: DateTimeFormats({} as DateTimeFormatsProps),
 	locale: Locale({
 		user: data.locale && data.locale.user ? data.locale.user : '',
 		site: data.locale && data.locale.site ? data.locale.site : '',
-	}),
+	} as LocaleProps),
 	nonce: data.eejs_api_nonce || '',
 	siteUrl: SiteUrl({
 		admin: data.paths && data.paths.admin_url ? data.paths.admin_url : '',
 		home: data.paths && data.paths.site_url ? data.paths.site_url : '',
-	}),
+	} as SiteUrlProps),
 	timezone: Timezone({
 		city: data.default_timezone && data.default_timezone.pretty ? data.default_timezone.pretty : '',
 		name: data.default_timezone && data.default_timezone.string ? data.default_timezone.string : '',
 		offset: data.default_timezone && data.default_timezone.offset ? data.default_timezone.offset : 0,
-	}),
+	} as TimezoneProps),
 };
