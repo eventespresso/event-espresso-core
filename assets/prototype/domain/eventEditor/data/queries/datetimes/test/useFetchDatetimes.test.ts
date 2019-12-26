3ba71428dbefb009bc413dc7850ad49e70c09188
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-26 23:54:57 +0530

    Update tests for useFetchDatetimes to make use of Apollo cache

--- assets/prototype/domain/eventEditor/data/queries/datetimes/test/useFetchDatetimes.test.ts
+++ assets/prototype/domain/eventEditor/data/queries/datetimes/test/useFetchDatetimes.test.ts
@@ -9,6 +9,17 @@ beforeEach(setup);
 afterEach(cleanup);
 
 describe('useFetchDatetimes()', () => {
+	it('checks for the error state', async () => {
+		const { result, waitForNextUpdate } = renderHook(() => useFetchDatetimes(), {
+			wrapper: contextWrapper(errorMocks),
+		});
+
+		await waitForNextUpdate(); // wait for response
+
+		// await waitForNextUpdate(); // wait for response
+		expect(result.current.error).toBeDefined();
+	});
+
 	it('checks for the loading state', async () => {
 		const { result, waitForNextUpdate } = renderHook(() => useFetchDatetimes(), {
 			wrapper: contextWrapper(successMocks),
@@ -21,44 +32,35 @@ describe('useFetchDatetimes()', () => {
 	});
 
 	it('checks for the response data', async () => {
-		const { result, waitForNextUpdate } = renderHook(() => useFetchDatetimes(), {
+		const { result } = renderHook(() => useFetchDatetimes(), {
 			wrapper: contextWrapper(successMocks),
 		});
 
-		expect(result.current.data).toBeUndefined();
-		expect(result.current.error).toBeUndefined();
-
-		await waitForNextUpdate(); // wait for response
+		// Data is already written above
 		expect(result.current.data).toBeDefined();
 		expect(result.current.error).toBeUndefined();
 	});
 
 	it('checks for the entries in response data', async () => {
-		const { result, waitForNextUpdate } = renderHook(() => useFetchDatetimes(), {
+		const { result } = renderHook(() => useFetchDatetimes(), {
 			wrapper: contextWrapper(successMocks),
 		});
 
-		await waitForNextUpdate(); // wait for response
-
 		expect(result.current.data).toHaveProperty('espressoDatetimes');
 
+		// has nodes
 		expect(result.current.data.espressoDatetimes).toHaveProperty('nodes');
 
+		// nodes is an array with `length` property
+		expect(result.current.data.espressoDatetimes.nodes).toHaveProperty('length');
+
+		// `nodes` length is positive
+		expect(result.current.data.espressoDatetimes.nodes.length).toEqual(nodes.length);
+
+		// nodes from cache are same as written to cache
 		expect(result.current.data.espressoDatetimes.nodes).toEqual(nodes);
 
+		// the id of first entity is same
 		expect(result.current.data.espressoDatetimes.nodes[0].id).toEqual(nodes[0].id);
-	});
-
-	it('checks for the error state', async () => {
-		const { result, waitForNextUpdate } = renderHook(() => useFetchDatetimes(), {
-			wrapper: contextWrapper(errorMocks),
-		});
-
-		expect(result.current.data).toBeUndefined();
-		expect(result.current.error).toBeUndefined();
-
-		await waitForNextUpdate(); // wait for response
-		expect(result.current.data).toBeUndefined();
-		expect(result.current.error).toBeDefined();
 	});
 });
