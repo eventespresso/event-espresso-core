1685130526ed0811554d4bb2f5da8862c7c9717a
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-30 20:56:09 +0530

    Create price query tests

---
+++ assets/prototype/domain/eventEditor/data/queries/prices/test/useFetchPrices.test.ts
@@ -0,0 +1,151 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useFetchPrices from '../useFetchPrices';
+import usePriceQueryOptions from '../usePriceQueryOptions';
+import { ApolloMockedProvider } from '../../../../context/ContextProviders';
+import { successMocks, errorMocks, setup, cleanup, nodes } from './data';
+import useInitTicketTestCache from '../../tickets/test/useInitTicketTestCache';
+
+beforeAll(setup);
+
+afterAll(cleanup);
+
+describe('useFetchPrices()', () => {
+	it('checks for the error state', async () => {
+		/* Set query options and the wrapper */
+		const {
+			result: { current: request },
+		} = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return usePriceQueryOptions();
+			},
+			{
+				wrapper: ApolloMockedProvider(),
+			}
+		);
+		const wrapper = ApolloMockedProvider(errorMocks.map((mock) => ({ ...mock, request })));
+		/* Set query options and the wrapper */
+
+		const { result, waitForNextUpdate } = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return useFetchPrices();
+			},
+			{
+				wrapper,
+			}
+		);
+
+		await waitForNextUpdate(); // wait for response
+
+		expect(result.current.error).toBeDefined();
+	});
+
+	it('checks for the loading state', async () => {
+		/* Set query options and the wrapper */
+		const {
+			result: { current: request },
+		} = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return usePriceQueryOptions();
+			},
+			{
+				wrapper: ApolloMockedProvider(),
+			}
+		);
+		const wrapper = ApolloMockedProvider(successMocks.map((mock) => ({ ...mock, request })));
+		/* Set query options and the wrapper */
+
+		const { result, waitForNextUpdate } = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return useFetchPrices();
+			},
+			{
+				wrapper,
+			}
+		);
+
+		expect(result.current.loading).toBe(true);
+
+		await waitForNextUpdate(); // wait for response
+		expect(result.current.loading).toBe(false);
+	});
+
+	it('checks for the response data', async () => {
+		/* Set query options and the wrapper */
+		const {
+			result: { current: request },
+		} = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return usePriceQueryOptions();
+			},
+			{
+				wrapper: ApolloMockedProvider(),
+			}
+		);
+		const wrapper = ApolloMockedProvider(successMocks.map((mock) => ({ ...mock, request })));
+		/* Set query options and the wrapper */
+
+		const { result } = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return useFetchPrices();
+			},
+			{
+				wrapper,
+			}
+		);
+
+		// Data is already written above
+		expect(result.current.data).toBeDefined();
+		expect(result.current.error).toBeUndefined();
+	});
+
+	it('checks for the entries in response data', async () => {
+		/* Set query options and the wrapper */
+		const {
+			result: { current: request },
+		} = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return usePriceQueryOptions();
+			},
+			{
+				wrapper: ApolloMockedProvider(),
+			}
+		);
+		const wrapper = ApolloMockedProvider(successMocks.map((mock) => ({ ...mock, request })));
+		/* Set query options and the wrapper */
+
+		const { result } = renderHook(
+			() => {
+				useInitTicketTestCache();
+				return useFetchPrices();
+			},
+			{
+				wrapper,
+			}
+		);
+
+		expect(result.current.data).toHaveProperty('espressoPrices');
+
+		// has nodes
+		expect(result.current.data.espressoPrices).toHaveProperty('nodes');
+
+		// nodes is an array with `length` property
+		expect(result.current.data.espressoPrices.nodes).toHaveProperty('length');
+
+		// `nodes` length is positive
+		expect(result.current.data.espressoPrices.nodes.length).toEqual(nodes.length);
+
+		// nodes from cache are same as written to cache
+		expect(result.current.data.espressoPrices.nodes).toEqual(nodes);
+
+		// the id of first entity is same
+		expect(result.current.data.espressoPrices.nodes[0].id).toEqual(nodes[0].id);
+	});
+});
---
+++ assets/prototype/domain/eventEditor/data/queries/prices/test/usePrices.test.ts
@@ -0,0 +1,70 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import usePrices from '../usePrices';
+import { ApolloMockedProvider } from '../../../../context/ContextProviders';
+import { setup, cleanup, nodes } from './data';
+import useInitPriceStatus from './useInitPriceStatus';
+import useInitPriceTestCache from './useInitPriceTestCache';
+
+beforeAll(setup);
+
+afterAll(cleanup);
+
+describe('usePrices()', () => {
+	const wrapper = ApolloMockedProvider();
+	it('checks for the empty tickets', () => {
+		const { result } = renderHook(
+			() => {
+				useInitPriceStatus();
+				return usePrices();
+			},
+			{ wrapper }
+		);
+
+		expect(result.current.length).toBe(0);
+	});
+
+	it('checks for the updated tickets cache', () => {
+		const { result } = renderHook(
+			() => {
+				useInitPriceTestCache();
+				return usePrices();
+			},
+			{ wrapper }
+		);
+
+		const { current: cachedPrices } = result;
+
+		expect(cachedPrices).toEqual(nodes);
+
+		expect(cachedPrices.length).toEqual(nodes.length);
+
+		expect(cachedPrices[0].id).toEqual(nodes[0].id);
+
+		expect(cachedPrices[0].name).toEqual(nodes[0].name);
+	});
+
+	it('returns the prices limitted to the supplied IDs', () => {
+		const filteredPriceIds = [nodes[1].id, nodes[2].id];
+		const filteredPrices = nodes.filter(({ id }) => filteredPriceIds.includes(id));
+		const { result } = renderHook(
+			() => {
+				useInitPriceTestCache();
+				return usePrices(filteredPriceIds);
+			},
+			{ wrapper }
+		);
+
+		const { current: cachedPrices } = result;
+
+		expect(cachedPrices).toEqual(filteredPrices);
+
+		expect(cachedPrices.length).toEqual(filteredPrices.length);
+
+		expect(cachedPrices.length).toBeLessThanOrEqual(nodes.length);
+
+		expect(cachedPrices[0].id).toEqual(filteredPrices[0].id);
+
+		expect(cachedPrices[0].name).toEqual(filteredPrices[0].name);
+	});
+});
