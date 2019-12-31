80c6cf0e0c9f86cf0130d3283885ee557c2e2534
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-12-31 17:52:53 +0530

    Create context aware wrapper for tests

--- assets/prototype/domain/eventEditor/context/test/TestContextProviders.js
+++ assets/prototype/domain/eventEditor/context/test/TestContextProviders.js
@@ -6,18 +6,46 @@ import { CommonProviders } from '../ContextProviders';
 import { useDomTestData } from './';
 import useResetApolloCache from './useResetApolloCache';
 
+/**
+ * A top level provider wrapped by Apollo MockedProvider.
+ *
+ * @param {ReactElement} children The element that should be wrapped.
+ * @returns {ReactElement} The wrapped element.
+ */
 export const ApolloMockedProvider = (mocks = []) => ({ children }) => {
 	return (
 		<MockedProvider mocks={mocks} cache={cache}>
-			<DOMTestDataProvider>{children}</DOMTestDataProvider>
+			<ApolloAwareWrapper>{children}</ApolloAwareWrapper>
 		</MockedProvider>
 	);
 };
 
-export const DOMTestDataProvider = ({ children }) => {
+/**
+ * A mid level provider wrapped by CommonProviders.
+ * It sets the DOM data and handles Apollo cache reset.
+ *
+ * @param {ReactElement} children The element that should be wrapped.
+ * @returns {ReactElement} The wrapped element.
+ */
+export const ApolloAwareWrapper = ({ children }) => {
 	// initialize DOM data
 	useDomTestData();
 	// clear Apollo cache on unmount
 	useResetApolloCache();
-	return <CommonProviders>{children}</CommonProviders>;
+	return (
+		<CommonProviders>
+			<ContextAwareWrapper>{children}</ContextAwareWrapper>
+		</CommonProviders>
+	);
+};
+
+/**
+ * A bottom level provider that's aware of all the contexts.
+ * Takes care of the operations that need contexts.
+ *
+ * @param {ReactElement} children The element that should be wrapped.
+ * @returns {ReactElement} The wrapped element.
+ */
+export const ContextAwareWrapper = ({ children }) => {
+	return <>{children}</>;
 };
