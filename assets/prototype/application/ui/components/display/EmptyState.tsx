4fd8918655437f74d4e1ed5b5ca73c0fcd478b48
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-01-01 22:32:08 +0200

    Updated EmptyState; js_only.

---
+++ assets/prototype/application/ui/components/display/EmptyState.tsx
@@ -0,0 +1,22 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+import { Callout, NonIdealState } from '@blueprintjs/core/lib/esm';
+
+interface EmptyStateProps {
+	children: React.ReactNode;
+	description: string;
+	title: string;
+}
+
+const EmptyState = ({ children, description, title }: EmptyStateProps) => (
+	<>
+		<Callout>
+			<NonIdealState icon={'help'} title={title} description={description} />
+		</Callout>
+		{children}
+	</>
+);
+
+export default EmptyState;
