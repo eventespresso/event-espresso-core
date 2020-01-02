e860b589a57d3ec65577f221f14e0d2ffbd28a20
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-01-02 20:36:23 +0200

    Added tests for lcFirst; js_only.

--- assets/prototype/application/utilities/text/changeCase/index.test.ts
+++ assets/prototype/application/utilities/text/changeCase/index.test.ts
@@ -1,5 +1,27 @@
 import { lcFirst, ucFirst } from './index';
 
+describe('lcFirst', () => {
+	test('should lowercase first letter of a string', () => {
+		expect(lcFirst('Test')).toBe('test');
+	});
+
+	test(`if first character is already lowered it shouldn't change anything`, () => {
+		expect(lcFirst('test')).toBe('test');
+	});
+
+	test(`shouldn't do anything if we pass a number as a string`, () => {
+		expect(lcFirst('123')).toBe('123');
+	});
+
+	test(`shouldn't do anything if we pass null`, () => {
+		expect(lcFirst(null)).toBe(undefined);
+	});
+
+	test(`shouldn't do anything if we pass undefined`, () => {
+		expect(lcFirst(undefined)).toBe(undefined);
+	});
+});
+
 describe('ucFirst', () => {
 	test('should uppercase first letter of a string', () => {
 		expect(ucFirst('test')).toBe('Test');
