6c35996468a854bb28841bff69221066965a32eb
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-01-02 19:42:51 +0200

    Added tests; js_only.

--- assets/prototype/application/utilities/text/index.test.ts
+++ assets/prototype/application/utilities/text/index.test.ts
@@ -1,7 +1,23 @@
-import { ucFirst } from './index';
+import { lcFirst, ucFirst } from './index';
 
-describe.only('ucFirst', () => {
+describe('ucFirst', () => {
 	test('should uppercase first letter of a string', () => {
 		expect(ucFirst('test')).toBe('Test');
+	});
+
+	test(`if string is already capitalized it shouldn't change anything`, () => {
+		expect(ucFirst('Test')).toBe('Test');
+	});
+
+	test(`shouldn't do anything if we pass a number as a string`, () => {
+		expect(ucFirst('123')).toBe('123');
+	});
+
+	test(`shouldn't do anything if we pass null`, () => {
+		expect(ucFirst(null)).toBe(undefined);
+	});
+
+	test(`shouldn't do anything if we pass undefined`, () => {
+		expect(ucFirst(undefined)).toBe(undefined);
 	});
 });
--- assets/prototype/application/utilities/text/index.ts
+++ assets/prototype/application/utilities/text/index.ts
@@ -3,6 +3,18 @@
  */
 import { is } from 'ramda';
 
-export const lcFirst = (str: string): string => is(String, str) && str.charAt(0).toLowerCase() + str.substring(1);
+export const lcFirst = (str: string): string | undefined => {
+	if (is(String, str)) {
+		return str.charAt(0).toLowerCase() + str.substring(1);
+	}
 
-export const ucFirst = (str: string): string => is(String, str) && str.charAt(0).toUpperCase() + str.substring(1);
+	return undefined;
+};
+
+export const ucFirst = (str: string): string | undefined => {
+	if (is(String, str)) {
+		return str.charAt(0).toUpperCase() + str.substring(1);
+	}
+
+	return undefined;
+};
