da70c7e34c582518f00f2484da7b5f714df1286e
Vadim Nicolai <vadim@eventespresso.com>
2020-01-02 21:53:35 +0200

    Add `changeCase` Text Utility; closes #2131. (#2167)
    * Text utilities; js_only.
    
    * Added tests; js_only.
    
    * Added tests for lcFirst; js_only.
    
    * Added index.ts; js_only.

--- assets/prototype/application/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const lcfirst = (str: string): string => str.charAt(0).toLowerCase() + str.substr(1);
-
-export const ucfirst = (str: string): string => str.charAt(0).toUpperCase() + str.substr(1);
