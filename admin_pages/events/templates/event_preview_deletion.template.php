8570cdfa4b932b0ff4b72f9a49e259496ff6adcf
Mike Nelson <michael@eventespresso.com>
2020-01-03 13:48:06 -0800

    show what exactly will be deleted (if WP_DEBUG is on, even show join models)

--- admin_pages/events/templates/event_preview_deletion.template.php
+++ admin_pages/events/templates/event_preview_deletion.template.php
@@ -1,43 +1,59 @@
 <h2><?php esc_html_e('Please Confirm You Want to Permanently Delete the Following Data', 'event_espresso'); ?></h2>
-
-<?php foreach($models_and_ids_to_delete as $model_name => $ids){
-  $model = EE_Registry::instance()->load_model($model_name);
-  $count = count($ids);
-  ?><h3><?php echo esc_html(
-      sprintf(
-          _x('%1$d %2$s', 'Count of items', 'event_espresso'),
-          $count,
-          $model->item_name($count)
-      )
+<?php
+if (WP_DEBUG) { ?>
+    <p class="attention"><?php esc_html_e('Because WP_DEBUG is on, ALL models being deleted are mentioned below.', 'event_espresso'); ?></p>
+    <?php
+}
+foreach ($models_and_ids_to_delete as $model_name => $ids) {
+    $model = EE_Registry::instance()->load_model($model_name);
+    $count = count($ids);
+    ?><h3><?php echo esc_html(
+        sprintf(
+            _x('%1$d %2$s', 'Count of items', 'event_espresso'),
+            $count,
+            $model->item_name($count)
+        )
     ); ?></h3>
     <?php
     $sample_only = false;
-    if($count > $quantity_to_preview){
+    if ($count > $quantity_to_preview) {
         $sample_only = true;
-        $ids = array_slice($ids,0,$quantity_to_preview);
+        $ids = array_slice($ids, 0, $quantity_to_preview);
     }
-    if($sample_only){
-        ?><h3><?php esc_html_e('First %1$d %2$s to Delete...', 'event_espresso'); ?></h3>
-    <?php
+    if ($sample_only) {
+        ?><p><?php
+        echo esc_html(
+            sprintf(
+                __('First %1$d %2$s to Delete...', 'event_espresso'),
+                count($ids),
+                $model->item_name($count)
+            )
+        ); ?></p>
+        <?php
     }
     ?>
     <ul>
-        <?php foreach($ids as $id){
+        <?php foreach ($ids as $id) {
             $model_obj = $model->get_one_by_ID($id);
-            if(! $model_obj instanceof EE_Base_Class){
+            if (!$model_obj instanceof EE_Base_Class) {
                 continue;
             }
-            $model_obj_name = $model_obj->name();
+            if ($model_obj instanceof EE_Datetime) {
+                $model_obj_name = $model_obj->get_dtt_display_name(true);
+            } else {
+                $model_obj_name = $model_obj->name();
+            }
+
             ?>
-        <li>
-        <?php echo esc_html(
-                sprintf(
-                    _x('%1$s (%2$s)', 'Model object name (ID)', 'event_espresso'),
-                    $model_obj->name(),
-                    $model_obj->ID()
-                )
-        );?>
-        </li>
+            <li>
+                <?php echo esc_html(
+                    sprintf(
+                        _x('%1$s (ID "%2$s")', 'Model object name (ID)', 'event_espresso'),
+                        $model_obj_name,
+                        $model_obj->ID()
+                    )
+                ); ?>
+            </li>
             <?php
         }
         ?>
@@ -47,7 +63,7 @@
     <?php
 }
 ?>
-<form action="<?php echo $form_url;?>" method="POST">
-    <input type="hidden" value="<?php echo esc_attr($deletion_job_code);?>" name="deletion_job_code">
+<form action="<?php echo $form_url; ?>" method="POST">
+    <input type="hidden" value="<?php echo esc_attr($deletion_job_code); ?>" name="deletion_job_code">
     <input type="submit" value="<?php echo esc_attr(esc_html__('Confirm', 'event_espresso')); ?>">
 </form>
