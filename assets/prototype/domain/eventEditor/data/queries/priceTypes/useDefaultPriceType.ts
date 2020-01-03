a9eb5735972fded8a8fa8344c0cc370636d12d00
Brent Christensen <brent@eventespresso.com>
2020-01-02 17:04:22 -0800

    use predefined getDefaultPriceType selection predicate

--- assets/prototype/domain/eventEditor/data/queries/priceTypes/useDefaultPriceType.ts
+++ assets/prototype/domain/eventEditor/data/queries/priceTypes/useDefaultPriceType.ts
@@ -1,15 +1,13 @@
-import isEmpty from 'ramda/src/isEmpty';
 import usePriceTypes from './usePriceTypes';
 import { PriceType } from '../../types';
-import { isDefaultPriceType } from '../../../../shared/predicates/priceTypes/selectionPredicates';
+import { getDefaultPriceType } from '../../../../shared/predicates/priceTypes/selectionPredicates';
 
 /**
  * A custom react hook for retrieving the default price type object.
  */
 const useDefaultPriceType = (): PriceType => {
 	const allPriceTypes: PriceType[] = usePriceTypes();
-	const percentSurchargePriceTypes: PriceType[] = allPriceTypes.filter(isDefaultPriceType);
-	return !isEmpty(percentSurchargePriceTypes) ? percentSurchargePriceTypes[0] : null;
+	return getDefaultPriceType(allPriceTypes);
 };
 
 export default useDefaultPriceType;
