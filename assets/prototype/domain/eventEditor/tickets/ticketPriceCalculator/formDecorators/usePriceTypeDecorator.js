1f43cd664b4fb205123a9f01f6885b78f8ac6bba
Brent Christensen <brent@eventespresso.com>
2020-01-06 07:50:16 -0800

    use more properties for finding price to update

--- assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/formDecorators/usePriceTypeDecorator.js
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/formDecorators/usePriceTypeDecorator.js
@@ -1,4 +1,4 @@
-import { map, propEq, when } from 'ramda';
+import { allPass, map, propEq, when } from 'ramda';
 import { isEqual, getFromFormData } from './utilities';
 import useTicketPriceCalculator from '../hooks/useTicketPriceCalculator';
 import usePriceTypes from '../../../data/queries/priceTypes/usePriceTypes';
@@ -16,6 +16,7 @@ const usePriceTypeDecorator = () => {
 		updates: (value, name, formData) => {
 			const pricePath = name.replace('.priceType', '');
 			const price = getFromFormData(pricePath, formData);
+			const reverseCalc = getFromFormData('ticket.reverseCalculate', formData);
 			let priceType = getPriceTypeForPrice(price);
 			priceType = priceType || defaultPriceType;
 			const updatedPrice = {
@@ -28,11 +29,25 @@ const usePriceTypeDecorator = () => {
 				priceType: priceType.id,
 			};
 			const updatedPrices = map(
-				when(propEq('id', updatedPrice.id), () => updatedPrice),
+				when(
+					// Need to replace the existing price in the form data based on several criteria,
+					// since id will be blank for any newly added price modifiers, and several prices
+					// may all be using the same base price type (like $ surcharge).
+					// May have to implement some kind of unique random key for each row
+					// because it is entirely possible that a user could add multiple price modifiers
+					// with the exact same details (would be silly but that doesn't mean it's not possible)
+					// which would make the following checks fail, resulting in the wrong row being updated
+					allPass([
+						propEq('id', updatedPrice.id),
+						propEq('name', updatedPrice.name),
+						propEq('amount', updatedPrice.amount),
+						propEq('priceType', updatedPrice.priceType),
+					]),
+					() => updatedPrice
+				),
 				formData.prices
 			);
 			const formDataWithUpdatedPrice = { ticket: formData.ticket, prices: updatedPrices };
-			const reverseCalc = getFromFormData('ticket.reverseCalculate', formDataWithUpdatedPrice);
 			const formDataCalculations = reverseCalc
 				? calculator({
 						data: formDataWithUpdatedPrice,
