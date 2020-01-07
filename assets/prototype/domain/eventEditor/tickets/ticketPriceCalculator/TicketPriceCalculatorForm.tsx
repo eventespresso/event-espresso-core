c148f217e5697e00b70c89178c37ec9702de5678
Brent Christensen <brent@eventespresso.com>
2020-01-06 16:36:00 -0800

    extract logic into TicketPriceModifierRowIterator: convert to TypeScript

---
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/TicketPriceCalculatorForm.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FieldArray } from 'react-final-form-arrays';
+import { H2, HTMLTable } from '@blueprintjs/core';
+
+import TicketPriceModifierRowIterator from './TicketPriceModifierRowIterator';
+import TicketPriceTotalRow from './TicketPriceTotalRow';
+
+// just temporary
+import styles from './inlineStyles';
+
+const TicketPriceCalculatorForm = ({ form, values: { ticket } }) => {
+	const calcDir = !!ticket.reverseCalculate;
+	const toggleCalcDir = () => form.mutators.toggleCalcDir('ticket.reverseCalculate');
+	const ticketName = ticket.name ? ` for Ticket: ${ticket.name} ` : '';
+	return (
+		<>
+			<H2 style={styles.hdr}>{`Ticket Price Calculator${ticketName}`}</H2>
+			<div style={styles.div}>
+				<HTMLTable interactive striped style={styles.table}>
+					<thead>
+						<tr>
+							<th style={{ ...styles.colWidth7h, ...styles.cell }}>ID</th>
+							<th style={{ ...styles.colWidth15, ...styles.type }}>Price Type</th>
+							<th style={{ ...styles.colWidth20, ...styles.cell }}>Label</th>
+							<th style={{ ...styles.colWidth30, ...styles.cell }}>Description</th>
+							<th style={{ ...styles.colWidth15, ...styles.amount }}>Amount</th>
+							<th style={{ ...styles.colWidth7h, ...styles.actions }}>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<FieldArray name={'prices'} children={TicketPriceModifierRowIterator} />
+					</tbody>
+					<tfoot>
+						<TicketPriceTotalRow ticket={ticket} calcDir={calcDir} toggleCalcDir={toggleCalcDir} />
+					</tfoot>
+				</HTMLTable>
+			</div>
+		</>
+	);
+};
+
+export default TicketPriceCalculatorForm;
