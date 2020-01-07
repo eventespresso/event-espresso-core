b9cc1239aa818f3284fee78b3e9ea40b0e6b0793
Brent Christensen <brent@eventespresso.com>
2020-01-06 16:09:59 -0800

    money utilities

--- assets/prototype/application/utilities/money/index.js
+++ assets/prototype/application/utilities/money/index.js
@@ -1,1 +1,4 @@
-export { amountsMatch } from './amountsMatch';
+export { default as amountsMatch } from './amountsMatch';
+export { default as formatAmount } from './formatAmount';
+export { default as parsedAmount } from './parsedAmount';
+export { default as useMoneyDisplay } from './useMoneyDisplay';
---
+++ assets/prototype/application/utilities/money/useMoneyDisplay.ts
@@ -0,0 +1,29 @@
+import formatAmount from './formatAmount';
+import useConfig from '../../services/config/useConfig';
+import { CurrencyProps } from '../../valueObjects/config/types';
+
+export type MoneyDisplay = {
+	// the currency sign if the currency displays it before the amount (or '')
+	afterAmount: string;
+	// the currency sign if the currency displays it before the amount (or '')
+	beforeAmount: string;
+	// the full currency config object
+	currency: CurrencyProps;
+	// function for formatting the amount using the correct number of decimal places for the currency
+	formatAmount: (amount: number | string) => string;
+};
+
+const useMoneyDisplay = (): MoneyDisplay => {
+	const { config } = useConfig();
+	const afterAmount = config.currency.signB4 ? '' : config.currency.sign;
+	const beforeAmount = config.currency.signB4 ? config.currency.sign : '';
+	const formatMoney = formatAmount(config.currency.decimalPlaces);
+	return {
+		afterAmount,
+		beforeAmount,
+		currency: config.currency,
+		formatAmount: formatMoney,
+	};
+};
+
+export default useMoneyDisplay;
