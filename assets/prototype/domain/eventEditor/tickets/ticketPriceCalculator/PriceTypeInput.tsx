fedd97ce6269189edb6edc639b368cc90b1355e6
Brent Christensen <brent@eventespresso.com>
2020-01-06 21:38:18 -0800

    extract logic into PriceTypeInput; add types

---
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/PriceTypeInput.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Field } from 'react-final-form';
+
+// just temporary
+import styles from './inlineStyles';
+import { Price } from '../../data/types';
+import usePriceTypeForPrice from '../../data/queries/priceTypes/usePriceTypeForPrice';
+
+interface PriceTypeInputProps {
+	name: string;
+	price: Price;
+	priceTypes: Price[];
+	modifierOptions: Price[];
+}
+
+const PriceTypeInput = ({ name, price, priceTypes, modifierOptions }: PriceTypeInputProps): React.ReactNode => {
+	const relatedPriceType = usePriceTypeForPrice(price.id);
+	const options = price.isBasePrice ? priceTypes : modifierOptions;
+	return (
+		<Field
+			component={'select'}
+			initialValue={relatedPriceType.id}
+			name={`${name}.priceType`}
+			disabled={price.isBasePrice}
+			style={styles.input}
+		>
+			{options.map(
+				(option: Price): React.ReactNode => (
+					<option key={option.id} value={option.id}>
+						{option.name}
+					</option>
+				)
+			)}
+		</Field>
+	);
+};
+
+export default PriceTypeInput;
