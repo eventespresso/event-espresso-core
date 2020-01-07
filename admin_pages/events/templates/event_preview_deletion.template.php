35e71d2eb6737d1c9307c995777a4a037db358b2
Mike Nelson <michael@eventespresso.com>
2020-01-07 10:51:59 -0800

    use a less generalized template to preview event deletion. Just show the events, datetimes and first 10 registrations

--- admin_pages/events/templates/event_preview_deletion.template.php
+++ admin_pages/events/templates/event_preview_deletion.template.php
@@ -1,69 +1,94 @@
 <h2><?php esc_html_e('Please Confirm You Want to Permanently Delete the Following Data', 'event_espresso'); ?></h2>
-<?php
-if (WP_DEBUG) { ?>
-    <p class="attention"><?php esc_html_e('Because WP_DEBUG is on, ALL models being deleted are mentioned below.', 'event_espresso'); ?></p>
+<h3>
     <?php
-}
-foreach ($models_and_ids_to_delete as $model_name => $ids) {
-    $model = EE_Registry::instance()->load_model($model_name);
-    $count = count($ids);
-    ?><h3><?php echo esc_html(
-        sprintf(
-            _x('%1$d %2$s', 'Count of items', 'event_espresso'),
-            $count,
-            $model->item_name($count)
-        )
-    ); ?></h3>
+    printf(
+        esc_html(
+            // translators: 1: number of events
+            _n('%1$d Event', '%1$d Events', count($events), 'event_espresso')
+        ),
+        count($events)
+
+    );
+    ?>
+</h3>
+<ul>
     <?php
-    $sample_only = false;
-    if ($count > $quantity_to_preview) {
-        $sample_only = true;
-        $ids = array_slice($ids, 0, $quantity_to_preview);
-    }
-    if ($sample_only) {
-        ?><p><?php
-        echo esc_html(
-            sprintf(
-                __('First %1$d %2$s to Delete...', 'event_espresso'),
-                count($ids),
-                $model->item_name($count)
-            )
-        ); ?></p>
+    foreach ($events as $event) {
+        ?>
+        <li>
+            <?php echo $event->name(); ?>
+        </li>
         <?php
     }
     ?>
-    <ul>
-        <?php foreach ($ids as $id) {
-            $model_obj = $model->get_one_by_ID($id);
-            if (!$model_obj instanceof EE_Base_Class) {
-                continue;
-            }
-            if ($model_obj instanceof EE_Datetime) {
-                $model_obj_name = $model_obj->get_dtt_display_name(true);
-            } else {
-                $model_obj_name = $model_obj->name();
-            }
+</ul>
+<h3>
+    <?php
+    printf(
+        esc_html(
+            // translators: 1: number of datetimes
+            _n('%1$d Datetime', '%1$d Datetimes', count($datetimes),'event_espresso')
+        ),
+        count($datetimes)
 
-            ?>
-            <li>
-                <?php echo esc_html(
-                    sprintf(
-                        _x('%1$s (ID "%2$s")', 'Model object name (ID)', 'event_espresso'),
-                        $model_obj_name,
-                        $model_obj->ID()
-                    )
-                ); ?>
-            </li>
-            <?php
-        }
+    );
+    ?>
+</h3>
+<ul>
+    <?php
+    foreach ($datetimes as $datetime) {
         ?>
-    </ul>
-
-
+        <li>
+            <?php echo $datetime->get_dtt_display_name(true); ?>
+        </li>
+        <?php
+    }
+    ?>
+</ul>
+<h3>
+    <?php
+    printf(
+        esc_html(
+            _n('$1%d Registration', '%1$d Registrations', $reg_count, 'event_espresso')
+        ),
+        $reg_count
+    );
+    ?>
+</h3>
+<?php
+if ($reg_count > count($registrations)) {
+    ?>
+    <p class="notice">
+        <?php
+        printf(
+            esc_html__('Only showing first %1$d.', 'event_espresso'),
+            count($registrations)
+        );
+        ?>
+    </p>
     <?php
 }
 ?>
+<ul>
+    <?php
+    foreach ($registrations as $registration) {
+        ?>
+        <li>
+            <?php
+            printf(
+                esc_html(
+                    _x('%1$s (%2$d of %3$d)', 'Registration name (number of count)', 'event_espresso')
+                ),
+                $registration->attendeeName(true),
+                $registration->count(),
+                $registration->group_size()
+            ); ?>
+        </li>
+        <?php
+    }
+    ?>
+</ul>
 <form action="<?php echo $form_url; ?>" method="POST">
-    <?php echo $form->get_html_and_js();?>
+    <?php echo $form->get_html_and_js(); ?>
     <input type="submit" value="<?php echo esc_attr(esc_html__('Confirm', 'event_espresso')); ?>">
 </form>
