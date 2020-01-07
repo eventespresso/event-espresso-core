cba6a7028df96d1aed710014f12a20973201fcdb
Brent Christensen <brent@eventespresso.com>
2020-01-06 21:47:52 -0800

    fix types and props

--- assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/TicketPriceModifierRowIterator.tsx
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/TicketPriceModifierRowIterator.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
 import TicketPriceModifierRow from './TicketPriceModifierRow';
-import { Price } from '../../data/types';
+import { FieldArrayProps } from './types';
 
-interface Mutators {
-	fields: {
-		map: <R>(iterator: (name: string, index: number) => R) => R[];
-		push: (value: Price) => void;
-		remove: (index: number) => Price;
-		reset: (name: string) => void;
-		sort: () => void;
-		value: Price[];
-	};
-}
-
-const TicketPriceModifierRowIterator: React.ReactNode = ({ fields }: Mutators, calcDir: boolean) => {
+const TicketPriceModifierRowIterator: React.ReactNode = ({ fields }: FieldArrayProps, reverseCalculate: boolean) => {
 	const { push, remove, reset, sort } = fields;
 	return fields.map((name, index) => {
 		const price = fields.value[index];
@@ -25,7 +14,7 @@ const TicketPriceModifierRowIterator: React.ReactNode = ({ fields }: Mutators, c
 				name={name}
 				fields={{ push, remove, reset, sort }}
 				price={price}
-				calcDir={calcDir}
+				reverseCalculate={reverseCalculate}
 			/>
 		) : null;
 	});
