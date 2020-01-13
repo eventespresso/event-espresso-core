a85b1a2c6627e6a08b25de6fdcc14a40cf03f5cf
Brent Christensen <brent@eventespresso.com>
2020-01-13 13:48:12 -0800

    remove unnecessary event handling; js_only

--- assets/prototype/domain/eventEditor/tickets/TicketsList/AddNewTicketButton.tsx
+++ assets/prototype/domain/eventEditor/tickets/TicketsList/AddNewTicketButton.tsx
@@ -7,20 +7,16 @@ import {
 	EditorId,
 	EditorState,
 } from '../../../../application/ui/components/layout/editor-modal/useEditorModalState/types';
-import { ClickEvent, EspressoButton } from '../../../../application/ui/components/input';
+import { EspressoButton } from '../../../../application/ui/components/input';
 
 const AddNewTicketButton: React.FunctionComponent = () => {
 	const editorState: EditorState = useEditorModalState();
 	const editorId: EditorId = useTicketEditorId('add-new-ticket', 'xyz');
 
-	const onClick: ClickEvent = (click) => {
-		click.preventDefault();
-		click.stopPropagation();
+	const onClick = (): void => {
 		editorState.openEditor(editorId);
 	};
-	const onClose: ClickEvent = (click) => {
-		click.preventDefault();
-		click.stopPropagation();
+	const onClose = (): void => {
 		editorState.closeEditor(editorId);
 	};
 	const isOpen = editorState.isEditorOpen(editorId);
