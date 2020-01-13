ff61e293170ecc1ccbcb61c9f7c9c15082581287
Brent Christensen <brent@eventespresso.com>
2020-01-13 15:36:12 -0800

    don't import components from WP packages js_only

--- assets/prototype/application/ui/components/display/espressoIcon/EspressoIcon.tsx
+++ assets/prototype/application/ui/components/display/espressoIcon/EspressoIcon.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import classNames from 'classnames';
-import { Dashicon } from '@wordpress/components';
-import { Path, SVG } from '@wordpress/components/src/primitives';
 
 import { SvgPath } from './svgPaths';
 import { EspressoIconProps, Icon } from './types';
@@ -9,7 +7,13 @@ import { EspressoIconProps, Icon } from './types';
 /**
  * Custom Event Espresso Dashicons
  */
-const EspressoIcon: React.FunctionComponent<EspressoIconProps> = ({ icon, svgSize = 20, className, ...otherProps }) => {
+const EspressoIcon: React.FunctionComponent<EspressoIconProps> = ({
+	icon,
+	svgSize = 20,
+	className,
+	isPressed,
+	...otherProps
+}) => {
 	let path: string;
 	switch (icon) {
 		case Icon.CALCULATOR:
@@ -31,28 +35,27 @@ const EspressoIcon: React.FunctionComponent<EspressoIconProps> = ({ icon, svgSiz
 		case Icon.SAVE:
 			path = SvgPath.SAVE;
 			break;
-
-		default:
-			return <Dashicon icon={icon} size={svgSize} className={className} {...otherProps} />;
 	}
 
 	if (!path) {
 		return null;
 	}
+	const htmlClass = classNames(className, 'dashicon', `dashicons-${icon}`, 'espresso-icon');
+
 	return (
-		<SVG
-			aria-hidden
-			role='img'
-			focusable='false'
-			className={classNames(className, 'dashicon', `dashicons-${icon}`, 'espresso-icon')}
+		<svg
+			role={'img'}
+			aria-hidden={true}
+			focusable={false}
 			xmlns='http://www.w3.org/2000/svg'
 			width={svgSize}
 			height={svgSize}
 			viewBox='0 0 20 20'
+			className={htmlClass}
 			{...otherProps}
 		>
-			<Path d={path} />
-		</SVG>
+			<path d={path} />
+		</svg>
 	);
 };
 
