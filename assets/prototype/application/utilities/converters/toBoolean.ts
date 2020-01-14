0c4ce36502fb449f62b1208167fed7319400e5b4
Brent Christensen <brent@eventespresso.com>
2020-01-14 09:32:56 -0800

    better boolean conversion

--- assets/prototype/application/utilities/converters/toBoolean.ts
+++ assets/prototype/application/utilities/converters/toBoolean.ts
@@ -1,17 +1,31 @@
-const toBoolean = (value: boolean | number | string) => {
-	value = typeof value === 'string' ? value.toLowerCase().trim() : value;
-	switch (value) {
-		case 'true':
-		case 'yes':
-		case '1':
-			return true;
-		case 'false':
-		case 'no':
-		case '0':
-			return false;
-		default:
-			return Boolean(value);
+/**
+ * returns value if already a boolean.
+ * returns true if passed value is "true", "yes", "1", or any positive number.
+ * otherwise returns false
+ */
+const toBoolean = (value: any): boolean => {
+	// if already a boolean, then just return it
+	if (typeof value === 'boolean') {
+		return value;
 	}
+	// return true if a string with value "true", "yes", or "1"
+	if (typeof value === 'string') {
+		value = value.toLowerCase().trim();
+		switch (value) {
+			case 'true':
+			case 'yes':
+			case '1':
+				return true;
+			default:
+				return false;
+		}
+	}
+	// if a number, then parse as a boolean
+	if (typeof value === 'number') {
+		return Boolean(value);
+	}
+	// evenrything else is false
+	return false;
 };
 
 export default toBoolean;
