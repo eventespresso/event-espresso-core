b9580d6749780fbfca552db4a18dbc64a71e2a64
Brent Christensen <brent@eventespresso.com>
2020-01-13 17:48:31 -0800

    MOAR TYPES !!!

--- assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/types.ts
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/types.ts
@@ -1,6 +1,55 @@
-import { Price } from '../../data/types';
+import { Price, Ticket } from '../../data/types';
 
-export interface TicketPriceCalculatorMutators {
+export interface AddPriceModifierDataProps extends WithPriceProp {
+	modifiers: Price[];
+	name: string;
+	push: (price: Price) => void;
+	reset: (name: string) => void;
+	sort: () => void;
+}
+
+export interface FieldArrayProps {
+	fields: TpcFormMutators;
+}
+
+export interface FormProps {
+	mutators: TpcFormMutators;
+}
+
+export interface PriceInputProps extends WithPriceProp {
+	name: string;
+}
+
+export interface TpcAction {
+	data: TpcFormData;
+	path: string;
+	type: string;
+}
+
+export interface TpcButtonDataProps extends WithTicketProp {}
+
+export interface TpcFormData extends WithPriceArrayProp, WithTicketProp {}
+
+export interface TpcForm extends WithTicketProp {
+	form: FormProps;
+	values: WithTicketProp;
+}
+
+export interface TpcFormElement extends withRevCalc, WithTicketProp, WithToggleCalcDir {}
+
+export interface TpcModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	onOpen?: () => void;
+}
+
+export interface TpcModifierFormRowProps extends FieldArrayProps, WithPriceProp, withRevCalc {
+	index?: number;
+	modifierOptions?: Price[];
+	name?: string;
+}
+
+export interface TpcFormMutators extends WithToggleCalcDir {
 	map: <R>(iterator: (name: string, index: number) => R) => R[];
 	push: (value: Price) => void;
 	remove: (index: number) => Price;
@@ -9,11 +58,26 @@ export interface TicketPriceCalculatorMutators {
 	value: Price[];
 }
 
-export interface FieldArrayProps {
-	fields: TicketPriceCalculatorMutators;
+export interface UpdatedTpcFormDataPath {
+	[key: string]: string | number;
 }
 
-export interface PriceInputProps {
-	name: string;
+export interface WithPriceProp {
 	price: Price;
+}
+
+export interface WithPriceArrayProp {
+	prices: Price[];
+}
+
+export interface withRevCalc {
+	reverseCalculate?: boolean;
+}
+
+export interface WithTicketProp {
+	ticket: Ticket;
+}
+
+export interface WithToggleCalcDir {
+	toggleCalcDir: () => void;
 }
