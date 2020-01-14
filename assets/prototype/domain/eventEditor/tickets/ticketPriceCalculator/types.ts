4da1813a5e351ba924649cc41689035ad6da8e1b
Brent Christensen <brent@eventespresso.com>
2020-01-14 13:03:02 -0800

    fix TpcForm type

--- assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/types.ts
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/types.ts
@@ -39,7 +39,7 @@ export type TpcCallback = (func: any) => UpdatedTpcFormDataPath;
 
 export interface TpcFormData extends WithPriceArrayProp, WithTicketProp {}
 
-export interface TpcForm extends WithTicketProp {
+export interface TpcForm {
 	form: FormProps;
 	values: WithTicketProp;
 }
