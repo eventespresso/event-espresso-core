279e328a1fa7203bf8cfaf167cf93cb135c9e62b
Brent Christensen <brent@eventespresso.com>
2020-01-13 18:20:09 -0800

    convert to TS

---
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/TicketPriceCalculatorButtonData.tsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+
+import TicketPriceCalculatorButton from './TicketPriceCalculatorButton';
+import { WithTicketProp } from './types';
+import { TicketContext, TicketContextProps } from '../../context/TicketProvider';
+
+const TicketPriceCalculatorButtonData: React.FunctionComponent<WithTicketProp> = ({ ticket }) => {
+	const { editors, editorState } = useContext<TicketContextProps>(TicketContext);
+	const isOpen = editorState.isEditorOpen(editors.calculator);
+	const onClose = (): void => editorState.closeEditor(editors.calculator);
+	const onOpen = (): void => editorState.openEditor(editors.calculator);
+
+	return <TicketPriceCalculatorButton ticket={ticket} onOpen={onOpen} onClose={onClose} isOpen={isOpen} />;
+};
+
+export default TicketPriceCalculatorButtonData;
