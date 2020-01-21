cc90156247ae8159814ee31494e76135aa671ccd
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-01-22 00:35:17 +0530

    Hammer them with tests js_only

--- assets/prototype/application/utilities/money/test/formatAmount.test.ts
+++ assets/prototype/application/utilities/money/test/formatAmount.test.ts
@@ -1,32 +1,86 @@
 import { formatAmount } from '../';
 
+const testCases = [
+	{
+		desc: 'checks for formatted amount to be a string',
+		decimalPlaces: 2,
+		amount: 5,
+		testFn: (val: any): boolean => typeof val === 'string',
+		result: true,
+	},
+	{
+		desc: 'adds leading zeros for integers',
+		decimalPlaces: 2,
+		amount: 5,
+		result: '5.00',
+	},
+	{
+		desc: 'adds leading zeros for positive float',
+		decimalPlaces: 2,
+		amount: 5.2,
+		result: '5.20',
+	},
+	{
+		desc: 'adds leading zeros for negative float',
+		decimalPlaces: 2,
+		amount: -0.5,
+		result: '-0.50',
+	},
+	{
+		desc: 'returns the same result for positive integer when decimalplaces is 0',
+		decimalPlaces: 0,
+		amount: 5,
+		result: '5',
+	},
+	{
+		desc: 'removes leading zeros for float when decimalplaces is 0',
+		decimalPlaces: 0,
+		amount: 5.0,
+		result: '5',
+	},
+	{
+		desc: 'rounds off positive float to integer when decimalplaces is 0',
+		decimalPlaces: 0,
+		amount: 5.2,
+		result: '5',
+	},
+	{
+		desc: 'rounds off negative float to integer when decimalplaces is 0',
+		decimalPlaces: 0,
+		amount: -0.5,
+		result: '-1',
+	},
+	{
+		desc: 'returns NaN when amount is null',
+		decimalPlaces: 3,
+		amount: null,
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+	{
+		desc: 'returns NaN when amount is empty string',
+		decimalPlaces: 3,
+		amount: '',
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+	{
+		desc: 'returns NaN when amount is undefined',
+		decimalPlaces: 3,
+		amount: undefined,
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+];
+
 describe('formatAmount', () => {
-	it('checks for formatted amount to be a string', () => {
-		const decimalPlaces = 2;
+	for (const testCase of testCases) {
+		it(testCase.desc, () => {
+			const format = formatAmount(testCase.decimalPlaces);
+			const result = format(testCase.amount);
+			const expectedResult = testCase.testFn ? testCase.testFn(result) : result;
 
-		const format = formatAmount(decimalPlaces);
-		const result = format(5);
-
-		expect(typeof result).toBe('string');
-	});
-
-	it('checks for formatted amount for positive number of deicmal places', () => {
-		const decimalPlaces = 2;
-
-		const format = formatAmount(decimalPlaces);
-
-		expect(format(5)).toEqual('5.00');
-		expect(format(5.2)).toEqual('5.20');
-		expect(format(-0.5)).toEqual('-0.50');
-	});
-
-	it('checks for formatted amount for 0 deicmal places', () => {
-		const decimalPlaces = 0;
-
-		const format = formatAmount(decimalPlaces);
-
-		expect(format(5.0)).toEqual('5');
-		expect(format(5.2)).toEqual('5');
-		expect(format(-0.5)).toEqual('-1');
-	});
+			expect(testCase.result).toBe(expectedResult);
+		});
+	}
 });
--- assets/prototype/application/utilities/money/test/parsedAmount.test.ts
+++ assets/prototype/application/utilities/money/test/parsedAmount.test.ts
@@ -1,15 +1,55 @@
 import { parsedAmount } from '../';
 
-describe('parsedAmount', () => {
-	it('checks for parsed amount to be a number', () => {
-		const result = parsedAmount('5.256');
+const testCases = [
+	{
+		desc: 'checks for parsed amount to be a number',
+		amount: 5,
+		testFn: (val: any): boolean => typeof val === 'number',
+		result: true,
+	},
+	{
+		desc: 'returns a numeric float for a numeric float',
+		amount: 8.256,
+		result: 8.256,
+	},
+	{
+		desc: 'returns a numeric float for a string float',
+		amount: '8.256',
+		result: 8.256,
+	},
+	{
+		desc: 'returns NaN for a non-numeric string',
+		amount: 'carrot',
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+	{
+		desc: 'returns NaN for an empty string',
+		amount: '',
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+	{
+		desc: 'returns NaN when amount is null',
+		amount: null,
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+	{
+		desc: 'returns NaN when amount is undefined',
+		amount: undefined,
+		testFn: (val: any): boolean => isNaN(val),
+		result: true,
+	},
+];
 
-		expect(typeof result).toBe('number');
-	});
+describe('formatAmount', () => {
+	for (const testCase of testCases) {
+		it(testCase.desc, () => {
+			const result = parsedAmount(testCase.amount);
+			const expectedResult = testCase.testFn ? testCase.testFn(result) : result;
 
-	it('checks for parsed amount to be same as expected', () => {
-		const result = parsedAmount('8.256');
-
-		expect(result).toEqual(8.256);
-	});
+			expect(testCase.result).toBe(expectedResult);
+		});
+	}
 });
