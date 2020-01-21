e3b62860640792a669f81cf278a88d5cff6199ae
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-01-21 17:46:14 +0530

    Improve destructuring js_only

--- assets/prototype/domain/shared/dateRangeInput/dateDisplay.tsx
+++ assets/prototype/domain/shared/dateRangeInput/dateDisplay.tsx
@@ -20,11 +20,8 @@ interface DateTagProps extends DateFCProps {
 	date: Date;
 }
 
-export const DateTag: React.FC<DateTagProps> = ({
-	date,
-	withTime = false,
-	format: formatString = withTime ? FORMAT_TIME : FORMAT,
-}): JSX.Element => {
+export const DateTag: React.FC<DateTagProps> = ({ date, withTime = false, format: formatStr }): JSX.Element => {
+	const formatString = formatStr || (withTime ? FORMAT_TIME : FORMAT);
 	if (isValid(date)) {
 		return <Tag intent={Intent.SUCCESS}>{format(date, formatString)}</Tag>;
 	} else {
@@ -36,11 +33,14 @@ export const DateRangeDisplay: React.FC<DateRangeDisplayProps> = ({
 	className,
 	range: [start, end],
 	withTime = false,
-	format = withTime ? FORMAT_TIME : FORMAT,
-}): JSX.Element => (
-	<div className={classNames('docs-date-range', className)}>
-		<DateTag withTime={withTime} date={start} format={format} />
-		<Icon icon='arrow-right' />
-		<DateTag withTime={withTime} date={end} format={format} />
-	</div>
-);
+	format,
+}): JSX.Element => {
+	const formatString = format || (withTime ? FORMAT_TIME : FORMAT);
+	return (
+		<div className={classNames('docs-date-range', className)}>
+			<DateTag withTime={withTime} date={start} format={formatString} />
+			<Icon icon='arrow-right' />
+			<DateTag withTime={withTime} date={end} format={formatString} />
+		</div>
+	);
+};
