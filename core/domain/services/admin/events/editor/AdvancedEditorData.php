6f59309af5f703913b259d3bafa844fe35222b0d
Brent Christensen <brent@eventespresso.com>
2020-01-21 10:43:54 -0800

    only load eeEditorData once

--- core/domain/services/admin/events/editor/AdvancedEditorData.php
+++ core/domain/services/admin/events/editor/AdvancedEditorData.php
@@ -116,18 +116,6 @@ class AdvancedEditorData
                     'admin_footer',
                     static function () use ($data) {
                         wp_add_inline_script(
-                            EspressoEditorAssetManager::JS_HANDLE_EDITOR,
-                            "
-var eeEditorData={$data};
-",
-                            'before'
-                        );
-                    }
-                );
-                add_action(
-                    'admin_footer',
-                    static function () use ($data) {
-                        wp_add_inline_script(
                             EspressoEditorAssetManager::JS_HANDLE_EDITOR_PROTOTYPE,
                             "
 var eeEditorData={$data};
