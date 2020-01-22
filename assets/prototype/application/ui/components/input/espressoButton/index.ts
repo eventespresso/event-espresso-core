2f13bde690c40b5bc9291f15e9303c614dda8f4b
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-01-22 23:28:34 +0530

    Fix EspressoButton types

--- assets/prototype/application/ui/components/input/espressoButton/index.ts
+++ assets/prototype/application/ui/components/input/espressoButton/index.ts
@@ -1,2 +1,2 @@
 export { default as EspressoButton } from './EspressoButton';
-export { ClickEvent, EspressoButtonProps, EspressoButtonSize, EspressoButtonStyle } from './types';
+export { ClickHandler, EspressoButtonProps, EspressoButtonSize, EspressoButtonStyle } from './types';
