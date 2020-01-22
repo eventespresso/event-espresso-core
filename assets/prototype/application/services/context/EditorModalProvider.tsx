deff41e755a9255be554159d4baf90d16eecd719
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-01-22 23:30:08 +0530

    Create Modal Context

---
+++ assets/prototype/application/services/context/EditorModalProvider.tsx
@@ -0,0 +1,19 @@
+/**
+ * External imports
+ */
+import React, { createContext } from 'react';
+
+import { ProviderProps } from './types';
+import { useEditorModalManager } from '../../ui/components/layout/eeditorModal';
+
+const EditorModalContext = createContext(null);
+
+const { Provider, Consumer: EditorModalConsumer } = EditorModalContext;
+
+const EditorModalProvider: React.FC<ProviderProps> = (props): JSX.Element => {
+	const editorModal = useEditorModalManager();
+
+	return <Provider value={editorModal}>{props.children}</Provider>;
+};
+
+export { EditorModalContext, EditorModalProvider, EditorModalConsumer };
