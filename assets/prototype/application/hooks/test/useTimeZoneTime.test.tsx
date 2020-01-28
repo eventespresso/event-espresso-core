52c7fcf19e9343f3dd3bfcace90f7f4ab0e748d3
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-01-28 18:32:36 +0530

    Update comments js_only

--- assets/prototype/application/hooks/test/useTimeZoneTime.test.tsx
+++ assets/prototype/application/hooks/test/useTimeZoneTime.test.tsx
@@ -8,8 +8,8 @@ describe('useTimeZoneTime', () => {
 
 	describe('localTimeToUtc', () => {
 		it('returns UTC date for a given local date as ISO string', () => {
-			const testDate = '2020-01-28T13:00:20'; // IST
-			const expectedDate = '2020-01-28T07:30:20.000Z'; // 5:30 ahead of UTC
+			const testDate = '2020-01-28T13:00:20'; // in IST
+			const expectedDate = '2020-01-28T07:30:20.000Z'; // in UTC (5:30 behind IST)
 			const {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
@@ -20,8 +20,8 @@ describe('useTimeZoneTime', () => {
 		});
 
 		it('returns UTC date for a given local date as Date insatance', () => {
-			const testDate = new Date(2020, 0 /* Jan */, 28, 13, 0, 20, 0); // IST
-			const expectedDate = '2020-01-28T07:30:20.000Z'; // 5:30 ahead of UTC
+			const testDate = new Date(2020, 0 /* Jan */, 28, 13, 0, 20, 0); // in IST
+			const expectedDate = '2020-01-28T07:30:20.000Z'; // in UTC (5:30 behind IST)
 			const {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
@@ -34,8 +34,8 @@ describe('useTimeZoneTime', () => {
 
 	describe('utcToLocalTime', () => {
 		it('returns local date for a given UTC date as ISO string', () => {
-			const testDate = '2020-01-28T07:30:20'; // IST
-			const expectedDate = '2020-01-28T02:00:20.000Z'; // 5:30 behind IST
+			const testDate = '2020-01-28T07:30:20'; // in UTC
+			const expectedDate = '2020-01-28T02:00:20.000Z'; // in IST (5:30 ahead of UTC)
 			const {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
@@ -46,8 +46,8 @@ describe('useTimeZoneTime', () => {
 		});
 
 		it('returns local date for a given UTC date as Date insatance', () => {
-			const testDate = new Date(2020, 0 /* Jan */, 28, 7, 30, 20, 0); // IST
-			const expectedDate = '2020-01-28T02:00:20.000Z'; // 5:30 behind IST
+			const testDate = new Date(2020, 0 /* Jan */, 28, 7, 30, 20, 0); // in UTC
+			const expectedDate = '2020-01-28T02:00:20.000Z'; // in IST (5:30 ahead of UTC)
 			const {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
