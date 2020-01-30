9990ac8804cd38697fd1e66304c067ceec2d520e
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-01-30 15:12:51 +0530

    Add mutation feedback toaster js_only

--- assets/prototype/application/services/apollo/mutations/useEntityMutation.ts
+++ assets/prototype/application/services/apollo/mutations/useEntityMutation.ts
@@ -7,6 +7,8 @@ import { pathOr } from 'ramda';
 
 import { mutations, useMutators } from '../../../../domain/eventEditor/data/mutations';
 import useIfMounted from '../../../hooks/useIfMounted';
+import useToaster from '../../toaster/useToaster';
+import { ucFirst } from '../../../utilities/text';
 import {
 	EntityMutation,
 	OnMutationCompletedFn,
@@ -31,6 +33,7 @@ const useEntityMutation = (type: EntityType, id?: string): EntityMutation => {
 	const [result, setResult] = useState(DEFAULT_RESULT);
 	const mutators = useMutators();
 	const ifMounted = useIfMounted();
+	const toaster = useToaster();
 
 	/**
 	 * @param {string} mutationType Type of mutation - CREATE|UPDATE|DELETE
@@ -119,9 +122,23 @@ const useEntityMutation = (type: EntityType, id?: string): EntityMutation => {
 	};
 
 	/**
+	 * @param {string} mutationType Type of mutation - CREATE|UPDATE|DELETE
+	 * @param {string} suffix       Suffix to be added to mutation type
+	 */
+	const getToasterMessage = (mutationType: MutationType, suffix: 'ing' | 'ed' = 'ing'): any => {
+		// For example "CREATE" will become "creating" or "created"
+		const verb = mutationType.toLowerCase().replace(/e$/, suffix);
+		// e.g. "updating datetime"
+		return `${verb} ${type.toLowerCase()}`;
+	};
+
+	/**
 	 *
 	 */
-	const onMutationStart = (): void => {
+	const onMutationStart = (mutationType: MutationType): void => {
+		const message = getToasterMessage(mutationType);
+		toaster.loading(true, message);
+
 		updateResult({
 			loading: true,
 			error: undefined,
@@ -133,7 +150,13 @@ const useEntityMutation = (type: EntityType, id?: string): EntityMutation => {
 	/**
 	 *
 	 */
-	const onMutationComplete = (response: FetchResult, onCompleted: OnMutationCompletedFn): void => {
+	const onMutationComplete = (
+		response: FetchResult,
+		onCompleted: OnMutationCompletedFn,
+		mutationType: MutationType
+	): void => {
+		toaster.dismiss(getToasterMessage(mutationType));
+		toaster.success(`successfully ${getToasterMessage(mutationType, 'ed')}`);
 		const { data, errors } = response;
 		const error = errors && errors.length > 0 ? new ApolloError({ graphQLErrors: errors }) : undefined;
 
@@ -152,7 +175,10 @@ const useEntityMutation = (type: EntityType, id?: string): EntityMutation => {
 	/**
 	 *
 	 */
-	const onMutationError = (error: Error, onError: OnMutationErrorFn): void => {
+	const onMutationError = (error: Error, onError: OnMutationErrorFn, mutationType: MutationType): void => {
+		toaster.dismiss(getToasterMessage(mutationType));
+		toaster.error(`error ${getToasterMessage(mutationType)}`);
+
 		updateResult({
 			loading: false,
 			error,
@@ -177,17 +203,17 @@ const useEntityMutation = (type: EntityType, id?: string): EntityMutation => {
 	/**
 	 *
 	 */
-	const mutate = (options: CustomMutationOptions): MutationResult => {
+	const mutate = (options: CustomMutationOptions, mutationType: MutationType): MutationResult => {
 		const { onCompleted, onError, ...mutationOptions } = options;
-		onMutationStart();
+		onMutationStart(mutationType);
 		client
 			.mutate(mutationOptions)
 			.then((response: FetchResult) => {
-				onMutationComplete(response, onCompleted);
+				onMutationComplete(response, onCompleted, mutationType);
 				return response;
 			})
 			.catch((error: Error) => {
-				onMutationError(error, onError);
+				onMutationError(error, onError, mutationType);
 				throw error;
 			});
 
--- assets/prototype/application/services/apollo/mutations/useEntityMutator.ts
+++ assets/prototype/application/services/apollo/mutations/useEntityMutator.ts
@@ -1,11 +1,12 @@
 import useEntityMutation from './useEntityMutation';
 import {
-	EntityMutator,
-	MutationInput,
 	BackwardSubscription,
-	MutationResult,
 	CustomMutationOptions,
+	EntityMutator,
 	EntityType,
+	MutationInput,
+	MutationResult,
+	MutationType,
 } from './types';
 
 /**
@@ -20,7 +21,7 @@ const useEntityMutator = (type: EntityType, id = ''): EntityMutator => {
 	 * @param {object} subscriptions
 	 */
 	const createEntity = (input: MutationInput, subscriptions: BackwardSubscription = {}): MutationResult => {
-		return subscribeAndMutate(getCreateMutation(input), subscriptions);
+		return subscribeAndMutate(getCreateMutation(input), subscriptions, MutationType.Create);
 	};
 
 	/**
@@ -28,7 +29,7 @@ const useEntityMutator = (type: EntityType, id = ''): EntityMutator => {
 	 * @param {object} subscriptions
 	 */
 	const updateEntity = (input: MutationInput, subscriptions: BackwardSubscription = {}): MutationResult => {
-		return subscribeAndMutate(getUpdateMutation(input), subscriptions);
+		return subscribeAndMutate(getUpdateMutation(input), subscriptions, MutationType.Update);
 	};
 
 	/**
@@ -36,7 +37,7 @@ const useEntityMutator = (type: EntityType, id = ''): EntityMutator => {
 	 * @param {object} subscriptions
 	 */
 	const deleteEntity = (input: MutationInput = {}, subscriptions: BackwardSubscription = {}): MutationResult => {
-		return subscribeAndMutate(getDeleteMutation(input), subscriptions);
+		return subscribeAndMutate(getDeleteMutation(input), subscriptions, MutationType.Delete);
 	};
 
 	/**
@@ -45,7 +46,8 @@ const useEntityMutator = (type: EntityType, id = ''): EntityMutator => {
 	 */
 	const subscribeAndMutate = (
 		options: CustomMutationOptions,
-		subscriptions: BackwardSubscription
+		subscriptions: BackwardSubscription,
+		mutationType: MutationType
 	): MutationResult => {
 		// These are backward subscriptions towards components
 		// i.e. when a component wants to be notified
@@ -71,7 +73,7 @@ const useEntityMutator = (type: EntityType, id = ''): EntityMutator => {
 				fwdOnError(error);
 			}
 		};
-		return mutate({ ...mutationOptions, onCompleted, onError });
+		return mutate({ ...mutationOptions, onCompleted, onError }, mutationType);
 	};
 
 	return {
