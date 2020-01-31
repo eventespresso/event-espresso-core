cfd1af7b192090f897a1ae50be094a9738f40674
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-01-31 12:52:10 +0200

    Added DEVELOPMENT const; js_only.

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
-process.env.NODE_ENV = 'development';
+const DEVELOPMENT = 'development';
+process.env.NODE_ENV = DEVELOPMENT;
 
 const fs = require('fs-extra');
 const paths = require('../config/paths');
 const webpack = require('webpack');
 const config = require('../config/webpack.config.js');
-const devConfig = config('development');
+const devConfig = config(DEVELOPMENT);
 
 webpack(devConfig).watch({}, (err, stats) => {
 	if (err) {
