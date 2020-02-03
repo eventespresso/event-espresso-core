4b3135dc00cbbc4083fa674b13c7de4146fcf31d
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-02-04 00:32:45 +0530

    Remove unnecessary test for isTrashed js_only

--- assets/prototype/domain/shared/predicates/shared/isTrashed/index.test.ts
+++ assets/prototype/domain/shared/predicates/shared/isTrashed/index.test.ts
@@ -1,29 +1,7 @@
-import { omit } from 'ramda';
-
 import isTrashed from './';
 import { nodes as datetimes } from '../../../../eventEditor/data/queries/datetimes/test/data';
 
 describe('isTrashed', () => {
-	it('should return false if isTrashed prop is undefined', () => {
-		datetimes.forEach((datetime) => {
-			const newDatetime: any = omit(['isTrashed'], datetime);
-			const result = isTrashed(newDatetime);
-			expect(result).toBe(undefined);
-		});
-
-		datetimes.forEach((datetime) => {
-			const newDatetime = { ...datetime, isTrashed: null };
-			const result = isTrashed(newDatetime);
-			expect(result).toBe(null);
-		});
-
-		datetimes.forEach((datetime) => {
-			const newDatetime = { ...datetime, isTrashed: undefined };
-			const result = isTrashed(newDatetime);
-			expect(result).toBe(undefined);
-		});
-	});
-
 	it(`should return false if isTrashed prop is boolean and it's value is false`, () => {
 		datetimes.forEach((datetime) => {
 			const newDatetime = { ...datetime, isTrashed: false };
