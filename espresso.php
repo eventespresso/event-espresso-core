24965b620a75e5cf6782bed9a828c43467bff743
Viktor Szépe <viktor@szepe.net>
2020-02-05 23:35:03 +0100

    Normalize main plugin file
    and fix comments in PHPDoc blocks

--- espresso.php
+++ espresso.php
@@ -1,4 +1,4 @@
-<?php defined('ABSPATH') || exit('No direct script access allowed');
+<?php
 /*
   Plugin Name:Event Espresso
   Plugin URI: http://eventespresso.com/pricing/?ee_ver=ee4&utm_source=ee4_plugin_admin&utm_medium=link&utm_campaign=wordpress_plugins_page&utm_content=support_link
@@ -37,11 +37,15 @@
  * @link            {@link http://www.eventespresso.com}
  * @since           4.0
  */
+
+if (!defined('ABSPATH')) {
+    exit('No direct script access allowed');
+}
+
 if (function_exists('espresso_version')) {
     if (! function_exists('espresso_duplicate_plugin_error')) {
         /**
-         *    espresso_duplicate_plugin_error
-         *    displays if more than one version of EE is activated at the same time
+         * Displays a notice if more than one version of EE is activated at the same time.
          */
         function espresso_duplicate_plugin_error()
         {
@@ -52,37 +56,6 @@ if (function_exists('espresso_version')) {
                     echo esc_html__(
                         'Can not run multiple versions of Event Espresso! One version has been automatically deactivated. Please verify that you have the correct version you want still active.',
                         'event_espresso'
-                    ); ?>
-                </p>
-            </div>
-            <?php
-            espresso_deactivate_plugin(plugin_basename(__FILE__));
-        }
-    }
-    add_action('admin_notices', 'espresso_duplicate_plugin_error', 1);
-} else {
-    define('EE_MIN_PHP_VER_REQUIRED', '5.4.0');
-    if (! version_compare(PHP_VERSION, EE_MIN_PHP_VER_REQUIRED, '>=')) {
-        /**
-         * espresso_minimum_php_version_error
-         *
-         * @return void
-         */
-        function espresso_minimum_php_version_error()
-        {
-            ?>
-            <div class="error">
-                <p>
-                    <?php
-                    printf(
-                        esc_html__(
-                            'We\'re sorry, but Event Espresso requires PHP version %1$s or greater in order to operate. You are currently running version %2$s.%3$sIn order to update your version of PHP, you will need to contact your current hosting provider.%3$sFor information on stable PHP versions, please go to %4$s.',
-                            'event_espresso'
-                        ),
-                        EE_MIN_PHP_VER_REQUIRED,
-                        PHP_VERSION,
-                        '<br/>',
-                        '<a href="http://php.net/downloads.php">http://php.net/downloads.php</a>'
                     );
                     ?>
                 </p>
@@ -90,44 +63,75 @@ if (function_exists('espresso_version')) {
             <?php
             espresso_deactivate_plugin(plugin_basename(__FILE__));
         }
-
-        add_action('admin_notices', 'espresso_minimum_php_version_error', 1);
-    } else {
-        define('EVENT_ESPRESSO_MAIN_FILE', __FILE__);
-        /**
-         * espresso_version
-         * Returns the plugin version
-         *
-         * @return string
-         */
-        function espresso_version()
-        {
-            return apply_filters('FHEE__espresso__espresso_version', '4.10.4.rc.007');
-        }
-
-        /**
-         * espresso_plugin_activation
-         * adds a wp-option to indicate that EE has been activated via the WP admin plugins page
-         */
-        function espresso_plugin_activation()
-        {
-            update_option('ee_espresso_activation', true);
-        }
-
-        register_activation_hook(EVENT_ESPRESSO_MAIN_FILE, 'espresso_plugin_activation');
-
-        require_once __DIR__ . '/core/bootstrap_espresso.php';
-        bootstrap_espresso();
     }
+    add_action('admin_notices', 'espresso_duplicate_plugin_error', 1, 0);
+    return;
 }
+
+define('EE_MIN_PHP_VER_REQUIRED', '5.4.0');
+
+if (! version_compare(PHP_VERSION, EE_MIN_PHP_VER_REQUIRED, '>=')) {
+    /**
+     * Displays a notice about required PHP version.
+     */
+    function espresso_minimum_php_version_error()
+    {
+        ?>
+        <div class="error">
+            <p>
+                <?php
+                printf(
+                    esc_html__(
+                        'We\'re sorry, but Event Espresso requires PHP version %1$s or greater in order to operate. You are currently running version %2$s.%3$sIn order to update your version of PHP, you will need to contact your current hosting provider.%3$sFor information on stable PHP versions, please go to %4$s.',
+                        'event_espresso'
+                    ),
+                    EE_MIN_PHP_VER_REQUIRED,
+                    PHP_VERSION,
+                    '<br/>',
+                    '<a href="http://php.net/downloads.php">http://php.net/downloads.php</a>'
+                );
+                ?>
+            </p>
+        </div>
+        <?php
+        espresso_deactivate_plugin(plugin_basename(__FILE__));
+    }
+    add_action('admin_notices', 'espresso_minimum_php_version_error', 1, 0);
+    return;
+}
+
+define('EVENT_ESPRESSO_MAIN_FILE', __FILE__);
+
+/**
+ * Returns the plugin version
+ *
+ * @return string
+ */
+function espresso_version()
+{
+    return apply_filters('FHEE__espresso__espresso_version', '4.10.4.rc.007');
+}
+
+/**
+ * Adds a wp-option to indicate that EE has been activated via the admin plugins page.
+ */
+function espresso_plugin_activation()
+{
+    update_option('ee_espresso_activation', true);
+}
+register_activation_hook(EVENT_ESPRESSO_MAIN_FILE, 'espresso_plugin_activation');
+
+require_once __DIR__ . '/core/bootstrap_espresso.php';
+bootstrap_espresso();
+
 if (! function_exists('espresso_deactivate_plugin')) {
     /**
-     *    deactivate_plugin
-     * usage:  espresso_deactivate_plugin( plugin_basename( __FILE__ ));
+     * Deactivate plugin.
+     *
+     * Usage:  espresso_deactivate_plugin(plugin_basename(__FILE__));
      *
      * @access public
-     * @param string $plugin_basename - the results of plugin_basename( __FILE__ ) for the plugin's main file
-     * @return    void
+     * @param string $plugin_basename The results of plugin_basename(__FILE__) for the plugin's main file.
      */
     function espresso_deactivate_plugin($plugin_basename = '')
     {
