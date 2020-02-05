4e0a9f9f1d58bd58ab851d94b798c834061d8e71
Brent Christensen <brent@eventespresso.com>
2020-02-04 17:26:39 -0800

    remove unnecessary logic after updating form data

--- assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/buttons/AddPriceModifierButtonData.tsx
+++ assets/prototype/domain/eventEditor/tickets/ticketPriceCalculator/buttons/AddPriceModifierButtonData.tsx
@@ -1,23 +1,18 @@
 // @ts-nocheck
 import React, { useCallback } from 'react';
-import clone from 'ramda/src/clone';
 import { __ } from '@wordpress/i18n';
 
 import AddPriceModifierButton from './AddPriceModifierButton';
 import { AddPriceModifierDataProps } from '../types';
 import usePriceTypeForPrice from '../../../data/queries/priceTypes/usePriceTypeForPrice';
 import { Price } from '../../../data/types';
-// import { findEntityByGuid } from '../../../../shared/predicates/shared/selectionPredicates';
-import { useRelations } from '../../../../../application/services/apollo/relations';
 
 const AddPriceModifierButtonData = ({ name, price, push, reset, sort }: AddPriceModifierDataProps): JSX.Element => {
-	const { addRelation } = useRelations();
 	const baseType = usePriceTypeForPrice(price.id);
 	const addPriceModifier = useCallback(() => {
 		if (Number(price.amount)) {
-			const priceClone = clone(price);
 			const newPrice: Price = {
-				...priceClone,
+				...price,
 				id: '',
 				isBasePrice: baseType.isBasePrice,
 				isDiscount: baseType.isDiscount,
@@ -25,12 +20,6 @@ const AddPriceModifierButtonData = ({ name, price, push, reset, sort }: AddPrice
 				isTax: baseType.isTax,
 				order: baseType.order,
 			};
-			addRelation({
-				entity: 'prices',
-				entityId: newPrice.id,
-				relation: 'priceTypes',
-				relationId: baseType.id,
-			});
 			push(newPrice);
 			reset(name);
 			sort();
@@ -38,7 +27,7 @@ const AddPriceModifierButtonData = ({ name, price, push, reset, sort }: AddPrice
 			alert(__('Please enter an amount for the new price modifier.'));
 			return;
 		}
-	}, []);
+	}, [price.name, price.priceType, price.desc, price.amount]);
 	return <AddPriceModifierButton addPriceModifier={addPriceModifier} />;
 };
 export default AddPriceModifierButtonData;
