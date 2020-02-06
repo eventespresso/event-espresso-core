04b63a5a7aef9bd8501cd1c1f8d84f07371b5780
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-02-06 16:23:56 +0530

    More type fixes js_only

--- assets/src/domain/shared/entities/tickets/predicates/selectionPredicates.ts
+++ assets/src/domain/shared/entities/tickets/predicates/selectionPredicates.ts
@@ -5,12 +5,14 @@ import { entityHasGuid } from '../../../services/predicates';
 import { Ticket } from '../../../../eventEditor/services/apollo/types';
 const NO_INDEX = -1;
 
-export const isTicketField = (val, key): boolean => indexOf(key, ticketFields) > NO_INDEX;
+export const isTicketField = (val: any, key: keyof Ticket): boolean => indexOf(key, ticketFields) > NO_INDEX;
 export const isExpired = propEq('isExpired', true);
-export const updateTicketPrice = (amount): ((obj: Ticket) => Record<keyof Ticket, number> & Ticket) =>
+export const updateTicketPrice = (amount: number): ((obj: Ticket) => Record<keyof Ticket, number> & Ticket) =>
 	assoc<number, keyof Ticket>('price', amount);
-export const updateReverseCalculate = (reverseCalculate): ((obj: Ticket) => Record<keyof Ticket, number> & Ticket) =>
-	assoc<number, keyof Ticket>('reverseCalculate', reverseCalculate);
+export const updateReverseCalculate = (
+	reverseCalculate: boolean
+): ((obj: Ticket) => Record<keyof Ticket, boolean> & Ticket) =>
+	assoc<boolean, keyof Ticket>('reverseCalculate', reverseCalculate);
 
 export const updateTicketPriceForTicket = ({ tickets, guid, amount }): Ticket[] =>
 	map(when(entityHasGuid(guid), updateTicketPrice(amount)), tickets);
