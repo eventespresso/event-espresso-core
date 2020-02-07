3623183b752ad62100615c4a3dd437ea9ef0d1b7
Brent Christensen <brent@eventespresso.com>
2020-02-07 14:53:26 -0800

    EntityPaperFrame

---
+++ assets/src/application/ui/layout/EntityPaperFrame/index.tsx
@@ -0,0 +1,22 @@
+import { Card } from 'antd';
+import React from 'react';
+
+import EntityIDs from '@appDisplay/EntityIDs';
+import './style.css';
+
+/**
+ * EntityPaperFrame
+ * adds a styled frame that gives the appearance
+ * of a piece of paper on a surface
+ */
+const EntityPaperFrame = ({ children, entity }) => {
+	return (
+		<div id={`ee-entity-paper-frame-${entity.id}`} className='ee-entity-paper-frame-wrapper'>
+			<EntityIDs dbid={entity.dbId} guid={entity.id} />
+			<div className='ee-entity-paper-frame'></div>
+			<div className='ee-entity-wrapper'>{children}</div>
+		</div>
+	);
+};
+
+export default EntityPaperFrame;
