409d228635e61e28a392a0bd1e75140155d2e4b5
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-02-10 19:55:15 +0530

    Style it! :)

--- assets/src/application/ui/forms/ee-form/Submit.tsx
+++ assets/src/application/ui/forms/ee-form/Submit.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
+import { useForm } from 'react-final-form';
 
 import { Button } from './adapters';
 import { SubmitProps } from './types';
 
-const Submit: React.FC<SubmitProps> = ({ submitting, label, ...rest }) => {
+const Submit: React.FC<SubmitProps> = ({ submitting, submitButton, resetButton }) => {
+	const form = useForm();
 	return (
-		<div className='submit-wrap'>
+		<div className='submit-wrapper'>
 			<Button
 				icon='save'
 				htmlType='submit'
 				disabled={submitting}
 				loading={submitting}
 				className='submit-button'
-				{...rest}
+				type='primary'
+				{...submitButton}
 			>
-				{label || __('Submit')}
+				{submitButton.label || __('Submit')}
 			</Button>
+			&nbsp;
+			{resetButton ? (
+				<Button
+					htmlType='reset'
+					disabled={submitting}
+					className='reset-button'
+					onClick={() => form.reset()}
+					{...resetButton}
+				>
+					{resetButton.label || __('Reset')}
+				</Button>
+			) : null}
 		</div>
 	);
 };
--- assets/src/application/ui/forms/ee-form/index.tsx
+++ assets/src/application/ui/forms/ee-form/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Form as ReactFinalForm } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
 
 import { EspressoFormProps } from './types';
-import FormRenderer from './FormRenderer';
+import FormRenderer from './renderers/FormRenderer';
 import { formConfig } from './config';
+import './styles.css';
 
 const EspressoForm: React.FC<EspressoFormProps> = ({ onSubmit, initialValues, validate, ...rest }) => {
 	return (
-		<ReactFinalForm
-			initialValues={initialValues}
-			onSubmit={onSubmit}
-			validate={validate}
-			render={FormRenderer}
-			{...rest}
-		/>
+		<div className='ee-form'>
+			<ReactFinalForm
+				initialValues={initialValues}
+				onSubmit={onSubmit}
+				validate={validate}
+				render={FormRenderer}
+				mutators={{ ...arrayMutators }}
+				{...rest}
+			/>
+		</div>
 	);
 };
 
