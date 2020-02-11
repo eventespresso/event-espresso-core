cc9c0c3a148dbc5ffb462aed7417e5a5cf16d27d
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-02-11 16:20:21 +0200

    Added EntityPagination; js_only.

---
+++ assets/src/application/ui/layout/entityList/pagination/EntityPagination.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Pagination } from 'antd';
+import { PaginationProps } from 'antd/lib/pagination';
+
+import './style.scss';
+
+/**
+ * Adds pagination to an "EntityList" component
+ * @return EntityPagination
+ */
+const EntityPagination = ({
+	current,
+	onChange,
+	pageSize = 10,
+	pageSizeOptions = ['10', '20', '30', '40'],
+	showTotal,
+	total,
+}: PaginationProps) => {
+	return (
+		<div className='ee-entity-pagination'>
+			<Pagination
+				current={current}
+				onChange={onChange}
+				pageSize={pageSize}
+				pageSizeOptions={pageSizeOptions}
+				showTotal={showTotal}
+				total={total}
+			/>
+		</div>
+	);
+};
+
+export default EntityPagination;
--- assets/src/application/ui/layout/entityList/pagination/index.js
+++ assets/src/application/ui/layout/entityList/pagination/index.js
@@ -1,2 +1,2 @@
-export { default as EntityPagination } from './entity-pagination';
-export { default as useEntityPagination } from './use-entity-pagination';
+export { default as EntityPagination } from './EntityPagination';
+export { default as useEntityPagination } from './useEntityPagination';
