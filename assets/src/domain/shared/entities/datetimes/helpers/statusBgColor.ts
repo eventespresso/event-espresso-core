9372e900c1c7480f91d3640c78d20260a96fd0ff
Brent Christensen <brent@eventespresso.com>
2020-02-11 09:51:58 -0800

    statusBgColor() for setting datetime background colour classes js_only

---
+++ assets/src/domain/shared/entities/datetimes/helpers/statusBgColor.ts
@@ -0,0 +1,19 @@
+import { Datetime } from '@edtrServices/apollo/types';
+
+const statusBgColor = (date: Datetime): string => {
+	if (date.isTrashed) {
+		return 'ee-status-background-color-DTT';
+	}
+	if (date.isExpired) {
+		return 'ee-status-background-color-DTE';
+	}
+	if (date.isSoldOut) {
+		return 'ee-status-background-color-DTS';
+	}
+	if (date.isActive) {
+		return 'ee-status-background-color-DTA';
+	}
+	return 'ee-status-background-color-DTU';
+};
+
+export default statusBgColor;
