561f37f63cea2bf491ce76805777927a4272642c
Brent Christensen <brent@eventespresso.com>
2020-02-11 17:39:57 -0800

    better date handling and typing

--- assets/src/application/ui/calendars/dateDisplay/BiggieCalendarDate/index.tsx
+++ assets/src/application/ui/calendars/dateDisplay/BiggieCalendarDate/index.tsx
@@ -1,13 +1,13 @@
 import classNames from 'classnames';
-import { format, isValid } from 'date-fns';
-import React from 'react';
+import { format, parseISO, isValid } from 'date-fns';
+import React, { useCallback } from 'react';
 
 import EspressoButton from '@appInputs/EspressoButton';
 import { CalendarDateProps, CalendarPosition } from '../types';
 import './style.css';
 
 export interface BiggieCalendarDateProps extends CalendarDateProps {
-	date: Date;
+	date: Date | string;
 }
 
 /**
@@ -23,34 +23,37 @@ export const BiggieCalendarDate: React.FC<BiggieCalendarDateProps> = ({
 	editButton = {},
 	showTime = false,
 }) => {
-	if (!isValid(date)) {
+	const onEditHandler = useCallback((event) => onEdit(event), [onEdit]);
+	const dateObject = date instanceof Date ? date : parseISO(date);
+	if (!isValid(dateObject)) {
 		return null;
 	}
 	const classes = classNames('ee-biggie-calendar-date-bg', htmlClass, {
 		'ee-bcd-pos-left': position === CalendarPosition.LEFT,
 		'ee-bcd-pos-right': position !== CalendarPosition.LEFT,
 	});
-	const editDateButton =
-		typeof onEdit === 'function' ? (
-			<EspressoButton
-				className='ee-edit-calendar-date-btn'
-				onClick={(event: React.MouseEvent<HTMLButtonElement>) => onEdit(event)}
-				onKeyPress={(event: React.MouseEvent<HTMLButtonElement>) => onEdit(event)}
-				tooltip={editButton.tooltip}
-				labelPosition={editButton.tooltipPosition}
-				icon='calendar'
-			/>
-		) : null;
+
+	const editDateButton = typeof onEdit === 'function' && (
+		<EspressoButton
+			className='ee-edit-calendar-date-btn'
+			onClick={onEditHandler}
+			onKeyPress={onEditHandler}
+			tooltip={editButton.tooltip}
+			labelPosition={editButton.tooltipPosition}
+			icon='calendar'
+		/>
+	);
+
 	return (
 		<div className={classes}>
 			{headerText && <div className='ee-biggie-calendar-date-header'>{headerText}</div>}
 			<div className='ee-biggie-calendar-date'>
-				<div className='ee-bcd-weekday'>{format(date, 'eeee')}</div>
-				<div className='ee-bcd-month'>{format(date, 'MMMM')}</div>
+				<div className='ee-bcd-weekday'>{format(dateObject, 'eeee')}</div>
+				<div className='ee-bcd-month'>{format(dateObject, 'MMMM')}</div>
 				<div className='ee-bcd-month-day-sep'></div>
-				<div className='ee-bcd-day'>{format(date, 'ee')}</div>
-				<div className='ee-bcd-year'>{format(date, 'yyyy')}</div>
-				{showTime && <div className='ee-bcd-time'>{format(date, 'h:mm a')}</div>}
+				<div className='ee-bcd-day'>{format(dateObject, 'ee')}</div>
+				<div className='ee-bcd-year'>{format(dateObject, 'yyyy')}</div>
+				{showTime && <div className='ee-bcd-time'>{format(dateObject, 'h:mm a')}</div>}
 			</div>
 			{footerText && <div className='ee-biggie-calendar-date-footer'>{footerText}</div>}
 			{editDateButton}
