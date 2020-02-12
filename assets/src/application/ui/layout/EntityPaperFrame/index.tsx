201b72585357d157744dc9e92598b87bd7af07bf
Brent Christensen <brent@eventespresso.com>
2020-02-12 08:17:17 -0800

    better control over positioning of elements

--- assets/src/application/ui/layout/EntityPaperFrame/index.tsx
+++ assets/src/application/ui/layout/EntityPaperFrame/index.tsx
@@ -13,8 +13,9 @@ const EntityPaperFrame = ({ children, entity }) => {
 	return (
 		<div id={`ee-entity-paper-frame-${entity.id}`} className='ee-entity-paper-frame-wrapper'>
 			<EntityIDs dbid={entity.dbId} guid={entity.id} />
-			<div className='ee-entity-paper-frame'></div>
-			<div className='ee-entity-wrapper'>{children}</div>
+			<div className='ee-entity-paper-frame'>
+				<div className='ee-entity-inner-wrapper'>{children}</div>
+			</div>
 		</div>
 	);
 };
