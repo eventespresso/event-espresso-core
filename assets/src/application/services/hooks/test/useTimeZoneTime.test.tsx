31ca1ffff4844ab376041ce7551763f950e63a43
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-02-13 19:47:21 +0530

    Fix timezone tests

--- assets/src/application/services/hooks/test/useTimeZoneTime.test.tsx
+++ assets/src/application/services/hooks/test/useTimeZoneTime.test.tsx
@@ -1,4 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
+import { formatISO } from 'date-fns';
 
 import useTimeZoneTime from '../useTimeZoneTime';
 import { ApolloMockedProvider } from '../../../../domain/eventEditor/services/context/TestContext';
@@ -14,9 +15,9 @@ describe('useTimeZoneTime', () => {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
 
-			const result = TZ.localTimeToUtc(testDate).toISOString();
+			const result = TZ.localTimeToUtc(testDate);
 
-			expect(result).toBe(expectedDate);
+			expect(result.toISOString()).toBe(expectedDate);
 		});
 
 		it('returns UTC date for a given local date as Date insatance', () => {
@@ -26,35 +27,35 @@ describe('useTimeZoneTime', () => {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
 
-			const result = TZ.localTimeToUtc(testDate).toISOString();
+			const result = TZ.localTimeToUtc(testDate);
 
-			expect(result).toBe(expectedDate);
+			expect(result.toISOString()).toBe(expectedDate);
 		});
 	});
 
 	describe('utcToLocalTime', () => {
 		it('returns local date for a given UTC date as ISO string', () => {
-			const testDate = '2020-01-28T07:30:20'; // in UTC
-			const expectedDate = '2020-01-28T13:00:20.000Z'; // in IST (5:30 ahead of UTC)
+			const testDate = '2020-01-28T07:30:20.123Z'; // in UTC
+			const expectedDate = '2020-01-28T13:00:20+5.5:30'; // in IST (5:30 ahead of UTC)
 			const {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
 
-			const result = TZ.utcToLocalTime(testDate).toISOString();
+			const result = TZ.utcToLocalTime(testDate);
 
-			expect(result).toBe(expectedDate);
+			expect(formatISO(result)).toBe(expectedDate);
 		});
 
 		it('returns local date for a given UTC date as Date insatance', () => {
-			const testDate = new Date(2020, 0 /* Jan */, 28, 7, 30, 20, 0); // in UTC
-			const expectedDate = '2020-01-28T13:00:20.000Z'; // in IST (5:30 ahead of UTC)
+			const testDate = new Date('2020-01-28T07:30:20.123Z'); // in UTC
+			const expectedDate = '2020-01-28T13:00:20+5.5:30'; // in IST (5:30 ahead of UTC)
 			const {
 				result: { current: TZ },
 			} = renderHook(() => useTimeZoneTime(), { wrapper });
 
-			const result = TZ.utcToLocalTime(testDate).toISOString();
+			const result = TZ.utcToLocalTime(testDate);
 
-			expect(result).toBe(expectedDate);
+			expect(formatISO(result)).toBe(expectedDate);
 		});
 	});
 });
