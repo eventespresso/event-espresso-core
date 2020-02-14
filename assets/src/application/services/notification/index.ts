4b50c9dfc2c4606cf88221260eabe02e6596c46a
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-02-14 18:18:17 +0200

    Added notification service; js_only.

---
+++ assets/src/application/services/notification/index.ts
@@ -0,0 +1,34 @@
+import { notification as defaultNotification } from 'antd';
+import { ArgsProps } from 'antd/lib/notification';
+
+interface NotificationProps extends ArgsProps {
+	type: 'success' | 'info' | 'warning' | 'error';
+}
+
+const getHash = (message): string => {
+	const msg = JSON.stringify(message);
+	return msg
+		.split('')
+		.reduce((a, b) => {
+			a = (a << 5) - a + b.charCodeAt(0);
+			return a & a;
+		}, 0)
+		.toString();
+};
+
+const notification = ({ description, duration, message, placement, type }: NotificationProps): void => {
+	const hash = getHash(message);
+	const args: ArgsProps = {
+		message,
+		description,
+		duration: duration,
+		key: hash,
+		placement: placement || 'bottomRight',
+	};
+
+	setTimeout(() => {
+		defaultNotification[type](args);
+	}, 0);
+};
+
+export default notification;
