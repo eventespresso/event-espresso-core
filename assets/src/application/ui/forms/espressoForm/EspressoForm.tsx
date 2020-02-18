cee229914daf0c09a6f9ecda5c7a980c8c86fcaf
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-02-18 19:30:42 +0530

    Update EspressoForm to support formWrapper

---
+++ assets/src/application/ui/forms/espressoForm/EspressoForm.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Form as ReactFinalForm } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
+
+import { EspressoFormProps } from './types';
+import FormRenderer from './renderers/FormRenderer';
+import { formConfig } from './config';
+import { FormProvider } from './context';
+import './styles.scss';
+
+const EspressoForm: React.FC<EspressoFormProps> = ({ onSubmit, mutators, layout, ...rest }) => {
+	const context = { layout };
+	return (
+		<div className='ee-form'>
+			<FormProvider value={context}>
+				<ReactFinalForm
+					onSubmit={onSubmit}
+					render={FormRenderer}
+					mutators={{
+						...arrayMutators,
+						...mutators,
+					}}
+					{...rest}
+				/>
+			</FormProvider>
+		</div>
+	);
+};
+
+export const TestForm = () => <EspressoForm {...formConfig} />;
+
+export default EspressoForm;
---
+++ assets/src/application/ui/forms/espressoForm/TestForm.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { formConfig } from './config';
+import EspressoForm from './EspressoForm';
+
+const TestForm = () => <EspressoForm {...formConfig} />;
+
+export default TestForm;
