1d8757f7288b8d4089ce4c4ea239a652d2634a89
Brent Christensen <brent@eventespresso.com>
2020-02-18 15:47:59 -0800

    turn off EDTR v1 css

--- assets/ZZZ/editor/index.js
+++ assets/ZZZ/editor/index.js
@@ -1,6 +1,6 @@
-import './editor.css';
-import '../components/ui/styles/themes/default';
+// import './editor.css';
+// import '../components/ui/styles/themes/default';
 // export * from './events';
 // export * from './helpers';
-import '../../../node_modules/@blueprintjs/core/lib/css/blueprint.css';
-import '../../../node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css';
+// import '../../../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+// import '../../../node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css';
