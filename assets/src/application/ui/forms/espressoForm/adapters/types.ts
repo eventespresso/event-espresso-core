4d9b250600de3b7dc5c68c2be68430c1561199ff
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-02-19 15:51:11 +0530

    Fix Select adapter types js_only

--- assets/src/application/ui/forms/espressoForm/adapters/types.ts
+++ assets/src/application/ui/forms/espressoForm/adapters/types.ts
@@ -1,12 +1,17 @@
-import { SelectProps as AntdSelectProps, SelectValue, OptionProps } from 'antd/lib/select';
+import { SelectProps as AntdSelectProps, SelectValue } from 'antd/lib/select';
 import { ButtonProps as AntdButtonProps } from 'antd/lib/button';
 import { FieldRendererProps } from '../types';
 import { FormItemProps as AntdFormItemProps } from 'antd/es/form';
 
 export type ButtonProps = AntdButtonProps;
 
-export interface SelectProps extends FieldRendererProps<SelectValue>, AntdSelectProps<SelectValue> {
+export interface SelectProps extends Pick<FieldRendererProps, 'input'>, Pick<AntdSelectProps<SelectValue>, 'mode'> {
 	options: SelectOptions;
+}
+
+export interface OptionProps {
+	label: string;
+	value: string;
 }
 
 export interface SelectOptionProps extends OptionProps {
