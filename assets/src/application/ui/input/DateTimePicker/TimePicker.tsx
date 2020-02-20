2b6cdb048bb0f4ed652e2d3639945a516c531cba
Brent Christensen <brent@eventespresso.com>
2020-02-20 13:33:48 -0800

    OOPS!!! fix copy pasta error js_only

--- assets/src/application/ui/input/DateTimePicker/TimePicker.tsx
+++ assets/src/application/ui/input/DateTimePicker/TimePicker.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { parse } from 'date-fns';
 import TimePickerBase from './TimePickerBase';
-import { CONVERT_TO_MOMENT_DATE_FORMAT } from '../../../constants';
+import { CONVERT_TO_MOMENT_TIME_FORMAT } from '../../../constants';
 
 const TimePicker = ({ input, ...rest }) => {
 	return (
 		<TimePickerBase
 			{...input}
 			{...rest}
-			value={input.value ? parse(input.value, CONVERT_TO_MOMENT_DATE_FORMAT, new Date()) : input.value}
+			value={input.value ? parse(input.value, CONVERT_TO_MOMENT_TIME_FORMAT, new Date()) : input.value}
 			onChange={(_, dateString) => input.onChange(dateString)}
 		/>
 	);
