e168bbaeef676ab414145c6122e563a75ebbd620
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-02-20 23:59:33 +0200

    Updated isRecentlyExpired; js_only.

--- assets/ZZZ/data/model/datetime/status-helper.js
+++ assets/ZZZ/data/model/datetime/status-helper.js
@@ -1,21 +1,6 @@
 import { __ } from '@eventespresso/i18n';
 
-import { MONTH_IN_SECONDS } from './constants';
 import parseInfinity from '../../../utils/parse-infinity';
-
-/**
- * @function
- * @param {Object} DateTimeEntity model object
- * @param {boolean} includeTrashed if true will not filter out trashed entities
- * @return {boolean} true if end date is in the past
- */
-export const isRecentlyExpired = (DateTimeEntity, includeTrashed = false) => {
-	return (
-		isValidEntityOrArchive(DateTimeEntity, includeTrashed) &&
-		DateTimeEntity.end.diffNow().asSeconds() < 0 &&
-		DateTimeEntity.end.diffNow().asSeconds() > MONTH_IN_SECONDS * -1
-	);
-};
 
 /**
  * @function
--- assets/src/domain/shared/entities/datetimes/predicates/isRecentlyExpired/index.ts
+++ assets/src/domain/shared/entities/datetimes/predicates/isRecentlyExpired/index.ts
@@ -1,19 +1,21 @@
 import { parseISO } from 'date-fns';
 
-import { now } from '../filters';
 import { Datetime } from '../../../../../eventEditor/services/apollo/types';
-import TIME from '../../../../../../application/constants/time';
 import { diff } from '../../../../../../application/services/utilities/date';
+import { isValidOrTrashed } from '../../../../services/predicates';
+import { now } from '../filters';
+import TIME from '../../../../../../application/constants/time';
 
 /**
  * @function
  * @param {Object} date date event object
  * @return {boolean} true if end date is in the past
  */
-const isRecentlyExpired = ({ endDate }: Datetime): boolean => {
+const isRecentlyExpired = (date: Datetime): boolean => {
 	return (
-		diff('seconds', parseISO(endDate), now) < 0 &&
-		diff('seconds', parseISO(endDate), now) > TIME.MONTH_IN_SECONDS * -1
+		isValidOrTrashed(date) &&
+		diff('seconds', parseISO(date.endDate), now) < 0 &&
+		diff('seconds', parseISO(date.endDate), now) > TIME.MONTH_IN_SECONDS * -1
 	);
 };
 
