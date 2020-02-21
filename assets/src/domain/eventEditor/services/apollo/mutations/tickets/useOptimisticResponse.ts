149c82dc51a95df768905d90f1c6ca65a6fd1118
Brent Christensen <brent@eventespresso.com>
2020-02-20 22:50:57 -0800

    OK you can NOT use the shortcuts cuz of tests :\

--- assets/src/domain/eventEditor/services/apollo/mutations/tickets/useOptimisticResponse.ts
+++ assets/src/domain/eventEditor/services/apollo/mutations/tickets/useOptimisticResponse.ts
@@ -2,9 +2,9 @@ import { useCallback } from 'react';
 import { pathOr } from 'ramda';
 import { useApolloClient } from '@apollo/react-hooks';
 
-import { MutationType, MutationInput } from '@appServices/apollo/mutations/types';
-import { removeNullAndUndefined } from '@appServices/utilities/predicates';
-import { ucFirst } from '@appServices/utilities/text';
+import { MutationType, MutationInput } from '../../../../../../application/services/apollo/mutations/types';
+import { removeNullAndUndefined } from '../../../../../../application/services/utilities/predicates';
+import { ucFirst } from '../../../../../../application/services/utilities/text';
 import { queries } from '../../queries';
 import { Ticket, TicketItem } from '../../types';
 import { PLUS_ONE_MONTH, PLUS_TWO_MONTHS } from '../../../../../shared/constants/defaultDates';
