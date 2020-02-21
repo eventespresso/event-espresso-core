4585c99caa30c9be9ac416999648c22a82303275
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-02-21 18:22:21 +0200

    Updated ticket status order; js_only.

--- assets/src/domain/shared/entities/tickets/helpers/status.ts
+++ assets/src/domain/shared/entities/tickets/helpers/status.ts
@@ -7,21 +7,22 @@ import isTrashed from '../../../services/predicates/isTrashed';
 import { TICKET_STATUS_ID } from '../constants';
 
 const status = (ticket: Ticket): string => {
+	if (isTrashed(ticket)) {
+		return TICKET_STATUS_ID.TRASHED;
+	}
 	if (isExpired({ ticket })) {
 		return TICKET_STATUS_ID.EXPIRED;
 	}
 	if (isOnSale(ticket)) {
 		return TICKET_STATUS_ID.ONSALE;
 	}
-	if (isPending(ticket)) {
-		return TICKET_STATUS_ID.PENDING;
-	}
 	if (isSoldOut(ticket)) {
 		return TICKET_STATUS_ID.SOLD_OUT;
 	}
-	if (isTrashed(ticket)) {
-		return TICKET_STATUS_ID.ARCHIVED;
+	if (isPending(ticket)) {
+		return TICKET_STATUS_ID.PENDING;
 	}
+
 	return '';
 };
 
--- assets/src/domain/shared/entities/tickets/predicates/isSoldOut/index.ts
+++ assets/src/domain/shared/entities/tickets/predicates/isSoldOut/index.ts
@@ -1,12 +1,10 @@
-import { is } from 'ramda';
-
 import { Ticket } from '@edtrServices/apollo/types';
 import { isValidOrTrashed } from '../../../../services/predicates';
 
 const isSoldOut = (ticket: Ticket): boolean => {
 	const { quantity, sold } = ticket;
 
-	return isValidOrTrashed(ticket) && is(Number, quantity) && isFinite(quantity) && quantity <= sold;
+	return isValidOrTrashed(ticket) && isFinite(quantity) && quantity <= sold;
 };
 
 export default isSoldOut;
