9433a89403456b0ea017b814db999941c35d72bc
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-02 17:33:51 +0530

    Remove date filters

--- assets/src/domain/eventEditor/ui/ticketAssignmentsManager/useTAMDatesAndTickets.ts
+++ assets/src/domain/eventEditor/ui/ticketAssignmentsManager/useTAMDatesAndTickets.ts
@@ -1,26 +1,21 @@
 import { useDatetimes, useTickets, useDatetimeItem, useTicketItem } from '@edtrServices/apollo/queries';
 import { TAMProps, DatesAndTickets } from './types';
-import { useEntityPagination } from '@appLayout/entityList/pagination';
-import useDatesListFilterState from '../datetimes/datesList/filterBar/useDatesListFilterState';
-import { Datetime } from '@edtrServices/apollo/types';
 
 const useTAMDatesAndTickets = ({ assignmentType, entityId }: TAMProps): DatesAndTickets => {
 	const allDatetimes = useDatetimes();
 	const allTickets = useTickets();
-	const singleDate = useDatetimeItem({ id: entityId });
+	const singleDatetime = useDatetimeItem({ id: entityId });
 	const singleTicket = useTicketItem({ id: entityId });
-	const { filteredEntities } = useDatesListFilterState(allDatetimes);
-	const { paginatedEntities: paginatedDates } = useEntityPagination<Datetime>({ entities: filteredEntities });
 
 	switch (assignmentType) {
 		case 'forAll':
 			return {
-				datetimes: paginatedDates,
+				datetimes: allDatetimes,
 				tickets: allTickets,
 			};
 		case 'forDate':
 			return {
-				datetimes: [singleDate],
+				datetimes: [singleDatetime],
 				tickets: allTickets,
 			};
 		case 'forTicket':
