9837562a6fc8b4a48d9dbc1952eccaa1eb6a2720
Brent Christensen <brent@eventespresso.com>
2020-03-04 14:55:39 -0800

    add aria attributes for labels and tooltips for better a11y

--- assets/src/application/ui/forms/espressoForm/renderers/FieldRenderer.tsx
+++ assets/src/application/ui/forms/espressoForm/renderers/FieldRenderer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import uuidv4 from 'uuid/v4';
 
 import { FormItem, MappedField } from '../adapters';
 import { FieldRendererProps } from '../types';
@@ -9,7 +10,7 @@ import { getValidateStatus } from '../utils';
 import useFormItemLayout from '../hooks/useFormItemLayout';
 
 const FieldRenderer: React.FC<FieldRendererProps> = (props) => {
-	const { label, desc, required, meta, before, after, info, formItemProps, ...rest } = props;
+	const { after, before, desc, formItemProps, info, label, meta, required, ...rest } = props;
 
 	const formItemLayout = useFormItemLayout();
 
@@ -18,8 +19,10 @@ const FieldRenderer: React.FC<FieldRendererProps> = (props) => {
 		return <MappedField {...rest} />;
 	}
 
+	const tooltipKey = info ? uuidv4() : null;
+
 	const fieldInfo = info ? (
-		<Tooltip placement='right' title={info}>
+		<Tooltip placement='right' title={info} id={tooltipKey}>
 			<InfoCircleOutlined className='tooltip' />
 		</Tooltip>
 	) : null;
@@ -33,6 +36,7 @@ const FieldRenderer: React.FC<FieldRendererProps> = (props) => {
 					{fieldInfo}
 				</>
 			}
+			htmlFor={props.input.name}
 			colon={false}
 			extra={desc}
 			required={required}
@@ -45,7 +49,7 @@ const FieldRenderer: React.FC<FieldRendererProps> = (props) => {
 		>
 			<>
 				{before}
-				<MappedField {...rest} />
+				<MappedField aria-label={label} aria-describedby={tooltipKey} {...rest} />
 				{after}
 			</>
 		</FormItem>
