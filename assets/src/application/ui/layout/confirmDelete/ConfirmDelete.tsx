4044a41bda1059f796d43de1d574d6ff4f674c66
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-05 21:01:09 +0530

    use message in place of title js_only

--- assets/src/application/ui/layout/confirmDelete/ConfirmDelete.tsx
+++ assets/src/application/ui/layout/confirmDelete/ConfirmDelete.tsx
@@ -3,9 +3,14 @@ import { Popconfirm } from 'antd';
 import { PopconfirmProps } from 'antd/lib/popconfirm';
 import { __ } from '@wordpress/i18n';
 
-const ConfirmDelete: React.FC<Partial<PopconfirmProps>> = ({ children, ...props }) => {
+interface ConfirmDeleteProps extends Omit<PopconfirmProps, 'title'> {
+	message: string;
+}
+
+const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ children, message, ...props }) => {
+	const title = message || __('Are you sure you want to delete this?');
 	return (
-		<Popconfirm title={__('Are you sure delete it?')} okText={__('Yes')} cancelText={__('No')} {...props}>
+		<Popconfirm title={title} okText={__('Yes')} cancelText={__('No')} {...props}>
 			{children}
 		</Popconfirm>
 	);
