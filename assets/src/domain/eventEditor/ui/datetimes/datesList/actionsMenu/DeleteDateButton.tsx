525cfebc66d9d970f464e94217a71224f408c15c
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-05 18:17:28 +0530

    Add delete confirmation to date and ticket deletion js_only

--- assets/src/domain/eventEditor/ui/datetimes/datesList/actionsMenu/DeleteDateButton.tsx
+++ assets/src/domain/eventEditor/ui/datetimes/datesList/actionsMenu/DeleteDateButton.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 
 import { EspressoButton, Icon } from '@application/ui/input';
-import { MutationResult } from '@appServices/apollo/mutations';
 import { ListItemProps } from '@edtrInterfaces/types';
 import { useDatetimeMutator } from '@edtrServices/apollo/mutations';
+import { ConfirmDelete } from '@appLayout/confirmDelete';
 
 const DeleteDateButton: React.FC<ListItemProps> = ({ id, ...rest }) => {
 	const { deleteEntity } = useDatetimeMutator(id);
 
 	return (
-		<EspressoButton
-			icon={Icon.TRASH}
-			tooltip={__('delete datetime')}
-			tooltipProps={{ placement: 'right' }}
-			onClick={(): MutationResult => deleteEntity({ id })}
-			{...rest}
-		/>
+		<ConfirmDelete onConfirm={() => deleteEntity({ id })}>
+			<EspressoButton
+				icon={Icon.TRASH}
+				tooltip={__('delete datetime')}
+				tooltipProps={{ placement: 'right' }}
+				{...rest}
+			/>
+		</ConfirmDelete>
 	);
 };
 
