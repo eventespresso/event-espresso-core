9a95fa7cb398e185b126a709188adaa6107c6059
Brent Christensen <brent@eventespresso.com>
2020-03-04 17:42:39 -0800

    tweak inline edit input styles js_only

--- assets/src/application/ui/input/InlineEditInput/InlineEditHeading.tsx
+++ assets/src/application/ui/input/InlineEditInput/InlineEditHeading.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import Editable from './Editable';
 import { HeadingProps } from './types';
 
-const InlineEditHeading: React.FC<HeadingProps> = ({ ellipsis, ...rest }) => {
+const InlineEditHeading: React.FC<HeadingProps> = ({ ellipsis, level = 4, ...rest }) => {
 	ellipsis = ellipsis ? ellipsis : { rows: 1, expandable: false };
-	return <Editable inputType='heading' ellipsis={ellipsis} level={4} {...rest} />;
+	return <Editable inputType='heading' ellipsis={ellipsis} level={level} {...rest} />;
 };
 
 export default InlineEditHeading;
