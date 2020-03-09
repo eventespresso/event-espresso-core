8a1fa5dfc52f7a1ec949991c65bcaba11c003c20
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-03-09 17:17:06 +0200

    Small cleanup; js_only.

--- assets/src/domain/shared/services/utils/url/getAdminUrl.ts
+++ assets/src/domain/shared/services/utils/url/getAdminUrl.ts
@@ -8,10 +8,6 @@ type Props = {
 
 /**
  * Return the admin url for a given page and action.
- * @param { string } page  The main ee admin page string
- * @param { string } action This should correspond to the action for the admin
- * 							page.
- * @return { string } A full url for the given arguments.
  */
 const getAdminUrl = ({
 	action = ADMIN_ROUTE_ACTION_DEFAULT,
