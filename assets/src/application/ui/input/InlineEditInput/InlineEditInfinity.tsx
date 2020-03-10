0c52c854c8f2a2fd906ba8e9f20cb457e1ae0fa9
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-10 23:22:17 +0530

    Create InlineEditInfinity component

---
+++ assets/src/application/ui/input/InlineEditInput/InlineEditInfinity.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+import parseInfinity from '@appServices/utilities/parseInfinity';
+import Editable from './Editable';
+import { TextProps } from './types';
+
+const InlineEditInfinity: React.FC<TextProps> = ({ children, onChange, className, ...rest }) => {
+	const [value, setValue] = useState(children);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const editable: TextProps['editable'] = {
+		editing: isEditing,
+		onChange: (val) => {
+			const parsedValue = parseInfinity(val);
+			setValue(parsedValue);
+			setIsEditing(false);
+			if (typeof onChange === 'function') {
+				onChange(`${parsedValue}`);
+			}
+		},
+		onStart: () => setIsEditing(true),
+	};
+
+	let output: string;
+	switch (true) {
+		case isEditing && value < 0:
+			output = '';
+			break;
+		case !isEditing && value < 0:
+			output = '∞';
+			break;
+		default:
+			output = `${value}`;
+			break;
+	}
+
+	const htmlClasseName = classNames('ee-infinity-sign', className);
+
+	return (
+		<Editable {...rest} editable={editable} inputType='text' className={htmlClasseName}>
+			{output}
+		</Editable>
+	);
+};
+
+export default InlineEditInfinity;
