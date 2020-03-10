a951a324c2da64e25abad5d6039afbc457280bfd
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-10 16:17:16 +0530

    Add title support for ItemCount

--- assets/src/application/ui/display/ItemCount/index.tsx
+++ assets/src/application/ui/display/ItemCount/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge } from 'antd';
+import { Badge, Tooltip } from 'antd';
 import { BadgeProps } from 'antd/lib/badge';
 import classNames from 'classnames';
 
@@ -10,15 +10,18 @@ interface ItemCountProps extends BadgeProps {
 	showClock?: boolean;
 }
 
-const ItemCount: React.FC<ItemCountProps> = ({ children, count, ...props }) => {
+const ItemCount: React.FC<ItemCountProps> = ({ children, title, count, ...props }) => {
 	const className = classNames(props.className, 'ee-item-count', {
 		'ee-item-count--has-items': count > 0,
 		'ee-item-count--no-items': count === 0,
 	});
 	const offset = props.offset || [-5, 5];
 
+	const countBadge = <span className='ant-badge-count'>{count}</span>;
+	const countNode = title ? <Tooltip title={title}>{countBadge}</Tooltip> : countBadge;
+
 	return (
-		<Badge {...props} className={className} count={count} offset={offset} showZero>
+		<Badge {...props} className={className} count={countNode} offset={offset} showZero>
 			{children}
 		</Badge>
 	);
