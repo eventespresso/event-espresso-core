002c0da46c7d08b3d9e5bf7de7c9f7f3b0fe6dfb
Brent Christensen <brent@eventespresso.com>
2020-03-10 22:53:04 -0700

    add zeroCountChar prop; tweak offset

--- assets/src/application/ui/display/ItemCount/index.tsx
+++ assets/src/application/ui/display/ItemCount/index.tsx
@@ -9,18 +9,12 @@ interface ItemCountProps extends BadgeProps {
 	zeroCountChar?: string | JSX.Element;
 }
 
-const ItemCount: React.FC<ItemCountProps> = ({
-	children,
-	count,
-	title = ' ',
-	zeroCountChar = '!',
-	...props
-}) => {
+const ItemCount: React.FC<ItemCountProps> = ({ children, count, title = ' ', zeroCountChar = '!', ...props }) => {
 	const className = classNames(props.className, 'ee-item-count', {
 		'ee-item-count--has-items': count > 0,
 		'ee-item-count--no-items': count === 0,
 	});
-	const offset = props.offset || [5, 5];
+	const offset = props.offset || [7, 7];
 	const value = count > 0 ? count : zeroCountChar;
 
 	const countNode = (
