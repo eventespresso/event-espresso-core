1f8289efd35a80ae025118c7d6b62c313aab85e3
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-12 21:56:58 +0530

    Fix TS js_only

--- assets/src/application/ui/layout/entityList/withValidFilterState.tsx
+++ assets/src/application/ui/layout/entityList/withValidFilterState.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { EntityListFilterBar, EntityListFilterBarProps } from './filterBar';
+import { EntityListFilterStateManager as ELFSM } from './filterBar/filterState';
 
-const withValidFilterState = (FilterBarComponent: React.ReactType): React.FC<EntityListFilterBarProps> => ({
-	filterState,
-	...props
-}) => {
+const withValidFilterState = <FS extends ELFSM>(
+	FilterBarComponent: React.ReactType
+): React.FC<EntityListFilterBarProps<FS>> => ({ filterState, ...props }) => {
 	return filterState ? <FilterBarComponent filterState={filterState} {...props} /> : null;
 };
 
