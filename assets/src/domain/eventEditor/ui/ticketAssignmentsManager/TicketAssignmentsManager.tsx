960ed8bfdadd4a97d7f0cc169694af6e8bd8d620
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-17 15:58:42 +0530

    Initialize data conditionally to use relations only for dates used in TAM

--- assets/src/domain/eventEditor/ui/ticketAssignmentsManager/TicketAssignmentsManager.tsx
+++ assets/src/domain/eventEditor/ui/ticketAssignmentsManager/TicketAssignmentsManager.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ErrorMessage from './ErrorMessage';
 import { Table } from './table';
 import { TAMProps } from './types';
 import useTAMDatesAndTickets from './useTAMDatesAndTickets';
+import useTAMState from './useTAMState';
+import { useRelations } from '@appServices/apollo/relations';
 
 const TicketAssignmentsManager: React.FC<TAMProps> = ({ assignmentType, entityId }) => {
 	const datesAndTickets = useTAMDatesAndTickets({ assignmentType, entityId });
+
+	const relations = useRelations();
+	const { initialize, isInitialized } = useTAMState();
+
+	useEffect(() => {
+		if (!isInitialized()) {
+			// If TAM is only for a single datetime
+			// limit relations to that datetime
+			const forDate = assignmentType === 'forDate' ? entityId : null;
+			// initialize with existing data
+			initialize(relations.getData(), forDate);
+		}
+	}, [isInitialized()]);
 
 	return (
 		<>
