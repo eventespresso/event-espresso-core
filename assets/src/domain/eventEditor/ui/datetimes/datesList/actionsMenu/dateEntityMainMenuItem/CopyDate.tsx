bc57e4066b04252a7c63a625da80a61d02a17d1a
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-03-19 18:21:47 +0200

    Updated CopyDate; js_only.

--- assets/src/domain/eventEditor/ui/datetimes/datesList/actionsMenu/dateEntityMainMenuItem/CopyDate.tsx
+++ assets/src/domain/eventEditor/ui/datetimes/datesList/actionsMenu/dateEntityMainMenuItem/CopyDate.tsx
@@ -6,17 +6,25 @@ import { Copy } from '@application/ui/layout/entityActionsMenu/entityMenuItems';
 import { useDatetimeContext } from '@edtrHooks/index';
 import { useDatetimeItem } from '@edtrServices/apollo/queries';
 import { useDatetimeMutator } from '@edtrServices/apollo/mutations';
+import { useRelations } from '@appServices/apollo/relations';
 
 const CopyDate = (props) => {
 	const { id } = useDatetimeContext();
 	const datetime = useDatetimeItem({ id });
 	const { createEntity } = useDatetimeMutator();
+	const { getRelations } = useRelations();
 	const newDatetime = pick(
 		['capacity', 'description', 'endDate', 'isPrimary', 'name', 'order', 'reserved', 'sold', 'startDate'],
 		datetime
 	);
-	const onClick = () => createEntity(newDatetime);
+	const tickets = getRelations({
+		entity: 'datetimes',
+		entityId: id,
+		relation: 'tickets',
+	});
 	const title = __('copy datetime');
+
+	const onClick = () => createEntity({ ...newDatetime, tickets });
 
 	return <Copy {...props} onClick={onClick} title={title} />;
 };
