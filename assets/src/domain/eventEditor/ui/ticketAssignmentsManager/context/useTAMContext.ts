4fb24a85bd6a374057db2c322639d25ea0a80723
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-19 17:40:13 +0530

    re-enable invariant js_only

--- assets/src/domain/eventEditor/ui/ticketAssignmentsManager/context/useTAMContext.ts
+++ assets/src/domain/eventEditor/ui/ticketAssignmentsManager/context/useTAMContext.ts
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
 import { Context } from './ContextProvider';
 import { ContextProps } from './types';
-// import invariant from 'invariant';
+import invariant from 'invariant';
 
 const useTAMContext = (): ContextProps => {
 	const value = useContext(Context);
 
-	// invariant(value, 'useTAMContext must be used inside TAM <ContextProvider> component');
+	invariant(value, 'useTAMContext must be used inside TAM <ContextProvider> component');
 
 	return value;
 };
