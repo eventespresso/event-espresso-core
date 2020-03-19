032bbce534d3f177d6c6cd17b2be102aa54a7b7f
Rafael Goulart <rafaelgou@pm.me>
2020-03-19 14:04:50 -0300

    Implement email verification in check-out #121 (#2523)
    * Implement email verification in check-out #121
    
    * Implement email verification in check-out - small adjusts #121
    
    * Fix phpcs errors, emailc moved to email_confirm
    
    * Fix missing lint error
    
    * Adjust phpdoc for new validation
    
    * Remove unused migration and field group activation
    
    * set a limit that matches the regular email field's limit
    
    * wrap validation error message in filter hook to allow developers to customize
    
    Co-authored-by: Josh Feck <josh@nxnweb.com>

--- admin_pages/registration_form/templates/questions_main_meta_box.template.php
+++ admin_pages/registration_form/templates/questions_main_meta_box.template.php
@@ -122,6 +122,13 @@ if ($QST_system === 'country') {
                     $id = '';
                 }
 
+                // Only display Confirm email for
+                if (empty($QST_system)
+                    || (! empty($QST_system) && $QST_system !== EEM_Attendee::system_question_email_confirm)
+                ) {
+                    unset($question_types[ EEM_Question::QST_type_email_confirm ]);
+                }
+
                 echo EEH_Form_Fields::select_input(
                     'QST_type' . $id,
                     $question_types,
