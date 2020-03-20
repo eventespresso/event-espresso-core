9ab574b912e035d1535fbdeb0ce06a4d3dc28565
Brent Christensen <brent@eventespresso.com>
2020-03-19 19:40:46 -0700

    add index files for folders

---
+++ assets/src/application/services/utilities/index.js
@@ -0,0 +1,12 @@
+export * from './array';
+export * from './boolean';
+export * from './converters';
+export * from './date';
+export * from './dom';
+export * from './function';
+export * from './money';
+export * from './number';
+export * from './predicates';
+export * from './text';
+
+export { cancelClickEvent } from './cancelClickEvent';
