0fb7671b9204b4a9905a5b78cbe43e5cfa8b1975
Brent Christensen <brent@eventespresso.com>
2020-03-20 14:48:00 -0700

    add tests for isBooleanTrue()

--- assets/src/application/services/utilities/boolean/isBooleanTrue.ts
+++ assets/src/application/services/utilities/boolean/isBooleanTrue.ts
@@ -3,6 +3,6 @@ import { is } from 'ramda';
 /**
  * returns true if supplied value is a boolean AND is actually true
  */
-const isBooleanTrue = (maybeBoolean: boolean): boolean => is(Boolean, maybeBoolean) && maybeBoolean;
+const isBooleanTrue = (maybeBoolean: any): boolean => is(Boolean, maybeBoolean) && maybeBoolean;
 
 export default isBooleanTrue;
