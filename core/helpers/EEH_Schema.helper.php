ddc15f738a3b9c333833bac8d4ff79ceaa7bcc74
Josh Feck <josh@nxnweb.com>
2020-03-24 15:52:51 -0400

    Add eventAttendanceMode schema property (#2596)
    * Add eventAttendanceMode schema property
    
    * use typesafe comparison
    
    Co-Authored-By: Brent Christensen <brent@eventespresso.com>
    
    * use typesafe comparison
    
    Co-Authored-By: Brent Christensen <brent@eventespresso.com>
    
    Co-authored-by: Brent Christensen <brent@eventespresso.com>

--- core/helpers/EEH_Schema.helper.php
+++ core/helpers/EEH_Schema.helper.php
@@ -31,6 +31,7 @@ class EEH_Schema
             'event_description' => '',
             'event_start' => '',
             'event_end' => '',
+            'event_attendance_mode' => '',
             'event_status' => '',
             'currency' => '',
             'event_tickets' => array(),
@@ -59,6 +60,7 @@ class EEH_Schema
             default:
                 $event_status = 'EventScheduled';
         }
+        $template_args['event_attendance_mode'] = 'OfflineEventAttendanceMode';
         $template_args['event_status'] = $event_status;
         $template_args['currency'] = EE_Registry::instance()->CFG->currency->code;
         foreach ($event->tickets() as $original_ticket) {
@@ -95,6 +97,12 @@ class EEH_Schema
             $template_args['venue_locality'] = $venue->city();
             $template_args['venue_region'] = $venue->state_name();
             $template_args['venue_address'] = $venue->address();
+            if ($venue->virtual_url() !== '') {
+                $template_args['event_attendance_mode'] = 'OnlineEventAttendanceMode';
+            }
+            if ($venue->virtual_url() !== '' && $venue->address() !== '') {
+                $template_args['event_attendance_mode'] = 'MixedEventAttendanceMode';
+            }
         }
         $template_args['event_image'] = $event->feature_image_url();
         $template_args = apply_filters(
