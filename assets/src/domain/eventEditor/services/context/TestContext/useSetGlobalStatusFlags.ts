1dff7808099232ac92ad7dd20fbdcfffecb0524b
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-25 15:36:14 +0530

    Fix react warning for component update during render

--- assets/src/domain/eventEditor/services/context/TestContext/useSetGlobalStatusFlags.ts
+++ assets/src/domain/eventEditor/services/context/TestContext/useSetGlobalStatusFlags.ts
@@ -1,13 +1,17 @@
+import { useEffect } from 'react';
 import { useStatus, TypeName } from '../../../../../application/services/apollo/status';
 
 const useSetGlobalStatusFlags = (): void => {
 	const { setIsLoaded, isLoaded } = useStatus();
-	// Set loaded status for all entities
-	for (const entity in TypeName) {
-		if (!isLoaded(TypeName[entity])) {
-			setIsLoaded(TypeName[entity], true);
+
+	useEffect(() => {
+		// Set loaded status for all entities
+		for (const entity in TypeName) {
+			if (!isLoaded(TypeName[entity])) {
+				setIsLoaded(TypeName[entity], true);
+			}
 		}
-	}
+	}, []);
 };
 
 export default useSetGlobalStatusFlags;
