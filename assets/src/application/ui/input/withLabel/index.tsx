71a136db9d4935355f00202431b849aee7d13b98
Vadim Nicolai <vadim@eventespresso.com>
2020-03-25 16:42:01 +0200

    Update assets/src/application/ui/input/withLabel/index.tsx
    Co-Authored-By: Manzoor Wani <manzoorwani.jk@gmail.com>

--- assets/src/application/ui/input/withLabel/index.tsx
+++ assets/src/application/ui/input/withLabel/index.tsx
@@ -5,17 +5,21 @@ interface Props {
 	label?: string;
 }
 
-const withLabel = (Component: JSX.Element) => ({ id, label }: Props): JSX.Element => {
-	return (
-		<div className={'ee-base-input'}>
-			{label && (
-				<label className='ee-base-input-label' htmlFor={id}>
-					{label}
-				</label>
-			)}
-			{Component}
-		</div>
-	);
+const withLabel = (output: React.ReactNode): React.FC<Props> => {
+	const Component: React.FC<Props> = ({ id, label } ) => {
+		return (
+			<div className={'ee-base-input'}>
+				{label && (
+					<label className='ee-base-input-label' htmlFor={id}>
+						{label}
+					</label>
+				)}
+				{output}
+			</div>
+		);
+	}
+
+	return Component;
 };
 
 export default withLabel;
