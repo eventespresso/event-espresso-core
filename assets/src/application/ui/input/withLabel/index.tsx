524539501f42548bea54efdfa77e72ca0bc3d9ac
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-03-25 22:49:15 +0200

    WithLabel update; js_only.

--- assets/src/application/ui/input/withLabel/index.tsx
+++ assets/src/application/ui/input/withLabel/index.tsx
@@ -6,18 +6,18 @@ interface Props {
 }
 
 const withLabel = (output: React.ReactNode): React.FC<Props> => {
-	const Component: React.FC<Props> = ({ id, label } ) => {
+	const Component: React.FC<Props> = ({ id, label }) => {
 		return (
-			<div className={'ee-base-input'}>
+			<div className={'ee-with-label'}>
 				{label && (
-					<label className='ee-base-input-label' htmlFor={id}>
+					<label className='ee-with-label__input-label' htmlFor={id}>
 						{label}
 					</label>
 				)}
 				{output}
 			</div>
 		);
-	}
+	};
 
 	return Component;
 };
