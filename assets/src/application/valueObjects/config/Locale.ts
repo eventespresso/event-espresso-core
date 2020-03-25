675d6ce7dff98f1a8857bb1312f2607a17c77da5
Brent Christensen <brent@eventespresso.com>
2020-03-25 08:00:27 -0700

    add some magic js_only

--- assets/src/application/valueObjects/config/Locale.ts
+++ assets/src/application/valueObjects/config/Locale.ts
@@ -1,10 +1,10 @@
 import { LocaleProps } from './types';
 
 export const Locale = ({ user, site }: LocaleProps): LocaleProps => {
-	const userLocale = user || 'en-US';
-	const siteLocale = site || 'en-US';
+	const userLocale = user?.replace('_', '-') || 'en-US';
+	const siteLocale = site?.replace('_', '-') || 'en-US';
 	return {
-		user: userLocale.replace('_', '-'),
-		site: siteLocale.replace('_', '-'),
+		user: userLocale,
+		site: siteLocale,
 	};
 };
