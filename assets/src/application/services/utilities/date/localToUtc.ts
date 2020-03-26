e2f65fd836826f3f859e080eb8c61282f73566d4
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-03-26 22:35:46 +0530

    Clean up and fix tests js_only

--- assets/src/application/services/utilities/date/localToUtc.ts
+++
@@ -1,9 +0,0 @@
-import { zonedTimeToUtc } from 'date-fns-tz';
-
-import { TzDateFn } from './types';
-
-const localToUtc: TzDateFn = (date, timezone) => {
-	return zonedTimeToUtc(date, timezone);
-};
-
-export default localToUtc;
