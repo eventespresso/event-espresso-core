46dfcd3c14e7e559ca7f758428ab6c6e89ddb072
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-03-31 18:35:50 +0300

    Small cleanup; js_only.

--- assets/src/application/ui/layout/entityActionsMenu/entityMenuItems/Trash.tsx
+++ assets/src/application/ui/layout/entityActionsMenu/entityMenuItems/Trash.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
 
-import { ConfirmDelete } from '@appLayout/confirmDelete';
 import { EspressoDropdownMenuItem } from '@application/ui/layout/espressoDropdownMenu';
 import { Icon } from '@application/ui/input';
 import { MenuItemProps } from './types';
@@ -10,14 +9,13 @@ const Trash: React.FC<MenuItemProps> = ({ onClick, ...props }) => {
 	const title = props.title || __('trash');
 
 	return (
-		<ConfirmDelete onConfirm={onClick}>
-			<EspressoDropdownMenuItem
-				{...props}
-				icon={Icon.TRASH}
-				popoverProps={{ position: 'right center' }}
-				title={title}
-			/>
-		</ConfirmDelete>
+		<EspressoDropdownMenuItem
+			{...props}
+			icon={Icon.TRASH}
+			onClick={onClick}
+			popoverProps={{ position: 'right center' }}
+			title={title}
+		/>
 	);
 };
 
