88bd50e33cf36f45f71b652a4f1b5f35ca9d96a6
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-01 01:06:03 +0300

    Removed validation from switchTenseForDate; js_only.

--- assets/src/application/services/utilities/date/switchTenseForDate.ts
+++ assets/src/application/services/utilities/date/switchTenseForDate.ts
@@ -1,13 +1,7 @@
-import { isNil } from 'ramda';
-
 import { diff } from '@appServices/utilities';
 import { now } from '@sharedServices/utils/dateAndTime';
 
-const switchTenseForDate = (date: Date, textForPastDate: string, textForFutureDate: string): string | null => {
-	if (isNil(date) || isNil(textForPastDate) || isNil(textForFutureDate)) {
-		return null;
-	}
-
+const switchTenseForDate = (date: Date, textForPastDate: string, textForFutureDate: string): string => {
 	return diff('minutes', date, now) < 0 ? textForPastDate : textForFutureDate;
 };
 
