79534eb6736920e7c9220a9d873ad05585fc0d96
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-03 13:35:31 +0300

    Small cleanup; js_only.

--- assets/src/domain/eventEditor/ui/tickets/old/constants.js
+++
@@ -1,1 +0,0 @@
-export const TICKET_PRICE_CALCULATOR_FORM_INPUT_PREFIX = 'ee-ticket-price-calculator';
--- assets/src/domain/eventEditor/ui/tickets/old/price-modifier-row/index.js
+++
@@ -1,9 +0,0 @@
-export { default as AddPriceModifierActionButton }
-	from './add-price-modifier-action-button';
-export { default as DeletePriceModifierActionButton }
-	from './delete-price-modifier-action-button';
-export { default as PriceAmountInput } from './price-amount-input';
-export { default as PriceDescriptionInput } from './price-description-input';
-export { default as PriceIdInput } from './price-id-input';
-export { default as PriceNameInput } from './price-name-input';
-export { default as PriceTypeInput } from './price-type-input';
