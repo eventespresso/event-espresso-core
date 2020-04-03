d66f91e922f002f0240a78dad25353eadf224a7c
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-03 17:29:15 +0530

    Create and register isChained filter

--- assets/src/domain/eventEditor/services/filterState/tickets/useTicketsFilterBarService.ts
+++ assets/src/domain/eventEditor/services/filterState/tickets/useTicketsFilterBarService.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { useFilterBarService } from '@appLayout/entityList/filterBar';
 import { salesFilter, statusFilter } from '@sharedEntities/tickets/predicates/filters';
@@ -7,6 +7,7 @@ import { domain, ticketsList } from '@edtrServices/constants';
 import { entityListSearch } from '@appServices/utilities/text';
 import { Ticket } from '@edtrServices/apollo';
 import { TicketsFilterStateManager } from '@edtrServices/filterState';
+import useIsChainedFilter from './useIsChainedFilter';
 
 type Domain = typeof domain;
 type TFSM = TicketsFilterStateManager;
@@ -17,6 +18,37 @@ const useTicketsFilterBarService = (): void => {
 		registerSearch: registerTicketsSearch,
 		registerSorter: registerTicketsSorter,
 	} = useFilterBarService<Domain, typeof ticketsList, Ticket, TFSM>(domain, ticketsList);
+
+	/**
+	 * isChained filter needs special treatment :)
+	 *
+	 * Unlike other sorters and filters, isChained filter is dependent upon
+	 * external state which can change. Thus we need to update our filter callback
+	 * to make sure the stale state is not bound to the filter callback.
+	 */
+	const [isChainedFilter, isChainedDeps] = useIsChainedFilter();
+	// To avoid multiple filter registrations, we will store the aleady registered
+	// filter unsubscribe callback in ref to use it to remove the existing filter.
+	const chainedFilterRef = useRef<VoidFunction>();
+	useEffect(() => {
+		// If already register
+		if (typeof chainedFilterRef.current === 'function') {
+			// de-register
+			chainedFilterRef.current();
+		}
+		// Register isChained filter
+		const unsubscribeIsChainedFilter = registerTicketsFilter(({ entityList, filterState }) => {
+			return isChainedFilter({ isChained: filterState.isChained, tickets: entityList });
+		});
+
+		// update ref.
+		chainedFilterRef.current = unsubscribeIsChainedFilter;
+
+		// Housekeeping
+		return (): void => {
+			unsubscribeIsChainedFilter();
+		};
+	}, [isChainedDeps]);
 
 	useEffect(() => {
 		// Register sales filter
