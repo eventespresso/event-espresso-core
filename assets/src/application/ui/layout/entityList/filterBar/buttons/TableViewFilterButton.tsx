f8d5f27dfb0febc086523c39622076fc3db9bc5e
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-06 15:23:07 +0530

    Improve memoization and fix tooltip js_only

--- assets/src/application/ui/layout/entityList/filterBar/buttons/TableViewFilterButton.tsx
+++ assets/src/application/ui/layout/entityList/filterBar/buttons/TableViewFilterButton.tsx
@@ -5,6 +5,7 @@ import { __ } from '@wordpress/i18n';
 import { EspressoButton, EspressoButtonType, Icon } from '@application/ui/input';
 import { LabelPosition } from '@application/ui/display';
 import { TableViewFilterButtonProps } from '../types';
+import { getPropsAreEqual } from '@appServices/utilities';
 
 const TableViewFilterButton: React.FC<TableViewFilterButtonProps> = React.memo(({ listId, setTableView, view }) => {
 	const className = classNames('ee-filter-bar__btn', { 'ee-filter-bar__btn--active': view === 'table' });
@@ -14,11 +15,10 @@ const TableViewFilterButton: React.FC<TableViewFilterButtonProps> = React.memo((
 		<EspressoButton
 			buttonType={EspressoButtonType.MINIMAL}
 			className={className}
-			disabled={view === 'table'}
 			icon={Icon.TABLE_VIEW}
 			id={filterId}
 			label={__('table view')}
-			onClick={setTableView}
+			onClick={view !== 'table' ? setTableView : null}
 			tooltip={__('table view')}
 			labelClassName={'ee-filter-bar__btn-wrap'}
 			labelPosition={LabelPosition.BOTTOM_CENTER}
@@ -26,4 +26,4 @@ const TableViewFilterButton: React.FC<TableViewFilterButtonProps> = React.memo((
 	);
 });
 
-export default TableViewFilterButton;
+export default React.memo(TableViewFilterButton, getPropsAreEqual(['listId'], ['view']));
