010c92805b0900b1dc3acaeb7581834b319c35f0
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-08 22:08:26 +0300

    EmptyState updated; js_only.

--- assets/src/application/ui/display/EmptyState.tsx
+++ assets/src/application/ui/display/EmptyState.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Empty, Typography } from 'antd';
-
-const { Text, Title } = Typography;
+import { Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/core';
 
 interface EmptyStateProps {
 	children?: React.ReactNode;
@@ -12,16 +10,27 @@ interface EmptyStateProps {
 
 const EmptyState = ({ className, children, description, title }: EmptyStateProps) => {
 	return (
-		<Empty className={className} description='' image={Empty.PRESENTED_IMAGE_SIMPLE}>
-			{title && (
-				<Title type='secondary' level={4}>
-					{title}
-				</Title>
+		<Alert
+			status='warning'
+			variant='subtle'
+			flexDirection='column'
+			justifyContent='center'
+			textAlign='center'
+			height='200px'
+		>
+			<AlertIcon name='question-outline' size='96px' color='gray.100' />
+
+			<AlertTitle mt={4} mb={1} fontSize='lg'>
+				{title}
+			</AlertTitle>
+			{description && (
+				<AlertDescription maxWidth='sm'>
+					Thanks for submitting your application. Our team will get back to you soon.
+				</AlertDescription>
 			)}
-			{description && <Text>{description}</Text>}
 			{children && children}
-		</Empty>
+		</Alert>
 	);
 };
 
-export default EmptyState;
+export default React.memo(EmptyState);
