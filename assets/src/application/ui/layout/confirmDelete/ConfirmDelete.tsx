b9cf1dce763d6005471dcc8c658ae5935543fbf9
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-08 23:04:48 +0300

    Updated ConfirmDelete; js_only.

--- assets/src/application/ui/layout/confirmDelete/ConfirmDelete.tsx
+++ assets/src/application/ui/layout/confirmDelete/ConfirmDelete.tsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import { Popconfirm } from 'antd';
-import { PopconfirmProps } from 'antd/lib/popconfirm';
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	Button,
+} from '@chakra-ui/core';
+import { EspressoButton, EspressoButtonProps } from '@application/ui/input';
 import { __ } from '@wordpress/i18n';
 
-interface ConfirmDeleteProps extends Omit<PopconfirmProps, 'title'> {
+interface ConfirmDeleteProps {
+	buttonProps: EspressoButtonProps;
+	onConfirm: VoidFunction;
 	title?: string;
 }
 
-const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ children, ...props }) => {
+const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ buttonProps, onConfirm, ...props }) => {
+	const [isOpen, setIsOpen] = React.useState(false);
+	const cancelRef = React.useRef();
+	const onClose = () => setIsOpen(false);
+	const onDelete = () => {
+		onClose();
+		onConfirm();
+	};
 	const title = props.title || __('Are you sure you want to delete this?');
 
 	return (
-		<Popconfirm {...props} cancelText={__('No')} okText={__('Yes')} title={title}>
-			{children}
-		</Popconfirm>
+		<>
+			<EspressoButton {...buttonProps} onClick={() => setIsOpen(true)} />
+
+			<AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+				<AlertDialogOverlay />
+				<AlertDialogContent>
+					{buttonProps?.tooltip && (
+						<AlertDialogHeader fontSize='lg' fontWeight='bold' textTransform='capitalize'>
+							{buttonProps.tooltip}
+						</AlertDialogHeader>
+					)}
+					<AlertDialogBody>{title}</AlertDialogBody>
+					<AlertDialogFooter>
+						<Button ref={cancelRef} onClick={onClose}>
+							{__('No')}
+						</Button>
+						<Button variantColor='red' onClick={onDelete} ml={3}>
+							{__('Yes')}
+						</Button>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+		</>
 	);
 };
 
--- assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/buttons/DeletePriceModifierButton.tsx
+++ assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/buttons/DeletePriceModifierButton.tsx
@@ -2,20 +2,17 @@ import React, { useCallback } from 'react';
 import { __ } from '@wordpress/i18n';
 
 import { ConfirmDelete } from '@appLayout/confirmDelete';
-import { EspressoButton, Icon } from '@application/ui/input';
+import { Icon } from '@application/ui/input';
 import { PriceModifierProps } from '../types';
 import { useDataState } from '../data';
 
 const DeletePriceModifierButton: React.FC<PriceModifierProps> = ({ price }) => {
 	const { deletePrice } = useDataState();
-	const onClick = useCallback(() => deletePrice(price.id, price.isNew), [price.id, price.isNew]);
 
-	return (
-		<ConfirmDelete
-			children={<EspressoButton icon={Icon.TRASH} tooltip={__('delete price modifier')} />}
-			onConfirm={onClick}
-		/>
-	);
+	const buttonProps = { icon: Icon.TRASH, tooltip: __('delete price modifier') };
+	const onConfirm = useCallback(() => deletePrice(price.id, price.isNew), [price.id, price.isNew]);
+
+	return <ConfirmDelete buttonProps={buttonProps} onConfirm={onConfirm} />;
 };
 
 export default DeletePriceModifierButton;
