f38a4b92b634459d19b71eb5e85c0f0aa2f0ee65
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-09 22:54:30 +0300

    Updated Alert component; js_only.

---
+++ assets/src/infrastructure/ui/display/Alert.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Alert as ChakraAlert, AlertIcon, AlertTitle, AlertDescription, AlertProps } from '@chakra-ui/core';
+
+interface Props extends AlertProps {
+	description: string;
+	title: string;
+}
+
+const Alert: React.FC<Props> = ({ description, title, ...props }) => (
+	<ChakraAlert
+		{...props}
+		variant='subtle'
+		flexDirection='column'
+		justifyContent='center'
+		textAlign='center'
+		height='200px'
+	>
+		<AlertIcon size='40px' mr={0} />
+		<AlertTitle mt={4} mb={1} fontSize='lg'>
+			{title}
+		</AlertTitle>
+		<AlertDescription maxWidth='sm'>{description}</AlertDescription>
+	</ChakraAlert>
+);
+
+export default Alert;
