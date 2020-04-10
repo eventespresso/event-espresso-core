d42cc79c165dde2f676624ed364c573ffe1401ab
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-10 17:59:16 +0530

    Add support for isClosable in Modal

--- assets/src/infrastructure/ui/layout/modal/types.ts
+++ assets/src/infrastructure/ui/layout/modal/types.ts
@@ -3,11 +3,12 @@ import { Modal } from '@chakra-ui/core';
 import { ButtonProps } from '@infraUI/inputs';
 
 export interface ModalProps extends React.ComponentProps<typeof Modal> {
-	className?: string;
 	bodyClassName?: string;
-	content?: React.ReactNode;
-	title?: React.ReactNode;
-	footerContent?: React.ReactNode;
-	submitButtonProps?: ButtonProps;
 	cancelButtonProps?: ButtonProps;
+	className?: string;
+	content?: React.ReactNode;
+	footerContent?: React.ReactNode;
+	isClosable?: boolean;
+	submitButtonProps?: ButtonProps;
+	title?: React.ReactNode;
 }
