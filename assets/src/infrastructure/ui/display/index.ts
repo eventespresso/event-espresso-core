e9accdb58012ce8887841faf7c9876c8894c42a9
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-10 17:10:42 +0300

    Small cleanup; js_only.

--- assets/src/infrastructure/ui/display/index.ts
+++ assets/src/infrastructure/ui/display/index.ts
@@ -4,11 +4,7 @@ export { default as AlertDialog } from './AlertDialog';
 export { default as Collapse } from './Collapse';
 export { default as Popover } from './Popover';
 export { default as Spinner } from './Spinner';
-<<<<<<< HEAD
-export { default as TimezoneTimeInfo } from './TimezoneTimeInfo';
 export { default as Tooltip } from './Tooltip';
-=======
->>>>>>> Updated TimezoneTimeInfo; js_only.
 
 export * from '../../../application/ui/display/loadingNotice';
 export * from './types';
