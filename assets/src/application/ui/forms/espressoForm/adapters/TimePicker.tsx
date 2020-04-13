448aff9095041a52193aef4b74ebe1b81f1f63d2
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-13 22:17:19 +0530

    use onChangeValue for consistent API js_only

--- assets/src/application/ui/forms/espressoForm/adapters/TimePicker.tsx
+++ assets/src/application/ui/forms/espressoForm/adapters/TimePicker.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { Timepicker } from '@infraUI/inputs/dateTime';
 import { FieldRendererProps } from '../types';
 
-const TimePicker: React.FC<FieldRendererProps> = ({ input, meta, ...rest }) => {
+const TimePicker: React.FC<FieldRendererProps> = ({ input: { onChange, ...input }, meta, ...rest }) => {
 	return (
 		<Timepicker
 			{...input}
 			{...rest}
 			format='12'
+			onChangeValue={onChange}
 			// 300 seconds(5 minutes)
 			step={300}
 		/>
