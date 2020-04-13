93c214f8d0c04891f97f29e8838cfc414d5d59d4
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-13 22:23:36 +0530

    Fix path js_only

--- assets/src/application/ui/input/index.ts
+++ assets/src/application/ui/input/index.ts
@@ -1,6 +1,5 @@
 export { default as CurrencyInput } from './CurrencyInput';
 
-export * from './dateTime';
 export * from './EspressoButton';
 export * from './InlineEditInput';
 export * from './SearchInput';
