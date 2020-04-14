b943c1a04125c6f74b245d79eebece3ad52752cc
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-14 21:38:50 +0530

    Fix TS error js_only

--- assets/src/application/ui/display/TimezoneTimeInfo/Trigger.tsx
+++ assets/src/application/ui/display/TimezoneTimeInfo/Trigger.tsx
@@ -2,17 +2,24 @@ import React, { forwardRef } from 'react';
 
 import { Tooltip } from '@infraUI/display';
 import { IconButton } from '@infraUI/inputs';
+import { Icon } from '@chakra-ui/core';
 
 interface Props {
 	label: string;
 	onClick?: VoidFunction;
 }
 
-const Trigger: React.FC<Props> = forwardRef(({ label, onClick }, ref) => {
+const Trigger = forwardRef<typeof IconButton, Props>(({ label, onClick }, ref) => {
 	return (
 		// TODO: refactor this by using `withTooltip` when that is updated with chakra components
 		<Tooltip onClick={onClick} title={label}>
-			<IconButton aria-label={label} icon='info-outline' onClick={onClick} ref={ref} variant='unstyled' />
+			<IconButton
+				aria-label={label}
+				icon={() => <Icon name='info-outline' />}
+				onClick={onClick}
+				ref={ref}
+				variant='unstyled'
+			/>
 		</Tooltip>
 	);
 });
