8eeae5fa4e279969e4b16b9b705b0b715460d6a6
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-04-15 19:16:54 +0530

    Update CurrencyInput

--- assets/src/application/ui/input/CurrencyInput/index.tsx
+++ assets/src/application/ui/input/CurrencyInput/index.tsx
@@ -1,5 +1,4 @@
-// @ts-nocheck
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { InlineEditText } from '../InlineEditInput';
 import { parsedAmount, useMoneyDisplay } from '@appServices/utilities/money';
@@ -9,11 +8,10 @@ const nullFunc = (args?: any) => {};
 interface CurrencyInputProps {
 	amount: string | number;
 	id: string;
-	inputProps?: React.HTMLAttributes;
 	onChange?: (result?: { amount: string | number; id: string }) => void;
 	placeholder?: string;
 	tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-	wrapperProps?: React.HTMLAttributes;
+	wrapperProps?: React.HTMLAttributes<Element>;
 }
 
 const CurrencyInput: React.FC<CurrencyInputProps> = ({
@@ -21,7 +19,6 @@ const CurrencyInput: React.FC<CurrencyInputProps> = ({
 	id = '',
 	onChange = nullFunc,
 	tag = 'p',
-	inputProps = {},
 	wrapperProps = {},
 }) => {
 	const { formatAmount, beforeAmount, afterAmount } = useMoneyDisplay();
@@ -29,21 +26,21 @@ const CurrencyInput: React.FC<CurrencyInputProps> = ({
 	const after = afterAmount ? <span className={'ee-currency-input__after-amount'}> {afterAmount}</span> : '';
 	const formattedAmount = formatAmount(amount);
 	const Wrapper = tag;
+
+	const onChangeHandler = useCallback(
+		(value: string) => {
+			const newAmount = parsedAmount(value);
+			if (newAmount !== amount) {
+				onChange({ amount: newAmount, id });
+			}
+		},
+		[onChange]
+	);
+
 	return (
 		<Wrapper className={'ee-currency-input'} {...wrapperProps}>
 			{before}
-			<InlineEditText
-				key={id}
-				onChange={(value: string) => {
-					const newAmount = parsedAmount(value);
-					if (newAmount !== amount) {
-						onChange({ amount: newAmount, id });
-					}
-				}}
-				{...inputProps}
-			>
-				{formattedAmount}
-			</InlineEditText>
+			<InlineEditText as='span' key={id} onChangeValue={onChangeHandler} value={formattedAmount} />
 			{after}
 		</Wrapper>
 	);
