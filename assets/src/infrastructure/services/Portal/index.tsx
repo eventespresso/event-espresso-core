8026dd4271ba7ef25d346e5d4b75e3022e6e9e6c
Brent Christensen <brent@eventespresso.com>
2020-04-15 18:57:03 -0700

    ChakraPortal

---
+++ assets/src/infrastructure/services/Portal/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Portal as ChakraPortal, PortalProps } from '@chakra-ui/core';
+
+const Portal = (props: PortalProps) => <ChakraPortal {...props} />;
+
+export default Portal;
