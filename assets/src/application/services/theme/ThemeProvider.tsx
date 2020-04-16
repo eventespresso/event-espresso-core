8689f97c016ecf7d6fa41d71e5d0387acbe4f797
Brent Christensen <brent@eventespresso.com>
2020-04-15 19:17:25 -0700

    delete uneeded files; debug theme dump

--- assets/src/application/services/theme/ThemeProvider.tsx
+++ assets/src/application/services/theme/ThemeProvider.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider as ChakraThemeProvider, CSSReset } from '@chakra-ui/core'
 import theme from './theme';
 
 const ThemeProvider: React.FC = ({ children }) => {
+	console.log('%c 	ThemeProvider', 'color: blue;', theme);
 	return (
 		<ChakraThemeProvider theme={theme}>
 			<CSSReset />
