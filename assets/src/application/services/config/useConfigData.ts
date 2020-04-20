99a3a16c39a637c1cbc85505fa09d4e5a22dd8b3
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-20 17:55:35 +0300

    Small cleanup; js_only.

--- assets/src/application/services/config/useConfigData.ts
+++ assets/src/application/services/config/useConfigData.ts
@@ -23,24 +23,24 @@ export const useConfigData = (): ConfigDataProps => {
 	}
 
 	return {
-		brandName: data.brandName || 'Event Espresso',
-		currency: Currency(data.currency_config as CurrencyProps),
+		brandName: data?.brandName ?? 'Event Espresso',
+		currency: Currency(data?.currency_config as CurrencyProps),
 		currentUser: CurrentUser({} as CurrentUserProps),
 		generalSettings: null,
 		dateTimeFormats: DateTimeFormats({} as DateTimeFormatsProps),
 		locale: Locale({
-			user: data.locale && data.locale.user ? data.locale.user : '',
-			site: data.locale && data.locale.site ? data.locale.site : '',
+			user: data?.locale?.user ?? '',
+			site: data?.locale?.site ?? '',
 		} as LocaleProps),
-		nonce: data.eejs_api_nonce || '',
+		nonce: data?.eejs_api_nonce ?? '',
 		siteUrl: SiteUrl({
-			admin: data.paths && data.paths.admin_url ? data.paths.admin_url : '',
-			home: data.paths && data.paths.site_url ? data.paths.site_url : '',
+			admin: data?.paths?.admin_url ?? '',
+			home: data?.paths?.site_url ?? '',
 		} as SiteUrlProps),
 		timezone: Timezone({
-			city: data.default_timezone && data.default_timezone.pretty ? data.default_timezone.pretty : '',
-			name: data.default_timezone && data.default_timezone.string ? data.default_timezone.string : '',
-			offset: data.default_timezone && data.default_timezone.offset ? data.default_timezone.offset : 0,
+			city: data?.default_timezone?.pretty ?? '',
+			name: data?.default_timezone?.string ?? '',
+			offset: data?.default_timezone?.offset ?? 0,
 		} as TimezoneProps),
 	};
 };
