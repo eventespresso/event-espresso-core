6fb4456dfc3bbbf39a1b35cddbdab46f030a1ca1
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-22 13:06:19 +0300

    Added styles to DropdownToggle; js_only.

--- assets/src/application/ui/layout/dropdownMenu/DropdownToggle/index.tsx
+++ assets/src/application/ui/layout/dropdownMenu/DropdownToggle/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import classNames from 'classnames';
 
 import { More } from '@appDisplay/icons';
-import { DropdownToggleProps } from './types';
+import { DropdownToggleProps } from '../types';
 import { IconButton } from '@infraUI/inputs';
 import { MenuToggle } from '@infraUI/layout/menu';
 import { Tooltip } from '@infraUI/display';
+
+import './style.scss';
 
 const DropdownToggle = React.forwardRef<typeof MenuToggle, DropdownToggleProps>(
 	({ icon = More, isOpen, tooltip, ...toggleProps }, ref) => {
