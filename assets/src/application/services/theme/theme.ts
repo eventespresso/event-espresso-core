b62410cca936a3dd861ee4cfad4c07d95d0e2040
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-04-22 11:32:41 +0300

    Adjusted zIndices; js_only.

--- assets/src/application/services/theme/theme.ts
+++ assets/src/application/services/theme/theme.ts
@@ -2,8 +2,11 @@ import { theme as chakraTheme, DefaultTheme } from '@chakra-ui/core';
 
 type CustomTheme = DefaultTheme;
 
+const zIndices = { ...chakraTheme.zIndices, popover: 1501 };
+
 const theme: CustomTheme = {
 	...chakraTheme,
+	zIndices,
 };
 
 export default theme;
