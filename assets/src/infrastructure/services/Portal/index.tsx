e7ded1ceb772decd51a95803ba23fc546b13cbf2
Brent Christensen <brent@eventespresso.com>
2020-04-22 18:53:15 -0700

    fix types

--- assets/src/infrastructure/services/Portal/index.tsx
+++ assets/src/infrastructure/services/Portal/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Portal as ChakraPortal, PortalProps } from '@chakra-ui/core';
 
-const Portal = (props: PortalProps) => <ChakraPortal {...props} />;
+const Portal: React.FC<PortalProps> = (props) => <ChakraPortal {...props} />;
 
 export default Portal;
