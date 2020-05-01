42cfd991bf85f063f90da9639020eaf2588717a2
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-02 00:28:28 +0300

    Updated Sort icon; js_only.

--- assets/src/application/ui/layout/entityList/filterBar/buttons/ToggleSortingButton.tsx
+++ assets/src/application/ui/layout/entityList/filterBar/buttons/ToggleSortingButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { __ } from '@wordpress/i18n';
 
-import { Sort } from '@appDisplay/icons';
+import { Sort } from '@appDisplay/icons/svgs';
 import { IconButton } from '@appInputs/Button';
 import { LabelPosition } from '@application/ui/display';
 import { ToggleSortingButtonProps } from '../types';
