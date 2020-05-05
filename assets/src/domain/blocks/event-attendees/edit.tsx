78b48024c9b2c588eb6d3e1bb9da036e83ff8426
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-06 02:00:15 +0530

    Clean up js_only

--- assets/src/domain/blocks/event-attendees/edit.tsx
+++ assets/src/domain/blocks/event-attendees/edit.tsx
@@ -5,16 +5,6 @@ import { AttendeesEditProps } from './types';
 import Controls from './controls';
 import AttendeesDisplay from './AttendeesDisplay';
 
-/* const AttendeesDisplay: React.FC<AttendeesEditProps> = ({ attributes }) => (
-	<Placeholder>
-		<div>
-			<h4>Hello, this is the cool new EE Block, if you feel lost, find a way :)</h4>
-			<span>Attributes:</span>
-			<pre>{JSON.stringify(attributes, null, 4)}</pre>
-		</div>
-	</Placeholder>
-); */
-
 const EventAttendeesEdit: React.FC<AttendeesEditProps> = (props) => {
 	return (
 		<>
