5c103b6b77dc6009eeeaf02da6adcb8591561dab
Vadim Nicolai <vadim@eventespresso.com>
2020-05-06 22:59:54 +0300

    Confirmation Modals improvements; closes #2613. (#2777)
    * CloseModal btn has been updated; js_only.
    
    * Refactor ModalWithAlert; js_only.

--- assets/src/application/ui/layout/modal/ModalWithAlert.tsx
+++ assets/src/application/ui/layout/modal/ModalWithAlert.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
-import { useDisclosure } from '@chakra-ui/core';
-import { __ } from '@wordpress/i18n';
 import { canUseDOM } from '@appServices/utilities/dom';
 import { ESCAPE } from '@wordpress/keycodes';
+import { __ } from '@wordpress/i18n';
 
-import { AlertDialog } from '@infraUI/display';
-import { Button } from '@application/ui/input';
+import { useConfirmationDialog } from '@appDisplay/confirm';
 import { Modal, ModalProps } from '@infraUI/layout/modal';
 
 interface Props extends ModalProps {
@@ -16,11 +14,12 @@ interface Props extends ModalProps {
 }
 
 const ModalWithAlert: React.FC<Props> = ({ children, showAlertOnEscape, ...props }) => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const cancelRef = React.useRef();
-	const cancelBtnText = props.cancelBtnText || __('No');
-	const header = props.header || __('Are you sure you want to close this?');
-	const okBtnText = props.okBtnText || __('Yes');
+	const confirmText = props.header || __('Are you sure you want to close this?');
+	const { confirmationDialog, onOpen } = useConfirmationDialog({
+		confirmText,
+		onConfirm: props.onClose as VoidFunction,
+	});
+
 	const onEscape = ({ keyCode }): void => {
 		if (keyCode === ESCAPE) {
 			onOpen();
@@ -44,16 +43,7 @@ const ModalWithAlert: React.FC<Props> = ({ children, showAlertOnEscape, ...props
 			<Modal {...props} closeOnEsc={!showAlertOnEscape}>
 				{children}
 			</Modal>
-			{showAlertOnEscape && (
-				<AlertDialog
-					cancelButton={<Button buttonText={cancelBtnText} ref={cancelRef} onClick={onClose} />}
-					header={header}
-					isOpen={isOpen}
-					leastDestructiveRef={cancelRef}
-					okButton={<Button buttonText={okBtnText} variantColor='red' onClick={props.onClose} ml={3} />}
-					onClose={onClose}
-				/>
-			)}
+			{showAlertOnEscape && confirmationDialog}
 		</>
 	);
 };
