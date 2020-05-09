bdee656d87b5cde4466705508cb092d9cfebfafe
Brent Christensen <brent@eventespresso.com>
2020-05-08 18:36:59 -0700

    add check for tests_get_phpunit_version

--- tests/includes/listener-loader.php
+++ tests/includes/listener-loader.php
@@ -1,6 +1,9 @@
 <?php
 
-if ( version_compare( tests_get_phpunit_version(), '7.0', '>=' ) ) {
+$phpunit_version = function_exists('tests_get_phpunit_version')
+    ? tests_get_phpunit_version()
+    : '6.0';
+if ( version_compare($phpunit_version, '7.0', '>=' ) ) {
 	require __DIR__ . '/phpunit7/speed-trap-listener.php';
 } else {
 	require __DIR__ . '/speed-trap-listener.php';
