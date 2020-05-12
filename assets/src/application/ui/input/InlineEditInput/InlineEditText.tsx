cf641a108d49d08c77dd6f927715fc82b8abbc59
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-12 16:39:59 +0300

    Updated InlineEditText and InlineEditTextArea; js_only.

--- assets/src/application/ui/input/InlineEditInput/InlineEditText.tsx
+++ assets/src/application/ui/input/InlineEditInput/InlineEditText.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import Dotdotdot from 'react-dotdotdot';
+
+import { Textfit } from 'react-textfit';
 
 import { TextProps } from './types';
-import { InlineEdit } from '@infraUI/inputs';
+import { InlineEdit, InlineEditPreviewProps } from '@infraUI/inputs';
+
+const Preview: React.FC<InlineEditPreviewProps> = ({ value, onRequestEdit, isEditing }) => {
+	if (isEditing) {
+		return null;
+	}
+
+	if (value.length > 50) {
+		return (
+			<Dotdotdot clamp={2}>
+				<span onClick={onRequestEdit}>{value}</span>
+			</Dotdotdot>
+		);
+	}
+
+	return (
+		<Textfit
+			max={24} // based on --ee-font-size-bigger: 1.5rem;
+			mode='single'
+		>
+			<span onClick={onRequestEdit}>{value}</span>
+		</Textfit>
+	);
+};
 
 const InlineEditText: React.FC<TextProps> = ({ placeholder = '', tag: as, ...props }) => {
-	return <InlineEdit {...props} as={as} inputType='text' placeholder={placeholder} />;
+	return <InlineEdit {...props} as={as} inputType='text' placeholder={placeholder} Preview={Preview} />;
 };
 
 export default InlineEditText;
