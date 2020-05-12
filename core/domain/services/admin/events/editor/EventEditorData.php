f0baab9b6e2862d3cae75881ac8d5c0fdd450a10
Brent Christensen <brent@eventespresso.com>
2020-05-12 13:22:50 -0700

    inject EEM_Event

--- core/domain/services/admin/events/editor/EventEditorData.php
+++ core/domain/services/admin/events/editor/EventEditorData.php
@@ -3,6 +3,7 @@
 namespace EventEspresso\core\domain\services\admin\events\editor;
 
 use EEM_Datetime;
+use EEM_Event;
 use EEM_Price;
 use EEM_Price_Type;
 use EEM_Ticket;
@@ -24,6 +25,11 @@ abstract class EventEditorData implements EventEditorDataInterface
     protected $datetime_model;
 
     /**
+     * @var EEM_Event $event_model
+     */
+    protected $event_model;
+
+    /**
      * @var EEM_Price $price_model
      */
     protected $price_model;
@@ -43,17 +49,20 @@ abstract class EventEditorData implements EventEditorDataInterface
      * AdvancedEditorAdminForm constructor.
      *
      * @param EEM_Datetime $datetime_model
+     * @param EEM_Event $event_model
      * @param EEM_Price    $price_model
      * @param EEM_Price_Type $price_type_model
      * @param EEM_Ticket   $ticket_model
      */
     public function __construct(
         EEM_Datetime $datetime_model,
+        EEM_Event $event_model,
         EEM_Price $price_model,
         EEM_Price_Type $price_type_model,
         EEM_Ticket $ticket_model
     ) {
         $this->datetime_model = $datetime_model;
+        $this->event_model = $event_model;
         $this->price_model = $price_model;
         $this->price_type_model = $price_type_model;
         $this->ticket_model = $ticket_model;
