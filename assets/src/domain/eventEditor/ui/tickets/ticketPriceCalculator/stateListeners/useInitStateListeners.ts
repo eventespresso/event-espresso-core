fb778393f5f24d44acdbac0395cf11a0e76ddcbd
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-13 16:49:09 +0530

    Fix decimal places based on user config js_only

--- assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/stateListeners/useInitStateListeners.ts
+++ assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/stateListeners/useInitStateListeners.ts
@@ -5,18 +5,20 @@ import { calculateBasePrice, calculateTicketTotal } from '../utils';
 import usePriceChangeListener from './usePriceChangeListener';
 import usePriceTypeChangeListener from './usePriceTypeChangeListener';
 import useTicketTotalChangeListener from './useTicketTotalChangeListener';
+import { useMoneyDisplay } from '@application/services';
 
 const useStateListeners = (): void => {
 	const { getData, reverseCalculate, setPrices, updateTicketPrice } = useDataState();
+	const { formatAmount } = useMoneyDisplay();
 
 	const updateBasePrice = useCallback(() => {
-		const newPrices = calculateBasePrice(getData());
+		const newPrices = calculateBasePrice(getData(), formatAmount);
 		setPrices(newPrices);
 	}, [getData, setPrices]);
 
 	const updateTicketTotal = useCallback(() => {
 		const ticketTotal = calculateTicketTotal(getData());
-		updateTicketPrice(ticketTotal);
+		updateTicketPrice(parseFloat(formatAmount(ticketTotal)));
 	}, [getData, updateTicketPrice]);
 
 	const calculatePrice = useCallback(() => {
