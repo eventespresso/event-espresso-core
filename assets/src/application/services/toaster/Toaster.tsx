3e8b29fee8e47d34d7d9cda2cf36f6c59a501ea6
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-13 18:52:20 +0300

    Extracted toasterIcons; js_only.

--- assets/src/application/services/toaster/Toaster.tsx
+++ assets/src/application/services/toaster/Toaster.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { theme } from '@chakra-ui/core';
 
-import { Check, CloseCircleOutlined, InfoCircleOutlined, Spinner } from '@appDisplay/icons/svgs';
-
+import toasterIcons from './toasterIcons';
 import { ToasterProps } from './types';
 
-const { colors } = theme;
-const fontSize = '1.2rem';
-
 const Toaster: React.FC<ToasterProps> = ({ message = 'loading...', type }) => {
-	const iconMapping = {
-		error: <CloseCircleOutlined color='rgb(255, 77, 79)' fontSize={fontSize} />,
-		info: <InfoCircleOutlined color={colors.blue['500']} fontSize={fontSize} />,
-		loading: <Spinner className='ee-loading-spinner' color={colors.cyan['500']} fontSize={fontSize} />,
-		success: <Check color={colors.green['500']} fontSize={fontSize} />,
-		warning: <InfoCircleOutlined color={colors.orange[500]} fontSize={fontSize} />,
-	};
-
 	return (
 		<div className='ee-toaster-notice__toast-body'>
-			{iconMapping[type]}
+			{toasterIcons[type]}
 			{message}
 		</div>
 	);
