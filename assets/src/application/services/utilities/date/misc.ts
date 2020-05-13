2dde08634b117f0c11ed7beb95ce2c9c6c22841a
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-13 21:33:15 +0300

    Piping; js_only.

--- assets/src/application/services/utilities/date/misc.ts
+++ assets/src/application/services/utilities/date/misc.ts
@@ -1,14 +1,16 @@
-import { setHours, setMinutes, setSeconds, setYear, setMonth, setDate } from 'date-fns';
+import { pipe } from 'ramda';
+import { setHours, setMinutes, setSeconds, setYear, setMonth, setDate } from 'date-fns/fp';
 
 /**
  * Sets the time of the date object to zero hour
  */
-export const setTimeToZeroHour = (date: Date): Date => setSeconds(setMinutes(setHours(date, 0), 0), 0);
+export const setTimeToZeroHour = (date: Date): Date => pipe(setHours(0), setMinutes(0), setSeconds(0))(date);
 
 /**
  * Sets the date, month and year of the date object to those of today
  */
 export const setDateToToday = (date: Date): Date => {
 	const today = new Date();
-	return setYear(setMonth(setDate(date, today.getDate()), today.getMonth()), today.getFullYear());
+
+	return pipe(setDate(today.getDate()), setMonth(today.getMonth()), setYear(today.getFullYear()))(date);
 };
