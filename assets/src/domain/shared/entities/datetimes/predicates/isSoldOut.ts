c61db86cad0e8731280b00732a9683505b8048d6
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-13 12:27:03 +0300

    Prettify; js_only.

--- assets/src/domain/shared/entities/datetimes/predicates/isSoldOut.ts
+++ assets/src/domain/shared/entities/datetimes/predicates/isSoldOut.ts
@@ -1,10 +1,7 @@
 import { Datetime } from '@edtrServices/apollo/types';
 import { isBooleanTrue } from '@appServices/utilities';
 
-const isSoldOut = (date: Datetime): boolean => isBooleanTrue(date.isSoldOut) || (
-	isFinite(date.capacity)
-	&& date.capacity > -1
-	&& date.capacity <= date.sold
-);
+const isSoldOut = (date: Datetime): boolean =>
+	isBooleanTrue(date.isSoldOut) || (isFinite(date.capacity) && date.capacity > -1 && date.capacity <= date.sold);
 
 export default isSoldOut;
--- assets/src/domain/shared/entities/tickets/predicates/isSoldOut/index.ts
+++ assets/src/domain/shared/entities/tickets/predicates/isSoldOut/index.ts
@@ -1,10 +1,8 @@
 import { isBooleanTrue } from '@appServices/utilities';
 import { Ticket } from '@edtrServices/apollo/types';
 
-const isSoldOut = (ticket: Ticket): boolean => isBooleanTrue(ticket.isSoldOut) || (
-	isFinite(ticket.quantity)
-	&& ticket.quantity > -1
-	&& ticket.quantity <= ticket.sold
-);
+const isSoldOut = (ticket: Ticket): boolean =>
+	isBooleanTrue(ticket.isSoldOut) ||
+	(isFinite(ticket.quantity) && ticket.quantity > -1 && ticket.quantity <= ticket.sold);
 
 export default isSoldOut;
