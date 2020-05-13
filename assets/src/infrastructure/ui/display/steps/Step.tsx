439b5746d29f27b6684900f639236e337f45ccd4
Brent Christensen <brent@eventespresso.com>
2020-05-12 18:50:17 -0700

    gosh... now dem form steps sure done look purdy

--- assets/src/infrastructure/ui/display/steps/Step.tsx
+++ assets/src/infrastructure/ui/display/steps/Step.tsx
@@ -3,17 +3,37 @@ import classNames from 'classnames';
 
 import { StepProps } from './types';
 
-const Step: React.FC<StepProps> = ({ active, className, disabled, icon: Icon, showStepNumber, stepNumber, title }) => {
-	const newClassName = classNames('ee-steps__step', className, {
-		active,
-		disabled,
-	});
+const Step: React.FC<StepProps> = ({
+	active,
+	className,
+	description = '',
+	disabled,
+	icon: Icon,
+	showStepNumber,
+	stepNumber,
+	title,
+	vertical = false
+}) => {
+	const newClassName = classNames(
+		'ee-form-step',
+		className,
+		{
+			'ee-form-step--active': active,
+			'ee-form-step--disabled': disabled,
+			'ee-form-step--vertical': vertical,
+			'ee-form-step--inline': !vertical,
+			'ee-form-step--no-icon': !Icon,
+		}
+	);
 	return (
-		<div className={newClassName}>
-			{Icon && <Icon />}
-			{showStepNumber && stepNumber}{' '}
-			{title}
-		</div>
+		<li className={ newClassName }>
+			{ showStepNumber && <div className={ 'ee-form-step__number' }>{ stepNumber }</div> }
+			{ Icon && <div className={ 'ee-form-step__icon' }><Icon /></div> }
+			<div className={ 'ee-form-step__content' }>
+				{ title && <div className={ 'ee-form-step__title' }>{ title }</div> }
+				{ description && <div className={ 'ee-form-step__desc' }>{ description }</div> }
+			</div>
+		</li>
 	);
 };
 
--- assets/src/infrastructure/ui/display/steps/types.ts
+++ assets/src/infrastructure/ui/display/steps/types.ts
@@ -1,15 +1,18 @@
 export interface CommonProps {
 	className?: string;
 	showStepNumber?: boolean;
+	vertical?: boolean;
 }
 
 export interface StepsProps extends CommonProps {
 	current?: number;
+	heading?: string;
 	initial?: number;
 }
 
 export interface StepProps extends CommonProps {
 	active?: boolean;
+	description?: string;
 	disabled?: boolean;
 	icon?: React.ComponentType;
 	stepIndex?: number; // 0, 1, 2, 3
