06977fc60c6f66a3da1f31e9ef12e89c2b6bec72
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-14 14:06:14 +0300

    Small improvement; js_only.

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ function resolveTsconfigPathsToModuleNameMapper() {
 
 	const moduleNameMapper = {
 		'^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/config/jest/__mocks__/fileMock.js',
 	};
 
 	Object.entries(paths).forEach(([alias, path]) => {
