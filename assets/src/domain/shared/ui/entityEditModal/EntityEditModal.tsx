978ad89f1ed1d4aa565d3783a1edad250bcdc86b
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-15 21:04:26 +0530

    Update EntityEditModal

--- assets/src/domain/shared/ui/entityEditModal/EntityEditModal.tsx
+++ assets/src/domain/shared/ui/entityEditModal/EntityEditModal.tsx
@@ -4,11 +4,13 @@ import { __ } from '@wordpress/i18n';
 import { Modal } from '@infraUI/layout/modal';
 import { EntityEditModalProps } from './types';
 
+import './styles.scss';
+
 const EntityEditModal: React.FC<EntityEditModalProps> = ({ isOpen, onClose, title, children }) => {
 	return (
 		<Modal
-			bodyClassName='ee-modal-form__body'
-			className='ee-modal-form'
+			bodyClassName='ee-entity-edit-modal__body'
+			className='ee-entity-edit-modal'
 			isOpen={isOpen}
 			onClose={onClose}
 			title={title}
