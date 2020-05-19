7d256c8bfad3ec088cfda17befba3923c5e439ba
Brent Christensen <brent@eventespresso.com>
2020-05-19 16:31:57 -0700

    tweak console log

--- assets/src/domain/index.ts
+++ assets/src/domain/index.ts
@@ -3,15 +3,12 @@ import './shared/services/publicPath';
 const domain = window?.eeDomain;
 
 if (domain) {
+	console.log(`%c importing: /domain/${domain}/entryPoint.ts`, 'color: SkyBlue;');
 	import(
 		/* webpackExclude: /(shared)/ */
 		/* webpackChunkName: "[request]" */
 		`./${domain}/entryPoint.ts`
-	)
-		.then(() => {
-			console.log('domain', domain);
-		})
-		.catch(console.error);
+	).catch(console.error);
 } else {
 	console.error('No domain supplied');
 }
