6515c2c7aed74b25aeedb074f851e57b6704aeac
Brent Christensen <brent@eventespresso.com>
2020-05-19 16:31:29 -0700

    TypeScript copy of @wordpress/dom-ready

--- assets/src/application/services/utilities/dom/index.ts
+++ assets/src/application/services/utilities/dom/index.ts
@@ -1,3 +1,4 @@
 export { default as canUseDOM } from './canUseDOM';
+export { default as domReady } from './domReady';
 export { default as getHTMLElementClientRect } from './getHTMLElementClientRect';
 export { default as renderDomElement } from './renderDomElement';
