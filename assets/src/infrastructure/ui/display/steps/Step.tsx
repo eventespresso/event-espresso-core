f7a4b37d027b39a4d3a415d326736016eab05bf6
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-21 16:56:19 +0300

    TAM responsiveness; js_only.

--- assets/src/infrastructure/ui/display/steps/Step.tsx
+++ assets/src/infrastructure/ui/display/steps/Step.tsx
@@ -14,24 +14,25 @@ const Step: React.FC<StepProps> = ({
 	vertical = false,
 	...props
 }) => {
-	const className = classNames(
-		props.className,
-		'ee-form-step',
-		{
-			'ee-form-step--active': active,
-			'ee-form-step--disabled': disabled,
-			'ee-form-step--vertical': vertical,
-			'ee-form-step--inline': !vertical,
-			'ee-form-step--no-icon': !Icon,
-		}
-	);
+	const className = classNames(props.className, 'ee-form-step', {
+		'ee-form-step--active': active,
+		'ee-form-step--disabled': disabled,
+		'ee-form-step--vertical': vertical,
+		'ee-form-step--inline': !vertical,
+		'ee-form-step--no-icon': !Icon,
+	});
+
 	return (
-		<li className={ className }>
-			{ showStepNumber && <div className={ 'ee-form-step__number' }>{ stepNumber }</div> }
-			{ Icon && <div className={ 'ee-form-step__icon' }><Icon /></div> }
-			<div className={ 'ee-form-step__content' }>
-				{ title && <div className={ 'ee-form-step__title' }>{ title }</div> }
-				{ description && <div className={ 'ee-form-step__desc' }>{ description }</div> }
+		<li className={className}>
+			{showStepNumber && <div className={'ee-form-step__number'}>{stepNumber}</div>}
+			{Icon && (
+				<div className={'ee-form-step__icon'}>
+					<Icon />
+				</div>
+			)}
+			<div className={'ee-form-step__content'}>
+				{title && <div className={'ee-form-step__title'}>{title}</div>}
+				{description && <div className={'ee-form-step__desc'}>{description}</div>}
 			</div>
 		</li>
 	);
--- assets/src/infrastructure/ui/display/steps/Steps.tsx
+++ assets/src/infrastructure/ui/display/steps/Steps.tsx
@@ -2,6 +2,7 @@ import React, { Children, cloneElement } from 'react';
 import classNames from 'classnames';
 import { __ } from '@wordpress/i18n';
 
+import { Heading } from '@infraUI/display';
 import { StepsProps } from './types';
 import './style.scss';
 
@@ -14,26 +15,31 @@ const Steps: React.FC<StepsProps> = ({
 	...props
 }) => {
 	const wrapperClassName = classNames(props.className, 'ee-form-steps__wrapper');
+
 	const listClassName = classNames('ee-form-steps', {
 		'ee-form-steps--vertical': vertical,
 		'ee-form-steps--inline': !vertical,
 	});
+
 	const heading = props.heading ?? __('Steps');
+
 	return (
-		<div className={ wrapperClassName }>
-			<h3>{ heading }</h3>
-			<ul className={ listClassName }>
-				{ Children.map(children, (child: any, index) => {
+		<div className={wrapperClassName}>
+			<Heading as='h3' mb={{ base: 4, md: 0 }}>
+				{heading}
+			</Heading>
+			<ul className={listClassName}>
+				{Children.map(children, (child: any, index) => {
 					const stepNumber = initial + index;
 					const childProps = {
 						stepNumber: `${stepNumber + 1}`,
 						stepIndex: stepNumber,
 						active: stepNumber === current,
 						showStepNumber,
-						...props
+						...props,
 					};
 					return cloneElement(child, childProps);
-				}) }
+				})}
 			</ul>
 		</div>
 	);
