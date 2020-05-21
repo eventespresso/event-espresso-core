42686e314597df77e612e04c85de1efaca076a6f
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-21 22:37:17 +0530

    Better safe than sorry js_only

--- assets/src/application/services/utilities/memo/reactFinalFormState.ts
+++ assets/src/application/services/utilities/memo/reactFinalFormState.ts
@@ -1,8 +1,8 @@
 import { FormRenderProps } from 'react-final-form';
 
 const reactFinalFormState = (prevProps: FormRenderProps, nextProps: FormRenderProps): boolean => {
-	const prevValue = JSON.stringify(prevProps.form.getState());
-	const nextValue = JSON.stringify(nextProps.form.getState());
+	const prevValue = JSON.stringify(prevProps.form?.getState());
+	const nextValue = JSON.stringify(nextProps.form?.getState());
 	return prevValue === nextValue;
 };
 
