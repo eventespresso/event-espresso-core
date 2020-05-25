68132b491b74dad5b337b3343d502ab26f88906a
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-25 22:58:53 +0300

    Added colors variables; js_only.

--- assets/src/application/ui/layout/entityList/filterBar/buttons/ViewSwitchBtnGroup.tsx
+++ assets/src/application/ui/layout/entityList/filterBar/buttons/ViewSwitchBtnGroup.tsx
@@ -4,14 +4,13 @@ import { __ } from '@wordpress/i18n';
 import { ButtonGroup } from '@application/ui/input';
 import CardViewFilterButton from './CardViewFilterButton';
 import TableViewFilterButton from './TableViewFilterButton';
-
 import { ViewSwitchBtnGroupProps } from '../types';
 
 import { getPropsAreEqual } from '@appServices/utilities';
 
 const ViewSwitchBtnGroup: React.FC<ViewSwitchBtnGroupProps> = ({ listId, setCardView, setTableView, view }) => {
 	return (
-		<ButtonGroup marginBottom='2rem'>
+		<ButtonGroup>
 			<TableViewFilterButton listId={listId} setTableView={setTableView} view={view} />
 			<CardViewFilterButton listId={listId} setCardView={setCardView} view={view} />
 		</ButtonGroup>
