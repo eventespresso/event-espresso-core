ffcf225c3ce3b2aee7ff706aa374c521116c2d6d
Brent Christensen <brent@eventespresso.com>
2020-05-26 16:52:01 -0700

    PueRequests Route

---
+++ core/domain/entities/routing/handlers/admin/PueRequests.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace EventEspresso\core\domain\entities\routing\handlers\admin;
+
+use EE_Dependency_Map;
+use EventEspresso\core\domain\entities\routing\handlers\Route;
+
+/**
+ * Class PueRequests
+ * loads resources for PUE
+ *
+ * @package EventEspresso\core\domain\entities\routing\handlers\admin
+ * @author  Brent Christensen
+ * @since   \$VID:$
+ */
+class PueRequests extends Route
+{
+
+    /**
+     * returns true if the current request matches this route
+     *
+     * @return bool
+     * @since   $VID:$
+     */
+    public function matchesCurrentRequest()
+    {
+        return $this->request->isAdmin() && apply_filters('FHEE__EE_System__brew_espresso__load_pue', true);
+    }
+
+
+    /**
+     * @since $VID:$
+     */
+    protected function registerDependencies()
+    {
+        $this->dependency_map->registerDependencies(
+            'EventEspresso\core\services\licensing\LicenseService',
+            [
+                'EventEspresso\core\domain\services\pue\Stats'  => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\domain\services\pue\Config' => EE_Dependency_Map::load_from_cache,
+            ]
+        );
+        $this->dependency_map->registerDependencies(
+            'EventEspresso\core\domain\services\pue\Stats',
+            [
+                'EventEspresso\core\domain\services\pue\Config'        => EE_Dependency_Map::load_from_cache,
+                'EE_Maintenance_Mode'                                  => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\domain\services\pue\StatsGatherer' => EE_Dependency_Map::load_from_cache,
+            ]
+        );
+        $this->dependency_map->registerDependencies(
+            'EventEspresso\core\domain\services\pue\Config',
+            [
+                'EE_Network_Config' => EE_Dependency_Map::load_from_cache,
+                'EE_Config'         => EE_Dependency_Map::load_from_cache,
+            ]
+        );
+        $this->dependency_map->registerDependencies(
+            'EventEspresso\core\domain\services\pue\StatsGatherer',
+            [
+                'EEM_Payment_Method' => EE_Dependency_Map::load_from_cache,
+                'EEM_Event'          => EE_Dependency_Map::load_from_cache,
+                'EEM_Datetime'       => EE_Dependency_Map::load_from_cache,
+                'EEM_Ticket'         => EE_Dependency_Map::load_from_cache,
+                'EEM_Registration'   => EE_Dependency_Map::load_from_cache,
+                'EEM_Transaction'    => EE_Dependency_Map::load_from_cache,
+                'EE_Config'          => EE_Dependency_Map::load_from_cache,
+            ]
+        );
+    }
+
+
+    /**
+     * implements logic required to run during request
+     *
+     * @return bool
+     * @since   $VID:$
+     */
+    protected function requestHandler()
+    {
+        // pew pew pew
+        $this->loader->getShared('EventEspresso\core\services\licensing\LicenseService');
+        do_action('AHEE__EE_System__brew_espresso__after_pue_init');
+        return true;
+    }
+}
