7542887cd761b6f8e512e5414fcda8c866fa5ae8
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-26 20:28:34 +0530

    Improve useLogIfChanged

--- assets/src/application/services/hooks/useLogIfChanged.ts
+++ assets/src/application/services/hooks/useLogIfChanged.ts
@@ -1,15 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useCallback } from 'react';
 
-const useLogIfChanged = <T extends any>(name: string, value: T): void => {
+type ToString = <T>(value: T) => string;
+
+const useLogIfChanged = <T extends any>(name: string, value: T, toString?: ToString): void => {
 	const previous = useRef<T>(value);
 
-	const getValue = (value: any): any => {
-		if (typeof value === 'object') {
-			return JSON.stringify(value);
-		} else {
-			return value.toString();
-		}
-	};
+	const getValue = useCallback<ToString>(
+		(value) => {
+			if (toString) {
+				return toString(value);
+			}
+			if (typeof value === 'object') {
+				return JSON.stringify(value);
+			} else {
+				return value.toString();
+			}
+		},
+		[toString]
+	);
 
 	if (!Object.is(previous.current, value)) {
 		console.log(`${name} changed. Old: ${getValue(previous.current)}, New: ${getValue(value)} `);
