3456745fa6f629b7d39c81efdf4f45acce512fe0
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-26 17:45:34 +0300

    TicketPriceField styles update; js_only.

--- assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/fields/TicketPrice.tsx
+++ assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/fields/TicketPrice.tsx
@@ -3,11 +3,12 @@ import React, { useCallback } from 'react';
 import { useDataState } from '../data';
 import BaseField from './BaseField';
 import { BaseFieldProps, TicketPriceFieldProps } from './types';
+import TicketPriceField from './TicketPriceField';
 import { useMoneyDisplay } from '@appServices/utilities/money';
 
 type BFP = BaseFieldProps<number>;
 
-const TicketPriceField: React.FC<TicketPriceFieldProps> = (props) => {
+const PriceField: React.FC<TicketPriceFieldProps> = (props) => {
 	const { ticket, updateTicketPrice } = useDataState();
 	const { afterAmount, beforeAmount } = useMoneyDisplay();
 
@@ -16,12 +17,12 @@ const TicketPriceField: React.FC<TicketPriceFieldProps> = (props) => {
 	const setValue: BFP['setValue'] = useCallback((value) => updateTicketPrice(value), [updateTicketPrice]);
 
 	return (
-		<div className='ee-ticket-price-field'>
-			<div className='ee-ticket-price-field__before'>{beforeAmount}</div>
-			<BaseField {...props} getValue={getValue} setValue={setValue} name={'ticket.price'} />
-			<div className='ee-ticket-price-field__after'>{afterAmount}</div>
-		</div>
+		<TicketPriceField
+			after={afterAmount}
+			before={beforeAmount}
+			field={<BaseField {...props} getValue={getValue} setValue={setValue} name={'ticket.price'} />}
+		/>
 	);
 };
 
-export default TicketPriceField;
+export default PriceField;
--- assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/fields/index.ts
+++ assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/fields/index.ts
@@ -1,3 +1,5 @@
 export { default as PriceField } from './PriceField';
 
+export { default as TicketPrice } from './TicketPrice';
+
 export { default as TicketPriceField } from './TicketPriceField';
