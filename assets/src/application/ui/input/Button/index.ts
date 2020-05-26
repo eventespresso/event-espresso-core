6a6629e7440fa516d0ec0d2f813d531f54765122
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-05-26 23:42:29 +0300

    Added ButtonRow; js_only.

--- assets/src/application/ui/input/Button/index.ts
+++ assets/src/application/ui/input/Button/index.ts
@@ -2,6 +2,8 @@ export { default as Button } from './Button';
 
 export { default as ButtonGroup } from './ButtonGroup';
 
+export { default as ButtonRow } from './ButtonRow';
+
 export { default as IconButton } from './IconButton';
 
 export { default as Link } from './Link';
