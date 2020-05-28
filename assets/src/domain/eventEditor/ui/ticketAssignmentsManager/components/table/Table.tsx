6902cf0b6e28452d5c91b12158566d82ddbd54aa
Brent Christensen <brent@eventespresso.com>
2020-05-28 14:37:06 -0700

    fix TAM display & memoize

--- assets/src/domain/eventEditor/ui/ticketAssignmentsManager/components/table/Table.tsx
+++ assets/src/domain/eventEditor/ui/ticketAssignmentsManager/components/table/Table.tsx
@@ -3,25 +3,22 @@ import { __ } from '@wordpress/i18n';
 
 import useGetBodyRows from './useGetBodyRows';
 import useGetHeaderRows from './useGetHeaderRows';
-import { ResponsiveTable } from '@appLayout/espressoTable';
 import { DatesAndTickets } from '../../types';
+import { ResponsiveTable } from '@appLayout/espressoTable';
+import { useMemoStringify } from '@appServices/hooks';
 
 import './styles.scss';
 
 const Table: React.FC<DatesAndTickets> = ({ datetimes, tickets }) => {
 	const bodyRows = useGetBodyRows({ datetimes, tickets });
+	const className = useMemoStringify({ tableClassName: 'ticket-assignment-manager' });
 	const headerRows = useGetHeaderRows(tickets);
-
-	return (
-		<ResponsiveTable
-			bodyRows={bodyRows}
-			headerRows={headerRows}
-			metaData={{
-				tableId: 'ticket-assignment-manager-table',
-				tableCaption: __('Ticket Assignment Manager'),
-			}}
-		/>
-	);
+	const metaData = useMemoStringify({
+		isScrollable: true,
+		tableId: 'ticket-assignment-manager-table',
+		tableCaption: __('Ticket Assignment Manager'),
+	});
+	return <ResponsiveTable bodyRows={bodyRows} headerRows={headerRows} metaData={metaData} className={className} />;
 };
 
 export default Table;
