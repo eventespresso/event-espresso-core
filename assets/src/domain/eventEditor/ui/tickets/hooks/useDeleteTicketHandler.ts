66e8887baaf2318bdf6b07aa4c1cd162d7565e0a
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-29 22:52:14 +0530

    Delete realated prices  from Apollo cache when ticket is permanently deleted js_only

--- assets/src/domain/eventEditor/ui/tickets/hooks/useDeleteTicketHandler.ts
+++ assets/src/domain/eventEditor/ui/tickets/hooks/useDeleteTicketHandler.ts
@@ -1,36 +1,58 @@
 import { useCallback } from 'react';
 
 import useTicketPrices from '@edtrServices/apollo/queries/tickets/useTicketPrices';
-import { EntityId } from '@dataServices/types';
-import { useTicketMutator, usePriceMutator } from '@edtrServices/apollo/mutations';
+import type { EntityId } from '@dataServices/types';
+import { useTicketMutator } from '@edtrServices/apollo/mutations';
 import { getGuids } from '@appServices/predicates';
+import { usePriceQueryOptions } from '@edtrServices/apollo/queries';
+import type { PricesList } from '@edtrServices/apollo';
+import { useApolloClient } from '@apollo/react-hooks';
+import { assocPath } from 'ramda';
 
 type Callback = (deletePermanently?: boolean) => Promise<void>;
 
 const useDeleteTicketHandler = (id: EntityId): Callback => {
 	const { deleteEntity: deleteTicket } = useTicketMutator();
 	const relatedPrices = useTicketPrices(id);
-	const { deleteEntity: deletePrice } = usePriceMutator();
+	const priceQueryOptions = usePriceQueryOptions();
+	const client = useApolloClient();
 
-	const onDeleteTicket = useCallback<VoidFunction>(() => {
+	/**
+	 * Deletes the related prices from Apollo cache,
+	 * permanent deletion is handled server-side
+	 */
+	const deleteRelatedPrices = useCallback<VoidFunction>(() => {
 		// The prices that are not default or tax prices.
 		const pricesToDelete = relatedPrices.filter(({ isDefault, isTax }) => !isDefault && !isTax);
+		// if we have nothing to delete
+		if (!pricesToDelete.length) {
+			return;
+		}
 		const priceIdsToDelete = getGuids(pricesToDelete);
-		priceIdsToDelete.forEach((id) => {
-			deletePrice({ id, deletePermanently: true });
+		// read existing data from Apollo cache
+		const data = client.readQuery<PricesList>(priceQueryOptions);
+		// filter out the related prices from Apollo cache data
+		const pricesToRetain = data?.espressoPrices?.nodes?.filter(({ id }) => !priceIdsToDelete.includes(id)) || [];
+		// avoid the dirty object creation using assocPath
+		const newData = assocPath(['espressoPrices', 'nodes'], pricesToRetain, data);
+
+		// write the data back to cache
+		client.writeQuery<PricesList>({
+			...priceQueryOptions,
+			data: newData,
 		});
-	}, [deletePrice, relatedPrices]);
+	}, [client, relatedPrices]);
 
 	return useCallback<Callback>(
 		(deletePermanently) => {
 			return (
 				deleteTicket({ id, deletePermanently })
 					// delete prices only if ticket is deleted permanently
-					.then(() => deletePermanently && onDeleteTicket())
+					.then(() => deletePermanently && deleteRelatedPrices())
 					.catch(console.error)
 			);
 		},
-		[deleteTicket, id, onDeleteTicket]
+		[deleteTicket, id, deleteRelatedPrices]
 	);
 };
 
