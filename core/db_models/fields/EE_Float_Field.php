3ca3c49e5489b2a8c7acb5b977423317262d7b8a
Brent Christensen <brent@eventespresso.com>
2020-06-04 20:41:38 -0700

    add Currency_Config prop + formatting

--- core/db_models/fields/EE_Float_Field.php
+++ core/db_models/fields/EE_Float_Field.php
@@ -1,11 +1,16 @@
 <?php
 
 /**
- * Text_Fields is a base class for any fields which are have float value. (Exception: foreign and private key fields.
- * Wish PHP had multiple-inheritance for this...)
+ * EE_Float_Field is a base class for any fields which are have float value.
+ * (Exception: foreign and private key fields. Wish PHP had multiple-inheritance for this...)
  */
 class EE_Float_Field extends EE_Model_Field_Base
 {
+    /**
+     * @var EE_Currency_Config
+     */
+    public $currency;
+
     /**
      * @param string $table_column
      * @param string $nicename
@@ -16,6 +21,7 @@ class EE_Float_Field extends EE_Model_Field_Base
     {
         parent::__construct($table_column, $nicename, $nullable, $default_value);
         $this->setSchemaType('number');
+        $this->currency = EE_Registry::instance()->CFG->currency;
     }
 
 
@@ -25,58 +31,54 @@ class EE_Float_Field extends EE_Model_Field_Base
      * So if you want to pass in a string that NEEDS to interpret periods as decimal marks, call floatval() on it first.
      * Returns a float
      *
-     * @param type $value_inputted_for_field_on_model_object
+     * @param float|string $value
      * @return float
      */
-    public function prepare_for_set($value_inputted_for_field_on_model_object)
+    public function prepare_for_set($value)
     {
-//      echo __LINE__."$value_inputted_for_field_on_model_object<br>";
-        // remove whitespaces and thousands separators
-        if (is_string($value_inputted_for_field_on_model_object)) {
-            $value_inputted_for_field_on_model_object = str_replace(
-                array(" ", EE_Config::instance()->currency->thsnds),
+        if (is_string($value)) {
+            // remove whitespaces and thousands separators
+            $value_stripped = str_replace(
+                array(" ", $this->currency->thsnds),
                 "",
-                $value_inputted_for_field_on_model_object
+                $value
             );
-// echo __LINE__."$value_inputted_for_field_on_model_object<br>";
-// normalize it so periods are decimal marks (we don't care where you're from: we're talking PHP now)
-            $value_inputted_for_field_on_model_object = str_replace(
-                EE_Config::instance()->currency->dec_mrk,
+            // normalize it so periods are decimal marks (we don't care where you're from: we're talking PHP now)
+            $normalized_value = str_replace(
+                $this->currency->dec_mrk,
                 ".",
-                $value_inputted_for_field_on_model_object
+                $value_stripped
             );
-// echo __LINE__."$value_inputted_for_field_on_model_object<br>";
-// double-check there's absolutely nothing left on this string besides numbers
-            $value_inputted_for_field_on_model_object = preg_replace(
+            // double-check there's absolutely nothing left on this string besides numbers
+            $value = preg_replace(
                 "/[^0-9,.]/",
                 "",
-                $value_inputted_for_field_on_model_object
+                $normalized_value
             );
         }
-//      echo __LINE__."$value_inputted_for_field_on_model_object<br>";
-        return floatval($value_inputted_for_field_on_model_object);
+        return (float) $value;
     }
+
 
     /**
      * Returns the number formatted according to local custom (set by the country of the blog).
      *
      * @param float $value_on_field_to_be_outputted
+     * @param null  $schema
      * @return string
      */
     public function prepare_for_pretty_echoing($value_on_field_to_be_outputted, $schema = null)
     {
-        $EE = EE_Registry::instance();
         return number_format(
             $value_on_field_to_be_outputted,
-            $EE->CFG->currency->dec_plc,
-            $EE->CFG->currency->dec_mrk,
-            $EE->CFG->currency->thsnds
+            $this->currency->dec_plc,
+            $this->currency->dec_mrk,
+            $this->currency->thsnds
         );
     }
 
     public function prepare_for_set_from_db($value_found_in_db_for_model_object)
     {
-//      echo "prepare for set from db of ";d($value_found_in_db_for_model_object);
-        return floatval($value_found_in_db_for_model_object);
+        return (float) $value_found_in_db_for_model_object;
     }
 }
