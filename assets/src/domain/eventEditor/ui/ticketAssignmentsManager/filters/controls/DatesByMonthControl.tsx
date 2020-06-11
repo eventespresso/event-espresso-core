150ada9fb4e807747673ece24b8ff98b5bacb259
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-06-11 18:21:15 +0530

    show all dates by default js_only

--- assets/src/domain/eventEditor/ui/ticketAssignmentsManager/filters/controls/DatesByMonthControl.tsx
+++ assets/src/domain/eventEditor/ui/ticketAssignmentsManager/filters/controls/DatesByMonthControl.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { __ } from '@wordpress/i18n';
-import { pluck, last } from 'ramda';
 
 import { useDatetimes } from '@edtrServices/apollo/queries';
 import { SelectInput } from '@appInputs/SelectInput';
 import { FilterStateManager } from '../filterState';
-import { getMonthsListFromDatetimes, getYearMonthForNextDate } from '../../utils';
+import { getMonthsListFromDatetimes } from '../../utils';
 
 type DatesByMonthControlProps = Pick<FilterStateManager, 'datesByMonth' | 'setDatesByMonth'>;
 
@@ -15,27 +14,6 @@ const DatesByMonthControl: React.FC<DatesByMonthControlProps> = React.memo(({ da
 
 	// e.g. "2020:4" for May 2020
 	const yearMonth = datesByMonth.join(':');
-
-	useEffect(() => {
-		/**
-		 * It's possible that the month in filter state is not present in the dropdown list,
-		 * e.g. when a new event is created, the default date (the only one) is for next month.
-		 * In this case we will try to find the immediate next datetime and select its month.
-		 */
-		// After .flat() it will be like ["2019:11", "2020:2", "2020:3", "2020:4", "2020:9"]
-		const yearMonthOptions = monthsList.map(({ options }) => pluck('value', options)).flat();
-		if (!yearMonthOptions.includes(yearMonth)) {
-			const nextYearMonth = getYearMonthForNextDate(datetimes);
-
-			// if we are lucky to have a next/upcoming month
-			if (nextYearMonth) {
-				setDatesByMonth(nextYearMonth);
-			} else {
-				// as a fallback, set the last one (latest) in the list
-				setDatesByMonth(last<string>(yearMonthOptions));
-			}
-		}
-	}, []);
 
 	// Add all dates option at the top, "0:0" to match the "year:month" format
 	const monthsListWithAllDates = [{ value: '0:0', label: __('All Dates') }, ...monthsList];
