63be5bf981ecf90963b5291bc19ffabc62516983
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-06-11 19:32:55 +0300

    Small improvement; js_only.

--- assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/components/NoPricesBanner.tsx
+++ assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/components/NoPricesBanner.tsx
@@ -6,18 +6,19 @@ import { Banner } from '@application/ui/display';
 import AddDefaultPricesButton from '../buttons/AddDefaultPricesButton';
 import { TicketPriceCalculatorProps } from './TicketPriceCalculator';
 
-interface Props extends Pick<TicketPriceCalculatorProps, 'isWithinStepper'> {}
+interface Props extends Pick<TicketPriceCalculatorProps, 'standalone'> {}
 
-const NoPricesBanner: React.FC<Props> = ({ isWithinStepper }) => {
-	let descriptionText = 'Click the button below to load your default prices into the calculator. Additional ticket price modifiers can be added or removed.';
+const NoPricesBanner: React.FC<Props> = ({ standalone }) => {
+	let description = __(
+		'Click the button below to load your default prices into the calculator. Additional ticket price modifiers can be added or removed.'
+	);
 
-	if (isWithinStepper) {
-		descriptionText =
-			descriptionText +
-			' Click the save button below to assign which dates this ticket will be available for purchase on.';
+	if (standalone) {
+		description =
+			description +
+			__(' Click the save button below to assign which dates this ticket will be available for purchase on.');
 	}
 
-	const description = __(descriptionText);
 	const title = __('This Ticket is Currently Free');
 
 	return (
