4dc758ca451b724ee32e9a47be0e1b006814f69e
Brent Christensen <brent@eventespresso.com>
2020-06-18 09:28:42 -0700

    tweak tooltip colors for better a11y

--- assets/src/application/ui/display/withTooltip/withTooltip.tsx
+++ assets/src/application/ui/display/withTooltip/withTooltip.tsx
@@ -33,7 +33,7 @@ const withTooltip = <P extends withTooltipProps>(WrappedComponent: React.Compone
 			});
 			tooltipProps = { ...tooltipProps, className: 'ee-mobile-help-text__tooltip' };
 			toolTipped = (
-				<div className='ee-mobile-help-text__btn-wrap'>
+				<div className='ee-mobile-help-text__wrap'>
 					<WrappedComponent
 						{ ...(props as P) }
 						ref={ forwardedRef }
