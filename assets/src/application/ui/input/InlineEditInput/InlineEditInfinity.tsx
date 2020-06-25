597d7ca7cc2d707deefa168739d29ceb12df2b77
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-06-25 22:32:45 +0300

    Removed infinity icon and update; js_only.

--- assets/src/application/ui/input/InlineEditInput/InlineEditInfinity.tsx
+++ assets/src/application/ui/input/InlineEditInput/InlineEditInfinity.tsx
@@ -2,7 +2,6 @@ import React, { useCallback } from 'react';
 import classNames from 'classnames';
 
 import parseInfinity from '@appServices/utilities/number/parseInfinity';
-import { Infinity } from '@appDisplay/icons/svgs';
 import { InlineEdit, InlineEditPreviewProps } from '@infraUI/inputs';
 import { isInfinite } from '@application/services';
 
@@ -10,40 +9,42 @@ import TabbableText from './TabbableText'
 import { TextProps } from './types';
 
 const Preview: React.FC<InlineEditPreviewProps> = ({ value, onRequestEdit, isEditing }) => {
-	const isInfinity = isInfinite(value);
-	const className = classNames('ee-inline-edit__infinity', {
-		'ee-infinity-sign': isInfinity,
-	});
+    const isInfinity = isInfinite(value);
+    const className = classNames('ee-inline-edit__infinity', {
+        'ee-infinity-sign': isInfinity,
+    });
 
-	if (isEditing) {
-		return null;
-	}
+    if (isEditing) {
+        return null;
+    }
 
-	return <TabbableText className={className} icon={isInfinity && <Infinity />} onRequestEdit={onRequestEdit} text={!isInfinity && value} />;
+    const output = isInfinity ? '∞' : value;
+
+    return <TabbableText className={className} onRequestEdit={onRequestEdit} text={output} />;
 };
 
 const InlineEditInfinity: React.FC<TextProps> = ({ onChangeValue, value, ...rest }) => {
-	const isInfinity = isInfinite(value);
+    const isInfinity = isInfinite(value);
 
-	const onChangeHandler = useCallback<TextProps['onChangeValue']>(
-		(val) => {
-			const parsedValue = parseInfinity(val);
-			if (typeof onChangeValue === 'function') {
-				onChangeValue(parsedValue);
-			}
-		},
-		[onChangeValue]
-	);
+    const onChangeHandler = useCallback<TextProps['onChangeValue']>(
+        (val) => {
+            const parsedValue = parseInfinity(val);
+            if (typeof onChangeValue === 'function') {
+                onChangeValue(parsedValue);
+            }
+        },
+        [onChangeValue]
+    );
 
-	return (
-		<InlineEdit
-			{...rest}
-			inputType='number'
-			onChangeValue={onChangeHandler}
-			Preview={Preview}
-			value={isInfinity ? '' : value}
-		/>
-	);
+    return (
+        <InlineEdit
+            {...rest}
+            inputType='number'
+            onChangeValue={onChangeHandler}
+            Preview={Preview}
+            value={isInfinity ? '' : value}
+        />
+    );
 };
 
 export default InlineEditInfinity;
--- assets/src/application/ui/input/InlineEditInput/index.tsx
+++ assets/src/application/ui/input/InlineEditInput/index.tsx
@@ -1,3 +1,5 @@
+import './style.scss'
+
 export { default as InlineEditText } from './InlineEditText';
 
 export { default as InlineEditTextArea } from './InlineEditTextArea';
