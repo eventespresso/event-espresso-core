ddb9829b5e264857059a404a1eb72d0921538ded
Brent Christensen <brent@eventespresso.com>
2020-06-30 14:38:34 -0700

    spoof gql setup

--- tests/testcases/core/domain/services/graphql/GraphQLUnitTestCase.php
+++ tests/testcases/core/domain/services/graphql/GraphQLUnitTestCase.php
@@ -10,6 +10,9 @@ use EventEspresso\core\exceptions\InvalidInterfaceException;
 use EventEspresso\core\services\loaders\LoaderFactory;
 use EventEspresso\tests\mocks\core\domain\entities\routing\handlers\shared\GQLRequestsMock;
 use InvalidArgumentException;
+use WPGraphQL;
+use WPGraphQL\Data\Config;
+use WPGraphQL\Router;
 
 /**
  * Class GraphQLUnitTestCase
@@ -40,16 +43,13 @@ class GraphQLUnitTestCase extends EE_UnitTestCase
         if (! class_exists('WPGraphQL')) {
             require_once EE_THIRD_PARTY . 'wp-graphql/wp-graphql.php';
         }
-        do_action('after_setup_theme');
-        $eeSystem = EE_System::instance();
-        remove_action('init', [$eeSystem, 'set_hooks_for_core'], 1);
-        remove_action('init', [$eeSystem, 'perform_activations_upgrades_and_migrations'], 3);
-        remove_action('init', [$eeSystem, 'load_CPTs_and_session'], 5);
-        remove_action('init', [$eeSystem, 'load_controllers'], 7);
-        remove_action('init', [$eeSystem, 'core_loaded_and_ready'], 9);
-        remove_action('init', [$eeSystem, 'initialize'], 10);
-        remove_action('init', [$eeSystem, 'initialize_last'], 100);
-        do_action('init');
+
+        new Config();
+        new Router();
+        $wp_gql = WPGraphQL::instance();
+        do_action('graphql_init', $wp_gql);
+        $wp_gql->setup_types();
+
         GQLRequestsMock::register();
         // load handler for EE GraphQL requests
         $graphQL_manager = LoaderFactory::getLoader()->getNew(
