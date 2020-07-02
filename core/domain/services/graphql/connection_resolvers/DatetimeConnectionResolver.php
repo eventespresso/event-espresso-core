46772d1d006694be68be363502cb860bc3cfaa54
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-07-02 18:14:03 +0530

    Update WP GraphQL (#3015)
    * Move should_execute to parent class
    
    * Update WP GraphQL

--- core/domain/services/graphql/connection_resolvers/DatetimeConnectionResolver.php
+++ core/domain/services/graphql/connection_resolvers/DatetimeConnectionResolver.php
@@ -51,25 +51,6 @@ class DatetimeConnectionResolver extends AbstractConnectionResolver
     }
 
     /**
-     * Determine whether the Query should execute. If it's determined that the query should
-     * not be run based on context such as, but not limited to, who the user is, where in the
-     * ResolveTree the Query is, the relation to the node the Query is connected to, etc
-     *
-     * Return false to prevent the query from executing.
-     *
-     * @return bool
-     */
-    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
-    public function should_execute()
-    {
-        if (false === $this->should_execute) {
-            return false;
-        }
-
-        return $this->should_execute;
-    }
-
-    /**
      * Here, we map the args from the input, then we make sure that we're only querying
      * for IDs. The IDs are then passed down the resolve tree, and deferred resolvers
      * handle batch resolution of the posts.
