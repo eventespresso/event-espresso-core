71ec4d30db0ec3b727501d57324689c322597682
Brent Christensen <brent@eventespresso.com>
2020-07-07 11:22:55 -0700

    load CoreBlocksAssetManager and add JS

--- core/domain/entities/editor/CoreBlocksAssetManager.php
+++ core/domain/entities/editor/CoreBlocksAssetManager.php
@@ -2,7 +2,13 @@
 
 namespace EventEspresso\core\domain\entities\editor;
 
+use DomainException;
+use EventEspresso\core\domain\services\assets\CoreAssetManager;
+use EventEspresso\core\domain\services\assets\ReactAssetManager;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidEntityException;
 use EventEspresso\core\services\assets\BlockAssetManager;
+use EventEspresso\core\services\collections\DuplicateCollectionIdentifierException;
 
 /**
  * Class CoreBlocksAssetManager
@@ -14,7 +20,6 @@ use EventEspresso\core\services\assets\BlockAssetManager;
  */
 class CoreBlocksAssetManager extends BlockAssetManager
 {
-    const JS_HANDLE_CORE_BLOCKS_EDITOR = 'eventespresso-blocks';
     const JS_HANDLE_CORE_BLOCKS = 'eventespresso-blocks';
 
 
@@ -23,7 +28,45 @@ class CoreBlocksAssetManager extends BlockAssetManager
      */
     public function setAssetHandles()
     {
-        $this->setEditorScriptHandle(CoreBlocksAssetManager::JS_HANDLE_CORE_BLOCKS_EDITOR);
+        $this->setEditorScriptHandle(CoreBlocksAssetManager::JS_HANDLE_CORE_BLOCKS);
         $this->setScriptHandle(CoreBlocksAssetManager::JS_HANDLE_CORE_BLOCKS);
+    }
+
+
+    /**
+     * @throws InvalidDataTypeException
+     * @throws InvalidEntityException
+     * @throws DuplicateCollectionIdentifierException
+     * @throws DomainException
+     */
+    public function addAssets()
+    {
+        parent::addAssets();
+        $this->registerJavascript();
+    }
+
+
+    /**
+     * Register javascript assets
+     *
+     * @throws InvalidDataTypeException
+     * @throws InvalidEntityException
+     * @throws DuplicateCollectionIdentifierException
+     * @throws DomainException
+     */
+    private function registerJavascript()
+    {
+        $this->addJs(
+            CoreBlocksAssetManager::JS_HANDLE_CORE_BLOCKS,
+            [
+                ReactAssetManager::JS_HANDLE_REACT,
+                ReactAssetManager::JS_HANDLE_REACT_DOM,
+                CoreAssetManager::JS_HANDLE_JS_CORE,
+                'wp-components',
+                'wp-i18n',
+                'wp-keycodes',
+                'wp-url',
+            ]
+        )->setRequiresTranslation();
     }
 }
--- core/domain/entities/routing/handlers/admin/GutenbergEditor.php
+++ core/domain/entities/routing/handlers/admin/GutenbergEditor.php
@@ -3,7 +3,10 @@
 namespace EventEspresso\core\domain\entities\routing\handlers\admin;
 
 use EE_Dependency_Map;
+use EventEspresso\core\domain\entities\editor\CoreBlocksAssetManager;
 use EventEspresso\core\domain\entities\routing\data_nodes\domains\EventEditor;
+use EventEspresso\core\domain\entities\routing\data_nodes\EventEspressoData;
+use EventEspresso\core\domain\services\assets\EspressoCoreAppAssetManager;
 
 /**
  * Class GutenbergEditor
@@ -48,6 +51,19 @@ class GutenbergEditor extends AdminRoute
                 'EventEspresso\core\services\json\JsonDataNodeValidator' => EE_Dependency_Map::load_from_cache,
             ]
         );
+        $this->dependency_map->registerDependencies(
+            'EventEspresso\core\domain\entities\editor\CoreBlocksAssetManager',
+            [
+                'EventEspresso\core\domain\Domain'                   => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\assets\AssetCollection' => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\assets\Registry'        => EE_Dependency_Map::load_from_cache,
+            ]
+        );
+        /** @var EventEspressoData $primary_data_node */
+        $primary_data_node = $this->loader->getShared(
+            'EventEspresso\core\domain\entities\routing\data_nodes\EventEspressoData'
+        );
+        $primary_data_node->setTargetScript(CoreBlocksAssetManager::JS_HANDLE_CORE_BLOCKS);
         /** @var EventEditor $data_node */
         $data_node = $this->loader->getShared(
             'EventEspresso\core\domain\entities\routing\data_nodes\domains\GutenbergEditorData'
@@ -65,7 +81,7 @@ class GutenbergEditor extends AdminRoute
     protected function requestHandler()
     {
         $this->asset_manager = $this->loader->getShared(
-            'EventEspresso\core\domain\services\assets\EspressoCoreAppAssetManager'
+            'EventEspresso\core\domain\entities\editor\CoreBlocksAssetManager'
         );
         add_action('admin_enqueue_scripts', [$this->asset_manager, 'enqueueBrowserAssets'], 100);
         return true;
