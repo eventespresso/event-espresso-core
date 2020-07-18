a50869ca663ed3552356e19fba0665b552cf5592
Brent Christensen <brent@eventespresso.com>
2020-07-17 17:08:10 -0700

    CNO duties

--- assets/src/application/ui/forms/espressoForm/adapters/DatePicker.tsx
+++ assets/src/application/ui/forms/espressoForm/adapters/DatePicker.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { Datepicker } from '@infraUI/inputs/dateTime';
+import { DatePicker as DatePickerComponent } from '../../../input';
 import { FieldRendererProps } from '../types';
 
 // meta does not need to goto the input
 // format from RFF is different (function) than needed by DatePicker (string)
 const DatePicker: React.FC<FieldRendererProps> = ({ input: { onChange, ...input }, meta, format, ...rest }) => {
-	return <Datepicker {...input} {...rest} onChangeValue={onChange} />;
+	return <DatePickerComponent {...input} {...rest} onChangeValue={onChange} />;
 };
 
 export default DatePicker;
