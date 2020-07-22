0591666278f827743ff2af8fdf7f1201298b8a07
eebot <team@eventespresso.com>
2020-07-22 13:45:12 +0000

    Deployed from eventespresso/barista@b56cd2e01d14c4248056478f9058aba8b9bed88e

---
+++ assets/dist/static/js/edtrServices.8eca333a.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('eventespresso-adapters', 'eventespresso-constants', 'eventespresso-data', 'eventespresso-hooks', 'eventespresso-predicates', 'eventespresso-services', 'eventespresso-toaster', 'react', 'wp-i18n'), 'version' => '1ebd3c410a845a90ad647dcb31180076');
