d27f4eb42cdf58d451066fed6830118f52cc4e96
Brent Christensen <brent@eventespresso.com>
2020-07-29 17:12:45 -0700

    add ee-icon-button--link class

--- assets/src/application/ui/input/Button/Link.tsx
+++ assets/src/application/ui/input/Button/Link.tsx
@@ -6,7 +6,13 @@ import { LinkProps } from './types';
 import './style.scss';
 
 const Link: React.FC<LinkProps> = ({ children, href, icon, tooltip, tooltipProps, ...props }) => {
-	const className = classNames('ee-btn-base ee-icon-button ee-icon-button--borderless', props.className);
+	const className = classNames(
+		props.className,
+		'ee-btn-base',
+		'ee-icon-button',
+		'ee-icon-button--link',
+		'ee-icon-button--borderless'
+	);
 
 	const link = (
 		<a href={href} className={className} target='_blank' rel='noopener norefferer'>
