58a649b2f138efbe5a19a7f154289ef1a809a16d
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-04 20:10:41 +0530

    Create date amd ticket bulk edit handlers

---
+++ core/domain/services/graphql/mutators/DatetimeBulkUpdate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\mutators;
+
+use EEM_Datetime;
+use EventEspresso\core\domain\services\graphql\types\Datetime;
+
+class DatetimeBulkUpdate extends EntityMutator
+{
+
+    /**
+     * Defines the mutation data modification closure.
+     *
+     * @param EEM_Datetime $model
+     * @param Datetime     $type
+     * @return callable
+     */
+    public static function mutateAndGetPayload(EEM_Datetime $model, Datetime $type)
+    {
+        $entityMutator = DatetimeUpdate::mutateAndGetPayload($model, $type);
+        $bulkMutator = new BulkEntityMutator($entityMutator);
+        return array($bulkMutator, 'updateEntities');
+    }
+}
