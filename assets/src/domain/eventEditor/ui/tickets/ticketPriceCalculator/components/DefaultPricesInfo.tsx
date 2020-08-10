ae5cf682107bc31c5d49a941a413b90b7c1675b9
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-10 14:28:38 +0300

    Updated DefaultPricesInfo.

--- assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/components/DefaultPricesInfo.tsx
+++ assets/src/domain/eventEditor/ui/tickets/ticketPriceCalculator/components/DefaultPricesInfo.tsx
@@ -1,8 +1,11 @@
 import React, { useCallback } from 'react';
+import { any } from 'ramda';
 import { __ } from '@wordpress/i18n';
 
+import { isDefault } from '@sharedEntities/prices/predicates/selectionPredicates';
 import useConfig from '@appServices/config/useConfig';
 import { useEdtrState } from '@edtrHooks/edtrState';
+import { useDataState } from '../data';
 
 const DefaultPricesInfo = () => {
 	const config = useConfig();
@@ -10,17 +13,21 @@ const DefaultPricesInfo = () => {
 	const href = adminUrl + 'admin.php?page=pricing';
 
 	const { setPricesPollInterval } = useEdtrState();
+	const { prices } = useDataState();
+	const hasDefaultPrice = any(isDefault, prices);
 
 	const onClickLink = useCallback(() => {
 		setPricesPollInterval(4000); // 4 seconds
 	}, [setPricesPollInterval]);
 
 	return (
-		<div className='ee-tpc__default-prices-info'>
-			<a href={href} target='_blank' rel='noopener noreferrer' onClick={onClickLink}>
-				{__('Modify default prices.')}
-			</a>
-		</div>
+		hasDefaultPrice && (
+			<div className='ee-tpc__default-prices-info'>
+				<a href={href} target='_blank' rel='noopener noreferrer' onClick={onClickLink}>
+					{__('Modify default prices.')}
+				</a>
+			</div>
+		)
 	);
 };
 
