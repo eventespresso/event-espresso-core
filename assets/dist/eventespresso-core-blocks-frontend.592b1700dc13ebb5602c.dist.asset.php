912ab5f716afd75e02afa200895a7c85e8d7fcfc
Brent Christensen <brent@eventespresso.com>
2020-08-13 15:59:37 -0700

    update build files

--- assets/dist/eventespresso-core-blocks-frontend.592b1700dc13ebb5602c.dist.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '592b1700dc13ebb5602cf04fbf3844c8');
--- assets/dist/eventespresso-data-stores.482765b7054847fb1115.dist.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('eejs-core', 'eventespresso-helpers', 'eventespresso-model', 'eventespresso-validators', 'eventespresso-value-objects', 'eventespresso-vendor', 'lodash', 'wp-api-fetch', 'wp-data', 'wp-hooks', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-url'), 'version' => '482765b7054847fb1115aa5f31542485');
