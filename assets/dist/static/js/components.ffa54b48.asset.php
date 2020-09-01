66d5bad1dabc8018dbb8dc71e111fb00316d06cd
eebot <team@eventespresso.com>
2020-09-01 17:57:46 +0000

    Deployed from eventespresso/barista@76d1e11fa59659e16ab6289dba777978260e6031

--- assets/dist/static/js/components.ffa54b48.asset.php
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:assets/dist/static/js/components.cf4f8648.asset.php
-<?php return array('dependencies' => array('eventespresso-adapters', 'eventespresso-constants', 'eventespresso-dates', 'eventespresso-form', 'eventespresso-hooks', 'eventespresso-registry', 'eventespresso-richTextEditor', 'eventespresso-services', 'eventespresso-utils', 'react', 'react-dom', 'wp-i18n', 'wp-keycodes', 'wp-url'), 'version' => 'b41934edc8684619374156aa825e49bd');
-=======
-<?php return array('dependencies' => array('eventespresso-adapters', 'eventespresso-constants', 'eventespresso-dates', 'eventespresso-form', 'eventespresso-hooks', 'eventespresso-registry', 'eventespresso-richTextEditor', 'eventespresso-services', 'eventespresso-utils', 'react', 'react-dom', 'wp-i18n', 'wp-keycodes', 'wp-url'), 'version' => '5c656ff57334447087ebdd309a6bd30a');
->>>>>>> f4f28f4ff0... Deployed from eventespresso/barista@4bcc5dcce666fbff6d1aa84501f5397536bfd294:assets/dist/static/js/components.ffa54b48.asset.php
