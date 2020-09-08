e60a768c66aec63f818e56646cd968d37b9b8e1e
EE DevBox Server <admin@eventespresso.com>
2020-09-08 13:44:03 -0700

    Bumping version to 4.10.7.p and prepped for release

--- assets/src/higher-order-components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as withBaseControl } from './base-control';
-export { default as withMoney } from './money';
-export { default as withLatestCheckin } from './with-latest-checkin';
-
--- tests/mocks/addons/new-messages-template-pack/templates/email_payment_reminder_event_list.template.php
+++
@@ -1,6 +0,0 @@
-<!-- Callout Panel -->
-<p class="callout"><strong><?php _e('Event:', 'event_espresso'); ?> [EVENT_LINK]</strong></p>
-<!-- /Callout Panel -->
-<div class="content">
-	[TICKET_LIST]
-</div>
