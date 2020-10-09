74e65650e1cd2a13e52104cf55487475d79e7a19
Brent Christensen <brent@eventespresso.com>
2020-10-09 12:46:07 -0700

    new DependencyHandler class and Interface

---
+++ core/services/dependencies/DependencyHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace EventEspresso\core\services\dependencies;
+
+use EE_Dependency_Map;
+
+abstract class DependencyHandler implements DependencyHandlerInterface
+{
+
+    /**
+     * @var EE_Dependency_Map $dependency_map
+     */
+    protected $dependency_map;
+
+
+    /**
+     * DependencyHandler constructor.
+     *
+     * @param EE_Dependency_Map $dependency_map
+     */
+    public function __construct(EE_Dependency_Map $dependency_map)
+    {
+        $this->dependency_map = $dependency_map;
+    }
+}
---
+++ core/services/dependencies/DependencyHandlerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EventEspresso\core\services\dependencies;
+
+interface DependencyHandlerInterface
+{
+    /**
+     * @since $VID:$
+     */
+    public function registerDependencies();
+}
