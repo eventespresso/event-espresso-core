617df8d6a02cccbd4dad4fcef8b462f1952dbffe
Brent Christensen <brent@eventespresso.com>
2020-10-13 13:03:07 -0700

    fix tableExists queries; code cleanup

--- core/services/database/TableAnalysis.php
+++ core/services/database/TableAnalysis.php
@@ -2,6 +2,9 @@
 
 namespace EventEspresso\core\services\database;
 
+use EE_Base;
+use wpdb;
+
 /**
  * Class TableAnalysis
  * For analyzing database tables; should not perform any manipulation, or have
@@ -11,7 +14,7 @@ namespace EventEspresso\core\services\database;
  * @subpackage
  * @author                Mike Nelson
  */
-class TableAnalysis extends \EE_Base
+class TableAnalysis extends EE_Base
 {
 
     /**
@@ -25,7 +28,7 @@ class TableAnalysis extends \EE_Base
      * except if it currently has the wpdb->base_prefix on the front, in which case
      * it will have the wpdb->base_prefix on it
      *
-     * @global \wpdb $wpdb
+     * @global wpdb $wpdb
      * @param string $table_name
      * @return string $tableName, having ensured it has the wpdb prefix on the front
      */
@@ -40,7 +43,7 @@ class TableAnalysis extends \EE_Base
      * Indicates whether or not the table has any entries. $table_name can
      * optionally start with $wpdb->prefix or not
      *
-     * @global \wpdb $wpdb
+     * @global wpdb $wpdb
      * @param string $table_name
      * @return bool
      */
@@ -50,7 +53,7 @@ class TableAnalysis extends \EE_Base
         $table_name = $this->ensureTableNameHasPrefix($table_name);
         if ($this->tableExists($table_name)) {
             $count = $wpdb->get_var("SELECT COUNT(*) FROM $table_name");
-            return absint($count) === 0 ? true : false;
+            return absint($count) === 0;
         }
         return false;
     }
@@ -60,63 +63,22 @@ class TableAnalysis extends \EE_Base
      * Indicates whether or not the table exists. $table_name can optionally
      * have the $wpdb->prefix on the beginning, or not.
      *
-     * @global \wpdb $wpdb
-     * @global array EZSQL_Error
-     * @param        $table_name
+     * @global wpdb $wpdb
+     * @param string $table_name
      * @return bool
      */
     public function tableExists($table_name)
     {
-        global $wpdb, $EZSQL_ERROR;
+        global $wpdb;
         $table_name = $this->ensureTableNameHasPrefix($table_name);
-        // ignore if this causes an sql error
-        $old_error = $wpdb->last_error;
-        $old_suppress_errors = $wpdb->suppress_errors();
-        $old_show_errors_value = $wpdb->show_errors(false);
-        $ezsql_error_cache = $EZSQL_ERROR;
-        $wpdb->get_results("SELECT * from $table_name LIMIT 1");
-        $wpdb->show_errors($old_show_errors_value);
-        $wpdb->suppress_errors($old_suppress_errors);
-        $new_error = $wpdb->last_error;
-        $wpdb->last_error = $old_error;
-        $EZSQL_ERROR = $ezsql_error_cache;
-        // if there was a table doesn't exist error
-        if (! empty($new_error)) {
-            if (in_array(
-                \EEH_Activation::last_wpdb_error_code(),
-                array(
-                    1051, // bad table
-                    1109, // unknown table
-                    117, // no such table
-                )
-            )
-                ||
-                preg_match(
-                    '~^Table .* doesn\'t exist~',
-                    $new_error
-                ) // in case not using mysql and error codes aren't reliable, just check for this error string
-            ) {
-                return false;
-            } else {
-                // log this because that's weird. Just use the normal PHP error log
-                error_log(
-                    sprintf(
-                        __(
-                            'Event Espresso error detected when checking if table existed: %1$s (it wasn\'t just that the table didn\'t exist either)',
-                            'event_espresso'
-                        ),
-                        $new_error
-                    )
-                );
-            }
-        }
-        return true;
+        $query = $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) );
+        return $wpdb->get_var( $query ) === $table_name;
     }
 
 
     /**
-     * @param $table_name
-     * @param $index_name
+     * @param string $table_name
+     * @param string $index_name
      * @return array of columns used on that index, Each entry is an object with the following properties {
      * @type string Table
      * @type string Non_unique "0" or "1"
