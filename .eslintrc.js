a661b632b84682edb9aa901aa279fc698f8d9b71
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-10-20 22:38:04 +0530

    Disable certain lint rules js_only

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,16 @@ module.exports = {
 			files: [ '*.js' ],
 			rules: {
 				"react/react-in-jsx-scope": "off",
+				"jsdoc/no-undefined-types": "off",
+				"@wordpress/i18n-translator-comments": "off",
+				"@wordpress/valid-sprintf": "off",
+				"jsdoc/check-types": "off",
+				"jsdoc/check-tag-names": "off",
+				"react/no-render-return-value": "off",
+				"jsdoc/check-param-names": "off",
+				"jsdoc/valid-types":"off",
+				"no-unused-vars": "off",
+				"jest/expect-expect": "off"
 			},
 			globals: {
 				eejsdata:true
