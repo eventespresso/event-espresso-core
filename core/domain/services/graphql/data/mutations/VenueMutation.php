266a316454b7e160acb291a28417986cf4bdc11b
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-10-20 19:35:19 +0530

    Complete venue mutation

--- core/domain/services/graphql/data/mutations/VenueMutation.php
+++ core/domain/services/graphql/data/mutations/VenueMutation.php
@@ -35,7 +35,73 @@ class VenueMutation
             $args['VNU_short_desc'] = sanitize_text_field($input['shortDescription']);
         }
 
-        // Likewise the other fields...
+        if (! empty($input['identifier'])) {
+            $args['VNU_identifier'] = sanitize_title($input['identifier']);
+        }
+
+        if (! empty($input['created'])) {
+            $args['VNU_created'] = new DateTime(sanitize_text_field($input['created']));
+        }
+
+        if (! empty($input['order'])) {
+            $args['VNU_order'] = absint($input['order']);
+        }
+
+        if (! empty($input['wpUser'])) {
+            $args['VNU_wp_user'] = absint($input['wpUser']);
+        }
+
+        if (! empty($input['address'])) {
+            $args['VNU_address'] = sanitize_text_field($input['address']);
+        }
+
+        if (! empty($input['address2'])) {
+            $args['VNU_address2'] = sanitize_text_field($input['address2']);
+        }
+
+        if (! empty($input['city'])) {
+            $args['VNU_city'] = sanitize_text_field($input['city']);
+        }
+
+        if (! empty($input['state'])) {
+            $args['STA_ID'] = absint($input['state']);
+        }
+
+        if (! empty($input['country'])) {
+            $args['CNT_ISO'] = sanitize_text_field($input['country']);
+        }
+
+        if (! empty($input['zip'])) {
+            $args['VNU_zip'] = sanitize_text_field($input['zip']);
+        }
+
+        if (! empty($input['capacity'])) {
+            $args['VNU_capacity'] = absint($input['capacity']);
+        }
+
+        if (! empty($input['phone'])) {
+            $args['VNU_phone'] = sanitize_text_field($input['phone']);
+        }
+
+        if (! empty($input['virtualPhone'])) {
+            $args['VNU_virtual_phone'] = sanitize_text_field($input['virtualPhone']);
+        }
+
+        if (! empty($input['url'])) {
+            $args['VNU_url'] = sanitize_text_field($input['url']);
+        }
+
+        if (! empty($input['virtualUrl'])) {
+            $args['VNU_virtual_url'] = sanitize_text_field($input['virtualUrl']);
+        }
+
+        if (! empty($input['googleMapLink'])) {
+            $args['VNU_google_map_link'] = sanitize_text_field($input['googleMapLink']);
+        }
+
+        if (! empty($input['enableForGmap'])) {
+            $args['VNU_enable_for_gmap'] = (bool) $input['enableForGmap'];
+        }
 
         return apply_filters(
             'FHEE__EventEspresso_core_domain_services_graphql_data_mutations__venue_args',
