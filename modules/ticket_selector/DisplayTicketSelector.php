fe4c551a8533956c7d2169b9f8f7deb5c263e766
Tony Warwick <tony@eventespresso.com>
2020-11-24 15:34:06 +0000

    Display error on iframe output (#2754)
    * Display an error on the ticket selector iFrame output when the ID does not correspond to a valid event.
    With WP_DEBUG enabled and a logged in user with the edit_pages cap show a detailed message, otherwise a simple message.
    
    * Use ExceptionStackTraceDisplay
    
    * Break out the iFrame error code into its own function.

--- modules/ticket_selector/DisplayTicketSelector.php
+++ modules/ticket_selector/DisplayTicketSelector.php
@@ -16,6 +16,7 @@ use EEM_Event;
 use EEM_Ticket;
 use EventEspresso\core\exceptions\InvalidDataTypeException;
 use EventEspresso\core\exceptions\InvalidInterfaceException;
+use EventEspresso\core\exceptions\ExceptionStackTraceDisplay;
 use InvalidArgumentException;
 use WP_Post;
 
@@ -201,7 +202,7 @@ class DisplayTicketSelector
         remove_filter('FHEE__EE_Ticket_Selector__display_ticket_selector_submit', '__return_true');
         // poke and prod incoming event till it tells us what it is
         if (! $this->setEvent($event)) {
-            return false;
+            return $this->handleMissingEvent();
         }
         // is the event expired ?
         $template_args['event_is_expired'] = ! is_admin() ? $this->event->is_expired() : false;
@@ -753,5 +754,48 @@ class DisplayTicketSelector
     public function formClose()
     {
         return '</form>';
+    }
+
+
+    /**
+     * handleMissingEvent
+     * Returns either false or an error to display when no valid event is passed.
+     *
+     * @return mixed
+     * @throws ExceptionStackTraceDisplay
+     * @throws InvalidInterfaceException
+     */
+    protected function handleMissingEvent()
+    {
+        // If this is not an iFrame request, simply return false.
+        if (! $this->isIframe()) {
+            return false;
+        }
+        // This is an iFrame so return an error.
+        // Display stack trace if WP_DEBUG is enabled.
+        if (WP_DEBUG === true && current_user_can('edit_pages')) {
+            $event_id = EE_Registry::instance()->REQ->get('event', 0);
+            new ExceptionStackTraceDisplay(
+                new InvalidArgumentException(
+                    sprintf(
+                        esc_html__(
+                            'A valid Event ID is required to display the ticket selector.%3$sAn Event with an ID of "%1$s" could not be found.%3$sPlease verify that the embed code added to this post\'s content includes an "%2$s" argument and that its value corresponds to a valid Event ID.',
+                            'event_espresso'
+                        ),
+                        $event_id,
+                        'event',
+                        '<br />'
+                    )
+                )
+            );
+            return '';
+        }
+        // If WP_DEBUG is not enabled, display a message stating the event could not be found.
+        return EEH_HTML::p(
+            esc_html__(
+                'A valid Event could not be found. Please contact the event administrator for assistance.',
+                'event_espresso'
+            )
+        );
     }
 }
