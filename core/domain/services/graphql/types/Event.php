09c150757b3a2ba40e9012c378e72f69d68eba14
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-12-22 13:44:21 +0530

    fix field type

--- core/domain/services/graphql/types/Event.php
+++ core/domain/services/graphql/types/Event.php
@@ -77,7 +77,7 @@ class Event extends TypeBase
                 'ID',
                 esc_html__('The event ID.', 'event_espresso')
             ),
-            new GraphQLOutputField(
+            new GraphQLField(
                 'defaultRegStatus',
                 $this->namespace . 'RegistrationStatusEnum',
                 'default_registration_status',
