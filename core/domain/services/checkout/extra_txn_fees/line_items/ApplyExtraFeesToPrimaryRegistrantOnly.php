3570bf52f7100f1f9b27f7d465f0a7f5c0c04638
Brent Christensen <brent@eventespresso.com>
2021-01-27 11:02:53 -0800

    ExtraTxnFeesForLineItemsHandler

---
+++ core/domain/services/checkout/extra_txn_fees/line_items/ApplyExtraFeesToPrimaryRegistrantOnly.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace EventEspresso\core\domain\services\checkout\extra_txn_fees\line_items;
+
+use EE_Error;
+use EEI_Line_Item;
+use EventEspresso\core\domain\services\checkout\extra_txn_fees\ExtraTxnFeesHandler;
+
+/**
+ * Class ApplyExtraFeesToAllRegistrationsEvenly
+ *
+ * @author  Brent Christensen
+ * @package EventEspresso\core\domain\services\checkout\extra_txn_fees\payments
+ * @since   $VID:$
+ */
+class ApplyExtraFeesToPrimaryRegistrantOnly implements ExtraTxnFeesDistributionStrategyInterface
+{
+
+    /**
+     * @param EEI_Line_Item $line_item
+     * @param EEI_Line_Item $child_line_item
+     * @param float         $original_li_total
+     * @param float         $running_total_for_all_tickets
+     * @param float         $running_total_for_specific_ticket
+     * @param bool          $is_primary_registrant
+     * @param int           $total_reg_count
+     * @param string        $context
+     * @throws EE_Error
+     */
+    public function adjustUnitPriceForNonTicketLineItem(
+        EEI_Line_Item $line_item,
+        EEI_Line_Item $child_line_item,
+        $original_li_total,
+        $running_total_for_all_tickets,
+        $running_total_for_specific_ticket,
+        $is_primary_registrant,
+        $total_reg_count,
+        $context
+    ) {
+        $percent_of_running_total = $running_total_for_all_tickets
+            ? $original_li_total / $running_total_for_all_tickets
+            : 0;
+        $line_item->set_total($running_total_for_specific_ticket * $percent_of_running_total);
+
+        if ($context === ExtraTxnFeesHandler::CONTEXT_ONE_REGISTRATION && ! $is_primary_registrant) {
+            $child_line_item->set_unit_price(0);
+            $child_line_item->set_quantity(0);
+            $child_line_item->set_total(0);
+        }
+
+        if (! $line_item->is_percent()) {
+            $line_item->set_unit_price($line_item->total() / $line_item->quantity());
+        }
+    }
+
+
+    /**
+     * @param EEI_Line_Item $line_item
+     * @param EEI_Line_Item $child_line_item
+     * @param bool          $is_primary_registrant
+     * @param int           $total_reg_count
+     * @param string        $context
+     */
+    public function adjustUnitPriceAndQtyForTicketLineItem(
+        EEI_Line_Item $line_item,
+        EEI_Line_Item $child_line_item,
+        $is_primary_registrant,
+        $total_reg_count,
+        $context
+    ) {
+        $child_line_item->set_quantity($line_item->quantity());
+        $child_line_item->set_total($child_line_item->unit_price() * $child_line_item->quantity());
+    }
+}
