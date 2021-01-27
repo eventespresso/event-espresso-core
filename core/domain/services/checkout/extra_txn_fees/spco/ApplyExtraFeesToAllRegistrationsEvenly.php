fc28e4dfc67cac2588b048f98dc713268ad57523
Brent Christensen <brent@eventespresso.com>
2021-01-27 11:03:08 -0800

    ExtraTxnFeesForRegistrantsHandler

---
+++ core/domain/services/checkout/extra_txn_fees/spco/ApplyExtraFeesToAllRegistrationsEvenly.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EventEspresso\core\domain\services\checkout\extra_txn_fees\spco;
+
+use EE_Error;
+use EE_Registration;
+use ReflectionException;
+
+/**
+ * Class ApplyExtraFeesToAllRegistrationsEvenly
+ *
+ * @author  Brent Christensen
+ * @package EventEspresso\core\domain\services\checkout\extra_txn_fees\payments
+ * @since   $VID:$
+ */
+class ApplyExtraFeesToAllRegistrationsEvenly implements ExtraTxnFeesDistributionStrategyInterface
+{
+    /**
+     * @param float             $extra_fees
+     * @param EE_Registration[] $registrations
+     * @throws EE_Error
+     * @throws ReflectionException
+     */
+    public function applyExtraFeesToRegistrants($extra_fees, array $registrations)
+    {
+        // divide remaining fees by number of registrations and apply to each
+        $extra_payment = $extra_fees / count($registrations);
+        foreach ($registrations as $registration) {
+            if ($registration instanceof EE_Registration) {
+                $new_registration_total = $registration->final_price() + $extra_payment;
+                $registration->set_final_price($new_registration_total);
+                $registration->save();
+            }
+        }
+    }
+}
---
+++ core/domain/services/checkout/extra_txn_fees/spco/ApplyExtraFeesToPrimaryRegistrantOnly.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace EventEspresso\core\domain\services\checkout\extra_txn_fees\spco;
+
+use EE_Error;
+use EE_Registration;
+use ReflectionException;
+
+/**
+ * Class ApplyExtraFeesToAllRegistrationsEvenly
+ *
+ * @author  Brent Christensen
+ * @package EventEspresso\core\domain\services\checkout\extra_txn_fees\payments
+ * @since   $VID:$
+ */
+class ApplyExtraFeesToPrimaryRegistrantOnly implements ExtraTxnFeesDistributionStrategyInterface
+{
+    /**
+     * @param float             $extra_fees
+     * @param EE_Registration[] $registrations
+     * @throws EE_Error
+     * @throws ReflectionException
+     */
+    public function applyExtraFeesToRegistrants($extra_fees, array $registrations)
+    {
+        foreach ($registrations as $registration) {
+            if ($registration instanceof EE_Registration && $registration->is_primary_registrant()) {
+                $primary_registrant_total = $registration->final_price() + $extra_fees;
+                $registration->set_final_price($primary_registrant_total);
+                $registration->save();
+            }
+        }
+    }
+}
