90099ee26343759f694d5891ec20a53f6d459b6f
Tony Warwick <tony@eventespresso.com>
2021-02-17 15:30:58 +0000

    Set the dateFormat to mm/dd/yy on the datepicker.

--- admin_pages/payments/assets/espresso_payments_admin.js
+++ admin_pages/payments/assets/espresso_payments_admin.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
-
 	$( '.datepicker' ).datepicker({
 		defaultDate: "-1m",
-		numberOfMonths: 2
-	});/**/
+		numberOfMonths: 2,
+		dateFormat: 'mm/dd/yy'
+	});
 });
