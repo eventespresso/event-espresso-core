420f4e72640b085d412cec751b746b6e946dbf22
Brent Christensen <brent@eventespresso.com>
2021-03-19 15:00:32 -0700

    auto-format to PSR12

--- core/db_models/EEM_Event_Question_Group.model.php
+++ core/db_models/EEM_Event_Question_Group.model.php
@@ -1,12 +1,20 @@
 <?php
+
 /**
- * model for handling has-and-belongs-to-many relatinobetween events and question groups.
- * If you want to query based on a non-primary of foreign key on this model, you can't use the EE_HABTM_Relation on Event nor or Question Group.
- * You'll need to use each's EE_Has_Many_Relation relation to this model. Eg, when querying for question groups which apply to primary attendees,
- * you'd do a query like EEM_Question_Group::instance()->get_all(array(array('Event_Question_Group.EQG_primary'=>1)));
+ * model for handling has-and-belongs-to-many relation between events and question groups.
+ * If you want to query based on a non-primary or foreign key on this model,
+ * you can't use the EE_HABTM_Relation on Event nor or Question Group.
+ * You'll need to use the EE_Has_Many_Relation for each relation to this model.
+ * for example:
+ * when querying for question groups which apply to primary attendees,
+ * you'd do a query like:
+ *      EEM_Question_Group::instance()->get_all(
+ *          [['Event_Question_Group.EQG_primary' => 1]]
+ *      );
  */
 class EEM_Event_Question_Group extends EEM_Base
 {
+
     /**
      * Name of the field indicating an event should use the question group for the primary attendee
      */
@@ -20,45 +28,86 @@ class EEM_Event_Question_Group extends EEM_Base
     // private instance of the Event_Question_Group object
     protected static $_instance = null;
 
+
     protected function __construct($timezone = null)
     {
-        $this->singular_item = __('Event to Question Group Link', 'event_espresso');
-        $this->plural_item = __('Event to Question Group Links', 'event_espresso');
-        $this->_tables = array(
-            'Event_Question_Group'=>new EE_Primary_Table('esp_event_question_group', 'EQG_ID')
-        );
-        $this->_fields = array(
-            'Event_Question_Group'=>array(
-                'EQG_ID'=>new EE_Primary_Key_Int_Field('EQG_ID', __('Event to Question Group Link ID', 'event_espresso')),
-                'EVT_ID'=>new EE_Foreign_Key_Int_Field('EVT_ID', __('Event ID', 'event_espresso'), false, 0, 'Event'),
-                'QSG_ID'=>new EE_Foreign_Key_Int_Field('QSG_ID', __('Question Group Id', 'event_espresso'), false, 0, 'Question_Group'),
-                'EQG_primary'=>new EE_Boolean_Field('EQG_primary', __('Flag indicating question is only for primary attendees', 'event_espresso'), false, false),
-                'EQG_additional'=>new EE_Boolean_Field('EQG_additional', __('Flag indicating question is only for additional attendees', 'event_espresso'), false, false)
-            )
-        );
-        $this->_model_relations = array(
-            'Event'=>new EE_Belongs_To_Relation(),
-            'Question_Group'=>new EE_Belongs_To_Relation()
-        );
+        $this->singular_item    = __('Event to Question Group Link', 'event_espresso');
+        $this->plural_item      = __('Event to Question Group Links', 'event_espresso');
+        $this->_tables          = [
+            'Event_Question_Group' => new EE_Primary_Table('esp_event_question_group', 'EQG_ID'),
+        ];
+        $this->_fields          = [
+            'Event_Question_Group' => [
+                'EQG_ID'         => new EE_Primary_Key_Int_Field(
+                    'EQG_ID',
+                    __('Event to Question Group Link ID', 'event_espresso')
+                ),
+                'EVT_ID'         => new EE_Foreign_Key_Int_Field(
+                    'EVT_ID',
+                    __('Event ID', 'event_espresso'),
+                    false,
+                    0,
+                    'Event'
+                ),
+                'QSG_ID'         => new EE_Foreign_Key_Int_Field(
+                    'QSG_ID',
+                    __('Question Group Id', 'event_espresso'),
+                    false,
+                    0,
+                    'Question_Group'
+                ),
+                'EQG_primary'    => new EE_Boolean_Field(
+                    'EQG_primary',
+                    __(
+                        'Flag indicating question is only for primary attendees',
+                        'event_espresso'
+                    ),
+                    false,
+                    false
+                ),
+                'EQG_additional' => new EE_Boolean_Field(
+                    'EQG_additional',
+                    __(
+                        'Flag indicating question is only for additional attendees',
+                        'event_espresso'
+                    ),
+                    false,
+                    false
+                ),
+            ],
+        ];
+        $this->_model_relations = [
+            'Event'          => new EE_Belongs_To_Relation(),
+            'Question_Group' => new EE_Belongs_To_Relation(),
+        ];
+
         // this model is generally available for reading
         $path_to_event = 'Event';
-        $this->_cap_restriction_generators[ EEM_Base::caps_read ] = new EE_Restriction_Generator_Event_Related_Public($path_to_event);
-        $this->_cap_restriction_generators[ EEM_Base::caps_read_admin ] = new EE_Restriction_Generator_Event_Related_Protected($path_to_event);
-        $this->_cap_restriction_generators[ EEM_Base::caps_edit ] = new EE_Restriction_Generator_Event_Related_Protected($path_to_event);
-        $this->_cap_restriction_generators[ EEM_Base::caps_delete ] = new EE_Restriction_Generator_Event_Related_Protected($path_to_event, EEM_Base::caps_edit);
+
+        $this->_cap_restriction_generators[ EEM_Base::caps_read ]       =
+            new EE_Restriction_Generator_Event_Related_Public($path_to_event);
+        $this->_cap_restriction_generators[ EEM_Base::caps_read_admin ] =
+            new EE_Restriction_Generator_Event_Related_Protected($path_to_event);
+        $this->_cap_restriction_generators[ EEM_Base::caps_edit ]       =
+            new EE_Restriction_Generator_Event_Related_Protected($path_to_event);
+        $this->_cap_restriction_generators[ EEM_Base::caps_delete ]     =
+            new EE_Restriction_Generator_Event_Related_Protected($path_to_event, EEM_Base::caps_edit);
+
         parent::__construct($timezone);
     }
+
 
     /**
      * Decides whether to use the 'EQG_primary' or newer 'EQG_additional' for use in queries, based on whether
      * this is concerning primary attendees or additional attendees.
      * If 1, true, or "primary" is passed in, returns EQG_primary. If 0, false, or "additional" is passed in, returns
      * EQG_additional.
-     * @since 4.10.0.p
+     *
      * @param string|boolean|int $context
      * @return string
+     * @since 4.10.0.p
      */
-    public function fieldNameForContext($context)
+    public function fieldNameForContext($context): string
     {
         // Basically do a strict switch statement.
         switch (true) {
