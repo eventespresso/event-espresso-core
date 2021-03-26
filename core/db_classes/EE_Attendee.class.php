670368adbdcf471bc6e99cf082db9b8d595610fe
Brent Christensen <brent@eventespresso.com>
2021-03-26 11:10:13 -0700

    add error details

--- core/db_classes/EE_Attendee.class.php
+++ core/db_classes/EE_Attendee.class.php
@@ -587,7 +587,12 @@ class EE_Attendee extends EE_CPT_Base implements EEI_Contact, EEI_Address, EEI_A
     public function save_and_clean_billing_info_for_payment_method($billing_form, $payment_method)
     {
         if (! $billing_form instanceof EE_Billing_Attendee_Info_Form) {
-            EE_Error::add_error(__('Cannot save billing info because there is none.', 'event_espresso'));
+            EE_Error::add_error(
+                __('Cannot save billing info because there is none.', 'event_espresso'),
+                __FILE__,
+                __FUNCTION__,
+                __LINE__
+            );
             return false;
         }
         $billing_form->clean_sensitive_data();
