be6f3faa12d8289a7f2a51262b34b6588f38796c
Brent Christensen <brent@eventespresso.com>
2021-05-27 16:25:00 -0700

    Reg Form Builder: Form Input Model (#3406)

--- core/db_models/fields/EE_Field_With_Model_Name.php
+++ core/db_models/fields/EE_Field_With_Model_Name.php
@@ -12,32 +12,33 @@ abstract class EE_Field_With_Model_Name extends EE_Model_Field_Base
      * Usually the name of a single model. However, as in the case for custom post types,
      * it can actually be an array of models
      *
-     * @var string or array
+     * @var string[]
      */
     protected $_model_name_pointed_to;
 
+
     /**
-     * @param string  $table_column  name fo column for field
-     * @param string  $nicename      should eb internationalized with __('blah','event_espresso')
-     * @param boolean $nullable
-     * @param mixed   $default_value if this is a integer field, it shoudl be an int. if it's a string field, it shoul
-     *                               dbe a string
-     * @param string  $model_name    eg 'Event','Answer','Term', etc. Basically its the model class's name without the
-     *                               "EEM_"
+     * @param string          $table_column  name of column for field
+     * @param string          $nicename      should be internationalized with __('blah','event_espresso')
+     * @param boolean         $nullable
+     * @param int|string      $default_value data type should match field type
+     * @param string|string[] $model_name    eg 'Event','Answer','Term', etc.
+     *                                       Basically its the model class's name without the "EEM_"
      */
     public function __construct($table_column, $nicename, $nullable, $default_value, $model_name)
     {
-        $this->_model_name_pointed_to = $model_name;
+        $this->_model_name_pointed_to = (array) $model_name;
         parent::__construct($table_column, $nicename, $nullable, $default_value);
     }
+
 
     /**
      * Returns the name of the model(s) pointed to
      *
+     * @return string[] string or array of strings
      * @deprecated since version 4.6.7
-     * @return mixed string or array of strings
      */
-    public function get_model_name_pointed_to()
+    public function get_model_name_pointed_to(): array
     {
         EE_Error::doing_it_wrong(
             'get_model_name_pointed_to',
@@ -50,43 +51,48 @@ abstract class EE_Field_With_Model_Name extends EE_Model_Field_Base
         return $this->_model_name_pointed_to;
     }
 
+
     /**
      * Gets the model names pointed to by this field, always as an array
      * (even if there's only one)
      *
-     * @return array of model names pointed to by this field
+     * @return string[] of model names pointed to by this field
      */
-    public function get_model_names_pointed_to()
+    public function get_model_names_pointed_to(): array
     {
-        if (is_array($this->_model_name_pointed_to)) {
-            return $this->_model_name_pointed_to;
-        } else {
-            return array($this->_model_name_pointed_to);
-        }
+        return is_array($this->_model_name_pointed_to)
+            ? $this->_model_name_pointed_to
+            : [$this->_model_name_pointed_to];
     }
+
 
     /**
      * Returns the model's classname (eg EE_Event instead of just Event)
      *
-     * @return array
+     * @return string[]
      */
-    public function get_model_class_names_pointed_to()
+    public function get_model_class_names_pointed_to(): array
     {
-        $model_names = array();
+        $model_names = [];
         if (is_array($this->_model_name_pointed_to)) {
             foreach ($this->_model_name_pointed_to as $model_name) {
                 $model_names[] = "EE_" . $model_name;
             }
         } else {
-            $model_names = array("EE_" . $this->_model_name_pointed_to);
+            $model_names = ["EE_" . $this->_model_name_pointed_to];
         }
         return $model_names;
     }
 
-    public function is_model_obj_of_type_pointed_to($model_obj_or_ID)
+
+    /**
+     * @param int|EE_Base_Class $model_obj
+     * @return bool
+     */
+    public function is_model_obj_of_type_pointed_to($model_obj): bool
     {
         foreach ($this->get_model_class_names_pointed_to() as $model_obj_classname) {
-            if ($model_obj_or_ID instanceof $model_obj_classname) {
+            if ($model_obj instanceof $model_obj_classname) {
                 return true;
             }
         }
