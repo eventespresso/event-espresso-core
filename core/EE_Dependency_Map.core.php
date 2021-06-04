5502d633ef54c46afa78eff37ee3e1b4d969a176
Brent Christensen <brent@eventespresso.com>
2021-06-04 08:39:18 -0700

    Extract Attendee Info Reg Form Processing into New Classes (#3419)

--- core/EE_Dependency_Map.core.php
+++ core/EE_Dependency_Map.core.php
@@ -802,31 +802,8 @@ class EE_Dependency_Map
                 'EventEspresso\core\services\form\meta\inputs\Select'   => EE_Dependency_Map::load_from_cache,
                 'EventEspresso\core\services\form\meta\inputs\Text'     => EE_Dependency_Map::load_from_cache,
             ],
-            'EventEspresso\core\domain\services\registration\form\v1\RegForm' => [
-                null,
-                'EE_Registration_Config' => EE_Dependency_Map::load_from_cache,
-            ],
-            'EventEspresso\core\domain\services\registration\form\v1\RegistrationForm' => [
-                null,
-                null,
-                null,
-                null,
-                'EEM_Event_Question_Group' => EE_Dependency_Map::load_from_cache,
-            ],
-            'EventEspresso\core\domain\services\registration\form\v1\QuestionGroupRegForm' => [
-                null,
-                null,
-                null,
-                'EventEspresso\core\domain\services\registration\form\v1\RegFormQuestionFactory' => EE_Dependency_Map::load_from_cache,
-            ],
-            'EventEspresso\core\domain\services\registration\form\v1\CountryOptions' => [
-                null,
-                'EEM_Answer' => EE_Dependency_Map::load_from_cache,
-                'EEM_Country' => EE_Dependency_Map::load_from_cache,
-            ],
-            'EventEspresso\core\domain\services\registration\form\v1\StateOptions' => [
-                null,
-                'EEM_State' => EE_Dependency_Map::load_from_cache,
+            'EventEspresso\core\domain\services\registration\form\v1\RegFormDependencyHandler' => [
+                'EE_Dependency_Map' => EE_Dependency_Map::load_from_cache,
             ],
         ];
     }
