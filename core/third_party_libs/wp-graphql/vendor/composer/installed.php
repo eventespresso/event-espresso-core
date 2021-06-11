b093f0efb369a843e6843157de27bb2c5f104a75
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-06-11 21:10:21 +0530

    Update WP GraphQL to the latest version (#3425)

--- core/third_party_libs/wp-graphql/vendor/composer/installed.php
+++ core/third_party_libs/wp-graphql/vendor/composer/installed.php
@@ -1,42 +1,41 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'v1.3.5',
-    'version' => '1.3.5.0',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v1.3.10',
+        'version' => '1.3.10.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6cdf0c599493e83575055b22cfd0447253e19e75',
+        'name' => 'wp-graphql/wp-graphql',
+        'dev' => false,
     ),
-    'reference' => 'b2471c50f338080ea10c8f2f519cf141b32273b1',
-    'name' => 'wp-graphql/wp-graphql',
-  ),
-  'versions' => 
-  array (
-    'ivome/graphql-relay-php' => 
-    array (
-      'pretty_version' => 'v0.5.0',
-      'version' => '0.5.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'b227c17e79564d147db2162329f5bdbe19192313',
+    'versions' => array(
+        'ivome/graphql-relay-php' => array(
+            'pretty_version' => 'v0.5.0',
+            'version' => '0.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ivome/graphql-relay-php',
+            'aliases' => array(),
+            'reference' => 'b227c17e79564d147db2162329f5bdbe19192313',
+            'dev_requirement' => false,
+        ),
+        'webonyx/graphql-php' => array(
+            'pretty_version' => 'v14.4.0',
+            'version' => '14.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webonyx/graphql-php',
+            'aliases' => array(),
+            'reference' => 'aab3d49181467db064b41429cde117a7589625fc',
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wp-graphql' => array(
+            'pretty_version' => 'v1.3.10',
+            'version' => '1.3.10.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6cdf0c599493e83575055b22cfd0447253e19e75',
+            'dev_requirement' => false,
+        ),
     ),
-    'webonyx/graphql-php' => 
-    array (
-      'pretty_version' => 'v14.4.0',
-      'version' => '14.4.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'aab3d49181467db064b41429cde117a7589625fc',
-    ),
-    'wp-graphql/wp-graphql' => 
-    array (
-      'pretty_version' => 'v1.3.5',
-      'version' => '1.3.5.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'b2471c50f338080ea10c8f2f519cf141b32273b1',
-    ),
-  ),
 );
