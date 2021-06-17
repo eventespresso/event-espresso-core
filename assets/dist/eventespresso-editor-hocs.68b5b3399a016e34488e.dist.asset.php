a13b120db0694abebbe28b8aa4c08c7467dae38c
Brent Christensen <brent@eventespresso.com>
2021-06-17 08:24:38 -0700

    rebuild assets

--- assets/dist/eventespresso-editor-hocs.68b5b3399a016e34488e.dist.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-data'), 'version' => '68b5b3399a016e34488e6d2106fc07df');
--- assets/dist/eventespresso-helpers.b37cef99829de4d21578.dist.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('eejs-core', 'eventespresso-vendor', 'lodash'), 'version' => 'b37cef99829de4d215786f32a34934b5');
