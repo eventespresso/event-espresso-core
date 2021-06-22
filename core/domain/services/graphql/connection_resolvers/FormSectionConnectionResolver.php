54a29501399bdad857f3529e351694df3e144a9e
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-06-23 02:25:04 +0530

    Form Builder | Add GraphQL schema for Form Section queries (#3434)
    Co-authored-by: Brent Christensen <brent@eventespresso.com>

---
+++ core/domain/services/graphql/connection_resolvers/FormSectionConnectionResolver.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\connection_resolvers;
+
+use EE_Error;
+use EEM_Form_Section;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use InvalidArgumentException;
+use ReflectionException;
+
+/**
+ * Class FormSectionConnectionResolver
+ */
+class FormSectionConnectionResolver extends AbstractConnectionResolver
+{
+    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function get_loader_name()
+    {
+        return 'espresso_formSection';
+    }
+
+    /**
+     * @return EEM_Form_Section
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
+    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function get_query()
+    {
+        return EEM_Form_Section::instance();
+    }
+
+
+    /**
+     * Return an array of item IDs from the query
+     *
+     * @return array
+     */
+    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function get_ids()
+    {
+        $results = $this->query->get_col($this->query_args);
+
+        return ! empty($results) ? $results : [];
+    }
+
+
+    /**
+     * Here, we map the args from the input, then we make sure that we're only querying
+     * for IDs. The IDs are then passed down the resolve tree, and deferred resolvers
+     * handle batch resolution of the posts.
+     *
+     * @return array
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
+    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function get_query_args()
+    {
+        $where_params = [];
+        $query_args   = [];
+
+        $query_args['limit'] = $this->getLimit();
+
+        // Avoid multiple entries by join.
+        $query_args['group_by'] = 'FSC_UUID';
+
+        $where_params = apply_filters(
+            'FHEE__EventEspresso_core_domain_services_graphql_connection_resolvers__form_section_where_params',
+            $where_params,
+            $this->source,
+            $this->args
+        );
+
+        if (! empty($where_params)) {
+            $query_args[] = $where_params;
+        }
+
+
+        /**
+         * Return the $query_args
+         */
+        return apply_filters(
+            'FHEE__EventEspresso_core_domain_services_graphql_connection_resolvers__form_section_query_args',
+            $query_args,
+            $this->source,
+            $this->args
+        );
+    }
+}
---
+++ core/domain/services/graphql/types/FormSection.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\types;
+
+use EEM_Form_Section;
+use EventEspresso\core\services\graphql\fields\GraphQLFieldInterface;
+use EventEspresso\core\services\graphql\types\TypeBase;
+use EventEspresso\core\services\graphql\fields\GraphQLField;
+use EventEspresso\core\services\graphql\fields\GraphQLOutputField;
+
+/**
+ * Class FormSection
+ * Description
+ *
+ * @package EventEspresso\core\domain\services\graphql\types
+ * @author  Manzoor Wani
+ * @since   $VID:$
+ */
+class FormSection extends TypeBase
+{
+
+    /**
+     * FormSection constructor.
+     *
+     * @param EEM_Form_Section $form_section_model
+     */
+    public function __construct(EEM_Form_Section $form_section_model)
+    {
+        $this->model = $form_section_model;
+        $this->setName($this->namespace . 'FormSection');
+        $this->setDescription(__('A form section', 'event_espresso'));
+        $this->setIsCustomPostType(false);
+
+        parent::__construct();
+    }
+
+
+    /**
+     * @return GraphQLFieldInterface[]
+     * @since $VID:$
+     */
+    public function getFields()
+    {
+        $fields = [
+            new GraphQLField(
+                'id',
+                ['non_null' => 'ID'],
+                null,
+                esc_html__('The globally unique ID for the object.', 'event_espresso')
+            ),
+            new GraphQLOutputField(
+                'dbId',
+                ['non_null' => 'Int'],
+                'ID',
+                esc_html__('Form section ID', 'event_espresso')
+            ),
+            new GraphQLField(
+                'appliesTo',
+                $this->namespace . 'FormSectionAppliesToEnum',
+                'appliesTo',
+                esc_html__('Form user type that this form section should be presented to.', 'event_espresso')
+            ),
+            /* new GraphQLField(
+                'adminLabel',
+                'String',
+                'adminLabel',
+                esc_html__('The form section label that should be show to the admins.', 'event_espresso')
+            ), */
+            new GraphQLField(
+                'belongsTo',
+                'String',
+                'belongsTo',
+                esc_html__('UUID or ID of related entity this form section belongs to.', 'event_espresso')
+            ),
+            /* new GraphQLField(
+                'description',
+                'String',
+                'description',
+                esc_html__('Description of the form section.', 'event_espresso')
+            ), */
+            new GraphQLField(
+                'htmlClass',
+                'String',
+                'htmlClass',
+                esc_html__('HTML classes to be applied to this form section\'s container.', 'event_espresso')
+            ),
+            new GraphQLField(
+                'order',
+                'Int',
+                'order',
+                esc_html__('Order in which form section appears in a form.', 'event_espresso')
+            ),
+            /* new GraphQLField(
+                'showDescription',
+                'Boolean',
+                'showDescription',
+                esc_html__('Whether to display description of the form section.', 'event_espresso')
+            ),
+            new GraphQLField(
+                'showName',
+                'Boolean',
+                'showName',
+                esc_html__('Whether to display the admin label to non-admin users.', 'event_espresso')
+            ), */
+            new GraphQLField(
+                'status',
+                $this->namespace . 'FormSectionStatusEnum',
+                'status',
+                esc_html__(
+                    'Whether form section is active, archived, shared, trashed, or used as a default on new forms.',
+                    'event_espresso'
+                )
+            ),
+            new GraphQLOutputField(
+                'wpUser',
+                'User',
+                null,
+                esc_html__('WP User that created this form section.', 'event_espresso')
+            ),
+        ];
+
+        return apply_filters(
+            'FHEE__EventEspresso_core_domain_services_graphql_types__form_section_fields',
+            $fields,
+            $this->name,
+            $this->model
+        );
+    }
+}
