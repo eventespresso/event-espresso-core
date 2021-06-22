ed51ba60d3ea19d2c1ecf7fe9c7e890ae950ec5f
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-06-23 02:25:04 +0530

    Form Builder | Add GraphQL schema for Form Section queries (#3434)
    Co-authored-by: Brent Christensen <brent@eventespresso.com>

--- core/domain/entities/routing/handlers/shared/GQLRequests.php
+++ core/domain/entities/routing/handlers/shared/GQLRequests.php
@@ -136,6 +136,10 @@ class GQLRequests extends Route
             ['EEM_Event' => EE_Dependency_Map::load_from_cache]
         );
         $this->dependency_map->registerDependencies(
+            'EventEspresso\core\domain\services\graphql\types\FormSection',
+            ['EEM_Form_Section' => EE_Dependency_Map::load_from_cache]
+        );
+        $this->dependency_map->registerDependencies(
             'EventEspresso\core\domain\services\graphql\types\Ticket',
             ['EEM_Ticket' => EE_Dependency_Map::load_from_cache]
         );
@@ -170,6 +174,10 @@ class GQLRequests extends Route
         $this->dependency_map->registerDependencies(
             'EventEspresso\core\domain\services\graphql\connections\RootQueryAttendeesConnection',
             ['EEM_Attendee' => EE_Dependency_Map::load_from_cache]
+        );
+        $this->dependency_map->registerDependencies(
+            'EventEspresso\core\domain\services\graphql\connections\RootQueryFormSectionsConnection',
+            ['EEM_Form_Section' => EE_Dependency_Map::load_from_cache]
         );
         $this->dependency_map->registerDependencies(
             'EventEspresso\core\domain\services\graphql\connections\DatetimeTicketsConnection',
