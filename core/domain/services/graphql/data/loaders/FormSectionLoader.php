d101f5400fd2f5a1e5f0afab1c262c2f8f3586a7
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-06-22 18:58:33 +0530

    Add loader and update field resolver

---
+++ core/domain/services/graphql/data/loaders/FormSectionLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\data\loaders;
+
+use EE_Error;
+use EEM_Base;
+use EEM_Form_Section;
+use EventEspresso\core\exceptions\InvalidDataTypeException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use InvalidArgumentException;
+
+/**
+ * Class FormSectionLoader
+ */
+class FormSectionLoader extends AbstractLoader
+{
+    /**
+     * @return EEM_Base
+     * @throws EE_Error
+     * @throws InvalidArgumentException
+     * @throws InvalidDataTypeException
+     * @throws InvalidInterfaceException
+     */
+    protected function getQuery(): EEM_Base
+    {
+        return EEM_Form_Section::instance();
+    }
+
+    /**
+     * @param array $keys
+     * @return array
+     */
+    protected function getWhereParams(array $keys): array
+    {
+        return [
+            'FSC_UUID' => ['IN', $keys],
+        ];
+    }
+}
