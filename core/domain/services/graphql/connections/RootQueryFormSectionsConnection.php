d552dbc16ca1e6d716123bca8331fb8a045e4e09
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-06-24 18:17:47 +0530

    Form Builder | Add form section query args to GQL

--- core/domain/services/graphql/connections/RootQueryFormSectionsConnection.php
+++ core/domain/services/graphql/connections/RootQueryFormSectionsConnection.php
@@ -41,6 +41,7 @@ class RootQueryFormSectionsConnection extends AbstractRootQueryConnection
             'toType'             => $this->namespace . 'FormSection',
             'fromFieldName'      => lcfirst($this->namespace) . 'FormSections',
             'connectionTypeName' => "{$this->namespace}RootQueryFormSectionsConnection",
+            'connectionArgs'     => $this->get_connection_args(),
             'resolve'            => [$this, 'resolveConnection'],
         ];
     }
@@ -57,5 +58,49 @@ class RootQueryFormSectionsConnection extends AbstractRootQueryConnection
     public function getConnectionResolver($entity, $args, $context, $info): AbstractConnectionResolver
     {
         return new FormSectionConnectionResolver($entity, $args, $context, $info);
+    }
+
+    /**
+     * Given an optional array of args, this returns the args to be used in the connection
+     *
+     * @param array $args The args to modify the defaults
+     * @return array
+     */
+    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function get_connection_args(array $args = []): array
+    {
+        $newArgs = [
+            'appliesTo' => [
+                'type'        => ['list_of' => $this->namespace . 'FormSectionAppliesToEnum'],
+                'description' => esc_html__(
+                    'Form user types to get the form sections for.',
+                    'event_espresso'
+                ),
+            ],
+            'belongsTo' => [
+                'type'        => ['list_of' => 'ID'],
+                'description' => esc_html__(
+                    'Related entity IDs to get the form sections for.',
+                    'event_espresso'
+                ),
+            ],
+            'status' => [
+                'type'        => ['list_of' => $this->namespace . 'FormSectionStatusEnum'],
+                'description' => esc_html__(
+                    'Filter the form sections by status.',
+                    'event_espresso'
+                ),
+            ],
+        ];
+
+        $newArgs = apply_filters(
+            'FHEE__EventEspresso_core_domain_services_graphql_connections__form_section_args',
+            $newArgs,
+            $args
+        );
+        return array_merge(
+            $newArgs,
+            $args
+        );
     }
 }
