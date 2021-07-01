c00fd41117daed8ea0433bf149ba41d820734282
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-07-01 15:38:45 +0530

    Form builder | EDTR Rehydration (#3451)
    * Fix registrationForm for EE_Event
    
    * Fix string foreign key UPPERCASE issue
    
    * Fix input options for form meta
    
    * Add Form Builder rehydration data for EDTR
    
    * types, you know
    
    * Add PR checks
    
    * Use sanitize_key
    
    * Fix lints
    
    * Fix more lints
    
    * Confusing creature called PHP lints
    
    * Fix PHP unit tests
    
    * Update EEM_Form_Element.model.php

--- core/db_models/fields/EE_Foreign_Key_String_Field.php
+++ core/db_models/fields/EE_Foreign_Key_String_Field.php
@@ -2,6 +2,28 @@
 
 class EE_Foreign_Key_String_Field extends EE_Foreign_Key_Field_Base
 {
+    /**
+     * Whether the value should be converted to uppercase on insertion.
+     *
+     * @var boolean
+     */
+    protected $is_uppercase;
+
+
+    /**
+     * @param string          $table_column  name of column for field
+     * @param string          $nicename      should be internationalized with __('blah','event_espresso')
+     * @param boolean         $nullable
+     * @param int|string      $default_value data type should match field type
+     * @param string|string[] $model_name    eg 'Event','Answer','Term', etc.
+     *                                       Basically its the model class's name without the "EEM_"
+     * @param boolean         $is_uppercase  Whether the value should be converted to uppercase on insertion.
+     */
+    public function __construct($table_column, $nicename, $nullable, $default_value, $model_name, $is_uppercase = true)
+    {
+        $this->is_uppercase = $is_uppercase;
+        parent::__construct($table_column, $nicename, $nullable, $default_value, $model_name);
+    }
 
     /**
      * removes all tags when setting
@@ -14,6 +36,7 @@ class EE_Foreign_Key_String_Field extends EE_Foreign_Key_Field_Base
         if ($this->is_model_obj_of_type_pointed_to($value_inputted_for_field_on_model_object)) {
             $value_inputted_for_field_on_model_object = $value_inputted_for_field_on_model_object->ID();
         }
-        return strtoupper(wp_strip_all_tags($value_inputted_for_field_on_model_object));
+        $clean_value = wp_strip_all_tags($value_inputted_for_field_on_model_object);
+        return $this->is_uppercase ? strtoupper($clean_value) : $clean_value;
     }
 }
