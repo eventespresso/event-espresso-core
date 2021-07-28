ad43411e620c79ce2b84930ea7ac4671f3351481
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-07-29 04:09:13 +0530

    Enhance Country and State GraphQL schema (#3483)

---
+++ core/domain/services/graphql/inputs/StatesConnectionOrderbyInput.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\inputs;
+
+use EventEspresso\core\services\graphql\fields\GraphQLFieldInterface;
+use EventEspresso\core\services\graphql\inputs\InputBase;
+use EventEspresso\core\services\graphql\fields\GraphQLField;
+
+/**
+ * Class StatesConnectionOrderbyInput
+ * Description
+ *
+ * @package EventEspresso\core\domain\services\graphql\inputs
+ * @author  Manzoor Wani
+ * @since   $VID:$
+ */
+class StatesConnectionOrderbyInput extends InputBase
+{
+
+    /**
+     * StatesConnectionOrderbyInput constructor.
+     */
+    public function __construct()
+    {
+        $this->setName($this->namespace . 'StatesConnectionOrderbyInput');
+        $this->setDescription(esc_html__('Options for ordering the connection', 'event_espresso'));
+        parent::__construct();
+    }
+
+
+    /**
+     * @return GraphQLFieldInterface[]
+     */
+    protected function getFields(): array
+    {
+        return [
+            new GraphQLField(
+                'field',
+                ['non_null' => $this->namespace . 'StatesConnectionOrderbyEnum']
+            ),
+            new GraphQLField(
+                'order',
+                'OrderEnum'
+            ),
+        ];
+    }
+}
