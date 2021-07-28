bc2a703ed9dfe8c185eb84e34bc733110c022e71
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-07-28 21:34:15 +0530

    Add countries list query to RootQuery

---
+++ core/domain/services/graphql/inputs/CountriesConnectionOrderbyInput.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\inputs;
+
+use EventEspresso\core\services\graphql\fields\GraphQLFieldInterface;
+use EventEspresso\core\services\graphql\inputs\InputBase;
+use EventEspresso\core\services\graphql\fields\GraphQLField;
+
+/**
+ * Class CountriesConnectionOrderbyInput
+ * Description
+ *
+ * @package EventEspresso\core\domain\services\graphql\inputs
+ * @author  Manzoor Wani
+ * @since   $VID:$
+ */
+class CountriesConnectionOrderbyInput extends InputBase
+{
+
+    /**
+     * CountriesConnectionOrderbyInput constructor.
+     */
+    public function __construct()
+    {
+        $this->setName($this->namespace . 'CountriesConnectionOrderbyInput');
+        $this->setDescription(esc_html__('Options for ordering the connection', 'event_espresso'));
+        parent::__construct();
+    }
+
+
+    /**
+     * @return GraphQLFieldInterface[]
+     */
+    protected function getFields(): array
+    {
+        return [
+            new GraphQLField(
+                'field',
+                ['non_null' => $this->namespace . 'CountriesConnectionOrderbyEnum']
+            ),
+            new GraphQLField(
+                'order',
+                'OrderEnum'
+            ),
+        ];
+    }
+}
