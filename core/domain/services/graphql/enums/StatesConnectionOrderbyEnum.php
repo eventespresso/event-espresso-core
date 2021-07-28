b8e56e4759b994aa6501855dbcc2a037a63e061a
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-07-28 21:34:31 +0530

    Add states list query to RootQuery

---
+++ core/domain/services/graphql/enums/StatesConnectionOrderbyEnum.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace EventEspresso\core\domain\services\graphql\enums;
+
+use EventEspresso\core\services\graphql\enums\EnumBase;
+
+/**
+ * Class StatesConnectionOrderbyEnum
+ * Description
+ *
+ * @package EventEspresso\core\domain\services\graphql\enums
+ * @author  Manzoor Wani
+ * @since   $VID:$
+ */
+class StatesConnectionOrderbyEnum extends EnumBase
+{
+
+    /**
+     * StatesConnectionOrderbyEnum constructor.
+     */
+    public function __construct()
+    {
+        $this->setName($this->namespace . 'StatesConnectionOrderbyEnum');
+        $this->setDescription(esc_html__('Field to order the connection by', 'event_espresso'));
+        parent::__construct();
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function getValues(): array
+    {
+        return [
+            'NAME'     => [
+                'value'       => 'CNT_name',
+                'description' => esc_html__('Order by state name', 'event_espresso'),
+            ],
+            'COUNRY_ISO'     => [
+                'value'       => 'CNT_ISO',
+                'description' => esc_html__('Order by country ISO', 'event_espresso'),
+            ],
+        ];
+    }
+}
