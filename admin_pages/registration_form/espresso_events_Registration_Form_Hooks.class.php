40c0bc30ff27ebbed6dcb4e8f1b28c63f75eafa3
Brent Christensen <brent@eventespresso.com>
2021-08-12 11:28:38 -0700

    fix escaping

--- admin_pages/registration_form/espresso_events_Registration_Form_Hooks.class.php
+++ admin_pages/registration_form/espresso_events_Registration_Form_Hooks.class.php
@@ -24,27 +24,27 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
     protected function _set_hooks_properties()
     {
 
-        $this->_name = 'registration_form';
-        $this->_metaboxes = array(
-            0 => array(
-                'page_route' => array('edit', 'create_new'),
+        $this->_name      = 'registration_form';
+        $this->_metaboxes = [
+            0 => [
+                'page_route' => ['edit', 'create_new'],
                 'func'       => 'primary_questions',
                 'label'      => esc_html__('Questions for Primary Registrant', 'event_espresso'),
                 'priority'   => 'default',
                 'context'    => 'side',
-            ),
-        );
+            ],
+        ];
 
         // hook into the handler for saving question groups
         add_filter(
             'FHEE__Events_Admin_Page___insert_update_cpt_item__event_update_callbacks',
-            array($this, 'modify_callbacks'),
+            [$this, 'modify_callbacks'],
             10
         );
 
         // hook into revision restores (we're hooking into the global action because EE_Admin_Hooks classes are already
         // restricted by page)
-        add_action('AHEE_EE_Admin_Page_CPT__restore_revision', array($this, 'restore_revision'), 10, 2);
+        add_action('AHEE_EE_Admin_Page_CPT__restore_revision', [$this, 'restore_revision'], 10, 2);
     }
 
 
@@ -57,7 +57,7 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
     public function modify_callbacks($callbacks)
     {
         // now let's add the question group callback
-        $callbacks[] = array($this, 'primary_question_group_update');
+        $callbacks[] = [$this, 'primary_question_group_update'];
         return $callbacks;
     }
 
@@ -70,20 +70,20 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
      * @return EE_Base_Class
      * @throws EE_Error
      * @throws InvalidArgumentException
-     * @throws ReflectionException
      * @throws InvalidDataTypeException
      * @throws InvalidInterfaceException
      */
     public function restore_revision($post_id, $revision_id)
     {
-        $EVT_MDL = EE_Registry::instance()->load_model('Event');
-        $post_evt = $EVT_MDL->get_one_by_ID($post_id);
+        $post_evt = EEM_Event::instance()->get_one_by_ID($post_id);
         // restore revision for primary questions
-        $post_evt->restore_revision(
-            $revision_id,
-            ['Question_Group'],
-            ['Question_Group' => ['Event_Question_Group.EQG_primary' => true]]
-        );
+        if ($post_evt instanceof EE_Event) {
+            $post_evt->restore_revision(
+                $revision_id,
+                ['Question_Group'],
+                ['Question_Group' => ['Event_Question_Group.EQG_primary' => true]]
+            );
+        }
         return $post_evt;
     }
 
@@ -97,16 +97,19 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
      * @throws InvalidArgumentException
      * @throws InvalidDataTypeException
      * @throws InvalidInterfaceException
+     * @throws ReflectionException
      */
     public function primary_questions($post_id, $post)
     {
         $this->_event = $this->_adminpage_obj->get_event_object();
-        $event_id = $this->_event->ID();
+        $event_id     = $this->_event->ID();
         ?>
         <div class="inside">
-            <p><strong>
+            <p>
+                <strong>
                     <?php esc_html_e('Question Groups', 'event_espresso'); ?>
-                </strong><br/>
+                </strong>
+                <br />
                 <?php
                 printf(
                     esc_html__(
@@ -121,54 +124,63 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
             <?php
 
             $qsg_where['QSG_deleted'] = false;
-            $query_params = apply_filters(
+            $query_params             = apply_filters(
                 'FHEE__espresso_events_Registration_Form_Hooks__primary_questions__question_group_query_parameters',
-                array($qsg_where, 'order_by' => array('QSG_order' => 'ASC'))
+                [$qsg_where, 'order_by' => ['QSG_order' => 'ASC']]
             );
-            $QSGs = EEM_Question_Group::instance()->get_all($query_params);
-            $EQGs = ! empty($event_id)
+            $QSGs                     = EEM_Question_Group::instance()->get_all($query_params);
+            $EQGs                     = ! empty($event_id)
                 ? $this->_event->get_many_related(
                     'Question_Group',
                     [['Event_Question_Group.EQG_primary' => true]]
                 )
-                : array();
-            $EQGids = array_keys($EQGs);
+                : [];
+            $EQGids                   = array_keys($EQGs);
 
             if (! empty($QSGs)) {
                 $html = count($QSGs) > 10 ? '<div style="height:250px;overflow:auto;">' : '';
                 foreach ($QSGs as $QSG) {
-                    $checked = in_array($QSG->ID(), $EQGids, true)
-                               || $QSG->get('QSG_system') === 1
+                    $QSG_ID          = absint($QSG->ID());
+                    $checked         = in_array($QSG_ID, $EQGids, true) || $QSG->get('QSG_system') === 1
                         ? ' checked="checked"'
                         : '';
-                    $visibility = $QSG->get('QSG_system') === 1
+                    $visibility      = $QSG->get('QSG_system') === 1
                         ? ' style="visibility:hidden"'
                         : '';
-                    $edit_query_args = $this->_adminpage_obj->is_caf() ? array(
-                        'action' => 'edit_question_group',
-                        'QSG_ID' => $QSG->ID(),
-                    ) : array('action' => 'question_groups');
-                    $edit_link = EE_Admin_Page::add_query_args_and_nonce(
+                    $edit_query_args = $this->_adminpage_obj->is_caf()
+                        ? [
+                            'action' => 'edit_question_group',
+                            'QSG_ID' => $QSG_ID,
+                        ]
+                        : ['action' => 'question_groups'];
+                    $edit_link       = EE_Admin_Page::add_query_args_and_nonce(
                         $edit_query_args,
                         EE_FORMS_ADMIN_URL
                     );
+                    $edit_link_title = sprintf(
+                        esc_attr__('Edit %s Group', 'event_espresso'),
+                        $QSG->get('QSG_name')
+                    );
 
                     $html .= '
-					<p id="event-question-group-' . $QSG->ID() . '">
-						<input value="' . $QSG->ID() . '" type="checkbox"'
-                             . $visibility
-                             . ' name="question_groups[' . $QSG->ID() . ']"' . $checked . ' />
-						<a href="' . $edit_link . '" title="'
-                             . sprintf(
-                                 esc_attr__('Edit %s Group', 'event_espresso'),
-                                 $QSG->get('QSG_name')
-                             )
-                             . '" target="_blank">' . $QSG->get('QSG_name') . '</a>
+					<p id="event-question-group-' . $QSG_ID . '">
+						<input value="' . $QSG_ID . '" 
+						    type="checkbox"
+						    name="question_groups[' . $QSG_ID . ']" '
+                             . esc_attr($visibility)
+                             . esc_attr($checked) . ' 
+                         />
+						<a href="' . esc_url_raw($edit_link) . '" 
+						    title="' . esc_attr($edit_link_title) . '" 
+						    target="_blank"
+						    >
+						    ' . $QSG->get('QSG_name') . '
+                        </a>
 					</p>';
                 }
                 $html .= count($QSGs) > 10 ? '</div>' : '';
 
-                echo esc_html($html);
+                echo $html; // already escaped
             } else {
                 esc_html_e(
                     'There seems to be a problem with your questions. Please contact support@eventespresso.com',
@@ -183,19 +195,20 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
 
 
     /**
-     * @param EE_Event $evtobj
+     * @param EE_Event $event
      * @param array    $data
      * @return bool
      * @throws EE_Error
+     * @throws ReflectionException
      */
-    public function primary_question_group_update($evtobj, $data)
+    public function primary_question_group_update($event, $data)
     {
-        $question_groups = ! empty($data['question_groups']) ? (array) $data['question_groups'] : array();
-        $added_qgs = array_keys($question_groups);
-        $success = array();
+        $question_groups = ! empty($data['question_groups']) ? (array) $data['question_groups'] : [];
+        $added_qgs       = array_keys($question_groups);
+        $success         = true;
 
         // let's get all current question groups associated with this event.
-        $current_qgs = $evtobj->get_many_related(
+        $current_qgs = $event->get_many_related(
             'Question_Group',
             [['Event_Question_Group.EQG_primary' => true]]
         );
@@ -203,22 +216,24 @@ class espresso_events_Registration_Form_Hooks extends EE_Admin_Hooks
 
         // now let's get the groups selected in the editor and update (IF we have data)
         if (! empty($question_groups)) {
-            foreach ($question_groups as $id => $val) {
+            foreach ($question_groups as $QSG_ID => $val) {
                 // add to event
                 if ($val) {
-                    $qg = $evtobj->_add_relation_to($id, 'Question_Group', ['EQG_primary' => true]);
+                    $qg = $event->_add_relation_to($QSG_ID, 'Question_Group', ['EQG_primary' => true]);
                 }
-                $success[] = ! empty($qg) ? 1 : 0;
+                // trip success to false if result is empty
+                $success = ! empty($qg) ? $success : false;
             }
         }
 
         // wait a minute... are there question groups missing in the saved groups that ARE with the current event?
         $removed_qgs = array_diff($current_qgs, $added_qgs);
 
-        foreach ($removed_qgs as $qgid) {
-            $qg = $evtobj->_remove_relation_to($qgid, 'Question_Group', ['EQG_primary' => true]);
-            $success[] = ! empty($qg) ? 1 : 0;
+        foreach ($removed_qgs as $QSG_ID) {
+            $qg = $event->_remove_relation_to($QSG_ID, 'Question_Group', ['EQG_primary' => true]);
+            // trip success to false if result is empty
+            $success = ! empty($qg) ? $success : false;
         }
-        return in_array(0, $success, true) ? false : true;
+        return $success;
     }
 }
