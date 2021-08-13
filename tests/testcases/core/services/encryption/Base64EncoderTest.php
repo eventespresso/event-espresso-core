5ab2a7d623b7126ae55ff2a96f89444e273d53dd
Brent Christensen <brent@eventespresso.com>
2021-08-13 15:34:39 -0700

    Base64EncoderTest

---
+++ tests/testcases/core/services/encryption/Base64EncoderTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace EventEspresso\tests\testcases\core\services\encryption;
+
+use EventEspresso\core\services\encryption\Base64Encoder;
+use EventEspresso\tests\includes\RandomDataGenerator;
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_AssertionFailedError;
+use RuntimeException;
+
+/**
+ * tests/testcases/core/services/encryption/Base64EncoderTest.php
+ */
+class Base64EncoderTest extends TestCase
+{
+    /**
+     * @var Base64Encoder
+     */
+    protected $b64;
+
+    /**
+     * @var RandomDataGenerator $rdg
+     */
+    protected $rdg;
+
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->b64 = new Base64Encoder;
+        $this->rdg = new RandomDataGenerator();
+    }
+
+
+    public function validBase64DataProvider()
+    {
+        $this->rdg = new RandomDataGenerator();
+        return [
+            [$this->rdg->getString()],
+            [$this->rdg->generateRandomString(mt_rand(5, 100))],
+            [$this->rdg->generateRandomString(mt_rand(5, 100))],
+            [$this->rdg->generateRandomString(mt_rand(5, 100), true, true)],
+            [$this->rdg->generateRandomString(mt_rand(5, 100), false, true)],
+            [$this->rdg->generateRandomString(mt_rand(5, 100), true, true, true)],
+            [$this->rdg->generateRandomString(mt_rand(5, 100), false, true, true)],
+            [$this->rdg->generateRandomString(mt_rand(5, 100), true, false, true)],
+            [$this->rdg->generateRandomString(mt_rand(5, 100), false, false, true)],
+        ];
+    }
+
+    /**
+     * @dataProvider validBase64DataProvider
+     * @throws PHPUnit_Framework_AssertionFailedError
+     * @throws AssertionFailedError
+     * @throws AssertionFailedError
+     */
+    public function testValidBase64($string)
+    {
+        $this->assertTrue(
+            $this->b64->isValidBase64(
+                $this->b64->encodeString(
+                    $string
+                )
+            )
+        );
+        $this->assertFalse(
+            $this->b64->isValidBase64(
+                $string . "!@#$%"
+            )
+        );
+    }
+
+    /**
+     * @dataProvider validBase64DataProvider
+     * @return void
+     * @throws RuntimeException
+     */
+    public function testBase64StringEncoding($string)
+    {
+        $this->assertEquals(
+            $string,
+            $this->b64->decodeString(
+                $this->b64->encodeString(
+                    $string
+                )
+            )
+        );
+    }
+
+    public function base64UrlDataProvider()
+    {
+        $this->rdg = new RandomDataGenerator();
+        return [
+            ['https://eventespresso.com'],
+            [$this->rdg->generateRandomURL()],
+            [$this->rdg->generateRandomURL()],
+            [$this->rdg->generateRandomURL()],
+            [$this->rdg->generateRandomURL(true)],
+            [$this->rdg->generateRandomURL(true)],
+            [$this->rdg->generateRandomURL(true)],
+        ];
+    }
+
+
+    /**
+     * @dataProvider base64UrlDataProvider
+     * @return void
+     * @throws RuntimeException
+     */
+    public function testBase64UrlEncoding($url)
+    {
+        $this->assertEquals(
+            $url,
+            $this->b64->decodeUrl(
+                $this->b64->encodeUrl($url)
+            )
+        );
+    }
+}
