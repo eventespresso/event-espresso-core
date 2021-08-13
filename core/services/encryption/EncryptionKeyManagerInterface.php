7174950a863f6009c07cc4814ff2bd2df2e31599
Brent Christensen <brent@eventespresso.com>
2021-08-12 22:08:52 -0700

    EncryptionKeyManagerInterface

---
+++ core/services/encryption/EncryptionKeyManagerInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace EventEspresso\core\services\encryption;
+
+/**
+ * Class EncryptionKeyManager
+ * for storing and retrieving keys used by encryption methods
+ *
+ * @author  Brent Christensen
+ * @package EventEspresso\core\services\encryption
+ * @since   $VID:$
+ */
+interface EncryptionKeyManagerInterface
+{
+    /**
+     * add an encryption key
+     *
+     * @param string $encryption_key_identifier - name of the encryption key to use
+     * @param string $encryption_key            - cryptographically secure passphrase. will generate if necessary
+     * @param bool   $overwrite                 - prevents accidental overwriting of an existing key which would be bad
+     * @return bool
+     */
+    public function addEncryptionKey($encryption_key_identifier, $encryption_key = '', $overwrite = false);
+
+
+    /**
+     * returns cryptographically secure passphrase. will use default if necessary
+     *
+     * @param string $encryption_key_identifier - used for saving encryption key. will use default if necessary
+     * @param string $generate                  - will generate a new key if the requested one does not exist
+     * @return string
+     */
+    public function getEncryptionKey($encryption_key_identifier = '', $generate = false);
+
+
+    /**
+     * creates a new encryption key
+     *
+     * @param int $length number of characters for random string
+     * @return string
+     */
+    public function generateEncryptionKey($length = 64);
+}
