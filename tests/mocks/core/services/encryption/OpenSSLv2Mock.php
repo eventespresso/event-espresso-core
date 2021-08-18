01da39ab5300442649448fcd48a11745f6715e4a
Brent Christensen <brent@eventespresso.com>
2021-08-17 19:33:26 -0700

    mock shells (will add more later)

---
+++ tests/mocks/core/services/encryption/OpenSSLv2Mock.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EventEspresso\tests\mocks\core\services\encryption;
+
+use EventEspresso\core\services\encryption\openssl\OpenSSLv2;
+
+class OpenSSLv2Mock extends OpenSSLv2
+{
+
+}
