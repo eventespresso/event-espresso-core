8dbe27a4b7c360c7c55404dc4cbf367fbdd226da
Brent Christensen <brent@eventespresso.com>
2021-11-25 13:55:54 -0800

    add constants & getEventId(); throw exceptions

--- modules/ticket_selector/ProcessTicketSelectorPostData.php
+++ modules/ticket_selector/ProcessTicketSelectorPostData.php
@@ -15,57 +15,108 @@ use EventEspresso\core\services\request\RequestInterface;
  */
 class ProcessTicketSelectorPostData
 {
+
+    const DATA_KEY_EVENT_ID      = 'id';
+
+    const DATA_KEY_MAX_ATNDZ     = 'max_atndz';
+
+    const DATA_KEY_QUANTITY      = 'qty';
+
+    const DATA_KEY_RETURN_URL    = 'return_url';
+
+    const DATA_KEY_ROWS          = 'rows';
+
+    const DATA_KEY_TICKET_ID     = 'ticket_id';
+
+    const DATA_KEY_TOTAL_TICKETS = 'total_tickets';
+
+    const INPUT_KEY_EVENT_ID    = 'tkt-slctr-event-id';
+
+    const INPUT_KEY_MAX_ATNDZ    = 'tkt-slctr-max-atndz-';
+
+    const INPUT_KEY_ROWS         = 'tkt-slctr-rows-';
+
+    const INPUT_KEY_QTY          = 'tkt-slctr-qty-';
+
+    const INPUT_KEY_TICKET_ID    = 'tkt-slctr-ticket-id-';
+
+    const INPUT_KEY_RETURN_URL   = 'tkt-slctr-return-url-';
+
+
     /**
      * @var int
      */
-    private $event_id;
+    protected $event_id;
 
     /**
      * @var array
      */
-    private $inputs_to_clean = [];
+    protected $inputs_to_clean = [];
+
+    /**
+     * @var array
+     */
+    protected $valid_data = [];
 
     /**
      * @var RequestInterface
      */
-    private $request;
-
-    /**
-     * @var array
-     */
-    private $valid_data = [];
+    protected $request;
 
 
     /**
-     * @param int              $event_id
      * @param RequestInterface $request
      */
-    public function __construct(int $event_id, RequestInterface $request)
+    public function __construct(RequestInterface $request)
     {
-        if (! absint($event_id)) {
-            throw new DomainException(esc_html__('The event id provided was not valid.', 'event_espresso'));
-        }
-        $this->event_id        = $event_id;
         $this->request         = $request;
         $this->inputs_to_clean = [
-            'max_atndz'  => 'tkt-slctr-max-atndz-',
-            'rows'       => 'tkt-slctr-rows-',
-            'qty'        => 'tkt-slctr-qty-',
-            'ticket_id'  => 'tkt-slctr-ticket-id-',
-            'return_url' => 'tkt-slctr-return-url-',
+            self::DATA_KEY_MAX_ATNDZ     => self::INPUT_KEY_MAX_ATNDZ,
+            self::DATA_KEY_ROWS          => self::INPUT_KEY_ROWS,
+            self::DATA_KEY_QUANTITY      => self::INPUT_KEY_QTY,
+            self::DATA_KEY_TOTAL_TICKETS => self::INPUT_KEY_TICKET_ID,
+            self::DATA_KEY_RETURN_URL    => self::INPUT_KEY_RETURN_URL,
         ];
     }
 
 
     /**
+     * @return int
+     * @throws DomainException
+     */
+    public function getEventId(): int
+    {
+        // do we have an event id?
+        if ($this->event_id === null) {
+            $this->event_id = $this->request->getRequestParam(self::INPUT_KEY_EVENT_ID, 0, 'int');
+            if (! $this->event_id) {
+                // $_POST['tkt-slctr-event-id'] was not set ?!?!?!?
+                throw new DomainException(
+                    sprintf(
+                        esc_html__(
+                            'An event id was not provided or was not received.%sPlease click the back button on your browser and try again.',
+                            'event_espresso'
+                        ),
+                        '<br/>'
+                    )
+                );
+            }
+        }
+        // event id is valid
+        return $this->event_id;
+    }
+
+
+    /**
      * @return array
+     * @throws DomainException
      */
     public function validatePostData(): array
     {
         // grab valid id
-        $this->valid_data['id'] = $this->event_id;
+        $this->valid_data[ self::DATA_KEY_EVENT_ID ] = $this->getEventId();
         // let's track the total number of tickets ordered.'
-        $this->valid_data['total_tickets'] = 0;
+        $this->valid_data[ self:: DATA_KEY_TOTAL_TICKETS ] = 0;
         // cycle through $inputs_to_clean array
         foreach ($this->inputs_to_clean as $what => $input_to_clean) {
             $input_key = "{$input_to_clean}{$this->event_id}";
@@ -73,35 +124,46 @@ class ProcessTicketSelectorPostData
             if ($this->request->requestParamIsSet($input_key)) {
                 switch ($what) {
                     // integers
-                    case 'event_id':
-                    case 'rows':
-                    case 'max_atndz':
-                        $this->valid_data[ $what ] = $this->request->getRequestParam($input_key, 0, 'int');
+                    case self::DATA_KEY_ROWS:
+                    case self::DATA_KEY_MAX_ATNDZ:
+                        $this->processInteger($what, $input_key);
                         break;
                     // arrays of integers
-                    case 'qty':
+                    case self::DATA_KEY_QUANTITY:
                         $this->processQuantity($input_key);
                         break;
                     // array of integers
-                    case 'ticket_id':
-                        $this->processTicketID($input_key);
+                    case self::DATA_KEY_TOTAL_TICKETS:
+                        $this->processTicketIDs($input_key);
                         break;
-                    case 'return_url':
+                    case self::DATA_KEY_RETURN_URL:
                         $this->processReturnURL($input_key);
                         break;
-                }    // end switch $what
+                }
             }
-        }    // end foreach $this->inputs_to_clean
+        }
         return $this->valid_data;
     }
 
 
     /**
+     * @param string $what
      * @param string $input_key
+     */
+    protected function processInteger(string $what, string $input_key)
+    {
+        $this->valid_data[ $what ] = $this->request->getRequestParam($input_key, 0, 'int');
+    }
+
+
+    /**
+     * @param string $input_key
+     * @throws DomainException
      */
     protected function processQuantity(string $input_key)
     {
-        $max_atndz = $this->valid_data['max_atndz'] ?? $this->request->getRequestParam($input_key, 0, 'int');
+        // first get number of ticket option rows
+        $rows = $this->request->getRequestParam(self::INPUT_KEY_ROWS . $this->event_id, 1, 'int');
         $raw_qty = $this->request->getRequestParam($input_key);
         // explode integers by the dash if qty is a string
         $delimiter = is_string($raw_qty) && strpos($raw_qty, '-') ? '-' : '';
@@ -109,8 +171,6 @@ class ProcessTicketSelectorPostData
         $row_qty = $this->request->getRequestParam($input_key, [], 'int', true, $delimiter);
         // if qty is coming from a radio button input, then we need to assemble an array of rows
         if ($delimiter === '-') {
-            // get number of rows
-            $rows = $this->request->getRequestParam('tkt-slctr-rows-' . $this->event_id, 1, 'int');
             $row  = isset($row_qty[0]) ? absint($row_qty[0]) : 1;
             $qty  = isset($row_qty[1]) ? absint($row_qty[1]) : 0;
             // restructure the row qty array so that $row is now the key instead of the first value
@@ -121,27 +181,24 @@ class ProcessTicketSelectorPostData
                 }
             }
         }
+        if (empty($row_qty) || ! is_array($row_qty) || $rows !== count($row_qty)) {
+            throw new DomainException(
+                sprintf(
+                    esc_html__(
+                        'An error occurred while trying to retrieve the ticket selections for the event.%sPlease click the back button on your browser and try again.',
+                        'event_espresso'
+                    ),
+                    '<br/>'
+                )
+            );
+        }
         ksort($row_qty);
         // cycle thru values
         foreach ($row_qty as $qty) {
             $qty = absint($qty);
             // sanitize as integers
-            $this->valid_data['qty'][]         = $qty;
-            $this->valid_data['total_tickets'] += $qty;
-        }
-    }
-
-
-    /**
-     * @param string $input_key
-     */
-    protected function processTicketID(string $input_key)
-    {
-        $ticket_ids = (array) $this->request->getRequestParam($input_key, [], 'int', true);
-        // cycle thru values
-        foreach ($ticket_ids as $key => $value) {
-            // allow only integers
-            $this->valid_data['ticket_id'][ $key ] = absint($value);
+            $this->valid_data[ self::DATA_KEY_QUANTITY ][]     = $qty;
+            $this->valid_data[ self:: DATA_KEY_TOTAL_TICKETS ] += $qty;
         }
     }
 
@@ -161,6 +218,33 @@ class ProcessTicketSelectorPostData
             // use event list url instead, but append anchor
             $input_value = EEH_Event_View::event_archive_url() . '#' . $input_value;
         }
-        $this->valid_data['return_url'] = $input_value;
+        $this->valid_data[ self::DATA_KEY_RETURN_URL ] = $input_value;
+    }
+
+
+    /**
+     * @param string $input_key
+     * @throws DomainException
+     */
+    protected function processTicketIDs(string $input_key)
+    {
+        $ticket_ids = (array) $this->request->getRequestParam($input_key, [], 'int', true);
+        $filtered_ticket_ids = array_filter($ticket_ids);
+        if (empty($filtered_ticket_ids)) {
+            throw new DomainException(
+                sprintf(
+                    esc_html__(
+                        'An error occurred while trying to retrieve the ticket IDs for the event.%sPlease click the back button on your browser and try again.',
+                        'event_espresso'
+                    ),
+                    '<br/>'
+                )
+            );
+        }
+        // cycle thru values
+        foreach ($ticket_ids as $key => $value) {
+            // allow only integers
+            $this->valid_data[ self::DATA_KEY_TICKET_ID ][ $key ] = absint($value);
+        }
     }
 }
