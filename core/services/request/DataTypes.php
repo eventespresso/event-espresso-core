cd2758fdfe10fc98ed13d9e0fe4fde0b97aee617
Brent Christensen <brent@eventespresso.com>
2022-01-20 17:10:21 -0800

    DataTypes Constants for defining data types within EE's Request related classes

---
+++ core/services/request/DataTypes.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EventEspresso\core\services\request;
+
+/**
+ * Class DataTypes
+ * Constants for defining data types used within EE's Request related classes
+ *
+ * @author  Brent Christensen
+ * @package EventEspresso\core\services\request
+ * @since   $VID:$
+ */
+class DataTypes
+{
+    const BOOL   = 'bool';
+
+    const FLOAT  = 'float';
+
+    const FQCN   = 'fqcn';
+
+    const HTML   = 'html';
+
+    const INT    = 'int';
+
+    const KEY    = 'key';
+
+    const TITLE  = 'title';
+
+    const URL    = 'url';
+
+    const STRING = 'string';
+
+}
