7a36d5afc8d14cbd7bcbed0583523a2efdce382f
Brent Christensen <brent@eventespresso.com>
2022-02-07 17:12:44 -0800

    fix button classes

--- caffeinated/admin/extend/events/Extend_Events_Admin_Page.core.php
+++ caffeinated/admin/extend/events/Extend_Events_Admin_Page.core.php
@@ -307,7 +307,7 @@ class Extend_Events_Admin_Page extends Events_Admin_Page
                        . $href
                        . '" title="'
                        . $title
-                       . '" id="ee-duplicate-event-button" class="button button--secondary"  value="duplicate_event">'
+                       . '" id="ee-duplicate-event-button" class="button button--small button--secondary"  value="duplicate_event">'
                        . $title
                        . '</a>';
         }
