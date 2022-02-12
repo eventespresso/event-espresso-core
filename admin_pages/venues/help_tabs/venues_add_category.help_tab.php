c8cc479b566afb794a0bcdfbf67480e03ca19b0d
Hossein Rafiei <hoseinrafiei@gmail.com>
2022-02-12 12:19:58 +0330

    Removed help tours from admin venues menu.

--- admin_pages/venues/help_tabs/venues_add_category.help_tab.php
+++ admin_pages/venues/help_tabs/venues_add_category.help_tab.php
@@ -25,14 +25,6 @@
     </li>
 </ul>
 <p>
-    <strong><?php esc_html_e('Recommendations', 'event_espresso'); ?></strong>
-    <br />
-    <?php esc_html_e(
-        'Want to see a tour of this screen? Click on the Add New Venue Category Tour button which appears on the right side of the page.',
-        'event_espresso'
-    ); ?>
-</p>
-<p>
     <strong><?php esc_html_e('Screen Options', 'event_espresso'); ?></strong>
     <br />
     <?php esc_html_e(
