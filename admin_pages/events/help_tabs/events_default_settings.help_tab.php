c2494219cb9168b5f9a1da4790b7303be2d40c17
Hossein Rafiei <hoseinrafiei@gmail.com>
2022-02-18 19:04:29 +0330

    Removed help tours from admin events menu. (#3733)

--- admin_pages/events/help_tabs/events_default_settings.help_tab.php
+++ admin_pages/events/help_tabs/events_default_settings.help_tab.php
@@ -3,13 +3,6 @@
     <?php esc_html_e('Customize the default settings for your events.', 'event_espresso'); ?>
 </p>
 <p>
-    <strong><?php esc_html_e('Recommendations', 'event_espresso'); ?></strong><br />
-    <?php esc_html_e(
-        'Want to see a tour of this screen? Click on the Default Settings Tour button which appears on the right side of the page.',
-        'event_espresso'
-    ); ?>
-</p>
-<p>
     <strong><?php esc_html_e('Screen Options', 'event_espresso'); ?></strong><br />
     <?php esc_html_e(
         'You can customize the information that is shown on this page by toggling the Screen Options tab. Then you can add or remove checkmarks to hide or show certain content.',
