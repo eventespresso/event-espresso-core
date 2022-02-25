3cfb8a41abe8835976d5211f8d9fe0769b00ce29
Brent Christensen <brent@eventespresso.com>
2022-02-24 17:31:16 -0800

    update attendee admin templates

--- admin_pages/registrations/templates/attendee_details_after_title_form_fields.template.php
+++ admin_pages/registrations/templates/attendee_details_after_title_form_fields.template.php
@@ -6,28 +6,31 @@ EEH_Template_Validator::verify_instanceof($attendee, '$attendee', 'EE_Attendee')
 ?>
     <div id="titlediv">
         <div id="titlewrap">
-            <label class="hidden" id="attendee-first-name-text" for="ATT_fname">
-                <?php esc_html_e('First Name:', 'event_espresso'); ?>
-            </label>
-            <input class='smaller-text-field'
-                   id="ATT_fname"
-                   name="ATT_fname"
-                   placeholder="<?php esc_html_e('First Name', 'event_espresso'); ?>"
-                   required
-                   type="text"
-                   value="<?php echo esc_attr($attendee->get('ATT_fname')); ?>"
-            />
-            <label class="hidden" id="attendee-first-name-text" for="ATT_lname">
-                <?php esc_html_e('Last Name:', 'event_espresso'); ?>
-            </label>
-            <input class='smaller-text-field'
-                   id="ATT_lname"
-                   name="ATT_lname"
-                   placeholder="<?php esc_html_e('Last Name', 'event_espresso'); ?>"
-                   type="text"
-                   value="<?php echo esc_attr($attendee->get('ATT_lname')); ?>"
-            />
-            <div style="clear:both"></div>
+            <div>
+                <label id='attendee-first-name-text' for='ATT_fname'>
+                    <?php esc_html_e('First Name:', 'event_espresso'); ?>
+                </label>
+                <input class='smaller-text-field'
+                       id="ATT_fname"
+                       name="ATT_fname"
+                       placeholder="<?php esc_html_e('First Name', 'event_espresso'); ?>"
+                       required
+                       type="text"
+                       value="<?php echo esc_attr($attendee->get('ATT_fname')); ?>"
+                />
+            </div>
+            <div>
+                <label id='attendee-first-name-text' for='ATT_lname'>
+                    <?php esc_html_e('Last Name:', 'event_espresso'); ?>
+                </label>
+                <input class='smaller-text-field'
+                       id="ATT_lname"
+                       name="ATT_lname"
+                       placeholder="<?php esc_html_e('Last Name', 'event_espresso'); ?>"
+                       type="text"
+                       value="<?php echo esc_attr($attendee->get('ATT_lname')); ?>"
+                />
+            </div>
         </div>
     </div>
 <?php
