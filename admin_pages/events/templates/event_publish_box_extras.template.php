07890faf56671a0554c68a42c4f23ee252bde034
Brent Christensen <brent@eventespresso.com>
2022-03-08 16:17:41 -0800

    improve pub box layout and style

--- admin_pages/events/templates/event_publish_box_extras.template.php
+++ admin_pages/events/templates/event_publish_box_extras.template.php
@@ -10,38 +10,47 @@
  */
 ?>
 <div class="misc-pub-section">
-    <span class="dashicons dashicons-groups ee-icon-color-ee-green ee-icon-size-20"></span>
-    <a href="<?php echo esc_url_raw($view_approved_reg_url); ?>" target="_blank">
+    <span class="dashicons dashicons-groups ee-status-color--RAP"></span>
+    <a class='ee-reg-list-link ee-status-color--RAP'
+       href="<?php echo esc_url_raw($view_approved_reg_url); ?>"
+       target="_blank"
+    >
+        <span class='ee-reg-list-link__reg-count'><?php echo absint($approved_regs); ?></span>
         <?php printf(
             esc_html__('%s Registrations', 'event_espresso'),
             EEH_Template::pretty_status(EEM_Registration::status_id_approved, false, 'sentence')
         ); ?>
+        <span class='dashicons dashicons-external'></span>
     </a>
-    :
-    <?php echo absint($approved_regs); ?>
 </div>
 <div class="misc-pub-section">
-    <span class="dashicons dashicons-groups ee-icon-color-ee-blue ee-icon-size-20"></span>
-    <a href="<?php echo esc_url_raw($view_pending_payment_reg_url); ?>" target='_blank'>
+    <span class="dashicons dashicons-groups ee-status-color--RPP"></span>
+    <a class='ee-reg-list-link ee-status-color--RPP'
+       href="<?php echo esc_url_raw($view_pending_payment_reg_url); ?>"
+       target='_blank'
+    >
+        <span class='ee-reg-list-link__reg-count'><?php echo absint($pending_payment_regs); ?></span>
         <?php printf(
             esc_html__('%s Registrations', 'event_espresso'),
             EEH_Template::pretty_status(EEM_Registration::status_id_pending_payment, false, 'sentence')
         ); ?>
+        <span class='dashicons dashicons-external'></span>
     </a>
-    :
-    <?php echo absint($pending_payment_regs); ?>
 </div>
 <div class="misc-pub-section">
-    <span class="dashicons dashicons-groups ee-icon-color-ee-orange ee-icon-size-20"></span>
-    <a href="<?php echo esc_url_raw($view_not_approved_reg_url); ?>" target='_blank'>
+    <span class="dashicons dashicons-groups ee-status-color--RNA"></span>
+    <a class='ee-reg-list-link ee-status-color--RNA'
+       href="<?php echo esc_url_raw($view_not_approved_reg_url); ?>"
+       target='_blank'
+    >
+        <span class='ee-reg-list-link__reg-count'><?php echo absint($not_approved_regs); ?></span>
         <?php printf(
             esc_html__('%s Registrations', 'event_espresso'),
             EEH_Template::pretty_status(EEM_Registration::status_id_not_approved, false, 'sentence')
         ); ?>
+        <span class='dashicons dashicons-external'></span>
     </a>
-    :
-    <?php echo absint($not_approved_regs); ?>
+
 </div>
 
 <?php echo $event_editor_overview_add; ?>
-<br />
