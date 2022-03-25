97e58109ebed6b9dfb3efaf5769d07947c750254
Brent Christensen <brent@eventespresso.com>
2022-03-25 13:29:58 -0700

    Fix Legacy Editor Ticket Saving (#3777)
    Co-authored-by: Hossein Rafiei <hoseinrafiei@gmail.com>

--- caffeinated/admin/new/pricing/templates/event_tickets_datetime_ticket_datetimes_list_item.template.php
+++ caffeinated/admin/new/pricing/templates/event_tickets_datetime_ticket_datetimes_list_item.template.php
@@ -14,11 +14,11 @@
 
 <li class="datetime-ticket clickable<?php echo $ticket_datetime_selected; ?><?php echo $tkt_status_class; ?>"
     data-datetime-row="<?php echo absint($dtt_row); ?>" data-context="ticket-datetime"
-    data-ticket-row="<?php echo absint($tkt_row); ?>">
-    <input type="checkbox" name="ticket_datetime[<?php echo absint($dtt_row); ?>][<?php echo absint($tkt_row); ?>]"
+    data-ticket-row="<?php echo esc_attr($tkt_row); ?>">
+    <input type="checkbox" name="ticket_datetime[<?php echo absint($dtt_row); ?>][<?php echo esc_attr($tkt_row); ?>]"
            class="datetime-ticket-checkbox" value="1"<?php echo $ticket_datetime_checked; ?>>
     <span class="dashicons dashicons-clock ee-icon-size-20"></span><span
         class="ticket-list-ticket-name"><?php echo $DTT_name; ?></span>
     <span class="clickable gear-icon dashicons dashicons-admin-generic" data-datetime-row="<?php echo absint($dtt_row); ?>"
-          data-context="ticket-datetime" data-ticket-row="<?php echo absint($tkt_row); ?>"></span>
+          data-context="ticket-datetime" data-ticket-row="<?php echo esc_attr($tkt_row); ?>"></span>
 </li>
