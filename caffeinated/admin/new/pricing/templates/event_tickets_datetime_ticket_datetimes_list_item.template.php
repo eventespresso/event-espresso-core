0c0bbaf5c7d6a2cbcbeacef742273a9895157346
Hossein Rafiei <hoseinrafiei@gmail.com>
2022-03-29 15:37:28 +0430

    Fixed a conflict with master.

--- caffeinated/admin/new/pricing/templates/event_tickets_datetime_ticket_datetimes_list_item.template.php
+++ caffeinated/admin/new/pricing/templates/event_tickets_datetime_ticket_datetimes_list_item.template.php
@@ -18,14 +18,14 @@
     data-ticket-row="<?php echo esc_attr($tkt_row); ?>"
 >
     <input type="checkbox"
-           name="ticket_datetime[<?php echo esc_attr($dtt_row); ?>][<?php echo esc_attr($tkt_row); ?>]"
-           class="datetime-ticket-checkbox"
-           value="1"<?php echo $ticket_datetime_checked; ?>
+        name="ticket_datetime[<?php echo esc_attr($dtt_row); ?>][<?php echo esc_attr($tkt_row); ?>]"
+        class="datetime-ticket-checkbox"
+        value="1"<?php echo $ticket_datetime_checked; ?>
     />
     <span class="dashicons dashicons-clock ee-icon-size-20"></span>
     <span class="ticket-list-ticket-name"><?php echo $DTT_name; ?></span>
     <span class="clickable gear-icon dashicons dashicons-admin-generic"
-          data-datetime-row="<?php echo esc_attr($dtt_row); ?>"
-          data-context="ticket-datetime"
-          data-ticket-row="<?php echo esc_attr($tkt_row); ?>"></span>
+        data-datetime-row="<?php echo esc_attr($dtt_row); ?>"
+        data-context="ticket-datetime"
+        data-ticket-row="<?php echo esc_attr($tkt_row); ?>"></span>
 </li>
