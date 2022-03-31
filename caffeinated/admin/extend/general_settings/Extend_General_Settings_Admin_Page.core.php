2e5a766ecd4f18592c2f6f5350cb1f61ccdf3e4f
Brent Christensen <brent@eventespresso.com>
2022-03-30 17:18:48 -0700

    fix data type + formatting

--- caffeinated/admin/extend/general_settings/Extend_General_Settings_Admin_Page.core.php
+++ caffeinated/admin/extend/general_settings/Extend_General_Settings_Admin_Page.core.php
@@ -25,16 +25,13 @@ class Extend_General_Settings_Admin_Page extends General_Settings_Admin_Page
 
     protected function _extend_page_config()
     {
-
         $this->_admin_base_path = EE_CORE_CAF_ADMIN_EXTEND . 'general_settings';
 
         // filters and action hooks here
-        add_action('AHEE__admin_option_settings__template__before', array($this, 'debug_logging_options'), 9);
+        add_action('AHEE__admin_option_settings__template__before', [$this, 'debug_logging_options'], 9);
         add_filter(
             'FHEE__General_Settings_Admin_Page___update_admin_option_settings__CFG_admin',
-            array($this, 'update_debug_logging_options'),
-            10,
-            1
+            [$this, 'update_debug_logging_options']
         );
     }
 
@@ -49,14 +46,17 @@ class Extend_General_Settings_Admin_Page extends General_Settings_Admin_Page
      *
      * @return void
      */
-    public function debug_logging_options($template_args = array())
+    public function debug_logging_options(array $template_args = [])
     {
-        $template_args['use_remote_logging'] = isset(EE_Registry::instance()->CFG->admin->use_remote_logging) ? absint(
-            EE_Registry::instance()->CFG->admin->use_remote_logging
-        ) : false;
+        $template_args['use_remote_logging'] = isset(EE_Registry::instance()->CFG->admin->use_remote_logging)
+            ? absint(EE_Registry::instance()->CFG->admin->use_remote_logging)
+            : false;
+
         $template_args['remote_logging_url'] = isset(EE_Registry::instance()->CFG->admin->remote_logging_url)
                                                && ! empty(EE_Registry::instance()->CFG->admin->remote_logging_url)
-            ? stripslashes(EE_Registry::instance()->CFG->admin->remote_logging_url) : '';
+            ? stripslashes(EE_Registry::instance()->CFG->admin->remote_logging_url)
+            : '';
+
         $template = GEN_SET_CAF_TEMPLATE_PATH . 'debug_log_settings.template.php';
         EEH_Template::display_template($template, $template_args);
     }
@@ -65,18 +65,19 @@ class Extend_General_Settings_Admin_Page extends General_Settings_Admin_Page
     /**
      * update_debug_logging_options
      *
-     * @param array $admin_options
+     * @param EE_Admin_Config $admin_options
      *
-     * @return array
+     * @return EE_Admin_Config
      */
-    public function update_debug_logging_options($admin_options = array())
+    public function update_debug_logging_options(EE_Admin_Config $admin_options): EE_Admin_Config
     {
-        $admin_options->use_remote_logging = isset($this->_req_data['use_remote_logging']) ? absint(
-            $this->_req_data['use_remote_logging']
-        ) : $admin_options->use_remote_logging;
-        $admin_options->remote_logging_url = isset($this->_req_data['remote_logging_url']) ? esc_url_raw(
-            $this->_req_data['remote_logging_url']
-        ) : $admin_options->remote_logging_url;
+        $admin_options->use_remote_logging = isset($this->_req_data['use_remote_logging'])
+            ? absint($this->_req_data['use_remote_logging'])
+            : $admin_options->use_remote_logging;
+
+        $admin_options->remote_logging_url = isset($this->_req_data['remote_logging_url'])
+            ? esc_url_raw($this->_req_data['remote_logging_url'])
+            : $admin_options->remote_logging_url;
 
         return $admin_options;
     }
