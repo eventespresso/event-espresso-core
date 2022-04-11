f99e722fffa2b5037d817c028e70e7db9ec527bd
Brent Christensen <brent@eventespresso.com>
2022-04-11 14:17:42 -0700

    fix critical shortcodes page selector

--- admin_pages/general_settings/General_Settings_Admin_Page.core.php
+++ admin_pages/general_settings/General_Settings_Admin_Page.core.php
@@ -1380,7 +1380,7 @@ class General_Settings_Admin_Page extends EE_Admin_Page
             }
         }
         if ($echo) {
-            echo wp_kses($output, AllowedTags::getAllowedTags());
+            echo wp_kses($output, AllowedTags::getWithFormTags());
             return '';
         }
         return $output;
