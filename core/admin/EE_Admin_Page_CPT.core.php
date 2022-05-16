1a98ba0f0046bb192a4d0be53cd58c0b505eefc7
Hossein Rafiei <hoseinrafiei@gmail.com>
2022-05-16 15:51:28 +0430

    Kses - Part 9 (#3831)
    * Escaped some variables.
    * Fixed a html markup issue.

--- core/admin/EE_Admin_Page_CPT.core.php
+++ core/admin/EE_Admin_Page_CPT.core.php
@@ -5,6 +5,7 @@ use EventEspresso\core\exceptions\InvalidInterfaceException;
 use EventEspresso\core\services\loaders\LoaderFactory;
 use EventEspresso\core\services\loaders\LoaderInterface;
 use EventEspresso\core\services\request\middleware\RecommendedVersions;
+use EventEspresso\core\services\request\sanitizers\AllowedTags;
 
 /**
  * EE_Admin_Page_CPT class
@@ -351,7 +352,7 @@ abstract class EE_Admin_Page_CPT extends EE_Admin_Page
         // inject our Admin page nav tabs...
         // let's make sure the nav tabs are set if they aren't already
         // if ( empty( $this->_nav_tabs ) ) $this->_set_nav_tabs();
-        add_action('post_edit_form_tag', [$this, 'inject_nav_tabs']);
+        add_action('edit_form_top', [$this, 'inject_nav_tabs']);
         // modify the post_updated messages array
         add_action('post_updated_messages', [$this, 'post_update_messages'], 10);
         // add shortlink button to cpt edit screens.  We can do this as a universal thing BECAUSE,
@@ -1345,14 +1346,7 @@ abstract class EE_Admin_Page_CPT extends EE_Admin_Page
      */
     public function inject_nav_tabs()
     {
-        // can we hijack and insert the nav_tabs?
-        $nav_tabs = $this->_get_main_nav_tabs();
-        // first close off existing form tag
-        $html = '>';
-        $html .= $nav_tabs;
-        // now let's handle the remaining tag ( missing ">" is CORRECT )
-        $html .= '<span></span';
-        echo $html;  // already escaped
+        echo wp_kses($this->_get_main_nav_tabs(), AllowedTags::getWithFormTags());
     }
 
 
@@ -1484,7 +1478,7 @@ abstract class EE_Admin_Page_CPT extends EE_Admin_Page
         $admin_page = ! empty($this->_req_data['post']) ? 'post-php' : 'post-new-php';
         ?>
         <script type="text/javascript">
-            adminpage = '<?php echo $admin_page; ?>';
+            adminpage = '<?php echo esc_js($admin_page); ?>';
         </script>
         <?php
     }
