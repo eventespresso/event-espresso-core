96972809c06a3a696c4bc0ea9ba513035b915ee1
Hossein Rafiei <hoseinrafiei@gmail.com>
2022-05-24 18:51:21 +0430

    Added a new class to escape multiple attributes.

--- caffeinated/admin/new/pricing/templates/event_tickets_metabox_main.template.php
+++ caffeinated/admin/new/pricing/templates/event_tickets_metabox_main.template.php
@@ -17,6 +17,7 @@
  */
 
 use EventEspresso\core\services\request\sanitizers\AllowedTags;
+use EventEspresso\core\services\request\sanitizers\MultipleAttributes;
 
 ?>
 
@@ -140,7 +141,7 @@ use EventEspresso\core\services\request\sanitizers\AllowedTags;
                         'Available Tickets',
                         'event_espresso'
                     ); ?></span></h3>
-        <div class="event-tickets-container ee-create-ticket-button">
+        <div class="event-tickets-container ee-create-ticket-button" <?php echo MultipleAttributes::clean($show_tickets_container, AllowedTags::getAllowedTags()); ?>>
             <button class="ee-create-ticket-button button-secondary ee-create-button" data-context="ticket"><?php
                 esc_html_e(
                     'Create Ticket',
@@ -148,7 +149,7 @@ use EventEspresso\core\services\request\sanitizers\AllowedTags;
                 ); ?></button>
         </div>
         <div style="clear:both"></div>
-        <div class="event-tickets-container">
+        <div class="event-tickets-container" <?php echo MultipleAttributes::clean($show_tickets_container, AllowedTags::getAllowedTags()); ?>>
             <table class="ticket-table">
                 <thead>
                 <tr valign="top">
---
+++ core/services/request/sanitizers/MultipleAttributes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EventEspresso\core\services\request\sanitizers;
+
+/**
+ * Class MultipleAttributes
+ *
+ * @author  Hossein Rafiei
+ * @package EventEspresso\core\services\request\sanitizers
+ * @since   4.10.30
+ */
+class MultipleAttributes
+{
+    /**
+     * @param string $attributes
+     * @param array $allowed_tags
+     * @param string $tag
+     * @return mixed|string
+     */
+    public static function clean(string $attributes, array $allowed_tags, string $tag = 'div')
+    {
+        if (trim($attributes) === '') {
+            return '';
+        }
+        $html = '<' . $tag . ' ' . $attributes . '>';
+        $escaped = wp_kses($html, $allowed_tags);
+        $start = strpos($escaped, ' ');
+        $end = strpos($escaped, '>');
+        if ($start === false || $end === false) {
+            return '';
+        }
+        $length = $end - $start;
+        return trim(substr($escaped, $start, $length));
+    }
+}
