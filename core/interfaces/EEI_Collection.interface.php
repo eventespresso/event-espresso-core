e25e6c9f330a2350ec3311f810bac55b0f57e13d
Brent Christensen <brent@eventespresso.com>
2022-05-31 17:16:49 -0700

    add typing, code cleanup, & formatting

--- core/interfaces/EEI_Collection.interface.php
+++ core/interfaces/EEI_Collection.interface.php
@@ -14,67 +14,52 @@
 interface EEI_Collection
 {
     /**
-     * add
-     *
      * attaches an object to the Collection
      * and sets any supplied data associated with the current iterator entry
      * by calling EEI_Collection::set_info()
      *
-     * @access public
      * @param object $object
-     * @param mixed  $info
+     * @param array|int|string $info
      * @return bool
      */
-    public function add($object, $info = null);
+    public function add($object, $info = ''): bool;
 
 
 
     /**
-     * set_info
-     *
      * Sets the info associated with an object in the Collection
      *
-     * @access public
-     * @param object $object
-     * @param mixed  $info
+     * @param object           $object
+     * @param array|int|string $info
      * @return bool
      */
-    public function set_info($object, $info = null);
+    public function set_info($object, $info = ''): bool;
 
 
 
     /**
-     * get_by_info
-     *
      * finds and returns an object in the Collection based on the info that was set using set_info() or add()
      *
-     * @access public
-     * @param mixed
+     * @param array|int|string $info
      * @return null | object
      */
-    public function get_by_info($info);
+    public function get_by_info($info = '');
 
 
 
     /**
-     * has
-     *
      * returns TRUE or FALSE depending on whether the supplied object is within the Collection
      *
-     * @access public
      * @param object $object
      * @return bool
      */
-    public function has($object);
+    public function has($object): bool;
 
 
 
     /**
-     * remove
-     *
      * detaches an object from the Collection
      *
-     * @access public
      * @param $object
      * @return void
      */
