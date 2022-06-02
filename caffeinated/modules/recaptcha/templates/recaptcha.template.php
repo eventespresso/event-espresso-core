325ef52572d8262e080cb2c2f70ef07f959d13ef
Nazar Kolivoshka <nazar@eventespresso.com>
2022-06-02 14:16:01 +0300

    Revert "remove fallback recaptcha area ?"
    This reverts commit 9ad3234ba94ca0d46c4ef8f40fbd9851ad15a936.

--- caffeinated/modules/recaptcha/templates/recaptcha.template.php
+++ caffeinated/modules/recaptcha/templates/recaptcha.template.php
@@ -10,8 +10,26 @@ if (! defined('EVENT_ESPRESSO_VERSION')) {
     <div id="espresso-recaptcha-dv">
         <div class="g-recaptcha" data-sitekey="<?php echo esc_attr($recaptcha_publickey); ?>"
              data-theme="<?php echo esc_attr($recaptcha_theme); ?>" data-type="<?php echo esc_attr($recaptcha_type); ?>"
-             data-callback="espresso_recaptcha_verification">
-        </div>
+             data-callback="espresso_recaptcha_verification"></div>
+        <noscript>
+            <div style="width: 302px; height: 352px;">
+                <div style="width: 302px; height: 352px; position: relative;">
+                    <div style="width: 302px; height: 352px; position: absolute;">
+                        <iframe
+                            src="https://www.google.com/recaptcha/api/fallback?k=<?php echo esc_attr($recaptcha_publickey); ?>"
+                            frameborder="0" scrolling="no" style="width: 302px; height:352px; border-style: none;">
+                        </iframe>
+                    </div>
+                    <div
+                        style="width: 250px; height: 80px; position: absolute; border-style: none; bottom: 21px; left: 25px; margin: 0; padding: 0; right: 25px;">
+            <textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response"
+                      style="width: 250px; height: 80px; border: 1px solid #c1c1c1; margin: 0; padding: 0; resize: none;"
+                      title="">
+            </textarea>
+                    </div>
+                </div>
+            </div>
+        </noscript>
     </div>
 <?php
 // End of file recaptcha.template.php
