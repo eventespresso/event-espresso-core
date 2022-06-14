ab79f66fcc411150efa0e1cb75ddf1718d1138f2
EE DevBox Server <admin@eventespresso.com>
2022-06-14 04:59:02 -0700

    Bumping version to 4.10.31.p and prepped for release

--- assets/src/components/form/model/select/index.js
+++
@@ -1,4 +0,0 @@
-export { default as EventSelect, EditorEventSelect } from './event';
-export { default as DatetimeSelect, EditorDatetimeSelect } from './datetime';
-export { default as StatusSelect, EditorStatusSelect } from './status';
-export { default as TicketSelect, EditorTicketSelect } from './ticket';
