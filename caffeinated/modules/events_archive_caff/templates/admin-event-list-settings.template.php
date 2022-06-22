37ab8469405daa2ba4360c40b06eecc0d2c68b02
Majid Abbasi <majidabbasi@Majids-MacBook-Air.local>
2022-06-22 22:16:07 +0400

    Add span wrapper to the base url

--- caffeinated/modules/events_archive_caff/templates/admin-event-list-settings.template.php
+++ caffeinated/modules/events_archive_caff/templates/admin-event-list-settings.template.php
@@ -56,8 +56,10 @@ $event_archive_url = EEH_Event_View::event_archive_url();
             </label>
         </th>
         <td>
-            <?php echo site_url() . '/ '
-                      . EEH_Form_Fields::text(
+            <span class="base-url"><?php 
+                echo site_url() . '/ ';
+            ?></span>
+            <?php echo EEH_Form_Fields::text(
                           'not_used',
                           EE_Registry::instance()->CFG->core->event_cpt_slug,
                           'event_cpt_slug',
