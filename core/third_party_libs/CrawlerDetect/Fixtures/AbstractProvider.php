d8c0be4d858765b0898f9ca99c22119964f43395
Brent Christensen <brent@eventespresso.com>
2022-08-03 11:29:27 -0700

    move CrawlerDetect up a folder

--- core/third_party_libs/CrawlerDetect/Fixtures/AbstractProvider.php
+++ core/third_party_libs/CrawlerDetect/Fixtures/AbstractProvider.php
@@ -8,7 +8,7 @@
  * with this source code in the file LICENSE.
  */
 
-namespace EventEspressoVendor\Jaybizzle\CrawlerDetect\Fixtures;
+namespace EventEspressoVendor\CrawlerDetect\Fixtures;
 
 /**
  * Class AbstractProvider
