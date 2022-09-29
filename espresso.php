05d3d971578fd69294731a4a7250167e5da5fed6
Tony Warwick <Pebblo@hotmail.com>
2022-09-29 17:39:42 +0100

    Set min PHP required version to be 7.2.0

--- espresso.php
+++ espresso.php
@@ -61,7 +61,7 @@ if (function_exists('espresso_version')) {
     }
     add_action('admin_notices', 'espresso_duplicate_plugin_error', 1);
 } else {
-    define('EE_MIN_PHP_VER_REQUIRED', '5.6.2');
+    define('EE_MIN_PHP_VER_REQUIRED', '7.2.0');
     if (! version_compare(PHP_VERSION, EE_MIN_PHP_VER_REQUIRED, '>=')) {
         /**
          * espresso_minimum_php_version_error
