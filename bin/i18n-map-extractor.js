2273548a2e317bfd2d5d3da1de8e6b3cd809a5cc
tn3rb <brent@eventespresso.com>
2023-03-15 09:43:31 -0700

    modules is a Set not an Array

--- bin/i18n-map-extractor.js
+++ bin/i18n-map-extractor.js
@@ -1,22 +1,16 @@
-const recast = require( 'recast' );
-const { readFileSync, writeFileSync } = require( 'fs' );
-const NormalModule = require('webpack/lib/NormalModule');
-const { forEach, reduce, includes, isEmpty, isFunction } = require( 'lodash' );
+import recast from 'recast';
+import { readFileSync, writeFileSync } from 'fs';
+import NormalModule from '../node_modules/webpack/lib/NormalModule.js';
+import lodashPkg from 'lodash';
+const { includes, isEmpty, isFunction } = lodashPkg;
 
 class wpi18nExtractor {
-
 	/**
 	 * @function constructor
 	 * @param options object
 	 */
-	constructor( options ) {
-		const DEFAULT_FUNCTIONS = [
-			'__',
-			'_n',
-			'_x',
-			'_nx',
-			'sprintf',
-		];
+	constructor(options) {
+		const DEFAULT_FUNCTIONS = ['__', '_n', '_x', '_nx', 'sprintf'];
 		this.options = options || {};
 		this.options.aliases = this.options.aliases || {};
 		this.options.filename = this.options.filename || 'translation-map.json';
@@ -25,30 +19,22 @@ class wpi18nExtractor {
 		this.functionNames = this.options.functionNames || DEFAULT_FUNCTIONS;
 	}
 
-
 	/**
 	 * @function apply
 	 * @param compiler object
 	 * @returns void
 	 */
-	apply( compiler ) {
+	apply = (compiler) => {
 		const { processChunks } = this;
 		const extractor = this;
-		if ( compiler.hasOwnProperty( 'hooks' ) ) {
-			compiler.hooks.thisCompilation.tap(
-				'webpack-i18n-map-extractor',
-				compilation => {
-					compilation.hooks.optimizeChunks.tap(
-						'webpack-i18n-map-extractor',
-						chunks => {
-							processChunks( chunks, extractor );
-						},
-					);
-				},
-			);
+		if (compiler.hasOwnProperty('hooks')) {
+			compiler.hooks.thisCompilation.tap('webpack-i18n-map-extractor', (compilation) => {
+				compilation.hooks.optimizeChunks.tap('webpack-i18n-map-extractor', (chunks) => {
+					processChunks(chunks, extractor);
+				});
+			});
 		}
-	}
-
+	};
 
 	/**
 	 * @function processChunks
@@ -56,72 +42,80 @@ class wpi18nExtractor {
 	 * @param extractor object
 	 * @returns void
 	 */
-	processChunks(chunks, extractor) {
+	processChunks = (chunks, extractor) => {
 		const { options, translationMap, parseSourcesToMap } = extractor;
-		let chunkName, finalMap;
-		forEach( chunks, function( chunk ) {
-			// only process if chunk.name is available and not in the list of
-			// chunks to exclude
-			if ( chunk.name && options.excludes.indexOf( chunk.name ) === -1 ) {
+		// console.log('');
+		// console.log('wpi18nExtractor::processChunks()');
+		// console.log('%c chunks', 'color: Pink;', chunks);
+		// console.log('%c options', 'color: HotPink;', options);
+		let finalMap;
+		chunks.forEach(function (chunk) {
+			// only process if chunk.name is available and not in the list of chunks to exclude
+			if (chunk.name && options.excludes.indexOf(chunk.name) === -1) {
+				console.log('%c chunk', 'color: LimeGreen;', chunk.name);
 				// get chunk.name from alias if it exists
-				chunkName = options.aliases.hasOwnProperty( chunk.name ) ?
-					options.aliases[ chunk.name ] :
-					chunk.name;
+				const chunkName = options.aliases.hasOwnProperty(chunk.name) ? options.aliases[chunk.name] : chunk.name;
 				if (chunk.hasOwnProperty('_groups')) {
+					console.log('%c chunk hasOwnProperty _groups', 'color: LimeGreen;');
 					const [firstGroup] = chunk._groups;
+
 					if (firstGroup.hasOwnProperty('_modulePreOrderIndices')) {
-						Array.from(
-							firstGroup._modulePreOrderIndices,
-							function (module) {
-								parseSourcesToMap(module, chunkName, extractor );
-							}
-						);
+						// clone set then loop over elements and parse sources
+						const modulePreOrderIndices = new Set(firstGroup._modulePreOrderIndices);
+						modulePreOrderIndices.forEach(function (module) {
+							parseSourcesToMap(module, chunkName, extractor);
+						});
 					}
 				}
 			}
-		} );
+		});
 		//get existing json and merge
 		try {
-			finalMap = JSON.parse( readFileSync( './' + options.filename ) );
-		} catch ( e ) {
+			finalMap = JSON.parse(readFileSync('./' + options.filename));
+		} catch (e) {
 			finalMap = {};
 		}
-		finalMap = Object.assign( {}, finalMap, translationMap );
-		writeFileSync( './' + options.filename,
-			JSON.stringify( finalMap, null, 2 ),
-			'utf-8',
-		);
-	}
-
+		finalMap = Object.assign({}, finalMap, translationMap);
+		writeFileSync('./' + options.filename, JSON.stringify(finalMap, null, 2), 'utf-8');
+	};
 
 	/**
 	 * @function parseSourcesToMap
-	 * @param modules object
+	 * @param source Set
 	 * @param chunkName string
 	 * @param extractor object
 	 * @returns void
 	 */
-	parseSourcesToMap( modules, chunkName, extractor ) {
+	parseSourcesToMap = (source, chunkName, extractor) => {
+		console.log('');
+		console.log('wpi18nExtractor::parseSourcesToMap()');
 		const { getStringsFromModule } = extractor;
-		reduce(
-			Array.from( modules ),
-			function( mapped, module ) {
-				if ( ! ( module instanceof NormalModule ) || ! isFunction( module.originalSource )) {
-					return mapped;
+		console.error('chunkName', chunkName);
+		source.forEach(
+			// function (mapped, module) {
+			(module) => {
+				if (!(module instanceof NormalModule) || !isFunction(module.originalSource)) {
+					return;
 				}
-				if ( ! mapped.hasOwnProperty( chunkName ) ) {
-					mapped[ chunkName ] = [];
+				if (!extractor.translationMap.hasOwnProperty(chunkName)) {
+					extractor.translationMap[chunkName] = [];
 				}
-				const translations = getStringsFromModule( module, extractor );
-				if (! isEmpty( translations ) ) {
-					mapped[ chunkName ] = mapped[ chunkName ].concat(translations);
+				const translations = getStringsFromModule(module, extractor);
+				console.log('');
+				console.log('%c translations', 'color: HotPink;', translations);
+				if (!isEmpty(translations)) {
+					extractor.translationMap[chunkName] = extractor.translationMap[chunkName].concat(translations);
+					console.log('*******************************************************');
+					console.log(
+						'%c extractor.translationMap[chunkName]',
+						'color: LimeGreen;',
+						extractor.translationMap[chunkName]
+					);
+					console.log('*******************************************************');
 				}
-				return mapped;
-			},
-			extractor.translationMap,
+			}
 		);
-	}
-
+	};
 
 	/**
 	 * @function getStringsFromModule
@@ -129,44 +123,39 @@ class wpi18nExtractor {
 	 * @param extractor object
 	 * @returns array
 	 */
-	getStringsFromModule( module, extractor ) {
+	getStringsFromModule = (module, extractor) => {
 		const originalSource = module.originalSource();
-		if (! isFunction( originalSource.source )) {
+		if (!isFunction(originalSource?.source)) {
 			return [];
 		}
 		const source = originalSource.source();
-		if ( isEmpty( source ) ) {
+		if (isEmpty(source)) {
 			return [];
 		}
 		let translations = [];
 		const { parse, types } = recast;
 		try {
-			const ast = parse( source );
-			types.visit( ast, {
-				visitCallExpression: function( path ) {
+			const ast = parse(source);
+			types.visit(ast, {
+				visitCallExpression: function (path) {
 					const node = path.node;
-					if ( includes( extractor.functionNames,
-						node.callee.name,
-					) &&
-						node.arguments
-					) {
+					if (includes(extractor.functionNames, node.callee.name) && node.arguments) {
 						const translation = extractor.extractStringFromFunctionCall(
-							types.getFieldValue( node, 'arguments' ),
-							node.callee.name,
+							types.getFieldValue(node, 'arguments'),
+							node.callee.name
 						);
-						if (! isEmpty( translation ) ) {
+						if (!isEmpty(translation)) {
 							translations = translations.concat(translation);
 						}
 					}
-					this.traverse( path );
+					this.traverse(path);
 				},
-			} );
-		} catch ( e ) {
+			});
+		} catch (e) {
 			//we just want to skip parsing errors.
 		}
 		return translations;
-	}
-
+	};
 
 	/**
 	 * @function extractStringFromFunctionCall
@@ -174,19 +163,19 @@ class wpi18nExtractor {
 	 * @param functionName string
 	 * @returns string
 	 */
-	extractStringFromFunctionCall( args, functionName ) {
-		switch ( functionName ) {
+	extractStringFromFunctionCall = (args, functionName) => {
+		switch (functionName) {
 			case '__':
 			case 'sprintf':
 			case '_n':
-				return args[ 0 ].value;
+				return args[0].value;
 			case '_x':
-				return args[ 1 ].value + '\u0004' + args[ 0 ].value;
+				return args[1].value + '\u0004' + args[0].value;
 			case '_nx':
-				return args[ 3 ].value + '\u0004' + args[ 0 ].value;
+				return args[3].value + '\u0004' + args[0].value;
 		}
 		return '';
-	}
+	};
 }
 
-module.exports = wpi18nExtractor;
+export default wpi18nExtractor;
