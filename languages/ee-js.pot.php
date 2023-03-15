d9ccce32060d1a114a29efdb467594586e2e6ec5
EE DevBox Server <admin@eventespresso.com>
2023-03-15 14:18:27 -0700

    Bumping version to 5.0.0.rc.027. Previous Commit message: "Merge branch 'EE5Update' "

---
+++ languages/ee-js.pot.php
@@ -0,0 +1,307 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: assets/src/blocks/event-attendees/edit.js:279
+	__( 'To get started, select what event you want to show attendees from in the block settings.', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:290
+	__( 'There are no attendees for selected options.', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:334
+	__( 'Filter By Settings', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:362
+	__( 'Select Registration Status', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:368
+	__( 'Number of Attendees to Display:', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:377
+	_n_noop( 'Used to adjust the number of attendees displayed (There is %d total attendee for the current filter settings).', 'Used to adjust the number of attendees displayed (There are %d total attendees for the current filter settings).', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:387
+	__( 'Order Attendees by:', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:391
+	__( 'Attendee id', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:395
+	__( 'Last name only', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:399
+	__( 'First name only', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:403
+	__( 'First, then Last name', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:410
+	__( 'Last, then First name', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:420
+	__( 'Sort order:', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:424
+	__( 'Ascending', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:428
+	__( 'Descending', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:435
+	__( 'Gravatar Setttings', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:437
+	__( 'Display Gravatar', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:442
+	__( 'Gravatar images are shown for each attendee.', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:446
+	__( 'No gravatar images are shown for each attendee.', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:454
+	__( 'Size of Gravatar', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:463
+	__( 'Archive Settings', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:465
+	__( 'Display on Archives', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:470
+	__( 'Attendees are shown whenever this post is listed in an archive view.', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/edit.js:474
+	__( 'Attendees are hidden whenever this post is listed in an archive view.', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/index.js:20
+	__( 'Event Attendees', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/index.js:22
+	__( 'Displays a list of people that have registered for the specified event', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/index.js:32
+	__( 'event', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/index.js:33
+	__( 'attendees', 'event_espresso' ),
+
+	// Reference: assets/src/blocks/event-attendees/index.js:34
+	__( 'list', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/base/default-select-configuration.js:97
+	__( 'Select…', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/datetime.js:31
+	__( 'Retrieving Datetimes.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/datetime.js:32
+	__( 'No Datetimes.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/datetime.js:33
+	__( 'Select Datetime…', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/datetime.js:37
+	__( 'Select Datetime', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/event.js:24
+	__( 'Retrieving Events.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/event.js:25
+	__( 'No Events.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/event.js:26
+	__( 'Select Event…', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/event.js:30
+	__( 'Select Event', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/status.js:29
+	__( 'Retrieving Statuses.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/status.js:30
+	__( 'No Statuses.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/status.js:31
+	__( 'Select Status…', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/status.js:35
+	__( 'Select Status', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/ticket.js:25
+	__( 'Retrieving Tickets.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/ticket.js:26
+	__( 'No Tickets.', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/ticket.js:27
+	__( 'Select Ticket…', 'event_espresso' ),
+
+	// Reference: assets/src/components/form/model/select/ticket.js:31
+	__( 'Select Ticket', 'event_espresso' ),
+
+	// Reference: assets/src/components/query/limit/index.js:12
+	__( 'Limit', 'event_espresso' ),
+
+	// Reference: assets/src/components/ui/image/avatar.js:36
+	__( 'contact avatar', 'event_espresso' ),
+
+	// Reference: assets/src/data/eventespresso/core/model/checkin/actions.js:88
+	__( 'Toggling the checkin failed. Usually this is due to the checkin not having access', 'event_espresso' ),
+
+	// Reference: assets/src/data/eventespresso/lists/selectors.js:100
+	__( 'The given identifier (%s) does not exist in the state.', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/assertions.js:111
+	__( 'The provided item must be a Map object', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/assertions.js:21
+	__( 'The provided entity (%s) does not have the given property (%s)', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/assertions.js:54
+	__( 'The provided immutable object (%s) does not have the given path (%s)', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/assertions.js:77
+	__( 'The provided value is not an array.', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/assertions.js:95
+	__( 'The provided items must not be empty', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/primary-keys.js:109
+	__( 'The provided array of entities must not be empty', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/primary-keys.js:135
+	__( 'The provided object of entities must be a Map object', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:102
+	__( 'sent', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:105
+	__( 'queued for sending', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:108
+	__( 'failed', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:111
+	__( 'debug only', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:114
+	__( 'messenger is executing', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:117
+	__( 'queued for resending', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:120
+	__( 'queued for generating', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:123
+	__( 'failed sending, can be retried', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:134
+	__( 'published', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:137
+	__( 'scheduled', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:140
+	__( 'draft', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:143
+	__( 'pending', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:146
+	__( 'private', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:149
+	__( 'trashed', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:180
+	__( 'archived', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:192
+	__( 'on sale', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:203
+	__( 'cancelled', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:206
+	__( 'sold out', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:209
+	__( 'expired', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:212
+	__( 'inactive', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:215
+	__( 'upcoming', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:218
+	__( 'active', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:221
+	__( 'postponed', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:232
+	__( 'check-in', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:233
+	__( 'check-ins', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:236
+	__( 'check-out', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:237
+	__( 'check-outs', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:24
+	__( 'pending payment', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:240
+	__( 'never checked in', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:25
+	__( 'pending payments', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:260
+	__( 'unknown', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:28
+	__( 'approved', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:31
+	__( 'not approved', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:43
+	__( 'wait list', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:44
+	__( 'wait lists', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:56
+	__( 'overpaid', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:59
+	__( 'complete', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:62
+	__( 'incomplete', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:68
+	__( 'abandoned', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:79
+	__( 'accepted', 'event_espresso' ),
+
+	// Reference: assets/src/data/model/status/helpers.js:88
+	__( 'declined', 'event_espresso' ),
+
+	// Reference: assets/src/exit-modal-survey/index.js:40
+	__( 'Do you have a moment to share why you are deactivating Event Espresso?', 'event_espresso' ),
+
+	// Reference: assets/src/exit-modal-survey/index.js:44
+	__( 'Sure I\'ll help', 'event_espresso' ),
+
+	// Reference: assets/src/exit-modal-survey/index.js:45
+	__( 'Skip', 'event_espresso' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
